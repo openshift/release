ab23ab4ba0a12274357752f96a6480a1c2ce1b9f
Clayton Coleman <ccoleman@redhat.com>
2016-12-14 08:32:09 -0500

    Add k8s-robot-test

--- cluster/ci/up.sh
+++ cluster/ci/up.sh
@@ -10,13 +10,13 @@ docker start -a gce-ci
 # source data/config.sh
 # oc login "https://${MASTER_DNS_NAME}" # has to be through UI
 # grant cluster admin to user
-# oc apply -f setup/roles.yaml
 # set up router certificate
+# add self-provisioner to "self-provisioners" group
 # oc project ci
 # oc secrets new origin-gce data -o yaml > setup/secrets.yaml
+# oc apply -f config/roles.yaml
+# oc replace --force -f config/route-docker-registry.yaml -n default
 # oadm policy remove-cluster-role-from-group self-provisioner system:authenticated:oauth
-# add self-provisioner to "self-provisioners" group
 # oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:kube-system:heapster
-# oc process -f setup/heapster-standalone.yaml | oc apply -f - -n kube-system
-# change registry DNS name to registry.svc.ci.openshift.org
+# oc process -f config/heapster-standalone.yaml | oc apply -f - -n kube-system
 # set env var COCKPIT_KUBE_URL=https://registry-console-default.svc.ci.openshift.org
