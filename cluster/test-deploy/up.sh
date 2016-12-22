f84d8539d22615042566cd8c5da1dd09c8388163
Clayton Coleman <ccoleman@redhat.com>
2016-12-22 01:03:13 -0500

    Parameterize up by arguments

--- cluster/test-deploy/up.sh
+++ cluster/test-deploy/up.sh
@@ -2,19 +2,27 @@
 
 set -euo pipefail
 
-docker rm gce-pr || true
-docker create --name gce-pr -e STARTUP_SCRIPT_FILE=/usr/local/install/data/startup.sh openshift/origin-gce:latest $@
-docker cp data gce-pr:/usr/local/install
-docker start -a gce-pr
+build=$1
+data=$2
+url=$3
 
-# source data/config.sh
-# oc login "https://${MASTER_DNS_NAME}" # has to be through UI
-# grant cluster admin to user
-# oc apply -f setup/roles.yaml
-# set up router certificate
-# oc project ci
-# oc secrets new origin-gce data -o yaml > setup/secrets.yaml
-# oadm policy remove-cluster-role-from-group self-provisioner system:authenticated:oauth
-# add self-provisioner to "self-provisioners" group
-# change registry DNS name to registry.svc.ci.openshift.org
-# set env var COCKPIT_KUBE_URL=https://registry-console-default.svc.ci.openshift.org
+startup="$( mktemp -d )/startup.sh"
+cat << STARTUP > "${startup}"
+#!/bin/bash
+set -euo pipefail
+
+cat << EOF > /etc/yum.repos.d/origin-override.repo
+[origin-override]
+baseurl = "${url}"
+gpgcheck = 0
+name = OpenShift Origin Release
+enabled = 1
+EOF
+STARTUP
+
+docker rm gce-pr &>/dev/null || true
+docker create --name gce-pr -e STARTUP_SCRIPT_FILE=/usr/local/install/data/startup.sh openshift/origin-gce:latest ansible-gce -e "pull_identifier=pr${build}" "playbooks/provision.yaml" >/dev/null
+docker cp "${data}" gce-pr:/usr/local/install
+docker cp "${startup}" gce-pr:/usr/local/install/data/
+rm "${startup}"
+docker start -a gce-pr
