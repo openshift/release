d4a0fbc8dbb374331a04051fb4a5ec382ed2bf64
Clayton Coleman <ccoleman@redhat.com>
2017-01-02 16:34:46 -0500

    Switch to using environment for main PR parameters

--- cluster/test-deploy/up.sh
+++ cluster/test-deploy/up.sh
@@ -12,21 +12,6 @@ if [[ -z "${3-}" && -z "${4-}" ]]; then
   playbook="playbooks/deprovision.yaml"
 fi
 
-# configure the startup script to use the provided repository URL for binaries
-startup="$( mktemp -d )/startup.sh"
-cat << STARTUP > "${startup}"
-#!/bin/bash
-set -euo pipefail
-
-cat << EOF > /etc/yum.repos.d/origin-override.repo
-[origin-override]
-baseurl = "${url}"
-gpgcheck = 0
-name = OpenShift Origin Release
-enabled = 1
-EOF
-STARTUP
-
 # start a container with the custom playbook inside it
 docker rm gce-pr-$build &>/dev/null || true
 args=""
@@ -35,12 +20,10 @@ if [[ -n "${OPENSHIFT_ANSIBLE_REPO-}" ]]; then
   docker volume create --name gce-pr-$build-volume >/dev/null
   args="-v gce-pr-$build-volume:/usr/share/ansible/openshift-ansible "
 fi
-docker create --name gce-pr-$build $args openshift/origin-gce:latest ansible-gce -e "pull_identifier=pr${build}" "${@:5}" "${playbook}" >/dev/null
-docker cp "${data}" gce-pr-$build:/usr/local/install
+docker create -e "PR_NUMBER=pr${build}" -e "PR_REPO_URL=${url}" --name gce-pr-$build $args openshift/origin-gce:latest ansible-playbook "${@:5}" "${playbook}" >/dev/null
+tar -c -C "${data}" . | docker cp - gce-pr-$build:/usr/share/ansible/openshift-ansible-gce/playbooks/files
 if [[ -n "${OPENSHIFT_ANSIBLE_REPO-}" ]]; then
-  docker cp "${OPENSHIFT_ANSIBLE_REPO}/" gce-pr-$build:/usr/share/ansible/
+  tar -c -C "${OPENSHIFT_ANSIBLE_REPO}" . | docker cp - gce-pr-$build:/usr/share/ansible/openshift-ansible/
 fi
-docker cp "${startup}" gce-pr-$build:/usr/local/install/data/
-rm "${startup}"
 docker start -a gce-pr-$build
-docker cp gce-pr-$build:/usr/local/install/data/admin.kubeconfig admin.kubeconfig &>/dev/null || true
+docker cp gce-pr-$build:/tmp/admin.kubeconfig admin.kubeconfig &>/dev/null || true
