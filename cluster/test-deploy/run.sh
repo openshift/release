5766eb668464cce97d39397a56901a77bb62176b
Clayton Coleman <ccoleman@redhat.com>
2017-01-06 14:13:05 -0500

    React to changes in base origin-gce; make run generic

--- cluster/test-deploy/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-docker rm gce-pr &>/dev/null || true
-docker create $@ --name gce-pr -it openshift/origin-gce:latest /bin/bash >/dev/null
-tar -c -C data . | docker cp - gce-pr:/usr/share/ansible/openshift-ansible-gce/playbooks/files
-docker start -ai gce-pr
