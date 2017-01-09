8aadd14768356599af227efb406380979156ffdd
Clayton Coleman <ccoleman@redhat.com>
2017-01-09 12:40:38 -0500

    Ensure ownership is correct on mount

--- cluster/bin/run.sh
+++ cluster/bin/run.sh
@@ -3,8 +3,9 @@
 set -euo pipefail
 
 ctr=gce-cluster
+opts="--mode='ug+rwX' --owner=root --group=root"
 
 docker rm $ctr &>/dev/null || true
 docker create $@ --name $ctr -it openshift/origin-gce:latest /bin/bash >/dev/null
-tar --mode='ug+rwX' -c -C data . | docker cp - $ctr:/usr/share/ansible/openshift-ansible-gce/playbooks/files
+tar ${opts} -c -C data . | docker cp - $ctr:/usr/share/ansible/openshift-ansible-gce/playbooks/files
 docker start -ai $ctr
