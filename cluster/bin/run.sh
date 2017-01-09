864e1907a7d5483a99d8673983d44cb87339c822
Clayton Coleman <ccoleman@redhat.com>
2017-01-09 14:22:13 -0500

    Stop container before deleting

--- cluster/bin/run.sh
+++ cluster/bin/run.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 ctr=gce-cluster
 opts="--mode=ug+rwX --owner=0 --group=0"
 
+docker stop $ctr &>/dev/null || true
 docker rm $ctr &>/dev/null || true
 docker create $@ --name $ctr -it openshift/origin-gce:latest /bin/bash >/dev/null
 tar ${opts} -c -C data . | docker cp - $ctr:/usr/share/ansible/openshift-ansible-gce/playbooks/files
