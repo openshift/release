49ec9835e329a5fb2f84ff6c6ef5143332e1c511
Cesar Wong <cewong@redhat.com>
2017-09-18 10:46:06 -0400

    Allow specifying Origin Source URL in Origin CI Pipelines

--- tools/release-ci/pkg/pullrefs/cloneref.go
+++ tools/release-ci/pkg/pullrefs/cloneref.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 // Runner is an object that knows how to keep track of a list
@@ -21,21 +22,26 @@ func CloneRef(ref SourceRef, dir string, existing bool, runner Runner) error {
 		return fmt.Errorf("a repository URL must be specified")
 	}
 
+	branch := ref.Branch
+	remote := "origin"
 	if !existing {
 		runner.Add(func() error { return os.MkdirAll(dir, 0644) })
 		runner.AddCmd("", "git", "clone", ref.RepositoryURL, dir)
 	} else {
-		runner.AddCmd(dir, "git", "fetch", "origin")
+		runner.AddCmd(dir, "git", "remote", "add", "cloneremote", ref.RepositoryURL)
+		runner.AddCmd(dir, "git", "fetch", "cloneremote")
+		branch = fmt.Sprintf("cloneremote/%s", ref.Branch)
+		remote = "cloneremote"
 	}
 	if len(ref.BranchCommit) > 0 {
 		runner.AddCmd(dir, "git", "checkout", ref.BranchCommit)
 	} else {
 		if len(ref.Branch) > 0 {
-			runner.AddCmd(dir, "git", "checkout", ref.Branch)
+			runner.AddCmd(dir, "git", "checkout", branch)
 		}
 	}
 	for _, prRef := range ref.PullRefs {
-		runner.AddCmd(dir, "git", "fetch", "origin", fmt.Sprintf("pull/%d/head", prRef.Number))
+		runner.AddCmd(dir, "git", "fetch", remote, fmt.Sprintf("pull/%d/head", prRef.Number))
 		runner.AddCmd(dir, "git", "merge", prRef.Commit)
 	}
 	return runner.Run()
@@ -65,6 +71,7 @@ func (r *execRunner) Add(f func() error) {
 
 func (r *execRunner) AddCmd(dir, name string, args ...string) {
 	r.Add(func() error {
+		fmt.Fprintf(r.stdOut, "executing: %s %s\n", name, strings.Join(args, " "))
 		cmd := exec.Command(name, args...)
 		cmd.Stdout = r.stdOut
 		cmd.Stderr = r.stdErr
