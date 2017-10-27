e27c0f95afe5402426b5c661c253a7e95bc5b475
Steve Kuznetsov <skuznets@redhat.com>
2017-10-27 10:08:32 -0700

    Make the cloneref tool more idempotent and generic
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- tools/release-ci/cmd/cloneref.go
+++ tools/release-ci/cmd/cloneref.go
@@ -1,10 +1,10 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 
 	"github.com/spf13/cobra"
 
@@ -13,10 +13,10 @@ import (
 
 // clonerefCmd clones a PULL_REFS specification
 var clonerefCmd = &cobra.Command{
-	Use:   "cloneref REPOSITORY_URL [PULL_REFS]",
-	Short: "Clones a PULL_REFS specification from Prow into the specified directory",
-	Long: `Clones a PULL_REFS specification from Prow into the specified directory
-	
+	Use:   "cloneref",
+	Short: "Clones a PULL_REFS specification from Prow into the computed directory",
+	Long: `Clones a PULL_REFS specification from Prow into the computed directory
+
 PULL_REFS is expected to be in the form of:
 
 base_branch:commit_sha_of_base_branch,pull_request_no:commit_sha_of_pull_request_no,...
@@ -25,65 +25,53 @@ For example:
 master:97d901d,4:bcb00a1
 
 And for multiple pull requests that have been batched:
-master:97d901d,4:bcb00a1,6:ddk2tka
-
-If PULL_REFS is not specified as an argument, the command will try to find it as
-an environment variable.
-
-The destination directory may be specified with --destination-dir. If not specified,
-the current working directory will be used.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		if err := cloneRefs(args); err != nil {
+master:97d901d,4:bcb00a1,6:ddk2tka`,
+	Run: func(cmd *cobra.Command, _ []string) {
+		if err := cloneRefs(); err != nil {
 			fmt.Printf("Error: %v\n", err)
 			os.Exit(1)
 		}
 	},
 }
 
-var cloneDestinationDir string
+var srcRoot string
 
 func init() {
 	RootCmd.AddCommand(clonerefCmd)
-	clonerefCmd.Flags().StringVar(&cloneDestinationDir, "destination-dir", "", "The destination directory of the clone operation")
+	clonerefCmd.Flags().StringVar(&srcRoot, "src-root", "", "The root of source checkouts")
 }
 
-func cloneRefs(args []string) error {
+func cloneRefs() error {
 	// Check that git is available
 	if _, err := exec.LookPath("git"); err != nil {
-		return fmt.Errorf("git not found. Ensure that it is available and in the path")
+		return errors.New("git not found. Ensure that it is available and in the path")
 	}
 
-	if len(args) == 0 {
-		return fmt.Errorf("A repository URL must be specified as an argument")
-	}
-	repositoryURL := args[0]
-
-	pullRefs := getPullRefs(args[1:])
-	if len(pullRefs) == 0 {
-		return fmt.Errorf("PULL_REFS must be specified either as an argument or as an environment variable")
+	if len(srcRoot) < 0 {
+		return errors.New("a source root must be specified")
 	}
 
+	repoOwner, ok := os.LookupEnv("REPO_OWNER")
+	if !ok {
+		return errors.New("REPO_OWNER environment variable must be set")
+	}
+
+	repoName, ok := os.LookupEnv("REPO_NAME")
+	if !ok {
+		return errors.New("REPO_NAME environment variable must be set")
+	}
+	repositoryUrl := fmt.Sprintf("https://github.com/%s/%s.git", repoOwner, repoName)
+	cloneDir := fmt.Sprintf("%s/src/github.com/%s/%s", srcRoot, repoOwner, repoName)
+
+	pullRefs, ok := os.LookupEnv("PULL_REFS")
+	if !ok {
+		return errors.New("PULL_REFS environment variable must be set")
+	}
 	sourceRef, err := pullrefs.ParsePullRefs(pullRefs)
 	if err != nil {
 		return err
 	}
-	sourceRef.RepositoryURL = repositoryURL
+	sourceRef.RepositoryURL = repositoryUrl
 
-	if len(cloneDestinationDir) == 0 {
-		cloneDestinationDir, err = os.Getwd()
-		if err != nil {
-			return err
-		}
-	}
-
-	dirExists := false
-	_, err = os.Stat(cloneDestinationDir)
-	if err == nil {
-		_, err := os.Stat(filepath.Join(cloneDestinationDir, ".git"))
-		if err == nil {
-			dirExists = true
-		}
-	}
-
-	return pullrefs.CloneRef(*sourceRef, cloneDestinationDir, dirExists, pullrefs.NewExecRunner(os.Stdout, os.Stderr))
+	return pullrefs.CloneRef(*sourceRef, cloneDir, pullrefs.NewExecRunner(os.Stdout, os.Stderr))
 }
