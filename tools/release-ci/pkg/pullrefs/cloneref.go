9e49e8496ebde95c03bf7a9aee2dc098dc60e247
Steve Kuznetsov <skuznets@redhat.com>
2017-10-27 10:08:32 -0700

    Make the cloneref tool more idempotent and generic
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- tools/release-ci/pkg/pullrefs/cloneref.go
+++ tools/release-ci/pkg/pullrefs/cloneref.go
@@ -17,32 +17,14 @@ type Runner interface {
 }
 
 // CloneRef clones a SourceRef in the specified directory
-func CloneRef(ref SourceRef, dir string, existing bool, runner Runner) error {
-	if len(ref.RepositoryURL) == 0 {
-		return fmt.Errorf("a repository URL must be specified")
-	}
-
-	branch := ref.Branch
-	remote := "origin"
-	if !existing {
-		runner.Add(func() error { return os.MkdirAll(dir, 0644) })
-		runner.AddCmd("", "git", "clone", ref.RepositoryURL, dir)
-	} else {
-		runner.AddCmd(dir, "git", "remote", "add", "cloneremote", ref.RepositoryURL)
-		runner.AddCmd(dir, "git", "fetch", "cloneremote")
-		branch = fmt.Sprintf("cloneremote/%s", ref.Branch)
-		remote = "cloneremote"
-	}
-	if len(ref.BranchCommit) > 0 {
-		runner.AddCmd(dir, "git", "checkout", ref.BranchCommit)
-	} else {
-		if len(ref.Branch) > 0 {
-			runner.AddCmd(dir, "git", "checkout", branch)
-		}
-	}
+func CloneRef(ref SourceRef, cloneDest string, runner Runner) error {
+	runner.Add(func() error { return os.MkdirAll(cloneDest, 0755) })
+	runner.AddCmd(cloneDest, "git", "init")
+	runner.AddCmd(cloneDest, "git", "fetch", ref.RepositoryURL, ref.Branch)
+	runner.AddCmd(cloneDest, "git", "checkout", ref.BranchCommit)
 	for _, prRef := range ref.PullRefs {
-		runner.AddCmd(dir, "git", "fetch", remote, fmt.Sprintf("pull/%d/head", prRef.Number))
-		runner.AddCmd(dir, "git", "merge", prRef.Commit)
+		runner.AddCmd(cloneDest, "git", "fetch", ref.RepositoryURL, fmt.Sprintf("pull/%d/head", prRef.Number))
+		runner.AddCmd(cloneDest, "git", "merge", prRef.Commit)
 	}
 	return runner.Run()
 }
@@ -73,6 +55,7 @@ func (r *execRunner) AddCmd(dir, name string, args ...string) {
 	r.Add(func() error {
 		fmt.Fprintf(r.stdOut, "executing: %s %s\n", name, strings.Join(args, " "))
 		cmd := exec.Command(name, args...)
+		cmd.Dir = dir
 		cmd.Stdout = r.stdOut
 		cmd.Stderr = r.stdErr
 		return cmd.Run()
