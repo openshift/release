e937e87c66fef7a74707cf2234c80f47ad806279
Steve Kuznetsov <skuznets@redhat.com>
2017-12-04 14:48:43 -0800

    Update build pipeline to use newer API
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- jobs/openshift/openshift-ansible/build.groovy
+++ jobs/openshift/openshift-ansible/build.groovy
@@ -6,12 +6,12 @@ buildPipeline(
   "build",
   cloneStep("openshift-ansible:test-base"),
   [buildStep(
-    "build-tox",
-    "src", "tox",
-    "RUN umask 0002 && /usr/bin/tox --notest"
+    /* from */ "src",
+    /*  to  */ "tox",
+    /*  do  */ "RUN umask 0002 && /usr/bin/tox --notest"
   ),buildStep(
-    "build-rpms",
-    "src", "rpms",
-    "RUN umask 0002 && mkdir -p /srv/openshift-ansible/noarch && tito tag --offline --accept-auto-changelog && tito build --output=/srv/ --rpm --test --offline --quiet && createrepo /srv/openshift-ansible/noarch"
+    /* from */ "src",
+    /*  to  */ "rpms",
+    /*  do  */ "RUN umask 0002 && mkdir -p /srv/openshift-ansible/noarch && tito tag --offline --accept-auto-changelog && tito build --output=/srv/ --rpm --test --offline --quiet && createrepo /srv/openshift-ansible/noarch"
   )]
 )
