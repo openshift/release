edf4d689433250a0442856ebfd2bb5ebe30bf7ba
Michalis Kargakis <kargakis@protonmail.ch>
2018-03-05 19:05:41 +0100

    Fix permissions by cloning in /tmp

--- projects/acs-engine/run-tests.sh
+++ projects/acs-engine/run-tests.sh
@@ -5,10 +5,12 @@
 set -eux
 
 # Clone PR
+mkdir -p /tmp/go
+export GOPATH=/tmp/go
 # Unfortunately go get is broken for acs-engine
 # See https://github.com/Azure/acs-engine/issues/1160
 go get github.com/Azure/acs-engine || true
-cd /go/src/github.com/Azure/acs-engine
+cd /tmp/go/src/github.com/Azure/acs-engine
 git remote set-url origin https://github.com/kargakis/acs-engine
 git fetch origin pull/${PULL_NUMBER}/head
 git checkout FETCH_HEAD
