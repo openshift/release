7898f56867e28c3c2d017aca34592984c61b4513
Michalis Kargakis <kargakis@protonmail.ch>
2018-09-10 21:51:53 +0200

    Recurse in templates to pick up nested directories

--- ci-operator/populate-templates.sh
+++ ci-operator/populate-templates.sh
@@ -10,7 +10,7 @@ set -o pipefail
 
 templates="$( dirname "${BASH_SOURCE[0]}" )/templates"
 
-for templates_file in $( find "${templates}/" -mindepth 1 -maxdepth 1 -type f -name "*.yaml" ); do
+for templates_file in $( find "${templates}/" -mindepth 1 -type f -name "*.yaml" ); do
 	template_name="$( basename "${templates_file}" ".yaml" )"
 	oc create configmap "prow-job-${template_name}" "--from-file=${templates_file}" -o yaml --dry-run | oc apply -f -
 done
