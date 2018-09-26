c93cfe36ba11d2fdd596f58d9c1ebe8195076ec4
Steve Kuznetsov <skuznets@redhat.com>
2018-09-26 15:25:05 -0700

    Enable generation presubmit for openshift/cluster-capacity
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- hack/validate-generated-config.sh
+++ hack/validate-generated-config.sh
@@ -20,7 +20,6 @@ blacklist=(
 	"openshift/api"
 	"openshift/builder"
 	"openshift/cluster-api-provider-libvirt"
-	"openshift/cluster-capacity"
 	"openshift/cluster-kube-controller-manager-operator"
 	"openshift/cluster-kube-scheduler-operator"
 	"openshift/cluster-monitoring-operator"
