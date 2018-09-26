d61962bb2a2267bba1137e4250c402c57dbb6f9b
Steve Kuznetsov <skuznets@redhat.com>
2018-09-26 14:33:48 -0700

    Enable generation presubmit for openshift/service-ca-operator
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- hack/validate-generated-config.sh
+++ hack/validate-generated-config.sh
@@ -58,7 +58,6 @@ blacklist=(
 	"openshift/origin-web-console-server"
 	"openshift/prometheus-operator"
 	"openshift/release-controller"
-	"openshift/service-ca-operator"
 	"openshift/service-catalog"
 	"openshift/service-serving-cert-signer"
 	"openshift/telemeter"
