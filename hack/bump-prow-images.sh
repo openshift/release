cc1430b91b383ebc801f0d037751d26fc91d2633
Steve Kuznetsov <skuznets@redhat.com>
2018-11-30 14:33:47 -0800

    Bump to latest images
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- hack/bump-prow-images.sh
+++ hack/bump-prow-images.sh
@@ -13,7 +13,8 @@ latest_tag="$( gcloud container images list-tags gcr.io/k8s-prow/plank --format=
 sed -i "s|\(gcr\.io/k8s-prow/[a-z-]*:\)v[0-9][0-9]*-[a-z0-9][a-z0-9]*|\1${latest_tag}|g" $( find cluster/ci/config/prow/openshift -type f ) cluster/ci/config/prow/config.yaml ci-operator/jobs/infra-periodics.yaml
 
 # some images are in a different bucket and are pushed in a different cadence
+# and are not necessarily in order, so we need to do some silliness
 for image in 'label_sync' 'commenter'; do
-	latest_label_sync_tag="$( gcloud container images list-tags gcr.io/k8s-testimages/${image} --format='value(tags)' --limit 1 | grep -Po "v[0-9]+\-[a-z0-9]+" )"
+	latest_label_sync_tag="$( gcloud container images list-tags gcr.io/k8s-testimages/${image} --format='value(tags)' --limit 10 | grep "latest," | grep -Po "v[0-9]+\-[a-z0-9]+" )"
 	sed -i "s|\(gcr\.io/k8s-testimages/${image}:\)v[0-9][0-9]*-[a-z0-9][a-z0-9]*|\1${latest_tag}|g" ci-operator/jobs/infra-periodics.yaml
 done
