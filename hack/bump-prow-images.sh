9dedb2f1a367418b8b99473d04fbcfb7eebd3e79
Steve Kuznetsov <skuznets@redhat.com>
2018-11-30 07:45:04 -0800

    Add docs and better versions
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- hack/bump-prow-images.sh
+++ hack/bump-prow-images.sh
@@ -4,6 +4,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# upstream builds every single image from the test-infra repo on
+# every commit and pushes them to gcr.io/k8s-prow. If we look at
+# the latest tag on a known image we can grab every image at that
+# tag to pull our cluster to the latest
 latest_tag="$( gcloud container images list-tags gcr.io/k8s-prow/plank --format='value(tags)' --limit 1 | grep -Po "v[0-9]+\-[a-z0-9]+" )"
 
 sed -i "s|\(gcr\.io/k8s-prow/[a-z-]*:\)v[0-9][0-9]*-[a-z0-9][a-z0-9]*|\1${latest_tag}|g" $( find cluster/ci/config/prow/openshift -type f ) cluster/ci/config/prow/config.yaml
