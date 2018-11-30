2618e82a806f679472cc48206cfeb21e0d46f07c
Steve Kuznetsov <skuznets@redhat.com>
2018-11-30 14:02:28 -0800

    Some images will remain in k8s-testimages
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- hack/bump-prow-images.sh
+++ hack/bump-prow-images.sh
@@ -11,3 +11,9 @@ set -o pipefail
 latest_tag="$( gcloud container images list-tags gcr.io/k8s-prow/plank --format='value(tags)' --limit 1 | grep -Po "v[0-9]+\-[a-z0-9]+" )"
 
 sed -i "s|\(gcr\.io/k8s-prow/[a-z-]*:\)v[0-9][0-9]*-[a-z0-9][a-z0-9]*|\1${latest_tag}|g" $( find cluster/ci/config/prow/openshift -type f ) cluster/ci/config/prow/config.yaml ci-operator/jobs/infra-periodics.yaml
+
+# some images are in a different bucket and are pushed in a different cadence
+for image in 'label_sync' 'commenter'; do
+	latest_label_sync_tag="$( gcloud container images list-tags gcr.io/k8s-testimages/${image} --format='value(tags)' --limit 1 | grep -Po "v[0-9]+\-[a-z0-9]+" )"
+	sed -i "s|\(gcr\.io/k8s-testimages/${image}:\)v[0-9][0-9]*-[a-z0-9][a-z0-9]*|\1${latest_tag}|g" ci-operator/jobs/infra-periodics.yaml
+done
