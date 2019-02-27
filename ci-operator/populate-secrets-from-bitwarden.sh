e9a634f2556d7a994bb5166ff865a6fc0c936dbf
Bruno Barcarol Guimarães <bbarcaro@redhat.com>
2019-02-27 13:02:40 +0000

    bitwarden: add `--overwrite` to `oc label` calls

--- ci-operator/populate-secrets-from-bitwarden.sh
+++ ci-operator/populate-secrets-from-bitwarden.sh
@@ -102,7 +102,10 @@ function merge_pull_secrets() {
 }
 
 function update_secret() {
+    local name
+    name=$2
     oc create secret "$@" --dry-run -o yaml | oc apply -f -
+	oc label secret --overwrite "${name}" "ci.openshift.io/managed=true"
 }
 
 # retrieve the value of a field and format it as a string, for
@@ -117,7 +120,6 @@ function field_value() {
 # with the token recorded as the password for the account
 for master in "ci.openshift.redhat.com" "kata-jenkins-ci.westus2.cloudapp.azure.com"; do
 	update_secret generic "jenkins-credentials-${master}" "$( format_field "${master}" ".login.password" "password" )"
-	oc label secret "jenkins-credentials-${master}" "ci.openshift.io/managed=true"
 done
 
 # Client certificates for the ci.dev Jenkins
@@ -128,45 +130,37 @@ update_secret generic "jenkins-credentials-${master}"            \
 	"$( format_attachment "${master}" cert.pem )"                \
 	"$( format_attachment "${master}" key.pem )"                 \
 	"$( format_attachment "${master}" ca.pem )"
-oc label secret "jenkins-credentials-${master}" "ci.openshift.io/managed=true"
 
 # OAuth tokens for GitHub are stored as a text field named
 # "GitHub OAuth Token" on login credential items for each robot.
 for login in "openshift-bot" "openshift-build-robot" "openshift-cherrypick-robot" "openshift-ci-robot" "openshift-merge-robot" "openshift-publish-robot"; do
 	update_secret generic "github-credentials-${login}" "$( format_field_value "${login}" "GitHub OAuth Token" "oauth" )"
-	oc label secret "github-credentials-${login}" "ci.openshift.io/managed=true"
 done
 
 # Configuration for Slack ci-chat-bot is stored under "Token"
 # and the key value is "token" in the secret
 update_secret generic ci-chat-bot-slack-token "$( format_field_value ci-chat-bot-slack-token "Token" "token" )"
-oc label secret "ci-chat-bot-slack-token" "ci.openshift.io/managed=true"
 
 # Configuration for GitHub OAuth Apps are stored
 # as an opaque field "Client Configuration"
 update_secret generic github-app-credentials "$( format_field_value deck-ci.svc.ci.openshift.org "Client Configuration" "config.json" )"
-oc label secret "github-app-credentials" "ci.openshift.io/managed=true"
 
 # Cookie secret to encrypt frontend and backend
 # communication is stored in the "Cookie" field
 update_secret generic cookie "$( format_field_value deck-ci.svc.ci.openshift.org Cookie "cookie" )"
-oc label secret "cookie" "ci.openshift.io/managed=true"
 
 # HMAC token for encrypting GitHub webhook payloads
 # is stored in the "HMAC Token" field
 update_secret generic github-webhook-credentials "$( format_field_value hmac "HMAC Token" "hmac" )"
-oc label secret "github-webhook-credentials" "ci.openshift.io/managed=true"
 
 # DeploymentConfig token is used to auth trigger events
 # for DeploymentConfigs from GitHub
 update_secret generic github-deploymentconfig-trigger "$( format_field_value github-deploymentconfig-webhook-token "Token" "WebHookSecretKey" )"
-oc label secret "github-deploymentconfig-trigger" "ci.openshift.io/managed=true"
 
 # Credentials for GCE service accounts are stored
 # as an attachment on each distinct credential
 for account in "aos-pubsub-subscriber" "ci-vm-operator" "gcs-publisher"; do
 	update_secret generic "gce-sa-credentials-${account}" "$( format_attachment "${account}" credentials.json service-account.json )"
-	oc label secret "gce-sa-credentials-${account}" "ci.openshift.io/managed=true"
 done
 
 
@@ -176,23 +170,19 @@ for account in "aos-serviceaccount" "jenkins-ci-provisioner"; do
 		"$( format_attachment "${account}" credentials.json service-account.json )" \
 		"$( format_attachment "${account}" ssh-privatekey )"   \
 		"$( format_attachment "${account}" ssh-publickey )"
-	oc label secret "gce-sa-credentials-${account}" "ci.openshift.io/managed=true"
 done
 
 # Credentials for registries are stored as
 # separate fields on individual items
 for registry in "docker.io" "quay.io"; do
 	update_secret generic "registry-push-credentials-${registry}" $( format_field_value "${registry}" "Push Credentials" "config.json" )
-	oc label secret "registry-push-credentials-${registry}" "ci.openshift.io/managed=true"
 	# we want to be able to build and push out to registries
 	oc secrets link builder "registry-push-credentials-${registry}"
 done
 
 registry="quay.io"
 update_secret generic "registry-pull-credentials-${registry}" $( format_field_value "${registry}" "Pull Credentials" "config.json" )
-oc label secret "registry-pull-credentials-${registry}" "ci.openshift.io/managed=true"
 update_secret generic "ci-pull-credentials" --type=kubernetes.io/dockerconfigjson $( format_field_value "${registry}" "Pull Credentials" ".dockerconfigjson" )
-oc label secret "ci-pull-credentials" "ci.openshift.io/managed=true"
 
 # Cluster credentials aggregate multiple items
 # of information for easy consumption by tests
@@ -202,7 +192,6 @@ update_secret generic "cluster-secrets-${target_cloud}"                \
 	"$( format_attachment "jenkins-ci-iam" .awscred )"                   \
 	"$( format_attachment "jenkins-ci-iam" ssh-privatekey )"             \
 	"$( format_attachment "jenkins-ci-iam" ssh-publickey )"
-oc label secret "cluster-secrets-${target_cloud}" "ci.openshift.io/managed=true"
 
 target_cloud="gcp"
 update_secret generic "cluster-secrets-${target_cloud}"                       \
@@ -212,7 +201,6 @@ update_secret generic "cluster-secrets-${target_cloud}"                       \
 	"$( format_attachment "jenkins-ci-provisioner" ssh-publickey )"             \
 	"$( format_attachment "mirror.openshift.com" cert-key.pem ops-mirror.pem )" \
 	"$( format_field_value telemeter "Telemeter Token" "telemeter-token" )"
-oc label secret "cluster-secrets-${target_cloud}" "ci.openshift.io/managed=true"
 
 # Configuration for the .git-credentials used by the release controller to clone
 # private repositories to generate changelogs
