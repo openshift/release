b15dc5433af5ba64811544779e96266bebb85516
Dipak Pawar <dipakpawar231@gmail.com>
2019-05-21 04:59:09 +0530

    use registry-push secret to push to quay.io

--- ci-operator/populate-secrets-from-bitwarden.sh
+++ ci-operator/populate-secrets-from-bitwarden.sh
@@ -180,7 +180,7 @@ done
 
 # Credentials for registries are stored as
 # separate fields on individual items
-for registry in "docker.io" "quay.io" "quay.io/openshift-knative" "quay.io/openshiftio"; do
+for registry in "docker.io" "quay.io" "quay.io/openshift-knative"; do
 	update_secret generic "registry-push-credentials-${registry//\//\-}" $( format_field_value "${registry}" "Push Credentials" "config.json" )
 	# we want to be able to build and push out to registries
 	oc secrets link builder "registry-push-credentials-${registry//\//\-}"
