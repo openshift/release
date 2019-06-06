d5a049b33da2f8913b1db49dde1387d976c08f71
Bruno Barcarol Guimarães <bbarcaro@redhat.com>
2019-06-06 11:12:57 +0000

    bump-prow-images.sh: increase column width, DRY

--- hack/bump-prow-images.sh
+++ hack/bump-prow-images.sh
@@ -16,7 +16,8 @@ EOF
 
 new_tag=""
 
-target_files=( $( find cluster/ci/config/prow ci-operator/ -type f ) )
+targets=( cluster/ci/config/prow ci-operator/ )
+target_files=( $( find "${targets[@]}" -type f ) )
 for component in $( grep -Porh "(?<=gcr.io/k8s-prow/).*(?=:v)" "${target_files[@]}" | sort | uniq ); do
 	current_tag="$( grep -Porh "(?<=${component}:)v[0-9]{8}-[a-z0-9]+" "${target_files[@]}" | head -n 1 )"
 	latest_tag="$( gcloud container images list-tags "gcr.io/k8s-prow/${component}" --format='value(tags)' --limit 1 | grep -Po "v[0-9]+\-[a-z0-9]+" )"
@@ -26,10 +27,10 @@ for component in $( grep -Porh "(?<=gcr.io/k8s-prow/).*(?=:v)" "${target_files[@
 	current_sha="${current_tag#*-}"
 	latest_sha="${latest_tag#*-}"
 	if [[ "${current_tag}" != "${latest_tag}" ]]; then
-		printf '|%-20s|%-22s|%-22s|[link](github.com/kubernetes/test-infra/compare/%s...%s)|\n' "\`${component}\`" "\`${current_tag}\`" "\`${latest_tag}\`" "${current_sha}" "${latest_sha}"| tee -a "${workspace}/commit.txt"
+		printf '|%-21s|%-22s|%-22s|[link](github.com/kubernetes/test-infra/compare/%s...%s)|\n' "\`${component}\`" "\`${current_tag}\`" "\`${latest_tag}\`" "${current_sha}" "${latest_sha}"| tee -a "${workspace}/commit.txt"
 		sed -i "s|\(gcr\.io/k8s-prow/${component}:\)v[0-9][0-9]*-[a-z0-9][a-z0-9]*|\1${latest_tag}|g" "${target_files[@]}"
 	fi
 done
 
-git add cluster/ci/config/prow/ ci-operator/
+git add "${targets[@]}"
 git commit -m "$( cat "${workspace}/commit.txt" )"
