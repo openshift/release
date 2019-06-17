c1bf52587ffc87dc7a30fea8ff5efd75887306a6
Petr Muller <muller@redhat.com>
2019-06-17 17:17:41 +0200

    config-cleanup: Add quality bar script for core services
    Require OWNERS, README.md, Makefile.
    Require Makefile to implement `resources` and `admin-resources`.

---
+++ core-services/_hack/validate-core-service.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# This script validates all core services meet quality criteria
+
+set -euo pipefail
+
+function validate_required_files() {
+    local service_path="$1"
+    for required in OWNERS README.md Makefile; do
+        local required_path="$service_path/$required"
+        if [[ ! -s "$required_path" ]]; then
+            echo "[ERROR] $required file not found: $required_path"
+            echo "[ERROR] All core services should have $required file"
+            return 1
+        fi
+    done
+
+    return 0
+}
+
+validate_makefile() {
+    local service_path="$1"
+    local makefile="$service_path/Makefile"
+    if [[ ! -s "$makefile" ]]; then
+        echo "[ERROR] Makefile not found: $makefile"
+        echo "[ERROR] All core services should have a Makefile"
+        return 1
+    fi
+
+    for target in "resources" "admin-resources"; do
+        if ! make -C "$service_path" "$target" --dry-run; then
+            echo "[ERROR] Dry-run of 'make $target' did not succeed, Makefile likely does not provide this required target"
+            return 1
+        fi
+    done
+
+    return 0
+}
+
+to_validate="$1"
+if [[ ! -d "$to_validate" ]]; then
+    echo "[ERROR] Directory not found: $to_validate"
+    echo "Usage: validate-core-service.sh DIRECTORY"
+    exit 1
+fi
+
+validate_required_files "$to_validate" &&
+    validate_makefile "$to_validate"
