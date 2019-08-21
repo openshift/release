913b8c1a18267e28e629be04ac82560b38880c1c
Steve Kuznetsov <skuznets@redhat.com>
2019-08-20 17:13:25 -0700

    Revert "Merge pull request #4803 from stevekuznetsov/skuznets/bump-prow"
    This reverts commit bc9dc5464a4b10eabeced2e8f972f1c8a76646b0, reversing
    changes made to e1ec67049bbbf88eb8f71b66b765b5ce13238b1d.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- hack/images.sh
+++ hack/images.sh
@@ -1,2 +1,2 @@
-MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20190820-888ff5ffe
-MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20190820-888ff5ffe
+MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20190819-d37a254a4
+MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20190819-d37a254a4
