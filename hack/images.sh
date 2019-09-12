5116c8646535c24410ad937c1c1a496b1056fced
Steve Kuznetsov <skuznets@redhat.com>
2019-09-12 14:46:23 -0700

    Revert "Merge pull request #5011 from openshift-bot/autobump"
    This reverts commit 924ffb5b8e2a00a1372854806e8da54afb0e4127, reversing
    changes made to 2c4776dd47a2b9eb640dbccd5b86d804c7b916cd.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- hack/images.sh
+++ hack/images.sh
@@ -1,2 +1,2 @@
-MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20190912-b38f9a963
-MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20190912-b38f9a963
+MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20190911-918053970
+MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20190911-918053970
