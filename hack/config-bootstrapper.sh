9b02d212e4338f7b87ed3561b0edc6a1c85ab5c5
Petr Muller <muller@redhat.com>
2019-09-13 16:39:54 +0200

    Point config-validating jobs to new config locations

--- hack/config-bootstrapper.sh
+++ hack/config-bootstrapper.sh
@@ -5,5 +5,5 @@ dir=$(realpath "$(dirname "${BASH_SOURCE}")/..")
 config-bootstrapper \
     --dry-run=false \
     --source-path "${dir}" \
-    --config-path "${dir}/cluster/ci/config/prow/config.yaml" \
-    --plugin-config "${dir}/cluster/ci/config/prow/plugins.yaml"
+    --config-path "${dir}/core-services/prow/02_config/_config.yaml" \
+    --plugin-config "${dir}/core-services/prow/02_config/_plugins.yaml"
