99e8fa3ea86a1d454a1ceb60012d3d8bf9fa9d70
Petr Muller <afri@afri.cz>
2019-11-13 14:13:57 +0100

    Revert "Update prow to v20191113-cce53531b, and other images as necessary."

--- hack/images.sh
+++ hack/images.sh
@@ -1,2 +1,2 @@
-MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20191113-cce53531b
-MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20191113-cce53531b
+MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20191112-9f0441013
+MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20191112-9f0441013
