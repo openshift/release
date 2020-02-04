2a1a6aa517d57c49b5feb5ec1fb36e602c37c6c5
W. Trevor King <wking@tremily.us>
2020-02-04 13:39:04 -0800

    ipi/install/rbac: Drop system:unauthenticated group
    We have a pull secret to provide authenticated image access.  By
    excluding unauthenticated access, we avoid accidentally passing tests
    by relying on public image access.  With this commit, we ensure that
    the pull secret is getting passed through to the cluster and wired up
    to all the image-pulling code that should consume it.

--- ci-operator/step-registry/ipi/install/rbac/ipi-install-rbac-commands.sh
+++ ci-operator/step-registry/ipi/install/rbac/ipi-install-rbac-commands.sh
@@ -5,5 +5,4 @@ set -o errexit
 set -o pipefail
 
 # We want the cluster to be able to access these images
-oc adm policy add-role-to-group system:image-puller system:unauthenticated --namespace "${NAMESPACE}"
-oc adm policy add-role-to-group system:image-puller system:authenticated   --namespace "${NAMESPACE}"
+oc adm policy add-role-to-group system:image-puller system:authenticated --namespace "${NAMESPACE}"
