3aea5f7ffa66608dbfdc33e68eec3c1f34bb591c
Petr Muller <muller@redhat.com>
2020-02-07 17:52:37 +0100

    Make hack/validate* scripts use & require path arg

--- hack/validate-ci-operator-config-filename.py
+++ hack/validate-ci-operator-config-filename.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-'''
+"""
 This script validates the ci-operator's config filename, to be
 in $org-$repo-$branch.yaml format. Assuming that the folders tree will be
 $config_dir/$org/$repo/
-'''
+"""
 
 import sys
 import os
@@ -13,7 +13,7 @@ from argparse import ArgumentParser
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument("--config-dir", default="ci-operator/config",
+    parser.add_argument("--config-dir", required=True,
                         help="root dir of ci-operator configs")
     return parser.parse_args()
 
@@ -27,11 +27,11 @@ def main():
 
     for root, _, files in os.walk(args.config_dir):
         for name in files:
+            root_path = os.path.relpath(root, args.config_dir)
             if name.endswith(".yml"):
                 print(f"ERROR: Only .yaml extensions are allowed, not .yml as in {root_path}/{name}")
                 sys.exit(1)
             if name.endswith(".yaml"):
-                root_path = os.path.relpath(root, args.config_dir)
                 filename, _ = os.path.splitext(name)
                 try:
                     org, repo = os.path.split(root_path)
