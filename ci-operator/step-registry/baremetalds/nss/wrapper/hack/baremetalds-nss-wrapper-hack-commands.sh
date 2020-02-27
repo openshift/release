a0f962be9bb11211f4764e749b1a248b0cbad221
Andrea Fasano <afasano@redhat.com>
2020-02-27 10:53:51 +0100

    shared dir test

--- ci-operator/step-registry/baremetalds/nss/wrapper/hack/baremetalds-nss-wrapper-hack-commands.sh
+++ ci-operator/step-registry/baremetalds/nss/wrapper/hack/baremetalds-nss-wrapper-hack-commands.sh
@@ -4,8 +4,13 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-echo "Copying nss artifacts to ${SHARED_DIR}"
-cp /bin/mock-nss.sh /usr/lib64/libnss_wrapper.so ${SHARED_DIR}
+env
+
+dir=/tmp/shared
+mkdir "${dir}/"
+
+echo "Copying nss artifacts to ${DIR}"
+cp /bin/mock-nss.sh /usr/lib64/libnss_wrapper.so ${DIR}
 
 
 
---
+++ ci-operator/step-registry/baremetalds/setup/baremetalds-setup-commands.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+env
+
+dir=/tmp/shared
+
+echo "Copying nss artifacts from ${DIR}"
+ls ${DIR}
+
+
+
+
