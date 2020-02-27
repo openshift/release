c68839f4fa8276f87fb370c5c8899d2ae8d931da
Andrea Fasano <afasano@redhat.com>
2020-02-27 17:47:12 +0100

    Testing /tmp/secret

--- ci-operator/step-registry/baremetalds/nss/wrapper/hack/baremetalds-nss-wrapper-hack-commands.sh
+++ ci-operator/step-registry/baremetalds/nss/wrapper/hack/baremetalds-nss-wrapper-hack-commands.sh
@@ -9,6 +9,15 @@ env
 
 dir=/tmp/secret
 
+echo ""
+echo "------------ /tmp"
+ls -ll tmp
+
+if [ ! -d ${dir} ]; then
+    echo "Making ${dir}"
+    mkdir ${dir}
+  fi
+
 echo "Copying nss artifacts to ${dir}"
 cp /bin/mock-nss.sh /usr/lib64/libnss_wrapper.so ${dir}
 
