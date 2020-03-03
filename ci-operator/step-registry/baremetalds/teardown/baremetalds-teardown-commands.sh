137cf8a22cbc11575938bef31ae43534e2491299
Andrea Fasano <afasano@redhat.com>
2020-03-03 08:42:16 +0100

    copy logs test

--- ci-operator/step-registry/baremetalds/teardown/baremetalds-teardown-commands.sh
+++ ci-operator/step-registry/baremetalds/teardown/baremetalds-teardown-commands.sh
@@ -22,23 +22,28 @@ if [ "${CLUSTER_TYPE}" != "packet" ] ; then
 fi
 
 # Terraform setup and teardown for packet server
-terraform_home=${ARTIFACT_DIR}/terraform
-mkdir -p ${terraform_home}
-cd ${terraform_home}
+### terraform_home=${ARTIFACT_DIR}/terraform
+### mkdir -p ${terraform_home}
+### cd ${terraform_home}
 
-cp ${SHARED_DIR}/terraform.* ${terraform_home}
+### cp ${SHARED_DIR}/terraform.* ${terraform_home}
+
+whoami
 
 # Logs fetching
-export IP=$(cat ${SHARED_DIR}/packet-server-ip)
+### export IP=$(cat ${SHARED_DIR}/packet-server-ip)
+export IP=147.75.69.55
 if [ -n "$IP" ] ; then
    echo "Getting logs"
-   ssh $SSHOPTS root@$IP tar -czf - /root/dev-scripts/logs | tar -C ${ARTIFACT_DIR} -xzf -
-   sed -i -e 's/.*auths.*/*** PULL_SECRET ***/g' ${ARTIFACT_DIR}/root/dev-scripts/logs/*
+### ###   ssh $SSHOPTS root@$IP tar -czf - /root/dev-scripts/logs | tar -C ${ARTIFACT_DIR} -xzf -
+   mkdir -p ${ARTIFACT_DIR}/dev-scripts/logs/
+   scp $SSHOPTS -r root@IP:/root/dev-scripts/logs ${ARTIFACT_DIR}/dev-scripts/logs/
+   sed -i -e 's/.*auths.*/*** PULL_SECRET ***/g' ${ARTIFACT_DIR}/dev-scripts/logs/*
 fi
 
-echo "Deprovisioning cluster ..."
-terraform init
-for r in {1..5}; do terraform destroy -auto-approve && break ; done
+### echo "Deprovisioning cluster ..."
+### terraform init
+### for r in {1..5}; do terraform destroy -auto-approve && break ; done
 
 
 
