24966b797dd5436d91c974640dfb461bd279aa91
Andrea Fasano <afasano@redhat.com>
2020-03-05 09:59:34 +0100

    Run full conformance suite except storage tests

--- ci-operator/step-registry/baremetalds/e2e/test/baremetalds-e2e-test-commands.sh
+++ ci-operator/step-registry/baremetalds/e2e/test/baremetalds-e2e-test-commands.sh
@@ -38,7 +38,7 @@ scp $SSHOPTS /usr/bin/openshift-tests /usr/bin/kubectl root@$IP:/usr/local/bin
 # Tests execution
 set +e
 echo "### Running tests"
-ssh $SSHOPTS root@$IP openshift-tests run "openshift/conformance/parallel" --dry-run \| grep 'Area:Networking' \| openshift-tests run -o /tmp/artifacts/e2e.log --junit-dir /tmp/artifacts/junit -f -
+ssh $SSHOPTS root@$IP openshift-tests run "openshift/conformance/parallel" --dry-run \| grep -v 'sig-storage' \| openshift-tests run -o /tmp/artifacts/e2e.log --junit-dir /tmp/artifacts/junit -f -
 rv=$?
 echo "### Fetching results"
 ssh $SSHOPTS root@$IP tar -czf - /tmp/artifacts | tar -C ${ARTIFACT_DIR} -xzf - 
