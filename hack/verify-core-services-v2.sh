b47bc49fffe700d72fcd9648fd998a263b31117c
Alvaro Aleman <alv2412@googlemail.com>
2020-03-06 16:53:09 +0100

    Omit the names so timestamp gets omited

--- hack/verify-core-services-v2.sh
+++ hack/verify-core-services-v2.sh
@@ -16,8 +16,13 @@ actual_diff="$(diff \
   --recursive \
   -u \
   --new-file \
+  --label=o \
+  --label=m \
   $repo_root/core-services/prow/ \
   $repo_root/core-services-v2/prow/ || true)"
+
+# Remove timestamp, this will stop working in the year 10000
+#actual_diff="$(echo $actual_diff| sed -E 's/\s*[0-9]{4}-[0-9]{2}-[0-9]{2}.+?(?=@@)//g')"
 
 if [[ "$update" = true ]]; then
   echo "$actual_diff" > $diffFile
@@ -31,6 +36,6 @@ if [[ -n "$diffDiff" ]]; then
   echo "ERROR: Diff does not match expected"
   echo "ERROR: Diff from expected:"
   echo "$diffDiff"
-  echo "ERROR: If this is expeted, please run 'make update-core-services-v2 and commit the result'"
+  echo "ERROR: If this is expected, please run 'make update-core-services-v2 and commit the result'"
   exit 1
 fi
