4047fe1e298436969f7f58ee5ad84d61ad441462
Andrea Fasano <afasano@redhat.com>
2020-03-10 10:02:03 +0100

    Add tests filter

--- ci-operator/step-registry/baremetalds/e2e/test/baremetalds-e2e-test-commands.sh
+++ ci-operator/step-registry/baremetalds/e2e/test/baremetalds-e2e-test-commands.sh
@@ -37,11 +37,50 @@ bash ${HOME}/mock-nss.sh
 echo "### Copying test binaries"
 scp $SSHOPTS /usr/bin/openshift-tests /usr/bin/kubectl root@$IP:/usr/local/bin
 
+# List of exclude cases
+read -d '' EXCL << EOF
+sig-storage
+custom build with buildah being created from new-build
+docker build using a pull secret Building from a template
+prune builds based on settings in the buildconfig
+result image should have proper labels set
+Image policy
+deploymentconfigs adoption
+Alerts
+templateinstance readiness test
+oc adm must-gather
+capture build stages and durations
+deploymentconfigs with multiple image change triggers
+Managed cluster should
+forcePull should affect pulling builder images
+s2i build with a root user image
+Networking Granular Checks: Services
+Image layer subresource
+openshift mongodb image creating from a template
+capture build stages and durations
+process valueFrom in build strategy environment variables
+result image should have proper labels set S2I build from a template
+oc new-app
+Image append
+oc tag
+forcePull should affect pulling builder images
+templateinstance readiness test
+Multi-stage image builds
+Image extract
+TestDockercfgTokenDeletedController
+process valueFrom in build strategy environment variables
+Prometheus when installed on the cluster
+build can reference a cluster service with a build being created from new-build
+deploymentconfigs with multiple image change triggers
+deploymentconfigs should respect image stream tag reference policy
+EOF
+
 # Tests execution
 set +e
 echo "### Running tests"
-ssh $SSHOPTS root@$IP openshift-tests run "openshift/conformance/parallel" --dry-run \| grep -v 'sig-storage' \| openshift-tests run -o /tmp/artifacts/e2e.log --junit-dir /tmp/artifacts/junit -f -
+ssh $SSHOPTS root@$IP openshift-tests run "openshift/conformance/parallel" --dry-run \| grep -Fvf <(echo "$EXCL") \| openshift-tests run -o /tmp/artifacts/e2e.log --junit-dir /tmp/artifacts/junit -f -
 rv=$?
+
 echo "### Fetching results"
 ssh $SSHOPTS root@$IP tar -czf - /tmp/artifacts | tar -C ${ARTIFACT_DIR} -xzf - 
 set -e
