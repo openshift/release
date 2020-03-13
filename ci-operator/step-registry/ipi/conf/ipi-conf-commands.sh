6db1f860c7618a345cba9a1fdd093174af77a37e
W. Trevor King <wking@tremily.us>
2020-03-13 12:10:19 -0700

    ci-operator/step-registry: Pivot to explicitly-AWS
    And drop all the variant stuff out.  Personally, I prefer a single
    chain with a variant knob to make it easy to say "I don't care, pick
    something for me" without having to ask for a different workflow.  But
    Steve prefers having workflows and jobs tailored to specific
    provider/variant combinations.  This commit is an attempt to move in
    that direction.  I've dropped the unused refs, but they can be dug
    back up out of Git if/when there are jobs that consume them (e.g. GCP
    jobs, OVN jobs, pre-existing subnet jobs, etc.).
    
    Shifting the job definitions to their new alphabetical location is
    required by the generated-config presubmit [1].
    
    [1]: https://prow.svc.ci.openshift.org/view/gcs/origin-ci-test/pr-logs/pull/openshift_release/7625/pull-ci-openshift-release-master-generated-config/17978

--- ci-operator/step-registry/ipi/conf/ipi-conf-commands.sh
+++ ci-operator/step-registry/ipi/conf/ipi-conf-commands.sh
@@ -9,19 +9,6 @@ cluster_name=${NAMESPACE}-${JOB_NAME_HASH}
 
 out=${SHARED_DIR}/install-config.yaml
 
-cluster_variant=
-if [[ -e "${SHARED_DIR}/install-config-variant.txt" ]]; then
-	cluster_variant=$(<"${SHARED_DIR}/install-config-variant.txt")
-fi
-
-function has_variant() {
-	regex="(^|,)$1($|,)"
-	if [[ $cluster_variant =~ $regex ]]; then
-		return 0
-	fi
-	return 1
-}
-
 echo "Installing from release ${RELEASE_IMAGE_LATEST}"
 
 ssh_pub_key=$(<"${cluster_profile}/ssh-publickey")
@@ -36,9 +23,3 @@ pullSecret: >
 sshKey: |
   ${ssh_pub_key}
 EOF
-
-if has_variant fips; then
-	cat >> "${out}" <<-EOF
-	fips: true
-	EOF
-fi
