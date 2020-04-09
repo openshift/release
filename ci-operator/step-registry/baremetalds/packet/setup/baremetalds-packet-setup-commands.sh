cbac044ed0331481bfbc16cdb3e0b3c6cb8920ca
Andrea Fasano <afasano@redhat.com>
2020-04-09 18:39:03 +0200

    Ensure terraform files are shared even in case of interruption

--- ci-operator/step-registry/baremetalds/packet/setup/baremetalds-packet-setup-commands.sh
+++ ci-operator/step-registry/baremetalds/packet/setup/baremetalds-packet-setup-commands.sh
@@ -43,25 +43,33 @@ resource "packet_device" "server" {
 }
 EOF
 
+# Ensure files are always shared 
+finished()
+{
+  if [[ -e ${terraform_home}/terraform.tfstate ]]
+  then
+    # Sharing terraform artifacts required by teardown
+    cp ${terraform_home}/terraform.* ${SHARED_DIR}
+
+    # Sharing artifacts required by other steps
+    jq -r '.modules[0].resources["packet_device.server"].primary.attributes.access_public_ipv4' ${terraform_home}/terraform.tfstate > ${SHARED_DIR}/server-ip
+  fi
+}
+trap finished EXIT TERM
+
+
 terraform init
 
 # Packet returns transients errors when creating devices.
 # example, `Oh snap, something went wrong! We've logged the error and will take a look - please reach out to us if you continue having trouble.`
 # therefore the terraform apply needs to be retried a few time before giving up.
 rc=1
-# shellcheck disable=SC20347
 for _ in {1..5}; do terraform apply -auto-approve && rc=0 && break ; done
 if test "${rc}" -eq 1; then 
   echo >&2 "Failed to create packet server"
   exit 1
 fi
 
-# Sharing terraform artifacts required by teardown
-cp ${terraform_home}/terraform.* ${SHARED_DIR}
-
-# Sharing artifacts required by other steps
-jq -r '.modules[0].resources["packet_device.server"].primary.attributes.access_public_ipv4' terraform.tfstate > /tmp/server-ip
-cp /tmp/server-ip ${SHARED_DIR}
 
 
 
