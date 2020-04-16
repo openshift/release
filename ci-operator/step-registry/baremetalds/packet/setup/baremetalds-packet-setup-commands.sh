4b09711bbe93995a619331e73dc4d5afe77dc2a1
Andrea Fasano <afasano@redhat.com>
2020-04-16 18:55:31 +0200

    Test commit

--- ci-operator/step-registry/baremetalds/packet/setup/baremetalds-packet-setup-commands.sh
+++ ci-operator/step-registry/baremetalds/packet/setup/baremetalds-packet-setup-commands.sh
@@ -4,80 +4,82 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-cluster_profile=/var/run/secrets/ci.openshift.io/cluster-profile
-
-export CLUSTER_NAME=${NAMESPACE}-${JOB_NAME_HASH}
-
 echo "************ baremetalds packet setup command ************"
-env | sort
 
-set +x
-export PACKET_PROJECT_ID=b3c1623c-ce0b-45cf-9757-c61a71e06eac
-PACKET_AUTH_TOKEN=$(cat ${cluster_profile}/.packetcred)
-export PACKET_AUTH_TOKEN
-set -x
+# cluster_profile=/var/run/secrets/ci.openshift.io/cluster-profile
 
-# Initial check
-if [ "${CLUSTER_TYPE}" != "packet" ] ; then
-    echo >&2 "Unsupported cluster type '${CLUSTER_TYPE}'"
-    exit 1
-fi
+# export CLUSTER_NAME=${NAMESPACE}-${JOB_NAME_HASH}
 
-# Terraform setup and init for packet server
-terraform_home=/tmp/terraform
-mkdir -p ${terraform_home}
-cd ${terraform_home}
+# echo "************ baremetalds packet setup command ************"
+# env | sort
 
-case "${JOB_TYPE}" in 
+# set +x
+# export PACKET_PROJECT_ID=b3c1623c-ce0b-45cf-9757-c61a71e06eac
+# PACKET_AUTH_TOKEN=$(cat ${cluster_profile}/.packetcred)
+# export PACKET_AUTH_TOKEN
+# set -x
 
-  presubmit)
-    prow_job_url="https://prow.svc.ci.openshift.org/view/gcs/origin-ci-test/pr-logs/pull/${REPO_OWNER}_${REPO_NAME}/${PULL_NUMBER}/${JOB_NAME}/${BUILD_ID}"
-    ;;
+# # Initial check
+# if [ "${CLUSTER_TYPE}" != "packet" ] ; then
+#     echo >&2 "Unsupported cluster type '${CLUSTER_TYPE}'"
+#     exit 1
+# fi
 
-  periodic)
-    prow_job_url="https://prow.svc.ci.openshift.org/view/gcs/origin-ci-test/logs/${JOB_NAME}/${BUILD_ID}"
-    ;;
+# # Terraform setup and init for packet server
+# terraform_home=/tmp/terraform
+# mkdir -p ${terraform_home}
+# cd ${terraform_home}
 
-  *)
-    prow_job_url="<none>"
-    ;;
-esac
+# case "${JOB_TYPE}" in 
 
-cat > ${terraform_home}/terraform.tf <<-EOF
-provider "packet" {
-}
+#   presubmit)
+#     prow_job_url="https://prow.svc.ci.openshift.org/view/gcs/origin-ci-test/pr-logs/pull/${REPO_OWNER}_${REPO_NAME}/${PULL_NUMBER}/${JOB_NAME}/${BUILD_ID}"
+#     ;;
 
-resource "packet_device" "server" {
-  count            = "1"
-  project_id       = "$PACKET_PROJECT_ID"
-  hostname         = "ipi-$CLUSTER_NAME"
-  plan             = "c2.medium.x86"
-  facilities       = ["sjc1", "ewr1"]
-  operating_system = "centos_8"
-  billing_cycle    = "hourly"
-  tags             = ["prow_job_id=$PROW_JOB_ID", "leased_resource=$LEASED_RESOURCE", "prow_job_url=$prow_job_url"]
-}
-EOF
+#   periodic)
+#     prow_job_url="https://prow.svc.ci.openshift.org/view/gcs/origin-ci-test/logs/${JOB_NAME}/${BUILD_ID}"
+#     ;;
 
-terraform init
+#   *)
+#     prow_job_url="<none>"
+#     ;;
+# esac
 
-# Packet returns transients errors when creating devices.
-# example, `Oh snap, something went wrong! We've logged the error and will take a look - please reach out to us if you continue having trouble.`
-# therefore the terraform apply needs to be retried a few time before giving up.
-rc=1
-# shellcheck disable=SC20347
-for _ in {1..5}; do terraform apply -auto-approve && rc=0 && break ; done
-if test "${rc}" -eq 1; then 
-  echo >&2 "Failed to create packet server"
-  exit 1
-fi
+# cat > ${terraform_home}/terraform.tf <<-EOF
+# provider "packet" {
+# }
 
-# Sharing terraform artifacts required by teardown
-cp ${terraform_home}/terraform.* ${SHARED_DIR}
+# resource "packet_device" "server" {
+#   count            = "1"
+#   project_id       = "$PACKET_PROJECT_ID"
+#   hostname         = "ipi-$CLUSTER_NAME"
+#   plan             = "c2.medium.x86"
+#   facilities       = ["sjc1", "ewr1"]
+#   operating_system = "centos_8"
+#   billing_cycle    = "hourly"
+#   tags             = ["prow_job_id=$PROW_JOB_ID", "leased_resource=$LEASED_RESOURCE", "prow_job_url=$prow_job_url"]
+# }
+# EOF
 
-# Sharing artifacts required by other steps
-jq -r '.modules[0].resources["packet_device.server"].primary.attributes.access_public_ipv4' terraform.tfstate > /tmp/server-ip
-cp /tmp/server-ip ${SHARED_DIR}
+# terraform init
+
+# # Packet returns transients errors when creating devices.
+# # example, `Oh snap, something went wrong! We've logged the error and will take a look - please reach out to us if you continue having trouble.`
+# # therefore the terraform apply needs to be retried a few time before giving up.
+# rc=1
+# # shellcheck disable=SC20347
+# for _ in {1..5}; do terraform apply -auto-approve && rc=0 && break ; done
+# if test "${rc}" -eq 1; then 
+#   echo >&2 "Failed to create packet server"
+#   exit 1
+# fi
+
+# # Sharing terraform artifacts required by teardown
+# cp ${terraform_home}/terraform.* ${SHARED_DIR}
+
+# # Sharing artifacts required by other steps
+# jq -r '.modules[0].resources["packet_device.server"].primary.attributes.access_public_ipv4' terraform.tfstate > /tmp/server-ip
+# cp /tmp/server-ip ${SHARED_DIR}
 
 
 
--- ci-operator/step-registry/baremetalds/packet/teardown/baremetalds-packet-teardown-commands.sh
+++ ci-operator/step-registry/baremetalds/packet/teardown/baremetalds-packet-teardown-commands.sh
@@ -4,36 +4,38 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-cluster_profile=/var/run/secrets/ci.openshift.io/cluster-profile
-
 echo "************ baremetalds packet teardown command ************"
-env | sort
 
-set +x
-export SSH_PRIV_KEY_PATH=${cluster_profile}/ssh-privatekey
-export SSHOPTS="-o ConnectTimeout=5 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=90 -i ${SSH_PRIV_KEY_PATH}"
-PACKET_AUTH_TOKEN=$(cat ${cluster_profile}/.packetcred)
-export PACKET_AUTH_TOKEN
-set -x
+# cluster_profile=/var/run/secrets/ci.openshift.io/cluster-profile
 
-# Initial check
-if [ "${CLUSTER_TYPE}" != "packet" ] ; then
-    echo >&2 "Unsupported cluster type '${CLUSTER_TYPE}'"
-    exit 1
-fi
+# echo "************ baremetalds packet teardown command ************"
+# env | sort
 
-echo "-------[ $SHARED_DIR ]"
-ls -ll ${SHARED_DIR}
+# set +x
+# export SSH_PRIV_KEY_PATH=${cluster_profile}/ssh-privatekey
+# export SSHOPTS="-o ConnectTimeout=5 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=90 -i ${SSH_PRIV_KEY_PATH}"
+# PACKET_AUTH_TOKEN=$(cat ${cluster_profile}/.packetcred)
+# export PACKET_AUTH_TOKEN
+# set -x
 
-# Shutdown packet server
-terraform_home=/tmp/terraform
-mkdir -p ${terraform_home}
-cp ${SHARED_DIR}/terraform.* ${terraform_home}
-echo "Deprovisioning cluster..."
-cd ${terraform_home}
-terraform init
-# shellcheck disable=SC2034
-for r in {1..5}; do terraform destroy -auto-approve && break ; done
+# # Initial check
+# if [ "${CLUSTER_TYPE}" != "packet" ] ; then
+#     echo >&2 "Unsupported cluster type '${CLUSTER_TYPE}'"
+#     exit 1
+# fi
+
+# echo "-------[ $SHARED_DIR ]"
+# ls -ll ${SHARED_DIR}
+
+# # Shutdown packet server
+# terraform_home=/tmp/terraform
+# mkdir -p ${terraform_home}
+# cp ${SHARED_DIR}/terraform.* ${terraform_home}
+# echo "Deprovisioning cluster..."
+# cd ${terraform_home}
+# terraform init
+# # shellcheck disable=SC2034
+# for r in {1..5}; do terraform destroy -auto-approve && break ; done
 
 
 
