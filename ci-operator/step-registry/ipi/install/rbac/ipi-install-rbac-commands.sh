ed5d03f4a50d4199e00b6df9158a68f14aafb27c
Bruno Barcarol Guimarães <bbarcaro@redhat.com>
2020-04-16 10:00:45 +0000

    Revert "step-registry: ipi-install-rbac: allow pulls for unauthenticated"
    This reverts commit 2f3f5e7e5abf400c43110095fb58684046b5fe88.
    
    The temporary measure should not be necessary anymore after the changes
    to the pull secret:
    
    https://github.com/openshift/release/pull/8341
    
    The RBAC setup can be restored to the original intended state:
    
    https://github.com/openshift/release/pull/7023

--- ci-operator/step-registry/ipi/install/rbac/ipi-install-rbac-commands.sh
+++ ci-operator/step-registry/ipi/install/rbac/ipi-install-rbac-commands.sh
@@ -6,4 +6,3 @@ set -o pipefail
 
 # We want the cluster to be able to access these images
 oc adm policy add-role-to-group system:image-puller system:authenticated --namespace "${NAMESPACE}"
-oc adm policy add-role-to-group system:image-puller system:unauthenticated --namespace "${NAMESPACE}"
