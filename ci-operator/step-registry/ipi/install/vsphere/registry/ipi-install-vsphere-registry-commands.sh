ed6e8a2fb9616eb1e957da2f838f097c1fe5fc15
Joseph Callen <jcallen@redhat.com>
2020-05-21 19:54:47 -0400

    logging

--- ci-operator/step-registry/ipi/install/vsphere/registry/ipi-install-vsphere-registry-commands.sh
+++ ci-operator/step-registry/ipi/install/vsphere/registry/ipi-install-vsphere-registry-commands.sh
@@ -7,7 +7,7 @@ set -o pipefail
 function operator_progress() {
 while true; do
         progress=$(oc -o yaml get clusteroperators.config.openshift.io $1 -o go-template='{{ range .status.conditions -}}{{if eq .type "Progressing" -}}{{.status}}{{end -}}{{end -}}')
-        if [[ "${progress}" == "True" ]]; then
+        if [[ "${progress}" == "False" ]]; then
                 sleep 15
                 printf "."
         else
@@ -21,6 +21,12 @@ export KUBECONFIG=${SHARED_DIR}/kubeconfig
 
 oc patch configs.imageregistry.operator.openshift.io cluster --type merge --patch '{"spec":{"managementState":"Managed","storage":{"emptyDir":{}}}}'
 
+printf "Sleep 30.../n"
+sleep 30
+
+
+printf "wait for image-registry"
 operator_progress image-registry
+printf "wait for kube-apiserver"
 operator_progress kube-apiserver
 
