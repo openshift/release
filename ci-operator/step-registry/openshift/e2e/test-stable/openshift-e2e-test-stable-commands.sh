706567f6021d482156b2c443f777d0737f5425e7
Andrew Kiselev <akiselev@redhat.com>
2020-05-20 21:32:00 -0400

    e2e-test-stable includes stable subset of tests

---
+++ ci-operator/step-registry/openshift/e2e/test-stable/openshift-e2e-test-stable-commands.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+export AWS_SHARED_CREDENTIALS_FILE=${CLUSTER_PROFILE_DIR}/.awscred
+export AZURE_AUTH_LOCATION=${CLUSTER_PROFILE_DIR}/osServicePrincipal.json
+export GCP_SHARED_CREDENTIALS_FILE=${CLUSTER_PROFILE_DIR}/gce.json
+export HOME=/tmp/home
+export PATH=/usr/libexec/origin:$PATH
+
+trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
+
+mkdir -p "${HOME}"
+
+# if the cluster profile included an insights secret, install it to the cluster to
+# report support data from the support-operator
+if [[ -f "${CLUSTER_PROFILE_DIR}/insights-live.yaml" ]]; then
+    oc create -f "${CLUSTER_PROFILE_DIR}/insights-live.yaml" || true
+fi
+
+# set up cloud-provider-specific env vars
+KUBE_SSH_BASTION="$( oc --insecure-skip-tls-verify get node -l node-role.kubernetes.io/master -o 'jsonpath={.items[0].status.addresses[?(@.type=="ExternalIP")].address}' ):22"
+KUBE_SSH_KEY_PATH=${CLUSTER_PROFILE_DIR}/ssh-privatekey
+export KUBE_SSH_BASTION KUBE_SSH_KEY_PATH
+case "${CLUSTER_TYPE}" in
+gcp)
+    export GOOGLE_APPLICATION_CREDENTIALS="${GCP_SHARED_CREDENTIALS_FILE}"
+    export KUBE_SSH_USER=core
+    mkdir -p ~/.ssh
+    cp "${CLUSTER_PROFILE_DIR}/ssh-privatekey" ~/.ssh/google_compute_engine || true
+    export TEST_PROVIDER='{"type":"gce","region":"us-east1","multizone": true,"multimaster":true,"projectid":"openshift-gce-devel-ci"}'
+    ;;
+aws)
+    mkdir -p ~/.ssh
+    cp "${CLUSTER_PROFILE_DIR}/ssh-privatekey" ~/.ssh/kube_aws_rsa || true
+    export PROVIDER_ARGS="-provider=aws -gce-zone=us-east-1"
+    # TODO: make openshift-tests auto-discover this from cluster config
+    REGION="$(oc get -o jsonpath='{.status.platformStatus.aws.region}' infrastructure cluster)"
+    ZONE="$(oc get -o jsonpath='{.items[0].metadata.labels.failure-domain\.beta\.kubernetes\.io/zone}' nodes)"
+    export TEST_PROVIDER="{\"type\":\"aws\",\"region\":\"${REGION}\",\"zone\":\"${ZONE}\",\"multizone\":true,\"multimaster\":true}"
+    export KUBE_SSH_USER=core
+    ;;
+azure4) export TEST_PROVIDER=azure;;
+vsphere) export TEST_PROVIDER=vsphere;;
+*) echo >&2 "Unsupported cluster type '${CLUSTER_TYPE}'"; exit 1;;
+esac
+
+mkdir -p /tmp/output
+cd /tmp/output
+
+if [[ "${CLUSTER_TYPE}" == gcp ]]; then
+    pushd /tmp
+    curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-256.0.0-linux-x86_64.tar.gz
+    tar -xzf google-cloud-sdk-256.0.0-linux-x86_64.tar.gz
+    export PATH=$PATH:/tmp/google-cloud-sdk/bin
+    mkdir gcloudconfig
+    export CLOUDSDK_CONFIG=/tmp/gcloudconfig
+    gcloud auth activate-service-account --key-file="${GCP_SHARED_CREDENTIALS_FILE}"
+    gcloud config set project openshift-gce-devel-ci
+    popd
+fi
+
+test_suite=openshift/conformance/parallel
+if [[ -e "${SHARED_DIR}/test-suite.txt" ]]; then
+    test_suite=$(<"${SHARED_DIR}/test-suite.txt")
+fi
+
+openshift-tests run "${test_suite}" --dry-run | \
+    grep -v "Kubectl Port forwarding With a server listening on localhost that expects a client request should support a client that connects, sends DATA" | \
+    grep -v "The HAProxy router should expose prometheus metrics for a route" | \
+    openshift-tests run --provider "${TEST_PROVIDER}" \
+    -o /tmp/artifacts/e2e.log \
+    --junit-dir /tmp/artifacts/junit -f -
