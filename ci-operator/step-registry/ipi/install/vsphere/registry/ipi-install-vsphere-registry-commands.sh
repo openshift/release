3644d125c131daf658a45e18011d38173bcdd4b2
Joseph Callen <jcallen@redhat.com>
2020-05-21 22:41:24 -0400

    should be true for progressing

--- ci-operator/step-registry/ipi/install/vsphere/registry/ipi-install-vsphere-registry-commands.sh
+++ ci-operator/step-registry/ipi/install/vsphere/registry/ipi-install-vsphere-registry-commands.sh
@@ -7,7 +7,7 @@ set -o pipefail
 function operator_progress() {
 while true; do
         progress=$(oc -o yaml get clusteroperators.config.openshift.io $1 -o go-template='{{ range .status.conditions -}}{{if eq .type "Progressing" -}}{{.status}}{{end -}}{{end -}}'|| true)
-        if [[ "${progress}" == "False" ]]; then
+        if [[ "${progress}" == "True" ]]; then
                 sleep 15
                 echo "."
         else
