de94d3fbd4fe19b815f178f965b2b162661fe95a
Stephen Benjamin <stephen@redhat.com>
2020-05-26 11:23:06 -0400

    baremetal: copy install-gather as well
    We're enabling the log gathering features of the installer in baremetal
    ipi, we need to grab that tarball as well. This also refactors the
    gather to only use 1 scp statement.

--- ci-operator/step-registry/baremetalds/devscripts/gather/baremetalds-devscripts-gather-commands.sh
+++ ci-operator/step-registry/baremetalds/devscripts/gather/baremetalds-devscripts-gather-commands.sh
@@ -27,9 +27,7 @@ SSHOPTS="-o ConnectTimeout=5 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/
 
 function getlogs() {
   echo "### Downloading logs..."
-  ssh $SSHOPTS root@$IP tar -czC "/tmp/artifacts/must-gather" -f "/tmp/artifacts/must-gather.tar.gz" .
-  scp $SSHOPTS root@$IP:/tmp/artifacts/must-gather.tar.gz ${ARTIFACT_DIR}
-  scp $SSHOPTS root@$IP:/var/tmp/sosreport-\* ${ARTIFACT_DIR}
+  scp $SSHOPTS root@$IP:/tmp/artifacts/\*.tar\* ${ARTIFACT_DIR}
 }
 
 # Gather logs regardless of what happens after this
@@ -37,11 +35,16 @@ trap getlogs EXIT
 
 echo "### Gathering logs..."
 timeout -s 9 15m ssh $SSHOPTS root@$IP bash - << EOF |& sed -e 's/.*auths.*/*** PULL_SECRET ***/g'
+cd dev-scripts
+
+# Get install-gather, if there is one
+cp log-bundle\* /tmp/artifacts || true
+
 # Get must-gather
 export MUST_GATHER_PATH=/tmp/artifacts/must-gather
-cd dev-scripts
 make gather
+tar -czC "/tmp/artifacts/must-gather" -f "/tmp/artifacts/must-gather.tar.gz" .
 
 # Get sosreport including sar data
-sosreport --ticket-number "\$HOSTNAME" --batch -o sar,filesys,networkmanager,virsh,libvirt,kvm
+sosreport --ticket-number "\$HOSTNAME" --batch -o sar,filesys,networkmanager,virsh,libvirt,kvm --tmp-dir /tmp/artifacts
 EOF
