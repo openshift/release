eea1d1910c43acf2489ba1c4454e576bc64c4d1c
Andrea Fasano <afasano@redhat.com>
2020-06-05 10:29:44 +0200

    Restrict error notification to msg field only

--- ci-operator/step-registry/baremetalds/packet/teardown/baremetalds-packet-teardown-commands.sh
+++ ci-operator/step-registry/baremetalds/packet/teardown/baremetalds-packet-teardown-commands.sh
@@ -44,7 +44,7 @@ cat > packet-teardown.yaml <<-EOF
     - name: Send notification message via Slack in case of failure
       slack:
         token: "{{ 'T027F3GAJ/B011TAG710V/' + lookup('file', slackhook_path + '/.slackhook') }}"
-        msg: 'Packet teardown failed: {{ ansible_failed_result }}'
+        msg: 'Packet teardown failed. Error msg: {{ ansible_failed_result.msg }}'
         username: 'Ansible on {{ packet_hostname }}'
         channel: "#team-edge-installer"
         color: warning
