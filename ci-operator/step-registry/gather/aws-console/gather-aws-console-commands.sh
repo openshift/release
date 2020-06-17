3ff8e696c5c14f74c5e48d8e412899fdab1da91d
W. Trevor King <wking@tremily.us>
2020-06-17 14:49:37 -0700

    gather/aws/console: Add a step to gather console logs on AWS
    History of this logic:
    
    * Initially landed for nodes in 362635a516
      (ci-operator/templates/openshift/installer/cluster-launch-installer-e2e:
      Gather node console logs on AWS, 2019-12-02, #6189).
    
    * Grew --text in ecba1142ee (installer artifacts: keep text version of
      instance output, 2020-01-02, #6536).
    
    * Grew machine handling in c701443500
      (ci-operator/templates/openshift/installer/cluster-launch-installer-e2e:
      Gather console logs from Machine providerID too, 2020-01-29, #6906).
    
    * The node-provider-IDs creation was ported to steps in f476a5200c
      (step-registry: update deprovision step, 2020-01-30, #6708), but
      without any consumer for the collected file.
    
    The aws-instance-ids.txt injection allows install-time steps to
    register additional instances for later console collection (for a
    proxy instance, bootstrap instance, etc.).
    
    Approvers are:
    
    * Myself and Vadim, who have touched this logic in the past.
    * Alberto, representing the machine-API space that needs console logs
      to debug failed-to-boot issues.
    * Colin, representing the RHCOS/machine-config space that needs
      console logs to debug RHCOS issues.
    
    TMPDIR documentation is based on POSIX [1].
    
    [1]: https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap08.html#tag_08_03

---
+++ ci-operator/step-registry/gather/aws-console/gather-aws-console-commands.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
+
+export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
+
+if test ! -f "${SHARED_DIR}/metadata.json"
+then
+	echo "No metadata.json, so unknown AWS region, so unable to gathering console logs."
+	exit 0
+fi
+
+if test -f "${KUBECONFIG}"
+then
+	oc --request-timeout=5s get nodes -o jsonpath --template '{range .items[*]}{.spec.providerID}{"\n"}{end}' | sed 's|.*/||' > "${TMPDIR}/node-provider-IDs.txt" &
+	wait "$!"
+
+	oc --request-timeout=5s -n openshift-machine-api get machines -o jsonpath --template '{range .items[*]}{.spec.providerID}{"\n"}{end}' | sed 's|.*/||' >> "${TMPDIR}/node-provider-IDs" &
+	wait "$!"
+else
+	echo "No kubeconfig; skipping providerID extraction."
+	exit 0
+fi
+
+if test -f "${SHARED_DIR}/aws-instance-ids.txt"
+then
+	cat "${SHARED_DIR}/aws-instance-ids.txt" >> "${TMPDIR}/node-provider-IDs.txt"
+fi
+
+REGION="$(jq -r .aws.region "${SHARED_DIR}/metadata.json")"
+cat "${TMPDIR}/node-provider-IDs.txt" | sort | uniq | while read -r INSTANCE_ID
+do
+	echo "Gathering console logs for ${INSTANCE_ID}"
+	aws --region "${REGION}" ec2 get-console-output --instance-id "${INSTANCE_ID}" --output text > "${ARTIFACT_DIR}/${INSTANCE_ID}" &
+	wait "$!"
+done
