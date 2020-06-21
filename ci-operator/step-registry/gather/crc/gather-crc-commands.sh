7db2fb1aec1d0bcb98fa96f381de6036f2399523
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2020-06-21 22:39:35 +0530

    Add step to gather the crc artifacts

---
+++ ci-operator/step-registry/gather/crc/gather-crc-commands.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+
+trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
+
+INSTANCE_PREFIX="${NAMESPACE}"-"${JOB_NAME_HASH}"
+
+mkdir -p "${HOME}"/.ssh
+mock-nss.sh
+
+# gcloud compute will use this key rather than create a new one
+cp "${CLUSTER_PROFILE_DIR}"/ssh-privatekey "${HOME}"/.ssh/google_compute_engine
+chmod 0600 "${HOME}"/.ssh/google_compute_engine
+cp "${CLUSTER_PROFILE_DIR}"/ssh-publickey "${HOME}"/.ssh/google_compute_engine.pub
+
+gcloud auth activate-service-account --quiet --key-file "${CLUSTER_PROFILE_DIR}"/gce.json
+gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
+gcloud --quiet config set compute/zone "${GOOGLE_COMPUTE_ZONE}"
+gcloud --quiet config set compute/region "${GOOGLE_COMPUTE_REGION}"
+
+
+echo "scp crc logs back to pod"
+LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute scp \
+    --quiet \
+    --project "${GOOGLE_PROJECT_ID}" \
+    --zone "${GOOGLE_COMPUTE_ZONE}" \
+    --recurse packer@"${INSTANCE_PREFIX}":~/.crc/crc.log ${ARTIFACT_DIR}
+
+echo "scp intergration test logs back to pod"
+LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute scp \
+    --quiet \
+    --project "${GOOGLE_PROJECT_ID}" \
+    --zone "${GOOGLE_COMPUTE_ZONE}" \
+    --recurse packer@"${INSTANCE_PREFIX}":~/crc/test/integration/out/test-results/ ${ARTIFACT_DIR}
