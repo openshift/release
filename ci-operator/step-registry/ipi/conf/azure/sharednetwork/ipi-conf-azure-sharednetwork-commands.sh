10668b8cfe10d7d4643c6615cea20ec1654c6a37
W. Trevor King <wking@tremily.us>
2020-07-07 22:36:44 -0700

    ci-operator/step-registry/ipi/conf/*/sharednetwork: Quote CONFIG
    To make ShellCheck happy, addressing:
    
      $ sudo podman run --rm --volume "${PWD}:/workdir:ro,z" --workdir /workdir registry.svc.ci.openshift.org/ci/shellcheck sh -c 'find ci-operator/step-registry/ipi -name "*.sh" -print0 | xargs -0 -n1 shellcheck'
    
      In ci-operator/step-registry/ipi/conf/aws/sharednetwork/ipi-conf-aws-sharednetwork-commands.sh line 13:
      aws_region=$(/tmp/yq r ${CONFIG} 'platform.aws.region')
                             ^-------^ SC2086: Double quote to prevent globbing and word splitting.
    
      Did you mean:
      aws_region=$(/tmp/yq r "${CONFIG}" 'platform.aws.region')
    
      For more information:
        https://www.shellcheck.net/wiki/SC2086 -- Double quote to prevent globbing ...
    
      In ci-operator/step-registry/ipi/conf/azure/sharednetwork/ipi-conf-azure-sharednetwork-commands.sh line 13:
      azure_region=$(/tmp/yq r ${CONFIG} 'platform.azure.region')
                               ^-------^ SC2086: Double quote to prevent globbing and word splitting.
    
      Did you mean:
      azure_region=$(/tmp/yq r "${CONFIG}" 'platform.azure.region')
    
      For more information:
        https://www.shellcheck.net/wiki/SC2086 -- Double quote to prevent globbing ...

--- ci-operator/step-registry/ipi/conf/azure/sharednetwork/ipi-conf-azure-sharednetwork-commands.sh
+++ ci-operator/step-registry/ipi/conf/azure/sharednetwork/ipi-conf-azure-sharednetwork-commands.sh
@@ -10,7 +10,7 @@ curl -L https://github.com/mikefarah/yq/releases/download/3.3.0/yq_linux_amd64 -
 CONFIG="${SHARED_DIR}/install-config.yaml"
 PATCH="${SHARED_DIR}/install-config-sharednetwork.yaml.patch"
 
-azure_region=$(/tmp/yq r ${CONFIG} 'platform.azure.region')
+azure_region=$(/tmp/yq r "${CONFIG}" 'platform.azure.region')
 
 cat >> "${PATCH}" << EOF
 platform:
