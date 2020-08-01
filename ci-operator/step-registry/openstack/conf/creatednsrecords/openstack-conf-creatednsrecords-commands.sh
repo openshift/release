7c5dcc74d506ba49281ad9fd059685b73263eb06
Adolfo Duarte <adduarte@redhat.com>
2020-07-31 23:21:33 -0700

    OpenStack: IPI OpenShift on Openstack step registry deployment
    - Adds step registry for e2e ipi test
    - Adds config file for ci-operator
    - Adds presubmit jobs for openstack ipi
    - Deploys ipi openshift on Openstack  with minimal config
    - Run always disabled
    - Does not run e2e

---
+++ ci-operator/step-registry/openstack/conf/creatednsrecords/openstack-conf-creatednsrecords-commands.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
+
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_DEFAULT_OUTPUT=json
+export AWS_PROFILE=openshift-ci-infra
+
+CLUSTER_NAME=$(<"${SHARED_DIR}"/CLUSTER_NAME)
+LB_FIP_IP=$(<"${SHARED_DIR}"/LB_FIP_IP)
+INGRESS_FIP_IP=$(<"${SHARED_DIR}"/INGRESS_FIP_IP)
+HOSTED_ZONE_ID=$(aws route53 list-hosted-zones-by-name --dns-name "${BASE_DOMAIN}" | python -c '
+import json,sys;
+print(json.load(sys.stdin)["HostedZones"][0]["Id"].split("/")[-1])'
+)
+
+cat > ${SHARED_DIR}/api-record.json <<EOF
+{
+"Comment": "Create the public OpenShift API record",
+"Changes": [{
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "api.${CLUSTER_NAME}.${BASE_DOMAIN}.",
+        "Type": "A",
+        "TTL": 300,
+        "ResourceRecords": [{"Value": "${LB_FIP_IP}"}]
+      }
+}]}
+EOF
+cp ${SHARED_DIR}/api-record.json ${ARTIFACT_DIR}/api-record.json
+aws route53 change-resource-record-sets --hosted-zone-id "$HOSTED_ZONE_ID" --change-batch file://${SHARED_DIR}/api-record.json
+
+echo "Creating DNS record for *.apps.$CLUSTER_NAME.$BASE_DOMAIN. -> $INGRESS_FIP_IP"
+cat > ${SHARED_DIR}/ingress-record.json <<EOF
+{
+"Comment": "Create the public OpenShift Ingress record",
+"Changes": [{
+  "Action": "UPSERT",
+  "ResourceRecordSet": {
+    "Name": "*.apps.${CLUSTER_NAME}.${BASE_DOMAIN}.",
+    "Type": "A",
+    "TTL": 300,
+    "ResourceRecords": [{"Value": "${INGRESS_FIP_IP}"}]
+    }
+}]}
+EOF
+cp ${SHARED_DIR}/ingress-record.json ${ARTIFACT_DIR}/ingress-record.json
+aws route53 change-resource-record-sets --hosted-zone-id "$HOSTED_ZONE_ID" --change-batch file://${SHARED_DIR}/ingress-record.json
---
+++ ci-operator/step-registry/openstack/conf/generateconfig/openstack-conf-generateconfig-commands.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+#Read necessary variables
+CLUSTER_NAME=$(<"${SHARED_DIR}"/CLUSTER_NAME)
+LB_FIP_IP=$(<"${SHARED_DIR}"/LB_FIP_IP)
+
+PULL_SECRET=$(<"${CLUSTER_PROFILE_DIR}"/pull-secret)
+SSH_PUB_KEY=$(<"${CLUSTER_PROFILE_DIR}"/ssh-publickey)
+
+CONFIG="${SHARED_DIR}/install-config.yaml"
+if [[ "${CONFIG_TYPE}" == "minimal" ]]; then
+cat > "${CONFIG}" << EOF
+apiVersion: ${CONFIG_API_VERSION}
+baseDomain: ${BASE_DOMAIN}
+metadata:
+  name: ${CLUSTER_NAME}
+platform:
+  openstack:
+    cloud:            ${OS_CLOUD}
+    externalNetwork:  ${OPENSTACK_EXTERNAL_NETWORK}
+    computeFlavor:    ${OPENSTACK_COMPUTE_FLAVOR}
+    lbFloatingIP:     ${LB_FIP_IP}
+pullSecret: >
+  ${PULL_SECRET}
+sshKey: |
+  ${SSH_PUB_KEY}
+EOF
+else
+    echo "NO valid install config type specified. Please check  CONFIG_TYPE"
+    exit 1
+fi
+
+# Lets  check the syntax of yaml file by reading it.
+python -c 'import yaml;
+import sys;
+data = yaml.safe_load(open(sys.argv[1]))' ${SHARED_DIR}/install-config.yaml
