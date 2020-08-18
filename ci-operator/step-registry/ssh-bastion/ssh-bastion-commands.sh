86c593e10d3491c41a8c261ad8046438233bc9eb
Russell Teague <rteague@redhat.com>
2020-08-18 14:16:40 -0400

    Change ssh-bastion to cli image

--- ci-operator/step-registry/ssh-bastion/ssh-bastion-commands.sh
+++ ci-operator/step-registry/ssh-bastion/ssh-bastion-commands.sh
@@ -6,6 +6,8 @@ set -o pipefail
 
 echo "${USER:-default}:x:$(id -u):$(id -g):Default User:$HOME:/sbin/nologin" >> /etc/passwd
 
+yum install openssh-clients
+
 # Setting up ssh bastion host
 export SSH_BASTION_NAMESPACE=test-ssh-bastion
 curl https://raw.githubusercontent.com/eparis/ssh-bastion/master/deploy/deploy.sh | bash -x
