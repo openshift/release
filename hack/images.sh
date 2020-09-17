1fa6c948989036b35d1e1fb424b8295b6862c89b
Nikolaos Moraitis <nmoraiti@redhat.com>
2020-09-18 00:06:04 +0200

    Revert "Update prow to v20200917-481df8ba90, and other images as necessary."

--- hack/images.sh
+++ hack/images.sh
@@ -1,2 +1,2 @@
-MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20200917-481df8ba90
-MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20200917-481df8ba90
+MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20200917-989540424b
+MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20200917-989540424b
