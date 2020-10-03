34a130ef49ef4f4956dcefe419c374cb5959f9e1
Dan Winship <danwinship@redhat.com>
2020-10-03 18:26:42 -0400

    Fix sdn ovs-multitenant job

--- ci-operator/step-registry/sdn/conf/multi-manifest/sdn-conf-multi-manifest-commands.sh
+++ ci-operator/step-registry/sdn/conf/multi-manifest/sdn-conf-multi-manifest-commands.sh
@@ -4,20 +4,13 @@ set -o nounset
 set -o pipefail
 
 cat > "${SHARED_DIR}/manifest_cluster-network-03-config.yml" << EOF
-apiVersion: config.openshift.io/v1
+apiVersion: operator.openshift.io/v1
 kind: Network
 metadata:
   name: cluster
 spec:
-  serviceNetwork:
-  - "172.30.0.0/16"
-  clusterNetwork:
-  - cidr: "10.128.0.0/14"
-    hostPrefix: 23
-  networkType: OpenShiftSDN
-  openshiftSDNConfig:
-    mode: Multitenant
-    vxlanPort: 4789
-    enableUnidling: true
-    useExternalOpenvswitch: false
+  defaultNetwork:
+    type: OpenShiftSDN
+    openshiftSDNConfig:
+      mode: Multitenant
 EOF
