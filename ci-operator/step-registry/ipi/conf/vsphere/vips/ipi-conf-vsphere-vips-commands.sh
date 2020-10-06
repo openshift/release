d9681b8e3814ac6e61c556c54a8c2a51530e94bc
Joseph Callen <jcallen@redhat.com>
2020-10-05 20:27:10 -0400

    vsphere - move jobs to individual networks
    Using leased resource (boskos) to provide a
    port group name within vSphere. That name:
    `ci-segment-N` with N as the third octet of
    the subnet e.g. `192.168.N.0/27`.
    
    Approver list change, add check into workflow

--- ci-operator/step-registry/ipi/conf/vsphere/vips/ipi-conf-vsphere-vips-commands.sh
+++ ci-operator/step-registry/ipi/conf/vsphere/vips/ipi-conf-vsphere-vips-commands.sh
@@ -4,31 +4,16 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-tfvars_path=/var/run/secrets/ci.openshift.io/cluster-profile/vmc.secret.auto.tfvars
-cluster_name=${NAMESPACE}-${JOB_NAME_HASH}
-ipam_token=$(grep -oP 'ipam_token\s*=\s*"\K[^"]+' ${tfvars_path})
+# ensure LEASED_RESOURCE is set
+if [[ -z "${LEASED_RESOURCE}" ]]; then
+  echo "Failed to acquire lease"
+  exit 1
+fi
 
-# Array to hold virtual ips:
-# 0: API
-# 1: Ingress
-declare -a vips
+third_octet=$(grep -oP 'ci-segment-\K[[:digit:]]+' <(echo "${LEASED_RESOURCE}"))
 
-echo "Reserving virtual ip addresses from the IPAM server..."
-for i in {0..1}
-do
-  args=$(jq -n \
-            --arg hostn "$cluster_name-$i" \
-            --arg token "$ipam_token" \
-            '{network: "172.31.252.0", hostname: $hostn, ipam: "ipam.vmc.ci.openshift.org", ipam_token: $token}')
-
-  vip_json=$(echo "$args" | bash <(curl -s https://raw.githubusercontent.com/openshift/installer/master/upi/vsphere/ipam/cidr_to_ip.sh))
-  vips[$i]=$(echo "$vip_json" | jq -r .ip_address )
-  if [[ -z ${vips[$i]} ]]; then
-    echo "error: Unable to reserve virtual IP address, exiting" 1>&2
-    exit 1
-  fi
-  echo "${vips[$i]}" >> "${SHARED_DIR}"/vips.txt
-done
+echo "192.168.${third_octet}.2" >> "${SHARED_DIR}"/vips.txt
+echo "192.168.${third_octet}.3" >> "${SHARED_DIR}"/vips.txt
 
 echo "Reserved the following IP addresses..."
 cat "${SHARED_DIR}"/vips.txt
