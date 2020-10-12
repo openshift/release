5e8ba7c58ee7b12a1aad50379f5e0a64b37f5b81
Joseph Callen <jcallen@redhat.com>
2020-10-12 14:57:27 -0400

    vSphere: remove the use of upi-installer image; which will prepare for upgrade testing
    The `upi-installer` image was used in IPI because our previous
    vSphere enviornment used a self-signed certificate.
    Since moving to VMC it is a valid certificate we no longer need
    to add it to the trust.
    
    The `upi-installer` image also causes issues with trying
    to test upgrades between versions since its not apart of
    `release`.
    
    Add the ability to install the `awscli` package via pip for
    both python2 and python3 since 4.5 and 4.6 use different versions.

--- ci-operator/step-registry/ipi/conf/vsphere/dns/ipi-conf-vsphere-dns-commands.sh
+++ ci-operator/step-registry/ipi/conf/vsphere/dns/ipi-conf-vsphere-dns-commands.sh
@@ -13,6 +13,26 @@ cluster_domain="${cluster_name}.${base_domain}"
 export AWS_SHARED_CREDENTIALS_FILE=${CLUSTER_PROFILE_DIR}/.awscred
 export AWS_MAX_ATTEMPTS=7
 export AWS_RETRY_MODE=adaptive
+export HOME=/tmp
+
+if ! command -v aws &> /dev/null
+then
+
+    echo "$(date -u --rfc-3339=seconds) - Install AWS cli..."
+    python_version=$(python -c 'import sys;print(sys.version_info.major)')
+    export PATH="${HOME}/.local/bin:${PATH}"
+    if [[ $python_version -eq 2 ]]
+    then
+        easy_install --user pip  # our Python 2.7.5 is even too old for ensurepip
+        pip install --user awscli
+    elif [[ $python_version -eq 3 ]]
+    then
+        pip3 install --user awscli
+    else
+        echo "$(date -u --rfc-3339=seconds) - No pip available exiting..."
+        exit 1
+    fi
+fi
 
 # Load array created in setup-vips:
 # 0: API
