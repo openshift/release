8a04e8acac0d6a972eaee4a12a23c8520120fa65
Brad Williams <brad.williams@redhat.com>
2020-10-15 09:27:41 -0400

    Adding files-cache redirect resources

--- hack/generators/release-controllers/content/__init__.py
+++ hack/generators/release-controllers/content/__init__.py
@@ -5,6 +5,6 @@ from .osd_rc_rbac import add_osd_rc_service_account_resources
 from .art_publish_permissions import add_art_publish
 from .art_namespaces_config_updater import add_art_namespace_config_updater_rbac
 from .art_namespaces_rbac import add_imagestream_namespace_rbac
-from .redirect_and_files_cache_resources import add_redirect_and_files_cache_resources
+from .redirect_resources import add_redirect_resources
 from .art_rpm_mirroring_services import add_rpm_mirror_service
 from .bump_versioned_resources import bump_versioned_resources
