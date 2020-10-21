89ce786dcfac62a4f51436b3f98b55dae725988d
Jan Safranek <jsafrane@redhat.com>
2020-10-21 17:08:38 +0200

    Add random sleep to e2e-aws-csi jobs
    The tests flake with "connection refused" at the startup. Add a random
    sleep before the test starts to check if it's caused by the test themselves
    or by OCP not being ready to process requests so early after installation.
    
    Using random sleep interval to gather data when there is a sleep and when
    there is none.
    
    TODO: remove after few weeks.

--- ci-operator/step-registry/storage/conf/csi-aws-ebs/storage-conf-csi-aws-ebs-commands.sh
+++ ci-operator/step-registry/storage/conf/csi-aws-ebs/storage-conf-csi-aws-ebs-commands.sh
@@ -6,6 +6,15 @@ set -o pipefail
 cd /go/src/github.com/openshift/aws-ebs-csi-driver-operator
 cp test/e2e/manifest.yaml ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
 
+# Temporary hack to postpone openshift-test startup by few minutes to de-flake
+# https://bugzilla.redhat.com/show_bug.cgi?id=1890131
+# TODO(jsafrane): remove in a few days (weeks)
+# Get a random number between 0-2.
+SLEEP_MINUTES=$( shuf -i 0-2 -n 1 )
+SLEEP_SECONDS=$[ $SLEEP_MINUTES * 60 ]
+echo POSTPONING TESTS BY: $SLEEP_SECONDS
+sleep $SLEEP_SECONDS
+
 # For debugging
 echo "Using ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}:"
 cat ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
