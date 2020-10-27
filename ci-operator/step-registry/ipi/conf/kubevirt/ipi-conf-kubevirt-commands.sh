c1576654c8f0d558f547878fbe76921359332750
Danielle Barda <dbarda@redhat.com>
2020-10-27 12:31:27 +0200

    Adding e2e-kubevirt

--- ci-operator/step-registry/ipi/conf/kubevirt/ipi-conf-kubevirt-commands.sh
+++ ci-operator/step-registry/ipi/conf/kubevirt/ipi-conf-kubevirt-commands.sh
@@ -6,27 +6,29 @@ set -o pipefail
 
 CONFIG="${SHARED_DIR}/install-config.yaml"
 
-
-KUBEVIRT_BASE_DOMAIN="tenant1.origin-on-kubevirt.dev.openshift.com"
-KUBEVIRT_API_VIP=192.168.123.15
-KUBEVIRT_INGRESS_VIP=192.168.123.20
-KUBEVIRT_KUBECONFIG=${HOME}/.kube/config
+KUBEVIRT_BASE_DOMAIN="testci.tenant1.dev.openshift.com"
+KUBEVIRT_API_VIP=10.123.124.15
+KUBEVIRT_INGRESS_VIP=10.123.124.20
+KUBEVIRT_CIDR="10.123.124.0/24"
 KUBEVIRT_NAMESPACE=tenantcluster
-KUBEVIRT_NETWORK_NAME=tenantcluster
+KUBEVIRT_NETWORK_NAME=mynet
 KUBEVIRT_TENANT_STORAGE_CLASS_NAME=standard
-KUBEVIRT_VOLUME_ACCESS_MODE=ReadWriteMany
+KUBEVIRT_VOLUME_ACCESS_MODE=ReadWriteOnce
 
 cat >> "${CONFIG}" << EOF
 baseDomain: ${KUBEVIRT_BASE_DOMAIN}
+metadata:
+  name: ${KUBEVIRT_NAMESPACE}
+networking:
+  machineNetwork:
+  - cidr: ${KUBEVIRT_CIDR}
 platform:
   kubevirt:
     # TODO this section is WIP - see the installer PR
-    IngressVIP: ${KUBEVIRT_INGRESS_VIP}
+    ingressVIP: ${KUBEVIRT_INGRESS_VIP}
     apiVIP: ${KUBEVIRT_API_VIP}
-    kubeconfig: ${KUBEVIRT_KUBECONFIG}
     namespace: ${KUBEVIRT_NAMESPACE}
     networkName: ${KUBEVIRT_NETWORK_NAME}
     storageClass: ${KUBEVIRT_TENANT_STORAGE_CLASS_NAME}
     persistentVolumeAccessMode: ${KUBEVIRT_VOLUME_ACCESS_MODE}
 EOF
-
