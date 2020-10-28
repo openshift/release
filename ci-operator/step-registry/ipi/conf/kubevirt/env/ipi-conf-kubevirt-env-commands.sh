e52432cac3338e214a270d2f0e476b18b7330dc7
Danielle Barda <dbarda@redhat.com>
2020-10-28 09:36:03 +0200

    Adding new ipi-conf for kubevirt

---
+++ ci-operator/step-registry/ipi/conf/kubevirt/env/ipi-conf-kubevirt-env-commands.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+cluster_name=${NAMESPACE}-${JOB_NAME_HASH}
+
+out=${SHARED_DIR}/install-config.yaml
+
+if [[ -z "$RELEASE_IMAGE_LATEST" ]]; then
+  echo "RELEASE_IMAGE_LATEST is an empty string, exiting"
+  exit 1
+fi
+
+echo "Installing from release ${RELEASE_IMAGE_LATEST}"
+
+ssh_pub_key=$(<"${CLUSTER_PROFILE_DIR}/ssh-publickey")
+pull_secret=$(<"${CLUSTER_PROFILE_DIR}/pull-secret")
+
+cat > "${out}" << EOF
+apiVersion: v1
+metadata:
+  name: ${cluster_name}
+pullSecret: >
+  ${pull_secret}
+sshKey: |
+  ${ssh_pub_key}
+EOF
