f82060bdb695c7a9be3a6767e5a8185ee6a3f4f1
Bruno Barcarol Guimarães <bbarcaro@redhat.com>
2020-10-28 09:27:01 +0000

    Revert "Update prow to v20201027-f58a04556a, and other images as necessary."

--- hack/images.sh
+++ hack/images.sh
@@ -1,2 +1,2 @@
-MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20201027-f58a04556a
-MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20201027-f58a04556a
+MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20201023-18bef78278
+MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20201023-18bef78278
