c0a3eb544b597bc1d424daaeefd335988085120f
Danielle Barda <dbarda@redhat.com>
2020-11-05 10:36:27 +0200

    Duplicate ipi-install-install to ipi-install-kubevirt-install

---
+++ ci-operator/step-registry/ipi/install/kubevirt/install/ipi-install-kubevirt-install-commands.sh
@@ -0,0 +1,76 @@
+#!/bin/bash -x
+
+echo "*****1"
+set -o nounset
+set -o errexit
+set -o pipefail
+
+trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
+
+if [[ -z "$OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE" ]]; then
+  echo "OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE is an empty string, exiting"
+  exit 1
+fi
+
+echo "*****2"
+echo "Installing from release ${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE}"
+export SSH_PRIV_KEY_PATH=${CLUSTER_PROFILE_DIR}/ssh-privatekey
+export PULL_SECRET_PATH=${CLUSTER_PROFILE_DIR}/pull-secret
+export OPENSHIFT_INSTALL_INVOKER=openshift-internal-ci/${JOB_NAME}/${BUILD_ID}
+export HOME=/tmp
+
+case "${CLUSTER_TYPE}" in
+aws) export AWS_SHARED_CREDENTIALS_FILE=${CLUSTER_PROFILE_DIR}/.awscred;;
+azure4) export AZURE_AUTH_LOCATION=${CLUSTER_PROFILE_DIR}/osServicePrincipal.json;;
+gcp) export GOOGLE_CLOUD_KEYFILE_JSON=${CLUSTER_PROFILE_DIR}/gce.json;;
+kubevirt) export KUBEVIRT_KUBECONFIG=${HOME}/.kube/config;;
+vsphere) ;;
+openstack) export OS_CLIENT_CONFIG_FILE=${CLUSTER_PROFILE_DIR}/clouds.yaml ;;
+openstack-vexxhost) export OS_CLIENT_CONFIG_FILE=${CLUSTER_PROFILE_DIR}/clouds.yaml ;;
+*) echo >&2 "Unsupported cluster type '${CLUSTER_TYPE}'"
+esac
+
+echo "*****3"
+dir=/tmp/installer
+mkdir "${dir}/"
+cp "${SHARED_DIR}/install-config.yaml" "${dir}/"
+cat ${dir}/install-config.yaml
+
+echo "*****4"
+KUBECONFIG=/tmp/secret-kube/kubeconfig-infra-cluster
+cat /tmp/secret-kube/kubeconfig-infra-cluster
+
+echo "*****5"
+# move private key to ~/.ssh/ so that installer can use it to gather logs on
+# bootstrap failure
+mkdir -p ~/.ssh
+cp "${SSH_PRIV_KEY_PATH}" ~/.ssh/
+
+echo "*****6"
+openshift-install --dir="${dir}" create manifests &
+wait "$!"
+
+echo "*****7"
+sed -i '/^  channel:/d' "${dir}/manifests/cvo-overrides.yaml"
+
+while IFS= read -r -d '' item
+do
+  manifest="$( basename "${item}" )"
+  cp "${item}" "${dir}/manifests/${manifest##manifest_}"
+done <   <( find "${SHARED_DIR}" -name "manifest_*.yml" -print0)
+
+TF_LOG=debug openshift-install --dir="${dir}" create cluster 2>&1 | grep --line-buffered -v password &
+
+set +e
+wait "$!"
+ret="$?"
+cp "${dir}"/log-bundle-*.tar.gz "${ARTIFACT_DIR}/" 2>/dev/null
+set -e
+
+sed 's/password: .*/password: REDACTED/' "${dir}/.openshift_install.log" >"${ARTIFACT_DIR}/.openshift_install.log"
+cp \
+    -t "${SHARED_DIR}" \
+    "${dir}/auth/kubeconfig" \
+    "${dir}/auth/kubeadmin-password" \
+    "${dir}/metadata.json"
+exit "$ret"
