5a8f1211de84f4d74fe853ab0d3f05e299c158d7
Raul Sevilla <rsevilla@redhat.com>
2020-11-29 00:24:15 +0100

    Add copy-cli step
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- ci-operator/step-registry/openshift-perfscale/kubelet-density/openshift-perfscale-kubelet-density-commands.sh
+++ ci-operator/step-registry/openshift-perfscale/kubelet-density/openshift-perfscale-kubelet-density-commands.sh
@@ -9,17 +9,17 @@ export ES_SERVER=https://search-cloud-perf-lqrf3jjtaqo7727m7ynd2xyt4y.us-west-2.
 export ES_PORT=443
 export ES_INDEX=ripsaw-kube-burner
 export PROM_URL=https://prometheus-k8s.openshift-monitoring.svc.cluster.local:9091
-export JOB_TIMEOUT=3600
+export JOB_TIMEOUT=1800
 export WORKLOAD_NODE=""
 export CERBERUS_URL=""
 export STEP_SIZE=30s
 export CLEANUP=false
 export CLEANUP_WHEN_FINISH=false
-export LOG_LEVEL=debug
+export LOG_LEVEL=info
 
 # Kubelet-density and kubelet-density-heavy specific
 export NODE_COUNT=3
-export PODS_PER_NODE=250
+export PODS_PER_NODE=50
 
 # Metadata
 export METADATA_COLLECTION=true
@@ -29,12 +29,9 @@ export LOG_STREAMING=true
 export CLEANUP_WHEN_FINISH=true
 
 pushd /tmp
-curl -O https://raw.githubusercontent.com/cloud-bulldozer/e2e-benchmarking/master/workloads/kube-burner/common.sh
-curl -O https://raw.githubusercontent.com/cloud-bulldozer/e2e-benchmarking/master/workloads/kube-burner/kube-burner-crd.yaml
-curl -O https://raw.githubusercontent.com/cloud-bulldozer/e2e-benchmarking/master/workloads/kube-burner/run_kubeletdensity_test_fromgit.sh
-chmod +x run_kubeletdensity_test_fromgit.sh
-./run_kubeletdensity_test_fromgit.sh
+curl -sS https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.6.6/openshift-client-linux-4.6.6.tar.gz | tar xz
+export PATH=${PATH}:/tmp
 
-# Wait one minute before triggering next workload
-# sleep 1m
-# exec ./run_kubeletdensity-heavy_test_fromgit.sh
+git clone https://github.com/cloud-bulldozer/e2e-benchmarking.git --depth=1
+pushd e2e-benchmarking/workloads/kube-burner
+./run_kubeletdensity_test_fromgit.sh
