270607e5ebe6b61434ca38c2deb4cbb41b8be8a7
Eran Israeli <eisraeli@redhat.com>
2020-11-30 15:57:35 +0200

    WIP: Assisted-service workflow

--- ci-operator/step-registry/assisted/e2e/init/assisted-e2e-init-commands.sh
+++ ci-operator/step-registry/assisted/e2e/init/assisted-e2e-init-commands.sh
@@ -4,4 +4,14 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# # TODO: Also on multistage test the shared directory is currently
+# # read-only, so some oc commands might fail to read the kubeconfig file.
+# ASSISTED_SHARED_DIR=/tmp/shared_dir
+# mkdir -p $ASSISTED_SHARED_DIR
+# cp -f ${SHARED_DIR}/* ${ASSISTED_SHARED_DIR}
+# export KUBECONFIG=${ASSISTED_SHARED_DIR}/kubeconfig
+
 echo "test"
+oc whoami
+cat /etc/os-release
+oc get pods -o wide
