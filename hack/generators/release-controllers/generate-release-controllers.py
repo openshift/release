ea06fb5c1b9c94bbbcfcff682693085048ef4e93
Brad Williams <brad.williams@redhat.com>
2020-12-09 12:44:27 -0500

    Adding the ability to automatically generate the Next version's configuration

--- hack/generators/release-controllers/generate-release-controllers.py
+++ hack/generators/release-controllers/generate-release-controllers.py
@@ -15,9 +15,9 @@
 import logging
 import sys
 import pathlib
-import glob
 import os
 import json
+import argparse
 
 # Change python path so we can import genlib
 sys.path.append(str(pathlib.Path(__file__).absolute().parent.parent.joinpath('lib')))
@@ -25,20 +25,51 @@ import genlib
 sys.path.append(str(pathlib.Path(__file__).absolute().parents[0]))
 import content
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s:%(levelname)-7s] %(message)s')
 logger = logging.getLogger()
+
+
+class RCPaths:
+    def __init__(self, git_clone_dir):
+        self.path_base = pathlib.Path(git_clone_dir)
+        self.path_rc_deployments = self.path_base.joinpath('clusters/app.ci/release-controller')
+        self.path_rc_release_resources = self.path_base.joinpath('core-services/release-controller')
+
+        self.path_rc_build_configs = self.path_rc_release_resources
+        self.path_rc_build_configs.mkdir(exist_ok=True)
+
+        self.path_rc_annotations = self.path_rc_release_resources.joinpath('_releases')
+        self.path_priv_rc_annotations = self.path_rc_annotations.joinpath('priv')  # location where priv release controller annotations are generated
+        self.path_priv_rc_annotations.mkdir(exist_ok=True)
+
+        self.path_ci_operator_config_release = self.path_base.joinpath('ci-operator/config/openshift/release')
+        self.path_ci_operator_jobs_release = self.path_base.joinpath('ci-operator/jobs/openshift/release')
 
 
 class Config:
 
-    def __init__(self, releases_4x):
+    def __init__(self, git_clone_dir):
         self.rc_deployment_domain = 'apps.ci.l2s4.p1.openshiftapps.com'
         self.rc_release_domain = 'svc.ci.openshift.org'
         self.rc_deployment_namespace = 'ci'
         self.arches = ('x86_64', 's390x', 'ppc64le')
-        self.releases_4x = releases_4x
+        self.paths = RCPaths(git_clone_dir)
+        self.releases = self._get_releases()
 
-    def get_arch_suffix(self, arch):
+    def _get_releases(self):
+        releases = []
+
+        # Collect the 4.x releases...
+        for name in self.paths.path_ci_operator_jobs_release.glob('openshift-release-release-4.*-periodics.yaml'):
+            bn = os.path.splitext(os.path.basename(name))[0]  # e.g. openshift-release-release-4.4-periodics
+            major_minor = bn.split('-')[-2]  # 4.4
+            releases.append(major_minor)
+
+        releases.sort()  # Glob does provide any guarantees on ordering, so force an order by sorting.
+        return releases
+
+    @staticmethod
+    def get_arch_suffix(arch):
         suffix = ''
         if arch not in ('amd64', 'x86_64'):
             suffix += f'-{arch}'
@@ -71,59 +102,46 @@ class Context:
         self.rc_service_name = self.rc_route_name
 
 
-def run(git_clone_dir):
-    releases_4x = []
-    for name in glob.glob(f'{git_clone_dir}/ci-operator/jobs/openshift/release/openshift-release-release-4.*-periodics.yaml'):
-        bn = os.path.splitext(os.path.basename(name))[0]  # e.g. openshift-release-release-4.4-periodics
-        major_minor = bn.split('-')[-2]  # 4.4
-        releases_4x.append(major_minor)
+def run(git_clone_dir, bump=False):
 
-    path_base = pathlib.Path(git_clone_dir)
-    path_rc_deployments = path_base.joinpath('clusters/app.ci/release-controller')
-    path_rc_release_resources = path_base.joinpath('core-services/release-controller')
+    config = Config(git_clone_dir)
 
-    path_rc_build_configs = path_rc_release_resources
-    path_rc_build_configs.mkdir(exist_ok=True)
+    # Generate version specific files (if necessary)
+    content.bump_versioned_resources(config, bump)
 
-    path_rc_annotations = path_rc_release_resources.joinpath('_releases')
-    path_priv_rc_annotations = path_rc_annotations.joinpath('priv')  # location where priv release controller annotations are generated
-    path_priv_rc_annotations.mkdir(exist_ok=True)
-
-    releases_4x.sort()  # Glob does provide any guarantees on ordering, so force an order by sorting.
-    config = Config(releases_4x)
     for private in (False, True):
         for arch in config.arches:
             context = Context(config, arch, private)
 
-            with genlib.GenDoc(path_rc_deployments.joinpath(f'deploy-{context.is_namespace}-controller.yaml'), context) as gendoc:
+            with genlib.GenDoc(config.paths.path_rc_deployments.joinpath(f'deploy-{context.is_namespace}-controller.yaml'), context) as gendoc:
                 content.add_osd_rc_deployments(gendoc)
 
-            with genlib.GenDoc(path_rc_release_resources.joinpath(f'admin_config_updater_rbac{context.suffix}.yaml'), context) as gendoc:
+            with genlib.GenDoc(config.paths.path_rc_release_resources.joinpath(f'admin_config_updater_rbac{context.suffix}.yaml'), context) as gendoc:
                 content.add_art_namespace_config_updater_rbac(gendoc)
 
-            with genlib.GenDoc(path_rc_release_resources.joinpath(f'admin_deploy-{context.is_namespace}-controller.yaml'), context) as gendoc:
+            with genlib.GenDoc(config.paths.path_rc_release_resources.joinpath(f'admin_deploy-{context.is_namespace}-controller.yaml'), context) as gendoc:
                 content.add_imagestream_namespace_rbac(gendoc)
 
-            with genlib.GenDoc(path_rc_release_resources.joinpath(f'deploy-{context.is_namespace}-controller.yaml'), context) as gendoc:
+            with genlib.GenDoc(config.paths.path_rc_release_resources.joinpath(f'deploy-{context.is_namespace}-controller.yaml'), context) as gendoc:
                 content.add_redirect_and_files_cache_resources(gendoc)
 
-    with genlib.GenDoc(path_rc_deployments.joinpath('serviceaccount.yaml'), context=config) as gendoc:
+    with genlib.GenDoc(config.paths.path_rc_deployments.joinpath('serviceaccount.yaml'), context=config) as gendoc:
         content.add_osd_rc_service_account_resources(gendoc)
 
-    with genlib.GenDoc(path_rc_release_resources.joinpath('admin_deploy-ocp-publish-art.yaml'), context=config) as gendoc:
+    with genlib.GenDoc(config.paths.path_rc_release_resources.joinpath('admin_deploy-ocp-publish-art.yaml'), context=config) as gendoc:
         content.add_art_publish(gendoc)
 
-    with genlib.GenDoc(path_rc_release_resources.joinpath(f'rpms-ocp-3.11.yaml'), context=config) as gendoc:
+    with genlib.GenDoc(config.paths.path_rc_release_resources.joinpath(f'rpms-ocp-3.11.yaml'), context=config) as gendoc:
         content.add_rpm_mirror_service(gendoc, git_clone_dir, '3.11')
 
-    for major_minor in releases_4x:
-        with genlib.GenDoc(path_rc_release_resources.joinpath(f'rpms-ocp-{major_minor}.yaml'), context=config) as gendoc:
+    for major_minor in config.releases:
+        with genlib.GenDoc(config.paths.path_rc_release_resources.joinpath(f'rpms-ocp-{major_minor}.yaml'), context=config) as gendoc:
             content.add_rpm_mirror_service(gendoc, git_clone_dir, major_minor)
 
         # If there is an annotation defined for the public release controller, use it as a template
         # for the private annotations.
-        for annotation_path in path_rc_annotations.glob(f'release-ocp-*.json'):
-            if annotation_path.name.endswith(('ci.json')):  # There are no CI annotations for the private controllers
+        for annotation_path in config.paths.path_rc_annotations.glob(f'release-ocp-*.json'):
+            if annotation_path.name.endswith('ci.json'):  # There are no CI annotations for the private controllers
                 continue
             if '-stable' in annotation_path.name:  # There are no stable streams in private release controllers
                 continue
@@ -144,12 +162,19 @@ def run(git_clone_dir):
                 # TODO: Private jobs are disabled until the -priv variants can be generated by prowgen
                 test_config['disabled'] = True
 
-            with path_priv_rc_annotations.joinpath(annotation_filename).open(mode='w+', encoding='utf-8') as f:
+            with config.paths.path_priv_rc_annotations.joinpath(annotation_filename).open(mode='w+', encoding='utf-8') as f:
                 json.dump(priv_annotation, f, sort_keys=True, indent=4)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2 or sys.argv[1] in ('--help', '-h', '-help'):
-        print('Required parameter missing. Specify path to openshift/release clone directory.')
-        exit(1)
-    run(sys.argv[1])
+    parser = argparse.ArgumentParser(description='Release Controller Configuration Generator')
+    parser.add_argument('-b', '--bump', help='Create configuration for the next release (4.x+1).', action='store_true')
+    parser.add_argument('-v', '--verbose', help='Enable verbose output.', action='store_true')
+    parser.add_argument('clone_dir', help='Specify path to openshift/release clone directory.')
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+
+    run(args.clone_dir, args.bump)
