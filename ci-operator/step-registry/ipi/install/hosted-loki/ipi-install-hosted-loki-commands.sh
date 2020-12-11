9761601fde1cb4e10edc181585ccc8c111a56f8d
Vadim Rutkovsky <vrutkovs@redhat.com>
2020-12-11 14:21:56 +0100

    hosted-loki: tweak backoff_config and batchwait
    Accumulate more logs before sending to hosted Loki and tweak backoff 
    config. This should prevent promtail from throwing HTTP 429 errors.

--- ci-operator/step-registry/ipi/install/hosted-loki/ipi-install-hosted-loki-commands.sh
+++ ci-operator/step-registry/ipi/install/hosted-loki/ipi-install-hosted-loki-commands.sh
@@ -71,11 +71,11 @@ data:
   promtail.yaml: |-
     client:
       backoff_config:
-        max_period: 5s
+        max_period: 5m
         max_retries: 20
-        min_period: 100ms
+        min_period: 1s
       batchsize: 102400
-      batchwait: 1s
+      batchwait: 10s
       bearer_token_file: /tmp/shared/bearer_token
       timeout: 10s
       url: ${LOKI_ENDPOINT}/push
