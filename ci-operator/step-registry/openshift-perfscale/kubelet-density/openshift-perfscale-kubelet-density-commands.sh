9dca388d252c8069af9c95e1b58b31a92d826031
Raul Sevilla <rsevilla@redhat.com>
2020-12-11 14:34:03 +0100

    Alert profile
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- ci-operator/step-registry/openshift-perfscale/kubelet-density/openshift-perfscale-kubelet-density-commands.sh
+++ ci-operator/step-registry/openshift-perfscale/kubelet-density/openshift-perfscale-kubelet-density-commands.sh
@@ -2,6 +2,7 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -x 
 
 export QPS=20
 export BURST=20
@@ -29,9 +30,21 @@ export LOG_STREAMING=true
 export CLEANUP_WHEN_FINISH=true
 
 pushd /tmp
-curl -sS https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.6.6/openshift-client-linux-4.6.6.tar.gz | tar xz
+curl -sS https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable/openshift-client-linux.tar.gz | tar xz
 export PATH=${PATH}:/tmp
 
-git clone https://github.com/cloud-bulldozer/e2e-benchmarking.git --depth=1
-pushd e2e-benchmarking/workloads/kube-burner
-./run_kubeletdensity_test_fromgit.sh
+#git clone https://github.com/cloud-bulldozer/e2e-benchmarking.git --depth=1
+token=$(oc sa get-token -n openshift-monitoring prometheus-k8s)
+prometheus_url=https://$(oc get route -n openshift-monitoring prometheus-k8s -o yaml -o jsonpath="{.spec.host}")
+
+warmup=https://raw.githubusercontent.com/rsevilla87/cluster-perf-ci/master/warm-up.yml
+load_cluster=https://raw.githubusercontent.com/rsevilla87/cluster-perf-ci/master/load-cluster.yml
+git clone https://github.com/cloud-bulldozer/kube-burner.git --depth=1
+pushd kube-burner
+make build -j $(nproc)
+# Warm-up
+./bin/kube-burner init -c ${warmup} -u ${prometheus_url} -t ${token} -a https://raw.githubusercontent.com/rsevilla87/cluster-perf-ci/master/alert-profiles/generalistic.yml --uuid $(uuidgen)
+
+# Load-cluster
+./bin/kube-burner init -c ${load_cluster} -u ${prometheus_url} -t ${token} -a https://raw.githubusercontent.com/rsevilla87/cluster-perf-ci/master/alert-profiles/generalistic.yml --uuid $(uuidgen) -m https://raw.githubusercontent.com/rsevilla87/cluster-perf-ci/master/metric-profiles/metrics.yml 2>&1 | tee -a kube-burner.log
+cp kube-burner-job.log ${ARTIFACT_DIR}/kube-burner-job.log
