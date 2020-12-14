bae0b05056de17bb642c17e1302ae7866eb9f6b6
W. Trevor King <wking@tremily.us>
2020-12-14 14:20:24 -0800

    ci-operator/step-registry/ipi/conf/aws: Get region from Boskos lease
    Like 811bd7a15c (ci-operator/step-registry/ipi/conf/azure: Get region
    from Boskos lease, 2020-10-14, #12584), but for AWS.
    
    I'm keeping a switch for AWS to give folks a pattern for selecting
    zones, if AWS breaks a zone in a particular region.  We should
    probably distribute that (and the shared subnets, for shared-subnet
    tests?) via leases as well, but baby steps.
    
    I'm leaving ci-operator/templates alone; hopefully those will be gone
    soon.  I've already updated ci-tools with
    openshift/ci-tools@00ebab17e1 (pkg/steps/clusterinstall/template: Get
    region from Boskos lease, 2020-12-11, openshift/ci-tools#1527).
    
    I'm also normalizing to uppercase shell variables, now that we are no
    longer constrained by Go template expansion.  Hmm, at least that's why
    I thought the variables used to be lowercase, see 55286bc6c1
    (ci-operator/templates/openshift/installer/cluster-launch-installer-upi-e2e:
    Push AWS-specific default base domain down into the template,
    2019-09-23, #5151).  But looking at the templates when f04f8f9389
    (step-registry: add configure and install IPI steps, 2020-01-14, #6708),
    I'm now not sure why these step commands were using lowercase
    variable names.

--- ci-operator/step-registry/ipi/conf/aws/ipi-conf-aws-commands.sh
+++ ci-operator/step-registry/ipi/conf/aws/ipi-conf-aws-commands.sh
@@ -8,16 +8,13 @@ CONFIG="${SHARED_DIR}/install-config.yaml"
 
 expiration_date=$(date -d '4 hours' --iso=minutes --utc)
 
-case "$((RANDOM % 4))" in
-0) aws_region=us-east-1
-   zone_1=us-east-1b
-   zone_2=us-east-1c;;
-1) aws_region=us-east-2;;
-2) aws_region=us-west-1;;
-3) aws_region=us-west-2;;
-*) echo >&2 "invalid AWS region index"; exit 1;;
+REGION="${LEASED_RESOURCE}"
+case "${REGION}" in
+us-east-1)
+   ZONE_1=us-east-1b
+   ZONE_2=us-east-1c;;
 esac
-echo "AWS region: ${aws_region} (zones: ${zone_1:-${aws_region}a} ${zone_2:-${aws_region}b})"
+echo "AWS region: ${REGION} (zones: ${ZONE_1:-${REGION}a} ${ZONE_2:-${REGION}b})"
 
 cat >> "${CONFIG}" << EOF
 baseDomain: origin-ci-int-aws.dev.rhcloud.com
@@ -26,19 +23,19 @@ controlPlane:
   platform:
     aws:
       zones:
-      - ${zone_1:-${aws_region}a}
-      - ${zone_2:-${aws_region}b}
+      - ${ZONE1:-${REGION}a}
+      - ${ZONE2:-${REGION}b}
 compute:
 - name: worker
   platform:
     aws:
       type: ${COMPUTE_NODE_TYPE}
       zones:
-      - ${zone_1:-${aws_region}a}
-      - ${zone_2:-${aws_region}b}
+      - ${ZONE1:-${REGION}a}
+      - ${ZONE2:-${REGION}b}
 platform:
   aws:
-    region: ${aws_region}
+    region: ${REGION}
     userTags:
       expirationDate: ${expiration_date}
 EOF
