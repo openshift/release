3895208f1452beb1ae46267fc7d3bb1f70d0461c
bipuladh <badhikar@redhat.com>
2021-01-13 00:06:39 +0530

    Add support for selecting multiple or single zone based on environment variable

--- ci-operator/step-registry/ipi/conf/aws/ipi-conf-aws-commands.sh
+++ ci-operator/step-registry/ipi/conf/aws/ipi-conf-aws-commands.sh
@@ -8,13 +8,24 @@ CONFIG="${SHARED_DIR}/install-config.yaml"
 
 expiration_date=$(date -d '4 hours' --iso=minutes --utc)
 
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
 REGION="${LEASED_RESOURCE}"
 case "${REGION}" in
 us-east-1)
-   ZONE_1=us-east-1b
-   ZONE_2=us-east-1c;;
+    ZONES=("us-east-1b" "us-east-1c")
+    ;;
+*)
+    ZONES=("${REGION}a" "${REGION}b")
 esac
-echo "AWS region: ${REGION} (zones: ${ZONE_1:-${REGION}a} ${ZONE_2:-${REGION}b})"
+
+ZONES_COUNT=${ZONES_COUNT:-2}
+ZONES=("${ZONES[@]:0:${ZONES_COUNT}}")
+ZONES_STR="[ "
+ZONES_STR+=$(join_by , "${ZONES[@]}")
+ZONES_STR+=" ]"
+echo "AWS region: ${REGION} (zones: ${ZONES_STR})"
+
 
 cat >> "${CONFIG}" << EOF
 baseDomain: origin-ci-int-aws.dev.rhcloud.com
@@ -22,17 +33,13 @@ controlPlane:
   name: master
   platform:
     aws:
-      zones:
-      - ${ZONE1:-${REGION}a}
-      - ${ZONE2:-${REGION}b}
+      zones: ${ZONES_STR}
 compute:
 - name: worker
   platform:
     aws:
       type: ${COMPUTE_NODE_TYPE}
-      zones:
-      - ${ZONE1:-${REGION}a}
-      - ${ZONE2:-${REGION}b}
+      zones: ${ZONES_STR}
 platform:
   aws:
     region: ${REGION}
