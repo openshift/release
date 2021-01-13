4f4c4538b487e21a1545cc5412ab792e41edcb99
Danielle Barda <dbarda@redhat.com>
2021-01-13 21:02:18 +0200

    kubevirt provider CI: Adding a support in leases

--- ci-operator/step-registry/ipi/conf/kubevirt/ipi-conf-kubevirt-commands.sh
+++ ci-operator/step-registry/ipi/conf/kubevirt/ipi-conf-kubevirt-commands.sh
@@ -4,16 +4,25 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# ensure LEASED_RESOURCE is set
+if [[ -z "${LEASED_RESOURCE}" ]]; then
+  echo "Failed to acquire lease"
+  exit 1
+fi
+
+echo "Tenant cluster: ${LEASED_RESOURCE}"
+
+HOME=/tmp
 CONFIG="${SHARED_DIR}/install-config.yaml"
 
 KUBEVIRT_BASE_DOMAIN="ci.ocpcnv.ovirt.org"
-KUBEVIRT_API_VIP=10.123.124.10
-KUBEVIRT_INGRESS_VIP=10.123.124.11
+KUBEVIRT_API_VIP=$(<"${HOME}/secret-kube/${LEASED_RESOURCE}-api-vip")
+KUBEVIRT_INGRESS_VIP=$(<"${HOME}/secret-kube/${LEASED_RESOURCE}-ingress-vip")
 KUBEVIRT_CIDR="10.123.124.0/24"
 CLUSTER_NETWORK_CIDR="10.128.0.0/14"
 SERVICE_NETWROK_CIDR="172.30.0.0/16"
 KUBEVIRT_NAMESPACE=ipi-ci
-KUBEVIRT_TENANT_CLUSTER_NAME=t1
+KUBEVIRT_TENANT_CLUSTER_NAME=$(<"${HOME}/secret-kube/${LEASED_RESOURCE}-cluster-name")
 KUBEVIRT_NETWORK_NAME=mynet
 KUBEVIRT_VOLUME_ACCESS_MODE=ReadWriteOnce
 
