8d43388779ba423c837c780ce68a2242f83e56ec
Andrew Stoycos <astoycos@redhat.com>
2021-01-13 14:41:40 -0800

    TEST
    Signed-off-by: Andrew Stoycos <astoycos@redhat.com>

--- ci-operator/step-registry/gather/core-dump/gather-core-dump-commands.sh
+++ ci-operator/step-registry/gather/core-dump/gather-core-dump-commands.sh
@@ -11,6 +11,6 @@ echo "Gathering node core dumps ..."
 
 mkdir -p ${ARTIFACT_DIR}/core-dumps
 
-oc adm must-gather --dest-dir="${ARTIFACT_DIR}/core-dumps" -- sh -c "bash -x /usr/bin/gather_core_dumps |& tee gather-$(date +%s) || true"
+oc adm must-gather --dest-dir="${ARTIFACT_DIR}/core-dumps" -- sh -c "bash -x /usr/bin/gather_core_dumps |& tee gather-$(date +%s)"
 tar -czC "${ARTIFACT_DIR}/core-dumps" -f "${ARTIFACT_DIR}/core.dumps.tar.gz" .
 rm -rf "${ARTIFACT_DIR}/core-dumps"
