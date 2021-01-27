9e405839f89b5bcc1ccc4d2bdd240e1c87b434ce
Yuval Goldberg <yuvigoldi@gmail.com>
2021-01-27 10:32:57 +0200

    Add e2e-metal-assisted workflow to openshift/assisted-installer-agnet

--- ci-operator/step-registry/baremetalds/assisted/setup/baremetalds-assisted-setup-commands.sh
+++ ci-operator/step-registry/baremetalds/assisted/setup/baremetalds-assisted-setup-commands.sh
@@ -60,12 +60,15 @@ cd "\${REPO_DIR}"
 
 set +x
 echo "export PULL_SECRET='\$(cat /root/pull-secret)'" >> /root/config
+echo "export PUBLIC_CONTAINER_REGISTRIES=quay.io,\$(echo ${RELEASE_IMAGE_LATEST} | cut -d'/' -f1)" >> /root/config
+echo "export ASSISTED_SERVICE_HOST=${IP}" >> /root/config
+
+# Override default images
+echo "export AGENT_DOCKER_IMAGE=${ASSISTED_AGENT_IMAGE}" >> /root/config
 
 if [ "\${OPENSHIFT_INSTALL_RELEASE_IMAGE:-}" = "" ]; then
     echo "export OPENSHIFT_INSTALL_RELEASE_IMAGE=${RELEASE_IMAGE_LATEST}" >> /root/config
 fi
-echo "export PUBLIC_CONTAINER_REGISTRIES=quay.io,\$(echo ${RELEASE_IMAGE_LATEST} | cut -d'/' -f1)" >> /root/config
-echo "export ASSISTED_SERVICE_HOST=${IP}" >> /root/config
 set -x
 
 if [[ -e /root/assisted-additional-config ]]
