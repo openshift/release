fc5889a69d6ba777729f04f80f7385c9b5887494
Petr Muller <afri@afri.cz>
2021-02-05 12:15:51 +0100

    Revert "Update Prow to v20210204-b06ec788ea, Boskos to v20210203-17d4b61"

--- hack/images.sh
+++ hack/images.sh
@@ -1,2 +1,2 @@
-MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20210204-b06ec788ea
-MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20210204-b06ec788ea
+MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20210203-567fc1ab64
+MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20210203-567fc1ab64
