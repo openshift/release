1c474f5bbc59a9095f8b073ad126a6d85817a110
Bruno Barcarol Guimarães <bbarcaro@redhat.com>
2021-02-09 13:00:01 +0000

    Revert "Update Prow to v20210209-ed3f510f45, Boskos to v20210203-17d4b61"

--- hack/images.sh
+++ hack/images.sh
@@ -1,2 +1,2 @@
-MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20210209-ed3f510f45
-MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20210209-ed3f510f45
+MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20210208-5e490580b8
+MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20210208-5e490580b8
