dbe282118f3a68ee08fe6a67b8efae1f8f6047f5
Vadim Rutkovsky <vrutkovs@redhat.com>
2021-02-16 11:00:04 +0100

    ipi-conf-mirror: mirror repo into stable-mirrored

--- ci-operator/step-registry/ipi/conf/mirror/ipi-conf-mirror-commands.sh
+++ ci-operator/step-registry/ipi/conf/mirror/ipi-conf-mirror-commands.sh
@@ -6,21 +6,23 @@ set -o pipefail
 mkdir -p ${HOME}/.docker
 cp ${CLUSTER_PROFILE_DIR}/pull-secret ${HOME}/.docker/config.json
 oc registry login
-MIRROR_BASE=$( oc get is release -o 'jsonpath={.status.publicDockerImageRepository}' )
+MIRROR_REPO=$( oc get is release -o 'jsonpath={.status.publicDockerImageRepository}' )
+MIRROR_BASE=$(dirname ${REPO_NAME})
+MIRROR_IMAGESTREAM="stable-mirrored"
 oc adm release new \
   --from-release ${RELEASE_IMAGE_LATEST} \
-  --to-image ${MIRROR_BASE}-scratch:release \
-  --mirror ${MIRROR_BASE}-scratch \
+  --to-image ${MIRROR_BASE}/${MIRROR_IMAGESTREAM}:release \
+  --mirror ${MIRROR_BASE}/${MIRROR_IMAGESTREAM} \
   || echo 'ignore: the release could not be reproduced from its inputs'
 oc adm release mirror \
-  --from ${MIRROR_BASE}-scratch:release \
-  --to ${MIRROR_BASE} \
+  --from ${MIRROR_BASE}/${MIRROR_IMAGESTREAM}:release \
+  --to ${MIRROR_REPO} \
   --to-release-image ${MIRROR_BASE}:mirrored
-oc delete imagestream "$(basename "${MIRROR_BASE}-scratch")"
+oc delete imagestream "$(basename "${MIRROR_BASE}/${MIRROR_IMAGESTREAM}")"
 
 cat >> ${SHARED_DIR}/install-config.yaml << EOF
 imageContentSources:
-- source: "${MIRROR_BASE}-scratch"
+- source: "${MIRROR_BASE}/${MIRROR_IMAGESTREAM}"
   mirrors:
-  - "${MIRROR_BASE}"
+  - "${MIRROR_REPO}"
 EOF
