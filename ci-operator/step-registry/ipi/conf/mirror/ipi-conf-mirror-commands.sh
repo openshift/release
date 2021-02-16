3214b0c9603dcfc922d95359fc09bca5da981b13
Vadim Rutkovsky <vrutkovs@redhat.com>
2021-02-16 11:46:46 +0100

    ipi-conf-mirror: debug

--- ci-operator/step-registry/ipi/conf/mirror/ipi-conf-mirror-commands.sh
+++ ci-operator/step-registry/ipi/conf/mirror/ipi-conf-mirror-commands.sh
@@ -6,16 +6,22 @@ set -o pipefail
 mkdir -p ${HOME}/.docker
 cp ${CLUSTER_PROFILE_DIR}/pull-secret ${HOME}/.docker/config.json
 oc registry login
+# Repository to be mirrored
 MIRROR_REPO=$( oc get is release -o 'jsonpath={.status.publicDockerImageRepository}' )
+# server + name
 MIRROR_BASE=$(dirname ${REPO_NAME})
+# Tag of the mirrored release
 MIRROR_TAG="mirorred"
+# Imagestream created by ci-operator which holds the mirrored images
 MIRROR_IMAGESTREAM="stable-${MIRROR_TAG}"
 
+echo "MIRROR_REPO: ${MIRROR_REPO}"
+echo "MIRROR_BASE: ${MIRROR_BASE}"
+echo "MIRROR_TAG: ${MIRROR_TAG}"
+echo "MIRROR_IMAGESTREAM: ${MIRROR_IMAGESTREAM}"
+
 # Cleanup mirrored imagestream
-TAGS=$(oc get is ${MIRROR_IMAGESTREAM} -o 'jsonpath={.spec.tags[*].name}')
-for tag in ${TAGS[@]}; do
-  oc delete istag "${MIRROR_IMAGESTREAM}:$tag"
-done
+oc get is ${MIRROR_IMAGESTREAM} -o 'jsonpath={.spec.tags[*].name}' | xargs -n1 -I {} oc delete istag "${MIRROR_IMAGESTREAM}:{}"
 
 oc adm release new \
   --from-release ${RELEASE_IMAGE_LATEST} \
