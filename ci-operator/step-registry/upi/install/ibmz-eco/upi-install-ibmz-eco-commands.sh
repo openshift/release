f49a5104868170e0ffb382352fda8fbc104748b9
Ashley Freeth <ASHFREET@uk.ibm.com>
2021-03-10 15:56:48 +0000

    Add initial install commands and ref for IBM Z Ecosystem

--- ci-operator/step-registry/upi/install/ibmz-eco/upi-install-ibmz-eco-commands.sh
+++ ci-operator/step-registry/upi/install/ibmz-eco/upi-install-ibmz-eco-commands.sh
@@ -1,3 +1,48 @@
 #!/bin/bash
 
-# TODO: Deploy IBM Z cluster in Z Ecosystem environment
+set -o nounset
+set -o errexit
+set -o pipefail
+
+trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
+
+#export HOME=/tmp
+#export OPENSHIFT_INSTALL_INVOKER=openshift-internal-ci/${JOB_NAME_SAFE}/${BUILD_ID}
+export http_proxy="http://204.90.115.172:8080"
+export https_proxy="http://204.90.115.172:8080"
+
+ibmz_eco_cloud_auth=/var/run/secrets/clouds.yaml
+cluster_name=$(<"${SHARED_DIR}"/clustername.txt)
+installer_dir=/deploy
+cluster_dir=/deploy/ocp_clusters/${cluster_name}
+
+echo "$(date -u --rfc-3339=seconds) - Copying config from shared dir..."
+
+pushd ${installer_dir}
+
+cp -t "${installer_dir}" \
+    "${SHARED_DIR}/terraform.tfvars" \
+    ${ibmz_eco_cloud_auth}
+
+echo "$(date +%s)" > "${SHARED_DIR}/TEST_TIME_INSTALL_START"
+
+echo "$(date -u --rfc-3339=seconds) - Deploying cluster on IBM Z Ecosystem Cloud..."
+/entrypoint.sh apply &
+wait "$!"
+
+set +e
+wait "$!"
+ret="$?"
+set -e
+
+echo "$(date +%s)" > "${SHARED_DIR}/TEST_TIME_INSTALL_END"
+
+touch /tmp/install-complete
+
+sed 's/password: .*/password: REDACTED/' "${cluster_dir}/.openshift_install.log" >>"${ARTIFACT_DIR}/.openshift_install.log"
+
+cp -t "${SHARED_DIR}" \
+    "${cluster_dir}/ocp_install/auth/kubeconfig" \
+    "${cluster_dir}/ocp_install/metadata.json"
+
+exit "$ret"
