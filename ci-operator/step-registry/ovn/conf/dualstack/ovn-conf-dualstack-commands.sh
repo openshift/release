d6e1c096f44cc86bfb04ce15a4262e15b42948f3
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2021-03-16 19:25:35 +0100

    add azure dual-stack jobs to ovn-kubernetes
    ovn-conf for previous versions
    
    Signed-off-by: Antonio Ojea <antonio.ojea.garcia@gmail.com>

--- ci-operator/step-registry/ovn/conf/dualstack/ovn-conf-dualstack-commands.sh
+++ ci-operator/step-registry/ovn/conf/dualstack/ovn-conf-dualstack-commands.sh
@@ -20,3 +20,17 @@ networking:
   - 172.30.0.0/16
   - fd00:172:16::/112
 EOF
+
+# DualStack is only enabled by default on Openshift 4.8+
+# it requires to enable the feature gate in 4.7
+if [[ ${REQUIRE_IPV6_DUALSTACK_NO_UPGRADE} == "true" ]]; then
+  echo "Adding IPv6DualStackNoUpgrade feature gate"
+  cat >> "${SHARED_DIR}/manifest_ipv6-dual-stack-no-upgrade.yaml.yml" << EOF
+apiVersion: config.openshift.io/v1
+kind: FeatureGate
+metadata:
+  name: cluster
+spec:
+  featureSet: IPv6DualStackNoUpgrade
+EOF
+fi
