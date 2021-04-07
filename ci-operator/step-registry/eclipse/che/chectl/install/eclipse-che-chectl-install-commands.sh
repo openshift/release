7f69c73e4bee86da7c94249922644f8de7b94ea6
Radim Hopp <rhopp@redhat.com>
2021-04-07 15:54:23 +0200

    Playing with variables&dependencies
    Signed-off-by: Radim Hopp <rhopp@redhat.com>

--- ci-operator/step-registry/eclipse/che/chectl/install/eclipse-che-chectl-install-commands.sh
+++ ci-operator/step-registry/eclipse/che/chectl/install/eclipse-che-chectl-install-commands.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 set -e
 
-echo "$CHECLUSTER_CR_PATCH"
+mkdir /usr/chectl
+cd /usr/chectl
+
+
+echo "$CHECTL_PARAMS"
+
+echo "$CHECLUSTER_CR_PATCH" > checluster_patch.yaml
+
+cat checluster_patch.yaml
 
 ## install chectl
 
-curl "$(curl https://che-incubator.github.io/chectl/download-link/next-linux-x64)" -L -o chectl.tar.gz
+curl "$(curl https://che-incubator.github.io/chectl/download-link/next-linux-x64)" -L -o /tmp/chectl.tar.gz
 
-tar -xvf chectl.tar.gz
+tar -xvf /tmp/chectl.tar.gz -C /tmp
+ln -s /usr/bin/chectl /tmp/chectl/bin/chectl
 
-mv chectl /tmp
-/tmp/bin/chectl --version
+/tmp/chectl/bin/chectl --version
+chectl --version
 
 ## run chectl
+echo "$CHECTL_PARAMS"
+eval echo "$CHECTL_PARAMS"
 
-/tmp/chectl/bin/chectl server:deploy --telemetry=off -p openshift
+chectl "$(eval echo "${CHECTL_PARAMS}")"
 
 
