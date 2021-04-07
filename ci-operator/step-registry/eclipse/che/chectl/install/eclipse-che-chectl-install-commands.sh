cd66d06e7d045bc002f510d6217336d61bc1ebb9
Radim Hopp <rhopp@redhat.com>
2021-04-07 22:03:14 +0200

    Fixing the script #3
    Signed-off-by: Radim Hopp <rhopp@redhat.com>

--- ci-operator/step-registry/eclipse/che/chectl/install/eclipse-che-chectl-install-commands.sh
+++ ci-operator/step-registry/eclipse/che/chectl/install/eclipse-che-chectl-install-commands.sh
@@ -21,15 +21,13 @@ cat checluster_patch.yaml
 curl "$(curl https://che-incubator.github.io/chectl/download-link/next-linux-x64)" -L -o /tmp/chectl.tar.gz
 
 tar -xf /tmp/chectl.tar.gz -C /tmp
-ln -s /tmp/chectl/bin/chectl /usr/bin/chectl || true
 
 /tmp/chectl/bin/chectl --version
-chectl --version
 
 ## run chectl
 echo "$CHECTL_PARAMS"
 eval echo "$CHECTL_PARAMS"
 
-chectl "$(eval echo "${CHECTL_PARAMS}")"
+/tmp/chectl/bin/chectl "$(eval echo "${CHECTL_PARAMS}")"
 
 
