63407f94667c35b8b7b995fa63fea1d92a4a76fd
Hemant Kumar <hekumar@redhat.com>
2021-04-08 10:10:52 -0400

    Fix CI steps for vsphere operator

--- ci-operator/step-registry/storage/conf/csi-vmware-vsphere/storage-conf-csi-vmware-vsphere-commands.sh
+++ ci-operator/step-registry/storage/conf/csi-vmware-vsphere/storage-conf-csi-vmware-vsphere-commands.sh
@@ -5,7 +5,6 @@ set -o pipefail
 
 cd /go/src/github.com/openshift/vmware-vsphere-csi-driver-operator
 cp test/e2e/manifest.yaml ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
-cp test/e2e/storageclass-ci.yaml ${SHARED_DIR}/storageclass-ci.yaml
 
 # For debugging
 echo "Using ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}:"
