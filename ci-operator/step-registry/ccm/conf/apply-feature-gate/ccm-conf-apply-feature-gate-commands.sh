48cd82f5d5b4127c5ad1acd6ac4e8e00d5f19ce2
Danil-Grigorev <dgrigore@redhat.com>
2021-04-21 16:51:05 +0200

    Wait for operators to become ready post FG change
    - Configure test skips
    - Uniquely identify cccmo src image for tests and reuse in other jobs

--- ci-operator/step-registry/ccm/conf/apply-feature-gate/ccm-conf-apply-feature-gate-commands.sh
+++ ci-operator/step-registry/ccm/conf/apply-feature-gate/ccm-conf-apply-feature-gate-commands.sh
@@ -7,7 +7,7 @@ chmod ug+x /tmp/bin/oc
 
 export CCM_NAMESPACE="openshift-cloud-controller-manager"
 
-echo "Apply external cloud-controller-manager FeatureGate configuration"
+echo "$(date -u --rfc-3339=seconds) - Apply external cloud-controller-manager FeatureGate configuration"
 
 cat <<EOF | oc apply -f -
 ---
@@ -28,7 +28,7 @@ EOF
 
 function waitForCCMDeploymentCreation() {
   while [ "$(oc get deploy -n ${CCM_NAMESPACE} -o name | wc -l)" == 0 ]; do
-    echo "Wait for CCCMO operands creation"
+    echo "$(date -u --rfc-3339=seconds) - Wait for CCCMO operands creation"
     sleep 5
   done
 }
@@ -36,7 +36,24 @@ export -f waitForCCMDeploymentCreation
 
 timeout --foreground 3m bash -c waitForCCMDeploymentCreation
 
-echo "Wait for operands to be ready"
+echo "$(date -u --rfc-3339=seconds) - Wait for operands to be ready"
 oc wait --all -n "${CCM_NAMESPACE}" --for=condition=Available=True deployment --timeout=3m
 
+
+echo "$(date -u --rfc-3339=seconds) - Wait for some time for cluster operators to reconcile feature gate change"
+sleep 30
+
+echo "$(date -u --rfc-3339=seconds) - Wait for the imageregistry operator to go available..."
+oc wait --all --for=condition=Available=True clusteroperators.config.openshift.io --timeout=10m
+
+echo "$(date -u --rfc-3339=seconds) - Wait for the imageregistry to rollout..."
+oc wait --all --for=condition=Progressing=False clusteroperators.config.openshift.io --timeout=30m
+
+echo "$(date -u --rfc-3339=seconds) - Wait until imageregistry config changes are observed by kube-apiserver..."
+sleep 60
+
+echo "$(date -u --rfc-3339=seconds) - Waits for kube-apiserver to finish rolling out..."
+oc wait --all --for=condition=Progressing=False clusteroperators.config.openshift.io --timeout=30m
+
+oc wait --all --for=condition=Degraded=False clusteroperators.config.openshift.io --timeout=1m
 
