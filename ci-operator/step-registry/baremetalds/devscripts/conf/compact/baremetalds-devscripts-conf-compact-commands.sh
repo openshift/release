824d76f36da66a9e520247a79cf3a8b9383eb22d
Andrea Fasano <afasano@redhat.com>
2021-04-23 15:43:38 +0200

    Remove compact conf step

--- ci-operator/step-registry/baremetalds/devscripts/conf/compact/baremetalds-devscripts-conf-compact-commands.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o nounset
-set -o errexit
-set -o pipefail
-
-echo "************ baremetalds devscripts conf compact command ************"
-
-echo "export NUM_WORKERS=0" >> "${SHARED_DIR}/dev-scripts-additional-config"
