087072c9c4ec90035cf1c5552be07a9a6e057831
Petr Muller <afri@afri.cz>
2021-04-28 13:50:14 +0200

    WIP: Add a temporary new job to use the run-bundle step
    Signed-off-by: Rashmi Gottipati <chowdary.grashmi@gmail.com>

--- ci-operator/step-registry/optional-operators/ci/run-bundle/optional-operators-ci-run-bundle-commands.sh
+++ ci-operator/step-registry/optional-operators/ci/run-bundle/optional-operators-ci-run-bundle-commands.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+echo "Deploying an operator in the bundle format"
+
+# (todo): hardcoding this for now to test in CI; will fix
+operator-sdk run bundle "quay.io/rashmigottipati/api-operator:1.0.1"
