ff12a7b853e0c474f16665e837616add87ef8e3b
Joseph Callen <jcallen@redhat.com>
2021-04-30 08:55:21 -0400

    vsphere: increase quota

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -84,7 +84,7 @@ for i in range(10, 20):
 for i in range(1, 3):
     CONFIG['kubevirt-quota-slice']['tenant-cluster-{}'.format(i)] = 1
 
-for i in range(0,10):
+for i in range(0,12):
     CONFIG['vsphere-quota-slice']['ci-segment-{}'.format(i)] = 1
 
 config = {
