dd24fa6b060b5e797b5eac95cf54741d325c662c
Adolfo Duarte <adduarte@redhat.com>
2021-05-01 19:09:35 -0700

    Test installation with compute pool additional networks
    added to /test openstack-install-multiplenetworks to master branch
    
    workflow added:
     openstack-install-multiplenetworks
    
    steps added:
     create additional networks
     delete additional networks

--- ci-operator/step-registry/openstack/conf/generateconfig/openstack-conf-generateconfig-commands.sh
+++ ci-operator/step-registry/openstack/conf/generateconfig/openstack-conf-generateconfig-commands.sh
@@ -32,6 +32,49 @@ pullSecret: >
 sshKey: |
   ${SSH_PUB_KEY}
 EOF
+
+elif [[ "${CONFIG_TYPE}" == "advanced" ]]; then
+  cat > "${CONFIG}" << EOF
+apiVersion: ${CONFIG_API_VERSION}
+baseDomain: ${BASE_DOMAIN}
+compute:
+- name: worker
+  platform:
+    openstack:
+EOF
+  if [[ -f "${SHARED_DIR}/ADDITIONAL_NETWORK_IDS" ]]; then
+    cat >> "${CONFIG}" <<EOF
+      additionalNetworkIDs:
+EOF
+    for NET_ID in $(cat ${SHARED_DIR}/ADDITIONAL_NETWORK_IDS); do
+        cat >> "${CONFIG}" << EOF
+      - ${NET_ID}
+EOF
+    done
+  else
+    cat >> "${CONFIG}" << EOF
+     {}
+EOF
+  fi
+  cat >> "${CONFIG}" << EOF
+metadata:
+  name: ${CLUSTER_NAME}
+platform:
+  openstack:
+    cloud:            ${OS_CLOUD}
+    externalNetwork:  ${OPENSTACK_EXTERNAL_NETWORK}
+    computeFlavor:    ${OPENSTACK_COMPUTE_FLAVOR}
+    lbFloatingIP:     ${LB_FIP_IP}
+    externalDNS:
+      - 1.1.1.1
+      - 1.0.0.1
+pullSecret: >
+  ${PULL_SECRET}
+sshKey: |
+  ${SSH_PUB_KEY}
+EOF
+
+
 else
     echo "NO valid install config type specified. Please check  CONFIG_TYPE"
     exit 1
