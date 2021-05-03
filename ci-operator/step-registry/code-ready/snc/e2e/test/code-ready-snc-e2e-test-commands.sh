993ea9f3816e0c9717d7fc7f94c2d34a23cdc5bc
Petr Muller <afri@afri.cz>
2021-05-03 16:52:39 +0200

    upi-gcp-nested: unhardcode GCP project/region/zone
    To ensure reusability, GCP project needs to be supplied via cluster
    profile (parameter is technically fine but the cluster profile contains
    the project ID together with necessary credentials). Region needs to be
    inferred from Boskos lease. Zone needs to be read from GCP based on
    region.

--- ci-operator/step-registry/code-ready/snc/e2e/test/code-ready-snc-e2e-test-commands.sh
+++ ci-operator/step-registry/code-ready/snc/e2e/test/code-ready-snc-e2e-test-commands.sh
@@ -3,6 +3,14 @@ set -euo pipefail
 
 trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
 
+GOOGLE_PROJECT_ID="$(< ${CLUSTER_PROFILE_DIR}/openshift_gcp_project)"
+GOOGLE_COMPUTE_REGION="${LEASED_RESOURCE}"
+GOOGLE_COMPUTE_ZONE="$(< ${SHARED_DIR}/openshift_gcp_compute_zone)"
+if [[ -z "${GOOGLE_COMPUTE_ZONE}" ]]; then
+  echo "Expected \${SHARED_DIR}/openshift_gcp_compute_zone to contain the GCP zone"
+  exit 1
+fi
+
 INSTANCE_PREFIX="${NAMESPACE}"-"${JOB_NAME_HASH}"
 
 mkdir -p "${HOME}"/.ssh
@@ -24,7 +32,7 @@ gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
 gcloud --quiet config set compute/zone "${GOOGLE_COMPUTE_ZONE}"
 gcloud --quiet config set compute/region "${GOOGLE_COMPUTE_REGION}"
 
-cat  > "${HOME}"/run-tests.sh << 'EOF'
+cat > "${HOME}"/run-tests.sh << 'EOF'
 #!/bin/bash
 
 set -euo pipefail
