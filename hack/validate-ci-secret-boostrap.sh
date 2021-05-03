62bd15d91f9c8c4b2fa83325433705f7a1698e26
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2021-05-03 10:58:18 -0400

    Ci-Tools: Run o/release ci-secret-bootstrap validation

--- hack/validate-ci-secret-boostrap.sh
+++ hack/validate-ci-secret-boostrap.sh
@@ -2,7 +2,15 @@
 
 set -euo pipefail
 
-exec /usr/bin/ci-secret-bootstrap \
+cd $(dirname $0)/..
+
+BOOTSTRAP_BINARY=${BOOTSTRAP_BINARY:-/usr/bin/ci-secret-bootstrap}
+
+if [[ ! -x ${BOOTSTRAP_BINARY} ]]; then
+  cd ../ci-tools && go build -race=true -o ${BOOTSTRAP_BINARY} ./cmd/ci-secret-bootstrap && cd -
+fi
+
+exec ${BOOTSTRAP_BINARY} \
 		--vault-addr=https://vault.ci.openshift.org \
 		--vault-role=secret-bootstrap \
 		--vault-prefix=kv/dptp \
