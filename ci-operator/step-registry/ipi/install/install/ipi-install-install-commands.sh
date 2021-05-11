aba462effbc93822d55935bc18311f0f5ea8b36d
Vadim Rutkovsky <vrutkovs@redhat.com>
2021-05-11 09:47:27 +0200

    ipi install: save console URL
    cluster-bot in steps mode looks for `console.url` file in shared dir to determine if installation was a success and show information to the user.
    
    This is required for `launch vsphere` commands to complete

--- ci-operator/step-registry/ipi/install/install/ipi-install-install-commands.sh
+++ ci-operator/step-registry/ipi/install/install/ipi-install-install-commands.sh
@@ -103,6 +103,8 @@ echo "$(date +%s)" > "${SHARED_DIR}/TEST_TIME_INSTALL_END"
 
 if test "${ret}" -eq 0 ; then
   touch  "${SHARED_DIR}/success"
+  # Save console URL in `console.url` file so that ci-chat-bot could report success
+  echo "https://$(env KUBECONFIG=${dir}/auth/kubeconfig oc -n openshift-console get routes console -o=jsonpath='{.spec.host}')" > "${SHARED_DIR}/console.url"
 fi
 
 exit "$ret"
