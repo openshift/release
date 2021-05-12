4ebb5291e07fed6a7500c7df85d3788c314745ed
rna-afk <anarayan@redhat.com>
2021-05-12 13:24:13 -0400

    upi: Fix errors when openshift-install config map is missing
    Adding a fix that creates the openshift-install config map if it
    is not present in order to pass the start and end times information.

--- ci-operator/step-registry/ipi/install-times-collection/ipi-install-times-collection-commands.sh
+++ ci-operator/step-registry/ipi/install-times-collection/ipi-install-times-collection-commands.sh
@@ -6,4 +6,7 @@ set -o pipefail
 echo "Updating openshift-install ConfigMap with the start and end times."
 START_TIME=$(cat "$SHARED_DIR/CLUSTER_INSTALL_START_TIME")
 END_TIME=$(cat "$SHARED_DIR/CLUSTER_INSTALL_END_TIME")
-oc patch configmap openshift-install -n openshift-config -p '{"data":{"startTime":"'"$START_TIME"'","endTime":"'"$END_TIME"'"}}'
+if ! oc patch configmap openshift-install -n openshift-config -p '{"data":{"startTime":"'"$START_TIME"'","endTime":"'"$END_TIME"'"}}'
+then
+    oc create configmap openshift-install -n openshift-config --from-literal=startTime="$START_TIME" --from-literal=endTime="$END_TIME"
+fi
