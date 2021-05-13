3ce1ab90099d9022812b9ed9b5b1bdde8488a8f8
Jan Safranek <jsafrane@redhat.com>
2021-05-13 10:05:15 +0200

    Try to save storage objects only 10 times
    To speed up tests that failed installation.

--- ci-operator/step-registry/storage/obj/save/storage-obj-save-commands.sh
+++ ci-operator/step-registry/storage/obj/save/storage-obj-save-commands.sh
@@ -7,7 +7,12 @@ ARTIFACT_DIR=${ARTIFACT_DIR:-/tmp}
 SAVE_FILE=${SHARED_DIR}/initial-objects
 STORAGE_OBJECTS=pv,csidriver,storageclass
 
-while ! oc get $STORAGE_OBJECTS --no-headers --ignore-not-found -o name > $SAVE_FILE; do
+# Try only 10 times, just in case a cluster setup failed.
+for i in `seq 10`; do
+    echo "Attempt $i"
+    if oc get $STORAGE_OBJECTS --no-headers --ignore-not-found -o name > $SAVE_FILE; then
+        break;
+    fi
     # try until the command succeds
     sleep 5
 done
