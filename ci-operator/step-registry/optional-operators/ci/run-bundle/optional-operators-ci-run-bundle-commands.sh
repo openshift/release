354221c3c8fe3ba3f4b3a36107cd17cd2d276387
Rashmi Gottipati <chowdary.grashmi@gmail.com>
2021-05-13 17:42:21 -0400

    Add optional parameters to run bundle command
    Signed-off-by: Rashmi Gottipati <chowdary.grashmi@gmail.com>

--- ci-operator/step-registry/optional-operators/ci/run-bundle/optional-operators-ci-run-bundle-commands.sh
+++ ci-operator/step-registry/optional-operators/ci/run-bundle/optional-operators-ci-run-bundle-commands.sh
@@ -6,6 +6,8 @@ set -o pipefail
 
 echo "Deploying an operator in the bundle format using operator-sdk run bundle command"
 
-echo "$OO_BUNDLE_IMG"
 cd /tmp
-operator-sdk run bundle "$OO_BUNDLE_IMG"
+operator-sdk run bundle "${OO_BUNDLE_IMG}" \
+                        --index-image "${OO_INDEX_IMG}" \
+                        --namespace "${OO_INSTALL_NAMESPACE}" \
+                        --install-mode "${OO_TARGET_NAMESPACES}"
