ef77ccf816286810bbdb89becd21be72372e140f
Petr Muller <afri@afri.cz>
2021-06-02 18:26:05 +0200

    cvp: replace rehearsal defaults with proper multi-stage params
    This change is a step towards obsoleting the custom parameter passing
    interface between cvp-trigger and ci-operator. We start with replacing
    the "rehearsal defaults" by multi-stage parameters where values are
    defined in tests (this has a side benefit of allowing each job to have a
    separate set of good defaults).
    
    The actual values are (for now!) only used when `OO_*` values are not
    set (`OO_*` values are passed by the custom parameter code). Next step
    is to make `cvp-trigger` override the values of the multi-stage params
    with the same values it passes to `OO_*` params. Then we can change the
    steps to actually use the multi-stage params instead of the custom ones.
    Then we can remove the custom code from both `cvp-trigger` and
    `ci-operator`.

--- ci-operator/step-registry/optional-operators/cvp-common/probe/optional-operators-cvp-common-probe-commands.sh
+++ ci-operator/step-registry/optional-operators/cvp-common/probe/optional-operators-cvp-common-probe-commands.sh
@@ -4,21 +4,12 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-# This value serves as a default when the parameters are not set, which should
-# only happen in rehearsals. Production jobs should always set the OO_* variable.
-REHEARSAL_BUNDLE="brew.registry.redhat.io/rh-osbs-stage/e2e-e2e-test-operator-bundle-container:8.0-3"
-REHEARSAL_INDEX="brew.registry.redhat.io/rh-osbs-stage/iib:23576"
-REHEARSAL_PACKAGE="e2e-test-operator"
-REHEARSAL_CHANNEL="4.3"
-REHEARSAL_INSTALL_NAMESPACE="!create"
-REHEARSAL_TARGET_NAMESPACES="!install"
-
-OO_BUNDLE="${OO_BUNDLE:-$REHEARSAL_BUNDLE}"
-OO_INDEX="${OO_INDEX:-$REHEARSAL_INDEX}"
-OO_PACKAGE="${OO_PACKAGE:-$REHEARSAL_PACKAGE}"
-OO_CHANNEL="${OO_CHANNEL:-$REHEARSAL_CHANNEL}"
-OO_INSTALL_NAMESPACE="${OO_INSTALL_NAMESPACE:-$REHEARSAL_INSTALL_NAMESPACE}"
-OO_TARGET_NAMESPACES="${OO_TARGET_NAMESPACES:-$REHEARSAL_TARGET_NAMESPACES}"
+OO_BUNDLE="${OO_BUNDLE:-$BUNDLE_IMAGE}"
+OO_INDEX="${OO_INDEX:-$INDEX_IMAGE}"
+OO_PACKAGE="${OO_PACKAGE:-$PACKAGE}"
+OO_CHANNEL="${OO_CHANNEL:-$CHANNEL}"
+OO_INSTALL_NAMESPACE="${OO_INSTALL_NAMESPACE:-$INSTALL_NAMESPACE}"
+OO_TARGET_NAMESPACES="${OO_TARGET_NAMESPACES:-$TARGET_NAMESPACES}"
 
 echo "Accessing the bundle image: $OO_BUNDLE"
 oc image info --filter-by-os linux/amd64  -a /var/run/brew-pullsecret/.dockerconfigjson "$OO_BUNDLE"
