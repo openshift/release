ccfc89d23b908d8855ab01cac1fc9a04e7ea6e53
Pierre Prinetti <pierreprinetti@redhat.com>
2021-06-09 09:50:38 +0200

    openstack: Auto find flavor and network names
    With this PR, you can switch clouds (e.g. from MOC to Vexxhost) by just
    changing cluster_profile in the config file for the job. The name matrix
    is in openstack-conf-resourcenames.
    
    Override by setting OPENSTACK_EXTERNAL_NETWORK and/or
    OPENSTACK_COMPUTE_FLAVOR.

--- ci-operator/step-registry/openstack/conf/clouds/openstack-conf-clouds-commands.sh
+++ ci-operator/step-registry/openstack/conf/clouds/openstack-conf-clouds-commands.sh
@@ -4,14 +4,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-
 CLUSTER_TYPE="${CLUSTER_TYPE_OVERRIDE:-$CLUSTER_TYPE}"
-
-# We have to truncate cluster name to 14 chars, because there is a limitation in the install-config
-# Now it looks like "ci-op-rl6z646h-65230".
-# We will remove "ci-op-" prefix from there to keep just last 14 characters. and it cannot start with a "-"
-UNSAFE_CLUSTER_NAME=${NAMESPACE}-${JOB_NAME_HASH}
-SAFE_CLUSTER_NAME=${UNSAFE_CLUSTER_NAME#"ci-op-"}
-echo "${SAFE_CLUSTER_NAME}" > ${SHARED_DIR}/CLUSTER_NAME
 
 cp "/var/run/cluster-secrets/${CLUSTER_TYPE}/clouds.yaml"  "${SHARED_DIR}/clouds.yaml"
