dac691737543b5948ac84f0a362a5124bc7cb404
Lisa Rashidi-Ranjbar <lranjbar@redhat.com>
2021-06-17 06:54:57 -0700

    assisted-operator-ztp: Add config for the spoke cluster image
    Add a new environment variable ASSISTED_OPENSHIFT_INSTALL_RELEASE_IMAGE
    to control the version that Assisted Installer uses for the spoke cluster.
    
    Signed-off-by: Lisa Rashidi-Ranjbar <lranjbar@redhat.com>

--- ci-operator/step-registry/baremetalds/assisted/operator/ztp/baremetalds-assisted-operator-ztp-commands.sh
+++ ci-operator/step-registry/baremetalds/assisted/operator/ztp/baremetalds-assisted-operator-ztp-commands.sh
@@ -20,6 +20,7 @@ fi
 
 # Copy job variables to the packet server
 echo "export OPENSHIFT_INSTALL_RELEASE_IMAGE=${OPENSHIFT_INSTALL_RELEASE_IMAGE}" >> /tmp/assisted-vars.conf
+echo "export ASSISTED_OPENSHIFT_INSTALL_RELEASE_IMAGE=${ASSISTED_OPENSHIFT_INSTALL_RELEASE_IMAGE}" >> /tmp/assisted-vars.conf
 echo "export ASSISTED_NAMESPACE=${ASSISTED_NAMESPACE}" >> /tmp/assisted-vars.conf
 echo "export ASSISTED_CLUSTER_NAME=${ASSISTED_CLUSTER_NAME}" >> /tmp/assisted-vars.conf
 echo "export ASSISTED_CLUSTER_DEPLOYMENT_NAME=${ASSISTED_CLUSTER_DEPLOYMENT_NAME}" >> /tmp/assisted-vars.conf
