9d4973418bc6c9b0ee3b2af30144229b9ad0bb46
Yuval Goldberg <yuvigoldi@gmail.com>
2021-06-17 11:59:20 +0300

    Use find command to get assisted kubeconfigs instead of ls
    ls returns only the relative names, for example
    
    ls /tmp/tmp.Catddr1jWr/kubeconfig
    kubeconfig_test-infra-cluster-assisted-installer  kubeconfig_test-infra-cluster-assisted-installer_2
    
    with find the result would have the absolute path
    
    find /tmp/tmp.Catddr1jWr/kubeconfig -type f
    /tmp/tmp.Catddr1jWr/kubeconfig/kubeconfig_test-infra-cluster-assisted-installer
    /tmp/tmp.Catddr1jWr/kubeconfig/kubeconfig_test-infra-cluster-assisted-installer_2

--- ci-operator/step-registry/baremetalds/assisted/test/baremetalds-assisted-test-commands.sh
+++ ci-operator/step-registry/baremetalds/assisted/test/baremetalds-assisted-test-commands.sh
@@ -29,7 +29,7 @@ scp "${SSHOPTS[@]}" "${SHARED_DIR}/test-list" "root@${IP}:/tmp/test-list"
 
 echo "### Running tests"
 timeout --kill-after 10m 120m ssh "${SSHOPTS[@]}" "root@${IP}" bash - << EOF
-    for kubeconfig in \$(ls \${KUBECONFIG}); do
+    for kubeconfig in \$(find \${KUBECONFIG} -type f); do
         export KUBECONFIG=\${kubeconfig}
         name=\$(basename \${kubeconfig})
         openshift-tests run "openshift/conformance/parallel" --dry-run | \
