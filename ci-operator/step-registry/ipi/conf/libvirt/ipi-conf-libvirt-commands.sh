ef8ce0133ac050f4b050dd99493fbca69e182a1b
Rafael Fonseca <r4f4rfs@gmail.com>
2021-06-21 15:29:06 +0200

    libvirt: some code restructuring and cleanup
    - remove unused files/variables
    - use functions to make the intent of blocks of code more clear
    - remove ipi-conf-libvirt-chain since it contained only one step
    - make the distinction between ipi-install-libvirt chain & ref by
    renaming the latter to ipi-install-libvirt-install
    - properly capture return status of tests
    
    Signed-off-by: Rafael Fonseca <r4f4rfs@gmail.com>

--- ci-operator/step-registry/ipi/conf/libvirt/ipi-conf-libvirt-commands.sh
+++ ci-operator/step-registry/ipi/conf/libvirt/ipi-conf-libvirt-commands.sh
@@ -4,20 +4,72 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-touch ${SHARED_DIR}/cluster-config.yaml
-MULTIARCH_RELEASE_IMAGE_INITIAL=registry.ci.openshift.org/ocp-${ARCH}/release-${ARCH}:${BRANCH}
-if [[ -n "${MULTIARCH_RELEASE_IMAGE_INITIAL}" ]]; then
-  echo "Installing from initial release ${MULTIARCH_RELEASE_IMAGE_INITIAL}"
-  OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE="${MULTIARCH_RELEASE_IMAGE_INITIAL}"
-  yq write --inplace ${SHARED_DIR}/cluster-config.yaml OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE ${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE}
+function write_shared_dir() {
+  local key="$1"
+  local value="$2"
+  yq write --inplace ${SHARED_DIR}/cluster-config.yaml $key $value
+}
+
+# TO-DO Remove once Boskos Monitor Controller for MA CI merges
+# Assume lease hasn't been cleaned
+function cleanup_leftover_resources() {
+  local CONNECT=${REMOTE_LIBVIRT_URI}
+
+  # Remove conflicting domains
+  for DOMAIN in $(mock-nss.sh virsh -c "${CONNECT}" list --all --name | grep "${LEASED_RESOURCE}")
+  do
+    mock-nss.sh virsh -c "${CONNECT}" destroy "${DOMAIN}" || true
+    mock-nss.sh virsh -c "${CONNECT}" undefine "${DOMAIN}" || true
+  done
+
+  # Remove conflicting pools
+  for POOL in $(mock-nss.sh virsh -c "${CONNECT}" pool-list --all --name | grep "${LEASED_RESOURCE}")
+  do
+    mock-nss.sh virsh -c "${CONNECT}" pool-destroy "${POOL}" || true
+    mock-nss.sh virsh -c "${CONNECT}" pool-delete "${POOL}" || true
+    mock-nss.sh virsh -c "${CONNECT}" pool-undefine "${POOL}" || true
+  done
+
+  # Remove conflicting networks
+  for NET in $(mock-nss.sh virsh -c "${CONNECT}" net-list --all --name | grep "${LEASED_RESOURCE}")
+  do
+    mock-nss.sh virsh -c "${CONNECT}" net-destroy "${NET}" || true
+    mock-nss.sh virsh -c "${CONNECT}" net-undefine "${NET}" || true
+  done
+
+  # Detect conflicts
+  CONFLICTING_DOMAINS=$(mock-nss.sh virsh -c "${CONNECT}" list --all --name | grep "${LEASED_RESOURCE}" || true)
+  CONFLICTING_POOLS=$(mock-nss.sh virsh -c "${CONNECT}" pool-list --all --name | grep "${LEASED_RESOURCE}" || true)
+  CONFLICTING_NETWORKS=$(mock-nss.sh virsh -c "${CONNECT}" net-list --all --name | grep "${LEASED_RESOURCE}" || true)
+  if [ ! -z "$CONFLICTING_DOMAINS" ] || [ ! -z "$CONFLICTING_POOLS" ] || [ ! -z "$CONFLICTING_NETWORKS" ]; then
+    echo "Could not ensure clean state for lease ${LEASED_RESOURCE}"
+    echo "Conflicting domains: $CONFLICTING_DOMAINS"
+    echo "Conflicting pools: $CONFLICTING_POOLS"
+    echo "Conflicting networks: $CONFLICTING_NETWORKS"
+    exit 1
+  fi
+}
+
+# ensure LEASED_RESOURCE is set
+if [[ -z "${LEASED_RESOURCE}" ]]; then
+  echo "Failed to acquire lease"
+  exit 1
 fi
 
-openshift-install version
-
+# ensure RELEASE_IMAGE_LATEST is set
 if [[ -z "$RELEASE_IMAGE_LATEST" ]]; then
   echo "RELEASE_IMAGE_LATEST is an empty string, exiting"
   exit 1
 fi
+
+# create a file for storing shared information between steps
+touch ${SHARED_DIR}/cluster-config.yaml
+
+OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE="registry.ci.openshift.org/ocp-${ARCH}/release-${ARCH}:${BRANCH}"
+echo "Installing from initial release ${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE}"
+write_shared_dir OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE ${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE}
+
+openshift-install version
 
 CONFIG="${SHARED_DIR}/install-config.yaml"
 # TO_DO Remove CLUSTER SUBNET 126 after HA-Proxy changes on host
@@ -40,14 +92,6 @@ declare -A SUBNETS
   SUBNETS["${CLUSTER_TYPE}-2-3"]="3"
   SUBNETS["${CLUSTER_TYPE}-2-4"]="4"
   SUBNETS["${CLUSTER_TYPE}-2-5"]="5"
-
-# Debug echo "SUBNET=${SUBNETS[${LEASED_RESOURCE}]}"
-
-# ensure LEASED_RESOURCE is set
-if [[ -z "${LEASED_RESOURCE}" ]]; then
-  echo "Failed to acquire lease"
-  exit 1
-fi
 
 # get cluster subnet or default it to 126
 # TO-DO default to 1 after HA-Proxy changes on host
@@ -93,52 +137,18 @@ if [[ -z "${REMOTE_LIBVIRT_URI}" ]]; then
   REMOTE_LIBVIRT_URI="qemu+tcp://${REMOTE_LIBVIRT_HOSTNAME}/system"
 fi
 # Debug echo "Remote Libvirt=${REMOTE_LIBVIRT_URI}"
-yq write --inplace ${SHARED_DIR}/cluster-config.yaml REMOTE_LIBVIRT_URI ${REMOTE_LIBVIRT_URI}
-yq write --inplace ${SHARED_DIR}/cluster-config.yaml CLUSTER_NAME ${LEASED_RESOURCE}-${JOB_NAME_HASH}
-yq write --inplace ${SHARED_DIR}/cluster-config.yaml CLUSTER_SUBNET ${CLUSTER_SUBNET}
+
+write_shared_dir REMOTE_LIBVIRT_URI ${REMOTE_LIBVIRT_URI}
+write_shared_dir CLUSTER_NAME ${LEASED_RESOURCE}-${JOB_NAME_HASH}
+write_shared_dir CLUSTER_SUBNET ${CLUSTER_SUBNET}
 
 # Test the remote connection
 mock-nss.sh virsh -c ${REMOTE_LIBVIRT_URI} list
 
-# Create a list of shared variables among containers
-printf "REMOTE_LIBVIRT_URI=%s\n" "${REMOTE_LIBVIRT_URI}" > ${ARTIFACT_DIR}/shared_variables
-# TO-DO Remove once Boskos Monitor Controller for MA CI merges
-# Assume lease hasn't been cleaned
-CONNECT=${REMOTE_LIBVIRT_URI}
-# Remove conflicting domains
-for DOMAIN in $(mock-nss.sh virsh -c "${CONNECT}" list --all --name | grep "${LEASED_RESOURCE}")
-do
-  mock-nss.sh virsh -c "${CONNECT}" destroy "${DOMAIN}" || true
-  mock-nss.sh virsh -c "${CONNECT}" undefine "${DOMAIN}" || true
-done
-# Remove conflicting pools
-for POOL in $(mock-nss.sh virsh -c "${CONNECT}" pool-list --all --name | grep "${LEASED_RESOURCE}")
-do
-  mock-nss.sh virsh -c "${CONNECT}" pool-destroy "${POOL}" || true
-  mock-nss.sh virsh -c "${CONNECT}" pool-undefine "${POOL}" || true
-done
-# Remove conflicting networks
-for NET in $(mock-nss.sh virsh -c "${CONNECT}" net-list --all --name | grep "${LEASED_RESOURCE}")
-do
-  mock-nss.sh virsh -c "${CONNECT}" net-destroy "${NET}" || true
-  mock-nss.sh virsh -c "${CONNECT}" net-undefine "${NET}" || true
-done
-# Detect conflicts
-CONFLICTING_DOMAINS=$(mock-nss.sh virsh -c "${CONNECT}" list --all --name | grep "${LEASED_RESOURCE}" || true)
-CONFLICTING_POOLS=$(mock-nss.sh virsh -c "${CONNECT}" pool-list --all --name | grep "${LEASED_RESOURCE}" || true)
-CONFLICTING_NETWORKS=$(mock-nss.sh virsh -c "${CONNECT}" net-list --all --name | grep "${LEASED_RESOURCE}" || true)
-if [ ! -z "$CONFLICTING_DOMAINS" ] || [ ! -z "$CONFLICTING_POOLS" ] || [ ! -z "$CONFLICTING_NETWORKS" ]; then
-  echo "Could not ensure clean state for lease ${LEASED_RESOURCE}"
-  echo "Conflicting domains: $CONFLICTING_DOMAINS"
-  echo "Conflicting pools: $CONFLICTING_POOLS"
-  echo "Conflicting networks: $CONFLICTING_NETWORKS"
-  exit 1
-fi
+# in case the cluster deprovision failed in a previous run
+cleanup_leftover_resources
 
-NETWORK_NAME="br$(printf ${LEASED_RESOURCE} | tail -c 3)"
 CLUSTER_NAME="${LEASED_RESOURCE}-${JOB_NAME_HASH}"
-ssh_pub_key=$(<"${CLUSTER_PROFILE_DIR}/ssh-publickey")
-pull_secret=$(<"${CLUSTER_PROFILE_DIR}/pull-secret")
 cat >> "${CONFIG}" << EOF
 apiVersion: v1
 baseDomain: ${LEASED_RESOURCE}
@@ -169,9 +179,9 @@ platform:
       dnsmasqOptions:
       - name: "address"
         value: "/.apps.${CLUSTER_NAME}.${LEASED_RESOURCE}/192.168.${CLUSTER_SUBNET}.1"
-      if: ${NETWORK_NAME}
+      if: "br$(printf ${LEASED_RESOURCE} | tail -c 3)"
 pullSecret: >
-  ${pull_secret}
+  $(<"${CLUSTER_PROFILE_DIR}/pull-secret")
 sshKey: |
-  ${ssh_pub_key}
+  $(<"${CLUSTER_PROFILE_DIR}/ssh-publickey")
 EOF
