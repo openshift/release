98834053be052d081d8dc2923736dd2467f2e7c8
Yuval Goldberg <yuvigoldi@gmail.com>
2021-06-22 09:21:44 +0300

    Remove LSO steps from assisted-operator workflow
    The entire disconnected logic should be implemented in the same place
    as where the connected logic is located.
    Currently that's assisted-service repository hence moving the logic
    from openshift/release including deleting LSO steps.
    
    assisted-service PR - https://github.com/openshift/assisted-service/pull/2052

--- ci-operator/step-registry/baremetalds/assisted/operator/setup/baremetalds-assisted-operator-setup-commands.sh
+++ ci-operator/step-registry/baremetalds/assisted/operator/setup/baremetalds-assisted-operator-setup-commands.sh
@@ -21,6 +21,7 @@ cd /root/dev-scripts
 source common.sh
 source utils.sh
 source network.sh
+export -f wrap_if_ipv6 ipversion
 
 REPO_DIR="/home/assisted-service"
 if [ ! -d "\${REPO_DIR}" ]; then
@@ -34,47 +35,15 @@ cd "\${REPO_DIR}"
 
 export DISCONNECTED="${DISCONNECTED:-}"
 
-echo "### Setup hive..."
-
-if [ "\${DISCONNECTED}" = "true" ]; then
-  export LOCAL_REGISTRY="\${LOCAL_REGISTRY_DNS_NAME}:\${LOCAL_REGISTRY_PORT}"
-
-  source deploy/operator/mirror_utils.sh
-
-  export AUTHFILE="\${XDG_RUNTIME_DIR}/containers/auth.json"
-  mkdir -p \$(dirname \${AUTHFILE})
-
-  merge_authfiles "\${PULL_SECRET_FILE}" "\${REGISTRY_CREDS}" "\${AUTHFILE}"
-  hack/setup_env.sh hive_from_upstream
-  deploy/operator/setup_hive.sh from_upstream
-else
-  deploy/operator/setup_hive.sh with_olm
-fi
-
 echo "### Setup assisted installer..."
 
 # TODO: remove this and support mirroring an index referenced by digest value
 # https://issues.redhat.com/browse/MGMT-6858
 export INDEX_IMAGE="\$(dirname ${INDEX_IMAGE})/pipeline:ci-index"
 
-export OPENSHIFT_VERSIONS=\$(cat data/default_ocp_versions.json |
-    jq -rc 'with_entries(.key = "4.8") | with_entries(
-      {
-        key: .key,
-        value: {rhcos_image:   .value.rhcos_image,
-                rhcos_version: .value.rhcos_version,
-                rhcos_rootfs:  .value.rhcos_rootfs}
-      }
-    )')
-
-if [ "\${DISCONNECTED}" = "true" ]; then
-  export MIRROR_BASE_URL="http://\$(wrap_if_ipv6 \${PROVISIONING_HOST_IP})/images"
-  export IRONIC_IMAGES_DIR
-fi
-
 images=(${ASSISTED_AGENT_IMAGE} ${ASSISTED_CONTROLLER_IMAGE} ${ASSISTED_INSTALLER_IMAGE})
 export PUBLIC_CONTAINER_REGISTRIES=\$(for image in \${images}; do echo \${image} | cut -d'/' -f1; done | sort -u | paste -sd "," -)
 
-deploy/operator/setup_assisted_operator.sh
+deploy/operator/deploy.sh
 
 EOF
