7ef62d873e897a425b89e7ab5d6c3835071476e1
Vadim Rutkovsky <vrutkovs@redhat.com>
2021-06-22 11:36:24 +0200

    step-registry: add ipi-conf-user-workload-monitoring step
    This step would ensure cluster is configured with user workload monitoring feature enabled

--- ci-operator/step-registry/ipi/conf/user-workload-monitoring/ipi-conf-user-workload-monitoring-commands.sh
+++ ci-operator/step-registry/ipi/conf/user-workload-monitoring/ipi-conf-user-workload-monitoring-commands.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-set -o errexit
-set -o nounset
-set -o pipefail
 
-# Every cluster that can should have a PV for prometheus data so that data is preserved
-# across reschedules of pods. This may need to be conditionally disabled in the future
-# if certain instance types are used that cannot access persistent volumes.
+set -o nounset
+set -o errexit
+set -o pipefail
 
 # Use yq to create cluster monitoring config, as other steps may adjust it
 curl -L https://github.com/mikefarah/yq/releases/download/3.3.0/yq_linux_amd64 -o /tmp/yq && chmod +x /tmp/yq
 
 CONFIG="${SHARED_DIR}/manifest_cluster-monitoring-config.yaml"
-PATCH="${SHARED_DIR}/cluster-monitoring-config.yaml.patch"
+PATCH="/tmp/cluster-monitoring-config.yaml.patch"
 
 # Create config if empty
 touch "${CONFIG}"
@@ -29,14 +26,7 @@ EOF
 fi
 
 cat >> "${PATCH}" << EOF
-prometheusK8s:
-  volumeClaimTemplate:
-    metadata:
-      name: prometheus-data
-    spec:
-      resources:
-        requests:
-          storage: 10Gi
+enableUserWorkload: true
 EOF
 
 CONFIG_CONTENTS="$(echo "${CONFIG_CONTENTS}" | /tmp/yq m - "${PATCH}")"
