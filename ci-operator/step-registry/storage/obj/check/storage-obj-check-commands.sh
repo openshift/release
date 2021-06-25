21e23d34f44eaead5a15772d07ddd883c33046b8
Jan Safranek <jsafrane@redhat.com>
2021-06-25 10:46:09 +0200

    Add a fake junit file
    A fake junit file with a tests failed + passed should mark the test as
    flake, not as failure.

--- ci-operator/step-registry/storage/obj/check/storage-obj-check-commands.sh
+++ ci-operator/step-registry/storage/obj/check/storage-obj-check-commands.sh
@@ -22,6 +22,28 @@ function check_objects() {
     diff -u $EXPECTED $GOT
 }
 
+function report_flake() {
+    # Create a junit file with a flake, not a failure.
+    # In other words, have a test failed + passed there. CI will interpret it as a flake.
+    TEST="[sig-storage] Ensure that all storage objects are deleted during the tests"
+    MESSAGE="$1"
+    STDOUT="$2"
+    mkdir -p ${ARTIFACT_DIR}/junit/
+    cat  >${ARTIFACT_DIR}/junit/junit_check.xml << EOF
+<?xml version="1.0"?>
+<testsuite name="$TEST" tests="2" skipped="0" failures="1" time="1">
+  <testcase name="$TEST" time="0">
+    <failure message="">$MESSAGE</failure>
+    <system-out>$STDOUT</system-out>
+  </testcase>
+  <testcase name="$TEST" time="0"/>
+</testsuite>
+EOF
+    # For build-log.txt
+    echo $MESSAGE
+    echo $STDOUT
+}
+
 
 # Fail fast if step storage-obj-save did not succeed.
 if [ ! -e $SAVE_FILE ]; then
@@ -41,7 +63,7 @@ while true; do
     oc get $STORAGE_OBJECTS --no-headers --ignore-not-found -o name &> $CURRENT_OBJS || :
 
     # for debugging
-    oc get $STORAGE_OBJECTS -o yaml > $CURRENT_OBJS.yaml || :
+    oc get $STORAGE_OBJECTS -o yaml &> $CURRENT_OBJS.yaml || :
 
     if check_objects $CURRENT_OBJS ; then
         echo "All objects deleted"
@@ -50,13 +72,16 @@ while true; do
 
     NOW=$( date "+%s" )
     if [ "$NOW" -gt "$FINAL_TIME" ]; then
-        echo "ERROR: Timed out waiting for storage objects to get deleted."
-        echo "ERROR: It seems that some test left some of them behind or API server failed."
-        echo "ERROR: Check the diff between expected and existing objects above."
-        exit 1
+        echo "Timeout"
+        MSG="Error checking storage objects. Either some test left PVs / StorageClasses behind or connection to the API server failed. Check the diff between expected and existing objects in the test log."
+        STDOUT=$(check_objects $CURRENT_OBJS || :) # We know the diff fails, override its exit code with ':'
+        report_flake "$MSG" "$STDOUT"
+        exit 0
     fi
 
     STEP=$[ $STEP + 1 ]
 
     sleep 10
 done
+
+exit 0
