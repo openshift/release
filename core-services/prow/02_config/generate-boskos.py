f00aec1349b1e4976eca5e0e2b2c17ecee8d004b
Joseph Callen <jcallen@redhat.com>
2021-06-30 14:52:28 -0400

    vsphere: Update boskos for new segments
    Continued DHCP issues, modifications in segment
    dhcp config which can only be changed via delete and create.

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -98,7 +98,7 @@ for i in range(10, 20):
 for i in range(1, 3):
     CONFIG['kubevirt-quota-slice']['tenant-cluster-{}'.format(i)] = 1
 
-for i in range(40,53):
+for i in range(0,13):
     CONFIG['vsphere-quota-slice']['ci-segment-{}'.format(i)] = 1
 
 config = {
