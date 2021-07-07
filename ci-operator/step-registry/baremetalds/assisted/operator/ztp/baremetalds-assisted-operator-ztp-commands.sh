089519c548d92fd9fd4d1bf72d4e48348ddaee55
Yuval Goldberg <yuvigoldi@gmail.com>
2021-07-07 16:46:58 +0300

    Remove assisted ZTP redundant vars
    Currently, only ASSISTED_OPENSHIFT_INSTALL_RELEASE_IMAGE could and
    should be modified from the CI. All the other variables could stay
    hardcoded as part of the script on assisted-service.
    
    Configure ZTP variables on operator-setup step and use them in latter
    with `source /root/config`

--- ci-operator/step-registry/baremetalds/assisted/operator/ztp/baremetalds-assisted-operator-ztp-commands.sh
+++ ci-operator/step-registry/baremetalds/assisted/operator/ztp/baremetalds-assisted-operator-ztp-commands.sh
@@ -12,31 +12,10 @@ source "${SHARED_DIR}/packet-conf.sh"
 
 tar -czf - . | ssh "${SSHOPTS[@]}" "root@${IP}" "cat > /root/assisted-service.tar.gz"
 
-# Copy additional dev-script variables, if present
-if [[ -e "${SHARED_DIR}/ds-vars.conf" ]]
-then
-  scp "${SSHOPTS[@]}" "${SHARED_DIR}/ds-vars.conf" "root@${IP}:ds-vars.conf"
-fi
-
-# Copy job variables to the packet server
-echo "export OPENSHIFT_INSTALL_RELEASE_IMAGE=${OPENSHIFT_INSTALL_RELEASE_IMAGE}" >> /tmp/assisted-vars.conf
-echo "export ASSISTED_OPENSHIFT_INSTALL_RELEASE_IMAGE=${ASSISTED_OPENSHIFT_INSTALL_RELEASE_IMAGE}" >> /tmp/assisted-vars.conf
-echo "export ASSISTED_NAMESPACE=${ASSISTED_NAMESPACE}" >> /tmp/assisted-vars.conf
-echo "export ASSISTED_CLUSTER_NAME=${ASSISTED_CLUSTER_NAME}" >> /tmp/assisted-vars.conf
-echo "export ASSISTED_CLUSTER_DEPLOYMENT_NAME=${ASSISTED_CLUSTER_DEPLOYMENT_NAME}" >> /tmp/assisted-vars.conf
-echo "export ASSISTED_INFRAENV_NAME=${ASSISTED_INFRAENV_NAME}" >> /tmp/assisted-vars.conf
-echo "export ASSISTED_PRIVATEKEY_NAME=${ASSISTED_PRIVATEKEY_NAME}" >> /tmp/assisted-vars.conf
-echo "export ASSISTED_PULLSECRET_NAME=${ASSISTED_PULLSECRET_NAME}" >> /tmp/assisted-vars.conf
-echo "export ASSISTED_PULLSECRET_JSON=${ASSISTED_PULLSECRET_JSON}" >> /tmp/assisted-vars.conf
-scp "${SSHOPTS[@]}" "/tmp/assisted-vars.conf" "root@${IP}:assisted-vars.conf"
-
 # shellcheck disable=SC2087
 ssh "${SSHOPTS[@]}" "root@${IP}" bash - << EOF |& sed -e 's/.*auths\{0,1\}".*/*** PULL_SECRET ***/g'
 
 set -xeo pipefail
-
-source /root/ds-vars.conf
-source /root/assisted-vars.conf
 
 cd /root/dev-scripts
 source common.sh
@@ -52,9 +31,13 @@ if [ ! -d "\${REPO_DIR}" ]; then
 fi
 
 cd "\${REPO_DIR}/deploy/operator/ztp/"
+
 echo "### Deploying SNO spoke cluster..."
-export DISCONNECTED="${DISCONNECTED:-}"
+
 export EXTRA_BAREMETALHOSTS_FILE="/root/dev-scripts/\${EXTRA_BAREMETALHOSTS_FILE}"
+
+source /root/config
+
 ./deploy_spoke_cluster.sh
 
 EOF
