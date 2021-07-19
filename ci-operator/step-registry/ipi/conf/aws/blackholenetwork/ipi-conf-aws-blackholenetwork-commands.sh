91baaeecc8fee1a46a1c873bfd9c5ef9742f4c49
Johnny Liu <jialiu@redhat.com>
2021-07-19 13:22:38 +0800

    fixing zones mismatch issue

--- ci-operator/step-registry/ipi/conf/aws/blackholenetwork/ipi-conf-aws-blackholenetwork-commands.sh
+++ ci-operator/step-registry/ipi/conf/aws/blackholenetwork/ipi-conf-aws-blackholenetwork-commands.sh
@@ -9,6 +9,8 @@ export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 # TODO: move to image
 curl -L https://github.com/mikefarah/yq/releases/download/3.3.0/yq_linux_amd64 -o /tmp/yq && chmod +x /tmp/yq
 
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
 EXPIRATION_DATE=$(date -d '4 hours' --iso=minutes --utc)
 TAGS="Key=expirationDate,Value=${EXPIRATION_DATE}"
 
@@ -303,7 +305,7 @@ EOF
 
 MAX_ZONES_COUNT="$(cat "${SHARED_DIR}/maxzonescount")"
 
-ZONE_COUNT=3
+ZONE_COUNT=${ZONES_COUNT:-2}
 if [[ "${MAX_ZONES_COUNT}" -lt 3 ]]
 	
 then
@@ -330,7 +332,23 @@ echo "Subnets : ${subnets}"
 # save stack information to ${SHARED_DIR} for deprovision step
 echo "${STACK_NAME}" >> "${SHARED_DIR}/blackholenetworkstackname"
 
+# Generate working availability zones from the region
+mapfile -t AVAILABILITY_ZONES < <(aws --region "${REGION}" ec2 describe-availability-zones | jq -r '.AvailabilityZones[] | select(.State == "available") | .ZoneName' | sort -u)
+ZONES=("${AVAILABILITY_ZONES[@]:0:${ZONES_COUNT}}")
+ZONES_STR="[ "
+ZONES_STR+=$(join_by , "${ZONES[@]}")
+ZONES_STR+=" ]"
+echo "AWS region: ${REGION} (zones: ${ZONES_STR})"
+
 cat >> "${PATCH}" << EOF
+controlPlane:
+  platform:
+    aws:
+      zones: ${ZONES_STR}
+compute:
+- platform:
+    aws:
+      zones: ${ZONES_STR}
 platform:
   aws:
     subnets: ${subnets}
