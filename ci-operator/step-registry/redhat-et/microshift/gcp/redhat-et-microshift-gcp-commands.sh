b76cf6de9eaacc4209605cfba53845e8ecb9868f
jcope <jcope@redhat.com>
2021-07-27 15:23:12 -0500

    check env

--- ci-operator/step-registry/redhat-et/microshift/gcp/redhat-et-microshift-gcp-commands.sh
+++ ci-operator/step-registry/redhat-et/microshift/gcp/redhat-et-microshift-gcp-commands.sh
@@ -3,9 +3,11 @@ set -euo pipefail
 
 trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
 
-GOOGLE_PROJECT_ID="${GOOGLE_COMPUTE_ID:?'expected GOOGLE_PROJECT_ID to be set in env'}"
-GOOGLE_COMPUTE_REGION="${GOOGLE_COMPUTE_REGION:?'expected GOOGLE_COMPUTE_REGION to be set in env'}"
-GOOGLE_COMPUTE_ZONE="${GOOGLE_COMPUTE_ZONE:?'expected GOOGLE_COMPUTE_ZONE to be set in env'}"
+echo "`id`"
+
+GOOGLE_PROJECT_ID="us-east1"
+GOOGLE_COMPUTE_REGION="us-east1-a"
+GOOGLE_COMPUTE_ZONE="openshift-gce-devel"
 
 INSTANCE_PREFIX="microshift-release-ci-${NAMESPACE}-${JOB_NAME_HASH}"
 
@@ -14,15 +16,15 @@ chmod 0700 "${HOME}"/.ssh
 mock-nss.sh
 
 # gcloud compute will use this key rather than create a new one
-chmod 0600 "${HOME}"/.ssh/google_compute_engine
-echo 'ServerAliveInterval 30' | tee -a "${HOME}"/.ssh/config
-echo 'ServerAliveCountMax 1200' | tee -a "${HOME}"/.ssh/config
-chmod 0600 "${HOME}"/.ssh/config
+mock-nss.sh chmod 0600 "${HOME}"/.ssh/google_compute_engine
+mock-nss.sh echo 'ServerAliveInterval 30' | tee -a "${HOME}"/.ssh/config
+mock-nss.sh echo 'ServerAliveCountMax 1200' | tee -a "${HOME}"/.ssh/config
+mock-nss.sh chmod 0600 "${HOME}"/.ssh/config
 
-gcloud auth activate-service-account --quiet --key-file ""
-gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
-gcloud --quiet config set compute/zone "${GOOGLE_COMPUTE_ZONE}"
-gcloud --quiet config set compute/region "${GOOGLE_COMPUTE_REGION}"
+mock-nss.sh gcloud auth activate-service-account --quiet --key-file ""
+mock-nss.sh gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
+mock-nss.sh gcloud --quiet config set compute/zone "${GOOGLE_COMPUTE_ZONE}"
+mock-nss.sh gcloud --quiet config set compute/region "${GOOGLE_COMPUTE_REGION}"
 
 LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute instances create "$INSTANCE_PREFIX" --image-family=rhel-8 \
   --image-project=rhel-cloud \
