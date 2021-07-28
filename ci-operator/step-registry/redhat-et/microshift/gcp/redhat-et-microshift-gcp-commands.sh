8fb87a9959aa1896c50cc4735f9320de1c3704d1
jcope <jcope@redhat.com>
2021-07-28 09:40:45 -0500

    debugging

--- ci-operator/step-registry/redhat-et/microshift/gcp/redhat-et-microshift-gcp-commands.sh
+++ ci-operator/step-registry/redhat-et/microshift/gcp/redhat-et-microshift-gcp-commands.sh
@@ -16,15 +16,15 @@ chmod 0700 "${HOME}"/.ssh
 mock-nss.sh
 
 # gcloud compute will use this key rather than create a new one
-mock-nss.sh chmod 0600 "${HOME}"/.ssh/google_compute_engine
-mock-nss.sh echo 'ServerAliveInterval 30' | tee -a "${HOME}"/.ssh/config
-mock-nss.sh echo 'ServerAliveCountMax 1200' | tee -a "${HOME}"/.ssh/config
-mock-nss.sh chmod 0600 "${HOME}"/.ssh/config
+chmod 0600 "${HOME}"/.ssh/google_compute_engine
+echo 'ServerAliveInterval 30' | tee -a "${HOME}"/.ssh/config
+echo 'ServerAliveCountMax 1200' | tee -a "${HOME}"/.ssh/config
+chmod 0600 "${HOME}"/.ssh/config
 
-mock-nss.sh gcloud auth activate-service-account --quiet --key-file ""
-mock-nss.sh gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
-mock-nss.sh gcloud --quiet config set compute/zone "${GOOGLE_COMPUTE_ZONE}"
-mock-nss.sh gcloud --quiet config set compute/region "${GOOGLE_COMPUTE_REGION}"
+LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud auth activate-service-account --quiet --key-file ""
+LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
+LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud --quiet config set compute/zone "${GOOGLE_COMPUTE_ZONE}"
+LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud --quiet config set compute/region "${GOOGLE_COMPUTE_REGION}"
 
 LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute instances create "$INSTANCE_PREFIX" --image-family=rhel-8 \
   --image-project=rhel-cloud \
