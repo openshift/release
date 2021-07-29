2e31f1e9f7818ca984200cb3cebb9090830df783
Emilien Macchi <emilien@redhat.com>
2021-07-28 20:38:34 -0400

    openstack/bastion: collect squid logs
    If things go wrong with the proxy, we'll have squid logs which should
    help to debug.

--- ci-operator/step-registry/openstack/deprovision/bastionproxy/openstack-deprovision-bastionproxy-commands.sh
+++ ci-operator/step-registry/openstack/deprovision/bastionproxy/openstack-deprovision-bastionproxy-commands.sh
@@ -11,6 +11,28 @@ fi
 
 export OS_CLIENT_CONFIG_FILE=${SHARED_DIR}/clouds.yaml
 CLUSTER_NAME=$(<"${SHARED_DIR}"/CLUSTER_NAME)
+BASTION_FIP=$(<"${SHARED_DIR}"/BASTION_FIP)
+
+# configure the local container environment to have the correct SSH configuration
+if ! whoami &> /dev/null; then
+    if [[ -w /etc/passwd ]]; then
+        echo "${BASTION_USER}:x:$(id -u):0:${BASTION_USER:-centos} user:${HOME}:/sbin/nologin" >> /etc/passwd
+    fi
+fi
+# shellcheck disable=SC2140
+SSH_ARGS="-o ConnectTimeout=10 -o "StrictHostKeyChecking=no" -i ${CLUSTER_PROFILE_DIR}/ssh-privatekey"
+SSH_CMD="ssh ${SSH_ARGS} ${BASTION_USER}@${BASTION_FIP}"
+SCP_CMD="scp ${SSH_ARGS}"
+
+>&2 echo "Collecting squid logs from 'bastionproxy-$CLUSTER_NAME'"
+$SSH_CMD bash - <<EOF
+mkdir -p /tmp/squid-logs
+sudo cp /var/log/squid/access.log /tmp/squid-logs || true
+sudo cp /var/log/squid/cache.log /tmp/squid-logs || true
+sudo chown -R ${BASTION_USER}: /tmp/squid-logs || true
+tar -czC "/tmp" -f "/tmp/squid-logs.tar.gz" squid-logs/
+EOF
+$SCP_CMD ${BASTION_USER}@${BASTION_FIP}:/tmp/squid-logs.tar.gz ${ARTIFACT_DIR}
 
 >&2 echo "Starting the server cleanup for cluster name '$CLUSTER_NAME'"
 openstack server delete "bastionproxy-$CLUSTER_NAME" || >&2 echo "Failed to delete server bastionproxy-$CLUSTER_NAME"
