d0cb41368e4227b7f6b70b2eb5a358af82ba6055
jcope <jcope@redhat.com>
2021-07-29 11:04:15 -0500

    drop nss, set home dir to readable by root group

--- ci-operator/step-registry/redhat-et/microshift/gcp/redhat-et-microshift-gcp-commands.sh
+++ ci-operator/step-registry/redhat-et/microshift/gcp/redhat-et-microshift-gcp-commands.sh
@@ -9,19 +9,17 @@ GOOGLE_COMPUTE_ZONE="openshift-gce-devel"
 
 INSTANCE_PREFIX="microshift-release-ci-${NAMESPACE}-${JOB_NAME_HASH}"
 
-mock-nss.sh
-
 # gcloud compute will use this key rather than create a new one
 chmod 0600 "${HOME}"/.ssh/google_compute_engine
 echo 'ServerAliveInterval 30' | tee -a "${HOME}"/.ssh/config
 echo 'ServerAliveCountMax 1200' | tee -a "${HOME}"/.ssh/config
 chmod 0600 "${HOME}"/.ssh/config
 
-LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud auth activate-service-account --quiet --key-file "$HOME/.ssh/google_compute_engine"
-LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
-LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud --quiet config set compute/zone "${GOOGLE_COMPUTE_ZONE}"
-LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud --quiet config set compute/region "${GOOGLE_COMPUTE_REGION}"
+gcloud auth activate-service-account --quiet --key-file "$HOME/.ssh/google_compute_engine"
+gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
+gcloud --quiet config set compute/zone "${GOOGLE_COMPUTE_ZONE}"
+gcloud --quiet config set compute/region "${GOOGLE_COMPUTE_REGION}"
 
-LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute instances create "$INSTANCE_PREFIX" --image-family=rhel-8 \
+gcloud compute instances create "$INSTANCE_PREFIX" --image-family=rhel-8 \
   --image-project=rhel-cloud \
   --zone=us-central1-a
