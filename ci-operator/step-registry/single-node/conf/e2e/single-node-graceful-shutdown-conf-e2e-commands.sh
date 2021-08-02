13e15c67de5fe1e5996deebc01dde23bdc25a200
Skyler Clark <wgahnagl@protonmail.com>
2021-08-02 10:32:06 -0400

    adds ci job for graceful shutdown

---
+++ ci-operator/step-registry/single-node/conf/e2e/single-node-graceful-shutdown-conf-e2e-commands.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Reserve more system memory per node than a typical multi-node cluster
+# to facilitate E2E tests on a single node.
+cat > "${SHARED_DIR}/manifest_single-node-reserve-sys-mem-kubeletconfig.yml" << EOF
+apiVersion: machineconfiguration.openshift.io/v1
+kind: KubeletConfig
+metadata:
+  name: single-node-reserve-sys-mem
+spec:
+  containers:
+    machineConfigPoolSelector:
+    matchLabels:
+      pools.operator.machineconfiguration.openshift.io/master: ""
+  kubeletConfig:
+    shutdownGracePeriod: 600s
+    shutdownGracePeriodCriticalPods: 300s
+    systemReserved:
+      memory: 3Gi
+EOF
