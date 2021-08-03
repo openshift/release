ef706d016e377abe83e2cd8f7136d418d10b5d72
Hemant Kumar <hekumar@redhat.com>
2021-08-03 12:34:43 -0400

    Write steps for running e2e tests for EFS CSI driver

---
+++ ci-operator/step-registry/storage/conf/csi-aws-efs/storage-create-csi-aws-efs-commands.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+export STORAGEClASS_LOCATION=${SHARED_DIR}/efs-sc.yaml
+export MANIFEST_LOCATION=${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
+
+/usr/bin/create-efs-volume
+
+echo "Using storageclass ${STORAGEClASS_LOCATION}"
+cat ${STORAGEClASS_LOCATION}
+
+oc create -f ${STORAGEClASS_LOCATION}
+echo "Created storageclass from file ${STORAGEClASS_LOCATION}"
+
+oc create -f - <<EOF
+apiVersion: operator.openshift.io/v1
+kind: ClusterCSIDriver
+metadata:
+    name: efs.csi.aws.com
+spec:
+  managementState: Managed
+EOF
+
+echo "Created cluster CSI driver object"
+
+# For debugging
+echo "Using ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}:"
+cat ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
