f18946d5c9ac840fb21236b4126c2b336c71ece2
Johnny Liu <jialiu@redhat.com>
2021-08-03 13:42:20 +0800

    adding yj install

--- ci-operator/step-registry/ipi/conf/aws/ipi-conf-aws-commands.sh
+++ ci-operator/step-registry/ipi/conf/aws/ipi-conf-aws-commands.sh
@@ -4,6 +4,9 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# TODO: move to image
+curl -L https://github.com/mikefarah/yq/releases/download/3.3.0/yq_linux_amd64 -o /tmp/yq && chmod +x /tmp/yq
+
 export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 
 if [[ "${CLUSTER_TYPE}" == "aws-arm64" ]]; then
