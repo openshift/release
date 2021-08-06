e2fdf21c9e1bf429af8dc1f27c6172a7da7894da
Adam Kaplan <adam.kaplan@redhat.com>
2021-08-06 14:44:43 -0400

    Update deploy and e2e steps:
    - Deploy step uses image dependencies to install CI images
    - Add timeout env argument

--- ci-operator/step-registry/csi-driver-shared-resource/e2e/csi-driver-shared-resource-e2e-commands.sh
+++ ci-operator/step-registry/csi-driver-shared-resource/e2e/csi-driver-shared-resource-e2e-commands.sh
@@ -5,15 +5,14 @@ set -o errexit
 set -o pipefail
 
 # Runs an e2e suite for the Shared Resource CSI driver
-# The suite assumes that the CSI driver prerequisites have been deployed to the cluster.
-# However, the test runs its own deployment of the driver.
+# The suite assumes that the CSI driver has been deployed to the cluster.
 # 
 # The KUBECONFIG environment variable must be set for the step to run to full completion.
 #
 # The command utilizes two environment variables:
 #
-# - DRIVER_IMAGE: The driver image to deploy. Defaults to the latest origin upstream image.
 # - TEST_SUITE: The test suite to run. Defaults to "normal", can also be "disruptive" and "slow".
+# - TEST_TIMEOUT: The test suite to run. Defaults to "30m", can be any parsable duration.
 
 echo "Starting step csi-driver-shared-resource-e2e."
 if ! [[ -f ${KUBECONFIG} ]]; then
@@ -21,11 +20,11 @@ if ! [[ -f ${KUBECONFIG} ]]; then
     exit 0
 fi
 
-image=${DRIVER_IMAGE:-"quay.io/openshift/origin-csi-driver-shared-resource:latest"}
 suite=${TEST_SUITE:-"normal"}
+timeout=${TEST_TIMEOUT:-"30m"}
 
-echo "Starting e2e test suite ${suite} with driver image ${image}."
-KUBERNETES_CONFIG=${KUBECONFIG} IMAGE_NAME=${image} go test -race -count 1 -tags "${suite}" -timeout 30m -v ./test/e2e/...
+echo "Starting e2e test suite ${suite} with timeout ${timeout}."
+make test-e2e-no-deploy TEST_SUITE="${suite}" TEST_TIMEOUT="${timeout}"
 echo "Finished e2e test suite ${suite}."
 
 echo "Step csi-driver-shared-resource-e2e completed."
