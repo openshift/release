8ac6021fd8e860d9db9e2245396e762dc70d41db
Jianwei Hou <jhou@redhat.com>
2021-08-06 18:14:23 +0800

    run tests in parallel

--- ci-operator/step-registry/cucushift/cucushift-commands.sh
+++ ci-operator/step-registry/cucushift/cucushift-commands.sh
@@ -4,6 +4,8 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+scl enable rh-ruby27 bash
+
 # prepare users
 users=""
 data_htpasswd=""
@@ -49,20 +51,17 @@ spec:
 EOF
 
 # Export those parameters before running
-export BUSHSLICER_DEFAULT_ENVIRONMENT=ocp4
-export BUSHSLICER_REPORT_DIR=${ARTIFACT_DIR}
-
-export OPENSHIFT_ENV_OCP4_ADMIN_CREDS_SPEC=${KUBECONFIG}
-
-export OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS=${users}
-
 hosts=`grep server ${KUBECONFIG} | cut -d '/' -f 3 | cut -d ':' -f 1`
 export OPENSHIFT_ENV_OCP4_HOSTS="${hosts}:lb"
+export OPENSHIFT_ENV_OCP4_ADMIN_CREDS_SPEC=${KUBECONFIG}
 
 ver_cli=`oc version | grep Client | cut -d ' ' -f 3`
 export BUSHSLICER_CONFIG="{'environments': {'ocp4': {'version': '${ver_cli:0:3}'}}}"
+export BUSHSLICER_DEFAULT_ENVIRONMENT=ocp4
+export BUSHSLICER_REPORT_DIR=${ARTIFACT_DIR}
 
 cd verification-tests
-scl enable rh-ruby27 -- cucumber -p junit --tags "@smoke and not @admin and not @destructive"
-#scl enable rh-ruby27 cucumber -p junit --tags "@admin and not @destructive"
-#scl enable rh-ruby27 cucumber -p junit --tags "@destructive"
+parallel_cucumber -n 4 --first-is-1 --type cucumber --prefix-output-with-test-env-number --exec \
+    'export OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS=$(echo ${users} | cut -d "," -f ${TEST_ENV_NUMBER});
+     export WORKSPACE=$HOME/workdir/dir${TEST_ENV_NUMBER};
+     parallel_cucumber --group-by found --only-group ${TEST_ENV_NUMBER} -o "--tags \"@smoke and not @admin and not @destructive and not @flake and not @inactive\" -p junit"'
