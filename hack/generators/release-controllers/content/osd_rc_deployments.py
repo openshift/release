a4ef7119fabfc2aeab33d293dbc20a34e7f68fe9
Alex Pavel <apavel@redhat.com>
2021-08-12 11:15:21 -0700

    release-controller: add servicemonitors
    PR #21042 intended to add prometheus scraping for release-controller via
    scrape annotations, however the DPTP prometheus is not configured to use
    those annotations. This PR removes those annotations and instead adds
    servicemonitors similar to other DPTP tools.

--- hack/generators/release-controllers/content/osd_rc_deployments.py
+++ hack/generators/release-controllers/content/osd_rc_deployments.py
@@ -79,6 +79,7 @@ def _add_osd_rc_service(gendoc):
         },
         'spec': {
             'ports': [{
+                'name': 'main',
                 'port': 443 if context.private else 80,
                 'targetPort': 8443 if context.private else 8080
             }],
@@ -88,6 +89,34 @@ def _add_osd_rc_service(gendoc):
         }
     })
 
+def _add_osd_rc_servicemonitor(gendoc):
+    annotations = {}
+    context = gendoc.context
+
+    gendoc.append({
+        'apiVersion': 'monitoring.coreos.com/v1',
+        'kind': 'ServiceMonitor',
+        'metadata': {
+            'name': context.rc_service_name,
+            'namespace': 'prow-monitoring',
+            'annotations': annotations,
+        },
+        'spec': {
+            'endpoints': [{
+                'interval': '30s',
+                'port': 'main',
+                'scheme': 'https',
+            }],
+            'namespaceSelector': {
+                'matchNames': ['ci'],
+            },
+            'selector': {
+                'matchLabels': {
+                    'app': context.rc_service_name,
+                }
+            }
+        }
+    })
 
 def _get_osd_rc_deployment_sidecars(context):
     sidecars = list()
@@ -163,12 +192,7 @@ def _add_osd_rc_deployment(gendoc):
                 'metadata': {
                     'labels': {
                         'app': context.rc_service_name
-                    },
-                    'annotations': {
-                        'prometheus.io/scrape': 'true',
-                        'prometheus.io/port': '80',
-                        'prometheus.io/path': '/metrics'
-                    },
+                    }
                 },
                 'spec': {
                     'containers': [
@@ -223,4 +247,5 @@ the app.ci clusters.
     _add_osd_rc_bootstrap(gendoc)
     _add_osd_rc_route(gendoc)
     _add_osd_rc_service(gendoc)
+    _add_osd_rc_servicemonitor(gendoc)
     _add_osd_rc_deployment(gendoc)
