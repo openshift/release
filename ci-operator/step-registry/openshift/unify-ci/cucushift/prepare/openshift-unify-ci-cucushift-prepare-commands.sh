92017ed3603784f51215fda8c2b62a80da19e854
Jian Li <jianl@redhat.com>
2021-08-17 16:50:27 +0800

    Comment out test script for temp test

--- ci-operator/step-registry/openshift/unify-ci/cucushift/prepare/openshift-unify-ci-cucushift-prepare-commands.sh
+++ ci-operator/step-registry/openshift/unify-ci/cucushift/prepare/openshift-unify-ci-cucushift-prepare-commands.sh
@@ -48,20 +48,20 @@ spec:
         name: htpass-secret
 EOF
 
-# Export those parameters before running
-export BUSHSLICER_DEFAULT_ENVIRONMENT=ocp4
-export OPENSHIFT_ENV_OCP4_USER_MANAGER=UpgradeUserManager
-export BUSHSLICER_REPORT_DIR=${ARTIFACT_DIR}
-export OPENSHIFT_ENV_OCP4_ADMIN_CREDS_SPEC=${KUBECONFIG}
-export KUBECONFIG=${KUBECONFIG}
-export OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS=${users}
+# # Export those parameters before running
+# export BUSHSLICER_DEFAULT_ENVIRONMENT=ocp4
+# export OPENSHIFT_ENV_OCP4_USER_MANAGER=UpgradeUserManager
+# export BUSHSLICER_REPORT_DIR=${ARTIFACT_DIR}
+# export OPENSHIFT_ENV_OCP4_ADMIN_CREDS_SPEC=${KUBECONFIG}
+# export KUBECONFIG=${KUBECONFIG}
+# export OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS=${users}
 
-hosts=`grep server ${KUBECONFIG} | cut -d '/' -f 3 | cut -d ':' -f 1`
-export OPENSHIFT_ENV_OCP4_HOSTS="${hosts}:lb"
+# hosts=`grep server ${KUBECONFIG} | cut -d '/' -f 3 | cut -d ':' -f 1`
+# export OPENSHIFT_ENV_OCP4_HOSTS="${hosts}:lb"
 
-ver_cli=`oc version | grep Client | cut -d ' ' -f 3`
-export BUSHSLICER_CONFIG="{'environments': {'ocp4': {'version': '${ver_cli:0:3}'}}}"
+# ver_cli=`oc version | grep Client | cut -d ' ' -f 3`
+# export BUSHSLICER_CONFIG="{'environments': {'ocp4': {'version': '${ver_cli:0:3}'}}}"
 
-cd verification-tests
-scl enable rh-ruby27 cucumber -p junit --tags "@upgrade-prepare"
-# bash -l -c "bundle exec cucumber --tags @upgrade-prepare --format junit --out ./"
+# cd verification-tests
+# scl enable rh-ruby27 cucumber -p junit --tags "@upgrade-prepare"
+# # bash -l -c "bundle exec cucumber --tags @upgrade-prepare --format junit --out ./"
