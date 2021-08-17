e3ba810309894f878bfe0e2584f541cf4bc9dc7b
Skyler Clark <wgahnagl@protonmail.com>
2021-08-17 17:06:45 -0400

    change kubelet overwriting to yq change

--- ci-operator/step-registry/single-node/conf/e2e/graceful-shutdown/single-node-conf-e2e-graceful-shutdown-commands.sh
+++ ci-operator/step-registry/single-node/conf/e2e/graceful-shutdown/single-node-conf-e2e-graceful-shutdown-commands.sh
@@ -4,22 +4,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Reserve more system memory per node than a typical multi-node cluster
-# to facilitate E2E tests on a single node
-# set shutdown grace period toenable testing graceful shutdown
-cat > "${SHARED_DIR}/manifest_single-node-reserve-sys-mem-kubeletconfig.yml" << EOF
-apiVersion: machineconfiguration.openshift.io/v1
-kind: KubeletConfig
-metadata:
-  name: single-node-reserve-sys-mem
-spec:
-  containers:
-    machineConfigPoolSelector:
-    matchLabels:
-      pools.operator.machineconfiguration.openshift.io/master: ""
-  kubeletConfig:
-    shutdownGracePeriod: 600s
-    shutdownGracePeriodCriticalPods: 300s
-    systemReserved:
-      memory: 3Gi
-EOF
+# set shutdown grace period to enable testing graceful shutdown
+yq e -i '.spec.kubeletConfig |= . + {"shutdownGracePeriod": "600s", "shutdownGracePeriodCriticalPods": "300s"}' "${SHARED_DIR}/manifest_single-node-kubeletconfig.yml"
