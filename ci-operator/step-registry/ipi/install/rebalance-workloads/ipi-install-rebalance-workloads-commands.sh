8fc320a5a7ae886f5284ad72be3aede69e641c59
Simon Pasquier <spasquie@redhat.com>
2021-08-24 14:15:47 +0200

    Add logs to ipi-install-rebalance-workloads-commands.sh script

--- ci-operator/step-registry/ipi/install/rebalance-workloads/ipi-install-rebalance-workloads-commands.sh
+++ ci-operator/step-registry/ipi/install/rebalance-workloads/ipi-install-rebalance-workloads-commands.sh
@@ -13,23 +13,29 @@ function rebalancePods() {
   # If the workload is spread across more than 1 node, we don't need to rebalance its pods
   NODE_COUNT=$(wc -w <<< "${NODE}")
   if [ "${NODE_COUNT}" -ne 1 ]; then
+    echo "pods matching ${LABEL_SELECTOR} in namespace ${NS} are correctly spread"
     return
   fi
 
+  echo "need to rebalance pods matching ${LABEL_SELECTOR} in namespace ${NS}..."
+
   oc adm cordon "${NODE}"
-  
+
   # Get all pods except one to keep it on the original node
   PODS=$(oc get pods -n "${NS}" -l "${LABEL_SELECTOR}" -o jsonpath=\{'.items[*].metadata.name'\} | tr ' ' '\n' | head -n-1)
 
   for POD in $PODS; do
     PVC=$(oc get pod -n "${NS}" "${POD}" -ojson | jq -r '.spec.volumes[] | select(.persistentVolumeClaim!=null) | .persistentVolumeClaim.claimName')
     if [ -n "${PVC}" ]; then
+      echo "deleting PVC ${PVC} in namespace {NS}"
       oc delete pvc -n "${NS}" "${PVC}"
     fi
+    echo "deleting pod ${POD} in namespace {NS}"
     oc delete pod -n "${NS}" "${POD}"
   done
 
   oc adm uncordon "${NODE}"
+  echo "pods matching ${LABEL_SELECTOR} in namespace ${NS} have been rebalanced"
 }
 
 rebalancePods openshift-monitoring "app.kubernetes.io/name=prometheus"
