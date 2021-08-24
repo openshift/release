ad34a5de4a738e21018bace11f7e9a6b7a1989a0
Brad Williams <brad.williams@redhat.com>
2021-08-24 18:03:15 -0400

    Changing reservered variable name

--- ci-operator/step-registry/openshift/release/analysis/aggregator/openshift-release-analysis-aggregator-commands.sh
+++ ci-operator/step-registry/openshift/release/analysis/aggregator/openshift-release-analysis-aggregator-commands.sh
@@ -4,4 +4,4 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-echo "job-run-aggregator analyze-job-runs --google-oauth-credential-file=${GOOGLE_OAUTH_CREDENTIAL_FILE} --job=${JOB_NAME} --payload-tag=${PAYLOAD_TAG} --job-start-time=${JOB_START_TIME} --working-dir=${WORKING_DIR}"
+echo "job-run-aggregator analyze-job-runs --google-oauth-credential-file=${GOOGLE_OAUTH_CREDENTIAL_FILE} --job=${VERIFICATION_JOB_NAME} --payload-tag=${PAYLOAD_TAG} --job-start-time=${JOB_START_TIME} --working-dir=${WORKING_DIR}"
