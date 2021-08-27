6a6b2bbfe620e41a98bfee3f5160c077f38f5c72
Brad Williams <brad.williams@redhat.com>
2021-08-27 13:53:23 -0400

    Enabling the job-run-aggregator

--- ci-operator/step-registry/openshift/release/analysis/aggregator/openshift-release-analysis-aggregator-commands.sh
+++ ci-operator/step-registry/openshift/release/analysis/aggregator/openshift-release-analysis-aggregator-commands.sh
@@ -4,4 +4,8 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-echo "job-run-aggregator analyze-job-runs --google-oauth-credential-file=${GOOGLE_OAUTH_CREDENTIAL_FILE} --job=${VERIFICATION_JOB_NAME} --payload-tag=${PAYLOAD_TAG} --job-start-time=${JOB_START_TIME} --working-dir=${WORKING_DIR}"
+job-run-aggregator analyze-job-runs \
+  --google-oauth-credential-file=${GOOGLE_OAUTH_CREDENTIAL_FILE} \
+  --job=${VERIFICATION_JOB_NAME} --payload-tag=${PAYLOAD_TAG} \
+  --job-start-time=${JOB_START_TIME} \
+  --working-dir=${WORKING_DIR}
