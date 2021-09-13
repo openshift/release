5396ae5d826d40ea428ac7f82288b82a050610c7
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2021-09-13 16:28:12 -0400

    set datastore path based on target cloud

--- ci-operator/step-registry/ipi/conf/vsphere/ipi-conf-vsphere-commands.sh
+++ ci-operator/step-registry/ipi/conf/vsphere/ipi-conf-vsphere-commands.sh
@@ -14,6 +14,7 @@ vsphere_datacenter="SDDC-Datacenter"
 vsphere_datastore="WorkloadDatastore"
 vsphere_cluster="Cluster-1"
 vsphere_url="vcenter.sddc-44-236-21-251.vmwarevmc.com"
+VSPHERE_CLUSTER_LOCATION=VMC
 TFVARS_PATH=/var/run/vault/vsphere/secret.auto.tfvars
 
 declare -a vips
@@ -22,7 +23,8 @@ mapfile -t vips < "${SHARED_DIR}/vips.txt"
 # **testing** for IBM cloud, only run specific jobs on specific lease numbers
 if [ $((${LEASED_RESOURCE//[!0-9]/})) -ge 88 ]; then     
   echo Scheduling job on IBM Cloud instance
-  TFVARS_PATH=/var/run/vault/ibmcloud/secret.auto.tfvars
+  VSPHERE_CLUSTER_LOCATION=IBM
+  TFVARS_PATH=/var/run/vault/ibmcloud/secret.auto.tfvars  
   vsphere_url="ibmvcenter.vmc-ci.devcluster.openshift.com"
   vsphere_datacenter="IBMCloud"
   vsphere_cluster="vcs-ci-workload"
@@ -34,7 +36,7 @@ vsphere_user=$(grep -oP 'vsphere_user\s*=\s*"\K[^"]+' ${TFVARS_PATH})
 vsphere_password=$(grep -oP 'vsphere_password\s*=\s*"\K[^"]+' ${TFVARS_PATH})
 base_domain=$(<"${SHARED_DIR}"/basedomain.txt)
 machine_cidr=$(<"${SHARED_DIR}"/machinecidr.txt)
-
+echo "${VCENTER_CLUSTER_LOCATION}" > "${SHARED_DIR}/vsphere_cluster_location"
 
 cat >> "${CONFIG}" << EOF
 baseDomain: $base_domain
--- ci-operator/step-registry/storage/conf/csi-vsphere/storage-conf-csi-vsphere-commands.sh
+++ ci-operator/step-registry/storage/conf/csi-vsphere/storage-conf-csi-vsphere-commands.sh
@@ -3,7 +3,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+export VSPHERE_DATASTORE_PATH="ds:///vmfs/volumes/vsan:86c7dbc3da924855-b20d5cd1f4eec976/"
+VSPHERE_CLUSTER_LOCATION=$(cat ${SHARED_DIR}/vsphere_cluster_location)
+
+if [ "$VSPHERE_CLUSTER_LOCATION" == "IBM" ]; then
+    export VSPHERE_DATASTORE_PATH="ds:///vmfs/volumes/vsan:523ea352e875627d-b090c96b526bb79c/"
+fi
+
 cd /go/src/github.com/openshift/vmware-vsphere-csi-driver-operator
+envsubst < test/e2e/storageclass-ci.yaml > test/e2e/storageclass-ci.yaml
 cp test/e2e/manifest.yaml ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
 
 # For debugging
