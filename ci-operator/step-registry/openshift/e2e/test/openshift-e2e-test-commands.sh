ec8607d8cae92bd864269454187e27edc11f32fb
Yunfei Jiang <61686698+yunjiang29@users.noreply.github.com>
2021-09-14 15:23:32 +0800

    fix e2e test

--- ci-operator/step-registry/openshift/e2e/test/openshift-e2e-test-commands.sh
+++ ci-operator/step-registry/openshift/e2e/test/openshift-e2e-test-commands.sh
@@ -110,7 +110,7 @@ gcp)
     REGION="$(oc get -o jsonpath='{.status.platformStatus.gcp.region}' infrastructure cluster)"
     export TEST_PROVIDER="{\"type\":\"gce\",\"region\":\"${REGION}\",\"multizone\": true,\"multimaster\":true,\"projectid\":\"${PROJECT}\"}"
     ;;
-aws|aws-arm64)
+aws|aws-arm64|aws-china)
     mkdir -p ~/.ssh
     cp "${CLUSTER_PROFILE_DIR}/ssh-privatekey" ~/.ssh/kube_aws_rsa || true
     export PROVIDER_ARGS="-provider=aws -gce-zone=us-east-1"
