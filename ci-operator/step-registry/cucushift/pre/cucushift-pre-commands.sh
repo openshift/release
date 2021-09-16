0bb88c2c05d56b739000578812559dd45b1b07f7
liangxia <lxia@redhat.com>
2021-09-16 11:37:11 +0800

    Fix CLI version info

--- ci-operator/step-registry/cucushift/pre/cucushift-pre-commands.sh
+++ ci-operator/step-registry/cucushift/pre/cucushift-pre-commands.sh
@@ -58,7 +58,7 @@ done
 
 # configure cucushift runtime environment variables
 hosts=`grep server ${KUBECONFIG} | cut -d '/' -f 3 | cut -d ':' -f 1`
-ver_cli=`oc version | grep Client | cut -d ' ' -f 3`
+ver_cli=`oc version --client | cut -d ' ' -f 3 | cut -d '.' -f1,2`
 users=${users::-1}
 
 runtime_env=${SHARED_DIR}/runtime_env
@@ -69,11 +69,10 @@ export CUCUMBER_PUBLISH_QUIET=true
 export DISABLE_WAIT_PRINT=true
 
 # Export those parameters before running
+export BUSHSLICER_DEFAULT_ENVIRONMENT=ocp4
 export OPENSHIFT_ENV_OCP4_HOSTS="${hosts}:lb"
 export OPENSHIFT_ENV_OCP4_ADMIN_CREDS_SPEC=file:///tmp/kubeconfig
-
-export BUSHSLICER_CONFIG="{'environments': {'ocp4': {'version': '${ver_cli:0:3}'}}}"
-export BUSHSLICER_DEFAULT_ENVIRONMENT=ocp4
+export BUSHSLICER_CONFIG="{'environments': {'ocp4': {'version': '${ver_cli}'}}}"
 
 export USERS=${users}
 EOF
