dd923b1f41e7494f71ed01e80180bc2d4b6fffe9
Pawan Pinjarkar <pawan.pinjarkar@gmail.com>
2021-09-24 13:45:41 -0400

    assisted-service operator upgrade job improvements

--- ci-operator/step-registry/baremetalds/assisted/operator/ztp/after-upgrade/baremetalds-assisted-operator-ztp-after-upgrade-commands.sh
+++ ci-operator/step-registry/baremetalds/assisted/operator/ztp/after-upgrade/baremetalds-assisted-operator-ztp-after-upgrade-commands.sh
@@ -10,22 +10,6 @@ echo "************ baremetalds assisted operator ztp command ************"
 # shellcheck source=/dev/null
 source "${SHARED_DIR}/packet-conf.sh"
 
-# # ZTP scripts have a lot of default values for the spoke cluster configuration. Adding this so that they can be changed.
-# if [[ -n "${ASSISTED_ZTP_CONFIG:-}" ]]; then
-#   readarray -t config <<< "${ASSISTED_ZTP_CONFIG}"
-#   for var in "${config[@]}"; do
-#     if [[ ! -z "${var}" ]]; then
-#       echo "export ${var}" >> "${SHARED_DIR}/assisted-ztp-config"
-#     fi
-#   done
-# fi
-
-# # Copy configuration for ZTP vars if present
-# if [[ -e "${SHARED_DIR}/assisted-ztp-config" ]]
-# then
-#   scp "${SSHOPTS[@]}" "${SHARED_DIR}/assisted-ztp-config" "root@${IP}:assisted-ztp-config"
-# fi
-
 tar -czf - . | ssh "${SSHOPTS[@]}" "root@${IP}" "cat > /root/assisted-service.tar.gz"
 
 # shellcheck disable=SC2087
@@ -52,27 +36,21 @@ cd "\${REPO_DIR}/deploy/operator/"
 echo "### Deploying spoke cluster..."
 
 export EXTRA_BAREMETALHOSTS_FILE="/root/dev-scripts/\${EXTRA_BAREMETALHOSTS_FILE}"
+export ASSISTED_OPENSHIFT_INSTALL_RELEASE_IMAGE=${ASSISTED_OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE}
 
 source /root/config
 
-# # Inject job configuration for ZTP, if available
-# if [[ -e /root/assisted-ztp-config ]]
-# then
-#   source /root/assisted-ztp-config
-# fi
-
-# TODO(ppinjark): Fix after_upgrade.sh file to use override variables
-# source upgrade/after_upgrade.sh
-
+export ASSISTED_OPENSHIFT_VERSION="${ASSISTED_OPENSHIFT_VERSION_OVERRIDE}"
 export ASSISTED_UPGRADE_OPERATOR="${ASSISTED_UPGRADE_OPERATOR_OVERRIDE}"
 export ASSISTED_STOP_AFTER_AGENT_DISCOVERY="${ASSISTED_STOP_AFTER_AGENT_DISCOVERY_OVERRIDE}"
+export ASSISTED_DEPLOYMENT_METHOD="${ASSISTED_DEPLOYMENT_METHOD}"
+export CHANNEL="${CHANNEL_UPGRADE_OVERRIDE}"
 export ASSISTED_CLUSTER_NAME="${ASSISTED_CLUSTER_NAME}"
 export ASSISTED_CLUSTER_DEPLOYMENT_NAME="${ASSISTED_CLUSTER_DEPLOYMENT_NAME}"
 export ASSISTED_AGENT_CLUSTER_INSTALL_NAME="${ASSISTED_AGENT_CLUSTER_INSTALL_NAME}"
 export ASSISTED_INFRAENV_NAME="${ASSISTED_INFRAENV_NAME}"
 
-# TODO(ppinjark): Fix common.sh file to use override variables
-export ASSISTED_OPENSHIFT_VERSION="openshift-v4.9.0"
+source upgrade/after_upgrade.sh
 
 ./ztp/deploy_spoke_cluster.sh
 
