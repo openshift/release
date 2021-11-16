21428c543705cea0249975022066ee81a54139e7
Akhil Rane <akhil131192@gmail.com>
2021-11-16 17:48:15 -0500

    add workflow for creating cluster with user OIDC provider on GCP

---
+++ ci-operator/step-registry/ipi/conf/gcp/oidc-creds-provision/ipi-conf-gcp-oidc-creds-provision-commands.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+MPREFIX="${SHARED_DIR}/manifest"
+TPREFIX="${SHARED_DIR}/tls"
+infra_name=${NAMESPACE}-${JOB_NAME_HASH}
+export GCP_SHARED_CREDENTIALS_FILE=${CLUSTER_PROFILE_DIR}/gce.json
+export GOOGLE_APPLICATION_CREDENTIALS="${GCP_SHARED_CREDENTIALS_FILE}"
+PROJECT="$(< ${CLUSTER_PROFILE_DIR}/openshift_gcp_project)"
+
+# extract ccoctl from the release image
+oc registry login
+CCO_IMAGE=$(oc adm release info --image-for='cloud-credential-operator' "$RELEASE_IMAGE_LATEST")
+cd "/tmp"
+oc image extract "$CCO_IMAGE" --file="/usr/bin/ccoctl"
+chmod 555 "/tmp/ccoctl"
+
+# extract gcp credentials requests from the release image
+oc adm release extract --credentials-requests --cloud=gcp --to="/tmp/credrequests" "$RELEASE_IMAGE_LATEST"
+
+# create required credentials infrastructure and installer manifests for workload identity
+export GOOGLE_APPLICATION_CREDENTIALS="${GCP_SHARED_CREDENTIALS_FILE}"
+"/tmp/ccoctl" gcp create-all --name="${infra_name}" --project="${PROJECT}" --region="${LEASED_RESOURCE}" --credentials-requests-dir="/tmp/credrequests" --output-dir="/tmp"
+
+# copy generated service account signing from ccoctl target directory into shared directory
+cp "/tmp/tls/bound-service-account-signing-key.key" "${TPREFIX}_bound-service-account-signing-key.key"
+
+# copy generated secret manifests from ccoctl target directory into shared directory
+cd "/tmp/manifests"
+for FILE in *; do cp $FILE "${MPREFIX}_$FILE"; done
