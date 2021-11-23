9dc328b4e186803992e5ff7bb5bb282863c2ef5e
Bruno Barcarol Guimarães <bbarcaro@redhat.com>
2021-11-23 12:13:34 +0000

    dptp logs: group by component

--- docs/dptp-triage-sop/logs/table.py
+++ docs/dptp-triage-sop/logs/table.py
@@ -1,70 +1,72 @@
 #!/usr/bin/env python3
+import itertools
 import json
 import os
 import sys
 
 from tabulate import tabulate
 
-headers = ["time", "level", "component", "message", "file", "func", "fields"]
-truncated_headers = ["time", "level", "component", "message"]
-ignored_fields = ["kubernetes", "severity", "source_type", "stream"]
+headers = ["time", "level", "message", "file", "func", "fields"]
+truncated_headers = ["time", "level", "message"]
+ignored_fields = ["component", "kubernetes", "severity", "source_type", "stream"]
+term_width = os.get_terminal_size().columns
 
 output_file = sys.argv[1] + ".table"
 output = open(output_file, "w+")
 truncated_output = sys.stdout
+write_both = lambda x: output.write(x) + truncated_output.write(x)
+
 data = json.load(open(sys.argv[1]))
-entries = []
+data = itertools.chain(*data)
+data = filter(lambda x: x["field"] == "@message", data)
+data = map(lambda x: json.loads(x["value"]), data)
+data = sorted(data, key=lambda x: x.get("component", ""))
+data = itertools.groupby(data, lambda x: x.get("component", ""))
+for component, fields in data:
+    entries = []
+    for raw_entry in fields:
+        for key in ignored_fields:
+            raw_entry.pop(key, None)
+        entry = []
+        entry.append(raw_entry.pop("time", ""))
+        entry.append(raw_entry.pop("level", ""))
+        message = "msg=" + raw_entry.pop("msg", "")
+        if "error" in raw_entry:
+            message += ", error=" + raw_entry.pop("error", "")
+        entry.append(message)
+        entry.append(raw_entry.pop("file", ""))
+        entry.append(raw_entry.pop("func", ""))
+        fields = []
+        for key, value in sorted(raw_entry.items()):
+            fields.append("{}={}".format(key, value))
+        entry.append(",".join(fields))
+        entries.append(entry)
+    width = term_width
+    largest = [0] * (len(truncated_headers) - 1)
+    for entry in entries:
+        for i in range(len(largest)):
+            if len(entry[i]) > largest[i]:
+                largest[i] = len(entry[i])
 
-for item in data:
-	for field in item:
-		if field["field"] == "@message":
-			raw_entry = json.loads(field["value"])
-			for key in ignored_fields:
-				raw_entry.pop(key, None)
+    for item in largest:
+        width -= item
 
-			entry = [
-				raw_entry.pop("time", ""),
-				raw_entry.pop("level", ""),
-				raw_entry.pop("component", ""),
-			]
-			
-			message = "msg=" + raw_entry.pop("msg", "")
-			if "error" in raw_entry:
-				message += ", error=" + raw_entry.pop("error", "")
-			entry.append(message)
+    truncated_entries = []
+    width -= 2 * (len(truncated_headers) - 1)
+    width -= len("  ...  ")
+    for entry in entries:
+        truncated_entry = []
+        for item in entry[:len(truncated_headers)]:
+            if len(item) > width:
+                bound = int(width/2)
+                truncated_entry.append(item[0:bound] + " ... " + item[-bound:])
+            else:
+                truncated_entry.append(item)
+        truncated_entries.append(truncated_entry)
 
-			entry.append(raw_entry.pop("file", ""))
-			entry.append(raw_entry.pop("func", ""))
+    write_both("{}\n{}\n\n".format(component, '-' * len(component)))
+    output.write(tabulate(entries, headers=headers))
+    truncated_output.write(tabulate(truncated_entries, headers=headers))
+    write_both("\n\n\n")
 
-			fields = []
-			for key, value in sorted(raw_entry.items()):
-				fields.append("{}={}".format(key, value))
-			entry.append(",".join(fields))
-			entries.append(entry)
-
-width = os.get_terminal_size().columns
-largest = [0] * (len(truncated_headers) - 1)
-for entry in entries:
-	for i in range(len(largest)):
-		if len(entry[i]) > largest[i]:
-			largest[i] = len(entry[i])
-
-for item in largest:
-	width -= item
-
-truncated_entries = []
-width -= 2 * (len(truncated_headers) - 1)
-width -= len("  ...  ")
-for entry in entries:
-	truncated_entry = []
-	for item in entry[:len(truncated_headers)]:
-		if len(item) > width:
-			bound = int(width/2)
-			truncated_entry.append(item[0:bound] + " ... " + item[-bound:])
-		else:
-			truncated_entry.append(item)
-	truncated_entries.append(truncated_entry)
-
-output.write(tabulate(entries, headers=headers))
-truncated_output.write(tabulate(truncated_entries, headers=headers))
-truncated_output.write(f"\n\nWrote full table to {output_file}\n")
+truncated_output.write(f"Wrote full table to {output_file}\n")
