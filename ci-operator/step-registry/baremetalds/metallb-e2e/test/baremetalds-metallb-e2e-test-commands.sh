174c5ec138333065ff0794daf355ae4f14c86d4b
Sabina Aledort <saledort@redhat.com>
2021-11-25 10:58:50 +0200

    Add an env variable to set metallb E2E tests configuration values
    Adding this for the run_e2e step which requires the cluster type
    to run the relevant test suite.
    
    Also set the metallb repo to pull and test the d/s repo.

--- ci-operator/step-registry/baremetalds/metallb-e2e/test/baremetalds-metallb-e2e-test-commands.sh
+++ ci-operator/step-registry/baremetalds/metallb-e2e/test/baremetalds-metallb-e2e-test-commands.sh
@@ -11,4 +11,16 @@ source "${SHARED_DIR}/packet-conf.sh"
 
 echo "### Copying metallb directory"
 scp "${SSHOPTS[@]}" -r /go/src/github.com/openshift/metallb "root@${IP}:/root/dev-scripts/metallb/"
-ssh "${SSHOPTS[@]}" "root@${IP}" make -C /root/dev-scripts/metallb run_e2e
+
+# Inject additional variables directly.
+run_e2e_command="make -C /root/dev-scripts/metallb run_e2e"
+if [[ -n "${E2E_TESTS_CONFIG:-}" ]]; then
+  readarray -t config <<< "${E2E_TESTS_CONFIG}"
+  for var in "${config[@]}"; do
+    if [[ ! -z "${var}" ]]; then
+      run_e2e_command="${run_e2e_command} ${var}"
+    fi
+  done
+fi
+
+ssh "${SSHOPTS[@]}" "root@${IP}" ${run_e2e_command}
