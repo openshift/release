dfd533ea02d5dd7a501f241b9dade6f6d49aac3d
Pierre Prinetti <pierreprinetti@redhat.com>
2021-11-26 15:11:40 +0100

    debug

--- ci-operator/step-registry/shiftstack/bugwatcher/pretriage/shiftstack-bugwatcher-pretriage-commands.sh
+++ ci-operator/step-registry/shiftstack/bugwatcher/pretriage/shiftstack-bugwatcher-pretriage-commands.sh
@@ -2,12 +2,4 @@
 
 set -Eeuo pipefail
 
-BUGZILLA_API_KEY="$(</var/run/bugzilla/api-key)"
-SLACK_HOOK="$(</var/run/slack-hooks/forum-shiftstack)"
-TEAM_MEMBERS="$(</var/run/team/team.json)"
-
-export BUGZILLA_API_KEY
-export SLACK_HOOK
-export TEAM_MEMBERS
-
-python3 ./main.py
+whoami
