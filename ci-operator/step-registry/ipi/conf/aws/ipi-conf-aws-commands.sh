e2e684fb50aaea3f9606049e6a2be3a530b69865
Yunfei Jiang <yunjiang@redhat.com>
2021-12-08 20:03:13 +0800

    a

--- ci-operator/step-registry/ipi/conf/aws/ipi-conf-aws-commands.sh
+++ ci-operator/step-registry/ipi/conf/aws/ipi-conf-aws-commands.sh
@@ -10,7 +10,7 @@ curl -L https://github.com/mikefarah/yq/releases/download/3.3.0/yq_linux_amd64 -
 export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 
 echo "YUNFEI:"
-echo "ls ${CLUSTER_PROFILE_DIR}/"
+echo "ls -la ${CLUSTER_PROFILE_DIR}/"
 ls ${CLUSTER_PROFILE_DIR}/
 echo "cat ${CLUSTER_PROFILE_DIR}/yunjiang_test"
 cat ${CLUSTER_PROFILE_DIR}/yunjiang_test
