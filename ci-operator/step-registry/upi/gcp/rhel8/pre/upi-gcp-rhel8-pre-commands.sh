e3f9ae7547500622b567460ab071675577de3762
Jon Cope <jcope@redhat.com>
2021-12-09 20:19:18 -0600

    use rhel stream

--- ci-operator/step-registry/upi/gcp/rhel8/pre/upi-gcp-rhel8-pre-commands.sh
+++ ci-operator/step-registry/upi/gcp/rhel8/pre/upi-gcp-rhel8-pre-commands.sh
@@ -39,8 +39,8 @@ gcloud compute firewall-rules create "${INSTANCE_PREFIX}" \
 
 # TODO: make this rhel8, need subscription
 gcloud compute instances create "${INSTANCE_PREFIX}" \
-  --image-family centos-stream-8 \
-  --image-project centos-cloud \
+  --image-family rhel-8 \
+  --image-project rhel-cloud \
   --zone "${GOOGLE_COMPUTE_ZONE}" \
   --machine-type e2-standard-8 \
   --boot-disk-type pd-ssd \
