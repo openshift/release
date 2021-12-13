a574459e52779e5032cb4386400de32063475b03
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2021-12-13 10:55:41 -0500

    Rebalance leases between IBM and VMC

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -137,7 +137,7 @@ for i in range(10, 24):
 for i in range(1, 7):
     CONFIG['ovirt-upgrade-quota-slice']['ovirt-upgrade-{}'.format(i)] = 1
 
-for i in range(81,100):
+for i in range(81,95):
     CONFIG['vsphere-quota-slice']['ci-segment-{}'.format(i)] = 1
 
 for i in range(60,62):
