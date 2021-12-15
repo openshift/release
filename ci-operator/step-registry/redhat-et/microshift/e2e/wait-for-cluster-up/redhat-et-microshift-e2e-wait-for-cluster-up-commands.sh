29886e20485fa17c2b1831151d7e58b0a31962c3
Ryan Cook <rcook@redhat.com>
2021-12-15 16:07:51 -0500

    roll back wait

--- ci-operator/step-registry/redhat-et/microshift/e2e/wait-for-cluster-up/redhat-et-microshift-e2e-wait-for-cluster-up-commands.sh
+++ ci-operator/step-registry/redhat-et/microshift/e2e/wait-for-cluster-up/redhat-et-microshift-e2e-wait-for-cluster-up-commands.sh
@@ -33,21 +33,59 @@ gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
 gcloud --quiet config set compute/zone "${GOOGLE_COMPUTE_ZONE}"
 gcloud --quiet config set compute/region "${GOOGLE_COMPUTE_REGION}"
 
+cat > "${HOME}"/wait_for_node_ready.sh <<'EOF'
+#!/bin/bash
+set -xeuo pipefail
+
+systemctl enable --now microshift.service
+
+microshift-containerized
+source /etc/microshift-containerized/microshift-containerized.conf
+
+start=$(date '+%s')
+to=300
+
+# Wait for node to post ready
+while :; do
+  if [ $(( $(date '+%s') - start )) -ge $to ]; then
+    echo "timed out waiting for node to start ($to seconds)" >&2
+    exit 1
+  fi
+  echo "waiting for node response" >&2
+  # get the condation where type == Ready, where condition.statusx == True.
+  node="$(oc get nodes -o jsonpath='{.items[*].status.conditions}' | jq '.[] | select(.type == "Ready") | select(.status == "True")')" || echo ''
+  if [ "$node" ]; then
+    echo "node posted ready status" >&2
+    break
+  fi
+  sleep 10
+done
+
+EOF
+chmod +x "${HOME}"/wait_for_node_ready.sh
+
 LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute scp \
   --quiet \
   --project "${GOOGLE_PROJECT_ID}" \
   --zone "${GOOGLE_COMPUTE_ZONE}" \
   --recurse /tmp/validate-microshift rhel8user@"${INSTANCE_PREFIX}":~/validate-microshift
 
-LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute --project "${GOOGLE_PROJECT_ID}" ssh \
+LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute scp \
+  --quiet \
+  --project "${GOOGLE_PROJECT_ID}" \
   --zone "${GOOGLE_COMPUTE_ZONE}" \
-  rhel8user@"${INSTANCE_PREFIX}" \
-  --command 'sudo systemctl enable --now microshift.service'
+  --recurse "${HOME}"/wait_for_node_ready.sh rhel8user@"${INSTANCE_PREFIX}":~/wait_for_node_ready.sh
+
+LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute scp \
+  --quiet \
+  --project "${GOOGLE_PROJECT_ID}" \
+  --zone "${GOOGLE_COMPUTE_ZONE}" \
+  --recurse "${HOME}"/wait_for_node_ready.sh rhel8user@"${INSTANCE_PREFIX}":~/wait_for_node_ready.sh
 
 LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute --project "${GOOGLE_PROJECT_ID}" ssh \
   --zone "${GOOGLE_COMPUTE_ZONE}" \
   rhel8user@"${INSTANCE_PREFIX}" \
-  --command 'until [ -f /var/lib/microshift/resources/kubeadmin/kubeconfig ];do sleep 2; done'
+  --command 'sudo systemctl enable --now microshift.service'
 
 LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute --project "${GOOGLE_PROJECT_ID}" ssh \
   --zone "${GOOGLE_COMPUTE_ZONE}" \
