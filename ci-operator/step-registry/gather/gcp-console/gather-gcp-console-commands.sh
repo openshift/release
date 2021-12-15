f3debfac62505af70e7f573de02edfb7f9edd1ab
jiwei <jiwei@redhat.com>
2021-12-15 09:31:38 +0800

    check if 'proxy-conf.sh' exists

--- ci-operator/step-registry/gather/gcp-console/gather-gcp-console-commands.sh
+++ ci-operator/step-registry/gather/gcp-console/gather-gcp-console-commands.sh
@@ -17,6 +17,17 @@ fi
 
 gcloud config set project "$(jq -r .gcp.projectID "${SHARED_DIR}/metadata.json")"
 
+# For disconnected or otherwise unreachable environments, we want to
+# have steps use an HTTP(S) proxy to reach the API server. This proxy
+# configuration file should export HTTP_PROXY, HTTPS_PROXY, and NO_PROXY
+# environment variables, as well as their lowercase equivalents (note
+# that libcurl doesn't recognize the uppercase variables).
+if test -f "${SHARED_DIR}/proxy-conf.sh"
+then
+	# shellcheck disable=SC1090
+	source "${SHARED_DIR}/proxy-conf.sh"
+fi
+
 if test -f "${KUBECONFIG}"
 then
 	oc --request-timeout=5s get nodes -o jsonpath --template '{range .items[*]}{.spec.providerID}{"\n"}{end}' | sed 's|.*/||' > "${TMPDIR}/node-provider-IDs.txt" &
