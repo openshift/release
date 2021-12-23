dd41797d0ecaeaed669766e35ea4c0575ccbc1a5
Osher De Paz <osherdepaz@gmail.com>
2021-12-23 16:52:50 +0200

    Add bump-ocp step to be run nightly

--- ci-operator/step-registry/baremetalds/assisted/tools/report/baremetalds-assisted-tools-report-commands.sh
+++ ci-operator/step-registry/baremetalds/assisted/tools/report/baremetalds-assisted-tools-report-commands.sh
@@ -6,8 +6,14 @@ set -o pipefail
 
 echo "************ baremetalds assisted tools report command ************"
 
-export CI_CREDENTIALS_DIR=/var/run/assisted-installer-bot
+set +e
+IS_REHEARSAL=$(expr "${REPO_OWNER:-}" = "openshift" "&" "${REPO_NAME:-}" = "release")
+set -e
+
+if (( ! ${IS_REHEARSAL} )); then
+    export WEBHOOK
+    WEBHOOK=$(cat ${CI_CREDENTIALS_DIR}/triage-webhook)
+fi
 
 JIRA_ACCESS_TOKEN=$(cat ${CI_CREDENTIALS_DIR}/jira-access-token) \
-    WEBHOOK=$(cat ${CI_CREDENTIALS_DIR}/triage-webhook) \
     ./tools/triage_status_report.py
