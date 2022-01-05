ce8f8a1b88097c728412e3a1c55047a4f2eef0dc
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2022-01-05 16:37:26 -0500

    AWS Pruner: Run hypershift deprovisioning first
    Otherwise the job always fails for Hypersift cluters, because the
    openshift-install destroy doesn't know how to clean up everything and
    thus fails, even though the following hypershift destroy finds
    everything.

--- core-services/ipi-deprovision/aws.sh
+++ core-services/ipi-deprovision/aws.sh
@@ -20,9 +20,9 @@ function deprovision() {
   REGION="$(cat ${WORKDIR}/metadata.json|jq .aws.region -r)"
   INFRA_ID="$(cat ${WORKDIR}/metadata.json|jq '.aws.identifier[0]|keys[0]' -r|cut -d '/' -f3|tr -d '\n')"
   HYPERSHIFT_BASE_DOMAIN="${HYPERSHIFT_BASE_DOMAIN:-origin-ci-int-aws.dev.rhcloud.com}"
-  timeout --signal=SIGQUIT 30m openshift-install --dir "${WORKDIR}" --log-level error destroy cluster && touch "${WORKDIR}/success" || touch "${WORKDIR}/failure"
   timeout --signal=SIGQUIT 30m hypershift destroy infra aws --aws-creds "${AWS_SHARED_CREDENTIALS_FILE}" --infra-id "${INFRA_ID}" --base-domain "${HYPERSHIFT_BASE_DOMAIN}" --region "${REGION}" || touch "${WORKDIR}/failure"
   timeout --signal=SIGQUIT 30m hypershift destroy iam aws --aws-creds "${AWS_SHARED_CREDENTIALS_FILE}" --infra-id "${INFRA_ID}" --region "${REGION}" || touch "${WORKDIR}/failure"
+  timeout --signal=SIGQUIT 30m openshift-install --dir "${WORKDIR}" --log-level error destroy cluster && touch "${WORKDIR}/success" || touch "${WORKDIR}/failure"
 }
 
 logdir="${ARTIFACTS}/deprovision"
