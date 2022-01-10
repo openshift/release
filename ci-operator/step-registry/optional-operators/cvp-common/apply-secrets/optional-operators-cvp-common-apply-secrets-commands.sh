3dbc5433675658f29ee5e6b2a15218999870445d
kpavic <kpavic@redhat.com>
2022-01-10 17:24:45 +0100

    WIP: Test applying kube_objects from mounted files

--- ci-operator/step-registry/optional-operators/cvp-common/apply-secrets/optional-operators-cvp-common-apply-secrets-commands.sh
+++ ci-operator/step-registry/optional-operators/cvp-common/apply-secrets/optional-operators-cvp-common-apply-secrets-commands.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# Steps for extracting and applying the kube_secrets ISV parameter
+# Steps for extracting and applying the kube_objects
 # Expects the standard Prow environment variables to be set
 
 REHEARSAL_INSTALL_NAMESPACE="!create"
-
-PYXIS_URL="${PYXIS_URL:-""}"
 # The namespace into which the operator and catalog will be
 # installed. Special value `!create` means that a new namespace will be created.
 OO_INSTALL_NAMESPACE="${OO_INSTALL_NAMESPACE:-$REHEARSAL_INSTALL_NAMESPACE}"
+KUBE_OBJECTS_LOCATION="/var/run/kube-objects-${OO_PACKAGE}/kube_objects"
 
 # Check if PYXIS_URL exists, skip the whole step if not.
-if [[ -z "$PYXIS_URL" ]]; then
-    echo "PYXIS_URL is not defined, skipping step cvp-common-apply-secrets!"
+if [[ ! -f "${KUBE_OBJECTS_LOCATION}" ]]; then
+    echo "The custom kube_objects are not defined, skipping step cvp-common-apply-secrets!"
     exit 0
 else
-    echo "PYXIS_URL is defined, proceeding with cvp-common-apply-secrets step."
+    echo "The custom kube_objects are defined, proceeding with cvp-common-apply-secrets step."
 fi
 
 echo "Creating a new NAMESPACE"
@@ -43,21 +42,8 @@ fi
 # Creating file that contains namespace name
 echo "$OO_INSTALL_NAMESPACE" > "${SHARED_DIR}"/operator-install-namespace.txt
 
-GPG_KEY='/var/run/cvp-pyxis-gpg-secret/cvp-gpg.key' # Secret file which will be mounted by DPTP
-GPG_PASS='/var/run/cvp-pyxis-gpg-secret/cvp-gpg.pass' # Secret file which will be mounted by DPTP
-PKCS12_CERT='/var/run/cvp-pyxis-gpg-secret/cvp-dptp.cert' # Secret file which will be mounted by DPTP
-PKCS12_KEY='/var/run/cvp-pyxis-gpg-secret/cvp-dptp.key' # Secret file which will be mounted by DPTP
-
-echo "Fetching the kube_objects from Pyxis for ISV pid ${PYXIS_URL}"
-touch /tmp/get_kubeObjects.txt
-curl --key "${PKCS12_KEY}" --cert "${PKCS12_CERT}" "${PYXIS_URL}" | jq -r ".container.kube_objects" > /tmp/get_kubeObjects.txt
-
-echo "Decrypting the kube_objects fetched from Pyxis"
-gpg --batch --yes --quiet --pinentry-mode loopback --import --passphrase-file "${GPG_PASS}" "${GPG_KEY}"
-gpg --batch --yes --quiet --pinentry-mode loopback --decrypt --passphrase-file "${GPG_PASS}" /tmp/get_kubeObjects.txt > /tmp/kube_objects.yaml
-
 echo "Applying the kube_objects on the testing OCP cluster"
-oc apply -f /tmp/kube_objects.yaml -n "$OO_INSTALL_NAMESPACE"
+oc apply -f "${KUBE_OBJECTS_LOCATION}" -n "${OO_INSTALL_NAMESPACE}"
 
-# Remove the kube objects file just in case
-rm -rf /tmp/kube_objects.yaml
+# TODO Remove after confirming POC
+oc get sa -n "${OO_INSTALL_NAMESPACE}"
