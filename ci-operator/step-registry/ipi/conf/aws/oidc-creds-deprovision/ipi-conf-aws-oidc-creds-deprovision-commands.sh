4ff667480eee42b81400fad5b45e67b480f3685a
lwan-wanglin <lwan@redhat.com>
2022-01-10 16:49:26 +0800

    Adding cucushift rehearse - AWS Proxy & sts

--- ci-operator/step-registry/ipi/conf/aws/oidc-creds-deprovision/ipi-conf-aws-oidc-creds-deprovision-commands.sh
+++ ci-operator/step-registry/ipi/conf/aws/oidc-creds-deprovision/ipi-conf-aws-oidc-creds-deprovision-commands.sh
@@ -9,9 +9,11 @@ export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 REGION="${LEASED_RESOURCE}"
 
 # extract ccoctl from the release image
-CCO_IMAGE=$(oc adm release info --image-for='cloud-credential-operator' "$RELEASE_IMAGE_LATEST")
+cp ${CLUSTER_PROFILE_DIR}/pull-secret /tmp/pull-secret
+oc registry login --to /tmp/pull-secret
+CCO_IMAGE=$(oc adm release info --image-for='cloud-credential-operator' "$RELEASE_IMAGE_LATEST" -a /tmp/pull-secret)
 cd "/tmp"
-oc image extract "$CCO_IMAGE" --file="/usr/bin/ccoctl"
+oc image extract "$CCO_IMAGE" --file="/usr/bin/ccoctl" -a /tmp/pull-secret
 chmod 555 "/tmp/ccoctl"
 
 # delete credentials infrastructure created by oidc-creds-provision configure step
--- ci-operator/step-registry/ipi/conf/aws/oidc-creds-provision/ipi-conf-aws-oidc-creds-provision-commands.sh
+++ ci-operator/step-registry/ipi/conf/aws/oidc-creds-provision/ipi-conf-aws-oidc-creds-provision-commands.sh
@@ -11,14 +11,15 @@ export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 REGION="${LEASED_RESOURCE}"
 
 # extract ccoctl from the release image
-oc registry login
-CCO_IMAGE=$(oc adm release info --image-for='cloud-credential-operator' "$RELEASE_IMAGE_LATEST")
+cp ${CLUSTER_PROFILE_DIR}/pull-secret /tmp/pull-secret
+oc registry login --to /tmp/pull-secret
+CCO_IMAGE=$(oc adm release info --image-for='cloud-credential-operator' "$RELEASE_IMAGE_LATEST" -a /tmp/pull-secret)
 cd "/tmp"
-oc image extract "$CCO_IMAGE" --file="/usr/bin/ccoctl"
+oc image extract "$CCO_IMAGE" --file="/usr/bin/ccoctl" -a /tmp/pull-secret
 chmod 555 "/tmp/ccoctl"
 
 # extract aws credentials requests from the release image
-oc adm release extract --credentials-requests --cloud=aws --to="/tmp/credrequests" "$RELEASE_IMAGE_LATEST"
+oc adm release extract --credentials-requests --cloud=aws --to="/tmp/credrequests" "$RELEASE_IMAGE_LATEST" -a /tmp/pull-secret
 
 # create required credentials infrastructure and installer manifests
 "/tmp/ccoctl" aws create-all --name="${infra_name}" --region="${REGION}" --credentials-requests-dir="/tmp/credrequests" --output-dir="/tmp"
