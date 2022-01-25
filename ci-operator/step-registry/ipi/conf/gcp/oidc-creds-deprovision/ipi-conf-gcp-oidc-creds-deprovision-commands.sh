4ca0fa78871fb7b148e6ef7ffd0e950ce25a0ba7
W. Trevor King <wking@tremily.us>
2022-01-24 16:38:09 -0800

    ci-operator/step-registry/ipi/conf: Run ccoctl steps from cloud-credential-operator
    And, when necessary, use 'cli: latest' [1] to inject an 'oc' command
    from the release:latest image [2].
    
    [1]: https://docs.ci.openshift.org/docs/architecture/step-registry/#injecting-the-oc-cli
    [2]: https://docs.ci.openshift.org/docs/architecture/ci-operator/#testing-with-an-existing-openshift-release

--- ci-operator/step-registry/ipi/conf/gcp/oidc-creds-deprovision/ipi-conf-gcp-oidc-creds-deprovision-commands.sh
+++ ci-operator/step-registry/ipi/conf/gcp/oidc-creds-deprovision/ipi-conf-gcp-oidc-creds-deprovision-commands.sh
@@ -9,12 +9,6 @@ export GCP_SHARED_CREDENTIALS_FILE=${CLUSTER_PROFILE_DIR}/gce.json
 export GOOGLE_APPLICATION_CREDENTIALS="${GCP_SHARED_CREDENTIALS_FILE}"
 PROJECT="$(< ${CLUSTER_PROFILE_DIR}/openshift_gcp_project)"
 
-# extract ccoctl from the release image
-CCO_IMAGE=$(oc adm release info --image-for='cloud-credential-operator' "$RELEASE_IMAGE_LATEST")
-cd "/tmp"
-oc image extract "$CCO_IMAGE" --file="/usr/bin/ccoctl"
-chmod 555 "/tmp/ccoctl"
-
 # delete credentials infrastructure created by oidc-creds-provision-provision configure step
 export GOOGLE_APPLICATION_CREDENTIALS="${GCP_SHARED_CREDENTIALS_FILE}"
-"/tmp/ccoctl" gcp delete --name="${infra_name}" --project="${PROJECT}"
+ccoctl gcp delete --name="${infra_name}" --project="${PROJECT}"
