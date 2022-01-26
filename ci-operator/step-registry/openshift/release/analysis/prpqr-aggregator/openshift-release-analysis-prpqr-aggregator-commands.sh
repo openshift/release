b0526831bc38241befd6ebd3f665aa88726bd496
Nikolaos Moraitis <nmoraiti@redhat.com>
2022-01-26 12:00:20 +0100

    add the prpqr-aggregator ci-operator step in registry
    Signed-off-by: Nikolaos Moraitis <nmoraiti@redhat.com>

---
+++ ci-operator/step-registry/openshift/release/analysis/prpqr-aggregator/openshift-release-analysis-prpqr-aggregator-commands.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+job-run-aggregator analyze-job-runs \
+  --google-service-account-credential-file ${GOOGLE_SA_CREDENTIAL_FILE} \
+  --job=${VERIFICATION_JOB_NAME} \
+  --aggregation-id=a${AGGREGATION_ID} \
+  --explicit-gcs-prefix=${EXPLICIT_GCS_PREFIX} \
+  --job-start-time=${JOB_START_TIME} \
+  --working-dir=${WORKING_DIR} \
+  --timeout=4h40m
