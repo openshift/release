1c118c730a685bb5540ade35b7596ecb70f303ef
Jose Valdes <jvaldes@redhat.com>
2022-02-08 16:10:35 -0500

    Inject latest oc cli

--- ci-operator/step-registry/windows/e2e/operator/test/with-custom-vxlan-port/windows-e2e-operator-test-with-custom-vxlan-port-commands.sh
+++ ci-operator/step-registry/windows/e2e/operator/test/with-custom-vxlan-port/windows-e2e-operator-test-with-custom-vxlan-port-commands.sh
@@ -4,6 +4,12 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# check cli
+echo "$PATH"
+command -V oc
+oc version
+/cli/oc version
+
 export AWS_SHARED_CREDENTIALS_FILE=${CLUSTER_PROFILE_DIR}/.awscred
 export KUBE_SSH_KEY_PATH=${CLUSTER_PROFILE_DIR}/ssh-privatekey
 make run-ci-e2e-test
