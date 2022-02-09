c0a307ea267101fd351c74f3bf2565570044034a
Emilien Macchi <emilien@redhat.com>
2022-02-09 12:09:07 -0500

    preflight-common/health: add support for proxy
    When the cluster is reachable behind a proxy (like it's the case for
    some baremetal & openstack jobs), we need this famous snippet to run so
    we can use this step registry.

--- ci-operator/step-registry/operator-pipelines/preflight-common/health/operator-pipelines-preflight-common-health-commands.sh
+++ ci-operator/step-registry/operator-pipelines/preflight-common/health/operator-pipelines-preflight-common-health-commands.sh
@@ -9,6 +9,16 @@ echo "Health endpoint and cluster operators check"
 
 export KUBECONFIG
 
+# For disconnected or otherwise unreachable environments, we want to
+# have steps use an HTTP(S) proxy to reach the API server. This proxy
+# configuration file should export HTTP_PROXY, HTTPS_PROXY, and NO_PROXY
+# environment variables, as well as their lowercase equivalents (note
+# that libcurl doesn't recognize the uppercase variables).
+if test -f "${SHARED_DIR}/proxy-conf.sh"
+then
+	# shellcheck disable=SC1090
+	source "${SHARED_DIR}/proxy-conf.sh"
+fi
 
 echo "Checking readyz endpoint"
 
