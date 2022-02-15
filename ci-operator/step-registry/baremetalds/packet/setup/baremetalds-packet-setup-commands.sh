5b24ca32a54a6aba47c0e19cfe768d6493cf25ab
Derek Higgins <derekh@redhat.com>
2022-02-15 10:19:50 +0000

    Remove vault workaround
    equinix have now inserted their own workaround into the
    Centos Images

--- ci-operator/step-registry/baremetalds/packet/setup/baremetalds-packet-setup-commands.sh
+++ ci-operator/step-registry/baremetalds/packet/setup/baremetalds-packet-setup-commands.sh
@@ -111,24 +111,6 @@ cat > packet-setup.yaml <<-EOF
             ssh "\${SSHOPTS[@]}" "root@\${IP}" hostname && break
             sleep 10
         done
-
-        ssh "\${SSHOPTS[@]}" "root@\${IP}" bash - << EOC
-            if [[ ${PACKET_OS} == "centos_8" ]]; then
-              echo "Making sure we use vault mirror, as default CentOS repositories are already EOL..."
-              
-              sed -i '/mirrorlist=.*/d' /etc/yum.repos.d/CentOS-AppStream.repo
-              grep -qF 'vault.centos.org' /etc/yum.repos.d/CentOS-AppStream.repo || \
-                echo 'baseurl=http://vault.centos.org/\\\$contentdir/\\\$releasever/AppStream/\\\$basearch/os/' >> /etc/yum.repos.d/CentOS-AppStream.repo
-
-              sed -i '/mirrorlist=.*/d' /etc/yum.repos.d/CentOS-Base.repo
-              grep -qF 'vault.centos.org' /etc/yum.repos.d/CentOS-Base.repo || \
-                echo 'baseurl=http://vault.centos.org/\\\$contentdir/\\\$releasever/BaseOS/\\\$basearch/os/' >> /etc/yum.repos.d/CentOS-Base.repo
-
-              sed -i '/mirrorlist=.*/d' /etc/yum.repos.d/CentOS-Extras.repo
-              grep -qF 'vault.centos.org' /etc/yum.repos.d/CentOS-Extras.repo || \
-                echo 'baseurl=http://vault.centos.org/\\\$contentdir/\\\$releasever/extras/\\\$basearch/os/' >> /etc/yum.repos.d/CentOS-Extras.repo
-            fi
-        EOC
       dest: "${SHARED_DIR}/packet-conf.sh"
 EOF
 
