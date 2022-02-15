a09095860be650de17be24a86764c6c1fa30f417
Andrea Fasano <afasano@redhat.com>
2022-02-15 06:20:05 -0500

    Fix centos baseurl for all the repos

--- ci-operator/step-registry/baremetalds/packet/setup/baremetalds-packet-setup-commands.sh
+++ ci-operator/step-registry/baremetalds/packet/setup/baremetalds-packet-setup-commands.sh
@@ -113,20 +113,10 @@ cat > packet-setup.yaml <<-EOF
         done
 
         ssh "\${SSHOPTS[@]}" "root@\${IP}" bash - << EOC
-            if [[ ${PACKET_OS} == "centos_8" ]]; then
-              echo "Making sure we use vault mirror, as default CentOS repositories are already EOL..."
-              
-              sed -i '/mirrorlist=.*/d' /etc/yum.repos.d/CentOS-AppStream.repo
-              grep -qF 'vault.centos.org' /etc/yum.repos.d/CentOS-AppStream.repo || \
-                echo 'baseurl=http://vault.centos.org/\\\$contentdir/\\\$releasever/AppStream/\\\$basearch/os/' >> /etc/yum.repos.d/CentOS-AppStream.repo
-
-              sed -i '/mirrorlist=.*/d' /etc/yum.repos.d/CentOS-Base.repo
-              grep -qF 'vault.centos.org' /etc/yum.repos.d/CentOS-Base.repo || \
-                echo 'baseurl=http://vault.centos.org/\\\$contentdir/\\\$releasever/BaseOS/\\\$basearch/os/' >> /etc/yum.repos.d/CentOS-Base.repo
-
-              sed -i '/mirrorlist=.*/d' /etc/yum.repos.d/CentOS-Extras.repo
-              grep -qF 'vault.centos.org' /etc/yum.repos.d/CentOS-Extras.repo || \
-                echo 'baseurl=http://vault.centos.org/\\\$contentdir/\\\$releasever/extras/\\\$basearch/os/' >> /etc/yum.repos.d/CentOS-Extras.repo
+            if [[ ${PACKET_OS} == "centos_8" && ! -f "/etc/yum.repos.d/centos-vault.repo" ]]; then
+              echo "Making sure we use vault mirror, as default CentOS repositories are already EOL..."              
+              sed -i '/mirrorlist=.*/d' /etc/yum.repos.d/CentOS-*
+              sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
             fi
         EOC
       dest: "${SHARED_DIR}/packet-conf.sh"
