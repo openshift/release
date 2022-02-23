b67afc0cf21466a6fcb719257532abe32b9cc61e
Christoph Stäbler <cstabler@redhat.com>
2022-02-23 18:08:49 +0100

    Add CI e2e jobs for k-nmstate against OCP OVN on IPv6
    Signed-off-by: Christoph Stäbler <cstabler@redhat.com>

--- ci-operator/step-registry/kubernetes-nmstate/e2e/operator/kubernetes-nmstate-e2e-operator-commands.sh
+++ ci-operator/step-registry/kubernetes-nmstate/e2e/operator/kubernetes-nmstate-e2e-operator-commands.sh
@@ -10,6 +10,29 @@ function check_clusteroperators_status() {
     echo "$(date) - all clusteroperators are done progressing."
 }
 
+function mirror_nmstate_images_to_ds_registry() {
+    source "${SHARED_DIR}/packet-conf.sh"
+    source "${SHARED_DIR}/ds-vars.conf"
+    
+    echo "$(date) - mirroring ${HANDLER_IMAGE} and ${OPERATOR_IMAGE} to ${DS_REGISTRY}..."
+
+    DST_HANDLER_IMAGE="${DS_REGISTRY}/nmstate/handler:ci"
+    DST_OPERATOR_IMAGE="${DS_REGISTRY}/nmstate/operator:ci"
+    
+    ssh "${SSHOPTS[@]}" "root@${IP}" "oc image mirror $OPERATOR_IMAGE $DST_OPERATOR_IMAGE --registry-config=$DS_WORKING_DIR/pull_secret.json"
+    ssh "${SSHOPTS[@]}" "root@${IP}" "oc image mirror $HANDLER_IMAGE $DST_HANDLER_IMAGE --registry-config=$DS_WORKING_DIR/pull_secret.json"
+
+    export HANDLER_IMAGE=$DST_HANDLER_IMAGE
+    export OPERATOR_IMAGE=$DST_OPERATOR_IMAGE
+    
+    echo "$(date) - mirrored to ${DST_HANDLER_IMAGE} and ${DST_OPERATOR_IMAGE}"
+}
+
+source "${SHARED_DIR}/dev-scripts-additional-config"
+if [[ -n "${MIRROR_IMAGES}" ]]; then
+    mirror_nmstate_images_to_ds_registry
+fi
+
 oc wait --for=condition=Progressing=False --timeout=2m clusterversion/version
 check_clusteroperators_status
 
