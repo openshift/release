f8f0ab9899abdc95dbacf6bafd0ffc73d7e1bba0
Andrea Fasano <afasano@redhat.com>
2022-02-24 12:01:01 +0100

    Use n2 instances in the selected facilities

--- ci-operator/step-registry/baremetalds/packet/setup/baremetalds-packet-setup-commands.sh
+++ ci-operator/step-registry/baremetalds/packet/setup/baremetalds-packet-setup-commands.sh
@@ -41,7 +41,7 @@ cat > packet-setup.yaml <<-EOF
         hostnames: "{{ packet_hostname }}"
         operating_system: ${PACKET_OS}
         plan: ${PACKET_PLAN}
-        facility: any
+        facility: ${PACKET_FACILITY}
         wait_for_public_IPv: 4
         wait_timeout: 1800
         state: active
