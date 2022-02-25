866271931b6d57b3095f739fe95e526b58362355
Adrien Gentil <agentil@redhat.com>
2022-02-25 15:32:47 +0100

    MGMT-9344: assisted-installer-agent subsystem tests
    Add a new workflow to run agent subsystem tests on an equinix metal
    machine. The workflow is composed of the following steps:
    - Provision an equinix metal machine (baremetalds step)
    - Install required packages to run the tests (skipper and docker) and
      copy agent source code onto the remote machine
    - Run the subsystem tests on the remote machine
    - Collect the logs and the junit report to the artifact directory
    - Teardown the machine (baremetalds step)
    
    The workflow will be run on presubmit and daily for master and
    release-ocm-2.4 branches.

---
+++ ci-operator/step-registry/assisted/agent/setup/assisted-agent-setup-commands.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+set -x
+
+echo "************ assisted agent setup command ************"
+
+# Fetch packet basic configuration
+# shellcheck source=/dev/null
+source "${SHARED_DIR}/packet-conf.sh"
+
+tar -czf - . | ssh "${SSHOPTS[@]}" "root@${IP}" "cat > /root/assisted.tar.gz"
+
+echo "### Setting up tests"
+timeout --kill-after 10m 120m ssh "${SSHOPTS[@]}" "root@${IP}" bash - << EOF
+    # install and start docker
+    dnf install -y 'dnf-command(config-manager)'
+    dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+    dnf install -y docker-ce docker-ce-cli containerd.io
+
+    # attempt at addressing quay.io flakyness
+    mkdir -p /etc/docker
+    echo '{"debug": true, "max-concurrent-downloads": 1, "max-download-attempts": 50}' | tee /etc/docker/daemon.json
+    systemctl enable --now docker
+
+    # install skipper
+    dnf install -y  python3-pip
+    pip3 install strato-skipper
+
+    # misc tools
+    dnf install -y sos
+
+    # setup the directory where the tests will the run
+    REPO_DIR="/home/assisted"
+    mkdir -p "\${REPO_DIR}"
+
+    # NVMe makes it faster
+    NVME_DEVICE="/dev/nvme0n1"
+    if [ -e "\$NVME_DEVICE" ];
+    then
+        mkfs.xfs -f "\${NVME_DEVICE}"
+        mount "\${NVME_DEVICE}" "\${REPO_DIR}"
+    fi
+
+    # copy the agent sources on the remote machine
+    tar -xzvf assisted.tar.gz -C "\${REPO_DIR}"
+    chown -R root:root "\${REPO_DIR}"
+EOF
