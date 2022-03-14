32ddf07e4eceea7e96de11f5eb337d6d7e531449
Emilien Macchi <emilien@redhat.com>
2022-03-14 13:28:52 -0400

    openstack/nfv: change manifests directory
    named "stable" in master.

--- ci-operator/step-registry/openstack/provision/sriov-operator/openstack-provision-sriov-operator-commands.sh
+++ ci-operator/step-registry/openstack/provision/sriov-operator/openstack-provision-sriov-operator-commands.sh
@@ -67,8 +67,7 @@ if [ -n "${is_dev_version:-}" ]; then
     git clone --branch release-${oc_version} https://github.com/openshift/sriov-network-operator /tmp/sriov-network-operator
     pushd /tmp/sriov-network-operator
     # Until https://github.com/openshift/sriov-network-operator/pull/613 merges
-    # Also we need to hardcode 4.10 for now because 4.11 manifests are not here yet.
-    cp manifests/4.10/supported-nic-ids_v1_configmap.yaml deploy/configmap.yaml
+    cp manifests/stable/supported-nic-ids_v1_configmap.yaml deploy/configmap.yaml
 
     # We need to skip the bits where it tries to install Skopeo
     export SKIP_VAR_SET=1
