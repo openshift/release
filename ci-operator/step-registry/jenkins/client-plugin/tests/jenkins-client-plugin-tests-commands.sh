3707f0e1fd2d5f8d6a2e83ba43e107124b3ef258
gabemontero <gmontero@redhat.com>
2022-03-18 15:27:33 -0400

    jenkins plugin e2e imagestreamtag now in plugin repos 'make test-e2e'

--- ci-operator/step-registry/jenkins/client-plugin/tests/jenkins-client-plugin-tests-commands.sh
+++ ci-operator/step-registry/jenkins/client-plugin/tests/jenkins-client-plugin-tests-commands.sh
@@ -16,21 +16,6 @@ if ! [[ -f ${KUBECONFIG} ]]; then
     exit 0
 fi
 
-echo "Jenkins image from CI pipeline:jenkins: ${JENKINS_IMAGE}"
-if [[ -z ${JENKINS_IMAGE} ]]; then
-    echo "No jenkins image env var found, not overriding jenkins imagestream for e2e test of jenkins-client-plugin."
-else
-    echo "Tagging the CI generated Jenkins image ${JENKINS_IMAGE} from pipeline:jenkins into the test cluster's jenkins imagestream in the openshift namespace"
-    echo "Current contents of the jenkins imagestream in the openshift namespace"
-    oc describe is jenkins -n openshift
-    echo "Tagging ${JENKINS_IMAGE} into the jenkins imagestream in the openshift namespace"
-    oc tag --source=docker ${JENKINS_IMAGE} openshift/jenkins:2
-    # give some time for the image import to finish; watching from the CLI is non-trivial
-    sleep 30
-    echo "New contents of the jenkins imagestream in the openshift namespace"
-    oc describe is jenkins -n openshift
-fi
-
 make test-e2e
 
 echo "Step jenkins-client-plugin-tests completed."
