d7a7cbe143c031ccdf2215365242b4d7e65d56bb
Jianwei Hou <jhou@redhat.com>
2022-03-24 14:09:04 +0800

    Fix cucushift-e2e test result summary

--- ci-operator/step-registry/cucushift/e2e/report/cucushift-e2e-report-commands.sh
+++ ci-operator/step-registry/cucushift/e2e/report/cucushift-e2e-report-commands.sh
@@ -5,12 +5,8 @@ set -o errexit
 set -o pipefail
 
 # only exit 0 if junit result has no failures
-echo "Summarizing test result..."
-failures=$(grep '<testsuite failures="[1-9].*"' "${ARTIFACT_DIR}" -r | wc -l || true)
-if [ $((failures)) == 0 ]; then
-    echo "All tests have passed"
-    exit 0
-else
-    echo "There are ${failures} test failures"
+if [[ -f "${SHARED_DIR}/cucushift-e2e-failures" ]]; then
+    cat "${SHARED_DIR}/cucushift-e2e-failures"
+    echo "Please investigate these failures from build artifacts"
     exit 1
 fi
