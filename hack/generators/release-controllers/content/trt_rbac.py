65e7e0f2121373ca985d322ef4fa9907cd78c039
Brad Williams <brad.williams@redhat.com>
2022-03-24 13:39:23 -0400

    Allow TRT admins to check clusterversions

--- hack/generators/release-controllers/content/trt_rbac.py
+++ hack/generators/release-controllers/content/trt_rbac.py
@@ -2,45 +2,6 @@ import genlib
 from config import Context
 
 
-def _add_trt_admin_cluster_role(gendoc):
-    gendoc.append({
-        'apiVersion': 'authorization.openshift.io/v1',
-        'kind': 'ClusterRole',
-        'metadata': {
-            'name': 'trt-admin',
-        },
-        'rules': [
-            {
-                'apiGroups': ['image.openshift.io'],
-                'resources': ['imagestreamimages',
-                              'imagestreammappings',
-                              'imagestreams',
-                              'imagestreams/secrets',
-                              'imagestreamtags',
-                              'imagetags'],
-                'verbs': ['create',
-                          'delete',
-                          'deletecollection',
-                          'get',
-                          'list',
-                          'patch',
-                          'update',
-                          'watch']
-            },
-            {
-                'apiGroups': ['image.openshift.io'],
-                'resources': ['imagestreamimports'],
-                'verbs': ['create']
-            },
-            {
-                'apiGroups': ['image.openshift.io'],
-                'resources': ['imagestreams/layers'],
-                'verbs': ['get',
-                          'update']
-            }]
-    })
-
-
 def _add_trt_admin_cluster_role_bindings(gendoc, namespace):
     gendoc.append({
         'apiVersion': 'rbac.authorization.k8s.io/v1',
@@ -63,9 +24,7 @@ def _add_trt_admin_cluster_role_bindings(gendoc, namespace):
 
 
 def generate_trt_rbac(config):
-    with genlib.GenDoc(config.paths.path_trt_resources.joinpath('admin_trt.yaml')) as gendoc:
-        _add_trt_admin_cluster_role(gendoc)
-
+    with genlib.GenDoc(config.paths.path_trt_resources.joinpath('admin_generated_rbac.yaml')) as gendoc:
         for private in (False, True):
             for arch in config.arches:
                 context = Context(config, arch, private)
