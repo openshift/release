4c253649563f2fb6a9280f108bc19da869ab0e28
ehila <ehila@redhat.com>
2022-03-24 16:25:19 -0400

    test: verify upgraded tuned restart behavior
    Signed-off-by: ehila <ehila@redhat.com>

---
+++ ci-operator/step-registry/ipi/conf/custommcpool/ipi-conf-custommcpool-commands.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+if test ! -f "${KUBECONFIG}"
+then
+	echo "No kubeconfig, can not continue."
+	exit 0
+fi
+
+node_name=$(oc get nodes -l=node-role.kubernetes.io/worker="" -o=jsonpath={.items[0].metadata.name})
+oc label node "$node_name" "node-role.kubernetes.io/tuned"=""
+
+# Create infra machineconfigpool
+oc create -f - <<EOF
+apiVersion: machineconfiguration.openshift.io/v1
+kind: MachineConfigPool
+metadata:
+  name: mccustompool
+  labels:
+    "pools.operator.machineconfiguration.openshift.io/tuned": ""
+spec:
+  machineConfigSelector:
+    matchLabels:
+      machineconfiguration.openshift.io/role: tuned
+  nodeSelector:
+    matchLabels:
+      node-role.kubernetes.io/tuned: ""
+EOF
+
