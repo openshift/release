cc075165e0ce6a77a35ccb3d4f09a118c2388ac3
jiwei <jiwei@redhat.com>
2022-04-02 12:14:12 +0800

    deal with 'customer_vpc_subnets.yaml'

--- ci-operator/step-registry/ipi/conf/gcp/ipi-conf-gcp-commands.sh
+++ ci-operator/step-registry/ipi/conf/gcp/ipi-conf-gcp-commands.sh
@@ -4,6 +4,9 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# TODO: move to image
+curl -L https://github.com/mikefarah/yq/releases/download/3.3.0/yq_linux_amd64 -o /tmp/yq && chmod +x /tmp/yq
+
 CONFIG="${SHARED_DIR}/install-config.yaml"
 
 GCP_BASE_DOMAIN="$(< ${CLUSTER_PROFILE_DIR}/public_hosted_zone)"
@@ -58,3 +61,7 @@ spec:
 EOF
 fi
 
+if [[ -s "${SHARED_DIR}/customer_vpc_subnets.yaml" ]]; then
+  /tmp/yq m -x -i "${CONFIG}" "${SHARED_DIR}/customer_vpc_subnets.yaml"
+  cat "${CONFIG}"
+fi
