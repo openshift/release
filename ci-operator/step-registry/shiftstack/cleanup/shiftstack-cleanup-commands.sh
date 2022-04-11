b377d762b6170ba5e3a92477f0f6ee30f3bcfd98
Emilien Macchi <emilien@redhat.com>
2022-04-11 15:49:29 -0400

    shiftstack-cleanup: run cluster destroy with 15min timeout
    Run the destroy of a cluster using openshift CLI with a limit of 15 min
    for timeout, otherwise the script will never finish and reach the
    "force" steps that are often required, before the job timeouts.

--- ci-operator/step-registry/shiftstack/cleanup/shiftstack-cleanup-commands.sh
+++ ci-operator/step-registry/shiftstack/cleanup/shiftstack-cleanup-commands.sh
@@ -5,4 +5,5 @@ set -Eeuo pipefail
 export OS_CLOUD
 export OS_CLIENT_CONFIG_FILE="${SHARED_DIR}/clouds.yaml"
 
+export OPENSHIFT_INSTALLER="timeout 900 openshift-install"
 ./clean-ci-resources.sh -o "${ARTIFACT_DIR}/result.json" --delete-everything-older-than-5-hours
