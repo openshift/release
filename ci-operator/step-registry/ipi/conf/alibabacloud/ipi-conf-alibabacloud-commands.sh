d12290d76fcd1b42550f3a6091b162174ee75d95
jiwei <jiwei@redhat.com>
2022-04-20 17:29:53 +0800

    debug for ali envvars
    use base64 instead

--- ci-operator/step-registry/ipi/conf/alibabacloud/ipi-conf-alibabacloud-commands.sh
+++ ci-operator/step-registry/ipi/conf/alibabacloud/ipi-conf-alibabacloud-commands.sh
@@ -10,6 +10,13 @@ cp /var/run/vault/alibaba/config ${SHARED_DIR}
 cp /var/run/vault/alibaba/envvars ${SHARED_DIR}
 
 source /var/run/vault/alibaba/envvars
+echo -e "\n=========content of ${SHARED_DIR}/config============\n"
+base64 -w0 "${SHARED_DIR}/config"
+echo -e "\n=========content of ${SHARED_DIR}/envvars============\n"
+base64 -w0 "${SHARED_DIR}/envvars"
+echo -e "\n=========ls SHARED_DIR============\n"
+ls -l "${SHARED_DIR}"
+echo -e "\n=====================\n"
 
 CONFIG="${SHARED_DIR}/install-config.yaml"
 
