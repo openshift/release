1fb675f780d7f7fc598347da6a40f37cc3ceb43d
Amoghrd <aramesha@redhat.com>
2022-04-28 15:47:16 -0400

    AzureMAG fully private

--- ci-operator/step-registry/ipi/conf/azuremag/ipi-conf-azuremag-commands.sh
+++ ci-operator/step-registry/ipi/conf/azuremag/ipi-conf-azuremag-commands.sh
@@ -4,6 +4,9 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# TODO: move to image
+curl -L https://github.com/mikefarah/yq/releases/download/3.3.0/yq_linux_amd64 -o /tmp/yq && chmod +x /tmp/yq
+
 CONFIG="${SHARED_DIR}/install-config.yaml"
 
 REGION="${LEASED_RESOURCE}"
@@ -21,6 +24,8 @@ elif [[ "${SIZE_VARIANT}" == "large" ]]; then
 elif [[ "${SIZE_VARIANT}" == "compact" ]]; then
   master_type=Standard_E8_v3
 fi
+
+cp "/var/run/cluster-secrets-azuremag-qe/osServicePrincipal.json" "${SHARED_DIR}/osServicePrincipal.json"
 
 cat >> "${CONFIG}" << EOF
 baseDomain: ${BASE_DOMAIN}
@@ -41,3 +46,20 @@ compute:
     azure:
       type: ${COMPUTE_NODE_TYPE}
 EOF
+
+if [ -z "${OUTBOUND_TYPE}" ]; then
+  echo "Outbound Type is not defined"
+else
+  if [ X"${OUTBOUND_TYPE}" == X"UserDefinedRouting" ]; then
+    echo "Writing 'outboundType: UserDefinedRouting' to install-config"
+    PATCH="${SHARED_DIR}/install-config-outboundType.yaml.patch"
+    cat > "${PATCH}" << EOF
+platform:
+  azure:
+    outboundType: ${OUTBOUND_TYPE}
+EOF
+    /tmp/yq m -x -i "${CONFIG}" "${PATCH}"
+  else
+    echo "${OUTBOUND_TYPE} is not supported yet" || exit 1
+  fi
+fi
