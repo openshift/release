970a816bdbe4ab64abd5e4cc12db2b7cd29e10ee
Skyler Clark <skylerjaneclark@gmail.com>
2022-05-17 10:39:17 -0400

    fix

--- ci-operator/step-registry/crio-test/agent/setup/crio-test-agent-setup-commands.sh
+++ ci-operator/step-registry/crio-test/agent/setup/crio-test-agent-setup-commands.sh
@@ -31,5 +31,5 @@ timeout --kill-after 10m 120m ssh "${SSHOPTS[@]}" "root@${IP}" bash - << EOF
     chown -R root:root "\${REPO_DIR}"
     cd "\${REPO_DIR}/contrib/test/integration"
     echo "localhost" >> hosts
-    ansible-playbook -i hosts "host=localhost" --connection=local
+    ansible-playbook main.yml -i hosts "host=localhost" --connection=local
 EOF
