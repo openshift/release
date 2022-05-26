7491df5fe6243612dbdb947ac3ba5055fb9725f2
nicklesimba <simha.nikhil@gmail.com>
2022-05-26 14:47:13 -0500

    using proper directory structure to allow must-gather execution
    Signed-off-by: nicklesimba <simha.nikhil@gmail.com>

--- ci-operator/step-registry/gather/extra/gather-extra-commands.sh
+++ ci-operator/step-registry/gather/extra/gather-extra-commands.sh
@@ -135,7 +135,11 @@ for path in "${paths[@]}" ; do
   done < ${output_dir}.audit_logs_listing
 done
 
+# change to the network artifact dir
+mkdir -p ${ARTIFACT_DIR}/network/multus_logs/
+pushd ${ARTIFACT_DIR}/network/multus_logs/ || return
 oc get node -oname | xargs oc adm must-gather -- /usr/bin/gather_multus_logs
+popd || return
 
 echo "INFO: gathering quay tcpdump packet headers if present"
 output_dir="${ARTIFACT_DIR}/tcpdump/"
