fc370a35ee67638fdc49b939f3d441e38f3b7a0d
house-d <105243888+house-d@users.noreply.github.com>
2022-06-02 09:54:41 -0600

    more debug

--- ci-operator/step-registry/stackrox/initial/stackrox-initial-commands.sh
+++ ci-operator/step-registry/stackrox/initial/stackrox-initial-commands.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo 'error: test executing initial commands.sh'
+set -vex
+echo 'info: test executing initial commands.sh'
 exec .openshift-ci/dispatch.sh "${TEST_SUITE:-${JOB_NAME_SAFE#merge-}}"
