b3cdc1d230afc88bd5a10fec09855994e64a9333
Stephen Benjamin <stephen@redhat.com>
2022-06-13 07:46:19 -0400

    Add step to enable crio debug logging and turn it on for azure
    Needed to troubleshoot https://bugzilla.redhat.com/show_bug.cgi?id=2082271

---
+++ ci-operator/step-registry/openshift/manifests/crio-debug/openshift-manifests-crio-debug-commands.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo "Configuring cri-o with debug logging...."
+cat >> "${SHARED_DIR}/manifest_crio_debug_logging.yaml" << EOF
+apiVersion: machineconfiguration.openshift.io/v1
+kind: ContainerRuntimeConfig
+metadata:
+ name: custom-loglevel
+spec:
+ containerRuntimeConfig:
+   logLevel: debug
+EOF
+
+echo "manifest_crio_debug_logging.yaml"
+echo "---------------------------------------------"
+cat "${SHARED_DIR}/manifest_crio_debug_logging.yaml"
