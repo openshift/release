bc1a8846ccd7a9db9ca73b21c0a665903e57b536
Pavel Sturc <psturc@redhat.com>
2022-06-16 21:00:38 +0200

    fix: step-registry checks

--- ci-operator/step-registry/redhat-appstudio/e2e/redhat-appstudio-e2e-commands.sh
+++ ci-operator/step-registry/redhat-appstudio/e2e/redhat-appstudio-e2e-commands.sh
@@ -7,16 +7,18 @@ set -o pipefail
 export PATH=$PATH:/tmp/bin
 mkdir -p /tmp/bin
 
-export GITHUB_USER=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/github-user)
-export GITHUB_TOKEN=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/github-token)
-export QUAY_TOKEN=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/quay-token)
+export GITHUB_USER GITHUB_TOKEN QUAY_TOKEN
+
+GITHUB_USER=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/github-user)
+GITHUB_TOKEN=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/github-token)
+QUAY_TOKEN=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/quay-token)
 
 git config --global user.name "redhat-appstudio-qe-bot"
 git config --global user.email redhat-appstudio-qe-bot@redhat.com
 
-mkdir -p ${HOME}/creds
+mkdir -p "${HOME}/creds"
 GIT_CREDS_PATH="${HOME}/creds/file"
 git config --global credential.helper "store --file ${GIT_CREDS_PATH}"
-echo "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com" > ${GIT_CREDS_PATH}
+echo "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com" > "${GIT_CREDS_PATH}"
 
 /bin/bash .ci/oci-e2e-deployment.sh
