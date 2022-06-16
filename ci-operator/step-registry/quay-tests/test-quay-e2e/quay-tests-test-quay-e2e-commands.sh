fda4429a5eac754723662d454abc11e5141af2f9
lzha <lzha@redhat.com>
2022-06-16 15:38:35 +0800

    add quay e2e testing

---
+++ ci-operator/step-registry/quay-tests/test-quay-e2e/quay-tests-test-quay-e2e-commands.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+#Navigate to Quay E2E testing Directory
+cd quay-frontend-tests
+#Check Skopeo Version
+skopeo -v
+
+#Set Kubeconfig:
+cp -L $KUBECONFIG /tmp/kubeconfig && export KUBECONFIG_PATH=/tmp/kubeconfig
+
+#Create Artifact Directory:
+ARTIFACT_DIR=${ARTIFACT_DIR:=/tmp/artifacts}
+mkdir -p $ARTIFACT_DIR
+
+
+function copyArtifacts {
+    cp -r ./cypress/results/* $ARTIFACT_DIR && cp -r ./cypress/screenshots/* $ARTIFACT_DIR
+}
+
+# Install Dependcies defined in packages.json
+yarn install
+
+#Finally Copy the Junit Testing XML files and Screenshots to /tmp/artifacts
+trap copyArtifacts EXIT
+
+#Trigget Quay E2E Testing
+set +x
+quay_route=$(oc get quayregistry quay -n quay-enterprise -o jsonpath='{.status.registryEndpoint}')
+echo "quay route name is $quay_route"
+export CYPRESS_QUAY_ENDPOINT=$quay_route
+yarn run smoke
