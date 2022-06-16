ce8295f06635d3d3b1d260f9782f9962c2cd0642
lzha <lzha@redhat.com>
2022-06-16 15:55:28 +0800

    update test commands and run make jobs

--- ci-operator/step-registry/quay-tests/test-quay-e2e/quay-tests-test-quay-e2e-commands.sh
+++ ci-operator/step-registry/quay-tests/test-quay-e2e/quay-tests-test-quay-e2e-commands.sh
@@ -16,16 +16,8 @@ cp -L $KUBECONFIG /tmp/kubeconfig && export KUBECONFIG_PATH=/tmp/kubeconfig
 ARTIFACT_DIR=${ARTIFACT_DIR:=/tmp/artifacts}
 mkdir -p $ARTIFACT_DIR
 
-
-function copyArtifacts {
-    cp -r ./cypress/results/* $ARTIFACT_DIR && cp -r ./cypress/screenshots/* $ARTIFACT_DIR
-}
-
 # Install Dependcies defined in packages.json
 yarn install
-
-#Finally Copy the Junit Testing XML files and Screenshots to /tmp/artifacts
-trap copyArtifacts EXIT
 
 #Trigget Quay E2E Testing
 set +x
@@ -33,3 +25,6 @@ quay_route=$(oc get quayregistry quay -n quay-enterprise -o jsonpath='{.status.r
 echo "quay route name is $quay_route"
 export CYPRESS_QUAY_ENDPOINT=$quay_route
 yarn run smoke
+
+#Archive testing reulsts Junit XML file and Screenshots
+cp -r ./cypress/results/* $ARTIFACT_DIR && cp -r ./cypress/screenshots/* $ARTIFACT_DIR
