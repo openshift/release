18b5939a5443b6575554a1c13916adc6bf1c94d1
Li Zhang <lzha@redhat.com>
2022-06-17 21:20:29 +0800

    update test script to archive e2e testing video doe

--- ci-operator/step-registry/quay-tests/test-quay-e2e/quay-tests-test-quay-e2e-commands.sh
+++ ci-operator/step-registry/quay-tests/test-quay-e2e/quay-tests-test-quay-e2e-commands.sh
@@ -13,7 +13,7 @@ mkdir -p $ARTIFACT_DIR
 
 
 function copyArtifacts {
-    cp -r ./cypress/results/* $ARTIFACT_DIR && cp -r ./cypress/screenshots/* $ARTIFACT_DIR
+    cp -r ./cypress/results/* $ARTIFACT_DIR && cp -r ./cypress/videos/smoke/* $ARTIFACT_DIR
 }
 
 # Install Dependcies defined in packages.json
