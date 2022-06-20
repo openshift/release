5f29ab1bdc046991dbe441408ef6c7e7be4fcf82
Yang Yang <yanyang@redhat.com>
2022-06-20 15:17:58 +0800

    Support multi-hop image mirror

--- ci-operator/step-registry/cucushift/upgrade/mirror-images/by-oc-adm/cucushift-upgrade-mirror-images-by-oc-adm-commands.sh
+++ ci-operator/step-registry/cucushift/upgrade/mirror-images/by-oc-adm/cucushift-upgrade-mirror-images-by-oc-adm-commands.sh
@@ -7,44 +7,46 @@ set -o pipefail
 # Check if a build is signed
 function check_signed() {
     local digest algorithm hash_value response
-    digest="$(echo "${target}" | cut -f2 -d@)"
+    digest="$(echo "${TARGET}" | cut -f2 -d@)"
     algorithm="$(echo "${digest}" | cut -f1 -d:)"
     hash_value="$(echo "${digest}" | cut -f2 -d:)"
     response=$(curl --silent --output /dev/null --write-out %"{http_code}" "https://mirror2.openshift.com/pub/openshift-v4/signatures/openshift/release/${algorithm}=${hash_value}/signature-1")
     if (( response == 200 )); then
-        echo "${target} is signed" && return 0
+        echo "${TARGET} is signed" && return 0
     else
-        echo "Seem like ${target} is not signed" && return 1
+        echo "Seem like ${TARGET} is not signed" && return 1
     fi
 }
 
 function mirror_image(){
-    mirror_release_image="${MIRROR_REGISTRY_HOST}/${target#*/}"
-    mirror_release_image_repo="${mirror_release_image%:*}"
-    mirror_release_image_repo="${mirror_release_image_repo%@sha256*}"
+    local mirror_release_image target_version
+    mirror_release_image="${MIRROR_REGISTRY_HOST}/${TARGET#*/}"
+    MIRROR_RELEASE_IMAGE_REPO="${mirror_release_image%:*}"
+    MIRROR_RELEASE_IMAGE_REPO="${MIRROR_RELEASE_IMAGE_REPO%@sha256*}"
+    export MIRROR_RELEASE_IMAGE_REPO
 
-    target_version=$(oc adm release info "${target}" --output=json | jq .metadata.version)
+    target_version=$(oc adm release info "${TARGET}" --output=json | jq .metadata.version)
    
-    echo "Mirroring ${target_version} (${target}) to ${mirror_release_image_repo}"
+    echo "Mirroring ${target_version} (${TARGET}) to ${MIRROR_RELEASE_IMAGE_REPO}"
 
-    oc adm release mirror -a "${new_pull_secret}" --insecure=true \
-        --from="${target}" \
-        --to="${mirror_release_image_repo}" \
-        --apply-release-image-signature="${apply_sig}" | tee "${mirror_out_file}"
+    oc adm release mirror -a "${PULL_SECRET}" --insecure=true \
+        --from="${TARGET}" \
+        --to="${MIRROR_RELEASE_IMAGE_REPO}" \
+        --apply-release-image-signature="${APPLY_SIG}" | tee "${MIRROR_OUT_FILE}"
 }
 
 function update_icsp(){
+    local source_release_image_repo
     source_release_image_repo="${RELEASE_IMAGE_LATEST%:*}"
     source_release_image_repo="${source_release_image_repo%@sha256*}"
-    if [[ "${source_release_image_repo}" != "${mirror_release_image_repo}" ]] && ! oc get ImageContentSourcePolicy example -oyaml; then
+    if [[ "${source_release_image_repo}" != "${MIRROR_RELEASE_IMAGE_REPO}" ]] && ! oc get ImageContentSourcePolicy example -oyaml; then
         echo "Target image has different repo with source image and icsp example is not present, creating icsp"
-        if [[ ! -f "${mirror_out_file}" ]]; then
-            echo >&2 "${mirror_out_file} not found" && return 1
+        if [[ ! -f "${MIRROR_OUT_FILE}" ]]; then
+            echo >&2 "${MIRROR_OUT_FILE} not found" && return 1
         fi
-        sed -n '/To use the new mirrored repository for upgrades, use the following to create an ImageContentSourcePolicy:/,/configmap\/sha256.*/{//!p;}' "${mirror_out_file}"  | grep -v '^$' > "${icsp_file}"
-        echo "cat ${icsp_file}:"
-        cat ${icsp_file}
-        oc create -f "${icsp_file}"
+        sed -n '/To use the new mirrored repository for upgrades, use the following to create an ImageContentSourcePolicy:/,/configmap\/sha256.*/{//!p;}' "${MIRROR_OUT_FILE}"  | grep -v '^$' > "${ICSP_FILE}"
+        echo "cat ${ICSP_FILE}:\n$(cat "${ICSP_FILE}")"       
+        oc create -f "${ICSP_FILE}"
     fi
 }
 
@@ -59,6 +61,17 @@ then
     source "${SHARED_DIR}/proxy-conf.sh"
 fi
 
+# Get the target upgrades release, by default, RELEASE_IMAGE_TARGET is the target release
+# If it's serial upgrades then override-upgrade file will store the release and overrides RELEASE_IMAGE_TARGET
+# upgrade-edge file expects a comma separated releases list like target_release1,target_release2,...
+export TARGET_RELEASES=("${RELEASE_IMAGE_TARGET}")
+if [[ -f "${SHARED_DIR}/upgrade-edge" ]]; then
+    release_string="$(< "${SHARED_DIR}/upgrade-edge")"
+    # shellcheck disable=SC2207
+    TARGET_RELEASES=($(echo "$release_string" | tr ',' ' ')) 
+fi
+echo "Upgrade targets are ${TARGET_RELEASES[*]}"
+
 # private mirror registry host
 # <public_dns>:<port>
 if [[ ! -f "${SHARED_DIR}/mirror_registry_url" ]]; then
@@ -67,24 +80,30 @@ fi
 
 MIRROR_REGISTRY_HOST=$(head -n 1 "${SHARED_DIR}/mirror_registry_url")
 echo "MIRROR_REGISTRY_HOST: ${MIRROR_REGISTRY_HOST}"
+export MIRROR_REGISTRY_HOST
 
 # combine custom registry credential and default pull secret
 if [[ ! -f "/var/run/vault/mirror-registry/registry_creds" ]]; then
     echo >&2 "/var/run/vault/mirror-registry/registry_creds does not exist." && exit 1
 fi
-new_pull_secret="${SHARED_DIR}/new_pull_secret"
+export PULL_SECRET="${SHARED_DIR}/new_pull_secret"
 registry_cred=$(head -n 1 "/var/run/vault/mirror-registry/registry_creds" | base64 -w 0)
-jq --argjson a "{\"${MIRROR_REGISTRY_HOST}\": {\"auth\": \"$registry_cred\"}}" '.auths |= . + $a' "${CLUSTER_PROFILE_DIR}/pull-secret" > "${new_pull_secret}"
+jq --argjson a "{\"${MIRROR_REGISTRY_HOST}\": {\"auth\": \"$registry_cred\"}}" '.auths |= . + $a' "${CLUSTER_PROFILE_DIR}/pull-secret" > "${PULL_SECRET}"
 
-trap 'rm -f "${new_pull_secret}"' ERR EXIT TERM
+trap 'rm -f "${PULL_SECRET}"' ERR EXIT TERM
 
-mirror_out_file="${SHARED_DIR}/mirror"
-icsp_file="${SHARED_DIR}/icsp.yaml"
-target="${RELEASE_IMAGE_TARGET}"
-apply_sig="true"
-if ! check_signed; then
-    echo "You're mirroring an unsigned images, don't apply signature"
-    apply_sig="false"
-fi
-mirror_image 
-update_icsp
+export MIRROR_OUT_FILE="${SHARED_DIR}/mirror"
+export ICSP_FILE="${SHARED_DIR}/icsp.yaml"
+
+for target in "${TARGET_RELEASES[@]}"
+do
+    export TARGET="${target}"
+    export APPLY_SIG="true"
+    if ! check_signed; then
+        echo "You're mirroring an unsigned images, don't apply signature"
+        APPLY_SIG="false"
+    fi
+    mirror_image 
+    update_icsp
+    rm -f "${MIRROR_OUT_FILE}" "${ICSP_FILE}"
+done
