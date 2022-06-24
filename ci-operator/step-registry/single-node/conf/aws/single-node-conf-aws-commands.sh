1b29089937cd146975bfa0c370b169d7b7d65a5f
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2022-06-24 11:50:38 +0200

    iops:5000->15000, size:150->1000

--- ci-operator/step-registry/single-node/conf/aws/single-node-conf-aws-commands.sh
+++ ci-operator/step-registry/single-node/conf/aws/single-node-conf-aws-commands.sh
@@ -38,7 +38,7 @@ cfg["networking"]["networkType"] = "'${NETWORK_TYPE}'"
 platform = cfg["controlPlane"]["platform"]
 if "aws" in platform:
     platform["aws"]["type"] = "'${SINGLE_NODE_AWS_INSTANCE_TYPE}'"
-    platform["aws"]["rootVolume"] = {"iops": 5000, "size": 150, "type": "gp3" }
+    platform["aws"]["rootVolume"] = {"iops": 15000, "size": 1000, "type": "gp3" }
 else:
     raise ValueError("This step only applies to AWS, please use the correct step for your platform")
 
