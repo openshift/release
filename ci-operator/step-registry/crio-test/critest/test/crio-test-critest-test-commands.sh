78fa6781f51cdd02f4b231f342c2f2827fad4218
Skyler Clark <skylerjaneclark@gmail.com>
2022-06-28 10:31:19 -0400

    adds integration and critest

---
+++ ci-operator/step-registry/crio-test/critest/test/crio-test-critest-test-commands.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+set -o pipefail
+set -x
+
+echo "beginning e2e test"
+# shellcheck source=/dev/null
+source "${SHARED_DIR}/packet-conf.sh"
+
+tar -czf - . | ssh "${SSHOPTS[@]}" "root@${IP}" "cat > /root/crio-test.tar.gz"
+timeout --kill-after 10m 120m ssh "${SSHOPTS[@]}" "root@${IP}" bash - << EOF 
+    ansible-playbook e2e-main.yml -i hosts -e "host=localhost" -e "GOPATH=/usr/local/go" --connection=local -vvv 
+EOF
