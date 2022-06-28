b0284c5667b73be0ee731789178e2f247cb392f1
Sohan Kunkerkar <sohank2602@gmail.com>
2022-06-28 19:16:01 -0400

    change the setup steps

--- ci-operator/step-registry/crio-test/e2e/test/crio-test-e2e-test-commands.sh
+++ ci-operator/step-registry/crio-test/e2e/test/crio-test-e2e-test-commands.sh
@@ -5,10 +5,10 @@ set -o pipefail
 set -x
 
 echo "beginning e2e test"
-# shellcheck source=/dev/null
-source "${SHARED_DIR}/packet-conf.sh"
+# # shellcheck source=/dev/null
+# source "${SHARED_DIR}/packet-conf.sh"
 
-tar -czf - . | ssh "${SSHOPTS[@]}" "root@${IP}" "cat > /root/crio-test.tar.gz"
-timeout --kill-after 10m 120m ssh "${SSHOPTS[@]}" "root@${IP}" bash - << EOF 
-    ansible-playbook e2e-main.yml -i hosts -e "host=localhost" -e "GOPATH=/usr/local/go" --connection=local -vvv 
-EOF
+# tar -czf - . | ssh "${SSHOPTS[@]}" "root@${IP}" "cat > /root/crio-test.tar.gz"
+# timeout --kill-after 10m 120m ssh "${SSHOPTS[@]}" "root@${IP}" bash - << EOF 
+#     ansible-playbook e2e-main.yml -i hosts -e "host=localhost" -e "GOPATH=/usr/local/go" --connection=local -vvv 
+# EOF
