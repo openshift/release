2d36b6af0d9ed37a7106fbe64e7dec9a9989d7ee
Pierre Prinetti <pierreprinetti@redhat.com>
2022-06-30 11:33:08 +0200

    shiftstack: Run pretriage against Jira

--- ci-operator/step-registry/shiftstack/bugwatcher/pretriage/shiftstack-bugwatcher-pretriage-commands.sh
+++ ci-operator/step-registry/shiftstack/bugwatcher/pretriage/shiftstack-bugwatcher-pretriage-commands.sh
@@ -2,12 +2,18 @@
 
 set -Eeuo pipefail
 
-BUGZILLA_API_KEY="$(</var/run/bugzilla/api-key)"
+JIRA_TOKEN="$(<var/run/bugwatcher/jira-token)"
+BUGZILLA_API_KEY="$(</var/run/bugwatcher/bugzilla-api-key)"
 SLACK_HOOK="$(</var/run/slack-hooks/forum-shiftstack)"
 TEAM_MEMBERS="$(</var/run/team/team.json)"
 
 export BUGZILLA_API_KEY
 export SLACK_HOOK
 export TEAM_MEMBERS
+export JIRA_TOKEN
 
-./pretriage.py
+case "$TICKETING_SYSTEM" in
+	bugzilla) echo 'Running against Bugzilla.'; exec ./pretriage.py ;;
+	jira)     echo 'Running against Jira.';     exec /bin/pretriage ;;
+	*) echo "Unknown value for TICKETING_SYSTEM: '$TICKETING_SYSTEM'"; exit 1 ;;
+esac
