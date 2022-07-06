f16a3be3d17406151aef4965a2f2fdf6604d8abf
Frank A. Zdarsky <fzdarsky@redhat.com>
2022-07-06 16:55:44 +0000

    Add pull secret, add oc CLI client

--- ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
+++ ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
@@ -7,5 +7,12 @@ set -o pipefail
 echo "Environment:"
 printenv
 
+echo "Credentials:"
+ls -al /secrets/ci-pull-credentials || true
+ls -al /etc/pull-secret || true
+
+mkdir ~/.docker
+cp /etc/ci-pull-credentials/.dockerconfigjson ~/.docker/config.json || true
+
 echo "./scripts/rebase.sh to ${TARGET_RELEASE_IMAGE}"
 ./scripts/rebase.sh to "${TARGET_RELEASE_IMAGE}"
