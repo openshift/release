d0b8c3973837c87b8e1167c1b16f121a07c0960e
Frank A. Zdarsky <fzdarsky@redhat.com>
2022-07-06 17:22:08 +0000

    Test another secret location

--- ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
+++ ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
@@ -8,11 +8,13 @@ echo "Environment:"
 printenv
 
 echo "Credentials:"
+export PULL_SECRET_PATH=${CLUSTER_PROFILE_DIR}/pull-secret
 ls -al /secrets/ci-pull-credentials || true
 ls -al /etc/pull-secret || true
+ls -al "${PULL_SECRET_PATH}"/etc/pull-secret || true
 
 mkdir ~/.docker
-cp /etc/ci-pull-credentials/.dockerconfigjson ~/.docker/config.json || true
+cp "${PULL_SECRET_PATH}"/.dockerconfigjson ~/.docker/config.json || true
 
 echo "./scripts/rebase.sh to ${TARGET_RELEASE_IMAGE}"
 ./scripts/rebase.sh to "${TARGET_RELEASE_IMAGE}"
