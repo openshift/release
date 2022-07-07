bd43e359c36db72e44d0ab1b588c7abd6b206577
Li Zhang <lzha@redhat.com>
2022-07-07 17:27:15 +0800

    add quay test on ocp 4.10

--- ci-operator/step-registry/quay-tests/test-quay-e2e/quay-tests-test-quay-e2e-commands.sh
+++ ci-operator/step-registry/quay-tests/test-quay-e2e/quay-tests-test-quay-e2e-commands.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 #Set Kubeconfig:
 cd quay-frontend-tests
 skopeo -v
+terraform version
 (cp -L $KUBECONFIG /tmp/kubeconfig || true) && export KUBECONFIG_PATH=/tmp/kubeconfig
 
 #Create Artifact Directory:
