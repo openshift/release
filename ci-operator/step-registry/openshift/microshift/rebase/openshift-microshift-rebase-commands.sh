5bcd4029886924e76303f342e9309d72a79ebfa0
Frank A. Zdarsky <fzdarsky@redhat.com>
2022-07-08 00:50:24 +0000

    pause

--- ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
+++ ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
@@ -14,9 +14,11 @@ echo "  go: $(go version)"
 echo "  jq: $(jq --version)"
 echo "  oc: $(oc version)"
 
+sleep 600
+
 # fetch pull-secret for the central CI registry
 mkdir -p ~/.docker
-oc --namespace microshift registry login --service-account image-puller --registry-config="${HOME}/.docker/config.json"
+oc registry login --registry-config="${HOME}/.docker/config.json"
 
 # call the rebase script
 echo "./scripts/rebase.sh to ${TARGET_RELEASE_IMAGE}"
