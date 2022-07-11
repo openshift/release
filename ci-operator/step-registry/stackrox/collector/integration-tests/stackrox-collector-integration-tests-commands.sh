3a3d74adc81d19e0413ddda87d30e4b6ac0e823e
Giles Hutton <ghutton@redhat.com>
2022-07-11 09:31:41 +0100

    ROX-11222: runs the scripts, in dry-run mode

--- ci-operator/step-registry/stackrox/collector/integration-tests/stackrox-collector-integration-tests-commands.sh
+++ ci-operator/step-registry/stackrox/collector/integration-tests/stackrox-collector-integration-tests-commands.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# function cleanup() {
-#     .openshift-ci/jobs/teardown-vm.sh
-# }
-# 
-# 
-# .openshift-ci/jobs/create-vm.sh
-# .openshift-ci/jobs/run-integration-tests.sh
+function cleanup() {
+    .openshift-ci/jobs/teardown-vm.sh
+}
 
-ls -alh /tmp/secret/
-ls -alh /tmp/secret/stackrox-collector-int-tests
+trap cleanup EXIT
+
+.openshift-ci/jobs/create-vm.sh
+.openshift-ci/jobs/run-integration-tests.sh
 
