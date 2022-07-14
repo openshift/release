7de0513c98288543cac18159b9005d0db331e5bd
Pierre Prinetti <pierreprinetti@redhat.com>
2022-07-14 10:51:22 +0200

    openstack-gather: Quote variables to avoid globbing

--- ci-operator/step-registry/openstack/gather/openstack-gather-commands.sh
+++ ci-operator/step-registry/openstack/gather/openstack-gather-commands.sh
@@ -13,14 +13,14 @@ openstack server list | grep "$CLUSTER_NAME" > "${ARTIFACT_DIR}/openstack_nodes.
 
 for server in $(openstack server list -c Name -f value | grep "$CLUSTER_NAME" | sort); do
 	echo -e "\n$ openstack server show $server" >> "${ARTIFACT_DIR}/openstack_nodes.log"
-	openstack server show $server               >> "${ARTIFACT_DIR}/openstack_nodes.log"
+	openstack server show "$server"               >> "${ARTIFACT_DIR}/openstack_nodes.log"
 
-	openstack console log show $server          &> "${ARTIFACT_DIR}/nodes/console_${server}.log"
+	openstack console log show "$server"          &> "${ARTIFACT_DIR}/nodes/console_${server}.log"
 done
 
 openstack port list | grep "$CLUSTER_NAME" > "${ARTIFACT_DIR}/openstack_ports.log"
 
 for port in $(openstack port list -c Name -f value | grep "$CLUSTER_NAME" | sort); do
 	echo -e "\n$ openstack port show $port" >> "${ARTIFACT_DIR}/openstack_ports.log"
-	openstack port show $port               >> "${ARTIFACT_DIR}/openstack_ports.log"
+	openstack port show "$port"               >> "${ARTIFACT_DIR}/openstack_ports.log"
 done
