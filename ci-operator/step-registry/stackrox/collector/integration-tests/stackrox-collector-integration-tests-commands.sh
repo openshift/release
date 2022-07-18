a2237b97a6f07fd232ad2bf461da034482a0fe85
Giles Hutton <ghutton@redhat.com>
2022-07-18 10:07:13 +0100

    ROX-11222: temporarily set up env for integration tests

--- ci-operator/step-registry/stackrox/collector/integration-tests/stackrox-collector-integration-tests-commands.sh
+++ ci-operator/step-registry/stackrox/collector/integration-tests/stackrox-collector-integration-tests-commands.sh
@@ -3,6 +3,14 @@
 set -eo pipefail
 
 export JOB_ID="${PROW_JOB_ID:0:8}"
+export GCLOUD_INSTANCE="collector-osci-${COLLECTION_METHOD}-${VM_TYPE}-${IMAGE_FAMILY}-${JOB_ID}"
+export GCP_SSH_KEY_FILE="/tmp/GCP_SSH_KEY"
+export GCLOUD_OPTIONS="--ssh-key-file=${GCP_SSH_KEY_FILE}"
+export REMOTE_HOST_TYPE=gcloud
+export VM_CONFIG="${VM_TYPE}.${IMAGE_FAMILY}"
+export COLLECTOR_REPO="quay.io/rhacs-eng/collector"
+
+export COLLECTOR_IMAGE="${COLLECTOR_REPO}:3.9.0"
 
 env
 
