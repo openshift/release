5ef7eed3b8cdd9a4491be1a369f6804688636e78
Ryan Phillips <rphillips@redhat.com>
2022-07-19 11:18:14 -0500

    migrate to gcp

--- ci-operator/step-registry/cri-o/critest/test/cri-o-critest-test-commands.sh
+++ ci-operator/step-registry/cri-o/critest/test/cri-o-critest-test-commands.sh
@@ -2,22 +2,43 @@
 set -o nounset
 set -o errexit
 set -o pipefail
-set -x
 
 # shellcheck source=/dev/null
-source "${SHARED_DIR}/packet-conf.sh"
-echo "${IP}"     
-echo "${SSHOPTS[@]}"
+source "${SHARED_DIR}/env"
 
-tar -czf - . | ssh "${SSHOPTS[@]}" "root@${IP}" "cat > /root/cri-o.tar.gz"
-timeout --kill-after 10m 120m ssh "${SSHOPTS[@]}" "root@${IP}" bash - << EOF 
-    export HOME=/root
-    mkdir /tmp/artifacts 
-    mkdir /logs
-    mkdir /logs/artifacts 
-    mkdir /tmp/artifacts/logs
+#####################################
+###############Log In################
+#####################################
 
-    dnf install python39 -y
+GOOGLE_PROJECT_ID="$(< ${CLUSTER_PROFILE_DIR}/openshift_gcp_project)"
+export GCP_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/gce.json"
+sa_email=$(jq -r .client_email ${GCP_SHARED_CREDENTIALS_FILE})
+if ! gcloud auth list | grep -E "\*\s+${sa_email}"
+then
+  gcloud auth activate-service-account --key-file="${GCP_SHARED_CREDENTIALS_FILE}"
+  gcloud config set project "${GOOGLE_PROJECT_ID}"
+fi
+
+mkdir -p "${HOME}"/.ssh
+chmod 0700 "${HOME}"/.ssh
+
+cp "${CLUSTER_PROFILE_DIR}"/ssh-privatekey "${HOME}"/.ssh/google_compute_engine
+chmod 0600 "${HOME}"/.ssh/google_compute_engine
+cp "${CLUSTER_PROFILE_DIR}"/ssh-publickey "${HOME}"/.ssh/google_compute_engine.pub
+
+#####################################
+#####################################
+
+instance_name=$(<"${SHARED_DIR}/gcp-instance-ids.txt")
+
+tar -czf - . | gcloud compute ssh --zone "${ZONE}" ${instance_name} -- "cat > ~/crio-test.tar.gz"
+timeout --kill-after 10m 120m gcloud compute ssh --zone "${ZONE}" ${instance_name} -- bash - << EOF 
+    export GOROOT=/usr/local/go
+    echo GOROOT="/usr/local/go" | sudo tee -a /etc/environment
+    cat /etc/environment 
+    mkdir -p /logs/artifacts
+    mkdir -p /tmp/artifacts/logs
+    sudo dnf install python39 -y
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     python3.9 get-pip.py
     python3.9 -m pip install ansible
@@ -25,15 +46,7 @@ timeout --kill-after 10m 120m ssh "${SSHOPTS[@]}" "root@${IP}" bash - << EOF
     # setup the directory where the tests will the run
     REPO_DIR="/root/cri-o"
     mkdir -p "\${REPO_DIR}"
-    # NVMe makes it faster
-    NVME_DEVICE="/dev/nvme0n1"
-    if [ -e "\$NVME_DEVICE" ];
-    then
-        mkfs.xfs -f "\${NVME_DEVICE}"
-        mount "\${NVME_DEVICE}" "\${REPO_DIR}"
-    fi
     # copy the agent sources on the remote machine
-    tar -xzvf cri-o.tar.gz -C "\${REPO_DIR}"
     chown -R root:root "\${REPO_DIR}"
     cd "\${REPO_DIR}/contrib/test/ci"
     echo "localhost" >> hosts
