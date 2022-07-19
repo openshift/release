523495f8deb5449eb5e6437eb2c651d3e80f2fd0
Giles Hutton <ghutton@redhat.com>
2022-07-19 20:34:57 +0100

    ROX-11222: sudo didnt work

--- ci-operator/step-registry/stackrox/collector/integration-tests/stackrox-collector-integration-tests-commands.sh
+++ ci-operator/step-registry/stackrox/collector/integration-tests/stackrox-collector-integration-tests-commands.sh
@@ -7,4 +7,4 @@ source .openshift-ci/jobs/integration-tests/env.sh
 env
 
 .openshift-ci/jobs/integration-tests/gcloud-init.sh
-sudo -u circleci .openshift-ci/jobs/integration-tests/run-integration-tests.sh
+.openshift-ci/jobs/integration-tests/run-integration-tests.sh
