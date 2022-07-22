5013f731f85e74c704a684e15ee0298cc5bcfe50
Jakub Guzik <jguzik@redhat.com>
2022-07-22 09:19:26 +0200

    Revert "Bumping Prow and Boskos" (#30723)
    This reverts commit 9a62aee70718795135bbe5aa5e1419ca6a205e97.

--- hack/images.sh
+++ hack/images.sh
@@ -1,2 +1,2 @@
-MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20220721-109f7d84fb
-MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20220721-109f7d84fb
+MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20220720-a455f84b28
+MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20220720-a455f84b28
