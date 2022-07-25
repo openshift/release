c714f8d4497bb286bd5f46832e58ac83cd68b904
Jon Cope <jcope@redhat.com>
2022-07-25 15:20:50 -0500

    pause for debugging

--- ci-operator/step-registry/openshift/microshift/e2e/pre-rpm-install/openshift-microshift-e2e-pre-rpm-install-commands.sh
+++ ci-operator/step-registry/openshift/microshift/e2e/pre-rpm-install/openshift-microshift-e2e-pre-rpm-install-commands.sh
@@ -35,7 +35,9 @@ gcloud --quiet config set compute/region "${GOOGLE_COMPUTE_REGION}"
 LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute --project "${GOOGLE_PROJECT_ID}" ssh \
   --zone "${GOOGLE_COMPUTE_ZONE}" \
   rhel8user@"${INSTANCE_PREFIX}" \
-  --command 'sudo rpm --rebuilddb || { sudo ps aux | grep rpm && exit 1; }'
+  --command 'sudo ps aux | grep rpm'
+
+sleep 10000000000
 
 LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute --project "${GOOGLE_PROJECT_ID}" ssh \
   --zone "${GOOGLE_COMPUTE_ZONE}" \
