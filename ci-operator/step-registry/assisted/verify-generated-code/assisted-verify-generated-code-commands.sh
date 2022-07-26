6c9b8c5636f76a075d34c586ba068b3225cfe9ee
Osher De Paz <osherdepaz@gmail.com>
2022-07-26 15:43:08 +0300

    MGMT-11347: unify assisted-verify-generated-code steps

--- ci-operator/step-registry/assisted/verify-generated-code/assisted-verify-generated-code-commands.sh
+++ ci-operator/step-registry/assisted/verify-generated-code/assisted-verify-generated-code-commands.sh
@@ -9,6 +9,10 @@ echo "************ assisted verify-generated-code command ************"
 
 export GOCACHE=/tmp/
 export GOPROXY=https://proxy.golang.org
+
+# ignore file permissions
+git config core.filemode false
+
 git add . && git commit -m "initial commit after prow's substitutions & rebases"
 make generate
 git diff --exit-code  # this will fail if 'make generate' caused any diff
