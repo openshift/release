3129768d725980daf605bf9b70629e9cb89a1a06
Frank A. Zdarsky <fzdarsky@redhat.com>
2022-07-28 16:54:55 +0000

    add yq to PATH

--- ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
+++ ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
@@ -15,17 +15,18 @@ cp /secrets/ci-pull-secret/.dockercfg "$HOME/.pull-secret.json" || {
 }
 
 # Install yq (code borrowed from the ipi/install step-registry)
+mkdir -p "$HOME/bin"
 YQ_URL=https://github.com/mikefarah/yq/releases/download/v4.26.1/yq_linux_amd64
 YQ_HASH=9e35b817e7cdc358c1fcd8498f3872db169c3303b61645cc1faf972990f37582
 echo "${YQ_HASH} -" > /tmp/sum.txt
-if ! curl -Ls "${YQ_URL}" | tee /tmp/yq | sha256sum -c /tmp/sum.txt >/dev/null 2>/dev/null; then
-  echo "ERROR: Expected file at ${YQ_URL} to have checksum ${YQ_HASH} but instead got $(sha256sum </tmp/yq | cut -d' ' -f1)"
-  strings /tmp/yq
+if ! curl -Ls "${YQ_URL}" | tee "$HOME/bin/yq" | sha256sum -c /tmp/sum.txt >/dev/null 2>/dev/null; then
+  echo "ERROR: Expected file at ${YQ_URL} to have checksum ${YQ_HASH} but instead got $(sha256sum <"$HOME/bin/yq" | cut -d' ' -f1)"
+  strings "$HOME/bin/yq"
   exit 1
 fi
 echo "Downloaded yq; sha256 checksum matches expected ${YQ_HASH}."
-chmod +x /tmp/yq
-alias yq="/tmp/yq"
+chmod +x "$HOME/bin/yq"
+export PATH="$HOME/bin":$PATH
 
 # Determine pull specs for release images
 release_amd64="$(oc get configmap/release-release-images-latest -o yaml \
