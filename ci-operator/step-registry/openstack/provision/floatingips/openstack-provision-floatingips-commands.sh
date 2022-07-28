d3ba860c63529806cd02742bf82f5eb8f1d51c18
Pierre Prinetti <pierreprinetti@redhat.com>
2022-07-28 18:23:31 +0200

    openstack: provision-floatingips beautification

--- ci-operator/step-registry/openstack/provision/floatingips/openstack-provision-floatingips-commands.sh
+++ ci-operator/step-registry/openstack/provision/floatingips/openstack-provision-floatingips-commands.sh
@@ -5,28 +5,33 @@ set -o errexit
 set -o pipefail
 
 if [[ "${CONFIG_TYPE}" == "proxy" ]]; then
-    echo "Skipping step due to CONFIG_TYPE being \"${CONFIG_TYPE}\"."
+    echo "Skipping step due to CONFIG_TYPE being '${CONFIG_TYPE}'."
     exit 0
 fi
 
-export OS_CLIENT_CONFIG_FILE=${SHARED_DIR}/clouds.yaml
-CLUSTER_NAME=$(<"${SHARED_DIR}"/CLUSTER_NAME)
+export OS_CLIENT_CONFIG_FILE="${SHARED_DIR}/clouds.yaml"
+CLUSTER_NAME="$(<"${SHARED_DIR}/CLUSTER_NAME")"
 OPENSTACK_EXTERNAL_NETWORK="${OPENSTACK_EXTERNAL_NETWORK:-$(<"${SHARED_DIR}/OPENSTACK_EXTERNAL_NETWORK")}"
 
-LB_FIP_AND_ID=$(openstack floating ip create --description $CLUSTER_NAME.api-fip $OPENSTACK_EXTERNAL_NETWORK --format value -c 'floating_ip_address' -c 'id')
-echo ${LB_FIP_AND_ID} |awk '{print $1}' > ${SHARED_DIR}/API_IP
-echo ${LB_FIP_AND_ID} |awk '{print $2}' > ${SHARED_DIR}/LB_FIP_UID
-cp ${SHARED_DIR}/API_IP ${ARTIFACT_DIR}
-cp ${SHARED_DIR}/LB_FIP_UID ${ARTIFACT_DIR}
-#Mark the fip for deletion
-echo ${LB_FIP_AND_ID} |awk '{print $2}' >> ${SHARED_DIR}/DELETE_FIPS
-cp ${SHARED_DIR}/DELETE_FIPS ${ARTIFACT_DIR}
+collect_artifacts() {
+	for f in API_IP INGRESS_IP DELETE_FIPS; do
+		if [[ -f "${SHARED_DIR}/${f}" ]]; then
+			cp "${SHARED_DIR}/${f}" "${ARTIFACT_DIR}/"
+		fi
+	done
+}
+trap collect_artifacts EXIT TERM
 
-INGRESS_FIP_AND_ID=$(openstack floating ip create --description ${CLUSTER_NAME}.ingress-fip ${OPENSTACK_EXTERNAL_NETWORK} --format value -c 'floating_ip_address' -c 'id')
-echo ${INGRESS_FIP_AND_ID} |awk '{print $1}' > ${SHARED_DIR}/INGRESS_IP
-echo ${INGRESS_FIP_AND_ID} |awk '{print $2}' > ${SHARED_DIR}/INGRESS_FIP_UID
-cp ${SHARED_DIR}/INGRESS_IP ${ARTIFACT_DIR}
-cp ${SHARED_DIR}/INGRESS_FIP_UID ${ARTIFACT_DIR}
-#Mark the fip for deletion
-echo ${INGRESS_FIP_AND_ID} |awk '{print $2}' >> ${SHARED_DIR}/DELETE_FIPS
-cp ${SHARED_DIR}/DELETE_FIPS ${ARTIFACT_DIR}
+API_FIP=$(openstack floating ip create \
+		--description "$CLUSTER_NAME.api-fip" \
+		"$OPENSTACK_EXTERNAL_NETWORK" \
+		--format json -c floating_ip_address -c id)
+jq -r '.floating_ip_address' <<<"$API_FIP" >  "${SHARED_DIR}/API_IP"
+jq -r '.id'                  <<<"$API_FIP" >> "${SHARED_DIR}/DELETE_FIPS"
+
+INGRESS_FIP="$(openstack floating ip create \
+		--description "${CLUSTER_NAME}.ingress-fip" \
+		"$OPENSTACK_EXTERNAL_NETWORK" \
+		--format json -c floating_ip_address -c id)"
+jq -r '.floating_ip_address' <<<"$INGRESS_FIP" >  "${SHARED_DIR}/INGRESS_IP"
+jq -r '.id'                  <<<"$INGRESS_FIP" >> "${SHARED_DIR}/DELETE_FIPS"
