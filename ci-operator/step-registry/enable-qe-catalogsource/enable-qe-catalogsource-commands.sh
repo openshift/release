1a6d835dd31eb746692964742674828376357617
Jian Zhang <jiazha@redhat.com>
2022-07-29 23:21:01 +0800

    add marketplace checking step (#30917)

--- ci-operator/step-registry/enable-qe-catalogsource/enable-qe-catalogsource-commands.sh
+++ ci-operator/step-registry/enable-qe-catalogsource/enable-qe-catalogsource-commands.sh
@@ -125,10 +125,42 @@ EOF
     set -e 
 }
 
+# From 4.11 on, the marketplace is optional.
+# That means, once the marketplace disabled, its "openshift-marketplace" project will NOT be created as default.
+# But, for OLM, its global namespace still is "openshift-marketplace"(details: https://bugzilla.redhat.com/show_bug.cgi?id=2076878),
+# so we need to create it manually so that optional operator teams' test cases can be run smoothly.
+function check_marketplace () {
+    # caps=`oc get clusterversion version -o=jsonpath="{.status.capabilities.enabledCapabilities}"`
+    # if [[ ${caps} =~ "marketplace" ]]; then
+    #     echo "marketplace installed, skip..."
+    #     return 0
+    # fi
+    
+    run_command "oc get ns openshift-marketplace"; ret=$?
+    if [[ $ret -eq 0 ]]; then
+        echo "openshift-marketplace project AlreadyExists, skip creating."
+        return 0
+    fi
+
+    cat <<EOF | oc create -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  labels:
+    security.openshift.io/scc.podSecurityLabelSync: "false"
+    pod-security.kubernetes.io/enforce: baseline
+    pod-security.kubernetes.io/audit: baseline
+    pod-security.kubernetes.io/warn: baseline
+  name: openshift-marketplace
+EOF
+
+}
+
 set_proxy
 run_command "oc whoami"
 run_command "oc version -o yaml"
 update_global_auth
 sleep 5
 create_icsp_connected
+check_marketplace
 create_catalog_sources
