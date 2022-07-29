c71c358a7b61848bddabdfd01fb097af297daa14
Pierre Prinetti <pierreprinetti@redhat.com>
2022-07-29 13:34:10 +0200

    openstack-gather: Collect bootstrap logs

--- ci-operator/step-registry/openstack/gather/openstack-gather-commands.sh
+++ ci-operator/step-registry/openstack/gather/openstack-gather-commands.sh
@@ -1,9 +1,42 @@
 #!/usr/bin/env bash
 
 set -o nounset
+set -x
 
 export OS_CLIENT_CONFIG_FILE="${SHARED_DIR}/clouds.yaml"
 CLUSTER_NAME=$(<"${SHARED_DIR}/CLUSTER_NAME")
+
+collect_bootstrap_logs() {
+	if [ "$CLUSTER_NAME" != "" ]; then
+		declare -a GATHER_BOOTSTRAP_ARGS
+		declare -a FIPS
+		
+		BOOTSTRAP_NODE=$(openstack server list --format value -c Name | awk "/${CLUSTER_NAME}-.{5}-bootstrap/ {print}")
+		if [ "$BOOTSTRAP_NODE" != "" ]; then
+			echo "Collecting bootstrap logs..."
+			CLUSTER_ID=${BOOTSTRAP_NODE%-bootstrap}
+			openstack security group rule create ${CLUSTER_ID}-master --protocol tcp --dst-port 22:22 --remote-ip 0.0.0.0/0
+			FIP=$(openstack floating ip create "$OPENSTACK_EXTERNAL_NETWORK" --description "${CLUSTER_ID}-bootstrap" --format value --column floating_ip_address)
+			FIPS+=("${FIP}")
+			openstack server add floating ip ${CLUSTER_ID}-bootstrap ${FIP}
+			GATHER_BOOTSTRAP_ARGS+=('--bootstrap' "${FIP}")
+			
+			for idx in 0 1 2; do
+				if openstack server show ${CLUSTER_ID}-master-${idx} &> /dev/null; then
+					FIP=$(openstack floating ip create "$OPENSTACK_EXTERNAL_NETWORK" --description "${CLUSTER_ID}-master-${idx}" --format value --column floating_ip_address)
+					FIPS+=("${FIP}")
+					openstack server add floating ip ${CLUSTER_ID}-master-${idx} ${FIP}
+					GATHER_BOOTSTRAP_ARGS+=('--master' "${FIP}")
+				fi
+			done
+			
+			SSH_PRIV_KEY_PATH="${CLUSTER_PROFILE_DIR}/ssh-privatekey"
+			openshift-install gather bootstrap --key "${SSH_PRIV_KEY_PATH}" "${GATHER_BOOTSTRAP_ARGS[@]}"
+			cp log-bundle-*.tar.gz "${ARTIFACT_DIR}"
+			openstack floating ip delete "${FIPS[@]}"
+		fi
+	fi
+}
 
 mkdir -p "${ARTIFACT_DIR}/nodes"
 
@@ -27,3 +60,5 @@ for subnet in $(openstack subnet list -c Name -f value | grep "$CLUSTER_NAME" | 
 	echo -e "\n$ openstack subnet show $subnet" >> "${ARTIFACT_DIR}/openstack_subnets.log"
 	openstack subnet show "$subnet"             >> "${ARTIFACT_DIR}/openstack_subnets.log"
 done
+
+collect_bootstrap_logs
