545e935abb0d225840a5b07a5f9a1fd3470c1c4e
Pierre Prinetti <pierreprinetti@redhat.com>
2022-07-29 15:20:09 +0200

    openstack-gather: Continue on missing resources

--- ci-operator/step-registry/openstack/gather/openstack-gather-commands.sh
+++ ci-operator/step-registry/openstack/gather/openstack-gather-commands.sh
@@ -40,23 +40,22 @@ collect_bootstrap_logs() {
 
 mkdir -p "${ARTIFACT_DIR}/nodes"
 
-openstack server list | grep "$CLUSTER_NAME" > "${ARTIFACT_DIR}/openstack_nodes.log"
-
-for server in $(openstack server list -c Name -f value | grep "$CLUSTER_NAME" | sort); do
-	echo -e "\n$ openstack server show $server" >> "${ARTIFACT_DIR}/openstack_nodes.log"
+openstack server list --name "$CLUSTER_NAME" > "${ARTIFACT_DIR}/openstack_nodes.log"
+for server in $(openstack server list --name "$CLUSTER_NAME" -c Name -f value | sort); do
+	echo -e "\n$ openstack server show $server"   >> "${ARTIFACT_DIR}/openstack_nodes.log"
 	openstack server show "$server"               >> "${ARTIFACT_DIR}/openstack_nodes.log"
 
 	openstack console log show "$server"          &> "${ARTIFACT_DIR}/nodes/console_${server}.log"
 done
 
-openstack port list | grep "$CLUSTER_NAME" > "${ARTIFACT_DIR}/openstack_ports.log"
-
-for port in $(openstack port list -c Name -f value | grep "$CLUSTER_NAME" | sort); do
+openstack port list | grep "$CLUSTER_NAME" > "${ARTIFACT_DIR}/openstack_ports.log" || true
+for port in $(openstack port list -c Name -f value | { grep "$CLUSTER_NAME" || true; } | sort); do
 	echo -e "\n$ openstack port show $port" >> "${ARTIFACT_DIR}/openstack_ports.log"
 	openstack port show "$port"               >> "${ARTIFACT_DIR}/openstack_ports.log"
 done
 
-for subnet in $(openstack subnet list -c Name -f value | grep "$CLUSTER_NAME" | sort); do
+openstack subnet list | grep "$CLUSTER_NAME" > "${ARTIFACT_DIR}/openstack_subnets.log" || true
+for subnet in $(openstack subnet list -c Name -f value | { grep "$CLUSTER_NAME" || true; } | sort); do
 	echo -e "\n$ openstack subnet show $subnet" >> "${ARTIFACT_DIR}/openstack_subnets.log"
 	openstack subnet show "$subnet"             >> "${ARTIFACT_DIR}/openstack_subnets.log"
 done
