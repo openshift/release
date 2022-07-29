openshift/release

commit 81a4f11f0c88770a336241fdc77fb6aab5b86a09

`assisted-baremetal-operator-gather` shouldn't use escape characters (#30909)
This shell script is already using a "literal" heredoc (`"EOF"` as opposed to `EOF`) to
prevent the outer shell from trying to interpret the nested shell script, so the `\` symbols
are unnecessary.

We've noticed these errors coming from the gather step recently:

```
bash: line 12: syntax error near unexpected token `('
```

Hopefully this will fix those errors

ci-operator/step-registry/assisted/baremetal/operator/gather/assisted-baremetal-operator-gather-commands.sh
@@ -27,7 +27,7 @@ trap getlogs EXIT
 
 echo "### Gathering logs..."
 # shellcheck disable=SC2087
-timeout -s 9 30m ssh "${SSHOPTS[@]}" "root@${IP}" bash - << "EOF"
+timeout -s 9 30m ssh "${SSHOPTS[@]}" DISCONNECTED="${DISCONNECTED:-}" "root@${IP}" bash - << "EOF"
 set -xeo pipefail
 
 # Get sosreport including sar data
@@ -39,27 +39,26 @@ cp -v -r /var/log/swtpm/libvirt/qemu /tmp/artifacts/libvirt-qemu || true
 ls -ltr /var/lib/swtpm-localca/ >> /tmp/artifacts/libvirt-qemu/ls-swtpm-localca.txt || true
 
 # Get information about the machine that was leased against equinix metal (e.g.: location)
-EQUINIX_METADATA_TMP=\$(mktemp)
-curl --output "\${EQUINIX_METADATA_TMP}" "https://metadata.platformequinix.com/metadata" || true
+EQUINIX_METADATA_TMP=$(mktemp)
+curl --output "${EQUINIX_METADATA_TMP}" "https://metadata.platformequinix.com/metadata" || true
 # Filter out "ssh_keys" section to prevent emails to be leaked
-jq 'del(.ssh_keys)' "\${EQUINIX_METADATA_TMP}" > "/tmp/artifacts/equinix-metadata.json" || true
-rm "\${EQUINIX_METADATA_TMP}"
+jq 'del(.ssh_keys)' "${EQUINIX_METADATA_TMP}" > "/tmp/artifacts/equinix-metadata.json" || true
+rm "${EQUINIX_METADATA_TMP}"
 
 cp -R ./reports /tmp/artifacts || true
 
 REPO_DIR="/home/assisted-service"
-if [ ! -d "\${REPO_DIR}" ]; then
-  mkdir -p "\${REPO_DIR}"
+if [ ! -d "${REPO_DIR}" ]; then
+  mkdir -p "${REPO_DIR}"
 
   echo "### Untar assisted-service code..."
-  tar -xzvf /root/assisted-service.tar.gz -C "\${REPO_DIR}"
+  tar -xzvf /root/assisted-service.tar.gz -C "${REPO_DIR}"
 fi
 
-cd "\${REPO_DIR}"
+cd "${REPO_DIR}"
 
 # Get assisted logs
 export LOGS_DEST=/tmp/artifacts
-export DISCONNECTED="${DISCONNECTED:-}"
 deploy/operator/gather.sh
 
 EOF

commit 2a98291ee86219210a4d78b88f741c1dc5ebac07

Fix broken `assisted-baremetal-operator-gather`
Broken by 81a4f11f0c88770a336241fdc77fb6aab5b86a09. SSH environment
variables should come after the destination host.

ci-operator/step-registry/assisted/baremetal/operator/gather/assisted-baremetal-operator-gather-commands.sh
@@ -27,7 +27,7 @@ trap getlogs EXIT
 
 echo "### Gathering logs..."
 # shellcheck disable=SC2087
-timeout -s 9 30m ssh "${SSHOPTS[@]}" DISCONNECTED="${DISCONNECTED:-}" "root@${IP}" bash - << "EOF"
+timeout -s 9 30m ssh "${SSHOPTS[@]}" "root@${IP}" DISCONNECTED="${DISCONNECTED:-}" bash - << "EOF"
 set -xeo pipefail
 
 # Get sosreport including sar data