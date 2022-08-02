03f53f7410d03c657fcf2e234668937ea84be428
Jianli Wei <jiwei@redhat.com>
2022-08-02 11:26:36 +0800

    GCP UPI private cluster & XPN ('gcp-qe' cluster profile) (#30947)

--- ci-operator/step-registry/gcp/deprovision/bastionhost/gcp-deprovision-bastionhost-commands.sh
+++ ci-operator/step-registry/gcp/deprovision/bastionhost/gcp-deprovision-bastionhost-commands.sh
@@ -8,8 +8,12 @@ if [ ! -f "${SHARED_DIR}/bastion-destroy.sh" ]; then
   echo "No 'bastion-destroy.sh' found, aborted." && exit 0
 fi
 
+if [[ -s "${SHARED_DIR}/xpn.json" ]] && [[ -f "${CLUSTER_PROFILE_DIR}/xpn_creds.json" ]]; then
+  echo "Activating XPN service-account..."
+  GOOGLE_CLOUD_XPN_KEYFILE_JSON="${CLUSTER_PROFILE_DIR}/xpn_creds.json"
+  gcloud auth activate-service-account --key-file="${GOOGLE_CLOUD_XPN_KEYFILE_JSON}"
+fi
 GOOGLE_PROJECT_ID="$(< ${CLUSTER_PROFILE_DIR}/openshift_gcp_project)"
-
 export GCP_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/gce.json"
 sa_email=$(jq -r .client_email ${GCP_SHARED_CREDENTIALS_FILE})
 if ! gcloud auth list | grep -E "\*\s+${sa_email}"
