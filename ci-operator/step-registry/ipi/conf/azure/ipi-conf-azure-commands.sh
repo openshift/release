fcb0de4850f84eaf1b10b81ed97125be684f9208
maxu <maxu@redhat.com>
2022-08-05 09:41:38 +0800

    bug: 2082395 Make azure baseDomainResoureGroup optional for private (#29967)

--- ci-operator/step-registry/ipi/conf/azure/ipi-conf-azure-commands.sh
+++ ci-operator/step-registry/ipi/conf/azure/ipi-conf-azure-commands.sh
@@ -4,6 +4,22 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+function getVersion() {
+  local release_image=""
+  if [ -n "${RELEASE_IMAGE_INITIAL-}" ]; then
+    release_image=${RELEASE_IMAGE_INITIAL}
+  elif [ -n "${RELEASE_IMAGE_LATEST-}" ]; then
+    release_image=${RELEASE_IMAGE_LATEST}     
+  fi
+  
+  local version=""
+  if [ ${release_image} != "" ]; then
+    oc registry login
+    version=$(oc adm release info ${release_image} --output=json | jq -r '.metadata.version' | cut -d. -f 1,2)    
+  fi
+  echo "${version}"
+}
+
 # TODO: move to image
 curl -L https://github.com/mikefarah/yq/releases/download/3.3.0/yq_linux_amd64 -o /tmp/yq && chmod +x /tmp/yq
 
@@ -29,7 +45,6 @@ cat >> "${CONFIG}" << EOF
 baseDomain: ${BASE_DOMAIN}
 platform:
   azure:
-    baseDomainResourceGroupName: os4-common
     region: ${REGION}
 controlPlane:
   name: master
@@ -59,4 +74,28 @@ EOF
   else
     echo "${OUTBOUND_TYPE} is not supported yet" || exit 1
   fi
+fi
+
+version=$(getVersion)
+echo "get ocp version: ${version}"
+REQUIRED_OCP_VERSION="4.12"
+isOldVersion=true
+if [ -n "${version}" ] && [ "$(printf '%s\n' "${REQUIRED_OCP_VERSION}" "${version}" | sort --version-sort | head -n1)" = "${REQUIRED_OCP_VERSION}" ]; then
+  isOldVersion=false
+fi
+
+PUBLISH=$(/tmp/yq r "${CONFIG}" "publish")
+echo "publish: ${PUBLISH}"
+echo "is Old Version: ${isOldVersion}"
+if [ ${isOldVersion} = true ] || [ -z "${PUBLISH}" ] || [ X"${PUBLISH}" == X"External" ]; then
+  echo "Write the 'baseDomainResourceGroupName: os4-common' to install-config"
+  PATCH="${SHARED_DIR}/install-config-baseDomainRG.yaml.patch"
+    cat > "${PATCH}" << EOF
+platform:
+  azure:
+    baseDomainResourceGroupName: os4-common
+EOF
+    /tmp/yq m -x -i "${CONFIG}" "${PATCH}"
+else
+  echo "Omit baseDomainResourceGroupName for private cluster"
 fi
