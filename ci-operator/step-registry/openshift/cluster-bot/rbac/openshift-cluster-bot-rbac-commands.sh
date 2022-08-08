f2776738a68b0637562a08684a6593d93cb54fb9
Eris Hoxha <ehoxha@redhat.com>
2022-08-08 16:58:32 +0200

    add role-grantor access to cluster-bot service account

--- ci-operator/step-registry/openshift/cluster-bot/rbac/openshift-cluster-bot-rbac-commands.sh
+++ ci-operator/step-registry/openshift/cluster-bot/rbac/openshift-cluster-bot-rbac-commands.sh
@@ -13,3 +13,16 @@ unset KUBECONFIG
 # from a secret written to the namespace.
 oc -n "${NAMESPACE}" create role "ci-chat-bot-secret-reader-${BUILD_ID}" --verb get --resource=secrets --resource-name="${JOB_NAME_SAFE}"
 oc -n "${NAMESPACE}" create rolebinding "ci-chat-bot-secret-reader-binding-${BUILD_ID}" --serviceaccount "ci:ci-chat-bot" --role "ci-chat-bot-secret-reader-${BUILD_ID}"
+
+
+# Grant Cluster-bot roles to allow giving access to  cluster-initiators
+
+# allow the cluster-bot service account (ci:ci-chat-bot) to create a RoleBinding in the namespace
+oc -n "${NAMESPACE}" create role "ci-chat-bot-role-binder-${BUILD_ID}" --verb create --resource=rolebindings
+oc -n "${NAMESPACE}" create rolebinding "ci-chat-bot-role-binder-binding-${BUILD_ID}" --serviceaccount "ci:ci-chat-bot" --role "ci-chat-bot-role-binder-${BUILD_ID}"
+
+# grand permission (explicitly) to the cluster-bot service account (ci:ci-chat-bot) to bind `admin resourceName
+oc -n "${NAMESPACE}" create role "ci-chat-bot-role-grantor-${BUILD_ID}" --verb bind --resource=clusterroles --resource-name=admin
+oc -n "${NAMESPACE}" create rolebinding "ci-chat-bot-role-grantor-binding-${BUILD_ID}" --serviceaccount "ci:ci-chat-bot" --role "ci-chat-bot-role-grantor-${BUILD_ID}"
+
+
