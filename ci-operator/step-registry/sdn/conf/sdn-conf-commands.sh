d24624c3547b33ff7acba63c94d2b30895f9ed0e
Jamo Luhrsen <jluhrsen@gmail.com>
2022-08-10 10:35:24 -0700

    use yq to change networking type (#31196)
    this allows this step to overwrite the existing
    type if it's there instead of appending it for
    an additional time which breaks cluster installs.
    
    Signed-off-by: Jamo Luhrsen <jluhrsen@gmail.com>
    
    Signed-off-by: Jamo Luhrsen <jluhrsen@gmail.com>

--- ci-operator/step-registry/sdn/conf/sdn-conf-commands.sh
+++ ci-operator/step-registry/sdn/conf/sdn-conf-commands.sh
@@ -3,10 +3,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-cat >> "${SHARED_DIR}/install-config.yaml" << EOF
-networking:
-  networkType: OpenShiftSDN
-EOF
+echo "## Install yq"
+curl -L https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -o /tmp/yq && chmod +x /tmp/yq
+echo "   yq installed"
+
+/tmp/yq -i '.networking.networkType="OpenShiftSDN"' "${SHARED_DIR}/install-config.yaml"
 
 echo "install-config.yaml"
 echo "-------------------"
