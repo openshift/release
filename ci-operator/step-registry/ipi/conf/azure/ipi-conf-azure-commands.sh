15efe4d98b12d173a879b62f50eb36b4c16555f8
May Xu <maxu@redhat.com>
2022-08-12 19:02:27 +0800

    add debug info dump the yq sha256sum

--- ci-operator/step-registry/ipi/conf/azure/ipi-conf-azure-commands.sh
+++ ci-operator/step-registry/ipi/conf/azure/ipi-conf-azure-commands.sh
@@ -23,6 +23,9 @@ function getVersion() {
 # TODO: move to image
 curl -L https://github.com/mikefarah/yq/releases/download/3.3.0/yq_linux_amd64 -o /tmp/yq && chmod +x /tmp/yq
 
+echo "get the yq "
+sha256sum /tmp/yq
+
 CONFIG="${SHARED_DIR}/install-config.yaml"
 
 REGION="${LEASED_RESOURCE}"
