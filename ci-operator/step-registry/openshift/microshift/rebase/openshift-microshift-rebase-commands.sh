633bbfb2815a8c07f29d1ac5f7b9389e625a2259
jcope <jcope@redhat.com>
2022-08-15 18:27:52 -0500

    install jq

--- ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
+++ ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
@@ -14,18 +14,6 @@ cp /secrets/ci-pull-secret/.dockercfg "$HOME/.pull-secret.json" || {
     echo "ERROR: Could not copy registry secret file"
 }
 
-# Install yq (code borrowed from the ipi/install step-registry)
-YQ_URL=https://github.com/mikefarah/yq/releases/download/v4.26.1/yq_linux_amd64
-YQ_HASH=9e35b817e7cdc358c1fcd8498f3872db169c3303b61645cc1faf972990f37582
-echo "${YQ_HASH} -" > /tmp/sum.txt
-if ! curl -Ls "${YQ_URL}" | tee /tmp/yq | sha256sum -c /tmp/sum.txt >/dev/null 2>/dev/null; then
-  echo "ERROR: Expected file at ${YQ_URL} to have checksum ${YQ_HASH} but instead got $(sha256sum </tmp/yq | cut -d' ' -f1)"
-  strings /tmp/yq
-  exit 1
-fi
-echo "Downloaded yq; sha256 checksum matches expected ${YQ_HASH}."
-chmod +x /tmp/yq
-
 # Determine pull specs for release images
 release_amd64="$(oc get configmap/release-release-images-latest -o yaml \
     | /tmp/yq '.data."release-images-latest.yaml"' \
