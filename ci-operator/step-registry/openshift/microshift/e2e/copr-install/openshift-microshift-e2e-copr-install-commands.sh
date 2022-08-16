d84bb7f1bbb528593611f3b3f127953435bafeef
Jon Cope <jcope@redhat.com>
2022-08-16 17:10:42 -0500

    Microshift: use rhel-8 image stream for GCP test box (#31262)
    * update GCP image to latest rhel-8
    
    * pause to inspect gcp instance for missing rhsm bin
    
    * public rhel image does not include rhsm
    
    * pin image
    
    * fix copr script
    
    * drop todo

--- ci-operator/step-registry/openshift/microshift/e2e/copr-install/openshift-microshift-e2e-copr-install-commands.sh
+++ ci-operator/step-registry/openshift/microshift/e2e/copr-install/openshift-microshift-e2e-copr-install-commands.sh
@@ -57,6 +57,7 @@ on_exit(){
 trap "on_exit; exit;" EXIT
 
 set -xe
+dnf install subscription-manager -y
 subscription-manager repos --enable rhocp-4.10-for-rhel-8-x86_64-rpms
 dnf install -y cri-o cri-tools firewalld
 systemctl enable crio --now
