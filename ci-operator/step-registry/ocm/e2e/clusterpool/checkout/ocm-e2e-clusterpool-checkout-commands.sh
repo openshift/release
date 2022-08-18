5032627407c946ce5633699a50c36b62ce358a75
Chris Ahl <cahl@redhat.com>
2022-08-17 20:49:21 -0400

    debug prow cluster pool checkout problem
    Signed-off-by: Chris Ahl <cahl@redhat.com>

--- ci-operator/step-registry/ocm/e2e/clusterpool/checkout/ocm-e2e-clusterpool-checkout-commands.sh
+++ ci-operator/step-registry/ocm/e2e/clusterpool/checkout/ocm-e2e-clusterpool-checkout-commands.sh
@@ -3,6 +3,9 @@
 temp=$(mktemp -d -t ocm-XXXXX)
 cd $temp || exit 1
 
+echo "==== BUILD_HARNESS_OS: ${BUILD_HARNESS_OS}"
+
+
 cp "$MAKEFILE" ./Makefile
 
 pools=""
@@ -27,19 +30,19 @@ for ((i=1;i<=CLUSTERPOOL_HUB_COUNT;i++)); do
         make clusterpool/checkout \
             CLUSTERPOOL_NAME=$pool \
             CLUSTERPOOL_CLUSTER_CLAIM=$tmp_claim
-    
+
         if [[ "$?" == 0 ]]; then
             cluster_claim="$tmp_claim"
             break
         fi
     done
-    
+
     if [[ -z "$cluster_claim" ]]; then
         echo "No cluster was checked out for hub $i. Tried these cluster pools:"
         echo "$pools"
         exit 1
     fi
-    
+
     echo "$cluster_claim" >> "${SHARED_DIR}/${CLUSTER_CLAIM_FILE}"
 done
 
@@ -52,19 +55,19 @@ for ((i=1;i<=CLUSTERPOOL_MANAGED_COUNT;i++)); do
         make clusterpool/checkout \
             CLUSTERPOOL_NAME=$pool \
             CLUSTERPOOL_CLUSTER_CLAIM=$tmp_claim
-    
+
         if [[ "$?" == 0 ]]; then
             cluster_claim="$tmp_claim"
             break
         fi
     done
-    
+
     if [[ -z "$cluster_claim" ]]; then
         echo "No cluster was checked out for managed $i. Tried these cluster pools:"
         echo "$pools"
         exit 1
     fi
-    
+
     echo "$cluster_claim" >> "${SHARED_DIR}/${CLUSTER_CLAIM_FILE}"
 done
 
