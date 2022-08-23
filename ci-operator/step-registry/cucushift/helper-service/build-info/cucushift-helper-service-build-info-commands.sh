2a50d33f0e941fb70ad6f4bb3d689a1a99c87ec2
Liang Xia <lxia@redhat.com>
2022-08-23 10:37:54 +0800

    Allow ROSA/OSD testing to skip collecting build info

--- ci-operator/step-registry/cucushift/helper-service/build-info/cucushift-helper-service-build-info-commands.sh
+++ ci-operator/step-registry/cucushift/helper-service/build-info/cucushift-helper-service-build-info-commands.sh
@@ -7,6 +7,6 @@ set -o pipefail
 if [[ -n "${RELEASE_IMAGE_LATEST}" ]]; then
     echo "Release info for: ${RELEASE_IMAGE_LATEST}"
     oc adm release info "${RELEASE_IMAGE_LATEST}" || true
-    PAYLOAD_VERSION="$(oc adm release info "${RELEASE_IMAGE_LATEST}" --output=jsonpath="{.metadata.version}")"
+    PAYLOAD_VERSION="$(oc adm release info "${RELEASE_IMAGE_LATEST}" --output=jsonpath="{.metadata.version}" || true)"
     echo "${PAYLOAD_VERSION}" > "${ARTIFACT_DIR}/ocp-build-info"
 fi
