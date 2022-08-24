1d6994a9b137ecf2c38cb8d6f8e947a573748d2d
Melvin Hillsman <mhillsma@redhat.com>
2022-08-24 12:00:48 -0500

    Add preprod resource for certified-operators pipeline (#31627)
    Introducing a significant change to preflight-trigger and
    for future-proofing work related to preflight and preflight-trigger
    
    Signed-off-by: Melvin Hillsman <mhillsma@redhat.com>
    
    Signed-off-by: Melvin Hillsman <mhillsma@redhat.com>

---
+++ ci-operator/step-registry/operator-pipelines/preflight-preprod/decrypt/operator-pipelines-preflight-preprod-decrypt-commands.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# This step will decrypt preflight artifacts.
+# https://github.com/redhat-openshift-ecosystem/openshift-preflight
+
+# GPG keys are stored in vault for DPTP and OSD for the hosted pipeline
+# Should new keys be generated, the private key 'Real name' MUST be 
+# Preflight Trigger and the public key 'Real name' MUST be Operator
+# Pipelines; the email address for either key is trivial
+
+gpg_private_key_file=/var/run/operator-pipelines-gpg/private
+gpg_public_key_file=/var/run/operator-pipelines-gpg/public
+
+export PFLT_DOCKERCONFIG
+
+if [ -n "${PFLT_DOCKERCONFIG}" ]
+then
+    preflight-trigger decrypt \
+    --gpg-decryption-private-key ${gpg_private_key_file} \
+    --gpg-decryption-public-key ${gpg_public_key_file} \
+    --pflt-docker-config ${PFLT_DOCKERCONFIG} \
+    --output-path ${SHARED_DIR}/decrypted_config.json
+
+    echo "Artifacts decrypted and accessible"
+    exit 0
+else
+    echo "No artifacts to decrypt"
+    exit 0
+fi
