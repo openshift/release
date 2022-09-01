957d967838f334c750ecd8b3d50646684eac2c66
Sonam Maheshwari <43322504+sonam1412@users.noreply.github.com>
2022-09-01 18:04:14 +0200

    [CVP-3057] add ech statement inside if block (#31900)

--- ci-operator/step-registry/optional-operators/subscribe/optional-operators-subscribe-commands.sh
+++ ci-operator/step-registry/optional-operators/subscribe/optional-operators-subscribe-commands.sh
@@ -60,9 +60,8 @@ EOF
     )
 fi
 
-echo "Setting label security.openshift.io/scc.podSecurityLabelSync value to true on the namespace \"$OO_INSTALL_NAMESPACE\""
-
 if [[ "${OO_INSTALL_NAMESPACE}" =~ ^openshift- ]]; then
+    echo "Setting label security.openshift.io/scc.podSecurityLabelSync value to true on the namespace \"$OO_INSTALL_NAMESPACE\""
     oc label --overwrite ns "${OO_INSTALL_NAMESPACE}" security.openshift.io/scc.podSecurityLabelSync=true
 fi
 
