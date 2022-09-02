15cdcbfa638c406167cff03beada551287bb9c00
Pierre Prinetti <pierreprinetti@redhat.com>
2022-09-02 16:17:11 +0200

    openstack: Refactor openstack-conf-installconfig
    Use yq for a more readable script, that doesn't rely on the order of the
    operations.
    
    Also fix availability zones configuration: before this patch, a cloud
    with more than one available zone would crash the script.

--- ci-operator/step-registry/openstack/conf/installconfig/openstack-conf-installconfig-commands.sh
+++ ci-operator/step-registry/openstack/conf/installconfig/openstack-conf-installconfig-commands.sh
@@ -20,139 +20,114 @@ INGRESS_IP=$(<"${SHARED_DIR}/INGRESS_IP")
 PULL_SECRET=$(<"${CLUSTER_PROFILE_DIR}/pull-secret")
 SSH_PUB_KEY=$(<"${CLUSTER_PROFILE_DIR}/ssh-publickey")
 
-CONFIG="${SHARED_DIR}/install-config.yaml"
-
-case "$CONFIG_TYPE" in
-  minimal|proxy)
-    ;;
-  *)
-    echo "No valid install config type specified. Please check CONFIG_TYPE"
-    exit 1
-    ;;
-esac
-
-mapfile -t ZONES < <(printf "%s" "${ZONES}") >/dev/null
+IFS=' ' read -ra ZONES <<< "$ZONES"
 MAX_ZONES_COUNT=${#ZONES[@]}
 
-if [[ ${ZONES_COUNT} -gt ${MAX_ZONES_COUNT} ]]; then
-  echo "Too many zones were requested: ${ZONES_COUNT}; only ${MAX_ZONES_COUNT} are available: ${ZONES[*]}"
-  exit 1
+if [ "${ZONES_COUNT}" -gt 1 ]; then
+	# For now, we only support a cluster within a single AZ.
+	# This will change in the future.
+	echo "Wrong ZONE_COUNT: can only be 0 or 1, got ${ZONES_COUNT}"
+	exit 1
+fi
+if [ "${ZONES_COUNT}" -gt "${MAX_ZONES_COUNT}" ]; then
+	echo "Too many zones were requested: ${ZONES_COUNT}; only ${MAX_ZONES_COUNT} are available: ${ZONES[*]}"
+	exit 1
 fi
 
-if [[ "${ZONES_COUNT}" == "0" ]]; then
-  ZONES_STR="[]"
-elif [[ "${ZONES_COUNT}" == "1" ]]; then
-  function join_by { local IFS="$1"; shift; echo "$*"; }
-  ZONES=("${ZONES[@]:0:${ZONES_COUNT}}")
-  ZONES_STR="[ "
-  ZONES_STR+=$(join_by , "${ZONES[@]}")
-  ZONES_STR+=" ]"
-else
-  # For now, we only support a cluster within a single AZ.
-  # This will change in the future.
-  echo "Wrong ZONE_COUNT, can only be 0 or 1, got ${ZONES_COUNT}"
-  exit 1
-fi
-echo "OpenStack Availability Zones: ${ZONES_STR}"
+ZONES_JSON="$(echo -n "${ZONES[@]:0:${ZONES_COUNT}}" | jq -cRs '(. / " ")')"
+echo "OpenStack Availability Zones: '${ZONES_JSON}'"
 
-cat > "${CONFIG}" << EOF
+INSTALL_CONFIG="${SHARED_DIR}/install-config.yaml"
+
+cat > "$INSTALL_CONFIG" <<EOF
 apiVersion: v1
-baseDomain: ${BASE_DOMAIN}
+baseDomain: '${BASE_DOMAIN}'
 metadata:
-  name: ${CLUSTER_NAME}
-networking:
-  networkType: ${NETWORK_TYPE}
-EOF
-if [[ "${CONFIG_TYPE}" == "proxy" ]]; then
-cat >> "${CONFIG}" << EOF
-  machineNetwork:
-  - cidr: $(<"${SHARED_DIR}"/MACHINES_SUBNET_RANGE)
-EOF
-fi
-cat >> "${CONFIG}" << EOF
-platform:
-  openstack:
-    cloud:             ${OS_CLOUD}
-EOF
-if [[ "${CONFIG_TYPE}" == "minimal" ]]; then
-cat >> "${CONFIG}" << EOF
-    externalDNS:
-      - 1.1.1.1
-      - 1.0.0.1
-    lbFloatingIP:      ${API_IP}
-    ingressFloatingIP: ${INGRESS_IP}
-    externalNetwork:   ${OPENSTACK_EXTERNAL_NETWORK}
-EOF
-elif [[ "${CONFIG_TYPE}" == "proxy" ]]; then
-cat >> "${CONFIG}" << EOF
-    machinesSubnet:    $(<"${SHARED_DIR}"/MACHINES_SUBNET_ID)
-    apiVIP:            ${API_IP}
-    ingressVIP:        ${INGRESS_IP}
-EOF
-fi
-cat >> "${CONFIG}" << EOF
+  name: '${CLUSTER_NAME}'
 compute:
 - name: worker
   replicas: ${WORKER_REPLICAS}
   platform:
     openstack:
-      type: ${OPENSTACK_COMPUTE_FLAVOR}
-      zones: ${ZONES_STR}
-EOF
-if [[ "${ZONES_COUNT}" == "1" ]]; then
-cat >> "${CONFIG}" << EOF
-      rootVolume:
-        type: tripleo
-        size: 30
-        zones: ${ZONES_STR}
-EOF
-fi
-if [[ ${ADDITIONAL_WORKERS_NETWORKS} != "" ]]; then
-    cat >> "${CONFIG}" << EOF
-      additionalNetworkIDs:
-EOF
-    for network in $ADDITIONAL_WORKERS_NETWORKS; do
-        if ! openstack network show "${network}" > /dev/null 2>&1; then
-            echo "Network ${network} does not exist"
-            exit 1
-        fi
-        net_id=$(openstack network show -f value -c id "${network}")
-        cat >> "${CONFIG}" << EOF
-      - ${net_id}
-EOF
-    done
-fi
-
-cat >> "${CONFIG}" << EOF
+      type: '${OPENSTACK_COMPUTE_FLAVOR}'
 controlPlane:
   name: master
+  replicas: 3
   platform:
     openstack:
-      type: ${OPENSTACK_CONTROLPLANE_FLAVOR}
-      zones: ${ZONES_STR}
-  replicas: 3
+      type: '${OPENSTACK_CONTROLPLANE_FLAVOR}'
+networking:
+  networkType: '${NETWORK_TYPE}'
+platform:
+  openstack:
+    cloud: '${OS_CLOUD}'
 pullSecret: >-
   ${PULL_SECRET}
 sshKey: |-
   ${SSH_PUB_KEY}
 EOF
-if [[ "${CONFIG_TYPE}" == "proxy" && -f "${SHARED_DIR}"/PROXY_INTERFACE ]]; then
-  PROXY_INTERFACE=$(<"${SHARED_DIR}"/PROXY_INTERFACE)
-  SQUID_AUTH=$(<"${SHARED_DIR}"/SQUID_AUTH)
-cat >> "${CONFIG}" << EOF
-proxy:
-  httpProxy: http://${SQUID_AUTH}@${PROXY_INTERFACE}:3128/
-  httpsProxy: https://${SQUID_AUTH}@${PROXY_INTERFACE}:3130/
-additionalTrustBundle: |
-$(awk '{print "  "$0}' "${SHARED_DIR}/domain.crt")
-EOF
+
+case "$CONFIG_TYPE" in
+	minimal)
+		yq --yaml-output --in-place ".
+			| .platform.openstack.externalDNS = [\"1.1.1.1\", \"1.0.0.1\"]
+			| .platform.openstack.externalNetwork = \"${OPENSTACK_EXTERNAL_NETWORK}\"
+			| .platform.openstack.ingressFloatingIP = \"${INGRESS_IP}\"
+			| .platform.openstack.lbFloatingIP = \"${API_IP}\"
+		" "$INSTALL_CONFIG"
+		;;
+	proxy)
+		yq --yaml-output --in-place ".
+			| .networking.machineNetwork[0].cidr = \"$(<"${SHARED_DIR}"/MACHINES_SUBNET_RANGE)\"
+			| .platform.openstack.apiVIP = \"${API_IP}\"
+			| .platform.openstack.ingressVIP = \"${INGRESS_IP}\"
+			| .platform.openstack.machinesSubnet = \"$(<"${SHARED_DIR}"/MACHINES_SUBNET_ID)\"
+		" "$INSTALL_CONFIG"
+
+		if [[ -f "${SHARED_DIR}/PROXY_INTERFACE" ]]; then
+			PROXY_INTERFACE=$(<"${SHARED_DIR}/PROXY_INTERFACE")
+			SQUID_AUTH=$(<"${SHARED_DIR}/SQUID_AUTH")
+			yq --yaml-output --in-place ".
+				| .proxy.httpProxy  = \"http://${SQUID_AUTH}@${PROXY_INTERFACE}:3128/\"
+				| .proxy.httpsProxy = \"https://${SQUID_AUTH}@${PROXY_INTERFACE}:3130/\"
+				| .additionalTrustBundle = \"$(<"${SHARED_DIR}/domain.crt")\"
+			" "$INSTALL_CONFIG"
+		fi
+		;;
+	*)
+		echo "No valid install config type specified. Please check CONFIG_TYPE"
+		exit 1
+		;;
+esac
+
+if [[ "${ZONES_COUNT}" -gt '0' ]]; then
+	yq --yaml-output --in-place ".
+		| .compute[0].platform.openstack.zones = ${ZONES_JSON}
+		| .controlPlane.platform.openstack.zones = ${ZONES_JSON}
+		| .compute[0].platform.openstack.rootVolume.type = \"tripleo\"
+		| .compute[0].platform.openstack.rootVolume.size = 30
+		| .compute[0].platform.openstack.rootVolume.zones = ${ZONES_JSON}
+	" "$INSTALL_CONFIG"
 fi
 
-if [ "${FIPS_ENABLED}" = "true" ]; then
-  echo "Adding 'fips: true' to install-config.yaml"
-  cat >> "${CONFIG}" << EOF
-fips: true
-EOF
+if [[ ${ADDITIONAL_WORKERS_NETWORKS:-} != "" ]]; then
+	declare -a networks
+	for network in $ADDITIONAL_WORKERS_NETWORKS; do
+		networks+=("$(openstack network show -f value -c id "${network}")")
+	done
+
+	networks_json="$(echo -n "${networks[@]}" | jq -cRs '(. / " ")')"
+
+	yq --yaml-output --in-place ".
+		| .compute[0].platform.openstack.additionalNetworkIDs = ${networks_json}
+	" "$INSTALL_CONFIG"
+fi
+
+if [ "${FIPS_ENABLED:-}" = "true" ]; then
+	echo "Adding 'fips: true' to install-config.yaml"
+	yq --yaml-output --in-place ".
+		| .fips = true
+	" "$INSTALL_CONFIG"
 fi
 
 # Regenerate install-config.yaml to fill in unset values with default values.
@@ -160,9 +135,9 @@ fi
 (
 	declare dir=''
 	dir="$(mktemp -d)"
-	cp "${CONFIG}" "${dir}/install-config.yaml"
+	cp "$INSTALL_CONFIG" "${dir}/install-config.yaml"
 	openshift-install --dir "$dir" create install-config
-	cp "${dir}/install-config.yaml" "${CONFIG}"
+	cp "${dir}/install-config.yaml" "$INSTALL_CONFIG"
 )
 
 # Make a redacted version available for debugging in the artifacts dir.
@@ -173,16 +148,16 @@ data["pullSecret"] = "redacted"
 if "proxy" in data:
     data["proxy"] = "redacted"
 print(yaml.dump(data))
-' "${SHARED_DIR}/install-config.yaml" > "${ARTIFACT_DIR}/install-config.yaml"
+' "$INSTALL_CONFIG" > "${ARTIFACT_DIR}/install-config.yaml"
 
-# This block will remove the ports created in openstack-provision-machinesubnet-commands.sh
+# Remove the ports created in openstack-provision-machinesubnet-commands.sh
 # since the installer will create them again, based on install-config.yaml.
 if [[ ${OPENSTACK_PROVIDER_NETWORK} != "" ]]; then
-  echo "Provider network detected, will clean-up reserved ports"
-  for p in api ingress; do
-    if openstack port show "${CLUSTER_NAME}-${CONFIG_TYPE}-${p}" >/dev/null; then
-      echo "Port exists for ${CLUSTER_NAME}-${CONFIG_TYPE}-${p}: removing it"
-      openstack port delete "${CLUSTER_NAME}-${CONFIG_TYPE}-${p}"
-    fi
-  done
+	echo "Provider network detected: cleaning up reserved ports"
+	for p in api ingress; do
+		if openstack port show "${CLUSTER_NAME}-${CONFIG_TYPE}-${p}" >/dev/null; then
+			echo "Port exists for ${CLUSTER_NAME}-${CONFIG_TYPE}-${p}: removing it"
+			openstack port delete "${CLUSTER_NAME}-${CONFIG_TYPE}-${p}"
+		fi
+	done
 fi
