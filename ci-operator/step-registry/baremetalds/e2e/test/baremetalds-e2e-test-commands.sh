11aec01ead8ccbc240f536968ddb2d2196f640c1
Trevor Vardeman <34040350+tvardema@users.noreply.github.com>
2022-09-14 06:08:02 -0500

    Creating new 4.11 nightly job for ARM metal IPI e2e tests (#29748)
    Adding ARM image overrides for metal IPI.
    Removed hard-coded and unnecessary value changes.
    Overwriting the squid-proxy image on all arches.
    Marking a pair of exports on arm that should be reviewed pending a separate JIRA.
    Changed initial nightly workflow from e2e-ipv4 to the default e2e workflow.
    
    Co-authored-by: @aleskandro
    Co-authored-by: @deepsm007
    
    JIRA: ARMOCP-384

--- ci-operator/step-registry/baremetalds/e2e/test/baremetalds-e2e-test-commands.sh
+++ ci-operator/step-registry/baremetalds/e2e/test/baremetalds-e2e-test-commands.sh
@@ -8,7 +8,7 @@ function mirror_test_images() {
         echo "### Mirroring test images"
 
         DEVSCRIPTS_TEST_IMAGE_REPO=${DS_REGISTRY}/localimages/local-test-image
-        
+
         openshift-tests images --to-repository ${DEVSCRIPTS_TEST_IMAGE_REPO} > /tmp/mirror
         scp "${SSHOPTS[@]}" /tmp/mirror "root@${IP}:/tmp/mirror"
 
