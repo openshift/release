fbdc5bb3ff06578575a88bb6ca3b900d6f1186d6
yasun <yasun@redhat.com>
2022-09-18 21:50:51 +0800

    Using the default aws credentials but not the user osdCcsAdmin's credentials to provision cluster

--- ci-operator/step-registry/osd-ccs/cluster/provision/aws/osd-ccs-cluster-provision-aws-commands.sh
+++ ci-operator/step-registry/osd-ccs/cluster/provision/aws/osd-ccs-cluster-provision-aws-commands.sh
@@ -13,17 +13,22 @@ OPENSHIFT_VERSION=${OPENSHIFT_VERSION:-}
 CHANNEL_GROUP=${CHANNEL_GROUP}
 MULTI_AZ=${MULTI_AZ:-false}
 ETCD_ENCRYPTION=${ETCD_ENCRYPTION:-false}
+DISABLE_WORKLOAD_MONITORING=${DISABLE_WORKLOAD_MONITORING:-false}
 CLUSTER_TIMEOUT=${CLUSTER_TIMEOUT}
-CLOUD_PROVIDER_REGION=${LEASED_RESOURCE}
+
+default_compute_nodes=2
+if [[ "$MULTI_AZ" == "true" ]]; then
+  default_compute_nodes=3
+fi
+COMPUTE_NODES=${COMPUTE_NODES:-$default_compute_nodes}
 
 # Obtain aws credentials
-AWSCRED="${SHARED_DIR}/osdCcsAdmin.awscred"
+CLOUD_PROVIDER_REGION=${LEASED_RESOURCE}
+AWSCRED="${CLUSTER_PROFILE_DIR}/.awscred"
 if [[ -f "${AWSCRED}" ]]; then
   AWS_ACCOUNT_ID=$(cat "${AWSCRED}" | grep aws_account_id | tr -d ' ' | cut -d '=' -f 2)
   AWS_ACCESS_KEY_ID=$(cat "${AWSCRED}" | grep aws_access_key_id | tr -d ' ' | cut -d '=' -f 2)
   AWS_SECRET_ACCESS_KEY=$(cat "${AWSCRED}" | grep aws_secret_access_key | tr -d ' ' | cut -d '=' -f 2)
-  
-  AWS_ARGS="--aws-account-id ${AWS_ACCOUNT_ID} --aws-access-key-id ${AWS_ACCESS_KEY_ID} --aws-secret-access-key ${AWS_SECRET_ACCESS_KEY}"
 else
   echo "Did not find compatible cloud provider cluster_profile"
   exit 1
@@ -68,50 +73,70 @@ if [[ -z "$OPENSHIFT_VERSION" ]]; then
   echo "Requested cluster version not available!"
   exit 1
 fi
+OPENSHIFT_VERSION="openshift-v${OPENSHIFT_VERSION}"
 echo "Select openshift version ${OPENSHIFT_VERSION}"
 
-# Switches
-MULTI_AZ_SWITCH=""
-if [[ "$MULTI_AZ" == "true" ]]; then
-  MULTI_AZ_SWITCH="--multi-az"
-fi
-
-ETCD_ENCRYPTION_SWITCH=""
-if [[ "$ETCD_ENCRYPTION" == "true" ]]; then
-  ETCD_ENCRYPTION_SWITCH="--etcd-encryption"
-fi
-
+# Cluster parameters
 echo "Parameters for cluster request:"
 echo "  Cluster name: ${CLUSTER_NAME}"
 echo "  Cloud provider region: aws"
 echo "  Cloud provider region: ${CLOUD_PROVIDER_REGION}"
 echo "  Compute machine type: ${COMPUTE_MACHINE_TYPE}"
+echo "  Compute nodes: ${COMPUTE_NODES}"
 echo "  Multi-az: ${MULTI_AZ}"
 echo "  Openshift version: ${OPENSHIFT_VERSION}"
 echo "  Channel group: ${CHANNEL_GROUP}"
 echo "  Etcd encryption: ${ETCD_ENCRYPTION}"
-echo "  Cluster nodes: ${COMPUTE_NODES}"
+echo "  Disable workload monitoring: ${DISABLE_WORKLOAD_MONITORING}"
 
-mkdir -p "${SHARED_DIR}"
-CLUSTER_ID_FILE="${SHARED_DIR}/cluster-id"
-CLUSTER_INFO="${ARTIFACT_DIR}/cluster.txt"
-CLUSTER_INSTALL_LOG="${ARTIFACT_DIR}/.cluster_install.log"
+# Cluster payload
+# Using the default aws credentials but not the user osdCcsAdmin's credentials to provision cluster
+CLUSTER_PAYLOAD='{
+  "name": "'${CLUSTER_NAME}'",
+  "region": {
+    "id": "'${CLOUD_PROVIDER_REGION}'"
+  },
+  "nodes": {
+    "compute_machine_type": {
+      "id": "'${COMPUTE_MACHINE_TYPE}'"
+    },
+    "compute": '${COMPUTE_NODES}'
+  },
+  "managed": true,
+  "product": {
+    "id": "osd"
+  },
+  "cloud_provider": {
+    "id": "aws"
+  },
+  "multi_az": '${MULTI_AZ}',
+  "etcd_encryption": '${ETCD_ENCRYPTION}',
+  "disable_user_workload_monitoring": '${DISABLE_WORKLOAD_MONITORING}',
+  "version": {
+    "id": "'${OPENSHIFT_VERSION}'",
+    "channel_group": "'${CHANNEL_GROUP}'"
+  },
+  "properties": {
+    "use_local_credentials": "true"
+  },
+  "ccs": {
+    "enabled": true,
+    "disable_scp_checks": false
+  },
+  "aws": {
+    "access_key_id": "'${AWS_ACCESS_KEY_ID}'",
+    "account_id": "'${AWS_ACCOUNT_ID}'",
+    "secret_access_key": "'${AWS_SECRET_ACCESS_KEY}'"
+  }
+}'
 
-ocm create cluster ${AWS_ARGS} \
-                   --ccs "${CLUSTER_NAME}" \
-                   --compute-nodes "${COMPUTE_NODES}" \
-                   --version "${OPENSHIFT_VERSION}" \
-                   --channel-group "${CHANNEL_GROUP}" \
-                   --compute-machine-type "${COMPUTE_MACHINE_TYPE}" \
-                   --region "${CLOUD_PROVIDER_REGION}" \
-                   ${MULTI_AZ_SWITCH} \
-                   ${ETCD_ENCRYPTION_SWITCH} \
-                   > "${CLUSTER_INFO}"
+echo $CLUSTER_PAYLOAD | ocm post /api/clusters_mgmt/v1/clusters > "${ARTIFACT_DIR}/cluster.txt"
 
 # Store the cluster ID for the post steps and the cluster deprovision
-CLUSTER_ID=$(cat "${CLUSTER_INFO}" | grep '^ID:' | tr -d '[:space:]' | cut -d ':' -f 2)
+mkdir -p "${SHARED_DIR}"
+CLUSTER_ID=$(cat "${ARTIFACT_DIR}/cluster.txt" | jq '.id' | tr -d '"')
 echo "Cluster ${CLUSTER_NAME} is being created with cluster-id: ${CLUSTER_ID}"
-echo -n "${CLUSTER_ID}" > "${CLUSTER_ID_FILE}"
+echo -n "${CLUSTER_ID}" > "${SHARED_DIR}/cluster-id"
 
 echo "Waiting for cluster ready..."
 start_time=$(date +"%s")
@@ -120,7 +145,7 @@ while true; do
   CLUSTER_STATE=$(ocm get cluster "${CLUSTER_ID}" | jq -r '.status.state')
   echo "Cluster state: ${CLUSTER_STATE}"
   if [[ "${CLUSTER_STATE}" == "ready" ]]; then
-    echo "Cluster is reported as ready"
+    echo "Cluster ${CLUSTER_ID} is reported as ready"
     break
   fi
   if (( $(date +"%s") - $start_time >= $CLUSTER_TIMEOUT )); then
@@ -128,12 +153,12 @@ while true; do
     exit 1
   fi
   if [[ "${CLUSTER_STATE}" != "installing" && "${CLUSTER_STATE}" != "pending" ]]; then
-    ocm get "/api/clusters_mgmt/v1/clusters/${CLUSTER_ID}/logs/install" > "${CLUSTER_INSTALL_LOG}" || echo "error: Unable to pull installation log."
+    ocm get "/api/clusters_mgmt/v1/clusters/${CLUSTER_ID}/logs/install" > "${ARTIFACT_DIR}/.cluster_install.log" || echo "error: Unable to pull installation log."
     echo "error: Cluster reported invalid state: ${CLUSTER_STATE}"
     exit 1
   fi
 done
-ocm get "/api/clusters_mgmt/v1/clusters/${CLUSTER_ID}/logs/install" > "${CLUSTER_INSTALL_LOG}"
+ocm get "/api/clusters_mgmt/v1/clusters/${CLUSTER_ID}/logs/install" > "${ARTIFACT_DIR}/.cluster_install.log"
 
 # Print console.url and api.url
 CONSOLE_URL=$(ocm get /api/clusters_mgmt/v1/clusters/${CLUSTER_ID} | jq -r '.console.url')
