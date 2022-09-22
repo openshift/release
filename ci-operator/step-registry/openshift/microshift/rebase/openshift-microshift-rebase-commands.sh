23c55a92a8ac65ce427976cb5373c04ce40edb50
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2022-09-22 10:59:27 +0200

    USHIFT-179: auto PR creation for rebase

--- ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
+++ ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
@@ -29,5 +29,10 @@ echo "Pull spec for amd64 release image: ${pullspec_release_amd64}"
 echo "Pull spec for arm64 release image: ${pullspec_release_arm64}"
 
 # Call rebase script
-echo "./scripts/rebase.sh to \"${pullspec_release_amd64}\" \"${pullspec_release_arm64}\""
 ./scripts/rebase.sh to "${pullspec_release_amd64}" "${pullspec_release_arm64}"
+
+APP_ID=$(cat /secrets/microshift-pr-creation-test/app_id) \
+KEY=/secrets/microshift-pr-creation-test/key \
+ORG=pmtk \
+REPO=microshift \
+./scripts/create_pr.py
