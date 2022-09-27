5e6592a97aa812d1859ac12c9aea2d6d37849bb8
Giles Hutton <ghutton@redhat.com>
2022-09-27 08:10:22 +0100

    Refactors script usage in stackrox/collector (#32025)
    * Refactors script usage in stackrox/collector
    
    * make update following rebase to master
    
    * Renames config back to master
    
    * retargets to branch that fixes secrets use in integration tests
    
    * make update after switching back to master
    
    * Normalises secret mount and switches branch to test
    
    * Fixes support package use of vm creation/teardown scripts
    
    * make update, switching back to master

--- ci-operator/step-registry/stackrox/collector/update-support-packages/setup/stackrox-collector-update-support-packages-setup-commands.sh
+++ ci-operator/step-registry/stackrox/collector/update-support-packages/setup/stackrox-collector-update-support-packages-setup-commands.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 source .openshift-ci/jobs/update-support-packages/env.sh
 
-.openshift-ci/jobs/integration-tests/gcloud-init.sh
-.openshift-ci/jobs/integration-tests/create-vm.sh
+.openshift-ci/scripts/gcloud-init.sh
+.openshift-ci/scripts/vms/create-vm.sh
 
