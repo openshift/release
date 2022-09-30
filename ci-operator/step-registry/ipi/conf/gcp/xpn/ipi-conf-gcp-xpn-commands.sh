b087392bef1d7229bef322eac193dea230488e5f
AnnaZivkovic <github@squid.systems>
2022-09-30 12:54:40 -0700

    Added IPI GCP XPN workflow
    The XPN workflow configures the Host Projects Shared VPC, Network, and Subnets and modifies the install config to use these fields. PassthoughMode is also added to the installconfig so the proper service accounts are used. However, the correct permissions will need to be added to gce.json account(s) in the openshift-dev-installer project.

---
+++ ci-operator/step-registry/ipi/conf/gcp/xpn/ipi-conf-gcp-xpn-commands.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+CONFIG="${SHARED_DIR}/install-config.yaml"
+PATCH=/tmp/install-config-xpn.yaml.patch
+
+HOST_PROJECT="openshift-dev-installer"
+HOST_PROJECT_NETWORK="installer-shared-vpc"
+HOST_PROJECT_CONTROL_SUBNET="installer-shared-vpc-subnet-1"
+HOST_PROJECT_COMPUTE_SUBNET="installer-shared-vpc-subnet-2"
+
+cat > "${PATCH}" << EOF
+featureSet: TechPreviewNoUpgrade
+credentialsMode: Passthrough
+platform:
+  gcp:
+    computeSubnet: ${HOST_PROJECT_COMPUTE_SUBNET}
+    controlPlaneSubnet: ${HOST_PROJECT_CONTROL_SUBNET}
+    createFirewallRules: Disabled
+    network: ${HOST_PROJECT_NETWORK}
+    networkProjectID: ${HOST_PROJECT}
+EOF
+
+yq-go m -x -i "${CONFIG}" "${PATCH}"
