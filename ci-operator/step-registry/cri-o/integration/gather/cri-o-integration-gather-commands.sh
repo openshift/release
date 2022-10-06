2a52869742255befe715a0e4ec8efde1d53bc427
Skyler Clark <skylerjaneclark@gmail.com>
2022-10-06 12:51:32 -0400

    maybe this?

--- ci-operator/step-registry/cri-o/integration/gather/cri-o-integration-gather-commands.sh
+++ ci-operator/step-registry/cri-o/integration/gather/cri-o-integration-gather-commands.sh
@@ -34,7 +34,7 @@ instance_name=$(<"${SHARED_DIR}/gcp-instance-ids.txt")
 
 function getlogs() {
   echo "### Downloading logs..."
-  gcloud compute scp --recurse --zone "${ZONE}" --recurse "${instance_name}:/tmp/artifacts/*" "${ARTIFACT_DIR}"
+  gcloud compute scp --recurse --zone "${ZONE}" --recurse "root@${instance_name}:/tmp/artifacts/*" "${ARTIFACT_DIR}"
 }
 
 # Gather logs regardless of what happens after this
