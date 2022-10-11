33a6254b4ed9052ee0608826532d3fd14b458f21
Martin André <m.andre@redhat.com>
2022-10-11 14:25:42 +0200

    OCPBUGS-2098: have gather bootstrap work in disconnected envs (#32925)
    * OCPBUGS-2098: have gather bootstrap work in disconnected envs
    
    Use the proxy when working on a disconnected environment and avoid
    creating FIPs to gather the bootstrap logs.
    
    * Do not create FIP if bootstrap already has one
    
    OpenStack doesn't like it and fails with:
    
        Cannot associate floating IP <FIP> with port <PortID> using fixed IP
        <FixedIP>, as that fixed IP already has a floating IP on external
        network
    
    Check if the bootstrap port already has a FIP attached to it, and use
    it in that case.

--- ci-operator/step-registry/openstack/gather/openstack-gather-commands.sh
+++ ci-operator/step-registry/openstack/gather/openstack-gather-commands.sh
@@ -6,35 +6,55 @@ set -x
 export OS_CLIENT_CONFIG_FILE="${SHARED_DIR}/clouds.yaml"
 CLUSTER_NAME=$(<"${SHARED_DIR}/CLUSTER_NAME")
 OPENSTACK_EXTERNAL_NETWORK="${OPENSTACK_EXTERNAL_NETWORK:-$(<"${SHARED_DIR}/OPENSTACK_EXTERNAL_NETWORK")}"
+CREATE_FIPS=1
+
+if test -f "${SHARED_DIR}/proxy-conf.sh"
+then
+    # shellcheck disable=SC1090
+    source "${SHARED_DIR}/proxy-conf.sh"
+    CREATE_FIPS=0
+fi
 
 collect_bootstrap_logs() {
 	if [ "$CLUSTER_NAME" != "" ]; then
 		declare -a GATHER_BOOTSTRAP_ARGS
 		declare -a FIPS
+		IP=
 		
 		BOOTSTRAP_NODE=$(openstack server list --format value -c Name | awk "/${CLUSTER_NAME}-.{5}-bootstrap/ {print}")
 		if [ "$BOOTSTRAP_NODE" != "" ]; then
 			echo "Collecting bootstrap logs..."
 			CLUSTER_ID=${BOOTSTRAP_NODE%-bootstrap}
 			openstack security group rule create ${CLUSTER_ID}-master --protocol tcp --dst-port 22:22 --remote-ip 0.0.0.0/0
-			FIP=$(openstack floating ip create "$OPENSTACK_EXTERNAL_NETWORK" --description "${CLUSTER_ID}-bootstrap" --format value --column floating_ip_address)
-			FIPS+=("${FIP}")
-			openstack server add floating ip ${CLUSTER_ID}-bootstrap ${FIP}
-			GATHER_BOOTSTRAP_ARGS+=('--bootstrap' "${FIP}")
+			if [[ $CREATE_FIPS == 1 ]]; then
+				IP=$(openstack floating ip list --port ${CLUSTER_ID}-bootstrap-port -c "Floating IP Address" -f value)
+				if [[ ${IP} == "" ]]; then
+					IP=$(openstack floating ip create "$OPENSTACK_EXTERNAL_NETWORK" --description "${CLUSTER_ID}-bootstrap" --format value --column floating_ip_address)
+				fi
+				FIPS+=("${IP}")
+				openstack server add floating ip ${CLUSTER_ID}-bootstrap ${IP}
+			else
+				IP=$(openstack server show ${BOOTSTRAP_NODE} --colum addresses --format json | jq -r '.addresses | .[][0]')
+			fi
+			GATHER_BOOTSTRAP_ARGS+=('--bootstrap' "${IP}")
 			
 			for idx in 0 1 2; do
 				if openstack server show ${CLUSTER_ID}-master-${idx} &> /dev/null; then
-					FIP=$(openstack floating ip create "$OPENSTACK_EXTERNAL_NETWORK" --description "${CLUSTER_ID}-master-${idx}" --format value --column floating_ip_address)
-					FIPS+=("${FIP}")
-					openstack server add floating ip ${CLUSTER_ID}-master-${idx} ${FIP}
-					GATHER_BOOTSTRAP_ARGS+=('--master' "${FIP}")
+					if [[ $CREATE_FIPS == 1 ]]; then
+						IP=$(openstack floating ip create "$OPENSTACK_EXTERNAL_NETWORK" --description "${CLUSTER_ID}-master-${idx}" --format value --column floating_ip_address)
+						FIPS+=("${IP}")
+						openstack server add floating ip ${CLUSTER_ID}-master-${idx} ${IP}
+					else
+						IP=$(openstack server show ${CLUSTER_ID}-master-${idx} --colum addresses --format json | jq -r '.addresses | .[][0]')
+					fi
+					GATHER_BOOTSTRAP_ARGS+=('--master' "${IP}")
 				fi
 			done
 			
 			SSH_PRIV_KEY_PATH="${CLUSTER_PROFILE_DIR}/ssh-privatekey"
 			openshift-install gather bootstrap --key "${SSH_PRIV_KEY_PATH}" "${GATHER_BOOTSTRAP_ARGS[@]}"
 			cp log-bundle-*.tar.gz "${ARTIFACT_DIR}"
-			openstack floating ip delete "${FIPS[@]}"
+			echo "${FIPS[@]}" | xargs --no-run-if-empty openstack floating ip delete
 		fi
 	fi
 }
