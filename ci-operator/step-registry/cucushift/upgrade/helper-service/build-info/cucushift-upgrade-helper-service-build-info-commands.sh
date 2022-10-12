5993f944864d6f5dc077c7f2d05d96764412ff4e
dis <dis@redhat.com>
2022-10-12 16:10:22 +0530

    rename variables to hold initial and upgraded values

--- ci-operator/step-registry/cucushift/upgrade/helper-service/build-info/cucushift-upgrade-helper-service-build-info-commands.sh
+++ ci-operator/step-registry/cucushift/upgrade/helper-service/build-info/cucushift-upgrade-helper-service-build-info-commands.sh
@@ -9,13 +9,12 @@ BUILD_INFO_FILE="${ARTIFACT_DIR}/ocp-build-info"
 if [[ -n "${RELEASE_IMAGE_LATEST}" ]]; then
     echo "Release info for: ${RELEASE_IMAGE_LATEST}"
     oc adm release info "${RELEASE_IMAGE_LATEST}" || true
-    LATEST_VERSION="$(oc adm release info "${RELEASE_IMAGE_LATEST}" --output=jsonpath="{.metadata.version}")"
-    echo "${LATEST_VERSION}" > "${BUILD_INFO_FILE}"
+    INITIAL_VERSION="$(oc adm release info "${RELEASE_IMAGE_LATEST}" --output=jsonpath="{.metadata.version}")"
+    echo "INITIAL_VERSION : ${INITIAL_VERSION}" >> "${BUILD_INFO_FILE}"
 fi
 if [[ -n "${RELEASE_IMAGE_TARGET}" ]]; then 
-    echo "${RELEASE_IMAGE_TARGET}"
     echo "Release info for: ${RELEASE_IMAGE_TARGET}"
     oc adm release info "${RELEASE_IMAGE_TARGET}" || true
-    TARGET_VERSION="$(oc adm release info "${RELEASE_IMAGE_TARGET}" --output=jsonpath="{.metadata.version}")"
-    echo "${TARGET_VERSION}" > "${BUILD_INFO_FILE}"
+    UPGRADED_VERSION="$(oc adm release info "${RELEASE_IMAGE_TARGET}" --output=jsonpath="{.metadata.version}")"
+    echo "UPGRADED_VERSION : ${UPGRADED_VERSION}" >> "${BUILD_INFO_FILE}"
 fi
