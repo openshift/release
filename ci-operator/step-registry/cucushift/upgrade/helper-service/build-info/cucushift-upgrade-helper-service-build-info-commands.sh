755d411bf5567d9348cb4deb812f53c1953dd81c
Dinesh Kumar S <dis@redhat.com>
2022-10-13 09:59:44 +0530

    OCPQE-11259  add build version for upgrade tests as image target value (#32737)
    * add build version for image target value
    
    * test variable commit1
    
    * add support for env variables in ref.yaml
    
    * rename variables to hold initial and upgraded values
    
    * remove var name in file
    
    * rename variables

--- ci-operator/step-registry/cucushift/upgrade/helper-service/build-info/cucushift-upgrade-helper-service-build-info-commands.sh
+++ ci-operator/step-registry/cucushift/upgrade/helper-service/build-info/cucushift-upgrade-helper-service-build-info-commands.sh
@@ -10,5 +10,11 @@ if [[ -n "${RELEASE_IMAGE_LATEST}" ]]; then
     echo "Release info for: ${RELEASE_IMAGE_LATEST}"
     oc adm release info "${RELEASE_IMAGE_LATEST}" || true
     LATEST_VERSION="$(oc adm release info "${RELEASE_IMAGE_LATEST}" --output=jsonpath="{.metadata.version}")"
-    echo "${LATEST_VERSION}" > "${BUILD_INFO_FILE}"
+    echo "${LATEST_VERSION}" >> "${BUILD_INFO_FILE}"
+fi
+if [[ -n "${RELEASE_IMAGE_TARGET}" ]]; then 
+    echo "Release info for: ${RELEASE_IMAGE_TARGET}"
+    oc adm release info "${RELEASE_IMAGE_TARGET}" || true
+    TARGET_VERSION="$(oc adm release info "${RELEASE_IMAGE_TARGET}" --output=jsonpath="{.metadata.version}")"
+    echo "${TARGET_VERSION}" >> "${BUILD_INFO_FILE}"
 fi
