9af7414f3183a18c7feaa5b394fd9060856acb59
dis <dis@redhat.com>
2022-10-13 09:00:56 +0530

    remove var name in file

--- ci-operator/step-registry/cucushift/upgrade/helper-service/build-info/cucushift-upgrade-helper-service-build-info-commands.sh
+++ ci-operator/step-registry/cucushift/upgrade/helper-service/build-info/cucushift-upgrade-helper-service-build-info-commands.sh
@@ -10,11 +10,11 @@ if [[ -n "${RELEASE_IMAGE_LATEST}" ]]; then
     echo "Release info for: ${RELEASE_IMAGE_LATEST}"
     oc adm release info "${RELEASE_IMAGE_LATEST}" || true
     INITIAL_VERSION="$(oc adm release info "${RELEASE_IMAGE_LATEST}" --output=jsonpath="{.metadata.version}")"
-    echo "INITIAL_VERSION : ${INITIAL_VERSION}" >> "${BUILD_INFO_FILE}"
+    echo "${INITIAL_VERSION}" >> "${BUILD_INFO_FILE}"
 fi
 if [[ -n "${RELEASE_IMAGE_TARGET}" ]]; then 
     echo "Release info for: ${RELEASE_IMAGE_TARGET}"
     oc adm release info "${RELEASE_IMAGE_TARGET}" || true
     UPGRADED_VERSION="$(oc adm release info "${RELEASE_IMAGE_TARGET}" --output=jsonpath="{.metadata.version}")"
-    echo "UPGRADED_VERSION : ${UPGRADED_VERSION}" >> "${BUILD_INFO_FILE}"
+    echo "${UPGRADED_VERSION}" >> "${BUILD_INFO_FILE}"
 fi
