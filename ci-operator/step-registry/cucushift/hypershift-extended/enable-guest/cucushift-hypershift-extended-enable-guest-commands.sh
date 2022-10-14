582b6a39aef466ae371107f4eb64cc369b3087cc
Liangquan Li <liangli@redhat.com>
2022-10-14 14:30:40 +0800

    hypershift: enable guest fix (#33120)

--- ci-operator/step-registry/cucushift/hypershift-extended/enable-guest/cucushift-hypershift-extended-enable-guest-commands.sh
+++ ci-operator/step-registry/cucushift/hypershift-extended/enable-guest/cucushift-hypershift-extended-enable-guest-commands.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -xeuo pipefail
+set -euo pipefail
 
 if [ ! -f "${SHARED_DIR}/nested_kubeconfig" ]; then
   exit 1
@@ -12,9 +12,7 @@ CLUSTER_NAME=$(oc get hostedclusters -n clusters -o=jsonpath='{.items[0].metadat
 
 kubedamin_password=$(oc get secret -n "$NAMESPACE-$CLUSTER_NAME" kubeadmin-password --template='{{.data.password | base64decode}}')
 echo $kubedamin_password > "$SHARED_DIR/hostedcluster_kubedamin_password"
-echo "hostedcluster_kubedamin_password path:$SHARED_DIR/hostedcluster_kubedamin_password"
-cat "$SHARED_DIR/hostedcluster_kubedamin_password"
-echo "https://$(oc --kubeconfig="$SHARED_DIR"/hostedcluster.kubeconfig -n openshift-console get routes console -o=jsonpath='{.spec.host}')" > "$SHARED_DIR/hostedcluster_console.url"
+echo "https://$(oc --kubeconfig="$SHARED_DIR"/nested_kubeconfig -n openshift-console get routes console -o=jsonpath='{.spec.host}')" > "$SHARED_DIR/hostedcluster_console.url"
 echo "hostedcluster_console.url path:$SHARED_DIR/hostedcluster_console.url"
 cat "$SHARED_DIR/hostedcluster_console.url"
 
