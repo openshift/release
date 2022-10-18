5c01dc7da8eb2236d37ae7eecf90f98917142220
Giles Hutton <ghutton@redhat.com>
2022-10-18 11:45:14 +0100

    ROX-12484: converts integration tests to use ansible

--- ci-operator/step-registry/stackrox/collector/integration-tests/setup/stackrox-collector-integration-tests-setup-commands.sh
+++ ci-operator/step-registry/stackrox/collector/integration-tests/setup/stackrox-collector-integration-tests-setup-commands.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 source .openshift-ci/jobs/integration-tests/env.sh
 
 .openshift-ci/scripts/gcloud-init.sh
-.openshift-ci/scripts/vms/create-vm.sh
 
+make -C ansible BUILD_TYPE=ci integration-tests-setup integration-tests-provision
