4852f1d742bf56f7352be1a3c3731c698b72679c
Maysa Macedo <maysa.macedo95@gmail.com>
2022-10-19 13:20:50 +0200

    Gather bootstrap logs on Restricted installations with Proxy
    On restricted installations with Proxy, the bootstrap, masters
    and Proxy have private addresses, consequently is not possible
    to access the Proxy to gather the logs. This commit fixes
    the issue by accessing the bastion node, which has access to the
    restricted resources.

--- ci-operator/step-registry/openstack/gather/openstack-gather-commands.sh
+++ ci-operator/step-registry/openstack/gather/openstack-gather-commands.sh
@@ -15,6 +15,8 @@ then
     # shellcheck disable=SC1090
     source "${SHARED_DIR}/proxy-conf.sh"
     CREATE_FIPS=0
+    BASTION_FIP=$(<"${SHARED_DIR}/BASTION_FIP")
+    BASTION_USER=$(<"${SHARED_DIR}/BASTION_USER")
 fi
 
 collect_bootstrap_logs() {
@@ -54,11 +56,36 @@ collect_bootstrap_logs() {
 					GATHER_BOOTSTRAP_ARGS+=('--master' "${IP}")
 				fi
 			done
-			
+			# Ideally this would be removed once the openshift-install gather bootstrap starts supporting proxy https://issues.redhat.com/browse/CORS-2367
 			SSH_PRIV_KEY_PATH="${CLUSTER_PROFILE_DIR}/ssh-privatekey"
-			openshift-install gather bootstrap --key "${SSH_PRIV_KEY_PATH}" "${GATHER_BOOTSTRAP_ARGS[@]}"
-			cp log-bundle-*.tar.gz "${ARTIFACT_DIR}"
-			echo "${FIPS[@]}" | xargs --no-run-if-empty openstack floating ip delete
+			if test -f "${SHARED_DIR}/proxy-conf.sh"
+			then
+				# configure the local container environment to have the correct SSH configuration
+				if ! whoami &> /dev/null; then
+					if [[ -w /etc/passwd ]]; then
+						echo "${BASTION_USER}:x:$(id -u):0:${BASTION_USER} user:${HOME}:/sbin/nologin" >> /etc/passwd
+					fi
+				fi
+				SSH_ARGS="-o ConnectTimeout=10 -o StrictHostKeyChecking=no -i $SSH_PRIV_KEY_PATH"
+				SSH_CMD="ssh $SSH_ARGS $BASTION_USER@$BASTION_FIP"
+				SCP_CMD="scp $SSH_ARGS"
+				if ! $SSH_CMD uname -a; then
+					echo "ERROR: Bastion proxy is not reachable via $BASTION_FIP"
+					exit 1
+				fi
+				echo "Moving credentials and openshift binary to Bastion Proxy"
+				$SCP_CMD $SSH_PRIV_KEY_PATH /bin/openshift-install $BASTION_USER@$BASTION_FIP:/tmp
+				echo "Gathering bootstrap logs from Bastion Proxy"
+				$SSH_CMD bash - << EOF
+/tmp/openshift-install gather bootstrap --key /tmp/ssh-privatekey ${GATHER_BOOTSTRAP_ARGS[@]}
+EOF
+				echo "Copying logs"
+				$SCP_CMD $BASTION_USER@$BASTION_FIP:/home/$BASTION_USER/log-bundle-*.tar.gz "${SHARED_DIR}"/
+			else
+				openshift-install gather bootstrap --key "${SSH_PRIV_KEY_PATH}" "${GATHER_BOOTSTRAP_ARGS[@]}"
+				cp log-bundle-*.tar.gz "${ARTIFACT_DIR}"
+				echo "${FIPS[@]}" | xargs --no-run-if-empty openstack floating ip delete
+			fi
 		fi
 	fi
 }
