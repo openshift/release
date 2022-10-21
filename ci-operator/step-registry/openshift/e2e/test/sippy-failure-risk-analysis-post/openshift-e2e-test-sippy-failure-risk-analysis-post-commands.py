dc38e265d5c328f159e9318a578600665118997e
Devan Goodwin <dgoodwin@redhat.com>
2022-10-21 11:06:29 -0300

    Fix python

--- ci-operator/step-registry/openshift/e2e/test/sippy-failure-risk-analysis-post/openshift-e2e-test-sippy-failure-risk-analysis-post-commands.py
+++ ci-operator/step-registry/openshift/e2e/test/sippy-failure-risk-analysis-post/openshift-e2e-test-sippy-failure-risk-analysis-post-commands.py
@@ -2,5 +2,5 @@
 import os
 import subprocess
 
-result = subprocess.run(['ls', '-l', os.environ('SHARED_DIR')], stdout=subprocess.PIPE)
+result = subprocess.run(['ls', '-l', os.getenv('SHARED_DIR', '/')], stdout=subprocess.PIPE)
 print(result.stdout)
