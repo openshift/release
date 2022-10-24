b14d5137f1c3d383f1b9105ab93f29f0bd39c0d3
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-10-24 15:23:40 +0200

    MGMT-11274: Add optional nutanix integration test in assisted-test-infra (#31726)
    * Add assisted-nutanix test to assisted-test-infra
    
    Optional ad-hoc job
    
    * Add assisted-nutanix workflow
    
    This workflow is similar to assisted-vsphere:
    * Prepares Nutanix credentials
    * Creates a test machine on Nutanix clusters
    * Installs assisted-test-infra packages and minikube on CI machine via ssh
    * runs `make test` to create Nutanix machines, form the cluster and run tests
    * tears down the cluster and CI machine
    
    * assisted-baremetal-conf, assisted-common-verify: support nutanix platform

---
+++ ci-operator/step-registry/assisted/nutanix/postinstall/assisted-nutanix-postinstall-commands.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+echo "************ nutanix assisted test-infra post-install ************"
+source ${SHARED_DIR}/platform-conf.sh
+source ${SHARED_DIR}/nutanix_context.sh
+
+# Find the first kubeconfig saved by assisted common post-install step
+# shellcheck disable=SC2044
+for kubeconfig in $(find ${SHARED_DIR} -name "kubeconfig*" -exec echo {} \;); do
+    # Copy kubeconfig so that other nutanix steps could use it
+    cp -rvf ${kubeconfig} ${SHARED_DIR}/kubeconfig
+    break
+done
+export KUBECONFIG=${SHARED_DIR}/kubeconfig
+oc project default
+
+echo "Patching infrastructure/cluster"
+oc patch infrastructure/cluster --type=merge --patch-file=/dev/stdin <<-EOF
+{
+  "spec": {
+    "platformSpec": {
+      "nutanix": {
+        "prismCentral": {
+          "address": "${NUTANIX_ENDPOINT}",
+          "port": ${NUTANIX_PORT}
+        },
+        "prismElements": [
+          {
+            "endpoint": {
+              "address": "${PE_HOST}",
+              "port": ${PE_PORT}
+            },
+            "name": "${NUTANIX_CLUSTER_NAME}"
+          }
+        ]
+      },
+      "type": "Nutanix"
+    }
+  }
+}
+EOF
+echo "infrastructure/cluster created"
+
+cat <<EOF | oc create -f -
+apiVersion: v1
+kind: Secret
+metadata:
+   name: nutanix-credentials
+   namespace: openshift-machine-api
+type: Opaque
+stringData:
+  credentials: |
+    [{"type":"basic_auth","data":{"prismCentral":{"username":"${NUTANIX_USERNAME}","password":"${NUTANIX_PASSWORD}"},"prismElements":null}}]
+EOF
+echo "machine API credentials created"
+
+until \
+  oc wait --all=true clusteroperator --for='condition=Available=True' >/dev/null && \
+  oc wait --all=true clusteroperator --for='condition=Progressing=False' >/dev/null && \
+  oc wait --all=true clusteroperator --for='condition=Degraded=False' >/dev/null;  do
+    echo "$(date --rfc-3339=seconds) Clusteroperators not yet ready"
+    sleep 1s
+done
