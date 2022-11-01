openshift/release

commit 54a7c64f7b75e9e2c1f7424f2894fdef126df483

C2S/SC2S (#28149)

ci-operator/step-registry/aws/deprovision/s3buckets/aws-deprovision-s3buckets-commands.sh
@@ -9,6 +9,12 @@ trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wa
 export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 REGION="${LEASED_RESOURCE}"
 
+# Special setting for C2S/SC2S
+if [ "${CLUSTER_TYPE}" == "aws-c2s" ] || [ "${CLUSTER_TYPE}" == "aws-sc2s" ]; then
+  source_region=$(jq -r ".\"${REGION}\".source_region" "${CLUSTER_PROFILE_DIR}/shift_project_setting.json")
+  REGION=$source_region
+fi
+
 echo "Deleting s3 bucket of bastion host"
 
 s3_bucket_list="${SHARED_DIR}/to_be_removed_s3_bucket_list"

ci-operator/step-registry/aws/deprovision/stacks/aws-deprovision-stacks-commands.sh
@@ -9,6 +9,12 @@ trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wa
 export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 REGION="${LEASED_RESOURCE}"
 
+# Special setting for C2S/SC2S
+if [ "${CLUSTER_TYPE}" == "aws-c2s" ] || [ "${CLUSTER_TYPE}" == "aws-sc2s" ]; then
+  source_region=$(jq -r ".\"${REGION}\".source_region" "${CLUSTER_PROFILE_DIR}/shift_project_setting.json")
+  REGION=$source_region
+fi
+
 echo "Deleting AWS CloudFormation stacks"
 
 stack_list="${SHARED_DIR}/to_be_removed_cf_stack_list"

ci-operator/step-registry/aws/provision/bastionhost/aws-provision-bastionhost-commands.sh
@@ -41,10 +41,12 @@ if [[ "${BASTION_HOST_AMI}" == "" ]]; then
   aws --region $REGION s3 mb "s3://${s3_bucket_name}"
   echo "s3://${s3_bucket_name}" > "$SHARED_DIR/to_be_removed_s3_bucket_list"
   aws --region $REGION s3 cp ${bastion_ignition_file} "${ign_location}"
+  echo "core" > "${SHARED_DIR}/bastion_ssh_user"
 else
   # use BYO bastion host
   ami_id=${BASTION_HOST_AMI}
   ign_location="NA"
+  echo "ec2-user" > "${SHARED_DIR}/bastion_ssh_user"
 fi
 
 echo -e "AMI ID: $ami_id"
@@ -269,7 +271,6 @@ BASTION_HOST_PRIVATE_DNS="$(aws --region "${REGION}" cloudformation describe-sta
 
 echo "${BASTION_HOST_PUBLIC_DNS}" > "${SHARED_DIR}/bastion_public_address"
 echo "${BASTION_HOST_PRIVATE_DNS}" > "${SHARED_DIR}/bastion_private_address"
-echo "core" > "${SHARED_DIR}/bastion_ssh_user"
 
 # echo proxy IP to ${SHARED_DIR}/proxyip
 echo "${BASTION_HOST_PUBLIC_DNS}" > "${SHARED_DIR}/proxyip"

ci-operator/step-registry/gather/aws-console/gather-aws-console-commands.sh
@@ -14,6 +14,8 @@ then
 	exit 0
 fi
 
+REGION="$(jq -r .aws.region "${SHARED_DIR}/metadata.json")"
+
 # while gathering logs from a private cluster, proxy setting is required for connecting cluster
 if test -f "${SHARED_DIR}/proxy-conf.sh"
 then
@@ -39,7 +41,17 @@ fi
 
 aws --version
 
-REGION="$(jq -r .aws.region "${SHARED_DIR}/metadata.json")"
+# Special setting for C2S/SC2S, running aws command in source region (us-east-1)
+if [ "${CLUSTER_TYPE}" == "aws-c2s" ] || [ "${CLUSTER_TYPE}" == "aws-sc2s" ]; then
+  source_region=$(jq -r ".\"${REGION}\".source_region" "${CLUSTER_PROFILE_DIR}/shift_project_setting.json")
+  REGION=$source_region
+  if [ ! -f "${SHARED_DIR}/unset-proxy.sh" ]; then
+    echo "ERROR, unset-proxy.sh does not exist, exit now."
+    exit 1
+  fi
+  source "${SHARED_DIR}/unset-proxy.sh"
+fi
+
 cat "${TMPDIR}/node-provider-IDs.txt" | sort | grep . | uniq | while read -r INSTANCE_ID
 do
 	echo "Gathering console logs for ${INSTANCE_ID}"

ci-operator/step-registry/gather/extra/gather-extra-commands.sh
@@ -550,14 +550,25 @@ queue ${ARTIFACT_DIR}/metrics/job_metrics.json oc --insecure-skip-tls-verify rsh
 
 wait
 
+# C2S/SC2S proxy can not access internet
+if [ "${CLUSTER_TYPE}" == "aws-c2s" ] || [ "${CLUSTER_TYPE}" == "aws-sc2s" ]; then
+  source "${SHARED_DIR}/unset-proxy.sh"
+fi
 # This is a temporary conversion of cluster operator status to JSON matching the upgrade - may be moved to code in the future
 curl -sL https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 >/tmp/jq && chmod ug+x /tmp/jq
+
 mkdir -p ${ARTIFACT_DIR}/junit/
 <${ARTIFACT_DIR}/clusteroperators.json /tmp/jq -r 'def one(condition; t): t as $t | first([.[] | select(condition)] | map(.type=t)[]) // null; def msg: "Operator \(.type) (\(.reason)): \(.message)"; def xmlfailure: if .failure then "<failure message=\"\(.failure | @html)\">\(.failure | @html)</failure>" else "" end; def xmltest: "<testcase name=\"\(.name | @html)\">\( xmlfailure )</testcase>"; def withconditions: map({name: "operator conditions \(.metadata.name)"} + ((.status.conditions // [{type:"Available",status: "False",message:"operator is not reporting conditions"}]) | (one(.type=="Available" and .status!="True"; "unavailable") // one(.type=="Degraded" and .status=="True"; "degraded") // one(.type=="Progressing" and .status=="True"; "progressing") // null) | if . then {failure: .|msg} else null end)); .items | withconditions | "<testsuite name=\"Operator results\" tests=\"\( length )\" failures=\"\( [.[] | select(.failure)] | length )\">\n\( [.[] | xmltest] | join("\n"))\n</testsuite>"' >${ARTIFACT_DIR}/junit/junit_install_status.xml
 
 # This is an experimental wiring of autogenerated failure detection.
 echo "Detect known failures from symptoms (experimental) ..."
 curl -f https://gist.githubusercontent.com/smarterclayton/03b50c8f9b6351b2d9903d7fb35b342f/raw/symptom.sh 2>/dev/null | bash -s ${ARTIFACT_DIR} > ${ARTIFACT_DIR}/junit/junit_symptoms.xml
+
+if test -f "${SHARED_DIR}/proxy-conf.sh"
+then
+    # shellcheck disable=SC1090
+    source "${SHARED_DIR}/proxy-conf.sh"
+fi
 
 # Create custom-link-tools.html from custom-links.txt
 REPORT="${ARTIFACT_DIR}/custom-link-tools.html"

ci-operator/step-registry/gather/must-gather/gather-must-gather-commands.sh
@@ -105,9 +105,26 @@ function installCamgi() {
     CAMGI_VERSION="0.8.0"
     pushd /tmp
 
+    # no internet access in C2S/SC2S env, disable proxy 
+    if [ "${CLUSTER_TYPE}" == "aws-c2s" ] || [ "${CLUSTER_TYPE}" == "aws-sc2s" ]; then
+      if [ ! -f "${SHARED_DIR}/unset-proxy.sh" ]; then
+        echo "ERROR, unset-proxy.sh does not exist."
+        return 1
+      fi
+      source "${SHARED_DIR}/unset-proxy.sh"
+    fi
+
     curl -L -o camgi.tar https://github.com/elmiko/camgi.rs/releases/download/v"$CAMGI_VERSION"/camgi-"$CAMGI_VERSION"-linux-x86_64.tar
     tar xvf camgi.tar
     sha256sum -c camgi.sha256
+
+    if [ "${CLUSTER_TYPE}" == "aws-c2s" ] || [ "${CLUSTER_TYPE}" == "aws-sc2s" ]; then
+      if [ ! -f "${SHARED_DIR}/proxy-conf.sh" ]; then
+        echo "ERROR, proxy-conf.sh does not exist."
+        return 1
+      fi
+      source "${SHARED_DIR}/proxy-conf.sh"
+    fi
 
     popd
 }

ci-operator/step-registry/ipi/conf/aws/ipi-conf-aws-commands.sh
@@ -12,14 +12,18 @@ expiration_date=$(date -d '8 hours' --iso=minutes --utc)
 
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
-REGION="${LEASED_RESOURCE}"
 
-# m6a (AMD) are more cost effective than other x86 instance types
-# for general purpose work. Use by default, when supported in the
-# region.
-IS_M6A_REGION="no"
-if aws ec2 describe-instance-type-offerings --region "${REGION}" | grep -q m6a ; then
-  IS_M6A_REGION="yes"
+# REGION: the region that OCP will be installed
+# aws_source_region: for non-C2S/SC2S cluster, it's the same as REGION, for C2S/SC2S it's the source region that emulator runs on.
+#             e.g. for instance, if installing a cluster on a C2S (us-iso-east-1) region and its emulator runs on us-east-1:
+#                  so the REGION is us-iso-east-1, and aws_source_region is us-east-1
+REGION="${LEASED_RESOURCE}"
+aws_source_region="${REGION}"
+
+if [[ "${CLUSTER_TYPE}" == "aws-c2s" ]] || [[ "${CLUSTER_TYPE}" == "aws-sc2s" ]]; then
+  # in C2S/SC2S use source_region (us-east-1) to communicate with AWS services
+  aws_source_region=$(jq -r ".\"${REGION}\".source_region" "${CLUSTER_PROFILE_DIR}/shift_project_setting.json")
+  echo "C2S/SC2S source region: $aws_source_region"
 fi
 
 function eval_instance_capacity() {
@@ -62,24 +66,6 @@ function eval_instance_capacity() {
 
 # BootstrapInstanceType gets its value from pkg/types/aws/defaults/platform.go
 architecture=${OCP_ARCH:-"amd64"}
-if [[ "${CLUSTER_TYPE}" == "aws-arm64" ]]; then
-  architecture="arm64"
-fi
-
-# Do not change auto-types unless it is coordinated with the cloud
-# financial operations team. Savings plans may be in place to
-# decrease the cost of certain instance families.
-if [[ "${COMPUTE_NODE_TYPE}" == "" ]]; then
-  if [ "${architecture}" = "arm64" ]; then
-    COMPUTE_NODE_TYPE="m6g.xlarge"
-  else
-    if [[ "${IS_M6A_REGION}" == "yes" ]]; then
-      COMPUTE_NODE_TYPE=$(eval_instance_capacity "m6a.xlarge" "m6i.xlarge")
-    else
-      COMPUTE_NODE_TYPE="m6i.xlarge"
-    fi
-  fi
-fi
 
 CONTROL_PLANE_INSTANCE_SIZE="xlarge"
 if [[ "${SIZE_VARIANT}" == "xlarge" ]]; then
@@ -90,10 +76,46 @@ elif [[ "${SIZE_VARIANT}" == "compact" ]]; then
   CONTROL_PLANE_INSTANCE_SIZE="2xlarge"
 fi
 
-if [[ x"${architecture}" == x"arm64" ]]; then
-  arch_instance_type=m6g
+if [[ "${CLUSTER_TYPE}" == "aws-c2s" ]] || [[ "${CLUSTER_TYPE}" == "aws-sc2s" ]]; then
+  # C2S & SC2S
+
+  # Not all instance family are supported by SHIFT emulator
+  #   see https://bugzilla.redhat.com/show_bug.cgi?id=2020181
+
+  if [[ "${COMPUTE_NODE_TYPE}" == "" ]]; then
+    COMPUTE_NODE_TYPE="m5.xlarge"
+  fi
+  arch_instance_type="m5"
+  CONTROL_PLANE_INSTANCE_TYPE="${arch_instance_type}.${CONTROL_PLANE_INSTANCE_SIZE}"
+elif [[ "${CLUSTER_TYPE}" == "aws-arm64" ]]; then
+  # ARM 64
+  architecture="arm64"
+  if [[ "${COMPUTE_NODE_TYPE}" == "" ]]; then
+    COMPUTE_NODE_TYPE="m6g.xlarge"
+  fi
+  arch_instance_type="m6g"
   CONTROL_PLANE_INSTANCE_TYPE="${arch_instance_type}.${CONTROL_PLANE_INSTANCE_SIZE}"
 else
+  # AMD 64
+  # m6a (AMD) are more cost effective than other x86 instance types
+  # for general purpose work. Use by default, when supported in the
+  # region.
+  IS_M6A_REGION="no"
+  if aws ec2 describe-instance-type-offerings --region "${REGION}" | grep -q m6a ; then
+    IS_M6A_REGION="yes"
+  fi
+
+  # Do not change auto-types unless it is coordinated with the cloud
+  # financial operations team. Savings plans may be in place to
+  # decrease the cost of certain instance families.
+  if [[ "${COMPUTE_NODE_TYPE}" == "" ]]; then
+    if [[ "${IS_M6A_REGION}" == "yes" ]]; then
+      COMPUTE_NODE_TYPE=$(eval_instance_capacity "m6a.xlarge" "m6i.xlarge")
+    else
+      COMPUTE_NODE_TYPE="m6i.xlarge"
+    fi
+  fi
+
   if [[ "${IS_M6A_REGION}" == "yes" ]]; then
     CONTROL_PLANE_INSTANCE_TYPE=$(eval_instance_capacity "m6a.${CONTROL_PLANE_INSTANCE_SIZE}" "m6i.${CONTROL_PLANE_INSTANCE_SIZE}")
   else
@@ -110,23 +132,23 @@ if [[ "${SIZE_VARIANT}" == "compact" ]]; then
 fi
 
 # Generate working availability zones from the region
-mapfile -t AVAILABILITY_ZONES < <(aws --region "${REGION}" ec2 describe-availability-zones | jq -r '.AvailabilityZones[] | select(.State == "available") | .ZoneName' | sort -u)
+mapfile -t AVAILABILITY_ZONES < <(aws --region "${aws_source_region}" ec2 describe-availability-zones | jq -r '.AvailabilityZones[] | select(.State == "available") | .ZoneName' | sort -u)
 # Generate availability zones with OpenShift Installer required instance types
 
 if [[ "${COMPUTE_NODE_TYPE}" == "${BOOTSTRAP_NODE_TYPE}" && "${COMPUTE_NODE_TYPE}" == "${CONTROL_PLANE_INSTANCE_TYPE}" ]]; then ## all regions are the same
-  mapfile -t INSTANCE_ZONES < <(aws --region "${REGION}" ec2 describe-instance-type-offerings --location-type availability-zone --filters Name=instance-type,Values="${BOOTSTRAP_NODE_TYPE}","${CONTROL_PLANE_INSTANCE_TYPE}","${COMPUTE_NODE_TYPE}" | jq -r '.InstanceTypeOfferings[].Location' | sort | uniq -c | grep ' 1 ' | awk '{print $2}')
+  mapfile -t INSTANCE_ZONES < <(aws --region "${aws_source_region}" ec2 describe-instance-type-offerings --location-type availability-zone --filters Name=instance-type,Values="${BOOTSTRAP_NODE_TYPE}","${CONTROL_PLANE_INSTANCE_TYPE}","${COMPUTE_NODE_TYPE}" | jq -r '.InstanceTypeOfferings[].Location' | sort | uniq -c | grep ' 1 ' | awk '{print $2}')
 elif [[ "${CONTROL_PLANE_INSTANCE_TYPE}" == null && "${COMPUTE_NODE_TYPE}" == null  ]]; then ## two null regions
-  mapfile -t INSTANCE_ZONES < <(aws --region "${REGION}" ec2 describe-instance-type-offerings --location-type availability-zone --filters Name=instance-type,Values="${BOOTSTRAP_NODE_TYPE}","${CONTROL_PLANE_INSTANCE_TYPE}","${COMPUTE_NODE_TYPE}" | jq -r '.InstanceTypeOfferings[].Location' | sort | uniq -c | grep ' 1 ' | awk '{print $2}')
+  mapfile -t INSTANCE_ZONES < <(aws --region "${aws_source_region}" ec2 describe-instance-type-offerings --location-type availability-zone --filters Name=instance-type,Values="${BOOTSTRAP_NODE_TYPE}","${CONTROL_PLANE_INSTANCE_TYPE}","${COMPUTE_NODE_TYPE}" | jq -r '.InstanceTypeOfferings[].Location' | sort | uniq -c | grep ' 1 ' | awk '{print $2}')
 elif [[ "${CONTROL_PLANE_INSTANCE_TYPE}" == null || "${COMPUTE_NODE_TYPE}" == null ]]; then ## one null region
   if [[ "${BOOTSTRAP_NODE_TYPE}" == "${COMPUTE_NODE_TYPE}" || "${BOOTSTRAP_NODE_TYPE}" == "${CONTROL_PLANE_INSTANCE_TYPE}" || "${CONTROL_PLANE_INSTANCE_TYPE}" == "${COMPUTE_NODE_TYPE}" ]]; then ## "one null region and duplicates"
-    mapfile -t INSTANCE_ZONES < <(aws --region "${REGION}" ec2 describe-instance-type-offerings --location-type availability-zone --filters Name=instance-type,Values="${BOOTSTRAP_NODE_TYPE}","${CONTROL_PLANE_INSTANCE_TYPE}","${COMPUTE_NODE_TYPE}" | jq -r '.InstanceTypeOfferings[].Location' | sort | uniq -c | grep ' 1 ' | awk '{print $2}')
+    mapfile -t INSTANCE_ZONES < <(aws --region "${aws_source_region}" ec2 describe-instance-type-offerings --location-type availability-zone --filters Name=instance-type,Values="${BOOTSTRAP_NODE_TYPE}","${CONTROL_PLANE_INSTANCE_TYPE}","${COMPUTE_NODE_TYPE}" | jq -r '.InstanceTypeOfferings[].Location' | sort | uniq -c | grep ' 1 ' | awk '{print $2}')
   else ## "one null region and no duplicates"
-    mapfile -t INSTANCE_ZONES < <(aws --region "${REGION}" ec2 describe-instance-type-offerings --location-type availability-zone --filters Name=instance-type,Values="${BOOTSTRAP_NODE_TYPE}","${CONTROL_PLANE_INSTANCE_TYPE}","${COMPUTE_NODE_TYPE}" | jq -r '.InstanceTypeOfferings[].Location' | sort | uniq -c | grep ' 2 ' | awk '{print $2}')
+    mapfile -t INSTANCE_ZONES < <(aws --region "${aws_source_region}" ec2 describe-instance-type-offerings --location-type availability-zone --filters Name=instance-type,Values="${BOOTSTRAP_NODE_TYPE}","${CONTROL_PLANE_INSTANCE_TYPE}","${COMPUTE_NODE_TYPE}" | jq -r '.InstanceTypeOfferings[].Location' | sort | uniq -c | grep ' 2 ' | awk '{print $2}')
   fi
 elif [[ "${BOOTSTRAP_NODE_TYPE}" == "${COMPUTE_NODE_TYPE}" || "${BOOTSTRAP_NODE_TYPE}" == "${CONTROL_PLANE_INSTANCE_TYPE}" || "${CONTROL_PLANE_INSTANCE_TYPE}" == "${COMPUTE_NODE_TYPE}" ]]; then ## duplicates regions with no null region
-  mapfile -t INSTANCE_ZONES < <(aws --region "${REGION}" ec2 describe-instance-type-offerings --location-type availability-zone --filters Name=instance-type,Values="${BOOTSTRAP_NODE_TYPE}","${CONTROL_PLANE_INSTANCE_TYPE}","${COMPUTE_NODE_TYPE}" | jq -r '.InstanceTypeOfferings[].Location' | sort | uniq -c | grep ' 2 ' | awk '{print $2}')
+  mapfile -t INSTANCE_ZONES < <(aws --region "${aws_source_region}" ec2 describe-instance-type-offerings --location-type availability-zone --filters Name=instance-type,Values="${BOOTSTRAP_NODE_TYPE}","${CONTROL_PLANE_INSTANCE_TYPE}","${COMPUTE_NODE_TYPE}" | jq -r '.InstanceTypeOfferings[].Location' | sort | uniq -c | grep ' 2 ' | awk '{print $2}')
 elif [[ "${BOOTSTRAP_NODE_TYPE}" != "${COMPUTE_NODE_TYPE}" && "${COMPUTE_NODE_TYPE}" != "${CONTROL_PLANE_INSTANCE_TYPE}" ]]; then   # three different regions
-  mapfile -t INSTANCE_ZONES < <(aws --region "${REGION}" ec2 describe-instance-type-offerings --location-type availability-zone --filters Name=instance-type,Values="${BOOTSTRAP_NODE_TYPE}","${CONTROL_PLANE_INSTANCE_TYPE}","${COMPUTE_NODE_TYPE}" | jq -r '.InstanceTypeOfferings[].Location' | sort | uniq -c | grep ' 3 ' | awk '{print $2}')
+  mapfile -t INSTANCE_ZONES < <(aws --region "${aws_source_region}" ec2 describe-instance-type-offerings --location-type availability-zone --filters Name=instance-type,Values="${BOOTSTRAP_NODE_TYPE}","${CONTROL_PLANE_INSTANCE_TYPE}","${COMPUTE_NODE_TYPE}" | jq -r '.InstanceTypeOfferings[].Location' | sort | uniq -c | grep ' 3 ' | awk '{print $2}')
 fi
 # Generate availability zones based on these 2 criteria
 mapfile -t ZONES < <(echo "${AVAILABILITY_ZONES[@]}" "${INSTANCE_ZONES[@]}" | sed 's/ /\n/g' | sort -R | uniq -d)
@@ -230,6 +252,12 @@ if [ "$REGION" == "us-gov-west-1" ] || [ "$REGION" == "us-gov-east-1" ] || [ "$R
   curl -sL https://raw.githubusercontent.com/yunjiang29/ocp-test-data/main/coreos-for-non-public-regions/images.json -o /tmp/ami.json
   RHCOS_AMI=$(jq -r .architectures.x86_64.images.aws.regions.\"${REGION}\".\"${ocp_version}\".image /tmp/ami.json)
   echo "RHCOS_AMI: $RHCOS_AMI, ocp_version: $ocp_version"
+fi
+
+if [[ "${CLUSTER_TYPE}" == "aws-c2s" ]] || [[ "${CLUSTER_TYPE}" == "aws-sc2s" ]]; then
+  jq --version
+  openshift-install version
+  RHCOS_AMI=$(openshift-install coreos print-stream-json | jq -r ".architectures.x86_64.images.aws.regions.\"${aws_source_region}\".image")
 fi
 
 if [ ! -z ${RHCOS_AMI} ]; then

ci-operator/step-registry/ipi/conf/mirror/ipi-conf-mirror-commands.sh
@@ -27,11 +27,18 @@ echo '{"auths":{}}' | jq --argjson a "{\"${MIRROR_REGISTRY_HOST}\": {\"auth\": \
 
 # Additional CA & pull secret patch
 CONFIG_PATCH="${SHARED_DIR}/pull_secret_ca.yaml.patch"
+
+additional_trust_bundle="${SHARED_DIR}/additional_trust_bundle"
+cat /var/run/vault/mirror-registry/client_ca.crt > "${additional_trust_bundle}"
+if [ X"${CLUSTER_TYPE}" == X"aws-c2s" ] || [ X"${CLUSTER_TYPE}" == X"aws-sc2s" ]; then
+  echo >> "${additional_trust_bundle}"
+  cat "${CLUSTER_PROFILE_DIR}/shift-ca-chain.cert.pem" >> "${additional_trust_bundle}"
+fi
 cat > "${CONFIG_PATCH}" << EOF
 pullSecret: >
   $(cat "${mirror_registry_pull_secret}" | jq -c .)
 additionalTrustBundle: |
-`sed 's/^/  /g' "/var/run/vault/mirror-registry/client_ca.crt"`
+`sed 's/^/  /g' "${additional_trust_bundle}"`
 EOF
 yq-go m -x -i "${CONFIG}" "${CONFIG_PATCH}"
 

commit 1e7e716fe4acc13b1ed5fc1fdf72f86a08c8ddb6

FIXME: Default $CLUSTER_TYPE for new (s)c2s conditions
This is a band-aid for an issue introduced by #28149 which added some
checks for a `$CLUSTER_TYPE` variable which apparently is unset in some
affected tests. This collides with `set -o nounset` (aka `set -u`) and
blows up, spuriously failing tests e.g. in post steps during
must-gather.

This commit changes files
- in which a `$CLUSTER_TYPE` check was introduced by #28149
- where `$CLUSTER_TYPE` was not previously referenced (because those
must have been fine, or they would have also been exploding tests)
...to default to the empty string if unset.

It also changes `[` style conditions, which spawn a subshell, to `[[`
style, which a) don't; and b) allow use of bash regexes to consolidate
the check into a single expression.

FIXME: The author of the aforementioned PR should validate these code
paths and make sure it is appropriate to default `$CLUSTER_TYPE`
therein; and/or make sure it gets set to an appropriate value, whereupon
the defaulting should be removed as it could cover up future cases where
that variable should be set, but isn't.

ci-operator/step-registry/aws/deprovision/s3buckets/aws-deprovision-s3buckets-commands.sh
@@ -10,7 +10,7 @@ export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 REGION="${LEASED_RESOURCE}"
 
 # Special setting for C2S/SC2S
-if [ "${CLUSTER_TYPE}" == "aws-c2s" ] || [ "${CLUSTER_TYPE}" == "aws-sc2s" ]; then
+if [[ "${CLUSTER_TYPE:-}" =~ ^aws-s?c2s$ ]]; then
   source_region=$(jq -r ".\"${REGION}\".source_region" "${CLUSTER_PROFILE_DIR}/shift_project_setting.json")
   REGION=$source_region
 fi

ci-operator/step-registry/aws/deprovision/stacks/aws-deprovision-stacks-commands.sh
@@ -10,7 +10,7 @@ export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 REGION="${LEASED_RESOURCE}"
 
 # Special setting for C2S/SC2S
-if [ "${CLUSTER_TYPE}" == "aws-c2s" ] || [ "${CLUSTER_TYPE}" == "aws-sc2s" ]; then
+if [[ "${CLUSTER_TYPE:-}" =~ ^aws-s?c2s$ ]]; then
   source_region=$(jq -r ".\"${REGION}\".source_region" "${CLUSTER_PROFILE_DIR}/shift_project_setting.json")
   REGION=$source_region
 fi

ci-operator/step-registry/aws/provision/bastionhost/aws-provision-bastionhost-commands.sh
@@ -11,7 +11,7 @@ export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 REGION="${LEASED_RESOURCE}"
 
 # Using source region for C2S and SC2S
-if [[ "${CLUSTER_TYPE}" == "aws-c2s" ]] || [[ "${CLUSTER_TYPE}" == "aws-sc2s" ]]; then
+if [[ "${CLUSTER_TYPE:-}" =~ ^aws-s?c2s$ ]]; then
   REGION=$(jq -r ".\"${LEASED_RESOURCE}\".source_region" "${CLUSTER_PROFILE_DIR}/shift_project_setting.json")
 fi
 

ci-operator/step-registry/gather/aws-console/gather-aws-console-commands.sh
@@ -42,7 +42,7 @@ fi
 aws --version
 
 # Special setting for C2S/SC2S, running aws command in source region (us-east-1)
-if [ "${CLUSTER_TYPE}" == "aws-c2s" ] || [ "${CLUSTER_TYPE}" == "aws-sc2s" ]; then
+if [[ "${CLUSTER_TYPE:-}" =~ ^aws-s?c2s$ ]]; then
   source_region=$(jq -r ".\"${REGION}\".source_region" "${CLUSTER_PROFILE_DIR}/shift_project_setting.json")
   REGION=$source_region
   if [ ! -f "${SHARED_DIR}/unset-proxy.sh" ]; then

ci-operator/step-registry/gather/extra/gather-extra-commands.sh
@@ -551,7 +551,7 @@ queue ${ARTIFACT_DIR}/metrics/job_metrics.json oc --insecure-skip-tls-verify rsh
 wait
 
 # C2S/SC2S proxy can not access internet
-if [ "${CLUSTER_TYPE}" == "aws-c2s" ] || [ "${CLUSTER_TYPE}" == "aws-sc2s" ]; then
+if [[ "${CLUSTER_TYPE:-}" =~ ^aws-s?c2s$ ]]; then
   source "${SHARED_DIR}/unset-proxy.sh"
 fi
 # This is a temporary conversion of cluster operator status to JSON matching the upgrade - may be moved to code in the future

ci-operator/step-registry/gather/must-gather/gather-must-gather-commands.sh
@@ -106,7 +106,7 @@ function installCamgi() {
     pushd /tmp
 
     # no internet access in C2S/SC2S env, disable proxy 
-    if [ "${CLUSTER_TYPE}" == "aws-c2s" ] || [ "${CLUSTER_TYPE}" == "aws-sc2s" ]; then
+    if [[ "${CLUSTER_TYPE:-}" =~ ^aws-s?c2s$ ]]; then
       if [ ! -f "${SHARED_DIR}/unset-proxy.sh" ]; then
         echo "ERROR, unset-proxy.sh does not exist."
         return 1
@@ -118,7 +118,7 @@ function installCamgi() {
     tar xvf camgi.tar
     sha256sum -c camgi.sha256
 
-    if [ "${CLUSTER_TYPE}" == "aws-c2s" ] || [ "${CLUSTER_TYPE}" == "aws-sc2s" ]; then
+    if [[ "${CLUSTER_TYPE:-}" =~ ^aws-s?c2s$ ]]; then
       if [ ! -f "${SHARED_DIR}/proxy-conf.sh" ]; then
         echo "ERROR, proxy-conf.sh does not exist."
         return 1

ci-operator/step-registry/ipi/conf/aws/ipi-conf-aws-commands.sh
@@ -20,7 +20,7 @@ function join_by { local IFS="$1"; shift; echo "$*"; }
 REGION="${LEASED_RESOURCE}"
 aws_source_region="${REGION}"
 
-if [[ "${CLUSTER_TYPE}" == "aws-c2s" ]] || [[ "${CLUSTER_TYPE}" == "aws-sc2s" ]]; then
+if [[ "${CLUSTER_TYPE}" =~ ^aws-s?c2s$ ]]; then
   # in C2S/SC2S use source_region (us-east-1) to communicate with AWS services
   aws_source_region=$(jq -r ".\"${REGION}\".source_region" "${CLUSTER_PROFILE_DIR}/shift_project_setting.json")
   echo "C2S/SC2S source region: $aws_source_region"
@@ -76,7 +76,7 @@ elif [[ "${SIZE_VARIANT}" == "compact" ]]; then
   CONTROL_PLANE_INSTANCE_SIZE="2xlarge"
 fi
 
-if [[ "${CLUSTER_TYPE}" == "aws-c2s" ]] || [[ "${CLUSTER_TYPE}" == "aws-sc2s" ]]; then
+if [[ "${CLUSTER_TYPE}" =~ ^aws-s?c2s$ ]]; then
   # C2S & SC2S
 
   # Not all instance family are supported by SHIFT emulator
@@ -254,7 +254,7 @@ if [ "$REGION" == "us-gov-west-1" ] || [ "$REGION" == "us-gov-east-1" ] || [ "$R
   echo "RHCOS_AMI: $RHCOS_AMI, ocp_version: $ocp_version"
 fi
 
-if [[ "${CLUSTER_TYPE}" == "aws-c2s" ]] || [[ "${CLUSTER_TYPE}" == "aws-sc2s" ]]; then
+if [[ "${CLUSTER_TYPE}" =~ ^aws-s?c2s$ ]]; then
   jq --version
   openshift-install version
   RHCOS_AMI=$(openshift-install coreos print-stream-json | jq -r ".architectures.x86_64.images.aws.regions.\"${aws_source_region}\".image")

ci-operator/step-registry/ipi/conf/mirror/ipi-conf-mirror-commands.sh
@@ -30,7 +30,7 @@ CONFIG_PATCH="${SHARED_DIR}/pull_secret_ca.yaml.patch"
 
 additional_trust_bundle="${SHARED_DIR}/additional_trust_bundle"
 cat /var/run/vault/mirror-registry/client_ca.crt > "${additional_trust_bundle}"
-if [ X"${CLUSTER_TYPE}" == X"aws-c2s" ] || [ X"${CLUSTER_TYPE}" == X"aws-sc2s" ]; then
+if [[ "${CLUSTER_TYPE:-}" =~ ^aws-s?c2s$ ]]; then
   echo >> "${additional_trust_bundle}"
   cat "${CLUSTER_PROFILE_DIR}/shift-ca-chain.cert.pem" >> "${additional_trust_bundle}"
 fi

ci-operator/step-registry/ipi/deprovision/deprovision/ipi-deprovision-deprovision-commands.sh
@@ -39,7 +39,7 @@ fi
 echo "Copying the installation artifacts to the Installer's asset directory..."
 cp -ar "${SHARED_DIR}" /tmp/installer
 
-if [[ "${CLUSTER_TYPE}" == "aws-c2s" ]] || [[ "${CLUSTER_TYPE}" == "aws-sc2s" ]]; then
+if [[ "${CLUSTER_TYPE}" =~ ^aws-s?c2s$ ]]; then
   # C2S/SC2S regions do not support destory
   #   replace ${AWS_REGION} with source_region(us-east-1) in metadata.json as a workaround"
   
@@ -60,7 +60,7 @@ fi
 
 # Check if proxy is set
 if test -f "${SHARED_DIR}/proxy-conf.sh"; then
-  if [[ "${CLUSTER_TYPE}" == "aws-c2s" ]] || [[ "${CLUSTER_TYPE}" == "aws-sc2s" ]]; then
+  if [[ "${CLUSTER_TYPE}" =~ ^aws-s?c2s$ ]]; then
     echo "proxy-conf.sh detected, but not reqquired by C2S/SC2S while destroying cluster, skip proxy setting"
   else
     echo "Private cluster setting proxy"

ci-operator/step-registry/ipi/install/install/aws/ipi-install-install-aws-commands.sh
@@ -95,7 +95,7 @@ esac
 
 
 # set CA_BUNDLE for C2S and SC2S 
-if [ "${CLUSTER_TYPE}" == "aws-c2s" ] || [ "${CLUSTER_TYPE}" == "aws-sc2s" ]; then
+if [[ "${CLUSTER_TYPE}" =~ ^aws-s?c2s$ ]]; then
   export AWS_CA_BUNDLE=${SHARED_DIR}/additional_trust_bundle
 fi
 