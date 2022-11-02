18333c34c5bb51be2a7577afeaa4037a893e88fb
Yanping Zhang <yanpzhan@redhat.com>
2022-11-02 18:30:35 +0800

    update console script for hypershift guest case (#33425)

--- ci-operator/step-registry/openshift-extended/web-tests/openshift-extended-web-tests-commands.sh
+++ ci-operator/step-registry/openshift-extended/web-tests/openshift-extended-web-tests-commands.sh
@@ -6,4 +6,13 @@ fi
 
 pwd && ls -ltr
 cd frontend || exit 0
-./console-test-frontend.sh || exit 0
+pwd && ls -ltr
+res=0
+oc get node --kubeconfig=${KUBECONFIG} | grep master || res=$?
+if [ $res -eq 0 ] ; then
+    echo "Testing on normal cluster"
+    ./console-test-frontend.sh || exit 0
+else
+    echo "Testing on hypershift guest cluster"
+    ./console-test-frontend-hypershift.sh || exit 0
+fi
