96e06e3009a0e5f53324fd0868ecf66552b77a5d
Nikolaos Moraitis <nmoraiti@redhat.com>
2022-11-03 14:28:11 +0200

    Automatically generate all multi-arch namespaces for image-mirroring (#33722)
    * automatically generate all multi-arch namespaces for image-mirroring
    
    Signed-off-by: Nikolaos Moraitis <nmoraiti@redhat.com>
    
    * regenerate image-mirroring configuration for multi-arch
    
    Signed-off-by: Nikolaos Moraitis <nmoraiti@redhat.com>
    
    Signed-off-by: Nikolaos Moraitis <nmoraiti@redhat.com>

--- hack/image-mirroring/supplemental_ci_images_mirror_gen.py
+++ hack/image-mirroring/supplemental_ci_images_mirror_gen.py
@@ -24,6 +24,9 @@ Note that this folder is automatically generated.
 """
 
 
+NAMESPACES_FILE = "clusters/app.ci/multi-arch/namespaces.yaml"
+
+
 def generate_job(arch, os_filter):
     return f"""
 agent: kubernetes
@@ -70,7 +73,22 @@ spec:
 """
 
 
+def generate_namespaces(namespaces):
+    ret = []
+    for namespace in namespaces:
+        metadata = {"name": namespace}
+        ret.append({
+            "apiVersion": "v1",
+            "kind": "Namespace",
+            "metadata": metadata
+        })
+    return {"kind": "List",
+            "apiVersion": "v1",
+            "items": ret}
+
+
 def generate_mappings():
+    namespaces = []
     print("Removing all core-services/image-mirroring-* directories")
     for path in glob.glob('core-services/image-mirroring-*', recursive=False):
         try:
@@ -119,12 +137,19 @@ def generate_mappings():
 
                         _, namespace, image_name = dest.split("/")
                         namespace = f"{namespace}-{arch}"
+                        namespaces.append(namespace)
 
                         new_dest = f"{APPCI_REGISTRY}/{namespace}/{image_name}"
 
                         print(f"Writing to {to_write}")
                         with open(to_write, 'a+', encoding="utf-8") as f:
                             f.write(f"{source} {new_dest}\n")
+
+    if not os.path.exists(os.path.dirname(NAMESPACES_FILE)):
+        os.makedirs(os.path.dirname(NAMESPACES_FILE))
+    with open(NAMESPACES_FILE, 'w', encoding="utf-8") as file:
+        yaml.round_trip_dump(generate_namespaces(list(dict.fromkeys(namespaces))), file, indent=2,
+                             default_flow_style=False, explicit_start=False)
 
 
 def generate_periodics():
