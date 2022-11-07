7eaf5a0b8e6ee95154c982fed3ff907a1c429772
Alessandro Di Stefano <aleskandro@redhat.com>
2022-11-07 14:08:07 +0100

    [Baremetal QE] Adds reserve/release nodes step (#33668)

--- ci-operator/step-registry/baremetal/lab/post/release-nodes/baremetal-lab-post-release-nodes-commands.sh
+++ ci-operator/step-registry/baremetal/lab/post/release-nodes/baremetal-lab-post-release-nodes-commands.sh
@@ -1,2 +1,52 @@
-#!/bin/sh
-echo Hello world
+#!/bin/bash
+
+if [ -n "${LOCAL_TEST}" ]; then
+  # Setting LOCAL_TEST to any value will allow testing this script with default values against the ARM64 bastion @ RDU2
+  # Also needs SHARED_DIR to be set appropriately (e.g., to the same dir used by the pre-reserve-nodes step tested earlier)
+  export AUX_HOST=openshift-qe-bastion.arm.eng.rdu2.redhat.com
+  # shellcheck disable=SC2155
+  export NAMESPACE=test-ci-op CLUSTER_PROFILE_DIR=~/.ssh
+fi
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+SSHOPTS=(-o 'ConnectTimeout=5'
+  -o 'StrictHostKeyChecking=no'
+  -o 'UserKnownHostsFile=/dev/null'
+  -o 'ServerAliveInterval=90'
+  -o LogLevel=ERROR
+  -i "${CLUSTER_PROFILE_DIR}/packet-ssh-key")
+
+CLUSTER_NAME=$(<"${SHARED_DIR}/cluster_name")
+
+timeout -s 9 15m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" bash -s -- "$CLUSTER_NAME" << 'EOF'
+BUILD_USER=ci-op
+BUILD_ID="$1"
+
+LOCK="/tmp/reserved_file.lock"
+LOCK_FD=200
+touch $LOCK
+exec 200>$LOCK
+
+set -e
+trap catch_exit ERR INT
+
+function catch_exit {
+  echo "Error. Releasing lock $LOCK_FD ($LOCK)"
+  flock -u $LOCK_FD
+  exit 1
+}
+
+echo "Acquiring lock $LOCK_FD ($LOCK) (waiting up to 10 minutes)"
+flock -w 600 $LOCK_FD
+echo "Lock acquired $LOCK_FD ($LOCK)"
+
+sed -i "/,${BUILD_ID},${BUILD_USER},/d" /etc/hosts_pool_reserved
+sed -i "/,${BUILD_ID},${BUILD_USER},/d" /etc/vips_reserved
+
+echo "Releasing lock $LOCK_FD ($LOCK)"
+flock -u $LOCK_FD
+
+EOF
--- ci-operator/step-registry/baremetal/lab/pre/reserve-nodes/baremetal-lab-pre-reserve-nodes-commands.sh
+++ ci-operator/step-registry/baremetal/lab/pre/reserve-nodes/baremetal-lab-pre-reserve-nodes-commands.sh
@@ -1,2 +1,50 @@
-#!/bin/sh
-echo Hello world
+#!/bin/bash
+
+if [ -n "${LOCAL_TEST}" ]; then
+  # Setting LOCAL_TEST to any value will allow testing this script with default values against the ARM64 bastion @ RDU2
+  export N_MASTERS=1 N_WORKERS=1 IPI=true AUX_HOST=openshift-qe-bastion.arm.eng.rdu2.redhat.com
+  # shellcheck disable=SC2155
+  export NAMESPACE=test-ci-op SHARED_DIR=$(mktemp -d) CLUSTER_PROFILE_DIR=~/.ssh
+fi
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+SSHOPTS=(-o 'ConnectTimeout=5'
+  -o 'StrictHostKeyChecking=no'
+  -o 'UserKnownHostsFile=/dev/null'
+  -o 'ServerAliveInterval=90'
+  -o LogLevel=ERROR
+  -i "${CLUSTER_PROFILE_DIR}/ssh-key")
+
+# The hostname of nodes and the cluster names have limited length for BM.
+# Other profiles add to the cluster_name the suffix "-${JOB_NAME_HASH}".
+echo "${NAMESPACE}" > "${SHARED_DIR}/cluster_name"
+
+timeout -s 9 180m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" bash -s -- \
+  "${NAMESPACE}" "${N_MASTERS}" "${N_WORKERS}" "${IPI}" << 'EOF'
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o allexport
+
+BUILD_USER=ci-op
+BUILD_ID="${1}"
+N_MASTERS="${2}"
+N_WORKERS="${3}"
+IPI="${4}"
+set +o allexport
+
+# shellcheck disable=SC2174
+mkdir -m 755 -p {/var/builds,/opt/tftpboot,/opt/html}/${BUILD_ID}
+touch /etc/{hosts_pool_reserved,vips_reserved}
+# The current implementation of the following scripts is different based on the auxiliary host. Keeping the script in
+# the remote aux servers temporarily.
+bash /usr/local/bin/reserve_hosts.sh
+bash /usr/local/bin/reserve_vips.sh
+EOF
+
+scp "${SSHOPTS[@]}" "root@${AUX_HOST}:/var/builds/${NAMESPACE}/*.yaml" "${SHARED_DIR}/"
+
+more "${SHARED_DIR}"/* |& sed 's/pass.*$/pass ** HIDDEN **/g'
