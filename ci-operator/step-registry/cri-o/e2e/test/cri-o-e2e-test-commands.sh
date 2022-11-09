7ad0f8297e7400a4407e04b6af6fd8fa6b9d9cfb
Skyler Clark <skylerjaneclark@gmail.com>
2022-11-09 15:02:15 -0500

    consolidates gather step

--- ci-operator/step-registry/cri-o/e2e/test/cri-o-e2e-test-commands.sh
+++ ci-operator/step-registry/cri-o/e2e/test/cri-o-e2e-test-commands.sh
@@ -5,29 +5,8 @@ set -o pipefail
 
 # shellcheck source=/dev/null
 source "${SHARED_DIR}/env"
-
-#####################################
-###############Log In################
-#####################################
-
-GOOGLE_PROJECT_ID="$(< ${CLUSTER_PROFILE_DIR}/openshift_gcp_project)"
-export GCP_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/gce.json"
-sa_email=$(jq -r .client_email ${GCP_SHARED_CREDENTIALS_FILE})
-if ! gcloud auth list | grep -E "\*\s+${sa_email}"
-then
-  gcloud auth activate-service-account --key-file="${GCP_SHARED_CREDENTIALS_FILE}"
-  gcloud config set project "${GOOGLE_PROJECT_ID}"
-fi
-
-mkdir -p "${HOME}"/.ssh
-chmod 0700 "${HOME}"/.ssh
-
-cp "${CLUSTER_PROFILE_DIR}"/ssh-privatekey "${HOME}"/.ssh/google_compute_engine
-chmod 0600 "${HOME}"/.ssh/google_compute_engine
-cp "${CLUSTER_PROFILE_DIR}"/ssh-publickey "${HOME}"/.ssh/google_compute_engine.pub
-
-#####################################
-#####################################
+chmod +x ${SHARED_DIR}/login_script.sh
+${SHARED_DIR}/login_script.sh
 
 instance_name=$(<"${SHARED_DIR}/gcp-instance-ids.txt")
 USE_CONMONRS=${USE_CONMONRS:-false}
