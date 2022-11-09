a7749fb97400095418320e8ec91d4bacf7e3bfc2
Skyler Clark <skylerjaneclark@gmail.com>
2022-11-09 12:04:35 -0500

    tries to clean up sh files

--- ci-operator/step-registry/cri-o/cgroupv2/e2e/test/cri-o-cgroupv2-e2e-test-commands.sh
+++ ci-operator/step-registry/cri-o/cgroupv2/e2e/test/cri-o-cgroupv2-e2e-test-commands.sh
@@ -31,27 +31,9 @@ cp "${CLUSTER_PROFILE_DIR}"/ssh-publickey "${HOME}"/.ssh/google_compute_engine.p
 
 instance_name=$(<"${SHARED_DIR}/gcp-instance-ids.txt")
 
-tar -czf - . | gcloud compute ssh --zone="${ZONE}" ${instance_name} -- "cat > \${HOME}/cri-o.tar.gz"
 timeout --kill-after 10m 400m gcloud compute ssh --zone="${ZONE}" ${instance_name} -- bash - << EOF 
-    export GOROOT=/usr/local/go
-    echo GOROOT="/usr/local/go" | sudo tee -a /etc/environment
-    mkdir -p \${HOME}/logs/artifacts
-    mkdir -p /tmp/artifacts/logs
-
-    sudo dnf install python39 -y
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python3.9 get-pip.py
-    python3.9 -m pip install ansible
-
-    # setup the directory where the tests will the run
     REPO_DIR="/home/deadbeef/cri-o"
-    mkdir -p "\${REPO_DIR}"
-    # copy the agent sources on the remote machine
-    tar -xzf cri-o.tar.gz -C "\${REPO_DIR}"
     cd "\${REPO_DIR}/contrib/test/ci"
-    echo "localhost" >> hosts
     ansible-playbook e2e-main.yml -i hosts -e "TEST_AGENT=prow" --connection=local -vvv --tags setup,e2e --extra-vars "cgroupv2=True"
-    sudo chown -R deadbeef /tmp/artifacts/*
-    sudo chmod -R 777 /tmp/artifacts/*
 EOF
 
