7fba9a674ff9f58448b3b2b7f40cbbe86ff88dfb
Liang Xia <lxia@redhat.com>
2022-11-11 15:41:39 +0800

    Make sure version info not null before write to file (#33953)

--- ci-operator/step-registry/cucushift/helper-service/build-info/cucushift-helper-service-build-info-commands.sh
+++ ci-operator/step-registry/cucushift/helper-service/build-info/cucushift-helper-service-build-info-commands.sh
@@ -4,7 +4,13 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+TMPFILE='/tmp/tmp-build-info.tmp'
+
 echo "Release info for: ${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE}"
 oc adm release info "${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE}" || true
-PAYLOAD_VERSION="$(oc adm release info "${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE}" --output=jsonpath="{.metadata.version}" || true)"
-echo "${PAYLOAD_VERSION}" > "${ARTIFACT_DIR}/ocp-build-info"
+PAYLOAD_VERSION="$(oc adm release info "${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE}" --output=jsonpath="{.metadata.version}" 2>"$TMPFILE" || true)"
+if [[ -n "${PAYLOAD_VERSION}" ]]; then
+    echo "${PAYLOAD_VERSION}" > "${ARTIFACT_DIR}/ocp-build-info"
+fi
+
+cat "$TMPFILE"
