d941cd41ee6437ca24dce36aace9112698fd669e
Giles Hutton <ghutton@redhat.com>
2022-11-14 11:24:52 +0000

    StackRox Collector Release 3.12 (#34001)
    * StackRox Collector Release 3.12
    
    * Adds missing release-3.12 owners file

---
+++ ci-operator/step-registry/stackrox/collector/release-3.12/integration-tests/stackrox-collector-release-3.12-integration-tests-commands.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+source .openshift-ci/jobs/integration-tests/env.sh
+
+env
+
+.openshift-ci/scripts/gcloud-init.sh
+.openshift-ci/jobs/integration-tests/run-integration-tests.sh
