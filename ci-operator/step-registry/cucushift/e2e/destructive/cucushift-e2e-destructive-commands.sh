bf1bf1a6bca3b1137e24ecfdac26e0e3d106d1a6
Liang Xia <lxia@redhat.com>
2022-11-15 13:37:04 +0800

    Let cucushift-e2e-report do the report (#34017)

--- ci-operator/step-registry/cucushift/e2e/destructive/cucushift-e2e-destructive-commands.sh
+++ ci-operator/step-registry/cucushift/e2e/destructive/cucushift-e2e-destructive-commands.sh
@@ -30,8 +30,6 @@ echo "Summarizing test result..."
 failures=$(grep '<testsuite failures="[1-9].*"' "${ARTIFACT_DIR}" -r | wc -l || true)
 if [ $((failures)) == 0 ]; then
     echo "All tests have passed"
-    exit 0
 else
     echo "There are ${failures} test failures"
-    exit 1
 fi
