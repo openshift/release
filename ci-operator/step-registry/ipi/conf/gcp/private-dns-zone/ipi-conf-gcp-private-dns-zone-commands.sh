0dcdd1639168d013394ea2e0bd4144f8ce9d7301
Jianli Wei <jiwei@redhat.com>
2022-11-15 19:17:38 +0800

    GCP IPI XPN (#33928)

---
+++ ci-operator/step-registry/ipi/conf/gcp/private-dns-zone/ipi-conf-gcp-private-dns-zone-commands.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+CONFIG="${SHARED_DIR}/install-config.yaml"
+PATCH="${SHARED_DIR}/install-config-patch.yaml"
+
+CLUSTER_NAME=${NAMESPACE}-${JOB_NAME_HASH}
+CLUSTER_PVTZ_PROJECT="$(< ${SHARED_DIR}/cluster-pvtz-project)"
+
+cat > "${PATCH}" << EOF
+platform:
+  gcp:
+    privateDNSZone: 
+      id: ${CLUSTER_NAME}-private-zone
+      project: ${CLUSTER_PVTZ_PROJECT}
+EOF
+yq-go m -x -i "${CONFIG}" "${PATCH}"
+echo "Updated platform.gcp.privateDNSZone in '${CONFIG}'."
+
+echo "(debug)--------------------"
+yq-go r "${CONFIG}" platform
+echo "(debug)--------------------"
