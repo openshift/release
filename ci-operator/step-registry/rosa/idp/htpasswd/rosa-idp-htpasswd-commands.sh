f1da15a56b6124302546046d40f604bb1a485bc4
yasun <yasun@redhat.com>
2022-11-17 19:07:54 +0800

    remove the ocm jobs from the 4.12 stable periodics list
    revert incorrect change

--- ci-operator/step-registry/rosa/idp/htpasswd/rosa-idp-htpasswd-commands.sh
+++ ci-operator/step-registry/rosa/idp/htpasswd/rosa-idp-htpasswd-commands.sh
@@ -39,7 +39,7 @@ fi
 # is over 10 minutes, so we give a loop to wait for the configuration to be active before timeout. 
 echo "Config htpasswd idp ..."
 IDP_USER="rosa-admin"
-IDP_PASSWD=$(openssl rand -base64 15)
+IDP_PASSWD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 15)
 rosa create idp -c ${CLUSTER_ID} \
                 -y \
                 --type htpasswd \
