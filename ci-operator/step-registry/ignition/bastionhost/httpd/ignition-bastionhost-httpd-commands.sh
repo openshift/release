7fe9da59654caa1bfab8fc752bb83a4a99d16e18
wwei <wwei@redhat.com>
2022-11-17 23:44:42 +0800

    debug vsphere

---
+++ ci-operator/step-registry/ignition/bastionhost/httpd/ignition-bastionhost-httpd-commands.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
+
+workdir=`mktemp -d`
+
+CLUSTER_NAME="${NAMESPACE}-${JOB_NAME_HASH}"
+bastion_ignition_file="${SHARED_DIR}/${CLUSTER_NAME}-bastion.ign"
+
+function patch_ignition_file()
+{
+  local base_ignition=$1
+  local patch_ignition=$2
+  t=$(mktemp)
+  # jq deepmerge 
+  # https://stackoverflow.com/questions/53661930/jq-recursively-merge-objects-and-concatenate-arrays
+  jq -s 'def deepmerge(a;b):
+  reduce b[] as $item (a;
+    reduce ($item | keys_unsorted[]) as $key (.;
+      $item[$key] as $val | ($val | type) as $type | .[$key] = if ($type == "object") then
+        deepmerge({}; [if .[$key] == null then {} else .[$key] end, $val])
+      elif ($type == "array") then
+        (.[$key] + $val | unique)
+      else
+        $val
+      end)
+    );
+  deepmerge({}; .)' "${base_ignition}" "${patch_ignition}" > "${t}"
+  mv "${t}" "${base_ignition}"
+  rm -f "${t}"
+}
+
+# httpd servcie
+httpd_service_file="${workdir}/httpd_service_file"
+cat > "${httpd_service_file}" << EOF
+[Unit]
+Description=HTTPD 2.4 Server
+After=network.target syslog.target
+
+[Service]
+Type=simple
+TimeoutStartSec=5m
+ExecStartPre=-/usr/bin/podman rm "httpd-8080"
+ExecStartPre=sh -c '/usr/bin/mkdir -p /var/www/html/; restorecon -r /var/www'
+
+ExecStart=/usr/bin/podman run --name "httpd-8080" -p 8080:8080 -p 80:8080 -v /var/www/html/:/var/www/html/ quay.io/openshifttest/httpd-24@sha256:8e16b154e440efb7b09d4322b8cb76dace09aeb068a7dcab4a414ed8958c8c39
+
+ExecReload=-/usr/bin/podman stop "httpd-8080"
+ExecReload=-/usr/bin/podman rm "httpd-8080"
+ExecStop=-/usr/bin/podman stop "httpd-8080"
+Restart=always
+RestartSec=30
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+HTTPD_SERVICE_CONTENT=$(sed ':a;N;$!ba;s/\n/\\n/g' "${httpd_service_file}" | sed 's/\"/\\"/g')
+
+# httpd ignition
+
+echo "Generate ignition config for fcos bastion host."
+
+cat > "${bastion_ignition_file}" << EOF
+{
+  "ignition": {
+    "config": {},
+    "security": {
+      "tls": {}
+    },
+    "timeouts": {},
+    "version": "3.0.0"
+  },
+  "passwd": {
+    "users": [
+      {
+        "name": "core",
+        "sshAuthorizedKeys": []
+      }
+    ]
+  },
+  "storage": {
+    "files": [
+    ],
+    "directories": [
+    ]
+  },
+  "systemd": {
+    "units": [
+      {
+        "enabled": false,
+        "mask": true,
+        "name": "zincati.service"
+      },
+      {
+        "contents": "${HTTPD_SERVICE_CONTENT}",
+        "enabled": true,
+        "name": "httpd.service"
+
+      }
+    ]
+  }
+}
+EOF
---
+++ ci-operator/step-registry/ipi/conf/httpd/ipi-conf-httpd-command.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+http_path="/var/www/html/"
+vsphere_rhcos_image_location=$(openshift-install coreos print-stream-json | jq -r .architectures.x86_64.artifacts.vmware.formats.ova.disk.location)
+vsphere_rhcos_image=`basename ${vsphere_rhcos_image_location}`
+
+SSH_PRIV_KEY_PATH="${CLUSTER_PROFILE_DIR}/ssh-privatekey"
+BASTION_IP=$(<"${SHARED_DIR}/bastion_private_address")
+BASTION_SSH_USER=$(<"${SHARED_DIR}/bastion_ssh_user")
+
+ssh -i "${SSH_PRIV_KEY_PATH}" -o UserKnownHostsFile=/dev/null \
+        -o StrictHostKeyChecking=no  ${BASTION_SSH_USER}@${BASTION_IP}  "sudo rm -rf ${http_path}${vsphere_rhcos_image} && sudo curl -Lk -o '${http_path}/${vsphere_rhcos_image}' '${vsphere_rhcos_image_location}'"
+
+CONFIG="${SHARED_DIR}/install-config.yaml"
+/tmp/yq -i '.platform.vsphere.clusterOSImage="http://${BASTION_IP}:80/${vsphere_rhcos_image}"' "${SHARED_DIR}/install-config.yaml"
+
+status=$?
+if [ X"$status" == X"0" ]; then
+  echo "${vsphere_rhcos_image}"
+else
+  exit $status
+fi
+
