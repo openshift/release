d789da683b1559b4219eb9b9acdba1b44221132d
deepsm007 <dmistry@redhat.com>
2022-11-29 11:35:47 -0500

    Collecting cluster data for analysis

--- ci-operator/step-registry/ipi/install/install/ipi-install-install-commands.sh
+++ ci-operator/step-registry/ipi/install/install/ipi-install-install-commands.sh
@@ -385,6 +385,53 @@ if test "${ret}" -eq 0 ; then
   touch  "${SHARED_DIR}/success"
   # Save console URL in `console.url` file so that ci-chat-bot could report success
   echo "https://$(env KUBECONFIG=${dir}/auth/kubeconfig oc -n openshift-console get routes console -o=jsonpath='{.spec.host}')" > "${SHARED_DIR}/console.url"
+
+  echo "Collecting cluster data for analysis..."
+  set +o errexit
+  set +o pipefail
+  if [ ! -f /tmp/jq ]; then
+    curl -L https://stedolan.github.io/jq/download/linux64/jq -o /tmp/jq && chmod +x /tmp/jq
+  fi
+  if ! pip -V; then
+    echo "pip is not installed: installing"
+    if python -c "import sys; assert(sys.version_info >= (3,0))"; then
+      python -m ensurepip --user || easy_install --user 'pip'
+    else
+      echo "python < 3, installing pip<21"
+      python -m ensurepip --user || easy_install --user 'pip<21'
+    fi
+  fi
+  echo "Installing python modules: json"
+  python3 -c "import json" || pip3 install --user pyjson
+  PLATFORM="$(env KUBECONFIG=${dir}/auth/kubeconfig oc get infrastructure/cluster -o json|/tmp/jq '.status.platform')"
+  TOPOLOGY="$(env KUBECONFIG=${dir}/auth/kubeconfig oc get infrastructure/cluster -o json|/tmp/jq '.status.infrastructureTopology')"
+  NETWORKTYPE="$(env KUBECONFIG=${dir}/auth/kubeconfig oc get network.operator cluster -o json|/tmp/jq '.spec.defaultNetwork.type')"
+  if [[ "$(env KUBECONFIG=${dir}/auth/kubeconfig oc get network.operator cluster -o json|/tmp/jq '.spec.clusterNetwork[0].cidr')" =~ .*":".*  ]]; then
+    NETWORKSTACK="IPv6"
+  else
+    NETWORKSTACK="IPv4"
+  fi
+  CLOUDREGION="$(env KUBECONFIG=${dir}/auth/kubeconfig oc get node -o json|/tmp/jq '.items[]|.metadata.labels'|grep topology.kubernetes.io/region|cut -d : -f 2| head -1| sed 's/,//g')"
+  CLOUDZONE="$(env KUBECONFIG=${dir}/auth/kubeconfig oc get node -o json|/tmp/jq '.items[]|.metadata.labels'|grep topology.kubernetes.io/zone|cut -d : -f 2| sort -u)"
+  CLUSTERVERSIONHISTORY="$(env KUBECONFIG=${dir}/auth/kubeconfig oc get clusterversion -o json|/tmp/jq '.items[]|.status.history'|grep version|cut -d : -f 2)"
+  CLOUDZONE="$(echo $CLOUDZONE | tr -d \")"
+  CLUSTERVERSIONHISTORY="$(echo $CLUSTERVERSIONHISTORY | tr -d \")"
+  python3 -c '
+import json;
+dictionary = {
+    "Platform": '$PLATFORM',
+    "Topology": '$TOPOLOGY',
+    "NetworkType": '$NETWORKTYPE',
+    "NetworkStack": "'$NETWORKSTACK'",
+    "CloudRegion": '"$CLOUDREGION"',
+    "CloudZone": "'"$CLOUDZONE"'".split(),
+    "ClusterVersionHistory": "'"$CLUSTERVERSIONHISTORY"'".split()
+}
+with open("'${ARTIFACT_DIR}/cluster-data.json'", "w") as outfile:
+    json.dump(dictionary, outfile)'
+  set -o errexit
+  set -o pipefail
+  echo "Done collecting cluster data for analysis!"
 fi
 
 echo "install-config.yaml"
