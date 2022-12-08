4692e07da586fe7ee1151c245bff99f58a649767
Pawan Pinjarkar <ppinjark@redhat.com>
2022-12-08 08:33:17 -0500

    Rename files and paths
    Signed-off-by: Pawan Pinjarkar <ppinjark@redhat.com>

--- ci-operator/step-registry/agent/gather/agent-gather-commands.sh
+++ ci-operator/step-registry/agent/gather/agent-gather-commands.sh
@@ -5,6 +5,10 @@ set -o errexit
 set -o pipefail
 
 echo "************ agent gather command ************"
+# Fetch packet basic configuration
+# shellcheck source=/dev/null
+source "${SHARED_DIR}/packet-conf.sh"
+
 echo "### Gathering logs..."
 timeout -s 9 5m ssh "${SSHOPTS[@]}" "root@${IP}" bash - <<EOF
 cd dev-scripts
