70e1a9ba0f1ce27d74e4b7bbb6bee90979342c55
Alessandro Di Stefano <aleskandro@redhat.com>
2022-12-09 10:22:32 +0100

    [Baremetal/QE] Adding dns config step (#34421)

--- ci-operator/step-registry/baremetal/lab/post/dns/baremetal-lab-post-dns-commands.sh
+++ ci-operator/step-registry/baremetal/lab/post/dns/baremetal-lab-post-dns-commands.sh
@@ -1,2 +1,30 @@
-#!/bin/sh
-echo Hello world
+#!/bin/bash
+
+if [ -n "${LOCAL_TEST}" ]; then
+  # Setting LOCAL_TEST to any value will allow testing this script with default values against the ARM64 bastion @ RDU2
+  export NAMESPACE=test-ci-op AUX_HOST=openshift-qe-bastion.arm.eng.rdu2.redhat.com \
+      SHARED_DIR=${SHARED_DIR:-$(mktemp -d)} CLUSTER_PROFILE_DIR=~/.ssh
+fi
+
+set -o nounset
+
+if [ -z "${AUX_HOST}" ]; then
+    echo "AUX_HOST is not filled. Failing."
+    exit 1
+fi
+
+SSHOPTS=(-o 'ConnectTimeout=5'
+  -o 'StrictHostKeyChecking=no'
+  -o 'UserKnownHostsFile=/dev/null'
+  -o 'ServerAliveInterval=90'
+  -o LogLevel=ERROR
+  -i "${CLUSTER_PROFILE_DIR}/ssh-key")
+
+timeout -s 9 180m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" bash -s -- ${NAMESPACE} << 'EOF'
+  set -o nounset
+  NAMESPACE="${1}"
+  sed -i "/; BEGIN ${NAMESPACE}/,/; END ${NAMESPACE}$/d" /opt/bind9_zones/{zone,internal_zone.rev}
+  docker start bind9
+  docker exec bind9 rndc reload
+  docker exec bind9 rndc flush
+EOF
