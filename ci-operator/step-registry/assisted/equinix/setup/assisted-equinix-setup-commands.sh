e8c36032a335ec3d393bc35d81d4d06beabff1c1
Adrien Gentil <agentil@redhat.com>
2022-12-13 16:38:58 +0100

    MGMT-12439: Fix equinix steps for assisted
    - Fix line continuation
    - `fix_uid.sh` script is not available, it will be performed as part of
      the ansible playbook

--- ci-operator/step-registry/assisted/equinix/setup/assisted-equinix-setup-commands.sh
+++ ci-operator/step-registry/assisted/equinix/setup/assisted-equinix-setup-commands.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
+set -x
 set -o nounset
 set -o errexit
 set -o pipefail
 
 echo "************  assisted equinix setup command ************"
 
-~/fix_uid.sh
-
 cd "${ANSIBLE_PLAYBOOK_DIRECTORY}"
 ansible-playbook --extra-vars "@vars/ci.yml" \
                  --extra-vars "@vars/ci_equinix_infrastucture.yml" \
-                 --extra-vars "${ANSIBLE_EXTRA_VARS}"
+                 --extra-vars "${ANSIBLE_EXTRA_VARS}" \
                  "${ANSIBLE_PLAYBOOK_CREATE_INFRA}"
