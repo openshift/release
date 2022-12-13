56ebb05c34bf71250ea84463e1be22d23295067b
deepsm007 <dmistry@redhat.com>
2022-12-13 09:47:22 -0500

    Fixing the minimum count for vsphere and return code error

--- ci-operator/step-registry/openshift/release/analysis/test-case-analysis/openshift-release-analysis-test-case-analysis-commands.sh
+++ ci-operator/step-registry/openshift/release/analysis/test-case-analysis/openshift-release-analysis-test-case-analysis-commands.sh
@@ -37,8 +37,9 @@ job-run-aggregator analyze-test-case \
 	--working-dir=${ARTIFACT_DIR}/aws-sdn-ipi \
 	--timeout=4h30m \
 	--test-group=${TEST_GROUP}
-if [ $? -gt 0 ]; then
-	ret=$?
+rc=$?
+if [ $rc -gt 0 ]; then
+	ret=$rc
 fi
 
 echo
@@ -55,8 +56,9 @@ job-run-aggregator analyze-test-case \
 	--working-dir=${ARTIFACT_DIR}/aws-sdn-upi \
 	--timeout=4h30m \
 	--test-group=${TEST_GROUP}
-if [ $? -gt 0 ]; then
-	ret=$?
+rc=$?
+if [ $rc -gt 0 ]; then
+	ret=$rc
 fi
 
 echo
@@ -73,8 +75,9 @@ job-run-aggregator analyze-test-case \
 	--working-dir=${ARTIFACT_DIR}/azure-ovn-ipi \
 	--timeout=4h30m \
 	--test-group=${TEST_GROUP}
-if [ $? -gt 0 ]; then
-	ret=$?
+rc=$?
+if [ $rc -gt 0 ]; then
+	ret=$rc
 fi
 
 echo
@@ -91,8 +94,9 @@ job-run-aggregator analyze-test-case \
 	--working-dir=${ARTIFACT_DIR}/gcp-sdn-ipi \
 	--timeout=4h30m \
 	--test-group=${TEST_GROUP}
-if [ $? -gt 0 ]; then
-	ret=$?
+rc=$?
+if [ $rc -gt 0 ]; then
+	ret=$rc
 fi
 
 echo
@@ -109,8 +113,9 @@ job-run-aggregator analyze-test-case \
 	--working-dir=${ARTIFACT_DIR}/vsphere-ovn-ipi \
 	--timeout=4h30m \
 	--test-group=${TEST_GROUP}
-if [ $? -gt 0 ]; then
-	ret=$?
+rc=$?
+if [ $rc -gt 0 ]; then
+	ret=$rc
 fi
 
 echo
@@ -122,31 +127,14 @@ job-run-aggregator analyze-test-case \
 	--platform=vsphere \
 	--network=ovn \
 	--infrastructure=upi \
-	--minimum-successful-count=${MINIMUM_SUCCESSFUL_COUNT} \
+	--minimum-successful-count=1 \
 	--job-start-time=${JOB_START_TIME} \
 	--working-dir=${ARTIFACT_DIR}/vsphere-ovn-upi \
 	--timeout=4h30m \
 	--test-group=${TEST_GROUP}
-if [ $? -gt 0 ]; then
-	ret=$?
-fi
-
-echo
-echo "********** Starting testcase analysis for:  vsphere-sdn-ipi "
-echo
-job-run-aggregator analyze-test-case \
-	--google-service-account-credential-file ${GOOGLE_SA_CREDENTIAL_FILE} \
-	--payload-tag=${PAYLOAD_TAG} \
-	--platform=vsphere \
-	--network=sdn \
-	--infrastructure=ipi \
-	--minimum-successful-count=${MINIMUM_SUCCESSFUL_COUNT} \
-	--job-start-time=${JOB_START_TIME} \
-	--working-dir=${ARTIFACT_DIR}/vsphere-sdn-ipi \
-	--timeout=4h30m \
-	--test-group=${TEST_GROUP}
-if [ $? -gt 0 ]; then
-	ret=$?
+rc=$?
+if [ $rc -gt 0 ]; then
+	ret=$rc
 fi
 
 echo
@@ -163,8 +151,9 @@ job-run-aggregator analyze-test-case \
 	--working-dir=${ARTIFACT_DIR}/metal-ovn-ipi \
 	--timeout=4h30m \
 	--test-group=${TEST_GROUP}
-if [ $? -gt 0 ]; then
-	ret=$?
+rc=$?
+if [ $rc -gt 0 ]; then
+	ret=$rc
 fi
 
 echo
@@ -181,8 +170,9 @@ job-run-aggregator analyze-test-case \
 	--working-dir=${ARTIFACT_DIR}/metal-sdn-ipi \
 	--timeout=4h30m \
 	--test-group=${TEST_GROUP}
-if [ $? -gt 0 ]; then
-	ret=$?
+rc=$?
+if [ $rc -gt 0 ]; then
+	ret=$rc
 fi
 
 echo
@@ -198,9 +188,11 @@ job-run-aggregator analyze-test-case \
 	--working-dir=${ARTIFACT_DIR}/aws-proxy \
 	--timeout=4h30m \
 	--test-group=${TEST_GROUP}
-if [ $? -gt 0 ]; then
-	ret=$?
+rc=$?
+if [ $rc -gt 0 ]; then
+	ret=$rc
 fi
 
+set -o errexit
 echo "Exiting with ret=${ret}"
 exit "${ret}"
