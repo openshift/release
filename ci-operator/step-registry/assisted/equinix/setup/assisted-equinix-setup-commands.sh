ad97d88e3889b6a93d62b1a78deb9ad19b77c583
Adrien Gentil <agentil@redhat.com>
2022-12-14 16:38:02 +0100

    MGMT-12439: fix uid in assisted equinix steps
    call `fix_uid.sh` before running running ansible because it requires it.
    The script will is available in the path: https://github.com/openshift/assisted-test-infra/pull/1949/files#diff-18f384cf7056dd29bdbdeea6b49eb5c293cf33449873d21809b3454d8e114e2bR10-R13

--- ci-operator/step-registry/assisted/equinix/setup/assisted-equinix-setup-commands.sh
+++ ci-operator/step-registry/assisted/equinix/setup/assisted-equinix-setup-commands.sh
@@ -7,6 +7,9 @@ set -o pipefail
 
 echo "************  assisted equinix setup command ************"
 
+# TODO: Remove once OpenShift CI supports it out of the box (see https://access.redhat.com/articles/4859371)
+fix_uid.sh
+
 cd "${ANSIBLE_PLAYBOOK_DIRECTORY}"
 ansible-playbook --extra-vars "@vars/ci.yml" \
                  --extra-vars "@vars/ci_equinix_infrastucture.yml" \
