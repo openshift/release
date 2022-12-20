6161ce1ecb56b0aec62f225988ca617f1d1fbda4
Li Zhang <lzha@redhat.com>
2022-12-20 12:30:24 +0800

    fix shell check error

--- ci-operator/step-registry/quay-tests/recycle-omr/quay-tests-recycle-omr-commands.sh
+++ ci-operator/step-registry/quay-tests/recycle-omr/quay-tests-recycle-omr-commands.sh
@@ -11,6 +11,7 @@ cp ${SHARED_DIR}/terraform.tgz .
 tar -xzvf terraform.tgz && ls
 OMR_CI_NAME=$(cat ${SHARED_DIR}/OMR_CI_NAME)
 OMR_HOST_NAME=$(cat ${SHARED_DIR}/OMR_HOST_NAME)
+echo "Start to destroy OMR $OMR_HOST_NAME ..."
 
 #Destroy Quay OMR
 export TF_VAR_quay_build_instance_name="${OMR_CI_NAME}"
@@ -19,4 +20,4 @@ export TF_VAR_quay_build_worker_security_group="${OMR_CI_NAME}"
 terraform init
 terraform destroy -auto-approve
 
-echo "Quay OMR "
+echo "Quay OMR $OMR_HOST_NAME is recycled successfully"
