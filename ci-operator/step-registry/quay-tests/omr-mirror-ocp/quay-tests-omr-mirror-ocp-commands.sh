1ab8f6f7173f4a73fcd775dc7820040457a89e84
Li Zhang <lzha@redhat.com>
2022-12-21 17:42:54 +0800

    add ocp install release imageg dependencies

--- ci-operator/step-registry/quay-tests/omr-mirror-ocp/quay-tests-omr-mirror-ocp-commands.sh
+++ ci-operator/step-registry/quay-tests/omr-mirror-ocp/quay-tests-omr-mirror-ocp-commands.sh
@@ -6,10 +6,13 @@ set -o pipefail
 
 
 #Check oc and ocp installer version
+echo "What's the oc version?"
 oc version
+
+echo "What's the ocp installer version?"
 openshift-install version
 
-echo "${RELEASE_IMAGE_LATEST}"
+echo "${OPENSHIFT_INSTALL_RELEASE_IMAGE}" || true
 
 OMR_HOST_NAME=$(cat ${SHARED_DIR}/OMR_HOST_NAME)
 echo "Start to mirror OCP Images to OMR $OMR_HOST_NAME ..."
@@ -18,6 +21,6 @@ cp /var/run/quay-qe-omr-secret/quaybuilder . && cp /var/run/quay-qe-omr-secret/q
 chmod 600 ./quaybuilder && chmod 600 ./quaybuilder.pub
 
 #Share the CA Cert of Quay OMR
-scp -i quaybuilder -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null  ec2-user@"${OMR_HOST_NAME}":/etc/quay-install/quay-rootCA/rootCA.pem ${SHARED_DIR}
+scp -i quaybuilder -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null  ec2-user@"${OMR_HOST_NAME}":/etc/quay-install/quay-rootCA/rootCA.pem ${SHARED_DIR} || true
 
 sleep 500
