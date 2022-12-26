21e2ebf7323eadadc292aeef7d7b0a0518225071
wwei <wwei@redhat.com>
2022-12-26 14:20:33 +0800

    add compact 3 node cluster for vsphere

--- ci-operator/step-registry/ipi/conf/httpd/ipi-conf-httpd-command.sh
+++ ci-operator/step-registry/ipi/conf/httpd/ipi-conf-httpd-command.sh
@@ -8,9 +8,13 @@ http_path="/var/www/html/"
 vsphere_rhcos_image_location=$(openshift-install coreos print-stream-json | jq -r .architectures.x86_64.artifacts.vmware.formats.ova.disk.location)
 vsphere_rhcos_image=`basename ${vsphere_rhcos_image_location}`
 
+ls -lrt ${SSH_PRIV_KEY_PATH}
+
 SSH_PRIV_KEY_PATH="${CLUSTER_PROFILE_DIR}/ssh-privatekey"
 BASTION_IP=$(<"${SHARED_DIR}/bastion_private_address")
 BASTION_SSH_USER=$(<"${SHARED_DIR}/bastion_ssh_user")
+chmod 600 ${SSH_PRIV_KEY_PATH}
+ls -lrt ${SSH_PRIV_KEY_PATH}
 
 ssh -i "${SSH_PRIV_KEY_PATH}" -o UserKnownHostsFile=/dev/null \
         -o StrictHostKeyChecking=no  ${BASTION_SSH_USER}@${BASTION_IP}  "sudo rm -rf ${http_path}${vsphere_rhcos_image} && sudo curl -Lk -o '${http_path}/${vsphere_rhcos_image}' '${vsphere_rhcos_image_location}'"
