99aeb312ebc673908e44acf574536f655147f01c
Kui Wang <kuiwang@redhat.com>
2023-01-04 15:15:32 +0800

    support alibaba (#35115)

--- ci-operator/step-registry/openshift-extended/upgrade/pre/openshift-extended-upgrade-pre-commands.sh
+++ ci-operator/step-registry/openshift-extended/upgrade/pre/openshift-extended-upgrade-pre-commands.sh
@@ -90,6 +90,15 @@ aws-usgov|aws-c2s|aws-sc2s)
     export KUBE_SSH_USER=core
     export TEST_PROVIDER="none"
     ;;
+alibabacloud)
+    mkdir -p ~/.ssh
+    cp "${CLUSTER_PROFILE_DIR}/ssh-privatekey" ~/.ssh/kube_alibaba_rsa || true
+    export SSH_CLOUD_PRIV_ALIBABA_USER="${QE_BASTION_SSH_USER:-core}"
+    export KUBE_SSH_USER=core
+    export PROVIDER_ARGS="-provider=alibabacloud -gce-zone=us-east-1"
+    REGION="$(oc get -o jsonpath='{.status.platformStatus.alibabacloud.region}' infrastructure cluster)"
+    export TEST_PROVIDER="{\"type\":\"alibabacloud\",\"region\":\"${REGION}\",\"multizone\":true,\"multimaster\":true}"
+;;
 azure4|azure-arm64)
     mkdir -p ~/.ssh
     cp "${CLUSTER_PROFILE_DIR}/ssh-privatekey" ~/.ssh/kube_azure_rsa || true
