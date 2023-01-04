1c197b65f62144eba2aef1e7f4cd320fe50e6553
Li Zhang <lzha@redhat.com>
2023-01-04 17:48:03 +0800

    fix the OMR hostname in pull secret

--- ci-operator/step-registry/quay-tests/omr-mirror-ocp/quay-tests-omr-mirror-ocp-commands.sh
+++ ci-operator/step-registry/quay-tests/omr-mirror-ocp/quay-tests-omr-mirror-ocp-commands.sh
@@ -19,14 +19,9 @@ echo "${OPENSHIFT_INSTALL_RELEASE_IMAGE}" || true
 cat "${CLUSTER_PROFILE_DIR}/pull-secret"
 
 OMR_HOST_NAME=$(cat ${SHARED_DIR}/OMR_HOST_NAME)
+OMR_HOST="${OMR_HOST_NAME}:8443"
 OMR_CRED="cXVheTpwYXNzd29yZA=="
 echo "Start to mirror OCP Images to OMR $OMR_HOST_NAME ..."
-
-cp /var/run/quay-qe-omr-secret/quaybuilder ${SHARED_DIR}/quaybuilder && cp /var/run/quay-qe-omr-secret/quaybuilder.pub  ${SHARED_DIR}/quaybuilder.pub || true
-chmod 600 ${SHARED_DIR}/quaybuilder && chmod 600 ${SHARED_DIR}/quaybuilder.pub || true
-
-#Share the CA Cert of Quay OMR
-scp -i ${SHARED_DIR}/quaybuilder -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null  ec2-user@"${OMR_HOST_NAME}":/etc/quay-install/quay-rootCA/rootCA.pem ${SHARED_DIR} || true
 
 target_release_image_repo="${OMR_HOST_NAME}:8443/openshift-release-dev/ocp-release"
 target_release_image="${target_release_image_repo}:4.12-x86_64"
@@ -38,7 +33,7 @@ echo "${target_release_image}" || true
 unset KUBECONFIG
 oc registry login
 
-jq --argjson a "{\"${OMR_HOST_NAME}\": {\"auth\": \"$OMR_CRED\"}}" '.auths |= . + $a' "${CLUSTER_PROFILE_DIR}/pull-secret" > "${new_pull_secret}"
+jq --argjson a "{\"${OMR_HOST}\": {\"auth\": \"$OMR_CRED\"}}" '.auths |= . + $a' "${CLUSTER_PROFILE_DIR}/pull-secret" > "${new_pull_secret}"
 
 cat "${new_pull_secret}" | jq || true
 
@@ -48,5 +43,3 @@ oc adm release -a "${new_pull_secret}" mirror --insecure=true \
  --to=${target_release_image_repo} \
  --to-release-image=${target_release_image} | tee "${mirror_output}" || true
 
-#debug
-sleep 600
