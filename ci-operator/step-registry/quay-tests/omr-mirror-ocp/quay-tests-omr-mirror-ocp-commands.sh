a98d41b79697fbf1bd52b344925af347f9dfb9d6
Luffy Zhang <lzha@redhat.com>
2023-01-05 14:57:17 +0800

    Quay add omr oc mirror step (#35144)
    * add oc mirror ocp release images to quay omr
    
    * run make update
    
    * fix issue of ocp installer
    
    * add ocp cluster profile name

---
+++ ci-operator/step-registry/quay-tests/omr-mirror-ocp/quay-tests-omr-mirror-ocp-commands.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+#check versions
+oc version
+openshift-install version
+
+mirror_output="${SHARED_DIR}/mirror_output"
+new_pull_secret="${SHARED_DIR}/new_pull_secret"
+install_config_icsp_patch="${SHARED_DIR}/install-config-icsp.yaml.patch"
+icsp_file="${SHARED_DIR}/local_registry_icsp_file.yaml"
+
+echo "${OPENSHIFT_INSTALL_RELEASE_IMAGE}" || true
+
+OMR_HOST_NAME=$(cat ${SHARED_DIR}/OMR_HOST_NAME)
+OMR_HOST="${OMR_HOST_NAME}:8443"
+OMR_CRED="cXVheTpwYXNzd29yZA=="
+echo "Start to mirror OCP Images to OMR $OMR_HOST_NAME ..."
+
+target_release_image_repo="${OMR_HOST_NAME}:8443/openshift-release-dev/ocp-release"
+target_release_image="${target_release_image_repo}:4.12-x86_64"
+
+echo "${target_release_image_repo}" || true
+echo "${target_release_image}" || true
+
+#Login the image registry of build farm
+unset KUBECONFIG
+oc registry login
+
+jq --argjson a "{\"${OMR_HOST}\": {\"auth\": \"$OMR_CRED\"}}" '.auths |= . + $a' "${CLUSTER_PROFILE_DIR}/pull-secret" > "${new_pull_secret}"
+
+# MIRROR IMAGES to OMR
+oc adm release -a "${new_pull_secret}" mirror --insecure=true \
+ --from=${OPENSHIFT_INSTALL_RELEASE_IMAGE} \
+ --to=${target_release_image_repo} \
+ --to-release-image=${target_release_image} | tee "${mirror_output}" || true
+
+grep -B 1 -A 10 "kind: ImageContentSourcePolicy" ${mirror_output} > "${icsp_file}"
+grep -A 6 "imageContentSources" ${mirror_output} > "${install_config_icsp_patch}"
+
+cat "${icsp_file}"
+cat "${install_config_icsp_patch}"
