2791b8c61ea1c63ec66d888563c5d1b9ef0e3a76
Li Zhang <lzha@redhat.com>
2023-01-06 21:40:12 +0800

    archive ocp install logs to debug

--- ci-operator/step-registry/quay-tests/omr-install-ocp/quay-tests-omr-install-ocp-commands.sh
+++ ci-operator/step-registry/quay-tests/omr-install-ocp/quay-tests-omr-install-ocp-commands.sh
@@ -16,6 +16,7 @@ AWS_DEFAULT_REGION="us-west-2" && export AWS_DEFAULT_REGION
 aws s3 ls
 
 cat "${SHARED_DIR}/new_pull_secret" | jq
+registry_ci_openshift_ci_auth=$(cat "${SHARED_DIR}/new_pull_secret" | jq '.auths."registry.ci.openshift.org".auth' | tr -d '"')
 cat "${SHARED_DIR}/local_registry_icsp_file.yaml"
 OMR_HOST_NAME=$(cat ${SHARED_DIR}/OMR_HOST_NAME)
 OCP_NAME="omrocpprowci$RANDOM"
@@ -73,19 +74,20 @@ platform:
       costCenter: 7536
 fips: false 
 sshKey: ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDN8nUzLnPHq9o6Crika8brT4i5CL0a0azoHJoHe02BH8/vgDyhgHin+1qDrHA414t6smDIhYRM/L503J0kD2/jUTPVqeFNmbxbzXnEXWv2RaAyKChMzw2PkrKiLntY4CxcukdSN6lqtJa8TH3/Vmy/YUOMJOKWEsYkg6qojDWPYbFHMubm6JWPydiEJJYPYCH7tHPaq4Y3CWNw+jx2sL69Sltnsdc/oj5Icl+u/ClF7lm0LPXkrkUF745ktCg6r06dLju3Ap+A0HJ/doTpCymZrt88eEy0RqW9koDYPJsRm380caT0J4wux3HlZiHP0b1mhx9pp7DB0FuhZHxeQawGs4V3aYDisBE27YMoMBqoCmBOqkVqC7uY47HOYiS15YHpriCXSnflE628e6a7zfFVV+CcrcqtcqPltZlXmbm2PeQY547VphB1nivinALOVM+CcSgOchX1Phmj63nXKt/IbsUJhUnZQicFhh2bJzXWKBtCQkodwTnu90RaKJN2pn8= lizhang@lzha-mac 
-pullSecret: '{"auths":{"${OMR_HOST_NAME}:8443": {"auth": "cXVheTpwYXNzd29yZA==","email": "lzha@redhat.com"}, "registry.ci.openshift.org": {"auth": "dXNlcm5hbWUtdW51c2VkOmV5SmhiR2NpT2lKU1V6STFOaUlzSW10cFpDSTZJa0Z3U3pBdGIwWjRiVzFGVEV0R01TMDBVRGt3Y2xBMFEyVkJUVGRETTBkV1JGcHZiRjlZZWkxRFFuTWlmUS5leUpwYzNNaU9pSnJkV0psY201bGRHVnpMM05sY25acFkyVmhZMk52ZFc1MElpd2lhM1ZpWlhKdVpYUmxjeTVwYnk5elpYSjJhV05sWVdOamIzVnVkQzl1WVcxbGMzQmhZMlVpT2lKeFpTSXNJbXQxWW1WeWJtVjBaWE11YVc4dmMyVnlkbWxqWldGalkyOTFiblF2YzJWamNtVjBMbTVoYldVaU9pSmpZMmt0YW1WdWEybHVjeTEwYjJ0bGJpMXRjbU15WnlJc0ltdDFZbVZ5Ym1WMFpYTXVhVzh2YzJWeWRtbGpaV0ZqWTI5MWJuUXZjMlZ5ZG1salpTMWhZMk52ZFc1MExtNWhiV1VpT2lKalkya3RhbVZ1YTJsdWN5SXNJbXQxWW1WeWJtVjBaWE11YVc4dmMyVnlkbWxqWldGalkyOTFiblF2YzJWeWRtbGpaUzFoWTJOdmRXNTBMblZwWkNJNklqTmlZMlZoWkdVNUxUQXdORGN0TkdWaU1pMDRZakUyTFRBNE9UUmtNbU01TnpWaVlpSXNJbk4xWWlJNkluTjVjM1JsYlRwelpYSjJhV05sWVdOamIzVnVkRHB4WlRwalkya3RhbVZ1YTJsdWN5SjkueF82T2diTk83eDFnX0lIXzhqWF9BUkRVVXptZ2tUb0xELWI2YWw1UDZYZnJMRXBJR3dUblpOalcyMDVCS3F1bGtzSXZrMTZaRkJScHFNaTcxaW9YVzZYaEd3RjE5WUdCcnpaZXd1N1AwdWdwUDMteGY0TWhZVUUwZk9IWlVqTkI1UEhMb09lUDRia1ZlVXFqa0lKVDRMQUpYZE1OZGlacHJORjFmV2JoamxJVk5LcndpSlFfTlF6ajI0cG1rRlI0NGhfSks0cVc2Y096UGQyOVRnOTJTQXhfd1UtZEdiZ1lQTkZyWGE5emo4bXpqdEs2VHZXczQwVlFrQ0FIOXFydWxMMjhSeF92VEhSZUZMYWEyNGR0TDBHWVdsLTc0dGN1WGdwYng5clFldThtY0dvNzJKRTVxSlBGZTBOczBxamtNaVZjMmNNaGxNOWU3VV85T2ZqdXh3","email": "lzha@redhat.com"}}}'
+pullSecret: '{"auths":{"${OMR_HOST_NAME}:8443": {"auth": "cXVheTpwYXNzd29yZA==","email": "lzha@redhat.com"}, "registry.ci.openshift.org": {"auth": "${registry_ci_openshift_ci_auth}","email": "lzha@redhat.com"}}}'
 additionalTrustBundle: |
 $(cat "${SHARED_DIR}/rootCA.pem" | awk '{print "    "$0}')
 $(cat "${SHARED_DIR}/install-config-mirrors")
 EOF
 
 cat "${SHARED_DIR}/install-config.yaml"
+cp "${SHARED_DIR}/install-config.yaml" ${ARTIFACT_DIR}
 
 cp "${SHARED_DIR}/install-config.yaml" /tmp
 openshift-install --dir=/tmp create cluster --log-level=debug || true
-ls /tmp
+cp /tmp/log-bundle-*.tar.gz ${ARTIFACT_DIR}
 
 cp "${SHARED_DIR}/install-config.yaml" /tmp
-openshift-install --dir=/tmp desttroy cluster --log-level=debug || true
+openshift-install --dir=/tmp destroy cluster --log-level=debug || true
 
 
