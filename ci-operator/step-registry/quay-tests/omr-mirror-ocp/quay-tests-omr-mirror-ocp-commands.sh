38cfbdc39ce1071f26b1dcc8acc6dd5e399fed12
Li Zhang <lzha@redhat.com>
2023-01-09 13:16:06 +0800

    fix oc mirror images issue

--- ci-operator/step-registry/quay-tests/omr-mirror-ocp/quay-tests-omr-mirror-ocp-commands.sh
+++ ci-operator/step-registry/quay-tests/omr-mirror-ocp/quay-tests-omr-mirror-ocp-commands.sh
@@ -20,8 +20,9 @@ OMR_HOST="${OMR_HOST_NAME}:8443"
 OMR_CRED="cXVheTpwYXNzd29yZA=="
 echo "Start to mirror OCP Images to OMR $OMR_HOST_NAME ..."
 
-target_release_image_repo="${OMR_HOST_NAME}:8443/openshift-release-dev/ocp-release"
-target_release_image="${target_release_image_repo}:4.12-x86_64"
+target_release_image="${OMR_HOST}/${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE#*/}"
+target_release_image_repo="${target_release_image%:*}"
+target_release_image_repo="${target_release_image_repo%@sha256*}"
 
 echo "${target_release_image_repo}" || true
 echo "${target_release_image}" || true
