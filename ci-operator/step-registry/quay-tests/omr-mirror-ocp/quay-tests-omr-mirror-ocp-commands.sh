82408f7967e88fca6e361dab7a74305ac9ffa9f0
Li Zhang <lzha@redhat.com>
2023-01-09 13:42:37 +0800

    update the OCP release env var

--- ci-operator/step-registry/quay-tests/omr-mirror-ocp/quay-tests-omr-mirror-ocp-commands.sh
+++ ci-operator/step-registry/quay-tests/omr-mirror-ocp/quay-tests-omr-mirror-ocp-commands.sh
@@ -20,7 +20,7 @@ OMR_HOST="${OMR_HOST_NAME}:8443"
 OMR_CRED="cXVheTpwYXNzd29yZA=="
 echo "Start to mirror OCP Images to OMR $OMR_HOST_NAME ..."
 
-target_release_image="${OMR_HOST}/${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE#*/}"
+target_release_image="${OMR_HOST}/${OPENSHIFT_INSTALL_RELEASE_IMAGE#*/}"
 target_release_image_repo="${target_release_image%:*}"
 target_release_image_repo="${target_release_image_repo%@sha256*}"
 
