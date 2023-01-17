f3afa69dda8046227277c46b9747fe0caa9458c3
Matthew Booth <mbooth@redhat.com>
2023-01-17 10:20:58 +0000

    Simplify shiftstack-liveliness (#35423)
    Landing https://github.com/shiftstack/shiftstack-ci/pull/199 and
    https://github.com/shiftstack/shiftstack-ci/pull/200 means we no longer
    need any complications in the liveliness check relating to users or ssh
    keys.

--- ci-operator/step-registry/shiftstack/liveliness/shiftstack-liveliness-commands.sh
+++ ci-operator/step-registry/shiftstack/liveliness/shiftstack-liveliness-commands.sh
@@ -9,25 +9,10 @@ OPENSTACK_EXTERNAL_NETWORK="${OPENSTACK_EXTERNAL_NETWORK:-$(<"${SHARED_DIR}/OPEN
 # Recycling BASTION_FLAVOR as it's a small flavor we can re-use.
 TESTING_FLAVOR="${TESTING_FLAVOR:-$(<"${SHARED_DIR}/BASTION_FLAVOR")}"
 
-# TODO - this logic could leave in shiftstack-ci/server script at some point.
-ssh-keygen -t rsa -N "" -f shiftstack-ci
-chmod 0600 shiftstack-ci
-eval "$(ssh-agent)"
-if ! whoami &> /dev/null; then
-  if [ -w /etc/passwd ]; then
-    echo "${IMAGE_USER:-cloud-user}:x:$(id -u):0:${IMAGE_USER:-cloud-user} user:${HOME}:/sbin/nologin" >> /etc/passwd
-  fi
-fi
-ssh-add shiftstack-ci
-openstack keypair create --public-key shiftstack-ci.pub shiftstack-ci-${CLUSTER_NAME} >/dev/null
-
 set +e
 echo "DEBUG: Running liveliness check script..."
-./server.sh -d -t -u ${IMAGE_USER} -f ${TESTING_FLAVOR} -i ${TESTING_IMAGE} -e ${OPENSTACK_EXTERNAL_NETWORK} -k shiftstack-ci-${CLUSTER_NAME} shiftstack-ci-${CLUSTER_NAME}
+./server.sh -d -t -l -f ${TESTING_FLAVOR} -i ${TESTING_IMAGE} -e ${OPENSTACK_EXTERNAL_NETWORK} shiftstack-ci-${CLUSTER_NAME}
 RC=$?
-echo "DEBUG: Removing shiftstack-ci-${CLUSTER_NAME} keypair"
-openstack keypair delete shiftstack-ci-${CLUSTER_NAME}
-rm shiftstack-ci shiftstack-ci.pub
 set -e
 
 if [ $RC -ne 0 ]; then
