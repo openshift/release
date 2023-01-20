ad1574a3dd032b34f19b710499bc51b797b6f3f4
ascerra@redhat.com <ascerra@redhat.com>
2023-01-20 12:48:36 -0500

    test just opp policy script

--- ci-operator/step-registry/layered-product/opp/apply-opp-policyset/layered-product-opp-apply-opp-policyset-commands.sh
+++ ci-operator/step-registry/layered-product/opp/apply-opp-policyset/layered-product-opp-apply-opp-policyset-commands.sh
@@ -4,143 +4,145 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-export KUBECONFIG=${SHARED_DIR}/kubeconfig
+# export KUBECONFIG=${SHARED_DIR}/kubeconfig
 
-# create the policies namespace
-oc apply -f - <<EOF
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: policies
-EOF
+# # create the policies namespace
+# oc apply -f - <<EOF
+# apiVersion: v1
+# kind: Namespace
+# metadata:
+#   name: policies
+# EOF
 
-# apply the configure subscription admin policy
-oc apply -f - <<EOF
-apiVersion: policy.open-cluster-management.io/v1
-kind: Policy
-metadata:
-  name: policy-configure-subscription-admin-hub
-  namespace: policies
-  annotations:
-    policy.open-cluster-management.io/standards: NIST SP 800-53
-    policy.open-cluster-management.io/categories: CM Configuration Management
-    policy.open-cluster-management.io/controls: CM-2 Baseline Configuration
-spec:
-  remediationAction: enforce
-  disabled: false
-  policy-templates:
-    - objectDefinition:
-        apiVersion: policy.open-cluster-management.io/v1
-        kind: ConfigurationPolicy
-        metadata:
-          name: policy-configure-subscription-admin-hub
-        spec:
-          remediationAction: enforce
-          severity: low
-          object-templates:
-            - complianceType: musthave
-              objectDefinition:
-                apiVersion: rbac.authorization.k8s.io/v1
-                kind: ClusterRole
-                metadata:
-                  name: open-cluster-management:subscription-admin
-                rules:
-                - apiGroups:
-                  - app.k8s.io
-                  resources:
-                  - applications
-                  verbs:
-                  - '*'
-                - apiGroups:
-                  - apps.open-cluster-management.io
-                  resources:
-                  - '*'
-                  verbs:
-                  - '*'
-                - apiGroups:
-                  - ""
-                  resources:
-                  - configmaps
-                  - secrets
-                  - namespaces
-                  verbs:
-                  - '*'
-            - complianceType: musthave
-              objectDefinition:
-                apiVersion: rbac.authorization.k8s.io/v1
-                kind: ClusterRoleBinding
-                metadata:
-                  name: open-cluster-management:subscription-admin
-                roleRef:
-                  apiGroup: rbac.authorization.k8s.io
-                  kind: ClusterRole
-                  name: open-cluster-management:subscription-admin
-                subjects:
-                - apiGroup: rbac.authorization.k8s.io
-                  kind: User
-                  name: kube:admin
-                - apiGroup: rbac.authorization.k8s.io
-                  kind: User
-                  name: system:admin
----
-apiVersion: policy.open-cluster-management.io/v1
-kind: PlacementBinding
-metadata:
-  name: binding-policy-configure-subscription-admin-hub
-  namespace: policies
-placementRef:
-  name: placement-policy-configure-subscription-admin-hub
-  kind: PlacementRule
-  apiGroup: apps.open-cluster-management.io
-subjects:
-- name: policy-configure-subscription-admin-hub
-  kind: Policy
-  apiGroup: policy.open-cluster-management.io
----
-apiVersion: apps.open-cluster-management.io/v1
-kind: PlacementRule
-metadata:
-  name: placement-policy-configure-subscription-admin-hub
-  namespace: policies
-spec:
-  clusterConditions:
-  - status: "True"
-    type: ManagedClusterConditionAvailable
-  clusterSelector:
-    matchExpressions:
-      - {key: name, operator: In, values: ["local-cluster"]}
-EOF
+# # apply the configure subscription admin policy
+# oc apply -f - <<EOF
+# apiVersion: policy.open-cluster-management.io/v1
+# kind: Policy
+# metadata:
+#   name: policy-configure-subscription-admin-hub
+#   namespace: policies
+#   annotations:
+#     policy.open-cluster-management.io/standards: NIST SP 800-53
+#     policy.open-cluster-management.io/categories: CM Configuration Management
+#     policy.open-cluster-management.io/controls: CM-2 Baseline Configuration
+# spec:
+#   remediationAction: enforce
+#   disabled: false
+#   policy-templates:
+#     - objectDefinition:
+#         apiVersion: policy.open-cluster-management.io/v1
+#         kind: ConfigurationPolicy
+#         metadata:
+#           name: policy-configure-subscription-admin-hub
+#         spec:
+#           remediationAction: enforce
+#           severity: low
+#           object-templates:
+#             - complianceType: musthave
+#               objectDefinition:
+#                 apiVersion: rbac.authorization.k8s.io/v1
+#                 kind: ClusterRole
+#                 metadata:
+#                   name: open-cluster-management:subscription-admin
+#                 rules:
+#                 - apiGroups:
+#                   - app.k8s.io
+#                   resources:
+#                   - applications
+#                   verbs:
+#                   - '*'
+#                 - apiGroups:
+#                   - apps.open-cluster-management.io
+#                   resources:
+#                   - '*'
+#                   verbs:
+#                   - '*'
+#                 - apiGroups:
+#                   - ""
+#                   resources:
+#                   - configmaps
+#                   - secrets
+#                   - namespaces
+#                   verbs:
+#                   - '*'
+#             - complianceType: musthave
+#               objectDefinition:
+#                 apiVersion: rbac.authorization.k8s.io/v1
+#                 kind: ClusterRoleBinding
+#                 metadata:
+#                   name: open-cluster-management:subscription-admin
+#                 roleRef:
+#                   apiGroup: rbac.authorization.k8s.io
+#                   kind: ClusterRole
+#                   name: open-cluster-management:subscription-admin
+#                 subjects:
+#                 - apiGroup: rbac.authorization.k8s.io
+#                   kind: User
+#                   name: kube:admin
+#                 - apiGroup: rbac.authorization.k8s.io
+#                   kind: User
+#                   name: system:admin
+# ---
+# apiVersion: policy.open-cluster-management.io/v1
+# kind: PlacementBinding
+# metadata:
+#   name: binding-policy-configure-subscription-admin-hub
+#   namespace: policies
+# placementRef:
+#   name: placement-policy-configure-subscription-admin-hub
+#   kind: PlacementRule
+#   apiGroup: apps.open-cluster-management.io
+# subjects:
+# - name: policy-configure-subscription-admin-hub
+#   kind: Policy
+#   apiGroup: policy.open-cluster-management.io
+# ---
+# apiVersion: apps.open-cluster-management.io/v1
+# kind: PlacementRule
+# metadata:
+#   name: placement-policy-configure-subscription-admin-hub
+#   namespace: policies
+# spec:
+#   clusterConditions:
+#   - status: "True"
+#     type: ManagedClusterConditionAvailable
+#   clusterSelector:
+#     matchExpressions:
+#       - {key: name, operator: In, values: ["local-cluster"]}
+# EOF
 
-# apply managedclusterset binding
-oc apply -f - <<EOF
-apiVersion: cluster.open-cluster-management.io/v1beta1
-kind: ManagedClusterSetBinding
-metadata:
-  name: default
-  namespace: policies
-spec:
-  clusterSet: default
-EOF
+# # apply managedclusterset binding
+# oc apply -f - <<EOF
+# apiVersion: cluster.open-cluster-management.io/v1beta1
+# kind: ManagedClusterSetBinding
+# metadata:
+#   name: default
+#   namespace: policies
+# spec:
+#   clusterSet: default
+# EOF
 
-sleep 60
+sleep 600
+
+sudo dnf install -y git
 
 git clone https://github.com/stolostron/policy-collection.git
 
 cd policy-collection/deploy/ 
 echo 'y' | ./deploy.sh -p policygenerator/policy-sets/community/openshift-plus -n policies -u https://github.com/stolostron/policy-collection.git
 
-sleep 60
+# sleep 60
 
-# wait for storage nodes to be ready
-RETRIES=30
-for i in $(seq "${RETRIES}"); do
-  if [[ $(oc get policies -n policies) != *"NonCompliant"* ]]; then
-    echo "OPP policyset is applied and compliant"
-    break
-  else
-    echo "Try ${i}/${RETRIES}: Policies are not compliant. Checking again in 30 seconds"
-    sleep 30
-  fi
-done
+# # wait for storage nodes to be ready
+# RETRIES=30
+# for i in $(seq "${RETRIES}"); do
+#   if [[ $(oc get policies -n policies) != *"NonCompliant"* ]]; then
+#     echo "OPP policyset is applied and compliant"
+#     break
+#   else
+#     echo "Try ${i}/${RETRIES}: Policies are not compliant. Checking again in 30 seconds"
+#     sleep 30
+#   fi
+# done
 
-echo "OPP policyset is applied and compliant"
+# echo "OPP policyset is applied and compliant"
