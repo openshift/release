230b60ec0bc530774d1e0a60bcc23347c0d03369
Vadim Rutkovsky <vrutkovs@redhat.com>
2023-01-23 11:31:03 +0100

    assisted-vsphere-teardown: change terraform files path
    After nutanix terraform files have been added to assited-test-infra the terraform file location has shifted.

--- ci-operator/step-registry/assisted/vsphere/teardown/cluster/assisted-vsphere-teardown-cluster-commands.sh
+++ ci-operator/step-registry/assisted/vsphere/teardown/cluster/assisted-vsphere-teardown-cluster-commands.sh
@@ -7,5 +7,6 @@ set -o pipefail
 echo "************ vsphere assisted test-infra teardown cluster command ************"
 /usr/local/bin/fix_uid.sh
 mkdir -p /home/assisted-test-infra/build/cluster/
-scp -F ${SHARED_DIR}/ssh_config -r "root@ci_machine:/home/assisted/build/terraform/*" "/home/assisted-test-infra/build/cluster/"
-for DIR in /home/assisted-test-infra/build/cluster/*; do (cd "$DIR" &&  terraform apply -destroy -input=false -auto-approve); done
+scp -F ${SHARED_DIR}/ssh_config -r "root@ci_machine:/home/assisted/build/terraform/*" "/home/assisted-test-infra/build/cluster/vsphere"
+cd /home/assisted-test-infra/build/cluster/vsphere/vsphere
+terraform apply -destroy -input=false -auto-approve
