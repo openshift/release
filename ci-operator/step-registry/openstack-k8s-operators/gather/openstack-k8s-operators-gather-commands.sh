b1c67210363a02fd779ff9f744242d901e753206
Balazs Gibizer <gibi@redhat.com>
2023-01-24 20:29:45 +0100

    Collect controller logs from the deployment (#35646)

--- ci-operator/step-registry/openstack-k8s-operators/gather/openstack-k8s-operators-gather-commands.sh
+++ ci-operator/step-registry/openstack-k8s-operators/gather/openstack-k8s-operators-gather-commands.sh
@@ -10,3 +10,8 @@ oc project openstack
 oc get OpenStackControlPlane openstack -o json
 
 oc get all
+
+oc get -o yaml MariaDB,RabbitMQCluster,KeystoneAPI,PlacementAPI,Glance,Cinder,NeutronAPI,Nova
+
+oc get pods --selector control-plane=controller-manager -o name | xargs -n1 oc logs
+
