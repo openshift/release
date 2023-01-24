a4b72846995b6e60a6bd6aa645eac03cacf3a957
Andrey Lebedev <alebedev@redhat.com>
2023-01-24 09:13:20 +0100

    aws-load-balancer-operator: e2e test on a cluster with egress proxy

---
+++ ci-operator/step-registry/aws-load-balancer/tag-vpc/aws-load-balancer-tag-vpc-commands.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# ipi-aws-pre-proxy workflow creates the VPC which doesn't have the 'kubernetes.io/cluster' tag required by ALBO.
+# Although the proper tagging is made on the subnets by the openshift installer.
+# This script fills the VPC tagging gap by retrieving the right infrastructure ID from the cluster's subnet and adding it to the VPC.
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+AWSCRED="${CLUSTER_PROFILE_DIR}/.awscred"
+CONFIG="${SHARED_DIR}/install-config.yaml"
+REGION="$(yq-go r "${CONFIG}" 'platform.aws.region')"
+
+if [ -f "${AWSCRED}" ]; then
+    echo "=> configuring aws"
+    export AWS_SHARED_CREDENTIALS_FILE="${AWSCRED}"
+    export AWS_DEFAULT_REGION="${REGION}"
+else
+    echo "Did not find compatible cloud provider cluster_profile"; exit 1
+fi
+
+AWS_SUBNET="$(yq-go r "${CONFIG}" 'platform.aws.subnets[0]')"
+echo "=> Using aws subnet: ${AWS_SUBNET}"
+VPC_ID="$(aws ec2 describe-subnets --subnet-ids "${AWS_SUBNET}" | jq -r '.[][0].VpcId')"
+echo "=> Using vpc id: ${VPC_ID}"
+INFRA_ID=$(aws ec2 describe-subnets --subnet-ids "${AWS_SUBNET}" | jq -r '.[][0].Tags[].Key' | grep 'kubernetes.io/cluster' | cut -d/ -f3)
+echo "=> Using infra id: ${INFRA_ID}"
+
+aws ec2 create-tags --resources "${VPC_ID}" --tags "Key=kubernetes.io/cluster/${INFRA_ID},Value=shared"
