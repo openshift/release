7afcf6b25b437bf0c9cd594ee364ba5166d9509e
ascerra@redhat.com <ascerra@redhat.com>
2023-01-25 21:36:37 -0500

    Add CLC test ref

--- ci-operator/step-registry/acm/tests/clc/acm-tests-clc-commands.sh
+++ ci-operator/step-registry/acm/tests/clc/acm-tests-clc-commands.sh
@@ -3,41 +3,26 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+export KUBECONFIG=${SHARED_DIR}/kubeconfig
 
-# THE FOLLOWING IS JUST FOR EXAMPLE PURPOSES FOR NOW.
-ls -al
-
-# Define the variables needed to create the MTR test configuration file. The variables defined in this step come from files in the `SHARED_DIR` and credentials from Vault.
+# Define the variables needed to create the MTR test configuration file. 
+# The variables defined in this step come from files in the `SHARED_DIR` and credentials from Vault.
 SECRETS_DIR="/tmp/secrets"
 
-# Example of setting a secret in the test script.
-EXAMPLE_CLC_SECRET=$(cat ${SECRETS_DIR}/clc/example_clc_secret)
+cd tmp/clc
+cp ${SECRETS_DIR}/clc/options.yaml ./
+ls
 
-# Login to the Hub cluster
-oc login --insecure-skip-tls-verify -u $HUB_OCP_USERNAME -p $HUB_OCP_PASSWORD $HUB_OCP_API_URL
+# Set the dynamic vars based on provisioned hub cluster.
+CYPRESS_CLC_OCP_IMAGE_VERSION=$(oc get clusterversion -o jsonpath='{.items[].status.history[].ve    rsion}{"\n"}')
+export CYPRESS_CLC_OCP_IMAGE_VERSION
+CYPRESS_BASE_URL=$(oc whoami --show-console)
+export CYPRESS_BASE_URL
+CYPRESS_OPTIONS_HUB_PASSWORD=$(oc get secret acm-interop-aws.kubeadmin-password)
+export CYPRESS_OPTIONS_HUB_PASSWORD
 
-# Run the script that gets the manmaged clusters created on the Hub
-python3 generate_managedclusters_data.py
-# Get the managed clusters info
-cat managedClusters.json |jq -r '.managedClusters[0].name' > /tmp/managed.cluster.name
-cat /tmp/managed.cluster.name
-cat managedClusters.json |jq -r '.managedClusters[0].base_domain' > /tmp/managed.cluster.base.domain
-cat /tmp/managed.cluster.base.domain
-cat managedClusters.json |jq -r '.managedClusters[0].api_url' > /tmp/managed.cluster.api.url
-cat /tmp/managed.cluster.api.url
-cat managedClusters.json |jq -r '.managedClusters[0].username' > /tmp/managed.cluster.username
-cat /tmp/managed.cluster.username
-cat managedClusters.json |jq -r '.managedClusters[0].password' > /tmp/managed.cluster.password
-cat /tmp/managed.cluster.password
+sleep 600
 
+# run the test execution script
+./execute_clc_interop_commands.sh
 
-# cat managedClusters.json |jq -r '.managedClusters[0].name' > managed.cluster.name
-# cat managed.cluster.name
-# cat managedClusters.json |jq -r '.managedClusters[0].base_domain' > managed.cluster.base.domain
-# cat managed.cluster.base.domain
-# cat managedClusters.json |jq -r '.managedClusters[0].api_url' > managed.cluster.api.url
-# cat managed.cluster.api.url
-# cat managedClusters.json |jq -r '.managedClusters[0].username' > managed.cluster.username
-# cat managed.cluster.username
-# cat managedClusters.json |jq -r '.managedClusters[0].password' > managed.cluster.password
-# cat managed.cluster.password
