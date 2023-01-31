62386308f81f5ff712f68ac91fa67b3b3f60d3e9
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-01-31 14:48:23 +0100

    ushift/debug-rebase

--- ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
+++ ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+export DRY_RUN=y
+
+git remote add pmtk https://github.com/pmtk/microshift.git
+git fetch pmtk
+git switch --track pmtk/rebase/recipe
+
 ./scripts/auto-rebase/rebase_job_entrypoint.sh
+
+git diff rebase/recipe
