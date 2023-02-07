a71efebd27787595f8ed743ad5da134fc748b38e
xueqzhan <kenzhang@redhat.com>
2023-02-07 13:49:45 -0500

    Copy kubeconfig as kubeconfig-minimal as soon as it is available

--- ci-operator/step-registry/observers/resource-watch/observers-resource-watch-commands.sh
+++ ci-operator/step-registry/observers/resource-watch/observers-resource-watch-commands.sh
@@ -23,12 +23,16 @@ function cleanup() {
 trap cleanup EXIT
 
 # $KUBECONFIG could not be available when the observer first starts
-echo "waiting for $KUBECONFIG to exist"
-while [ ! -s "$KUBECONFIG" ]
+echo "waiting for $KUBECONFIG or $KUBECONFIGMINIMAL to exist"
+while [[ ! -s "$KUBECONFIG" && ! -s "$KUBECONFIGMINIMAL" ]]
 do
   sleep 1
 done
 echo 'kubeconfig received!'
+
+if ! [[ -s ${KUBECONFIG} ]]; then
+  export KUBECONFIG="$KUBECONFIGMINIMAL"
+fi
 
 # For disconnected or otherwise unreachable environments, we want to
 # have steps use an HTTP(S) proxy to reach the API server. This proxy
@@ -41,6 +45,6 @@ then
   source "${SHARED_DIR}/proxy-conf.sh"
 fi
 
-openshift-tests run-resourcewatch --kubeconfig $KUBECONFIG --namespace default &
-openshift-tests run-monitor --artifact-dir $STORE_PATH &
+openshift-tests run-resourcewatch --kubeconfig $KUBECONFIG --namespace default > "${ARTIFACT_DIR}/run-resourcewatch.log" 2>&1 &
+openshift-tests run-monitor --artifact-dir $STORE_PATH > "${ARTIFACT_DIR}/run-monitor.log" 2>&1 &
 wait
