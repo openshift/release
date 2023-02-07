d4fc5cc240bfcb434a406925450be31be5dcd73d
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-02-07 09:19:56 +0100

    debug ushift/e2e-reboot

--- ci-operator/step-registry/openshift/microshift/e2e/reboot-tests/openshift-microshift-e2e-reboot-tests-commands.sh
+++ ci-operator/step-registry/openshift/microshift/e2e/reboot-tests/openshift-microshift-e2e-reboot-tests-commands.sh
@@ -51,16 +51,96 @@ echo "deployment posted ready status" >&2
 EOF
 chmod +x "${HOME}"/reboot-test.sh
 
+cat > "${HOME}"/debug-info.sh <<'EOF'
+#!/bin/bash
+
+declare -a commands_to_run=()
+function to_run() {
+    cmd="$@"
+    commands_to_run+=("${cmd}")
+}
+
+export KUBECONFIG=/var/lib/microshift/resources/kubeadmin/kubeconfig
+
+to_run oc get cm -n kube-public microshift-version -o=jsonpath='{.data}'
+to_run microshift version
+to_run microshift version -o yaml
+to_run oc version
+to_run sudo crictl version
+to_run uname -a
+to_run cat /etc/*-release
+
+RESOURCES=(nodes configmaps deployments daemonsets statefulsets services routes replicasets persistentvolumeclaims persistentvolumes storageclasses endpoints endpointslices csidrivers csinodes)
+for resource in ${RESOURCES[*]}; do
+    to_run oc get "${resource}" -A
+    to_run oc get "${resource}" -A -o yaml
+done
+
+to_run oc get events -A --sort-by=.metadata.creationTimestamp
+
+TO_DESCRIBE=(deployments daemonsets statefulsets replicasets)
+for ns in $(kubectl get namespace -o jsonpath='{.items..metadata.name}'); do
+    oc get namespace $ns -o yaml
+
+    for resource_type in ${TO_DESCRIBE[*]}; do
+        for resource in $(kubectl get $resource_type -n $ns -o name); do
+            to_run oc describe -n $ns $resource
+        done
+    done
+
+    for pod in $(kubectl get pods -n $ns -o name); do
+            to_run oc describe -n $ns $pod
+            to_run oc get -n $ns $pod -o yaml
+            for container in $(kubectl get -n $ns $pod -o jsonpath='{.spec.containers[*].name}'); do
+                to_run oc logs -n $ns $pod $container
+                to_run oc logs --previous=true -n $ns $pod $container
+            done
+    done
+done
+
+to_run nmcli
+to_run ip a
+to_run ip route
+to_run sudo crictl images --digests
+to_run sudo crictl ps
+to_run sudo crictl pods
+to_run ls -lah /etc/cni/net.d/
+to_run find /etc/cni/net.d/ -type f -exec echo {} \; -exec sudo cat {} \; -exec echo \;
+to_run dnf list --installed
+to_run dnf history
+to_run sudo systemctl -a -l
+to_run sudo journalctl -xu microshift
+to_run sudo journalctl -xu microshift-etcd
+
+echo -e "\n=== DEBUG INFORMATION ===\n"
+echo "Following commands will be executed:"
+for cmd in "${commands_to_run[@]}"; do
+    echo "    - ${cmd}"
+done
+
+for cmd in "${commands_to_run[@]}"; do
+    echo -e "\n\n$ ${cmd}"
+    ${cmd} 2>&1 || true
+done
+EOF
+chmod +x "${HOME}"/debug-info.sh
+
 LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute scp \
   --quiet \
   --project "${GOOGLE_PROJECT_ID}" \
   --zone "${GOOGLE_COMPUTE_ZONE}" \
   --recurse "${HOME}"/reboot-test.sh rhel8user@"${INSTANCE_PREFIX}":~/reboot-test.sh
 
+LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute scp \
+  --quiet \
+  --project "${GOOGLE_PROJECT_ID}" \
+  --zone "${GOOGLE_COMPUTE_ZONE}" \
+  --recurse "${HOME}"/debug-info.sh rhel8user@"${INSTANCE_PREFIX}":~/debug-info.sh
+
 LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute --project "${GOOGLE_PROJECT_ID}" ssh \
   --zone "${GOOGLE_COMPUTE_ZONE}" \
   rhel8user@"${INSTANCE_PREFIX}" \
-  --command 'sudo ~/reboot-test.sh'
+  --command 'sudo ~/reboot-test.sh ; res=$?; sudo ~/debug-info.sh; exit $res'
 
 # now reboot the machine
 LD_PRELOAD=/usr/lib64/libnss_wrapper.so gcloud compute instances stop "${INSTANCE_PREFIX}" --zone "${GOOGLE_COMPUTE_ZONE}"
