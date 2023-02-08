13bdd56872fa7c88aaf2c0876cc3060933f2f8bf
Stephen Benjamin <stephen@bitbin.de>
2023-02-08 14:47:54 -0500

    Revert "Retry on install infrastructure failures (#32756)"
    This reverts commit 49d7ec15f14cbd8f9cd4b727836e907c857061e0.

--- ci-operator/step-registry/gather/must-gather/gather-must-gather-commands.sh
+++ ci-operator/step-registry/gather/must-gather/gather-must-gather-commands.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -o nounset
-set +o errexit
+set -o errexit
+set -o pipefail
 
 function createInstallJunit() {
   EXIT_CODE_CONFIG=3
@@ -10,35 +11,18 @@ function createInstallJunit() {
   EXIT_CODE_CLUSTER=6
   if test -f "${SHARED_DIR}/install-status.txt"
   then
-    EXIT_CODE=`tail -n1 "${SHARED_DIR}/install-status.txt" | awk '{print $1}'`
-    cp "${SHARED_DIR}/install-status.txt" "${ARTIFACT_DIR}/"
+    EXIT_CODE=`cat ${SHARED_DIR}/install-status.txt | awk '{print $1}'`
+    cp "${SHARED_DIR}/install-status.txt" ${ARTIFACT_DIR}/
     if [ "$EXIT_CODE" ==  0  ]
     then
-      grep -q "^$EXIT_CODE_INFRA$" "${SHARED_DIR}/install-status.txt"
-      PREVIOUS_INFRA_FAILURE=$((1-$?))
-
       cat >"${ARTIFACT_DIR}/junit_install.xml" <<EOF
-      <testsuite name="cluster install" tests="6" failures="$PREVIOUS_INFRA_FAILURE">
+      <testsuite name="cluster install" tests="6" failures="0">
         <testcase name="install should succeed: other"/>
         <testcase name="install should succeed: configuration"/>
         <testcase name="install should succeed: infrastructure"/>
         <testcase name="install should succeed: cluster bootstrap"/>
         <testcase name="install should succeed: cluster creation"/>
         <testcase name="install should succeed: overall"/>
-EOF
-
-      # If we ultimately succeeded, but encountered at least 1 infra
-      # failure, insert that failure case so CI tracks it as a flake.
-      if [ "$PREVIOUS_INFRA_FAILURE" = 1 ]
-      then
-      cat >>"${ARTIFACT_DIR}/junit_install.xml" <<EOF
-        <testcase name="install should succeed: infrastructure">
-          <failure message="">openshift cluster install failed with infrastructure setup</failure>
-        </testcase>
-EOF
-      fi
-
-      cat >>"${ARTIFACT_DIR}/junit_install.xml" <<EOF
       </testsuite>
 EOF
     elif [ "$EXIT_CODE" == "$EXIT_CODE_CONFIG" ]
@@ -53,6 +37,7 @@ EOF
           <failure message="">openshift cluster install failed overall</failure>
         </testcase>
       </testsuite>
+EOF
 EOF
     elif [ "$EXIT_CODE" == "$EXIT_CODE_INFRA" ]
     then
@@ -118,7 +103,7 @@ EOF
 # see https://github.com/elmiko/camgi.rs for more information
 function installCamgi() {
     CAMGI_VERSION="0.8.1"
-    pushd /tmp || exit
+    pushd /tmp
 
     # no internet access in C2S/SC2S env, disable proxy
     if [[ "${CLUSTER_TYPE:-}" =~ ^aws-s?c2s$ ]]; then
@@ -141,7 +126,7 @@ function installCamgi() {
       source "${SHARED_DIR}/proxy-conf.sh"
     fi
 
-    popd || exit
+    popd
 }
 
 if test ! -f "${KUBECONFIG}"
