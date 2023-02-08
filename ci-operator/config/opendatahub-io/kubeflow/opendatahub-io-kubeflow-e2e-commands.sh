4979810dd264200113a694f785e20a198a4fe9e1
mroman <mroman@redhat.com>
2023-02-08 09:45:07 +0100

    Fix bash script

--- ci-operator/config/opendatahub-io/kubeflow/opendatahub-io-kubeflow-e2e-commands.sh
+++ ci-operator/config/opendatahub-io/kubeflow/opendatahub-io-kubeflow-e2e-commands.sh
@@ -1,21 +1,21 @@
-        #Gather tag and image for deployment
-        IN=$ODH_NOTEBOOK_CONTROLLER_IMAGE
-        arrIN=(${IN//:/ })
-        export IMG=${arrIN[1]}  
-        export TAG=${arrIN[2]}
+#Gather tag and image for deployment
+IN=$ODH_NOTEBOOK_CONTROLLER_IMAGE
+arrIN=(${IN//:/ })
+export IMG=${arrIN[0]}  
+export TAG=${arrIN[1]}
 
-        # Setup and test odh-nbc
-        pushd components/odh-notebook-controller
-        oc new-project odh-notebook-controller-system
-        make deploy -e K8S_NAMESPACE=odh-notebook-controller-system
+# Setup and test odh-nbc
+pushd components/odh-notebook-controller
+oc new-project odh-notebook-controller-system
+make deploy -e K8S_NAMESPACE=odh-notebook-controller-system
 
-        mkdir -p ~/.kube
-        cp /tmp/kubeconfig ~/.kube/config 2> /dev/null || cp /var/run/secrets/ci.openshift.io/multi-stage/kubeconfig ~/.kube/config
-        chmod 644 ~/.kube/config
-        export KUBECONFIG=~/.kube/config
+# Copy the current KUBECONFIG to a writable location so we modify the current context for the test run
+cp $KUBECONFIG /tmp/kubeconfig
+chmod 644 /tmp/kubeconfig
+export KUBECONFIG=/tmp/kubeconfig
 
-        make e2e-test -e K8S_NAMESPACE=odh-notebook-controller-system
+make e2e-test -e K8S_NAMESPACE=odh-notebook-controller-system
 
-        # Clean up
-        make undeploy -e K8S_NAMESPACE=odh-notebook-controller-system
-        oc delete project odh-notebook-controller-system
+# Clean up
+make undeploy -e K8S_NAMESPACE=odh-notebook-controller-system
+oc delete project odh-notebook-controller-system
