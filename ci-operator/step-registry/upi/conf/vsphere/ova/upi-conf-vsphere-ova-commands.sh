9f5b9b36ab13178620bf79c38763f572fac9abce
vr4manta <vr4manta@gmail.com>
2023-02-09 07:45:13 -0500

    Fixed variable to work with set -o.

--- ci-operator/step-registry/upi/conf/vsphere/ova/upi-conf-vsphere-ova-commands.sh
+++ ci-operator/step-registry/upi/conf/vsphere/ova/upi-conf-vsphere-ova-commands.sh
@@ -56,7 +56,7 @@ for i in "${!DATACENTERS[@]}"; do
 
     # Search the found networks for a network that exists in the target cluster.
     echo "$(date -u --rfc-3339=seconds) - Validating network configuration..."
-    if [ ${#NETWORKS[@]} > 1 ]; then
+    if [ ${!NETWORKS[@]} > 1 ]; then
         for NET in $NETWORKS; do
             case "$NET" in
                 DistributedVirtualPortgroup*)
