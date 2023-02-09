8705ba2d6a70535b888a7c810d554d73ee173eb9
Paige Rubendall <prubenda@redhat.com>
2023-02-09 10:41:04 -0500

    adding es server

--- ci-operator/step-registry/openshift-qe/node-density/openshift-qe-node-density-commands.sh
+++ ci-operator/step-registry/openshift-qe/node-density/openshift-qe-node-density-commands.sh
@@ -11,10 +11,17 @@ pushd /tmp
 python -m virtualenv ./venv_qe
 source ./venv_qe/bin/activate
 
+ls -la /secret
+ES_PASSWORD=$(cat "/secret/password")
+ES_USERNAME=$(cat "/secret/username")
+
+echo "username $ES_USERNAME"
 git clone https://github.com/cloud-bulldozer/e2e-benchmarking
 pushd e2e-benchmarking/workloads/kube-burner
 export WORKLOAD=node-density
 export NODE_COUNT=3
 export PODS_PER_NODE=245
 export POD_READY_THRESHOLD=120000ms
+export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
+
 ./run.sh
