d959962d41f3f9eea653f9126ad17c58bb57706b
Diane Frazzette <dfrazzet@redhat.com>
2023-02-09 12:42:41 -0500

    Update ci-operator/step-registry/sbo/tests/acceptance/sbo-tests-acceptance-commands.sh
    Co-authored-by: Pavel Macík <pavel.macik@gmail.com>

--- ci-operator/step-registry/sbo/tests/acceptance/sbo-tests-acceptance-commands.sh
+++ ci-operator/step-registry/sbo/tests/acceptance/sbo-tests-acceptance-commands.sh
@@ -6,9 +6,16 @@ set -o pipefail
 
 export KUBECONFIG=${SHARED_DIR}/kubeconfig
 
-if [ "$(echo -e $CLONEREFS_OPTIONS | jq -r '.refs[] | select(.org == "openshift").repo')" == "release" ]; then
-  OPERATOR_INDEX_IMAGE_REF=quay.io/redhat-developer/servicebinding-operator:index;
-else
-  OPERATOR_INDEX_IMAGE_REF=quay.io/redhat-developer/servicebinding-operator:pr-${PULL_NUMBER}-${PULL_PULL_SHA:0:8}-index;
-fi;
-make -k VERBOSE=2 OPERATOR_INDEX_IMAGE_REF=$OPERATOR_INDEX_IMAGE_REF CATSRC_NAME=sbo-pr-checks SKIP_REGISTRY_LOGIN=true EXTRA_BEHAVE_ARGS="--tags=~@crdv1beta1 --tags=~@olm-descriptors --tags=~@upgrade-with-olm --tags=~@disable-openshift-4.12 --tags=~@disable-openshift-4.8+ --tags=~@disable-openshift-4.9+ --tags=~@disable-openshift-4.10+ --tags=~@disable-openshift-4.11+ --tags=~@disable-openshift-4.12+" -o registry-login test-acceptance-with-bundle test-acceptance-artifacts
+# Install SBO from `redhat-operators` catalog source
+export CATSRC_NAME=redhat-operators
+export OPERATOR_CHANNEL=stable
+export OPERATOR_PACKAGE=rh-service-binding-operator
+export SKIP_REGISTRY_LOGIN=true
+
+./install.sh 
+
+# Execute Acceptance Tests
+export TEST_ACCEPTANCE_START_SBO=remote
+export EXTRA_BEHAVE_ARGS="--tags=~@crdv1beta1 --tags=~@olm-descriptors --tags=~@upgrade-with-olm --tags=~@disable-openshift-4.12 --tags=~@disable-openshift-4.8+ --tags=~@disable-openshift-4.9+ --tags=~@disable-openshift-4.10+ --tags=~@disable-openshift-4.11+ --tags=~@disable-openshift-4.12+"
+
+make -k VERBOSE=2 -o registry-login test-acceptance test-acceptance-artifacts
