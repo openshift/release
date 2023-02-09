f5760b9d007eb619494c49eab081e66b3236302a
Yuanlin <yuanlin.xu@redhat.com>
2023-02-09 13:14:40 -0500

    Test cluster claim
    Signed-off-by: Yuanlin <yuanlin.xu@redhat.com>

--- ci-operator/step-registry/servicemesh/istio/e2e/servicemesh-istio-e2e-commands.sh
+++ ci-operator/step-registry/servicemesh/istio/e2e/servicemesh-istio-e2e-commands.sh
@@ -75,15 +75,6 @@ spec:
         add: ["IPC_LOCK","SYS_ADMIN"]
       privileged: true
       runAsUser: 0
-    resources:
-      requests:
-        memory: 1Gi
-        cpu: '2'
-        ${RESOURCE_REQUEST:-}
-      limits:
-        memory: 1Gi
-        cpu: '2'
-        ${RESOURCE_REQUEST:-}
     volumeMounts:
     - mountPath: /lib/modules
       name: modules
