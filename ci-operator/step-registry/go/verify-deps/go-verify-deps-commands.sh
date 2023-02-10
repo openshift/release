83a099b4d4c05ae798b93fa3d93eec2aca71fdec
Jan Safranek <jsafrane@redhat.com>
2023-02-10 10:56:37 +0100

    Add verify-deps job to our CSI driver forks
    We want to make sure that vendor/ directory is correct to make sure
    official offline builds pass.

---
+++ ci-operator/step-registry/go/verify-deps/go-verify-deps-commands.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -o errexit # Nozero exit code of any of the commands below will fail the test.
+set -o nounset
+set -o pipefail
+
+die_modlist() {
+    echo "ERROR: go list -mod=readonly -m all failed with the message listed above."
+    echo "Cachito used in ART builds will fail to resolve dependencies offline."
+    echo "Typically, a pinned version is missing in go.mod, e.g.:"
+    echo "    replace k8s.io/foo => k8s.io/foo v0.26.0"
+    exit 1
+}
+
+echo "Checking that all modules can be resolved offline"
+go list -mod=readonly -m all || die_modlist
+
+echo "Checking that vendor/ is correct"
+go mod tidy
+go mod vendor
+CHANGES=$(git status --porcelain)
+if [ -n "$CHANGES" ] ; then
+    echo "ERROR: detected vendor inconsistency after 'go mod tidy; go mod vendor':"
+    echo "$CHANGES"
+    exit 1
+fi
