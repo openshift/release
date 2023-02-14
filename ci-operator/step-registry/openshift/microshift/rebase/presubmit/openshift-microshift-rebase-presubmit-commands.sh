06d496c38f9c4a6b85a11b5a4226139b0a9d2237
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-02-14 10:16:59 +0100

    test

--- ci-operator/step-registry/openshift/microshift/rebase/presubmit/openshift-microshift-rebase-presubmit-commands.sh
+++ ci-operator/step-registry/openshift/microshift/rebase/presubmit/openshift-microshift-rebase-presubmit-commands.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+printenv
+
+git remote add pmtk https://github.com/pmtk/microshift.git
+git fetch pmtk
+git switch --track pmtk/rebase/recipe-simple-presubmit
+
 ./scripts/auto-rebase/presubmit.py
