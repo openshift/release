82633169e717acb1e0769cecb86c201f9cd60f4d
Siddhesh Ghadi <Siddhesh.Ghadi@ibm.com>
2023-02-15 05:15:41 +0000

    Fix debug commands

--- ci-operator/step-registry/stackrox/automation-flavors/powervs-e2e/cluster-create/stackrox-automation-flavors-powervs-e2e-cluster-create-commands.sh
+++ ci-operator/step-registry/stackrox/automation-flavors/powervs-e2e/cluster-create/stackrox-automation-flavors-powervs-e2e-cluster-create-commands.sh
@@ -9,11 +9,11 @@ POWERVS_OCP_DIR="/tmp/powervs-ocp"
 SECRET_DIR="/tmp/vault/powervs-rhr-creds"
 
 # DEBUG
-ls -l "$SECRET_DIR"
-whoami
-uid
-cat /etc/os-release
-sudo whoami
+ls -l "$SECRET_DIR" || true
+whoami || true
+id || true
+cat /etc/os-release || true
+sudo whoami || true
 
 # tmp dir for cluster files
 mkdir -p "$POWERVS_OCP_DIR"
@@ -70,7 +70,7 @@ function setup_automation(){
   curl https://raw.githubusercontent.com/ocp-power-automation/openshift-install-power/devel/openshift-install-powervs --output openshift-install-powervs --silent
   chmod +x openshift-install-powervs
     
-  ./openshift-install-powervs setup &> "${POWERVS_OCP_DIR}/setup-log.txt"
+  ./openshift-install-powervs setup
   
   cp -f ${PULL_SECRET_FILE} ${POWERVS_OCP_DIR}/pull-secret.txt
   cp -f ${PUBLIC_KEY_FILE} ${POWERVS_OCP_DIR}/id_rsa.pub
