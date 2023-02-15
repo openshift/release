26eaa94babfef445fd32685f1a3b91a15f7e4202
Christian Glombek <cglombek@redhat.com>
2023-02-15 16:22:44 +0100

    hack: annotate `release-scos-next` imagestream

--- hack/annotate.sh
+++ hack/annotate.sh
@@ -79,6 +79,7 @@ done
 
 annotate "origin" "release" "okd-4.y-stable.json"
 annotate "origin" "release-scos" "okd-scos-4.y-stable.json"
+annotate "origin" "release-scos-next" "okd-scos-4.y-next.json"
 annotate "ocp" "release" "ocp-4.y-stable.json"
 annotate "ocp-s390x" "release-s390x" "ocp-4.y-stable-s390x.json"
 annotate "ocp-ppc64le" "release-ppc64le" "ocp-4.y-stable-ppc64le.json"
