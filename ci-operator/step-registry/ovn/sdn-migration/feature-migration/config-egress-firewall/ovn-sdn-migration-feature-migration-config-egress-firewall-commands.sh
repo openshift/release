7055e33d9f6aeb9affc77236c26543fac99d5e7c
Peng Liu <pliu@redhat.com>
2023-02-15 22:56:38 +0800

    Add test for SDN reverse migration (#34526)
    We will supoort SDN reverse migration since 4.13. It means users start
    from a cluster with OVN-K installed, then they want to switch the network
    plugins to OpenShiftSDN.
    
    Signed-off-by: Peng Liu <pliu@redhat.com>

--- ci-operator/step-registry/ovn/sdn-migration/feature-migration/config-egress-firewall/ovn-sdn-migration-feature-migration-config-egress-firewall-commands.sh
+++ ci-operator/step-registry/ovn/sdn-migration/feature-migration/config-egress-firewall/ovn-sdn-migration-feature-migration-config-egress-firewall-commands.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-oc create ns test-migration
+config_egress_network_policy_cr() {
 cat <<EOF | oc apply -f -
 apiVersion: network.openshift.io/v1
 kind: EgressNetworkPolicy
@@ -14,6 +14,36 @@ metadata:
 spec:
   egress: []
 EOF
+  oc patch egressnetworkpolicy -n test-migration default --type='merge' --patch "${JSON_PATCH}"
+}
+
+config_egress_firewall_cr() {
+cat <<EOF | oc apply -f -
+apiVersion: k8s.ovn.org/v1
+kind: EgressFirewall
+metadata:
+  name: default
+  namespace: test-migration
+spec:
+  egress: []
+EOF
+  oc patch egressfirewalls -n test-migration default --type='merge' --patch "${JSON_PATCH}"
+}
+
+TMPDIR=$(mktemp -d)
+pushd ${TMPDIR}
+
+echo "check the cluster running CNI"
+RUNNING_CNI=$(oc get network.operator cluster -o=jsonpath='{.spec.defaultNetwork.type}')
+
 PATCH=(\{\"spec\": "$EGRESS_FIREWALL_SPEC"\})
 JSON_PATCH=$(echo "${PATCH[@]}")
-oc patch egressnetworkpolicy -n test-migration default --type='merge' --patch "${JSON_PATCH}"
+oc create ns test-migration
+
+if [[ $RUNNING_CNI == "OpenShiftSDN" ]]; then
+  echo "It's an OpenShiftSDN cluster, create a EgressNetworkPolicy CR"
+  config_egress_network_policy_cr
+elif [[ $RUNNING_CNI == "OVNKubernetes" ]]; then
+  echo "It's an OVNKubernetes cluster, create a EgressFirewall CR"
+  config_egress_firewall_cr
+fi
