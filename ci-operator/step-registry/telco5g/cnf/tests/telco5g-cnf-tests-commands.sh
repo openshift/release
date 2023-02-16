0216b66cc4deb8cae5741016888de07bc95ed046
Sergey <6213510+sshnaidm@users.noreply.github.com>
2023-02-16 13:21:29 +0200

    telco5g: Add jobs router - collect config and define env vars (#36382)
    Discover all required variables to define the job type and trigger.
    Collect them into one file that will be sources in all steps.

--- ci-operator/step-registry/telco5g/cnf/tests/telco5g-cnf-tests-commands.sh
+++ ci-operator/step-registry/telco5g/cnf/tests/telco5g-cnf-tests-commands.sh
@@ -106,6 +106,8 @@ function get_skip_tests {
     echo "${skip_list}"
 }
 
+source $SHARED_DIR/main.env
+
 export FEATURES="${FEATURES:-sriov performance sctp xt_u32 ovn metallb multinetworkpolicy}" # next: ovs_qos
 export SKIP_TESTS_FILE="${SKIP_TESTS_FILE:-${SHARED_DIR}/telco5g-cnf-tests-skip-list.txt}"
 export SCTPTEST_HAS_NON_CNF_WORKERS="${SCTPTEST_HAS_NON_CNF_WORKERS:-false}"
