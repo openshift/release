9cbde4c6cea7c4a8638497fd6828410d80c372df
Sergey <6213510+sshnaidm@users.noreply.github.com>
2023-02-16 17:24:24 +0200

    telco5g: Fix unset variables in env vars discovery (#36472)

--- ci-operator/step-registry/telco5g/jobs-router/telco5g-jobs-router-commands.sh
+++ ci-operator/step-registry/telco5g/jobs-router/telco5g-jobs-router-commands.sh
@@ -11,20 +11,20 @@ echo "************ telco cluster setup command ************"
 export MAINENV="$SHARED_DIR/main.env"
 
 echo "# THERE IS THE ENVS FILE FOR JOBS" > $MAINENV
-echo "export DEBUG_PROW_JOB_ID=${PROW_JOB_ID:-''}" >> $MAINENV
-echo "export DEBUG_JOB_NAME=${JOB_NAME:-''}" >> $MAINENV
-echo "export DEBUG_JOB_TYPE=${JOB_TYPE:-''}" >> $MAINENV
-echo "export DEBUG_RELEASE_IMAGE_LATEST=${RELEASE_IMAGE_LATEST:-''}" >> $MAINENV
+echo "export DEBUG_PROW_JOB_ID=${PROW_JOB_ID}" >> $MAINENV
+echo "export DEBUG_JOB_NAME=${JOB_NAME}" >> $MAINENV
+echo "export DEBUG_JOB_TYPE=${JOB_TYPE}" >> $MAINENV
+echo "export DEBUG_RELEASE_IMAGE_LATEST=${RELEASE_IMAGE_LATEST-}" >> $MAINENV
 echo "#######################################################" >> $MAINENV
 
 if [[ "$PROW_JOB_ID" = *"nightly"* ]] && [[ "$JOB_TYPE" == "periodic" ]]; then
     echo "export T5_JOB_TRIGGER=nightly" >> $MAINENV
     echo "export T5_JOB_DESC=nightly-${T5CI_JOB_TYPE}" >> $MAINENV
     echo "export T5_JOB_DESC_FULL=nightly-${T5CI_JOB_TYPE}-${T5CI_VERSION}" >> $MAINENV
-    if [[ -n "$RELEASE_IMAGE_LATEST" ]]; then
-        export IMG=$RELEASE_IMAGE_LATEST
-    elif [[ -n "$RELEASE_IMAGE_INITIAL" ]]; then
-        export IMG=$RELEASE_IMAGE_INITIAL
+    if [[ -n "${RELEASE_IMAGE_LATEST-}" ]]; then
+        export IMG=${RELEASE_IMAGE_LATEST-}
+    elif [[ -n "${RELEASE_IMAGE_INITIAL-}" ]]; then
+        export IMG=${RELEASE_IMAGE_INITIAL-}
     else
         export IMG=${PROW_JOB_ID/-telco5g/}
     fi
@@ -41,7 +41,7 @@ elif [[ "$JOB_TYPE" == "presubmit" ]] || [[ "$JOB_NAME" == *"rehears"* ]]; then
     echo "export T5_JOB_DESC=rehearse-${T5CI_JOB_TYPE}" >> $MAINENV
     echo "export T5_JOB_DESC_FULL=rehearse-${T5CI_JOB_TYPE}-${T5CI_VERSION}" >> $MAINENV
     echo "export KCLI_PARAM=\"-P tag=${T5CI_VERSION} -P version=nightly\"" >> $MAINENV
-    echo "export PULL_URL=https://github.com/${REPO_OWNER}/${REPO_NAME}/pull/${PULL_NUMBER}" >> $MAINENV
-    echo "export PULL_API_URL=https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/pulls/${PULL_NUMBER}" >> $MAINENV
+    echo "export PULL_URL=https://github.com/${REPO_OWNER-}/${REPO_NAME-}/pull/${PULL_NUMBER-}" >> $MAINENV
+    echo "export PULL_API_URL=https://api.github.com/repos/${REPO_OWNER-}/${REPO_NAME-}/pulls/${PULL_NUMBER-}" >> $MAINENV
 fi
 cp $MAINENV $ARTIFACT_DIR/main.env
