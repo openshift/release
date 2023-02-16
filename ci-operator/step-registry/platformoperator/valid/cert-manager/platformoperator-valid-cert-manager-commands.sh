0de104366da5513c586b01fcc0eae70a22724f60
Jian Zhang <jiazha@redhat.com>
2023-02-16 20:54:25 +0800

    add some platform operators before cluster building (#33471)

---
+++ ci-operator/step-registry/platformoperator/valid/cert-manager/platformoperator-valid-cert-manager-commands.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+cat <<EOF > ${SHARED_DIR}/manifest_cert_manager.yaml 
+---
+apiVersion: platform.openshift.io/v1alpha1
+kind: PlatformOperator
+metadata:
+  name: cert-manager-po
+spec:
+  package:
+    name: openshift-cert-manager-operator
+EOF
