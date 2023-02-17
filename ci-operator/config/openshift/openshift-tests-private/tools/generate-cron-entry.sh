3d0fd098bbcce4f07aa55ccdb00ef245f135bcc0
Liang Xia <lxia@redhat.com>
2023-02-17 17:57:46 +0800

    Enhance date format in cron entries

--- ci-operator/config/openshift/openshift-tests-private/tools/generate-cron-entry.sh
+++ ci-operator/config/openshift/openshift-tests-private/tools/generate-cron-entry.sh
@@ -61,11 +61,12 @@ case "$FN" in
 		echo "$MINUTE $HOUR * * *"
 		;;
 	2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31)
-		DAY_OF_MONTH_FINAL=$DAY_OF_MONTH
+		DAY_OF_MONTH_TMP=$DAY_OF_MONTH
 		for ((i=1 ; i<31/FN; ++i)) ; do
-			let DAY_OF_MONTH_TMP=(i*FN+DAY_OF_MONTH)%31+1
-			DAY_OF_MONTH_FINAL+=",$DAY_OF_MONTH_TMP"
+			let TMP=(i*FN+DAY_OF_MONTH)%31+1
+			DAY_OF_MONTH_TMP+=",$TMP"
 		done
+		DAY_OF_MONTH_FINAL=$(echo $DAY_OF_MONTH_TMP | sed 's/,/\n/g' | sort -n | paste -sd ',')
 		echo "$MINUTE $HOUR $DAY_OF_MONTH_FINAL * *"
 		;;
 	32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60)
