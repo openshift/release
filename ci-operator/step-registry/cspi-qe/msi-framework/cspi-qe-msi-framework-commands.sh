d4324d43affe3987f3086d3ce12d245607df146f
rnetser <rnetser@redhat.com>
2023-02-19 10:52:13 +0200

    wip: fw image mirror
    Co-authored-by: myakove <myakove@redhat.com>

--- ci-operator/step-registry/cspi-qe/msi-framework/cspi-qe-msi-framework-commands.sh
+++ ci-operator/step-registry/cspi-qe/msi-framework/cspi-qe-msi-framework-commands.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-set -o nounset
-set -o errexit
-set -o pipefail
 set -o verbose
 
 ADMIN_PASSWORD=$(cat "${KUBEADMIN_PASSWORD_FILE}")
 CLUSTER_API_URL=$(oc whoami --show-server)
-OUTPUT_DIR=${ARTIFACT_DIR}
 
 export USER_NAME=kubeadmin
 export PASSWORD="${ADMIN_PASSWORD}"
@@ -15,5 +11,6 @@ export API_URL="${CLUSTER_API_URL}"
 export NAMESPACE=openshift-streams
 export KUBECONFIG=${SHARED_DIR}/kubeconfig
 
+#/home/podman/.local/bin/poetry run python3 /home/podman/managed-services-integration-framework/ms-integration-framework/ms_interop_framework_execution_framework.py
 
-/bin/bash -c "make interop && cp -r /tmp/ $OUTPUT_DIR && oc delete -f scenario -n $NAMESPACE"
+sleep 50000
