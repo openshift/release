a3f2c5264f4d5cc8ac6b60b81693d385409c1250
Adrien Gentil <agentil@redhat.com>
2023-02-22 11:24:17 +0100

    MGMT-12454: get sos report from ARM machine
    - default inventory file if it is not already existing, we expect at
      least one machine in the `primary` group
    - reorder the gather playbook to gather common information (sosreport)
      from all the hosts, and specific infra-env information from the
      primary group

--- ci-operator/step-registry/assisted/common/setup/prepare/assisted-common-setup-prepare-commands.sh
+++ ci-operator/step-registry/assisted/common/setup/prepare/assisted-common-setup-prepare-commands.sh
@@ -36,12 +36,17 @@ cat > packing-test-infra.yaml <<-EOF
       ansible.builtin.file:
         path: "{{ SHARED_DIR }}/platform-conf.sh"
         state: touch
-    - name: Create ansible inventory
+    - name: Check if ansible inventory exists
+      stat:
+        path: "{{ SHARED_DIR }}/inventory"
+      register: inventory
+    - name: Create default ansible inventory
       ansible.builtin.copy:
         dest: "{{ SHARED_DIR }}/inventory"
         content: |
-          [all]
-          {{ lookup('env', 'IP') }} ansible_user=root ansible_ssh_user=root ansible_ssh_private_key_file={{ lookup('env', 'SSH_KEY_FILE') }} ansible_ssh_common_args="-o ConnectTimeout=5 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=90 -o LogLevel=ERROR"
+          [primary]
+          primary-{{ lookup('env', 'IP') }} ansible_host={{ lookup('env', 'IP') }} ansible_user=root ansible_ssh_user=root ansible_ssh_private_key_file={{ lookup('env', 'SSH_KEY_FILE') }} ansible_ssh_common_args="-o ConnectTimeout=5 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=90 -o LogLevel=ERROR"
+      when: not inventory.stat.exists
     - name: Create ssh config file
       ansible.builtin.copy:
         dest: "{{ SHARED_DIR }}/ssh_config"
@@ -137,7 +142,7 @@ EOF
 
 cat > run_test_playbook.yaml <<-"EOF"
 - name: Prepare remote host
-  hosts: all
+  hosts: primary
   vars:
     PLATFORM: "{{ lookup('env', 'PLATFORM') }}"
     PULL_PULL_SHA: "{{ lookup('env', 'PULL_PULL_SHA') | default('master', True) }}"
