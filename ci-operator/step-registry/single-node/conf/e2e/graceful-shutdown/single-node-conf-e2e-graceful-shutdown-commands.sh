41bbda9e5cde3dcbf71412ed9e4031464c8a2d1b
Jeremy Peterson <jerpeter@redhat.com>
2023-02-26 23:03:02 -0500

    Download yq if not available

--- ci-operator/step-registry/single-node/conf/e2e/graceful-shutdown/single-node-conf-e2e-graceful-shutdown-commands.sh
+++ ci-operator/step-registry/single-node/conf/e2e/graceful-shutdown/single-node-conf-e2e-graceful-shutdown-commands.sh
@@ -4,5 +4,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Download yq
+
+if ! command -v yq &> /dev/null
+then
+  curl -L "https://github.com/mikefarah/yq/releases/download/v4.30.5/yq_linux_$(uname -m | sed 's/aarch64/arm64/;s/x86_64/amd64/')" \
+    -o /tmp/yq && chmod +x /tmp/yq
+fi
+
 # set shutdown grace period to enable testing graceful shutdown
-yq e -i '.spec.kubeletConfig |= . + {"shutdownGracePeriod": "600s", "shutdownGracePeriodCriticalPods": "300s"}' "${SHARED_DIR}/manifest_single-node-kubeletconfig.yml"
+/tmp/yq e -i '.spec.kubeletConfig |= . + {"shutdownGracePeriod": "600s", "shutdownGracePeriodCriticalPods": "300s"}' "${SHARED_DIR}/manifest_single-node-kubeletconfig.yml"
