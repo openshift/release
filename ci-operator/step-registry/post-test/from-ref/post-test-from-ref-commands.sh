9564ae67a5240ee675e15f6ff8b289c181e13247
ascerra@redhat.com <ascerra@redhat.com>
2023-03-02 16:06:25 -0500

    Test post behavior

---
+++ ci-operator/step-registry/post-test/from-ref/post-test-from-ref-commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+echo "This is the pod that is running the post step from a ref in the config file."
+
+sleep 1800
---
+++ ci-operator/step-registry/post-test/from-workflow/post-test-from-workflow-commands.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+echo "This is the pod that is running the post step from the workflow"
