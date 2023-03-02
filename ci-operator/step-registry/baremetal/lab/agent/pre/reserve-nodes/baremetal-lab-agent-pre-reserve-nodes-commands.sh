4508e402ca0787e785f2758a35b220a679d72da2
Biagio Manzari <bmanzari@redhat.com>
2023-03-02 13:10:23 +0100

    - Use CI provided pull secret - Update to latest OCP version

--- ci-operator/step-registry/baremetal/lab/agent/pre/reserve-nodes/baremetal-lab-agent-pre-reserve-nodes-commands.sh
+++ ci-operator/step-registry/baremetal/lab/agent/pre/reserve-nodes/baremetal-lab-agent-pre-reserve-nodes-commands.sh
@@ -22,7 +22,13 @@ SSH_PUBLIC_KEY=\"$(ssh "${SSHOPTS[@]}" root@"${AUX_HOST}" cat /root/.ssh/id_rsa.
 
 echo "Connecting to ${AUX_HOST} to retrieve docker pull secret"
 
-PULL_SECRET=\'$(ssh "${SSHOPTS[@]}" root@"${AUX_HOST}" cat /root/.docker/config.json | jq -c)\'
+#PULL_SECRET=\'$(ssh "${SSHOPTS[@]}" root@"${AUX_HOST}" cat /root/.docker/config.json | jq -c)\'
+
+PULL_SECRET=$(< /tmp/secret/pull-secret/.dockerconfigjson jq -c)
+
+#PULL_SECRET_FILE="/tmp/secret/pull-secret/.dockerconfigjson"
+
+echo "Showing shared dir contents $(ls "${SHARED_DIR}")"
 
 if [ "${DEPLOYMENT_TYPE}" == "sno" ]; then
     N_WORKERS=1
