1af8763f22f0c5e95ea00301f65910bfe7733847
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-03-07 16:46:18 +0100

    wip hold do not merge /cc /hold

--- ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
+++ ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+export DRY_RUN=y
+
+git remote add pmtk https://github.com/pmtk/microshift.git
+git fetch pmtk
+git checkout -t pmtk/debug-failed-make-vendor
+
+set +e
 ./scripts/auto-rebase/rebase_job_entrypoint.sh
+res=$?
+
+git status
+echo -e '\n\n\n\n\n'
+git diff main
+
+exit $res
