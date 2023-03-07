d7e2e1f451b2df7130d083da98705b1e13bc7e46
Alessandro Di Stefano <aleskandro@redhat.com>
2023-03-07 22:05:05 +0100

    Baseline agent-installer steps for the BM labs automation (#37002)
    * Agent baseline steps
    
    * Apply doc suggestions from code review
    
    Co-authored-by: Manoj Hans <mhans@redhat.com>
    
    * Fix code from review
    
    * Update and dedup OWNERS
    
    ---------
    
    Co-authored-by: Manoj Hans <mhans@redhat.com>

--- ci-operator/step-registry/baremetal/lab/pre/reserve-nodes/baremetal-lab-pre-reserve-nodes-commands.sh
+++ ci-operator/step-registry/baremetal/lab/pre/reserve-nodes/baremetal-lab-pre-reserve-nodes-commands.sh
@@ -59,3 +59,30 @@ EOF
 echo "Node reservation concluded successfully."
 scp "${SSHOPTS[@]}" "root@${AUX_HOST}:/var/builds/${CLUSTER_NAME}/*.yaml" "${SHARED_DIR}/"
 more "${SHARED_DIR}"/*.yaml |& sed 's/pass.*$/pass ** HIDDEN **/g'
+
+
+# Example host element from the list in the hosts.yaml file:
+# - mac: 34:73:5a:9d:eb:e1 # The mac address of the interface connected to the baremetal network
+#  vendor: dell
+#  ip: *****
+#  host: openshift-qe-054
+#  arch: x86_64
+#  root_device: /dev/sdb
+#  root_dev_hctl: ""
+#  provisioning_mac: 34:73:5a:9d:eb:e2 # The mac address of the interface connected to the provisioning network (based on dynamic native-vlan)
+#  switch_port: ""
+#  switch_port_v2: ge-1/0/23@10.1.233.31:22 # Port in the managed switch (JunOS)
+#  ipi_disabled_ifaces: eno1 # Interfaces to disable in the hosts
+#  baremetal_iface: eno2 # The interface connected to the baremetal network
+#  bmc_address: openshift-qe-054-drac.mgmt..... # The address of the BMC
+#  bmc_scheme: ipmi
+#  bmc_base_uri: /
+#  bmc_user: ... # these are the ipmi credentials
+#  bmc_pass: ...
+#  console_kargs: tty1;ttyS0,115200n8 # The serial console kargs needed at boot time for allowing remote viewing of the console
+#  transfer_protocol_type: http # VirtualMedia Transfer Protocol Type
+#  redfish_user: ... # redfish credentials, ipmi and redfish credentials differ in some cases
+#  redfish_password: ...
+#  build_id: ci-op-testaget # not usually needed as it is the same as CLUSTER_NAME
+#  build_user: ci-op
+#  name: master-02 # This name must be either master or worker or bootstrap in order for the steps to set the role correctly
