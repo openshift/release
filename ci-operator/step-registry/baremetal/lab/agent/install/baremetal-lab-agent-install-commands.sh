aa17d8d1aa5b085d0ddd29452ce7cbacdabd5ceb
Biagio Manzari <bmanzari@redhat.com>
2023-03-11 09:40:39 +0100

    patch install-config for disconnected installs

--- ci-operator/step-registry/baremetal/lab/agent/install/baremetal-lab-agent-install-commands.sh
+++ ci-operator/step-registry/baremetal/lab/agent/install/baremetal-lab-agent-install-commands.sh
@@ -221,6 +221,11 @@ platform:
 "
 fi
 
+if [ "${DISCONNECTED}" == "true" ]; then
+  echo 'Disconnected install, patching install-config.yaml'
+  cat "$SHARED_DIR/install-config-icsp.yaml.patch" >> "$SHARED_DIR/install-config.yaml"
+fi
+
 cp "${SHARED_DIR}/install-config.yaml" "${INSTALL_DIR}/"
 cp "${SHARED_DIR}/agent-config.yaml" "${INSTALL_DIR}/"
 
