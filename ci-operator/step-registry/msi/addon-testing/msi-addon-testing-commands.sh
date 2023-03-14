a34c916d364ffeef098e0ae71e42c4950ce600f5
rnetser <rnetser@redhat.com>
2023-03-14 14:04:24 +0200

    update command

--- ci-operator/step-registry/msi/addon-testing/msi-addon-testing-commands.sh
+++ ci-operator/step-registry/msi/addon-testing/msi-addon-testing-commands.sh
@@ -5,10 +5,12 @@ set -o errexit
 set -o pipefail
 set -o verbose
 
-OUTPUT_DIR=${ARTIFACT_DIR}
-CLUSTER_NAME=$(cat "${SHARED_DIR}/CLUSTER_NAME")
+sleep 500000
 
-export KUBECONFIG=${SHARED_DIR}/kubeconfig
-
-poetry run pytest --tc=api_server:production --cluster-name "${CLUSTER_NAME}" --junitxml="${OUTPUT_DIR}/xunit_results.xml" \
---pytest-log-file="${OUTPUT_DIR}/pytest-tests.log" -s -o log_cli=true -p no:logging -m "$TEST_MARKERS"  --data-collector=data-collector.yaml
+#OUTPUT_DIR=${ARTIFACT_DIR}
+#CLUSTER_NAME=$(cat "${SHARED_DIR}/CLUSTER_NAME")
+#
+#export KUBECONFIG=${SHARED_DIR}/kubeconfig
+#
+#poetry run pytest --tc=api_server:production --cluster-name "${CLUSTER_NAME}" --junitxml="${OUTPUT_DIR}/xunit_results.xml" \
+#--pytest-log-file="${OUTPUT_DIR}/pytest-tests.log" -s -o log_cli=true -p no:logging -m "$TEST_MARKERS"  --data-collector=data-collector.yaml
