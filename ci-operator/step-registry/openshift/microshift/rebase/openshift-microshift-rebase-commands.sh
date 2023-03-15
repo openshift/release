40e92eb13f23bfbc7079d68fa56a799c1988d85c
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-03-15 16:09:44 +0100

    DO NOT MERGE - test MicroShift rebase 4.12 fix

--- ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
+++ ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+set -x
+
+export BASE_BRANCH=release-4.12
+git remote add pmtk https://github.com/pmtk/microshift.git
+git fetch pmtk
+git checkout -t pmtk/release-4.12-fix-rebase
+
 ./scripts/auto-rebase/rebase_job_entrypoint.sh
+
+git status
+
+git diff
