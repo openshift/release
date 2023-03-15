940c77dd5e543b884b465d773ce2cbe9dbb256ef
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2023-03-15 13:04:31 -0400

    Add OpenShift system pods disruption scenario
    This commit:
    - Adds a scenario to pick OpenShift system pods in openshift-* namespaces
      and disrupt them to check it's recovery.
    - Renames the pod-scenarios workload to etcd-disruption in order to
      match the behavior of the run.

---
+++ ci-operator/step-registry/redhat-chaos/random-system-pods-disruption/redhat-chaos-random-system-pods-disruption-command.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+cat /etc/os-release
+oc config view
+oc projects
+python3 --version
+pushd /tmp
+
+ls -la /root/kraken
+git clone https://github.com/redhat-chaos/krkn-hub.git
+pushd krkn-hub/
+
+echo "kubeconfig loc $$KUBECONFIG"
+
+export KRKN_KUBE_CONFIG=$KUBECONFIG
+export NAMESPACE="openshift-.*"
+
+./prow/pod-scenarios/prow_run.sh
