5cb39e4b897ee0eb56a7f3523810151dd27b5501
Liangquan Li <liangli@redhat.com>
2023-03-16 16:49:10 +0800

    update health_check() (#37314)

--- ci-operator/step-registry/cucushift/upgrade/hypershift/cp/toimage/cucushift-upgrade-hypershift-cp-toimage-commands.sh
+++ ci-operator/step-registry/cucushift/upgrade/hypershift/cp/toimage/cucushift-upgrade-hypershift-cp-toimage-commands.sh
@@ -87,102 +87,11 @@ function check_pod() {
     oc get pods --all-namespaces
 }
 
-function check_clusteroperators() {
-    local tmp_ret=0 tmp_clusteroperator input column last_column_name tmp_clusteroperator_1 rc null_version unavailable_operator degraded_operator
-
-    echo "Make sure every operator do not report empty column"
-    tmp_clusteroperator=$(mktemp /tmp/health_check-script.XXXXXX)
-    input="${tmp_clusteroperator}"
-    ${OC} get clusteroperator >"${tmp_clusteroperator}"
-    column=$(head -n 1 "${tmp_clusteroperator}" | awk '{print NF}')
-    last_column_name=$(head -n 1 "${tmp_clusteroperator}" | awk '{print $NF}')
-    if [[ ${last_column_name} == "MESSAGE" ]]; then
-        (( column -= 1 ))
-        tmp_clusteroperator_1=$(mktemp /tmp/health_check-script.XXXXXX)
-        awk -v end=${column} '{for(i=1;i<=end;i++) printf $i"\t"; print ""}' "${tmp_clusteroperator}" > "${tmp_clusteroperator_1}"
-        input="${tmp_clusteroperator_1}"
-    fi
-
-    while IFS= read -r line
-    do
-        rc=$(echo "${line}" | awk '{print NF}')
-        if (( rc != column )); then
-            echo >&2 "The following line have empty column"
-            echo >&2 "${line}"
-            (( tmp_ret += 1 ))
-        fi
-    done < "${input}"
-    rm -f "${tmp_clusteroperator}"
-
-    echo "Make sure every operator column reports version"
-    if null_version=$(${OC} get clusteroperator -o json | jq '.items[] | select(.status.versions == null) | .metadata.name') && [[ ${null_version} != "" ]]; then
-        echo >&2 "Null Version: ${null_version}"
-        (( tmp_ret += 1 ))
-    fi
-
-    echo "Make sure every operator reports correct version"
-    if incorrect_version=$(${OC} get clusteroperator --no-headers | awk -v var="${TARGET_VERSION}" '$2 != var') && [[ ${incorrect_version} != "" ]]; then
-        echo >&2 "Incorrect CO Version: ${incorrect_version}"
-        (( tmp_ret += 1 ))
-    fi
-
-    # In disconnected install, marketplace often get into False state, so it is better to remove it from cluster from flexy post-action
-    echo "Make sure every operator's AVAILABLE column is True"
-    if unavailable_operator=$(${OC} get clusteroperator | awk '$3 == "False"' | grep "False"); then
-        echo >&2 "Some operator's AVAILABLE is False"
-        echo >&2 "$unavailable_operator"
-        (( tmp_ret += 1 ))
-    fi
-    if ${OC} get clusteroperator -o json | jq '.items[].status.conditions[] | select(.type == "Available") | .status' | grep -iv "True"; then
-        echo >&2 "Some operators are unavailable, pls run 'oc get clusteroperator -o json' to check"
-        (( tmp_ret += 1 ))
-    fi
-
-    # In disconnected install, openshift-sample often get into Degrade state, so it is better to remove them from cluster from flexy post-action
-    #degraded_operator=$(${OC} get clusteroperator | grep -v "openshift-sample" | awk '$5 == "True"')
-    if degraded_operator=$(${OC} get clusteroperator | awk '$5 == "True"' | grep "True"); then
-        echo >&2 "Some operator's DEGRADED is True"
-        echo >&2 "$degraded_operator"
-        (( tmp_ret += 1 ))
-    fi
-    #co_check=$(${OC} get clusteroperator -o json | jq '.items[] | select(.metadata.name != "openshift-samples") | .status.conditions[] | select(.type == "Degraded") | .status'  | grep -iv 'False')
-    if ${OC} get clusteroperator -o json | jq '.items[].status.conditions[] | select(.type == "Degraded") | .status'  | grep -iv 'False'; then
-        echo >&2 "Some operators are Degraded, pls run 'oc get clusteroperator -o json' to check"
-        (( tmp_ret += 1 ))
-    fi
-
-    return $tmp_ret
-}
-
-function wait_clusteroperators_continous_success() {
-    local try=0 continous_successful_check=0 passed_criteria=3 max_retries=20
-    while (( try < max_retries && continous_successful_check < passed_criteria )); do
-        echo "Checking #${try}"
-        if check_clusteroperators; then
-            echo "Passed #${continous_successful_check}"
-            (( continous_successful_check += 1 ))
-        else
-            echo "cluster operators are not ready yet, wait and retry..."
-            continous_successful_check=0
-        fi
-        sleep 60
-        (( try += 1 ))
-    done
-    if (( continous_successful_check != passed_criteria )); then
-        echo >&2 "Some cluster operator does not get ready or not stable"
-        echo "Debug: current CO output is:"
-        oc get co
-        return 1
-    else
-        echo "All cluster operators status check PASSED"
-        return 0
-    fi
-}
-
 function health_check() {
     #1. Check all cluster operators get stable and ready
     echo "Step #1: check all cluster operators get stable and ready"
-    wait_clusteroperators_continous_success
+    oc wait clusteroperators --all --for=condition=Progressing=false --timeout=15m
+    oc wait clusterversion/version --for='condition=Available=True' --timeout=15m
 
     #2. Make sure every machine is in 'Ready' status
     echo "Step #2: Make sure every machine is in 'Ready' status"
@@ -194,7 +103,7 @@ function health_check() {
 }
 
 if [ ! -f "${SHARED_DIR}/mgmt_kubeconfig" ]; then
-  exit 1
+    exit 1
 fi
 echo "switch kubeconfig"
 export KUBECONFIG="${SHARED_DIR}/mgmt_kubeconfig"
@@ -213,19 +122,19 @@ oc patch hostedcluster "$cluster_name" -n "$HYPERSHIFT_NAMESPACE" --type=merge -
 
 _upgradeReady=1
 for ((i=1; i<=60; i++)); do
-  count=$(oc get hostedcluster -n clusters "$cluster_name" -ojsonpath='{.status.version.history[?(@.image=="'"${OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE}"'")].state}' | grep -c Completed || true)
-  if [ "$count" -eq 1 ] ; then
-      echo "HyperShift HostedCluster(CP) upgrade successful"
-      _upgradeReady=0
-      break
-  fi
-  echo "Try ${i}/60: HyperShift HostedCluster(CP) is not updated yet. Checking again in 30 seconds"
-  sleep 30
+    count=$(oc get hostedcluster -n clusters "$cluster_name" -ojsonpath='{.status.version.history[?(@.image=="'"${OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE}"'")].state}' | grep -c Completed || true)
+    if [ "$count" -eq 1 ] ; then
+        echo "HyperShift HostedCluster(CP) upgrade successful"
+        _upgradeReady=0
+        break
+    fi
+    echo "Try ${i}/60: HyperShift HostedCluster(CP) is not updated yet. Checking again in 30 seconds"
+    sleep 30
 done
 
 if [ $_upgradeReady -ne 0 ]; then
-  echo "HyperShift HostedCluster(CP) upgrade failed"
-  exit 1
+    echo "HyperShift HostedCluster(CP) upgrade failed"
+    exit 1
 fi
 
 export KUBECONFIG="${SHARED_DIR}/kubeconfig"
