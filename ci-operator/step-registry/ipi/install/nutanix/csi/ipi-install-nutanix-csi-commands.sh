b8b3b2010d6d00617d4acbc3672b1e9988eafb84
Ilya Alekseyev <ilya.alekseyev@nutanix.com>
2023-03-15 23:23:37 -0400

    Temporary remove PVC steps for image-registry and monitoring for Nutanix jobs
     - Due to unstable behaviour temporary remove PVC for image-registry
       and monitoring operators
     - Improve Nutanix CSI installation

--- ci-operator/step-registry/ipi/install/nutanix/csi/ipi-install-nutanix-csi-commands.sh
+++ ci-operator/step-registry/ipi/install/nutanix/csi/ipi-install-nutanix-csi-commands.sh
@@ -7,51 +7,6 @@ set -o pipefail
 source ${SHARED_DIR}/nutanix_context.sh
 
 echo "$(date -u --rfc-3339=seconds) - Creating CSI manifests..."
-cat > "${SHARED_DIR}/manifest_0000-nutanix-csi-crd-manifest.yaml" << EOF
-apiVersion: apiextensions.k8s.io/v1
-kind: CustomResourceDefinition
-metadata:
-  name: nutanixcsistorages.crd.nutanix.com
-spec:
-  group: crd.nutanix.com
-  names:
-    kind: NutanixCsiStorage
-    listKind: NutanixCsiStorageList
-    plural: nutanixcsistorages
-    singular: nutanixcsistorage
-  scope: Namespaced
-  versions:
-  - name: v1alpha1
-    schema:
-      openAPIV3Schema:
-        description: NutanixCsiStorage is the Schema for the nutanixcsistorages API
-        properties:
-          apiVersion:
-            description: 'APIVersion defines the versioned schema of this representation
-              of an object. Servers should convert recognized schemas to the latest
-              internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources'
-            type: string
-          kind:
-            description: 'Kind is a string value representing the REST resource this
-              object represents. Servers may infer this from the endpoint the client
-              submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds'
-            type: string
-          metadata:
-            type: object
-          spec:
-            description: Spec defines the desired state of NutanixCsiStorage
-            type: object
-            x-kubernetes-preserve-unknown-fields: true
-          status:
-            description: Status defines the observed state of NutanixCsiStorage
-            type: object
-            x-kubernetes-preserve-unknown-fields: true
-        type: object
-    served: true
-    storage: true
-    subresources:
-      status: {}
-EOF
 
 cat > "${SHARED_DIR}/manifest_0001-nutanix-csi-ntnx-system-namespace.yaml" << EOF
 apiVersion: v1
@@ -132,13 +87,37 @@ allowVolumeExpansion: true
 reclaimPolicy: Delete
 EOF
 
-oc apply -f "${SHARED_DIR}/manifest_0000-nutanix-csi-crd-manifest.yaml"
 oc apply -f "${SHARED_DIR}/manifest_0001-nutanix-csi-ntnx-system-namespace.yaml"
 oc apply -f "${SHARED_DIR}/manifest_0002-nutanix-csi-ntnx-secret.yaml"
 oc apply -f "${SHARED_DIR}/manifest_0003-nutanix-csi-operator-group.yaml"
 oc apply -f "${SHARED_DIR}/manifest_0004-nutanix-csi-subscription.yaml"
-oc apply -f "${SHARED_DIR}/manifest_0005-nutanix-csi-storage.yaml"
 oc apply -f "${SHARED_DIR}/manifest_0006-nutanix-csi-storage-class.yaml"
 
+echo "$(date -u --rfc-3339=seconds) - Waiting for CSI operator to be available..."
 sleep 60
-oc wait --for condition=Available=True --timeout=5m deployment/nutanix-csi-controller -n ntnx-system
+
+oc -n ntnx-system get all
+oc -n ntnx-system describe all
+oc -n ntnx-system get events
+oc -n ntnx-system get csv
+oc -n ntnx-system describe csv -l operators.coreos.com/nutanixcsioperator.ntnx-system=
+oc -n ntnx-system get subscription
+oc -n ntnx-system describe subscription
+oc get sc
+
+if oc wait --for condition=Available=True --timeout=5m deployment/nutanix-csi-operator-controller-manager -n ntnx-system ; then
+  echo "$(date -u --rfc-3339=seconds) - CSI operator controller manager is available"
+fi
+
+oc apply -f "${SHARED_DIR}/manifest_0005-nutanix-csi-storage.yaml"
+
+sleep 60
+
+oc -n ntnx-system get all
+oc -n ntnx-system describe all
+oc -n ntnx-system get events
+
+if oc wait --for condition=Available=True --timeout=5m deployment/nutanix-csi-controller -n ntnx-system ; then
+  echo "$(date -u --rfc-3339=seconds) - CSI operator is available"
+fi
+
