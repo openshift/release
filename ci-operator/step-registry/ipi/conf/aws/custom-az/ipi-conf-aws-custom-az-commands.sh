70a9b331a3951cd57d85bd1cedb971bae67e171b
ascerra@redhat.com <ascerra@redhat.com>
2023-03-21 16:28:28 -0400

    OPP onboarding using latest policies

---
+++ ci-operator/step-registry/ipi/conf/aws/custom-az/ipi-conf-aws-custom-az-commands.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
+REGION="${LEASED_RESOURCE}"
+CONFIG="${SHARED_DIR}/install-config.yaml"
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+mapfile -t AVAILABILITY_ZONES < <(aws --region "${REGION}" ec2 describe-availability-zones | jq -r '.AvailabilityZones[] | select(.State == "available") | .ZoneName' | sort -u)
+ZONES=("${AVAILABILITY_ZONES[@]:0:${ZONES_COUNT}}")
+ZONES_STR="[ $(join_by , "${ZONES[@]}") ]"
+echo "AWS region: ${REGION} (zones: ${ZONES_STR})"
+
+CONFIG_PATCH="${SHARED_DIR}/install-config-zones.yaml.patch"
+cat > "${CONFIG_PATCH}" << EOF
+controlPlane:
+  platform:
+    aws:
+      zones: ${ZONES_STR}
+compute:
+- platform:
+    aws:
+      zones: ${ZONES_STR}
+EOF
+yq-go m -x -i "${CONFIG}" "${CONFIG_PATCH}"
