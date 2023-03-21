a407d1178139dc05fb289570339f5a4b13739c84
Jacob Tanenbaum <jtanenba@redhat.com>
2023-03-21 15:37:53 -0400

    add e2e testing for toggling on hybrid overlay (#35751)
    * add e2e testing for toggling on hybrid overlay
    
    recently the ability to turn on hybrid overlay was created so I am
    adding an e2e test
    
    * autogenerated code from `make registry-metadata`

---
+++ ci-operator/step-registry/ovn/conf/turn-on-hybrid/ovn-conf-turn-on-hybrid-commands.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+
+# patch the network operator
+oc patch Network.operator.openshift.io cluster --type='merge' --patch '{"spec":{"defaultNetwork":{"ovnKubernetesConfig":{"hybridOverlayConfig":{"hybridClusterNetwork":[{"cidr": "10.132.0.0/14","hostPrefix": 23}]}}}}}'
+
+# wait for the ovnkube config map to reflect the change
+start_time=$(date +%s)
+while [ -z "$(oc get configmap -n openshift-ovn-kubernetes ovnkube-config -o yaml | grep hybridoverlay)" ]; do
+	if [ $(($(date +%s) - $start_time)) -gt 20 ]; then
+		echo "Timeout waiting for the ovn-kubernetes config map to update"
+		exit 1
+	fi
+done
+
+# verify that the ovnkube-master pods come back up
+start_time=$(date +%s)
+while [ "$(oc get daemonset.apps/ovnkube-master -n openshift-ovn-kubernetes | awk '{print $2==$4}' | tail -n +2)" -ne 1 ]; do
+	if [ $(($(date +%s) - $start_time)) -gt 180 ]; then
+		echo "Timeout waiting for the ovn-kubernetes master pods to come up"
+		exit 1
+	fi
+done
+
+# verify that the ovnkube-node pods come back up
+start_time=$(date +%s)
+while [ "$(oc get daemonset.apps/ovnkube-node -n openshift-ovn-kubernetes | awk '{print $2==$4}' | tail -n +2)" -ne 1 ]; do
+	if [ $(($(date +%s) - $start_time)) -gt 180 ]; then
+		echo "Timeout waiting for the ovn-kubernetes master pods to come up"
+		exit 1
+	fi
+done
+
