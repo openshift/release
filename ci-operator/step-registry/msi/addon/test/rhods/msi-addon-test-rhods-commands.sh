5e99f5bb3aa296324807c54001f97f9f986d9200
Omri Bar-Haim <obarhaim@redhat.com>
2023-03-26 19:10:16 +0300

    rhods onboarding

--- ci-operator/step-registry/msi/addon/test/rhods/msi-addon-test-rhods-commands.sh
+++ ci-operator/step-registry/msi/addon/test/rhods/msi-addon-test-rhods-commands.sh
@@ -5,9 +5,10 @@ set -o errexit
 set -o pipefail
 set -o verbose
 
+
 CLUSTER_NAME=$(cat "${SHARED_DIR}/cluster-name")
-OCM_TOKEN=$(cat /var/run/secrets/ci.openshift.io/cluster-profile/ocm-token)
+# OCM_TOKEN=$(cat /var/run/secrets/ci.openshift.io/cluster-profile/ocm-token)
 
 # TODO: understand how RHODS test should be ran from product QE image (quay.io/modh/ods-ci:latest)
-echo -e "cluster name: $CLUSTER_NAME\nocm_token: $OCM_TOKEN\naddon name: $ADDON_NAME\napi host: $API_HOST"
-sleep INF
+echo -e "cluster name: $CLUSTER_NAME\naddon name: $ADDON_NAME\napi host: $API_HOST\ntest marker: $TEST_MARKER\ntimeout: $TIMEOUT"
+sleep 2h
