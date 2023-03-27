5a3540fc1987bdbe948f069f23d0529460da1a11
Jose Luis Franco Arza <jfrancoa@redhat.com>
2023-03-27 16:37:20 +0200

    Enable cluster monitoring in WMCO default namespace.
    This patch adds the cluster-monitoring label to default
    WMCO namespace so that endpoints will be created and
    metrics get generated.

--- ci-operator/step-registry/windows/conf/operator/windows-conf-operator-commands.sh
+++ ci-operator/step-registry/windows/conf/operator/windows-conf-operator-commands.sh
@@ -8,6 +8,9 @@ if ! oc get ns "${SUB_INSTALL_NAMESPACE}"; then
   oc create ns "${SUB_INSTALL_NAMESPACE}"
 fi
 
+# Enable monitoring
+oc label namespace "${SUB_INSTALL_NAMESPACE}" openshift.io/cluster-monitoring=true
+
 oc create secret generic cloud-private-key \
   -n "${SUB_INSTALL_NAMESPACE}" \
   --from-file=private-key.pem="${CLUSTER_PROFILE_DIR}/ssh-privatekey"
