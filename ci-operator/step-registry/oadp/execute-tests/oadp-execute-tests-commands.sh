fdb46d4f00ad3dffd8b9d93a6e9259cf51a1a7e5
Caleb Evans <caevans@redhat.com>
2023-03-30 10:54:11 -0600

    Remove sleep

--- ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
+++ ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
@@ -4,8 +4,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-sleep 3600
-
 SECRETS_DIR="/tmp/secrets"
 OADP_GH_PAT=$(cat ${SECRETS_DIR}/oadp/oadp-gh-pat)
 OADP_GH_USER=$(cat ${SECRETS_DIR}/oadp/oadp-gh-user)
