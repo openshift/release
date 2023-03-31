7defb9b5011144c452f8cb59475a5b137cdac40e
Caleb Evans <caevans@redhat.com>
2023-03-31 11:59:22 -0600

    Fix virutal env again

--- ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
+++ ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
@@ -7,7 +7,7 @@ set -o pipefail
 # Install pip and setup virtual environment
 python3 -m pip install pip --upgrade
 python3 -m venv /alabama/venv
-source /venv/bin/activate
+source /alabama/venv/bin/activate
 pip install ansible_runner
 
 # Create required directories
