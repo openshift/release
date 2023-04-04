5725df977344ba0ad5f48d5a91e56f863df2274b
Liang Xia <lxia@redhat.com>
2023-04-04 09:33:34 +0800

    Refine cron entry

--- ci-operator/config/openshift/openshift-tests-private/tools/generate-cron-entry.sh
+++ ci-operator/config/openshift/openshift-tests-private/tools/generate-cron-entry.sh
@@ -68,7 +68,7 @@ case "$FN" in
 	1)
 		echo "$MINUTE $HOUR * * *"
 		;;
-	2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31)
+	2|3|4|5|6|7|10|14|28|30)
 		DAY_OF_MONTH_TMP=$DAY_OF_MONTH
 		for ((i=1 ; i<31/FN; ++i)) ; do
 			let TMP=(i*FN+DAY_OF_MONTH)%30+1
