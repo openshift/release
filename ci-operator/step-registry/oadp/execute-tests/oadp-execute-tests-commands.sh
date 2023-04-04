cec01e59bdba5812d5575540e789c30e9e87ac79
Caleb Evans <caevans@redhat.com>
2023-04-04 06:50:03 -0600

    Add another sleep for debugging

--- ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
+++ ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
@@ -10,6 +10,8 @@ export AWS_CONFIG_FILE=$CLUSTER_PROFILE_DIR/.aws
 INTEROP_AWS_ACCESS_KEY=$(cat $AWS_SHARED_CREDENTIALS_FILE | grep aws_access_key_id | tr -d ' ' | cut -d '=' -f 2)
 INTEROP_AWS_SECRET_KEY=$(cat $AWS_SHARED_CREDENTIALS_FILE | grep aws_secret_access_key | tr -d ' ' | cut -d '=' -f 2)
 
+sleep 3600
+
 # Create S3 Bucket to Use for Testing
 /bin/bash /home/jenkins/oadp-qe-automation/backup-locations/aws-s3/deploy.sh $BUCKET_NAME
 
