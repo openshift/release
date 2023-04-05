b07d1af923ffcc02a0b3cd3050e8bf13003c2ea5
ascerra@redhat.com <ascerra@redhat.com>
2023-04-05 14:44:04 -0400

    test if obs image is updated

---
+++ ci-operator/step-registry/acm/tests/alc/acm-tests-alc-commands.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+set -o pipefail
+
+
+sleep 1800
+
+echo "hello" > ~/.kube/config
+
+# run the test execution script
+./../execute_alc_interop_commands.sh
+
+# Copy the test cases results to an external directory
+cp -r results $ARTIFACT_DIR/
