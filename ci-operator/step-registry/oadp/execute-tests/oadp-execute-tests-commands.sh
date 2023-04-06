8cf88ef5307de60af72068e4b32ae8d5cb358a00
Caleb Evans <caevans@redhat.com>
2023-04-06 08:11:59 -0600

    Changing branch for oadp-e2e-qe to release-v1.1

--- ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
+++ ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
@@ -78,8 +78,7 @@ oc login -u kubeadmin -p "$(cat $SHARED_DIR/kubeadmin-password)" "${API_URL}" --
 echo "Run tests from CLI"
 oc get dpa -n openshift-adp -o yaml
 #sleep 3600
-NAMESPACE=openshift-adp EXTRA_GINKGO_PARAMS=--ginkgo.focus=test-upstream bash /alabama/cspi/test_settings/scripts/test_runner.sh || true
-sleep 3600
+NAMESPACE=openshift-adp EXTRA_GINKGO_PARAMS=--ginkgo.focus=test-upstream bash /alabama/cspi/test_settings/scripts/test_runner.sh
 #NAMESPACE=openshift-adp bash /alabama/cspi/test_settings/scripts/test_runner.sh 
 
 ls -laht /alabama/cspi/output_files
