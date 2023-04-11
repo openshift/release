46ea1e2c69e747ed9fe5614ddb0ffe5432b671cc
Shaowen Zhang <shaozhan@redhat.com>
2023-04-11 16:11:15 +0800

    Update cucushift-installer-rehearse-aws-ipi-sno-test-commands.sh

--- ci-operator/step-registry/cucushift/installer/rehearse/aws/ipi/sno/test/cucushift-installer-rehearse-aws-ipi-sno-test-commands.sh
+++ ci-operator/step-registry/cucushift/installer/rehearse/aws/ipi/sno/test/cucushift-installer-rehearse-aws-ipi-sno-test-commands.sh
@@ -5,13 +5,18 @@ set -o errexit
 set -o pipefail
 set -x
 
+# get AWS region
+REGION="${LEASED_RESOURCE}"
+
 echo "************ single-node test command ************"
 
 # Tests execution
 set +e
 
+export KUBECONFIG="${SHARED_DIR}/kubeconfig"
+
 NODE_NAME=`oc get node  |grep master | awk '{print $1}'`
-Instance_Type=`aws ec2 describe-instances --filters "Name=private-dns-name,Values=${NODE_NAME}" --query "Reservations[*].Instances[*].[InstanceType]"`
+Instance_Type=`aws ec2 describe-instances --region ${REGION} --filters "Name=private-dns-name,Values=${NODE_NAME}" --query "Reservations[*].Instances[*].[InstanceType]"`
 
 if [[ "${Instance_Type}" == "m6i.2xlarge" ]]; then
   echo "The installer defaults for Single Node AWS have taken effect, is ${Instance_Type}"
