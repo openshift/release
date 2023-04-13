50b615d157cba9c86e8b2cd6f49092db0256264b
Yunfei Jiang <yunjiang@redhat.com>
2023-04-13 10:33:00 +0800

    debug

--- ci-operator/step-registry/ipi/install/install/aws/ipi-install-install-aws-commands.sh
+++ ci-operator/step-registry/ipi/install/install/aws/ipi-install-install-aws-commands.sh
@@ -365,4 +365,6 @@ if test "${ret}" -eq 0 ; then
   echo "https://$(env KUBECONFIG=${dir}/auth/kubeconfig oc -n openshift-console get routes console -o=jsonpath='{.spec.host}')" > "${SHARED_DIR}/console.url"
 fi
 
+# DEBUG
+
 exit "$ret"
