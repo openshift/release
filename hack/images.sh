0e44909697294539cea87a947e529b50356e2331
Bruno Barcarol Guimarães <bbarcaro@redhat.com>
2023-04-13 15:03:51 +0000

    Revert "Bumping Prow and Boskos (#38138)"
    This reverts commit 44fded3e4f62979d9b89c6b215ae4917350e19a0.

--- hack/images.sh
+++ hack/images.sh
@@ -1,2 +1,2 @@
-MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20230412-a86d65c3c2
-MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20230412-a86d65c3c2
+MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20230405-46d7922c2a
+MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20230405-46d7922c2a
