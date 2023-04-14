e4ad0e8b6ff13043c0bd37e28cbe5071df83acbd
Liangquan Li <liangli@redhat.com>
2023-04-14 22:21:02 +0800

    add: operatorhub-subscribe-metallb-operator

---
+++ ci-operator/step-registry/operatorhub/subscribe/metallb-operator/operatorhub-subscribe-metallb-operator-commands.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+if [[ -z "${LO_SUB_INSTALL_NAMESPACE}" ]]; then
+  echo "ERROR: INSTALL_NAMESPACE is not defined"
+  exit 1
+fi
+
+if [[ -z "${METALLB_OPERATOR_SUB_PACKAGE}" ]]; then
+  echo "ERROR: PACKAGE is not defined"
+  exit 1
+fi
+
+if [[ -z "${METALLB_OPERATOR_SUB_CHANNEL}" ]]; then
+  echo "ERROR: CHANNEL is not defined"
+  exit 1
+fi
+
+if [[ "${METALLB_OPERATOR_SUB_TARGET_NAMESPACES}" == "!install" ]]; then
+  METALLB_OPERATOR_SUB_TARGET_NAMESPACES="${METALLB_OPERATOR_SUB_INSTALL_NAMESPACE}"
+fi
+
+echo "Installing ${METALLB_OPERATOR_SUB_PACKAGE} from ${METALLB_OPERATOR_SUB_CHANNEL} into ${METALLB_OPERATOR_SUB_INSTALL_NAMESPACE}, targeting ${METALLB_OPERATOR_SUB_TARGET_NAMESPACES}"
+
+# create the install namespace
+oc apply -f - <<EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: "${METALLB_OPERATOR_SUB_INSTALL_NAMESPACE}"
+  labels:
+    openshift.io/cluster-monitoring: "true"
+EOF
+
+# deploy new operator group
+oc apply -f - <<EOF
+apiVersion: operators.coreos.com/v1
+kind: OperatorGroup
+metadata:
+  name: "${METALLB_OPERATOR_SUB_INSTALL_NAMESPACE}"
+  namespace: "${METALLB_OPERATOR_SUB_INSTALL_NAMESPACE}"
+spec: {}
+EOF
+
+# subscribe to the operator
+cat <<EOF | oc apply -f -
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: "${METALLB_OPERATOR_SUB_PACKAGE}"
+  namespace: "${METALLB_OPERATOR_SUB_INSTALL_NAMESPACE}"
+spec:
+  channel: "${METALLB_OPERATOR_SUB_CHANNEL}"
+  installPlanApproval: Automatic
+  name: "${METALLB_OPERATOR_SUB_PACKAGE}"
+  source: "${METALLB_OPERATOR_SUB_SOURCE}"
+  sourceNamespace: openshift-marketplace
+EOF
+
+# can't wait before the resource exists. Need to sleep a bit before start watching
+sleep 60
+
+RETRIES=30
+CSV=
+for i in $(seq "${RETRIES}"); do
+  if [[ -z "${CSV}" ]]; then
+    CSV=$(oc get subscription -n "${METALLB_OPERATOR_SUB_INSTALL_NAMESPACE}" "${METALLB_OPERATOR_SUB_PACKAGE}" -o jsonpath='{.status.installedCSV}')
+  fi
+
+  if [[ -z "${CSV}" ]]; then
+    echo "Try ${i}/${RETRIES}: can't get the ${METALLB_OPERATOR_SUB_PACKAGE} yet. Checking again in 30 seconds"
+    sleep 30
+  fi
+
+  if [[ $(oc get csv -n ${METALLB_OPERATOR_SUB_INSTALL_NAMESPACE} ${CSV} -o jsonpath='{.status.phase}') == "Succeeded" ]]; then
+    echo "${METALLB_OPERATOR_SUB_PACKAGE} is deployed"
+    break
+  else
+    echo "Try ${i}/${RETRIES}: ${METALLB_OPERATOR_SUB_PACKAGE} is not deployed yet. Checking again in 30 seconds"
+    sleep 30
+  fi
+done
+
+if [[ $(oc get csv -n "${METALLB_OPERATOR_SUB_INSTALL_NAMESPACE}" "${CSV}" -o jsonpath='{.status.phase}') != "Succeeded" ]]; then
+  echo "Error: Failed to deploy ${METALLB_OPERATOR_SUB_PACKAGE}"
+  echo "csv ${CSV} YAML"
+  oc get csv "${CSV}" -n "${METALLB_OPERATOR_SUB_INSTALL_NAMESPACE}" -o yaml
+  echo
+  echo "csv ${CSV} Describe"
+  oc describe csv "${CSV}" -n "${METALLB_OPERATOR_SUB_INSTALL_NAMESPACE}"
+  exit 1
+fi
+
+echo "successfully installed ${METALLB_OPERATOR_SUB_PACKAGE}"
