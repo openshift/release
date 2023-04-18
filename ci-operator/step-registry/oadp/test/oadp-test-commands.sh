5e250b9a238b61612c8a2a53c5815d14abdecf72
Caleb Evans <caevans@redhat.com>
2023-04-18 11:26:28 -0600

    Add directory creation

--- ci-operator/step-registry/oadp/test/oadp-test-commands.sh
+++ ci-operator/step-registry/oadp/test/oadp-test-commands.sh
@@ -4,21 +4,11 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-#OADP_GIT_DIR="/alabama/cspi"
-#OADP_APPS_DIR="/alabama/oadpApps"
-#PYCLIENT_DIR="/alabama/pyclient"
-
 # Extract additional repository archives
 # mkdir -p {$OADP_GIT_DIR,$OADP_APPS_DIR,$PYCLIENT_DIR}
 ls -ltr /alabama
 ls -ltr $OADP_AUTOMATION_DIR
 
-#echo "Extract /home/jenkins/oadp-e2e-qe.tar.gz"
-#tar -xf /home/jenkins/oadp-e2e-qe.tar.gz -C "${OADP_GIT_DIR}" --strip-components 1
-#echo "Extract /home/jenkins/oadp-apps-deployer.tar.gz"
-#tar -xf /home/jenkins/oadp-apps-deployer.tar.gz -C "${OADP_APPS_DIR}" --strip-components 1
-#echo "Extract /home/jenkins/mtc-python-client.tar.gz"
-#tar -xf /home/jenkins/mtc-python-client.tar.gz -C "${PYCLIENT_DIR}" --strip-components 1
 ls -ltr $OADP_GIT_DIR
 ls -ltr $OADP_APPS_DIR
 ls -ltr $PYCLIENT_DIR
