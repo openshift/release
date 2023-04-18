0c699b18edc745e7f8dfc4dfb37e80e8c7dc354c
Caleb Evans <caevans@redhat.com>
2023-04-18 09:46:52 -0600

    Run test

---
+++ ci-operator/step-registry/oadp/test/oadp-test-commands.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# Set variables needed to login to AWS
+export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
+export AWS_CONFIG_FILE=$CLUSTER_PROFILE_DIR/.aws
+export AWS_ACCESS_KEY_ID=$(cat $AWS_SHARED_CREDENTIALS_FILE | grep aws_access_key_id | tr -d ' ' | cut -d '=' -f 2)
+export AWS_SECRET_ACCESS_KEY=$(cat $AWS_SHARED_CREDENTIALS_FILE | grep aws_secret_access_key | tr -d ' ' | cut -d '=' -f 2)
+
+# Set variables needed for test execution
+export OADP_CREDS_FILE="/tmp/test-settings/credentials"
+export PROVIDER="aws"
+export PROW_NAMESPACE=$NAMESPACE
+export NAMESPACE="openshift-adp"
+export BUCKET="${PROW_NAMESPACE}-${BUCKET_NAME}"
+export KUBECONFIG="/home/jenkins/.kube/config"
+export ANSIBLE_REMOTE_TMP="/tmp/"
+CONSOLE_URL=$(cat $SHARED_DIR/console.url)
+API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
+RESULTS_FILE="/alabama/cspi/output_files/api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443/junit_report.xml"
+OADP_GIT_DIR="/alabama/cspi"
+OADP_APPS_DIR="/alabama/oadpApps"
+PYCLIENT_DIR="/alabama/pyclient"
+
+# Extract additional repository archives
+ls -ltr /alabama
+mkdir -p "${OADP_GIT_DIR}"
+mkdir -p "${OADP_APPS_DIR}"
+mkdir -p "${PYCLIENT_DIR}"
+echo "Extract /home/jenkins/oadp-e2e-qe.tar.gz"
+tar -xf /home/jenkins/oadp-e2e-qe.tar.gz -C "${OADP_GIT_DIR}" --strip-components 1
+echo "Extract /home/jenkins/oadp-apps-deployer.tar.gz"
+tar -xf /home/jenkins/oadp-apps-deployer.tar.gz -C "${OADP_APPS_DIR}" --strip-components 1
+echo "Extract /home/jenkins/mtc-python-client.tar.gz"
+tar -xf /home/jenkins/mtc-python-client.tar.gz -C "${PYCLIENT_DIR}" --strip-components 1
+
+# Setup Python Virtual Environment
+echo "Create virtual environment and install required packages..."
+python3 -m venv /alabama/venv
+source /alabama/venv/bin/activate
+python3 -m pip install ansible_runner
+python3 -m pip install "${OADP_APPS_DIR}" --target "${OADP_GIT_DIR}/sample-applications/"
+python3 -m pip install "${PYCLIENT_DIR}"
