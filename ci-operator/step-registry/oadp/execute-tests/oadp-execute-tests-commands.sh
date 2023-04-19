9e179d776df9652e2a2b6f0fcb2465c4f5476cd4
Caleb Evans <caevans@redhat.com>
2023-04-19 11:50:04 -0600

    Remove volsync from oadpv1.0

--- ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
+++ ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
@@ -57,10 +57,10 @@ python3 -m pip install "${PYCLIENT_DIR}"
 # Execute tests
 echo "Executing tests..."
 cd $OADP_GIT_DIR
-EXTRA_GINKGO_PARAMS=--ginkgo.focus=test-upstream /bin/bash /alabama/cspi/test_settings/scripts/test_runner.sh || true
+EXTRA_GINKGO_PARAMS=--ginkgo.focus=test-upstream /bin/bash /alabama/cspi/test_settings/scripts/test_runner.sh
 
 # Archive results
 echo "Copying ${RESULTS_FILE} to ${ARTIFACT_DIR}/junit_oadp_interop_results.xml..."
-cp "${RESULTS_FILE}" "${ARTIFACT_DIR}/junit_oadp_interop_results.xml" || true
+cp "${RESULTS_FILE}" "${ARTIFACT_DIR}/junit_oadp_interop_results.xml"
 
 echo "Complete..."
