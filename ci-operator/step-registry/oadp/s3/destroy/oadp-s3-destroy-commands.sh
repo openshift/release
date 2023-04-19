55d9dbbee765a271198c11aa0cf14238c762a867
Caleb Evans <caevans@redhat.com>
2023-04-19 13:01:27 -0600

    Fix Shell check

--- ci-operator/step-registry/oadp/s3/destroy/oadp-s3-destroy-commands.sh
+++ ci-operator/step-registry/oadp/s3/destroy/oadp-s3-destroy-commands.sh
@@ -4,11 +4,13 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-# Set varaibles needed to login to AWS
+# Set variables needed to login to AWS
 export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 export AWS_CONFIG_FILE=$CLUSTER_PROFILE_DIR/.aws
-export AWS_ACCESS_KEY_ID=$(cat $AWS_SHARED_CREDENTIALS_FILE | grep aws_access_key_id | tr -d ' ' | cut -d '=' -f 2)
-export AWS_SECRET_ACCESS_KEY=$(cat $AWS_SHARED_CREDENTIALS_FILE | grep aws_secret_access_key | tr -d ' ' | cut -d '=' -f 2)
+AWS_ACCESS_KEY_ID=$(cat $AWS_SHARED_CREDENTIALS_FILE | grep aws_access_key_id | tr -d ' ' | cut -d '=' -f 2)
+AWS_SECRET_ACCESS_KEY=$(cat $AWS_SHARED_CREDENTIALS_FILE | grep aws_secret_access_key | tr -d ' ' | cut -d '=' -f 2)
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
 
 # Set BUCKET variable to a unique value
 export BUCKET="${NAMESPACE}-${BUCKET_NAME}"
