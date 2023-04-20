1be404f0c3b4e7420a6f7eb0f483a5a61d204055
Tomáš Nevrlka <tomas@nevrlka.com>
2023-04-20 09:39:17 +0200

    Add quay cleanup to cronjob (#38388)
    * feat: add clean-quay to cronjob
    
    * temporarily change git repo for testing
    
    * fix: mistake in variables
    
    * fix: source needed quay env vars
    
    * chore: add comment
    
    * fix: hardcode env vars instead of sourcing
    
    * fix: append quay env vars to export
    
    * chore: url change

--- ci-operator/step-registry/redhat-appstudio/clean-external-resources/redhat-appstudio-clean-external-resources-commands.sh
+++ ci-operator/step-registry/redhat-appstudio/clean-external-resources/redhat-appstudio-clean-external-resources-commands.sh
@@ -4,13 +4,16 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-export GITHUB_USER GITHUB_TOKEN GITHUB_ACCOUNTS_ARRAY PREVIOUS_RATE_REMAINING GITHUB_USERNAME_ARRAY GH_RATE_REMAINING CLEAN_REPOS_STATUS CLEAN_WEBHOOK_STATUS
+export GITHUB_USER GITHUB_TOKEN GITHUB_ACCOUNTS_ARRAY PREVIOUS_RATE_REMAINING GITHUB_USERNAME_ARRAY GH_RATE_REMAINING CLEAN_REPOS_STATUS CLEAN_WEBHOOK_STATUS DEFAULT_QUAY_ORG DEFAULT_QUAY_ORG_TOKEN
 
 GITHUB_USER=""
 GITHUB_TOKEN=""
 CLEAN_REPOS_STATUS=0
 CLEAN_WEBHOOK_STATUS=0
+CLEAN_QUAY_STATUS=0
 PREVIOUS_RATE_REMAINING=0
+DEFAULT_QUAY_ORG=redhat-appstudio-qe
+DEFAULT_QUAY_ORG_TOKEN=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/default-quay-org-token)
 
 # user stored: username:token,username:token
 IFS=',' read -r -a GITHUB_ACCOUNTS_ARRAY <<< "$(cat /usr/local/ci-secrets/redhat-appstudio-qe/github_accounts)"
@@ -39,7 +42,8 @@ git clone --branch main "https://${GITHUB_TOKEN}@github.com/redhat-appstudio/e2e
 
 make clean-gitops-repositories || CLEAN_REPOS_STATUS=$?
 make clean-github-webhooks || CLEAN_WEBHOOK_STATUS=$?
+make clean-quay || CLEAN_QUAY_STATUS=$?
 
-if [[ "${CLEAN_REPOS_STATUS}" -ne 0 || "${CLEAN_WEBHOOK_STATUS}" -ne 0 ]]; then
+if [[ "${CLEAN_REPOS_STATUS}" -ne 0 || "${CLEAN_WEBHOOK_STATUS}" -ne 0 || "${CLEAN_QUAY_STATUS}" -ne 0 ]]; then
     exit 1
 fi
