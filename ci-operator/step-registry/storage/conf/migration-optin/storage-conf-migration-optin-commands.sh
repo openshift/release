ec002bdf4348f2b1c552332f1ca253593f461234
Hemant Kumar <hekumar@redhat.com>
2023-04-20 16:15:04 -0400

    Change code that waits for migration completion

--- ci-operator/step-registry/storage/conf/migration-optin/storage-conf-migration-optin-commands.sh
+++ ci-operator/step-registry/storage/conf/migration-optin/storage-conf-migration-optin-commands.sh
@@ -3,4 +3,25 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-oc patch ClusterCSIDriver csi.vsphere.vmware.com  -p "{\"spec\":{\"vsphereStorageDriver\": \"CSIWithMigrationDriver\"}}" --type=merge -o yaml
+oc patch storage cluster  -p "{\"spec\":{\"vsphereStorageDriver\": \"CSIWithMigrationDriver\"}}" --type=merge -o yaml
+
+function check_for_migration() {
+    oc wait --for=condition=VSphereMigrationControllerAvailable=True --timeout=0 storage cluster || return 1
+}
+
+function wait_for_migration_completion() {
+    local COUNT=0
+
+    while true; do
+        if check_for_migration; then
+            echo "vSphere CSI migration is complete"
+            break
+        else
+            echo "Waiting for migration to finish ${COUNT}"
+            COUNT=$[ $COUNT+1 ]
+        fi
+        sleep 5
+    done
+}
+
+wait_for_migration_completion
