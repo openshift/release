aafe5f9bee79a90d755d488d1f709a949824be5f
Stephen Matula <smatula@redhat.com>
2023-04-20 07:07:51 -0400

    Update slack notification

--- ci-operator/step-registry/odo/tests/odo-tests-commands.sh
+++ ci-operator/step-registry/odo/tests/odo-tests-commands.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -x
 set -o nounset
 
 # Run tests
@@ -12,7 +11,8 @@ status=$?
 
 # Copy Results and artifacts to $ARTIFACT_DIR
 cp -r test-*.xml ${ARTIFACT_DIR}/ 2>/dev/null || :
+
+# Prepend junit_ to result xml files
 rename '/test-' '/junit_test-' ${ARTIFACT_DIR}/test-*.xml 2>/dev/null || :
 
-[ $status -ne 0 ] && [ $status -ne 255 ] && exit $status
-exit 0
+exit $status
