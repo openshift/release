966e654cb0ce988fbf566d25e503e0b597a117ea
He Liu <heli@redhat.com>
2023-04-24 11:06:12 +0800

    add debug

--- ci-operator/step-registry/cucushift/hypershift-extended/report/cucushift-hypershift-extended-report-commands.sh
+++ ci-operator/step-registry/cucushift/hypershift-extended/report/cucushift-hypershift-extended-report-commands.sh
@@ -6,6 +6,7 @@ REPORT_FILE_NAME=junit_operator.xml
 TEST_BEARER_TOKEN="55a6a7da-2faa-46cc-bb78-04eda767bc5f"
 REPORT_PORTAL_URL=https://reportportal-openshift.apps.ocp-c1.prod.psi.redhat.com/api/v1/${PROJECT}/launch/import
 
+sleep 1h
 # find report
 if [ ! -f "${SHARED_DIR}/${REPORT_FILE_NAME}" ]; then
     echo "${REPORT_FILE_NAME} not found error"
