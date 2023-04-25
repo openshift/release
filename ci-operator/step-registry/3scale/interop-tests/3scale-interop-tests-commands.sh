433a0a651e100aa6b5627260807da32ee3da3d47
cchetna <cchetna@redhat.com>
2023-04-25 12:55:57 +0530

    copy logs and xmls to artifact_dir

--- ci-operator/step-registry/3scale/interop-tests/3scale-interop-tests-commands.sh
+++ ci-operator/step-registry/3scale/interop-tests/3scale-interop-tests-commands.sh
@@ -6,3 +6,7 @@ set -o pipefail
 
 echo "Running 3scale interop tests"
 make smoke
+
+echo "Copying logs and xmls to ${ARTIFACT_DIR}"
+cp /test-run-results/junit-smoke.xml ${ARTIFACT_DIR}/junit_3scale_smoke.xml
+cp /test-run-results/report-smoke.html ${ARTIFACT_DIR}
