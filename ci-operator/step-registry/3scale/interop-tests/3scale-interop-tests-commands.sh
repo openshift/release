8f099a00257f1ddb132bf5fd664bce550a6b91fc
cchetna <cchetna@redhat.com>
2023-04-25 18:42:33 +0530

    login as kubeadmin for executing tests

--- ci-operator/step-registry/3scale/interop-tests/3scale-interop-tests-commands.sh
+++ ci-operator/step-registry/3scale/interop-tests/3scale-interop-tests-commands.sh
@@ -4,6 +4,16 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+export KUBECONFIG="$HOME/.kube/config"
+
+CONSOLE_URL=$(cat $SHARED_DIR/console.url)
+API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
+
+echo "Login as kubeadmin to the test cluster at ${API_URL}..."
+mkdir -p $HOME/.kube
+touch $HOME/.kube/config
+oc login -u kubeadmin -p "$(cat $SHARED_DIR/kubeadmin-password)" "${API_URL}" --insecure-skip-tls-verify=true
+
 echo "Running 3scale interop tests"
 make smoke
 
