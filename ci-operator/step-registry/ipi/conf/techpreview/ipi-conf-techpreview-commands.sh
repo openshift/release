49f49f38556c60ae2d39cacf11f818e31af9b736
Joel Speed <Joel.speed@hotmail.co.uk>
2023-04-27 09:54:16 +0100

    Update feature gate step registry steps to use the install config (#38769)
    * Update ipi-conf-techpreview to use install-config featureset
    
    * Remove ipi-conf-customnoupgrade

--- ci-operator/step-registry/ipi/conf/techpreview/ipi-conf-techpreview-commands.sh
+++ ci-operator/step-registry/ipi/conf/techpreview/ipi-conf-techpreview-commands.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-set -euo pipefail
 
-cat <<EOF > ${SHARED_DIR}/manifest_feature_gate.yaml
----
-apiVersion: config.openshift.io/v1
-kind: FeatureGate
-metadata:
-  annotations:
-    include.release.openshift.io/self-managed-high-availability: "true"
-    include.release.openshift.io/single-node-developer: "true"
-    release.openshift.io/create-only: "true"
-  name: cluster
-spec:
-  featureSet: TechPreviewNoUpgrade
+set -o nounset
+set -o errexit
+set -o pipefail
+
+CONFIG="${SHARED_DIR}/install-config.yaml"
+
+PATCH="${SHARED_DIR}/install-config-patch.yaml"
+cat > "${PATCH}" << EOF
+featureSet: ${FEATURE_SET}
 EOF
+yq-go m -x -i "${CONFIG}" "${PATCH}"
+echo "Updated featureSet in '${CONFIG}'."
+
+echo "The updated featureSet:"
+yq-go r "${CONFIG}" featureSet
