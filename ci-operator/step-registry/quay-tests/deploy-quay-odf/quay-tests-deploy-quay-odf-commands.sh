096e3b0e340894acee2f1b7101a3348fdd57bc05
Luffy Zhang <lzha@redhat.com>
2023-04-28 09:40:31 +0800

    Improve the checkpoint of quayregistry (#38794)
    * fix the checkpoint of quayregistry
    
    * add exit code once the check is failed
    
    * disable HPA

--- ci-operator/step-registry/quay-tests/deploy-quay-odf/quay-tests-deploy-quay-odf-commands.sh
+++ ci-operator/step-registry/quay-tests/deploy-quay-odf/quay-tests-deploy-quay-odf-commands.sh
@@ -4,7 +4,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-
 #Deploy ODF Operator to OCP namespace 'openshift-storage'
 OO_INSTALL_NAMESPACE=openshift-storage
 QUAY_OPERATOR_CHANNEL="$QUAY_OPERATOR_CHANNEL"
@@ -20,17 +19,17 @@ EOF
 
 OPERATORGROUP=$(oc -n "$OO_INSTALL_NAMESPACE" get operatorgroup -o jsonpath="{.items[*].metadata.name}" || true)
 if [[ -n "$OPERATORGROUP" ]]; then
-    echo "OperatorGroup \"$OPERATORGROUP\" exists: modifying it"
-    OG_OPERATION=apply
-    OG_NAMESTANZA="name: $OPERATORGROUP"
+  echo "OperatorGroup \"$OPERATORGROUP\" exists: modifying it"
+  OG_OPERATION=apply
+  OG_NAMESTANZA="name: $OPERATORGROUP"
 else
-    echo "OperatorGroup does not exist: creating it"
-    OG_OPERATION=create
-    OG_NAMESTANZA="generateName: oo-"
+  echo "OperatorGroup does not exist: creating it"
+  OG_OPERATION=create
+  OG_NAMESTANZA="generateName: oo-"
 fi
 
 OPERATORGROUP=$(
-    oc $OG_OPERATION -f - -o jsonpath='{.metadata.name}' <<EOF
+  oc $OG_OPERATION -f - -o jsonpath='{.metadata.name}' <<EOF
 apiVersion: operators.coreos.com/v1
 kind: OperatorGroup
 metadata:
@@ -42,7 +41,7 @@ EOF
 )
 
 SUB=$(
-    cat <<EOF | oc apply -f - -o jsonpath='{.metadata.name}'
+  cat <<EOF | oc apply -f - -o jsonpath='{.metadata.name}'
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
@@ -58,17 +57,16 @@ EOF
 )
 
 for _ in {1..60}; do
-    CSV=$(oc -n "$OO_INSTALL_NAMESPACE" get subscription "$SUB" -o jsonpath='{.status.installedCSV}' || true)
-    if [[ -n "$CSV" ]]; then
-        if [[ "$(oc -n "$OO_INSTALL_NAMESPACE" get csv "$CSV" -o jsonpath='{.status.phase}')" == "Succeeded" ]]; then
-            echo "ClusterServiceVersion \"$CSV\" ready"
-            break
-        fi
+  CSV=$(oc -n "$OO_INSTALL_NAMESPACE" get subscription "$SUB" -o jsonpath='{.status.installedCSV}' || true)
+  if [[ -n "$CSV" ]]; then
+    if [[ "$(oc -n "$OO_INSTALL_NAMESPACE" get csv "$CSV" -o jsonpath='{.status.phase}')" == "Succeeded" ]]; then
+      echo "ClusterServiceVersion \"$CSV\" ready"
+      break
     fi
-    sleep 10
+  fi
+  sleep 10
 done
 echo "ODF/OCS Operator is deployed successfully"
-
 
 #Deploy Quay Operator to OCP namespace 'quay-enterprise'
 cat <<EOF | oc apply -f -
@@ -90,7 +88,7 @@ spec:
 EOF
 
 SUB=$(
-    cat <<EOF | oc apply -f - -o jsonpath='{.metadata.name}'
+  cat <<EOF | oc apply -f - -o jsonpath='{.metadata.name}'
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
@@ -106,14 +104,14 @@ EOF
 )
 
 for _ in {1..60}; do
-    CSV=$(oc -n quay-enterprise get subscription quay-operator -o jsonpath='{.status.installedCSV}' || true)
-    if [[ -n "$CSV" ]]; then
-        if [[ "$(oc -n quay-enterprise get csv "$CSV" -o jsonpath='{.status.phase}')" == "Succeeded" ]]; then
-            echo "ClusterServiceVersion \"$CSV\" ready"
-            break
-        fi
+  CSV=$(oc -n quay-enterprise get subscription quay-operator -o jsonpath='{.status.installedCSV}' || true)
+  if [[ -n "$CSV" ]]; then
+    if [[ "$(oc -n quay-enterprise get csv "$CSV" -o jsonpath='{.status.phase}')" == "Succeeded" ]]; then
+      echo "ClusterServiceVersion \"$CSV\" ready"
+      break
     fi
-    sleep 10
+  fi
+  sleep 10
 done
 echo "Quay Operator is deployed successfully"
 
@@ -149,14 +147,17 @@ spec:
   components:
   - kind: monitoring
     managed: false
+  - kind: horizontalpodautoscaler
+    managed: false
 EOF
 
 for _ in {1..60}; do
-    if [[ "$(oc -n quay-enterprise get quayregistry quay -o jsonpath='{.status.conditions[?(@.type=="Available")].status}' || true)" == "True" ]]; then
-        echo "Quay is in ready status" >&2
-        exit 0
-    fi
-    sleep 15
+  if [[ "$(oc -n quay-enterprise get quayregistry quay -o jsonpath='{.status.conditions[?(@.type=="Available")].status}')" == "True" ]]; then
+    echo "Quay is in ready status" >&2
+    exit 0
+  fi
+  sleep 15
 done
 echo "Timed out waiting for Quay to become ready afer 15 mins" >&2
-oc -n quay-enterprise get quayregistries -o yaml > "$ARTIFACT_DIR/quayregistries.yaml"
+oc -n quay-enterprise get quayregistries -o yaml >"$ARTIFACT_DIR/quayregistries.yaml"
+exit 2
