06422ac6e0be1f762a2db05b0152a169a177b1c9
Sagi Shnaidman <sshnaidm@redhat.com>
2023-05-01 13:49:48 +0300

    telco5g: Run SNO job for telco5g CI

--- ci-operator/step-registry/telco5g/jobs-router/telco5g-jobs-router-commands.sh
+++ ci-operator/step-registry/telco5g/jobs-router/telco5g-jobs-router-commands.sh
@@ -35,16 +35,19 @@ if [[ "$PROW_JOB_ID" = *"nightly"* ]] && [[ "$JOB_TYPE" == "periodic" ]]; then
     echo "export T5_JOB_IMAGE=${IMG}" >> $MAINENV
     # In case of running on nightly releases we need to figure out what release exactly to use
     echo "export KCLI_PARAM=\"-P openshift_image=registry.ci.openshift.org/ocp/release:${IMG}\"" >> $MAINENV
+    echo "export SNO_PARAM=\"-f ${IMG}\"" >> $MAINENV
 elif [[ "$JOB_TYPE" == "periodic" ]]; then
     echo "export T5_JOB_TRIGGER=periodic" >> $MAINENV
     echo "export T5_JOB_DESC=periodic-${T5CI_JOB_TYPE}" >> $MAINENV
     echo "export T5_JOB_DESC_FULL=periodic-${T5CI_JOB_TYPE}-${T5CI_VERSION}" >> $MAINENV
     echo "export KCLI_PARAM=\"-P tag=${T5CI_VERSION} -P version=nightly\"" >> $MAINENV
+    echo "export SNO_PARAM=\"-t ${T5CI_VERSION} -r nightly\"" >> $MAINENV
 elif [[ "$JOB_TYPE" == "presubmit" ]] || [[ "$JOB_NAME" == *"rehears"* ]]; then
     echo "export T5_JOB_TRIGGER=rehearse" >> $MAINENV
     echo "export T5_JOB_DESC=rehearse-${T5CI_JOB_TYPE}" >> $MAINENV
     echo "export T5_JOB_DESC_FULL=rehearse-${T5CI_JOB_TYPE}-${T5CI_VERSION}" >> $MAINENV
     echo "export KCLI_PARAM=\"-P tag=${T5CI_VERSION} -P version=nightly\"" >> $MAINENV
+    echo "export SNO_PARAM=\"-t ${T5CI_VERSION} -r nightly\"" >> $MAINENV
     echo "export PULL_URL=https://github.com/${REPO_OWNER-}/${REPO_NAME-}/pull/${PULL_NUMBER-}" >> $MAINENV
     echo "export PULL_API_URL=https://api.github.com/repos/${REPO_OWNER-}/${REPO_NAME-}/pulls/${PULL_NUMBER-}" >> $MAINENV
 fi
