893982d5da53940ccfb07d652f26b4c17ae956e6
Riccardo piccoli <rpiccoli@redhat.com>
2023-05-05 10:55:59 +0200

    assisted installer: change triage bot time
    Signed-off-by: Riccardo piccoli <rpiccoli@redhat.com>

--- ci-operator/step-registry/assisted/tools/triage-report/assisted-tools-triage-report-commands.sh
+++ ci-operator/step-registry/assisted/tools/triage-report/assisted-tools-triage-report-commands.sh
@@ -7,13 +7,13 @@ set -o pipefail
 echo "************ assisted tools triage report command ************"
 
 set +e
-IS_REHEARSAL=$(expr "${REPO_OWNER:-}" = "openshift" "&" "${REPO_NAME:-}" = "release")
+#IS_REHEARSAL=$(expr "${REPO_OWNER:-}" = "openshift" "&" "${REPO_NAME:-}" = "release")
 set -e
 
-if (( ! ${IS_REHEARSAL} )) && [[ ${DRY_RUN} == "false" ]]; then
+#if (( ! ${IS_REHEARSAL} )) && [[ ${DRY_RUN} == "false" ]]; then
     export WEBHOOK
     WEBHOOK=$(cat ${CI_CREDENTIALS_DIR}/triage-webhook)
-fi
+#fi
 
 JIRA_ACCESS_TOKEN=$(cat ${CI_CREDENTIALS_DIR}/jira-access-token) \
     ./tools/triage_status_report.py
