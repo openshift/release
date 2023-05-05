9d699e84323c7992b28b01d277e9d64c90fc7603
Vincent Boulos <87659226+vboulos@users.noreply.github.com>
2023-05-04 20:59:08 -0400

    Vboulos add step rigistry for grc (#37587)
    * create the step registry for ALC and GRC
    
    * add the config file that triggers the acm interop tests for CLC, fetch and GRC
    
    * fix a typo in the acm-tests-alc-ref.yaml file
    
    * add owners files for GRC and ALC
    
    * remove ALC from multi stage test config
    
    * fix the grc test step name in multi stage test config
    
    * add the files that Make update has generated
    
    * remove unsued line of code in the acm-tests-grc-commands.sh
    
    * add the step-registry for obs
    
    * workout the values for base domain and cluster name
    
    * remove the obs from this PR
    
    * add step-registry for ACM Observability
    
    * modifications made bt the make update
    
    * add ALC and Obs base images in the acmqe-autotest config
    
    * modify the Obs tag in  the job
    
    * remove the obs base image for now
    
    * replace _ with - in obs
    
    * modify the ocp stream ti deploy stable instead of nightly version
    
    * modify the ocp release to latest
    
    * add the jobs modified by the make update
    
    * remove the obs from the job
    
    * add alc tests in the job
    
    * make execute_alc_interop_commands.sh run from its own dir
    
    * fix some ALC parameter names
    
    * modify the code to allow running automation on pre-existing hubs
    
    * add files generated by make update
    
    * minor update
    
    * add secrets directory to fetch
    
    * add creds for ci
    
    * modify the credentials namesapce for ci
    
    * change the browser to electron instead of chrom
    
    * include GRC in the acm-external job
    
    * add the RBAC to the grc vault
    
    * update the aws base domain to point to the new domain dedicated to the interop testing
    
    * add ci credentials to CLC
    
    * add debug to see the skip-ocp-deploy value
    
    * add Obs tests
    
    * make update changes
    
    * add Obs
    
    * fix the path to the ALC results folder
    
    * fix the path to the ALC results folder
    
    * make the Obs execute command executable
    
    * copy the kubeconfig to the directory used by Obs
    
    * include OBs only in testing
    
    * run Obs tests before GRC and ALC
    
    * create .kube file in the alabama dir
    
    * create .kube file in the alabama dir
    
    * update temporarily the BaseDomain and HUb cluster name in Obs
    
    * set the MINIO value as boolean instead of String
    
    * add a config file to install OCPand ACM only
    
    * add a config file to install OCPand ACM only
    
    * make update output
    
    * fix a typo
    
    * add 8 hours timeout in the deploy ocp only job
    
    * add pull secret to mch
    
    * make update
    
    * rename the image-ull-secret for mch
    
    * make the image pull secret name dymanic
    
    * disable the SKIP_OCP_DEPLOY by default
    
    * remove SKIP_OCP_DEPLOY= completely
    
    * delete test configs
    
    * remove the jobs
    
    * moving OBS to run last
    
    * make update
    
    * restore the config that promotes the acmqe-autotest image
    
    * make update
    
    * add the README files for ALC, GRC and OBS
    
    * Update ci-operator/jobs/stolostron/acmqe-autotest/stolostron-acmqe-autotest-main-periodics.yaml
    
    Co-authored-by: Adam Scerra <41153661+ascerra@users.noreply.github.com>
    
    * Update ci-operator/config/stolostron/acmqe-autotest/stolostron-acmqe-autotest-main__vboulos-acm-ocp4.12-lp-interop.yaml
    
    Co-authored-by: Adam Scerra <41153661+ascerra@users.noreply.github.com>
    
    * Update ci-operator/config/stolostron/acmqe-autotest/stolostron-acmqe-autotest-main__vboulos-acm-ocp4.12-lp-interop.yaml
    
    Co-authored-by: Adam Scerra <41153661+ascerra@users.noreply.github.com>
    
    * Update ci-operator/config/stolostron/acmqe-autotest/stolostron-acmqe-autotest-main__vboulos-acm-ocp4.12-lp-interop.yaml
    
    Co-authored-by: Adam Scerra <41153661+ascerra@users.noreply.github.com>
    
    * Update ci-operator/config/stolostron/acmqe-autotest/stolostron-acmqe-autotest-main__vboulos-acm-ocp4.12-lp-interop.yaml
    
    Co-authored-by: Adam Scerra <41153661+ascerra@users.noreply.github.com>
    
    * make update
    
    * remove config and jobs to merge the code
    
    ---------
    
    Co-authored-by: Adam Scerra <41153661+ascerra@users.noreply.github.com>

--- ci-operator/step-registry/acm/tests/grc/acm-tests-grc-commands.sh
+++ ci-operator/step-registry/acm/tests/grc/acm-tests-grc-commands.sh
@@ -16,17 +16,18 @@ fi
 
 export KUBECONFIG=${SHARED_DIR}/kubeconfig
 
-cp ${SECRETS_DIR}/clc/secret-options-yaml ./options.yaml
-
 # Set the dynamic vars based on provisioned hub cluster.
-CYPRESS_CLC_OCP_IMAGE_VERSION=$(oc get clusterversion version -o jsonpath='{.status.desired.version}' | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
-export CYPRESS_CLC_OCP_IMAGE_VERSION
+OC_CLUSTER_URL=$(oc whoami --show-server)
+export OC_CLUSTER_URL
 CYPRESS_BASE_URL=$(oc whoami --show-console)
 export CYPRESS_BASE_URL
-CYPRESS_OPTIONS_HUB_PASSWORD=$(cat $SHARED_DIR/kubeadmin-password)
-export CYPRESS_OPTIONS_HUB_PASSWORD
+OC_CLUSTER_PASS=$(cat $SHARED_DIR/kubeadmin-password)
+export OC_CLUSTER_PASS
+RBAC_PASS=$(cat $SECRETS_DIR/grc/rbac-pass)
+export RBAC_PASS
 
 # run the test execution script
-./execute_clc_interop_commands.sh
+./execute_grc_interop_commands.sh
 
-cp -r reports $ARTIFACT_DIR/
+# Copy the test cases results to an external directory
+cp -r test-output/cypress $ARTIFACT_DIR/
