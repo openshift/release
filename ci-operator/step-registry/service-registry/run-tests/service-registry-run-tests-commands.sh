59d0c4c2a3cc134ca85931f8f19e2576d70cb581
Radim Kubis <rkubis@redhat.com>
2023-05-05 10:46:50 +0200

    Update(step-registry): Script to run Red Hat Integration Service Registry tests

--- ci-operator/step-registry/service-registry/run-tests/service-registry-run-tests-commands.sh
+++ ci-operator/step-registry/service-registry/run-tests/service-registry-run-tests-commands.sh
@@ -4,8 +4,17 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+echo "Clone repository with system tests"
+git clone https://github.com/Apicurio/apicurio-registry-system-tests
+
 echo "Change directory to system tests in repository"
 cd apicurio-registry-system-tests/system-tests
+
+echo "Create target directory"
+mkdir target
+
+echo "Download Apicurio Registry CRD into target directory"
+wget https://raw.githubusercontent.com/Apicurio/apicurio-registry-operator/1.0.0-v2.0.0.final/packagemanifests/1.0.0-v2.0.0.final/registry.apicur.io_apicurioregistries.yaml -O target/registry.apicur.io_apicurioregistries.yaml
 
 echo "Run the tests"
 ./scripts/run-interop-tests.sh
