9624ca81ecf40132593126cdca27ca814bb39ae6
Biagio Manzari <bmanzari@redhat.com>
2023-05-09 16:57:29 +0200

    fix reverse ip6 use correct mac address

--- ci-operator/step-registry/baremetal/lab/pre/dns/baremetal-lab-pre-dns-commands.sh
+++ ci-operator/step-registry/baremetal/lab/pre/dns/baremetal-lab-pre-dns-commands.sh
@@ -23,6 +23,10 @@ function mac2ipv6() {
     awk -F: '{printf "fe80::%s%x%s:%sff:fe%s:%s%s\n", substr($1,1,1), xor(strtonum("0x"substr($1,2,1)),2), $2, $3, $4, $5, $6}'
 }
 
+function reverseIp6() {
+    echo "$1" | awk -F: 'BEGIN {OFS=""; }{addCount = 9 - NF; for(i=1; i<=NF;i++){if(length($i) == 0){ for(j=1;j<=addCount;j++){$i = ($i "0000");} } else { $i = substr(("0000" $i), length($i)+5-4);}}; print}' | rev | sed -e "s/./&./g"
+}
+
 
 # shellcheck disable=SC1090
 . <(yq e 'to_entries | .[] | (.key + "=\"" + .value + "\"")' < "${SHARED_DIR}"/external_vips.yaml)
@@ -51,7 +55,7 @@ for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
     exit 1
   fi
   # shellcheck disable=SC2154
-  IP6=$(mac2ipv6 "${provisioning_mac}")
+  IP6=$(mac2ipv6 "${mac}")
   echo "macaddress to ip6: ${IP6}" 
   DNS_FORWARD="${DNS_FORWARD}
 ${name}.${CLUSTER_NAME} IN A ${ip}
@@ -61,7 +65,7 @@ api-int.${CLUSTER_NAME} IN AAAA ${IP6}
 *.apps.${CLUSTER_NAME} IN AAAA ${IP6}"
   DNS_REVERSE_INTERNAL="${DNS_REVERSE_INTERNAL}
 $(echo "${IP6}." | ( rip=""; while read -r -d . b; do rip="$b${rip+.}${rip}"; done; echo "$rip" ))in-addr.arpa. IN PTR ${name}.${CLUSTER_NAME}.${BASE_DOMAIN}.
-$(echo "${ip}." | ( rip=""; while read -r -d . b; do rip="$b${rip+.}${rip}"; done; echo "$rip" ))in-addr.arpa. IN PTR ${name}.${CLUSTER_NAME}.${BASE_DOMAIN}."
+$("$(reverseIp6 "${IP6}")")ip6.arpa. IN PTR ${name}.${CLUSTER_NAME}.${BASE_DOMAIN}."
 done
 
 # TODO verify if the installation works with no external reverse dns entries
