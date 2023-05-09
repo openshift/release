1fbcad25ea5e253a14e3c1c2905112aaadbe7a6b
lisa <liswang@redhat.com>
2023-05-09 16:32:34 -0400

    test

--- ci-operator/step-registry/openshift-knative/e2e/openshift-knative-e2e-commands.sh
+++ ci-operator/step-registry/openshift-knative/e2e/openshift-knative-e2e-commands.sh
@@ -5,12 +5,9 @@ set -o pipefail
 set -x
 
 echo "This is the start SHARED_DIR: ${SHARED_DIR}"
-echo "password is: $SHARED_DIR/kubeadmin-password"
-echo "$(cat $SHARED_DIR/kubeadmin-password)"
 
-echo "$(ls -all $SHARED_DIR)"
-export KUBECONFIG="$SHARED_DIR/.kube/config"
-echo "KUBECONFIG: ${KUBECONFIG}"
+SECRETS_DIR="/tmp/secrets"
+export KUBECONFIG="{SHARED_DIR}/.kube/config"
 
 CONSOLE_URL=$(cat $SHARED_DIR/console.url)
 API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
@@ -19,26 +16,27 @@ echo "Login as Kubeadmin to the test cluster at ${API_URL}..."
 mkdir -p $SHARED_DIR/.kube
 touch $SHARED_DIR/.kube/config
 oc login -u kubeadmin -p "$(cat $SHARED_DIR/kubeadmin-password)" "${API_URL}" --insecure-skip-tls-verify=true
-oc whoami --show-console
+oc whoami --show-server
 
-echo "Login in my openstack cluster:"
-oc login https://api.liswang-test.osp.interop.ccitredhat.com:6443 --insecure-skip-tls-verify=true -u kubeadmin -p ywCdP-vQcho-xctBj-kmxoX
-oc whoami --show-console
+info "Current node/machine state:"
 
-oc get nodes -o wide
-
-echo "get running worker count:"
-oc -n openshift-machine-api get machines -l "machine.openshift.io/cluster-api-machine-role=worker" -o json
-
-echo "get worker machine set:"
-oc -n openshift-machine-api get machineset -o json
-
-echo "get machine:"
-oc  -n openshift-machine-api get machines
-
-echo "scale cluster:"
-first_worker_machine_set=3
-new_machineset_count=4
+first_worker_machine_set="$(oc -n openshift-machine-api get machineset -o json | jq -r '[.items[] |select(.spec.template.metadata.labels["machine.openshift.io/cluster-api-machine-role"]=="worker")][0] | .metadata.name'
+)"
+original_count="$(oc -n openshift-machine-api get machines -l "machine.openshift.io/cluster-api-machine-role=worker" -o json |     jq -r '.items | length')"
+echo "original_count: $original_count"
+current_replicas="$(oc -n openshift-machine-api get machineset "$first_worker_machine_set" -o json | jq -r '.spec.replicas')"
+new_machineset_count=$((current_replicas + 1))
 oc -n openshift-machine-api scale machineset "$first_worker_machine_set" --replicas="$new_machineset_count"
-sleep 80000
+
+expected_count=$((original_count + 1))
+# note: timeout of this loop is handled by the calling context
+while [[ "$(get_running_worker_count)" != "$expected_count" ]]; do
+    info "Current machine state does not have the running count we desire ($expected_count)"
+    oc  -n openshift-machine-api get machines
+    sleep 20
+done
+
 make test-e2e-with-kafka
+make teardown
+
+sleep 80000
