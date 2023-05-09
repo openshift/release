c393fcd08fc4404741ee291018019ff70375b04b
Kasturi Narra <knarra@knarra.remote.csb>
2023-05-09 13:12:32 +0530

    Adding PR to test SSO case

--- ci-operator/step-registry/openshift-extended/test/openshift-extended-test-commands.sh
+++ ci-operator/step-registry/openshift-extended/test/openshift-extended-test-commands.sh
@@ -276,6 +276,7 @@ function run {
         --provider "${TEST_PROVIDER}" -o "${ARTIFACT_DIR}/extended.log" \
         --timeout "${TEST_TIMEOUT}m" --junit-dir="${ARTIFACT_DIR}/junit" -f ./case_selected || ret_value=$?
     fi
+    sleep 3600
     set +x
     set +e
     rm -fr ./case_selected
@@ -421,3 +422,4 @@ function check_case_selected {
     fi
 }
 run
+sleep 4800
