614cda54ffd9e978cf08b2513a1ac9b596a89fae
Qiujie Li <qili@redhat.com>
2023-05-11 15:57:59 +0800

    add router-perf and node-density-heavy workload to perfscale ci

--- ci-operator/step-registry/openshift-qe/node-density-heavy/openshift-qe-node-density-heavy-commands.sh
+++ ci-operator/step-registry/openshift-qe/node-density-heavy/openshift-qe-node-density-heavy-commands.sh
@@ -14,18 +14,19 @@ source ./venv_qe/bin/activate
 ES_PASSWORD=$(cat "/secret/password")
 ES_USERNAME=$(cat "/secret/username")
 
-git clone https://github.com/cloud-bulldozer/e2e-benchmarking --depth=1
+git clone https://github.com/cloud-bulldozer/e2e-benchmarking
 pushd e2e-benchmarking/workloads/kube-burner
-export WORKLOAD=node-density
-current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker --output jsonpath="{.items[?(@.status.conditions[-1].type=='Ready')].status.conditions[-1].type}" | wc -w | xargs)
+export WORKLOAD=node-density-heavy
+
+current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker | grep -v "NotReady\\|SchedulingDisabled" | wc -l | xargs)
 export NODE_COUNT=$(($current_worker_count))
 export PODS_PER_NODE=230
-export POD_READY_THRESHOLD=180000ms
-export GEN_CSV=true
-export COMPARISON_CONFIG="clusterVersion.json podLatency.json containerMetrics.json kubelet.json etcd.json crio.json nodeMasters-max.json nodeWorkers.json"
-
 export CLEANUP_WHEN_FINISH=true
 
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
+export COMPARISON_CONFIG="clusterVersion.json podLatency.json containerMetrics.json kubelet.json etcd.json crio.json nodeMasters-max.json nodeWorkers.json"
+export GEN_CSV=true
+export GSHEET_KEY_LOCATION="${CLUSTER_PROFILE_DIR}/gce.json"
+export EMAIL_ID_FOR_RESULTS_SHEET='qili@redhat.com'
 
 ./run.sh
