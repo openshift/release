1a196354cc9d5f17dfab9ade43d9304b829d6f51
rnetser <rnetser@redhat.com>
2023-05-11 16:41:55 +0300

    MSI - add RHODS operator tests

--- ci-operator/step-registry/operator/install/operator-install-commands.sh
+++ ci-operator/step-registry/operator/install/operator-install-commands.sh
@@ -5,17 +5,11 @@ set -o errexit
 set -o pipefail
 set -o verbose
 
-CLUSTER_NAME=$(cat "${SHARED_DIR}/cluster-name")
-OCM_TOKEN=$(cat /var/run/secrets/ci.openshift.io/cluster-profile/ocm-token)
+export KUBECONFIG=${SHARED_DIR}/kubeconfig
 
-poetry run python app/cli.py addon \
-    --addons "${ADDON1_CONFIG}" \
-    --addons "${ADDON2_CONFIG}" \
-    --addons "${ADDON3_CONFIG}" \
-    --addons "${ADDON4_CONFIG}" \
-    --cluster "${CLUSTER_NAME}" \
-    --token "${OCM_TOKEN}" \
-    --api-host "${API_HOST}" \
+poetry run python3 app/cli.py operator \
+    --kubeconfig "${KUBECONFIG}" \
+    --name "${OPERATOR_NAME}" \
+    --namespace "${OPERATOR_NAMESPACE}" \
     --timeout "${TIMEOUT}" \
-    --parallel "${PARALLEL}" \
-    install \
+    install
