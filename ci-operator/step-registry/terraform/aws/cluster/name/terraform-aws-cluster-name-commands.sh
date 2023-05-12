73c3a1f3c679124e5de8ebaa8857c2ac97c44759
Asher Shoshan <ashoshan@redhat.com>
2023-05-12 21:27:23 +0300

    init

---
+++ ci-operator/step-registry/terraform/aws/cluster/name/terraform-aws-cluster-name-commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
+
+echo "${CLUSTER_NAME_PREFIX}-$(mktemp -u XXXXX | tr '[:upper:]' '[:lower:]') > ${SHARED_DIR}/cluster_name
