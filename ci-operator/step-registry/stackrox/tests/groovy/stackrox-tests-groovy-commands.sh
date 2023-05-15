b4ed4d68a7630e3c447a55c0cfc92b52b689630f
Brad Lugo <blugo@redhat.com>
2023-05-15 12:05:53 -0700

    Add test groovy step

---
+++ ci-operator/step-registry/stackrox/tests/groovy/stackrox-tests-groovy-commands.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# TODO :: Check if these files exist
+# shellcheck source=/dev/null
+source "scripts/ci/lib.sh"
+# shellcheck source=/dev/null
+source "tests/e2e/lib.sh"
+# shellcheck source=/dev/null
+source "scripts/ci/gcp.sh"
+# shellcheck source=/dev/null
+source "scripts/ci/sensor-wait.sh"
+# shellcheck source=/dev/null
+source "scripts/ci/create-webhookserver.sh"
+# shellcheck source=/dev/null
+source "tests/scripts/setup-certs.sh"
+# shellcheck source=/dev/null
+source "qa-tests-backend/scripts/lib.sh"
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+openshift_ci_mods
+openshift_ci_import_creds
+create_exit_trap
+
+# Might not need some of these but adding for consistency for now
+export CI_JOB_NAME="ocp-qa-e2e-tests"
+export DEPLOY_STACKROX_VIA_OPERATOR="true"
+export ORCHESTRATOR_FLAVOR="openshift"
+export ROX_POSTGRES_DATASTORE="true"
+
+gather_debug_for_cluster_under_test
+poll_for_system_test_images "3600"
+
+SHARED_STACKROX="${SHARED_DIR}/stackrox"
+DEPLOY_DIR="${SHARED_STACKROX}/deploy/${ORCHESTRATOR_FLAVOR}"
+
+# Essentially replicates reuse_config_part_1
+# There might be logic in here that is unnecessary
+info "Reusing config from a prior part 1 e2e test"
+
+export_test_environment
+setup_deployment_env false false
+export_default_TLS_certs "$DEPLOY_DIR/default_TLS_certs"
+export_client_TLS_certs "$DEPLOY_DIR/client_TLS_certs"
+
+create_webhook_server_port_forward
+export_webhook_server_certs "$DEPLOY_DIR/webhook_server_certs"
+get_ECR_docker_pull_password
+
+wait_for_api
+export_central_basic_auth_creds
+
+export CLUSTER="${ORCHESTRATOR_FLAVOR^^}"
+
+# Essentially replicates test_part_1
+# There might be logic in here that is unnecessary
+info "QA Automation Platform Part 1"
+
+if [[ "${ORCHESTRATOR_FLAVOR}" == "openshift" ]]; then
+    oc get scc qatest-anyuid || oc create -f "qa-tests-backend/src/k8s/scc-qatest-anyuid.yaml"
+fi
+
+export CLUSTER="${ORCHESTRATOR_FLAVOR^^}"
+
+# TODO :: Why is this here?
+rm -f FAIL
+
+if is_openshift_CI_rehearse_PR; then
+    info "On an openshift rehearse PR, running BAT tests only..."
+    test_target="bat-test"
+elif is_in_PR_context && pr_has_label ci-all-qa-tests; then
+    info "ci-all-qa-tests label was specified, so running all QA tests..."
+    test_target="test"
+elif is_in_PR_context; then
+    info "In a PR context without ci-all-qa-tests, running BAT tests only..."
+    test_target="bat-test"
+else
+    info "Running all QA tests by default..."
+    test_target="test"
+fi
+
+update_job_record "test_target" "${test_target}"
+
+make -C qa-tests-backend "${test_target}" || touch FAIL
+
+store_qa_test_results "part-1-tests"
+[[ ! -f FAIL ]] || die "Part 1 tests failed"
