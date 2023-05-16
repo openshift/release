6a95019b896f776084e24e0d08750f60903fa968
Manoj Hans <mhans@redhat.com>
2023-05-16 10:29:44 +0200

    add credentials on day 2 (#39161)

--- ci-operator/step-registry/cucushift/agent/vsphere/postinstall/cucushift-agent-vsphere-postinstall-commands.sh
+++ ci-operator/step-registry/cucushift/agent/vsphere/postinstall/cucushift-agent-vsphere-postinstall-commands.sh
@@ -17,6 +17,7 @@ oc get nodes
 # Backup
 oc get secret vsphere-creds -o yaml -n kube-system > "${SHARED_DIR}"/creds_backup.yaml
 oc get cm cloud-provider-config -o yaml -n openshift-config > "${SHARED_DIR}"/cloud-provider-config_backup.yaml
+oc get infrastructures.config.openshift.io -o yaml > "${SHARED_DIR}"/infrastructures.config.openshift.io_backup.yaml
 
 cat <<EOF | oc replace -f -
 apiVersion: v1
@@ -32,7 +33,7 @@ stringData:
   "${VSPHERE_VCENTER}.password": "${VSPHERE_PASSWORD}"
 EOF
 
-oc patch kubecontrollermanager cluster -p='{"spec": {"forceRedeploymentReason": "recovery-'"$( date --rfc-3339=ns )"'"}}' --type=merge
+oc patch kubecontrollermanager cluster -p='{"spec": {"forceRedeploymentReason": "recovery-'"$(date --rfc-3339=ns)"'"}}' --type=merge
 
 cat <<EOF | oc apply -f -
 apiVersion: v1
@@ -55,6 +56,55 @@ data:
     datacenters = "${VSPHERE_DATACENTER}"
 EOF
 
+OPENSHIFT_VERSION=$(oc get clusterversion -o jsonpath={..desired.version} | cut -d '.' -f 1,2)
+
+if awk "BEGIN {exit !($OPENSHIFT_VERSION > 4.12)}"; then
+
+  NODES=$(oc get node -o jsonpath='{.items[*].metadata.name}')
+
+  for NODE in $NODES; do
+    oc adm taint node "${NODE}" node.cloudprovider.kubernetes.io/uninitialized=true:NoSchedule || true
+  done
+
+  INFRASTRUCTURE_NAME=$(oc get infrastructures.config.openshift.io -o jsonpath='{.items[*].status.infrastructureName}')
+
+  cat <<EOF | oc apply -f - --overwrite=true
+apiVersion: v1
+kind: List
+items:
+- apiVersion: config.openshift.io/v1
+  kind: Infrastructure
+  metadata:
+    name: cluster
+  spec:
+    cloudConfig:
+      key: config
+      name: cloud-provider-config
+    platformSpec:
+      type: VSphere
+      vsphere:
+        failureDomains:
+        - name: generated-failure-domain
+          region: generated-region
+          server: ${VSPHERE_VCENTER}
+          topology:
+            computeCluster: /${VSPHERE_DATACENTER}/host/${INFRASTRUCTURE_NAME}
+            datacenter: ${VSPHERE_DATACENTER}
+            datastore: /${VSPHERE_DATACENTER}/datastore/${VSPHERE_DATASTORE}
+            networks:
+            - ${LEASED_RESOURCE}
+          zone: generated-zone
+        nodeNetworking:
+          external: {}
+          internal: {}
+        vcenters:
+        - datacenters:
+          - ${VSPHERE_DATACENTER}
+          port: 443
+          server: ${VSPHERE_VCENTER}
+EOF
+fi
+
 oc patch clusterversion version --type json -p '[{"op": "remove", "path": "/spec/channel"}]}]'
 
 until \
@@ -64,3 +114,5 @@ until \
     echo "$(date --rfc-3339=seconds) Clusteroperators not yet ready"
     sleep 1s
 done
+
+oc get co
