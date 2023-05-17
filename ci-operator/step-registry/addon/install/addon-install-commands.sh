a9e56fd46fe2820abb7899e39964878bca020839
rnetser <rnetser@redhat.com>
2023-05-17 11:36:56 +0300

    MSI - RHODS / GPU tests

--- ci-operator/step-registry/addon/install/addon-install-commands.sh
+++ ci-operator/step-registry/addon/install/addon-install-commands.sh
@@ -8,18 +8,13 @@ set -o verbose
 CLUSTER_NAME=$(cat "${SHARED_DIR}/cluster-name")
 OCM_TOKEN=$(cat /var/run/secrets/ci.openshift.io/cluster-profile/ocm-token)
 BREW_TOKEN=$(cat /var/run/secrets/ci.openshift.io/cluster-profile/brew-token)
-RUN_COMMAND="
-    --cluster ${CLUSTER_NAME} \
-    --token ${OCM_TOKEN} \
-    --api-host ${API_HOST} \
-    --timeout ${TIMEOUT} \
-    "
+RUN_COMMAND="poetry run python app/cli.py addon --cluster ${CLUSTER_NAME} --token ${OCM_TOKEN} --api-host ${API_HOST} --timeout ${TIMEOUT} "
 
 ADDONS_CMD=""
 for i in {1..4}; do
   ADDON_VALUE=$(eval "echo $"ADDON$i"_CONFIG")
   if [[ -n $ADDON_VALUE ]]; then
-    ADDONS_CMD+=" --addons ${ADDON_VALUE}"
+    ADDONS_CMD+=" --addons ${ADDON_VALUE} "
   fi
 done
 
@@ -37,8 +32,8 @@ if [ -n "${BREW_TOKEN}" ]; then
     RUN_COMMAND+=" --brew-token ${BREW_TOKEN} "
 fi
 
-echo "$RUN_COMMAND"
+RUN_COMMAND+=" install"
 
-poetry run python app/cli.py addon \
-    ${RUN_COMMAND} \
-    install
+echo "$RUN_COMMAND" | sed -r "s/token [=A-Za-z0-9\.\-]+/token hashed-token /g"
+
+${RUN_COMMAND}
