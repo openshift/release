82af883c66b195d6e1ce3843eaa7353d6a87dde3
rnetser <rnetser@redhat.com>
2023-05-17 11:36:56 +0300

    MSI - RHODS / GPU tests

--- ci-operator/step-registry/addon/uninstall/addon-uninstall-commands.sh
+++ ci-operator/step-registry/addon/uninstall/addon-uninstall-commands.sh
@@ -18,20 +18,18 @@ ADDONS_CMD=""
 for i in {1..4}; do
   ADDON_VALUE=$(eval "echo $"ADDON$i"_CONFIG")
   if [[ -n $ADDON_VALUE ]]; then
-    ADDONS_CMD="${ADDONS_CMD} --addons ${ADDON_VALUE}"
+    ADDONS_CMD+=" --addons ${ADDON_VALUE}"
   fi
 done
-
-echo "$ADDONS_CMD"
 
 RUN_COMMAND="${RUN_COMMAND} ${ADDONS_CMD}"
 
 if [ -n "${PARALLEL}" ]; then
-    RUN_COMMAND="${RUN_COMMAND} --parallel"
+    RUN_COMMAND+=" --parallel"
 fi
 
 if [ -n "${ROSA}" ]; then
-    RUN_COMMAND="${RUN_COMMAND} --rosa"
+    RUN_COMMAND+=" --rosa"
 fi
 
 echo "$RUN_COMMAND"
