17339e970f32427ea8d02f63cd31d1b650d44660
Caleb Evans <caevans@redhat.com>
2023-05-17 14:20:01 -0600

    Add an option to ignore test failures to firewatch

--- ci-operator/step-registry/firewatch/report-issues/firewatch-report-issues-commands.sh
+++ ci-operator/step-registry/firewatch/report-issues/firewatch-report-issues-commands.sh
@@ -7,9 +7,14 @@ set -o pipefail
 # Create the Jira configuration file
 firewatch jira_config_gen --token_path "${FIREWATCH_JIRA_API_TOKEN_PATH}" --server_url "${FIREWATCH_JIRA_SERVER}"
 
+command="firewatch report"
+
 if [ "$FIREWATCH_FAIL_WITH_TEST_FAILURES" = "true" ]; then
-    firewatch report --fail_with_test_failures
-else
-    firewatch report
+    command+=" --fail_with_test_failures"
 fi
 
+if [ "$FIREWATCH_IGNORE_TEST_FAILURES" = "true" ]; then
+    command+=" --ignore_test_failures"
+fi
+
+eval "$command"
