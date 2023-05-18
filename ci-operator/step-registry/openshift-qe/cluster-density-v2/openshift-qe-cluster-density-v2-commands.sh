bc9bd23706cb42205553496979fd9f86081e4a84
Paige Rubendall <64206430+paigerube14@users.noreply.github.com>
2023-05-18 08:23:28 -0400

    Cluster density v2 - perfscale  (#39269)
    * adding clsuter density v2
    
    * no grep get node list
    
    * taking out compare configs for cluster density and updating churn duration to be shorter
    
    * no gen csv
    
    * metadata
    
    * adding qe gcp
    
    * periodics auto update
    
    * depth clone

--- ci-operator/step-registry/openshift-qe/cluster-density-v2/openshift-qe-cluster-density-v2-commands.sh
+++ ci-operator/step-registry/openshift-qe/cluster-density-v2/openshift-qe-cluster-density-v2-commands.sh
@@ -14,17 +14,15 @@ source ./venv_qe/bin/activate
 ES_PASSWORD=$(cat "/secret/password")
 ES_USERNAME=$(cat "/secret/username")
 
-git clone https://github.com/cloud-bulldozer/e2e-benchmarking --depth=1
-pushd e2e-benchmarking/workloads/kube-burner
-export WORKLOAD=node-density
-current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker --output jsonpath="{.items[?(@.status.conditions[-1].type=='Ready')].status.conditions[-1].type}" | wc -w | xargs)
-export NODE_COUNT=$(($current_worker_count))
-export PODS_PER_NODE=230
-export POD_READY_THRESHOLD=120000ms
-export GEN_CSV=true
-export COMPARISON_CONFIG="clusterVersion.json podLatency.json containerMetrics.json kubelet.json etcd.json crio.json nodeMasters-max.json nodeWorkers.json"
 
-export CLEANUP_WHEN_FINISH=true
+git clone https://github.com/cloud-bulldozer/e2e-benchmarking --depth=1
+pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
+
+current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker --output jsonpath="{.items[?(@.status.conditions[-1].type=='Ready')].status.conditions[-1].type}" | wc -w | xargs)
+
+export ITERATIONS=$((9*$current_worker_count))
+export WORKLOAD=cluster-density-v2
+export EXTRA_FLAGS="--churn-duration=20m"
 
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
 
