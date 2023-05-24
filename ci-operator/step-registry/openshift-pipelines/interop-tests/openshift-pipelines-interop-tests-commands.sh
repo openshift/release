7ce78b8adc507bcd02bf35120a23aeaac42b42d7
cchetna <cchetna@redhat.com>
2023-05-24 16:09:25 +0530

    add sleep command

--- ci-operator/step-registry/openshift-pipelines/interop-tests/openshift-pipelines-interop-tests-commands.sh
+++ ci-operator/step-registry/openshift-pipelines/interop-tests/openshift-pipelines-interop-tests-commands.sh
@@ -6,13 +6,16 @@ set -o pipefail
 
 XML_PATH="/tmp/release-tests/reports/xml-report/result.xml"
 
+sleep 30m
+
 echo "Running gauge specs parallely..."
 gauge run --log-level=debug --verbose --tags sanity -p specs/clustertasks/ specs/pipelines/ specs/triggers/ specs/hub/ specs/metrics/ specs/pac/ specs/operator/addon.spec specs/operator/post-upgrade.spec specs/operator/pre-upgrade.spec
 cp $XML_PATH ${ARTIFACT_DIR}/junit_parallel_specs.xml
 
-echo "Running gauge specs serially..."
+echo "Running auto-prune spec..."
 gauge run --log-level=debug --verbose --tags sanity specs/operator/auto-prune.spec
 cp $XML_PATH ${ARTIFACT_DIR}/junit_auto_prune.xml
 
+echo "Running rbac spec..."
 gauge run --log-level=debug --verbose --tags sanity specs/operator/rbac.spec
 cp $XML_PATH ${ARTIFACT_DIR}/junit_rbac.xml
