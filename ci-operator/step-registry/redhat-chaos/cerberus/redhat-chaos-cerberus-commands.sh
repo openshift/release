f36c924aeabb2f3c82271306c4d5fc5ebde34282
Paige Rubendall <prubenda@redhat.com>
2023-05-24 12:49:18 -0400

    adding cerberus observer

---
+++ ci-operator/step-registry/redhat-chaos/cerberus/redhat-chaos-cerberus-commands.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+while [ ! -f "${KUBECONFIG}" ]; do
+  printf "%s: waiting for %s\n" "$(date --utc --iso=s)" "${KUBECONFIG}"
+  sleep 10
+done
+printf "%s: acquired %s\n" "$(date --utc --iso=s)" "${KUBECONFIG}"
+
+ls
+
+git clone https://github.com/redhat-chaos/krkn-hub.git
+pushd krkn-hub/cerberus
+
+echo "kubeconfig loc $$KUBECONFIG"
+
+export CERBERUS_KUBECONFIG=$KUBECONFIG
+export CERBERUS_WATCH_NAMESPACES="[^.*$]"
+export CERBERUS_IGNORE_PODS="[^installer*,^kube-burner*,^redhat-operators*,^certified-operators*,^collect-profiles*]"
+./prow_run.sh
+rc=$?
+echo "Finished running cerberus scenarios"
+echo "Return code: $rc"
