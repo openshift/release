5967ba012c64cc65636f87c36c43b995c72d1a0e
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2023-05-25 11:39:24 +0200

    USHIFT-924: Add performance monitoring logs to VM (#36845)
    * USHIFT-924: Add pcp and pmlogs collection refs
    
    Add refs to enable performance metrics collection in any job. By
    using performance co-pilot package now it is possible to enable
    performance metrics logging for later retrieval.
    The pmlogs ref will retrieve all the raw data and place it under
    the artifacts subdir for later analysis with other tools.
    
    * USHIFT-924: Add performance metrics collection
    
    * USHIFT-924: Adapt pcp to AWS
    
    * USHIFT-924: Adapt pmlogs to AWS
    
    * USHIFT-924: Add pcp and pmlogs to AWS pre/post
    
    * USHIFT-924: recursive copy from pmlogs dir

---
+++ ci-operator/step-registry/openshift/microshift/infra/pcp/openshift-microshift-infra-pcp-commands.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eux
+
+trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
+
+IP_ADDRESS="$(cat ${SHARED_DIR}/public_address)"
+HOST_USER="$(cat ${SHARED_DIR}/ssh_user)"
+
+echo "Using Host $IP_ADDRESS"
+
+mkdir -p "${HOME}/.ssh"
+cat <<EOF >"${HOME}/.ssh/config"
+Host ${IP_ADDRESS}
+  User ${HOST_USER}
+  IdentityFile ${CLUSTER_PROFILE_DIR}/ssh-privatekey
+  StrictHostKeyChecking accept-new
+  ServerAliveInterval 30
+  ServerAliveCountMax 1200
+EOF
+chmod 0600 "${HOME}/.ssh/config"
+
+ssh "${IP_ADDRESS}" "sudo dnf install -y pcp-zeroconf; sudo systemctl start pmcd; sudo systemctl start pmlogger"
