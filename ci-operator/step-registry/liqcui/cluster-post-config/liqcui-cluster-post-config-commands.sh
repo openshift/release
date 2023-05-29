45d7a36d5e6e744eba2a28e623f0405af6e122e3
psap-user <liqcui@redhat.com>
2023-05-29 12:21:15 +0000

    update code for cluster-post-config

---
+++ ci-operator/step-registry/liqcui/cluster-post-config/liqcui-cluster-post-config-commands.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+cat /etc/os-release
+oc config view
+oc projects
+python --version
+pushd /tmp
+python -m virtualenv ./venv_qe
+source ./venv_qe/bin/activate
+
+ls -ls ~/.kube/
+# Environment setup
+cd cluster-post-config
+./install-infra-workload.sh
+
+# Remove the folder to resolve OCPQE-12185
+rm -rf performance-dashboards
+DITTYBOPPER_REPO=https://github.com/cloud-bulldozer/performance-dashboards.git
+DITTYBOPPER_REPO_BRANCH=master
+DITTYBOPPER_PARAMS=""
+git clone --single-branch --branch $DITTYBOPPER_REPO_BRANCH --depth 1 $DITTYBOPPER_REPO
+pushd performance-dashboards/dittybopper
+./deploy.sh $DITTYBOPPER_PARAMS
+popd
