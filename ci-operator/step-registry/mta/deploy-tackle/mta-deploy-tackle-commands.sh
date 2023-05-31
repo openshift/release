1e325f48670d24b59349228032aca2dcdeca411d
Caleb Evans <caevans@redhat.com>
2023-05-31 11:35:23 -0600

    Add the start of the MTA scenario

---
+++ ci-operator/step-registry/mta/deploy-tackle/mta-deploy-tackle-commands.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# Deploy windup
+echo "Deploying Tackle"
+oc apply -f - <<EOF
+kind: Tackle
+apiVersion: tackle.konveyor.io/v1alpha1
+metadata:
+  name: tackle
+  namespace: mta
+spec:
+  hub_bucket_volume_size: "80Gi"
+  cache_data_volume_size: "20Gi"
+  rwx_supported: "true"
+EOF
+
+sleep 7200
