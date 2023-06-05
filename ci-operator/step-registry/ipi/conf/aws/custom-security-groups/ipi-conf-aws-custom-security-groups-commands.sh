2c37a2f3eeb10ea75f4bfbcc6beccc6b56f851ce
Brent Barbachem <barbacbd@dukes.jmu.edu>
2023-06-05 15:24:27 -0400

    ** Added a deprovision step for custom security groups

--- ci-operator/step-registry/ipi/conf/aws/custom-security-groups/ipi-conf-aws-custom-security-groups-commands.sh
+++ ci-operator/step-registry/ipi/conf/aws/custom-security-groups/ipi-conf-aws-custom-security-groups-commands.sh
@@ -8,28 +8,24 @@ trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wa
 
 export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 
+REGION="${LEASED_RESOURCE}"
 CONFIG="${SHARED_DIR}/install-config.yaml"
-subnet_ids_file="${SHARED_DIR}/subnet_ids"
 az_file="${SHARED_DIR}/availability_zones"
 sg_file="${SHARED_DIR}/security_groups"
 
-if [ ! -f "${subnet_ids_file}" ] || [ ! -f "${az_file}" ]; then
-  echo "File ${subnet_ids_file} or ${az_file} does not exist."
-  exit 1
+if [ ! -f "${az_file}" ]; then
+    availability_zones="$(aws --region "${REGION}" ec2 describe-availability-zones | jq -r '.AvailabilityZones[] | select(.State == "available") | .ZoneName' | sort -u)"
+    echo "${availability_zones}" > ${az_file}
 fi
-
-echo -e "subnets: $(cat ${subnet_ids_file})"
 echo -e "AZs: $(cat ${az_file})"
-REGION="$(yq-go r "${CONFIG}" 'platform.aws.region')"
-echo Using region: ${REGION}
 
 aws_subnet="$(yq-go r "${CONFIG}" 'platform.aws.subnets[0]')"
 metadata_name="$(yq-go r "${CONFIG}" 'metadata.name')"
 vpc_id="$(aws --region "${REGION}" ec2 describe-subnets --subnet-ids "${aws_subnet}" | jq -r '.[][0].VpcId')"
 echo "Using vpc_id: ${vpc_id}"
 
-security_group="$(aws ec2 create-security-group --region ${REGION} --description 'CI custom security groups' --group-name 'CI ${metadata_name} sg'  --vpc-id ${vpc_id})"
-echo ${security_group} > ${sg_file}
+security_group=$(aws ec2 create-security-group --region ${REGION} --description 'CI custom security groups' --group-name "${metadata_name}-sg"  --vpc-id ${vpc_id} --output text)
+echo "${security_group}" > "${sg_file}"
 echo "Security Group: ${security_group}"
 
 CONFIG_PATCH="${SHARED_DIR}/install-config-security-groups.yaml.patch"
@@ -37,10 +33,10 @@ cat > "${CONFIG_PATCH}" << EOF
 compute:
 - platform:
     aws:
-      additionalSecurityGroupIDs: $(cat "${subnet_ids_file}")
+      additionalSecurityGroupIDs: [$(cat "${sg_file}")]
 controlPlane:
   platform:
     aws:
-      additionalSecurityGroupIDs: $(cat "${subnet_ids_file}")
+      additionalSecurityGroupIDs: [$(cat "${sg_file}")]
 EOF
 yq-go m -x -i "${CONFIG}" "${CONFIG_PATCH}"
