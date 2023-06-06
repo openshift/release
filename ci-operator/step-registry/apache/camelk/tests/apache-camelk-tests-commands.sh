853a5b29297d47c8a0477d079a6ef86061ea84ba
Mrutyunjay Acharya <macharya@redhat.com>
2023-06-06 18:00:21 +0530

    Added test execution in step registry

--- ci-operator/step-registry/apache/camelk/tests/apache-camelk-tests-commands.sh
+++ ci-operator/step-registry/apache/camelk/tests/apache-camelk-tests-commands.sh
@@ -8,8 +8,11 @@ set -o pipefail
 # This files name is the key value and the contents is a single clear-text line containing the value.
 # See the Secrets Guide for more information.
 
-export NAMESPACE=$DEPL_PROJECT_NAME
+export NAMESPACE=$CAMELK_NAMESPACE
 echo "Running camelk interop tests"
+
+docker run --user 1000:1000 -e KUBECONFIG=/data/auth/kubeconfig/config quay.io/fuse_qe/camel-k-e2e:latest
+
 
 # This line is using the cat command to set the contents of a credential file to a variable.
 #CREDENTIAL=$(cat /tmp/secrets/credentials/password)
