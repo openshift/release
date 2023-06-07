e35d763af30eb109d71a3ea8596069afac7f0191
Asher Shoshan <ashoshan@redhat.com>
2023-06-07 14:59:10 +0300

    find version by channel

--- ci-operator/step-registry/rosa/terraform/e2e/teardown/rosa-terraform-e2e-teardown-commands.sh
+++ ci-operator/step-registry/rosa/terraform/e2e/teardown/rosa-terraform-e2e-teardown-commands.sh
@@ -19,6 +19,8 @@ go mod vendor
 
 EXIT_STATUS=0
 
+export GATEWAY_URL="$(echo "${TF_VARS}" | grep 'url' | awk -F '=' '{print $2}' | sed 's/[ |"]//g')"
+
 export ARCHIVE_NAME=cluster-terraform-archive
 export TF_FOLDER="${TF_FOLDER:-ci/e2e/terraform_provider_ocm_files}"
 make destroy_folder || EXIT_STATUS=$?
