ae7cd080aa4f276823ab68dbefde2e803eb24d67
psap-user <liqcui@redhat.com>
2023-06-08 08:25:45 +0000

    update code

---
+++ ci-operator/step-registry/openshift-qe/loaded-upgrade/openshift-qe-loaded-upgrade-commands.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+cat /etc/os-release
+#oc config view
+#oc projects
+#python --version
+#pushd /tmp
+#python -m virtualenv ./venv_qe
+#source ./venv_qe/bin/activate
+
+#ES_PASSWORD=$(cat "/secret/password")
+#ES_USERNAME=$(cat "/secret/username")
+
+#git clone https://github.com/cloud-bulldozer/e2e-benchmarking --depth=1
+#pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
+#export EXTRA_FLAGS="--pods-per-node=$PODS_PER_NODE --pod-ready-threshold=$POD_READY_THRESHOLD"
+#export WORKLOAD=loaded-upgrade
+
+#export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
+pwd
+echo  "------------------------------------------"
+ls
+echo $UPGRADE_FROM to UPGRADE_TO
+#./run.sh
+sleep 3600
