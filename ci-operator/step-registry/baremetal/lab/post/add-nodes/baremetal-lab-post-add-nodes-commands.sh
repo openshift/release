a5a532eb25136ed34742b979ddfd40594ad30a0a
Steeve Goveas <sgoveas@redhat.com>
2023-06-09 16:33:56 +0100

    Fixes and updates after review

--- ci-operator/step-registry/baremetal/lab/post/add-nodes/baremetal-lab-post-add-nodes-commands.sh
+++ ci-operator/step-registry/baremetal/lab/post/add-nodes/baremetal-lab-post-add-nodes-commands.sh
@@ -12,11 +12,6 @@ master00_bootMacAddr=$(yq e '.[] | select(.name == "master-00") | .provisioning_
 master00_username=$(yq e '.[] | select(.name == "master-00") | .bmc_user' ${SHARED_DIR}/hosts.yaml)
 master00_pass=$(yq e '.[] | select(.name == "master-00") | .bmc_pass' ${SHARED_DIR}/hosts.yaml)
 
-workera_addr=$(yq e '.[] | select(.name == "worker-a-00") | .bmc_address' ${SHARED_DIR}/hosts.yaml)
-workera_bootMacAddr=$(yq e '.[] | select(.name == "worker-a-00") | .provisioning_mac' ${SHARED_DIR}/hosts.yaml)
-workera_username=$(yq e '.[] | select(.name == "worker-a-00") | .bmc_user' ${SHARED_DIR}/hosts.yaml)
-workera_pass=$(yq e '.[] | select(.name == "worker-a-00") | .bmc_pass' ${SHARED_DIR}/hosts.yaml)
-
 cat > ${DIR}/bmo.yaml <<EOF
 apiVersion: metal3.io/v1alpha1
 kind: Provisioning
@@ -61,29 +56,75 @@ spec:
 
 EOF
 
-cat > ${DIR}/workera-bmc.yaml <<EOF
+oc create -f ${DIR}/bmo.yaml
+echo "Waiting up to 60 seconds for the metal3 pods to get created..."
+for i in $(seq 1 6); do
+  sleep 30
+  if $(oc get pods -n openshift-machine-api --no-headers -l k8s-app=metal3 | \
+       awk '{print $3}' | uniq | tr -d "\n" | grep -q -w Running); then
+    break
+  else
+    [ "$i" -eq 6 ] && \
+    echo "Creating resources to enable Metal platform components failed" && exit 1
+  fi
+done
+
+oc create -f ${DIR}/master00-bmc.yaml
+echo "Waiting up to 5 minutes for existing host enrollment"
+for i in $(seq 1 6); do
+  if [ "$i" -lt 6 ]; then
+    sleep 60
+    oc get bmh -A --no-headers | grep -w "externally provisioned"
+    if ! $(oc get bmh -A --no-headers | grep -q -w "externally provisioned"); then
+      echo "Enrolling existing host in progress"
+    else
+      sleep 30
+      oc get bmh -A --no-headers -o jsonpath='{.items[0].status.online}'
+      if ! $(oc get bmh -A --no-headers -o jsonpath='{.items[0].status.online}'| grep -q -w true); then
+        echo "Enrolled existing host is not Online"
+        exit 1
+      else
+        break
+      fi
+    fi
+  else
+    echo "Timeout reached while waiting for existing host enrollment. Failing..."
+    exit 1
+  fi
+done
+
+#Adding new worker node
+for addhost in $(yq e -o=j -I=0 '.[] | select(.name|test("worker-a-"))' "${SHARED_DIR}/hosts.yaml"); do
+  # shellcheck disable=SC1090
+  . <(echo "$addhost" | yq e 'to_entries | .[] | (.key + "=\"" + .value + "\"")')
+  if [ ${#bmc_address} -eq 0 ] || [ ${#bmc_user} -eq 0 ] || [ ${#bmc_pass} -eq 0 ]; then
+    echo "Error while unmarshalling hosts entries"
+    exit 1
+  fi
+  echo "Creating resource file for ${name}"
+  cat > ${DIR}/${name}-bmc.yaml <<EOF
 ---
 apiVersion: v1
 kind: Secret
 metadata:
-  name: workera-bmc
+  name: "${name}-bmc"
   namespace: openshift-machine-api
 type: Opaque
 data:
-  username: "$workera_username"
-  password: "$worker_pass"
+  username: "$redfish_user"
+  password: "$redfish_password"
 ---
 apiVersion: metal3.io/v1alpha1
 kind: BareMetalHost
 metadata:
-  name: worker-a-00
+  name: "$name"
   namespace: openshift-machine-api
 spec:
   bmc:
-    address: "redfish-virtualmedia://$workera_addr/redfish/v1/Systems/1"
-    credentialsName: "workera-bmc"
+    address: "redfish-virtualmedia://$bmc_address/redfish/v1/Systems/1"
+    credentialsName: "${name}-bmc"
     disableCertificateVerification: True
-  bootMACAddress: "$workera_bootMacAddr"
+  bootMACAddress: "$provisioning_mac"
   bootMode: legacy
   customDeploy:
     method: install_coreos
@@ -94,50 +135,27 @@ spec:
 
 EOF
 
-
-oc create -f ${DIR}/bmo.yaml 
-echo "Waiting up to 60 seconds for the metal3 pods to get created..."
-sleep 60
-while IFS= read -r line; do
-  if [[ $line != *"Running"* ]]; then
-    echo "Creating resources to enable Metal platform components failed"
-    exit 1
-  fi
-done <<< "oc get pods -n openshift-machine-api --no-headers -l k8s-app=metal3"
-
-oc create -f ${DIR}/master00-bmc.yaml
-echo "Waiting up to 5 minutes for existing host enrollment"
-for _ in $(seq 1 5); do
-  sleep 60
-  oc get bmh -A --no-headers | grep -q -w "externally provisioned"
-  if ! $(oc get bmh -A --no-headers | grep -q -w "externally provisioned"); then
-    echo "Enrolling existing host in progress"
-  else
-    sleep 30
-    oc get bmh -A --no-headers -o jsonpath='{.items[0].status.online}'
-    if ! $(oc get bmh -A --no-headers -o jsonpath='{.items[0].status.online}'| grep -q -w true); then
-      echo "Enrolled existing host is not Online"
+  echo "Adding node ${name} ..."
+  oc create -f ${DIR}/${name}-bmc.yaml
+  echo "Waiting up to 5 minutes for enrolling new host"
+  for i in $(seq 1 6); do
+    if [ "$i" < 6 ]; then
+      sleep 60
+      oc get bmh -A --no-headers | grep ${name} | grep -w "provisioned"
+      if ! $(oc get bmh -A --no-headers | grep ${name} | grep -q -w "provisioned"); then
+        echo "Enrolling new host in progress"
+      else
+        sleep 30
+        oc get bmh -A --no-headers -o jsonpath='{.items[1].status.online}'
+        if ! $(oc get bmh -A --no-headers -o jsonpath='{.items[1].status.online}'| grep -q -w true); then
+          echo "Enrolled existing host is not Online"
+          exit 1
+        fi
+        break
+      fi
+    else
+      echo "Timeout reached while waiting for new host enrollment. Failing..."
       exit 1
     fi
-  fi
+  done
 done
-
-oc create -f ${DIR}/worker03-bmc.yaml
-echo "Waiting up to 5 minutes for enrolling new host"
-for _ in $(seq 1 5); do
-  sleep 60
-  oc get bmh -A --no-headers | grep worker-a-00 | grep -q -w "provisioned"
-  if ! $(oc get bmh -A --no-headers | grep worker03 | grep -q -w "provisioned"); then
-    echo "Enrolling new host in progress"
-  else
-    sleep 30
-    oc get bmh -A --no-headers -o jsonpath='{.items[1].status.online}'
-    if ! $(oc get bmh -A --no-headers -o jsonpath='{.items[1].status.online}'| grep -q -w true); then
-      echo "Enrolled existing host is not Online"
-      exit 1
-    fi
-    exit 0
-  fi
-done
-echo "Timeout reached while waiting for new host enrollment. Failing..."
-exit 1
