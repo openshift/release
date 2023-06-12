d9def1f76d8a1e61512ac48cdf8ffa20496a9b4f
psap-user <liqcui@redhat.com>
2023-06-12 09:43:11 +0000

    update code for loaded upgrade

---
+++ ci-operator/step-registry/openshift-qe/loaded-upgrade/openshift-qe-loaded-upgrade-commands.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+cat /etc/os-release
+oc config view
+oc projects
+pushd /tmp
+python3 --version
+python3 -m venv venv3
+source venv3/bin/activate
+pip3 --version
+pip3 install --upgrade pip
+pip3 install -U datetime pyyaml
+pip3 list
+#ES_PASSWORD=$(cat "/secret/password")
+#ES_USERNAME=$(cat "/secret/username")
+RELEASE_IMAGE_LATEST=${RELEASE_IMAGE_LATEST:=""}
+if [[ -z "$RELEASE_IMAGE_LATEST" ]]; then
+    echo "RELEASE_IMAGE_LATEST is an empty string, exiting"
+    exit 1
+fi
+CURRENT_VERSION=$(oc get clusterversion -ojsonpath={..desired.version})
+OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE=${OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE:=$RELEASE_IMAGE_LATEST}
+TARGET_RELEASES="$(oc adm release info "${OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE}" --output=json | jq -r '.metadata.version')"
+echo  "-------------------------------------------------------------------------------------------"
+echo  Loaded Upgrade from [ $CURRENT_VERSION ] to [ $TARGET_RELEASES ]
+echo  "-------------------------------------------------------------------------------------------"
+
+if [ -d ocp-qe-perfscale-ci ];then
+     rm -rf ocp-qe-perfscale-ci
+fi
+git clone -b upgrade https://github.com/liqcui/ocp-qe-perfscale-ci.git --depth=1
+cd ocp-qe-perfscale-ci/upgrade_scripts 
+ENABLE_FORCE=${ENABLE_FORCE:=true}
+SCALE=${SCALE:=false}
+MAX_UNAVAILABLE=${MAX_UNAVAILABLE:=3}
+EUS_UPGRADE=${EUS_UPGRADE:=false}
+EUS_CHANNEL=${EUS_CHANNEL:="fast"} #fast,eus,candidate,stable
+
+echo TARGET_RELEASES is $TARGET_RELEASES
+./upgrade.sh $TARGET_RELEASES -f $ENABLE_FORCE -s $SCALE -u $MAX_UNAVAILABLE -e $EUS_UPGRADE -c $EUS_CHANNEL
+
+#export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
