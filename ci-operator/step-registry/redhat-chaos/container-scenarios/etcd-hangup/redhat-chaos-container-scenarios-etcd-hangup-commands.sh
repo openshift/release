f07d2ac798d4f30f2fadc52e8a76bcdd5951dbcc
Paige Rubendall <prubenda@redhat.com>
2023-06-13 12:39:01 -0400

    adding aro files

--- ci-operator/step-registry/redhat-chaos/container-scenarios/etcd-hangup/redhat-chaos-container-scenarios-etcd-hangup-commands.sh
+++ ci-operator/step-registry/redhat-chaos/container-scenarios/etcd-hangup/redhat-chaos-container-scenarios-etcd-hangup-commands.sh
@@ -13,14 +13,20 @@ ls -la /root/kraken
 git clone https://github.com/redhat-chaos/krkn-hub.git
 pushd krkn-hub/
 
+<<<<<<< HEAD
 echo "kubeconfig loc $$KUBECONFIG"
 echo "Using the flattened version of kubeconfig"
 oc config view --flatten > /tmp/config
 export KUBECONFIG=/tmp/config
+=======
+echo "kubeconfig loc $KUBECONFIG"
+>>>>>>> adding aro files
 
 export KRKN_KUBE_CONFIG=$KUBECONFIG
 export NAMESPACE=$TARGET_NAMESPACE
 export ENABLE_ALERTS=False
+
+
 ./prow/container-scenarios/prow_run.sh
 rc=$?
 echo "Finished running container scenarios"
