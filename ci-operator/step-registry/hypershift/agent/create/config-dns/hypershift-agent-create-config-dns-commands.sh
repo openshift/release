ced46951eadef8ffddc6c7a4c45ede32d193b608
Liangquan Li <liangli@redhat.com>
2023-06-15 10:39:40 +0800

    HyperShift-agent: temp

--- ci-operator/step-registry/hypershift/agent/create/config-dns/hypershift-agent-create-config-dns-commands.sh
+++ ci-operator/step-registry/hypershift/agent/create/config-dns/hypershift-agent-create-config-dns-commands.sh
@@ -6,19 +6,4 @@ set -o pipefail
 
 echo "************ config-dns command ************"
 
-source "${SHARED_DIR}/packet-conf.sh"
 
-CLUSTER_NAME="$(echo -n $PROW_JOB_ID|sha256sum|cut -c-20)"
-echo "$CLUSTER_NAME" > /tmp/hostedcluster_name
-scp "${SSHOPTS[@]}" "/tmp/hostedcluster_name" "root@${IP}:/home/hostedcluster_name"
-
-# shellcheck disable=SC2087
-ssh "${SSHOPTS[@]}" "root@${IP}" bash - << EOF |& sed -e 's/.*auths.*/*** PULL_SECRET ***/g'
-MASTER_NUM=\$(oc get node -lnode-role.kubernetes.io/master="" --no-headers | wc -l)
-HOSTEDCLUSTER_NAME=\$(cat /home/hostedcluster_name)
-echo "address=/api.\$HOSTEDCLUSTER_NAME.ostest.test.metalkube.org/192.168.111.\$((21+\$MASTER_NUM))" >> /etc/NetworkManager/dnsmasq.d/openshift-ostest.conf
-echo "address=/api-int.\$HOSTEDCLUSTER_NAME.ostest.test.metalkube.org/192.168.111.\$((21+\$MASTER_NUM))" >> /etc/NetworkManager/dnsmasq.d/openshift-ostest.conf
-
-systemctl restart NetworkManager.service
-set -x
-EOF
