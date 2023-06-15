3a943368ad4ca081c1103ab7bdacee52705ae541
psap-user <liqcui@redhat.com>
2023-06-15 05:46:12 +0000

    update

--- ci-operator/step-registry/openshift-qe/cluster-density/openshift-qe-cluster-density-commands.sh
+++ ci-operator/step-registry/openshift-qe/cluster-density/openshift-qe-cluster-density-commands.sh
@@ -22,7 +22,7 @@ current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/work
 export JOB_ITERATIONS=$((9*$current_worker_count))
 export WORKLOAD=cluster-density
 export GEN_CSV=true
-export CLEANUP_WHEN_FINISH=true
+export CLEANUP_WHEN_FINISH=${CLEANUP_WHEN_FINISH:=true}
 
 export COMPARISON_CONFIG="clusterVersion.json podLatency.json etcd.json nodeMasters-max.json nodeAggWorkers.json"
 
---
+++ ci-operator/step-registry/openshift-qe/loaded-upgrade/openshift-qe-loaded-upgrade-commands.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+cat /etc/os-release
+oc config view
+oc projects
+pushd /tmp
+python3 --version
+python3 -m venv venv3
+source venv3/bin/activate
+pip3 --version
+pip3 install --upgrade pip
+pip3 install -U datetime pyyaml
+pip3 list
+
+RELEASE_IMAGE_LATEST=${RELEASE_IMAGE_LATEST:=""}
+CURRENT_VERSION=$(oc get clusterversion -ojsonpath={..desired.version})
+
+if [[ -z "$RELEASE_IMAGE_LATEST" ]]; then
+    echo "RELEASE_IMAGE_LATEST is an empty string, exiting"
+    exit 1
+fi
+
+if [[ -s "${SHARED_DIR}/perfscale-override-upgrade" ]]; then
+      ALL_IMAGES="$(< "${SHARED_DIR}/perfscale-override-upgrade")" &&
+      echo "Overriding upgrade target to ${ALL_IMAGES}"
+      for IMAGE in $ALL_IMAGES
+      do
+	      RELEASES_VERSION+=("`oc adm release info "${IMAGE}" --output=json | jq -r '.metadata.version'`")
+      done
+      #RELEASES_VERSION_STR=$("echo ${RELEASES_VERSION[@]}")
+      TARGET_RELEASES=$(echo "${RELEASES_VERSION[@]}"| tr -s ' ' ',')
+else
+      OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE=${OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE:=$RELEASE_IMAGE_LATEST}
+      TARGET_RELEASES="$(oc adm release info "${OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE}" --output=json | jq -r '.metadata.version')"
+fi
+echo  "-------------------------------------------------------------------------------------------"
+echo  Loaded Upgrade from [ $CURRENT_VERSION ] to [ $TARGET_RELEASES ]
+echo  "-------------------------------------------------------------------------------------------"
+
+if [ -d ocp-qe-perfscale-ci ];then
+     rm -rf ocp-qe-perfscale-ci
+fi
+git clone -b upgrade https://github.com/liqcui/ocp-qe-perfscale-ci.git --depth=1
+cd ocp-qe-perfscale-ci/upgrade_scripts 
+ENABLE_FORCE=${ENABLE_FORCE:=true}
+SCALE=${SCALE:=false}
+MAX_UNAVAILABLE=${MAX_UNAVAILABLE:=3}
+EUS_UPGRADE=${EUS_UPGRADE:=false}
+EUS_CHANNEL=${EUS_CHANNEL:="fast"} #fast,eus,candidate,stable
+echo TARGET_RELEASES is $TARGET_RELEASES
+./upgrade.sh $TARGET_RELEASES -f $ENABLE_FORCE -s $SCALE -u $MAX_UNAVAILABLE -e $EUS_UPGRADE -c $EUS_CHANNEL
