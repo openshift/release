f4dae168db1c2d095afd4b3d242e83bf9598eb74
rnetser <rnetser@redhat.com>
2023-06-15 10:28:38 +0300

    remove ROSA and update documentaiton

--- ci-operator/step-registry/addon/uninstall/addon-uninstall-commands.sh
+++ ci-operator/step-registry/addon/uninstall/addon-uninstall-commands.sh
@@ -7,24 +7,20 @@ set -o verbose
 
 CLUSTER_NAME=$(cat "${SHARED_DIR}/cluster-name")
 OCM_TOKEN=$(cat /var/run/secrets/ci.openshift.io/cluster-profile/ocm-token)
-RUN_COMMAND="poetry run python app/cli.py addons --cluster ${CLUSTER_NAME} --token ${OCM_TOKEN} --api-host ${API_HOST} --timeout ${TIMEOUT} "
+RUN_COMMAND="poetry run python app/cli.py addons --cluster ${CLUSTER_NAME} --token ${OCM_TOKEN} --api-host ${API_HOST} "
 
 ADDONS_CMD=""
-for i in {1..6}; do
-  ADDON_VALUE=$(eval "echo $"ADDON$i"_CONFIG")
-  if [[ -n $ADDON_VALUE ]]; then
-    ADDONS_CMD+=" --addon ${ADDON_VALUE} "
-  fi
+for addon_value in $(env | grep -E '^ADDON[0-9]+_CONFIG'); do
+    addon_value=$(echo "$addon_value" | sed -E  's/^ADDON[0-9]+_CONFIG=//')
+    if  [ "${addon_value}" ]; then
+      ADDONS_CMD+=" --addon ${addon_value} "
+    fi
 done
 
 RUN_COMMAND="${RUN_COMMAND} ${ADDONS_CMD}"
 
 if [ -n "${PARALLEL}" ]; then
     RUN_COMMAND+=" --parallel"
-fi
-
-if [ -n "${ROSA}" ]; then
-    RUN_COMMAND+=" --rosa"
 fi
 
 RUN_COMMAND+=" uninstall"
