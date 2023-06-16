77576f3640d25a7be2e38dd5230581b5975668e9
Michael Pruitt <mpruitt@redhat.com>
2023-06-16 16:22:19 -0500

    disable test-runner pod restarts use rsync to gather project workspace artifacts

--- ci-operator/step-registry/syndesisio/deploy-syndesis-qe/syndesisio-deploy-syndesis-qe-commands.sh
+++ ci-operator/step-registry/syndesisio/deploy-syndesis-qe/syndesisio-deploy-syndesis-qe-commands.sh
@@ -7,25 +7,36 @@ set -o pipefail
 URL=$(oc whoami --show-server)
 export URL
 
-ADMIN_PASSWORD=$(cat "$KUBEADMIN_PASSWORD_FILE")
+ADMIN_PASSWORD="admin"
 export ADMIN_PASSWORD
 
-export ADMIN_USERNAME="kubeadmin"
+export ADMIN_USERNAME="admin"
 
 export FUSE_ONLINE_NAMESPACE="fuse-online"
 
-oc login --insecure-skip-tls-verify=true -u "${ADMIN_USERNAME}" -p "${ADMIN_PASSWORD}" "$(oc whoami --show-server)"
+SOURCE_DIR="/home/seluser/syndesis-qe"
+export SOURCE_DIR
 
-oc create --as system:admin user kubeadmin
-oc create --as system:admin identity kube:admin
-oc create --as system:admin useridentitymapping kube:admin kubeadmin
-oc adm policy --as system:admin add-cluster-role-to-user cluster-admin kubeadmin
+DEST_DIR="/tmp/test-artifacts"
+export DEST_DIR
+
+mkdir -p $DEST_DIR/endpoints
+mkdir -p $DEST_DIR/rest-common
+mkdir -p $DEST_DIR/rest-tests
+mkdir -p $DEST_DIR/target
+mkdir -p $DEST_DIR/ui-common
+mkdir -p $DEST_DIR/ui-tests
+mkdir -p $DEST_DIR/utilities
+mkdir -p $DEST_DIR/validation
+
+oc login --insecure-skip-tls-verify=true -u "kubeadmin" -p "$(cat ${KUBEADMIN_PASSWORD_FILE})" "$(oc whoami --show-server)"
 
 oc new-project test-runner
 
 oc adm policy add-scc-to-user anyuid -z default
 
-htpasswd -c -B -b /tmp/users.htpasswd kubeadmin "$ADMIN_PASSWORD"
+htpasswd -c -B -b /tmp/users.htpasswd admin "$ADMIN_PASSWORD"
+htpasswd -B -b /tmp/users.htpasswd user "user"
 
 oc create secret generic htpass-secret --from-file=htpasswd=/tmp/users.htpasswd -n openshift-config
 
@@ -46,10 +57,54 @@ EOF
 
 oc apply -f /tmp/htpasswd-oa.yaml
 
-oc new-app --name test-runner --image="$FUSE_ONLINE_TEST_RUNNER" -e ADMIN_USERNAME=$ADMIN_USERNAME -e ADMIN_PASSWORD=$(cat $KUBEADMIN_PASSWORD_FILE) -e URL=$(oc whoami --show-server) -e NAMESPACE=$FUSE_ONLINE_NAMESPACE
+oc adm policy add-cluster-role-to-user cluster-admin admin --rolebinding-name=cluster-admin
 
-# sleep for testing
-sleep 1400
+count=0
+status=0
+while [ $count -lt 60 ]; do
+    oc login --insecure-skip-tls-verify=true -u "admin" -p "admin" "$(oc whoami --show-server)" || status=$? || :
+    ((count=count+1))
+    if [ $status -eq 0 ]; then
+        break
+    fi
+    echo "Waiting to log in"
+    sleep 5
+    status=0
+done
 
-# need to copy out the test run artifacts from /test-run-results
+oc login --insecure-skip-tls-verify=true -u "kubeadmin" -p "$(cat ${KUBEADMIN_PASSWORD_FILE})" "$(oc whoami --show-server)"
 
+oc run test-runner --image="$FUSE_ONLINE_TEST_RUNNER" --env="ADMIN_USERNAME=$ADMIN_USERNAME" --env="ADMIN_PASSWORD=$ADMIN_PASSWORD" --env="URL=$(oc whoami --show-server)" --env="NAMESPACE=$FUSE_ONLINE_NAMESPACE" --port=8080 --restart=Never -- sleep 300
+
+POD_NAME="test-runner"
+
+oc wait --for=condition=Ready pod/"$POD_NAME" --timeout=-1s
+
+while oc get pod "$POD_NAME" | grep -q Running; do
+  oc rsync --compress=true "$POD_NAME:$SOURCE_DIR/endpoints" "$DEST_DIR/endpoints" || :
+  oc rsync --compress=true "$POD_NAME:$SOURCE_DIR/rest-common" "$DEST_DIR/rest-common" || :
+  oc rsync --compress=true "$POD_NAME:$SOURCE_DIR/rest-tests" "$DEST_DIR/rest-tests" || :
+  oc rsync --compress=true "$POD_NAME:$SOURCE_DIR/target" "$DEST_DIR/target" || :
+  oc rsync --compress=true "$POD_NAME:$SOURCE_DIR/ui-common" "$DEST_DIR/ui-common" || :
+  oc rsync --compress=true "$POD_NAME:$SOURCE_DIR/ui-tests" "$DEST_DIR/ui-tests" || :
+  oc rsync --compress=true "$POD_NAME:$SOURCE_DIR/utilities" "$DEST_DIR/utilities" || :
+  oc rsync --compress=true "$POD_NAME:$SOURCE_DIR/validation" "$DEST_DIR/validation" || :
+  oc logs "$POD_NAME" &> $DEST_DIR/test-runner.log || :
+  sleep 5
+done
+
+mkdir -p $ARTIFACT_DIR/test-run-results
+
+#while read -r FILE; do mkdir -p "$ARTIFACT_DIR"/test-run-results/"$(dirname "$FILE")"; cp "$FILE" "$ARTIFACT_DIR"/test-run-results/"$(dirname "$FILE")"; done <<< "$(find "$DEST_DIR" -type f -name "*.log")"
+
+#while read -r DIR; do mkdir -p "$ARTIFACT_DIR"/test-run-results/"$DIR"; cp -r "$DIR"/* "$ARTIFACT_DIR"/test-run-results/"$DIR"; done <<< "$(find "$DEST_DIR" -maxdepth 2 -type d -wholename "*target/cucumber*")"
+
+#cp $DEST_DIR/test-runner.log $ARTIFACT_DIR/test-run-results/ || :
+
+cp -r $DEST_DIR $ARTIFACT_DIR/test-run-results/ || :
+
+if oc get pod "$POD_NAME" | grep -q Completed; then
+	exit 0
+else
+	exit 1
+fi 
