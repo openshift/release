415817176088f74fdc89f7fe8449b48323d00ef2
Misha Sugakov <misha@stackrox.com>
2023-06-20 18:54:27 +0200

    Self-review

--- ci-operator/config/stackrox/stackrox/summarize.py
+++ ci-operator/config/stackrox/stackrox/summarize.py
@@ -4,8 +4,10 @@
 # whereas having a self-sufficient script should be more user-friendly.
 
 """
-The script reads ci-operator yaml config files from the current directory and outputs an overview table for visual
-controlling the configuration between multiple files.
+The script reads ci-operator yaml config files from the current directory as well as corresponding job configs and
+outputs an overview table for visual controlling the configuration between multiple files.
+
+The script requires Python 3.7 or newer to run.
 """
 
 import html
@@ -46,6 +48,8 @@ class TestEntry:
 
 @dataclasses.dataclass
 class ConfigFile:
+    """Represents YAML file under `ci-operator/config/**` directory."""
+
     filename: str
     load_error: Exception
     raw_parsed_yaml: typing.Any
@@ -88,19 +92,24 @@ class JobEntry:
         return safe_get_attr(self.raw_entry, "skip_if_only_changed")
 
     def dump_all_interesting_attrs(self):
-        # https://docs.ci.openshift.org/docs/how-tos/contributing-openshift-release/#tolerated-changes-to-generated-jobs
+        """
+        Returns YAML dump of attributes that can be self-managed (for presubmits jobs).
+        See https://docs.ci.openshift.org/docs/how-tos/contributing-openshift-release/#tolerated-changes-to-generated-jobs
+        """
         if self.raw_entry is None:
             return ""
+
         subset = {
             key: self.raw_entry[key] for key in
-            ["always_run", "run_if_changed", "skip_if_only_changed", "skip_report", "max_concurrency"] if
-            key in self.raw_entry
+            ["always_run", "run_if_changed", "skip_if_only_changed", "skip_report", "max_concurrency"]
+            if key in self.raw_entry
         }
         return yaml.safe_dump(subset)
 
 
 @dataclasses.dataclass
 class JobsFile:
+    """Represents YAML file under `ci-operator/jobs/**` directory."""
     filename: str
     load_error: Exception
     raw_parsed_yaml: typing.Any
@@ -112,6 +121,8 @@ class JobsFile:
 
 @dataclasses.dataclass
 class Data:
+    """The entire bag of data this program operates with."""
+
     config_dir: pathlib.Path
 
     all_test_names: typing.List[str] = None
@@ -124,26 +135,24 @@ class Data:
 
     @property
     def target_repo(self):
-        """
-        Returns org/name for the repo of `config_dir`.
-        """
+        """Returns org/name for the repo of `config_dir`."""
         return "/".join(self.config_dir.resolve().parts[-2:])
 
     @property
     def jobs_dir(self):
-        """
-        Derives `jobs` dir from `config` dir.
-        """
+        """Derives `jobs` dir from `config` dir."""
         dir = self.config_dir.resolve()  # repo_root/ci-operator/config/stackrox/stackrox
-        dir = dir.parent.parent.parent  # Dir will be at repo_root/ci-operator
+        dir = dir.parent.parent.parent  # Dir is now at repo_root/ci-operator
         return dir / "jobs" / self.target_repo
 
     @property
     def config_dir_relative(self):
+        """Returns `ci-operator/config/<org>/<repo>`."""
         return self.__relative_dir(self.config_dir)
 
     @property
     def jobs_dir_relative(self):
+        """Returns `ci-operator/jobs/<org>/<repo>`."""
         return self.__relative_dir(self.jobs_dir)
 
     @staticmethod
@@ -157,7 +166,7 @@ def safe_get_attr(raw_data, attr_key):
     """
     Traverses `raw_data` data structure of nested maps and returns an attribute pointed by `attr_key`.
     I.e. this is some kind of poor-man's XPath.
-    `attr` can be dot-separated, in which case each part serves as a key for the corresponding level of nested maps.
+    `attr_key` can be dot-separated, in which case each part serves as a key for the corresponding level of nested maps.
     Returns None if `raw_data` is None or `attr_key` does not resolve to anything.
     """
     location = raw_data
@@ -177,14 +186,15 @@ def safe_get_attr(raw_data, attr_key):
 def load_and_massage_data(dir):
     d = Data(config_dir=dir.resolve())
 
-    d.configs = load_raw_yamls(d.config_dir, ConfigFile)
+    d.configs = load_raw_yamls(d.config_dir, constructor_fn=ConfigFile)
     assign_short_filenames(d.configs)
     d.configs = sort_by_short_names(d.configs)
     assign_unordered_tests(d.configs)
     d.all_test_names = extract_and_sort_unique_names(d.configs)
     assign_ordered_entries(d.all_test_names, d.configs)
 
-    d.jobs_files = load_raw_yamls(d.jobs_dir, JobsFile)
+    d.jobs_files = load_raw_yamls(d.jobs_dir, constructor_fn=JobsFile)
+    # Postsubmits and periodics have much less degree of customization, so we filter them out.
     d.jobs_files = filter_out_all_but_presubmits_jobs_files(d.jobs_files)
     assign_short_filenames(d.jobs_files)
     d.jobs_files = sort_by_short_names(d.jobs_files)
@@ -231,8 +241,8 @@ def sort_by_short_names(items):
 def assign_unordered_tests(configs):
     for c in configs:
         c.unordered_entries = []
-        for test in safe_get_attr(c.raw_parsed_yaml, "tests") or []:
-            te = TestEntry(raw_entry=test)
+        for raw_test in safe_get_attr(c.raw_parsed_yaml, "tests") or []:
+            te = TestEntry(raw_entry=raw_test)
             if te.name:
                 c.unordered_entries.append(te)
 
@@ -244,14 +254,20 @@ def assign_unordered_jobs(jobs_files):
         if len(presubmits_dict) == 0:
             continue
         if len(presubmits_dict) > 1:
-            j.load_error = "Too many items under presubmits"
+            j.load_error = "Unexpected to see too many items under presubmits"
             continue
-        j.unordered_entries = [JobEntry(raw_entry=e) for e in list(presubmits_dict.values())[0]]
+        # There's only one attribute under `presubmits` in YAML and its name is key is repo name.
+        # The following line gets value of that attribute.
+        jobs_list = list(presubmits_dict.values())[0]
+        for raw_job in jobs_list:
+            je = JobEntry(raw_entry=raw_job)
+            if je.name:
+                j.unordered_entries.append(je)
 
 
 def extract_and_sort_unique_names(items):
     """
-    Collects name for each entry returned by entriesFn for each item in items.
+    Collects and orders unique names of each entry in unordered_entries of each item.
     Works both with ConfigFile/TestEntry and JobsFile/JobEntry.
     """
 
@@ -265,12 +281,12 @@ def extract_and_sort_unique_names(items):
     all_names_set = set()
     for x in items:
         for y in x.unordered_entries:
-            if y.name:
-                all_names_set.add(y.name)
+            all_names_set.add(y.name)
     return sorted(all_names_set, key=stackrox_sort_key)
 
 
 def assign_ordered_entries(all_names, items):
+    """Makes each item's ordered_entries to match the size and positions of elements in `all_names`."""
     for x in items:
         result = [None] * len(all_names)
         for y in x.unordered_entries:
@@ -313,7 +329,7 @@ def render_summary_tables_impl(buffer, data):
         buffer.write(txt)
 
     def cell(tag, content, css_class=None, html_escape=True):
-        return f"<{tag} class=\"{css_class}\"><div>{html.escape(content) if html_escape else content}</div></{tag}>"
+        return f"""<{tag} class="{css_class}"><div>{html.escape(content) if html_escape else content}</div></{tag}>"""
 
     def th(content, css_class=None):
         return cell("th", content, css_class=css_class)
@@ -322,7 +338,7 @@ def render_summary_tables_impl(buffer, data):
         return cell("td", content, css_class=css_class, html_escape=html_escape)
 
     def write_tr(items, css_class=None):
-        emit(f"<tr class=\"{css_class}\">")
+        emit(f"""<tr class="{css_class}">""")
         for i in items:
             emit(i)
         emit("</tr>\n")
@@ -412,7 +428,7 @@ def render_load_error(error):
     if error is None:
         return """<i class="bi bi-check" title="no error"></i>"""
     btn_text = """<i class="bi bi-bug-fill" title="YAML load error"></i>"""
-    return render_collapsible(btn_text, html.escape(error.__str__()))
+    return render_collapsible(btn_text, html.escape(str(error)))
 
 
 def render_test_entry(entry):
@@ -430,7 +446,7 @@ def render_test_entry(entry):
         btn_text += """<i class="bi bi-funnel-fill" title="Run if changed"></i>"""
     if btn_text == "":
         btn_text = """<i class="bi bi-hand-thumbs-up" title="no interesting flags"></i>"""
-    return render_collapsible(btn_text, html.escape(yaml_dump))
+    return render_collapsible(btn_text, yaml_dump)
 
 
 def render_job_entry(entry):
@@ -444,7 +460,7 @@ def render_job_entry(entry):
     if entry.run_if_changed:
         btn_text += f"""
             <br><span title="run_if_changed: {entry.run_if_changed}">ric:
-                <i class="bi bi-patch-exclamation-fill"></i>
+                <i class="bi bi-funnel-fill"></i>
             </span>
         """
     if entry.skip_if_only_changed:
@@ -454,11 +470,12 @@ def render_job_entry(entry):
             </span>
         """
 
-    return render_collapsible(btn_text, html.escape(entry.dump_all_interesting_attrs()))
+    return render_collapsible(btn_text, entry.dump_all_interesting_attrs())
 
 
 def render_collapsible(button_text, contents):
-    # This ensures each collapsible has a unique id.
+    # This ensures each collapsible has a unique id because ids in HTML must be unique and besides that's how the button
+    # knows what to expand and collapse.
     render_collapsible.next_id = (render_collapsible.next_id if hasattr(render_collapsible, "next_id") else 0) + 1
     id = f"collapse-id-{render_collapsible.next_id}"
 
@@ -469,10 +486,10 @@ def render_collapsible(button_text, contents):
             data-bs-toggle="collapse"
             data-bs-target="#{id}"
             aria-expanded="false" aria-controls="{id}"
-            title="Click to toggle showing contents"
+            title="Click to toggle contents display"
             >{button_text}</button><br>
         <div id="{id}" class="collapse">
-            <pre>{contents}</pre>
+            <pre>{html.escape(contents)}</pre>
         </div>
     """
 
@@ -487,13 +504,14 @@ page = """
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
+    <!-- Meta tags required for Bootstrap -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     
+    <!-- This page custom styles -->
     <style>
         .disable-wrap { white-space: nowrap; }
         .natural-vertical-alignment {
@@ -502,13 +520,14 @@ page = """
             min-width: 3em;
         }
         .vertical-text div {
+            /* Credits to https://stackoverflow.com/a/47245068/484050 */
             -ms-writing-mode: tb-rl;
             -webkit-writing-mode: vertical-rl;
             writing-mode: vertical-rl;
             transform: rotate(180deg);
         }
         .sticky-header thead th {
-            /* credits to https://stackoverflow.com/a/49510703/484050 */
+            /* Credits to https://stackoverflow.com/a/49510703/484050 */
             position: sticky;
             top: 0;
             z-index: 10;
@@ -516,7 +535,7 @@ page = """
             border-width: 1;
         }
         .sticky-header thead th {
-            /* credits to https://stackoverflow.com/a/52256954/484050 */
+            /* Credits to https://stackoverflow.com/a/52256954/484050 */
             box-shadow: inset 0 1px 0 rgb(222, 226, 230), inset 0 -1px 0 rgb(222, 226, 230);
             background-clip: padding-box;
         }
@@ -524,8 +543,8 @@ page = """
             text-align: right;
         }
         .collapsing {
-            /* Set animation duration to zero otherwise collapsing collapsible is inconvenient. */
-            /* credits to https://github.com/twbs/bootstrap/issues/31554#issuecomment-686726639 */
+            /* Set animation duration to zero otherwise collapsing collapsible is slow. */
+            /* Credits to https://github.com/twbs/bootstrap/issues/31554#issuecomment-686726639 */
             transition-duration: 0s;
         }
         .header-cell {
@@ -543,14 +562,15 @@ page = """
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
     
+    <!-- This page custom script -->
     <script>
         function toggleVertical(elt) {
             $(elt).toggleClass("vertical-text");
             let icon = $(elt).find("div button i");
             if ($(elt).hasClass("vertical-text")) {
-                icon.attr("class", "bi bi-arrows-angle-contract");
-            } else {
                 icon.attr("class", "bi bi-arrows-angle-expand");
+            } else {
+                icon.attr("class", "bi bi-arrows-angle-contract");
             }
         }
     
@@ -566,10 +586,6 @@ page = """
   </body>
 </html>
 """
-
-
-def render_job_summary_table(all_job_names, jobs_files):
-    pass
 
 
 def main():
