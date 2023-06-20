d9d15ad4a447db577bb500bd4798705575a01c31
E Hila <ehila@redhat.com>
2023-06-21 00:30:04 +0200

    fix: removing workload partitioning config (#40497)
    Signed-off-by: ehila <ehila@redhat.com>

--- ci-operator/step-registry/single-node/conf/e2e/single-node-conf-e2e-commands.sh
+++ ci-operator/step-registry/single-node/conf/e2e/single-node-conf-e2e-commands.sh
@@ -19,37 +19,3 @@ spec:
     systemReserved:
       memory: 3Gi
 EOF
-
-# Use first eight cores on a single node for workload partitioning (see https://github.com/openshift/enhancements/blob/master/enhancements/workload-partitioning/management-workload-partitioning.md#goals)
-# ... but avoid applying workload partitioning if OCP version < 4.10 (or if OCP_VERSION is not set)
-REQUIRED_OCP_VERSION="4.10"
-if [ "$(printf '%s\n' "${REQUIRED_OCP_VERSION}" "${OCP_VERSION}" | sort --version-sort | head -n1)" = "${REQUIRED_OCP_VERSION}" ]; then 
-  cat >"${SHARED_DIR}/manifest_single-node-workload-partitioning.yml" << EOF
-apiVersion: machineconfiguration.openshift.io/v1
-kind: MachineConfig
-metadata:
-  labels:
-    machineconfiguration.openshift.io/role: master
-  name: 02-master-workload-partitioning
-spec:
-  config:
-    ignition:
-      version: 3.2.0
-    storage:
-      files:
-      - contents:
-          source: data:text/plain;charset=utf-8;base64,W2NyaW8ucnVudGltZS53b3JrbG9hZHMubWFuYWdlbWVudF0KYWN0aXZhdGlvbl9hbm5vdGF0aW9uID0gInRhcmdldC53b3JrbG9hZC5vcGVuc2hpZnQuaW8vbWFuYWdlbWVudCIKYW5ub3RhdGlvbl9wcmVmaXggPSAicmVzb3VyY2VzLndvcmtsb2FkLm9wZW5zaGlmdC5pbyIKcmVzb3VyY2VzID0geyAiY3B1c2hhcmVzIiA9IDAsICJjcHVzZXQiID0gIjAtNyIgfQ==
-        mode: 420
-        overwrite: true
-        path: /etc/crio/crio.conf.d/01-workload-partitioning
-        user:
-          name: root
-      - contents:
-          source: data:text/plain;charset=utf-8;base64,ewogICJtYW5hZ2VtZW50IjogewogICAgImNwdXNldCI6ICIwLTciCiAgfQp9
-        mode: 420
-        overwrite: true
-        path: /etc/kubernetes/openshift-workload-pinning
-        user:
-          name: root
-EOF
-fi
