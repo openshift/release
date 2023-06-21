eee17d6dfc6f4b3d14d9671c7a4f8ea67324992b
Simranpal <sgill@redhat.com>
2023-06-21 11:21:07 -0700

    Update copy script

--- ci-operator/step-registry/quarkus/collect-results/quarkus-collect-results-commands.sh
+++ ci-operator/step-registry/quarkus/collect-results/quarkus-collect-results-commands.sh
@@ -4,8 +4,11 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+shopt -s nullglob
+
 echo "Copying results and xmls to ${ARTIFACT_DIR}"
-for PROJECT in ${PROJECTS//","/" "}; do
+ALL_PROJECTS=${PROJECTS:?Can not retrieve the list of projects}
+for PROJECT in ${ALL_PROJECTS//","/" "}; do
   for FILE in ./$PROJECT/target/failsafe-reports/*.xml; do
     FILENAME=$(basename $FILE)
     echo $FILENAME
