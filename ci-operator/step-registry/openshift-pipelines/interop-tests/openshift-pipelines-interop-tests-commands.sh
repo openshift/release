411e1169748aa81b36ee0663f062675b6f641307
chetna14manku <rchetna101@gmail.com>
2023-06-21 18:36:16 +0530

    login with oc command

--- ci-operator/step-registry/openshift-pipelines/interop-tests/openshift-pipelines-interop-tests-commands.sh
+++ ci-operator/step-registry/openshift-pipelines/interop-tests/openshift-pipelines-interop-tests-commands.sh
@@ -4,13 +4,19 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-export KUBECONFIG=${SHARED_DIR}/kubeconfig
-
+CONSOLE_URL=$(cat $SHARED_DIR/console.url)
+API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
 XML_PATH="/tmp/release-tests/reports/xml-report/result.xml"
 
-#echo "Running olm.spec to install operator..."
-#CATALOG_SOURCE=custom-operators CHANNEL=latest gauge run --log-level=debug --verbose --tags install specs/olm.spec
-#cp $XML_PATH ${ARTIFACT_DIR}/olm_specs.xml
+touch ${PWD}/config
+export KUBECONFIG=${PWD}/config
+
+echo "Login to the cluster as kubeadmin..."
+oc login -u kubeadmin -p "$(cat $SHARED_DIR/kubeadmin-password)" "${API_URL}" --insecure-skip-tls-verify=true
+
+echo "Running olm.spec to install operator..."
+CATALOG_SOURCE=redhat-operators CHANNEL=latest gauge run --log-level=debug --verbose --tags install specs/olm.spec
+cp $XML_PATH ${ARTIFACT_DIR}/junit_olm_specs.xml
 
 echo "Running gauge specs parallely..."
 gauge run --log-level=debug --verbose --tags sanity -p specs/clustertasks/ specs/pipelines/ specs/triggers/ specs/hub/ specs/metrics/ specs/pac/ specs/operator/addon.spec specs/operator/post-upgrade.spec specs/operator/pre-upgrade.spec
