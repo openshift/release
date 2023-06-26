8a15a943a9d25af4b5b27440991164954646d38b
chetna14manku <rchetna101@gmail.com>
2023-06-27 00:01:05 +0530

    update branch

--- ci-operator/step-registry/openshift-pipelines/interop-tests/openshift-pipelines-interop-tests-commands.sh
+++ ci-operator/step-registry/openshift-pipelines/interop-tests/openshift-pipelines-interop-tests-commands.sh
@@ -6,26 +6,33 @@ set -o pipefail
 
 CONSOLE_URL=$(cat $SHARED_DIR/console.url)
 API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
-XML_PATH="/tmp/release-tests/reports/xml-report/result.xml"
+export gauge_reports_dir=${ARTIFACT_DIR}
+export overwrite_reports=false
+export KUBECONFIG=$SHARED_DIR/kubeconfig
 
-touch ${PWD}/config
-export KUBECONFIG=${PWD}/config
+# Add timeout to ignore runner connection error
+gauge config runner_connection_timeout 600000 && gauge config runner_request_timeout 300000
 
-echo "Login to the cluster as kubeadmin..."
+echo "Login to the cluster as kubeadmin"
 oc login -u kubeadmin -p "$(cat $SHARED_DIR/kubeadmin-password)" "${API_URL}" --insecure-skip-tls-verify=true
 
-echo "Running olm.spec to install operator..."
-CATALOG_SOURCE=redhat-operators CHANNEL=latest gauge run --log-level=debug --verbose --tags install specs/olm.spec
-cp $XML_PATH ${ARTIFACT_DIR}/junit_olm_specs.xml
+echo "Running olm.spec to install operator"
+CATALOG_SOURCE=redhat-operators CHANNEL=${OLM_CHANNEL} gauge run --log-level=debug --verbose --tags install specs/olm.spec
 
-echo "Running gauge specs parallely..."
-gauge run --log-level=debug --verbose --tags sanity -p specs/clustertasks/ specs/pipelines/ specs/triggers/ specs/hub/ specs/metrics/ specs/pac/ specs/operator/addon.spec specs/operator/post-upgrade.spec specs/operator/pre-upgrade.spec
-cp $XML_PATH ${ARTIFACT_DIR}/junit_parallel_specs.xml
-
-echo "Running auto-prune spec..."
-gauge run --log-level=debug --verbose --tags sanity specs/operator/auto-prune.spec
-cp $XML_PATH ${ARTIFACT_DIR}/junit_auto_prune.xml
-
-echo "Running rbac spec..."
+echo "Running gauge specs"
+gauge run --log-level=debug --verbose --tags 'sanity & !tls' specs/clustertasks/clustertask-s2i.spec
+gauge run --log-level=debug --verbose --tags 'sanity & !tls'  specs/clustertasks/clustertask.spec
+gauge run --log-level=debug --verbose --tags 'sanity & !tls'  specs/pipelines/
+gauge run --log-level=debug --verbose --tags 'sanity & !tls'  specs/triggers/ || true
+gauge run --log-level=debug --verbose --tags 'sanity & !tls'  specs/metrics/
+gauge run --log-level=debug --verbose --tags 'sanity & !tls' -p specs/operator/addon.spec specs/operator/auto-prune.spec
 gauge run --log-level=debug --verbose --tags sanity specs/operator/rbac.spec
-cp $XML_PATH ${ARTIFACT_DIR}/junit_rbac.xml
+
+echo "Rename xml files to junit_test_*.xml"
+readarray -t path <<< "$(find ${ARTIFACT_DIR}/xml-report/ -name '*.xml')"
+for index in "${!path[@]}"; do
+  mv "${path[index]}" ${ARTIFACT_DIR}/junit_test_result$[index+1].xml
+done
+
+echo "sleep"
+sleep 2h
