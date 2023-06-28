d0541b0eaec653fdce85dd60ec6dd03035f4e081
ShudiLi <shudili@redhat.com>
2023-06-28 19:12:02 +0800

    Updated according to the review

--- ci-operator/step-registry/openshift/oc/certrotation/openshift-oc-certrotation-commands.sh
+++ ci-operator/step-registry/openshift/oc/certrotation/openshift-oc-certrotation-commands.sh
@@ -16,31 +16,48 @@ cusIngressKey="wilcard.key"
 cusIngressCsr="wildcard.csr"
 cusIngressCert="wildcard.pem"
 keyLength=4096
-baseDomin=$(oc get dns.config cluster -o=jsonpath=\"{.spec.baseDomain}\")
-defaultIngressDomin=$(oc get ingresscontroller default  -o=jsonpath=\"{.status.domain}\" -n openshift-ingress-operator)
-path=$(pwd)
+baseDomin=$(oc get dns.config cluster -o=jsonpath='{.spec.baseDomain}')
+defaultIngressDomin=$(oc get ingresscontroller default  -o=jsonpath='{.status.domain}' -n openshift-ingress-operator)
+currentPath=$(pwd)
 workPath="/tmp/replcertforingress"
 sudo mkdir $workPath; sudo chmod 664 $workPath; cd $workPath
-sudo echo "[cus]" >> tem.conf
-sudo echo "subjectAltName = DNS:*.${defaultIngressDomin}" >> tem.conf
+echo "[cus]" >> tem.conf
+echo "subjectAltName = DNS:*.$defaultIngressDomin" >> tem.conf
 
 ## create root ca for cluster proxy and certification for default ingress controller
-sudo openssl req -newkey rsa:$keyLength -nodes -sha256 -keyout $proxyKey -x509 -days 2 -subj "/CN=${baseDomin}" -out $proxyCert
-sudo openssl genrsa -out $cusIngressKey $keyLength
-sudo openssl req -new -key $cusIngressKey  -subj "/CN=${defaultIngressDomin}" -addext "subjectAltName = DNS:*.${defaultIngressDomin}" -out  $cusIngressCsr
-sudo openssl x509 -req -days 2 -CA $proxyCert -CAkey $proxyKey -CAserial caproxy.srl -CAcreateserial -extfile  tem.conf -extensions cus -in $cusIngressCsr -out $cusIngressCert
+openssl req -newkey rsa:$keyLength -nodes -sha256 -keyout $proxyKey -x509 -days 30 -subj "/CN=$baseDomin" -out $proxyCert
+openssl genrsa -out $cusIngressKey $keyLength
+openssl req -new -key $cusIngressKey  -subj "/CN=$defaultIngressDomin" -addext "subjectAltName = DNS:*.$defaultIngressDomin" -out  $cusIngressCsr
+openssl x509 -req -days 30 -CA $proxyCert -CAkey $proxyKey -CAserial caproxy.srl -CAcreateserial -extfile  tem.conf -extensions cus -in $cusIngressCsr -out $cusIngressCert
 
 ## create a config map and update the cluster-wide proxy configuration with the newly created config map
 oc create configmap custom-ca --from-file=ca-bundle.crt=$workPath/$proxyCert -n openshift-config
 oc patch proxy/cluster --type=merge --patch='{"spec":{"trustedCA":{"name":"custom-ca"}}}'
 
 ## create secret and update the Ingress Controller configuration with the newly created secret
-oc create secret tls custom-secret --cert=${workPath}/${cusIngressCert} --key=${workPath}/${cusIngressKey} -n openshift-ingress
+oc create secret tls custom-secret --cert=$workPath/$cusIngressCert --key=$workPath/$cusIngressKey -n openshift-ingress
 oc patch ingresscontroller.operator default --type=merge -p '{"spec":{"defaultCertificate": {"name": "custom-secret"}}}' -n openshift-ingress-operator
 
 ## Delete the router-ca secret.
 oc -n openshift-ingress-operator delete secrets/router-ca
-oc adm wait-for-stable-cluster
 
 ## remove workPath folder
-cd $path; sudo rm -rf $workPath
+cd $currentPath; sudo rm -rf $workPath
+
+## restart the Ingress Operator
+oldPod=$(oc -n openshift-ingress-operator get pods  -l name=ingress-operator -o=jsonpath='{.items[0].metadata.name}')
+oldRouterCaTime=$(oc -n openshift-ingress-operator get secrets/router-ca -o=jsonpath='{.metadata.creationTimestamp}')
+oc -n openshift-ingress-operator delete pods -l name=ingress-operator
+oc adm wait-for-stable-cluster
+
+## check if new ingress-operator pod and new secret router-ca are created or not
+newPod=$(oc -n openshift-ingress-operator get pods  -l name=ingress-operator -o=jsonpath='{.items[0].metadata.name}')
+newRouterCaTime=$(oc -n openshift-ingress-operator get secrets/router-ca -o=jsonpath='{.metadata.creationTimestamp}')
+if [ -z "$newPod" ] || [ X"$newPod" == X"$oldPod" ]
+then
+    echo "new ingress controller pod is not created or old ingress controller pod is not deleted" && exit 1
+fi
+if [ -z "$newRouterCaTime" ] || [ X"$newRouterCaTime" == X"$oldRouterCaTime" ]
+then
+    echo "new secret router-ca is not created or old secret router-ca is not deleted" && exit 1
+fi
