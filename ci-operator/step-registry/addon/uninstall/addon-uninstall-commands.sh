209ffc666e152673690db330e5c766a5e4eeab10
Ori Haran <oharan@redhat.com>
2023-07-02 09:31:25 +0300

    Make update

--- ci-operator/step-registry/addon/uninstall/addon-uninstall-commands.sh
+++ ci-operator/step-registry/addon/uninstall/addon-uninstall-commands.sh
@@ -18,6 +18,7 @@ for addon_value in $(env | grep -E '^ADDON[0-9]+_CONFIG' | sort  --version-sort)
       ADDONS_CMD+=" --addon ${addon_value} "
     fi
 done
+
 RUN_COMMAND="${RUN_COMMAND} ${ADDONS_CMD}"
 
 if [ -n "${PARALLEL}" ]; then
