a8853b862ff1dc0a60455f5b3338abb27982313d
Paige Rubendall <prubenda@redhat.com>
2023-07-05 11:57:34 -0400

    observer

---
+++ ci-operator/step-registry/redhat-chaos/cerberus/redhat-chaos-cerberus-commands.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+ls /usr/bin
+pwd 
+ls -la
+
+whoami
+
+ls -la /root
+
+cd /root/cerberus
+
+git branch -vv
+git log --shortstat -n 3
+cat start_cerberus.py
+
+while [ ! -f "${KUBECONFIG}" ]; do
+  printf "%s: waiting for %s\n" "$(date --utc --iso=s)" "${KUBECONFIG}"
+  sleep 10
+done
+printf "%s: acquired %s\n" "$(date --utc --iso=s)" "${KUBECONFIG}"
+
+
+echo "kubeconfig loc $KUBECONFIG"
+
+export CERBERUS_KUBECONFIG=$KUBECONFIG
+export CERBERUS_WATCH_NAMESPACES="[^.*$]"
+export CERBERUS_IGNORE_PODS="[^installer*,^kube-burner*,^redhat-operators*,^certified-operators*,^collect-profiles*]"
+
+
+
+./cerberus/prow_run.sh
+rc=$?
+echo "Finished running cerberus scenarios"
+echo "Return code: $rc"
