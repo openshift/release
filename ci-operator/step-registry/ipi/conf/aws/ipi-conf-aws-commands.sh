454bddcb695690640070d877f2b11da9f72f5827
Marco Braga <mrbraga@redhat.com>
2023-07-05 22:35:31 -0300

    fix/splat-1034: move opt-in as a prerequisite to phase 2

--- ci-operator/step-registry/ipi/conf/aws/ipi-conf-aws-commands.sh
+++ ci-operator/step-registry/ipi/conf/aws/ipi-conf-aws-commands.sh
@@ -315,7 +315,7 @@ EOF
 fi
 
 if [[ -n "${AWS_EDGE_POOL_ENABLED-}" ]]; then
-  local_zone=$(aws --region "${aws_source_region}" ec2 describe-availability-zones --all-availability-zones --filter Name=state,Values=available Name=zone-type,Values=local-zone | jq -r '.AvailabilityZones[].ZoneName' | shuf | tail -n 1)
+  local_zone=$(< "${SHARED_DIR}"/local-zone-name.txt)
   local_zones_str="[ $local_zone ]"
   patch_edge="${SHARED_DIR}/install-config-edge.yaml.patch"
   cat > "${patch_edge}" << EOF
