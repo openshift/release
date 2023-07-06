a5b6d014f84ef100a8a483cf918f3c821bec7781
Ori Haran <oharan@redhat.com>
2023-07-06 10:14:43 +0300

    Update operator-aws-ipi and operator-aws-osd, put rosa under aws

--- ci-operator/step-registry/addons-and-operators/aws/osd/addons-and-operators-aws-osd-workflow.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o nounset
-set -o errexit
-set -o pipefail
-set -o verbose
-
-OCM_TOKEN="/var/run/secrets/ci.openshift.io/cluster-profile/ocm-token"
-cp $OCM_TOKEN "${CLUSTER_PROFILE_DIR}/ocm_token}"
