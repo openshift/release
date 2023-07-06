ebfdda67b5da72aff97161b01fcf5b22c814d9eb
liornoy <lnoy@redhat.com>
2023-07-06 12:04:52 +0300

    WIP: donotmerger. test metallb
    Signed-off-by: liornoy <lnoy@redhat.com>

--- ci-operator/step-registry/baremetalds/metallb-e2e/test/baremetalds-metallb-e2e-test-commands.sh
+++ ci-operator/step-registry/baremetalds/metallb-e2e/test/baremetalds-metallb-e2e-test-commands.sh
@@ -9,26 +9,29 @@ echo "************ baremetalds metallb e2e test command ************"
 # shellcheck source=/dev/null
 source "${SHARED_DIR}/packet-conf.sh"
 
+# Extract and format the oc version to branch
+extract_version() {
+  local output="$1"
+  local version_line=$(echo "$output" | grep "$2")
+  local version=$(echo "$version_line" | awk '{print $3}')
+  local major=$(echo "$version" | cut -d '.' -f 1)
+  local minor=$(echo "$version" | cut -d '.' -f 2)
+  local oc_branch="release-$major.$minor"
+  echo "$oc_branch"
+}
+
+OC_BRANCH=$(extract_version "$(oc version)" "Server Version:")
 METALLB_SRC_DIR="/go/src/github.com/openshift/metallb"
 METALLB_OPERATOR_SRC_DIR="/go/src/github.com/openshift/metallb-operator"
 METALLB_REPO=${METALLB_REPO:-"https://github.com/openshift/metallb.git"}
-METALLB_BRANCH=${METALLB_BRANCH:-"main"}
+METALLB_BRANCH="${OC_BRANCH}"
+METALLB_OPEATOR_BRANCH="${OC_BRANCH}"
 DONT_DEPLOY_OPERATOR=${DONT_DEPLOY_OPERATOR:-}
 
 if [ -d "${METALLB_SRC_DIR}" ]; then
   echo "### Copying metallb directory"
   scp "${SSHOPTS[@]}" -r "${METALLB_SRC_DIR}" "root@${IP}:/root/dev-scripts/"
 else
-  if [[ -n "${E2E_TESTS_CONFIG:-}" ]]; then
-    readarray -t config <<< "${E2E_TESTS_CONFIG}"
-    for var in "${config[@]}"; do
-      if [[ ! -z "${var}" ]]; then
-        if [[ "${var}" == *"METALLB_BRANCH"* ]]; then
-          METALLB_BRANCH="$(echo "${var}" | cut -d'=' -f2)"
-        fi
-      fi
-    done
-  fi
   echo "### Cloning metallb"
   ssh "${SSHOPTS[@]}" "root@${IP}" "cd /root/dev-scripts/ && rm -rf metallb/ && git clone ${METALLB_REPO} && cd metallb/ && git checkout ${METALLB_BRANCH}"
 fi
