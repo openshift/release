e892e353a1bdbf10028f99e359cd38b942124531
Sharada Vetsa <svetsa@redhat.com>
2023-07-06 10:25:36 -0400

    Perfscale rosa tests (#40540)
    * Add more Rosa and HCP coverage with scaling and also add coverage for new workloads
    Increase timeout to 30 min until we figure out ideal time to wait.
    
    * Update per feedback and more...
    Remove periodic runs for 3 worker nodes, adjust cron to distribute for all days and different timings to overcome quota issues, if any.
    Rename workloads to be more meaningful.
    
    * Add slack reporting for new periodic and relevant presubmit jobs
    
    * Update network tests to single zone only and 2 replicas only
    
    * Remove network tests until QE and Dev discuss and finalize single zone vs multi zone coverage.

--- ci-operator/step-registry/openshift-qe/node-density-heavy/openshift-qe-node-density-heavy-commands.sh
+++ ci-operator/step-registry/openshift-qe/node-density-heavy/openshift-qe-node-density-heavy-commands.sh
@@ -18,9 +18,6 @@ git clone https://github.com/cloud-bulldozer/e2e-benchmarking
 pushd e2e-benchmarking/workloads/kube-burner
 export WORKLOAD=node-density-heavy
 
-current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker | grep -v "NotReady\\|SchedulingDisabled" | wc -l | xargs)
-export NODE_COUNT=$(($current_worker_count))
-
 export CLEANUP_WHEN_FINISH=true
 
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
