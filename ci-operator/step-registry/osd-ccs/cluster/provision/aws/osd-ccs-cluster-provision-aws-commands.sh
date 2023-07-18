4ff32772699197e3c414e44c2cce1c74c66e2777
Omri Bar-Haim <obarhaim@redhat.com>
2023-07-18 11:36:17 +0300

    debugging awscred visibility issue

--- ci-operator/step-registry/osd-ccs/cluster/provision/aws/osd-ccs-cluster-provision-aws-commands.sh
+++ ci-operator/step-registry/osd-ccs/cluster/provision/aws/osd-ccs-cluster-provision-aws-commands.sh
@@ -27,13 +27,15 @@ COMPUTE_NODES=${COMPUTE_NODES:-$default_compute_nodes}
 CLOUD_PROVIDER_REGION=${LEASED_RESOURCE}
 AWSCRED="${CLUSTER_PROFILE_DIR}/.awscred"
 if [[ -f "${AWSCRED}" ]]; then
-  AWS_ACCOUNT_ID=$(cat "${AWSCRED}" | grep aws_account_id | tr -d ' ' | cut -d '=' -f 2)
+  # AWS_ACCOUNT_ID=$(cat "${AWSCRED}" | grep aws_account_id | tr -d ' ' | cut -d '=' -f 2)
   AWS_ACCESS_KEY_ID=$(cat "${AWSCRED}" | grep aws_access_key_id | tr -d ' ' | cut -d '=' -f 2)
   AWS_SECRET_ACCESS_KEY=$(cat "${AWSCRED}" | grep aws_secret_access_key | tr -d ' ' | cut -d '=' -f 2)
 else
   echo "Did not find compatible cloud provider cluster_profile"
   exit 1
 fi
+
+sleep 1h
 
 # Log in
 OCM_VERSION=$(ocm version)
