1128874928eac073abbdc0b5dc276a3e2798ddeb
Andrew Collins <ancollin@redhat.com>
2023-07-18 12:13:58 -0500

    Calculate iterations only on workers. (#41112)
    We exclude infra and workload nodes from current_worker_count when calculating iterations.
    
    Updating metadata.jsons

--- ci-operator/step-registry/openshift-qe/cluster-density-v2/openshift-qe-cluster-density-v2-commands.sh
+++ ci-operator/step-registry/openshift-qe/cluster-density-v2/openshift-qe-cluster-density-v2-commands.sh
@@ -18,7 +18,7 @@ ES_USERNAME=$(cat "/secret/username")
 git clone https://github.com/cloud-bulldozer/e2e-benchmarking --depth=1
 pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
 
-current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker --output jsonpath="{.items[?(@.status.conditions[-1].type=='Ready')].status.conditions[-1].type}" | wc -w | xargs)
+current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker=,node-role.kubernetes.io/infra!=,node-role.kubernetes.io/workload!= --output jsonpath="{.items[?(@.status.conditions[-1].type=='Ready')].status.conditions[-1].type}" | wc -w | xargs)
 
 export ITERATIONS=$((9*$current_worker_count))
 export WORKLOAD=cluster-density-v2
