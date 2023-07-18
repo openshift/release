b43852780a461687ec5f0606e4505bd189680024
Paige Rubendall <prubenda@redhat.com>
2023-07-18 17:05:01 -0400

    adding iteration multiplier

--- ci-operator/step-registry/openshift-qe/cluster-density-v2/openshift-qe-cluster-density-v2-commands.sh
+++ ci-operator/step-registry/openshift-qe/cluster-density-v2/openshift-qe-cluster-density-v2-commands.sh
@@ -19,8 +19,8 @@ git clone https://github.com/cloud-bulldozer/e2e-benchmarking --depth=1
 pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
 
 current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker=,node-role.kubernetes.io/infra!=,node-role.kubernetes.io/workload!= --output jsonpath="{.items[?(@.status.conditions[-1].type=='Ready')].status.conditions[-1].type}" | wc -w | xargs)
-
-export ITERATIONS=$((9*$current_worker_count))
+iteration_multiplier=$((ITERATION_MULTIPLIER))
+export ITERATIONS=$(($iteration_multiplier*$current_worker_count))
 export WORKLOAD=cluster-density-v2
 export EXTRA_FLAGS="--churn-duration=20m"
 
