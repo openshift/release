d7fdbc1fa59744fcacd930bca36760a293bb4e04
Rabin Yasharzadehe <rabin@rabin.io>
2023-07-19 16:45:19 +0300

    [CNV] Wait for hco-webhook-service
    Signed-off-by: Rabin Yasharzadehe <rabin@rabin.io>
    
    Unset KUBERNETES_ ENV Variables
    
    
    Increase timout 180m

--- ci-operator/step-registry/interop-tests/deploy-cnv/interop-tests-deploy-cnv-commands.sh
+++ ci-operator/step-registry/interop-tests/deploy-cnv/interop-tests-deploy-cnv-commands.sh
@@ -4,6 +4,16 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# Unset the following environment variables to avoid issues with oc commands
+unset KUBERNETES_SERVICE_PORT_HTTPS
+unset KUBERNETES_SERVICE_PORT
+unset KUBERNETES_PORT_443_TCP
+unset KUBERNETES_PORT_443_TCP_PROTO
+unset KUBERNETES_PORT_443_TCP_ADDR
+unset KUBERNETES_SERVICE_HOST
+unset KUBERNETES_PORT
+unset KUBERNETES_PORT_443_TCP_PORT
+
 CNV_INSTALL_NAMESPACE=openshift-cnv
 CNV_OPERATOR_CHANNEL="$CNV_OPERATOR_CHANNEL"
 
@@ -57,6 +67,14 @@ for _ in {1..60}; do
     fi
     sleep 10
 done
+
+# Wait for the hco-webhook-service to be ready
+until \
+  [[ "$(oc get pods -n openshift-cnv -l name=hyperconverged-cluster-webhook --no-headers | wc -l)" -gt 0 ]]; do
+    echo "HCO webhook pod was not created yet."
+    sleep 5s
+done
+oc wait pods -n openshift-cnv -l name=hyperconverged-cluster-webhook --for=jsonpath='{.status.containerStatuses[0].ready}'=true --timeout=180m
 
 oc create -f - <<EOF
 apiVersion: hco.kubevirt.io/v1beta1
