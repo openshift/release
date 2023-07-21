8140f3816c4818bd2f96fc7827cb4eb077a2320f
Biagio Manzari <bmanzari@redhat.com>
2023-07-21 12:20:53 +0200

    aaa

--- ci-operator/step-registry/observers/oink/observers-oink-commands.sh
+++ ci-operator/step-registry/observers/oink/observers-oink-commands.sh
@@ -7,7 +7,7 @@ set -o pipefail
 export STORE_PATH="${ARTIFACT_DIR}/oink-store/${BUILD_ID}"
 
 function cleanup() {
-  printf "%s: Cluster installed, stopping video recording \n" "$(date --utc --iso=s)"
+  printf "%s: Stop recording \n" "$(date --utc --iso=s)"
   echo "killing resource watch"
   CHILDREN=$(jobs -p)
   if test -n "${CHILDREN}"
@@ -15,7 +15,7 @@ function cleanup() {
     kill ${CHILDREN} && wait
   fi
 
-  echo "ended oink watch gracefully"
+  echo "ended oink observer gracefully"
 
   exit 0
 }
@@ -38,17 +38,52 @@ while [ ! -f "${SHARED_DIR}/idrac-vnc-password" ]; do
 done
 printf "%s: acquired %s\n" "$(date --utc --iso=s)" "${SHARED_DIR}/idrac-vnc-password"
 
-echo "Installation started, recording"
+# $KUBECONFIG could not be available when the observer first starts
+echo "waiting for $KUBECONFIG or $KUBECONFIGMINIMAL to exist"
+while [[ ! -s "$KUBECONFIG" && ! -s "$KUBECONFIGMINIMAL" ]]
+do
+  sleep 30
+done
+echo "Installation started, recording Serial-over-Lan output"
+
+SSHOPTS=(-o 'ConnectTimeout=5'
+  -o 'StrictHostKeyChecking=no'
+  -o 'UserKnownHostsFile=/dev/null'
+  -o 'ServerAliveInterval=90'
+  -o LogLevel=ERROR
+  -i "${CLUSTER_PROFILE_DIR}/ssh-key")
+
+CLUSTER_NAME="${NAMESPACE}"
+
+OINK_DIR=/tmp/oink
+
+mkdir -p "${OINK_DIR}"
+
+# Observer pods dont support vars from external file, thus hardcoded user and host
+# Additionaly, for reasons unkown to the writer, $SHARED_DIR in an observer pod works differently. The workaround is to manually copy files to a writable directory
+scp "${SSHOPTS[@]}" "root@openshift-qe-bastion.arm.eng.rdu2.redhat.com:/var/builds/${CLUSTER_NAME}/*.yaml" "${OINK_DIR}/"
+
+scp "${SSHOPTS[@]}" "root@openshift-qe-bastion.arm.eng.rdu2.redhat.com:/opt/html/vnc-recorder" "${OINK_DIR}/"
+
+"${OINK_DIR}/vnc-recorder --help"
 
 vnc_password=$(< "${SHARED_DIR}/idrac-vnc-password")
 
 
 # shellcheck disable=SC2154
-for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
+for bmhost in $(yq e -o=j -I=0 '.[]' "${OINK_DIR}/hosts.yaml"); do
   # shellcheck disable=SC1090
   . <(echo "$bmhost" | yq e 'to_entries | .[] | (.key + "=\"" + .value + "\"")')
+  echo "SoL recording on ${bmc_address}"
+  # sleep 3600 \
+  #   | ipmitool -I lanplus -H "$bmc_address" -U "$bmc_user" -P "$bmc_pass" sol activate \
+  #   2>> "${ARTIFACT_DIR}/${name}_ipmi_sol_output_stderr.txt" >> "${ARTIFACT_DIR}/${name}_ipmi_sol_output_stdout.txt" &
+
   VNC_PORT=5901
-  ssh -L $VNC_PORT:"${bmc_address}":5901 root@openshift-qe-bastion.arm.eng.rdu2.redhat.com
-  /vnc-recorder --host locahost --port $VNC_PORT --password "$vnc_password" --outfile "$STORE_PATH/${name}/idrac.mp4"
+  ssh -L $VNC_PORT:"${bmc_address}":5901 root@openshift-qe-bastion.arm.eng.rdu2.redhat.com &
+  "${OINK_DIR}/vnc-recorder" --host locahost --port $VNC_PORT --password "$vnc_password" --outfile "${ARTIFACT_DIR}/${name}_idrac.mp4" &
   ((VNC_PORT=VNC_PORT+1))
 done
+
+# Keep the observer pod alive while SoL recording
+sleep 3600
