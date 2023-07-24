dd30a435a4d8a18f63667591ff75523c1891ab78
psap-user <liqcui@redhat.com>
2023-07-24 03:39:36 +0000

    update code

--- ci-operator/step-registry/openshift-qe/rosa-loaded-upgrade/openshift-qe-rosa-loaded-upgrade-commands.sh
+++ ci-operator/step-registry/openshift-qe/rosa-loaded-upgrade/openshift-qe-rosa-loaded-upgrade-commands.sh
@@ -8,16 +8,11 @@ function rosa_login()
   if [ $# -eq 1 ];then
     REGION=$1
   fi
-  OPENSHIFT_VERSION=${OPENSHIFT_VERSION:-}
-  CHANNEL_GROUP=${CHANNEL_GROUP:=""}
-  MULTI_AZ=${MULTI_AZ:=false}
-  ROSA_LOGIN_ENV=${ROSA_LOGIN_ENV:="staging"}
-  CLUSTER_NAME=${CLUSTER_NAME:=""}
-  SHARED_DIR=${SHARED_DIR:=""}
-  echo "${CLUSTER_NAME}" > "${SHARED_DIR}/cluster-name"
 
+  ROSA_LOGIN_ENV=${ROSA_LOGIN_ENV:="staging"}
   ROSA_VERSION=$(rosa version)
   ROSA_TOKEN=$(cat "${CLUSTER_PROFILE_DIR}/ocm-token")
+
   if [[ ! -z "${ROSA_TOKEN}" ]]; then
      echo "Logging into ${ROSA_LOGIN_ENV} with offline token using rosa cli ${ROSA_VERSION}"
      rosa login --env "${ROSA_LOGIN_ENV}" --token "${ROSA_TOKEN}"
@@ -29,27 +24,34 @@ function rosa_login()
      echo "Cannot login! You need to specify the offline token ROSA_TOKEN!"
      exit 1
   fi
-  echo rosa list clusters --region $REGION
+  echo
+  echo "######################################################################"
   rosa list clusters --region $REGION
-  sleep 14400
-
+  echo "######################################################################"
+  echo 
 }
 
 function classic_rosa_upgrade()
 {
-  CLUSTER_NAME=${CLUSTER_NAME:=""}
-  REGION=${REGION:=""}
-  UPGRADE_TO_VERSION=4.13.2
+
   SCHEDULE_OFFSET=15  #After $SCHEDULE_OFFSET minutes to upgrade
   SCHEDULE_DATETIME=`date  -d "+${SCHEDULE_OFFSET} min" "+%Y-%m-%d %H:%M"`
   SCHEDULE_DATE=$(echo $SCHEDULE_DATETIME | awk '{print $1}')
   SCHEDULE_TIME=$(echo $SCHEDULE_DATETIME | awk '{print $2}')
 
-
   RELEASE_IMAGE_LATEST=${RELEASE_IMAGE_LATEST:=""}
   CURRENT_VERSION=$(oc get clusterversion -ojsonpath={..desired.version})
   OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE=${OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE:=$RELEASE_IMAGE_LATEST}
 
+  SHARED_DIR=${SHARED_DIR:=""}
+  if [ $# -eq 1 ];then
+    REGION=$1
+  fi
+
+  CLUSTER_NAME=${CLUSTER_NAME:=""}
+  if [[ -z $CLUSTER_NAME && -s "${SHARED_DIR}/cluster-name" ]];then
+        CLUSTER_NAME=$(cat ${SHARED_DIR}/cluster-name)
+  fi
   if [[ -z "$RELEASE_IMAGE_LATEST" ]]; then
     echo "RELEASE_IMAGE_LATEST is an empty string, exiting"
     exit 1
@@ -67,30 +69,32 @@ function classic_rosa_upgrade()
       TARGET_RELEASES="$(oc adm release info "${OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE}" --output=json | jq -r '.metadata.version')"
   fi
   echo  "-------------------------------------------------------------------------------------------"
-  echo  Loaded Upgrade from [ $CURRENT_VERSION ] to [ $TARGET_RELEASES ]
+  echo  Loaded Upgrade from [ $CURRENT_VERSION ] to [ $TARGET_RELEASES ] for $CLUSTER_NAME on $REGION
   echo  "-------------------------------------------------------------------------------------------"
 
-  echo "######################################"
-  rosa list clusters --region $REGION 
-  echo "######################################"
-  echo rosa upgrade cluster -c $CLUSTER_NAME --mode=auto --region $REGION --version $UPGRADE_TO_VERSION  -schedule-date $SCHEDULE_DATE --schedule-time $SCHEDULE_TIME -y
+  rosa upgrade cluster -c $CLUSTER_NAME --mode=auto --region $REGION --version $TARGET_RELEASES  -schedule-date $SCHEDULE_DATE --schedule-time $SCHEDULE_TIME -y
 
-  echo rosa describe upgrade --region $REGION -c $CLUSTER_NAME 
+  echo "Checking ROSA upgrade status ..."
+  INIT=1
+  MAXRETRY=120
+  while true
+  do
+	  UPGRADE_STATE=$(rosa describe upgrade --region $REGION -c $CLUSTER_NAME | grep 'Upgrade State:' | awk -F':' '{print $2}' | tr -d ' ')
+	  if [[ $UPGRADE_STATE == "scheduled" ]];then
+                  echo "######################################################################"
+		  echo "ROSA Upgrade has been sucessfully scheduled"
+                  echo "######################################################################"
+		  break
+	  fi
+	  INIT=$(( $INIT + 1 ))
 
-#rosa describe upgrade --region us-east-2 -c liqcui-rosa01
-#        Cluster ID:                 24opmdsathi6vkjn3k8iusf3g9d19t74
-#        Next Run:                   2023-07-04 13:15:00 +0000 UTC
-#        Upgrade State:              scheduled
-#                Version:                    4.13.4
+	  if [[ $INIT -gt $MAXRETRY ]];then
+		  echo "Fail to schedule upgrade in limited time"
+		  exit 1
+	  fi
+	  sleep 10
+  done
 
- 
-
-#rosa describe upgrade --region us-east-2 -c liqcui-rosa01
-#                ID:                         f348d13e-1a6b-11ee-a6e3-0a580a810253
-#        Cluster ID:                 24opmdsathi6vkjn3k8iusf3g9d19t74
-#        Next Run:                   2023-07-04 13:15:00 +0000 UTC
-#        Upgrade State:              started
-#                Version:                    4.13.4
   INIT_NUM=1
   MAX_NUM=$(( $SCHEDULE_OFFSET * 60 / 5 )) 
   MAX_SECOND=$(( $MAX_NUM * 5 ))
@@ -101,7 +105,26 @@ function classic_rosa_upgrade()
 	  echo -n "."&&sleep 5
 	  INIT_NUM=$(( $INIT_NUM + 1 ))
   done
-  echo rosa create admin -c liqcui-rosa01 --region us-east-1
+
+  INIT=1
+  MAXRETRY=120
+  while true
+  do
+	  UPGRADE_STATE=$(rosa describe upgrade --region $REGION -c $CLUSTER_NAME | grep 'Upgrade State:' | awk -F':' '{print $2}' | tr -d ' ')
+	  if [[ $UPGRADE_STATE == "started" ]];then
+                  echo "######################################################################"
+		  echo "ROSA Upgrade has been sucessfully started"
+                  echo "######################################################################"
+		  break
+	  fi
+	  INIT=$(( $INIT + 1 ))
+
+	  if [[ $INIT -gt $MAXRETRY ]];then
+		  echo "Fail to started upgrade in limited time"
+		  exit 1
+	  fi
+	  sleep 10
+  done
 
 }
 
@@ -121,4 +144,4 @@ else
   exit 1
 fi
 rosa_login $CLOUD_PROVIDER_REGION
-classic_rosa_upgrade perf-rosa01 $CLOUD_PROVIDER_REGION
+classic_rosa_upgrade $CLOUD_PROVIDER_REGION
