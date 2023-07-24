59b36866d4ec5a9af3ecf8ebdb786596b85ce350
Bear Chen <97229105+bear-redhat@users.noreply.github.com>
2023-07-24 23:45:04 +0800

    Fix issue DPTP-3562 Increase timeout for kubepasswd (#41585)

--- ci-operator/step-registry/hypershift/hostedcluster/create/hostedcluster/hypershift-hostedcluster-create-hostedcluster-commands.sh
+++ ci-operator/step-registry/hypershift/hostedcluster/create/hostedcluster/hypershift-hostedcluster-create-hostedcluster-commands.sh
@@ -99,9 +99,9 @@ bin/hypershift create kubeconfig --namespace=clusters --name=${CLUSTER_NAME} >${
 }
 
 # Data for cluster bot.
-# The kubeadmin-password secret is reconciled only after the kas is available so we will wait up to 2 minutes for it to become available
+# The kubeadmin-password secret is reconciled only after the kas is available so we will wait up to 5 minutes for it to become available
 echo "Retrieving kubeadmin password"
-for _ in {1..8}; do
+for _ in {1..20}; do
   kubeadmin_pwd=`oc get secret --namespace=clusters ${CLUSTER_NAME}-kubeadmin-password --template='{{.data.password}}' | base64 -d` || true
   if [ -z $kubeadmin_pwd ]; then
     echo "kubeadmin password is not ready yet, waiting 15s"
