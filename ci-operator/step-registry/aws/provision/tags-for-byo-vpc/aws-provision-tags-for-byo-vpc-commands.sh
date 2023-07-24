4d8f5c54f2b892ef1f96b2c4f333e2eacf13b991
ascerra@redhat.com <ascerra@redhat.com>
2023-07-24 12:53:50 -0400

    PoC to test why byo-vpc is failing.

--- ci-operator/step-registry/aws/provision/tags-for-byo-vpc/aws-provision-tags-for-byo-vpc-commands.sh
+++ ci-operator/step-registry/aws/provision/tags-for-byo-vpc/aws-provision-tags-for-byo-vpc-commands.sh
@@ -4,33 +4,35 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-if [[ ${ENABLE_SHARED_VPC} == "yes" ]]; then
-  echo "Using shared account"
-  export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred_shared_account"
-else
-  export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
-fi
+sleep 30m
 
-REGION="${LEASED_RESOURCE}"
+# if [[ ${ENABLE_SHARED_VPC} == "yes" ]]; then
+#   echo "Using shared account"
+#   export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred_shared_account"
+# else
+#   export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
+# fi
 
-infra_id=$(jq -r '.infraID' ${SHARED_DIR}/metadata.json)
-vpc_id=$(head -n 1 ${SHARED_DIR}/vpc_id)
-private_subnet_ids=$(yq-go r -j ${SHARED_DIR}/private_subnet_ids | jq -r '[ . | join(" ") ] | @csv' | sed "s/\"//g")
-public_subnet_ids=$(yq-go r -j ${SHARED_DIR}/public_subnet_ids | jq -r '[ . | join(" ") ] | @csv' | sed "s/\"//g")
+# REGION="${LEASED_RESOURCE}"
 
-if [[ -z $vpc_id ]] || [[ -z $private_subnet_ids ]] || [[ -z $infra_id ]]; then
-  echo "Error: Can not get VPC id or private subnets, exit"
-  echo "vpc: $vpc_id, private_subnet_ids: $private_subnet_ids"
-  exit 1
-fi
+# infra_id=$(jq -r '.infraID' ${SHARED_DIR}/metadata.json)
+# vpc_id=$(head -n 1 ${SHARED_DIR}/vpc_id)
+# private_subnet_ids=$(yq-go r -j ${SHARED_DIR}/private_subnet_ids | jq -r '[ . | join(" ") ] | @csv' | sed "s/\"//g")
+# public_subnet_ids=$(yq-go r -j ${SHARED_DIR}/public_subnet_ids | jq -r '[ . | join(" ") ] | @csv' | sed "s/\"//g")
 
-echo "Adding tags for VPC: $vpc_id, tags: kubernetes.io/cluster/${infra_id}, value: shared."
-aws --region $REGION ec2 create-tags --resources $vpc_id --tags Key=kubernetes.io/cluster/${infra_id},Value=shared
+# if [[ -z $vpc_id ]] || [[ -z $private_subnet_ids ]] || [[ -z $infra_id ]]; then
+#   echo "Error: Can not get VPC id or private subnets, exit"
+#   echo "vpc: $vpc_id, private_subnet_ids: $private_subnet_ids"
+#   exit 1
+# fi
 
-echo "Adding tags for private subnets:$private_subnet_ids, tags: kubernetes.io/role/internal-elb, value is empty."
-aws --region $REGION ec2 create-tags --resources $private_subnet_ids --tags Key=kubernetes.io/role/internal-elb,Value=
+# echo "Adding tags for VPC: $vpc_id, tags: kubernetes.io/cluster/${infra_id}, value: shared."
+# aws --region $REGION ec2 create-tags --resources $vpc_id --tags Key=kubernetes.io/cluster/${infra_id},Value=shared
 
-if [[ ${APPLY_TAGS_TO_PUBLIC_SUBNETS} == "yes" ]]; then
-  echo "Adding tags for public subnets:$public_subnet_ids, tags: kubernetes.io/role/elb, value is empty."
-  aws --region $REGION ec2 create-tags --resources $public_subnet_ids --tags Key=kubernetes.io/role/elb,Value=
-fi
+# echo "Adding tags for private subnets:$private_subnet_ids, tags: kubernetes.io/role/internal-elb, value is empty."
+# aws --region $REGION ec2 create-tags --resources $private_subnet_ids --tags Key=kubernetes.io/role/internal-elb,Value=
+
+# if [[ ${APPLY_TAGS_TO_PUBLIC_SUBNETS} == "yes" ]]; then
+#   echo "Adding tags for public subnets:$public_subnet_ids, tags: kubernetes.io/role/elb, value is empty."
+#   aws --region $REGION ec2 create-tags --resources $public_subnet_ids --tags Key=kubernetes.io/role/elb,Value=
+# fi
