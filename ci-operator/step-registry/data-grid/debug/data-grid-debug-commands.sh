e71ef5fcece141e5e55e774fcbcc6f0b6e4c8404
Caleb Evans <caevans@redhat.com>
2023-07-26 10:35:16 -0600

    Add oc apply for cluster-monitoring-config

--- ci-operator/step-registry/data-grid/debug/data-grid-debug-commands.sh
+++ ci-operator/step-registry/data-grid/debug/data-grid-debug-commands.sh
@@ -4,4 +4,15 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+oc apply -f - <<EOF
+    apiVersion: v1
+    kind: ConfigMap
+    metadata:
+        name: cluster-monitoring-config
+        namespace: openshift-monitoring
+    data:
+        config.yaml: |
+            enableUserWorkload: true 
+EOF
+
 sleep 7200
