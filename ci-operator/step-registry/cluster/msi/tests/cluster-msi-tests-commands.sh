4aaee70a0473c6e1038539542ede9764d57ff800
rnetser <rnetser@redhat.com>
2023-07-30 13:27:41 +0300

    add missing quoation marks

--- ci-operator/step-registry/cluster/msi/tests/cluster-msi-tests-commands.sh
+++ ci-operator/step-registry/cluster/msi/tests/cluster-msi-tests-commands.sh
@@ -8,7 +8,12 @@ set -o verbose
 # Extract clusters archive from SHARED_DIR
 tar -xzvf "${SHARED_DIR}/clusters_data.tar.gz" --one-top-leve=/tmp/clusters-data
 
-RUN_COMMAND="poetry run pytest tests -o log_cli=true --junit-xml='${ARTIFACT_DIR}/xunit_results.xml' --pytest-log-file='${ARTIFACT_DIR}/pytest-tests.log' -m ${TEST_MARKER} "
+RUN_COMMAND="poetry run pytest tests \
+            -s -o log_cli=true \
+            --junit-xml='${ARTIFACT_DIR}/xunit_results.xml' \
+            --pytest-log-file='${ARTIFACT_DIR}/pytest-tests.log' \
+            -m ${TEST_MARKER} \
+            --data-collector=data-collector_containerized.yaml"
 KUBECONFIG_COMMAND="--kubeconfig-file-paths="${KUBECONFIG1_PATH},${KUBECONFIG2_PATH},${KUBECONFIG3_PATH}" "
 
 RUN_COMMAND+=" ${KUBECONFIG_COMMAND} "
