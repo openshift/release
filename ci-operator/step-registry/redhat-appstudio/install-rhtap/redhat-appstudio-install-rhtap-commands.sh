f1f878f578e4b77ed0fd49bf73fd9af26396d463
Naftaly Shprai <naftalysh@gmail.com>
2023-07-31 17:26:20 +0300

    Updated redhat-appstudio-load-test-commands.sh

--- ci-operator/step-registry/redhat-appstudio/install-rhtap/redhat-appstudio-install-rhtap-commands.sh
+++ ci-operator/step-registry/redhat-appstudio/install-rhtap/redhat-appstudio-install-rhtap-commands.sh
@@ -78,7 +78,6 @@ echo "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com" > "${GIT_CREDS_PATH}"
 
 cd "$(mktemp -d)"
 
-# git clone --branch main "https://${GITHUB_TOKEN}@github.com/redhat-appstudio/e2e-tests.git" . 
-git clone --branch RHTAP-1018/Load-test-measure-how-long-the-app-test-took "https://github.com/naftalysh/e2e-tests.git" .
+git clone --branch main "https://${GITHUB_TOKEN}@github.com/redhat-appstudio/e2e-tests.git" . 
 ./mage -v ci:prepareE2Ebranch
 ./mage -v bootstrapCluster
