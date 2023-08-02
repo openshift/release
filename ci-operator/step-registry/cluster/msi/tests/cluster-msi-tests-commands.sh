98e3ce8cbaad37425fa77a450d0bf877fbb7f7be
rnetser <rnetser@redhat.com>
2023-08-02 17:29:40 +0300

    update data-collector file

--- ci-operator/step-registry/cluster/msi/tests/cluster-msi-tests-commands.sh
+++ ci-operator/step-registry/cluster/msi/tests/cluster-msi-tests-commands.sh
@@ -8,20 +8,16 @@ set -o verbose
 # Extract clusters archive from SHARED_DIR
 tar -xzvf "${SHARED_DIR}/clusters_data.tar.gz" --one-top-leve=/tmp/clusters-data
 
-sleep 500000
-
-
 RUN_COMMAND="poetry run pytest tests \
             -s -o log_cli=true \
             --junit-xml='${ARTIFACT_DIR}/xunit_results.xml' \
             --pytest-log-file='${ARTIFACT_DIR}/pytest-tests.log' \
             -m ${TEST_MARKER} \
-            --data-collector=data-collector_containerized.yaml"
-KUBECONFIG_COMMAND="--kubeconfig-file-paths="${KUBECONFIG1_PATH},${KUBECONFIG2_PATH}" "
+            --data-collector=data-collector-openshift-ci.yaml"
+KUBECONFIG_COMMAND="--kubeconfig-file-paths="${CLUSTER1_KUBECONFIG_PATH},${CLUSTER2_KUBECONFIG_PATH}" "
 
 RUN_COMMAND+=" ${KUBECONFIG_COMMAND} "
 
 echo "$RUN_COMMAND"
-
 
 ${RUN_COMMAND}
