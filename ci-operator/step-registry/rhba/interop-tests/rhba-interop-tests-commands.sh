b69a64170ad955717c2f8b38bae9f17f273cd087
Jakub Schwan <jschwan@redhat.com>
2023-08-02 17:20:03 +0200

    Add junit_ prefix for xml test reports

--- ci-operator/step-registry/rhba/interop-tests/rhba-interop-tests-commands.sh
+++ ci-operator/step-registry/rhba/interop-tests/rhba-interop-tests-commands.sh
@@ -8,3 +8,11 @@ export TEST_COLLECT_BASE_DIR=${ARTIFACT_DIR}
 
 echo "Running tests..."
 /opt/runTest.sh
+
+echo "Adding junit prefix for xml test reports"
+cd "${ARTIFACT_DIR}"
+for file in "*.xml"
+do
+  mv "$file" "junit_${file}"
+done
+cd -
