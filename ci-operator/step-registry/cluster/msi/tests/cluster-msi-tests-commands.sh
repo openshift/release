01dc24982a4758f225da6c6d88750d36c1a29336
rnetser <rnetser@redhat.com>
2023-08-03 16:31:40 +0300

    update pytest cmd

--- ci-operator/step-registry/cluster/msi/tests/cluster-msi-tests-commands.sh
+++ ci-operator/step-registry/cluster/msi/tests/cluster-msi-tests-commands.sh
@@ -9,17 +9,17 @@ set -o verbose
 tar -xzvf "${SHARED_DIR}/clusters_data.tar.gz" --one-top-leve=/tmp/clusters-data
 
 RUN_COMMAND="poetry run pytest tests \
-            -s -o log_cli=true --pdb \
-            --junit-xml='${ARTIFACT_DIR}/xunit_results.xml' \
-            --pytest-log-file='${ARTIFACT_DIR}/pytest-tests.log' \
+            -o log_cli=true \
+            --junit-xml=${ARTIFACT_DIR}/xunit_results.xml \
+            --pytest-log-file=${ARTIFACT_DIR}/pytest-tests.log \
             -m ${TEST_MARKER} \
-            --data-collector=data-collector-openshift-ci.yaml --trace"
+            --data-collector=data-collector-openshift-ci.yaml \
+            --tc=home_dir:/tmp"
+
 KUBECONFIG_COMMAND="--kubeconfig-file-paths="${CLUSTER1_KUBECONFIG_PATH},${CLUSTER2_KUBECONFIG_PATH}" "
 
 RUN_COMMAND+=" ${KUBECONFIG_COMMAND} "
 
 echo "$RUN_COMMAND"
-
-sleep 50000000
 
 ${RUN_COMMAND}
