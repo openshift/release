b93e0a26d93e7d7278b71fe6037faad20cece5b5
Simon Kordas <skordas@redhat.com>
2023-08-07 13:10:03 -0400

    pod service route test

---
+++ ci-operator/step-registry/openshift-qe/pod-service-route/openshift-qe-pod-service-route-commands.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+cat /etc/os-release
+oc config view
+oc projects
+python --version
+pushd /tmp
+python -m virtualenv ./venv_qe
+source ./venv_qe/bin/activate
+
+ES_PASSWORD=$(cat "/secret/password")
+ES_USERNAME=$(cat "/secret/username")
+
+git clone https://github.com/cloud-bulldozer/e2e-benchmarking
+pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
+
+current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker=,node-role.kubernetes.io/infra!=,node-role.kubernetes.io/workload!= --output jsonpath="{.items[?(@.status.conditions[-1].type=='Ready')].status.conditions[-1].type}" | wc -w | xargs)
+
+export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
+
+export NAMESPACE_COUNT=$(($current_worker_count*$NUMBER_OF_NS_PER_NODE))
+export WORKLOAD=pods-service-route
+
+rm -rf "${SHARED_DIR}/${OUTPUT_FILE:?}"
+./run.sh |& tee "${SHARED_DIR}/${OUTPUT_FILE}"
