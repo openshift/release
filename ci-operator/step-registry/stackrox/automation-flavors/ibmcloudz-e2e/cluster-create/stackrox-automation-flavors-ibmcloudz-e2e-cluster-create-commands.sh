6aae6ea3069bee54f2ea90e30ac63153c51e8d93
Ganesh Bhure <ganesh.bhure@gmail.com>
2023-08-08 06:56:31 -0400

    Configurations to create and destroy OCP cluster on IBM cloud for Z

--- ci-operator/step-registry/stackrox/automation-flavors/ibmcloudz-e2e/cluster-create/stackrox-automation-flavors-ibmcloudz-e2e-cluster-create-commands.sh
+++ ci-operator/step-registry/stackrox/automation-flavors/ibmcloudz-e2e/cluster-create/stackrox-automation-flavors-ibmcloudz-e2e-cluster-create-commands.sh
@@ -18,12 +18,16 @@ PRIVATE_KEY_FILE="${SECRET_DIR}/PRIVATE_KEY_FILE"
 
 SSH_KEY_PATH="/tmp/id_rsa"
 SSH_ARGS="-i ${SSH_KEY_PATH} -o MACs=hmac-sha2-256 -o StrictHostKeyChecking=no -o LogLevel=ERROR -o UserKnownHostsFile=/dev/null"
-# k8smanager script is to create/delete k8s cluster on IBM cloud [https://github.ibm.com/Ganesh-Bhure2/stackrox-ci]
-SSH_CMD="/home/ubuntu/stackrox-ci/k8smanager create"
-
 # setup ssh key
 cp -f $PRIVATE_KEY_FILE $SSH_KEY_PATH
 chmod 400 $SSH_KEY_PATH
 
+### Kubernetes Cluster Creation. k8smanager script is to create/delete k8s cluster on IBM cloud [https://github.ibm.com/Ganesh-Bhure2/stackrox-ci]
+# SSH_CMD="/home/ubuntu/stackrox-ci/k8smanager create"
 # create k8s cluster. k8smanager script runs on an intermediate node [IP 163.66.94.115] on IBM cloud
-ssh $SSH_ARGS root@163.66.94.115 "$SSH_CMD"
+# ssh $SSH_ARGS root@163.66.94.115 "$SSH_CMD"
+
+### OCP Cluster Creation. ocpmanager script is to create/delete ocp cluster on IBM cloud [https://github.ibm.com/Ganesh-Bhure2/stackrox-ci]
+SSH_CMD="/home/ubuntu/stackrox-ci/ocpmanager create"
+# create ocp cluster. ocpmanager script runs on an intermediate node [IP 163.74.90.40] on IBM cloud
+ssh $SSH_ARGS root@163.74.90.40 "$SSH_CMD"
