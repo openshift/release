e3f9b9e73167f91a425d4f784bbc41e6613f8558
Flavius Lacatusu <flacatus@redhat.com>
2023-08-10 17:44:19 +0200

    update owners

--- ci-operator/step-registry/devfile/rhtap-nightly/devfile-rhtap-nightly-commands.sh
+++ ci-operator/step-registry/devfile/rhtap-nightly/devfile-rhtap-nightly-commands.sh
@@ -4,7 +4,9 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-ls -larth
-git clone https://github.com/devfile/registry.git -b main
+cd "$(mktemp -d)"
+git clone https://github.com/devfile/registry.git -b main && cd registry
+
+go install -mod=mod github.com/onsi/ginkgo/v2/ginkgo
 
 /bin/bash tests/check_rhtap_nightly.sh
