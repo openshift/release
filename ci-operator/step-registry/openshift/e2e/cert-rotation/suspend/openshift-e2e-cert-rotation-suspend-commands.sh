c9c29bc2910c18d2b10b8cd4082b55edaf8e29ba
Vadim Rutkovsky <vrutkovs@redhat.com>
2023-08-11 13:27:47 +0200

    cert-rotation: control_nodes and compute_nodes are proper bash arrays

--- ci-operator/step-registry/openshift/e2e/cert-rotation/suspend/openshift-e2e-cert-rotation-suspend-commands.sh
+++ ci-operator/step-registry/openshift/e2e/cert-rotation/suspend/openshift-e2e-cert-rotation-suspend-commands.sh
@@ -38,26 +38,26 @@ if [ -d "$KUBECONFIG" ]; then
   done
 fi
 
-control_nodes=$( ${OC} get nodes --selector='node-role.kubernetes.io/master' --template='{{ range $index, $_ := .items }}{{ range .status.addresses }}{{ if (eq .type "InternalIP") }}{{ if $index }} {{end }}{{ .address }}{{ end }}{{ end }}{{ end }}' )
+mapfile -d ' ' -t control_nodes < <( ${OC} get nodes --selector='node-role.kubernetes.io/master' --template='{{ range $index, $_ := .items }}{{ range .status.addresses }}{{ if (eq .type "InternalIP") }}{{ if $index }} {{end }}{{ .address }}{{ end }}{{ end }}{{ end }}' )
 
-compute_nodes=$( ${OC} get nodes --selector='!node-role.kubernetes.io/master' --template='{{ range $index, $_ := .items }}{{ range .status.addresses }}{{ if (eq .type "InternalIP") }}{{ if $index }} {{end }}{{ .address }}{{ end }}{{ end }}{{ end }}' )
+mapfile -d ' ' -t compute_nodes < <( ${OC} get nodes --selector='!node-role.kubernetes.io/master' --template='{{ range $index, $_ := .items }}{{ range .status.addresses }}{{ if (eq .type "InternalIP") }}{{ if $index }} {{end }}{{ .address }}{{ end }}{{ end }}{{ end }}' )
 
-function run-on {
-  for n in ${1}; do timeout ${COMMAND_TIMEOUT} ${SSH} core@"${n}" sudo 'bash -eEuxo pipefail' <<< ${2}; done
+function run-on-all-nodes {
+  for n in ${control_nodes[@]} ${compute_nodes[@]}; do timeout ${COMMAND_TIMEOUT} ${SSH} core@"${n}" sudo 'bash -eEuxo pipefail' <<< ${1}; done
 }
 
 function run-on-first-master {
-  timeout ${COMMAND_TIMEOUT} ${SSH} core@"${control_nodes[0]}" sudo 'bash -eEuxo pipefail' <<< ${1}
+  timeout ${COMMAND_TIMEOUT} ${SSH} "core@${control_nodes[0]}" sudo 'bash -eEuxo pipefail' <<< ${1}
 }
 
 function copy-file-from-first-master {
-  timeout ${COMMAND_TIMEOUT} ${SCP} core@"${control_nodes[0]}:${1}" "${2}"
+  timeout ${COMMAND_TIMEOUT} ${SCP} "core@${control_nodes[0]}:${1}" "${2}"
 }
 
-ssh-keyscan -H ${control_nodes} ${compute_nodes} >> ~/.ssh/known_hosts
+ssh-keyscan -H ${control_nodes[@]} ${compute_nodes[@]} >> ~/.ssh/known_hosts
 
 # Stop chrony service on all nodes
-run-on "${control_nodes} ${compute_nodes}" "systemctl disable chronyd --now"
+run-on-all-nodes "systemctl disable chronyd --now"
 
 # Backup lb-ext kubeconfig so that it could be compared to a new one
 KUBECONFIG_NODE_DIR="/etc/kubernetes/static-pod-resources/kube-apiserver-certs/secrets/node-kubeconfigs"
@@ -73,10 +73,10 @@ sudo timedatectl status
 
 # Skew clock on every node
 # TODO: Suspend, resume and make it resync time from host instead?
-run-on "${control_nodes} ${compute_nodes}" "timedatectl set-time ${SKEW} && timedatectl status"
+run-on-all-nodes "timedatectl set-time ${SKEW} && timedatectl status"
 
 # Restart kubelet
-run-on "${control_nodes} ${compute_nodes}" "systemctl restart kubelet"
+run-on-all-nodes "systemctl restart kubelet"
 
 # Wait for nodes to become unready and approve CSRs until nodes are ready again
 run-on-first-master "
