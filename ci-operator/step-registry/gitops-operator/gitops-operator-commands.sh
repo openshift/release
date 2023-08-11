65d9f09c489a5158babdbb383f2c681a2e4cd5f7
varshab1210 <varshab1210@gmail.com>
2023-08-11 11:43:27 +0530

    adding results to artifacts
    Signed-off-by: varshab1210 <varshab1210@gmail.com>

--- ci-operator/step-registry/gitops-operator/gitops-operator-commands.sh
+++ ci-operator/step-registry/gitops-operator/gitops-operator-commands.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-set -o pipefail
+set -x
 
 unset CI
 scripts/openshift-CI-kuttl-tests.sh
-make e2e-tests-sequential
+kubectl kuttl test test/openshift/e2e/sequential --config test/openshift/e2e/sequential/kuttl-test.yaml --report xml
+cp openshift-gitops-e2e.xml ${ARTIFACT_DIR}/junit_gitops-sequential.xml
