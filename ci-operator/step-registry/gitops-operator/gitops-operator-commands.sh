7b02966aaf3e902400d8768277fe2c1088b9b819
varshab1210 <varshab1210@gmail.com>
2023-08-11 16:11:02 +0530

    Move test results
    Signed-off-by: varshab1210 <varshab1210@gmail.com>

--- ci-operator/step-registry/gitops-operator/gitops-operator-commands.sh
+++ ci-operator/step-registry/gitops-operator/gitops-operator-commands.sh
@@ -6,5 +6,6 @@ unset CI
 scripts/openshift-CI-kuttl-tests.sh
 kubectl kuttl test test/openshift/e2e/sequential --config test/openshift/e2e/sequential/kuttl-test.yaml --report xml
 
+# Move results to Artifacts directory
 find . -type f -name "*.xml"
-# cp openshift-gitops-e2e.xml ${ARTIFACT_DIR}/junit_gitops-sequential.xml
+cp ./kuttl-test.xml ${ARTIFACT_DIR}/junit_gitops-sequential.xml
