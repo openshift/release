71c35f129739c3542135d914b283106379046184
Murali Krishnasamy <mukrishn@redhat.com>
2023-08-14 13:16:39 -0400

    check for `Ready` nodecount part of wait-ready

--- ci-operator/step-registry/rosa/cluster/wait-ready/nodes/rosa-cluster-wait-ready-nodes-commands.sh
+++ ci-operator/step-registry/rosa/cluster/wait-ready/nodes/rosa-cluster-wait-ready-nodes-commands.sh
@@ -91,9 +91,14 @@ function waitForReady() {
                 break
             fi
         else
-            echo "All nodes are ready to run workloads."
-            FINAL_NODE_STATE="Pass"
-            break
+            node_count="$(oc get nodes --no-headers -l node-role.kubernetes.io/worker --output jsonpath="{.items[?(@.status.conditions[-1].type=='Ready')].status.conditions[-1].type}" | wc -w | xargs)"
+            if (( "$node_count" >= "$1" )); then
+                echo "All nodes are ready to run workloads."
+                FINAL_NODE_STATE="Pass"
+                break
+            else
+                echo "Only $node_count/$1 worker nodes are ready."
+            fi
         fi
         export FINAL_NODE_STATE
         sleep 60
@@ -116,8 +121,19 @@ function getDesiredComputeCount {
     echo "--auto-scaling disabled, --replicas set, retrieving replica count desired"
     desired_compute_count=$(rosa describe cluster -c "$CLUSTER_ID"  -o json  |jq -r '.nodes.compute')
   fi
+  if [[ "$MP_REPLICAS" != "" ]]; then
+    echo "Extra Machinepool specified in the configuration"
+    if [[ "$is_hcp_cluster" == "true" ]]; then
+      mp_count=$((MP_REPLICAS*3))
+      echo "Additional $mp_count nodes created in this workflow"
+      desired_compute_count=$((desired_compute_count+mp_count))
+    else
+      echo "Additional $MP_REPLICAS nodes created in this workflow"
+      desired_compute_count=$((desired_compute_count+MP_REPLICAS))
+    fi
+  fi
   export desired_compute_count
-  echo "Desired worker node count: $desired_compute_count"
+  echo "Total desired node count: $desired_compute_count"
 }
 
 # Get cluster 
