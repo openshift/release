c7fdf6d239a3ea6d88c424a60aa98830395076da
He Liu <heli@redhat.com>
2023-08-15 15:50:46 +0800

    update for public hosted cluster

--- ci-operator/step-registry/cucushift/hypershift-extended/install-private/cucushift-hypershift-extended-install-private-commands.sh
+++ ci-operator/step-registry/cucushift/hypershift-extended/install-private/cucushift-hypershift-extended-install-private-commands.sh
@@ -8,12 +8,6 @@ bin/hypershift install --hypershift-image="${HYPERSHIFT_RELEASE_LATEST}" \
 --oidc-storage-provider-s3-credentials=${CLUSTER_PROFILE_DIR}/.awscred \
 --oidc-storage-provider-s3-bucket-name=${BUCKET_NAME} \
 --oidc-storage-provider-s3-region=${HYPERSHIFT_AWS_REGION} \
---private-platform=AWS \
---aws-private-creds=/etc/hypershift-pool-aws-credentials/awsprivatecred \
---aws-private-region="${HYPERSHIFT_AWS_REGION}" \
---external-dns-credentials=${CLUSTER_PROFILE_DIR}/.awscred \
---external-dns-provider=aws \
---external-dns-domain-filter=hypershift-ext.qe.devcluster.openshift.com \
 --wait-until-available
 
 echo "" > ${SHARED_DIR}/.awsprivatecred
