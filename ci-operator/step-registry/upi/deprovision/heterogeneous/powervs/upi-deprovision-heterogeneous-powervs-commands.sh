a81df19df8c6568b1ee01fdf89df64d822d022db
Shilpa Gokul <sgokul@redhat.com>
2023-08-15 08:24:04 -0400

    MIXEDARCH-271: ci changes
    - updates to the image-create for debugging
    - cleanup deprovision to populate the expected files
    - create service instance considers the time to become active with a wait-for loop
    - add comment on why/how the CentOS-Stream-8 image is used
    - externalize the jq,yq,go,terraform versions used
    - address spellcheck warnings
    - update the ibmcloud ref
    - reorganize the environment variables
    - allow loop to gracefully end
    - refactor style
    - avoid pipefail cases
    - update the deprovision environment variables.
    - attach a tag 'mac' to the resources to make it easier to identify orphaned resources
    - refactored the service-instance-delete
    - unified the style of the PR
    - set absolute path for public and private keys
    - cleaned up the shellcheck violations for the deprovision step
    - fixed the additional worker count
    - remove double destroy as the timing for destroy is now worked out
    
    Signed-off-by: Paul Bastide <pbastide@redhat.com>
    Signed-off-by: Shilpa Gokul <sgokul@redhat.com>

--- ci-operator/step-registry/upi/deprovision/heterogeneous/powervs/upi-deprovision-heterogeneous-powervs-commands.sh
+++ ci-operator/step-registry/upi/deprovision/heterogeneous/powervs/upi-deprovision-heterogeneous-powervs-commands.sh
@@ -4,73 +4,117 @@
 echo "Invoking upi deprovision heterogeneous powervs"
 
 IBMCLOUD_HOME_FOLDER=/tmp/ibmcloud
-mkdir -p ${IBMCLOUD_HOME_FOLDER}
+mkdir -p "${IBMCLOUD_HOME_FOLDER}"
 REGION="${LEASED_RESOURCE}"
 
-if [ -z "$(command -v ibmcloud)" ]; then
+if [ -z "$(command -v ibmcloud)" ]
+then
   echo "ibmcloud CLI doesn't exist, installing"
   curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
 fi
 
 function ic() {
-  HOME=${IBMCLOUD_HOME_FOLDER} ibmcloud "$@"
+  HOME="${IBMCLOUD_HOME_FOLDER}" ibmcloud "$@"
 }
 
 ic version
-ic login --apikey @${CLUSTER_PROFILE_DIR}/ibmcloud-api-key -r ${REGION}
-ic plugin install -f cloud-internet-services vpc-infrastructure cloud-object-storage power-iaas is
+ic login --apikey "@${CLUSTER_PROFILE_DIR}/ibmcloud-api-key" -r "${REGION}"
+ic plugin install -f cloud-internet-services vpc-infrastructure cloud-object-storage power-iaas is tg-cli
 
 echo "Check if the var.tfvars exists in ${SHARED_DIR}"
 if [ -f "${SHARED_DIR}/var.tfvars" ]
 then
-  # upgrade go to 1.20.7 or greater
-  if [ -z "$(command -v go)" ]; then
+  # upgrade go to GO_VERSION
+  if [ -z "$(command -v go)" ]
+  then
     echo "go is not installed, proceed to installing go"
-    cd /tmp && wget -q https://go.dev/dl/go1.20.7.linux-amd64.tar.gz && tar -C /tmp -xzf go1.20.7.linux-amd64.tar.gz \
+    cd /tmp && wget -q "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz" \
+      && tar -C /tmp -xzf "go${GO_VERSION}.linux-amd64.tar.gz" \
       && export PATH=$PATH:/tmp/go/bin && export GOCACHE=/tmp/go && export GOPATH=/tmp/go && export GOMODCACHE="/tmp/go/pkg/mod"
-    if [ -z "$(command -v go)" ]; then
+    if [ -z "$(command -v go)" ]
+    then
       echo "Installed go successfully"
     fi
   fi
 
-  # build terraform from source v1.5.5
-  cd ${IBMCLOUD_HOME_FOLDER} && curl -L https://github.com/hashicorp/terraform/archive/refs/tags/v1.5.5.tar.gz -o ${IBMCLOUD_HOME_FOLDER}/terraform.tar.gz \
-    && tar -xzf ${IBMCLOUD_HOME_FOLDER}/terraform.tar.gz && cd ${IBMCLOUD_HOME_FOLDER}/terraform-1.5.5 \
-    && go build -ldflags "-w -s -X 'github.com/hashicorp/terraform/version.dev=no'" -o bin/ . && cp bin/terraform /tmp/terraform
+  # build terraform
+  cd "${IBMCLOUD_HOME_FOLDER}" \
+    && curl -L "https://github.com/hashicorp/terraform/archive/refs/tags/v${TERRAFORM_VERSION}.tar.gz" \
+      -o "${IBMCLOUD_HOME_FOLDER}/terraform.tar.gz" \
+    && tar -xzf "${IBMCLOUD_HOME_FOLDER}/terraform.tar.gz" \
+    && cd "${IBMCLOUD_HOME_FOLDER}/terraform-${TERRAFORM_VERSION}" \
+    && go build -ldflags "-w -s -X 'github.com/hashicorp/terraform/version.dev=no'" -o bin/ . \
+    && cp bin/terraform /tmp/terraform
   export PATH=$PATH:/tmp
   t_ver1=$(/tmp/terraform -version)
   echo "terraform version: ${t_ver1}"
 
   echo "Destroy the terraform"
-  OCP_VERSION=$(cat ${SHARED_DIR}/OCP_VERSION)
+  OCP_VERSION=$(cat "${SHARED_DIR}/OCP_VERSION")
   # Fetch the ocp4-upi-compute-powervs repo to perform deprovisioning
-  cd ${IBMCLOUD_HOME_FOLDER} && curl -L https://github.com/IBM/ocp4-upi-compute-powervs/archive/refs/heads/release-${OCP_VERSION}.tar.gz -o ${IBMCLOUD_HOME_FOLDER}/ocp-${OCP_VERSION}.tar.gz \
-      && tar -xzf ${IBMCLOUD_HOME_FOLDER}/ocp-${OCP_VERSION}.tar.gz && mv ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs-release-${OCP_VERSION} ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs
+  cd "${IBMCLOUD_HOME_FOLDER}" && curl -L "https://github.com/IBM/ocp4-upi-compute-powervs/archive/refs/heads/release-${OCP_VERSION}.tar.gz" -o "${IBMCLOUD_HOME_FOLDER}/ocp-${OCP_VERSION}.tar.gz" \
+      && tar -xzf "${IBMCLOUD_HOME_FOLDER}/ocp-${OCP_VERSION}.tar.gz" \
+      && mv "${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs-release-${OCP_VERSION}" "${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs"
   # copy the var.tfvars file from ${SHARED_DIR}
-  cp ${SHARED_DIR}/var.tfvars ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs/var.tfvars
-  cp ${SHARED_DIR}/terraform.tfstate ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs/terraform.tfstate
+  cp "${SHARED_DIR}/var.tfvars" ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs/var.tfvars
+  cp "${SHARED_DIR}/terraform.tfstate" ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs/terraform.tfstate
 
-  # Invoke the destroy command and optimistically run twice due to synchronization issues
-  cd ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs \
-    && /tmp/terraform init -upgrade \
-    && /tmp/terraform destroy -var-file=var.tfvars -auto-approve -no-color || true \
+  # Copy over the key files and kubeconfig
+  export PRIVATE_KEY_FILE="${CLUSTER_PROFILE_DIR}/ssh-privatekey"
+  export PUBLIC_KEY_FILE="${CLUSTER_PROFILE_DIR}/ssh-publickey"
+  export KUBECONFIG="${SHARED_DIR}/kubeconfig"
+  cp "${PUBLIC_KEY_FILE}" "${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs/data/id_rsa.pub"
+  cp "${PRIVATE_KEY_FILE}" "${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs/data/id_rsa"
+  cp "${KUBECONFIG}" "${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs/data/kubeconfig"
+
+  # Invoke the destroy command
+  cd "${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs" \
+    && /tmp/terraform init -upgrade -no-color \
     && /tmp/terraform destroy -var-file=var.tfvars -auto-approve -no-color
 else
   echo "Error: File ${SHARED_DIR}/var.tfvars does not exists."
 fi
 
 # Delete the workspace created
-if [ -f "${SHARED_DIR}/POWERVS_SERVICE_INSTANCE_ID" ]
+if [ -f "${SHARED_DIR}/POWERVS_SERVICE_CRN" ]
 then
-  echo "Starting the deslete on the PowerVS resource"
-  SERVICE_ID=$(cat ${SHARED_DIR}/POWERVS_SERVICE_INSTANCE_ID)
+  echo "Starting the delete on the PowerVS resource"
+  POWERVS_CRN=$(< "${SHARED_DIR}/POWERVS_SERVICE_CRN")
+  POWERVS_SERVICE_INSTANCE_ID=$(echo "${POWERVS_CRN}" | sed 's|:| |g' | awk '{print $NF}')
   if [ -f "${SHARED_DIR}/RESOURCE_GROUP" ]
   then
-    RESOURCE_GROUP=$(cat ${SHARED_DIR}/RESOURCE_GROUP)
-    ic resource service-instance-delete ${SERVICE_ID} -g ${RESOURCE_GROUP} --force --recursive
+    # service-instance-delete uses a CRN
+    RESOURCE_GROUP=$(cat "${SHARED_DIR}/RESOURCE_GROUP")
+    ic resource service-instance-delete "${POWERVS_SERVICE_INSTANCE_ID}" -g "${RESOURCE_GROUP}" --force --recursive
   else
-    echo "WARNING- No RESOURCE_GROUP or POWERVS_SERVICE_INSTANCE_ID found, not deleting the workspace"
+    echo "WARNING: No RESOURCE_GROUP or POWERVS_SERVICE_INSTANCE_ID found, not deleting the workspace"
   fi
 fi
 
+# Report the straggler workspaces with tag 'mac-power-worker'
+EXTRA_CRNS=$(ic resource search "tags:\"mac-power-worker\"" --output json | jq -r '.items[].crn')
+echo "Checking the Straggler Workspaces: "
+echo "${EXTRA_CRNS}"
+for T_CRN in ${EXTRA_CRNS//'\n'/ }
+do
+  echo "-Straggler Workspace: ${T_CRN}"
+  ic pi st "${T_CRN}"
+
+  echo "-- Cloud Connection --"
+  ic pi cons --json | jq -r '.cloudConnections[] | .name,.cloudConnectionID'
+
+  echo "-- Network --"
+  ic pi nets
+
+  echo "-- Volumes --"
+  ic pi vols
+
+  echo "-- Instances --"
+  ic pi ins
+
+  echo "-- Gateway --"
+  echo "TIP: only delete your gateway"
+  ic tg gateways
+done
+echo "Done Checking"
 echo "IBM Cloud PowerVS resources destroyed successfully"
--- ci-operator/step-registry/upi/install/heterogeneous/powervs/upi-install-heterogeneous-powervs-commands.sh
+++ ci-operator/step-registry/upi/install/heterogeneous/powervs/upi-install-heterogeneous-powervs-commands.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -o nounset
-set -o pipefail
 
 trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
 
-if [ "${ADDITIONAL_WORKERS}" == "0" ]; then
+if [ "${ADDITIONAL_WORKERS}" == "0" ]
+then
     echo "No additional workers requested"
     exit 0
 fi
@@ -22,14 +22,17 @@ function wait_for_nodes_readiness()
   local expected_nodes=${1}
   local max_retries=${2:-10}
   local period=${3:-5}
-  for i in $(seq 1 "${max_retries}") max; do
-    if [ "${i}" == "max" ]; then
+  for i in $(seq 1 "${max_retries}") max
+  do
+    if [ "${i}" == "max" ]
+    then
       echo "[ERROR] Timeout reached. ${expected_nodes} ready nodes expected, found ${ready_nodes}... Failing."
       return 1
     fi
     sleep "${period}m"
     ready_nodes=$(get_ready_nodes_count)
-    if [ x"${ready_nodes}" == x"${expected_nodes}" ]; then
+    if [ "${ready_nodes}" == "${expected_nodes}" ]
+    then
         echo "[INFO] Found ${ready_nodes}/${expected_nodes} ready nodes, continuing..."
         return 0
     fi
@@ -45,7 +48,8 @@ echo "Cluster type is ${CLUSTER_TYPE}"
 case "$CLUSTER_TYPE" in
 *ibmcloud*)
   # Add code for ppc64le
-  if [ "${ADDITIONAL_WORKER_ARCHITECTURE}" == "ppc64le" ]; then
+  if [ "${ADDITIONAL_WORKER_ARCHITECTURE}" == "ppc64le" ]
+  then
       echo "Adding additional ppc64le nodes"
       REGION="${LEASED_RESOURCE}"
       IBMCLOUD_HOME_FOLDER=/tmp/ibmcloud
@@ -55,7 +59,8 @@ case "$CLUSTER_TYPE" in
 
       PATH=${PATH}:/tmp
       mkdir -p ${IBMCLOUD_HOME_FOLDER}
-      if [ -z "$(command -v ibmcloud)" ]; then
+      if [ -z "$(command -v ibmcloud)" ]
+      then
         echo "ibmcloud CLI doesn't exist, installing"
         curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
       fi
@@ -65,35 +70,41 @@ case "$CLUSTER_TYPE" in
       }
 
       # Check if jq,yq,git and openshift-install are installed
-      if [ -z "$(command -v yq)" ]; then
+      if [ -z "$(command -v yq)" ]
+      then
       	echo "yq is not installed, proceed to installing yq"
-      	curl -L "https://github.com/mikefarah/yq/releases/download/v4.30.5/yq_linux_$(uname -m | sed 's/aarch64/arm64/;s/x86_64/amd64/')" \
+      	curl -L "https://github.com/mikefarah/yq/releases/download/v${YQ_VERSION}/yq_linux_$(uname -m | sed 's/aarch64/arm64/;s/x86_64/amd64/')" \
           -o /tmp/yq && chmod +x /tmp/yq
       fi
 
-      if [ -z "$(command -v jq)" ]; then
+      if [ -z "$(command -v jq)" ]
+      then
         echo "jq is not installed, proceed to installing jq"
-        curl -L "https://github.com/jqlang/jq/releases/download/jq-1.6/jq-linux64" -o /tmp/jq && chmod +x /tmp/jq
+        curl -L "https://github.com/jqlang/jq/releases/download/jq-${JQ_VERSION}/jq-linux64" -o /tmp/jq && chmod +x /tmp/jq
       fi
 
-      if [ -z "$(command -v openshift-install)" ]; then
+      if [ -z "$(command -v openshift-install)" ]
+      then
         echo "openshift-install is not installed, proceed to installing openshift-install"
-        curl -L https://mirror.openshift.com/pub/openshift-v4/multi/clients/ocp/stable/ppc64le/openshift-install-linux.tar.gz -o /tmp/openshift-install && chmod +x /tmp/openshift-install
+        curl -L https://mirror.openshift.com/pub/openshift-v4/multi/clients/ocp/stable/ppc64le/openshift-install-linux.tar.gz \
+          -o /tmp/openshift-install && chmod +x /tmp/openshift-install
       fi
 
-      # upgrade go to 1.20.7 or greater
-      if [ -z "$(command -v go)" ]; then
+      # upgrade go to GO_VERSION
+      if [ -z "$(command -v go)" ]
+      then
         echo "go is not installed, proceed to installing go"
-        cd /tmp && wget -q https://go.dev/dl/go1.20.7.linux-amd64.tar.gz && tar -C /tmp -xzf go1.20.7.linux-amd64.tar.gz \
+        cd /tmp && wget -q https://go.dev/dl/go"${GO_VERSION}".linux-amd64.tar.gz && tar -C /tmp -xzf go"${GO_VERSION}".linux-amd64.tar.gz \
           && export PATH=$PATH:/tmp/go/bin && export GOCACHE=/tmp/go && export GOPATH=/tmp/go && export GOMODCACHE="/tmp/go/pkg/mod"
-        if [ -z "$(command -v go)" ]; then
+        if [ -z "$(command -v go)" ]
+        then
           echo "Installed go successfully"
         fi
       fi
 
-      # build terraform from source v1.5.5
-      cd ${IBMCLOUD_HOME_FOLDER} && curl -L https://github.com/hashicorp/terraform/archive/refs/tags/v1.5.5.tar.gz -o ${IBMCLOUD_HOME_FOLDER}/terraform.tar.gz \
-        && tar -xzf ${IBMCLOUD_HOME_FOLDER}/terraform.tar.gz && cd ${IBMCLOUD_HOME_FOLDER}/terraform-1.5.5 \
+      # build terraform from source using TERRAFORM_VERSION
+      cd "${IBMCLOUD_HOME_FOLDER}" && curl -L https://github.com/hashicorp/terraform/archive/refs/tags/v"${TERRAFORM_VERSION}".tar.gz -o "${IBMCLOUD_HOME_FOLDER}"/terraform.tar.gz \
+        && tar -xzf "${IBMCLOUD_HOME_FOLDER}"/terraform.tar.gz && cd "${IBMCLOUD_HOME_FOLDER}"/terraform-"${TERRAFORM_VERSION}" \
         && go build -ldflags "-w -s -X 'github.com/hashicorp/terraform/version.dev=no'" -o bin/ . && cp bin/terraform /tmp/terraform
       export PATH=$PATH:/tmp
       t_ver1=$(/tmp/terraform -version)
@@ -102,9 +113,9 @@ case "$CLUSTER_TYPE" in
       export PATH
       ic version
       echo "Logging into IBMCLOUD"
-      RESOURCE_GROUP=$(yq -r '.platform.ibmcloud.resourceGroupName' ${SHARED_DIR}/install-config.yaml)
+      RESOURCE_GROUP=$(yq -r '.platform.ibmcloud.resourceGroupName' "${SHARED_DIR}/install-config.yaml")
 
-      ic login --apikey @${CLUSTER_PROFILE_DIR}/ibmcloud-api-key -r ${REGION} -g ${RESOURCE_GROUP}
+      ic login --apikey "@${CLUSTER_PROFILE_DIR}/ibmcloud-api-key" -r "${REGION}" -g "${RESOURCE_GROUP}"
       ic plugin install -f cloud-internet-services vpc-infrastructure cloud-object-storage power-iaas is
 
       # create workspace for powervs from cli
@@ -119,67 +130,107 @@ case "$CLUSTER_TYPE" in
               ("jp-tok") echo "tok04" ;;
               (*) echo "$REGION" ;;
           esac)
-      echo "Region is ${REGION} PowerVS region is ${POWERVS_REGION} Resource Group is ${RESOURCE_GROUP}"
-      SERVICE_INSTANCE_OUTPUT=$(ic resource service-instance-create "${WORKSPACE_NAME}" "${SERVICE_NAME}" "${SERVICE_PLAN_NAME}" "${POWERVS_REGION}" -g "${RESOURCE_GROUP}")
+      echo "Region is ${REGION}"
+      echo "PowerVS region is ${POWERVS_REGION}"
+      echo "Resource Group is ${RESOURCE_GROUP}"
+      ic resource service-instance-create "${WORKSPACE_NAME}" "${SERVICE_NAME}" "${SERVICE_PLAN_NAME}" "${POWERVS_REGION}" -g "${RESOURCE_GROUP}" 2>&1 \
+        | tee /tmp/instance.id
+      
+      # Process the CRN into a variable
+      CRN=$(cat /tmp/instance.id | grep crn | awk '{print $NF}')
+      export CRN
+      echo "${CRN}" > "${SHARED_DIR}"/POWERVS_SERVICE_CRN
+      sleep 30
 
-      SERVICE_INSTANCE_ID=$(echo "$SERVICE_INSTANCE_OUTPUT" | grep -oE 'GUID:[[:space:]]+[^:[:space:]]+' | awk '{print $2}')
+      # Tag the resource for easier deletion
+      ic resource tag-attach --tag-names "mac-power-worker" --resource-id "${CRN}" --tag-type user
 
-      # Copy the service id to shared_dir to use in deprovisioning step
-      echo ${SERVICE_INSTANCE_ID} > ${SHARED_DIR}/POWERVS_SERVICE_INSTANCE_ID
-      cat ${SHARED_DIR}/POWERVS_SERVICE_INSTANCE_ID
+      # Waits for the created instance to become active... after 10 minutes it fails and exists
+      # Example content for TEMP_STATE
+      # active
+      # crn:v1:bluemix:public:power-iaas:osa21:a/3c24cb272ca44aa1ac9f6e9490ac5ecd:6632ebfa-ae9e-4b6c-97cd-c4b28e981c46::
+      COUNTER=0
+      SERVICE_STATE=""
+      while [ -z "${SERVICE_STATE}" ]
+      do
+        COUNTER=$((COUNTER+1)) 
+        TEMP_STATE="$(ic resource service-instances -g "${RESOURCE_GROUP}" --output json --type service_instance  | jq -r '.[] | select(.crn == "'"${CRN}"'") | .state')"
+        echo "Current State is: ${TEMP_STATE}"
+        echo ""
+        if [ "${TEMP_STATE}" == "active" ]
+        then
+          SERVICE_STATE="FOUND"
+        elif [[ $COUNTER -ge 20 ]]
+        then
+          SERVICE_STATE="ERROR"
+          echo "Service has not come up... login and verify"
+          exit 2
+        else
+          echo "Waiting for service to become active... [30 seconds]"
+          sleep 30
+        fi
+      done
 
-      echo ${RESOURCE_GROUP} > ${SHARED_DIR}/RESOURCE_GROUP
-      echo ${OCP_VERSION} > ${SHARED_DIR}/OCP_VERSION
+      echo "SERVICE_STATE: ${SERVICE_STATE}"
+
+      # This CRN is useful when manually destroying.
+      echo "PowerVS Service CRN: ${CRN}"
+
+      echo "${RESOURCE_GROUP}" > "${SHARED_DIR}"/RESOURCE_GROUP
+      echo "${OCP_VERSION}" > "${SHARED_DIR}"/OCP_VERSION
 
       # After the workspace is created, invoke the automation code
-      cd ${IBMCLOUD_HOME_FOLDER} \
-        && curl -L https://github.com/IBM/ocp4-upi-compute-powervs/archive/refs/heads/release-${OCP_VERSION}.tar.gz -o ${IBMCLOUD_HOME_FOLDER}/ocp-${OCP_VERSION}.tar.gz \
-        && tar -xzf ${IBMCLOUD_HOME_FOLDER}/ocp-${OCP_VERSION}.tar.gz \
-        && mv ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs-release-${OCP_VERSION} ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs
+      cd "${IBMCLOUD_HOME_FOLDER}" \
+        && curl -L https://github.com/IBM/ocp4-upi-compute-powervs/archive/refs/heads/release-"${OCP_VERSION}".tar.gz -o "${IBMCLOUD_HOME_FOLDER}"/ocp-"${OCP_VERSION}".tar.gz \
+        && tar -xzf "${IBMCLOUD_HOME_FOLDER}"/ocp-"${OCP_VERSION}".tar.gz \
+        && mv "${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs-release-${OCP_VERSION}" "${IBMCLOUD_HOME_FOLDER}"/ocp4-upi-compute-powervs
 
+      # The CentOS-Stream-8 image is stock-image on PowerVS.
+      # This image is available across all PowerVS workspaces.
+      # The VMs created using this image are used in support of ignition on PowerVS.
       echo "Creating the Centos Stream Image"
-      ic pi sl --json | jq -r '.[] | select(.Name == "'${WORKSPACE_NAME}'").CRN'  
-      ic pi st "$(ic pi sl --json | jq -r '.[] | select(.Name == "'${WORKSPACE_NAME}'").CRN')"
-      ic pi image-create CentOS-Stream-8
+      echo "PowerVS Target CRN is: ${CRN}"
+      ic pi st "${CRN}"
+      ic pi images
+      ic pi image-create CentOS-Stream-8 --json
       echo "Import image status is: $?"
 
       # Set the values to be used for generating var.tfvars
+      POWERVS_SERVICE_INSTANCE_ID=$(echo "${CRN}" | sed 's|:| |g' | awk '{print $NF}')
+      export POWERVS_SERVICE_INSTANCE_ID
       IC_API_KEY="$(< "${CLUSTER_PROFILE_DIR}/ibmcloud-api-key")"
       export IC_API_KEY
-      export PRIVATE_KEY_FILE=${CLUSTER_PROFILE_DIR}/ssh-privatekey
-      export PUBLIC_KEY_FILE=${CLUSTER_PROFILE_DIR}/ssh-publickey
-      export POWERVS_SERVICE_INSTANCE_ID=${SERVICE_INSTANCE_ID}
+      export PRIVATE_KEY_FILE="${CLUSTER_PROFILE_DIR}"/ssh-privatekey
+      export PUBLIC_KEY_FILE="${CLUSTER_PROFILE_DIR}"/ssh-publickey
       export INSTALL_CONFIG_FILE=${SHARED_DIR}/install-config.yaml
       export KUBECONFIG=${SHARED_DIR}/kubeconfig
 
       # Invoke create_var_file.sh to generate var.tfvars file
       echo "Creating the var file"
       cd ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs \
-        && bash scripts/create-var-file.sh /tmp/ibmcloud "${EXPECTED_NODES}"
+        && bash scripts/create-var-file.sh /tmp/ibmcloud "${ADDITIONAL_WORKERS}"
 
       # TODO:MAC check if the var.tfvars file is populated
-      VARFILE_OUTPUT=$(cat ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs/data/var.tfvars)
+      VARFILE_OUTPUT=$(cat "${IBMCLOUD_HOME_FOLDER}"/ocp4-upi-compute-powervs/data/var.tfvars)
       echo "varfile_output is ${VARFILE_OUTPUT}"
 
-      # copy the var.tfvars file and the POWERVS_SERVICE_INSTANCE_ID to ${SHARED_DIR} so that it can be used to destroy the
+      # copy the var.tfvars file and the POWERVS_SERVICE_CRN to ${SHARED_DIR} so that it can be used to destroy the
       # created resources. The FAILED_DEPLOY flag is only exported on a fail
-      cp ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs/data/var.tfvars ${SHARED_DIR}/var.tfvars
-      cd ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs/ \
-        && /tmp/terraform init -upgrade \
+      cp "${IBMCLOUD_HOME_FOLDER}"/ocp4-upi-compute-powervs/data/var.tfvars "${SHARED_DIR}"/var.tfvars
+      cd "${IBMCLOUD_HOME_FOLDER}"/ocp4-upi-compute-powervs/ \
+        && /tmp/terraform init -upgrade -no-color \
         && /tmp/terraform plan -var-file=data/var.tfvars -no-color \
         && /tmp/terraform apply -var-file=data/var.tfvars -auto-approve -no-color \
         || export FAILED_DEPLOY="true"
-      
+
       echo "Shared Directory: copy the terraform.tfstate"
-      cp ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs/terraform.tfstate ${SHARED_DIR}/terraform.tfstate
+      cp "${IBMCLOUD_HOME_FOLDER}"/ocp4-upi-compute-powervs/terraform.tfstate "${SHARED_DIR}"/terraform.tfstate
 
       # If the deploy fails, destroy right away and hard exit
-      if [ ! -z "${FAILED_DEPLOY}" ]
+      if [ -n "${FAILED_DEPLOY}" ]
       then
-        # sleep and then try again to clean any failure up.
-        cd ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs/ \
-          && /tmp/terraform apply -var-file=data/var.tfvars -auto-approve -no-color \
-          || sleep 120 && /tmp/terraform apply -var-file=data/var.tfvars -auto-approve -no-color
+        cd "${IBMCLOUD_HOME_FOLDER}"/ocp4-upi-compute-powervs/ \
+          && /tmp/terraform apply -var-file=data/var.tfvars -auto-approve -no-color
         echo "Failed to deploy... hard exit"
         exit 1
       fi
@@ -193,7 +244,8 @@ esac
 echo "Wait for the nodes to become ready..."
 wait_for_nodes_readiness ${EXPECTED_NODES}
 ret="$?"
-if [ "${ret}" != "0" ]; then
+if [ "${ret}" != "0" ]
+then
   echo "Some errors occurred, exiting with ${ret}."
   exit "${ret}"
 fi
