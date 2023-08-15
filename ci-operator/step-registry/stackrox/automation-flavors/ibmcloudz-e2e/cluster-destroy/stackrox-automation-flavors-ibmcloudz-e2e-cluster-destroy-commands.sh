b26ad5b038e5ea13e38390799a8bc388c79b01a1
Dale Fu <35507428+dale-fu@users.noreply.github.com>
2023-08-15 11:27:09 -0400

    Clean up and fix IBM Z OCP deployment (#42320)

--- ci-operator/step-registry/stackrox/automation-flavors/ibmcloudz-e2e/cluster-destroy/stackrox-automation-flavors-ibmcloudz-e2e-cluster-destroy-commands.sh
+++ ci-operator/step-registry/stackrox/automation-flavors/ibmcloudz-e2e/cluster-destroy/stackrox-automation-flavors-ibmcloudz-e2e-cluster-destroy-commands.sh
@@ -3,20 +3,17 @@
 set -o errexit
 set -o pipefail
 
+cd /tmp
+pwd
+
 SECRET_DIR="/tmp/vault/ibmcloudz-rhr-creds"
 PRIVATE_KEY_FILE="${SECRET_DIR}/PRIVATE_KEY_FILE"
 SSH_KEY_PATH="/tmp/id_rsa"
 SSH_ARGS="-i ${SSH_KEY_PATH} -o MACs=hmac-sha2-256 -o StrictHostKeyChecking=no -o LogLevel=ERROR -o UserKnownHostsFile=/dev/null"
-
-# setup ssh key
 cp -f $PRIVATE_KEY_FILE $SSH_KEY_PATH
 chmod 400 $SSH_KEY_PATH
 
-# delete k8s cluster. k8smanager script runs on an intermediate node [IP 163.66.94.115] on IBM cloud
-# KUBECONFIG_FILE="${SHARED_DIR}/kubeconfig"
-# SSH_CMD="/home/ubuntu/stackrox-ci/k8smanager delete"
-# ssh $SSH_ARGS root@163.66.94.115 "$SSH_CMD" > $KUBECONFIG_FILE
-
-# delete OCP cluster. ocpmanager script runs on an intermediate node [IP 163.74.90.40] on IBM cloud
+# The ocpmanager script creates/deletes OCP clusters on IBM Z via IBM Cloud [https://github.ibm.com/Ganesh-Bhure2/stackrox-ci]
+# The script runs on an intermediate node [IP 163.74.90.40] on IBM Cloud
 SSH_CMD="/root/ocpmanager delete"
-ssh $SSH_ARGS root@163.74.90.40 "$SSH_CMD"
+ssh $SSH_ARGS root@163.74.90.40 "export BUILD_ID=$BUILD_ID && $SSH_CMD"
