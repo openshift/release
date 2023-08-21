8a6b3606b71f88f059c65e7fd3ffcac346144cb9
Ori Haran <oharan@redhat.com>
2023-08-21 13:41:13 +0300

    Aligned execute-tests file

--- ci-operator/step-registry/camel-quarkus/execute-tests/camel-quarkus-execute-tests-commands.sh
+++ ci-operator/step-registry/camel-quarkus/execute-tests/camel-quarkus-execute-tests-commands.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#TODO check execution
-sleep 4h
-
 set -o nounset
 set -o errexit
 set -o pipefail
@@ -16,21 +13,16 @@ OCP_CRED_USR="kubeadmin"
 OCP_CRED_PSW="$(cat ${SHARED_DIR}/kubeadmin-password)"
 oc login ${OCP_API_URL} --username=${OCP_CRED_USR} --password=${OCP_CRED_PSW} --insecure-skip-tls-verify=true
 
-# source sdkman
-set +o nounset #disable strict mode
-source "/root/.sdkman/bin/sdkman-init.sh"
-set -o nounset #enable back strict mode
-
-# export maven env variable
-# export _JAVA_OPTIONS=-Duser.home=$HOME
+# Execute tests
+sh run.sh
 
 # Collect test reports into $ARTIFACT_DIR
 echo "Copying results and xmls to ${ARTIFACT_DIR}"
-PROJECTS=config,lifecycle-application,http/http-minimum,http/http-minimum-reactive,sql-db/sql-app,monitoring/microprofile-opentracing
 for PROJECT in ${PROJECTS//","/" "}; do
   for FILE in ./$PROJECT/target/failsafe-reports/TEST-*.xml; do
     FILENAME=$(basename $FILE)
-    echo $FILENAME
-    cp $FILE ${ARTIFACT_DIR}/junit_${FILENAME}
+    TARGET=${ARTIFACT_DIR}/junit_${FILENAME}
+    echo "Collecting ${TARGET}"
+    cp $FILE $TARGET
   done
 done
