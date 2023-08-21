9375802d82fe2d9f684f6ffa43130eda2b56495e
Ori Haran <oharan@redhat.com>
2023-08-21 13:38:18 +0300

    Revert "Aligned execute-tests file"
    This reverts commit 4e00e6d6a1932aec74c7f88099cff121094b4f82.

--- ci-operator/step-registry/camel-quarkus/execute-tests/camel-quarkus-execute-tests-commands.sh
+++ ci-operator/step-registry/camel-quarkus/execute-tests/camel-quarkus-execute-tests-commands.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#TODO check execution
+sleep 4h
+
 set -o nounset
 set -o errexit
 set -o pipefail
@@ -13,16 +16,21 @@ OCP_CRED_USR="kubeadmin"
 OCP_CRED_PSW="$(cat ${SHARED_DIR}/kubeadmin-password)"
 oc login ${OCP_API_URL} --username=${OCP_CRED_USR} --password=${OCP_CRED_PSW} --insecure-skip-tls-verify=true
 
-# Execute tests
-sh run.sh
+# source sdkman
+set +o nounset #disable strict mode
+source "/root/.sdkman/bin/sdkman-init.sh"
+set -o nounset #enable back strict mode
+
+# export maven env variable
+# export _JAVA_OPTIONS=-Duser.home=$HOME
 
 # Collect test reports into $ARTIFACT_DIR
 echo "Copying results and xmls to ${ARTIFACT_DIR}"
+PROJECTS=config,lifecycle-application,http/http-minimum,http/http-minimum-reactive,sql-db/sql-app,monitoring/microprofile-opentracing
 for PROJECT in ${PROJECTS//","/" "}; do
   for FILE in ./$PROJECT/target/failsafe-reports/TEST-*.xml; do
     FILENAME=$(basename $FILE)
-    TARGET=${ARTIFACT_DIR}/junit_${FILENAME}
-    echo "Collecting ${TARGET}"
-    cp $FILE $TARGET
+    echo $FILENAME
+    cp $FILE ${ARTIFACT_DIR}/junit_${FILENAME}
   done
 done
