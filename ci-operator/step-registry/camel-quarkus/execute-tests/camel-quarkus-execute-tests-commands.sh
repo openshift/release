20ee4805a94754f9fd70ba207c139932b8087597
Ori Haran <oharan@redhat.com>
2023-08-21 17:06:32 +0300

    REVERT Split execution ref and result collect ref

--- ci-operator/step-registry/camel-quarkus/execute-tests/camel-quarkus-execute-tests-commands.sh
+++ ci-operator/step-registry/camel-quarkus/execute-tests/camel-quarkus-execute-tests-commands.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
-# set -o nounset
-# set -o errexit
-# set -o pipefail
+set -o nounset
+set -o errexit
+set -o pipefail
 
-# shopt -s nullglob
+shopt -s nullglob
 
-## Set the ocp env variables and execute oc login
-#CONSOLE_URL=$(cat $SHARED_DIR/console.url)
-#OCP_API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
-#OCP_CRED_USR="kubeadmin"
-#OCP_CRED_PSW="$(cat ${SHARED_DIR}/kubeadmin-password)"
-#oc login ${OCP_API_URL} --username=${OCP_CRED_USR} --password=${OCP_CRED_PSW} --insecure-skip-tls-verify=true
-#
-## Execute tests
-#sh run.sh
-#
-## Collect test reports into $ARTIFACT_DIR
-#echo "Copying results and xmls to ${ARTIFACT_DIR}"
-#for PROJECT in ${PROJECTS//","/" "}; do
-#  for FILE in ./$PROJECT/target/failsafe-reports/TEST-*.xml; do
-#    FILENAME=$(basename $FILE)
-#    TARGET=${ARTIFACT_DIR}/junit_${FILENAME}
-#    echo "Collecting ${TARGET}"
-#    cp $FILE $TARGET
-#  done
-#done
+# Collect test reports into $ARTIFACT_DIR
+PROJECTS=$PROJECTS
+
+echo "Copying results and xmls to ${ARTIFACT_DIR}"
+for PROJECT in ${PROJECTS//","/" "}; do
+  for FILE in ./$PROJECT/target/failsafe-reports/TEST-*.xml; do
+    FILENAME=$(basename $FILE)
+    TARGET=${ARTIFACT_DIR}/junit_${FILENAME}
+    echo "Collecting ${TARGET}"
+    cp $FILE $TARGET
+  done
+done
