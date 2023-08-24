050528b9c22a1cddd8822c717eba1054a140ac9a
Vibhu Prashar <vibhu.sharma2929@gmail.com>
2023-08-24 16:28:24 +0530

    add step-registry for kepler-operator
    Signed-off-by: Vibhu Prashar <vibhu.sharma2929@gmail.com>

---
+++ ci-operator/step-registry/openshift-power-monitoring/install/kepler/openshift-power-monitoring-install-kepler-commands.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+validate_cluster() {
+    local label="operators.coreos.com/kepler-operator.openshift-operators"
+    local ret=0
+    echo "Validating cluster"
+    oc get subscriptions -l "$label" -n openshift-operators || {
+        echo "Missing subscription. Something wrong with Operator Installation"
+        ret=1
+    }
+    oc get crds | grep kepler || {
+        echo "Missing Kepler CRD. Something wrong with Operator Installation"
+        ret=1
+    }
+    oc get clusterrole,clusterrolebindings -l "$label" -A || {
+        echo "Missing clusterrole/clusterrolebindings. Something wrong with Operator Installation"
+        ret=1
+    }
+    return $ret
+}
+deploy_kepler() {
+    cat <<EOF | oc apply -f -
+    apiVersion: kepler.system.sustainable.computing.io/v1alpha1
+    kind: Kepler
+    metadata:
+        labels:
+            app.kubernetes.io/name: kepler
+            app.kubernetes.io/instance: kepler
+            app.kubernetes.io/part-of: kepler-operator
+        name: kepler
+    spec:
+    exporter:
+        port: 9103
+EOF
+
+}
+main() {
+    validate_cluster || {
+        return 1
+    }
+    deploy_kepler || {
+        return 1
+    }
+}
+main "$@"
