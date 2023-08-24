c82f66e86ead4cee5d8ce73715c2402487bd3fd3
Ori Haran <oharan@redhat.com>
2023-08-24 11:53:18 +0300

    Add source sdkman_2

--- ci-operator/step-registry/camel-quarkus/execute-tests/camel-quarkus-execute-tests-commands.sh
+++ ci-operator/step-registry/camel-quarkus/execute-tests/camel-quarkus-execute-tests-commands.sh
@@ -6,22 +6,19 @@ set -o pipefail
 
 shopt -s nullglob
 
-# Collect test reports into $ARTIFACT_DIR
-PROJECTS=$PROJECTS
+echo "Logging to oc:"
+sh ./oc_login.sh
 
 # source sdkman
 set +o nounset #disable strict mode
 source "/root/.sdkman/bin/sdkman-init.sh"
 set -o nounset #enable back strict mode
 
-sleep 1h
-echo "'mvn' version:"
-mvn --version
-echo "Logging to oc:"
-sh ./oc_login.sh
-source $SDKMAN_DIR/bin/sdkman-init.sh
 echo "Executing tests:"
 sh ./run.sh
+
+# Collect test reports into $ARTIFACT_DIR
+PROJECTS=$PROJECTS
 
 echo "Copying results and xmls to ${ARTIFACT_DIR}"
 for PROJECT in ${PROJECTS//","/" "}; do
