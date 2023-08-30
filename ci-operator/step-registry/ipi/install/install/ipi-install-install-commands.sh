3de0eadce694c6d81db74497f043d649bb742a62
jiwei <jiwei@redhat.com>
2023-08-30 17:41:19 +0800

    [4.14] gcp userLabels & userTags

--- ci-operator/step-registry/ipi/install/install/ipi-install-install-commands.sh
+++ ci-operator/step-registry/ipi/install/install/ipi-install-install-commands.sh
@@ -532,6 +532,9 @@ gcp)
     if [ -f "${SHARED_DIR}/gcp_min_permissions.json" ]; then
       echo "$(date -u --rfc-3339=seconds) - Using the IAM service account for the minimum permissions testing on GCP..."
       export GOOGLE_CLOUD_KEYFILE_JSON="${SHARED_DIR}/gcp_min_permissions.json"
+    elif [ -f "${SHARED_DIR}/user_tags_sa.json" ]; then
+      echo "$(date -u --rfc-3339=seconds) - Using the IAM service account for the userTags testing on GCP..."
+      export GOOGLE_CLOUD_KEYFILE_JSON="${SHARED_DIR}/user_tags_sa.json"
     fi
     ;;
 ibmcloud*)
