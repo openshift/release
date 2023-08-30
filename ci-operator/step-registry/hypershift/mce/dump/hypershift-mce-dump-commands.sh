efb2f2b0b5838a09f7dd049643defc47eeeba973
David Huynh <dhuynh@redhat.com>
2023-08-30 16:21:47 -0700

    update binary name for hcp for mce 2.4
    Signed-off-by: David Huynh <dhuynh@redhat.com>

--- ci-operator/step-registry/hypershift/mce/dump/hypershift-mce-dump-commands.sh
+++ ci-operator/step-registry/hypershift/mce/dump/hypershift-mce-dump-commands.sh
@@ -6,11 +6,18 @@ if [ -f "${SHARED_DIR}/proxy-conf.sh" ] ; then
   source "${SHARED_DIR}/proxy-conf.sh"
 fi
 
+MCE_VERSION=$(oc get $(oc get multiclusterengines -oname) -ojsonpath="{.status.currentVersion}" | cut -c 1-3)
+HYPERSHIFT_NAME=hcp
+if (( $(echo "$MCE_VERSION < 2.4" | bc -l) )); then
+  echo "MCE version is less than 2.4"
+  HYPERSHIFT_NAME=hypershift
+fi
+
 arch=$(arch)
 if [ "$arch" == "x86_64" ]; then
-  downURL=$(oc get ConsoleCLIDownload hypershift-cli-download -o json | jq -r '.spec.links[] | select(.text | test("Linux for x86_64")).href') && curl -k --output /tmp/hypershift.tar.gz ${downURL}
-  cd /tmp && tar -xvf /tmp/hypershift.tar.gz
-  chmod +x /tmp/hypershift
+  downURL=$(oc get ConsoleCLIDownload ${HYPERSHIFT_NAME}-cli-download -o json | jq -r '.spec.links[] | select(.text | test("Linux for x86_64")).href') && curl -k --output /tmp/${HYPERSHIFT_NAME}.tar.gz ${downURL}
+  cd /tmp && tar -xvf /tmp/${HYPERSHIFT_NAME}.tar.gz
+  chmod +x /tmp/${HYPERSHIFT_NAME}
   cd -
 fi
 
