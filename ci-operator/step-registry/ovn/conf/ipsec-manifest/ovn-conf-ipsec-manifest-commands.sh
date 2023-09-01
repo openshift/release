6bde882c8a61d49e9d8c35c3c087990dc5e40fc0
Yuval Kashtan <yuvalkashtan@gmail.com>
2023-09-01 04:04:42 +0300

    ovn-ipsec: WIP add debug info

--- ci-operator/step-registry/ovn/conf/ipsec-manifest/ovn-conf-ipsec-manifest-commands.sh
+++ ci-operator/step-registry/ovn/conf/ipsec-manifest/ovn-conf-ipsec-manifest-commands.sh
@@ -24,9 +24,11 @@ spec:
       ipsecConfig: {}
 EOF
 
+set -x
 # additional os extension for 4.14+
 cp "${CLUSTER_PROFILE_DIR}/pull-secret" /tmp/pull-secret
 oc registry login --to /tmp/pull-secret
+echo $RELEASE_IMAGE_LATEST
 ocp_version=$(oc adm release info --registry-config /tmp/pull-secret "${RELEASE_IMAGE_LATEST}" --output=json | jq -r '.metadata.version' | cut -d. -f 1,2)
 ocp_major_version=$( echo "${ocp_version}" | awk --field-separator=. '{print $1}' )
 ocp_minor_version=$( echo "${ocp_version}" | awk --field-separator=. '{print $2}' )
