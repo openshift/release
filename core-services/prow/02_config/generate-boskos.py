08d75b24e1e57749fbcc87a6660f58fa6f3c711b
Joseph Callen <jcallen@redhat.com>
2023-09-06 09:53:35 -0400

    vSphere 8 to vlan configuration

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -333,10 +333,8 @@ for i in range(223,229):
 for i in range(151,158):
     CONFIG['vsphere-multizone-quota-slice']['ci-segment-{}'.format(i)] = 1
 
-for i in range(200,204):
-    CONFIG['vsphere-8-quota-slice']['ci-segment-{}'.format(i)] = 1
-for i in range(205,214):
-    CONFIG['vsphere-8-quota-slice']['ci-segment-{}'.format(i)] = 1
+for i in [1256,1283,1153,1263,758,1211,1265,1285,1179,902,1252,1305,1309,1261,1262]:
+    CONFIG['vsphere-8-quota-slice']['ci-vlan-{}'.format(i)] = 1
 for i in [1148,1197,1207,1225,1227,1229,1232,1233,1234,1235,1237,1238,1240,1243,1246,1249,1254,1255,1260,1271,1272,1274,1279,1284,1287,1289,1296,1298,1300,1302,956]:
     CONFIG['vsphere-8-vpn-quota-slice']['ci-vlan-{}'.format(i)] = 1
 
