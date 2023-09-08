28ba6385b238dc2781f699983059f9b410abef1c
Stephen Benjamin <stephen@bitbin.de>
2023-09-08 10:49:32 -0400

    Revert "RELEASE_IMAGE_INITIAL take higher priority than RELEASE_IMAGE_LATEST to meet minor upgrade cases (#43049)" (#43157)
    This reverts commit f6e201e7cd52018f31785e065a9f13eca8265fed.

--- ci-operator/step-registry/ipi/conf/gcp/oidc-creds-provision/ipi-conf-gcp-oidc-creds-provision-commands.sh
+++ ci-operator/step-registry/ipi/conf/gcp/oidc-creds-provision/ipi-conf-gcp-oidc-creds-provision-commands.sh
@@ -16,33 +16,8 @@ export GCP_SHARED_CREDENTIALS_FILE=${CLUSTER_PROFILE_DIR}/gce.json
 export GOOGLE_APPLICATION_CREDENTIALS="${GCP_SHARED_CREDENTIALS_FILE}"
 PROJECT="$(< ${CLUSTER_PROFILE_DIR}/openshift_gcp_project)"
 
-# release-controller always expose RELEASE_IMAGE_LATEST when job configuraiton defines release:latest image
-echo "RELEASE_IMAGE_LATEST: ${RELEASE_IMAGE_LATEST:-}"
-# RELEASE_IMAGE_LATEST_FROM_BUILD_FARM is pointed to the same image as RELEASE_IMAGE_LATEST, 
-# but for some ci jobs triggerred by remote api, RELEASE_IMAGE_LATEST might be overridden with 
-# user specified image pullspec, to avoid auth error when accessing it, always use build farm 
-# registry pullspec.
+echo "RELEASE_IMAGE_LATEST: ${RELEASE_IMAGE_LATEST}"
 echo "RELEASE_IMAGE_LATEST_FROM_BUILD_FARM: ${RELEASE_IMAGE_LATEST_FROM_BUILD_FARM}"
-# seem like release-controller does not expose RELEASE_IMAGE_INITIAL, even job configuraiton defines 
-# release:initial image, once that, use 'oc get istag release:inital' to workaround it.
-echo "RELEASE_IMAGE_INITIAL: ${RELEASE_IMAGE_INITIAL:-}"
-if [[ -n ${RELEASE_IMAGE_INITIAL:-} ]]; then
-    tmp_release_image_initial=${RELEASE_IMAGE_INITIAL}
-    echo "Getting inital release image from RELEASE_IMAGE_INITIAL..."
-elif oc get istag "release:initial" -n ${NAMESPACE} &>/dev/null; then
-    tmp_release_image_initial=$(oc -n ${NAMESPACE} get istag "release:initial" -o jsonpath='{.tag.from.name}')
-    echo "Getting inital release image from build farm imagestream: ${tmp_release_image_initial}"
-fi
-# For some ci upgrade job (stable N -> nightly N+1), RELEASE_IMAGE_INITIAL and 
-# RELEASE_IMAGE_LATEST are pointed to different imgaes, RELEASE_IMAGE_INITIAL has 
-# higher priority than RELEASE_IMAGE_LATEST
-TESTING_RELEASE_IMAGE=""
-if [[ -n ${tmp_release_image_initial:-} ]]; then
-    TESTING_RELEASE_IMAGE=${tmp_release_image_initial}
-else
-    TESTING_RELEASE_IMAGE=${RELEASE_IMAGE_LATEST_FROM_BUILD_FARM}
-fi
-echo "TESTING_RELEASE_IMAGE: ${TESTING_RELEASE_IMAGE}"
 
 echo "> Extract gcp credentials requests from the release image"
 oc registry login
@@ -54,7 +29,7 @@ echo "OC Version:"
 which oc
 oc version --client
 oc adm release extract --help
-oc adm release extract --credentials-requests --cloud=gcp --to="/tmp/credrequests" ${ADDITIONAL_OC_EXTRACT_ARGS} "${TESTING_RELEASE_IMAGE}"
+oc adm release extract --credentials-requests --cloud=gcp --to="/tmp/credrequests" ${ADDITIONAL_OC_EXTRACT_ARGS} "${RELEASE_IMAGE_LATEST_FROM_BUILD_FARM}"
 
 echo "> Output gcp credentials requests to directory: /tmp/credrequests"
 ls "/tmp/credrequests"
--- ci-operator/step-registry/ipi/conf/vsphere/template/ipi-conf-vsphere-template-commands.sh
+++ ci-operator/step-registry/ipi/conf/vsphere/template/ipi-conf-vsphere-template-commands.sh
@@ -12,6 +12,18 @@ declare vsphere_cluster
 source "${SHARED_DIR}/vsphere_context.sh"
 # shellcheck source=/dev/null
 source "${SHARED_DIR}/govc.sh"
+export HOME="${HOME:-/tmp/home}"
+export XDG_RUNTIME_DIR="${HOME}/run"
+export REGISTRY_AUTH_PREFERENCE=podman # TODO: remove later, used for migrating oc from docker to podman
+mkdir -p "${XDG_RUNTIME_DIR}"
+# After cluster is set up, ci-operator make KUBECONFIG pointing to the installed cluster,
+# to make "oc registry login" interact with the build farm, set KUBECONFIG to empty,
+# so that the credentials of the build farm registry can be saved in docker client config file.
+# A direct connection is required while communicating with build-farm, instead of through proxy
+KUBECONFIG="" oc registry login
+
+VERSION=$(oc adm release info ${RELEASE_IMAGE_LATEST_FROM_BUILD_FARM} --output=json | jq -r '.metadata.version' | cut -d. -f 1,2)
+echo "OCP Version: $VERSION"
 
 if [ -z "${RHCOS_VM_TEMPLATE}" ]; then
   installer_bin=$(which openshift-install)
@@ -19,7 +31,7 @@ if [ -z "${RHCOS_VM_TEMPLATE}" ]; then
   echo "${ova_url}" > "${SHARED_DIR}"/ova_url.txt
   vm_template="${ova_url##*/}"
 
-  # select a hardware version for testing
+# select a hardware version for testing
   vsphere_version=$(govc about -json | jq -r .About.Version | awk -F'.' '{print $1}')
   hw_versions=(15 17 18 19)
   if [[ ${vsphere_version} -eq 8 ]]; then
@@ -35,7 +47,9 @@ else
   vm_template="${RHCOS_VM_TEMPLATE}"
 fi
 
-cat >> "${SHARED_DIR}"/enable_template_content.txt << EOF
+Z_VERSION=$(echo ${VERSION} | cut -d'.' -f2)
+if [ ${Z_VERSION} -gt 13 ]; then
+    cat >> "${SHARED_DIR}"/enable_template_content.txt << EOF
     failureDomains:
     - name: generated-failure-domain
       region: generated-region
@@ -49,3 +63,7 @@ cat >> "${SHARED_DIR}"/enable_template_content.txt << EOF
         - ${LEASED_RESOURCE}
       zone: generated-zone
 EOF
+else
+    echo "$VERSION does not support template setting"
+    exit 1    
+fi
