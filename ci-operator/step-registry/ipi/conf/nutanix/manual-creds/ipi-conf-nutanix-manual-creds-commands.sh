39ab1bda41ad0a5ffaf1b32138a8719c9d098c35
Stephen Benjamin <stephen@bitbin.de>
2023-09-08 10:39:32 -0400

    Revert "RELEASE_IMAGE_INITIAL take higher priority than RELEASE_IMAGE_LATEST to meet minor upgrade cases (#43049)"
    This reverts commit f6e201e7cd52018f31785e065a9f13eca8265fed.

--- ci-operator/step-registry/ipi/conf/nutanix/manual-creds/ipi-conf-nutanix-manual-creds-commands.sh
+++ ci-operator/step-registry/ipi/conf/nutanix/manual-creds/ipi-conf-nutanix-manual-creds-commands.sh
@@ -27,34 +27,8 @@ credentials:
     prismElements: null
 EOF
 
-# release-controller always expose RELEASE_IMAGE_LATEST when job configuraiton defines release:latest image
-echo "RELEASE_IMAGE_LATEST: ${RELEASE_IMAGE_LATEST:-}"
-# RELEASE_IMAGE_LATEST_FROM_BUILD_FARM is pointed to the same image as RELEASE_IMAGE_LATEST, 
-# but for some ci jobs triggerred by remote api, RELEASE_IMAGE_LATEST might be overridden with 
-# user specified image pullspec, to avoid auth error when accessing it, always use build farm 
-# registry pullspec.
+echo "RELEASE_IMAGE_LATEST: ${RELEASE_IMAGE_LATEST}"
 echo "RELEASE_IMAGE_LATEST_FROM_BUILD_FARM: ${RELEASE_IMAGE_LATEST_FROM_BUILD_FARM}"
-# seem like release-controller does not expose RELEASE_IMAGE_INITIAL, even job configuraiton defines 
-# release:initial image, once that, use 'oc get istag release:inital' to workaround it.
-echo "RELEASE_IMAGE_INITIAL: ${RELEASE_IMAGE_INITIAL:-}"
-if [[ -n ${RELEASE_IMAGE_INITIAL:-} ]]; then
-    tmp_release_image_initial=${RELEASE_IMAGE_INITIAL}
-    echo "Getting inital release image from RELEASE_IMAGE_INITIAL..."
-elif oc get istag "release:initial" -n ${NAMESPACE} &>/dev/null; then
-    tmp_release_image_initial=$(oc -n ${NAMESPACE} get istag "release:initial" -o jsonpath='{.tag.from.name}')
-    echo "Getting inital release image from build farm imagestream: ${tmp_release_image_initial}"
-fi
-# For some ci upgrade job (stable N -> nightly N+1), RELEASE_IMAGE_INITIAL and 
-# RELEASE_IMAGE_LATEST are pointed to different imgaes, RELEASE_IMAGE_INITIAL has 
-# higher priority than RELEASE_IMAGE_LATEST
-TESTING_RELEASE_IMAGE=""
-if [[ -n ${tmp_release_image_initial:-} ]]; then
-    TESTING_RELEASE_IMAGE=${tmp_release_image_initial}
-else
-    TESTING_RELEASE_IMAGE=${RELEASE_IMAGE_LATEST_FROM_BUILD_FARM}
-fi
-echo "TESTING_RELEASE_IMAGE: ${TESTING_RELEASE_IMAGE}"
-
 
 # Extract credential requests
 oc registry login
@@ -66,7 +40,7 @@ echo "OC Version:"
 which oc
 oc version --client
 oc adm release extract --help
-oc adm release extract --credentials-requests --cloud=nutanix --to "${CR_DIR}" ${ADDITIONAL_OC_EXTRACT_ARGS} "${TESTING_RELEASE_IMAGE}"
+oc adm release extract --credentials-requests --cloud=nutanix --to "${CR_DIR}" ${ADDITIONAL_OC_EXTRACT_ARGS} "${RELEASE_IMAGE_LATEST_FROM_BUILD_FARM}"
 
 echo "Extracted credentials requests:"
 ls -l "${CR_DIR}"
