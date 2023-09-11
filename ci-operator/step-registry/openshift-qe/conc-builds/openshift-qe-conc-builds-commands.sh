6b3cb16562de7567a07983d3483852965644ac6c
“Lena <lhorsley@redhat.com>
2023-09-11 08:37:39 -0400

    updates to test files

--- ci-operator/step-registry/openshift-qe/conc-builds/openshift-qe-conc-builds-commands.sh
+++ ci-operator/step-registry/openshift-qe/conc-builds/openshift-qe-conc-builds-commands.sh
@@ -15,7 +15,7 @@ ES_PASSWORD=$(cat "/secret/password")
 ES_USERNAME=$(cat "/secret/username")
 
 git clone https://github.com/lenahorsley/e2e-benchmarking/--depth=1
-pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
+pushd e2e-benchmarking/workloads/kube-burner
 export WORKLOAD=conc-builds
 
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
