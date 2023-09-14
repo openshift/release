2c7dcecf4361a7e57a0b8768d8d9151ad230a65c
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-09-14 09:41:28 +0200

    trigger rebase jobs

--- ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
+++ ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [[ "$JOB_NAME" == rehearse* ]]; then
-    echo "INFO: \$JOB_NAME starts with rehearse - running in DRY RUN mode"
-    export DRY_RUN=y
-fi
+# if [[ "$JOB_NAME" == rehearse* ]]; then
+#     echo "INFO: \$JOB_NAME starts with rehearse - running in DRY RUN mode"
+#     export DRY_RUN=y
+# fi
+
+sed -i '11,15d' ./scripts/auto-rebase/rebase_job_entrypoint.sh
 
 ./scripts/auto-rebase/rebase_job_entrypoint.sh
