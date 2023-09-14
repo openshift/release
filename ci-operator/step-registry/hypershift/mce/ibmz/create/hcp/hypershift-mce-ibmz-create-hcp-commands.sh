6028610c1074c0adff5af2461f8cdcb9c79ee0bb
Phani <phaneendra.ravuri@ibm.com>
2023-09-14 20:25:32 +0530

    Changed the logic to download hypershift cli using mce version

--- ci-operator/step-registry/hypershift/mce/ibmz/create/hcp/hypershift-mce-ibmz-create-hcp-commands.sh
+++ ci-operator/step-registry/hypershift/mce/ibmz/create/hcp/hypershift-mce-ibmz-create-hcp-commands.sh
@@ -42,15 +42,20 @@ else
 fi
 
 # Installing hypershift cli
+MCE_VERSION=$(oc get "$(oc get multiclusterengines -oname)" -ojsonpath="{.status.currentVersion}" | cut -c 1-3)
+HYPERSHIFT_CLI_NAME=hcp
+if (( $(echo "$MCE_VERSION < 2.4" | bc -l) )); then
+ echo "MCE version is less than 2.4, use hypershift command"
+ HYPERSHIFT_CLI_NAME=hypershift
+fi
+
 echo "$(date) Installing hypershift cli"
-mkdir /tmp/hypershift_cli
-oc get ConsoleCLIDownload
-downloadURL=$(oc get ConsoleCLIDownload hcp-cli-download -o json | jq -r '.spec.links[] | select(.text | test("Linux for x86_64")).href')
-curl -k --output /tmp/hypershift.tar.gz ${downloadURL}
-tar -xvf /tmp/hypershift.tar.gz -C /tmp/hypershift_cli
-chmod +x /tmp/hypershift_cli/hypershift
-PATH=$PATH:/tmp/hypershift_cli
-export PATH
+mkdir /tmp/${HYPERSHIFT_CLI_NAME}_cli
+downloadURL=$(oc get ConsoleCLIDownload ${HYPERSHIFT_CLI_NAME}-cli-download -o json | jq -r '.spec.links[] | select(.text | test("Linux for x86_64")).href')
+curl -k --output /tmp/${HYPERSHIFT_CLI_NAME}.tar.gz ${downloadURL}
+tar -xvf /tmp/${HYPERSHIFT_CLI_NAME}.tar.gz -C /tmp/${HYPERSHIFT_CLI_NAME}_cli
+chmod +x /tmp/${HYPERSHIFT_CLI_NAME}_cli/${HYPERSHIFT_CLI_NAME}
+export PATH=$PATH:/tmp/${HYPERSHIFT_CLI_NAME}_cli
 
 # Installing required tools
 mkdir /tmp/bin
