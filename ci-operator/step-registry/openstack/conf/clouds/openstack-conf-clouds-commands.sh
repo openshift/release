ff220c0f1c04ca1002e8aefb21fdc0c862fa8386
Pierre Prinetti <pierreprinetti@redhat.com>
2023-09-14 17:49:43 +0200

    openstack: Fix resourcenames dependency (#43371)
    Fix the jobs now, and the CLUSTER_NAME functionality later

--- ci-operator/step-registry/openstack/conf/clouds/openstack-conf-clouds-commands.sh
+++ ci-operator/step-registry/openstack/conf/clouds/openstack-conf-clouds-commands.sh
@@ -9,7 +9,10 @@ info() {
 }
 
 CLUSTER_TYPE="${CLUSTER_TYPE_OVERRIDE:-$CLUSTER_TYPE}"
-CLUSTER_NAME="$(<"${SHARED_DIR}/CLUSTER_NAME")"
+CLUSTER_NAME=''
+if [ -r "${SHARED_DIR}/CLUSTER_NAME" ]; then
+	CLUSTER_NAME="$(<"${SHARED_DIR}/CLUSTER_NAME")"
+fi
 
 clouds_yaml="$(mktemp)"
 cp "/var/run/cluster-secrets/${CLUSTER_TYPE}/clouds.yaml" "$clouds_yaml"
