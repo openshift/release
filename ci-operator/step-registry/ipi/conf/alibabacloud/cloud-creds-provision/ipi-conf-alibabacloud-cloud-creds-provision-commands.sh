aae58f8c74e176c51c4fc67d8c52609d138a1801
Jianlin Liu <jialiu@redhat.com>
2023-09-14 14:43:42 +0800

    [phase-2] RELEASE_IMAGE_INITIAL take higher priority than RELEASE_IMAGE_LATEST (#43192)

--- ci-operator/step-registry/ipi/conf/alibabacloud/cloud-creds-provision/ipi-conf-alibabacloud-cloud-creds-provision-commands.sh
+++ ci-operator/step-registry/ipi/conf/alibabacloud/cloud-creds-provision/ipi-conf-alibabacloud-cloud-creds-provision-commands.sh
@@ -9,16 +9,34 @@ MPREFIX="${SHARED_DIR}/manifest"
 cluster_id="${NAMESPACE}-${UNIQUE_HASH}"
 export ALIBABA_CLOUD_CREDENTIALS_FILE="${SHARED_DIR}/alibabacreds.ini"
 
-export HOME="${HOME:-/tmp/home}"
-export XDG_RUNTIME_DIR="${HOME}/run"
-export REGISTRY_AUTH_PREFERENCE=podman # TODO: remove later, used for migrating oc from docker to podman
-mkdir -p "${XDG_RUNTIME_DIR}"
-
-echo "RELEASE_IMAGE_LATEST: ${RELEASE_IMAGE_LATEST}"
+# release-controller always expose RELEASE_IMAGE_LATEST when job configuraiton defines release:latest image
+echo "RELEASE_IMAGE_LATEST: ${RELEASE_IMAGE_LATEST:-}"
+# RELEASE_IMAGE_LATEST_FROM_BUILD_FARM is pointed to the same image as RELEASE_IMAGE_LATEST, 
+# but for some ci jobs triggerred by remote api, RELEASE_IMAGE_LATEST might be overridden with 
+# user specified image pullspec, to avoid auth error when accessing it, always use build farm 
+# registry pullspec.
 echo "RELEASE_IMAGE_LATEST_FROM_BUILD_FARM: ${RELEASE_IMAGE_LATEST_FROM_BUILD_FARM}"
+# seem like release-controller does not expose RELEASE_IMAGE_INITIAL, even job configuraiton defines 
+# release:initial image, once that, use 'oc get istag release:inital' to workaround it.
+echo "RELEASE_IMAGE_INITIAL: ${RELEASE_IMAGE_INITIAL:-}"
+if [[ -n ${RELEASE_IMAGE_INITIAL:-} ]]; then
+    tmp_release_image_initial=${RELEASE_IMAGE_INITIAL}
+    echo "Getting inital release image from RELEASE_IMAGE_INITIAL..."
+elif oc get istag "release:initial" -n ${NAMESPACE} &>/dev/null; then
+    tmp_release_image_initial=$(oc -n ${NAMESPACE} get istag "release:initial" -o jsonpath='{.tag.from.name}')
+    echo "Getting inital release image from build farm imagestream: ${tmp_release_image_initial}"
+fi
+# For some ci upgrade job (stable N -> nightly N+1), RELEASE_IMAGE_INITIAL and 
+# RELEASE_IMAGE_LATEST are pointed to different imgaes, RELEASE_IMAGE_INITIAL has 
+# higher priority than RELEASE_IMAGE_LATEST
+TESTING_RELEASE_IMAGE=""
+if [[ -n ${tmp_release_image_initial:-} ]]; then
+    TESTING_RELEASE_IMAGE=${tmp_release_image_initial}
+else
+    TESTING_RELEASE_IMAGE=${RELEASE_IMAGE_LATEST_FROM_BUILD_FARM}
+fi
+echo "TESTING_RELEASE_IMAGE: ${TESTING_RELEASE_IMAGE}"
 
-# extract ccoctl from the release image
-oc registry login
 ADDITIONAL_OC_EXTRACT_ARGS=""
 if [[ "${EXTRACT_MANIFEST_INCLUDED}" == "true" ]]; then
   ADDITIONAL_OC_EXTRACT_ARGS="${ADDITIONAL_OC_EXTRACT_ARGS} --included --install-config=${SHARED_DIR}/install-config.yaml"
@@ -27,8 +45,16 @@ echo "OC Version:"
 which oc
 oc version --client
 oc adm release extract --help
+
 # extract alibabacloud credentials requests from the release image
-oc --loglevel 10 adm release extract --credentials-requests --cloud=alibabacloud --to="${CR_PATH}" ${ADDITIONAL_OC_EXTRACT_ARGS} "${RELEASE_IMAGE_LATEST_FROM_BUILD_FARM}"
+dir=$(mktemp -d)
+pushd "${dir}"
+cp ${CLUSTER_PROFILE_DIR}/pull-secret pull-secret
+oc registry login --to pull-secret
+oc adm release extract --registry-config pull-secret --credentials-requests --cloud=alibabacloud --to="${CR_PATH}" ${ADDITIONAL_OC_EXTRACT_ARGS} "${TESTING_RELEASE_IMAGE}"
+rm pull-secret
+popd
+
 echo "CR manifest files:"
 ls "${CR_PATH}"
 
