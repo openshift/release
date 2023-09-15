e1915e59315105aca798115abde9875a7858666f
Devan Goodwin <dgoodwin@redhat.com>
2023-09-15 13:08:58 -0300

    Try again with 6 vcpu 16gb

--- ci-operator/step-registry/ipi/conf/gcp/ipi-conf-gcp-commands.sh
+++ ci-operator/step-registry/ipi/conf/gcp/ipi-conf-gcp-commands.sh
@@ -21,7 +21,7 @@ fi
 # be active savings plans targeting this machine class.
 master_type=""
 # Temporary test to see if this helps the consistent high CPU alerts
-master_type_suffix="-custom-8-16384"
+master_type_suffix="-custom-6-16384"
 #master_type_suffix="-standard-4"
 if [[ "${SIZE_VARIANT}" == "xlarge" ]]; then
   master_type_suffix="-standard-32"
