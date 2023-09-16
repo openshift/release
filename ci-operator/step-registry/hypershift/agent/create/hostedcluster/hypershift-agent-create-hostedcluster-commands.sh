a165981c0d0de3d17ddf185056c82e1181d9e661
psap-user <liqcui@redhat.com>
2023-09-16 06:52:41 +0000

    update code

--- ci-operator/step-registry/hypershift/agent/create/hostedcluster/hypershift-agent-create-hostedcluster-commands.sh
+++ ci-operator/step-registry/hypershift/agent/create/hostedcluster/hypershift-agent-create-hostedcluster-commands.sh
@@ -18,6 +18,7 @@ oc extract secret/pull-secret -n openshift-config --to=/tmp --confirm
   --agent-namespace="clusters-${CLUSTER_NAME}" \
   --base-domain=${BASEDOMAIN} \
   --api-server-address=api.${CLUSTER_NAME}.${BASEDOMAIN} \
+  --node-upgrade-type="InPlace" \
   --release-image ${RELEASE_IMAGE}
 
 echo "Waiting for cluster to become available"
