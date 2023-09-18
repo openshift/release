67e387644c45e01899443b45e4cc5d255dcf947a
Krishna Harsha Voora <14876995+krishvoor@users.noreply.github.com>
2023-09-18 19:17:52 +0530

    Adds CPT as prefix to UUIDs (#43391)
    * Adds prefix CPT to UUIDs
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>
    
    * Updates for shellcheck
    
    * Updates to the uuid
    
    * Updates the follow:
    
    1) adds "-cpt" to the generated uuid
    2) updates the respective workload
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>
    
    * Minor updates to uuidgen
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>
    
    * shellcheck updates
    
    * Declare & assign seperately to avoid masking return values
    
    https://www.shellcheck.net/wiki/SC2155
    
    * Minor updates
    
    * Prefixing CPT-{UUID}
    
    * Minor nits
    
    ---------
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>

--- ci-operator/step-registry/openshift-qe/node-density/openshift-qe-node-density-commands.sh
+++ ci-operator/step-registry/openshift-qe/node-density/openshift-qe-node-density-commands.sh
@@ -19,6 +19,10 @@ pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
 export EXTRA_FLAGS="--pods-per-node=$PODS_PER_NODE --pod-ready-threshold=$POD_READY_THRESHOLD"
 export WORKLOAD=node-density
 
+# UUID Generation
+UUID="CPT-$(uuidgen)"
+export UUID
+
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
 
 rm -rf "${SHARED_DIR}/${OUTPUT_FILE:?}"
