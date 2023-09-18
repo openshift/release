135210f1ae1aa9abd80cb5a65ddf15ea4591712c
Stephen Matula <smatula@redhat.com>
2023-09-18 13:23:35 -0400

    Move to skupperproject

--- ci-operator/step-registry/rhsi/run-tests/rhsi-run-tests-commands.sh
+++ ci-operator/step-registry/rhsi/run-tests/rhsi-run-tests-commands.sh
@@ -21,7 +21,7 @@ cp -r /result/* $ARTIFACT_DIR/result 2>/dev/null || :
 cp -r /tmp/test.out $ARTIFACT_DIR/result 2>/dev/null || :
 
 # Prepend junit_ to result xml files
-rename '/junit' '/junit_rhsi' ${ARTIFACT_DIR}/result/*.xml 2>/dev/null || :
+for f in ${ARTIFACT_DIR}/result/*.xml; do nf="$(echo $f | sed -e 's/\/junit/\/junit_rhsi/')" ; mv -- "$f" "$nf"; done || :
 
 exit $status
 
