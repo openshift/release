6ef10de8d636f2d1bddb841d20a31656bf131ce3
Jianli Wei <jiwei@redhat.com>
2023-09-18 16:53:19 +0800

    skip CPMS check if UPI or SNO (#43282)

--- ci-operator/step-registry/cucushift/installer/check/control-plane-machinesets/cucushift-installer-check-control-plane-machinesets-commands.sh
+++ ci-operator/step-registry/cucushift/installer/check/control-plane-machinesets/cucushift-installer-check-control-plane-machinesets-commands.sh
@@ -36,7 +36,7 @@ function hasCPMS() {
         # 4.13+
         REQUIRED_OCP_VERSION="4.13"
         ;;
-    nutanix)
+    nutanix*)
         # 4.14+
         REQUIRED_OCP_VERSION="4.14"
         ;;
@@ -51,6 +51,32 @@ function hasCPMS() {
     return ${ret}
 }
 
+# check if the cluster is by IPI or UPI
+# return 0 if it is an IPI cluster, otherwise 1
+function isIPI() {
+    #oc get machines -n openshift-machine-api -o json | jq -r '.items[].metadata.labels."machine.openshift.io/cluster-api-machine-role"' | grep master
+    oc get cm -n openshift-config openshift-install -o yaml
+    if [ $? -eq 0 ]; then
+        # an IPI cluster
+        return 0
+    else
+        # a UPI cluster
+        return 1
+    fi
+}
+
+# check if it is a Single-Node cluster
+# return 0 if SNO cluster, otherwise 1
+function isSNO() {
+    local nodes_count
+    nodes_count=$(oc get nodes --no-headers | wc -l)
+    if (( ${nodes_count} == 1 )); then
+        return 0
+    else
+        return 1
+    fi
+}
+
 export KUBECONFIG=${SHARED_DIR}/kubeconfig
 
 if test -f "${SHARED_DIR}/proxy-conf.sh"
@@ -59,8 +85,18 @@ then
     source "${SHARED_DIR}/proxy-conf.sh"
 fi
 
+if ! isIPI; then
+    echo "INFO: 'controlplanemachinesets' is not available on UPI cluster, skip."
+    exit 0
+fi
+
+if isSNO; then
+    echo "INFO: 'controlplanemachinesets' is not available on Single-Node cluster, skip."
+    exit 0
+fi
+
 if ! hasCPMS; then
-    echo "INFO: 'controlplanemachinesets' is not supproted (OCP ${version} on ${CLUSTER_TYPE}), skip checking"
+    echo "INFO: 'controlplanemachinesets' is not supproted (OCP ${version} on ${CLUSTER_TYPE}), skip."
     exit 0
 fi
 
