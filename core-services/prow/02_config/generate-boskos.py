9cfccc315c1a80aac2f058f57a415b3fd9ce6861
Joseph Callen <jcallen@redhat.com>
2023-09-20 15:29:36 -0400

    vSphere: remove overlapping vlans (#43570)

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -336,11 +336,11 @@ for i in range(223,229):
 for i in range(151,158):
     CONFIG['vsphere-multizone-quota-slice']['ci-segment-{}'.format(i)] = 1
 
-for i in range(200,204):
+for i in range(202,204):
     CONFIG['vsphere-8-quota-slice']['ci-segment-{}'.format(i)] = 1
 for i in range(205,214):
     CONFIG['vsphere-8-quota-slice']['ci-segment-{}'.format(i)] = 1
-for i in [1153,1179,1211,1225,1232,1243,1252,1256,1260,1261,1262,1263,1265,1272,1274,1283,1285,1305,1309,1364,1367,758,902]:
+for i in [1153,1179,1211,1225,1232,1252,1256,1260,1261,1262,1263,1265,1272,1274,1283,1285,1305,1309,758,902]:
     CONFIG['vsphere-8-vpn-quota-slice']['bcr01a.dal10.{}'.format(i)] = 1
 
 
