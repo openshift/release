59731225fdf3e605043fdaa9a50cc32631237dd4
vishnuchalla <vishnuchalla47@gmail.com>
2023-09-21 13:04:53 -0400

    bug fix on cpt- prefix (#43562)
    Co-authored-by: Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.remote.csb>

--- ci-operator/step-registry/openshift-qe/router-perf/openshift-qe-router-perf-commands.sh
+++ ci-operator/step-registry/openshift-qe/router-perf/openshift-qe-router-perf-commands.sh
@@ -19,7 +19,7 @@ export GSHEET_KEY_LOCATION
 git clone https://github.com/cloud-bulldozer/e2e-benchmarking --depth=1
 pushd e2e-benchmarking/workloads/router-perf-v2
 # UUID Generation
-UUID="CPT-$(uuidgen)"
+UUID="perfscale-cpt-$(uuidgen)"
 export UUID
 # ES configuration
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
