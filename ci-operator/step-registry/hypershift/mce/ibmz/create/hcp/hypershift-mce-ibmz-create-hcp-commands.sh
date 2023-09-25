05ec2c9016327e11310011472b899c4ac877a233
Ravuri Phaneendra <rphaneen@redhat.com>
2023-09-25 17:07:41 +0530

    MULTIARCH-3435: Added HyperShift MCE IBMZ conformance workflow  (#43266)
    * Initial commit: created ibmz directory
    
    * Added creation shell script till downlaoding images
    
    * Added NMstateconfigs creation
    
    * Updated shell script for creation of HCP
    
    * added files for destroy and test directories
    
    * Added test commands to workflow
    
    * Updated periodics.yaml config
    
    * Updated -4.14__periodics.yaml
    
    * Syncing
    
    * Adding lnxocp04 port forwarding and SNI configs
    
    * Adding destroy chain for ibmz nodes
    
    * Few modifications as part of review
    
    * Modified deletion script
    
    * Added changes in config dir
    
    * Creation workflow for ibmcloud zVSI computes
    
    * Changing cluster names across the workflow
    
    * Adding e2e workflow for hcp-agent-ibmz workflow
    
    * Modifying the commands file name
    
    * modifying the destroy ref name
    
    * Updating the job config file using make
    
    * Removing the core_dns and deployment configs as we are not using yellow zone
    
    * Added new lines to metadata files
    
    * Removed unnecessary file changes in previous commit
    
    * Revert "Removed unnecessary file changes in previous commit"
    
    This reverts commit 2f879c5b1a26f2a1603111d492b7c8f88813b359.
    
    * Revert "Added new lines to metadata files"
    
    This reverts commit dfb1933a2a3d22525eaf93eafd29ea30a9ae392b.
    
    * Update export commands
    
    * Fixing metadata and config file checks
    
    * Fixing the shell check validations
    
    * Fixing shell checks validation
    
    * Fixing shell checks
    
    * Adding quotes to fix shell checks
    
    * removing the extra bracket
    
    * Added commands for debugging
    
    * Changed ConsoleCLIDownload name
    
    * Changed the logic to download hypershift cli using mce version
    
    * Adding HYPERSHIFT_CLI_NAME variable in destroy chain as well
    
    * Added OCP_RELEASE_VERSION directly instead of fetching it
    
    * Modified config file checks
    
    * removing unnecessary comamnds
    
    * Renaming the parameter for OCP_RELEASE_VERSION
    
    * Adding debug statements for HC failure check
    
    * Adding HA flags for single replicas fox fixing etcd issue
    
    * Changed ssh key for infraenv
    
    * renaming the zvsi-compute step using regex
    
    * Removing sudo
    
    * replacing wget with curl and removing yum install
    
    * Changing the IC CLI VERSION and path
    
    * changing the curl command
    
    * Updated ibmcloud cli download
    
    * Changed URL for ibmcloud cli download
    
    * Removed debug statement
    
    * Changing the pub key for VSI creation and username
    
    * Changed usernames in owners files
    
    * Changing curl command to bypass the cert
    
    * Updating metadata and jobs
    
    * copying the ssh key and changing permissions
    
    * Adding elif and fixing dig command
    
    * moidfying the ssh key with proper format
    
    * Added debug statements
    
    * Adding debug commands
    
    * Debug statements
    
    * changing the logic for private key
    
    * removing unnecessary exit command
    
    * modifying ssh key logic
    
    * Updated timeouts
    
    * Added debug statements at end of create script
    
    * Adding debug commands for destroy and create chain
    
    * Adding server alive interval for ssh remote connections
    
    * Fixing the delete chain for subnet
    
    * Fixing the resource reclamations issue
    
    * Fixing the shell check errors
    
    * Fixing shell check errors
    
    * Changing the cluster name for build03
    
    * Revert "Changing the cluster name for build03"
    
    This reverts commit 4da607a40027391ba9fb0cbae4f03cd07950b707.
    
    * Changing the cluster name to build03
    
    * Revert "Changing the cluster name to build03"
    
    This reverts commit 24e8de25f1cb4da4740aaca621ff765361cc1ff8.
    
    * fixing prow config errors after syncing
    
    * Adding sleep before deleting RG
    
    * Adding deleting command for resources downloaded
    
    * removing service instances validation
    
    * Fixing the prow config errors
    
    ---------
    
    Co-authored-by: Damisetti Veerabhadrarao <damisetti.veerabhadrarao@ibm.com>
    Co-authored-by: Phani <phaneendra.ravuri@ibm.com>

---
+++ ci-operator/step-registry/hypershift/mce/ibmz/create/hcp/hypershift-mce-ibmz-create-hcp-commands.sh
@@ -0,0 +1,222 @@
+#!/bin/bash
+
+set -x
+
+# Hosted Control Plane parameters
+hc_ns="hcp-ci"
+export hc_ns
+hc_name="agent-ibmz"
+export hc_name
+hcp_ns="$hc_ns-$hc_name"
+export hcp_ns
+
+# InfraEnv configs
+ssh_key_file="${AGENT_IBMZ_CREDENTIALS}/httpd-vsi-pub-key"
+ssh_key=$(cat ${ssh_key_file})
+export ssh_key
+
+# Creating cluster imageset
+cat <<EOF | oc create -f -
+apiVersion: hive.openshift.io/v1
+kind: ClusterImageSet
+metadata:
+  name: img-${hcp_ns}-appsub
+spec:
+  releaseImage: ${OCP_IMAGE_MULTI}
+EOF
+
+# Creating Provisioning
+if ! oc get provisioning provisioning-configuration &> /dev/null; then
+  # If it doesn't exist, create it
+  cat <<EOF | oc create -f -
+apiVersion: metal3.io/v1alpha1
+kind: Provisioning
+metadata:
+  name: provisioning-configuration
+spec:
+  provisioningNetwork: Disabled
+  watchAllNamespaces: true
+EOF
+else
+  echo "Resource provisioning-configuration already exists. Skipping creation."
+fi
+
+# Installing hypershift cli
+MCE_VERSION=$(oc get "$(oc get multiclusterengines -oname)" -ojsonpath="{.status.currentVersion}" | cut -c 1-3)
+HYPERSHIFT_CLI_NAME=hcp
+if (( $(echo "$MCE_VERSION < 2.4" | bc -l) )); then
+ echo "MCE version is less than 2.4, using the hypershift cli name."
+ HYPERSHIFT_CLI_NAME=hypershift
+fi
+
+echo "$(date) Installing hypershift cli"
+mkdir /tmp/${HYPERSHIFT_CLI_NAME}_cli
+downloadURL=$(oc get ConsoleCLIDownload ${HYPERSHIFT_CLI_NAME}-cli-download -o json | jq -r '.spec.links[] | select(.text | test("Linux for x86_64")).href')
+curl -k --output /tmp/${HYPERSHIFT_CLI_NAME}.tar.gz ${downloadURL}
+tar -xvf /tmp/${HYPERSHIFT_CLI_NAME}.tar.gz -C /tmp/${HYPERSHIFT_CLI_NAME}_cli
+chmod +x /tmp/${HYPERSHIFT_CLI_NAME}_cli/${HYPERSHIFT_CLI_NAME}
+export PATH=$PATH:/tmp/${HYPERSHIFT_CLI_NAME}_cli
+
+# Installing required tools
+mkdir /tmp/bin
+curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o /tmp/bin/jq && chmod +x /tmp/bin/jq
+curl -L https://github.com/mikefarah/yq/releases/download/v4.31.2/yq_linux_amd64 -o /tmp/bin/yq && chmod +x /tmp/bin/yq
+PATH=$PATH:/tmp/bin
+export PATH
+
+# Applying mirror config
+echo "$(date) Applying mirror config"
+cat <<EOF | oc create -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: mirror-config
+  namespace: multicluster-engine 
+  labels:
+    app: assisted-service
+data:
+  registries.conf: |
+    unqualified-search-registries = ["registry.access.redhat.com", "docker.io"]
+
+    [[registry]]
+      location = "registry.stage.redhat.io"
+      insecure = false
+      blocked = false
+      mirror-by-digest-only = true
+      prefix = ""
+
+      [[registry.mirror]]
+        location = "brew.registry.redhat.io"
+        insecure = false
+
+    [[registry]]
+      location = "registry.redhat.io/multicluster-engine"
+      insecure = false
+      blocked = false
+      mirror-by-digest-only = true
+      prefix = ""
+
+      [[registry.mirror]]
+        location = "brew.registry.redhat.io/multicluster-engine"
+        insecure = false
+EOF
+
+# Creating AgentServiceConfig
+echo "$(date) Creating AgentServiceConfig"
+envsubst <<"EOF" | oc apply -f -
+apiVersion: agent-install.openshift.io/v1beta1
+kind: AgentServiceConfig
+metadata:
+ name: agent
+spec:
+  mirrorRegistryRef:
+    name: mirror-config
+  databaseStorage:
+    accessModes:
+    - ReadWriteOnce
+    resources:
+      requests:
+        storage: 10Gi
+  filesystemStorage:
+    accessModes:
+    - ReadWriteOnce
+    resources:
+      requests:
+        storage: 10Gi
+  osImages:
+    - openshiftVersion: "${OCP_VERSION}"
+      version: "${OCP_RELEASE_VERSION}"
+      url: "${ISO_URL}"
+      rootFSUrl: "${ROOT_FS_URL}"
+      cpuArchitecture: s390x
+EOF
+
+oc wait --timeout=5m --for=condition=DeploymentsHealthy agentserviceconfig agent
+echo "$(date) AgentServiceConfig is ready"
+
+set +x
+# Setting up pull secret with brew token
+oc extract secret/pull-secret -n openshift-config --to=/tmp --confirm
+brew_token_file="${AGENT_IBMZ_CREDENTIALS}/brew-token"
+cat /tmp/.dockerconfigjson | jq --arg brew_token "$(cat ${brew_token_file})" '.auths += {"brew.registry.redhat.io": {"auth": $brew_token}}' > /tmp/pull-secret
+PULL_SECRET_FILE=/tmp/pull-secret
+set -x
+
+# Creating agent hosted cluster manifests
+echo "$(date) Creating agent hosted cluster manifests"
+oc create ns ${hcp_ns}
+mkdir /tmp/hc-manifests
+
+${HYPERSHIFT_CLI_NAME} create cluster agent \
+    --name=${hc_name} \
+    --pull-secret="${PULL_SECRET_FILE}" \
+    --agent-namespace=${hcp_ns} \
+    --base-domain=${HYPERSHIFT_BASEDOMAIN} \
+    --api-server-address=api.${hc_name}.${HYPERSHIFT_BASEDOMAIN} \
+    --ssh-key=${ssh_key_file} \
+    --control-plane-availability-policy "SingleReplica" \
+    --infra-availability-policy "SingleReplica" \
+    --namespace $hc_ns \
+    --release-image=${OCP_IMAGE_MULTI} --render > /tmp/hc-manifests/cluster-agent.yaml
+
+# Split the manifest to replace routing strategy of various services
+csplit -f /tmp/hc-manifests/manifest_ -k /tmp/hc-manifests/cluster-agent.yaml /---/ "{6}"
+
+# Service strategy to replace
+printf "  - service: APIServer
+    servicePublishingStrategy:
+      type: LoadBalancer
+  - service: OAuthServer
+    servicePublishingStrategy:
+      type: Route
+  - service: OIDC
+    servicePublishingStrategy:
+      type: Route
+  - service: Konnectivity
+    servicePublishingStrategy:
+      type: Route
+  - service: Ignition
+    servicePublishingStrategy:
+      type: Route
+  - service: OVNSbDb
+    servicePublishingStrategy:
+      type: Route
+" > /tmp/hc-manifests/replacement.yaml
+
+for file in /tmp/hc-manifests/manifest_*
+do
+    if grep -q 'kind: HostedCluster' "$file"
+    then
+        yq eval-all -i 'select(fileIndex==0).spec.services = select(fileIndex==1) | select(fileIndex==0)' "$file" "/tmp/hc-manifests/replacement.yaml"
+    fi
+done
+
+# Applying agent cluster manifests
+echo "$(date) Applying agent cluster manifests"
+ls /tmp/hc-manifests/manifest_* | awk ' { print " -f " $1 } ' | xargs oc apply
+
+oc wait --timeout=15m --for=condition=Available --namespace=${hc_ns} hostedcluster/${hc_name}
+echo "$(date) Agent cluster is available"
+
+# Applying InfraEnv
+echo "$(date) Applying InfraEnv"
+envsubst <<"EOF" | oc apply -f -
+apiVersion: agent-install.openshift.io/v1beta1
+kind: InfraEnv
+metadata:
+  name: ${hc_name}
+  namespace: ${hcp_ns}
+spec:
+  cpuArchitecture: s390x
+  pullSecretRef:
+    name: pull-secret
+  sshAuthorizedKey: ${ssh_key}
+EOF
+
+# Waiting for discovery iso file to ready
+oc wait --timeout=10m --for=condition=ImageCreated --namespace=${hcp_ns} infraenv/${hc_name}
+echo "$(date) ISO Download url is ready"
+
+# Download hosted cluster kubeconfig
+echo "$(date) Create hosted cluster kubeconfig"
+${HYPERSHIFT_CLI_NAME} create kubeconfig --namespace=${hc_ns} --name=${hc_name} >${SHARED_DIR}/${hc_name}_kubeconfig
