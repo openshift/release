a70e57f126a8944efc494278faa05e5147fec473
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2023-09-28 09:56:21 +0200

    USHIFT-1588: Refactor rebase to handle its dependencies

--- ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
+++ ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+set -xeuo pipefail
 
 if [[ "$JOB_NAME" == rehearse* ]]; then
     echo "INFO: \$JOB_NAME starts with rehearse - running in DRY RUN mode"
     export DRY_RUN=y
 fi
 
+export PATH="${HOME}/.local/bin:${PATH}"
+python3 -m ensurepip --upgrade
+pip3 install setuptools-rust cryptography pyyaml pygithub gitpython
+
+cd /go/src/github.com/openshift/microshift/
+DEST_DIR=${HOME}/.local/bin ./scripts/fetch_tools.sh yq
 ./scripts/auto-rebase/rebase_job_entrypoint.sh
