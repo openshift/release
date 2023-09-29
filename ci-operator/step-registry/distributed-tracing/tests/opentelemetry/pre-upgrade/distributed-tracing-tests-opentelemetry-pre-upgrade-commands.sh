e3066058ab41792155c8692b5e62eae0ed15be79
Ishwar Kanse <ikanse@redhat.com>
2023-09-29 23:54:59 +0530

    Update Distributed Tracing upstream jobs. (#43810)

--- ci-operator/step-registry/distributed-tracing/tests/opentelemetry/pre-upgrade/distributed-tracing-tests-opentelemetry-pre-upgrade-commands.sh
+++ ci-operator/step-registry/distributed-tracing/tests/opentelemetry/pre-upgrade/distributed-tracing-tests-opentelemetry-pre-upgrade-commands.sh
@@ -12,12 +12,13 @@ if [[ -n "${DOWNSTREAM_TESTS_COMMIT}" ]]; then
   git clone https://github.com/open-telemetry/opentelemetry-operator.git /tmp/otel-tests
   cd /tmp/otel-tests
   git checkout -b downstream-release "${DOWNSTREAM_TESTS_COMMIT}"
+  
   #Set parameters for running the test cases on OpenShift
   TARGETALLOCATOR_IMG=$TARGETALLOCATOR_IMG SED_BIN="$(which sed)" ./hack/modify-test-images.sh
   sed -i 's/- -duration=1m/- -duration=6m/' tests/e2e-autoscale/autoscale/03-install.yaml
 
   # Remove test cases to be skipped from the test run
-  IFS=' ' read -ra SKIP_TEST_ARRAY <<< "$SKIP_TESTS"
+  IFS=' ' read -ra SKIP_TEST_ARRAY <<< "$PRE_UPG_SKIP_TESTS"
   SKIP_TESTS_TO_REMOVE=""
   INVALID_TESTS=""
   for test in "${SKIP_TEST_ARRAY[@]}"; do
@@ -30,26 +31,6 @@ if [[ -n "${DOWNSTREAM_TESTS_COMMIT}" ]]; then
 
   if [[ -n "$INVALID_TESTS" ]]; then
     echo "These test cases are not valid to be skipped: $INVALID_TESTS"
-  fi
-
-  if [[ -n "$SKIP_TESTS_TO_REMOVE" ]]; then
-    rm -rf $SKIP_TESTS_TO_REMOVE
-  fi
-
-  # Remove test cases to be skipped from the test run
-  IFS=' ' read -ra SKIP_TEST_ARRAY <<< "$SKIP_TESTS"
-  SKIP_TESTS_TO_REMOVE=""
-  INVALID_TESTS=""
-  for test in "${SKIP_TEST_ARRAY[@]}"; do
-    if [[ "$test" == tests/* ]]; then
-      SKIP_TESTS_TO_REMOVE+=" $test"
-    else
-      INVALID_TESTS+=" $test"
-    fi
-  done
-
-  if [[ -n "$INVALID_TESTS" ]]; then
-    echo "These test cases are not valid to be skipped $INVALID_TESTS"
   fi
 
   if [[ -n "$SKIP_TESTS_TO_REMOVE" ]]; then
@@ -92,26 +73,6 @@ else
 
   if [[ -n "$INVALID_TESTS" ]]; then
     echo "These test cases are not valid to be skipped: $INVALID_TESTS"
-  fi
-
-  if [[ -n "$SKIP_TESTS_TO_REMOVE" ]]; then
-    rm -rf $SKIP_TESTS_TO_REMOVE
-  fi
-
-  # Remove test cases to be skipped from the test run
-  IFS=' ' read -ra SKIP_TEST_ARRAY <<< "$SKIP_TESTS"
-  SKIP_TESTS_TO_REMOVE=""
-  INVALID_TESTS=""
-  for test in "${SKIP_TEST_ARRAY[@]}"; do
-    if [[ "$test" == tests/* ]]; then
-      SKIP_TESTS_TO_REMOVE+=" $test"
-    else
-      INVALID_TESTS+=" $test"
-    fi
-  done
-
-  if [[ -n "$INVALID_TESTS" ]]; then
-    echo "These test cases are not valid to be skipped $INVALID_TESTS"
   fi
 
   if [[ -n "$SKIP_TESTS_TO_REMOVE" ]]; then
