8765bae5741b251396917d5b6081e1c97925f02c
Paige Rubendall <64206430+paigerube14@users.noreply.github.com>
2023-10-04 23:53:43 -0400

    Cni output (#43978)
    * ouptut file update
    
    * remove file

--- ci-operator/step-registry/openshift-qe/ingress-perf/openshift-qe-ingress-perf-commands.sh
+++ ci-operator/step-registry/openshift-qe/ingress-perf/openshift-qe-ingress-perf-commands.sh
@@ -22,9 +22,9 @@ pushd e2e-benchmarking/workloads/ingress-perf
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
 export ES_INDEX="ingress-performance"
 
+rm -f ${SHARED_DIR}/index.json
+
 # Start the Workload
-OUTPUT_FILE="index_data.json"
-rm -rf "${SHARED_DIR}/${OUTPUT_FILE:?}"
 ./run.sh
 
 folder_name=$(ls -t -d /tmp/*/ | head -1)
