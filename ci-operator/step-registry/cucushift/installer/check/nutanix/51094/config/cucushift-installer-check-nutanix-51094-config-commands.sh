31e43dd95b6f195d21c72332a63ed28fdd791a81
Shang Gao <sgao@redhat.com>
2023-10-09 23:51:46 +0800

    debug

--- ci-operator/step-registry/cucushift/installer/check/nutanix/51094/config/cucushift-installer-check-nutanix-51094-config-commands.sh
+++ ci-operator/step-registry/cucushift/installer/check/nutanix/51094/config/cucushift-installer-check-nutanix-51094-config-commands.sh
@@ -6,12 +6,39 @@ set -o pipefail
 
 echo "Start Running Case https://polarion.engineering.redhat.com/polarion/#/project/OSE/workitem?id=OCP-51094"
 
-# Setting clusterOSImage
-ocp_version="${BUILD_VERSION:1:4}"
-cluster_os_image=$(curl -s https://raw.githubusercontent.com/openshift/installer/release-"$ocp_version"/data/data/coreos/rhcos.json | jq -r '.architectures.x86_64.artifacts.nutanix.formats.qcow2.disk.location')
+CONFIG="${SHARED_DIR}/install-config.yaml"
 
-sed "s#nutanix:#nutanix:\n    clusterOSImage: $cluster_os_image#g" "${SHARED_DIR}/install-config.yaml"
-cat "${SHARED_DIR}/install-config.yaml"
+# Set specified master and worker configurations
+sed -i '/compute:/,/replicas: 3/d' "${CONFIG}"
+sed -i '/controlPlane:/,/replicas: 3/d' "${CONFIG}"
 
-# Restore
+cat >> "${CONFIG}" << EOF
+compute:
+- architecture: amd64
+  hyperthreading: Enabled
+  name: worker
+  platform:
+    nutanix:
+      cpus: 4
+      coresPerSocket: 2
+      memoryMiB: 20000
+      osDisk:
+        diskSizeGiB: 100
+  replicas: 3
+controlPlane:
+  architecture: amd64
+  hyperthreading: Enabled
+  name: master
+  platform:
+    nutanix:
+      cpus: 4
+      coresPerSocket: 2
+      memoryMiB: 20000
+      osDisk:
+        diskSizeGiB: 100
+  replicas: 3"
+EOF
+
+cat "${CONFIG}"
+
 # Restore
