e82e35524b147f02ffb0558ca5822bb567307d1a
Hemant Kumar <hekumar@redhat.com>
2023-10-09 16:15:36 -0400

    Remove tests from aws-ebs-csi-driver-operator repository

--- ci-operator/step-registry/storage/conf/csi-aws-ebs/storage-conf-csi-aws-ebs-commands.sh
+++ ci-operator/step-registry/storage/conf/csi-aws-ebs/storage-conf-csi-aws-ebs-commands.sh
@@ -9,7 +9,7 @@ if [ -d /go/src/github.com/openshift/csi-operator/legacy/ ]; then
     cp test/e2e/manifest.yaml ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
 else
     echo "Using regular csi directory"
-    cd /go/src/github.com/openshift/csi-operator/aws-ebs-csi-driver-operator
+    cd /go/src/github.com/openshift/aws-ebs-csi-driver-operator
     cp test/e2e/manifest.yaml ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
 fi
 
