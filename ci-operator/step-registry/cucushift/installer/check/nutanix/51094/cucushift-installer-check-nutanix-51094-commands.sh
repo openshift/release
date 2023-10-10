102b7c0c79a31927d580592ca41610a2ae1cfd3a
Shang Gao <sgao@redhat.com>
2023-10-10 13:25:08 +0800

    debug

--- ci-operator/step-registry/cucushift/installer/check/nutanix/51094/cucushift-installer-check-nutanix-51094-commands.sh
+++ ci-operator/step-registry/cucushift/installer/check/nutanix/51094/cucushift-installer-check-nutanix-51094-commands.sh
@@ -6,48 +6,50 @@ set -o pipefail
 
 echo "Start Running Case https://polarion.engineering.redhat.com/polarion/#/project/OSE/workitem?id=OCP-51169"
 
-# Ensure our UID, which is randomly generated, is in /etc/passwd. This is required
-# to be able to SSH.
-if ! whoami &> /dev/null; then
-    if [[ -w /etc/passwd ]]; then
-        echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
-    else
-        echo "/etc/passwd is not writeable, and user matching this uid is not found."
+# Check 2 worker nodes are joined in without error
+if [ "$(oc get node -l node-role.kubernetes.io/worker= --no-headers | wc -l)" != "2" ]; then
+    echo "Fail: check worker number"
+    exit 1
+fi
+
+# Check each node has customized physical resources as install_config
+IFS=' ' read -r -a node_names <<< "$(oc get machines -n openshift-machine-api -o jsonpath='{.items[*].status.addresses[?(@.type=="InternalDNS")].address}')"
+for node_name in "${node_names[@]}"; do
+    if [ "$(oc get machine "$node_name" -n openshift-machine-api -o jsonpath='{.spec.providerSpec.value.numCPUs}')" != "4" ]; then
+        echo "Fail: check $node_name numCPUs"
         exit 1
     fi
-fi
-
-sleep 3600
-
-oc get node -l node-role.kubernetes.io/worker= --no-headers | wc -l
-
-SSH_PRIV_KEY_PATH=${CLUSTER_PROFILE_DIR}/ssh-privatekey
-
-# Check MachineConfig have kerneltype applied
-if oc get machineconfigs | grep "99-master-kerneltype"; then
-    echo "Pass: check 99-master-kerneltype set"
-else
-    echo "Fail: check 99-master-kerneltype set"
-    exit 1
-fi
-if oc get machineconfigs | grep "99-worker-kerneltype"; then
-    echo "Pass: check 99-worker-kerneltype set"
-else
-    echo "Fail: check 99-worker-kerneltype set"
-    exit 1
-fi
-
-IFS=' ' read -r -a node_ips <<<"$(oc get machines -n openshift-machine-api -o jsonpath='{.items[*].status.addresses[?(@.type=="InternalIP")].address}')"
-
-# Check all nodes have RT kernel running
-for node_ip in "${node_ips[@]}"; do
-    uname="$(ssh -o "StrictHostKeyChecking no" -i "${SSH_PRIV_KEY_PATH}" core@"$node_ip" uname -r)"
-    if echo "$uname" | grep "rt"; then
-        echo "Pass: check node: $node_ip have RT kernel running"
-    else
-        echo "Fail: check node: $node_ip have RT kernel running"
+    if [ "$(oc get machine "$node_name" -n openshift-machine-api -o jsonpath='{.spec.providerSpec.value.numCoresPerSocket}')" != "2" ]; then
+        echo "Fail: check $node_name numCoresPerSocket"
+        exit 1
+    fi
+    if [ "$(oc get machine "$node_name" -n openshift-machine-api -o jsonpath='{.spec.providerSpec.value.memoryMiB}')" != "20000" ]; then
+        echo "Fail: check $node_name memoryMiB"
+        exit 1
+    fi
+    if [ "$(oc get machine "$node_name" -n openshift-machine-api -o jsonpath='{.spec.providerSpec.value.diskGiB}')" != "100" ]; then
+        echo "Fail: check $node_name diskGiB"
         exit 1
     fi
 done
+
+# Check nutanix platform parameters are configured correctly in the machineset
+oc get machineset -n openshift-machine-api -o jsonpath='{.items[0].spec.template.spec.providerSpec.value.numCPUs}'
+if [ "$(oc get machineset -n openshift-machine-api -o jsonpath='{.items[0].spec.template.spec.providerSpec.value.numCPUs}')" != "4" ]; then
+    echo "Fail: check $node_name numCPUs"
+    exit 1
+fi
+if [ "$(oc get machineset -n openshift-machine-api -o jsonpath='{.items[0].spec.template.spec.providerSpec.value.numCoresPerSocket}')" != "4" ]; then
+    echo "Fail: check $node_name numCoresPerSocket"
+    exit 1
+fi
+if [ "$(oc get machineset -n openshift-machine-api -o jsonpath='{.items[0].spec.template.spec.providerSpec.value.memoryMiB}')" != "4" ]; then
+    echo "Fail: check $node_name memoryMiB"
+    exit 1
+fi
+if [ "$(oc get machineset -n openshift-machine-api -o jsonpath='{.items[0].spec.template.spec.providerSpec.value.diskGiB}')" != "4" ]; then
+    echo "Fail: check $node_name diskGiB"
+    exit 1
+fi
 
 # Restore
