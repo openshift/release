81525a4b31192cea01e394e8ffda263785e0e8d6
Alessandro Di Stefano <aleskandro@redhat.com>
2023-10-11 03:13:20 +0200

    OCPQE-16721: Skip failing in the RP's overall CI test suite after pre-checks and before the extended-platform-tests run (#44136)
    * remove setting FORCE_SUCCESS_EXIT=yes as it is already the default
    
    * Fix the FORCE_SUCCESS_EXIT logic in the run function, after extended-platform-tests are run
    
    This let the logic for FORCE_SUCCESS_EXIT consistent along the steps using it. Moreover, we should return the expected exit code when FORCE_SUCCESS_EXIT is not enabled, not always 1.
    Finally, some of the logic has been moved to the bottom of the function, to allow the summary of results to be printed out regardless of the value of FORCE_SUCCESS_EXIT.
    
    * Create the skip_overall_if_fail file before running extended-platform-tests to ignore further failures to be notified in RP
    
    As of https://gitlab.cee.redhat.com/aosqe/prow-test-results-classfier/-/merge_requests/194, the data pipeline importing results from Prow to RP also creates a test suite to track the overall CI pipeline results and verify the test chain's steps are executing as expected.
    
    The overall CI test suite processing creates a test case for each step in the `test` chain. The test case is considered successful if the related step exits with 0. However, some steps are processed more fine-grained and should be allowed to fail from a given point in their execution.
    
    This commit lets the openshift-extended steps tolerate failures after all the pre-checks and preparation statements and before the extended-platform-tests suite starts running. Once it starts running, we consider further failures to be managed through the explicit JUnit XML files. This way, we avoid the need for specific JUnit files for each possible failure that is hard to predict, as they can depend on product bugs, automation issues, or other reasons.
    
    Refers OCPQE-16721
    Refers OCPQE-16722
    
    * Avoid notifying failures of the openshift-e2e-test-qe-report step in the RP's overall CI test suite

--- ci-operator/step-registry/openshift/e2e/test/qe/report/openshift-e2e-test-qe-report-commands.sh
+++ ci-operator/step-registry/openshift/e2e/test/qe/report/openshift-e2e-test-qe-report-commands.sh
@@ -6,6 +6,9 @@ set -o pipefail
 
 EXIT_STATUS=0
 
+# the exit code of this step is not expected to be caught from the overall test suite in RP. Excluding it
+touch "${ARTIFACT_DIR}/skip_overall_if_fail"
+
 # only exit 0 if junit result has no failures
 if [[ -f "${SHARED_DIR}/openshift-e2e-test-qe-report-openshift-extended-test-failures" ]]; then
     cat "${SHARED_DIR}/openshift-e2e-test-qe-report-openshift-extended-test-failures"
