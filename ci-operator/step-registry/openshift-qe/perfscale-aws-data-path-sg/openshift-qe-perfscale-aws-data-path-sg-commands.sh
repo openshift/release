2dee2db0a87ce47f3bc6c95bd1d70f8f68052ac5
Murali Krishnasamy <mukrishn@redhat.com>
2023-10-11 11:40:27 -0400

    generic for self-managed as well as managed

--- ci-operator/step-registry/openshift-qe/perfscale-aws-data-path-sg/openshift-qe-perfscale-aws-data-path-sg-commands.sh
+++ ci-operator/step-registry/openshift-qe/perfscale-aws-data-path-sg/openshift-qe-perfscale-aws-data-path-sg-commands.sh
@@ -6,21 +6,6 @@ set -o pipefail
 
 trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
 
-# Log in
-ROSA_VERSION=$(rosa version)
-ROSA_TOKEN=$(cat "${CLUSTER_PROFILE_DIR}/ocm-token")
-if [[ ! -z "${ROSA_TOKEN}" ]]; then
-  echo "Logging into ${ROSA_LOGIN_ENV} with offline token using rosa cli ${ROSA_VERSION}"
-  rosa login --env "${ROSA_LOGIN_ENV}" --token "${ROSA_TOKEN}"
-  if [ $? -ne 0 ]; then
-    echo "Login failed"
-    exit 1
-  fi
-else
-  echo "Cannot login! You need to specify the offline token ROSA_TOKEN!"
-  exit 1
-fi
-
 # Configure aws
 CLOUD_PROVIDER_REGION=${LEASED_RESOURCE}
 AWSCRED="${CLUSTER_PROFILE_DIR}/.awscred"
@@ -32,8 +17,7 @@ else
   exit 1
 fi
 
-CLUSTER_ID=$(cat "${SHARED_DIR}/cluster-id")
-CLUSTER_NAME=$(rosa describe cluster -c "${CLUSTER_ID}" -o json | jq -r '.name')
+CLUSTER_NAME=$(oc get infrastructure cluster -o json | jq -r '.status.apiServerURL' | awk -F.  '{print$2}')
 echo "Updating security group rules for data-path test on cluster $CLUSTER_NAME"
 
 VPC=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value|[0],State.Name,PrivateIpAddress,PublicIpAddress, PrivateDnsName, VpcId]' --output text | column -t | grep $CLUSTER_NAME | awk '{print $7}' | grep -v '^$' | sort -u)
@@ -45,6 +29,3 @@ do
     aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 10000-20000 --cidr 0.0.0.0/0
     aws ec2 authorize-security-group-ingress --group-id $sg --protocol udp --port 10000-20000 --cidr 0.0.0.0/0
 done
-
-
-
