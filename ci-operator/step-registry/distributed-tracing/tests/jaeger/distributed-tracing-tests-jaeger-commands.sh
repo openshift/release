d673edeb668014a3e06ba3d6090abcb64a1f0a10
Ishwar Kanse <ikanse@redhat.com>
2023-10-12 11:28:51 +0530

    debug commit

--- ci-operator/step-registry/distributed-tracing/tests/jaeger/distributed-tracing-tests-jaeger-commands.sh
+++ ci-operator/step-registry/distributed-tracing/tests/jaeger/distributed-tracing-tests-jaeger-commands.sh
@@ -17,10 +17,10 @@ curl -o hack/install/install-kuttl.sh -L https://raw.githubusercontent.com/Ishwa
 #Install kuttl
 ./hack/install/install-kuttl.sh
 
+sleep 3600
+
 # Run the e2e tests
 make run-e2e-tests ASSERT_IMG=$ASSERT_IMG VERBOSE=true USE_KIND_CLUSTER=false SKIP_ES_EXTERNAL=true JAEGER_OLM=true KAFKA_OLM=true PROMETHEUS_OLM=true CI=true PIPELINE=true E2E_TESTS_TIMEOUT=$E2E_TESTS_TIMEOUT
-
-sleep 3600
 
 JUNIT_PREFIX="junit_"
 for file in "$ARTIFACT_DIR"/*.xml; do
