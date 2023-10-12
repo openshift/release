df58bb7963e47d9a0aa097721de9578608a76e3d
Paige Rubendall <prubenda@redhat.com>
2023-10-12 09:02:35 -0400

    wrong folder

--- ci-operator/step-registry/redhat-chaos/service-disruption/redhat-chaos-service-disruption-commands.sh
+++ ci-operator/step-registry/redhat-chaos/service-disruption/redhat-chaos-service-disruption-commands.sh
@@ -28,7 +28,7 @@ export TELEMETRY_PASSWORD=$telemetry_password
 oc get nodes --kubeconfig $KRKN_KUBE_CONFIG
 
 echo $ENABLE_ALERTS
-./prow/service-disruption-scenarios/prow_run.sh
+./prow/namespace-scenarios/prow_run.sh
 rc=$?
 echo "Done running the test!" 
 echo "Return code: $rc"
