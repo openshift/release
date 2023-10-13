612a4f1232a53e18715c5e91c5d1086ad6d4d88c
Caleb Evans <caevans@redhat.com>
2023-10-13 15:59:01 -0600

    Adding logic to prepare-clusters

--- ci-operator/step-registry/mtc/prepare-clusters/mtc-prepare-clusters-commands.sh
+++ ci-operator/step-registry/mtc/prepare-clusters/mtc-prepare-clusters-commands.sh
@@ -3,4 +3,24 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-sleep 7200
+echo "Extracting cluster data."
+tar -xzvf "${SHARED_DIR}/clusters_data.tar.gz" --one-top-leve=/tmp/clusters-data
+
+# Install the Operator on both clusters
+echo "Installing the MTC operator on the source cluster."
+ansible-playbook /mtc-interop/install-mtc.yml \
+    -e version=${MTC_VERSION} \
+    -e isController=true \
+    -e kubeconfig_path=/tmp/clusters-data/aws/mtc-aws-ipi-source/auth/kubeconfig
+
+echo "Installing the MTC operator on the target cluster."
+ansible-playbook /mtc-interop/install-mtc.yml \
+    -e version=${MTC_VERSION} \
+    -e kubeconfig_path=/tmp/clusters-data/aws/mtc-aws-ipi-target/auth/kubeconfig
+
+
+# Configure the clusters prior to executing tests
+echo "Configuring the source and target clusters."
+ansible-playbook /mtc-interop/config_mtc.yml \
+    -e controller_kubeconfig=/tmp/clusters-data/aws/mtc-aws-ipi-source/auth/kubeconfig \
+    -e cluster_kubeconfig=/tmp/clusters-data/aws/mtc-aws-ipi-target/auth/kubeconfig
