9c62425ccdf583cc3d0710c5aa488546b201f3ed
Shang Gao <sgao@redhat.com>
2023-10-16 18:25:30 +0800

    debug

--- ci-operator/step-registry/cucushift/installer/check/vsphere/46245/cucushift-installer-check-vsphere-46245-commands.sh
+++ ci-operator/step-registry/cucushift/installer/check/vsphere/46245/cucushift-installer-check-vsphere-46245-commands.sh
@@ -6,6 +6,10 @@ set -o pipefail
 
 echo "Start Running Case https://polarion.engineering.redhat.com/polarion/#/project/OSE/workitem?id=OCP-46245"
 
+echo "$(date -u --rfc-3339=seconds) - Configuring govc exports..."
+# shellcheck source=/dev/null
+source "${SHARED_DIR}/govc.sh"
+
 # Check all virtual machines of cluster diskType are thin
 IFS=' ' read -r -a node_names <<<"$(oc get machines -n openshift-machine-api -o jsonpath='{.items[*].status.addresses[?(@.type=="InternalDNS")].address}')"
 for node_name in "${node_names[@]}"; do
