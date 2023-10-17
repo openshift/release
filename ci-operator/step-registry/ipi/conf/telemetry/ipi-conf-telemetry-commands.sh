0f8acc45a6644adcbd59590bd17bfe93940341ff
Justin Pierce <jupierce@redhat.com>
2023-10-17 15:31:43 -0400

    Remove cloud.openshift.com token when telemetry is disabled

--- ci-operator/step-registry/ipi/conf/telemetry/ipi-conf-telemetry-commands.sh
+++ ci-operator/step-registry/ipi/conf/telemetry/ipi-conf-telemetry-commands.sh
@@ -10,6 +10,15 @@ then
 	exit
 fi
 
+INSTALL_CONFIG="${SHARED_DIR}/install-config.yaml"
+
+# Some CI tests will assume telemetry is enabled if there is a token for cloud.openshift.com
+# https://github.com/openshift/ose/blob/0751903f21edfe65f65c5eadbdb3b24d260ea289/test/extended/prometheus/prometheus.go#L50
+# Remove it from the pull secret.
+PULL_SECRET_STAGING=$(cat "${INSTALL_CONFIG}" | yq-go '.pullSecret' | jq 'del(.auths["cloud.openshift.com"])')
+yq-go w --style folded -i "${INSTALL_CONFIG}" '.pullSecret' "${PULL_SECRET_STAGING}"
+
+
 CONFIG="${SHARED_DIR}/manifest_cluster-monitoring-config.yaml"
 PATCH="/tmp/cluster-monitoring-config.yaml.patch"
 
