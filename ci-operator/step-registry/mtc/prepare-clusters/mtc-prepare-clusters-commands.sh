21468a818726f8c1bd8ce22c7517dd458e4d9a87
Caleb Evans <caevans@redhat.com>
2023-10-18 21:44:24 -0600

    add support for direct transfer testing

--- ci-operator/step-registry/mtc/prepare-clusters/mtc-prepare-clusters-commands.sh
+++ ci-operator/step-registry/mtc/prepare-clusters/mtc-prepare-clusters-commands.sh
@@ -18,10 +18,20 @@ ansible-playbook /mtc-interop/install-mtc.yml \
     -e version=${MTC_VERSION} \
     -e kubeconfig_path=/tmp/clusters-data/aws/mtc-aws-ipi-source/auth/kubeconfig
 
-sleep 7200
+cp /mtc-interop/usr/bin/oc/oc /usr/bin/oc
+SOURCE_KUBEADMIN_PASSWORD_FILE="/tmp/clusters-data/aws/mtc-aws-ipi-target/auth/kubeadmin-password"
+SOURCE_KUBECONFIG="/tmp/clusters-data/aws/mtc-aws-ipi-target/auth/kubeconfig"
+SOURCE_KUBEADMIN_PASSWORD=$(cat $SOURCE_KUBEADMIN_PASSWORD_FILE)
+export KUBECONFIG=$SOURCE_KUBECONFIG
+oc login -u kubeadmin -p $SOURCE_KUBEADMIN_PASSWORD
+
+oc patch configs.imageregistry.operator.openshift.io/cluster --patch '{"spec":{"defaultRoute":true}}' --type=merge && sleep 10
+export EXPOSED_REGISTRY_PATH=$(oc get route default-route  -n openshift-image-registry -o jsonpath='{.spec.host}')
 
 # Configure the clusters prior to executing tests
 echo "Configuring the source and target clusters."
 ansible-playbook /mtc-interop/config_mtc.yml \
     -e controller_kubeconfig=/tmp/clusters-data/aws/mtc-aws-ipi-target/auth/kubeconfig \
-    -e cluster_kubeconfig=/tmp/clusters-data/aws/mtc-aws-ipi-source/auth/kubeconfig
+    -e cluster_kubeconfig=/tmp/clusters-data/aws/mtc-aws-ipi-source/auth/kubeconfig \
+    -e exposed_registry_path=${EXPOSED_REGISTRY_PATH}
+
