71056f11ee095e4d9c1859162a7a08c601809464
Christoph Stäbler <cstabler@redhat.com>
2023-10-20 10:34:53 +0200

    Default snyk project name to <gh-org>/<repo>

--- ci-operator/step-registry/openshift/ci-security/snyk/scan/openshift-ci-security-snyk-scan-commands.sh
+++ ci-operator/step-registry/openshift/ci-security/snyk/scan/openshift-ci-security-snyk-scan-commands.sh
@@ -13,6 +13,12 @@ chmod +x ${SNYK_DIR}/snyk
 echo snyk installed to ${SNYK_DIR}
 ${SNYK_DIR}/snyk --version
 
+CLONE_REFS=$(echo $CLONEREFS_OPTIONS | jq -r ".refs[] | select( .base_ref == \"$(git rev-parse --abbrev-ref HEAD)\" )")
+
+if [ -z "${PROJECT_NAME}" ]; then
+    PROJECT_NAME=$(echo $CLONE_REFS | jq -r "( .org + \"/\" + .repo )")
+fi
+
 snyk_deps() {
     echo Starting snyk dependencies scan
     PARAMS=(--project-name="$PROJECT_NAME" --org="$ORG_NAME")
