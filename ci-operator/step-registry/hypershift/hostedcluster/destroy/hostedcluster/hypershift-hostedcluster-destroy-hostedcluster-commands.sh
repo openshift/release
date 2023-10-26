93985368211c5bc87df2443240b4f60d3ce40c25
Bear <bear@redhat.com>
2023-10-26 11:17:12 -0230

    Remove broken clusters

--- ci-operator/step-registry/hypershift/hostedcluster/destroy/hostedcluster/hypershift-hostedcluster-destroy-hostedcluster-commands.sh
+++ ci-operator/step-registry/hypershift/hostedcluster/destroy/hostedcluster/hypershift-hostedcluster-destroy-hostedcluster-commands.sh
@@ -42,12 +42,18 @@ else
 fi
 
 createdAt=`oc -n clusters get hostedclusters $CLUSTER_NAME -o jsonpath='{.metadata.annotations.created-at}'`
-if [ -z $createdAt ] && [ "${PLATFORM}" == "aws" ]; then
-  echo Cluster is broken, skipping...
+if [ -z $createdAt ]; then
+  echo Cluster is broken.
   oc annotate -n clusters hostedcluster ${CLUSTER_NAME} "broken=true"
-  exit 0
+  if [ "$HYPERSHIFT_KEEP_BROKEN_CLUSTER" == "true" ]; then
+    echo "Keeping broken cluster"
+    exit 0
+  else
+    echo "Deleting broken cluster"
+  fi
+else
+  echo Cluster was successfully created at $createdAt
 fi
-echo Cluster successfully created at $createdAt
 
 echo "$(date) Deleting HyperShift cluster ${CLUSTER_NAME}"
 if [[ "${PLATFORM}" == "aws" ]]; then
