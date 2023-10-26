6f0dc5788400fc3d7cf0ee5c950798b4a7110fda
Christoph Stäbler <cstabler@redhat.com>
2023-10-26 20:06:10 +0200

    Snyk install jq if not installed (#44912)
    * Snyk install jq if not installed
    
    * Install jq to snyk tmp directory
    
    * Add checksum check

--- ci-operator/step-registry/openshift/ci-security/snyk/scan/openshift-ci-security-snyk-scan-commands.sh
+++ ci-operator/step-registry/openshift/ci-security/snyk/scan/openshift-ci-security-snyk-scan-commands.sh
@@ -13,6 +13,21 @@ chmod +x ${SNYK_DIR}/snyk
 echo snyk installed to ${SNYK_DIR}
 ${SNYK_DIR}/snyk --version
 
+# install jq if not installed
+if ! [ -x "$(command -v jq)" ]; then
+    JQ_CHECKSUM="2f312b9587b1c1eddf3a53f9a0b7d276b9b7b94576c85bda22808ca950569716" # https://github.com/jqlang/jq/tree/master/sig
+    curl -Lo ${SNYK_DIR}/jq "https://github.com/jqlang/jq/releases/download/jq-1.7/jq-linux-amd64"
+
+    actual_checksum=$(sha256sum ${SNYK_DIR}/jq | cut -d ' ' -f 1)
+    if [ "${actual_checksum}" != "${JQ_CHECKSUM}" ]; then
+        echo "Checksum of downloaded JQ didn't match expected checksum"
+        exit 1
+    fi
+
+    chmod +x ${SNYK_DIR}/jq
+    export PATH=${PATH}:${SNYK_DIR}
+fi
+
 CLONE_REFS=$(echo $CLONEREFS_OPTIONS | jq -r ".refs | map(select(.base_ref == \"$(git rev-parse --abbrev-ref HEAD)\")) | if length > 1 then del(.[] | select(.repo == \"release\" and .org == \"openshift\")) else . end | .[]")
 
 if [ -z "${PROJECT_NAME}" ]; then
