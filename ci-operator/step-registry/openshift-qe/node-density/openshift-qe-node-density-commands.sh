63b3695170ba6fbd967b22ae11cbedc279538249
Andrew Collins <ancollin@redhat.com>
2023-10-30 18:13:37 -0400

    Add ocp arm 24node control plane tests. (#44351)
    Removes smoke test and implement warmup runs for kube-burner control plane tests.
    Adds x86 4.14 node-density-cni

--- ci-operator/step-registry/openshift-qe/node-density/openshift-qe-node-density-commands.sh
+++ ci-operator/step-registry/openshift-qe/node-density/openshift-qe-node-density-commands.sh
@@ -16,8 +16,13 @@ ES_USERNAME=$(cat "/secret/username")
 
 git clone https://github.com/cloud-bulldozer/e2e-benchmarking --depth=1
 pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
-export EXTRA_FLAGS="--pods-per-node=$PODS_PER_NODE --pod-ready-threshold=$POD_READY_THRESHOLD"
 export WORKLOAD=node-density
+
+# A non-indexed warmup run
+ES_SERVER="" EXTRA_FLAGS="--pods-per-node=50 --pod-ready-threshold=60s" ./run.sh
+
+# The measurable run
+export EXTRA_FLAGS="--gc-metrics=true --pods-per-node=$PODS_PER_NODE --pod-ready-threshold=$POD_READY_THRESHOLD"
 
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
 
