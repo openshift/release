51552b9f7b677e8b7b162454bb043fd5b5aa9605
Vivek Shakya <vshakya@redhat.com>
2023-10-31 17:38:19 -0400

    onboard openshift pipelines on rosa classic

--- ci-operator/step-registry/openshift-pipelines/install-and-tests/openshift-pipelines-install-and-tests-commands.sh
+++ ci-operator/step-registry/openshift-pipelines/install-and-tests/openshift-pipelines-install-and-tests-commands.sh
@@ -4,17 +4,12 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-CONSOLE_URL=$(cat $SHARED_DIR/console.url)
-API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
 export gauge_reports_dir=${ARTIFACT_DIR}
 export overwrite_reports=false
 export KUBECONFIG=$SHARED_DIR/kubeconfig
 
 # Add timeout to ignore runner connection error
 gauge config runner_connection_timeout 600000 && gauge config runner_request_timeout 300000
-
-echo "Login to the cluster as kubeadmin"
-oc login -u kubeadmin -p "$(cat $SHARED_DIR/kubeadmin-password)" "${API_URL}" --insecure-skip-tls-verify=true
 
 echo "Running olm.spec to install operator"
 CATALOG_SOURCE=redhat-operators CHANNEL=${OLM_CHANNEL} gauge run --log-level=debug --verbose --tags install specs/olm.spec || true
