3014a6e63ea9d1131cac94f0509c9fad7dab40ae
lisa <liswang@redhat.com>
2023-10-31 10:49:40 -0400

    poc

--- ci-operator/step-registry/aap/tests/integration/aap-tests-integration-commands.sh
+++ ci-operator/step-registry/aap/tests/integration/aap-tests-integration-commands.sh
@@ -4,28 +4,9 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-echo "$(cat ${SECRETS_DIR}/QUAY_USER)"
 
-echo "Login into the cluster"
-CONSOLE_URL=$(cat $SHARED_DIR/console.url)
-oc login "https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443" \
-  --username="kubeadmin" \
-  --password="$(cat ${SHARED_DIR}/kubeadmin-password)" \
-  --insecure-skip-tls-verify=true
-
-echo "Run AAP Interop testing..."
-AAP_IMAGE_PULLSECRET="aap-secret"
-POD_NAME=aap-tests-pod
-CONTAINER_NAME=aap-tests-container
+echo "Run secuirty vulnerability testing..."
 IMAGE_NAME=quay.io/aap-ci/ansible-tests-integration-agent
-
-echo "Create pull secrets"
-SECRETS_DIR="/tmp/secrets/ci"
-oc create secret generic kubeconfig-secret --from-file $SHARED_DIR/kubeconfig
-oc create secret docker-registry ${AAP_IMAGE_PULLSECRET} \
-    --docker-server=quay.io \
-    --docker-username="$(cat ${SECRETS_DIR}/QUAY_USER)" \
-    --docker-password="$(cat ${SECRETS_DIR}/QUAY_PWD)"
 
 SNYK_TOKEN="$(cat $SNYK_TOKEN_PATH)"
 export SNYK_TOKEN
@@ -41,3 +22,6 @@ echo snyk installed to ${SNYK_DIR}
 ${SNYK_DIR}/snyk --version
 
 ${SNYK_DIR}/snyk container test --username="$(cat ${SECRETS_DIR}/QUAY_USER)" --password="$(cat ${SECRETS_DIR}/QUAY_PWD)" ${IMAGE_NAME} --json-file-output=${ARTIFACT_DIR}/vuln.json
+
+echo Full vulnerabilities report is available at ${ARTIFACT_DIR}/vuln.json
+
