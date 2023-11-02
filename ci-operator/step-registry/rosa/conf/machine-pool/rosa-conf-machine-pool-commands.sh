0da55e6ef0f5e9adf4aadf8484ace9a5177b7679
Penghao <pewang@redhat.com>
2023-11-02 10:14:38 +0800

    Unify rosa localzones macnine pool config with ocp (#45009)
    * Unify rosa localzones macnine pool config with ocp
    
    * Fix: not overwrite the customized label/taint

--- ci-operator/step-registry/rosa/conf/machine-pool/rosa-conf-machine-pool-commands.sh
+++ ci-operator/step-registry/rosa/conf/machine-pool/rosa-conf-machine-pool-commands.sh
@@ -77,6 +77,9 @@ fi
 LOCAL_ZONE_SWITCH=""
 if [[ "$LOCAL_ZONE" == "true" ]]; then
   LOCAL_ZONE_SWITCH=""
+  # Unify rosa localzones macnine pool config with ocp
+  LABELS="${LABELS},node-role.kubernetes.io/edge="
+  TAINTS="${TAINTS},node-role.kubernetes.io/edge=:NoSchedule"
   localzone_subnet_id=$(head -n 1 "${SHARED_DIR}/localzone_subnet_id")
   if [[ -z "${localzone_subnet_id}" ]]; then
     echo -e "The localzone_subnet_id is mandatory."
