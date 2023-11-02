74c12a7a24ebe00d35fdeca294a5f4c1a59118b5
Liang Xia <lxia@redhat.com>
2023-11-02 09:02:31 +0800

    Fix openshift-e2e-test-qe-report (#45133)

--- ci-operator/step-registry/openshift/e2e/test/qe/report/openshift-e2e-test-qe-report-commands.sh
+++ ci-operator/step-registry/openshift/e2e/test/qe/report/openshift-e2e-test-qe-report-commands.sh
@@ -4,25 +4,16 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-TEST_RESULT_FILE="${ARTIFACT_DIR}/test-results"
-
 # the exit code of this step is not expected to be caught from the overall test suite in RP. Excluding it
 touch "${ARTIFACT_DIR}/skip_overall_if_fail"
 
-if [[ -f "${SHARED_DIR}/openshift-e2e-test-qe-report-openshift-extended-test-results" ]] ; then
-    echo
-    cat "${SHARED_DIR}/openshift-e2e-test-qe-report-openshift-extended-test-results" | tee -a "${TEST_RESULT_FILE}"
-fi
+TEST_REPORT_FILE='openshift-e2e-test-qe-report'
+if [[ -f "${SHARED_DIR}/${TEST_REPORT_FILE}" ]] ; then
+    cat "${SHARED_DIR}/${TEST_REPORT_FILE}"
+    cp "${SHARED_DIR}/${TEST_REPORT_FILE}" "${ARTIFACT_DIR}/${TEST_REPORT_FILE}" || true
 
-if [[ -f "${SHARED_DIR}/openshift-e2e-test-qe-report-cucushift-results" ]] ; then
-    echo
-    cat "${SHARED_DIR}/openshift-e2e-test-qe-report-cucushift-results" | tee -a "${TEST_RESULT_FILE}"
-fi
-
-
-# only exit 0 if rest result has no 'Failing Scenarios:'
-if [[ -f "${TEST_RESULT_FILE}" ]] ; then
-    if (grep -q 'Failing Scenarios:' "${TEST_RESULT_FILE}") ; then
+    # only exit 0 if rest result has no 'Failing Scenarios:'
+    if (grep -q 'Failing Scenarios:' "${ARTIFACT_DIR}/${TEST_REPORT_FILE}") ; then
         exit 1
     fi
 fi
