8b048edc5c676f6d4f42cbcc20d527e1e81d4e99
Michael Pruitt <mpruitt@redhat.com>
2023-11-06 12:42:08 -0600

    firewatch-report-issues-ref: add secret entry for rh patterns server add --keep-job-dir option to report command for use by gitleaks detect scan add firewatch gitleaks command

--- ci-operator/step-registry/firewatch/report-issues/firewatch-report-issues-commands.sh
+++ ci-operator/step-registry/firewatch/report-issues/firewatch-report-issues-commands.sh
@@ -7,10 +7,12 @@ set -o pipefail
 # Create the Jira configuration file
 firewatch jira_config_gen --token_path "${FIREWATCH_JIRA_API_TOKEN_PATH}" --server_url "${FIREWATCH_JIRA_SERVER}"
 
-command="firewatch report"
+command="firewatch report --keep-job-dir"
 
 if [ "$FIREWATCH_FAIL_WITH_TEST_FAILURES" = "true" ]; then
     command+=" --fail_with_test_failures"
 fi
 
 eval "$command"
+
+firewatch gitleaks
