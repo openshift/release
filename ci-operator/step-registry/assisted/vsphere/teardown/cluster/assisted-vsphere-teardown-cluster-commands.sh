ff71c661e3e298d036fa1edb6f5d541477e0eaad
Adrien Gentil <agentil@redhat.com>
2023-11-07 18:13:36 +0100

    NO-ISSUE: Stop leaking vsphere password (#45398)

--- ci-operator/step-registry/assisted/vsphere/teardown/cluster/assisted-vsphere-teardown-cluster-commands.sh
+++ ci-operator/step-registry/assisted/vsphere/teardown/cluster/assisted-vsphere-teardown-cluster-commands.sh
@@ -8,8 +8,10 @@ echo "************ vsphere assisted test-infra teardown cluster command ********
 /usr/local/bin/fix_uid.sh
 
 ssh -F ${SHARED_DIR}/ssh_config "root@ci_machine" bash - << EOF |& sed -e 's/.*auths\{0,1\}".*/*** PULL_SECRET ***/g'
-set -xeuo pipefail
+set -euo pipefail
 source /root/config.sh
+
+set -x
 cd /home/assisted
 # Use minikube instead of spoke cluster
 unset KUBECONFIG
