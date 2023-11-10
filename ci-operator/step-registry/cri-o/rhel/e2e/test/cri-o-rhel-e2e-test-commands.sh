0b36db60aa1226994c01e90aa314738575c8611e
Ryan Phillips <rphillips@redhat.com>
2023-11-10 14:33:28 -0600

    ci-operator/cri-o: refactor cri-o CI to use new GCP infra
    Signed-off-by: Sohan Kunkerkar <sohank2602@gmail.com>
    Co-authored-by: Ryan Phillips <rphillips@redhat.com>

--- ci-operator/step-registry/cri-o/rhel/e2e/test/cri-o-rhel-e2e-test-commands.sh
+++ ci-operator/step-registry/cri-o/rhel/e2e/test/cri-o-rhel-e2e-test-commands.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-set -o nounset
-set -o errexit
-set -o pipefail
+set -xeuo pipefail
 
 # shellcheck source=/dev/null
 source "${SHARED_DIR}/env"
 chmod +x ${SHARED_DIR}/login_script.sh
 ${SHARED_DIR}/login_script.sh
 
-instance_name=$(<"${SHARED_DIR}/gcp-instance-ids.txt")
-
-timeout --kill-after 10m 400m gcloud compute ssh --zone="${ZONE}" ${instance_name} -- bash - << EOF 
+timeout --kill-after 10m 400m ssh "${SSHOPTS[@]}" ${IP} -- bash - <<EOF
     REPO_DIR="/home/deadbeef/cri-o"
     cd "\${REPO_DIR}/contrib/test/ci"
     ansible-playbook e2e-main.yml -i hosts -e "TEST_AGENT=prow" --connection=local -vvv --tags setup,e2e
