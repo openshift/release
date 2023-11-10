15f81521aacfe08fb02fd84246bedaaa47502e47
Patryk Matuszak <pmtk@redhat.com>
2023-11-10 13:18:30 +0100

    USHIFT-1879: Tweak PS4 for better debuggability (#45549)

--- ci-operator/step-registry/openshift/microshift/infra/iso-boot/openshift-microshift-infra-iso-boot-commands.sh
+++ ci-operator/step-registry/openshift/microshift/infra/iso-boot/openshift-microshift-infra-iso-boot-commands.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -xeuo pipefail
+export PS4='+ $(date "+%T.%N") \011'
 
 finalize() {
   scp -r "${INSTANCE_PREFIX}:/home/${HOST_USER}/microshift/_output/test-images/scenario-info" "${ARTIFACT_DIR}"
