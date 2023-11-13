f2bff789b9af77a2e88e77a9b044bcdde868a541
Michail Resvanis <mresvani@redhat.com>
2023-11-13 13:46:16 +0100

    Enhance SNO hostname change script
    This change:
    
    - adds KUBELET_NODEIP_HINT, because assisted-test-infra creates 2
      interfaces and we don't want random selection between the 2
    - reorders script functions to reflect their call order
    - fetches the etcd image from the running cluster, instead of relying to
      a hardcoded release image
    - waits for the temp etcd to be available, instead of sleep-ing for
      10s
    - fixes /var/lib/kubelet/* removal of dirs and files, after hostname
      change
    
    Signed-off-by: Michail Resvanis <mresvani@redhat.com>

--- ci-operator/step-registry/baremetalds/sno/hostname/baremetalds-sno-hostname-commands.sh
+++ ci-operator/step-registry/baremetalds/sno/hostname/baremetalds-sno-hostname-commands.sh
@@ -8,7 +8,22 @@ set -o pipefail
 # shellcheck source=/dev/null
 source "${SHARED_DIR}/packet-conf.sh"
 
-ssh "${SSHOPTS[@]}" "root@${IP}" bash - << "EOF"
+cat >"${SHARED_DIR}"/run-hostname-change-step.sh << "EOF"
+#!/usr/bin/env bash
+
+export SINGLE_NODE_IP="${SINGLE_NODE_IP:-192.168.127.10}"
+export SINGLE_NODE_NETWORK_PREFIX="$(echo ${SINGLE_NODE_IP} | cut -d '.' -f 1,2,3).0"
+
+# assisted-test-infra sets up 2 network interfaces that compete with each other
+# when setting the NODE_IP and KUBELET_NODEIP. Use KUBELET_NODEIP_HINT to
+# ensure the correct interface is chosen.
+#
+# https://access.redhat.com/articles/6956852
+ssh -o UserKnownHostsFile=/dev/null \
+  -o StrictHostKeyChecking=no \
+  -o LogLevel=ERROR \
+  "core@${SINGLE_NODE_IP}" \
+  "echo KUBELET_NODEIP_HINT=${SINGLE_NODE_NETWORK_PREFIX} | sudo tee /etc/default/nodeip-configuration"
 
 hostname_script=$(cat << "IEOF"
 #!/usr/bin/env bash
@@ -51,12 +66,26 @@ then
   systemctl stop kubelet.service
   # Forcefully remove all pods rather than just stop them, because a different hostname
   # requires new pods to be created by kubelet.
-  crictl rmp --force --all
+  until crictl rmp --force --all &> /dev/null
+  do
+    sleep 2
+  done
   systemctl stop crio.service
+
+  # manually remove multus and OVN client certs, as they are not reconciled after the hostname
+  # change and they contain the hostname in their CN
+  rm -rf /etc/cni/multus/certs/multus-client-*.pem \
+    /var/lib/ovn-ic/etc/ovnkube-node-certs/ovnkube-client-*.pem
 
   hostnamectl hostname another-hostname
 
-  cd /var/lib/kubelet && rm -rfv !\(config.json\)
+  # We should remove all files under /var/lib/kubelet, except for config.json. In order for the
+  # respective rm command to succeed, we ensure that all pod mounts have been successfully
+  # umount-ed.
+  cd /var/lib/kubelet
+  awk '$2 ~ path {print $2}' path=/var/lib/kubelet /proc/mounts |xargs --no-run-if-empty umount
+  find . ! -name 'config.json' -type f,d -exec rm -rf {} + || true
+
   reboot
   exit 0
 fi
@@ -121,3 +150,14 @@ done
 
 oc adm wait-for-stable-cluster --minimum-stable-period=5m --timeout=30m
 EOF
+
+chmod +x "${SHARED_DIR}"/run-hostname-change-step.sh
+scp "${SSHOPTS[@]}" "${SHARED_DIR}"/run-hostname-change-step.sh "root@${IP}:/usr/local/bin"
+
+timeout \
+  --kill-after 60m \
+  120m \
+  ssh \
+  "${SSHOPTS[@]}" \
+  "root@${IP}" \
+  /usr/local/bin/run-hostname-change-step.sh \
