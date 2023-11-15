63ee4b2b60d28d9ee8afe2434d224c1d8b9f6df8
Brad Williams <brad.williams@redhat.com>
2023-11-15 09:48:35 -0500

    [release-controller] Update development RBAC (again) (#45697)

--- hack/generators/release-controllers/content/rbac_resources.py
+++ hack/generators/release-controllers/content/rbac_resources.py
@@ -90,11 +90,55 @@ def _add_release_admin_cluster_role_bindings(gendoc, namespace):
     })
 
 
+def _release_controller_developers_cluster_role(gendoc):
+    gendoc.append({
+        'apiVersion': 'authorization.openshift.io/v1',
+        'kind': 'ClusterRole',
+        'metadata': {
+            'name': 'openshift-release-controller-developers'
+        },
+        'rules': [
+            {
+                'apiGroups': ['release.openshift.io'],
+                'resources': ['releasepayloads'],
+                'verbs': [
+                    'get',
+                    'list',
+                    'watch'
+                ]
+            },
+        ]
+    })
+
+
+def _add_release_controller_developers_cluster_role_bindings(gendoc, namespace):
+    gendoc.append({
+        'apiVersion': 'rbac.authorization.k8s.io/v1',
+        'kind': 'RoleBinding',
+        'metadata': {
+            'name': 'openshift-release-controller-developers-binding',
+            'namespace': namespace,
+        },
+        'roleRef': {
+            'apiGroup': 'rbac.authorization.k8s.io',
+            'kind': 'ClusterRole',
+            'name': 'openshift-release-controller-developers'
+        },
+        'subjects': [{
+            'apiGroup': 'rbac.authorization.k8s.io',
+            'kind': 'Group',
+            'name': 'openshift-release-controller-developers'
+        }]
+    })
+
+
 def generate_release_admin_rbac(config):
     with genlib.GenDoc(config.paths.path_crt_resources.joinpath('admin_generated_rbac.yaml')) as gendoc:
         _release_admins_cluster_role(gendoc)
+        _release_controller_developers_cluster_role(gendoc)
 
         for private in (False, True):
             for arch in config.arches:
                 context = Context(config, arch, private)
                 _add_release_admin_cluster_role_bindings(gendoc, context.is_namespace)
+                _add_release_controller_developers_cluster_role_bindings(gendoc, context.is_namespace)
