0a265e172bc296e29dc0f6043937361e06337008
Stuart Douglas <stuart.w.douglas@gmail.com>
2023-11-16 17:13:32 +1100

    STONEBLD-1824 add secrets for multi platform test (#45679)

--- ci-operator/step-registry/redhat-appstudio/e2e/redhat-appstudio-e2e-commands.sh
+++ ci-operator/step-registry/redhat-appstudio/e2e/redhat-appstudio-e2e-commands.sh
@@ -9,7 +9,7 @@ mkdir -p /tmp/bin
 
 export DEFAULT_QUAY_ORG DEFAULT_QUAY_ORG_TOKEN GITHUB_USER GITHUB_TOKEN QUAY_TOKEN QUAY_OAUTH_USER QUAY_OAUTH_TOKEN QUAY_OAUTH_TOKEN_RELEASE_SOURCE QUAY_OAUTH_TOKEN_RELEASE_DESTINATION OPENSHIFT_API OPENSHIFT_USERNAME OPENSHIFT_PASSWORD \
     GITHUB_ACCOUNTS_ARRAY PREVIOUS_RATE_REMAINING GITHUB_USERNAME_ARRAY GH_RATE_REMAINING PYXIS_STAGE_KEY PYXIS_STAGE_CERT BYOC_KUBECONFIG GITHUB_TOKENS_LIST OAUTH_REDIRECT_PROXY_URL CYPRESS_GH_USER CYPRESS_GH_PASSWORD CYPRESS_GH_2FA_CODE SPI_GITHUB_CLIENT_ID SPI_GITHUB_CLIENT_SECRET \
-    QE_SPRAYPROXY_HOST QE_SPRAYPROXY_TOKEN E2E_PAC_GITHUB_APP_ID E2E_PAC_GITHUB_APP_PRIVATE_KEY PAC_GITHUB_APP_WEBHOOK_SECRET SLACK_BOT_TOKEN
+    QE_SPRAYPROXY_HOST QE_SPRAYPROXY_TOKEN E2E_PAC_GITHUB_APP_ID E2E_PAC_GITHUB_APP_PRIVATE_KEY PAC_GITHUB_APP_WEBHOOK_SECRET SLACK_BOT_TOKEN MULTI_PLATFORM_AWS_ACCESS_KEY MULTI_PLATFORM_AWS_SECRET_ACCESS_KEY MULTI_PLATFORM_AWS_SSH_KEY
 
 DEFAULT_QUAY_ORG=redhat-appstudio-qe
 DEFAULT_QUAY_ORG_TOKEN=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/default-quay-org-token)
@@ -38,6 +38,9 @@ E2E_PAC_GITHUB_APP_ID=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/pac-github
 E2E_PAC_GITHUB_APP_PRIVATE_KEY=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/pac-github-app-private-key)
 PAC_GITHUB_APP_WEBHOOK_SECRET=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/pac-github-app-webhook-secret)
 SLACK_BOT_TOKEN=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/slack-bot-token)
+MULTI_PLATFORM_AWS_ACCESS_KEY=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/multi-platform-aws-access-key)
+MULTI_PLATFORM_AWS_SECRET_ACCESS_KEY=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/multi-platform-aws-secret-access-key)
+MULTI_PLATFORM_AWS_SSH_KEY=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/multi-platform-aws-ssh-key)
 
 # user stored: username:token,username:token
 IFS=',' read -r -a GITHUB_ACCOUNTS_ARRAY <<< "$(cat /usr/local/ci-secrets/redhat-appstudio-qe/github_accounts)"
