a18b1ef2f22793076a1e943c3a27636c59291225
Tomás Ó Dálaigh <tom1996.18@gmail.com>
2023-11-16 01:40:20 +0100

    Changes for gh-automation

--- hack/check-repo.sh
+++ hack/check-repo.sh
@@ -10,6 +10,10 @@ APPDATA=$(mktemp -d)
 trap 'rm -rf "${APPDATA}"' EXIT
 
 oc --context app.ci --namespace ci extract secret/openshift-prow-github-app --keys appid,cert --to "${APPDATA}"
+mkdir -p "${APPDATA}/prow-config"
+oc --context app.ci --namespace ci extract configmap/config --to "${APPDATA}/prow-config"
+mkdir -p "${APPDATA}/plugins"
+oc --context app.ci --namespace ci extract configmap/plugins --to "${APPDATA}/plugins"
 app_id=$(cat "${APPDATA}/appid")
 
 CONTAINER_ENGINE=${CONTAINER_ENGINE:-docker}
@@ -17,8 +21,10 @@ $CONTAINER_ENGINE pull registry.ci.openshift.org/ci/check-gh-automation:latest
 $CONTAINER_ENGINE run \
     --rm \
     --platform linux/amd64 \
-    -v "${APPDATA}/cert":/cert \
+    -v "${APPDATA}":/data \
     registry.ci.openshift.org/ci/check-gh-automation:latest \
     --repo="$1" \
     --bot=openshift-merge-robot --bot=openshift-ci-robot \
-    --github-app-id="$app_id" --github-app-private-key-path="/cert"
+    --config-path="/data/prow-config/config.yaml" \
+    --github-app-id="$app_id" --github-app-private-key-path="/data/cert" \
+    --plugin-config="/data/plugins/plugins.yaml" --supplemental-plugin-config-dir="/data/plugins"
