0895a733c4f90e264a3034d1be26726792780c16
Omri Bar-Haim <obarhaim@redhat.com>
2023-11-20 14:07:40 +0200

    initial commit for ACM Observability tests in step-registry and config

---
+++ ci-operator/step-registry/acm-observability/tests/acm-observability-tests-commands.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o verbose
+
+sleep 2h
+
+export KUBECONFIG=""
+export KUBEADMIN_PASSWORD=""
+
+# Login to the hub cluster as kube:admin
+oc login --username=kubeadmin --password=${KUBEADMIN_PASSWORD}
+
+# Run ACM Observability tests
+export KUBEADMIN_TOKEN=$(oc whoami -t)
+poetry run pytest -m acm_observability --cluster-name=${HUB_CLUSTER_NAME}
