bfd87f01da640c82536ce7f8a6b59bf3fe904948
Omri Bar-Haim <obarhaim@redhat.com>
2023-11-21 12:03:42 +0200

    initial commit for ACM Observability tests in step-registry and config

--- ci-operator/step-registry/acm-observability/tests/acm-observability-tests-commands.sh
+++ ci-operator/step-registry/acm-observability/tests/acm-observability-tests-commands.sh
@@ -5,6 +5,8 @@ set -o errexit
 set -o pipefail
 set -o verbose
 
+sleep 2h
+
 # Login to the ACM hub cluster as kube:admin
 KUBEADMIN_PASSWORD=$(cat ${SHARED_DIR}/${HUB_CLUSTER_NAME}/kubeadmin-password)
 oc login --username=kubeadmin --password=${KUBEADMIN_PASSWORD}
@@ -12,4 +14,18 @@ oc login --username=kubeadmin --password=${KUBEADMIN_PASSWORD}
 # Run ACM Observability tests
 KUBEADMIN_TOKEN=$(oc whoami -t)
 export KUBEADMIN_TOKEN
-poetry run pytest -m acm_observability --cluster-name=${HUB_CLUSTER_NAME}
+
+RUN_COMMAND="poetry run pytest tests \
+            -o log_cli=true \
+            --junit-xml=${ARTIFACT_DIR}/xunit_results.xml \
+            --pytest-log-file=${ARTIFACT_DIR}/pytest-tests.log \
+            -m acm-observability \
+            --data-collector=data-collector-openshift-ci.yaml "
+
+KUBECONFIG_COMMAND="--kubeconfig-file-paths="${HUB_CLUSTER_KUBECONFIG_PATH}"
+
+RUN_COMMAND+=" ${KUBECONFIG_COMMAND} "
+
+echo "$RUN_COMMAND"
+
+${RUN_COMMAND}
