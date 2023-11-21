05e9ed532c038dcc188ebabcb5c96bab1b1a9425
Omri Bar-Haim <obarhaim@redhat.com>
2023-11-21 17:03:46 +0200

    using yq to extract kubeadmin-token for acm observability tests

--- ci-operator/step-registry/acm-observability/tests/acm-observability-tests-commands.sh
+++ ci-operator/step-registry/acm-observability/tests/acm-observability-tests-commands.sh
@@ -5,26 +5,19 @@ set -o errexit
 set -o pipefail
 set -o verbose
 
-sleep 2h
+KUBECONFIG=$(cat $HUB_CLUSTER_DATA_PATH | yq .kubeconfig_path)
+KUBEADMIN_TOKEN=$(cat $HUB_CLUSTER_DATA_PATH | yq .kubeadmin_token)
 
-# Login to the ACM hub cluster as kube:admin
-KUBEADMIN_PASSWORD=$(cat ${SHARED_DIR}/${HUB_CLUSTER_NAME}/kubeadmin-password)
-oc login --username=kubeadmin --password=${KUBEADMIN_PASSWORD}
-
-# Run ACM Observability tests
-KUBEADMIN_TOKEN=$(oc whoami -t)
+export KUBECONFIG
 export KUBEADMIN_TOKEN
 
 RUN_COMMAND="poetry run pytest tests \
             -o log_cli=true \
             --junit-xml=${ARTIFACT_DIR}/xunit_results.xml \
             --pytest-log-file=${ARTIFACT_DIR}/pytest-tests.log \
-            -m acm-observability \
-            --data-collector=data-collector-openshift-ci.yaml "
-
-KUBECONFIG_COMMAND="--kubeconfig-file-paths="${HUB_CLUSTER_KUBECONFIG_PATH}"
-
-RUN_COMMAND+=" ${KUBECONFIG_COMMAND} "
+            -m ${TEST_MARKER} \
+            --cluster-name=${HUB_CLUSTER_NAME} \
+            --data-collector=data-collector-openshift-ci.yaml"
 
 echo "$RUN_COMMAND"
 
