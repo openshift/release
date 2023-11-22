21a1fd442c6d66efd66c451e6601173270890d5a
Yunfei Jiang <61686698+yunjiang29@users.noreply.github.com>
2023-11-22 11:08:39 +0800

    Remove auth output (#45793)

--- ci-operator/step-registry/sdn/conf/sdn-conf-commands.sh
+++ ci-operator/step-registry/sdn/conf/sdn-conf-commands.sh
@@ -4,7 +4,3 @@ set -o nounset
 set -o pipefail
 
 /tmp/yq -i '.networking.networkType="OpenShiftSDN"' "${SHARED_DIR}/install-config.yaml"
-
-echo "install-config.yaml"
-echo "-------------------"
-cat ${SHARED_DIR}/install-config.yaml | grep -v "password\|username\|pullSecret"
