bff12fa2b95c1055bac28f3c609e2b1f6e05db6c
Adrien Gentil <agentil@redhat.com>
2023-11-24 12:20:09 +0100

    escape EXTRA_PARAMS

--- ci-operator/step-registry/assisted/tools/multi-arch/publish/assisted-tools-multi-arch-publish-commands.sh
+++ ci-operator/step-registry/assisted/tools/multi-arch/publish/assisted-tools-multi-arch-publish-commands.sh
@@ -23,6 +23,6 @@ timeout --kill-after 10m 120m ssh "${SSHOPTS[@]}" "root@${IP}" bash -x - << EOF
 
     echo "${DOCKERFILE_IMAGE_PAIRS}" | tr -d '[:space:]' | awk -F , 'BEGIN{RS="|"}{printf("-f %s -t %s\n", \$1, \$2)}' | \
     while read -r params ; do
-      docker buildx build --platform linux/amd64,linux/arm64,linux/ppc64le,linux/s390x . ${EXTRA_PARAMS} \$params
+      docker buildx build --platform linux/amd64,linux/arm64,linux/ppc64le,linux/s390x . \${EXTRA_PARAMS} \$params
     done
 EOF
