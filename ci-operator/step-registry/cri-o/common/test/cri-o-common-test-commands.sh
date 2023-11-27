f4e9344b234a8a219c7c6e80168bcd3ad4569d13
Ryan Phillips <rphillips@redhat.com>
2023-11-27 14:39:53 -0600

    sync

--- ci-operator/step-registry/cri-o/common/test/cri-o-common-test-commands.sh
+++ ci-operator/step-registry/cri-o/common/test/cri-o-common-test-commands.sh
@@ -9,12 +9,15 @@ source "${SHARED_DIR}/env"
 chmod +x "${SHARED_DIR}/login_script.sh"
 ${SHARED_DIR}/login_script.sh
 
+ls -al /
+cd /workdir
+
 # Print the git branch
 branch=$(git rev-parse --abbrev-ref HEAD)
 echo "Printing the current branch of cri-o: $branch"
 
 # Trying to copy the content from /src
-tar -czf - /workdir | ssh "${SSHOPTS[@]}" ${IP} -- "cat > \${HOME}/cri-o.tar.gz"
+tar -czf - . | ssh "${SSHOPTS[@]}" ${IP} -- "cat > \${HOME}/cri-o.tar.gz"
 echo "Transferring source done"
 
 echo "Running remote setup command"
@@ -35,7 +38,7 @@ timeout --kill-after 10m 400m ssh "${SSHOPTS[@]}" ${IP} -- bash - <<EOF
     mkdir -p "\${REPO_DIR}"
 
     # copy the agent sources on the remote machine
-    sudo tar --strip-components=1 -xzf cri-o.tar.gz -C "\${REPO_DIR}"
+    sudo tar -xzf cri-o.tar.gz -C "\${REPO_DIR}"
     sudo chown -R deadbeef \${REPO_DIR}
     rm -f cri-o.tar.gz
     cd "\${REPO_DIR}/contrib/test/ci"
