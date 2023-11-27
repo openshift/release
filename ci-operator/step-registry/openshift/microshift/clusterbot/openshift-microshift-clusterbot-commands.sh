7be6df5c18ac6fd0045d56f33ea275b9277421bb
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2023-11-27 12:21:41 +0100

    USHIFT-1936: Automatic AMI based on region, instance and rhel version (#46027)
    * USHIFT-1936: Add static ami configuration data
    
    * USHIFT-1936: Compute AMI if its not provided with env var
    
    * USHIFT-1936: Remove AMIs from main jobs config
    
    * USHIFT-1936: Clusterbot workflow adaptations

--- ci-operator/step-registry/openshift/microshift/clusterbot/openshift-microshift-clusterbot-commands.sh
+++ ci-operator/step-registry/openshift/microshift/clusterbot/openshift-microshift-clusterbot-commands.sh
@@ -18,22 +18,6 @@ declare -A instance_types=(
 	[arm64]=t4g.large
 )
 
-## These ami's are for us-west-2 region.
-
-# ami-0d5b3039c1132e1b2   RHEL-9.2.0_HVM-20231115-x86_64-23-Hourly2-GP3
-# ami-04b4d3355a2e2a403   RHEL-9.3.0_HVM-20231101-x86_64-5-Hourly2-GP2
-declare -A x86_64_ami=(
-	["rhel-9.2"]=ami-0d5b3039c1132e1b2
-	["rhel-9.3"]=ami-04b4d3355a2e2a403
-)
-
-# ami-0addfb94c944af1cc   RHEL-9.2.0_HVM-20231115-arm64-23-Hourly2-GP3 
-# ami-0086e25ab5453b65e   RHEL-9.3.0_HVM-20231101-arm64-5-Hourly2-GP2
-declare -A arm64_ami=(
-	["rhel-9.2"]=ami-0addfb94c944af1cc
-	["rhel-9.3"]=ami-0086e25ab5453b65e
-)
-
 if [[ "${MICROSHIFT_OS}" != "rhel-9.2" ]] && [[ "${MICROSHIFT_OS}" != "rhel-9.3" ]]; then
 	echo "MICROSHIFT_OS must have value: rhel-9.2 or rhel-9.3"
 	exit 1
@@ -49,15 +33,6 @@ oc registry login -a /tmp/registry.json
 OCP_VERSION=$(oc adm release info -o=jsonpath='{.metadata.version}' -a /tmp/registry.json $RELEASE_IMAGE_LATEST | cut -d. -f1-2)
 rm -f /tmp/registry.json
 
-if [[ -z "${EC2_AMI+x}" ]] || [[ "${EC2_AMI}" == "" ]]; then
-	: EC2_AMI is empty, determining from OS and ARCH
-	if [[ "${MICROSHIFT_ARCH}" == "x86_64" ]]; then
-		EC2_AMI="${x86_64_ami[${MICROSHIFT_OS}]}"
-	else
-		EC2_AMI="${arm64_ami[${MICROSHIFT_OS}]}"
-	fi
-fi
-
 if [[ -z "${EC2_INSTANCE_TYPE+x}" ]] || [[ "${EC2_INSTANCE_TYPE}" == "" ]]; then
 	: EC2_INSTANCE_TYPE is empty, determining from ARCH
 	EC2_INSTANCE_TYPE="${instance_types[${MICROSHIFT_ARCH}]}"
@@ -68,7 +43,6 @@ cat <<EOF >"${MICROSHIFT_CLUSTERBOT_SETTINGS}"
 MICROSHIFT_OS=${MICROSHIFT_OS}
 MICROSHIFT_ARCH=${MICROSHIFT_ARCH}
 MICROSHIFT_GIT=${MICROSHIFT_GIT}
-EC2_AMI=${EC2_AMI}
 EC2_INSTANCE_TYPE=${EC2_INSTANCE_TYPE}
 OCP_VERSION=${OCP_VERSION}
 CURRENT_RELEASE=${CURRENT_RELEASE}
--- ci-operator/step-registry/openshift/microshift/infra/aws/ec2/openshift-microshift-infra-aws-ec2-commands.sh
+++ ci-operator/step-registry/openshift/microshift/infra/aws/ec2/openshift-microshift-infra-aws-ec2-commands.sh
@@ -10,6 +10,22 @@ trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wa
 #Save stacks events
 trap 'save_stack_events_to_shared' EXIT TERM INT
 
+# This map should be extended everytime AMIs from different regions/architectures/os versions
+# are added.
+declare -A ami_map=(
+  [us-west-2,x86_64,rhel-9.2]=ami-0d5b3039c1132e1b2
+  [us-west-2,x86_64,rhel-9.3]=ami-04b4d3355a2e2a403
+  [us-west-2,arm64,rhel-9.2]=ami-0addfb94c944af1cc
+  [us-west-2,arm64,rhel-9.3]=ami-0086e25ab5453b65e
+)
+
+# All graviton instances have a lower case g in the family part. Using
+# this we avoid adding the full map here.
+ARCH="x86_64"
+if [[ "${EC2_INSTANCE_TYPE%.*}" =~ .*"g".* ]]; then
+  ARCH="arm64"
+fi
+
 export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 
 REGION="${EC2_REGION:-$LEASED_RESOURCE}"
@@ -25,8 +41,7 @@ if [ -f "${MICROSHIFT_CLUSTERBOT_SETTINGS}" ]; then
 fi
 
 if [[ "${EC2_AMI}" == "" ]]; then
-  echo "must supply an AMI to use for EC2 Instance"
-  exit 1
+  EC2_AMI="${ami_map[$REGION,$ARCH,$MICROSHIFT_OS]}"
 fi
 
 ec2Type="VirtualMachine"
