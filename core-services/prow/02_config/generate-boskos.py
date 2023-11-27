eab9729eb1e55ddca559cfe752ec349b5c0847a5
Joseph Callen <jcallen@redhat.com>
2023-11-27 10:56:23 -0500

    vSphere remove unused cluster profiles (#46074)

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -213,32 +213,10 @@ CONFIG = {
     'packet-edge-quota-slice': {
         'default': 50,
     },
-    'vsphere-quota-slice':{
-        'default': 5
-    },
     'vsphere-2-quota-slice':{},
-    'vsphere-8-quota-slice':{},
-    'vsphere-ibm-7-quota-slice':{
-        'default': 5
-    },
-    'vsphere-dis-quota-slice':{},
     'vsphere-dis-2-quota-slice':{},
-    'vsphere-clusterbot-quota-slice':{},
-    'vsphere-clusterbot-2-quota-slice':{
-        'default': 5
-    },
-    'vsphere-connected-quota-slice':{},
     'vsphere-connected-2-quota-slice':{},
-    'vsphere-multizone-quota-slice':{
-        'default': 5
-    },
     'vsphere-multizone-2-quota-slice':{},
-    'vsphere-platform-none-quota-slice':{
-        'default': 5
-    },
-    'vsphere-platform-none-2-quota-slice':{
-        'default': 5
-    },
     'vsphere-8-vpn-quota-slice':{},
     'osd-ephemeral-quota-slice': {
         'default': 15,
@@ -367,17 +345,8 @@ for i in range(1, 7):
 for i in [1148,1197,1207,1225,1227,1229,1232,1233,1234,1235,1237,1238,1240,1243,1246,1249,1254,1255,1260,1271,1272,1274,1279,1284]:
     CONFIG['vsphere-2-quota-slice']['bcr03a.dal10.{}'.format(i)] = 1
 
-for i in range(230,235):
-    CONFIG['vsphere-dis-quota-slice']['devqe-segment-{}-disconnected'.format(i)] = 1
-
 for i in [990,1169,1166,1164,1146]:
     CONFIG['vsphere-dis-2-quota-slice']['bcr01a.dal12.{}'.format(i)] = 1
-
-for i in range(50,54):
-    CONFIG['vsphere-clusterbot-quota-slice']['ci-segment-{}'.format(i)] = 1
-
-for i in range(223,229):
-    CONFIG['vsphere-connected-quota-slice']['devqe-segment-{}'.format(i)] = 1
 
 for i in [871,991,1165,1154,1148,1140]:
     CONFIG['vsphere-connected-2-quota-slice']['bcr01a.dal12.{}'.format(i)] = 1
@@ -387,9 +356,6 @@ for i in [1287,1289,1296,1298,1300,1302]:
 
 for i in [1225,1232,1252,1256,1260,1261,1262,1263,1265,1272,1274,1283,1285,1305,1309]:
     CONFIG['vsphere-8-vpn-quota-slice']['bcr01a.dal10.{}'.format(i)] = 1
-
-for i in range(200,213):
-    CONFIG['vsphere-8-quota-slice']['ci-segment-{}'.format(i)] = 1
 
 
 config = {
