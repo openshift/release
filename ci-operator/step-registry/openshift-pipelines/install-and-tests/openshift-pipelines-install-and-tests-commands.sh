76840f9b64e790bc0802644b79ae96fab4cabfce
oharan2 <oharan@redhat.com>
2023-11-28 11:22:16 +0200

    Add sleep for debug

--- ci-operator/step-registry/openshift-pipelines/install-and-tests/openshift-pipelines-install-and-tests-commands.sh
+++ ci-operator/step-registry/openshift-pipelines/install-and-tests/openshift-pipelines-install-and-tests-commands.sh
@@ -6,10 +6,10 @@ set -o pipefail
 
 CONSOLE_URL=$(cat $SHARED_DIR/console.url)
 API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
-GOPROXY="https://proxy.golang.org/"
 export gauge_reports_dir=${ARTIFACT_DIR}
 export overwrite_reports=false
 export KUBECONFIG=$SHARED_DIR/kubeconfig
+export GOPROXY="https://proxy.golang.org/"
 
 # Add timeout to ignore runner connection error
 gauge config runner_connection_timeout 600000 && gauge config runner_request_timeout 300000
@@ -17,6 +17,7 @@ gauge config runner_connection_timeout 600000 && gauge config runner_request_tim
 echo "Login to the cluster as kubeadmin"
 oc login -u kubeadmin -p "$(cat $SHARED_DIR/kubeadmin-password)" "${API_URL}" --insecure-skip-tls-verify=true
 
+sleep 4h
 echo "Running olm.spec to install operator"
 CATALOG_SOURCE=redhat-operators CHANNEL=${OLM_CHANNEL} gauge run --log-level=debug --verbose --tags install specs/olm.spec || true
 
