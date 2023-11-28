8102f0e42b6c7d123fd4c1aef70ccfa04b02cf02
Ryan Phillips <rphillips@redhat.com>
2023-11-28 11:03:50 -0600

    sync

--- ci-operator/step-registry/cri-o/gather/cri-o-gather-commands.sh
+++ ci-operator/step-registry/cri-o/gather/cri-o-gather-commands.sh
@@ -7,19 +7,18 @@ chmod +x ${SHARED_DIR}/login_script.sh
 ${SHARED_DIR}/login_script.sh
 
 timeout --kill-after 10m 400m ssh "${SSHOPTS[@]}" ${IP} -- bash - <<EOF
-    sudo chown -R deadbeef /tmp/*
+    sudo chown -R ${SSH_USER} /tmp/*
     sudo chmod -R 777 /tmp/artifacts/*
 EOF
 
-
 function getlogs() {
-  echo "### Downloading logs..."
-  for i in "${!SSHOPTS[@]}"; do
-  if [[ ${SSHOPTS[i]} == "-l" && ${SSHOPTS[i+1]} == "deadbeef" ]]; then
-      SSHOPTS[i+1]="100"
-  fi
-  done
-  scp -r "${SSHOPTS[@]}" "deadbeef@${IP}:/tmp/artifacts/*" "${ARTIFACT_DIR}"
+	echo "### Downloading logs..."
+	for i in "${!SSHOPTS[@]}"; do
+		if [[ ${SSHOPTS[i]} == "-l" && ${SSHOPTS[i + 1]} == "${SSH_USER}" ]]; then
+			SSHOPTS[i + 1]="100"
+		fi
+	done
+	scp -O -r "${SSHOPTS[@]}" "${SSH_USER}@${IP}:/tmp/artifacts/*" "${ARTIFACT_DIR}"
 }
 
 # Gather logs regardless of what happens after this
