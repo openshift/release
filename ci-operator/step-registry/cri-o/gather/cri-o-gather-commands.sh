f7f534e25b94c73c2557cd499475bfed35563052
Sohan Kunkerkar <sohank2602@gmail.com>
2023-11-28 13:34:03 -0500

    ci-operator/step-registry/cri-o: fix SSHOPTS while gathering logs (#46104)
    Signed-off-by: Sohan Kunkerkar <sohank2602@gmail.com>

--- ci-operator/step-registry/cri-o/gather/cri-o-gather-commands.sh
+++ ci-operator/step-registry/cri-o/gather/cri-o-gather-commands.sh
@@ -7,19 +7,20 @@ chmod +x ${SHARED_DIR}/login_script.sh
 ${SHARED_DIR}/login_script.sh
 
 timeout --kill-after 10m 400m ssh "${SSHOPTS[@]}" ${IP} -- bash - <<EOF
-    sudo chown -R deadbeef /tmp/*
+    sudo chown -R ${USER} /tmp/*
     sudo chmod -R 777 /tmp/artifacts/*
 EOF
 
 
 function getlogs() {
-  echo "### Downloading logs..."
-  for i in "${!SSHOPTS[@]}"; do
-  if [[ ${SSHOPTS[i]} == "-l" && ${SSHOPTS[i+1]} == "deadbeef" ]]; then
-      SSHOPTS[i+1]="100"
-  fi
-  done
-  scp -r "${SSHOPTS[@]}" "deadbeef@${IP}:/tmp/artifacts/*" "${ARTIFACT_DIR}"
+    echo "### Downloading logs..."
+    for ((i = 0; i < ${#SSHOPTS[@]}; i++)); do
+      if [[ ${SSHOPTS[i]} == "-l" && ${SSHOPTS[i + 1]} == "${USER}" ]]; then
+        # Remove the "-l" and "deadbeef" options from the SSHOPTS array
+        SSHOPTS=("${SSHOPTS[@]:0:i}" "${SSHOPTS[@]:i + 2}")
+      fi
+    done
+    scp -rv "${SSHOPTS[@]}" "${USER}@${IP}:/tmp/artifacts/*" "${ARTIFACT_DIR}"
 }
 
 # Gather logs regardless of what happens after this
