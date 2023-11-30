5932e138744c5e17b9643277ed99966d2933249b
Liang Xia <lxia@redhat.com>
2023-11-30 09:37:48 +0800

    OCPQE-17643: Save all kinds of cucushift test results in yaml format (#46181)

--- ci-operator/step-registry/cucushift/e2e/destructive/cucushift-e2e-destructive-commands.sh
+++ ci-operator/step-registry/cucushift/e2e/destructive/cucushift-e2e-destructive-commands.sh
@@ -31,7 +31,7 @@ cucumber --tags "${E2E_RUN_TAGS} and ${E2E_SKIP_TAGS} and @destructive and @cons
 set +x
 
 # summarize test results
-echo "Summarizing test result..."
+echo "Summarizing test results..."
 failures=0 errors=0 skipped=0 tests=0
 grep -r -E -h -o 'testsuite.*tests="[0-9]+"' "${ARTIFACT_DIR}" | tr -d '[A-Za-z=\"_]' > /tmp/zzz-tmp.log
 while read -a row ; do
@@ -39,10 +39,20 @@ while read -a row ; do
     let failures+=${row[0]} errors+=${row[1]} skipped+=${row[2]} tests+=${row[3]} || true
 done < /tmp/zzz-tmp.log
 
-TEST_RESULT_FILE="${ARTIFACT_DIR}/test-results"
-echo -e "\nfailures: $failures, errors: $errors, skipped: $skipped, tests: $tests in cucushift-e2e-destructive" | tee -a "${TEST_RESULT_FILE}"
+TEST_RESULT_FILE="${ARTIFACT_DIR}/test-results.yaml"
+cat > "${TEST_RESULT_FILE}" <<- EOF
+cucushift:
+  total: $tests
+  failures: $failures
+  errors: $errors
+  skipped: $skipped
+EOF
+
 if [ $((failures)) != 0 ] ; then
-    echo "Failing Scenarios:" | tee -a "${TEST_RESULT_FILE}"
-    grep -h -r -E 'cucumber.*features/.*.feature' "${ARTIFACT_DIR}/.." | grep -v grep | cut -d'#' -f2 | sort -t':' -k3 | sed -E 's/^( +)?/  /' | tee -a "${TEST_RESULT_FILE}" || true
+    echo '  failingScenarios:' >> "${TEST_RESULT_FILE}"
+    readarray -t failingscenarios < <(grep -h -r -E 'cucumber.*features/.*.feature' "${ARTIFACT_DIR}/.." | cut -d':' -f3- | sed -E 's/^( +)//' | sort)
+    for (( i=0; i<failures; i++ )) ; do
+        echo "    - ${failingscenarios[$i]}" >> "${TEST_RESULT_FILE}"
+    done
 fi
-cat "${TEST_RESULT_FILE}" >> "${SHARED_DIR}/openshift-e2e-test-qe-report" || true
+cat "${TEST_RESULT_FILE}" | tee -a "${SHARED_DIR}/openshift-e2e-test-qe-report" || true
