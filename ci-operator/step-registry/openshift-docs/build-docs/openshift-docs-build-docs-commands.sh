4e2984251ae4b8fd6c659e3250dfa03601bd62fa
Aidan Reilly <74046732+aireilly@users.noreply.github.com>
2023-12-01 17:45:52 +0000

    adding openshift-docs build to CI (#45449)
    adding build_root.project_image.inputs.src to config
    
    adding base_image
    
    test
    
    cleanup
    
    add images.inputs.src.paths.source_path
    
    updates
    
    change to dockerfile_literal
    
    don't use repo Dockerfile
    
    Adding netlify ref step
    
    make src available to test steps
    
    Adding from_image to ref steps
    
    fix path typo
    
    no need to grep the AUTH secret, just export it
    
    cat the secret
    
    Adding bot build comment
    
    adjust build_root
    
    refactor preview comment as seperate step
    
    Adding complete distro logic + build from Travis
    
    change ref step and update main config
    
    adjust build config
    
    update
    
    remove base_images it's not required
    
    put base_images back
    
    Adding prow-test jobs
    
    removing tests from main
    
    Add lint-topicmaps ref step
    
    remove main for now
    
    modularize build step

---
+++ ci-operator/step-registry/openshift-docs/build-docs/openshift-docs-build-docs-commands.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o verbose
+
+NETLIFY_AUTH_TOKEN=$(cat /tmp/vault/ocp-docs-netlify-secret/NETLIFY_AUTH_TOKEN)
+
+export NETLIFY_AUTH_TOKEN
+
+echo "Building preview for PR#${PULL_NUMBER}..."
+
+git branch -m latest
+
+asciibinder build -d ${DISTRO}
+
+cp scripts/ocpdocs/_previewpage _preview/index.html
+
+cp scripts/ocpdocs/robots_preview.txt _preview/robots.txt
+
+# Touch ${SHARED_DIR}/NETLIFY_SUCCESS if the build succeeds
+netlify deploy --site ocpdocs-pr --auth ${NETLIFY_AUTH_TOKEN} --alias ${PULL_NUMBER} --dir=_preview && touch ${SHARED_DIR}/NETLIFY_SUCCESS
