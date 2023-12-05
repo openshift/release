bbff1e02d000c9952296eb6c127b286127576c8f
Vivek Shakya <vshakya@redhat.com>
2023-12-05 13:07:25 -0500

    service mesh operator tests on hypershift

--- ci-operator/step-registry/servicemesh/maistra-test-tool/execute/servicemesh-maistra-test-tool-execute-commands.sh
+++ ci-operator/step-registry/servicemesh/maistra-test-tool/execute/servicemesh-maistra-test-tool-execute-commands.sh
@@ -6,13 +6,13 @@ set -o pipefail
 
 CONSOLE_URL=$(cat $SHARED_DIR/console.url)
 OCP_API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
-OCP_CRED_USR="kubeadmin"
-OCP_CRED_PSW="$(cat ${SHARED_DIR}/kubeadmin-password)"
+# OCP_CRED_USR="kubeadmin"
+# OCP_CRED_PSW="$(cat ${SHARED_DIR}/kubeadmin-password)"
 
-export OCP_API_URL
-export OCP_CRED_USR
-export OCP_CRED_PSW
-
+# export OCP_API_URL
+# export OCP_CRED_USR
+# export OCP_CRED_PSW
+sleep 1800
 make test
 
 echo "Copying logs and xmls to ${ARTIFACT_DIR}"
