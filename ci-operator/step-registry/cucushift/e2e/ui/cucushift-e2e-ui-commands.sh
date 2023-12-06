474d2c805b62059a70b8b2384025845cf79c05ed
Liang Xia <lxia@redhat.com>
2023-12-06 14:33:41 +0800

    OCPQE-17643: Add test type to identity different cucushift tests (#46359)

--- ci-operator/step-registry/cucushift/e2e/ui/cucushift-e2e-ui-commands.sh
+++ ci-operator/step-registry/cucushift/e2e/ui/cucushift-e2e-ui-commands.sh
@@ -52,6 +52,7 @@ done < /tmp/zzz-tmp.log
 TEST_RESULT_FILE="${ARTIFACT_DIR}/test-results.yaml"
 cat > "${TEST_RESULT_FILE}" <<- EOF
 cucushift:
+  type: cucushift-e2e-ui
   total: $tests
   failures: $failures
   errors: $errors
