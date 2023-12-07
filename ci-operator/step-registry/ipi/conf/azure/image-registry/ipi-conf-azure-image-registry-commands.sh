b432341e72015c016f0703c72b035ad54731f383
jima <jima@redhat.com>
2023-12-07 11:50:35 +0800

    debug

---
+++ ci-operator/step-registry/ipi/conf/azure/image-registry/ipi-conf-azure-image-registry-commands.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+
+CONFIG="${SHARED_DIR}/install-config.yaml"
+
+vnet_rg=$(yq-go r ${CONFIG} 'platform.azure.networkResourceGroupName')
+vnet_name=$(yq-go r ${CONFIG} '.platform.azure.virtualNetwork')
+compute_subnet=$(yq-go r ${CONFIG} '.platform.azure.computeSubnet')
+
+cat > ${SHARED_DIR}/imageregistry-config.yaml <<EOF
+apiVersion: imageregistry.operator.openshift.io/v1
+kind: Config
+metadata:
+  name: cluster
+spec:
+  managementState: "Managed"
+  replicas: 2
+  rolloutStrategy: RollingUpdate
+  storage:
+    azure:
+      networkAccess:
+        internal:
+          networkResourceGroupName: ${vnet_rg}
+          subnetName: ${compute_subnet}
+          vnetName: ${vnet_name}
+        type: Internal
+EOF
