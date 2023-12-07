4393776aea5089766b04c6fa40932caefbbc1423
Dongbo Yan <dyan@redhat.com>
2023-12-07 12:46:45 +0800

    Add test for quay nightly build based on qe index (#46464)
    * Add test for quay nightly build based on qe index
    
    * make new job
    
    * make ci-operator-config
    
    ---------
    
    Co-authored-by: dyan <dyan@.redhat.com>

--- ci-operator/step-registry/quay-tests/deploy-quay-aws-s3/quay-tests-deploy-quay-aws-s3-commands.sh
+++ ci-operator/step-registry/quay-tests/deploy-quay-aws-s3/quay-tests-deploy-quay-aws-s3-commands.sh
@@ -6,6 +6,7 @@ set -o pipefail
 
 #Create AWS S3 Storage Bucket
 QUAY_OPERATOR_CHANNEL="$QUAY_OPERATOR_CHANNEL"
+QUAY_OPERATOR_SOURCE="$QUAY_OPERATOR_SOURCE"
 QUAY_AWS_S3_BUCKET="quayprowci$RANDOM"
 
 QUAY_AWS_ACCESS_KEY=$(cat /var/run/quay-qe-aws-secret/access_key)
@@ -83,7 +84,7 @@ spec:
   installPlanApproval: Automatic
   name: quay-operator
   channel: $QUAY_OPERATOR_CHANNEL
-  source: redhat-operators
+  source: $QUAY_OPERATOR_SOURCE
   sourceNamespace: openshift-marketplace
 EOF
 )
