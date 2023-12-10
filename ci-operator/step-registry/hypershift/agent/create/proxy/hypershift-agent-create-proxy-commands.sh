21c798a597b95eb9a1cdca2c328dc43f5eb1a8e4
Liangquan Li <liangli@redhat.com>
2023-12-10 14:05:09 +0800

    Adjust hypershift-agent-create-proxy (#46604)

--- ci-operator/step-registry/hypershift/agent/create/proxy/hypershift-agent-create-proxy-commands.sh
+++ ci-operator/step-registry/hypershift/agent/create/proxy/hypershift-agent-create-proxy-commands.sh
@@ -11,19 +11,11 @@ source "${SHARED_DIR}/packet-conf.sh" && scp "${SSHOPTS[@]}" "${SHARED_DIR}/nest
 ssh "${SSHOPTS[@]}" "root@${IP}" bash - << 'EOF' |& sed -e 's/.*auths.*/*** PULL_SECRET ***/g'
 set -x
 
-API_SERVER=$(cat nested_kubeconfig | yq ".clusters[0].cluster.server")
-EXTRACTED_API_SERVER=""
-if [[ "${API_SERVER}" =~ [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ ]]; then
-  echo "It is an IPv4 address: ${API_SERVER}"
-  EXTRACTED_API_SERVER=$(echo "${API_SERVER}" | grep -oP '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
-elif [[ "${API_SERVER}" =~ [0-9a-fA-F:]+ ]]; then
-  echo "It is an IPv6 address: ${API_SERVER}"
-  EXTRACTED_API_SERVER=$(echo "${API_SERVER}" | grep -oP '\[.*?\]')
-else
-  echo "It is a domain address: ${API_SERVER}"
-  EXTRACTED_API_SERVER=".$(echo "${API_SERVER}" | grep -oP '(?<=://)([^:/]+)')"
+API_SERVER=$(cat nested_kubeconfig | yq ".clusters[0].cluster.server" | sed 's|^http[s]*://||' | sed 's|:[0-9]*$||')
+if [[ ! $API_SERVER =~ \[ && ! $API_SERVER =~ \] && ! $API_SERVER =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    API_SERVER=".${API_SERVER}"
 fi
-sed -i "1 s|$| $EXTRACTED_API_SERVER|" $HOME/squid.conf
+sed -i "1 s|$| $API_SERVER|" $HOME/squid.conf
 cat $HOME/squid.conf
 
 sudo setenforce 0
