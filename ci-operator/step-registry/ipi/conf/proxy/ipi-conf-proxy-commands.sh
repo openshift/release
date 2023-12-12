6425a5a914477710e5978286ce4496f10d405e2e
Li Zhang <lzha@redhat.com>
2023-12-12 10:55:48 +0800

    rollback the public proxy to private proxy

--- ci-operator/step-registry/ipi/conf/proxy/ipi-conf-proxy-commands.sh
+++ ci-operator/step-registry/ipi/conf/proxy/ipi-conf-proxy-commands.sh
@@ -4,15 +4,15 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-if [[ ! -f "${SHARED_DIR}/proxy_public_url" ]]; then
-  echo "'${SHARED_DIR}/proxy_public_url' not found, abort." && exit 1
+if [[ ! -f "${SHARED_DIR}/proxy_private_url" ]]; then
+  echo "'${SHARED_DIR}/proxy_private_url' not found, abort." && exit 1
 fi
 
 CONFIG="${SHARED_DIR}/install-config.yaml"
 
-proxy_public_url=$(<"${SHARED_DIR}/proxy_public_url")
+proxy_private_url=$(<"${SHARED_DIR}/proxy_private_url")
 cat >>"${CONFIG}" <<EOF
 proxy:
-  httpProxy: ${proxy_public_url}
-  httpsProxy: ${proxy_public_url}
+  httpProxy: ${proxy_private_url}
+  httpsProxy: ${proxy_private_url}
 EOF
