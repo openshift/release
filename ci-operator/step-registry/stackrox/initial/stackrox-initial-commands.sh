0a25386369bd27a5d38aee77415b370135e7365f
davdhacs <105243888+davdhacs@users.noreply.github.com>
2023-12-11 17:42:49 -0700

    try repro dropin

--- ci-operator/step-registry/stackrox/initial/stackrox-initial-commands.sh
+++ ci-operator/step-registry/stackrox/initial/stackrox-initial-commands.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 job="${TEST_SUITE:-${JOB_NAME_SAFE#merge-}}"
 job="${job#nightly-}"
-exec .openshift-ci/dispatch.sh "${job}"
+
+ns="testname"
+oc patch scc anyuid --type merge -p '{
+    "allowHostDirVolumePlugin": true,
+    "allowHostNetwork": true,
+    "allowHostPorts": true,
+    "allowPrivilegedContainer": true,
+    "allowedCapabilities": [ "*" ],
+    "requiredDropCapabilities": []
+}'
+
+
+oc create ns "${ns}"
+oc patch scc anyuid --type json -p '[{ "op": "add", "path": "/users/-", "value": "system:serviceaccount:'"${ns}"':default" }]'
+oc -n "${ns}" create deployment nginx --image=nginx
+time oc -n "${ns}" rollout status deployment nginx -w
+
+
+#exec .openshift-ci/dispatch.sh "${job}"
