785801a3425f3a71a8944bf9a0f6d9b590440d78
davdhacs <105243888+davdhacs@users.noreply.github.com>
2023-12-12 10:42:59 -0700

    push into interop op run

--- ci-operator/step-registry/stackrox/operator-e2e/stackrox-operator-e2e-commands.sh
+++ ci-operator/step-registry/stackrox/operator-e2e/stackrox-operator-e2e-commands.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
 job="${TEST_SUITE:-${JOB_NAME_SAFE#merge-}}"
 job="${job#nightly-}"
+
+set -x
+set +eu
+
+tee operator/tests/run.sh <<EOF
+#!/usr/bin/env bash
+
+set -x
+ns="testname"
+oc patch scc anyuid --type merge -p '{
+    "allowHostDirVolumePlugin": true,
+    "allowHostNetwork": true,
+    "allowHostPorts": true,
+    "allowPrivilegedContainer": true,
+    "allowedCapabilities": [ "*" ],
+    "requiredDropCapabilities": []
+}'
+
+
+oc create ns "${ns}"
+oc patch scc anyuid --type json -p '[{ "op": "add", "path": "/users/-", "value": "system:serviceaccount:'"${ns}"':default" }]'
+oc -n "${ns}" create deployment nginx --image=nginx
+time oc -n "${ns}" rollout status deployment nginx -w
+
+EOF
+
+echo "Check script:"
+cat operator/tests/run.sh
+
+echo "Dispatch..."
+
 exec .openshift-ci/dispatch.sh "${job}"
