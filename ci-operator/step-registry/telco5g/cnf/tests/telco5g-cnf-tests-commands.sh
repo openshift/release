50250248710427f4cb4f97f61f20e2622970153c
Sandeep Yadav <sandyada@redhat.com>
2023-12-13 16:21:46 +0530

    Update our step for 4-15 branching (#46536)
    https://issues.redhat.com/browse/CNF-10592

--- ci-operator/step-registry/telco5g/cnf/tests/telco5g-cnf-tests-commands.sh
+++ ci-operator/step-registry/telco5g/cnf/tests/telco5g-cnf-tests-commands.sh
@@ -76,6 +76,19 @@ xt_u32 "Validate the module is enabled and works Should create an iptables rule 
 EOF
 }
 
+function create_tests_temp_skip_list_16 {
+# List of temporarly skipped tests for 4.16
+cat <<EOF >>"${SKIP_TESTS_FILE}"
+# <feature> <test name>
+
+# SKIPTEST
+# bz### https://issues.redhat.com/browse/OCPBUGS-10927
+# TESTNAME
+xt_u32 "Validate the module is enabled and works Should create an iptables rule inside a pod that has the module enabled"
+
+EOF
+}
+
 function is_bm_node {
     node=$1
 
@@ -250,9 +263,9 @@ fi
 export CNF_E2E_TESTS
 export CNF_ORIGIN_TESTS
 
-if [[ "$T5CI_VERSION" == "4.15" ]]; then
+if [[ "$T5CI_VERSION" == "4.16" ]]; then
     export CNF_BRANCH="master"
-    export CNF_TESTS_IMAGE="cnf-tests:4.14"
+    export CNF_TESTS_IMAGE="cnf-tests:4.15"
 else
     export CNF_BRANCH="release-${T5CI_VERSION}"
     export CNF_TESTS_IMAGE="cnf-tests:${T5CI_VERSION}"
@@ -270,7 +283,7 @@ if [[ ! -d "${CNF_REPO_DIR}" ]]; then
 fi
 
 pushd $CNF_REPO_DIR
-if [[ "$T5CI_VERSION" == "4.15" ]]; then
+if [[ "$T5CI_VERSION" == "4.15" ]] || [[ "$T5CI_VERSION" == "4.16" ]]; then
     echo "Updating all submodules for >=4.15 versions"
     # git version 1.8 doesn't work well with forked repositories, requires a specific branch to be set
     sed -i "s@https://github.com/openshift/metallb-operator.git@https://github.com/openshift/metallb-operator.git\n        branch = main@" .gitmodules
@@ -306,8 +319,12 @@ if [[ "$CNF_BRANCH" == *"4.14"* ]]; then
     create_tests_temp_skip_list_14
     export GINKGO_PARAMS=" --ginkgo.timeout 230m -ginkgo.slowSpecThreshold=0.001 -ginkgo.v -ginkgo.show-node-events --ginkgo.json-report ${ARTIFACT_DIR}/test_ginkgo.json --ginkgo.flake-attempts 4"
 fi
-if [[ "$CNF_BRANCH" == *"4.15"* ]] || [[ "$CNF_BRANCH" == *"master"* ]]; then
+if [[ "$CNF_BRANCH" == *"4.15"* ]]; then
     create_tests_temp_skip_list_15
+    export GINKGO_PARAMS=" --timeout 230m -slow-spec-threshold=0.001s -v --show-node-events --json-report test_ginkgo.json --flake-attempts 4"
+fi
+if [[ "$CNF_BRANCH" == *"4.16"* ]] || [[ "$CNF_BRANCH" == *"master"* ]]; then
+    create_tests_temp_skip_list_16
     export GINKGO_PARAMS=" --timeout 230m -slow-spec-threshold=0.001s -v --show-node-events --json-report test_ginkgo.json --flake-attempts 4"
 fi
 cp "$SKIP_TESTS_FILE" "${ARTIFACT_DIR}/"
