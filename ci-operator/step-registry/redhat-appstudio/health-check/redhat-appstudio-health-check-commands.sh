e906a447717c569d8080f1ad85ce98e62d5d9c75
Pavel Sturc <psturc@redhat.com>
2023-12-15 22:39:15 +0100

    fix: rhtap-health-check for periodic jobs (#46837)

--- ci-operator/step-registry/redhat-appstudio/health-check/redhat-appstudio-health-check-commands.sh
+++ ci-operator/step-registry/redhat-appstudio/health-check/redhat-appstudio-health-check-commands.sh
@@ -9,5 +9,13 @@ GITHUB_TOKEN=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/github-token)
 
 cd "$(mktemp -d)"
 git clone --branch main https://github.com/redhat-appstudio/qe-tools .
-make build
-./qe-tools prowjob health-check --notify-on-pr=true --fail-if-unhealthy=false
+make install
+
+command=(qe-tools prowjob health-check --fail-if-unhealthy=false)
+
+# flag "notify-on-pr" is valid only for "presubmit" type of job (which is triggered from a PR)
+if [ "${JOB_TYPE:-}" = "presubmit" ]; then
+    command+=(--notify-on-pr=true)
+fi
+
+"${command[@]}"
