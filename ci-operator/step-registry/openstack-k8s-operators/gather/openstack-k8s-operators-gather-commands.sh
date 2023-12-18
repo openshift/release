2c58019029c95c14f0b086ab4d8e87f449854a7c
Francesco Pantano <fpantano@redhat.com>
2023-12-18 14:00:51 +0100

    Update openstack-must-gather in Prow to gather Additional Namespaces (#46865)
    As per the recent change in [1], we need to update the Prow jobs to
    include the ADDITIONAL_NAMESPACES variable and invoke the must-gather
    passing the required environment variables.
    
    [1] https://github.com/openstack-k8s-operators/openstack-must-gather/pull/31
    
    Signed-off-by: Francesco Pantano <fpantano@redhat.com>

--- ci-operator/step-registry/openstack-k8s-operators/gather/openstack-k8s-operators-gather-commands.sh
+++ ci-operator/step-registry/openstack-k8s-operators/gather/openstack-k8s-operators-gather-commands.sh
@@ -5,11 +5,10 @@ set +eu
 
 MUST_GATHER_TIMEOUT=${MUST_GATHER_TIMEOUT:-"5m"}
 
-# OCP must-gather
 mkdir -p ${ARTIFACT_DIR}/must-gather/
-oc --insecure-skip-tls-verify adm must-gather --timeout=$MUST_GATHER_TIMEOUT \
---dest-dir ${ARTIFACT_DIR}/must-gather > ${ARTIFACT_DIR}/must-gather/must-gather.log
 
-# OSP must-gather
-oc --insecure-skip-tls-verify adm must-gather --image=quay.io/openstack-k8s-operators/openstack-must-gather:latest --timeout=$MUST_GATHER_TIMEOUT \
---dest-dir ${ARTIFACT_DIR}/must-gather > ${ARTIFACT_DIR}/must-gather/openstack-must-gather.log
+# Run the must-gather command
+oc --insecure-skip-tls-verify adm must-gather --image-stream=openshift/must-gather \
+    --image=quay.io/openstack-k8s-operators/openstack-must-gather:latest \
+    --timeout=$MUST_GATHER_TIMEOUT \
+    --dest-dir ${ARTIFACT_DIR}/must-gather -- ADDITIONAL_NAMESPACES=kuttl,sushy-emulator gather &> ${ARTIFACT_DIR}/must-gather/openstack-must-gather.log
