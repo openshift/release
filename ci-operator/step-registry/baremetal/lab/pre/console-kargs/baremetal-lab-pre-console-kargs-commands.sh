ecc82078007b954f2701f0fd28a2113c2bdb0047
Alessandro Di Stefano <aleskandro@redhat.com>
2023-12-19 19:46:48 +0100

    OCPQE-15772: Pivot new Baremetal QE infra (#46885)
    * OCPQE-15772: Pivot new Baremetal QE infra
    
    * Remove set -x in agent steps

--- ci-operator/step-registry/baremetal/lab/pre/console-kargs/baremetal-lab-pre-console-kargs-commands.sh
+++ ci-operator/step-registry/baremetal/lab/pre/console-kargs/baremetal-lab-pre-console-kargs-commands.sh
@@ -37,7 +37,7 @@ for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
   mac_prefix=${mac//:/-}
   role=${name%%-[0-9]*}
   role=${role%%-a*}
-  echo "Rendering ignition for ${name} (${role}) - ${bmc_address}..."
+  echo "Rendering ignition for ${name} (${role}) - #${host}..."
   butane --strict --raw -o "${SHARED_DIR}/${mac_prefix}-console-hook.ign" <<EOF
 variant: fcos
 version: 1.3.0
