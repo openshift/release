976099e0ad843b36b4ba03cf0f2204f45cfd0d21
Liang Xia <lxia@redhat.com>
2023-12-21 00:52:16 +0800

    OCPQE-18426: Optimize the test filter for cucushift (#46912)

--- ci-operator/step-registry/cucushift/e2e/devel/cucushift-e2e-devel-commands.sh
+++ ci-operator/step-registry/cucushift/e2e/devel/cucushift-e2e-devel-commands.sh
@@ -5,84 +5,118 @@ set -o errexit
 set -o pipefail
 
 PARALLEL_CUCUMBER_OPTIONS='--verbose-process-command --first-is-1 --type cucumber --serialize-stdout --combine-stderr --prefix-output-with-test-env-number'
-FORCE_SKIP_TAGS="customer security"
+CUCUSHIFT_FORCE_SKIP_TAGS="customer security"
 
-function show_time_used() {
+function show_test_execution_time() {
     local test_type time_used
     test_type="$1"
-
     time_used="$(( ${SECONDS}/60 ))"
     echo "###### ${test_type} tests took ${time_used} minutes"
 }
-
-if [ -f "${SHARED_DIR}/kubeconfig" ] ; then
-    export KUBECONFIG=${SHARED_DIR}/kubeconfig
-fi
-cp -Lrvf "${KUBECONFIG}" /tmp/kubeconfig
-
-if ! which kubectl; then
-    mkdir /tmp/bin
-    export PATH=$PATH:/tmp/bin
-    ln -s "$(which oc)" /tmp/bin/kubectl
-fi
-
-#shellcheck source=${SHARED_DIR}/runtime_env
-source "${SHARED_DIR}/runtime_env"
-if [ -f "${SHARED_DIR}/proxy-conf.sh" ] ; then
-    source "${SHARED_DIR}/proxy-conf.sh"
-fi
-
-IFS='.' read xversion yversion _ < <(oc version -o yaml | yq '.openshiftVersion')
-if [[ -n $xversion ]] && [[ $xversion -eq 4 ]] && [[ -n $yversion ]] && [[ $yversion =~ [12][0-9] ]] ; then
-    export E2E_RUN_TAGS="${E2E_RUN_TAGS} and @${xversion}.${yversion}"
-fi
-for tag in ${FORCE_SKIP_TAGS} ; do
-    if ! [[ "${E2E_SKIP_TAGS}" =~ $tag ]] ; then
-        export E2E_SKIP_TAGS="${E2E_SKIP_TAGS} and not $tag"
+function set_cluster_access() {
+    if [ -f "${SHARED_DIR}/kubeconfig" ] ; then
+        export KUBECONFIG=${SHARED_DIR}/kubeconfig
+	echo "KUBECONFIG: ${KUBECONFIG}"
     fi
-done
-echo "E2E_RUN_TAGS is '${E2E_RUN_TAGS}'"
-echo "E2E_SKIP_TAGS is '${E2E_SKIP_TAGS}'"
+    cp -Lrvf "${KUBECONFIG}" /tmp/kubeconfig
+    if [ -f "${SHARED_DIR}/proxy-conf.sh" ] ; then
+        source "${SHARED_DIR}/proxy-conf.sh"
+	echo "proxy: ${SHARED_DIR}/proxy-conf.sh"
+    fi
+}
+function preparation_for_test() {
+    if ! which kubectl > /dev/null ; then
+        mkdir --parents /tmp/bin
+        export PATH=$PATH:/tmp/bin
+        ln --symbolic "$(which oc)" /tmp/bin/kubectl
+    fi
+    #shellcheck source=${SHARED_DIR}/runtime_env
+    source "${SHARED_DIR}/runtime_env"
+}
+function echo_e2e_tags() {
+    echo "In function: ${FUNCNAME[1]}"
+    echo "E2E_RUN_TAGS: '${E2E_RUN_TAGS}'"
+    echo "E2E_SKIP_TAGS: '${E2E_SKIP_TAGS}'"
+}
+function filter_test_by_version() {
+    local xversion yversion
+    IFS='.' read xversion yversion _ < <(oc version -o yaml | yq '.openshiftVersion')
+    if [[ -n $xversion ]] && [[ $xversion -eq 4 ]] && [[ -n $yversion ]] && [[ $yversion =~ [12][0-9] ]] ; then
+        export E2E_RUN_TAGS="${E2E_RUN_TAGS} and @${xversion}.${yversion}"
+    fi
+    echo_e2e_tags
+}
+function filter_test_by_network() {
+    local networktype
+    networktype="$(oc get network.config/cluster -o yaml | yq '.spec.networkType')"
+    case "${networktype,,}" in
+        openshiftsdn)
+	    networktag='@network-openshiftsdn'
+	    ;;
+        ovnkubernetes)
+	    networktag='@network-ovnkubernetes'
+	    ;;
+        *)
+	    echo "######Expected network to be SDN/OVN, but got: $networktype"
+	    ;;
+    esac
+    if [[ -n $networktag ]] ; then
+        export E2E_RUN_TAGS="${E2E_RUN_TAGS} and ${networktag}"
+    fi
+    echo_e2e_tags
+}
+function filter_tests() {
+    filter_test_by_version
+    filter_test_by_network
+    # the following check should be the last one in filter_tests
+    for tag in ${CUCUSHIFT_FORCE_SKIP_TAGS} ; do
+        if ! [[ "${E2E_SKIP_TAGS}" =~ $tag ]] ; then
+            export E2E_SKIP_TAGS="${E2E_SKIP_TAGS} and not $tag"
+        fi
+    done
+    echo_e2e_tags
+}
+function test_execution() {
+    pushd verification-tests
+    # run normal tests in parallel
+    export BUSHSLICER_REPORT_DIR="${ARTIFACT_DIR}/parallel-normal"
+    SECONDS=0
+    parallel_cucumber -n "${PARALLEL}" ${PARALLEL_CUCUMBER_OPTIONS} --exec \
+        'export OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS=$(echo ${USERS} | cut -d "," -f ${TEST_ENV_NUMBER},$((${TEST_ENV_NUMBER}+${PARALLEL})),$((${TEST_ENV_NUMBER}+${PARALLEL}*2)),$((${TEST_ENV_NUMBER}+${PARALLEL}*3)));
+         export WORKSPACE=/tmp/dir${TEST_ENV_NUMBER};
+         parallel_cucumber --group-by found --only-group ${TEST_ENV_NUMBER} -o "--tags \"${E2E_RUN_TAGS} and ${E2E_SKIP_TAGS} and not @serial and not @console and not @admin\" -p junit"' || true
+    show_test_execution_time 'normal'
 
-cd verification-tests
-# run normal tests
-export BUSHSLICER_REPORT_DIR="${ARTIFACT_DIR}/parallel-normal"
-SECONDS=0
-parallel_cucumber -n "${PARALLEL}" ${PARALLEL_CUCUMBER_OPTIONS} --exec \
-    'export OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS=$(echo ${USERS} | cut -d "," -f ${TEST_ENV_NUMBER},$((${TEST_ENV_NUMBER}+${PARALLEL})),$((${TEST_ENV_NUMBER}+${PARALLEL}*2)),$((${TEST_ENV_NUMBER}+${PARALLEL}*3)));
-     export WORKSPACE=/tmp/dir${TEST_ENV_NUMBER};
-     parallel_cucumber --group-by found --only-group ${TEST_ENV_NUMBER} -o "--tags \"${E2E_RUN_TAGS} and ${E2E_SKIP_TAGS} and not @serial and not @console and not @admin\" -p junit"' || true
-show_time_used 'normal'
+    # run admin tests in parallel
+    export BUSHSLICER_REPORT_DIR="${ARTIFACT_DIR}/parallel-admin"
+    SECONDS=0
+    parallel_cucumber -n "${PARALLEL}" ${PARALLEL_CUCUMBER_OPTIONS} --exec \
+        'export OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS=$(echo ${USERS} | cut -d "," -f ${TEST_ENV_NUMBER},$((${TEST_ENV_NUMBER}+${PARALLEL})),$((${TEST_ENV_NUMBER}+${PARALLEL}*2)),$((${TEST_ENV_NUMBER}+${PARALLEL}*3)));
+         export WORKSPACE=/tmp/dir${TEST_ENV_NUMBER};
+         parallel_cucumber --group-by found --only-group ${TEST_ENV_NUMBER} -o "--tags \"${E2E_RUN_TAGS} and ${E2E_SKIP_TAGS} and not @serial and not @console and @admin\" -p junit"' || true
+    show_test_execution_time 'admin'
 
-# run admin tests
-export BUSHSLICER_REPORT_DIR="${ARTIFACT_DIR}/parallel-admin"
-SECONDS=0
-parallel_cucumber -n "${PARALLEL}" ${PARALLEL_CUCUMBER_OPTIONS} --exec \
-    'export OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS=$(echo ${USERS} | cut -d "," -f ${TEST_ENV_NUMBER},$((${TEST_ENV_NUMBER}+${PARALLEL})),$((${TEST_ENV_NUMBER}+${PARALLEL}*2)),$((${TEST_ENV_NUMBER}+${PARALLEL}*3)));
-     export WORKSPACE=/tmp/dir${TEST_ENV_NUMBER};
-     parallel_cucumber --group-by found --only-group ${TEST_ENV_NUMBER} -o "--tags \"${E2E_RUN_TAGS} and ${E2E_SKIP_TAGS} and not @serial and not @console and @admin\" -p junit"' || true
-show_time_used 'admin'
-
-# run the rest tests in serial
-export BUSHSLICER_REPORT_DIR="${ARTIFACT_DIR}/serial"
-export OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS="${USERS}"
-SECONDS=0
-set -x
-cucumber --tags "${E2E_RUN_TAGS} and ${E2E_SKIP_TAGS} and (@console or @serial)" -p junit || true
-set +x
-show_time_used 'console or serial'
-
-# summarize test results
-echo "Summarizing test results..."
-failures=0 errors=0 skipped=0 tests=0
-grep -r -E -h -o 'testsuite.*tests="[0-9]+"' "${ARTIFACT_DIR}" | tr -d '[A-Za-z=\"_]' > /tmp/zzz-tmp.log
-while read -a row ; do
-    # if the last ARG of command `let` evaluates to 0, `let` returns 1
-    let failures+=${row[0]} errors+=${row[1]} skipped+=${row[2]} tests+=${row[3]} || true
-done < /tmp/zzz-tmp.log
-
-TEST_RESULT_FILE="${ARTIFACT_DIR}/test-results.yaml"
-cat > "${TEST_RESULT_FILE}" <<- EOF
+    # run the rest tests in serial
+    export BUSHSLICER_REPORT_DIR="${ARTIFACT_DIR}/serial"
+    export OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS="${USERS}"
+    SECONDS=0
+    set -x
+    cucumber --tags "${E2E_RUN_TAGS} and ${E2E_SKIP_TAGS} and ((@console and @smoke) or @serial)" -p junit || true
+    set +x
+    show_test_execution_time 'smoke console or serial'
+    popd
+}
+function summarize_test_results() {
+    # summarize test results
+    echo "Summarizing test results..."
+    failures=0 errors=0 skipped=0 tests=0
+    grep -r -E -h -o 'testsuite.*tests="[0-9]+"' "${ARTIFACT_DIR}" | tr -d '[A-Za-z=\"_]' > /tmp/zzz-tmp.log
+    while read -a row ; do
+        # if the last ARG of command `let` evaluates to 0, `let` returns 1
+        let failures+=${row[0]} errors+=${row[1]} skipped+=${row[2]} tests+=${row[3]} || true
+    done < /tmp/zzz-tmp.log
+    TEST_RESULT_FILE="${ARTIFACT_DIR}/test-results.yaml"
+    cat > "${TEST_RESULT_FILE}" <<- EOF
 cucushift:
   type: cucushift-e2e-devel
   total: $tests
@@ -90,12 +124,21 @@ cucushift:
   errors: $errors
   skipped: $skipped
 EOF
+    if [ $((failures)) != 0 ] ; then
+        echo '  failingScenarios:' >> "${TEST_RESULT_FILE}"
+        readarray -t failingscenarios < <(grep -h -r -E 'cucumber.*features/.*.feature' "${ARTIFACT_DIR}/.." | cut -d':' -f3- | sed -E 's/^( +)//;s/\x1b\[[0-9;]*m$//' | sort)
+        for (( i=0; i<failures; i++ )) ; do
+            echo "    - ${failingscenarios[$i]}" >> "${TEST_RESULT_FILE}"
+        done
+    fi
+    cat "${TEST_RESULT_FILE}" | tee -a "${SHARED_DIR}/openshift-e2e-test-qe-report" || true
+}
 
-if [ $((failures)) != 0 ] ; then
-    echo '  failingScenarios:' >> "${TEST_RESULT_FILE}"
-    readarray -t failingscenarios < <(grep -h -r -E 'cucumber.*features/.*.feature' "${ARTIFACT_DIR}/.." | cut -d':' -f3- | sed -E 's/^( +)//;s/\x1b\[[0-9;]*m$//' | sort)
-    for (( i=0; i<failures; i++ )) ; do
-        echo "    - ${failingscenarios[$i]}" >> "${TEST_RESULT_FILE}"
-    done
-fi
-cat "${TEST_RESULT_FILE}" | tee -a "${SHARED_DIR}/openshift-e2e-test-qe-report" || true
+
+E2E_RUN_TAGS="${E2E_RUN_TAGS:?'Wrong test filter for E2E_RUN_TAGS'}"
+E2E_SKIP_TAGS="${E2E_SKIP_TAGS:='not @default-skip-tag-not-used'}"
+set_cluster_access
+preparation_for_test
+filter_tests
+test_execution
+summarize_test_results
