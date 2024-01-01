407077aa8949fe340196cf1a663b1e56ca6b50b1
oharan2 <oharan@redhat.com>
2024-01-01 23:18:33 +0200

    Add support for mtc steps running on ROSA classic, mtc-execute-test ref
    rh-pre-commit.version: 2.0.3
    rh-pre-commit.check-secrets: ENABLED

--- ci-operator/step-registry/mtc/prepare-clusters/mtc-prepare-clusters-commands.sh
+++ ci-operator/step-registry/mtc/prepare-clusters/mtc-prepare-clusters-commands.sh
@@ -3,28 +3,28 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-sleep 4h
-
 echo "Extracting cluster data"
 tar -xzvf "${SHARED_DIR}/clusters_data.tar.gz" --one-top-leve=/tmp/clusters-data
+
+SOURCE_KUBEADMIN_PASSWORD_FILE="/tmp/clusters-data/${TEST_PLATFORM}/${SOURCE_CLUSTER_NAME}/auth/kubeadmin-password"
+SOURCE_KUBECONFIG="/tmp/clusters-data/${TEST_PLATFORM}/${SOURCE_CLUSTER_NAME}/auth/kubeconfig"
+SOURCE_KUBEADMIN_PASSWORD=$(cat $SOURCE_KUBEADMIN_PASSWORD_FILE)
+TARGET_KUBECONFIG="/tmp/clusters-data/${TEST_PLATFORM}/${TARGET_CLUSTER_NAME}/auth/kubeconfig"
 
 # Install the Operator on both clusters
 echo "Installing the MTC operator on the source cluster"
 ansible-playbook /mtc-interop/install-mtc.yml \
     -e version=${MTC_VERSION} \
-    -e isController=true \
-    -e kubeconfig_path=/tmp/clusters-data/${TEST_PLATFORM}/${TARGET_CLUSTER_NAME}/auth/kubeconfig
+    -e kubeconfig_path=${SOURCE_KUBECONFIG}
 
 echo "Installing the MTC operator on the target cluster"
 ansible-playbook /mtc-interop/install-mtc.yml \
     -e version=${MTC_VERSION} \
-    -e kubeconfig_path=/tmp/clusters-data/${TEST_PLATFORM}/${SOURCE_CLUSTER_NAME}/auth/kubeconfig
+    -e isController=true \
+    -e kubeconfig_path=${TARGET_KUBECONFIG}
 
 # Log into target cluster
 cp /mtc-interop/usr/bin/oc/oc /usr/bin/oc
-SOURCE_KUBEADMIN_PASSWORD_FILE="/tmp/clusters-data/aws/mtc-aws-ipi-source/auth/kubeadmin-password"
-SOURCE_KUBECONFIG="/tmp/clusters-data/aws/mtc-aws-ipi-source/auth/kubeconfig"
-SOURCE_KUBEADMIN_PASSWORD=$(cat $SOURCE_KUBEADMIN_PASSWORD_FILE)
 export KUBECONFIG=$SOURCE_KUBECONFIG
 oc login -u kubeadmin -p $SOURCE_KUBEADMIN_PASSWORD
 
@@ -35,6 +35,6 @@ EXPOSED_REGISTRY_PATH=$(oc get route default-route  -n openshift-image-registry 
 # Configure the clusters prior to executing tests
 echo "Configuring the source and target clusters"
 ansible-playbook /mtc-interop/config_mtc.yml \
-    -e controller_kubeconfig=/tmp/clusters-data/${TEST_PLATFORM}/${TARGET_CLUSTER_NAME}/auth/kubeconfig \
-    -e cluster_kubeconfig=/tmp/clusters-data/${TEST_PLATFORM}/${SOURCE_CLUSTER_NAME}/auth/kubeconfig \
+    -e controller_kubeconfig=${TARGET_KUBECONFIG} \
+    -e cluster_kubeconfig=${SOURCE_KUBECONFIG} \
     -e exposed_registry_path=${EXPOSED_REGISTRY_PATH}
