a7e40069cb49710aacd046f28b3b0564729138c9
Aidan Reilly <74046732+aireilly@users.noreply.github.com>
2024-01-02 13:57:35 +0000

    updating 4.11 and prow test builds (#47142)

--- ci-operator/step-registry/openshift-docs/portal/openshift-docs-portal-commands.sh
+++ ci-operator/step-registry/openshift-docs/portal/openshift-docs-portal-commands.sh
@@ -5,11 +5,18 @@ set -o errexit
 set -o pipefail
 set -o verbose
 
-./scripts/get-updated-distros.sh | while read -r FILENAME; do
-    if [ "${FILENAME}" == "_topic_maps/${TOPIC_MAP}" ]; then 
-        python3 ${BUILD}.py --distro ${DISTRO} --product "OpenShift Container Platform" --version ${VERSION} --no-upstream-fetch
+./scripts/get-updated-distros.sh | while read -r filename; do
+    if [ "$filename" == "_topic_maps/_topic_map.yml" ]; then python3 "${BUILD}" --distro openshift-enterprise --product "OpenShift Container Platform" --version "${VERSION}" --no-upstream-fetch
+
+    elif [ "$filename" == "_topic_maps/_topic_map_osd.yml" ]; then python3 "${BUILD}" --distro openshift-dedicated --product "OpenShift Dedicated" --version "${VERSION}" --no-upstream-fetch
+
+    elif [ "$filename" == "_topic_maps/_topic_map_ms.yml" ]; then python3 "${BUILD}" --distro microshift --product "Microshift" --version "${VERSION}" --no-upstream-fetch
+
+    elif [ "$filename" == "_topic_maps/_topic_map_rosa.yml" ]; then python3 "${BUILD}" --distro openshift-rosa --product "Red Hat OpenShift Service on AWS" --version "${VERSION}" --no-upstream-fetch
+
+    elif [ "$filename" == "_distro_map.yml" ]; then python3 "${BUILD}" --distro openshift-enterprise --product "OpenShift Container Platform" --version "${VERSION}" --no-upstream-fetch
     else
-        echo "No modified AsciiDoc files in ${DISTRO} distro 🥳"
+        echo "No modified AsciiDoc files in distro 🥳"
     fi
 done
 
