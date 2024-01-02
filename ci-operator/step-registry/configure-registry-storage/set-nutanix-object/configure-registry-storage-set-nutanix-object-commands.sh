c06939b65dc78a56ee5a6f3bf295e9dfdc4e4b30
Wen Wang <wewang@redhat.com>
2024-01-02 13:06:41 +0800

    OCPQE-18524:Fix registry with nutanix object storage (#44567)
    * Fix registry storage for nutainx object storage issue
    
    * Update configure-registry-storage-set-nutanix-object-commands.sh
    
    * Update configure-registry-storage-set-nutanix-object-commands.sh
    
    * Update configure-registry-storage-set-nutanix-object-commands.sh
    
    * Update configure-registry-storage-set-nutanix-object-commands.sh
    
    * Update configure-registry-storage-set-nutanix-object-commands.sh
    
    * Update configure-registry-storage-set-nutanix-object-commands.sh
    
    * Update configure-registry-storage-set-nutanix-object-commands.sh

--- ci-operator/step-registry/configure-registry-storage/set-nutanix-object/configure-registry-storage-set-nutanix-object-commands.sh
+++ ci-operator/step-registry/configure-registry-storage/set-nutanix-object/configure-registry-storage-set-nutanix-object-commands.sh
@@ -32,15 +32,14 @@ else
   echo "Did not find compatible trusted ca"
   exit 1
 fi
-
-NUTANIX_OS_ENDPOINT="${CLUSTER_PROFILE_DIR}/nutnaix_os_endpoint"
+NUTANIX_OS_ENDPOINT=$(cat "${CLUSTER_PROFILE_DIR}/nutnaix_os_endpoint")
 if [[ ${NUTANIX_OS_ENDPOINT} == "" ]]; then
   echo "Did not find nutanix os endpoint, exit now"
   exit 1
 fi
-
+NEW_OS_ENDPOINT="https://ocp-registry-4-${OCP_MINOR_VERSION}.${NUTANIX_OS_ENDPOINT}"
 # configure image registry to use nutanix object bucket
-oc patch config.image/cluster -p '{"spec":{"managementState":"Managed","replicas":2,"storage":{"managementState":"Unmanaged","s3":{"bucket":"ocp-registry-4-'"${OCP_MINOR_VERSION}"'","region":"us-east-1","regionEndpoint":"'"${NUTANIX_OS_ENDPOINT}"'","trustedCA":{"name":"custom-ca"}}}}}' --type=merge
+oc patch config.image/cluster -p '{"spec":{"managementState":"Managed","replicas":2,"storage":{"managementState":"Unmanaged","s3":{"bucket":"ocp-registry-4-'"${OCP_MINOR_VERSION}"'","region":"us-east-1","regionEndpoint":"'"${NEW_OS_ENDPOINT}"'","trustedCA":{"name":"custom-ca"}}}}}' --type=merge
 # wait image registry to redeploy with new set
 check_imageregistry_back_ready(){
   local result="" iter=10 period=60
