d83238c003a64750de3b38a417cae4ccd04eb999
Stephen Goeddel <sgoeddel@redhat.com>
2024-01-06 09:31:02 -0500

    Revert "Bumping Prow and Boskos (#47376)" (#47377)
    This reverts commit 779cc9785ca51f3c287b2cd587edb06a1102b588.

--- hack/images.sh
+++ hack/images.sh
@@ -1,2 +1,2 @@
-MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20240106-2af485dc14
-MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20240106-2af485dc14
+MKPJ_IMG=gcr.io/k8s-prow/mkpj:v20240105-8718b1383a
+MKPOD_IMG=gcr.io/k8s-prow/mkpod:v20240105-8718b1383a
