0e6c99d7a0e8db897ba827a4570959b91a79edb8
Liangquan Li <liangli@redhat.com>
2024-01-08 18:18:04 +0800

    HyperShift: e2e-mce-agent-disconnected-dualstack-conformance (#46835)

--- ci-operator/step-registry/hypershift/mce/agent/disconnected/create/hostedcluster/hypershift-mce-agent-disconnected-create-hostedcluster-commands.sh
+++ ci-operator/step-registry/hypershift/mce/agent/disconnected/create/hostedcluster/hypershift-mce-agent-disconnected-create-hostedcluster-commands.sh
@@ -43,14 +43,18 @@ PLAYLOADIMAGE=\$(oc get clusterversion version -ojsonpath='{.status.desired.imag
 EXTRA_ARGS=""
 if [[ "\$DISCONNECTED" == "true" ]]; then
   PLAYLOADIMAGE=\$(oc get clusterversion version -ojsonpath='{.status.desired.image}')
-  HO_OPERATOR_IMAGE="\${PLAYLOADIMAGE//@sha256:[^ ]*/@\$(oc adm release info "\$PLAYLOADIMAGE" | grep hypershift | awk '{print \$2}')}"
-  podman pull "\$HO_OPERATOR_IMAGE"
+  HO_OPERATOR_IMAGE="\${PLAYLOADIMAGE//@sha256:[^ ]*/@\$(oc adm release info -a /tmp/.dockerconfigjson "\$PLAYLOADIMAGE" | grep hypershift | awk '{print \$2}')}"
+  podman pull --authfile /tmp/.dockerconfigjson "\$HO_OPERATOR_IMAGE"
   EXTRA_ARGS+=\$(echo "--annotations=hypershift.openshift.io/control-plane-operator-image=\$HO_OPERATOR_IMAGE ")
   EXTRA_ARGS+=\$(echo "--additional-trust-bundle /etc/pki/ca-trust/source/anchors/registry.2.crt ")
 fi
 
 if [[ "\${IP_STACK}" == "v6" ]]; then
   EXTRA_ARGS+="--cluster-cidr fd03::/48 --service-cidr fd04::/112 "
+fi
+
+if [[ "\${IP_STACK}" == "v4v6" ]]; then
+  EXTRA_ARGS+="--cluster-cidr 10.132.0.0/14 --cluster-cidr fd03::/48 --service-cidr 172.31.0.0/16 --service-cidr fd04::/112 "
 fi
 
 /tmp/hcp create cluster agent \${EXTRA_ARGS} \
