171589262c771435e57f7e05a27335357a387e7b
Luffy Zhang <lzha@redhat.com>
2024-01-09 11:22:40 +0800

    Add Quay310 DAST Testing (#47396)
    * add quay310 dast testing
    
    * fix the issue of quay 3.10 openapi file
    
    * add slack notification for dast testing jobs

--- ci-operator/step-registry/quay-tests/test-quay-dast/quay-tests-test-quay-dast-commands.sh
+++ ci-operator/step-registry/quay-tests/test-quay-dast/quay-tests-test-quay-dast-commands.sh
@@ -2,18 +2,41 @@
 
 set -euo pipefail
 
-QUAY_ACCESS_TOKEN=$(cat /var/run/quay-qe-stagequayio-secret/oauth2token)
-QUAY_OAUTH2_TOEKN="Bearer $QUAY_ACCESS_TOKEN"
-
 echo "The current ZAP Version is:"
 zap.sh -version
 
 echo "Clone Redhat Rapidast Repository..."
 cd /tmp && git clone https://github.com/RedHatProductSecurity/rapidast.git && cd rapidast || true
 
-echo "Generating Quay OpenAPI File..."
-curl https://stage.quay.io/api/v1/discovery > quay.json || true
-cat quay.json | jq > openapi.json && cp openapi.json $ARTIFACT_DIR || true
+if [ ${QUAY_ENV} = 'STAGE_QUAY_IO' ]; then
+   echo "Generating Stage Quay.io OpenAPI File..."
+   curl https://stage.quay.io/api/v1/discovery > quay.json || true
+   QUAY_URL="https://stage.quay.io"
+   QUAY_SHORT_NAME="stagequayio"
+   QUAY_ACCESS_TOKEN=$(cat /var/run/quay-qe-stagequayio-secret/oauth2token)
+   QUAY_OAUTH2_TOEKN="Bearer $QUAY_ACCESS_TOKEN"
+   cat quay.json | jq > openapi.json && cp openapi.json $ARTIFACT_DIR || true 
+fi
+
+if [ ${QUAY_ENV} = 'QUAY_IO' ]; then
+   echo "Generating Quay.io OpenAPI File..."
+   curl https://quay.io/api/v1/discovery > quay.json || true
+   QUAY_URL="https://quay.io"
+   QUAY_SHORT_NAME="quayio"
+   QUAY_ACCESS_TOKEN=$(cat /var/run/quay-qe-quayio-secret/oauth2token)
+   QUAY_OAUTH2_TOEKN="Bearer $QUAY_ACCESS_TOKEN"
+   cat quay.json | jq > openapi.json && cp openapi.json $ARTIFACT_DIR || true 
+fi
+
+if [ ${QUAY_ENV} = 'QUAY' ]; then
+   echo "Generating Quay OpenAPI File..."
+   QUAY_URL=$(cat "$SHARED_DIR"/quayroute)
+   QUAY_SHORT_NAME="quay"
+   QUAY_ACCESS_TOKEN=$(cat "$SHARED_DIR"/quay_oauth2_token)
+   QUAY_OAUTH2_TOEKN="Bearer $QUAY_ACCESS_TOKEN"
+   cat "$SHARED_DIR"/quay_api_discovery | jq > openapi.json && cp openapi.json $ARTIFACT_DIR || true 
+fi
+
 
 cat >>config-zap-prowci.yaml <<EOF
 config:
@@ -24,8 +47,8 @@ config:
   configVersion: 5
 # `application` contains data related to the application, not to the scans.
 application:
-  shortName: "stagequayio"
-  url: "https://stage.quay.io"
+  shortName: "${QUAY_SHORT_NAME}"
+  url: "${QUAY_URL}"
 # `general` is a section that will be applied to all scanners.
 general:
   #proxy:
