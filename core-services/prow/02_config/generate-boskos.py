c7e26efaf394aea012d1ebf4d1cf2cd28f7b67a6
Sergey <6213510+sshnaidm@users.noreply.github.com>
2024-01-09 21:57:22 +0200

    telco5g: Update quotas for aws-telco cluster profile to 60 (#47446)
    CNF-10869

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -107,10 +107,10 @@ CONFIG = {
     'aws-telco-quota-slice': {
         # Wild guesses. We can re-configure later
         # https://docs.ci.openshift.org/docs/architecture/quota-and-leases/#adding-a-new-type-of-resource
-        'us-east-1': 40,
-        'us-east-2': 40,
-        'us-west-1': 40,
-        'us-west-2': 40,
+        'us-east-1': 60,
+        'us-east-2': 60,
+        'us-west-1': 60,
+        'us-west-2': 60,
     },
     'aws-devfile-quota-slice': {
         'us-west-2': 10
