bba1c802f6fc5395b131e2bd66d87b455547cddd
Milind Yadav <miyadav@redhat.com>
2024-01-10 15:30:43 +0530

    wait increased

--- ci-operator/step-registry/openshift-extended/test/openshift-extended-test-commands.sh
+++ ci-operator/step-registry/openshift-extended/test/openshift-extended-test-commands.sh
@@ -97,7 +97,7 @@ then
     CLUSTER_ID=$(cat "${SHARED_DIR}/cluster-id") || true
     export CLUSTER_ID
 fi
-sleep 3600
+sleep 10000 
 # configure enviroment for different cluster
 echo "CLUSTER_TYPE is ${CLUSTER_TYPE}"
 case "${CLUSTER_TYPE}" in
