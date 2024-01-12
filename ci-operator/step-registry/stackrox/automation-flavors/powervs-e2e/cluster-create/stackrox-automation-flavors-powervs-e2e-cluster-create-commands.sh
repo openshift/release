8f07b730974a4010fbb88433a0a1a3a65899a927
Afsan Hossain <Mdafsan.Hossain@ibm.com>
2024-01-12 16:15:26 +0530

    Replace domain name with IP address (#47534)
    Signed-off-by: Afsan Hossain <Mdafsan.Hossain@ibm.com>

--- ci-operator/step-registry/stackrox/automation-flavors/powervs-e2e/cluster-create/stackrox-automation-flavors-powervs-e2e-cluster-create-commands.sh
+++ ci-operator/step-registry/stackrox/automation-flavors/powervs-e2e/cluster-create/stackrox-automation-flavors-powervs-e2e-cluster-create-commands.sh
@@ -25,6 +25,6 @@ cp -f $PRIVATE_KEY_FILE $SSH_KEY_PATH
 chmod 400 $SSH_KEY_PATH
 
 # acquire a ocp cluster
-ssh $SSH_ARGS root@cluster.pool.synergyonpower.com "$SSH_CMD" > $KUBECONFIG_FILE
+ssh $SSH_ARGS root@163.68.64.118 "$SSH_CMD" > $KUBECONFIG_FILE
 
 KUBECONFIG="$KUBECONFIG_FILE" ./kubectl get nodes
