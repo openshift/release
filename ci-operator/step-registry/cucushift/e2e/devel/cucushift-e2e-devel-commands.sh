b3eb49a2fde681cb30179924144e6277b7cbe205
Liang Xia <lxia@redhat.com>
2024-01-12 11:23:50 +0800

    OCPQE-18827: Apply test filter optimize for platform on the other type of tests

--- ci-operator/step-registry/cucushift/e2e/devel/cucushift-e2e-devel-commands.sh
+++ ci-operator/step-registry/cucushift/e2e/devel/cucushift-e2e-devel-commands.sh
@@ -4,9 +4,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-PARALLEL_CUCUMBER_OPTIONS='--verbose-process-command --first-is-1 --type cucumber --serialize-stdout --combine-stderr --prefix-output-with-test-env-number'
-CUCUSHIFT_FORCE_SKIP_TAGS="customer security"
-
 function show_test_execution_time() {
     local test_type time_used
     test_type="$1"
@@ -36,20 +33,19 @@ function preparation_for_test() {
 function echo_e2e_tags() {
     echo "In function: ${FUNCNAME[1]}"
     echo "E2E_RUN_TAGS: '${E2E_RUN_TAGS}'"
-    echo "E2E_SKIP_TAGS: '${E2E_SKIP_TAGS}'"
 }
 function filter_test_by_version() {
     local xversion yversion
     IFS='.' read xversion yversion _ < <(oc version -o yaml | yq '.openshiftVersion')
     if [[ -n $xversion ]] && [[ $xversion -eq 4 ]] && [[ -n $yversion ]] && [[ $yversion =~ [12][0-9] ]] ; then
-        export E2E_RUN_TAGS="${E2E_RUN_TAGS} and @${xversion}.${yversion}"
+        export E2E_RUN_TAGS="@${xversion}.${yversion} and ${E2E_RUN_TAGS}"
     fi
     echo_e2e_tags
 }
 function filter_test_by_arch() {
     local node_archs arch_tags
-    mapfile -t node_archs < <(oc get nodes -o yaml | yq '.items[].status.nodeInfo.architecture' | sort -u)
-    arch_tags="${node_archs[*]/#/ and @}"
+    mapfile -t node_archs < <(oc get nodes -o yaml | yq '.items[].status.nodeInfo.architecture' | sort -u | sed 's/^/@/g')
+    arch_tags="${node_archs[*]/%/ and}"
     case "${#node_archs[@]}" in
         0)
             echo "=========================="
@@ -58,12 +54,40 @@ function filter_test_by_arch() {
             echo "=========================="
             ;;
         1)
-            export E2E_RUN_TAGS="${E2E_RUN_TAGS} ${arch_tags[*]}"
+            export E2E_RUN_TAGS="${arch_tags[*]} ${E2E_RUN_TAGS}"
             ;;
         *)
-            export E2E_RUN_TAGS="${E2E_RUN_TAGS} ${arch_tags[*]} and @heterogeneous"
+            export E2E_RUN_TAGS="@heterogeneous and ${arch_tags[*]} ${E2E_RUN_TAGS}"
             ;;
     esac
+    echo_e2e_tags
+}
+function filter_test_by_platform() {
+    local platform ipixupi
+    ipixupi='upi'
+    if (oc get configmap openshift-install -n openshift-config &>/dev/null) ; then
+        ipixupi='ipi'
+    fi
+    platform="$(oc get infrastructure cluster -o yaml | yq '.status.platform' | tr 'A-Z' 'a-z')"
+    if [[ -n "$platform" ]] ; then
+        case "$platform" in
+            aws|azure|gcp|nutanix)
+                export E2E_RUN_TAGS="@${platform}-${ipixupi} and ${E2E_RUN_TAGS}"
+                ;;
+            alicloud|baremetal|ibmcloud|vsphere)
+                echo "TO_BE_DONE, need extra tests, got platform as '$platform'"
+                ;;
+            none)
+                export E2E_RUN_TAGS="@baremetal-upi and ${E2E_RUN_TAGS}"
+                ;;
+            external)
+                echo "Expected, got platform as '$platform'"
+                ;;
+            *)
+                echo "Unexpected, got platform as '$platform'"
+                ;;
+        esac
+    fi
     echo_e2e_tags
 }
 function filter_test_by_network() {
@@ -81,7 +105,7 @@ function filter_test_by_network() {
 	    ;;
     esac
     if [[ -n $networktag ]] ; then
-        export E2E_RUN_TAGS="${E2E_RUN_TAGS} and ${networktag}"
+        export E2E_RUN_TAGS="${networktag} and ${E2E_RUN_TAGS}"
     fi
     echo_e2e_tags
 }
@@ -89,7 +113,7 @@ function filter_test_by_sno() {
     local nodeno
     nodeno="$(oc get nodes --no-headers | wc -l)"
     if [[ $nodeno -eq 1 ]] ; then
-        export E2E_RUN_TAGS="${E2E_RUN_TAGS} and @singlenode"
+        export E2E_RUN_TAGS="@singlenode and ${E2E_RUN_TAGS}"
     fi
     echo_e2e_tags
 }
@@ -97,7 +121,7 @@ function filter_test_by_fips() {
     local data
     data="$(oc get configmap cluster-config-v1 -n kube-system -o yaml | yq '.data')"
     if ! (grep --ignore-case --quiet 'fips' <<< "$data") ; then
-        export E2E_RUN_TAGS="${E2E_RUN_TAGS} and not @fips"
+        export E2E_RUN_TAGS="not @fips and ${E2E_RUN_TAGS}"
     fi
     echo_e2e_tags
 }
@@ -160,7 +184,7 @@ function filter_test_by_capability() {
     for cap in ${versioncaps} ; do
         if ! (grep --ignore-case --quiet "$cap" <<< "$enabledcaps") ; then
             if [[ "${tagmaps[$cap]}" != 'xxx' ]] ; then
-                export E2E_RUN_TAGS="${E2E_RUN_TAGS} and not @${tagmaps[$cap]}"
+                export E2E_RUN_TAGS="not @${tagmaps[$cap]} and ${E2E_RUN_TAGS}"
             else
                 echo "TO_BE_DONE: find tag map for '$cap'"
             fi
@@ -169,18 +193,14 @@ function filter_test_by_capability() {
     echo_e2e_tags
 }
 function filter_tests() {
-    filter_test_by_version
-    filter_test_by_arch
-    filter_test_by_network
-    filter_test_by_sno
-    filter_test_by_fips
     filter_test_by_capability
-    # the following check should be the last one in filter_tests
-    for tag in ${CUCUSHIFT_FORCE_SKIP_TAGS} ; do
-        if ! [[ "${E2E_SKIP_TAGS}" =~ $tag ]] ; then
-            export E2E_SKIP_TAGS="${E2E_SKIP_TAGS} and not $tag"
-        fi
-    done
+    filter_test_by_fips
+    filter_test_by_sno
+    filter_test_by_network
+    filter_test_by_platform
+    filter_test_by_arch
+    filter_test_by_version
+
     echo_e2e_tags
 }
 function test_execution() {
@@ -191,7 +211,7 @@ function test_execution() {
     parallel_cucumber -n "${PARALLEL}" ${PARALLEL_CUCUMBER_OPTIONS} --exec \
         'export OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS=$(echo ${USERS} | cut -d "," -f ${TEST_ENV_NUMBER},$((${TEST_ENV_NUMBER}+${PARALLEL})),$((${TEST_ENV_NUMBER}+${PARALLEL}*2)),$((${TEST_ENV_NUMBER}+${PARALLEL}*3)));
          export WORKSPACE=/tmp/dir${TEST_ENV_NUMBER};
-         parallel_cucumber --group-by found --only-group ${TEST_ENV_NUMBER} -o "--tags \"${E2E_RUN_TAGS} and ${E2E_SKIP_TAGS} and not @serial and not @console and not @admin\" -p junit"' || true
+         parallel_cucumber --group-by found --only-group ${TEST_ENV_NUMBER} -o "--tags \"${E2E_RUN_TAGS} and not @serial and not @console and not @admin\" -p junit"' || true
     show_test_execution_time 'normal'
 
     # run admin tests in parallel
@@ -200,7 +220,7 @@ function test_execution() {
     parallel_cucumber -n "${PARALLEL}" ${PARALLEL_CUCUMBER_OPTIONS} --exec \
         'export OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS=$(echo ${USERS} | cut -d "," -f ${TEST_ENV_NUMBER},$((${TEST_ENV_NUMBER}+${PARALLEL})),$((${TEST_ENV_NUMBER}+${PARALLEL}*2)),$((${TEST_ENV_NUMBER}+${PARALLEL}*3)));
          export WORKSPACE=/tmp/dir${TEST_ENV_NUMBER};
-         parallel_cucumber --group-by found --only-group ${TEST_ENV_NUMBER} -o "--tags \"${E2E_RUN_TAGS} and ${E2E_SKIP_TAGS} and not @serial and not @console and @admin\" -p junit"' || true
+         parallel_cucumber --group-by found --only-group ${TEST_ENV_NUMBER} -o "--tags \"${E2E_RUN_TAGS} and not @serial and not @console and @admin\" -p junit"' || true
     show_test_execution_time 'admin'
 
     # run the rest tests in serial
@@ -208,7 +228,7 @@ function test_execution() {
     export OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS="${USERS}"
     SECONDS=0
     set -x
-    cucumber --tags "${E2E_RUN_TAGS} and ${E2E_SKIP_TAGS} and ((@console and @smoke) or @serial)" -p junit || true
+    cucumber --tags "${E2E_RUN_TAGS} and ((@console and @smoke) or @serial)" -p junit || true
     set +x
     show_test_execution_time 'smoke console or serial'
     popd
@@ -242,8 +262,24 @@ EOF
 }
 
 
-E2E_RUN_TAGS="${E2E_RUN_TAGS:?'Wrong test filter for E2E_RUN_TAGS'}"
-E2E_SKIP_TAGS="${E2E_SKIP_TAGS:='not @default-skip-tag-not-used'}"
+PARALLEL_CUCUMBER_OPTIONS='--verbose-process-command --first-is-1 --type cucumber --serialize-stdout --combine-stderr --prefix-output-with-test-env-number'
+CUCUSHIFT_FORCE_SKIP_TAGS="not @customer
+        and not @destructive
+        and not @flaky
+        and not @inactive
+        and not @long-duration
+        and not @prod-only
+        and not @qeci
+        and not @security
+        and not @stage-only
+        and not @upgrade-check
+        and not @upgrade-prepare
+"
+if [[ -z "$E2E_RUN_TAGS" ]] ; then
+    export E2E_RUN_TAGS="$CUCUSHIFT_FORCE_SKIP_TAGS"
+else
+    export E2E_RUN_TAGS="$E2E_RUN_TAGS and $CUCUSHIFT_FORCE_SKIP_TAGS"
+fi
 set_cluster_access
 preparation_for_test
 filter_tests
