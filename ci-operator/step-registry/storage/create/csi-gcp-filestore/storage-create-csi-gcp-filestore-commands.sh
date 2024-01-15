2abf2b7408aac0e3f16a4c03cf2d1f87b2b6f1ff
chaoyang <chaoyang@redhat.com>
2024-01-15 09:30:06 +0800

    Adding profile abut filestore with xpn (#46959)
    * Adding profile abut filestore with xpn
    
    * Change the filestore csi storageclass as non default
    
    * Using xpn.json

--- ci-operator/step-registry/storage/create/csi-gcp-filestore/storage-create-csi-gcp-filestore-commands.sh
+++ ci-operator/step-registry/storage/create/csi-gcp-filestore/storage-create-csi-gcp-filestore-commands.sh
@@ -3,25 +3,38 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-CLUSTER_NAME="$(oc get -o jsonpath='{.status.infrastructureName}{"\n"}' infrastructure cluster)"
-NETWORK_NAME="$CLUSTER_NAME-network"
-export CLUSTER_NAME
-export NETWORK_NAME
-export STORAGECLASS_LOCATION=${SHARED_DIR}/filestore-sc.yaml
-export MANIFEST_LOCATION=${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
-
 # For disconnected or otherwise unreachable environments, we want to
 # have steps use an HTTP(S) proxy to reach the API server. This proxy
 # configuration file should export HTTP_PROXY, HTTPS_PROXY, and NO_PROXY
 # environment variables, as well as their lowercase equivalents (note
 # that libcurl doesn't recognize the uppercase variables).
-if test -f "${SHARED_DIR}/proxy-conf.sh"
+
+if [[ -f "${SHARED_DIR}/proxy-conf.sh" ]]
 then
 	# shellcheck disable=SC1090
 	source "${SHARED_DIR}/proxy-conf.sh"
 fi
 
+CLUSTER_NAME="$(oc get -o jsonpath='{.status.infrastructureName}{"\n"}' infrastructure cluster)"
+if [[ -s "${SHARED_DIR}/xpn.json" ]]
+then
+	echo "Reading variables from 'xpn_project_setting.json'..."
+	cat ${CLUSTER_PROFILE_DIR}/xpn_project_setting.json
+	HOST_PROJECT=$(jq -r '.hostProject' "${CLUSTER_PROFILE_DIR}/xpn_project_setting.json")
+	HOST_PROJECT_NETWORK=$(jq -r '.clusterNetwork' "${CLUSTER_PROFILE_DIR}/xpn_project_setting.json")
+	NETWORK=$(basename ${HOST_PROJECT_NETWORK})
+	NETWORK_NAME=projects/${HOST_PROJECT}/global/networks/${NETWORK}
+else 
+	NETWORK_NAME="$CLUSTER_NAME-network"
+fi
+
+export CLUSTER_NAME
+export NETWORK_NAME
+export STORAGECLASS_LOCATION=${SHARED_DIR}/filestore-sc.yaml
+export MANIFEST_LOCATION=${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
+
 # Create StorageClass
+# shared vpc, parameter should add "connect-mode: PRIVATE_SERVICE_ACCESS"
 echo "Creating a StorageClass"
 cat <<EOF >>$STORAGECLASS_LOCATION
 apiVersion: storage.k8s.io/v1
@@ -32,10 +45,16 @@ provisioner: filestore.csi.storage.gke.io
 volumeBindingMode: WaitForFirstConsumer
 allowVolumeExpansion: true
 parameters:
+  connect-mode: DIRECT_PEERING
   network: $NETWORK_NAME
   labels: kubernetes-io-cluster-$CLUSTER_NAME=owned
 EOF
 
+if [[ -s "${SHARED_DIR}/xpn.json" ]]
+then
+	sed -i 's/DIRECT_PEERING/PRIVATE_SERVICE_ACCESS/' $STORAGECLASS_LOCATION 
+fi
+	
 echo "Using StorageClass file ${STORAGECLASS_LOCATION}"
 cat ${STORAGECLASS_LOCATION}
 
@@ -90,3 +109,5 @@ EOF
 
 echo "Using manifest file ${MANIFEST_LOCATION}"
 cat ${MANIFEST_LOCATION}
+
+oc get sc/filestore-csi -o yaml
