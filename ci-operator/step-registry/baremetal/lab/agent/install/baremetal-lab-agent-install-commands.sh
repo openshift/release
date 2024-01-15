00e7e983df5a434908db8554fb56aaa0e92beaeb
Nora Niu <zniu@redhat.com>
2024-01-15 16:37:39 +0800

    agent day2 install

--- ci-operator/step-registry/baremetal/lab/agent/install/baremetal-lab-agent-install-commands.sh
+++ ci-operator/step-registry/baremetal/lab/agent/install/baremetal-lab-agent-install-commands.sh
@@ -44,6 +44,37 @@ function get_ready_nodes_count() {
     grep -c -E ",True$"
 }
 
+function approve_csrs() {
+  while [[ ! -f '/tmp/scale-out-complete' ]]; do
+    sleep 30
+    echo "approve_csrs() running..."
+    oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' \
+      | xargs --no-run-if-empty oc adm certificate approve || true
+  done
+}
+
+# wait_for_nodes_readiness loops until the number of ready nodes objects is equal to the desired one
+function wait_for_nodes_readiness()
+{
+  local expected_nodes=${1}
+  local max_retries=${2:-10}
+  local period=${3:-5}
+  for i in $(seq 1 "${max_retries}") max; do
+    if [ "${i}" == "max" ]; then
+      echo "[ERROR] Timeout reached. ${expected_nodes} ready nodes expected, found ${ready_nodes}... Failing."
+      return 1
+    fi
+    sleep "${period}m"
+    ready_nodes=$(get_ready_nodes_count)
+    if [ x"${ready_nodes}" == x"${expected_nodes}" ]; then
+        echo "[INFO] Found ${ready_nodes}/${expected_nodes} ready nodes, continuing..."
+        return 0
+    fi
+    echo "[INFO] - ${expected_nodes} ready nodes expected, found ${ready_nodes}..." \
+      "Waiting ${period}min before retrying (timeout in $(( (max_retries - i) * (period) ))min)..."
+  done
+}
+
 function update_image_registry() {
   while ! oc patch configs.imageregistry.operator.openshift.io cluster --type merge \
                  --patch '{"spec":{"managementState":"Managed","storage":{"emptyDir":{}}}}'; do
@@ -173,6 +204,11 @@ case "${BOOT_MODE}" in
   for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
     # shellcheck disable=SC1090
     . <(echo "$bmhost" | yq e 'to_entries | .[] | (.key + "=\"" + .value + "\"")')
+    if [[ "${name}" == *-a-* ]] && [ "${ADDITIONAL_WORKERS_DAY2}" == "true" ]; then
+      # Do not mount image to additional workers if we need to run them as day2 (e.g., to test single-arch clusters based
+      # on a single-arch payload migrated to a multi-arch cluster)
+      continue
+    fi
     if [ "${transfer_protocol_type}" == "cifs" ]; then
       IP_ADDRESS="$(dig +short "${AUX_HOST}")"
       iso_path="${IP_ADDRESS}/isos/${CLUSTER_NAME}.${arch}.iso"
@@ -218,6 +254,11 @@ proxy="$(<"${CLUSTER_PROFILE_DIR}/proxy")"
 for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
   # shellcheck disable=SC1090
   . <(echo "$bmhost" | yq e 'to_entries | .[] | (.key + "=\"" + .value + "\"")')
+  if [[ "${name}" == *-a-* ]] && [ "${ADDITIONAL_WORKERS_DAY2}" == "true" ]; then
+    # Do not power on the additional workers if we need to run them as day2 (e.g., to test single-arch clusters based
+    # on a single-arch payload migrated to a multi-arch cluster)
+    continue
+  fi
   echo "Power on #${host} (${name})..."
   timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" prepare_host_for_boot "${host}" "${BOOT_MODE}"
 done
@@ -248,3 +289,36 @@ if ! wait "$!"; then
   # TODO: gather logs??
   exit 1
 fi
+
+# Exit normally if there is no day2 jobs
+if [ "${ADDITIONAL_WORKERS}" == "0" ]; then
+    echo "No additional workers requested"
+    exit 0
+fi
+# Extract the ignition file for additional workers
+oc extract -n openshift-machine-api secret/worker-user-data-managed --keys=userData --to=- > "${SHARED_DIR}"/worker.ign
+# Copy console-hook and worker.ign to the location for pxe boot
+echo -e "\nCopying ignition files into bastion host..."
+chmod 644 "${SHARED_DIR}"/*.ign
+scp "${SSHOPTS[@]}" "${SHARED_DIR}"/*.ign "root@${AUX_HOST}:/opt/html/${CLUSTER_NAME}/"
+# Power on additional day2 workers
+for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
+  # shellcheck disable=SC1090
+  . <(echo "$bmhost" | yq e 'to_entries | .[] | (.key + "=\"" + .value + "\"")')
+  if [[ "${name}" == *-a-* ]] && [ "${ADDITIONAL_WORKERS_DAY2}" == "true" ]; then
+    echo "Power on #${host} (${name})..."
+    timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" prepare_host_for_boot "${host}" "pxe"
+  fi
+done
+# Approve all pending csr and wait all nodes to be ready
+EXPECTED_NODES=$(( $(get_ready_nodes_count) + ADDITIONAL_WORKERS ))
+echo "Wait for the day2 workers to become ready too..."
+approve_csrs &
+wait_for_nodes_readiness ${EXPECTED_NODES}
+ret="$?"
+if [ "${ret}" != "0" ]; then
+  echo "Some errors occurred, exiting with ${ret}."
+  exit "${ret}"
+fi
+# let the approve_csr function finish
+touch /tmp/scale-out-complete
