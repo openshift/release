a12d6e4e384b4ba80e29e905bad6a7c5fca600bb
Raúl Sevilla <rsevilla@redhat.com>
2024-01-15 18:20:16 +0100

    PERFSCALE-2758: Enable reporting mode for not GA versions (#47532)
    * Enable reporting mode for not GA versions
    
    It was needed to overwrite the EXTRA_VARS variable since it was
    defaulted to churn-duration=20m
    
    Also adding the INDEXING variable to prevent indexing in pre-upgrade
    load jobs
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    * New PROFILE_TYPE flag
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    * Remove INDEXING
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    * Include ovn-ic jobs
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    * Reporting mode for 120nodes in 4.16
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    * Sort
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    * Enable reporting in missing 120nodes jobs
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    ---------
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- ci-operator/step-registry/openshift-qe/cluster-density-v2/openshift-qe-cluster-density-v2-commands.sh
+++ ci-operator/step-registry/openshift-qe/cluster-density-v2/openshift-qe-cluster-density-v2-commands.sh
@@ -31,7 +31,7 @@ iteration_multiplier=$(($ITERATION_MULTIPLIER_ENV))
 export ITERATIONS=$(($iteration_multiplier*$current_worker_count))
 
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
-export EXTRA_FLAGS+=" --gc-metrics=true "
+export EXTRA_FLAGS+=" --gc-metrics=true --profile-type=${PROFILE_TYPE}"
 
 rm -f ${SHARED_DIR}/index.json
 ./run.sh
