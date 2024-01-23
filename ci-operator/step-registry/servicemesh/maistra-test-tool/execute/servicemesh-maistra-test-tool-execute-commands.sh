5b9108e010dbf70ac04d6c305ece6c4a11b02acc
Simran <sgill@redhat.com>
2024-01-23 01:11:38 -0800

    Add & fix test execution for maistra in ROSA  (#47613)
    * Test script fixes
    
    * Add logic to check for ROSA platform
    
    * Fix exports in script
    
    * Fix command for ROSA login
    
    * Fix shellcheck issues
    
    * Add cypress ref & update fix kiali scripts
    
    * Add firewatch env variables to ROSA Classic config
    
    * Adding missing env variables
    
    * Fix for ci-operator-config-metadata job
    
    * Update firewatch config
    
    * Set correct security identity type for Rosa
    
    * Remove kiali cypress tests
    
    * Clean cypress tests
    
    ---------
    
    Co-authored-by: Filip Brychta <fbrychta@redhat.com>

--- ci-operator/step-registry/servicemesh/maistra-test-tool/execute/servicemesh-maistra-test-tool-execute-commands.sh
+++ ci-operator/step-registry/servicemesh/maistra-test-tool/execute/servicemesh-maistra-test-tool-execute-commands.sh
@@ -5,15 +5,22 @@ set -o errexit
 set -o pipefail
 
 CONSOLE_URL=$(cat $SHARED_DIR/console.url)
+export CONSOLE_URL
 OCP_API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
-OCP_CRED_USR="kubeadmin"
-OCP_CRED_PSW="$(cat ${SHARED_DIR}/kubeadmin-password)"
-
 export OCP_API_URL
-export OCP_CRED_USR
-export OCP_CRED_PSW
 
-make test
+# for interop
+if test -f ${SHARED_DIR}/kubeadmin-password
+then
+  OCP_CRED_USR="kubeadmin"
+  OCP_CRED_PSW="$(cat ${SHARED_DIR}/kubeadmin-password)"
+  oc login ${OCP_API_URL} --username=${OCP_CRED_USR} --password=${OCP_CRED_PSW} --insecure-skip-tls-verify=true
+  echo "Execute maistra tests"
+  make test
+else #for ROSA & Hypershift platforms
+  eval "$(cat "${SHARED_DIR}/api.login")"
+  ROSA=true make test
+fi
 
 echo "Copying logs and xmls to ${ARTIFACT_DIR}"
 cp -r tests/result-latest/* ${ARTIFACT_DIR}
