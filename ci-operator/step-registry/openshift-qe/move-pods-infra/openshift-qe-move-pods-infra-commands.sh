2327f745a38c314da6d169b0bc3cecb0a0a50bca
Simon Kordas <45798030+skordas@users.noreply.github.com>
2024-01-24 18:16:33 -0500

    4.15 gcp ipi sdn private configuration (#48035)

--- ci-operator/step-registry/openshift-qe/move-pods-infra/openshift-qe-move-pods-infra-commands.sh
+++ ci-operator/step-registry/openshift-qe/move-pods-infra/openshift-qe-move-pods-infra-commands.sh
@@ -531,6 +531,16 @@ then
 	exit 0
 fi
 
+# For disconnected or otherwise unreachable environments, we want to
+# have steps use an HTTP(S) proxy to reach the API server. This proxy
+# configuration file should export HTTP_PROXY, HTTPS_PROXY, and NO_PROXY
+# environment variables, as well as their lowercase equivalents (note
+# that libcurl doesn't recognize the uppercase variables).
+if test -f "${SHARED_DIR}/proxy-conf.sh"
+then
+	# shellcheck disable=SC1090
+	source "${SHARED_DIR}/proxy-conf.sh"
+fi
 
 # Download jq
 if [ ! -d /tmp/bin ];then
