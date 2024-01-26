f7bbb2567a1991cebf78fe37e358e47ac1c717f7
Ritu Mundhe <rmundhe@redhat.com>
2024-01-26 10:26:33 -0600

    cluster reuse

--- ci-operator/step-registry/osde2e/provision/osde2e-provision-commands.sh
+++ ci-operator/step-registry/osde2e/provision/osde2e-provision-commands.sh
@@ -19,4 +19,5 @@ fi
 export REPORT_DIR="${ARTIFACT_DIR}"
 
 /osde2e test --configs "${CONFIGS}" \
---secret-locations "${SECRET_LOCATIONS}" --provision-only
+--secret-locations "${SECRET_LOCATIONS}" \
+--provision-only
