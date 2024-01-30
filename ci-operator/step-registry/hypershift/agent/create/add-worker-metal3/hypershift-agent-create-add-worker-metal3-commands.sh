fe93a49f1cddb5a524a066f85e5e8eeb8984138c
Liangquan Li <liangli@redhat.com>
2024-01-30 18:04:28 +0800

    Add hypershift-agent-manual chain and job for conformance test (#48059)

--- ci-operator/step-registry/hypershift/agent/create/add-worker-metal3/hypershift-agent-create-add-worker-metal3-commands.sh
+++ ci-operator/step-registry/hypershift/agent/create/add-worker-metal3/hypershift-agent-create-add-worker-metal3-commands.sh
@@ -4,7 +4,7 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-echo "************ add-worker command ************"
+echo "************ add-worker metal3 command ************"
 
 source "${SHARED_DIR}/packet-conf.sh"
 tar -czf - . | ssh "${SSHOPTS[@]}" "root@${IP}" "cat > /root/assisted-service.tar.gz"
