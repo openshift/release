df5d076968a9bddf114bd23922379664265a2677
oharan2 <oharan@redhat.com>
2024-01-31 20:21:00 +0200

    Interop - Onboard MTC operator on ROSA (#45507)
    * Add firewatch-rosa-aws-sts workflow
    
    * Add MTC test on OCP 4.14 using ROSA
    
    * Make jobs
    
    * Fix Firewatch config
    
    * Update config with install-operators chain
    
    * Update scenario to run e2e tests
    
    * Update clusters platform to ROSA
    
    * Replace stream with channel-groups in cluster configs
    
    * Make jobs
    
    * Update clusters configs with rosa cli flags
    
    * Change clusters names to match scheme
    
    * Update firewatch config to match v2, change file with -rosa-classic
    
    rh-pre-commit.version: 2.0.3
    rh-pre-commit.check-secrets: ENABLED
    
    * Add support for mtc steps running on ROSA classic
    
    rh-pre-commit.version: 2.0.3
    rh-pre-commit.check-secrets: ENABLED
    
    * Make jobs
    
    rh-pre-commit.version: 2.0.3
    rh-pre-commit.check-secrets: ENABLED
    
    * Add support for mtc steps running on ROSA classic, mtc-execute-test ref
    
    rh-pre-commit.version: 2.0.3
    rh-pre-commit.check-secrets: ENABLED
    
    * Update cron value to: '0 6 25 10 *'
    
    rh-pre-commit.version: 2.0.3
    rh-pre-commit.check-secrets: ENABLED
    
    * Remove cluster health check for this scenario (2 clusters are running)
    
    rh-pre-commit.version: 2.0.3
    rh-pre-commit.check-secrets: ENABLED
    
    * Add support to use cluster health check for Target cluster
    
    rh-pre-commit.version: 2.0.3
    rh-pre-commit.check-secrets: ENABLED
    
    * Trying to use the existing rosa account-roles steps as a part of our workflow
    
    rh-pre-commit.version: 2.0.3
    rh-pre-commit.check-secrets: ENABLED
    
    * Use the firewatch-cluster workflow
    
    rh-pre-commit.version: 2.0.3
    rh-pre-commit.check-secrets: ENABLED

--- ci-operator/step-registry/mtc/prepare-clusters/mtc-prepare-clusters-commands.sh
+++ ci-operator/step-registry/mtc/prepare-clusters/mtc-prepare-clusters-commands.sh
@@ -6,23 +6,25 @@ set -o pipefail
 echo "Extracting cluster data"
 tar -xzvf "${SHARED_DIR}/clusters_data.tar.gz" --one-top-leve=/tmp/clusters-data
 
+SOURCE_KUBEADMIN_PASSWORD_FILE="/tmp/clusters-data/${TEST_PLATFORM}/${SOURCE_CLUSTER_NAME}/auth/kubeadmin-password"
+SOURCE_KUBECONFIG="/tmp/clusters-data/${TEST_PLATFORM}/${SOURCE_CLUSTER_NAME}/auth/kubeconfig"
+SOURCE_KUBEADMIN_PASSWORD=$(cat $SOURCE_KUBEADMIN_PASSWORD_FILE)
+TARGET_KUBECONFIG="/tmp/clusters-data/${TEST_PLATFORM}/${TARGET_CLUSTER_NAME}/auth/kubeconfig"
+
 # Install the Operator on both clusters
 echo "Installing the MTC operator on the source cluster"
 ansible-playbook /mtc-interop/install-mtc.yml \
     -e version=${MTC_VERSION} \
-    -e isController=true \
-    -e kubeconfig_path=/tmp/clusters-data/aws/mtc-aws-ipi-target/auth/kubeconfig
+    -e kubeconfig_path=${SOURCE_KUBECONFIG}
 
 echo "Installing the MTC operator on the target cluster"
 ansible-playbook /mtc-interop/install-mtc.yml \
     -e version=${MTC_VERSION} \
-    -e kubeconfig_path=/tmp/clusters-data/aws/mtc-aws-ipi-source/auth/kubeconfig
+    -e isController=true \
+    -e kubeconfig_path=${TARGET_KUBECONFIG}
 
 # Log into target cluster
 cp /mtc-interop/usr/bin/oc/oc /usr/bin/oc
-SOURCE_KUBEADMIN_PASSWORD_FILE="/tmp/clusters-data/aws/mtc-aws-ipi-source/auth/kubeadmin-password"
-SOURCE_KUBECONFIG="/tmp/clusters-data/aws/mtc-aws-ipi-source/auth/kubeconfig"
-SOURCE_KUBEADMIN_PASSWORD=$(cat $SOURCE_KUBEADMIN_PASSWORD_FILE)
 export KUBECONFIG=$SOURCE_KUBECONFIG
 oc login -u kubeadmin -p $SOURCE_KUBEADMIN_PASSWORD
 
@@ -33,6 +35,9 @@ EXPOSED_REGISTRY_PATH=$(oc get route default-route  -n openshift-image-registry 
 # Configure the clusters prior to executing tests
 echo "Configuring the source and target clusters"
 ansible-playbook /mtc-interop/config_mtc.yml \
-    -e controller_kubeconfig=/tmp/clusters-data/aws/mtc-aws-ipi-target/auth/kubeconfig \
-    -e cluster_kubeconfig=/tmp/clusters-data/aws/mtc-aws-ipi-source/auth/kubeconfig \
+    -e controller_kubeconfig=${TARGET_KUBECONFIG} \
+    -e cluster_kubeconfig=${SOURCE_KUBECONFIG} \
     -e exposed_registry_path=${EXPOSED_REGISTRY_PATH}
+
+# Prepare Target cluster to match static kubeconfig path for the next step 'cucushift-installer-check-cluster-health':
+cp $TARGET_KUBECONFIG ${SHARED_DIR}/kubeconfig
