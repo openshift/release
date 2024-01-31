5b3bec37d50e2c0aaeda5d62bacdcf5827ab1113
Vadim Rutkovsky <vrutkovs@redhat.com>
2024-01-31 17:15:51 +0100

    cert rotation suspend: sync up wait-for-stable-cluster periods with shutdown step

--- ci-operator/step-registry/openshift/e2e/cert-rotation/suspend/openshift-e2e-cert-rotation-suspend-commands.sh
+++ ci-operator/step-registry/openshift/e2e/cert-rotation/suspend/openshift-e2e-cert-rotation-suspend-commands.sh
@@ -131,7 +131,7 @@ oc -n openshift-ovn-kubernetes delete pod -l app=ovnkube-control-plane
 
 # Wait for operators to stabilize
 if
-  ! oc adm wait-for-stable-cluster --minimum-stable-period=5m --timeout=10m; then
+  ! oc adm wait-for-stable-cluster --minimum-stable-period=5m --timeout=60m; then
     oc get nodes
     oc get co | grep -v "True\s\+False\s\+False"
     exit 1
