5e4a3395fdef68040a9835d81bac2231536447ba
Liang Xia <lxia@redhat.com>
2024-02-02 15:15:34 +0800

    Enhance cron entry for f360 jobs (#48362)

--- ci-operator/config/openshift/openshift-tests-private/tools/generate-cron-entry.sh
+++ ci-operator/config/openshift/openshift-tests-private/tools/generate-cron-entry.sh
@@ -52,6 +52,15 @@ let DAY_OF_MONTH=10#${NUMBERS:4:2}%30+1
 let MONTH=10#${NUMBERS:6:2}%12+1
 let DAY_OF_WEEK=10#${NUMBERS:8:1}%7
 
+# For f360 jobs, hard code the cron to:
+# 	Feb 1, 0:00
+if [[ "${TEST_NAME}" =~ -f360 ]] ; then
+	MINUTE=0
+	HOUR=0
+	DAY_OF_MONTH=1
+	MONTH=2
+fi
+
 if [[ "${TEST_NAME}" =~ baremetal- ]] ; then
 	# Raleigh working hours, 8~17 (in UTC, 13~22)
 	WK_HOUR_BEGIN=13
