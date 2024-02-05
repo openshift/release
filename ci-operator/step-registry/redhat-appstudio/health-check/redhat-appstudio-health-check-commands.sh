4fabfd509ac7cbb0bf67cbce3818ca45d44bae0f
Pavel Sturc <psturc@redhat.com>
2024-02-05 10:00:45 +0100

    fix: konflux: update secret name (#48389)

--- ci-operator/step-registry/redhat-appstudio/health-check/redhat-appstudio-health-check-commands.sh
+++ ci-operator/step-registry/redhat-appstudio/health-check/redhat-appstudio-health-check-commands.sh
@@ -5,7 +5,7 @@ set -o errexit
 set -o pipefail
 
 export GITHUB_TOKEN
-GITHUB_TOKEN=$(cat /usr/local/ci-secrets/redhat-appstudio-qe/github-token)
+GITHUB_TOKEN=$(cat /usr/local/konflux-ci-secrets/redhat-appstudio-qe/github-token)
 
 cd "$(mktemp -d)"
 git clone --branch main https://github.com/redhat-appstudio/qe-tools .
