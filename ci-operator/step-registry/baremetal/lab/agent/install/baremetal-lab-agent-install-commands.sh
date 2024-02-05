6d0fe0e0a5ec0789e710f6bca66b9a4319451c55
Zhenying Niu <zniu@redhat.com>
2024-02-05 16:24:37 +0800

    Agent based installer with multi payload (#48148)

--- ci-operator/step-registry/baremetal/lab/agent/install/baremetal-lab-agent-install-commands.sh
+++ ci-operator/step-registry/baremetal/lab/agent/install/baremetal-lab-agent-install-commands.sh
@@ -173,6 +173,11 @@ case "${BOOT_MODE}" in
   for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
     # shellcheck disable=SC1090
     . <(echo "$bmhost" | yq e 'to_entries | .[] | (.key + "=\"" + .value + "\"")')
+    if [[ "${name}" == *-a-* ]] && [ "${ADDITIONAL_WORKERS_DAY2}" == "true" ]; then
+      # Do not mount image to additional workers if we need to run them as day2 (e.g., to test single-arch clusters based
+      # on a single-arch payload migrated to a multi-arch cluster)
+      continue
+    fi
     if [ "${transfer_protocol_type}" == "cifs" ]; then
       IP_ADDRESS="$(dig +short "${AUX_HOST}")"
       iso_path="${IP_ADDRESS}/isos/${CLUSTER_NAME}.${arch}.iso"
@@ -218,6 +223,11 @@ proxy="$(<"${CLUSTER_PROFILE_DIR}/proxy")"
 for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
   # shellcheck disable=SC1090
   . <(echo "$bmhost" | yq e 'to_entries | .[] | (.key + "=\"" + .value + "\"")')
+  if [[ "${name}" == *-a-* ]] && [ "${ADDITIONAL_WORKERS_DAY2}" == "true" ]; then
+    # Do not power on the additional workers if we need to run them as day2 (e.g., to test single-arch clusters based
+    # on a single-arch payload migrated to a multi-arch cluster)
+    continue
+  fi
   echo "Power on #${host} (${name})..."
   timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" prepare_host_for_boot "${host}" "${BOOT_MODE}"
 done
