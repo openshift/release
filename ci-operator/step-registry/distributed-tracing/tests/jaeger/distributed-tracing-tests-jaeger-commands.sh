0a5ac5b6e7abb4746107fe0f0a54e80bbf824f5a
Ishwar Kanse <ikanse@redhat.com>
2024-02-05 14:40:25 +0530

    debug jaeger

--- ci-operator/step-registry/distributed-tracing/tests/jaeger/distributed-tracing-tests-jaeger-commands.sh
+++ ci-operator/step-registry/distributed-tracing/tests/jaeger/distributed-tracing-tests-jaeger-commands.sh
@@ -18,6 +18,7 @@ curl -o hack/install/install-kuttl.sh -L https://raw.githubusercontent.com/Ishwa
 ./hack/install/install-kuttl.sh
 
 # Run the e2e tests
+#test
 make run-e2e-tests KAFKA_VERSION=$KAFKA_VERSION ASSERT_IMG=$ASSERT_IMG VERBOSE=true USE_KIND_CLUSTER=false SKIP_ES_EXTERNAL=true JAEGER_OLM=true KAFKA_OLM=true PROMETHEUS_OLM=true CI=true PIPELINE=true E2E_TESTS_TIMEOUT=$E2E_TESTS_TIMEOUT
 
 JUNIT_PREFIX="junit_"
