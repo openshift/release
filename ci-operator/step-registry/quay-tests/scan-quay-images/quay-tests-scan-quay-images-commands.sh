9c4d60a2842f55378d0bc8bd30e1eefd7245dfdb
Li Zhang <lzha@redhat.com>
2024-02-05 13:03:06 +0800

    fix error

--- ci-operator/step-registry/quay-tests/scan-quay-images/quay-tests-scan-quay-images-commands.sh
+++ ci-operator/step-registry/quay-tests/scan-quay-images/quay-tests-scan-quay-images-commands.sh
@@ -9,13 +9,13 @@ quay_security_testing_hostname="$(cat ${SHARED_DIR}/QUAY_SECURITY_TESTING_HOST_N
 cp /var/run/quay-qe-omr-secret/quaybuilder.pub .
 chmod 600 ./quaybuilder.pub
 
-quay_operator_image="brew.registry.redhat.io/rh-osbs/${QUAY_OPERATOR_IMAGE}"
-quay_app_image="brew.registry.redhat.io/rh-osbs/${QUAY_IMAGE}"
-quay_clair_image="brew.registry.redhat.io/rh-osbs/${QUAY_CLAIR_IMAGE}"
-quay_bridge_operator_image="brew.registry.redhat.io/rh-osbs/${QUAY_BRIDGE_OPERATOR_IMAGE}"
-quay_container_security_operator_image="brew.registry.redhat.io/rh-osbs/${QUAY_CONTAINER_SECURITY_OPERATOR_IMAGE}"
-quay_builder_image="brew.registry.redhat.io/rh-osbs/${QUAY_BUILDER_IMAGE}"
-quay_builder_qemu_image="brew.registry.redhat.io/rh-osbs/${QUAY_BUILDER_QEMU_IMAGE}"
+quay_operator_image="brew.registry.redhat.io/rh-osbs/$QUAY_OPERATOR_IMAGE"
+quay_app_image="brew.registry.redhat.io/rh-osbs/$QUAY_IMAGE"
+quay_clair_image="brew.registry.redhat.io/rh-osbs/$QUAY_CLAIR_IMAGE"
+quay_bridge_operator_image="brew.registry.redhat.io/rh-osbs/$QUAY_BRIDGE_OPERATOR_IMAGE"
+quay_container_security_operator_image="brew.registry.redhat.io/rh-osbs/$QUAY_CONTAINER_SECURITY_OPERATOR_IMAGE"
+quay_builder_image="brew.registry.redhat.io/rh-osbs/$QUAY_BUILDER_IMAGE"
+quay_builder_qemu_image="brew.registry.redhat.io/rh-osbs/$QUAY_BUILDER_QEMU_IMAGE"
       
 function scan_quay_images(){
     ssh -i quaybuilder -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null centos@$1 "sudo /usr/local/bin/grype $2 --scope all-layers > $3_image_vulnerability-report" || true
