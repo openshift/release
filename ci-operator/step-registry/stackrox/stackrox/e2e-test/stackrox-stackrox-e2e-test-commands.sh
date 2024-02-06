ffa250e8d341e1a5cdbe756cd86f74e77999a26e
davdhacs <105243888+davdhacs@users.noreply.github.com>
2024-02-06 15:31:50 -0700

    log collection method

--- ci-operator/step-registry/stackrox/stackrox/e2e-test/stackrox-stackrox-e2e-test-commands.sh
+++ ci-operator/step-registry/stackrox/stackrox/e2e-test/stackrox-stackrox-e2e-test-commands.sh
@@ -2,4 +2,5 @@
 export OPENSHIFT_CI_STEP_NAME="stackrox-stackrox-e2e-test"
 job="${TEST_SUITE:-${JOB_NAME_SAFE#merge-}}"
 job="${job#nightly-}"
+echo "COLLECTION_METHOD=${COLLECTION_METHOD:-}"
 exec .openshift-ci/dispatch.sh "${job}"
