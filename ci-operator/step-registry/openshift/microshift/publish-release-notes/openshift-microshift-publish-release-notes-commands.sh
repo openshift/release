f538176efd05bea16565fa9bf20d7911dbed0570
Doug Hellmann <dhellmann@redhat.com>
2024-02-06 10:45:21 -0500

    USHIFT-1349: automate publishing microshift pre-release info

---
+++ ci-operator/step-registry/openshift/microshift/publish-release-notes/openshift-microshift-publish-release-notes-commands.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -xeuo pipefail
+
+if [[ "$JOB_NAME" == rehearse* ]]; then
+    echo "INFO: \$JOB_NAME starts with rehearse - running in DRY RUN mode"
+    export DRY_RUN=y
+fi
+
+export PATH="${HOME}/.local/bin:${PATH}"
+
+# Load the github credentials needed by the release notes script
+APP_ID="$(cat /secrets/pr-creds/app_id)"
+export APP_ID
+export CLIENT_KEY=/secrets/pr-creds/key.pem
+
+cd /go/src/github.com/openshift/microshift/
+./scripts/release-notes/gen_ec_release_notes.sh
