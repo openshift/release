c2a1830027aa6fc919da5618bdfb6abf80664879
Li Zhang <lzha@redhat.com>
2024-02-06 17:01:40 +0800

    add slack notifications for Quay SDL testing

--- ci-operator/step-registry/quay-tests/scan-quay-images/quay-tests-scan-quay-images-commands.sh
+++ ci-operator/step-registry/quay-tests/scan-quay-images/quay-tests-scan-quay-images-commands.sh
@@ -15,14 +15,13 @@ quay_bridge_operator_image_tag="brew.registry.redhat.io/rh-osbs/${QUAY_BRIDGE_OP
 quay_container_security_operator_image_tag="brew.registry.redhat.io/rh-osbs/${QUAY_CONTAINER_SECURITY_OPERATOR_IMAGE}"
 quay_builder_image_tag="brew.registry.redhat.io/rh-osbs/${QUAY_BUILDER_IMAGE}"
 quay_builder_qemu_image_tag="brew.registry.redhat.io/rh-osbs/${QUAY_BUILDER_QEMU_IMAGE}"
-
-sleep 2000
       
 function scan_quay_images(){
     ssh -o StrictHostKeyChecking=no -o VerifyHostKeyDNS=no -o ConnectionAttempts=3 -i quaybuilder centos@$1 "sudo /usr/local/bin/grype $2 --scope all-layers > $3_image_vulnerability-report" || true
     scp -o StrictHostKeyChecking=no -o VerifyHostKeyDNS=no -o ConnectionAttempts=3 -i quaybuilder centos@$1:/home/centos/$3_image_vulnerability-report $ARTIFACT_DIR/$3_image_vulnerability-report || true
 }
 
+echo "start to scan quay images:"
 scan_quay_images "$quay_security_testing_hostname" "$quay_operator_image_tag" "quay_operator"
 scan_quay_images "$quay_security_testing_hostname" "$quay_app_image_tag" "quay_app"
 scan_quay_images "$quay_security_testing_hostname" "$quay_clair_image_tag" "quay_clair"
@@ -30,3 +29,5 @@ scan_quay_images "$quay_security_testing_hostname" "$quay_bridge_operator_image_
 scan_quay_images "$quay_security_testing_hostname" "$quay_container_security_operator_image_tag" "quay_container_security_operator"
 scan_quay_images "$quay_security_testing_hostname" "$quay_builder_image_tag" "quay_builder"
 scan_quay_images "$quay_security_testing_hostname" "$quay_builder_qemu_image_tag" "quay_builder_qemu"
+
+echo "completed scanning quay images, pls check the scan results in artifact directory."
