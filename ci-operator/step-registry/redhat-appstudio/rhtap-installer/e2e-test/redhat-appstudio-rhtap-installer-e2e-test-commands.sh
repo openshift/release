957470db34e759e9833a588c119289a3a92c29d9
Xin Jiang <53755997+xinredhat@users.noreply.github.com>
2024-02-07 19:24:29 +0800

    build rhtap-installer image for tests (#48312)
    * added rhtap installer tests
    
    rh-pre-commit.version: 2.1.0
    rh-pre-commit.check-secrets: ENABLED
    
    * updated referenced image
    
    rh-pre-commit.version: 2.1.0
    rh-pre-commit.check-secrets: ENABLED
    
    * build image rhtap-installer
    
    rh-pre-commit.version: 2.1.0
    rh-pre-commit.check-secrets: ENABLED

--- ci-operator/step-registry/redhat-appstudio/rhtap-installer/e2e-test/redhat-appstudio-rhtap-installer-e2e-test-commands.sh
+++ ci-operator/step-registry/redhat-appstudio/rhtap-installer/e2e-test/redhat-appstudio-rhtap-installer-e2e-test-commands.sh
@@ -4,33 +4,35 @@ set -o nounset
 set -o pipefail
 
 DEBUG_OUTPUT=/tmp/log.txt
-export ACS_API_TOKEN ACS_CENTRAL_ENDPOINT DEVELOPER_HUB_CATALOG_URL GITHUB_APP_ID GITHUB_APP_CLIENT_ID GITHUB_APP_CLIENT_SECRET GITHUB_APP_WEBHOOK_SECRET GITHUB_APP_WEBHOOK_URL GITHUB_APP_PRIVATE_KEY
-ACS_API_TOKEN=$(cat /usr/local/ci-secrets/rhtap/acs-api-token)
-ACS_CENTRAL_ENDPOINT=$(cat /usr/local/ci-secrets/rhtap/acs-central-endpoint)
-DEVELOPER_HUB_CATALOG_URL=https://github.com/redhat-appstudio/tssc-sample-templates/blob/main/all.yaml
-GITHUB_APP_ID=$(cat /usr/local/ci-secrets/rhtap/rhdh-github-app-id)
-GITHUB_APP_CLIENT_ID=$(cat /usr/local/ci-secrets/rhtap/rhdh-github-client-id)
-GITHUB_APP_CLIENT_SECRET=$(cat /usr/local/ci-secrets/rhtap/rhdh-github-client-secret)
-GITHUB_APP_WEBHOOK_SECRET=$(cat /usr/local/ci-secrets/rhtap/rhdh-github-webhook-secret)
-GITHUB_APP_WEBHOOK_URL=GITHUB_APP_WEBHOOK_URL
-GITHUB_APP_PRIVATE_KEY=$(base64 -d < /usr/local/ci-secrets/rhtap/rhdh-github-private-key)
+export ACS__API_TOKEN ACS__CENTRAL_ENDPOINT DEVELOPER_HUB__CATALOG__URL GITHUB__APP__APP_ID GITHUB__APP__CLIENT_ID GITHUB__APP__CLIENT_SECRET GITHUB__APP__WEBHOOK_SECRET GITHUB__APP__WEBHOOK_URL GITHUB__APP__PRIVATE_KEY
+ACS__API_TOKEN=$(cat /usr/local/rhtap-ci-secrets/rhtap/acs-api-token)
+ACS__CENTRAL_ENDPOINT=$(cat /usr/local/rhtap-ci-secrets/rhtap/acs-central-endpoint)
+DEVELOPER_HUB__CATALOG__URL=https://github.com/redhat-appstudio/tssc-sample-templates/blob/main/all.yaml
+GITHUB__APP__APP_ID=$(cat /usr/local/rhtap-ci-secrets/rhtap/rhdh-github-app-id)
+GITHUB__APP__CLIENT_ID=$(cat /usr/local/rhtap-ci-secrets/rhtap/rhdh-github-client-id)
+GITHUB__APP__CLIENT_SECRET=$(cat /usr/local/rhtap-ci-secrets/rhtap/rhdh-github-client-secret)
+GITHUB__APP__WEBHOOK_SECRET=$(cat /usr/local/rhtap-ci-secrets/rhtap/rhdh-github-webhook-secret)
+GITHUB__APP__WEBHOOK_URL=GITHUB_APP_WEBHOOK_URL
+GITHUB__APP__PRIVATE_KEY=$(base64 -d < /usr/local/rhtap-ci-secrets/rhtap/rhdh-github-private-key)
 
 wait_for_pipeline() {
-  if ! kubectl wait --for=condition=succeeded "$1" -n "$2" --timeout 300s >"$DEBUG_OUTPUT"; then
+  if ! oc wait --for=condition=succeeded "$1" -n "$2" --timeout 300s >"$DEBUG_OUTPUT"; then
     echo "[ERROR] Pipeline failed to complete successful" >&2
-    kubectl get pipelineruns "$1" -n "$2" >"$DEBUG_OUTPUT"
+    oc get pipelineruns "$1" -n "$2" >"$DEBUG_OUTPUT"
     exit 1
   fi
 }
 
-echo "Generate private-values.yaml file ..."
+echo "$HOME"
+
+echo "[INFO]Generate private-values.yaml file ..."
 ./bin/make.sh values
 
-echo "Install RHTAP ..."
-./bin/make.sh apply -- --values private-values.yaml
+echo "[INFO]Install RHTAP ..."
+./bin/make.sh apply -n rhtap -- --values private-values.yaml
 
 echo ""
-echo "Extract the configuration information from logs of the pipeline"
+echo "[INFO]Extract the configuration information from logs of the pipeline"
 
 cat << EOF > rhtap-pe-info.yaml
 apiVersion: tekton.dev/v1
@@ -53,7 +55,7 @@ spec:
               value: rhtap
 EOF
 
-pipeline_name=$(kubectl create -f rhtap-pe-info.yaml | cut -d' ' -f1 | awk -F'/' '{print $2}')
+pipeline_name=$(oc create -f rhtap-pe-info.yaml | cut -d' ' -f1 | awk -F'/' '{print $2}')
 wait_for_pipeline "pipelineruns/$pipeline_name" rhtap
 tkn -n rhtap pipelinerun logs "$$pipeline_name" -f >"$DEBUG_OUTPUT"
 
@@ -66,5 +68,6 @@ echo "callback-url: $callback_url"
 echo "webhook-url: $webhook_url"
 
 ##todo: handle the requests via sprayproxy
-echo "Trigger e2e tests"
-./test/e2e.sh -t test -- --values private-values.yaml
+echo "[INFO]Trigger e2e tests..."
+# ./test/e2e.sh -t test -- --values private-values.yaml
+./bin/make.sh -n rhtap test
