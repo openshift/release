568c2d1e04dd921dd4eecfcd93be0ae8fb7ae041
Xin Jiang <53755997+xinredhat@users.noreply.github.com>
2024-02-07 19:24:29 +0800

    build rhtap-installer image for tests (#48312)
    * added rhtap installer tests
    
    rh-pre-commit.version: 2.1.0
    rh-pre-commit.check-secrets: ENABLED
    
    * updated referenced image
    
    rh-pre-commit.version: 2.1.0
    rh-pre-commit.check-secrets: ENABLED
    
    * build image rhtap-installer
    
    rh-pre-commit.version: 2.1.0
    rh-pre-commit.check-secrets: ENABLED

--- ci-operator/step-registry/redhat-appstudio/rhtap-installer/static-checks/yamllint/redhat-appstudio-rhtap-installer-static-checks-yamllint-commands.sh
+++ ci-operator/step-registry/redhat-appstudio/rhtap-installer/static-checks/yamllint/redhat-appstudio-rhtap-installer-static-checks-yamllint-commands.sh
@@ -6,5 +6,40 @@ set -o pipefail
 
 pwd
 ls -l
-yamllint version
-# yamllint -c "$CONFIG_FILE"
+ls -l /
+CONFIG_FILE=yamllint.yaml
+cat <<EOF > "$CONFIG_FILE"
+---
+yaml-files:
+  - '*.yaml'
+  - '*.yml'
+rules:
+  braces: enable
+  brackets: enable
+  colons: enable
+  commas: enable
+  comments:
+    require-starting-space: true
+    ignore-shebangs: true
+    min-spaces-from-content: 1
+  comments-indentation: enable
+  document-end: disable
+  document-start: enable
+  empty-lines: enable
+  empty-values: disable
+  float-values: disable
+  hyphens: enable
+  indentation: enable
+  key-duplicates: enable
+  key-ordering: disable
+  line-length: disable
+  new-line-at-end-of-file: enable
+  new-lines: enable
+  octal-values: disable
+  quoted-strings: disable
+  trailing-spaces: enable
+  truthy:
+    check-keys: false
+EOF
+
+yamllint -c "$CONFIG_FILE"
