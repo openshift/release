f456a933a717dfefe2f405583a4a37ec9d4e387f
Jeff Roche <82384784+jeff-roche@users.noreply.github.com>
2024-02-07 14:36:31 -0500

    feat: ibu e2e ci workflow (#48128)
    fix: changing the default repo and adding the command to remove the seed vm once the seed is created
    
    feat: moved the upgrade to a new step name and put in the update script
    
    feat: added a configurable lca image ref
    
    fix: switched to tag formats rather than specifying the tag
    
    fix: added periodic seed gen and corresponding workflow
    
    fix: add a base image for test steps and periodic
    
    fix: added the release stanza to the periodic
    
    fix: pinning to build02
    
    fix: changing base image
    
    fix: trying cluster profile aws
    
    fix: seed vars update
    
    fix: dnf clean
    
    fix: installing jq as a dep
    
    fix: manually wait for seed generation
    
    fix: Owners update to use new alias
    
    chore: review comment updates
    
    fix: changed the wrong artifact dir

---
+++ ci-operator/step-registry/openshift/image-based/infra/aws/ec2/logs/openshift-image-based-infra-aws-ec2-logs-commands.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
+
+IP_ADDRESS="$(cat ${SHARED_DIR}/public_address)"
+HOST_USER="$(cat ${SHARED_DIR}/ssh_user)"
+INSTANCE_PREFIX="${HOST_USER}@${IP_ADDRESS}"
+
+echo "Using Host $IP_ADDRESS"
+
+mkdir -p "${HOME}/.ssh"
+cat <<EOF >"${HOME}/.ssh/config"
+Host ${IP_ADDRESS}
+  User ${HOST_USER}
+  IdentityFile ${CLUSTER_PROFILE_DIR}/ssh-privatekey
+  StrictHostKeyChecking accept-new
+  ServerAliveInterval 30
+  ServerAliveCountMax 1200
+EOF
+chmod 0600 "${HOME}/.ssh/config"
+
+cp "${HOME}/.ssh/config" "${SHARED_DIR}/ssh_config"
+
+scp "${INSTANCE_PREFIX}:/tmp/init_output.txt" "${ARTIFACT_DIR}/init_ec2_output.txt"
