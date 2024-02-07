0c673553d6a82c6413c05558c822e99bde69c43a
Saumya Vishwakarma <svishwak@redhat.com>
2024-02-07 16:18:49 +0530

    Fixing execute-tests for ROSA

--- ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
+++ ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
@@ -39,7 +39,15 @@ touch /tmp/test-settings/default_settings.json
 echo "Login as Kubeadmin to the test cluster at ${API_URL}..."
 mkdir -p /home/jenkins/.kube
 touch /home/jenkins/.kube/config
-oc login -u kubeadmin -p "$(cat $SHARED_DIR/kubeadmin-password)" "${API_URL}" --insecure-skip-tls-verify=true
+
+# Login for Interop
+if test -f ${SHARED_DIR}/kubeadmin-password
+then
+  oc login -u kubeadmin -p "$(cat $SHARED_DIR/kubeadmin-password)" "${API_URL}" --insecure-skip-tls-verify=true
+#Login for ROSA Classic and Hypershift platforms
+else
+  eval "$(cat "${SHARED_DIR}/api.login")"
+fi
 
 # Setup Python Virtual Environment
 echo "Create virtual environment and install required packages..."
