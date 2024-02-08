28dd0395be040a1b7d7b7c4d83511291f1f35620
Krishna Harsha Voora <krvoora@redhat.com>
2024-02-08 19:59:59 +0530

    PERFSCALE-2753: Add coverage for IBM's HCP 4.13
    This PR adds CP (small) & DP for 4.13 ROSA-HCP jobs
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>

--- ci-operator/step-registry/rosa/cluster/provision/rosa-cluster-provision-commands.sh
+++ ci-operator/step-registry/rosa/cluster/provision/rosa-cluster-provision-commands.sh
@@ -271,7 +271,7 @@ HYPERSHIFT_SWITCH=""
 if [[ "$HOSTED_CP" == "true" ]]; then
   HYPERSHIFT_SWITCH="--hosted-cp"
   if [[ "$ENABLE_SECTOR" == "true" ]]; then
-    PROVISION_SHARD_ID=$(cat ${SHARED_DIR}/provision_shard_ids | head -n 1)
+    PROVISION_SHARD_ID=${PROVISION_SHARD_ID}
     if [[ -z "$PROVISION_SHARD_ID" ]]; then
       echo -e "No available provision shard."
       exit 1
