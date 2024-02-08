f76526986249fea4f3d44691eb20f1dc61a7f4ea
Ritu Mundhe <rmundhe@redhat.com>
2024-02-08 13:08:41 -0600

    new step for ocp

---
+++ ci-operator/step-registry/osde2e/ocp/test/osde2e-ocp-test-commands.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+mkdir -p 
+
+export   REGION="$(oc get -o jsonpath='{.status.platformStatus.aws.region}' infrastructure cluster)"
+export   ZONE="$(oc get -o jsonpath='{.items[0].metadata.labels.failure-domain\.beta\.kubernetes\.io/zone}' nodes)"
+export TEST_PROVIDER="{\"type\":\"aws\",\"region\":\"${REGION}\",\"zone\":\"${ZONE}\",\"multizone\":true,\"multimaster\":true}"
+
+
+openshift-tests run "${OCP_SUITE}"
+  --provider "${TEST_PROVIDER}" \
+  -o "${ARTIFACT_DIR}/e2e.log" \
+  --junit-dir "${ARTIFACT_DIR}/junit" &
+ 
+
+ 
