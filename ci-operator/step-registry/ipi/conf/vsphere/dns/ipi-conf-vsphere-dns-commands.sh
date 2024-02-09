18942b76011e67dc79f07740ee792501795f3b5a
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2024-02-09 16:45:19 -0500

    direct clusterbot jobs to IBM load balancer vsphere-clusterbot (#48661)

--- ci-operator/step-registry/ipi/conf/vsphere/dns/ipi-conf-vsphere-dns-commands.sh
+++ ci-operator/step-registry/ipi/conf/vsphere/dns/ipi-conf-vsphere-dns-commands.sh
@@ -50,9 +50,9 @@ echo "${hosted_zone_id}" > "${SHARED_DIR}/hosted-zone.txt"
 if [ "${JOB_NAME_SAFE}" = "launch" ]; then
   # setup DNS records for clusterbot to point to the IBM VIP
   api_dns_target='"TTL": 60,
-    "ResourceRecords": [{"Value": "'${vips[0]}'"}, {"Value": "169.48.190.20"}]'
+    "ResourceRecords": [{"Value": "'${vips[0]}'"}, {"Value": "169.59.251.161"}, {"Value": "169.63.237.210"}]'
   apps_dns_target='"TTL": 60,
-    "ResourceRecords": [{"Value": "169.48.190.20"}]'
+    "ResourceRecords": [{"Value": "169.59.251.161"}, {"Value": "169.63.237.210"}]'
 else
   # Configure DNS direct to respective VIP
   api_dns_target='"TTL": 60,
