29b3759e3674a44c104581b46e2df348dd1444e4
aleskandro <aleskandro@redhat.com>
2024-02-13 11:16:33 +0000

    [IPI/BM] Fix dhcp-option to disable infra pxe
    - Fix domain names options in dhcp
    - Fix disabling bastion's managed PXE for hosts in BM IPI

--- ci-operator/step-registry/baremetal/lab/pre/dhcp-conf/baremetal-lab-pre-dhcp-conf-commands.sh
+++ ci-operator/step-registry/baremetal/lab/pre/dhcp-conf/baremetal-lab-pre-dhcp-conf-commands.sh
@@ -22,12 +22,12 @@ fi
 
 echo "Generating the DHCP/PXE config..."
 
-DHCP_CONF_OPTS="# DO NOT EDIT; BEGIN $CLUSTER_NAME
-dhcp-option-force=tag:$CLUSTER_NAME,15,$CLUSTER_NAME.$BASE_DOMAIN
-dhcp-option-force=tag:$CLUSTER_NAME,119,$CLUSTER_NAME.$BASE_DOMAIN
-# DO NOT EDIT; END $CLUSTER_NAME"
+DHCP_CONF_OPTS="
+tag:$CLUSTER_NAME,15,$CLUSTER_NAME.$BASE_DOMAIN
+tag:$CLUSTER_NAME,119,$CLUSTER_NAME.$BASE_DOMAIN
+"
 
-DHCP_CONF="# DO NOT EDIT; BEGIN $CLUSTER_NAME"
+DHCP_CONF=""
 
 for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
   # shellcheck disable=SC1090
@@ -40,9 +40,6 @@ for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
   DHCP_CONF="${DHCP_CONF}
 $mac,$ip,set:$CLUSTER_NAME,infinite"
 done
-
-DHCP_CONF="${DHCP_CONF}
-# DO NOT EDIT; END $CLUSTER_NAME"
 
 echo "Setting the DHCP/PXE config in the auxiliary host..."
 timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" bash -s -- \
