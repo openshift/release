08d53fc24b1f62a46445090c1fbf56463eb29443
Pawel Paszki <ppaszki@redhat.com>
2024-02-13 08:42:16 +0000

    test: eu-west-2 region in prod

--- ci-operator/step-registry/rosa/cluster/provision/rosa-cluster-provision-commands.sh
+++ ci-operator/step-registry/rosa/cluster/provision/rosa-cluster-provision-commands.sh
@@ -271,7 +271,7 @@ HYPERSHIFT_SWITCH=""
 if [[ "$HOSTED_CP" == "true" ]]; then
   HYPERSHIFT_SWITCH="--hosted-cp"
   if [[ "$ENABLE_SECTOR" == "true" ]]; then
-    PROVISION_SHARD_ID=$(cat ${SHARED_DIR}/provision_shard_ids | head -n 1)
+    PROVISION_SHARD_ID="93913069-c9d0-11ee-8fe1-0a580a83075b"
     if [[ -z "$PROVISION_SHARD_ID" ]]; then
       echo -e "No available provision shard."
       exit 1
