c254f7696696ef1b7f0ccf4ede274c68f07768ae
vishnuchalla <vishnuchalla47@gmail.com>
2024-02-15 10:27:02 -0500

    Adding CI tests for e2e-benchmarking (#48818)
    Signed-off-by: Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.remote.csb>
    Co-authored-by: Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.remote.csb>

---
+++ ci-operator/step-registry/cloud-bulldozer/e2e-benchmarking/cluster-density-v2/cloud-bulldozer-e2e-benchmarking-cluster-density-v2-commands.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+cat /etc/os-release
+oc config view
+oc projects
+
+if [[ "$JOB_TYPE" == "presubmit" ]] && [[ "$REPO_OWNER" = "cloud-bulldozer" ]] && [[ "$REPO_NAME" = "e2e-benchmarking" ]]; then
+    git clone https://github.com/${REPO_OWNER}/${REPO_NAME}
+    pushd ${REPO_NAME}
+    git pull origin pull/${PULL_NUMBER}/head:${PULL_NUMBER}
+    git switch ${PULL_NUMBER}
+    pushd workloads/kube-burner-ocp-wrapper
+    export WORKLOAD=cluster-density-v2
+
+    current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker=,node-role.kubernetes.io/infra!=,node-role.kubernetes.io/workload!= --output jsonpath="{.items[?(@.status.conditions[-1].type=='Ready')].status.conditions[-1].type}" | wc -w | xargs)
+
+    ES_SERVER="" ITERATIONS=${current_worker_count} CHURN=false ./run.sh
+else
+    echo "We are sorry, this job is only meant for cloud-bulldozer/e2e-benchmarking repo PR testing"
+fi
