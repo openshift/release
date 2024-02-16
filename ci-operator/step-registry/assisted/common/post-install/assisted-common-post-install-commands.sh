ef750bb063caa4180e9996fb3be75298a469d106
Adrien Gentil <agentil@redhat.com>
2024-02-16 14:52:42 +0100

    missing files

--- ci-operator/step-registry/assisted/common/post-install/assisted-common-post-install-commands.sh
+++ ci-operator/step-registry/assisted/common/post-install/assisted-common-post-install-commands.sh
@@ -18,26 +18,3 @@ EOF
 echo "### Copying kubeconfig files"
 export KUBECONFIG=${SHARED_DIR}/kubeconfig
 ssh -F "${SHARED_DIR}/ssh_config" "root@ci_machine" "find \${KUBECONFIG} -type f -exec cat {} \;" > ${KUBECONFIG}
-
-function update_image_registry() {
-  sleep 30
-
-  echo "$(date -u --rfc-3339=seconds) - Waiting for imageregistry config to be available"
-  while true; do
-    oc get configs.imageregistry.operator.openshift.io/cluster > /dev/null && break
-    sleep 15
-  done
-
-  echo "$(date -u --rfc-3339=seconds) - Patching image registry configuration..."
-  oc patch configs.imageregistry.operator.openshift.io cluster --type merge --patch '{"spec":{"managementState":"Managed","storage":{"emptyDir":{}}}}'
-}
-
-update_image_registry
-
-until \
-  oc wait --all=true clusteroperator --for='condition=Available=True' >/dev/null && \
-  oc wait --all=true clusteroperator --for='condition=Progressing=False' >/dev/null && \
-  oc wait --all=true clusteroperator --for='condition=Degraded=False' >/dev/null;  do
-    echo "$(date --rfc-3339=seconds) Clusteroperators not yet ready"
-    sleep 1s
-done
