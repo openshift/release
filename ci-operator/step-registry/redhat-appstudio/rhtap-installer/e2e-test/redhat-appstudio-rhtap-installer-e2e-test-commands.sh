917eb30de8b1d26b413c3797cc44aa2dec938ea1
Otávio Fernandes <otaviof@gmail.com>
2024-02-16 08:02:54 +0100

    feat: "rhtap-installer" New Attributes (#48777)
    Adding more `values.yaml` attributes as environment variables.

--- ci-operator/step-registry/redhat-appstudio/rhtap-installer/e2e-test/redhat-appstudio-rhtap-installer-e2e-test-commands.sh
+++ ci-operator/step-registry/redhat-appstudio/rhtap-installer/e2e-test/redhat-appstudio-rhtap-installer-e2e-test-commands.sh
@@ -4,7 +4,24 @@ set -o nounset
 set -o pipefail
 
 DEBUG_OUTPUT=/tmp/log.txt
-export ACS__API_TOKEN ACS__CENTRAL_ENDPOINT DEVELOPER_HUB__CATALOG__URL GITHUB__APP__APP_ID GITHUB__APP__CLIENT_ID GITHUB__APP__CLIENT_SECRET GITHUB__APP__WEBHOOK_SECRET GITHUB__APP__WEBHOOK_URL GITHUB__APP__PRIVATE_KEY
+
+export ACS__API_TOKEN \
+  ACS__CENTRAL_ENDPOINT \
+  DEVELOPER_HUB__CATALOG__URL \
+  GITHUB__APP__APP_ID GITHUB__APP__CLIENT_ID \
+  GITHUB__APP__CLIENT_SECRET \
+  GITHUB__APP__WEBHOOK_SECRET \
+  GITHUB__APP__WEBHOOK_URL \
+  GITHUB__APP__PRIVATE_KEY \
+  TPA__GUAC__PASSWORD \
+  TPA__KEYCLOAK__ADMIN_PASSWORD \
+  TPA__MINIO__ROOT_PASSWORD \
+  TPA__OIDC__TESTING_MANAGER_CLIENT_SECRET \
+  TPA__OIDC__TESTING_USER_CLIENT_SECRET \
+  TPA__OIDC__WALKER_CLIENT_SECRET \
+  TPA__POSTGRES__POSTGRES_PASSWORD \
+  TPA__POSTGRES__TPA_PASSWORD
+
 ACS__API_TOKEN=$(cat /usr/local/rhtap-ci-secrets/rhtap/acs-api-token)
 ACS__CENTRAL_ENDPOINT=$(cat /usr/local/rhtap-ci-secrets/rhtap/acs-central-endpoint)
 DEVELOPER_HUB__CATALOG__URL=https://github.com/redhat-appstudio/tssc-sample-templates/blob/main/all.yaml
@@ -14,6 +31,14 @@ GITHUB__APP__CLIENT_SECRET=$(cat /usr/local/rhtap-ci-secrets/rhtap/rhdh-github-c
 GITHUB__APP__WEBHOOK_SECRET=$(cat /usr/local/rhtap-ci-secrets/rhtap/rhdh-github-webhook-secret)
 GITHUB__APP__WEBHOOK_URL=GITHUB_APP_WEBHOOK_URL
 GITHUB__APP__PRIVATE_KEY=$(base64 -d < /usr/local/rhtap-ci-secrets/rhtap/rhdh-github-private-key)
+TPA__GUAC__PASSWORD="guac1234"
+TPA__KEYCLOAK__ADMIN_PASSWORD="admin123456"
+TPA__MINIO__ROOT_PASSWORD="minio123456"
+TPA__OIDC__TESTING_MANAGER_CLIENT_SECRET="ca48053c-3b82-4650-a98d-4cace7f2d567"
+TPA__OIDC__TESTING_USER_CLIENT_SECRET="0e6bf990-43b4-4efb-95d7-b24f2b94a525"
+TPA__OIDC__WALKER_CLIENT_SECRET="5460cc91-4e20-4edd-881c-b15b169f8a79"
+TPA__POSTGRES__POSTGRES_PASSWORD="postgres123456"
+TPA__POSTGRES__TPA_PASSWORD="postgres1234"
 
 wait_for_pipeline() {
   if ! oc wait --for=condition=succeeded "$1" -n "$2" --timeout 300s >"$DEBUG_OUTPUT"; then
