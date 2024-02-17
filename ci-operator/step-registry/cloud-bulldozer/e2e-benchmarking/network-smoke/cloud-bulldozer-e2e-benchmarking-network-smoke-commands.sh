b584db9cbe6e76efd01e0e41e30fd7f6f5b3bc66
vishnuchalla <vishnuchalla47@gmail.com>
2024-02-16 21:53:09 -0500

    Adding perfscale control-plane and data-path ci tests (#48895)
    Signed-off-by: Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.remote.csb>
    Co-authored-by: Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.remote.csb>

--- ci-operator/step-registry/cloud-bulldozer/e2e-benchmarking/network-smoke/cloud-bulldozer-e2e-benchmarking-network-smoke-commands.sh
+++ ci-operator/step-registry/cloud-bulldozer/e2e-benchmarking/network-smoke/cloud-bulldozer-e2e-benchmarking-network-smoke-commands.sh
@@ -14,12 +14,9 @@ if [[ "$JOB_TYPE" == "presubmit" ]] && [[ "$REPO_OWNER" = "cloud-bulldozer" ]] &
     pushd ${REPO_NAME}
     git pull origin pull/${PULL_NUMBER}/head:${PULL_NUMBER}
     git switch ${PULL_NUMBER}
-    pushd workloads/kube-burner-ocp-wrapper
-    export WORKLOAD=cluster-density-v2
-
-    current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker=,node-role.kubernetes.io/infra!=,node-role.kubernetes.io/workload!= --output jsonpath="{.items[?(@.status.conditions[-1].type=='Ready')].status.conditions[-1].type}" | wc -w | xargs)
-
-    ES_SERVER="" ITERATIONS=${current_worker_count} CHURN=false ./run.sh
+    pushd workloads/network-perf-v2
+    oc delete ns netperf --wait=true --ignore-not-found=true
+    ./run.sh
 else
     echo "We are sorry, this job is only meant for cloud-bulldozer/e2e-benchmarking repo PR testing"
 fi
--- ci-operator/step-registry/cloud-bulldozer/e2e-benchmarking/node-density/cloud-bulldozer-e2e-benchmarking-node-density-commands.sh
+++ ci-operator/step-registry/cloud-bulldozer/e2e-benchmarking/node-density/cloud-bulldozer-e2e-benchmarking-node-density-commands.sh
@@ -15,11 +15,8 @@ if [[ "$JOB_TYPE" == "presubmit" ]] && [[ "$REPO_OWNER" = "cloud-bulldozer" ]] &
     git pull origin pull/${PULL_NUMBER}/head:${PULL_NUMBER}
     git switch ${PULL_NUMBER}
     pushd workloads/kube-burner-ocp-wrapper
-    export WORKLOAD=cluster-density-v2
-
-    current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker=,node-role.kubernetes.io/infra!=,node-role.kubernetes.io/workload!= --output jsonpath="{.items[?(@.status.conditions[-1].type=='Ready')].status.conditions[-1].type}" | wc -w | xargs)
-
-    ES_SERVER="" ITERATIONS=${current_worker_count} CHURN=false ./run.sh
+    export WORKLOAD=node-density
+    ES_SERVER="" EXTRA_FLAGS="--pods-per-node=50 --pod-ready-threshold=60s" ./run.sh
 else
     echo "We are sorry, this job is only meant for cloud-bulldozer/e2e-benchmarking repo PR testing"
 fi
