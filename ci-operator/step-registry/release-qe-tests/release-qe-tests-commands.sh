159b5a450ceee4d7a82fc1b555458b72de52cfe3
Rio Liu <rioliu@redhat.com>
2024-02-19 16:31:08 +0800

    add few debug codes

--- ci-operator/step-registry/release-qe-tests/release-qe-tests-commands.sh
+++ ci-operator/step-registry/release-qe-tests/release-qe-tests-commands.sh
@@ -10,9 +10,11 @@ export APITOKEN=${prow_api_token}
 github_token=$(cat "/var/run/vault/tests-private-account/token-git")
 export GITHUB_TOKEN=${github_token}
 
+pip3 list
+python3 -V
 #VALID_RELEASES="4.11 4.12 4.13 4.14 4.15 4.16"
 VALID_RELEASES="4.16"
-for release in $VALID_RELEASES;
+for release in $VALID_RELEASES
 do
   echo "start job controller for $release"
   jobctl start-controller -r $release
