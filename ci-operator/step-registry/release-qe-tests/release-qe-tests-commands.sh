9cdc2909cea650ce5b92997c78bcd2bba88aaabf
Rio Liu <rioliu@redhat.com>
2024-02-19 23:44:15 +0800

    remove from image and few debug codes

--- ci-operator/step-registry/release-qe-tests/release-qe-tests-commands.sh
+++ ci-operator/step-registry/release-qe-tests/release-qe-tests-commands.sh
@@ -10,9 +10,8 @@ export APITOKEN=${prow_api_token}
 github_token=$(cat "/var/run/vault/tests-private-account/token-git")
 export GITHUB_TOKEN=${github_token}
 
-pip3 list
 python3 -V
-ls -l /usr/bin/python*
+
 #VALID_RELEASES="4.11 4.12 4.13 4.14 4.15 4.16"
 VALID_RELEASES="4.16"
 for release in $VALID_RELEASES
