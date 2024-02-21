b8f614273f20401b51d70919dc329ce1f0a52e82
Ishwar Kanse <ikanse@redhat.com>
2024-02-21 17:58:51 +0530

    Update Distributed Tracing QE steps to use Chainsaw. (#49058)

--- ci-operator/step-registry/distributed-tracing/tests/common/distributed-tracing-tests-common-commands.sh
+++ ci-operator/step-registry/distributed-tracing/tests/common/distributed-tracing-tests-common-commands.sh
@@ -7,9 +7,6 @@ set -o pipefail
 # Unset environment variables which conflict with kuttl
 unset NAMESPACE
 
-#Add manifest directory for kuttl
-mkdir /tmp/kuttl-manifests
-
 # setup proxy
 if test -f "${SHARED_DIR}/proxy-conf.sh"
 then
@@ -19,34 +16,29 @@ fi
 #Select the test suite based on the DT_TEST_TYPE
 if [[ "${DT_TEST_TYPE}" == "DAST" ]]; then
 
-  #Copy the distributed-tracing-qe repo files to a writable directory by kuttl
+  #Copy the distributed-tracing-qe repo files to a writable directory.
   cp -R /tmp/distributed-tracing-qe /tmp/distributed-tracing-tests && cd /tmp/distributed-tracing-tests
 
   # Execute Distributed Tracing tests
-  KUBECONFIG=$KUBECONFIG kuttl test \
-    --report=xml \
-    --artifacts-dir="$ARTIFACT_DIR" \
-    --parallel="$PARALLEL_TESTS" \
-    --report-name="$REPORT_NAME" \
-    --start-kind=false \
-    --timeout="$TIMEOUT" \
-    --manifest-dir=$MANIFEST_DIR \
-    tests/e2e-rh-sdl
+  chainsaw test \
+  --config ".chainsaw-rh-sdl.yaml" \
+  --report-name "$REPORT_NAME" \
+  --report-path "$ARTIFACT_DIR" \
+  --report-format "XML" \
+  --test-dir \
+  tests/e2e-rh-sdl
 
 else
 
-  #Copy the distributed-tracing-qe repo files to a writable directory by kuttl
+  #Copy the distributed-tracing-qe repo files to a writable directory.
   cp -R /tmp/distributed-tracing-qe /tmp/distributed-tracing-tests && cd /tmp/distributed-tracing-tests
 
   # Execute Distributed Tracing tests
-  KUBECONFIG=$KUBECONFIG kuttl test \
-    --report=xml \
-    --artifacts-dir="$ARTIFACT_DIR" \
-    --parallel="$PARALLEL_TESTS" \
-    --report-name="$REPORT_NAME" \
-    --start-kind=false \
-    --timeout="$TIMEOUT" \
-    --manifest-dir=$MANIFEST_DIR \
-    tests/e2e-acceptance
+  chainsaw test \
+  --report-name "$REPORT_NAME" \
+  --report-path "$ARTIFACT_DIR" \
+  --report-format "XML" \
+  --test-dir \
+  tests/e2e-acceptance
 
 fi
