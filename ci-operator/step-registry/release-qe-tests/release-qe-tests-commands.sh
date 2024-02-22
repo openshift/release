382e96a7f2bdb29aa4ddd8edf499f883a2cc6dd0
Xiaozheng Liu (Rio) <rioliu@redhat.com>
2024-02-22 14:25:04 +0800

    OCPQE-19032 add release-qe-test step and job (#48939)
    * add release-qe-test step and job
    
    * add few debug codes
    
    * make update
    
    * output jobctl content
    
    * try to fix from image of job-python3
    
    * change from image of job-python3 to oar-python3
    
    * remove from image and few debug codes
    
    * add cli base image
    
    * add cli image in step ref
    
    * correct cli image tag in step ref
    
    * add release for cli ref, add new image for jobctl
    
    * add ci-pull-credentials
    
    * update latest cli version to 4.16
    
    * remove cluster setting
    
    * add cluster config back to new test
    
    * remove cluster attribute for new test
    
    * add releasepayload modifier token in step
    
    * correct shell variable reference
    
    * add test code to list token
    
    * correct token file under mounted cred
    
    * correct cluster endpint
    
    * output token for debugging
    
    * refine oc cmds
    
    * update new nightly buld for testing
    
    * remove test code
    
    * add -e option for echo cmd
    
    * change cron job setting to every year

---
+++ ci-operator/step-registry/release-qe-tests/release-qe-tests-commands.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+prow_api_token=$(cat "/var/run/vault/tests-private-account/prow-api-token")
+export APITOKEN=$prow_api_token
+
+github_token=$(cat "/var/run/vault/tests-private-account/token-git")
+export GITHUB_TOKEN=$github_token
+
+release_payload_modifier_token=$(cat /var/run/vault/release-payload-modifier-token/token)
+export RELEASE_PAYLOAD_MODIFIER_TOKEN=$release_payload_modifier_token
+
+echo "Login cluster app.ci"
+oc login api.ci.l2s4.p1.openshiftapps.com:6443 --token=$RELEASE_PAYLOAD_MODIFIER_TOKEN
+
+echo -e "\n********* Start job controller *********\n"
+#VALID_RELEASES="4.11 4.12 4.13 4.14 4.15 4.16"
+VALID_RELEASES="4.16"
+for release in $VALID_RELEASES
+do
+  echo -e "\nstart job controller for $release"
+  jobctl start-controller -r $release
+  jobctl start-controller -r $release --no-nightly
+done
+
+echo -e "\n********* Start test result aggregator *********\n"
+jobctl start-aggregator
