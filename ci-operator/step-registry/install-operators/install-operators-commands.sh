cecdff6bf11edb37e92c76cf25c9c7df5822f2ac
oharan2 <oharan@redhat.com>
2024-02-26 22:33:37 +0200

    Revert deleting 'if' statements
    rh-pre-commit.version: 2.0.3
    rh-pre-commit.check-secrets: ENABLED

--- ci-operator/step-registry/install-operators/install-operators-commands.sh
+++ ci-operator/step-registry/install-operators/install-operators-commands.sh
@@ -4,8 +4,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-trap 'sleep 4h' EXIT TERM SIGINT INT
-
 # Set the cluster proxy configuration, if its present.
 if test -s "${SHARED_DIR}/proxy-conf.sh" ; then
     echo "setting the proxy"
@@ -27,9 +25,9 @@ readarray -t OPERATOR_ARRAY < <(jq --compact-output '.[]' <<< "$OPERATORS")
 for operator_obj in "${OPERATOR_ARRAY[@]}"; do
     # Set variables for this operator.
     operator_name=$(jq --raw-output '.name' <<< "$operator_obj")
-    operator_source="${(jq --raw-output '.source' <<< "$operator_obj"):-$DEFAULT_OPERATOR_SOURCE}"
-    operator_channel="${(jq --raw-output '.channel' <<< "$operator_obj"):-$DEFAULT_OPERATOR_CHANNEL}"
-    operator_install_namespace="${(jq --raw-output '.install_namespace' <<< "$operator_obj"):-$DEFAULT_OPERATOR_INSTALL_NAMESPACE}"
+    operator_source=$(jq --raw-output '.source // ""' <<< "$operator_obj")
+    operator_channel=$(jq --raw-output '.channel // ""' <<< "$operator_obj")
+    operator_install_namespace=$(jq --raw-output '.install_namespace // ""' <<< "$operator_obj")
     operator_group=$(jq --raw-output '.operator_group // ""' <<< "$operator_obj")
     operator_target_namespaces=$(jq --raw-output '.target_namespaces // ""' <<< "$operator_obj")
 
@@ -37,6 +35,21 @@ for operator_obj in "${OPERATOR_ARRAY[@]}"; do
     if [[ -z "${operator_name}" ]]; then
         echo "ERROR: name is not defined"
         exit 1
+    fi
+
+    # If source is not defined, use DEFAULT_OPERATOR_SOURCE.
+    if [[ -z "${operator_source}" ]]; then
+        operator_source="${DEFAULT_OPERATOR_SOURCE}"
+    fi
+
+    # If install_namespace not defined, use DEFAULT_OPERATOR_INSTALL_NAMESPACE.
+    if [[ -z "${operator_install_namespace}" ]]; then
+        operator_install_namespace="${DEFAULT_OPERATOR_INSTALL_NAMESPACE}"
+    fi
+
+    # If channel is not defined, use DEFAULT_OPERATOR_CHANNEL.
+    if [[ -z "${operator_channel}" ]]; then
+        operator_channel="${DEFAULT_OPERATOR_CHANNEL}"
     fi
 
     # If the channel is "!default", find the default channel of the operator
