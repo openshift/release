e63b3cff369345f28afad522904e385a73d583d9
Xin Jiang <xjiang@redhat.com>
2024-02-28 12:44:22 +0800

    added e2e test for rhtap-install ci
    rh-pre-commit.version: 2.1.0
    rh-pre-commit.check-secrets: ENABLED

--- ci-operator/step-registry/redhat-appstudio/rhtap-installer/e2e-test/redhat-appstudio-rhtap-installer-e2e-test-commands.sh
+++ ci-operator/step-registry/redhat-appstudio/rhtap-installer/e2e-test/redhat-appstudio-rhtap-installer-e2e-test-commands.sh
@@ -2,5 +2,28 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -x
+
+cat /etc/*release*
 
 echo "start rhtap-installer e2e test"
+nodejs_version=v21.6.2
+nodejs_platform=linux-x64
+nodejs="node-${nodejs_version}-${nodejs_platform}"
+
+cd /tmp
+curl -LO "https://nodejs.org/download/release/${nodejs_version}/${nodejs}.tar.gz"
+tar xf "${nodejs}.tar.gz"
+
+export NODEJS_HOME="/tmp/${nodejs}"
+export PATH=$PATH:$NODEJS_HOME/bin
+
+HOME=/tmp npm install yarn
+export PATH=$PATH:/tmp/node_modules/.bin
+cd -
+
+export npm_config_cache="/tmp/.npm"
+
+git clone https://github.com/flacatus/rhtap-e2e.git
+cd rhtap-e2e
+yarn && yarn test
