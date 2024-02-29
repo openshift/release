f8225b42356845790a803073d998af45bd8857f2
Sandeep Yadav <sandyada@redhat.com>
2024-02-29 13:02:57 +0530

    dnm test SNO - cnfdr27

--- ci-operator/step-registry/telco5g/sno-setup/telco5g-sno-setup-commands.sh
+++ ci-operator/step-registry/telco5g/sno-setup/telco5g-sno-setup-commands.sh
@@ -59,6 +59,9 @@ cat << EOF > $SHARED_DIR/get-cluster-name.yml
 - name: Grab and run kcli to install openshift cluster
   hosts: bastion
   gather_facts: false
+  vars:
+    cluster:
+      {"cnfdr27": {"port": 6443, "ip": "10.1.98.84", "hvip": "10.1.98.67"}}
   tasks:
   - name: Wait 300 seconds, but only start checking after 10 seconds
     wait_for_connection:
@@ -69,7 +72,7 @@ cat << EOF > $SHARED_DIR/get-cluster-name.yml
     retries: 15
     delay: 2
   - name: Discover cluster to run job
-    command: python3 ~/telco5g-lab-deployment/scripts/upstream_cluster_all.py --get-cluster $ADDITIONAL_ARG
+    command: python3 ~/telco5g-lab-deployment/scripts/upstream_cluster_all.py --get-cluster -c '{{ cluster|to_json }}'
     register: cluster
     environment:
       JOB_NAME: ${JOB_NAME:-'unknown'}
@@ -96,7 +99,7 @@ cat << EOF > $SHARED_DIR/release-cluster.yml
   tasks:
 
   - name: Release cluster from job
-    command: python3 ~/telco5g-lab-deployment/scripts/upstream_cluster_all.py --release-cluster $CLUSTER_NAME
+    command: python3 ~/telco5g-lab-deployment/scripts/upstream_cluster_all.py --release-cluster $CLUSTER_NAME -s
 EOF
 
 if [[ "$CLUSTER_ENV" != "upstreambil" ]]; then
