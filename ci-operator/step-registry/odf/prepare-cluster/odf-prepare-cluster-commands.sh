11ddf3f97b6f4c7c975c6c0fd82594c3b40525c5
Ori Haran <oriharan@distrobox-fedora.fedora>
2024-03-04 12:16:42 +0200

    Add worker nodes selection in odf-prepare-cluster.sh

--- ci-operator/step-registry/odf/prepare-cluster/odf-prepare-cluster-commands.sh
+++ ci-operator/step-registry/odf/prepare-cluster/odf-prepare-cluster-commands.sh
@@ -3,10 +3,13 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+sleep 4h
+
 # Temp debug
 trap 'sleep 4h' EXIT TERM SIGINT INT
 
 ODF_INSTALL_NAMESPACE="openshift-storage"
+ODF_OPERATOR_GROUP="openshift-storage-operator-group"
 
 echo "Creating the ODF installation namespace"
 oc apply -f - <<EOF
@@ -18,21 +21,26 @@ oc apply -f - <<EOF
       name: "${ODF_INSTALL_NAMESPACE}"
 EOF
 
-echo "Applying StorageSystem after operator installation completed"
+echo "Selecting worker nodes for ODF"
+oc label nodes cluster.ocs.openshift.io/openshift-storage='' --selector='node-role.kubernetes.io/worker' --overwrite
 
-cat <<EOF | oc apply -f -
-  apiVersion: odf.openshift.io/v1alpha1
-  kind: StorageSystem
-  metadata:
-    name: ocs-storagecluster-storagesystem
-    namespace: "${ODF_INSTALL_NAMESPACE}"
-  spec:
-    kind: storagecluster.ocs.openshift.io/v1
-    name: ocs-storagecluster
-    namespace: "${ODF_INSTALL_NAMESPACE}"
-EOF
+#echo "Applying StorageSystem after operator installation completed"
+#
+#cat <<EOF | oc apply -f -
+#  apiVersion: odf.openshift.io/v1alpha1
+#  kind: StorageSystem
+#  metadata:
+#    name: ocs-storagecluster-storagesystem
+#    namespace: "${ODF_INSTALL_NAMESPACE}"
+#  spec:
+#    kind: storagecluster.ocs.openshift.io/v1
+#    name: ocs-storagecluster
+#    namespace: "${ODF_INSTALL_NAMESPACE}"
+#EOF
+#
+#sleep 120
 
-sleep 120
+echo "INSTALL OPERATOR REF SIMULATE"
 
 echo "Deploying a StorageCluster"
 cat <<EOF | oc apply -f -
@@ -49,8 +57,10 @@ spec:
       spec:
         accessModes:
         - ReadWriteOnce
-        resources: {}
-        storageClassName: gp2-csi
+        resources:
+          requests:
+            storage: "${ODF_STORAGE_CLAIM}"
+        storageClassName: "${ODF_STORAGE_CLASS}"
         volumeMode: Block
     name: ocs-deviceset
     placement: {}
