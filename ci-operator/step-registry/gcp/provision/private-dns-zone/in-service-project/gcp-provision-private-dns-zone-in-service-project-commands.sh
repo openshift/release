e771667d8fc06167adacd50138aedeb934ad821d
Jianli Wei <jiwei@redhat.com>
2024-03-05 13:31:01 +0800

    [CORS-2591] Add support to BYO hosted zones while deploying OpenShift to a shared VPC (XPN) in GCP (#49407)

--- ci-operator/step-registry/gcp/provision/private-dns-zone/in-service-project/gcp-provision-private-dns-zone-in-service-project-commands.sh
+++ ci-operator/step-registry/gcp/provision/private-dns-zone/in-service-project/gcp-provision-private-dns-zone-in-service-project-commands.sh
@@ -15,6 +15,7 @@ else
 fi
 
 CLUSTER_NAME="${NAMESPACE}-${UNIQUE_HASH}"
+GCP_BASE_DOMAIN="$(< ${CLUSTER_PROFILE_DIR}/public_hosted_zone)"
 
 export GCP_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/gce.json"
 GCP_PROJECT="$(< ${CLUSTER_PROFILE_DIR}/openshift_gcp_project)"
@@ -25,7 +26,7 @@ then
   gcloud config set project "${GCP_PROJECT}"
 fi
 
-gcloud dns managed-zones create "${CLUSTER_NAME}-private-zone" --description "Pre-created private DNS zone" --visibility "private" --dns-name "${CLUSTER_NAME}.${BASE_DOMAIN%.}." --networks "${NETWORK}"
+gcloud dns managed-zones create "${CLUSTER_NAME}-private-zone" --description "Pre-created private DNS zone" --visibility "private" --dns-name "${CLUSTER_NAME}.${GCP_BASE_DOMAIN}." --networks "${NETWORK}"
 cat > "${SHARED_DIR}/private-dns-zone-destroy.sh" << EOF
 gcloud dns managed-zones delete -q ${CLUSTER_NAME}-private-zone
 EOF
