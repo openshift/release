a9cb200652e45adc6c091098ab3f3ac76c44efe5
Xue Li <xueli@redhat.com>
2024-03-11 13:49:27 +0800

    Support additional security groups in machinepool creation (#49628)
    Fix taints
    
    Support node pool waiting
    
    Fix shell failure
    
    Add a workflow to enable nodepool creation with SGs
    
    Fix shell failure
    
    Fix shell
    
    Fix jq command
    
    Fix commit
    
    Fix  TAINTS: unbound variable
    
    Fix MP_TAINTS issue
    
    fix readonly issue
    
    FIx

--- ci-operator/step-registry/rosa/cluster/wait-ready/nodes/rosa-cluster-wait-ready-nodes-commands.sh
+++ ci-operator/step-registry/rosa/cluster/wait-ready/nodes/rosa-cluster-wait-ready-nodes-commands.sh
@@ -150,6 +150,20 @@ function getDesiredComputeCount {
     echo "--auto-scaling disabled, --replicas set, retrieving replica count desired"
     desired_compute_count=$(rosa describe cluster -c "$CLUSTER_ID"  -o json  |jq -r '.nodes.compute')
   fi
+  MP_NAME=$(cat $SHARED_DIR/mp_name|| true)
+  if [[ ! -z ${MP_NAME} ]];then
+    compute_count=$(rosa describe machinepool ${MP_NAME}  -c "$CLUSTER_ID"  -o json  |jq -r '.replicas')
+      if [[ "$compute_count" = "null" ]]; then 
+        echo "--auto-scaling enabled, retrieving min_replica count desired"
+        if [[ $HOSTED_CP = "true" ]];then
+          compute_count=$(rosa describe machinepool  ${MP_NAME} -c "$CLUSTER_ID" -o json  | jq -r '.autoscaling.min_replica') 
+        else
+          compute_count=$(rosa describe machinepool  ${MP_NAME} -c "$CLUSTER_ID" -o json  | jq -r '.autoscaling.min_replicas') 
+        fi
+      fi
+    desired_compute_count=$(expr $desired_compute_count + $compute_count)
+  fi
+
   export desired_compute_count
   echo "Total desired node count: $desired_compute_count"
 }
