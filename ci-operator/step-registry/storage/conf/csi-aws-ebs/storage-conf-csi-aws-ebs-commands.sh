9350a7fd49023e01ea890fb74cadb5934b5997c5
Jonathan Dobson <jdobson@redhat.com>
2024-03-12 12:35:40 -0600

    OCPBUGS-30620: csi-operator: clean up legacy test manifests (#49655)
    * csi-operator: use test manifests from top-level directory
    
    * csi-operator: build test images from common Dockerfile.test

--- ci-operator/step-registry/storage/conf/csi-aws-ebs/storage-conf-csi-aws-ebs-commands.sh
+++ ci-operator/step-registry/storage/conf/csi-aws-ebs/storage-conf-csi-aws-ebs-commands.sh
@@ -3,12 +3,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [ -d /go/src/github.com/openshift/csi-operator/legacy/ ]; then
-    echo "Using legacy csi directory"
+if [ -d /go/src/github.com/openshift/csi-operator/test/e2e/aws-ebs/ ]; then
+    echo "Using csi-operator repo"
+    cd /go/src/github.com/openshift/csi-operator
+    cp test/e2e/aws-ebs/manifest.yaml ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
+elif [ -d /go/src/github.com/openshift/csi-operator/legacy/ ]; then
+    echo "Using csi-operator repo legacy dir"
     cd /go/src/github.com/openshift/csi-operator/legacy/aws-ebs-csi-driver-operator
     cp test/e2e/manifest.yaml ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
 else
-    echo "Using regular csi directory"
+    echo "Using aws-ebs-csi-driver-operator repo"
     cd /go/src/github.com/openshift/aws-ebs-csi-driver-operator
     cp test/e2e/manifest.yaml ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
 fi
