712a24b1545a42cfd3be29a35f693bf115d5ce8f
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2024-03-14 12:56:10 +0100

    USHIFT-2615: Set up pull secrets for rebase jobs

--- ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
+++ ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
@@ -10,6 +10,10 @@ export PATH="${HOME}/.local/bin:${PATH}"
 python3 -m ensurepip --upgrade
 pip3 install setuptools-rust cryptography pyyaml pygithub gitpython
 
+# Set up pull secrets for later downloading of containers.
+mkdir -p ${HOME}/.docker
+cp /secrets/ci-pull-secret ${HOME}/.docker/config.json
+
 cd /go/src/github.com/openshift/microshift/
 DEST_DIR=${HOME}/.local/bin ./scripts/fetch_tools.sh yq
 ./scripts/auto-rebase/rebase_job_entrypoint.sh
