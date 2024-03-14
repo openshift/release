66d04e309ce00b765243e017ee0487db9c77e646
Jianlin Liu <jialiu@redhat.com>
2024-03-14 12:01:02 +0800

    print more info to help debugging (#49816)

--- ci-operator/step-registry/cucushift/installer/check/info/cucushift-installer-check-info-commands.sh
+++ ci-operator/step-registry/cucushift/installer/check/info/cucushift-installer-check-info-commands.sh
@@ -8,6 +8,7 @@ function run_command() {
     local CMD="$1"
     echo "Running command: ${CMD}"
     eval "${CMD}"
+    echo ""
 }
 
 if [ -f "${SHARED_DIR}/kubeconfig" ] ; then
@@ -21,9 +22,13 @@ fi
 set +e
 run_command "oc get infrastructures.config.openshift.io cluster -oyaml"
 run_command "oc get clusterversion/version"
+run_command "oc get clusterversion/version -ojson"
 run_command "oc get nodes -owide"
+run_command "oc get machines.machine.openshift.io -n openshift-machine-api"
+run_command "oc get machines.machine.openshift.io -n openshift-machine-api -ojson"
 run_command "oc get clusteroperators"
 run_command "oc get machineconfig"
+run_command "oc get mcp"
 run_command "oc get proxies.config.openshift.io cluster -oyaml"
 run_command "oc get networks.config.openshift.io cluster -oyaml"
 run_command "oc get networks.operator.openshift.io cluster -oyaml"
