d84b0a9fb564bd4f76d47f5b83652b41065afa35
Aidan Reilly <74046732+aireilly@users.noreply.github.com>
2024-03-15 10:18:53 +0000

    Adding Vale bot account secret (#49906)

--- ci-operator/step-registry/openshift-docs/vale-review/openshift-docs-vale-review-commands.sh
+++ ci-operator/step-registry/openshift-docs/vale-review/openshift-docs-vale-review-commands.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# We don't care if the GH comment step fails
 # set -o nounset
 # set -o errexit
 # set -o pipefail
 # set -o verbose
 
-GITHUB_AUTH_TOKEN=$(cat /tmp/vault/ocp-docs-github-secret/GITHUB_AUTH_TOKEN)
+GITHUB_AUTH_TOKEN=$(cat /tmp/vault/ocp-docs-vale-github-secret/GITHUB_AUTH_TOKEN)
 
 export GITHUB_AUTH_TOKEN
 
