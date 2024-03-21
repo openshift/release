ae310e81fd31c28416f0406f51375a0d197a84f8
Liang Xia <lxia@redhat.com>
2024-03-21 10:46:56 +0800

    Optimize test filter on hypershift-hosted for upgrade jobs (#49938)

--- ci-operator/step-registry/cucushift/upgrade/check/cucushift-upgrade-check-commands.sh
+++ ci-operator/step-registry/cucushift/upgrade/check/cucushift-upgrade-check-commands.sh
@@ -134,6 +134,14 @@ function filter_test_by_proxy() {
     fi
     echo_upgrade_tags
 }
+function filter_test_by_hypershift() {
+    local topo
+    topo="$(oc get infrastructures.config.openshift.io cluster -o yaml | yq '.status.controlPlaneTopology')"
+    if [[ "_${topo}_" = '_External_' ]] ; then
+        export UPGRADE_CHECK_RUN_TAGS="@hypershift-hosted and ${UPGRADE_CHECK_RUN_TAGS}"
+    fi
+    echo_upgrade_tags
+}
 function filter_test_by_fips() {
     local data
     data="$(oc get configmap cluster-config-v1 -n kube-system -o yaml | yq '.data')"
@@ -212,6 +220,7 @@ function filter_test_by_capability() {
 function filter_tests() {
     filter_test_by_capability
     filter_test_by_fips
+    filter_test_by_hypershift
     filter_test_by_proxy
     filter_test_by_sno
     filter_test_by_network
