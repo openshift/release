0080650562c40436445618a1ec972c314d1ad779
vishnuchalla <vishnuchalla47@gmail.com>
2024-03-21 01:56:41 -0400

    Adding leftover e2e CI tests (#50025)
    Signed-off-by: Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.remote.csb>
    Co-authored-by: Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.remote.csb>

---
+++ ci-operator/step-registry/cloud-bulldozer/e2e-benchmarking/network-policy/cloud-bulldozer-e2e-benchmarking-network-policy-commands.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+cat /etc/os-release
+oc config view
+oc projects
+pushd /tmp
+
+
+if [[ "$JOB_TYPE" == "presubmit" ]] && [[ "$REPO_OWNER" = "cloud-bulldozer" ]] && [[ "$REPO_NAME" = "e2e-benchmarking" ]]; then
+    git clone https://github.com/${REPO_OWNER}/${REPO_NAME}
+    pushd ${REPO_NAME}
+    git pull origin pull/${PULL_NUMBER}/head:${PULL_NUMBER}
+    git switch ${PULL_NUMBER}
+    pushd workloads/kube-burner
+
+    export JOB_TIMEOUT=${JOB_TIMEOUT:=21600}
+    current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker=,node-role.kubernetes.io/infra!=,node-role.kubernetes.io/workload!= --output jsonpath="{.items[?(@.status.conditions[-1].type=='Ready')].status.conditions[-1].type}" | wc -w | xargs)
+    export WORKLOAD=${WORKLOAD:=networkpolicy-case1}
+
+    case $WORKLOAD in
+        networkpolicy-case1)
+            JOB_ITERATIONS=$(( 5 * $current_worker_count ))
+            ;;
+
+        networkpolicy-case2)
+            JOB_ITERATIONS=$(( 1 * $current_worker_count ))
+            ;;
+
+        networkpolicy-case3)
+            JOB_ITERATIONS=$(( 4 * $current_worker_count ))
+            ;;
+        *)
+            echo Unsupported $WORKLOAD workload type
+            ;;
+    esac
+    
+    echo $JOB_ITERATIONS is JOB_ITERATIONS
+    export JOB_ITERATIONS
+    ./run.sh
+else
+    echo "We are sorry, this job is only meant for cloud-bulldozer/e2e-benchmarking repo PR testing"
+fi
