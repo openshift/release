a3c37dab970438dbc1b3368f0063feca4070c121
Priti Kumari <pkumari@redhat.com>
2024-03-22 11:24:55 +0530

    Add CI for pipeline sample pull_request and push event type (#49986)
    rh-pre-commit.version: 2.1.0
    rh-pre-commit.check-secrets: ENABLED

--- ci-operator/step-registry/redhat-appstudio/tssc-sample-pipelines/validate-rhtap-pipelines/redhat-appstudio-tssc-sample-pipelines-validate-rhtap-pipelines-commands.sh
+++ ci-operator/step-registry/redhat-appstudio/tssc-sample-pipelines/validate-rhtap-pipelines/redhat-appstudio-tssc-sample-pipelines-validate-rhtap-pipelines-commands.sh
@@ -4,11 +4,10 @@ set -o nounset
 set -o pipefail
 
 status=0
-DEBUG_OUTPUT=/tmp/log.txt
-
 export OPENSHIFT_API \
   OPENSHIFT_PASSWORD \
-  GIT_REPO_URL
+  GIT_REPO_URL \
+  EVENT_TYPE
 
 ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROX_TOKEN=$(cat /usr/local/rhtap-ci-secrets/rhtap/acs-api-token)
@@ -16,6 +15,7 @@ ROX_ENDPOINT=$(cat /usr/local/rhtap-ci-secrets/rhtap/acs-central-endpoint)
 IMAGE_REPOSITORY=$(cat /usr/local/rhtap-ci-secrets/rhtap/quay-default-image-repository)
 DOCKER_USERNAME=$(cat /usr/local/rhtap-ci-secrets/rhtap/quay-robot-username)
 DOCKER_PASSWORD=$(cat /usr/local/rhtap-ci-secrets/rhtap/quay-robot-password)
+EVENT_TYPE=${EVENT_TYPE:-'push'}
 OPENSHIFT_API="$(yq e '.clusters[0].cluster.server' "$KUBECONFIG")"
 GIT_REPO_URL="https://github.com/prietyc123-qe-org/rhtap-go-sample"
 
@@ -33,7 +33,6 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-
 CONFIG_FILE="$ROOT_DIR/hack/build/build-config.env"
 cat <<EOF > "$CONFIG_FILE"
 # Set your own build configuration
@@ -45,34 +44,7 @@ ROX_ENDPOINT="${ROX_ENDPOINT:-in-cluster}"
 ROX_TOKEN="$ROX_TOKEN"
 EOF
 
-wait_for_pipeline() {
-    local timeout_seconds=$((15 * 60))
-    if ! oc wait --for=condition=succeeded "$1" -n "$2" --timeout "${timeout_seconds}s" >"$DEBUG_OUTPUT"; then
-        echo "[ERROR] RHTAP Pipeline failed to complete successful" >&2
-        oc get "$1" -n "$2" >"$DEBUG_OUTPUT"
-        exit 1
-    fi
-}
-
-NAMESPACE="test-pipeline$((RANDOM % 10000000))"
-
-echo "Create a new test project"
-oc new-project "$NAMESPACE"
-
-echo "Set the newly created project as the current project"
-oc project "$NAMESPACE"
-
-echo "Preparing rhtap sample pipelines build resources..."
-hack/build/prepare-build-resources.sh || status="$?" || :
-
-echo "Apply the rhtap tasks and pipelines in the test namspace $NAMESPACE..."
-oc apply -f "${ROOT_DIR}/pac/tasks"
-oc apply -f "${ROOT_DIR}/pac/pipelines/docker-build-rhtap.yaml"
-
-echo "Run the rhtap sample build pipeline..."
-hack/build/run-build.sh || status="$?" || :
-
-PIPELINE="${PIPELINE:-docker-build-rhtap}"
-wait_for_pipeline "pipelineruns/$PIPELINE" "$NAMESPACE"
+echo "Run the rhtap sample build pipeline test..."
+test/validate-pipeline.sh || status="$?" || :
 
 exit $status
