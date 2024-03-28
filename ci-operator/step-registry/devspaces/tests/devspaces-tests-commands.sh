54a3efd5a6dda2d1b3a4776da48cb55de21fdb32
lisa <liswang@redhat.com>
2024-03-28 17:05:50 -0400

    test

--- ci-operator/step-registry/devspaces/tests/devspaces-tests-commands.sh
+++ ci-operator/step-registry/devspaces/tests/devspaces-tests-commands.sh
@@ -11,18 +11,14 @@ export KUBECONFIG=$SHARED_DIR/kubeconfig
 # login for interop
 if test -f ${SHARED_DIR}/kubeadmin-password
 then
-  echo "SHARED:"
   OCP_CRED_USR="kubeadmin"
   export OCP_CRED_USR
   OCP_CRED_PSW="$(cat ${SHARED_DIR}/kubeadmin-password)"
   export OCP_CRED_PSW
   oc login -u kubeadmin -p "$(cat $SHARED_DIR/kubeadmin-password)" "${API_URL}" --insecure-skip-tls-verify=true
 else #login for ROSA & Hypershift platforms
-  echo "SHARE2"
   eval "$(cat "${SHARED_DIR}/api.login")"
 fi
-echo "SHARED: ${SHARED_DIR}/kubeadmin-password" || true
-echo "SHARED1: $(cat ${SHARED_DIR}/api.login)" || true
 
 cd /tmp/devspaces/scripts
 #cp /var/run/secrets/ci.openshift.io/multi-stage/kubeconfig ./
