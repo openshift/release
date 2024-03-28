1c967cd16b08351dbb40f789e2e555a3dcc8b1b0
He Liu <heli@redhat.com>
2024-03-28 22:09:01 +0800

    debug

--- ci-operator/step-registry/cucushift/hypershift-extended/capi/deprovision/cucushift-hypershift-extended-capi-deprovision-commands.sh
+++ ci-operator/step-registry/cucushift/hypershift-extended/capi/deprovision/cucushift-hypershift-extended-capi-deprovision-commands.sh
@@ -5,7 +5,7 @@ export KUBECONFIG="${SHARED_DIR}/kubeconfig"
 if [[ -f "${SHARED_DIR}/mgmt_kubeconfig" ]]; then
   export KUBECONFIG="${SHARED_DIR}/mgmt_kubeconfig"
 fi
-oc -n default delete clusters.cluster.x-k8s.io ${CLUSTER_NAME}
+oc -n default delete clusters.cluster.x-k8s.io ${CLUSTER_NAME} --ignore-not-found
 oc -n default delete secret rosa-creds-secret --ignore-not-found
 oc -n default delete AWSClusterControllerIdentity default --ignore-not-found
 
