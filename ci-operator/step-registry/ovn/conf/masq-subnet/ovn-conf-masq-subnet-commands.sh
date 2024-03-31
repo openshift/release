d23199959fa26e5796bc27fc5256c1b9bfb0c0ae
Jean Chen <jechen@redhat.com>
2024-03-31 11:28:52 -0400

    416-masq-subnet

---
+++ ci-operator/step-registry/ovn/conf/masq-subnet/ovn-conf-masq-subnet-commands.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+if [ "$MASQ_TEST" == "ipv4" ]; then
+cat >> "manifests/cluster-network-03-config.yml" << EOF
+apiVersion: operator.openshift.io/v1
+kind: Network
+metadata:
+  name: cluster
+spec:
+  defaultNetwork:
+    ovnKubernetesConfig:
+      gatewayConfig:
+        ipv4:
+          internalMasqueradeSubnet: $MASQ_SUBNET_IPV4
+EOF
+fi
+
+if [ "$MASQ_TEST" == "ipv6" ]; then
+cat >> "manifests/cluster-network-03-config.yml" << EOF
+apiVersion: operator.openshift.io/v1
+kind: Network
+metadata:
+  name: cluster
+spec:
+  defaultNetwork:
+    ovnKubernetesConfig:
+      gatewayConfig:
+        ipv6:
+          internalMasqueradeSubnet: $MASQ_SUBNET_IPV6
+EOF
+fi
+
+if [ "$MASQ_TEST" == "dual" ]; then
+cat >> "manifests/cluster-network-03-config.yml" << EOF
+apiVersion: operator.openshift.io/v1
+kind: Network
+metadata:
+  name: cluster
+spec:
+  defaultNetwork:
+    ovnKubernetesConfig:
+      gatewayConfig:
+        ipv4:
+          internalMasqueradeSubnet: $MASQ_SUBNET_IPV4
+        ipv6:
+          internalMasqueradeSubnet: $MASQ_SUBNET_IPV6
+EOF
+fi
+
+cat "manifests/cluster-network-03-config.yml"
