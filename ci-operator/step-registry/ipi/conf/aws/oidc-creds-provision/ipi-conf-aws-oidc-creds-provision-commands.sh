b1ec6f649c25a78cd8dd0733883e5fc80daeca9c
Yunfei Jiang <yunjiang@redhat.com>
2024-04-02 11:20:02 +0800

    debug

--- ci-operator/step-registry/ipi/conf/aws/oidc-creds-provision/ipi-conf-aws-oidc-creds-provision-commands.sh
+++ ci-operator/step-registry/ipi/conf/aws/oidc-creds-provision/ipi-conf-aws-oidc-creds-provision-commands.sh
@@ -7,7 +7,13 @@ set -o pipefail
 MPREFIX="${SHARED_DIR}/manifest"
 TPREFIX="${SHARED_DIR}/tls"
 infra_name=${NAMESPACE}-${UNIQUE_HASH}
-export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
+# export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
+if [[ -f "${SHARED_DIR}/aws_minimal_permission" ]]; then
+    echo "Setting AWS credential with minimal permision for installer"
+    export AWS_SHARED_CREDENTIALS_FILE=${SHARED_DIR}/aws_minimal_permission
+else
+    export AWS_SHARED_CREDENTIALS_FILE=${CLUSTER_PROFILE_DIR}/.awscred
+fi
 REGION="${LEASED_RESOURCE}"
 
 # release-controller always expose RELEASE_IMAGE_LATEST when job configuraiton defines release:latest image
