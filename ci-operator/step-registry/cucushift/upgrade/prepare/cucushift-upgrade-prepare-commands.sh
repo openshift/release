5c22fcadf45e67da39c2a1159d9b539c3e7f5c8d
Liang Xia <lxia@redhat.com>
2024-04-02 17:07:19 +0800

    Treat platform External as none (#50452)

--- ci-operator/step-registry/cucushift/upgrade/prepare/cucushift-upgrade-prepare-commands.sh
+++ ci-operator/step-registry/cucushift/upgrade/prepare/cucushift-upgrade-prepare-commands.sh
@@ -77,12 +77,8 @@ function filter_test_by_platform() {
     extrainfoCmd="oc get infrastructure cluster -o yaml | yq '.status'"
     if [[ -n "$platform" ]] ; then
         case "$platform" in
-            none|powervs)
+            external|none|powervs)
                 export UPGRADE_PRE_RUN_TAGS="@baremetal-upi and ${UPGRADE_PRE_RUN_TAGS}"
-                eval "$extrainfoCmd"
-                ;;
-            external)
-                echo "Expected, got platform as '$platform'"
                 eval "$extrainfoCmd"
                 ;;
             alibabacloud)
