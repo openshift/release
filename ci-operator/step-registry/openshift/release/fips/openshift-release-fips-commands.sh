8e84f777308a0ce05012f088440fcbfcd1178982
Ashwin Das <ashwindasr@gmail.com>
2024-04-02 15:29:47 -0400

    guard aginst digest based pullspecs

--- ci-operator/step-registry/openshift/release/fips/openshift-release-fips-commands.sh
+++ ci-operator/step-registry/openshift/release/fips/openshift-release-fips-commands.sh
@@ -4,6 +4,11 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+if [[ "${RELEASE_IMAGE_LATEST}" == *"@sha256"* ]]; then
+    echo "digest based pullspecs is not supported"
+    exit 1
+fi
+
 # Get major.minor from payload url
 major_minor=$(echo "${RELEASE_IMAGE_LATEST}" | awk -F':' '{print $2}' | awk -F'.' '{print $1"."$2}')
 
