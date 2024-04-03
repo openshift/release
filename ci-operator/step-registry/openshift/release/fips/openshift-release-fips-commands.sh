b81e731d0d0644664e565260d838a0a7dfc7ad59
Ashwin Das <ashwindasr@gmail.com>
2024-04-03 14:05:59 -0400

    refinement

--- ci-operator/step-registry/openshift/release/fips/openshift-release-fips-commands.sh
+++ ci-operator/step-registry/openshift/release/fips/openshift-release-fips-commands.sh
@@ -6,8 +6,8 @@ set -o pipefail
 
 payload_url="${RELEASE_IMAGE_LATEST}"
 
-if [[ "${RELEASE_IMAGE_LATEST}" == *"@sha256"* ]]; then
-    payload_url=$(echo "${RELEASE_IMAGE_LATEST}" | sed 's/@sha256.*/:latest/')
+if [[ "$payload_url" == *"@sha256"* ]]; then
+    payload_url=$(echo "$payload_url" | sed 's/@sha256.*/:latest/')
 fi
 
 ./check-payload scan payload -V "${MAJOR_MINOR}" --url "${payload_url}"
