e8af1289a8b34a32dddc06c89eeebb7237453ff9
Ashwin Das <ashwindasr@gmail.com>
2024-04-03 14:03:45 -0400

    handle digest payload_url

--- ci-operator/step-registry/openshift/release/fips/openshift-release-fips-commands.sh
+++ ci-operator/step-registry/openshift/release/fips/openshift-release-fips-commands.sh
@@ -4,12 +4,10 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+payload_url="${RELEASE_IMAGE_LATEST}"
+
 if [[ "${RELEASE_IMAGE_LATEST}" == *"@sha256"* ]]; then
-    echo "digest based pullspecs is not supported"
-    exit 1
+    payload_url=$(echo "${RELEASE_IMAGE_LATEST}" | sed 's/@sha256.*/:latest/')
 fi
 
-# Get major.minor from payload url
-major_minor=$(echo "${RELEASE_IMAGE_LATEST}" | awk -F':' '{print $2}' | awk -F'.' '{print $1"."$2}')
-
-./check-payload scan payload -V "$major_minor" --url "${RELEASE_IMAGE_LATEST}"
+./check-payload scan payload -V "${MAJOR_MINOR}" --url "${payload_url}"
