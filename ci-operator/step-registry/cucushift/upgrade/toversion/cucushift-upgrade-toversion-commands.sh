2b417e4026a12ec71a2fd3a1307391ec89fadd10
Jianlin Liu <jialiu@redhat.com>
2024-04-03 15:48:32 +0800

    re-export PATH to ensure extracted oc/ccoctl clients can be loaded correctly (#50466)

--- ci-operator/step-registry/cucushift/upgrade/toversion/cucushift-upgrade-toversion-commands.sh
+++ ci-operator/step-registry/cucushift/upgrade/toversion/cucushift-upgrade-toversion-commands.sh
@@ -87,6 +87,7 @@ function extract_ccoctl(){
     else
         chmod 775 /tmp/ccoctl
     fi
+    export PATH="$PATH"
 }
 
 function update_cloud_credentials_oidc(){
@@ -194,6 +195,7 @@ function extract_oc(){
         sleep 60
     done
     mv ${tmp_oc}/oc ${OC_DIR} -f
+    export PATH="$PATH"
     which oc
     oc version --client
     return 0
