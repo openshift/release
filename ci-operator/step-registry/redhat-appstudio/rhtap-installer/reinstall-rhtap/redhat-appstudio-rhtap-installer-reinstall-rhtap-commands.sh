94af166d133a46a6faf3b159ed920e9e65b8c8b3
Romain Arnaud <rarnaud@redhat.com>
2024-04-03 12:58:09 -0400

    rhtap: Update pipeline task name

--- ci-operator/step-registry/redhat-appstudio/rhtap-installer/reinstall-rhtap/redhat-appstudio-rhtap-installer-reinstall-rhtap-commands.sh
+++ ci-operator/step-registry/redhat-appstudio/rhtap-installer/reinstall-rhtap/redhat-appstudio-rhtap-installer-reinstall-rhtap-commands.sh
@@ -138,7 +138,7 @@ install_rhtap(){
     apiVersion: tekton.dev/v1
     kind: PipelineRun
     metadata:
-      generateName: trusted-application-pipeline-pe-info-
+      generateName: redhat-trusted-application-pipeline-pe-info-
       namespace: "$NAMESPACE"
     spec:
       pipelineSpec:
@@ -150,7 +150,7 @@ install_rhtap(){
                 - name: kind
                   value: task
                 - name: name
-                  value: trusted-application-pipeline-pe-info
+                  value: redhat-trusted-application-pipeline-pe-info
                 - name: namespace
                   value: "$NAMESPACE"
 EOF
