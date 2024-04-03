6c0793ae54166b0509615a2f19c322eab8f42f28
Jian Zhang <jiazha@redhat.com>
2024-04-03 18:00:32 +0800

    use the index image as the cache since OCP4.15 (#50339)

--- ci-operator/step-registry/enable-qe-catalogsource/logging/enable-qe-catalogsource-logging-commands.sh
+++ ci-operator/step-registry/enable-qe-catalogsource/logging/enable-qe-catalogsource-logging-commands.sh
@@ -107,6 +107,35 @@ EOF
 function create_catalog_sources()
 {
     echo "create QE catalogsource: qe-app-registry"
+    # get cluster Major.Minor version
+    kube_major=$(oc version -o json |jq -r '.serverVersion.major')
+    kube_minor=$(oc version -o json |jq -r '.serverVersion.minor')
+    # since OCP 4.15, the official catalogsource use this way. OCP4.14=K8s1.27
+    # details: https://issues.redhat.com/browse/OCPBUGS-31427
+    if [[ ${kube_major} -gt 1 || ${kube_minor} -gt 27 ]]; then
+        echo "the index image as the initContainer cache image)" 
+        cat <<EOF | oc create -f -
+apiVersion: operators.coreos.com/v1alpha1
+kind: CatalogSource
+metadata:
+  name: qe-app-registry
+  namespace: openshift-marketplace
+spec:
+  displayName: Production Operators
+  grpcPodConfig:
+    extractContent:
+      cacheDir: /tmp/cache
+      catalogDir: /configs
+    memoryTarget: 30Mi
+  image: ${LOGGING_INDEX_IMAGE}
+  publisher: OpenShift QE
+  sourceType: grpc
+  updateStrategy:
+    registryPoll:
+      interval: 15m
+EOF
+    else
+        echo "the index image as the server image" 
     cat <<EOF | oc create -f -
 apiVersion: operators.coreos.com/v1alpha1
 kind: CatalogSource
@@ -122,6 +151,7 @@ spec:
     registryPoll:
       interval: 15m
 EOF
+    fi
     set +e 
     COUNTER=0
     while [ $COUNTER -lt 600 ]
