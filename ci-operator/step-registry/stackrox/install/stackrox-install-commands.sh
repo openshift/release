02926bc89b2bd37d8aeed95cc5b6bc08bcb0f0d5
davdhacs <105243888+davdhacs@users.noreply.github.com>
2024-04-04 14:25:57 -0600

    kubeconfig from shared_dir

--- ci-operator/step-registry/stackrox/install/stackrox-install-commands.sh
+++ ci-operator/step-registry/stackrox/install/stackrox-install-commands.sh
@@ -2,6 +2,9 @@
 
 set +ev
 
+export KUBECONFIG=${SHARED_DIR}/kubeconfig
+ls -la "${SHARED_DIR}"
+
 /bin/bash <(curl -fsSL https://raw.githubusercontent.com/stackrox/stackrox/master/scripts/quick-helm-install.sh | sed -e 's/^logmein$//')
 
 kubectl get -n stackrox centrals.platform.stackrox.io stackrox-central-services --output=json
