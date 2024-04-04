5f89916c6f408f9482220f7da9ed766b3a10bb2f
Vincent Boulos <87659226+vboulos@users.noreply.github.com>
2024-04-04 16:45:03 -0400

    Vboulos acm210 ocp4.16 (#50199)
    * add ocp 4.16 with ACM 2.10, update component to OPPQE
    
    * add ocp 4.16 with ACM 2.10, update component to OPPQE
    
    * make update
    
    * update the EPIC in firewatch to reflect the repective stream
    
    * update manageddclusterset binding to vbeta2
    
    * minor update
    
    * minor update
    
    * minor clc update
    
    * fix the must-gather image
    
    * add slack message for the OCP 4.16 job

--- ci-operator/step-registry/acm/tests/clc-create/acm-tests-clc-create-commands.sh
+++ ci-operator/step-registry/acm/tests/clc-create/acm-tests-clc-create-commands.sh
@@ -35,6 +35,6 @@ CLOUD_PROVIDERS=$(cat $SECRETS_DIR/clc/ocp_cloud_providers)
 export CLOUD_PROVIDERS
 
 # run the test execution script
-./execute_clc_interop_commands.sh
+bash +x ./execute_clc_interop_commands.sh
 
 cp -r reports $ARTIFACT_DIR/
