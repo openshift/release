2bcad9f24eafc8a3191daa898624207f9ddca970
Ashwin Das <ashwindasr@gmail.com>
2024-04-05 15:04:55 -0400

    set runtime

--- ci-operator/step-registry/openshift/release/fips/openshift-release-fips-commands.sh
+++ ci-operator/step-registry/openshift/release/fips/openshift-release-fips-commands.sh
@@ -34,6 +34,9 @@ if [[ "$payload_url" == *"@sha256"* ]]; then
     payload_url=$(echo "$payload_url" | sed 's/@sha256.*/:latest/')
 fi
 
+echo "Setting runtime dir"
+mkdir -p /tmp/.docker/ ${XDG_RUNTIME_DIR}
+
 echo "copy creds"
 cp /tmp/import-secret/.dockerconfigjson /tmp/.docker/config.json
 
