bad6768faf7be43db7999251d137e68f832470fc
Ashwin Das <ashwindasr@gmail.com>
2024-04-07 12:28:44 -0400

    test with --kubeconfig

--- ci-operator/step-registry/openshift/release/fips/openshift-release-fips-commands.sh
+++ ci-operator/step-registry/openshift/release/fips/openshift-release-fips-commands.sh
@@ -35,8 +35,12 @@ oc project $namespace
 echo "Setting runtime dir"
 mkdir -p /tmp/.docker/ ${XDG_RUNTIME_DIR}
 
+echo "copy creds"
+cp /tmp/import-secret/.dockerconfigjson /tmp/.docker/config.json
+
 echo "Login to registry"
 oc registry login --to /tmp/.docker/config.json
+echo "Registry login successful"
 
 payload_url="${RELEASE_IMAGE_LATEST}"
 
@@ -46,8 +50,8 @@ fi
 
 # run node scan and check the result
 report="/tmp/fips-check-payload-scan.log"
-oc --request-timeout=300s debug node/"$master_node_0" -- chroot /host bash -c "unset KUBECONFIG; podman run --authfile /var/lib/kubelet/config.json --privileged -i -v /:/myroot registry.ci.openshift.org/ci/check-payload:latest scan payload -V $MAJOR_MINOR --url $payload_url &> $report" || true
-out=$(oc --request-timeout=300s debug node/"$master_node_0" -- chroot /host bash -c "cat /$report" || true)
+oc --request-timeout=300s -n $namespace debug node/"$master_node_0" -- chroot /host bash -c "podman run --authfile /var/lib/kubelet/config.json --privileged -i -v /:/myroot registry.ci.openshift.org/ci/check-payload:latest scan payload -V $MAJOR_MINOR --url $payload_url &> $report" --kubeconfig /tmp/.docker/config.json || true
+out=$(oc --request-timeout=300s -n $namespace debug node/"$master_node_0" -- chroot /host bash -c "cat /$report" || true)
 echo "The report is: $out"
 oc delete ns $namespace || true
 res=$(echo "$out" | grep -E 'Failure Report|Successful run with warnings|Warning Report' || true)
