b3c4ecc181d6527993ca2c9b19d2b8ad338846e7
Jianli Wei <jiwei@redhat.com>
2024-04-07 12:04:08 +0800

    OSD on GCP into shared VPC private cluster (#50326)

--- ci-operator/step-registry/osd-ccs/cluster/operators/wait-ready/osd-ccs-cluster-operators-wait-ready-commands.sh
+++ ci-operator/step-registry/osd-ccs/cluster/operators/wait-ready/osd-ccs-cluster-operators-wait-ready-commands.sh
@@ -6,6 +6,12 @@ set -o pipefail
 
 trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
 
+if test -f "${SHARED_DIR}/proxy-conf.sh"
+then
+    # shellcheck disable=SC1090
+    source "${SHARED_DIR}/proxy-conf.sh"
+fi
+
 # Even the cluster is shown ready on ocm side, and the cluster operators are available, some of the cluster operators are
 # still progressing. The ocp e2e test scenarios requires PROGRESSING=False for each cluster operator.
 echo "Wait for cluster operators' progressing ready..."
