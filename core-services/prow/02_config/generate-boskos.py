568a7ffe7ad3f12ae12673400f95b0162f61f098
Shang Gao <sgao@redhat.com>
2024-04-09 15:27:37 +0800

    Remove 871 network from devqe profile (#50667)

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -431,7 +431,7 @@ for i in [1148,1197,1207,1225,1227,1229,1232,1233,1234,1235,1237,1238,1240,1243,
 for i in [990,1169,1166,1164,1146]:
     CONFIG['vsphere-dis-2-quota-slice']['bcr01a.dal12.{}'.format(i)] = 1
 
-for i in [871,991,1165,1154,1148,1140]:
+for i in [991,1165,1154,1148,1140]:
     CONFIG['vsphere-connected-2-quota-slice']['bcr01a.dal12.{}'.format(i)] = 1
 
 for i in [1287,1289,1296,1298,1300,1302]:
