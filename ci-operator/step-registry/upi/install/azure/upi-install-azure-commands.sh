5d4885762550680f45f1b7af589f5fe0e7008368
jima <jima@redhat.com>
2024-04-16 15:58:08 +0800

    debug azure upi

--- ci-operator/step-registry/upi/install/azure/upi-install-azure-commands.sh
+++ ci-operator/step-registry/upi/install/azure/upi-install-azure-commands.sh
@@ -195,6 +195,7 @@ while [[ ${cmd_result} -eq 1 ]] || [[ "$status" == "pending" ]]
 do
   cmd_result=0
   status=$(az storage blob show --account-name $ACCOUNT_NAME --account-key $ACCOUNT_KEY --container-name vhd --name "rhcos.vhd" -o tsv --query properties.copy.status) || cmd_result=1
+  echo "status: ${status}"
 done
 if [[ "$status" != "success" ]]; then
   echo "Error copying VHD image ${VHD_URL}"
