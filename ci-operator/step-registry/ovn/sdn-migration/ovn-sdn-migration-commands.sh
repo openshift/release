c1095f5e2828ac5b3492e6e09577f0c2f2f5c1a3
weliang1 <weliang@redhat.com>
2024-04-17 17:36:57 -0400

    Testing Only

--- ci-operator/step-registry/ovn/sdn-migration/ovn-sdn-migration-commands.sh
+++ ci-operator/step-registry/ovn/sdn-migration/ovn-sdn-migration-commands.sh
@@ -5,123 +5,62 @@ set -o nounset
 set -o pipefail
 
 TARGET=${TARGET:-OVNKubernetes}
-# Check if the OVN_SDN_MIGRATION_TIMEOUT environment variable is set and is equal to "0s"
-if [ -n "$OVN_SDN_MIGRATION_TIMEOUT" ] && [ "$OVN_SDN_MIGRATION_TIMEOUT" = "0s" ]; then
-    unset OVN_SDN_MIGRATION_TIMEOUT
-fi
 
-co_timeout=${OVN_SDN_MIGRATION_TIMEOUT:-1200s}
-timeout "$co_timeout" bash <<EOT
+# Wait for ClusterOperators to reach the desired state
+timeout 2400s bash <<EOT
 until
-  oc wait co --all --for='condition=AVAILABLE=True' --timeout=10s && \
-  oc wait co --all --for='condition=PROGRESSING=False' --timeout=10s && \
-  oc wait co --all --for='condition=DEGRADED=False' --timeout=10s;
+  oc wait co --all --for='condition=Available=True' --timeout=10s &&
+  oc wait co --all --for='condition=Progressing=False' --timeout=10s &&
+  oc wait co --all --for='condition=Degraded=False' --timeout=10s;
 do
   sleep 10
-  echo "Some ClusterOperators Degraded=False,Progressing=True,or Available=False";
+  echo "Some ClusterOperators are not in the desired state (Degraded=False, Progressing=False, Available=True)";
 done
 EOT
 
-oc patch Network.operator.openshift.io cluster --type='merge'   --patch '{"spec":{"migration":null}}'
-cno_timeout=${OVN_SDN_MIGRATION_TIMEOUT:-120s}
-timeout "$cno_timeout" bash <<EOT
-until
-  ! oc get network -o yaml | grep migration > /dev/null
-do
-  echo "migration field is not cleaned by CNO"
-  sleep 3
-done
-EOT
+# Patch new setting for internalJoinSubnet and internalTransitSwitchSubnet
+oc patch network.operator.openshift.io cluster --type='merge'  -p='{"spec":{"defaultNetwork":{"ovnKubernetesConfig":{"ipv4":{"internalJoinSubnet": "100.65.0.0/16"}}}}}' 
+oc patch network.operator.openshift.io cluster --type='merge'  -p='{"spec":{"defaultNetwork":{"ovnKubernetesConfig":{"ipv4":{"internalTransitSwitchSubnet": "100.85.0.0/16"}}}}}' 
 
-# Change network to target network in Network.operator.openshift.io the CR to trigger machine config update by MCO.
-oc patch Network.operator.openshift.io cluster --type='merge' --patch "{\"spec\":{\"migration\":{\"networkType\":\"${TARGET}\"}}}"
-# Wait until MCO starts applying new machine config to nodes
-mco_timeout=${OVN_SDN_MIGRATION_TIMEOUT:-300s}
-oc wait mcp --all --for='condition=UPDATING=True' --timeout="$mco_timeout"
+# Patch the network configuration for live migration
+oc patch Network.config.openshift.io cluster --type='merge' --patch '{"metadata":{"annotations":{"network.openshift.io/network-type-migration":""}},"spec":{"networkType":"OVNKubernetes"}}'
 
-# Wait until MCO finishes its work or it reaches the 45min timeout
-mcp_timeout=${OVN_SDN_MIGRATION_TIMEOUT:-2700s}
-timeout "$mcp_timeout" bash <<EOT
-until
-  oc wait mcp --all --for='condition=UPDATED=True' --timeout=10s && \
-  oc wait mcp --all --for='condition=UPDATING=False' --timeout=10s && \
-  oc wait mcp --all --for='condition=DEGRADED=False' --timeout=10s;
+# Wait for the network migration to start
+timeout 300s bash <<EOT
+until 
+  oc get network -o yaml | grep NetworkTypeMigrationInProgress > /dev/null
 do
+  echo "Migration is not started yet"
   sleep 10
-  echo "Some MachineConfigPool DEGRADED=True,UPDATING=True,or UPDATED=False";
 done
 EOT
+echo "Start Live Migration process now"
 
-
-# Trigger ovn-kubernetes deployment
-oc patch Network.config.openshift.io cluster --type='merge' --patch "{\"spec\":{\"networkType\":\"${TARGET}\"}}"
-ovn_co_timeout=${OVN_SDN_MIGRATION_TIMEOUT:-60s}
-oc wait co network --for='condition=PROGRESSING=True' --timeout="$ovn_co_timeout"
-# Wait until the multus pods are restarted
-ovn_multus_timeout=${OVN_SDN_MIGRATION_TIMEOUT:-300s}
-timeout "$ovn_multus_timeout" oc rollout status ds/multus -n openshift-multus
-
-# Reboot all the nodes
-# Get the list of nodes in the cluster
-master_nodes=$(oc get nodes -o jsonpath='{.items[?(@.metadata.labels.node-role\.kubernetes\.io/master=="")].metadata.name}')
-worker_nodes=$(oc get nodes -o jsonpath='{.items[?(@.metadata.labels.node-role\.kubernetes\.io/worker=="")].metadata.name}')
-daemonset_name="machine-config-daemon"
-delay=1
-# reboot all the master nodes in sequence
-for node in $master_nodes; do
-    pods_on_node=$(oc get pods -n openshift-machine-config-operator -o jsonpath='{.items[?(@.spec.nodeName=="'$node'")].metadata.name}' -l k8s-app=$daemonset_name)
-    if [[ -n $pods_on_node ]]; then
-        echo "reboot master node $node in ${delay}m"
-        until oc rsh -n openshift-machine-config-operator "$pods_on_node" chroot /rootfs shutdown -r +$delay; do echo "cannot reboot node $node, retry"&&sleep 3; done
-        delay=$((delay+3))
-    fi
-done
-# reboot all the worker nodes
-for node in $worker_nodes; do
-    pods_on_node=$(oc get pods -n openshift-machine-config-operator -o jsonpath='{.items[?(@.spec.nodeName=="'$node'")].metadata.name}' -l k8s-app=$daemonset_name)
-    if [[ -n $pods_on_node ]]; then
-        echo "reboot worker node $node in ${delay}m"
-        until oc rsh -n openshift-machine-config-operator "$pods_on_node" chroot /rootfs shutdown -r +$delay; do echo "cannot reboot node $node, retry"&&sleep 3; done
-    fi
-done
-
-# Wait until all nodes reboot and the api-server is unreachable.
-sleep ${delay}m
-
-# Wait for nodes come back
-ovn_node_timeout=${OVN_SDN_MIGRATION_TIMEOUT:-1800s}
-timeout "$ovn_node_timeout" bash <<EOT
-until
-  oc wait node --all --for condition=ready --timeout=10s;
+# Wait for the live migration to fully complete
+timeout 3600s bash <<EOT
+until 
+  oc get network -o yaml | grep NetworkTypeMigrationCompleted > /dev/null && \
+  for NODE in \$(oc get nodes -o custom-columns=NAME:.metadata.name --no-headers); do oc get node \$NODE -o yaml | grep "k8s.ovn.org/node-transit-switch-port-ifaddr:" | grep "100.85";  done > /dev/null && \
+  for NODE in \$(oc get nodes -o custom-columns=NAME:.metadata.name --no-headers); do oc get node \$NODE -o yaml | grep "k8s.ovn.org/node-gateway-router-lrp-ifaddr:" | grep "100.65";  done > /dev/null && \
+  oc get network.config/cluster -o jsonpath='{.status.networkType}' | grep OVNKubernetes > /dev/null;
 do
-  sleep 10
-  echo "nodes not ready"
+  echo "Live migration is still in progress"
+  sleep 30
 done
 EOT
+echo "The Migration is completed"
 
-
-# Check all cluster operators back to normal. requires the main check on clusteroperator
-# status to succeed 3 times in a row with 30s pause in between checks
-all_co_timeout=${OVN_SDN_MIGRATION_TIMEOUT:-2700s}
-
-timeout "$all_co_timeout" bash <<EOT
-success_count=0
-until [ \$success_count -eq 3 ]; do
-  if oc wait co --all --for='condition=Available=True' --timeout=10s &&
-     oc wait co --all --for='condition=Progressing=False' --timeout=10s &&
-     oc wait co --all --for='condition=Degraded=False' --timeout=10s; then
-    echo "Check succeeded (\$success_count/3)"
-    ((success_count++))
-    if [ \$success_count -lt 3 ]; then
-      echo "Pausing for 30 seconds before the next check..."
-      sleep 30
-    fi
-  else
-    echo "Some ClusterOperators Degraded=False, Progressing=True, or Available=False"
-    success_count=0
-    sleep 10
-  fi
+# Check all ClusterOperators back to normal after migration
+timeout 3000s bash <<EOT
+until
+  oc wait co --all --for='condition=Available=True' --timeout=10s && \
+  oc wait co --all --for='condition=Progressing=False' --timeout=10s && \
+  oc wait co --all --for='condition=Degraded=False' --timeout=10s; 
+do
+  sleep 10 && echo "Some ClusterOperators are not in the desired state (Degraded=False, Progressing=False, Available=True)";
 done
-echo "All checks passed successfully 3 times in a row."
 EOT
+echo "All ClusterOperators are in the desired state"
+
+# Output the status of ClusterOperators
 oc get co
