3454e6452c8d2e68f4f1c7755c410f90533947d2
Vadim Rutkovsky <vrutkovs@redhat.com>
2024-04-19 13:26:48 +0200

    approve-csrs-with-timeout: iterate quicker
    This function is restarted until node is healthy

--- ci-operator/step-registry/openshift/e2e/cert-rotation/prepare-for-shutdown/openshift-e2e-cert-rotation-prepare-for-shutdown-commands.sh
+++ ci-operator/step-registry/openshift/e2e/cert-rotation/prepare-for-shutdown/openshift-e2e-cert-rotation-prepare-for-shutdown-commands.sh
@@ -53,7 +53,7 @@ cat << 'EOZ' > /tmp/approve-csrs-with-timeout.sh
   export KUBECONFIG=/etc/kubernetes/static-pod-resources/kube-apiserver-certs/secrets/node-kubeconfigs/localhost-recovery.kubeconfig
   echo -n "Approving CSRs"
   attempts=0
-  max_attempts=10
+  max_attempts=5
   while true; do
     echo -n '.'
     mapfile -d ' ' -t csrs < <(oc get csr --field-selector=spec.signerName='kubernetes.io/kube-apiserver-client-kubelet' --no-headers | grep Pending | cut -f1 -d" ")
@@ -68,7 +68,7 @@ cat << 'EOZ' > /tmp/approve-csrs-with-timeout.sh
       echo "Timeout waiting for new CSRs"
       break
     fi
-    sleep 15s
+    sleep 5s
   done
   echo "Done"
 EOZ
