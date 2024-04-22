e307f9d5deeac8af074b0e0662fc32ffcddc0f4f
Sean Zhao <szhao@redhat.com>
2024-04-22 09:31:46 +0800

    update shell

--- ci-operator/step-registry/quay-tests/quay-conformance-test/quay-tests-quay-conformance-test-commands.sh
+++ ci-operator/step-registry/quay-tests/quay-conformance-test/quay-tests-quay-conformance-test-commands.sh
@@ -10,22 +10,22 @@ cd /tmp && git clone https://github.com/opencontainers/distribution-spec.git && 
 go test -c  && ls || true
 go test -c -mod=mod  && ls || true
 
-QUAY_URL=$(cat "$SHARED_DIR"/quayroute)
-QUAY_ACCESS_TOKEN=$(cat "$SHARED_DIR"/quay_oauth2_token)
-QUAY_OAUTH2_TOEKN="Bearer $QUAY_ACCESS_TOKEN"
+# QUAY_URL=$(cat "$SHARED_DIR"/quayroute)
+# QUAY_ACCESS_TOKEN=$(cat "$SHARED_DIR"/quay_oauth2_token)
+# QUAY_OAUTH2_TOEKN="Bearer $QUAY_ACCESS_TOKEN"
 
 # Registry details
 OCI_ROOT_URL=$(cat "$SHARED_DIR"/quayroute)
 OCI_USERNAME=$(cat /var/run/quay-qe-quay-secret/username)
 OCI_PASSWORD=$(cat /var/run/quay-qe-quay-secret/password)
-
+OCI_NAMESPACE="myorg/myrepo"
+OCI_CROSSMOUNT_NAMESPACE="myorg/other"
 
 export OCI_ROOT_URL
 export OCI_USERNAME
 export OCI_PASSWORD
-export OCI_NAMESPACE="myorg/myrepo"
-export OCI_CROSSMOUNT_NAMESPACE="myorg/other"
-
+export OCI_NAMESPACE
+export OCI_CROSSMOUNT_NAMESPACE
 # Which workflows to run
 export OCI_TEST_PULL=1
 export OCI_TEST_PUSH=1
@@ -37,11 +37,12 @@ export OCI_HIDE_SKIPPED_WORKFLOWS=0
 export OCI_DEBUG=0
 export OCI_DELETE_MANIFEST_BEFORE_BLOBS=0 # defaults to OCI_DELETE_MANIFEST_BEFORE_BLOBS=1 if not set
 
-echo "parameterssss...."
-echo $OCI_ROOT_URL $OCI_USERNAME $OCI_PASSWORD $QUAY_URL $QUAY_ACCESS_TOKEN $QUAY_OAUTH2_TOEKN
+echo "begin conformance test...."
+echo $OCI_ROOT_URL 
 ./conformance.test
 
-go test -c   && ls  || true
+echo "test finished ...."
+ls  || true
 #Set Kubeconfig:
 # cd quay-frontend-tests
 # skopeo -v
