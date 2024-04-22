6c31bdce74f4a065e49395f5ca38e9f020d83758
Sean Zhao <szhao@redhat.com>
2024-04-22 14:30:19 +0800

    remove comments

--- ci-operator/step-registry/quay-tests/quay-conformance-test/quay-tests-quay-conformance-test-commands.sh
+++ ci-operator/step-registry/quay-tests/quay-conformance-test/quay-tests-quay-conformance-test-commands.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
 set -euo pipefail
-git version
 go version
 
 echo "Clone opencontainers distribution-spec Repository..."
 cd /tmp && git clone https://github.com/opencontainers/distribution-spec.git && cd distribution-spec/conformance || true
 
-go test -c  && ls || true
+# go test -c  && ls || true
 go test -c -mod=mod  && ls || true
-
-# QUAY_URL=$(cat "$SHARED_DIR"/quayroute)
-# QUAY_ACCESS_TOKEN=$(cat "$SHARED_DIR"/quay_oauth2_token)
-# QUAY_OAUTH2_TOEKN="Bearer $QUAY_ACCESS_TOKEN"
 
 # Registry details
 OCI_ROOT_URL=$(cat "$SHARED_DIR"/quayroute)
 OCI_USERNAME=$(cat /var/run/quay-qe-quay-secret/username)
 OCI_PASSWORD=$(cat /var/run/quay-qe-quay-secret/password)
-OCI_NAMESPACE="myorg/myrepo"
-OCI_CROSSMOUNT_NAMESPACE="myorg/other"
+OCI_NAMESPACE="myconformanceorg/myrepo"
+OCI_CROSSMOUNT_NAMESPACE="myconformanceorg/other"
 
 export OCI_ROOT_URL
 export OCI_USERNAME
 export OCI_PASSWORD
 export OCI_NAMESPACE
 export OCI_CROSSMOUNT_NAMESPACE
+
 # Which workflows to run
 export OCI_TEST_PULL=1
 export OCI_TEST_PUSH=1
@@ -37,58 +33,8 @@ export OCI_HIDE_SKIPPED_WORKFLOWS=0
 export OCI_DEBUG=0
 export OCI_DELETE_MANIFEST_BEFORE_BLOBS=0 # defaults to OCI_DELETE_MANIFEST_BEFORE_BLOBS=1 if not set
 
-echo "begin conformance test...."
-echo $OCI_ROOT_URL 
+echo "Begin conformance test"
 ./conformance.test || true
 
-echo "Copy test result to ARTIFACT_DIR ...."
+echo "Copy test result to ARTIFACT_DIR"
 mv report.html junit.xml $ARTIFACT_DIR || true
-
-echo "test finished ...."
-ls  || true
-#Set Kubeconfig:
-# cd quay-frontend-tests
-# skopeo -v
-# oc version
-# terraform version
-# (cp -L $KUBECONFIG /tmp/kubeconfig || true) && export KUBECONFIG_PATH=/tmp/kubeconfig
-
-# #Create Artifact Directory:
-# ARTIFACT_DIR=${ARTIFACT_DIR:=/tmp/artifacts}
-# mkdir -p $ARTIFACT_DIR
-
-
-# function copyArtifacts {
-#     JUNIT_PREFIX="junit_"
-#     cp -r ./cypress/results/* $ARTIFACT_DIR
-#     for file in "$ARTIFACT_DIR"/*; do
-#         if [[ ! "$(basename "$file")" =~ ^"$JUNIT_PREFIX" ]]; then
-#             mv "$file" "$ARTIFACT_DIR"/"$JUNIT_PREFIX""$(basename "$file")"
-#         fi
-#     done
-#     cp -r ./cypress/videos/* $ARTIFACT_DIR
-# }
-
-# # Install Dependcies defined in packages.json
-# yarn install || true
-
-# #Finally Copy the Junit Testing XML files and Screenshots to /tmp/artifacts
-# trap copyArtifacts EXIT
-
-# # Cypress Doc https://docs.cypress.io/guides/references/proxy-configuration
-# if [ "${QUAY_PROXY}" = "true" ]; then
-#     HTTPS_PROXY=$(cat $SHARED_DIR/proxy_public_url)
-#     export HTTPS_PROXY
-#     HTTP_PROXY=$(cat $SHARED_DIR/proxy_public_url)
-#     export HTTP_PROXY
-# fi
-
-# #Trigget Quay E2E Testing
-# set +x
-# quay_route=$(oc get quayregistry quay -n quay-enterprise -o jsonpath='{.status.registryEndpoint}') || true
-# echo "The Quay hostname is $quay_route"
-# quay_hostname=${quay_route#*//}
-# echo "The Quay hostname is $quay_hostname"
-# export CYPRESS_QUAY_ENDPOINT=$quay_hostname
-# yarn run smoke || true
-
