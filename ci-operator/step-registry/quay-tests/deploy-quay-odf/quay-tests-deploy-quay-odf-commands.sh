05412f7d883c5ca4a539da385426471dfa2347d3
Sean Zhao <szhao@redhat.com>
2024-04-23 16:17:42 +0800

    support iib

--- ci-operator/step-registry/quay-tests/deploy-quay-odf/quay-tests-deploy-quay-odf-commands.sh
+++ ci-operator/step-registry/quay-tests/deploy-quay-odf/quay-tests-deploy-quay-odf-commands.sh
@@ -15,6 +15,7 @@ QUAY_OPERATOR_CHANNEL="$QUAY_OPERATOR_CHANNEL"
 QUAY_OPERATOR_SOURCE="$QUAY_OPERATOR_SOURCE"
 ODF_OPERATOR_CHANNEL="$ODF_OPERATOR_CHANNEL"
 ODF_SUBSCRIPTION_NAME="$ODF_SUBSCRIPTION_NAME"
+QUAY_INDEX_IMAGE_BUILD="$QUAY_INDEX_IMAGE_BUILD"
 
 cat <<EOF | oc apply -f -
 apiVersion: v1
@@ -73,6 +74,63 @@ for _ in {1..60}; do
   sleep 10
 done
 echo "ODF/OCS Operator is deployed successfully"
+
+#Prepare resource for Quay operator 
+if [[ -z "$QUAY_INDEX_IMAGE_BUILD" ]]; then
+  echo "Installing Quay from released build"
+else
+  echo "Installing Quay from unreleased iib: $QUAY_INDEX_IMAGE_BUILD" 
+  QUAY_OPERATOR_SOURCE="brew-operator-catalog"
+  
+  #create image content source policy
+  cat <<EOF | oc apply -f -
+apiVersion: operator.openshift.io/v1alpha1
+kind: ImageContentSourcePolicy
+metadata:
+  name: brew-registry
+spec:
+  repositoryDigestMirrors:
+  - mirrors:
+    - brew.registry.redhat.io/rh-osbs/quay-quay-operator-rhel8
+    source: registry.redhat.io/quay/quay-operator-rhel8
+  - mirrors:
+    - brew.registry.redhat.io/rh-osbs/quay-quay-rhel8
+    source: registry.redhat.io/quay/quay-rhel8
+  - mirrors:
+    - brew.registry.redhat.io/rh-osbs/quay-quay-bridge-operator-rhel8
+    source: registry.redhat.io/quay/quay-bridge-operator-rhel8
+  - mirrors:
+    - brew.registry.redhat.io/rh-osbs/quay-quay-bridge-operator-rhel8
+    source: registry.redhat.io/quay/quay-bridge-rhel9-operator
+  - mirrors:
+    - brew.registry.redhat.io/rh-osbs/quay-quay-container-security-operator-rhel8
+    source: registry.redhat.io/quay/quay-container-security-operator-rhel8
+  - mirrors:
+    - brew.registry.redhat.io/rh-osbs/quay-clair-rhel8
+    source: registry.redhat.io/quay/clair-rhel8
+  - mirrors:
+    - brew.registry.redhat.io
+    source: registry.stage.redhat.io
+  - mirrors:
+    - brew.registry.redhat.io
+    source: registry-proxy.engineering.redhat.com
+EOF
+  
+  #Create custom catalog source
+  cat <<EOF | oc apply -f -
+apiVersion: operators.coreos.com/v1alpha1
+kind: CatalogSource
+metadata:
+  name: $QUAY_OPERATOR_SOURCE
+  namespace: openshift-marketplace
+spec:
+  sourceType: grpc
+  image: $QUAY_INDEX_IMAGE_BUILD
+  displayName: Brew Testing Operator Catalog
+  publisher: grpc
+EOF
+
+fi
 
 #Deploy Quay Operator to OCP namespace 'quay-enterprise'
 cat <<EOF | oc apply -f -
