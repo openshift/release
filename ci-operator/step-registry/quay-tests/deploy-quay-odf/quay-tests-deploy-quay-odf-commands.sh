0b3aa73d04f3d2bca481b9f9f088d9f2a5d3ea2c
Sean Zhao <szhao@redhat.com>
2024-04-24 10:15:50 +0800

    update comments

--- ci-operator/step-registry/quay-tests/deploy-quay-odf/quay-tests-deploy-quay-odf-commands.sh
+++ ci-operator/step-registry/quay-tests/deploy-quay-odf/quay-tests-deploy-quay-odf-commands.sh
@@ -75,10 +75,10 @@ for _ in {1..60}; do
 done
 echo "ODF/OCS Operator is deployed successfully"
 
-#By default, install Quay with released build
+#By default, install Quay Operator with released build
 if [[ -z "$QUAY_INDEX_IMAGE_BUILD" ]]; then
   echo "Installing Quay from released build"
-else  #Prepare icsp and catalogsource for Quay operator 
+else  #Install Quay operator with IIB
   echo "Installing Quay from unreleased iib: $QUAY_INDEX_IMAGE_BUILD" 
   QUAY_OPERATOR_SOURCE="brew-operator-catalog"
   
