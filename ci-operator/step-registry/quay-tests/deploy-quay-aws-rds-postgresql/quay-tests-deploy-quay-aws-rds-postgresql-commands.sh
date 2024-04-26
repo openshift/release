01c7c122d6b07094fc21fa873043e9e68f7d3cd5
Li Zhang <lzha@redhat.com>
2024-04-26 22:03:56 +0800

    updat the terraform code

--- ci-operator/step-registry/quay-tests/deploy-quay-aws-rds-postgresql/quay-tests-deploy-quay-aws-rds-postgresql-commands.sh
+++ ci-operator/step-registry/quay-tests/deploy-quay-aws-rds-postgresql/quay-tests-deploy-quay-aws-rds-postgresql-commands.sh
@@ -104,7 +104,7 @@ resource "aws_subnet" "quayrds1" {
 }
 
 resource "aws_subnet" "quayrds2" {
-  vpc_id            = "${aws_vpc.quayrds.id}"
+  vpc_id            = aws_vpc.quayrds.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-west-2c"
 }
