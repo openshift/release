bb797db44f9c53892030ed4ad870ee8eb2af2d86
Sean Zhao <szhao@redhat.com>
2024-04-28 18:14:15 +0800

    OCPQE-13468: Add Quay OCI Conformance Testing into Prow (#51016)
    * provisioning rosa env for quay
    
    * fix conflict
    
    * initinalize Quay OCI Conformance testing
    
    * initinalize Quay OCI Conformance testing
    
    * update periodics.yaml
    
    * update file
    
    * add clone conformance repo
    
    * add go version
    
    * include quay route
    
    * include quay route
    
    * include quay auth
    
    * update image
    
    * update secret
    
    * update shell
    
    * copy test report
    
    * remove comments
    
    * update jobs
    
    * update result status
    
    * remove -e in bash
    
    * continue when conformance.test fail
    
    * continue when conformance.test fail
    
    * continue when conformance.test fail
    
    * export OCI_REPORT_DIR
    
    * support iib
    
    * update ci-operator-config
    
    * update comments
    
    * update comments
    
    * change to quay-oci-conformance
    
    * change to quay-oci-conformance
    
    * change to quay-oci-conformance
    
    * make jobs
    
    * make ci-operator-config
    
    * update comments
    
    * roll back odf change
    
    * test released build
    
    * test with release build
    
    * rollback to iib
    
    * update shell return code

---
+++ ci-operator/step-registry/quay-tests/quay-oci-conformance-test/quay-tests-quay-oci-conformance-test-commands.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -euo pipefail
+go version
+
+echo "Clone opencontainers distribution-spec Repository..."
+cd /tmp && git clone https://github.com/opencontainers/distribution-spec.git && cd distribution-spec/conformance || true
+
+# go test -c  && ls || true
+go test -c -mod=mod  && ls || true
+
+# Registry details
+OCI_ROOT_URL=$(cat "$SHARED_DIR"/quayroute)
+OCI_USERNAME=$(cat /var/run/quay-qe-quay-secret/username)
+OCI_PASSWORD=$(cat /var/run/quay-qe-quay-secret/password)
+OCI_NAMESPACE="myconformanceorg/myrepo"
+OCI_CROSSMOUNT_NAMESPACE="myconformanceorg/other"
+OCI_REPORT_DIR=$ARTIFACT_DIR
+
+export OCI_ROOT_URL
+export OCI_USERNAME
+export OCI_PASSWORD
+export OCI_NAMESPACE
+export OCI_CROSSMOUNT_NAMESPACE
+
+# Which workflows to run
+export OCI_TEST_PULL=1
+export OCI_TEST_PUSH=1
+export OCI_TEST_CONTENT_DISCOVERY=1
+export OCI_TEST_CONTENT_MANAGEMENT=1
+
+# Extra settings
+export OCI_HIDE_SKIPPED_WORKFLOWS=0
+export OCI_DEBUG=0
+export OCI_DELETE_MANIFEST_BEFORE_BLOBS=0 # defaults to OCI_DELETE_MANIFEST_BEFORE_BLOBS=1 if not set
+
+#Generate test result to ARTIFACT_DIR
+export OCI_REPORT_DIR
+
+echo "Begin conformance test"
+./conformance.test || true
