d332ef5715f587eb8101a955b1f0afb75797a626
Liangquan Li <liangli@redhat.com>
2024-04-29 21:10:34 +0800

    add fix port

--- ci-operator/step-registry/hypershift/mce/agent/create/hostedcluster/hypershift-mce-agent-create-hostedcluster-commands.sh
+++ ci-operator/step-registry/hypershift/mce/agent/create/hostedcluster/hypershift-mce-agent-create-hostedcluster-commands.sh
@@ -42,7 +42,7 @@ fi
   --agent-namespace="${CLUSTER_NAMESPACE}" \
   --namespace local-cluster \
   --base-domain=${BASEDOMAIN} \
-  --api-server-address=api.${CLUSTER_NAME}.${BASEDOMAIN} \
+  --api-server-address=\"api.${CLUSTER_NAME}.${BASEDOMAIN}:32443\" \
   --image-content-sources "${SHARED_DIR}/mgmt_icsp.yaml" \
   --ssh-key="${SHARED_DIR}/id_rsa.pub" \
   --release-image ${RELEASE_IMAGE}
