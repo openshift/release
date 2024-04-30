a0a087dbf75919963418544747a8b4101b623d04
Paige Rubendall <prubenda@redhat.com>
2024-04-30 15:41:06 -0400

    adding test wait step
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED

--- ci-operator/step-registry/redhat-chaos/start-krkn/wait/redhat-chaos-start-krkn-wait-commands.sh
+++ ci-operator/step-registry/redhat-chaos/start-krkn/wait/redhat-chaos-start-krkn-wait-commands.sh
@@ -6,13 +6,13 @@ set -o pipefail
 set -o xtrace
 set -x
 
-sleep 30
-
 echo $SHARED_DIR
 
 env | grep SHARED_DIR
-
-echo "START" >> $SHARED_DIR/krkn_start.txt
-
 cat $SHARED_DIR/krkn_start.txt
 
+sleep 1000
+
+
+
+sleep 1000
