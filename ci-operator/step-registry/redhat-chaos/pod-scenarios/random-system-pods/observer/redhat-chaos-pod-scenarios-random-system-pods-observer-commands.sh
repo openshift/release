4d325d53d5c7f3ddd0e3a0d94a714d3ad36327da
Paige Rubendall <prubenda@redhat.com>
2024-05-01 12:27:38 -0400

    adding new project in observer to wait on"
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED

--- ci-operator/step-registry/redhat-chaos/pod-scenarios/random-system-pods/observer/redhat-chaos-pod-scenarios-random-system-pods-observer-commands.sh
+++ ci-operator/step-registry/redhat-chaos/pod-scenarios/random-system-pods/observer/redhat-chaos-pod-scenarios-random-system-pods-observer-commands.sh
@@ -7,13 +7,13 @@ set -o xtrace
 set -x
 ls
 
-krkn_start_file=${SHARED_DIR}/krkn_start.txt
-
-while [ ! -f $krkn_start_file ] ; do
-  printf "%s: waiting for %s\n" "$(date --utc --iso=s)" "${SHARED_DIR}"
-  sleep 3
+while [ ! -f "${KUBECONFIG}" ]; do
+  printf "%s: waiting for %s\n" "$(date --utc --iso=s)" "${KUBECONFIG}"
+  sleep 30
 done
-printf "%s: acquired %s\n" "$(date --utc --iso=s)" "${krkn_start_file}"
+printf "%s: acquired %s\n" "$(date --utc --iso=s)" "${KUBECONFIG}"
+
+echo "kubeconfig loc $KUBECONFIG"
 
 echo "kubeconfig loc $$KUBECONFIG"
 echo "Using the flattened version of kubeconfig"
@@ -23,8 +23,15 @@ export KUBECONFIG=/tmp/config
 export KRKN_KUBE_CONFIG=$KUBECONFIG
 export NAMESPACE=$TARGET_NAMESPACE 
 
+while [ $(oc get ns | grep -c "start-kraken") -lt 1 ]; do
+  echo "start kraken not found yet, waiting"
+  sleep 10
+done
+
+echo "starting pod scenarios"
 ./pod-scenarios/prow_run.sh
 rc=$?
 echo "Done running the test!" 
 echo "Return code: $rc"
 exit $rc
+
--- ci-operator/step-registry/redhat-chaos/start-krkn/redhat-chaos-start-krkn-commands.sh
+++ ci-operator/step-registry/redhat-chaos/start-krkn/redhat-chaos-start-krkn-commands.sh
@@ -6,13 +6,4 @@ set -o pipefail
 set -o xtrace
 set -x
 
-sleep 30
-
-echo $SHARED_DIR
-
-env | grep SHARED_DIR
-
-echo "START" >> $SHARED_DIR/krkn_start.txt
-
-cat $SHARED_DIR/krkn_start.txt
-
+oc new-project start-kraken
