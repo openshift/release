293d78cffb2e6c232f540224141d1ab626701060
Diane Frazzette <dfrazzet@redhat.com>
2024-05-02 16:32:10 -0400

    [INTEROP-7365] Update ACM config for 2.10. (#51270)
    * [INTEROP-7365] Update ACM config for 2.10.
    
    * [INTEROP-7365] Update mce-must-gather image to rhel9

--- ci-operator/step-registry/mce/must-gather/mce-must-gather-commands.sh
+++ ci-operator/step-registry/mce/must-gather/mce-must-gather-commands.sh
@@ -6,4 +6,4 @@ set -o pipefail
 export KUBECONFIG=${SHARED_DIR}/kubeconfig
 
 # MCE must-gather
-oc adm must-gather --image=registry.redhat.io/multicluster-engine/must-gather-rhel8:v"$(oc get csv  -n multicluster-engine -l operators.coreos.com/multicluster-engine.multicluster-engine= -o=jsonpath='{.items[].spec.version}')" --dest-dir="${ARTIFACT_DIR}"
+oc adm must-gather --image=registry.redhat.io/multicluster-engine/must-gather-rhel9:v"$(oc get csv  -n multicluster-engine -l operators.coreos.com/multicluster-engine.multicluster-engine= -o=jsonpath='{.items[].spec.version}')" --dest-dir="${ARTIFACT_DIR}"
