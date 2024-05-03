475e83982d50e3680a361e221082ed0f41e37b30
Jakob Möller <jmoller@redhat.com>
2024-05-03 17:04:12 +0200

    test: add topolvm conformance suite

--- ci-operator/step-registry/lvms/topolvm/metal/config/lvms-topolvm-metal-config-commands.sh
+++ ci-operator/step-registry/lvms/topolvm/metal/config/lvms-topolvm-metal-config-commands.sh
@@ -43,7 +43,7 @@ sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  "deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
