8058a6cc599894c4e82e35354e584d8f982923ac
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2024-05-08 16:31:54 +0200

    USHIFT-3100: Add skip list to conformance origin

--- ci-operator/step-registry/openshift/microshift/e2e/origin-conformance/openshift-microshift-e2e-origin-conformance-commands.sh
+++ ci-operator/step-registry/openshift/microshift/e2e/origin-conformance/openshift-microshift-e2e-origin-conformance-commands.sh
@@ -4,4 +4,21 @@ set -xeuo pipefail
 
 trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
 
-KUBECONFIG="${SHARED_DIR}/kubeconfig" openshift-tests run openshift/conformance -v 2 --provider=none -o "${ARTIFACT_DIR}/e2e.log" --junit-dir "${ARTIFACT_DIR}/junit"
+IP_ADDRESS="$(cat "${SHARED_DIR}"/public_address)"
+MICROSHIFT_URL="https://${IP_ADDRESS}:6443"
+CONFORMANCE_SKIP="/tmp/skip.txt"
+touch "${CONFORMANCE_SKIP}"
+CONFORMANCE_TEST_LIST="/tmp/tests.txt"
+touch "${CONFORMANCE_TEST_LIST}"
+export KUBECONFIG="${SHARED_DIR}/kubeconfig"
+
+
+if [ -f "${SHARED_DIR}/conformance-skip.txt" ]; then
+    cp ${SHARED_DIR}/conformance-skip.txt "${CONFORMANCE_SKIP}"
+fi
+while read -r test; do
+    grep -F "$test" "${CONFORMANCE_SKIP}" > /dev/null || echo "$test" >> "${CONFORMANCE_TEST_LIST}"
+done < <(openshift-tests run openshift/conformance --dry-run --provider none 2>/dev/null | egrep '^"\[')
+cp "${CONFORMANCE_TEST_LIST}" "${ARTIFACT_DIR}/tests.txt"
+
+openshift-tests run openshift/conformance -f "${CONFORMANCE_TEST_LIST}" -v 2 --provider=none -o "${ARTIFACT_DIR}/e2e.log" --junit-dir "${ARTIFACT_DIR}/junit"
