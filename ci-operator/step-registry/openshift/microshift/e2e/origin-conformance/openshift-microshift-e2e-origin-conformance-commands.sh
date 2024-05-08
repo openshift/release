8ef2ea73e1729921d2c985f8bfe45a8c334beee2
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2024-05-08 16:32:22 +0200

    USHIFT-3100: add configshim manifests to conformance origin

--- ci-operator/step-registry/openshift/microshift/e2e/origin-conformance/openshift-microshift-e2e-origin-conformance-commands.sh
+++ ci-operator/step-registry/openshift/microshift/e2e/origin-conformance/openshift-microshift-e2e-origin-conformance-commands.sh
@@ -10,6 +10,8 @@ CONFORMANCE_SKIP="/tmp/skip.txt"
 touch "${CONFORMANCE_SKIP}"
 CONFORMANCE_TEST_LIST="/tmp/tests.txt"
 touch "${CONFORMANCE_TEST_LIST}"
+export STATIC_CONFIG_MANIFEST_DIR=/tmp/manifests
+mkdir "${STATIC_CONFIG_MANIFEST_DIR}"
 export KUBECONFIG="${SHARED_DIR}/kubeconfig"
 
 
@@ -20,5 +22,32 @@ while read -r test; do
     grep -F "$test" "${CONFORMANCE_SKIP}" > /dev/null || echo "$test" >> "${CONFORMANCE_TEST_LIST}"
 done < <(openshift-tests run openshift/conformance --dry-run --provider none 2>/dev/null | egrep '^"\[')
 cp "${CONFORMANCE_TEST_LIST}" "${ARTIFACT_DIR}/tests.txt"
+
+cat > "${STATIC_CONFIG_MANIFEST_DIR}/infrastructure.yaml" <<EOF
+apiVersion: "config.openshift.io/v1"
+kind: Infrastructure
+metadata:
+  name: cluster
+spec:
+  platformSpec:
+    type: None
+status:
+  apiServerURL: ${MICROSHIFT_URL}
+  controlPlaneTopology: SingleReplica
+  infrastructureTopology: SingleReplica
+  platform: None
+  platformStatus:
+    type: None
+EOF
+cat > "${STATIC_CONFIG_MANIFEST_DIR}/network.yaml" <<EOF
+apiVersion: config.openshift.io/v1
+kind: Network
+metadata:
+  name: cluster
+spec:
+  networkType: OVNKubernetes
+status:
+  networkType: OVNKubernetes
+EOF
 
 openshift-tests run openshift/conformance -f "${CONFORMANCE_TEST_LIST}" -v 2 --provider=none -o "${ARTIFACT_DIR}/e2e.log" --junit-dir "${ARTIFACT_DIR}/junit"
