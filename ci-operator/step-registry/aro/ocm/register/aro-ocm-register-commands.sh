0ec2969f7733aed75382265b9cbf00fe107ca8c0
Mike Gahagan <mgahagan@redhat.com>
2024-05-09 09:04:57 -0400

    ref for registering ARO classic cluster to OCM

---
+++ ci-operator/step-registry/aro/ocm/register/aro-ocm-register-commands.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+export KUBECONFIG=${SHARED_DIR}/kubeconfig
+PULL_SECRET_FILE=${PULL_SECRET_FILE:="${CLUSTER_PROFILE_DIR}/pull-secret"}
+CLOUD_PULL_SECRET=$(jq '{ "cloud.openshift.com": .auths["cloud.openshift.com"] }' ${PULL_SECRET_FILE})
+UPDATED_PULL_SECRET_FILE="${SHARED_DIR}/pull-secret-new"
+CLUSTER_PULL_SECRET="${SHARED_DIR}/cluster-pull-secret"
+oc get secrets pull-secret -n openshift-config -o template='{{index .data ".dockerconfigjson"}}' | base64 -d > ${CLUSTER_PULL_SECRET}
+jq ".auths += ${CLOUD_PULL_SECRET}" < ${CLUSTER_PULL_SECRET} > ${UPDATED_PULL_SECRET_FILE}
+oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=${UPDATED_PULL_SECRET_FILE}
