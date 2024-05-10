f9c9cce0517e4cbd6e36da0431091922a40e064f
Logan Blyth <lblyth@redhat.com>
2024-05-10 14:54:50 -0400

    remove call to docker as CI most likely hits pull limit
    Signed-off-by: Logan Blyth <lblyth@redhat.com>

--- ci-operator/step-registry/calico/conf/calico-conf-commands.sh
+++ ci-operator/step-registry/calico/conf/calico-conf-commands.sh
@@ -20,6 +20,7 @@ tar -C /tmp -xf /tmp/calico-ocp.tgz
 
 # the tar file from tigera is called ocp when uncompressed
 pushd /tmp/ocp
+sed -i 's/docker/quay/' 02-tigera-operator.yaml
 for manifest in *.yaml ; do
   cp "${manifest}" "${SHARED_DIR}/manifest_${manifest}"
 done
