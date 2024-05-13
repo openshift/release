3460f6e1f4c93fd26207d17c0c165c5b35af1cfe
Shashank Reddy Boyapally <sboyapal@redhat.com>
2024-05-13 12:56:15 -0400

    horreum webhook integration
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>

--- ci-operator/step-registry/openshift-qe/node-density/openshift-qe-node-density-commands.sh
+++ ci-operator/step-registry/openshift-qe/node-density/openshift-qe-node-density-commands.sh
@@ -25,7 +25,7 @@ export WORKLOAD=node-density
 ES_SERVER="" EXTRA_FLAGS="--pods-per-node=50 --pod-ready-threshold=60s" ./run.sh
 
 # The measurable run
-export EXTRA_FLAGS="--gc-metrics=true --pods-per-node=$PODS_PER_NODE --pod-ready-threshold=$POD_READY_THRESHOLD --profile-type=${PROFILE_TYPE}"
+export EXTRA_FLAGS="--gc-metrics=true --pods-per-node=$PODS_PER_NODE --pod-ready-threshold=$POD_READY_THRESHOLD --profile-type=${PROFILE_TYPE} --local-indexing"
 
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
 
@@ -35,3 +35,34 @@ rm -f ${SHARED_DIR}/index.json
 
 folder_name=$(ls -t -d /tmp/*/ | head -1)
 jq ".iterations = $PODS_PER_NODE" $folder_name/index_data.json >> ${SHARED_DIR}/index_data.json
+
+metrics_folder_name=$(find . -maxdepth 1 -type d -name 'collected-metric*' | head -n 1)
+cp -r "${metrics_folder_name}" "${ARTIFACTS_DIR}/"
+
+WEBHOOK_USER=$(cat "/horreum-secret/horreum-webhook-user")
+
+export prow_base_url="https://prow.ci.openshift.org/view/gs/origin-ci-test/logs"
+export prow_pr_base_url="https://prow.ci.openshift.org/view/gs/test-platform-results/pr-logs/pull/openshift_release"
+export artifacts_base_url="https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/test-platform-results/logs"
+export artifacts_pr_base_url="https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/test-platform-results/pr-logs/pull/openshift_release"
+job_id=$JOB_NAME
+task_id=$BUILD_ID
+
+if [[ "${JOB_TYPE}" == "presubmit" ]]; then
+    build_url="${prow_pr_base_url}/${PULL_NUMBER}/${job_id}/${task_id}"
+    artifacts_url="${artifacts_pr_base_url}/${PULL_NUMBER}/${job_id}/"
+else
+    build_url="${prow_base_url}/${job_id}/${task_id}"
+    artifacts_url="${artifacts_base_url}/${job_id}/${task_id}"
+fi
+
+
+
+WEBHOOK_URL="https://snake-curious-easily.ngrok-free.app"
+JSON_DATA='{"jobName":"kube-burner-poc","parameters":{ "HOST": "localhost", "USER": "unknown-user", "BUILD_URL":"'"$build_url"'", "ARTIFACTS_URL":"'"$artifacts_url"'"}}'
+
+curl -X POST \
+     -u "user:$WEBHOOK_USER"  \
+     --header "Content-Type: application/json" \
+     "$WEBHOOK_URL" \
+     -d "$JSON_DATA"
