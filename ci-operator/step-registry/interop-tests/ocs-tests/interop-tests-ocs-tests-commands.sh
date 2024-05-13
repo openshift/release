512b33a8c7b4d4ae87ca0d639daca19f7a2c876f
Rabin Yasharzadehe <rabin@redhat.com>
2024-05-13 15:42:59 +0300

    Set cache folder for pytest
    Signed-off-by: Rabin Yasharzadehe <rabin@redhat.com>

--- ci-operator/step-registry/interop-tests/ocs-tests/interop-tests-ocs-tests-commands.sh
+++ ci-operator/step-registry/interop-tests/ocs-tests/interop-tests-ocs-tests-commands.sh
@@ -55,7 +55,7 @@ __EOF__
 set -x
 START_TIME=$(date "+%s")
 
-run-ci --color=yes tests/ -m 'acceptance and not ui' -k '' \
+run-ci --color=yes -o cache_dir=/tmp tests/ -m 'acceptance and not ui' -k '' \
   --ocsci-conf "${LOGS_CONFIG}" \
   --collect-logs \
   --ocs-version "${OCS_VERSION}" \
