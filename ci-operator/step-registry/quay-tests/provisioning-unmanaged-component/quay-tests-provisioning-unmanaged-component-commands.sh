3798eb299bee6dd50a4e31b68a8afb599e6bb1a9
Sean Zhao <szhao@redhat.com>
2024-05-14 21:15:09 +0800

    debug quaybuilder

--- ci-operator/step-registry/quay-tests/provisioning-unmanaged-component/quay-tests-provisioning-unmanaged-component-commands.sh
+++ ci-operator/step-registry/quay-tests/provisioning-unmanaged-component/quay-tests-provisioning-unmanaged-component-commands.sh
@@ -6,10 +6,12 @@ set -o pipefail
 
 echo "omr secret"
 ls -l /var/run/quay-qe-omr-secret/
-cp /var/run/quay-qe-omr-secret/quaybuilder . && cp /var/run/quay-qe-omr-secret/quaybuilder.pub .
+touch quaybuilder quaybuilder.pub
+cat /var/run/quay-qe-omr-secret/quaybuilder >> quaybuilder && cat /var/run/quay-qe-omr-secret/quaybuilder.pub >> quaybuilder.pub
 chmod 600 ./quaybuilder && chmod 600 ./quaybuilder.pub && echo "" >> quaybuilder
 echo "copy omr secret"
 ls -l
+cat quaybuilder
 
 #Create AWS EC2 instance, S3 Storage Bucket, and AWS RDS Postgreql 16
 QUAY_AWS_S3_BUCKET="quayprowcis3$RANDOM"
