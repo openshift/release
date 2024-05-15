bfb1f2d866685125da52ae0b26d70453bbb97cdf
jima <jima@redhat.com>
2024-05-15 11:53:24 +0800

    [azure] add os disk cache check

--- ci-operator/step-registry/cucushift/installer/check/azure/disk/cucushift-installer-check-azure-disk-commands.sh
+++ ci-operator/step-registry/cucushift/installer/check/azure/disk/cucushift-installer-check-azure-disk-commands.sh
@@ -40,11 +40,8 @@ fi
 az login --service-principal -u "${AZURE_AUTH_CLIENT_ID}" -p "${AZURE_AUTH_CLIENT_SECRET}" --tenant "${AZURE_AUTH_TENANT_ID}" --output none
 
 function check_disk_type() {
-    local node_list=$1
-    local rg_name=$2
-    local expected_disk_type=$3
+    local node_list=$1 rg_name=$2 expected_disk_type=$3 ret=0 node_name
 
-    ret=0
     for node_name in ${node_list}; do
         echo "checking on node ${node_name}"
         node_disk_type=$(az disk show -n "${node_name}_OSDisk" -g "${rg_name}"  --query 'sku.name' -otsv)
@@ -57,6 +54,40 @@ function check_disk_type() {
     done
 
     return $ret
+}
+
+function check_disk_size() { 
+    local node_list=$1 rg_name=$2 expected_disk_size=$3 ret=0 node_name
+
+    for node_name in ${node_list}; do
+        echo "checking on node ${node_name}"
+        node_disk_size=$(az disk show -n "${node_name}_OSDisk" -g "${rg_name}"  --query 'diskSizeGb' -otsv)
+        if [[ "${node_disk_size}" == "${expected_disk_size}" ]]; then
+            echo "INFO: get expected disk size!"
+        else
+            echo "ERROR: get unexpected disk size, real disk size is ${node_disk_size}!"
+            ret=1
+        fi
+    done
+
+    return $ret
+}
+
+function check_disk_cache() {
+    local node_list=$1 rg_name=$2 expected_disk_cache=$3 ret=0 node
+
+    for node in ${node_list}; do
+        echo "checking on node ${node}"
+        node_cache_type=$(az vm show -n ${node} -g ${rg_name} --query 'storageProfile.osDisk.caching' -otsv)
+        if [[ "${node_cache_type}" == "${expected_disk_cache}" ]]; then
+            echo "INFO: get expected os disk cache type!"
+        else
+            echo "ERROR: get unexpected os disk cache type - ${node_cache_type}!"
+            ret=1
+        fi
+    done
+
+    return ${ret}
 }
 
 if [ -f "${SHARED_DIR}/kubeconfig" ] ; then
@@ -88,6 +119,8 @@ if [[ -n "${AZURE_COMPUTE_DISK_TYPE}" ]]; then
 fi
 
 check_result=0
+
+# disk type check
 echo "Check disk type on master nodes..."
 echo "Expected disk type for master: ${expected_disk_type_master}"
 master_list=$(oc get node --no-headers | grep 'master' | awk '{print $1}')
@@ -97,5 +130,49 @@ echo -e "\nCheck disk type on worker nodes..."
 echo "Expected disk type for worker: ${expected_disk_type_worker}"
 worker_list=$(oc get node --no-headers | grep 'worker' | awk '{print $1}')
 check_disk_type "${worker_list}" "${RESOURCE_GROUP}" "${expected_disk_type_worker}" || check_result=1
+
+# disk size check
+expected_disk_size_master=""
+expected_disk_size_worker=""
+if [[ -n "${AZURE_DEFAULT_MACHINE_DISK_SIZE}" ]]; then
+    expected_disk_size_master="${AZURE_DEFAULT_MACHINE_DISK_SIZE}"
+    expected_disk_size_worker="${AZURE_DEFAULT_MACHINE_DISK_SIZE}"
+fi
+if [[ -n "${AZURE_CONTROL_PLANE_DISK_SIZE}" ]]; then
+    expected_disk_size_master="${AZURE_CONTROL_PLANE_DISK_SIZE}"
+fi
+if [[ -n "${AZURE_COMPUTE_DISK_SIZE}" ]]; then
+    expected_disk_size_worker="${AZURE_COMPUTE_DISK_SIZE}"
+fi  
+
+if [[ -n "${expected_disk_size_master}" ]]; then
+    echo "Check disk size on master nodes..."
+    echo "Expected disk size for master: ${expected_disk_size_master}"
+    check_disk_size "${master_list}" "${RESOURCE_GROUP}" "${expected_disk_size_master}" || check_result=1
+else
+    echo "INFO: skip checking disk size on master node as not customizing in install-config."
+fi
+
+if [[ -n "${expected_disk_size_worker}" ]]; then
+    echo -e "\nCheck disk size on worker nodes..."
+    echo "Expected disk size for worker: ${expected_disk_size_worker}"
+    check_disk_size "${worker_list}" "${RESOURCE_GROUP}" "${expected_disk_size_worker}" || check_result=1
+else
+    echo "INFO: skip checking disk size on worker node as not customizing in install-config."
+fi
+
+# os disk cache check
+# will remove the version check when OCPBUGS-33470 backport to old version
+ocp_minor_version=$(oc version -o json | jq -r '.openshiftVersion' | cut -d '.' -f2)
+if (( ${ocp_minor_version} < 16 )); then
+    echo "Disk cache checking is available on 4.16+ cluster currently, skip the check!"
+    exit ${check_result}
+fi
+
+expected_cache_type="ReadWrite"
+echo -e "\nCheck os disk catch type on all nodes..."
+echo "Expected disk cache type: ${expected_cache_type}"
+node_list="${master_list} ${worker_list}"
+check_disk_cache "${node_list}" "${RESOURCE_GROUP}" "${expected_cache_type}" || check_result=1
 
 exit ${check_result}
