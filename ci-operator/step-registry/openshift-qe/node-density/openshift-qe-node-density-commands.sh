5d866c22a241ced9e410049caf2fb75fb9093c35
Shashank Reddy Boyapally <sboyapal@redhat.com>
2024-05-16 09:43:29 -0500

    updated script
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>

--- ci-operator/step-registry/openshift-qe/node-density/openshift-qe-node-density-commands.sh
+++ ci-operator/step-registry/openshift-qe/node-density/openshift-qe-node-density-commands.sh
@@ -41,22 +41,16 @@ cp -r "${metrics_folder_name}" "${ARTIFACT_DIR}/"
 
 WEBHOOK_USER=$(cat "/horreum-secret/horreum-webhook-user")
 
-export prow_base_url="https://prow.ci.openshift.org/view/gs/origin-ci-test/logs"
-export prow_pr_base_url="https://prow.ci.openshift.org/view/gs/test-platform-results/pr-logs/pull/openshift_release"
 export artifacts_base_url="https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/test-platform-results/logs"
 export artifacts_pr_base_url="https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/test-platform-results/pr-logs/pull/openshift_release"
 job_id=$JOB_NAME
 task_id=$BUILD_ID
 
 if [[ "${JOB_TYPE}" == "presubmit" ]]; then
-    build_url="${prow_pr_base_url}/${PULL_NUMBER}/${job_id}/${task_id}"
     artifacts_url="${artifacts_pr_base_url}/${PULL_NUMBER}/${job_id}/${task_id}/artifacts"
 else
-    build_url="${prow_base_url}/${job_id}/${task_id}"
     artifacts_url="${artifacts_base_url}/${job_id}/${task_id}/artifacts"
 fi
-
-
 
 WEBHOOK_URL="https://snake-curious-easily.ngrok-free.app"
 JSON_DATA='{"jobName":"kube-burner-poc","parameters":{ "HOST": "localhost", "USER": "unknown-user", "ARTIFACT_URL":"'"$artifacts_url"'", "BUCKET_NAME":"test-platform-results", "TYPE":"report"}}'
