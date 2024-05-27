4f9ec222cfa05e326e9d1239df784fa139477d51
He Liu <heli@redhat.com>
2024-05-27 17:57:35 +0800

    update ps

--- ci-operator/step-registry/rosa/cluster/provision/rosa-cluster-provision-commands.sh
+++ ci-operator/step-registry/rosa/cluster/provision/rosa-cluster-provision-commands.sh
@@ -334,6 +334,7 @@ if [[ "$HOSTED_CP" == "true" ]]; then
     fi
     PROVISION_SHARD_ID=$(echo "$psList" | head -n 1)
 
+    PROVISION_SHARD_ID=87108e50-9b76-11ee-bba4-0a580a830612
     HYPERSHIFT_SWITCH="${HYPERSHIFT_SWITCH}  --properties provision_shard_id:${PROVISION_SHARD_ID}"
     record_cluster "properties" "provision_shard_id" ${PROVISION_SHARD_ID}
   fi
