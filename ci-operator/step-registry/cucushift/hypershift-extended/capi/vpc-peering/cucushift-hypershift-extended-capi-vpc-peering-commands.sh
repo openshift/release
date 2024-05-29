7d8350b19cbf3d5c078d058ce115751edef4a10c
He Liu <heli@redhat.com>
2024-05-29 15:31:46 +0800

    enabel external oidc in capi (#52499)

--- ci-operator/step-registry/cucushift/hypershift-extended/capi/vpc-peering/cucushift-hypershift-extended-capi-vpc-peering-commands.sh
+++ ci-operator/step-registry/cucushift/hypershift-extended/capi/vpc-peering/cucushift-hypershift-extended-capi-vpc-peering-commands.sh
@@ -16,10 +16,11 @@ function set_proxy () {
 function retry() {
     local check_func=$1
     shift
-    local max_retries=20
+    local max_retries=30
     local retry_delay=60
     local retries=0
 
+    echo "retry $check_func"
     while (( retries < max_retries )); do
         if $check_func "$@"; then
             return 0
@@ -45,15 +46,6 @@ function check_vpc_peering_connection() {
       fi
       echo "vpc peering is active now between capi mgmt vpc ${mgmt_vpc_id} and hc vpc ${hc_vpc_id}"
       return 0
-}
-
-function check_kubeconfig_secret() {
-    local res
-    res=$(oc get secret -A | grep ${CLUSTER_NAME}-kubeconfig)
-    if [[ -z "$res" ]]; then
-      return 1
-    fi
-    return 0
 }
 
 set_proxy
@@ -111,28 +103,9 @@ done
 # If the hosted cluster has been created, update the default security group to enable access from the management cluster's CAPI controller to the hosted cluster's API server.
 # in a standard rosa hcp, apiserver port is always 443, we need to expose 443 to the ip range of mgmt cidr.
 # capi kubeconfig is needed here to check capi resources
-if [[ ! -f "${SHARED_DIR}/mgmt_kubeconfig" ]]; then
-  echo "capi mgmt kubeconfig file mgmt_kubeconfig not found error"
-  exit 1
-fi
-export KUBECONFIG="${SHARED_DIR}/mgmt_kubeconfig"
-CLUSTER_NAME=$(cat "${SHARED_DIR}/cluster-name")
-if [[ -z "${CLUSTER_NAME}" ]] ; then
-  echo "Error: cluster name not found"
-  exit 1
-fi
-
-read -r namespace _ status _  <<< "$(oc get cluster -A --ignore-not-found | grep ${CLUSTER_NAME})"
-if [[ -n "${namespace}" ]] ; then
-  echo "found ${CLUSTER_NAME} in namespace ${namespace}, status is ${status}"
-  cluster_id=$(cat "${SHARED_DIR}/cluster-id")
-  dft_security_group_id=$(aws ec2 describe-security-groups --region ${REGION} --filters "Name=vpc-id,Values=${hc_vpc_id}" "Name=group-name,Values=${cluster_id}-default-sg" --query 'SecurityGroups[].GroupId' --output text)
-  aws ec2 authorize-security-group-ingress --region ${REGION} --group-id ${dft_security_group_id} --protocol tcp --port 443 --cidr ${mgmt_vpc_cidr}
-  retry check_kubeconfig_secret
-
-  set +x
-  oc get secret -n ${namespace} ${CLUSTER_NAME}-kubeconfig --ignore-not-found -ojsonpath='{.data.value}' | base64 -d > "${SHARED_DIR}/kubeconfig"
-fi
+cluster_id=$(cat "${SHARED_DIR}/cluster-id")
+dft_security_group_id=$(aws ec2 describe-security-groups --region ${REGION} --filters "Name=vpc-id,Values=${hc_vpc_id}" "Name=group-name,Values=${cluster_id}-default-sg" --query 'SecurityGroups[].GroupId' --output text)
+aws ec2 authorize-security-group-ingress --region ${REGION} --group-id ${dft_security_group_id} --protocol tcp --port 443 --cidr ${mgmt_vpc_cidr}
 
 echo "${dft_security_group_id}" > ${SHARED_DIR}/capi_hcp_default_security_group
 echo "vpc-peering config done"
