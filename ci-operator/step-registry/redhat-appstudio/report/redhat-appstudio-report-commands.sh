ce7939d44a4ac3bc67f7a43a5a7136fcf33f3fd1
Pavel Sturc <psturc@redhat.com>
2024-05-29 15:38:00 +0200

    chore: move qe-tools to konflux-ci org (#50541)

--- ci-operator/step-registry/redhat-appstudio/report/redhat-appstudio-report-commands.sh
+++ ci-operator/step-registry/redhat-appstudio/report/redhat-appstudio-report-commands.sh
@@ -5,7 +5,7 @@ set -o errexit
 set -o pipefail
 
 cd "$(mktemp -d)"
-git clone --branch main https://github.com/redhat-appstudio/qe-tools .
+git clone --branch main https://github.com/konflux-ci/qe-tools .
 make build
 ./qe-tools prowjob create-report --report-portal-format
 ./qe-tools webhook report-portal
