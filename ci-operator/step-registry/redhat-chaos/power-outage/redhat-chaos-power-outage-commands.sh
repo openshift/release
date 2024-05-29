f36bf4a875ff2486cc744bdc2cb7795635db7a14
Paige Rubendall <prubenda@redhat.com>
2024-05-29 15:14:52 -0400

    adding more clouds to power outage
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED

--- ci-operator/step-registry/redhat-chaos/power-outage/redhat-chaos-power-outage-commands.sh
+++ ci-operator/step-registry/redhat-chaos/power-outage/redhat-chaos-power-outage-commands.sh
@@ -15,9 +15,36 @@ oc config view --flatten > /tmp/config
 export KUBECONFIG=/tmp/config
 export KRKN_KUBE_CONFIG=$KUBECONFIG
 
-mkdir -p $HOME/.aws
-cat "/secret/telemetry/.awscred" > $HOME/.aws/config
-cat ${CLUSTER_PROFILE_DIR}/.awscred > $HOME/.aws/config
+export AWS_SHARED_CREDENTIALS_FILE=${CLUSTER_PROFILE_DIR}/.awscred
+
+platform=$(oc get infrastructure cluster -o jsonpath='{.status.platformStatus.type}') 
+if [ "$platform" = "AWS" ]; then
+    mkdir -p $HOME/.aws
+    cat ${CLUSTER_PROFILE_DIR}/.awscred > $HOME/.aws/config
+    export AWS_DEFAULT_REGION=us-west-2
+elif [ "$platform" = "GCP" ]; then
+    export GCP_SHARED_CREDENTIALS_FILE=${CLUSTER_PROFILE_DIR}/gce.json
+    export GOOGLE_APPLICATION_CREDENTIALS="${GCP_SHARED_CREDENTIALS_FILE}"
+elif [ "$platform" = "Azure" ]; then
+    export AZURE_AUTH_LOCATION=${CLUSTER_PROFILE_DIR}/osServicePrincipal.json
+    # jq is not available in the ci image...
+    AZURE_SUBSCRIPTION_ID="$(jq -r .subscriptionId ${AZURE_AUTH_LOCATION})"
+
+    AZURE_TENANT_ID="$(jq -r .tenantId ${AZURE_AUTH_LOCATION})"
+
+    AZURE_CLIENT_ID="$(jq -r .clientId ${AZURE_AUTH_LOCATION})"
+    
+    AZURE_CLIENT_SECRET="$(jq -r .clientSecret ${AZURE_AUTH_LOCATION})"
+
+elif [ "$platform" = "IBMCloud" ]; then
+# https://github.com/openshift/release/blob/3afc9cb376776ca27fbb1a4927281e84295f4810/ci-operator/step-registry/openshift-extended/upgrade/pre/openshift-extended-upgrade-pre-commands.sh#L158
+    export IBMCLOUD_CLI=ibmcloud
+    export IBMCLOUD_HOME=/output   
+    region="${LEASED_RESOURCE}"
+    export region
+    export IBMC_URL="https://${region}.iaas.cloud.ibm.com/v1"
+    export IBMC_APIKEY=${CLUSTER_PROFILE_DIR}/ibmcloud-api-key
+fi 
 ls -al /secret/telemetry/
 
 ES_PASSWORD=$(cat "/secret/es/password")
@@ -31,7 +58,7 @@ telemetry_password=$(cat "/secret/telemetry/telemetry_password")
 
 # set the secrets from the vault as env vars
 export TELEMETRY_PASSWORD=$telemetry_password
-export AWS_DEFAULT_REGION=us-west-2
+
 
 ./power-outage/prow_run.sh
 rc=$?
