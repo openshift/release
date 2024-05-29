e8fd25f2a0db3815db1ec0e6366002787d921b20
Shashank Reddy Boyapally <sboyapal@redhat.com>
2024-05-29 15:16:20 -0400

    added default values for trigger
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>

--- ci-operator/step-registry/openshift-qe/cluster-density-v2/openshift-qe-cluster-density-v2-commands.sh
+++ ci-operator/step-registry/openshift-qe/cluster-density-v2/openshift-qe-cluster-density-v2-commands.sh
@@ -32,12 +32,14 @@ export ITERATIONS=$(($iteration_multiplier*$current_worker_count))
 
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
 export EXTRA_FLAGS+=" --gc-metrics=true --profile-type=${PROFILE_TYPE} --local-indexing"
+export USE_WEBHOOK="false"
 
 rm -f ${SHARED_DIR}/index.json
 ./run.sh
 
 folder_name=$(ls -t -d /tmp/*/ | head -1)
 jq ".iterations = $ITERATIONS" $folder_name/index_data.json >> ${SHARED_DIR}/index_data.json
+
 
 if [[ "${USE_WEBHOOK}" == "true" ]]; then
     metrics_folder_name=$(find . -maxdepth 1 -type d -name 'collected-metric*' | head -n 1)
