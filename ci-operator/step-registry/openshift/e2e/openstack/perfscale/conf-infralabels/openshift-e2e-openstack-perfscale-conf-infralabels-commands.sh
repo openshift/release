2f54c66ff7a355f4c20f048483945864e9b5ae05
Miguel Carpio <mcarpio@redhat.com>
2024-05-31 01:26:24 +0200

    Adding openshift-e2e-openstack-perfscale-conf-infralabels step to add the infra label to all workers to host infra components

---
+++ ci-operator/step-registry/openshift/e2e/openstack/perfscale/conf-infralabels/openshift-e2e-openstack-perfscale-conf-infralabels-commands.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# For disconnected or otherwise unreachable environments, we want to
+# have steps use an HTTP(S) proxy to reach the API server. This proxy
+# configuration file should export HTTP_PROXY, HTTPS_PROXY, and NO_PROXY
+# environment variables, as well as their lowercase equivalents (note
+# that libcurl doesn't recognize the uppercase variables).
+if test -f "${SHARED_DIR}/proxy-conf.sh"
+then
+	# shellcheck disable=SC1090
+	source "${SHARED_DIR}/proxy-conf.sh"
+fi
+
+if test ! -f "${KUBECONFIG}"
+then
+	echo "No kubeconfig, can not continue."
+	exit 0
+fi
+
+# Adding the infra label to all workers
+for i in `oc get nodes -o 'jsonpath={range .items[*]}{.metadata.name}{"\n"}{end}' | grep worker`; do oc label nodes/$i node-role.kubernetes.io/infra=; done
