e9af41714b0503331ceb283e50de4c6cb4cead51
Caleb Evans <caevans@redhat.com>
2024-05-30 11:45:04 -0600

    Don't export the kubeconfig

--- ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
+++ ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
@@ -86,4 +86,4 @@ go mod edit -replace=gitlab.cee.redhat.com/app-mig/oadp-e2e-qe=$OADP_GIT_DIR/e2e
 go mod tidy
 sleep 7200
 #EXTRA_GINKGO_PARAMS=$OADP_TEST_FOCUS /bin/bash /alabama/cspi/test_settings/scripts/test_runner.sh
-export KUBECONFIG="${SHARED_DIR}/kubeconfig" && export TESTS_FOLDER="/alabama/cspi/e2e/kubevirt-plugin" && /bin/bash /alabama/cspi/test_settings/scripts/test_runner.sh
+export TESTS_FOLDER="/alabama/cspi/e2e/kubevirt-plugin" && /bin/bash /alabama/cspi/test_settings/scripts/test_runner.sh
