bbe9391779596611165a07917bbd71fc7bc68615
davdhacs <105243888+davdhacs@users.noreply.github.com>
2024-05-31 11:16:23 -0600

    Revert "ocp416 setup"
    This reverts commit 866d288bdf148a5d690b44819ae373e4d1a87056.

--- ci-operator/step-registry/stackrox/operator-e2e/stackrox-operator-e2e-commands.sh
+++ ci-operator/step-registry/stackrox/operator-e2e/stackrox-operator-e2e-commands.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 job="${TEST_SUITE:-${JOB_NAME_SAFE#merge-}}"
 job="${job#nightly-}"
-echo "Allow restricted SCC"
-echo oc create clusterrolebinding system:openshift:scc:restricted --clusterrole=system:openshift:scc:restricted --group=system:authenticated
-oc create clusterrolebinding system:openshift:scc:restricted --clusterrole=system:openshift:scc:restricted --group=system:authenticated || true
 exec .openshift-ci/dispatch.sh "${job}"
