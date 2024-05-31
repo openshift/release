7cb29d8f724379feb7572a14e801afd9fe8cd210
Caleb Evans <caevans@redhat.com>
2024-05-31 14:32:00 -0600

    Add sleep for debugging

--- ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
+++ ci-operator/step-registry/oadp/execute-tests/oadp-execute-tests-commands.sh
@@ -85,4 +85,5 @@ cd $OADP_GIT_DIR
 go mod edit -replace=gitlab.cee.redhat.com/app-mig/oadp-e2e-qe=$OADP_GIT_DIR/e2e
 go mod tidy
 #EXTRA_GINKGO_PARAMS=$OADP_TEST_FOCUS /bin/bash /alabama/cspi/test_settings/scripts/test_runner.sh
+sleep 7200
 export TESTS_FOLDER="/alabama/cspi/e2e/kubevirt-plugin" && /bin/bash /alabama/cspi/test_settings/scripts/test_runner.sh
