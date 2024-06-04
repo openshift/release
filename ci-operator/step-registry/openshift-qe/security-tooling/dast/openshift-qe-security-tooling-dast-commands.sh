ed3c68faa01dfe5c52d4e88479af55587c2dbcad
Paige Rubendall <prubenda@redhat.com>
2024-06-04 16:52:14 -0400

    adding few changes
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED

--- ci-operator/step-registry/openshift-qe/security-tooling/dast/openshift-qe-security-tooling-dast-commands.sh
+++ ci-operator/step-registry/openshift-qe/security-tooling/dast/openshift-qe-security-tooling-dast-commands.sh
@@ -21,13 +21,6 @@ DAST_PATH
 ls
 
 oc login -u kubeadmin -p $KUBEADMIN_PASSWORD_FILE
-HELM_DIR=$(mktemp -d)
-curl -sS -L https://get.helm.sh/helm-v3.11.2-linux-amd64.tar.gz | tar -xzC ${HELM_DIR}/ linux-amd64/helm
 
-${HELM_DIR}/linux-amd64/helm version  
-
-mv ${HELM_DIR}/linux-amd64/helm $WORKSPACE/helm
-PATH=$PATH:$WORKSPACE
-helm version
 pushd ocp-qe-perfscale-ci
 ./deploy_ssml_api.sh
