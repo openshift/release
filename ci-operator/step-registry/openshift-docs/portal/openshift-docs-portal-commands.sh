ef7c4296eeb76d7b8a1f2bc5edb2ef10a6b3e943
Aidan Reilly <74046732+aireilly@users.noreply.github.com>
2024-06-04 16:29:36 +0100

    adding validate-portal updates

--- ci-operator/step-registry/openshift-docs/portal/openshift-docs-portal-commands.sh
+++ ci-operator/step-registry/openshift-docs/portal/openshift-docs-portal-commands.sh
@@ -39,15 +39,32 @@ for DISTRO in "${DISTROS[@]}"; do
     ./scripts/get-updated-distros.sh | while read -r FILENAME; do
         if [ "${FILENAME}" == "${TOPICMAP}" ]; then
             echo -e "\e[91mBuilding openshift-docs with ${DISTRO} distro...\e[0m"
-            python3 "${BUILD}" --distro "${DISTRO}" --product "OpenShift Container Platform" --version "${VERSION}" --no-upstream-fetch >> "${LOG}" 2>&1
+            # Exit on failures
+            if ! python3 "${BUILD}" --distro "${DISTRO}" --product "OpenShift Container Platform" --version "${VERSION}" --no-upstream-fetch > "${LOG}" 2>&1; then
+                echo -e "\e[91mBuild failed for ${DISTRO}. Exiting...\e[0m"
+                # Grep for Asciidoctor errors in the log and exit 1 if found
+                check_errors "${LOG}"
+                exit 1
+            else
+                check_errors "${LOG}"
+            fi
         elif [ "${FILENAME}" == "_distro_map.yml" ]; then
-            python3 "${BUILD}" --distro "openshift-enterprise" --product "OpenShift Container Platform" --version "${VERSION}" --no-upstream-fetch >> "${LOG}" 2>&1
+            if ! python3 "${BUILD}" --distro "openshift-enterprise" --product "OpenShift Container Platform" --version "${VERSION}" --no-upstream-fetch > "${LOG}" 2>&1; then
+                echo -e "\e[91mBuild failed for openshift-enterprise. Exiting...\e[0m"
+                exit 1
+            else
+                check_errors "${LOG}"
+            fi
         fi
     done
 done
 
-check_errors "${LOG}"
-
 if [ -d "drupal-build" ]; then
-    python3 makeBuild.py
+    if ! python3 makeBuild.py > "${LOG}" 2>&1; then
+        echo -e "\e[91mPortal build failed. Exiting...\e[0m"
+        exit 1
+    else
+        check_errors "${LOG}"
+    fi
 fi
+
