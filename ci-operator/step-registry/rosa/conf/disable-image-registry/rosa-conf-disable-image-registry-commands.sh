a13a7e37e5a4873efea2d1fecab4e9a3704ee702
radtriste <tradisso@redhat.com>
2024-06-06 09:48:20 +0200

    [ROSA] OCM-8480  Disable image registry

---
+++ ci-operator/step-registry/rosa/conf/disable-image-registry/rosa-conf-disable-image-registry-commands.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
+
+export KUBECONFIG="${SHARED_DIR}/kubeconfig"
+
+function set_proxy () {
+    if test -s "${SHARED_DIR}/proxy-conf.sh" ; then
+        echo "setting the proxy"
+        # cat "${SHARED_DIR}/proxy-conf.sh"
+        echo "source ${SHARED_DIR}/proxy-conf.sh"
+        source "${SHARED_DIR}/proxy-conf.sh"
+    else
+        echo "no proxy setting."
+    fi
+}
+
+set_proxy
+
+echo "Disable image registry in OCP config..."
+oc patch configs.imageregistry.operator.openshift.io/cluster --type merge -p '{"spec":{"managementState":"Removed"}}'
+
+
