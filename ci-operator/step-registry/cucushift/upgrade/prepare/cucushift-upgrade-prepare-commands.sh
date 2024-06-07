5b45d933f74b118c63a9eeac45a5321f8d4d305c
Liang Xia <lxia@redhat.com>
2024-06-07 17:44:49 +0800

    Small enhancement to test result format (#52922)

--- ci-operator/step-registry/cucushift/upgrade/prepare/cucushift-upgrade-prepare-commands.sh
+++ ci-operator/step-registry/cucushift/upgrade/prepare/cucushift-upgrade-prepare-commands.sh
@@ -258,8 +258,7 @@ function summarize_test_results() {
     done < /tmp/zzz-tmp.log
     TEST_RESULT_FILE="${ARTIFACT_DIR}/test-results.yaml"
     cat > "${TEST_RESULT_FILE}" <<- EOF
-cucushift:
-  type: cucushift-upgrade-prepare
+cucushift-upgrade-prepare:
   total: $tests
   failures: $failures
   errors: $errors
