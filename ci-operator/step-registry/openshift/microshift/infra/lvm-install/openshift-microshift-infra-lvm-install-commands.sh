4576d0665b46f0ed08ae082be58e3886c7bc0a16
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2024-06-10 13:13:46 +0200

    NO-ISSUE: Add ocp conformance presubmits (#52945)
    * NO-ISSUE: Add conformance presubmits for main microshift
    
    * NO-ISSUE: Add conformance presubmits for 4.16 microshift
    
    * NO-ISSUE: Remove instance type from conformance presubmits
    
    * NO-ISSUE: Prepare lvm for c7i instances

--- ci-operator/step-registry/openshift/microshift/infra/lvm-install/openshift-microshift-infra-lvm-install-commands.sh
+++ ci-operator/step-registry/openshift/microshift/infra/lvm-install/openshift-microshift-infra-lvm-install-commands.sh
@@ -20,7 +20,7 @@ chmod 0600 "${HOME}/.ssh/config"
 
 device="/dev/xvdc"
 
-if [[ "${EC2_INSTANCE_TYPE%.*}" =~ .*"g".* || "${EC2_INSTANCE_TYPE%.*}" =~ "t3".* ]]; then
+if [[ "${EC2_INSTANCE_TYPE%.*}" =~ .*"g".* || "${EC2_INSTANCE_TYPE%.*}" =~ "t3".* || "${EC2_INSTANCE_TYPE%.*}" =~ c7i.2xlarge ]]; then
   # Sometimes, devices are in different order and nvme1 stores OS while nvme0 should hold LVM for topolvm.
   # If /dev/nvme0 is already partitioned (operating system), then use nvme1 for lvm.
   # If `partx /dev/nvme0n1` fails (rc=1), it couldn't read partition table, so it's the one to use for lvm.
