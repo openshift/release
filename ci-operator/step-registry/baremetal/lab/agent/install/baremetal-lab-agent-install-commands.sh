38ba1d5d50afdfd573ec4f1116d41fece1257c22
Biagio Manzari <bmanzari@redhat.com>
2024-06-11 14:40:53 +0200

    handle install success/failure

--- ci-operator/step-registry/baremetal/lab/agent/install/baremetal-lab-agent-install-commands.sh
+++ ci-operator/step-registry/baremetal/lab/agent/install/baremetal-lab-agent-install-commands.sh
@@ -242,7 +242,7 @@ http_proxy="${proxy}" https_proxy="${proxy}" HTTP_PROXY="${proxy}" HTTPS_PROXY="
   oinst agent wait-for bootstrap-complete 2>&1 &
 if ! wait $!; then
   # Used by observer pod
-  touch "${SHARED_DIR}/INSTALL_FAILED"
+  touch "${SHARED_DIR}/failure"
   # TODO: gather logs??
   echo "ERROR: Bootstrap failed. Aborting execution."
   exit 1
@@ -255,10 +255,10 @@ http_proxy="${proxy}" https_proxy="${proxy}" HTTP_PROXY="${proxy}" HTTPS_PROXY="
 if ! wait "$!"; then
   echo "ERROR: Installation failed. Aborting execution."
   # Used by observer pod
-  touch "${SHARED_DIR}/INSTALL_FAILED"
+  touch "${SHARED_DIR}/failure"
   # TODO: gather logs??
   exit 1
 fi
 
 # Used by observer pod
-touch "${SHARED_DIR}/INSTALL_COMPLETE"
+touch  "${SHARED_DIR}/success"
