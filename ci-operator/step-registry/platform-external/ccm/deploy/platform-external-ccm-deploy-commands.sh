af899a4dd0e4ceaa45092ce92a962ae646c1b532
Marco Braga <mrbraga@redhat.com>
2024-06-11 15:43:13 -0300

    SPLAT-1158: platform external e2e workflow using AWS as provider (#45921)
    * plat-external/ci/workflow/e2e: add external-aws workflow
    
    Introduce workflow to test e2e (default CI step for
    openshift/conformance suite) on clusters installed on provider
    AWS with platform type External (UPI method).
    
    * plat-external/install/step-1: setup openshift configuration
    
    Introduce steps to create the installer configuration files
    when deploying a cluster with platform type External
    following the documentation of the
    Infrastructure Provider Onboarding Guide (step 1):
    
    https://docs.providers.openshift.org/platform-external/installing/
    
    * plat-external/install/step-3: deploy CCM on aws
    
    Introduce step to deploy CCM to a cluster installed on AWS
    with platform type External following the documentation
    of Infrastructure Providers Onboarding Guide (step 3):
    
    https://docs.providers.openshift.org/platform-external/installing/
    
    * plat-external/install/step-2: create infrastructure on provider aws
    
    Introduce steps to create infrastructure on AWS
    with platform type External following the documentation
    of Infrastructure Provider Onboarding Guide (step 2):
    
    https://docs.providers.openshift.org/platform-external/installing/
    
    * plat-external/ci/workflow/opct: add external-aws workflow
    
    Introduce OPCT regular conformance workflow to a new structured
    directory under step-registry/opct to be used in the new
    infra provisioning introduced by AWS steps for platform external(
    using agnostic/upi install method).
    
    * plat-external/ci/jobs: presubmit and periodic to origin and installer
    
    Introduce jobs to execute the e2e workflows on a cluster installated
    with platform type External on AWS provider (upi method
    with customized CloudFormation templates).
    
    The opct conformance workflow is also added to periodic nightly for 4.16.
    
    Jobs added:
    - presubmit installer@master e2e
    - presubmit installer@master e2e with CCM
    - presubmit installer@release-4.16 e2e with CCM
    - presubmit origin@master e2e (without e2e skips)
    - presubmit origin@master e2e with CCM (without e2e skips)
    - periodic ci-4.16 weekly e2e
    - periodic ci-4.16 weekly e2e with CCM
    - periodic nightly-4.16 weekly e2e
    - periodic nightly-4.16 weekly e2e with CCM
    - periodic nightly-4.16 weekly opct with CCM
    
    * plat-external/ci/jobs/gen: presub and periodic to origin+installer
    
    Introducing the following jobs for the platform type External on AWS
    provider:
      - presubmit installer@master e2e
      - presubmit installer@master e2e with CCM
      - presubmit installer@release-4.16 e2e with CCM
      - presubmit origin@master e2e (without e2e skips)
      - presubmit origin@master e2e with CCM (without e2e skips)
      - periodic ci-4.16 weekly e2e
      - periodic ci-4.16 weekly e2e with CCM
      - periodic nightly-4.16 weekly e2e
      - periodic nightly-4.16 weekly e2e with CCM
      - periodic nightly-4.16 weekly opct with CCM

---
+++ ci-operator/step-registry/platform-external/ccm/deploy/platform-external-ccm-deploy-commands.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+#
+# Deploy CCM (provider agnostic) manifests and collect the logs.
+#
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+function turn_down() {
+  touch /tmp/ccm.done
+}
+trap turn_down EXIT
+
+export KUBECONFIG=${SHARED_DIR}/kubeconfig
+test -f "${SHARED_DIR}/deploy.env" && source "${SHARED_DIR}/deploy.env"
+
+source "${SHARED_DIR}/init-fn.sh" || true
+
+log "Starting CCM setup"
+
+log "Collecting current cluster state"
+
+log "Infrastructure CR:"
+oc get infrastructure -o yaml
+
+log "Nodes:"
+oc get nodes
+
+log "Pods:"
+oc get pods -A
+
+if [[ "${PLATFORM_EXTERNAL_CCM_ENABLED-}" != "yes" ]]; then
+  log "Ignoring CCM Installation setup. PLATFORM_EXTERNAL_CCM_ENABLED!=yes [${PLATFORM_EXTERNAL_CCM_ENABLED}]"
+  exit 0
+fi
+
+source ${SHARED_DIR}/ccm.env
+
+function stream_logs() {
+  log "[log-stream] Starting log streamer"
+  oc logs ${CCM_RESOURCE} -n ${CCM_NAMESPACE} >> ${ARTIFACT_DIR}/logs-ccm.txt 2>&1
+  log "[log-stream] Finish log streamer"
+}
+
+function watch_logs() {
+  log "[watcher] Starting watcher"
+  while true; do
+    test -f /tmp/ccm.done && break
+
+    log "[watcher] creating streamer..."
+    stream_logs &
+    PID_STREAM="$!"
+    log "[watcher] waiting streamer..."
+
+    test -f /tmp/ccm.done && break
+    sleep 10
+    kill -9 "${PID_STREAM}" || true
+  done
+  log "[watcher] done!"
+}
+
+log "Creating watcher"
+watch_logs &
+PID_WATCHER="$!"
+
+log "Deploying Cloud Controller Manager"
+
+while read -r manifest
+do
+  echo "Processing manifest ${SHARED_DIR}/$manifest";
+  oc create -f ${SHARED_DIR}/$manifest || true
+done <<< "$(cat "${SHARED_DIR}/ccm-manifests.txt")"
+
+## What will be an standard method?
+# For AWS:
+#CCM_STATUS_KEY=.status.availableReplicas
+# For OCI:
+#CCM_STATUS_KEY=.status.numberAvailable
+if [[ -z "${CCM_STATUS_KEY}" ]]; then
+  export CCM_STATUS_KEY=.status.availableReplicas
+fi
+until  oc wait --for=jsonpath="{${CCM_STATUS_KEY}}"=${CCM_REPLICAS_COUNT} ${CCM_RESOURCE} -n ${CCM_NAMESPACE} --timeout=10m &> /dev/null
+do
+  log "Waiting for minimum replicas avaialble..."
+  sleep 10
+done
+
+log "CCM Ready!"
+
+oc get all -n ${CCM_NAMESPACE}
+
+log "Collecting logs for CCM initialization - initial 30 seconds"
+sleep 30
+touch /tmp/ccm.done
+
+log "Sent signal to finish watcher"
+wait "$PID_WATCHER"
+
+log "Watcher done!"
+
+oc get all -n ${CCM_NAMESPACE}
