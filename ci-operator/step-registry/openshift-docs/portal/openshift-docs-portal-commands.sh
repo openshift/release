b08190998b0d0b211c1ab6bd5366dc28afeff3f5
Tim O'Keefe <tokeefe@redhat.com>
2024-06-12 11:27:23 -0400

    OLS-691: create prow job for openshift lightspeed docs (#53113)

--- ci-operator/step-registry/openshift-docs/portal/openshift-docs-portal-commands.sh
+++ ci-operator/step-registry/openshift-docs/portal/openshift-docs-portal-commands.sh
@@ -12,7 +12,7 @@ IFS=' ' read -r -a DISTROS <<< "${DISTROS}"
 for DISTRO in "${DISTROS[@]}"; do
 
     case "${DISTRO}" in
-        "openshift-enterprise"|"openshift-acs"|"openshift-pipelines"|"openshift-serverless"|"openshift-gitops"|"openshift-builds"|"openshift-service-mesh"|"openshift-opp"|"openshift-rhde")
+        "openshift-enterprise"|"openshift-acs"|"openshift-pipelines"|"openshift-serverless"|"openshift-gitops"|"openshift-builds"|"openshift-service-mesh"|"openshift-opp"|"openshift-rhde"|"openshift-lightspeed")
             TOPICMAP="_topic_maps/_topic_map.yml"
             ;;
         "openshift-rosa")
