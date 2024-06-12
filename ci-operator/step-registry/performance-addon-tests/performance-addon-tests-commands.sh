408f5934229f3cd5cc8636451a4cb989ec5f840b
Vivek Shakya <vshakya@vshakya-thinkpadt14gen4.remote.csb>
2024-06-12 17:14:40 -0400

    try operator install ref for pao

--- ci-operator/step-registry/performance-addon-tests/performance-addon-tests-commands.sh
+++ ci-operator/step-registry/performance-addon-tests/performance-addon-tests-commands.sh
@@ -4,42 +4,42 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-PAO_INSTALL_NAMESPACE=performance-addon-operator
+# PAO_INSTALL_NAMESPACE=performance-addon-operator
 
 
-echo "Installing PAO  into ${PAO_INSTALL_NAMESPACE}"
+# echo "Installing PAO  into ${PAO_INSTALL_NAMESPACE}"
 
-# create the install namespace
-oc apply -f - <<EOF
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: "${PAO_INSTALL_NAMESPACE}"
-EOF
+# # create the install namespace
+# oc apply -f - <<EOF
+# apiVersion: v1
+# kind: Namespace
+# metadata:
+#   name: "${PAO_INSTALL_NAMESPACE}"
+# EOF
 
-# deploy new operator group
-oc apply -f - <<EOF
-apiVersion: operators.coreos.com/v1
-kind: OperatorGroup
-metadata:
-  name: "${PAO_INSTALL_NAMESPACE}-operator-group"
-  namespace: "${PAO_INSTALL_NAMESPACE}"
-spec:
-  targetNamespaces:
-  - $(echo \"${PAO_INSTALL_NAMESPACE}\" | sed "s|,|\"\n  - \"|g")
-EOF
+# # deploy new operator group
+# oc apply -f - <<EOF
+# apiVersion: operators.coreos.com/v1
+# kind: OperatorGroup
+# metadata:
+#   name: "${PAO_INSTALL_NAMESPACE}-operator-group"
+#   namespace: "${PAO_INSTALL_NAMESPACE}"
+# spec:
+#   targetNamespaces:
+#   - $(echo \"${PAO_INSTALL_NAMESPACE}\" | sed "s|,|\"\n  - \"|g")
+# EOF
 
-oc apply -f - <<EOF
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  name: performance-addon-operator-subscription
-  namespace: "${PAO_INSTALL_NAMESPACE}"
-spec:
-  channel: stable
-  name: performace-addon-operator
-  source: redhat-operators
-  sourceNamespace: openshift-marketplace
-EOF
+# oc apply -f - <<EOF
+# apiVersion: operators.coreos.com/v1alpha1
+# kind: Subscription
+# metadata:
+#   name: performance-addon-operator-subscription
+#   namespace: "${PAO_INSTALL_NAMESPACE}"
+# spec:
+#   channel: stable
+#   name: performace-addon-operator
+#   source: redhat-operators
+#   sourceNamespace: openshift-marketplace
+# EOF
 
 sleep 1800
