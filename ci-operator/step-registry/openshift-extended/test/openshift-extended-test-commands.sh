78c17d211baeb758a4b34d87f3696b16cd8ac265
ShudiLi <shudili@redhat.com>
2024-06-13 01:50:46 -0700

    debug for a destructive test

--- ci-operator/step-registry/openshift-extended/test/openshift-extended-test-commands.sh
+++ ci-operator/step-registry/openshift-extended/test/openshift-extended-test-commands.sh
@@ -249,6 +249,8 @@ oc wait nodes --all --for=condition=Ready=true --timeout=15m
 oc wait clusteroperators --all --for=condition=Progressing=false --timeout=15m
 oc get clusterversion version -o yaml || true
 
+sleep 10800
+
 function remove_kubeadmin_user() {
     if [[ "$KUBEADMIN_REMOVED" == "true" ]]; then
         ret_delete_admin=0
