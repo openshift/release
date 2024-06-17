38d2939f68d7a5ed6da52ef222c1a6a4a1ea1e86
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2024-06-17 14:56:48 -0400

    synthesize subnet.json to preserve steps that rely upon it (#53304)
    * synthesize subnets.json for steps which require it
    
    * inject subnets.json to shared_dir

--- ci-operator/step-registry/ipi/conf/vsphere/check/vcm/ipi-conf-vsphere-check-vcm-commands.sh
+++ ci-operator/step-registry/ipi/conf/vsphere/check/vcm/ipi-conf-vsphere-check-vcm-commands.sh
@@ -23,6 +23,28 @@ declare vsphere_cluster
 declare vsphere_url
 declare VCENTER_AUTH_PATH
 
+
+function networkToSubnetsJson() {
+  local NETWORK_CACHE_PATH=$1
+  local NETWORK_RESOURCE=$2
+
+  TMPSUBNETSJSON="/tmp/subnets-${NETWORK_RESOURCE}.json"
+
+  jq -r '.[.spec.primaryRouterHostname] = .[.spec.vlanId] |
+  .[.spec.primaryRouterHostname][.spec.vlanId] = .spec |
+  .[.spec.primaryRouterHostname][.spec.vlanId].dnsServer = .spec.gateway |
+  .[.spec.primaryRouterHostname][.spec.vlanId].mask = .spec.netmask |
+  .[.spec.primaryRouterHostname][.spec.vlanId].CidrIPv6 = .spec.cidrIPv6' "${NETWORK_CACHE_PATH}" > "${TMPSUBNETSJSON}" 
+
+
+  if [ -f "${SHARED_DIR}/subnets.json" ]; then
+    jq -s '.[0] * .[1]' "${TMPSUBNETSJSON}" "${SHARED_DIR}/subnets.json"
+  else
+    cp "${TMPSUBNETSJSON}" "${SHARED_DIR}/subnets.json"
+  fi
+}
+
+
 function log() {
   echo "$(date -u --rfc-3339=seconds) - " + "$1"
 }
@@ -49,7 +71,7 @@ def yamlify2:
 EOF
 
 function getTypeInHeirarchy() {
-  local DATATYPE=$1  
+  local DATATYPE=$1
   local LEVEL=$2
 
   FOUND=0
@@ -68,7 +90,7 @@ function getTypeInHeirarchy() {
       FOUND=1
     fi
   done
-  
+
   return 0
 }
 
@@ -78,23 +100,23 @@ dvsJSON="{}"
 # getDVSInfo build a map of JSON data for a specific network path
 function getDVSInfo() {
   local NETWORK=$1
-  
+
   if [[ $(jq -r '.["'"${NETWORK}"'"]' <<< "${dvsJSON}") != "null" ]]; then
     return
   fi
   log "gathering clusters and UUIDs associated with ${NETWORK}"
 
   dvsJSON=$(jq -r '. += {"'"${NETWORK}"'": {}}' <<< "${dvsJSON}")
-  
+
   govc ls -json -t DistributedVirtualPortgroup "${NETWORK}" > /tmp/dvs.json
   elements=$(jq length /tmp/dvs.json)
   DVS_idx=0
   while [[ $DVS_idx -lt ${elements} ]]; do
     log "DVS: ${DVS_idx}"
-        
-    parentDVS=$(jq --compact-output -r .elements[${DVS_idx}].Object.Config.DistributedVirtualSwitch /tmp/dvs.json)    
+
+    parentDVS=$(jq --compact-output -r .elements[${DVS_idx}].Object.Config.DistributedVirtualSwitch /tmp/dvs.json)
     UUID_FORMATTED=$(govc object.collect -json "$(jq -r .Type <<< "${parentDVS}")":"$(jq -r .Value <<< "${parentDVS}")" | jq -r '.[] | select(.Name=="uuid") | .Val')
-    
+
     # determine the cluster where this dvs instance resides
     HOST0=$(jq --compact-output -r .elements[${DVS_idx}].Object.Host[0] /tmp/dvs.json)
     if [[ $(jq --compact-output -r .elements[${DVS_idx}].Object.Host[0].Type /tmp/dvs.json) != "HostSystem" ]]; then
@@ -102,20 +124,20 @@ function getDVSInfo() {
       continue
     fi
     HOST=$(govc object.collect -json HostSystem:"$(jq --compact-output -r .Value <<< "${HOST0}")")
-    
+
     if [[ $(jq -r .elements[${DVS_idx}].Object.Host[0].Type /tmp/dvs.json) != "HostSystem" ]]; then
       log "unable to get host. ${HOST0} is not a known type"
       continue
     fi
 
-    getTypeInHeirarchy "ClusterComputeResource" "${HOST}"    
+    getTypeInHeirarchy "ClusterComputeResource" "${HOST}"
     # shellcheck disable=SC2181
     if [ "$?" -ne 0 ];then
       log "could not determine the compute cluster resource for ${NETWORK}"
       exit 1
     fi
     local CLUSTER=${LEVEL_NAME}
-    
+
     getTypeInHeirarchy "Datacenter" "${HOST}"
     # shellcheck disable=SC2181
     if [ "$?" -ne 0 ];then
@@ -142,7 +164,7 @@ fi
 if [[ -n "${VSPHERE_EXTRA_LEASED_RESOURCE:-}" ]]; then
   log "creating extra lease resources"
 
-  i=1  
+  i=1
   for extra_leased_resource in ${VSPHERE_EXTRA_LEASED_RESOURCE}; do
     log "creating extra leased resource ${extra_leased_resource}"
 
@@ -167,7 +189,7 @@ spec:
 fi
 
 
-if [[ -n "${VSPHERE_BASTION_LEASED_RESOURCE:-}" ]]; then  
+if [[ -n "${VSPHERE_BASTION_LEASED_RESOURCE:-}" ]]; then
   log "creating bastion lease resource ${VSPHERE_BASTION_LEASED_RESOURCE}"
 
   # shellcheck disable=SC1078
@@ -208,11 +230,11 @@ fi
 for POOL in "${pools[@]}"; do
   log "creating lease for pool ${POOL}"
   requiredPool=""
-  if [ "$POOL" != "unspecified" ]; then 
+  if [ "$POOL" != "unspecified" ]; then
     requiredPool="required-pool: $POOL"
     log "setting required pool ${requiredPool}"
   fi
-  
+
   # shellcheck disable=SC1078
   echo "apiVersion: vspherecapacitymanager.splat.io/v1
 kind: Lease
@@ -243,31 +265,33 @@ for LEASE in $LEASES; do
   oc get leases.vspherecapacitymanager.splat.io -n vsphere-infra-helpers --kubeconfig "${SA_KUBECONFIG}" "${LEASE}" -o json > /tmp/lease.json
   VCENTER=$(jq -r '.status.server' < /tmp/lease.json )
   NETWORK_PATH=$(jq -r '.status.topology.networks[0]' < /tmp/lease.json)
-  NETWORK_RESOURCE=$(jq -r '.metadata.ownerReferences[] | select(.kind=="Network") | .name' < /tmp/lease.json) 
+  NETWORK_RESOURCE=$(jq -r '.metadata.ownerReferences[] | select(.kind=="Network") | .name' < /tmp/lease.json)
 
   log "got lease ${LEASE}"
 
-  portgroup_name=$(echo "$NETWORK_PATH" | cut -d '/' -f 4)  
+  portgroup_name=$(echo "$NETWORK_PATH" | cut -d '/' -f 4)
   log "portgroup ${portgroup_name}"
 
   bastion_leased_resource=$(jq .metadata.labels.VSPHERE_BASTION_LEASED_RESOURCE < /tmp/lease.json)
   extra_leased_resource=$(jq .metadata.labels.VSPHERE_EXTRA_LEASED_RESOURCE < /tmp/lease.json)
 
   NETWORK_CACHE_PATH="${SHARED_DIR}/NETWORK_${NETWORK_RESOURCE}.json"
-  
+
   if [ ! -f "$NETWORK_CACHE_PATH" ]; then
     log caching network resource "${NETWORK_RESOURCE}"
     oc get networks.vspherecapacitymanager.splat.io -n vsphere-infra-helpers --kubeconfig "${SA_KUBECONFIG}" "${NETWORK_RESOURCE}" -o json > "${NETWORK_CACHE_PATH}"
   fi
 
+  networkToSubnetsJson "${NETWORK_CACHE_PATH}" "${NETWORK_RESOURCE}" 
+
   if [ "${bastion_leased_resource}" != "null" ]; then
-    log "setting bastion portgroup ${portgroup_name} in vsphere_context.sh"  
-    cat >>"${SHARED_DIR}/vsphere_context.sh" <<EOF    
+    log "setting bastion portgroup ${portgroup_name} in vsphere_context.sh"
+    cat >>"${SHARED_DIR}/vsphere_context.sh" <<EOF
 export vsphere_bastion_portgroup="${portgroup_name}"
 EOF
 
   elif [ "${extra_leased_resource}" != "null" ]; then
-    log "setting extra leased network ${portgroup_name} in vsphere_context.sh"  
+    log "setting extra leased network ${portgroup_name} in vsphere_context.sh"
     cat >>"${SHARED_DIR}/vsphere_context.sh" <<EOF
 export vsphere_extra_portgroup_${extra_leased_resource}="${portgroup_name}"
 EOF
@@ -280,7 +304,10 @@ EOF
   log "discovered portgroup ${vcenter_portgroups[$VCENTER]}"
 done
 
-# retrieving resource pools 
+# debug, confirm correct subnets.json
+cat "${SHARED_DIR}/subnets.json"
+
+# retrieving resource pools
 RESOURCE_POOLS=$(oc get pools.vspherecapacitymanager.splat.io --kubeconfig "${SA_KUBECONFIG}" -n vsphere-infra-helpers -o=jsonpath='{.items[*].metadata.name}')
 
 declare -A pool_usernames
@@ -295,7 +322,7 @@ for RESOURCE_POOL in ${RESOURCE_POOLS}; do
   log "checking to see if ${RESOURCE_POOL} is in use"
   # check to see if this pool is in use by a lease
   FOUND=0
-  for _leaseJSON in "${SHARED_DIR}"/LEASE*; do 
+  for _leaseJSON in "${SHARED_DIR}"/LEASE*; do
     _VCENTER=$(jq -r .status.name < "${_leaseJSON}")
     log "checking if ${_VCENTER} == ${RESOURCE_POOL}"
     if [ "${_VCENTER,,}" = "${RESOURCE_POOL,,}" ]; then
@@ -306,16 +333,16 @@ for RESOURCE_POOL in ${RESOURCE_POOLS}; do
 
   if [ ${FOUND} -eq 0 ]; then
     log "resource pool ${RESOURCE_POOL} isn't in use. excluding from failure domains"
-    continue    
+    continue
   fi
 
   log "building local variables and platform spec for pool ${RESOURCE_POOL}"
   oc get pools.vspherecapacitymanager.splat.io --kubeconfig "${SA_KUBECONFIG}" -n vsphere-infra-helpers "${RESOURCE_POOL}" -o json > /tmp/pool.json
-  VCENTER_AUTH_PATH=$(jq -r '.metadata.annotations["ci-auth-path"]' < /tmp/pool.json)  
-  
+  VCENTER_AUTH_PATH=$(jq -r '.metadata.annotations["ci-auth-path"]' < /tmp/pool.json)
+
   declare vcenter_usernames
   declare vcenter_passwords
-  
+
   # shellcheck source=/dev/null
   source "${VCENTER_AUTH_PATH}"
   account_loc=$((RANDOM % 4))
@@ -324,7 +351,7 @@ for RESOURCE_POOL in ${RESOURCE_POOLS}; do
   vsphere_password="${vcenter_passwords[$account_loc]}"
   pool_usernames[$VCENTER]=${vsphere_user}
   pool_passwords[$VCENTER]=${vsphere_password}
-  
+
   name=$(jq -r '.spec.name' < /tmp/pool.json)
   server=$(jq -r '.spec.server' < /tmp/pool.json)
   region=$(jq -r '.spec.region' < /tmp/pool.json)
@@ -349,8 +376,8 @@ log "building vcenters for platform spec"
 for VCENTER in "${!pool_usernames[@]}"; do
   log "building ${VCENTER} in platform spec"
   declare -A _datacenters
-  for _poolJSON in "${SHARED_DIR}"/POOL*; do 
-    log "processing $_poolJSON"; 
+  for _poolJSON in "${SHARED_DIR}"/POOL*; do
+    log "processing $_poolJSON";
     _VCENTER=$(jq -r .spec.server < "${_poolJSON}")
     _DATACENTER=$(jq -r .spec.topology.datacenter < "${_poolJSON}")
     if [ "${_VCENTER}" = "${VCENTER}" ]; then
@@ -426,18 +453,18 @@ for LEASE in $LEASES; do
 
   echo "$(date -u --rfc-3339=seconds) - Find virtual machines attached to ${vsphere_portgroup} in DC ${vsphere_datacenter} and destroy"
   govc ls -json "${vsphere_portgroup}" |
-  jq '.elements[]?.Object.Vm[]?.Value' |  
+  jq '.elements[]?.Object.Vm[]?.Value' |
   xargs -I {} --no-run-if-empty govc ls -json -L VirtualMachine:{} |
   jq '.elements[].Path | select((contains("ova") or test("\\bci-segment-[0-9]?[0-9]?[0-9]-bastion\\b")) | not)' |
   xargs -I {} --no-run-if-empty govc vm.destroy {}
 done
 set -e
 
-for LEASE in "${SHARED_DIR}"/LEASE*; do      
-  if [[ $LEASE =~ "single" ]]; then 
+for LEASE in "${SHARED_DIR}"/LEASE*; do
+  if [[ $LEASE =~ "single" ]]; then
     continue
   fi
-  
+
   jq -r .status.envVars "${LEASE}" > /tmp/envvars
 
   # shellcheck source=/dev/null
