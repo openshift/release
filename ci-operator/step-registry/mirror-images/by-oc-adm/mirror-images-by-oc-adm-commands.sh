27545327d2e8d54539e6ed924c8fbd48ea239643
Jianlin Liu <jialiu@redhat.com>
2024-06-19 21:43:02 +0800

    support idms for mirror setting (#53337)

--- ci-operator/step-registry/mirror-images/by-oc-adm/mirror-images-by-oc-adm-commands.sh
+++ ci-operator/step-registry/mirror-images/by-oc-adm/mirror-images-by-oc-adm-commands.sh
@@ -11,14 +11,20 @@ export XDG_RUNTIME_DIR="${HOME}/run"
 export REGISTRY_AUTH_PREFERENCE=podman # TODO: remove later, used for migrating oc from docker to podman
 mkdir -p "${XDG_RUNTIME_DIR}"
 
+function run_command() {
+    local CMD="$1"
+    echo "Running command: ${CMD}"
+    eval "${CMD}"
+}
+
 mirror_output="${SHARED_DIR}/mirror_output"
 new_pull_secret="${SHARED_DIR}/new_pull_secret"
-install_config_icsp_patch="${SHARED_DIR}/install-config-icsp.yaml.patch"
-icsp_file="${SHARED_DIR}/local_registry_icsp_file.yaml"
+install_config_mirror_patch="${SHARED_DIR}/install-config-mirror.yaml.patch"
+cluster_mirror_conf_file="${SHARED_DIR}/local_registry_mirror_file.yaml"
 
 # private mirror registry host
 # <public_dns>:<port>
-MIRROR_REGISTRY_HOST=`head -n 1 "${SHARED_DIR}/mirror_registry_url"`
+MIRROR_REGISTRY_HOST=$(head -n 1 "${SHARED_DIR}/mirror_registry_url")
 if [ ! -f "${SHARED_DIR}/mirror_registry_url" ]; then
     echo "File ${SHARED_DIR}/mirror_registry_url does not exist."
     exit 1
@@ -45,8 +51,17 @@ unset KUBECONFIG
 oc registry login
 
 # combine custom registry credential and default pull secret
-registry_cred=`head -n 1 "/var/run/vault/mirror-registry/registry_creds" | base64 -w 0`
+registry_cred=$(head -n 1 "/var/run/vault/mirror-registry/registry_creds" | base64 -w 0)
 jq --argjson a "{\"${MIRROR_REGISTRY_HOST}\": {\"auth\": \"$registry_cred\"}}" '.auths |= . + $a' "${CLUSTER_PROFILE_DIR}/pull-secret" > "${new_pull_secret}"
+
+mirror_crd_type='icsp'
+regex_keyword_1="imageContentSources"
+regex_keyword_2="ImageContentSourcePolicy"
+if [[ "${ENABLE_IDMS}" == "yes" ]]; then
+    mirror_crd_type='idms'
+    regex_keyword_1="imageDigestSources"
+    regex_keyword_2="ImageDigestMirrorSet"
+fi
 
 # set the release mirror args
 args=(
@@ -56,20 +71,30 @@ args=(
     --insecure=true
 )
 
-# check whether the oc command supports the --keep-manifest-list and add it to the args array.
-if ! oc adm release mirror -h | grep -q -- --keep-manifest-list; then
-    echo "This oc version does not support --keep-manifest-list, skip it."
-else
+run_command "which oc"
+run_command "oc version --client"
+
+# check whether the oc command supports extra options and add them to the args array.
+if oc adm release mirror -h | grep -q -- --keep-manifest-list; then
     echo "Adding --keep-manifest-list to the mirror command."
     args+=(--keep-manifest-list=true)
+else
+    echo "This version of oc does not support --keep-manifest-list, skip it."
+fi
+
+if oc adm release mirror -h | grep -q -- --print-mirror-instructions; then
+    echo "Adding --print-mirror-instructions to the mirror command."
+    args+=(--print-mirror-instructions="${mirror_crd_type}")
+else
+    echo "This version of oc does not support --print-mirror-instructions=, skip it."
 fi
 
 # execute the mirror command
-oc adm release -a "${new_pull_secret}" mirror "${args[@]}" | tee "${mirror_output}"
+cmd="oc adm release -a '${new_pull_secret}' mirror ${args[*]} | tee '${mirror_output}'"
+run_command "$cmd"
 
-grep -B 1 -A 10 "kind: ImageContentSourcePolicy" ${mirror_output} > "${icsp_file}"
-grep -A 6 "imageContentSources" ${mirror_output} > "${install_config_icsp_patch}"
+grep -A 6 "${regex_keyword_1}" ${mirror_output} > "${install_config_mirror_patch}"
+grep -B 1 -A 10 "kind: ${regex_keyword_2}" ${mirror_output} > "${cluster_mirror_conf_file}"
 
-echo "${install_config_icsp_patch}:"
-cat "${install_config_icsp_patch}"
+run_command "cat '${install_config_mirror_patch}'"
 rm -f "${new_pull_secret}"
