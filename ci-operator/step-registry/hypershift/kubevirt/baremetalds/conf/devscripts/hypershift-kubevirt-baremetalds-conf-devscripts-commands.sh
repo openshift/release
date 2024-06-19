1303f807e034b3913b635896fdd0229697fbf6c3
Simone Tiraboschi <stirabos@redhat.com>
2024-06-19 16:30:19 +0200

    Try a workaround for OCPBUGS-35818
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- ci-operator/step-registry/hypershift/kubevirt/baremetalds/conf/devscripts/hypershift-kubevirt-baremetalds-conf-devscripts-commands.sh
+++ ci-operator/step-registry/hypershift/kubevirt/baremetalds/conf/devscripts/hypershift-kubevirt-baremetalds-conf-devscripts-commands.sh
@@ -10,3 +10,5 @@ echo "************ baremetalds conf devscripts command ************"
 if [[ -n "${MIRROR_OLM_REMOTE_INDEX:-}" ]]; then
     echo "export MIRROR_OLM_REMOTE_INDEX='${MIRROR_OLM_REMOTE_INDEX}'" | tee -a "${SHARED_DIR}/dev-scripts-additional-config"
 fi
+
+cat "${SHARED_DIR}/dev-scripts-additional-config"
