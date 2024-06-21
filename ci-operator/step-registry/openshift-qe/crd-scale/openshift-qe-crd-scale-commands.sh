8888f9e7c779a791fa419f045fda2e425c6ce11c
Joe Talerico (rook) <joe.talerico@gmail.com>
2024-06-21 14:07:42 -0400

    Enable CRD scale (#53310)
    Adding CRD Scale as a workload, also adding it to the control-plane
    workflow.
    
    Signed-off-by: Joe Talerico <rook@redhat.com>
    Co-authored-by: Joe Talerico <rook@redhat.com>

---
+++ ci-operator/step-registry/openshift-qe/crd-scale/openshift-qe-crd-scale-commands.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+cat /etc/os-release
+oc config view
+oc projects
+python --version
+pushd /tmp
+python -m virtualenv ./venv_qe
+source ./venv_qe/bin/activate
+
+ES_PASSWORD=$(cat "/secret/password")
+ES_USERNAME=$(cat "/secret/username")
+
+REPO_URL="https://github.com/cloud-bulldozer/e2e-benchmarking";
+LATEST_TAG=$(curl -s "https://api.github.com/repos/cloud-bulldozer/e2e-benchmarking/releases/latest" | jq -r '.tag_name');
+TAG_OPTION="--branch $(if [ "$E2E_VERSION" == "default" ]; then echo "$LATEST_TAG"; else echo "$E2E_VERSION"; fi)";
+git clone $REPO_URL $TAG_OPTION --depth 1
+pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
+export WORKLOAD=crd-scale
+
+export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
+export EXTRA_FLAGS=$FLAGS
+export EXTRA_FLAGS+=" --gc-metrics=true --profile-type=${PROFILE_TYPE} --iterations=${ITERATIONS}"
+
+#Debug to remove
+echo $EXTRA_FLAGS
+
+rm -f ${SHARED_DIR}/index.json
+./run.sh
+
+folder_name=$(ls -t -d /tmp/*/ | head -1)
+jq ".iterations = $ITERATIONS" $folder_name/index_data.json >> ${SHARED_DIR}/index_data.json
