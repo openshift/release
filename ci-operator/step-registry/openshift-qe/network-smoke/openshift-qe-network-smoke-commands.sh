3f0e69ca438ee165aa0856cdcf5278193d7fd20a
Miguel Carpio <mcarpio@redhat.com>
2024-06-26 13:34:32 +0200

    Loading proxy for openshift-qe-network-smoke-commands

--- ci-operator/step-registry/openshift-qe/network-smoke/openshift-qe-network-smoke-commands.sh
+++ ci-operator/step-registry/openshift-qe/network-smoke/openshift-qe-network-smoke-commands.sh
@@ -3,6 +3,18 @@ set -o errexit
 set -o nounset
 set -o pipefail
 set -x
+
+# For disconnected or otherwise unreachable environments, we want to
+# have steps use an HTTP(S) proxy to reach the API server. This proxy
+# configuration file should export HTTP_PROXY, HTTPS_PROXY, and NO_PROXY
+# environment variables, as well as their lowercase equivalents (note
+# that libcurl doesn't recognize the uppercase variables).
+if test -f "${SHARED_DIR}/proxy-conf.sh"
+then
+	# shellcheck disable=SC1090
+	source "${SHARED_DIR}/proxy-conf.sh"
+fi
+
 cat /etc/os-release
 oc config view
 oc projects
