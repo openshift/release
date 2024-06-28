49f9af666095464a236c0f8f51b3de5e9d2920ed
Liang Xia <lxia@redhat.com>
2024-06-28 10:52:33 +0800

    OCPQE-23276: handle junit results robustly

--- ci-operator/step-registry/openshift-extended/logging-test/web-tests/openshift-extended-logging-test-web-tests-commands.sh
+++ ci-operator/step-registry/openshift-extended/logging-test/web-tests/openshift-extended-logging-test-web-tests-commands.sh
@@ -39,23 +39,28 @@ fi
 
 # summarize test results
 echo "Summarizing test results..."
-failures=0 errors=0 skipped=0 tests=0
-grep -r -E -h -o 'testsuite[^>]+' "${ARTIFACT_DIR}/gui_test_screenshots/console-cypress.xml" 2>/dev/null | tr -d '[A-Za-z="_]' > /tmp/zzz-tmp.log
-while read -a row ; do
-    # if the last ARG of command `let` evaluates to 0, `let` returns 1
-    let errors+=${row[0]} failures+=${row[1]} skipped+=${row[2]} tests+=${row[3]} || true
+[[ -e "${ARTIFACT_DIR}" ]] || exit 0
+declare -A results=([failures]='0' [errors]='0' [skipped]='0' [tests]='0')
+grep -r -E -h -o 'testsuite.*tests="[0-9]+"[^>]+' "${ARTIFACT_DIR}/gui_test_screenshots/console-cypress.xml" 2>/dev/null > /tmp/zzz-tmp.log
+while read row ; do
+    for ctype in "${!results[@]}" ; do
+        count="$(sed -E "s/.*$ctype=\"([0-9]+)\".*/\1/" <<< $row)"
+        if [[ -n $count ]] ; then
+            let results[$ctype]+=count || true
+        fi
+    done
 done < /tmp/zzz-tmp.log
 
 TEST_RESULT_FILE="${ARTIFACT_DIR}/test-results.yaml"
 cat > "${TEST_RESULT_FILE}" <<- EOF
 openshift-extended-logging-test-web-tests:
-  total: $tests
-  failures: $failures
-  errors: $errors
-  skipped: $skipped
+  total: ${results[tests]}
+  failures: ${results[failures]}
+  errors: ${results[errors]}
+  skipped: ${results[skipped]}
 EOF
 
-if [ $((failures)) != 0 ] ; then
+if [ ${results[failures]} != 0 ] ; then
     echo '  failingScenarios:' >> "${TEST_RESULT_FILE}"
     readarray -t failingscenarios < <(find "${ARTIFACT_DIR}" -name 'cypress_report*.json' -exec yq '.results[].suites[].tests[] | select(.fail == true) | .fullTitle' {} \; | sort --unique)
     for (( i=0; i<${#failingscenarios[@]}; i++ )) ; do
