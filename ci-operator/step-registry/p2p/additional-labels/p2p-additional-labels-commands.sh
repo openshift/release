b11932640e2ae60723b0ff1aa58bf532ed579e4a
Caleb Evans <caevans@redhat.com>
2024-07-08 08:30:43 -0600

    Verify the operator versions file exists and the cluster_version variable is populated

--- ci-operator/step-registry/p2p/additional-labels/p2p-additional-labels-commands.sh
+++ ci-operator/step-registry/p2p/additional-labels/p2p-additional-labels-commands.sh
@@ -8,8 +8,12 @@ set -o pipefail
 eval "$(cat "${SHARED_DIR}/api.login")"
 
 # Populate operator version labels
-cat "${SHARED_DIR}/operator-versions" >> "${SHARED_DIR}/firewatch-additional-labels"
+if [ -f "${SHARED_DIR}/operator-versions" ]; then
+    cat "${SHARED_DIR}/operator-versions" >> "${SHARED_DIR}/firewatch-additional-labels"
+fi
 
 # Populate cluster version label
 cluster_version=$(oc get clusterversion -o jsonpath='{.items[0].status.desired.version}')
-echo "ocp-v${cluster_version}" >> "${SHARED_DIR}/firewatch-additional-labels"
+if [ -z "$cluster_version" ]; then
+    echo "ocp-v${cluster_version}" >> "${SHARED_DIR}/firewatch-additional-labels"
+fi
