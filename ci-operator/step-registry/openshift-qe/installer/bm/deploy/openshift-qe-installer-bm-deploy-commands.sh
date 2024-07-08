04508af18859dc753ed981d1f3db4c494a96e89b
Jose Castillo Lema <josecastillolema@gmail.com>
2024-07-08 11:43:56 +0200

    Fix bash warnings
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/deploy/openshift-qe-installer-bm-deploy-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/deploy/openshift-qe-installer-bm-deploy-commands.sh
@@ -51,16 +51,16 @@ envsubst < /tmp/all.yml > /tmp/all-updated.yml
 sshpass -p "$(cat /secret/login)" scp -q -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null /tmp/all-updated.yml root@${bastion}:~/jetlag/ansible/vars/all.yml
 sshpass -p "$(cat /secret/login)" scp -q -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null /secret/inventory root@${bastion}:~/jetlag/ansible/inventory/telco.inv
 
-export run_in_bastion='sshpass -p "$(cat /secret/login)" ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@${bastion}'
+run_in_bastion='sshpass -p "$(cat /secret/login)" ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@${bastion}'
 
 # Clean up previous attempts
-$run_in_bastion ./clean-resources.sh
+"${run_in_bastion[@]}" ./clean-resources.sh
 
 # Setup Bastion
-$run_in_bastion "
+"${run_in_bastion[@]}" "
    cd jetlag
-   if [[ -n "$JETLAG_PR" ]]; then
-     git fetch origin pull/"$JETLAG_PR"/head:dev
+   if [[ -n $JETLAG_PR ]]; then
+     git fetch origin pull/$JETLAG_PR/head:dev
      git switch dev
    else
      git switch main
@@ -69,7 +69,7 @@ $run_in_bastion "
    ansible-playbook -i ansible/inventory/telco.inv ansible/setup-bastion.yml | tee /tmp/ansible-setup-bastion-$(date +%s)"
 
 # Attempt Deployment
-$run_in_bastion "
+"${run_in_bastion[@]}" "
    cd jetlag
    source bootstrap.sh
    ansible-playbook -i ansible/inventory/telco.inv ansible/bm-deploy.yml -v | tee /tmp/ansible-setup-bastion-$(date +%s)"
