36a4786ab406ea5dba72e147f72d9cb95f32c058
Vivek Shakya <vshakya@vshakya-thinkpadt14gen4.remote.csb>
2024-07-09 07:01:29 -0400

    Web Terminal operator ROSA HCP Onboaring

---
+++ ci-operator/step-registry/web-terminal-operator/lp-rosa-tests/web-terminal-operator-lp-rosa-tests-commands.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+#set path for oc binary
+export PATH=$PATH:/workdir/ 
+
+CONSOLE_URL=$(cat $SHARED_DIR/console.url)
+export CONSOLE_URL
+OCP_API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
+export OCP_API_URL
+
+
+
+# login for interop
+if test -f ${SHARED_DIR}/kubeadmin-password
+then
+  OCP_CRED_USR="kubeadmin"
+  OCP_CRED_PSW="$(cat ${SHARED_DIR}/kubeadmin-password)"
+  oc login ${OCP_API_URL} --username=${OCP_CRED_USR} --password=${OCP_CRED_PSW} --insecure-skip-tls-verify=true
+else #login for ROSA & Hypershift platforms
+  eval "$(cat "${SHARED_DIR}/api.login")"
+fi
+
+cd /workdir
+
+sleep 3600
