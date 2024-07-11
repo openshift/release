1e644f159f37c28049bdb9eaf34ac92807161428
Liangquan Li <liangli@redhat.com>
2024-07-12 00:11:50 +0800

    HOSTEDCP-1827: Remove temporary repo from step: hypershift-agent-create-add-worker-metal3

--- ci-operator/step-registry/hypershift/agent/create/add-worker-metal3/hypershift-agent-create-add-worker-metal3-commands.sh
+++ ci-operator/step-registry/hypershift/agent/create/add-worker-metal3/hypershift-agent-create-add-worker-metal3-commands.sh
@@ -7,38 +7,87 @@ set -o pipefail
 echo "************ add-worker metal3 command ************"
 
 source "${SHARED_DIR}/packet-conf.sh"
-tar -czf - . | ssh "${SSHOPTS[@]}" "root@${IP}" "cat > /root/assisted-service.tar.gz"
 
-# shellcheck disable=SC2087
-ssh "${SSHOPTS[@]}" "root@${IP}" bash - << EOF |& sed -e 's/.*auths\{0,1\}".*/*** PULL_SECRET ***/g'
+CLUSTER_NAME="$(echo -n $PROW_JOB_ID|sha256sum|cut -c-20)"
+HOSTED_CLUSTER_NS=$(oc get hostedcluster -A -o=jsonpath="{.items[?(@.metadata.name=='$CLUSTER_NAME')].metadata.namespace}")
+HOSTED_CONTROL_PLANE_NAMESPACE=${HOSTED_CLUSTER_NS}"-"${CLUSTER_NAME}
 
+ssh "${SSHOPTS[@]}" "root@${IP}" bash -s -- "$CLUSTER_NAME" "$HOSTED_CONTROL_PLANE_NAMESPACE" "$EXTRA_BAREMETALHOSTS_FILE" << 'EOF' |& sed -e 's/.*auths\{0,1\}".*/*** PULL_SECRET ***/g'
 # prepending each printed line with a timestamp
-exec > >(awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), \$0 }') 2>&1
-
+exec > >(awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }') 2>&1
 set -xeo pipefail
-free -h
+CLUSTER_NAME="${1}"
+HOSTED_CONTROL_PLANE_NAMESPACE="${2}"
+EXTRA_BAREMETALHOSTS_FILE="${3}"
 
-cd /root/dev-scripts
-source common.sh
-source utils.sh
-source network.sh
+SSH_PUB_KEY=$(cat /root/.ssh/id_rsa.pub)
+cat <<END | oc apply -f -
+apiVersion: agent-install.openshift.io/v1beta1
+kind: InfraEnv
+metadata:
+  name: ${CLUSTER_NAME}
+  namespace: ${HOSTED_CONTROL_PLANE_NAMESPACE}
+spec:
+  cpuArchitecture: x86_64
+  pullSecretRef:
+    name: pull-secret
+  sshAuthorizedKey: ${SSH_PUB_KEY}
+END
 
-REPO_DIR="/home/assisted-service"
-if [ ! -d "\${REPO_DIR}" ]; then
-  mkdir -p "\${REPO_DIR}"
+while IFS= read -r host; do
+    host_name=$(echo "$host" | jq -r '.name')
+    driver_info_username=$(echo "$host" | jq -r '.driver_info.username' | base64)
+    driver_info_password=$(echo "$host" | jq -r '.driver_info.password' | base64)
+    driver_info_address=$(echo "$host" | jq -r '.driver_info.address')
+    bootMACAddress=$(echo "$host" | jq -r '.ports[0].address')
+    cat <<END | oc apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: '${host_name}-bmc-secret'
+  namespace: '${HOSTED_CONTROL_PLANE_NAMESPACE}'
+type: Opaque
+data:
+  username: '${driver_info_username}'
+  password: '${driver_info_password}'
+---
+apiVersion: metal3.io/v1alpha1
+kind: BareMetalHost
+metadata:
+  name: '${host_name}'
+  namespace: '${HOSTED_CONTROL_PLANE_NAMESPACE}'
+  labels:
+    infraenvs.agent-install.openshift.io: '${CLUSTER_NAME}'
+  annotations:
+    bmac.agent-install.openshift.io/hostname: '${host_name}'
+spec:
+  online: true
+  bootMACAddress: '${bootMACAddress}'
+  bmc:
+    address: '${driver_info_address}'
+    credentialsName: '${host_name}-bmc-secret'
+END
+done < <(jq -c '.[]' ${EXTRA_BAREMETALHOSTS_FILE})
 
-  echo "### Untar assisted-service code..."
-  tar -xzvf /root/assisted-service.tar.gz -C "\${REPO_DIR}"
-fi
+source ~/dev-scripts-additional-config
+set +e
+while true; do
+    count=$(oc get agent -n ${HOSTED_CONTROL_PLANE_NAMESPACE} --no-headers --ignore-not-found | wc -l)
+    if [ ${count} == ${NUM_EXTRA_WORKERS} ] ; then
+        echo "agent resources already exist"
+        break
+    fi
+    echo "Waiting on agent resources create"
+    sleep 60
+done
+set -e
+oc wait --all=true agent -n ${HOSTED_CONTROL_PLANE_NAMESPACE}  --for=condition=RequirementsMet
 
-cd "\${REPO_DIR}/deploy/operator/"
-git clone https://github.com/LiangquanLi930/deployhypershift.git
-cd "deployhypershift"
-
-echo "export EXTRA_BAREMETALHOSTS_FILE=/root/dev-scripts/\${EXTRA_BAREMETALHOSTS_FILE}" >> /root/config
-source /root/config
-
-./deploy_hypershift.sh
-
-set -x
+echo "scale nodepool replicas => $NUM_EXTRA_WORKERS"
+oc scale nodepool ${CLUSTER_NAME} -n $(oc get hostedcluster -A -o=jsonpath="{.items[?(@.metadata.name=='$CLUSTER_NAME')].metadata.namespace}") --replicas ${NUM_EXTRA_WORKERS}
+echo "wait agent ready"
+oc wait --all=true agent -n ${HOSTED_CONTROL_PLANE_NAMESPACE} --for=jsonpath='{.status.debugInfo.state}'=added-to-existing-cluster --timeout=30m
 EOF
+
+oc get InfraEnv -n ${HOSTED_CONTROL_PLANE_NAMESPACE} ${CLUSTER_NAME} -o yaml > "${ARTIFACT_DIR}/InfraEnv.yaml"
+oc get BareMetalHost -n ${HOSTED_CONTROL_PLANE_NAMESPACE} -o yaml > "${ARTIFACT_DIR}/extra_baremetalhosts.yaml"
