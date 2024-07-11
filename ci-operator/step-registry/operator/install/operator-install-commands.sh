a9785c402a26c35bf086c7d32e12db1495a3fe88
Caleb Evans <caevans@redhat.com>
2024-07-11 07:22:33 -0600

    Fix operator install

--- ci-operator/step-registry/operator/install/operator-install-commands.sh
+++ ci-operator/step-registry/operator/install/operator-install-commands.sh
@@ -54,8 +54,8 @@ ${RUN_COMMAND}
 
 for operator_value in $operator_configs; do
     operator_value=$(extract_operator_config "$operator_value")
-    if [ "${operator_config}" ]; then
-        name=$(echo $operator_config | sed -E 's/.*name=([^;]+);.*/\1/')
+    if [ "${operator_value}" ]; then
+        name=$(echo $operator_value | sed -E 's/.*name=([^;]+);.*/\1/')
         version=$(oc get csv -o json | jq -r --arg NAME_VALUE "$name" '.items[] | select(.metadata.name | contains($NAME_VALUE)) | .spec.version')
         echo "$name-v$version" >> "${SHARED_DIR}/operator-versions"
     fi
