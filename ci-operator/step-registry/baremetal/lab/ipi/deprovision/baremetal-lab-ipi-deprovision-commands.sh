f53181634702639136f2982e51f23dbd34a32ca1
Carlos Cardenosa <ccardeno@redhat.com>
2024-07-11 17:46:06 +0200

    Typos

--- ci-operator/step-registry/baremetal/lab/ipi/deprovision/baremetal-lab-ipi-deprovision-commands.sh
+++ ci-operator/step-registry/baremetal/lab/ipi/deprovision/baremetal-lab-ipi-deprovision-commands.sh
@@ -14,8 +14,9 @@ SSHOPTS=(-o 'ConnectTimeout=5'
 [ -z "${PULL_NUMBER:-}" ] && \
   timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" \
     test -f /var/builds/${NAMESPACE}/preserve && \
-  { echo "The cluster is expected to persist. Skipping deprovisioning..."; exit 0 }
-echo "No request to let the cluster persist detected. Deprovisioning...
+    { echo "The cluster is expected to persist. Skipping deprovisioning..."; exit 0; }
+echo "No request to let the cluster persist detected. Deprovisioning..."
+
 [ -z "${architecture}" ] && { echo "\$architecture is not filled. Failing."; exit 1; }
 
 echo "[INFO] Look for a bootstrap VM in the provisioning host and destroy it..."
