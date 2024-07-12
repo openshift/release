48788338ee27d7bd6b22b12532dd5383633f3c37
Paige Rubendall <prubenda@redhat.com>
2024-07-12 12:48:39 -0400

    adding return codes
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED

--- ci-operator/step-registry/redhat-chaos/zone-outage/redhat-chaos-zone-outage-commands.sh
+++ ci-operator/step-registry/redhat-chaos/zone-outage/redhat-chaos-zone-outage-commands.sh
@@ -26,18 +26,25 @@ ES_USERNAME=$(cat "/secret/es/username")
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
 export ELASTIC_INDEX=krkn_chaos_ci
 
-# read passwords from vault
+# # read passwords from vault
 telemetry_password=$(cat "/secret/telemetry/telemetry_password")
 
 # set the secrets from the vault as env vars
 export TELEMETRY_PASSWORD=$telemetry_password
-export AWS_DEFAULT_REGION=us-west-2
+export AWS_DEFAULT_REGION="${LEASED_RESOURCE}"
 
 NODE_NAME=$(oc get nodes --no-headers | head -n 1 | awk '{print $1}')
+rc=$?
+echo "Node name return code: $rc"
 
 VPC_ID=$(aws ec2 describe-instances --filter Name=private-dns-name,Values=$NODE_NAME  --query 'Reservations[*].Instances[*].NetworkInterfaces[*].VpcId' --output text)
+rc=$?
+echo "VPC return code: $rc"
 export VPC_ID
+
 SUBNET_ID=$(aws ec2 describe-subnets --filter Name=vpc-id,Values=$VPC_ID --query 'Subnets[*].SubnetId' --max-items 2) 
+rc=$?
+echo "Subnet return code: $rc"
 export SUBNET_ID
 
 ./zone-outages/prow_run.sh
