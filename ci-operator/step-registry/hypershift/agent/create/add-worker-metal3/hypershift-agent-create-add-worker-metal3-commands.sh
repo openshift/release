55b1ea175155960e6d184bbeda3965274917078b
Liangquan Li <liangli@redhat.com>
2024-07-16 20:47:50 +0800

    Change Agent namespace, not HOSTED_CONTROL_PLANE_NAMESPACE (#54396)

--- ci-operator/step-registry/hypershift/agent/create/add-worker-metal3/hypershift-agent-create-add-worker-metal3-commands.sh
+++ ci-operator/step-registry/hypershift/agent/create/add-worker-metal3/hypershift-agent-create-add-worker-metal3-commands.sh
@@ -10,15 +10,23 @@ source "${SHARED_DIR}/packet-conf.sh"
 
 CLUSTER_NAME="$(echo -n $PROW_JOB_ID|sha256sum|cut -c-20)"
 HOSTED_CLUSTER_NS=$(oc get hostedcluster -A -o=jsonpath="{.items[?(@.metadata.name=='$CLUSTER_NAME')].metadata.namespace}")
-HOSTED_CONTROL_PLANE_NAMESPACE=${HOSTED_CLUSTER_NS}"-"${CLUSTER_NAME}
+if [[ -z ${AGENT_NAMESPACE} ]] ; then
+  AGENT_NAMESPACE=${HOSTED_CLUSTER_NS}"-"${CLUSTER_NAME}
+fi
 
-ssh "${SSHOPTS[@]}" "root@${IP}" bash -s -- "$CLUSTER_NAME" "$HOSTED_CONTROL_PLANE_NAMESPACE" "$EXTRA_BAREMETALHOSTS_FILE" << 'EOF' |& sed -e 's/.*auths\{0,1\}".*/*** PULL_SECRET ***/g'
+ssh "${SSHOPTS[@]}" "root@${IP}" bash -s -- "$CLUSTER_NAME" "$AGENT_NAMESPACE" "$EXTRA_BAREMETALHOSTS_FILE" << 'EOF' |& sed -e 's/.*auths\{0,1\}".*/*** PULL_SECRET ***/g'
 # prepending each printed line with a timestamp
 exec > >(awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }') 2>&1
 set -xeo pipefail
 CLUSTER_NAME="${1}"
-HOSTED_CONTROL_PLANE_NAMESPACE="${2}"
+AGENT_NAMESPACE="${2}"
 EXTRA_BAREMETALHOSTS_FILE="${3}"
+
+oc get ns "${AGENT_NAMESPACE}" || oc create namespace "${AGENT_NAMESPACE}"
+if ! oc get secret pull-secret -n "${AGENT_NAMESPACE}"; then
+    oc extract secret/pull-secret -n openshift-config --to=/tmp --confirm
+    oc create secret generic pull-secret --from-file=.dockerconfigjson=/tmp/.dockerconfigjson --type=kubernetes.io/dockerconfigjson -n "${AGENT_NAMESPACE}"
+fi
 
 SSH_PUB_KEY=$(cat /root/.ssh/id_rsa.pub)
 cat <<END | oc apply -f -
@@ -26,12 +34,12 @@ apiVersion: agent-install.openshift.io/v1beta1
 kind: InfraEnv
 metadata:
   name: ${CLUSTER_NAME}
-  namespace: ${HOSTED_CONTROL_PLANE_NAMESPACE}
+  namespace: ${AGENT_NAMESPACE}
 spec:
   cpuArchitecture: x86_64
   pullSecretRef:
     name: pull-secret
-  sshAuthorizedKey: ${SSH_PUB_KEY}
+  sshAuthorizedKey: "${SSH_PUB_KEY}"
 END
 
 while IFS= read -r host; do
@@ -45,7 +53,7 @@ apiVersion: v1
 kind: Secret
 metadata:
   name: '${host_name}-bmc-secret'
-  namespace: '${HOSTED_CONTROL_PLANE_NAMESPACE}'
+  namespace: '${AGENT_NAMESPACE}'
 type: Opaque
 data:
   username: '${driver_info_username}'
@@ -55,7 +63,7 @@ apiVersion: metal3.io/v1alpha1
 kind: BareMetalHost
 metadata:
   name: '${host_name}'
-  namespace: '${HOSTED_CONTROL_PLANE_NAMESPACE}'
+  namespace: '${AGENT_NAMESPACE}'
   labels:
     infraenvs.agent-install.openshift.io: '${CLUSTER_NAME}'
   annotations:
@@ -72,7 +80,7 @@ done < <(jq -c '.[]' ${EXTRA_BAREMETALHOSTS_FILE})
 source ~/dev-scripts-additional-config
 set +e
 while true; do
-    count=$(oc get agent -n ${HOSTED_CONTROL_PLANE_NAMESPACE} --no-headers --ignore-not-found | wc -l)
+    count=$(oc get agent -n ${AGENT_NAMESPACE} --no-headers --ignore-not-found | wc -l)
     if [ ${count} == ${NUM_EXTRA_WORKERS} ] ; then
         echo "agent resources already exist"
         break
@@ -81,13 +89,13 @@ while true; do
     sleep 60
 done
 set -e
-oc wait --all=true agent -n ${HOSTED_CONTROL_PLANE_NAMESPACE}  --for=condition=RequirementsMet
+oc wait --all=true agent -n ${AGENT_NAMESPACE}  --for=condition=RequirementsMet
 
 echo "scale nodepool replicas => $NUM_EXTRA_WORKERS"
 oc scale nodepool ${CLUSTER_NAME} -n $(oc get hostedcluster -A -o=jsonpath="{.items[?(@.metadata.name=='$CLUSTER_NAME')].metadata.namespace}") --replicas ${NUM_EXTRA_WORKERS}
 echo "wait agent ready"
-oc wait --all=true agent -n ${HOSTED_CONTROL_PLANE_NAMESPACE} --for=jsonpath='{.status.debugInfo.state}'=added-to-existing-cluster --timeout=30m
+oc wait --all=true agent -n ${AGENT_NAMESPACE} --for=jsonpath='{.status.debugInfo.state}'=added-to-existing-cluster --timeout=30m
 EOF
 
-oc get InfraEnv -n ${HOSTED_CONTROL_PLANE_NAMESPACE} ${CLUSTER_NAME} -o yaml > "${ARTIFACT_DIR}/InfraEnv.yaml"
-oc get BareMetalHost -n ${HOSTED_CONTROL_PLANE_NAMESPACE} -o yaml > "${ARTIFACT_DIR}/extra_baremetalhosts.yaml"
+oc get InfraEnv -n ${AGENT_NAMESPACE} ${CLUSTER_NAME} -o yaml > "${ARTIFACT_DIR}/InfraEnv.yaml"
+oc get BareMetalHost -n ${AGENT_NAMESPACE} -o yaml > "${ARTIFACT_DIR}/extra_baremetalhosts.yaml"
