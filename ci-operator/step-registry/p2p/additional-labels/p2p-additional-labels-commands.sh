45a55c856de7346575813d590db3a05f46fbb345
Caleb Evans <caevans@redhat.com>
2024-07-16 09:43:41 -0600

    Update p2p-additional-labels to add operator versions (#54418)

--- ci-operator/step-registry/p2p/additional-labels/p2p-additional-labels-commands.sh
+++ ci-operator/step-registry/p2p/additional-labels/p2p-additional-labels-commands.sh
@@ -4,13 +4,24 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+operator_configs=$(env | grep -E '^OPERATOR[0-9]+_CONFIG' | sort --version-sort)
+
 # Login to cluster
 eval "$(cat "${SHARED_DIR}/api.login")"
 
 # Populate operator version labels
-if [ -f "${SHARED_DIR}/operator-versions" ]; then
-    cat "${SHARED_DIR}/operator-versions" >> "${SHARED_DIR}/firewatch-additional-labels"
-fi
+extract_operator_config() {
+    echo "$1" | sed -E 's/^OPERATOR[0-9]+_CONFIG=//'
+}
+
+for operator_value in $operator_configs; do
+    operator_value=$(extract_operator_config "$operator_value")
+    if [ "${operator_value}" ]; then
+        name=$(echo $operator_value | sed -E 's/.*name=([^;]+);.*/\1/')
+        version=$(oc get csv -o json | jq -r --arg NAME_VALUE "$name" '.items[] | select(.metadata.name | contains($NAME_VALUE)) | .spec.version')
+        echo "$name-v$version" >> "${SHARED_DIR}/firewatch-additional-labels"
+    fi
+done
 
 # Populate cluster version label
 cluster_version=$(oc get clusterversion -o jsonpath='{.items[0].status.desired.version}')
