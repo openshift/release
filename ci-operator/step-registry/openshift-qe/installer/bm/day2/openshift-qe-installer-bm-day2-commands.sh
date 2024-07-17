65c5e9d73c030d0674071509d0252741998fe901
Raúl Sevilla <rsevilla@redhat.com>
2024-07-17 16:19:42 +0200

    [PERFSCALE-3249] Ingress configuration for scalelab job (#54260)
    * Ingress configuration for scalelab job
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    * Update e2e-benchmarking version
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    * Improve infra nodes labeling logic
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    ---------
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/day2/openshift-qe-installer-bm-day2-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/day2/openshift-qe-installer-bm-day2-commands.sh
@@ -22,10 +22,12 @@ oc projects
 
 # Create infra-nodes for ingress-perf testing
 if [ ${INFRA} == "true" ]; then
-  for node in `oc get nodes -l node-role.kubernetes.io/worker= --no-headers | head -2 | awk '{print $1}'`; do
-    oc label node $node node-role.kubernetes.io/infra=""
-    oc label node $node node-role.kubernetes.io/worker-;
-  done
+  if [[ $(oc get nodes -l node-role.kubernetes.io/infra= --no-headers | wc -l) != 2 ]]; then
+    for node in `oc get nodes -l node-role.kubernetes.io/worker= --no-headers | head -2 | awk '{print $1}'`; do
+      oc label node $node node-role.kubernetes.io/infra=""
+      oc label node $node node-role.kubernetes.io/worker-;
+    done
+  fi
 fi
 
 if [ ${TELCO} == "true" ]; then
