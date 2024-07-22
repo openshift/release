c5cf20334808b1df91c369394c3ec3359bb7fcd1
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2024-07-22 13:48:27 -0400

    execute platform external with e2e tests on a weekly basis

--- ci-operator/step-registry/upi/deprovision/vsphere/external/diags/upi-deprovision-vsphere-external-diags-commands.sh
+++ ci-operator/step-registry/upi/deprovision/vsphere/external/diags/upi-deprovision-vsphere-external-diags-commands.sh
@@ -13,7 +13,15 @@ fi
 NAMESPACES="vsphere-cloud-controller-manager"
 
 echo "Running oc adm inspect..."
+
+echo "Creating inspect artifact directory"
 mkdir -p ${ARTIFACT_DIR}/inspect
-oc --insecure-skip-tls-verify adm inspect namespace ${NAMESPACES} --dest-dir ${ARTIFACT_DIR}/inspect > ${ARTIFACT_DIR}/inpsect/inspect.log
+ls -lt
+echo "Running oc adm inspect"
+oc --insecure-skip-tls-verify adm inspect namespace ${NAMESPACES} --dest-dir ${ARTIFACT_DIR}/inspect > ${ARTIFACT_DIR}/inspect/inspect.log
+
+echo "tarring artifacts"
 tar -czC "${ARTIFACT_DIR}/inspect" -f "${ARTIFACT_DIR}/inspect.tar.gz" .
+
+echo "cleaning up"
 rm -rf "${ARTIFACT_DIR}"/inspect
