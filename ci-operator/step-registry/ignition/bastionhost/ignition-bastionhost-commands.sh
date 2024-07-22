fee7ad1b3df206928e0a7d4da1a5de9b057661ef
sachinninganure <sninganu@redhat.com>
2024-07-22 03:18:21 -0400

    fix2

--- ci-operator/step-registry/ignition/bastionhost/ignition-bastionhost-commands.sh
+++ ci-operator/step-registry/ignition/bastionhost/ignition-bastionhost-commands.sh
@@ -463,6 +463,7 @@ echo "Ignition file '${SHARED_DIR}/${CLUSTER_NAME}-bastion.ign' created"
 
 rm -rf "${workdir}"
 curl -L https://github.com/liqcui/wondershaper/archive/refs/tags/v1.tar.gz>wondershap.tar.gz
+chmod 755 wondershap.tar.gz
 tar -xvf wondershap.tar.gz
 #git clone https://github.com/SachinNinganure/wondershaper
 #cd wondershaper
