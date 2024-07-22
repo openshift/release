4409c7d8bd769c00ca14e6719ce3e95d24d1bfe6
Qiong Wang <qiowang@redhat.com>
2024-07-22 21:35:52 +0800

    add ignore iface manifest

--- ci-operator/step-registry/baremetal/lab/ipi/conf/nmstate/baremetal-lab-ipi-conf-nmstate-commands.sh
+++ ci-operator/step-registry/baremetal/lab/ipi/conf/nmstate/baremetal-lab-ipi-conf-nmstate-commands.sh
@@ -6,7 +6,7 @@ set -o pipefail
 set -o nounset
 
 echo "Creating manifests for br-ex configuration on masters and workers"
-
+##### debug on
 MASTER_NM_CONF="${SHARED_DIR}/manifest_nm-conf-master.yaml"
 cat > "${MASTER_NM_CONF}" <<EOF
 apiVersion: machineconfiguration.openshift.io/v1
@@ -27,11 +27,13 @@ spec:
         overwrite: true
         path: /etc/NetworkManager/conf.d/debug.conf
 EOF
+##### debug off
+MASTER_BR_MANIFEST="${SHARED_DIR}/manifest_nmstate-br-ex-master.yaml"
+WORKER_BR_MANIFEST="${SHARED_DIR}/manifest_nmstate-br-ex-worker.yaml"
+MASTER_IGNORE_MANIFEST="${SHARED_DIR}/manifest_ignore-iface-master.yaml"
+WORKER_IGNORE_MANIFEST="${SHARED_DIR}/manifest_ignore-iface-worker.yaml"
 
-MASTER_MANIFEST="${SHARED_DIR}/manifest_nmstate-br-ex-master.yaml"
-WORKER_MANIFEST="${SHARED_DIR}/manifest_nmstate-br-ex-worker.yaml"
-
-cat > "${MASTER_MANIFEST}" <<EOF
+cat > "${MASTER_BR_MANIFEST}" <<EOF
 apiVersion: machineconfiguration.openshift.io/v1
 kind: MachineConfig
 metadata:
@@ -46,13 +48,43 @@ spec:
       files:
 EOF
 
-cat > "${WORKER_MANIFEST}" <<EOF
+cat > "${WORKER_BR_MANIFEST}" <<EOF
 apiVersion: machineconfiguration.openshift.io/v1
 kind: MachineConfig
 metadata:
   labels:
     machineconfiguration.openshift.io/role: worker
   name: 10-br-ex-worker
+spec:
+  config:
+    ignition:
+      version: 3.4.0
+    storage:
+      files:
+EOF
+
+cat > "${MASTER_IGNORE_MANIFEST}" <<EOF
+apiVersion: machineconfiguration.openshift.io/v1
+kind: MachineConfig
+metadata:
+  labels:
+    machineconfiguration.openshift.io/role: master
+  name: 10-ignore-iface-master
+spec:
+  config:
+    ignition:
+      version: 3.4.0
+    storage:
+      files:
+EOF
+
+cat > "${WORKER_IGNORE_MANIFEST}" <<EOF
+apiVersion: machineconfiguration.openshift.io/v1
+kind: MachineConfig
+metadata:
+  labels:
+    machineconfiguration.openshift.io/role: worker
+  name: 10-ignore-iface-worker
 spec:
   config:
     ignition:
@@ -93,35 +125,58 @@ for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
     copy-mac-from: ${baremetal_iface}
     ipv4:
       enabled: true
-      dhcp: false
-      address:
-      - ip: ${ip}
-        prefix-length: 22
+      dhcp: true
     ipv6:
       enabled: false
       dhcp: false"
-  contents_source="$(echo "${br_ex_configuration}" | base64 -w0)"
+  br_ex_contents_source="$(echo "${br_ex_configuration}" | base64 -w0)"
+
+  ignore_iface_configuration="
+  [device-${baremetal_iface}]
+  match-device=interface-name:${baremetal_iface}
+  keep-configuration=no"
+  ignore_iface_configuration="$(echo "${ignore_iface_configuration}" | base64 -w0)"
+
   if [[ "$name" =~ master* ]]; then
-    cat >> "${MASTER_MANIFEST}" <<EOF
+    cat >> "${MASTER_BR_MANIFEST}" <<EOF
       - contents:
-          source: data:text/plain;charset=utf-8;base64,${contents_source}
+          source: data:text/plain;charset=utf-8;base64,${br_ex_contents_source}
         mode: 0644
         overwrite: true
         path: /etc/nmstate/openshift/${name}.yml
 EOF
-  fi
-  if [[ "$name" =~ worker* ]]; then
-    cat >> "${WORKER_MANIFEST}" <<EOF
+    cat >> "${MASTER_IGNORE_MANIFEST}" <<EOF
       - contents:
-          source: data:text/plain;charset=utf-8;base64,${contents_source}
+          source: data:text/plain;charset=utf-8;base64,${ignore_iface_configuration}
+        mode: 0644
+        overwrite: true
+        path: /etc/NetworkManager/conf.d/10-ignore-${baremetal_iface}.conf
+EOF
+  fi
+
+  if [[ "$name" =~ worker* ]]; then
+    cat >> "${WORKER_BR_MANIFEST}" <<EOF
+      - contents:
+          source: data:text/plain;charset=utf-8;base64,${br_ex_contents_source}
         mode: 0644
         overwrite: true
         path: /etc/nmstate/openshift/${name}.yml
+EOF
+    cat >> "${WORKER_IGNORE_MANIFEST}" <<EOF
+      - contents:
+          source: data:text/plain;charset=utf-8;base64,${ignore_iface_configuration}
+        mode: 0644
+        overwrite: true
+        path: /etc/NetworkManager/conf.d/10-ignore-${baremetal_iface}.conf
 EOF
   fi
 done
 
 echo "manifests for br-ex configuration on masters"
-cat "${MASTER_MANIFEST}"
+cat "${MASTER_BR_MANIFEST}"
 echo "manifests for br-ex configuration on workers"
-cat "${WORKER_MANIFEST}"
+cat "${WORKER_BR_MANIFEST}"
+echo "manifests for ignore iface on masters"
+cat "${MASTER_IGNORE_MANIFEST}"
+echo "manifests for ignore iface on workers"
+cat "${WORKER_IGNORE_MANIFEST}"
