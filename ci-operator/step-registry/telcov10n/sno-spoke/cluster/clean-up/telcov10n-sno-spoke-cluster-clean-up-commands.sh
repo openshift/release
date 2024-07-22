209fcde738e9e827ff5ac411e82b384c60442fd7
Carlos Cardenosa <ccardeno@redhat.com>
2024-07-22 21:28:38 +0200

    Wait until the PODs clean up is done

--- ci-operator/step-registry/telcov10n/sno-spoke/cluster/clean-up/telcov10n-sno-spoke-cluster-clean-up-commands.sh
+++ ci-operator/step-registry/telcov10n/sno-spoke/cluster/clean-up/telcov10n-sno-spoke-cluster-clean-up-commands.sh
@@ -14,9 +14,12 @@ function set_hub_cluster_kubeconfig {
 
 function clean_up {
 
-  echo "************ telcov10n Clean up Gitea namespace and AgentServiceConfig CR ************"
+  echo "************ telcov10n Clean up AgentServiceConfig CR ************"
   set -x
   oc delete AgentServiceConfig || echo "The CR didn't exist..."
+  assisted_service_pod_name=$(oc -n multicluster-engine get pods --no-headers -o custom-columns=":metadata.name" | \
+    grep "^assisted-service" || echo "assisted-service")
+  oc -n multicluster-engine wait --for=delete pod/assisted-image-service-0 pod/${assisted_service_pod_name} --timeout=10m
   set +x
 }
 
