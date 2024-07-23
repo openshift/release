854d2b215b3d99aca0f240c0a5b107e3038d6d06
Sergio Regidor <sregidor@redhat.com>
2024-07-23 15:09:12 +0000

    MCO create multiple custom MCP

--- ci-operator/step-registry/mco/conf/day2/create-custom-mcp/README.md
+++ ci-operator/step-registry/mco/conf/day2/create-custom-mcp/README.md
@@ -9,7 +9,41 @@
 
 ## Purpose
 
-To create cusom MachineConfigPool with a specified name. This step is necessary to deploy 'realtime' or '64k-pages' kernels in multi-arch clusters.
+To create cusom MachineConfigPools with the specified names. This step is necessary to deploy 'realtime' or '64k-pages' kernels in multi-arch clusters.
+
+The step configuration consists on 3 space separated lista:
+
+```
+      MCO_CONF_DAY2_CUSTOM_MCP_NAME: list with all the names (the step will be skipped if empty)
+      MCO_CONF_DAY2_CUSTOM_MCP_FROM_LABEL: list with the labels used to create the pools defined in MCO_CONF_DAY2_CUSTOM_MCP_NAME (optional, if any pool doesn't have a corresponding label it will use the MCO_CONF_DAY2_CUSTOM_MCP_DEFAULT_FROM_LABEL value)
+      MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES: list with the number of nodes that will be added to the pools defined in MCO_CONF_DAY2_CUSTOM_MCP_NAME (optional, if any pool doesn't have a corresponding number of nodes it will use the MCO_CONF_DAY2_CUSTOM_MCP_DEFAULT_NUM_NODES value)
+
+```
+
+
+Valid lists examples
+
+```
+MCO_CONF_DAY2_CUSTOM_MCP_NAME: pool1 pool2 poo3
+
+3 custom pools with 1 node each that will be taken from the worker pool
+```
+
+```
+MCO_CONF_DAY2_CUSTOM_MCP_NAME: pool1 pool2
+MCO_CONF_DAY2_CUSTOM_MCP_FROM_LABEL: node-role.kubernetes.io/worker,kubernetes.io/arch=arm64
+MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES: -1
+
+2 custom pools. The first pool will include all worker nodes with arm64 arch. The second pool will have 1 node that will be taken from the worker pool.
+```
+
+
+```
+MCO_CONF_DAY2_CUSTOM_MCP_NAME: pool1 pool2 poo3
+MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES: 2 1 -1
+
+3 custom pools. The first pool will have 2 nodes, the second one will have 1 node, and the third one will have all nodes left.
+```
 
 
 Example of a multi-arch cluster where a new MachineConfigPool named 'infra' has been created with one "arm64" node in it.
@@ -32,6 +66,27 @@ Example of a multi-arch cluster where a new MachineConfigPool named 'infra' has 
     workflow: cucushift-installer-rehearse-aws-ipi
 ```
 
+Example for 2 custom machineconfigpools, one for amd64 with 2 nodes and another one for arm64 with 1 node
+
+```
+- as: aws-ipi-amd-mixarch-kerneltype-f28-day2-infra-custompool
+  cron: 14 4 13 * *
+  steps:
+    cluster_profile: aws-qe
+    dependencies:
+      OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE: release:multi-latest
+    env:
+      ADDITIONAL_WORKERS: "1"
+      BASE_DOMAIN: qe.devcluster.openshift.com
+      MCO_CONF_DAY2_CUSTOM_MCP_FROM_LABEL: node-role.kubernetes.io/worker,kubernetes.io/arch=arm64 node-role.kubernetes.io/worker,kubernetes.io/arch=amd64
+      MCO_CONF_DAY2_CUSTOM_MCP_NAME: infraarm infraamd
+      MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES: 1 2
+    test:
+    - chain: ipi-install-heterogeneous-day2-kerneltype
+    - chain: openshift-e2e-test-qe
+    workflow: cucushift-installer-rehearse-aws-ipi
+```
+
 
 ## Process
 
@@ -46,14 +101,20 @@ This script creates a MachineConfigPool resource and joins the nodes matching th
 ### Environment Variables
 
 - `MCO_CONF_DAY2_CUSTOM_MCP_NAME`
-  - **Definition**: The name of the custom MachineConfigPool that will be created
+  - **Definition**: Space separated list with the names of the custom MachineConfigPools that will be created
   - **If left empty**: No custom MCP will be created and this step will be skipped
 - `MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES`
-  - **Definition**: The number of nodes that will be removed from the worker pool and added to the new custom pool
-  - **If left empty**: It will default to 1. It the value is an empty string "" all nodes matching the MCO_CONF_DAY2_CUSTOM_MCP_FROM_LABEL label will be added to the pool
+  - **Definition**: Space separated list with the number of nodes that will be removed from the worker pool and added to the new custom pools
+  - **If left empty**: It will default to `MCO_CONF_DAY2_CUSTOM_MCP_DEFAULT_NUM_NODES`. It the value is negative all nodes matching the pool's label will be added to the pool
 - `MCO_CONF_DAY2_CUSTOM_MCP_FROM_LABEL`
-  - **Definition**: The label used to filter the nodes that can be added to the new pool. 
-  - **If left empty**: It will default to "node-role.kubernetes.io/worker" label
+  - **Definition**: Space separated list with the labels used to filter the nodes that can be added to the new pool. 
+  - **If left empty**: It will default to `MCO_CONF_DAY2_CUSTOM_MCP_DEFAULT_FROM_LABEL`
 - `MCO_CONF_DAY2_CUSTOM_MCP_TIMEOUT`
   - **Definition**: Maximum time that we will wait for the new custom pool to be updated
   - **If left empty**: It will default to 20m
+- `MCO_CONF_DAY2_CUSTOM_MCP_DEFAULT_NUM_NODES.`
+  - **Definition**: Default number of nodes that a pool will use if no number of nodes has been defined for it
+  - **If left empty**: It will default to 1
+- `MCO_CONF_DAY2_CUSTOM_MCP_DEFAULT_FROM_LABEL.`
+  - **Definition**: Default label that will be used to create a pool if no label has been defined for it
+  - **If left empty**: It will default to 1
--- ci-operator/step-registry/mco/conf/day2/create-custom-mcp/mco-conf-day2-create-custom-mcp-commands.sh
+++ ci-operator/step-registry/mco/conf/day2/create-custom-mcp/mco-conf-day2-create-custom-mcp-commands.sh
@@ -21,16 +21,14 @@ function set_proxy () {
 }
 
 function validate_params() {
+  local MCP_NUM_NODES=$1
+  local MCP_LABEL=$2
+
   # shellcheck disable=SC2086
-  NUM_WORKERS=$(oc get nodes -l $MCO_CONF_DAY2_CUSTOM_MCP_FROM_LABEL -oname | wc -l)
+  NUM_WORKERS=$(oc get nodes -l $MCP_LABEL -oname | wc -l)
 
-  if [ "$MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES" != "" ] && (( MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES > NUM_WORKERS  )) ; then
-    echo "ERROR: We are trying to add $MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES to our custom pool, but there are only $NUM_WORKERS nodes available with label $MCO_CONF_DAY2_CUSTOM_MCP_FROM_LABEL"
-    exit 255
-  fi
-
-  if [ "$MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES" != "" ] && (( MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES < 0  )) ; then
-    echo "ERROR: Refuse to create a custom MCP with a negative number of nodes."
+  if [ "$MCP_NUM_NODES" != "" ] && (( MCP_NUM_NODES > NUM_WORKERS  )) ; then
+    echo "ERROR: We are trying to add $MCP_NUM_NODES to our custom pool, but there are only $NUM_WORKERS nodes available with label $MCO_CONF_DAY2_CUSTOM_MCP_FROM_LABEL"
     exit 255
   fi
 }
@@ -39,6 +37,11 @@ function create_custom_mcp() {
   local MCP_NAME=$1
   local MCP_NUM_NODES=$2
   local MCP_LABEL=$3
+
+
+  echo "MCP_NAME=$MCP_NAME"
+  echo "MCP_LABEL=$MCP_LABEL"
+  echo "MCP_NUM_NODES=$MCP_NUM_NODES"
 
   echo "Creating custom MachineConfigPool with name $MCP_NAME from label $MCP_LABEL"
 
@@ -60,11 +63,20 @@ EOF
     echo "It has been requested to add 0 nodes to the pool. No node will be added to the custom MachineConfigPool."
     return
   fi
+
+  # If <0 is provided, we include all available nodes in this pool
+  if (( MCP_NUM_NODES < 0 ))
+  then
+    echo "A negative number of nodes was provided. We set an empty variable so that all nodes are added to the pool"
+    MCP_NUM_NODES=""
+  fi
+
   [ -z "$MCP_NUM_NODES" ] \
-    && echo "MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES variable is empty. All nodes matching the '$MCP_LABEL' label will be added to the custom pool" \
+    && echo "MCP_NUM_NODES variable is empty. All nodes matching the '$MCP_LABEL' label will be added to the custom pool" \
     || echo "Labeling $MCP_NUM_NODES worker nodes in order to add them to the new custom MCP"
+
   # shellcheck disable=SC2046
-  oc label node $(oc get nodes -l "$MCP_LABEL" -ojsonpath="{.items[:$MCP_NUM_NODES].metadata.name}") node-role.kubernetes.io/"$MCO_CONF_DAY2_CUSTOM_MCP_NAME"=
+  oc label node $(oc get nodes -l "$MCP_LABEL" -ojsonpath="{.items[:$MCP_NUM_NODES].metadata.name}") node-role.kubernetes.io/"$MCP_NAME"=
 }
 
 function wait_for_config_to_be_applied() {
@@ -84,8 +96,70 @@ function wait_for_config_to_be_applied() {
 
 
 set_proxy
-validate_params
-create_custom_mcp "$MCO_CONF_DAY2_CUSTOM_MCP_NAME" "$MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES" "$MCO_CONF_DAY2_CUSTOM_MCP_FROM_LABEL"
-wait_for_config_to_be_applied "$MCO_CONF_DAY2_CUSTOM_MCP_NAME" "$MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES" "$MCO_CONF_DAY2_CUSTOM_MCP_TIMEOUT"
+
+echo "MCO_CONF_DAY2_CUSTOM_MCP_NAME $MCO_CONF_DAY2_CUSTOM_MCP_NAME"
+read -r -a ARR_CUSTOM_MCP_NAME <<< "$MCO_CONF_DAY2_CUSTOM_MCP_NAME"
+
+echo "MCO_CONF_DAY2_CUSTOM_MCP_FROM_LABEL $MCO_CONF_DAY2_CUSTOM_MCP_FROM_LABEL"
+read -r -a ARR_CUSTOM_MCP_FROM_LABEL <<< "$MCO_CONF_DAY2_CUSTOM_MCP_FROM_LABEL"
+LEN_CUSTOM_MCP_FROM_LABEL=${#ARR_CUSTOM_MCP_FROM_LABEL[@]}
+echo "LEN_CUSTOM_MCP_FROM_LABEL $LEN_CUSTOM_MCP_FROM_LABEL"
+
+echo "MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES $MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES"
+read -r -a ARR_CUSTOM_MCP_NUM_NODES <<< "$MCO_CONF_DAY2_CUSTOM_MCP_NUM_NODES"
+LEN_CUSTOM_MCP_NUM_NODES=${#ARR_CUSTOM_MCP_NUM_NODES[@]}
+echo "LEN_CUSTOM_MCP_NUM_NODES $LEN_CUSTOM_MCP_NUM_NODES"
+
+
+echo "MCO_CONF_DAY2_CUSTOM_MCP_DEFAULT_FROM_LABEL $MCO_CONF_DAY2_CUSTOM_MCP_DEFAULT_FROM_LABEL"
+echo "MCO_CONF_DAY2_CUSTOM_MCP_DEFAULT_NUM_NODES $MCO_CONF_DAY2_CUSTOM_MCP_DEFAULT_NUM_NODES"
+
+# We cannot add the same node to more than one MCP, so we build an exclusion label
+# so that the nodes that have been added to previous custom pools are not candidates for the new pools
+# We would use a label like this: actualLabel + exclusionLabel
+#
+EXCLUSION_LABEL=""
+for POOL_NAME in "${ARR_CUSTOM_MCP_NAME[@]}"
+do
+  EXCLUSION_LABEL="$EXCLUSION_LABEL,!node-role.kubernetes.io/$POOL_NAME"
+done
+
+echo "EXCLUSION_LABEL $EXCLUSION_LABEL"
+
+INDEX=0
+for POOL_NAME in "${ARR_CUSTOM_MCP_NAME[@]}"
+do 
+
+  if (( INDEX > LEN_CUSTOM_MCP_FROM_LABEL-1  ));
+  then
+    POOL_LABEL="${MCO_CONF_DAY2_CUSTOM_MCP_DEFAULT_FROM_LABEL}"
+  else 
+    POOL_LABEL="${ARR_CUSTOM_MCP_FROM_LABEL[$INDEX]}"
+  fi
+
+  if (( INDEX > LEN_CUSTOM_MCP_NUM_NODES-1  ));
+  then
+    POOL_NUM_NODES="${MCO_CONF_DAY2_CUSTOM_MCP_DEFAULT_NUM_NODES}"
+  else 
+    POOL_NUM_NODES="${ARR_CUSTOM_MCP_NUM_NODES[$INDEX]}"
+  fi
+
+  POOL_FULL_LABEL="$POOL_LABEL$EXCLUSION_LABEL"
+
+  echo ' ------ '
+  echo "$POOL_NAME"
+  echo "$POOL_NUM_NODES"
+  echo "$POOL_LABEL"
+  echo "$POOL_FULL_LABEL"
+  echo ' ------ '
+
+
+  validate_params "$POOL_NUM_NODES" "$POOL_FULL_LABEL"
+  create_custom_mcp "$POOL_NAME" "$POOL_NUM_NODES" "$POOL_FULL_LABEL"
+  wait_for_config_to_be_applied "$POOL_NAME" "$POOL_NUM_NODES" "$MCO_CONF_DAY2_CUSTOM_MCP_TIMEOUT"
+
+  (( INDEX=INDEX+1 ))
+done
+
 
 oc get mcp
