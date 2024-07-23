61b650a3151ecac6d26b40c083091bcf7bf22da4
sachinninganure <sninganu@redhat.com>
2024-07-23 02:41:45 -0400

    wandershape-fix1:wq!

--- ci-operator/step-registry/ignition/bastionhost/ignition-bastionhost-commands.sh
+++ ci-operator/step-registry/ignition/bastionhost/ignition-bastionhost-commands.sh
@@ -472,4 +472,4 @@ tar xvf wondershap.tar.gz
 #git clone https://github.com/SachinNinganure/wondershaper
 cd wondershaper-1
 #./wondershaper -a eth0 -u 4096 -d 8192
-./wondershaper-1/wondershaper -a eth0 -u 70000 -d 80000
+./wondershaper -a eth0 -u 70000 -d 80000
