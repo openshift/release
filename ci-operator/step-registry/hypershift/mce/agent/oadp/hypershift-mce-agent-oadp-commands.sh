67773c7fea06fef946383bb0973b4ea759a07d13
Liangquan Li <liangli@redhat.com>
2024-07-25 10:51:24 +0800

    oadp sleep

--- ci-operator/step-registry/hypershift/mce/agent/oadp/hypershift-mce-agent-oadp-commands.sh
+++ ci-operator/step-registry/hypershift/mce/agent/oadp/hypershift-mce-agent-oadp-commands.sh
@@ -5,6 +5,33 @@ set -o errexit
 set -o pipefail
 set -x
 
+export KUBECONFIG="${SHARED_DIR}/nested_kubeconfig"
+oc create namespace oadp-helper
+IMAGE=$(oc get clusterversion version -ojsonpath='{.status.desired.image}')
+TOOLS_IMAGE=$(oc adm release info ${IMAGE} --image-for=tools)
+oc create secret generic oadp-kubeconfig-secret --from-file=kubeconfig="$KUBECONFIG" -n oadp-helper
+cat <<EOF | oc apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: oadp-helper
+  namespace: oadp-helper
+spec:
+  containers:
+    - name: patch-container
+      image: ${TOOLS_IMAGE}
+      command: [ "sh", "-c", "while true; do sleep 3600; done" ]
+      volumeMounts:
+        - name: kubeconfig
+          mountPath: /etc/kubernetes
+          readOnly: true
+  volumes:
+    - name: kubeconfig
+      secret:
+        secretName: oadp-kubeconfig-secret
+EOF
+
+export KUBECONFIG="${SHARED_DIR}/kubeconfig"
 cat <<EOF > /tmp/miniocred
 [default]
 aws_access_key_id=admin
@@ -27,6 +54,7 @@ spec:
           region: minio
           s3ForcePathStyle: 'true'
           s3Url: 'http://virthost.ostest.test.metalkube.org:9000'
+          insecureSkipTLSVerify: "true"
         credential:
           key: cloud
           name: cloud-credentials
@@ -44,6 +72,7 @@ spec:
         - openshift
         - aws
         - kubevirt
+        - csi
   snapshotLocations:
     - velero:
         config:
@@ -56,6 +85,8 @@ oc wait --timeout=20m --for=condition=Reconciled DataProtectionApplication/dpa-s
 oc wait --timeout=20m --all --for=jsonpath='{.status.phase}'=Available backupStorageLocation -n openshift-adp
 
 CLUSTER_NAME="$(echo -n $PROW_JOB_ID|sha256sum|cut -c-20)"
+NODEPOOL_REPLICAS=$(oc get nodepool -n local-cluster "${CLUSTER_NAME}" -ojsonpath='{.spec.replicas}')
+echo "scale down ${NODEPOOL_REPLICAS} => 0"
 oc scale nodepool ${CLUSTER_NAME} -n local-cluster --replicas 0
 oc patch nodepool -n local-cluster ${CLUSTER_NAME}  --type json -p '[{"op": "add", "path": "/spec/pausedUntil", "value": "true"}]'
 oc patch hostedcluster -n local-cluster ${CLUSTER_NAME}  --type json -p '[{"op": "add", "path": "/spec/pausedUntil", "value": "true"}]'
@@ -72,43 +103,91 @@ spec:
   includedNamespaces:
   - local-cluster
   - local-cluster-${CLUSTER_NAME}
-  includedResources: []
+  includedResources:
+  - sa
+  - role
+  - rolebinding
+  - pod
+  - pvc
+  - pv
+  - bmh
+  - configmap
+  - infraenv
+  - priorityclasses
+  - pdb
+  - agents
+  - hostedcluster
+  - nodepool
+  - secrets
+  - services
+  - deployments
+  - statefulsets
+  - hostedcontrolplane
+  - cluster
+  - agentcluster
+  - agentmachinetemplate
+  - agentmachine
+  - machinedeployment
+  - machineset
+  - machine
+  - route
   excludedResources: []
   storageLocation: dpa-sample-1
   ttl: 2h0m0s
   snapshotMoveData: true
   datamover: "velero"
   defaultVolumesToFsBackup: true
+  snapshotVolumes: true
 EOF
+#VolumeSnapshot
 oc wait --timeout=45m --for=jsonpath='{.status.phase}'=Completed backup/hc-clusters-hosted-backup -n openshift-adp
+#oc patch backupstoragelocation dpa-sample-1 -n openshift-adp --type merge --patch '{"spec":{"accessMode":"ReadOnly"}}'
 
 oc patch hostedcluster -n local-cluster ${CLUSTER_NAME}  --type json -p '[{"op": "add", "path": "/spec/pausedUntil", "value": "false"}]'
 oc patch nodepool -n local-cluster ${CLUSTER_NAME}  --type json -p '[{"op": "add", "path": "/spec/pausedUntil", "value": "false"}]'
-#oc delete hostedcluster/${CLUSTER_NAME} -n local-cluster
-##
-#cat <<EOF | oc apply -f -
-#apiVersion: velero.io/v1
-#kind: Restore
-#metadata:
-#  name: hc-clusters-hosted-restore
-#  namespace: openshift-adp
-#spec:
-#  includedNamespaces:
-#  - local-cluster
-#  - local-cluster-b4e09e601e3e26d055cd
-#  backupName: hc-clusters-hosted-backup
-#  restorePVs: true
-#  preserveNodePorts: true
-#  existingResourcePolicy: update
-#  excludedResources:
-#  - nodes
-#  - events
-#  - events.events.k8s.io
-#  - backups.velero.io
-#  - restores.velero.io
-#  - resticrepositories.velero.io
-#EOF
+oc delete hostedcluster/${CLUSTER_NAME} -n local-cluster
 #
-#oc wait --timeout=45m --for=jsonpath='{.status.phase}'=Completed restore/hc-clusters-hosted-restore -n openshift-adp
-#oc get backup -n openshift-adp hc-clusters-hosted-backup -o yaml > "${ARTIFACT_DIR}/backup.yaml"
-#oc get restore hc-clusters-hosted-restore -n openshift-adp  -o yaml > "${ARTIFACT_DIR}/restore.yaml"
+cat <<EOF | oc apply -f -
+apiVersion: velero.io/v1
+kind: Restore
+metadata:
+  name: hc-clusters-hosted-restore
+  namespace: openshift-adp
+spec:
+  includedNamespaces:
+  - local-cluster
+  - local-cluster-${CLUSTER_NAME}
+  backupName: hc-clusters-hosted-backup
+  restorePVs: true
+  preserveNodePorts: true
+  existingResourcePolicy: update
+  excludedResources:
+  - nodes
+  - events
+  - events.events.k8s.io
+  - backups.velero.io
+  - restores.velero.io
+  - resticrepositories.velero.io
+EOF
+while true; do
+    if oc get statefulset etcd -n "local-cluster-${CLUSTER_NAME}" > /dev/null 2>&1; then
+        DESIRED_REPLICAS=$(oc get statefulset etcd -n "local-cluster-${CLUSTER_NAME}" -o jsonpath='{.spec.replicas}')
+        CURRENT_REPLICAS=$(oc get statefulset etcd -n "local-cluster-${CLUSTER_NAME}" -o jsonpath='{.status.readyReplicas}')
+
+        if [ "$DESIRED_REPLICAS" == "$CURRENT_REPLICAS" ]; then
+            echo "StatefulSet etcd is fully available in namespace $NAMESPACE"
+            oc patch hostedcluster -n local-cluster ${CLUSTER_NAME}  --type json -p '[{"op": "add", "path": "/spec/pausedUntil", "value": "false"}]'
+            oc patch nodepool -n local-cluster ${CLUSTER_NAME}  --type json -p '[{"op": "add", "path": "/spec/pausedUntil", "value": "false"}]'
+            exit 0
+        fi
+    fi
+    sleep 5
+done
+oc wait --timeout=30m --for=condition=Available --namespace=local-cluster hostedcluster/${CLUSTER_NAME}
+oc patch hostedcluster -n local-cluster ${CLUSTER_NAME}  --type json -p '[{"op": "add", "path": "/spec/pausedUntil", "value": "false"}]'
+oc patch nodepool -n local-cluster ${CLUSTER_NAME}  --type json -p '[{"op": "add", "path": "/spec/pausedUntil", "value": "false"}]'
+oc scale nodepool ${CLUSTER_NAME} -n local-cluster --replicas ${NODEPOOL_REPLICAS}
+###
+#oc wait --timeout=45m --for=jsonpath='{.status.phase}'=Completed restore/hc-clusters-hosted-restore -n openshift-adp || true
+oc get backup -n openshift-adp hc-clusters-hosted-backup -o yaml > "${ARTIFACT_DIR}/backup.yaml"
+oc get restore hc-clusters-hosted-restore -n openshift-adp  -o yaml > "${ARTIFACT_DIR}/restore.yaml"
