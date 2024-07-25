a016114b89e67c13720339d4c651050b1b66ccdd
Neha Yadav <neha.yadav3@ibm.com>
2024-07-25 19:53:40 +0530

    Update hypershift-mce-power-heterogeneous-x86-create-workers-boot-commands.sh

--- ci-operator/step-registry/hypershift/mce/power/heterogeneous/x86/create/workers/boot/hypershift-mce-power-heterogeneous-x86-create-workers-boot-commands.sh
+++ ci-operator/step-registry/hypershift/mce/power/heterogeneous/x86/create/workers/boot/hypershift-mce-power-heterogeneous-x86-create-workers-boot-commands.sh
@@ -95,7 +95,7 @@ echo "$(date) Get workers hostnames"
 IFS=' ' read -r -a HOST_NAMES <<< "${INSTANCE_NAMES}"
 echo "HOST_NAMES: ${HOST_NAMES[@]}"  # Debug statement
 
-sleep 5h
+sleep 2h
 
 for ((i=1; i<=20; i++)); do
     agents=$(oc get agent -n ${HOSTED_CONTROL_PLANE_NAMESPACE} -o json)
