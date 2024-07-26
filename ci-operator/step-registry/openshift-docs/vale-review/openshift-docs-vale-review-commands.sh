d6425ef98855769139212b41e2bff49ec3950f57
Aidan Reilly <74046732+aireilly@users.noreply.github.com>
2024-07-26 01:28:23 +0100

    Moving docs build and deploy into validate-asciidoc step (#54806)
    make jobs run for new openshift-docs configs

--- ci-operator/step-registry/openshift-docs/vale-review/openshift-docs-vale-review-commands.sh
+++ ci-operator/step-registry/openshift-docs/vale-review/openshift-docs-vale-review-commands.sh
@@ -6,6 +6,13 @@
 # set -o pipefail
 # set -o verbose
 
+PR_AUTHOR=$(echo ${JOB_SPEC} | jq -r '.refs.pulls[0].author')
+
+if [ "$PR_AUTHOR" == "openshift-cherrypick-robot" ]; then
+  echo "openshift-cherrypick-robot PRs don't need a full docs build."
+  exit 0
+fi
+
 curl https://raw.githubusercontent.com/openshift/openshift-docs/main/scripts/prow-vale-review.sh > scripts/prow-vale-review.sh
 
 GITHUB_AUTH_TOKEN=$(cat /tmp/vault/ocp-docs-vale-github-secret/GITHUB_AUTH_TOKEN)
