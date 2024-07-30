2c44e2ba55eb55299145d97fabb47ba5935dc3a5
Eran Ifrach <eifrach@redhat.com>
2024-07-30 13:33:02 +0300

    NO-ISSUE: workaround for ansible-dnf

--- ci-operator/step-registry/assisted/equinix/setup/assisted-equinix-setup-commands.sh
+++ ci-operator/step-registry/assisted/equinix/setup/assisted-equinix-setup-commands.sh
@@ -10,6 +10,9 @@ echo "************  assisted equinix setup command ************"
 # TODO: Remove once OpenShift CI supports it out of the box (see https://access.redhat.com/articles/4859371)
 fix_uid.sh
 
+# Fix for issues with DNF and ansible core 2.17 on RHEL8 / RHEL9 systems
+pip install ansible==9.8
+
 cd "${ANSIBLE_PLAYBOOK_DIRECTORY}"
 ansible-playbook --extra-vars "@vars/ci.yml" \
                  --extra-vars "@vars/ci_equinix_infrastucture.yml" \
