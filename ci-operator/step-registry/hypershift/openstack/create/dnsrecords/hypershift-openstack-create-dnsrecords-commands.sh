b375dccdcf37035c793629080042a846854dee35
Emilien Macchi <emacchi@redhat.com>
2024-07-31 09:36:28 -0400

    Get DNS records working with hypershift-ci-jobs AWS credentials

--- ci-operator/step-registry/hypershift/openstack/create/dnsrecords/hypershift-openstack-create-dnsrecords-commands.sh
+++ ci-operator/step-registry/hypershift/openstack/create/dnsrecords/hypershift-openstack-create-dnsrecords-commands.sh
@@ -4,7 +4,7 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
+export AWS_SHARED_CREDENTIALS_FILE="/etc/hypershift-ci-jobs-awscreds/credentials"
 HOSTED_ZONE_ID="$(</var/run/aws/shiftstack-zone-id)"
 
 export AWS_DEFAULT_REGION=us-east-1
