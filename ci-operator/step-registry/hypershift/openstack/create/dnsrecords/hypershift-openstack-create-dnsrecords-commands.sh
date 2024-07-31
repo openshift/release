9133e721206a887aa8f6e9570babcf337a6275da
Emilien Macchi <emacchi@redhat.com>
2024-07-31 11:36:24 -0400

    Use AWS creds and figure out domain ID

--- ci-operator/step-registry/hypershift/openstack/create/dnsrecords/hypershift-openstack-create-dnsrecords-commands.sh
+++ ci-operator/step-registry/hypershift/openstack/create/dnsrecords/hypershift-openstack-create-dnsrecords-commands.sh
@@ -5,17 +5,22 @@ set -o errexit
 set -o pipefail
 
 export AWS_SHARED_CREDENTIALS_FILE="/etc/hypershift-ci-jobs-awscreds/credentials"
-HOSTED_ZONE_ID="$(</var/run/aws/shiftstack-zone-id)"
 
 export AWS_DEFAULT_REGION=us-east-1
 export AWS_DEFAULT_OUTPUT=json
-export AWS_PROFILE=profile
 
 # This was taken from other Hypershift jobs, this is how the hosted cluster
 # is named in CI.
 HASH="$(echo -n $PROW_JOB_ID|sha256sum)"
 CLUSTER_NAME=${HASH:0:20}
 INGRESS_FIP="$(<"${SHARED_DIR}/HCP_INGRESS_FIP")"
+
+echo "Getting the hosted zone ID for domain: ${BASE_DOMAIN}"
+HOSTED_ZONE_ID="$(aws route53 list-hosted-zones-by-name \
+            --dns-name "${BASE_DOMAIN}" \
+            --query "HostedZones[? Config.PrivateZone != \`true\` && Name == \`${BASE_DOMAIN}.\`].Id" \
+            --output text)"
+echo "${HOSTED_ZONE_ID}" > "${SHARED_DIR}/hosted-zone.txt"
 
 echo "Creating DNS records for $CLUSTER_NAME.$BASE_DOMAIN"
 cat > "${SHARED_DIR}/dns_up.json" <<EOF
