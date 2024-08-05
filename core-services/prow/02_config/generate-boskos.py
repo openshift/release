d30bf3085bcbe072ed0b273372f6507c610a9a70
Miguel Carpio <153778639+MiguelCarpio@users.noreply.github.com>
2024-08-05 10:11:41 +0200

    OSASINFRA-3551: Reducing the openstack-nfv-quota-slice to 4 (#55081)
    * Reducing the openstack-nfv-quota-slice to 4
    
    * boskos.yaml update

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -238,7 +238,7 @@ CONFIG = {
         'default': 1,
     },
     'openstack-nfv-quota-slice': {
-        'default': 5,
+        'default': 4,
     },
     'openstack-vh-mecha-central-quota-slice': {
         'default': 4,
