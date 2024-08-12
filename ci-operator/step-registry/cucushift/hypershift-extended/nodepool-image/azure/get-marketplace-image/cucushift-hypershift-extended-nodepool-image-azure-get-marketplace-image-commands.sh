ef7c882945e4ff5b98a7e3747aa20753e7909f9e
Feilian Xie <fxie@redhat.com>
2024-08-12 09:50:14 +0800

    Add heterogeneous aks jobs (#55398)
    Signed-off-by: Feilian Xie <fxie@redhat.com>

--- ci-operator/step-registry/cucushift/hypershift-extended/nodepool-image/azure/get-marketplace-image/cucushift-hypershift-extended-nodepool-image-azure-get-marketplace-image-commands.sh
+++ ci-operator/step-registry/cucushift/hypershift-extended/nodepool-image/azure/get-marketplace-image/cucushift-hypershift-extended-nodepool-image-azure-get-marketplace-image-commands.sh
@@ -7,6 +7,33 @@ if [[ -z $HYPERSHIFT_AZURE_MARKETPLACE_IMAGE_PUBLISHER ]]; then
     exit 0
 fi
 
+function get_image_info() {
+    local arch="$1"
+    local sku=""
+
+    case "$arch" in
+    x64)
+        sku="aro_${OCP_MAJOR_VERSION}${OCP_MINOR_VERSION}"
+        ;;
+    Arm64)
+        sku="${OCP_MAJOR_VERSION}${OCP_MINOR_VERSION}-arm"
+        ;;
+    *)
+        echo "Unsupported arch $arch" >&2
+        return 1
+        ;;
+    esac
+
+    az vm image list \
+        --architecture "$arch" \
+        --publisher "$HYPERSHIFT_AZURE_MARKETPLACE_IMAGE_PUBLISHER" \
+        --offer "$HYPERSHIFT_AZURE_MARKETPLACE_IMAGE_OFFER" \
+        --sku "$sku" \
+        --location "$AZURE_LOCATION" \
+        --all \
+        -o json | jq -r 'max_by(.version)'
+}
+
 AZURE_AUTH_LOCATION="${CLUSTER_PROFILE_DIR}/osServicePrincipal.json"
 AZURE_AUTH_CLIENT_ID="$(<"${AZURE_AUTH_LOCATION}" jq -r .clientId)"
 AZURE_AUTH_CLIENT_SECRET="$(<"${AZURE_AUTH_LOCATION}" jq -r .clientSecret)"
@@ -19,29 +46,36 @@ az login --service-principal -u "${AZURE_AUTH_CLIENT_ID}" -p "${AZURE_AUTH_CLIEN
 set -x
 
 IMAGE_JSON=/tmp/azure-marketplace-image.json
+IMAGE_JSON_EXTRA=/tmp/azure-marketplace-image-extra.json
+IMAGE_OFFER_FILE="${SHARED_DIR}"/azure-marketplace-image-offer
+IMAGE_OFFER_FILE_EXTRA="${SHARED_DIR}"/azure-marketplace-image-offer-extra
+IMAGE_PUBLISHER_FILE="${SHARED_DIR}"/azure-marketplace-image-publisher
+IMAGE_PUBLISHER_FILE_EXTRA="${SHARED_DIR}"/azure-marketplace-image-publisher-extra
 IMAGE_SKU_FILE="${SHARED_DIR}"/azure-marketplace-image-sku
+IMAGE_SKU_FILE_EXTRA="${SHARED_DIR}"/azure-marketplace-image-sku-extra
 IMAGE_VERSION_FILE="${SHARED_DIR}"/azure-marketplace-image-version
+IMAGE_VERSION_FILE_EXTRA="${SHARED_DIR}"/azure-marketplace-image-version-extra
 PULL_SECRET="${CLUSTER_PROFILE_DIR}"/pull-secret
-WRITABLE_PULL_SECRET=/tmp/pull-secret
+PULL_SECRET_WRITABLE=/tmp/pull-secret
 
 echo "Getting pull secret"
-cp "$PULL_SECRET" "$WRITABLE_PULL_SECRET"
-KUBECONFIG="" oc registry login --to "$WRITABLE_PULL_SECRET"
+cp "$PULL_SECRET" "$PULL_SECRET_WRITABLE"
+KUBECONFIG="" oc registry login --to "$PULL_SECRET_WRITABLE"
 
 echo "Extracting OCP version from release image"
-OCP_MAJOR_VERSION=$(oc adm release info "$RELEASE_IMAGE_LATEST" -a "$WRITABLE_PULL_SECRET" -o json | jq -r '.metadata.version' | cut -d . -f 1)
-OCP_MINOR_VERSION=$(oc adm release info "$RELEASE_IMAGE_LATEST" -a "$WRITABLE_PULL_SECRET" -o json | jq -r '.metadata.version' | cut -d . -f 2)
+OCP_MAJOR_VERSION=$(oc adm release info "$RELEASE_IMAGE_LATEST" -a "$PULL_SECRET_WRITABLE" -o json | jq -r '.metadata.version' | cut -d . -f 1)
+OCP_MINOR_VERSION=$(oc adm release info "$RELEASE_IMAGE_LATEST" -a "$PULL_SECRET_WRITABLE" -o json | jq -r '.metadata.version' | cut -d . -f 2)
 
-echo "Extracting Azure marketplace image info"
-az vm image list \
-    --architecture "$HYPERSHIFT_AZURE_MARKETPLACE_IMAGE_ARCH" \
-    --publisher "$HYPERSHIFT_AZURE_MARKETPLACE_IMAGE_PUBLISHER" \
-    --offer "$HYPERSHIFT_AZURE_MARKETPLACE_IMAGE_OFFER" \
-    --sku "aro_${OCP_MAJOR_VERSION}${OCP_MINOR_VERSION}" \
-    --location "$AZURE_LOCATION" \
-    --all \
-    -o json | jq -r 'max_by(.version)' > "$IMAGE_JSON"
-
-echo "Storing image info"
+echo "Extracting image info"
+get_image_info "$HYPERSHIFT_AZURE_MARKETPLACE_IMAGE_ARCH" > "$IMAGE_JSON"
 jq -r .sku "$IMAGE_JSON" > "$IMAGE_SKU_FILE"
 jq -r .version "$IMAGE_JSON" > "$IMAGE_VERSION_FILE"
+echo "$HYPERSHIFT_AZURE_MARKETPLACE_IMAGE_OFFER" > "$IMAGE_OFFER_FILE"
+echo "$HYPERSHIFT_AZURE_MARKETPLACE_IMAGE_PUBLISHER" > "$IMAGE_PUBLISHER_FILE"
+
+echo "Extracting extra image info"
+get_image_info "$HYPERSHIFT_AZURE_MARKETPLACE_IMAGE_ARCH_EXTRA" > "$IMAGE_JSON_EXTRA"
+jq -r .sku "$IMAGE_JSON_EXTRA" > "$IMAGE_SKU_FILE_EXTRA"
+jq -r .version "$IMAGE_JSON_EXTRA" > "$IMAGE_VERSION_FILE_EXTRA"
+echo "$HYPERSHIFT_AZURE_MARKETPLACE_IMAGE_OFFER" > "$IMAGE_OFFER_FILE_EXTRA"
+echo "$HYPERSHIFT_AZURE_MARKETPLACE_IMAGE_PUBLISHER" > "$IMAGE_PUBLISHER_FILE_EXTRA"
