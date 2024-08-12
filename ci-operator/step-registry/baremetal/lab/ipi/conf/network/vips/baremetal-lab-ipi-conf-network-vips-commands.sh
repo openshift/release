21337bef83c141a75779fc727c688f295b1effe9
Manoj Hans <mhans@redhat.com>
2024-08-12 11:23:59 +0200

    Update vips patch to support IPv4, IPv6 and dual-stack configurations

--- ci-operator/step-registry/baremetal/lab/ipi/conf/network/vips/baremetal-lab-ipi-conf-network-vips-commands.sh
+++ ci-operator/step-registry/baremetal/lab/ipi/conf/network/vips/baremetal-lab-ipi-conf-network-vips-commands.sh
@@ -5,16 +5,21 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-echo "Creating patch file to cobfigure networking: ${SHARED_DIR}/install-config.yaml"
-
 # shellcheck disable=SC2154
-cat > "${SHARED_DIR}/dual_vips_patch_install_config.yaml" <<EOF
+if [ "${AGENT_PLATFORM_TYPE}" = "none" ] || [ "${masters}" -eq 1 ]; then
+  echo "Skip IPv4 vips configuration as the platform is none."
+  exit
+fi
+
+echo "Creating patch file to configure ipv4 networking: ${SHARED_DIR}/vips_patch_install_config.yaml"
+# shellcheck disable=SC2154
+cat > "${SHARED_DIR}/vips_patch_install_config.yaml" <<EOF
 platform:
   baremetal:
     apiVIPs:
-    $([ "${ipv4_enabled}" == "true" ] && yq_value="$(yq ".api_vip" "${SHARED_DIR}/vips.yaml")" && [ -n "$yq_value" ] && echo "- $yq_value")
-    $([ "${ipv6_enabled}" == "true" ] && yq_value="$(yq ".api_vip_v6" "${SHARED_DIR}/vips.yaml")" && [ -n "$yq_value" ] && echo "- $yq_value")
+    $([ "${ipv4_enabled}" == "true" ] && echo "- $(yq ".api_vip" "${SHARED_DIR}/vips.yaml")")
+    $([ "${ipv6_enabled}" == "true" ] && echo "- $(yq ".api_vip_v6" "${SHARED_DIR}/vips.yaml")")
     ingressVIPs:
-    $([ "${ipv4_enabled}" == "true" ] && yq_value="$(yq ".ingress_vip" "${SHARED_DIR}/vips.yaml")" && [ -n "$yq_value" ] && echo "- $yq_value")
-    $([ "${ipv6_enabled}" == "true" ] && yq_value="$(yq ".ingress_vip_v6" "${SHARED_DIR}/vips.yaml")" && [ -n "$yq_value" ] && echo "- $yq_value")
+    $([ "${ipv4_enabled}" == "true" ] && echo "- $(yq ".ingress_vip" "${SHARED_DIR}/vips.yaml")")
+    $([ "${ipv6_enabled}" == "true" ] && echo "- $(yq ".ingress_vip_v6" "${SHARED_DIR}/vips.yaml")")
 EOF
