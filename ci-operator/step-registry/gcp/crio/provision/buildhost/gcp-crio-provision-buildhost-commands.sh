9e765c8c024d509723447af4ae8ce04e97d86a32
Deep Mistry <dmistry@redhat.com>
2024-08-15 11:04:34 -0400

    WIP: Run ARM periodic on ARM build farm node

--- ci-operator/step-registry/gcp/crio/provision/buildhost/gcp-crio-provision-buildhost-commands.sh
+++ ci-operator/step-registry/gcp/crio/provision/buildhost/gcp-crio-provision-buildhost-commands.sh
@@ -35,7 +35,7 @@ chmod 0600 ${SHARED_DIR}/vpc-sshkey
 #####################################
 
 workdir=$(mktemp -d)
-curl -L https://github.com/mikefarah/yq/releases/download/3.3.0/yq_linux_amd64 -o /tmp/yq && chmod +x /tmp/yq
+curl -L "https://github.com/mikefarah/yq/releases/download/3.3.0/yq_linux_$(uname -m | sed 's/aarch64/arm64/;s/x86_64/amd64/')" -o /tmp/yq && chmod +x /tmp/yq
 
 #####################################
 #########Save Login As Script###########
