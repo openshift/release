ef6791f91d10a796ad0e808b5de0e99985d9f5d8
Jan Safranek <jsafrane@redhat.com>
2024-08-15 14:44:52 +0200

    Add manifest with azure-disk OCP specific CSI certification tests

--- ci-operator/step-registry/storage/conf/csi-azure-disk/storage-conf-csi-azure-disk-commands.sh
+++ ci-operator/step-registry/storage/conf/csi-azure-disk/storage-conf-csi-azure-disk-commands.sh
@@ -7,8 +7,14 @@ if [ -d /go/src/github.com/openshift/csi-operator/ ]; then
     echo "Using csi-operator repo"
     cd /go/src/github.com/openshift/csi-operator
     cp test/e2e/azure-disk/manifest.yaml ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
+
+    if [ -n "${TEST_OCP_CSI_DRIVER_MANIFEST}" ] && [ "${ENABLE_LONG_CSI_CERTIFICATION_TESTS}" = "true" ]; then
+        cp test/e2e/azure-disk/ocp-manifest.yaml ${SHARED_DIR}/${TEST_OCP_CSI_DRIVER_MANIFEST}
+        echo "Using OCP specific manifest ${SHARED_DIR}/${TEST_OCP_CSI_DRIVER_MANIFEST}:"
+        cat ${SHARED_DIR}/${TEST_OCP_CSI_DRIVER_MANIFEST}
+    fi
 else
-    echo "Using azure-file-csi-driver-operator repo"
+    echo "Using azure-disk-csi-driver-operator repo"
     cd /go/src/github.com/openshift/azure-disk-csi-driver-operator
     cp test/e2e/manifest.yaml ${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
 fi
