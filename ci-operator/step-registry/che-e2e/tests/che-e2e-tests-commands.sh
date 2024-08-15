d2b190ddf03e8718aeb6142cae90801ec026bf72
oharan2 <oharan@redhat.com>
2024-08-15 11:11:38 +0300

    Add trap command for debug

--- ci-operator/step-registry/che-e2e/tests/che-e2e-tests-commands.sh
+++ ci-operator/step-registry/che-e2e/tests/che-e2e-tests-commands.sh
@@ -4,12 +4,13 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# Post test execution
+trap 'sleep 2h' SIGINT SIGTERM ERR EXIT TERM
+
 CONSOLE_URL=$(cat $SHARED_DIR/console.url)
 export CONSOLE_URL
 OCP_API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
 export OCP_API_URL
-RBAC_PASS=$(cat $SECRETS_DIR/grc/rbac-pass)
-export RBAC_PASS
 IDP_USER="rosa-admin"
 export IDP_USER
 IDP_LOGIN_PATH=$(cat $SHARED_DIR/api.login)
@@ -17,16 +18,21 @@ IDP_PASSWD=$(echo "${IDP_LOGIN_PATH}" | grep -oP '(?<=-p\s)[^\s]+')
 export IDP_PASSWD
 CHE_NAMESPACE=test-run-interop
 export CHE_NAMESPACE
+POD_NAME=interop-wto
+export POD_NAME
 
 oc delete project $CHE_NAMESPACE || true
 oc new-project $CHE_NAMESPACE
 oc project $CHE_NAMESPACE
 
+mkdir -p "${ARTIFACT_DIR}"/tests
+
 echo "Creating test Pod: interop-wto"
-cat <<EOF | oc create -f -apiVersion: v1
+cat <<EOF | oc create -f -
+apiVersion: v1
 kind: Pod
 metadata:
-  name: interop-wto
+  name: "${POD_NAME}"
   namespace: "${CHE_NAMESPACE}"
 spec:
   volumes:
@@ -63,6 +69,12 @@ spec:
           mountPath: /tmp/e2e/report/
         - mountPath: /dev/shm
           name: dshm
+      command: ["sh"]
+      args:
+        [
+          "-c",
+          "sleep 240",
+        ]
       resources:
         requests:
           memory: "3Gi"
@@ -71,25 +83,25 @@ spec:
           memory: "4Gi"
           cpu: "2"
     # Download results
-    - name: download-reports
-      image: eeacms/rsync
-      imagePullPolicy: IfNotPresent
-      volumeMounts:
-        - name: test-run-results
-          mountPath: /tmp/e2e/report/
-      command: ["sh"]
-      args:
-        [
-          "-c",
-          "sleep 240",
-        ]
+#    - name: download-reports
+#      image: eeacms/rsync
+#      imagePullPolicy: IfNotPresent
+#      volumeMounts:
+#        - name: test-run-results
+#          mountPath: /tmp/e2e/report/
+#      command: ["sh"]
+#      args:
+#        [
+#          "-c",
+#          "sleep 240",
+#        ]
   restartPolicy: Never
 EOF
 
-sleep 120
-
+oc wait --for=condition=ContainersReady=true pod/${POD_NAME} -n $CHE_NAMESPACE --timeout=300s || true
 echo "Extracting logs into artifact dir"
-oc rsync -n $CHE_NAMESPACE interop-wto:/tmp/e2e/report/ $ARTIFACT_DIR -c download-reports
+oc cp interop-wto:/tmp/e2e/report/ $ARTIFACT_DIR/tests -n $CHE_NAMESPACE
+oc rsync -n $CHE_NAMESPACE interop-wto:/tmp/e2e/report/ -c interop-wto-test $ARTIFACT_DIR/tests
 
 ## login for interop
 #if test -f ${SHARED_DIR}/kubeadmin-password
