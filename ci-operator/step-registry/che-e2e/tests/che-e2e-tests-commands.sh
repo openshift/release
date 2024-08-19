e5d4c13c3434a3d84ea923c49771271b6110c273
oharan2 <oharan@redhat.com>
2024-08-19 17:52:08 +0300

    Update verified tests script

--- ci-operator/step-registry/che-e2e/tests/che-e2e-tests-commands.sh
+++ ci-operator/step-registry/che-e2e/tests/che-e2e-tests-commands.sh
@@ -4,9 +4,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-# Post test execution
-trap 'sleep 2h' SIGINT SIGTERM ERR EXIT TERM
-
 CONSOLE_URL=$(cat $SHARED_DIR/console.url)
 export CONSOLE_URL
 OCP_API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
@@ -16,10 +13,8 @@ export IDP_USER
 IDP_LOGIN_PATH=$(cat $SHARED_DIR/api.login)
 IDP_PASSWD=$(echo "${IDP_LOGIN_PATH}" | grep -oP '(?<=-p\s)[^\s]+')
 export IDP_PASSWD
-CHE_NAMESPACE=test-run-interop
 export CHE_NAMESPACE
-POD_NAME=interop-wto
-export POD_NAME
+export TEST_POD_NAME
 
 oc delete project $CHE_NAMESPACE || true
 oc new-project $CHE_NAMESPACE
@@ -32,7 +27,7 @@ cat <<EOF | oc create -f -
 apiVersion: v1
 kind: Pod
 metadata:
-  name: "${POD_NAME}"
+  name: "${TEST_POD_NAME}"
   namespace: "${CHE_NAMESPACE}"
 spec:
   volumes:
@@ -69,12 +64,6 @@ spec:
           mountPath: /tmp/e2e/report/
         - mountPath: /dev/shm
           name: dshm
-      command: ["sh"]
-      args:
-        [
-          "-c",
-          "sleep 240",
-        ]
       resources:
         requests:
           memory: "3Gi"
@@ -83,32 +72,25 @@ spec:
           memory: "4Gi"
           cpu: "2"
     # Download results
-#    - name: download-reports
-#      image: eeacms/rsync
-#      imagePullPolicy: IfNotPresent
-#      volumeMounts:
-#        - name: test-run-results
-#          mountPath: /tmp/e2e/report/
-#      command: ["sh"]
-#      args:
-#        [
-#          "-c",
-#          "sleep 240",
-#        ]
+    - name: download-reports
+      image: eeacms/rsync
+      imagePullPolicy: IfNotPresent
+      volumeMounts:
+        - name: test-run-results
+          mountPath: /tmp/e2e/report/
+      command: ["sh"]
+      args:
+        [
+          "-c",
+          "sleep 240",
+        ]
   restartPolicy: Never
 EOF
 
-oc wait --for=condition=ContainersReady=true pod/${POD_NAME} -n $CHE_NAMESPACE --timeout=300s || true
+oc wait --for=condition=ContainersReady=true pod/${TEST_POD_NAME} -n $CHE_NAMESPACE --timeout=300s || true
 echo "Extracting logs into artifact dir"
-oc cp interop-wto:/tmp/e2e/report/ $ARTIFACT_DIR/tests -n $CHE_NAMESPACE
-oc rsync -n $CHE_NAMESPACE interop-wto:/tmp/e2e/report/ -c interop-wto-test $ARTIFACT_DIR/tests
+oc -n $CHE_NAMESPACE cp ${TEST_POD_NAME}:/tmp/e2e/report -c download-reports "${ARTIFACT_DIR}/tests"
 
-## login for interop
-#if test -f ${SHARED_DIR}/kubeadmin-password
-#then
-#  OCP_CRED_USR="kubeadmin"
-#  OCP_CRED_PSW="$(cat ${SHARED_DIR}/kubeadmin-password)"
-#  oc login ${OCP_API_URL} --username=${OCP_CRED_USR} --password=${OCP_CRED_PSW} --insecure-skip-tls-verify=true
-#else #login for ROSA & Hypershift platforms
-#  eval "$(cat "${SHARED_DIR}/api.login")"
-#fi
+# Try to collect logs with rsync command
+mkdir -p "${ARTIFACT_DIR}"/tests/debug
+oc -n $CHE_NAMESPACE rsync TEST_POD_NAME:/tmp/e2e/report/ -c download-reports "${ARTIFACT_DIR}/tests/debug" || true
