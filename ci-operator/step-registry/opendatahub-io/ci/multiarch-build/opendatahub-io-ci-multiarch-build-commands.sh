6083d1264be698e05289054c98622897a95d0d30
puneetsharma21 <puneet.sharma21@ibm.com>
2024-08-21 05:11:20 -0400

    docker installation

--- ci-operator/step-registry/opendatahub-io/ci/multiarch-build/opendatahub-io-ci-multiarch-build-commands.sh
+++ ci-operator/step-registry/opendatahub-io/ci/multiarch-build/opendatahub-io-ci-multiarch-build-commands.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 set -e
 
+# Install Docker and related components
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-compose-plugin \
+    -y
+
+sudo usermod -aG docker $USER
+newgrp docker
+
 # Create and use a new Buildx builder instance
 docker buildx create --use --name multiarch_builder
 
