954820894bff32c1721e955c8f555bfbae0f7cd9
Stephen Matula <smatula@redhat.com>
2024-08-21 08:00:04 -0400

    Updates for oadp-must-gather

--- ci-operator/step-registry/oadp/must-gather/oadp-must-gather-commands.sh
+++ ci-operator/step-registry/oadp/must-gather/oadp-must-gather-commands.sh
@@ -4,14 +4,28 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-if test -f "${SHARED_DIR}/must-gather-image.sh"
+if test ! -f "${KUBECONFIG}"
 then
-    echo "WARNING, must-gather-image.sh already exists"
-else
-    cat >"${SHARED_DIR}/must-gather-image.sh" <<EOF
-export MUST_GATHER_IMAGE=--image="$OADP_MUST_GATHER_IMAGE"
-EOF
+	echo "No kubeconfig, so no point in calling oadp-must-gather."
+	exit 0
 fi
 
-cat "${SHARED_DIR}/must-gather-image.sh"
+# For disconnected or otherwise unreachable environments, we want to
+# have steps use an HTTP(S) proxy to reach the API server. This proxy
+# configuration file should export HTTP_PROXY, HTTPS_PROXY, and NO_PROXY
+# environment variables, as well as their lowercase equivalents (note
+# that libcurl doesn't recognize the uppercase variables).
+if test -f "${SHARED_DIR}/proxy-conf.sh"
+then
+	# shellcheck disable=SC1090
+	source "${SHARED_DIR}/proxy-conf.sh"
+fi
+
+MUST_GATHER_IMAGE=--image=${OADP_MUST_GATHER_IMAGE}
+
+MUST_GATHER_TIMEOUT=${MUST_GATHER_TIMEOUT:-"15m"}
+
+echo "Running must-gather..."
+mkdir -p ${ARTIFACT_DIR}/must-gather
+oc --insecure-skip-tls-verify adm must-gather $MUST_GATHER_IMAGE --timeout=$MUST_GATHER_TIMEOUT --dest-dir ${ARTIFACT_DIR}/must-gather > ${ARTIFACT_DIR}/must-gather/must-gather.log
 
