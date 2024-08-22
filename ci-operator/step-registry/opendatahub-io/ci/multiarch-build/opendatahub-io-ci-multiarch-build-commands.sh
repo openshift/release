965bf5a7aa1c549a128c36fb5440bca0e1bfc2f6
puneetsharma21 <puneet.sharma21@ibm.com>
2024-08-22 07:56:09 -0400

    podman buildx

--- ci-operator/step-registry/opendatahub-io/ci/multiarch-build/opendatahub-io-ci-multiarch-build-commands.sh
+++ ci-operator/step-registry/opendatahub-io/ci/multiarch-build/opendatahub-io-ci-multiarch-build-commands.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 set -e
 
-# Install Docker and related components using sudo
-sudo yum update -y
-sudo yum install -y \
-    docker \
-    containerd \
-    docker-buildx-plugin
-
 # Create and use a new Buildx builder instance
-docker buildx create --use --name multiarch_builder
+podman buildx create --use --name multiarch_builder
 
 # Build and push multi-architecture image for ppc64le only
-docker buildx build --platform linux/ppc64le \
+podman buildx build --platform linux/ppc64le \
     -t quay.io/opendatahub-io/odh-dashboard:latest \
     -f ./Dockerfile \
     --push .
