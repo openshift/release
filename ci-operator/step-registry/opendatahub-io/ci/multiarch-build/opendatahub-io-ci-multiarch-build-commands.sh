eeb009f6aee7a91096747dfd323cf82baa613ae9
puneetsharma21 <puneet.sharma21@ibm.com>
2024-08-22 23:36:41 -0400

    dind

--- ci-operator/step-registry/opendatahub-io/ci/multiarch-build/opendatahub-io-ci-multiarch-build-commands.sh
+++ ci-operator/step-registry/opendatahub-io/ci/multiarch-build/opendatahub-io-ci-multiarch-build-commands.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+
+# Exit the script on any error
 set -e
 
-# Create and use a new Buildx builder instance
-podman buildx create --use --name multiarch_builder
+# Start Docker daemon in the background
+dockerd-entrypoint.sh &
 
-# Build and push multi-architecture image for ppc64le only
-podman buildx build --platform linux/ppc64le \
-    -t quay.io/opendatahub-io/odh-dashboard:latest \
-    -f ./Dockerfile \
-    --push .
+# Wait for Docker daemon to fully start
+sleep 10
+
+# Enable Buildx for multi-platform builds
+docker buildx create --use
+
+# Log in to the registry
+docker login -u $(cat ${SECRETS_PATH}/${REGISTRY_SECRET_FILE} | jq -r .auths["${REGISTRY_HOST}"].auth | base64 -d | cut -d':' -f1) \
+             -p $(cat ${SECRETS_PATH}/${REGISTRY_SECRET_FILE} | jq -r .auths["${REGISTRY_HOST}"].auth | base64 -d | cut -d':' -f2) \
+             ${REGISTRY_HOST}
+
+# Build and push the multi-architecture image
+docker buildx build --platform ${PLATFORMS} \
+                    -t ${REGISTRY_HOST}/${REGISTRY_ORG}/${IMAGE_REPO}:${IMAGE_TAG} \
+                    --push .
 
