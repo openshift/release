c796d28f5b71d28a3d3d4fe04b065bb3dbca6606
puneetsharma21 <puneet.sharma21@ibm.com>
2024-08-22 23:59:35 -0400

    modify

--- ci-operator/step-registry/opendatahub-io/ci/multiarch-build/opendatahub-io-ci-multiarch-build-commands.sh
+++ ci-operator/step-registry/opendatahub-io/ci/multiarch-build/opendatahub-io-ci-multiarch-build-commands.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Exit the script on any error
-set -e
-
 # Start Docker daemon in the background
 dockerd-entrypoint.sh &
 
@@ -13,12 +10,13 @@ sleep 10
 docker buildx create --use
 
 # Log in to the registry
-docker login -u $(cat ${SECRETS_PATH}/${REGISTRY_SECRET_FILE} | jq -r .auths["${REGISTRY_HOST}"].auth | base64 -d | cut -d':' -f1) \
-             -p $(cat ${SECRETS_PATH}/${REGISTRY_SECRET_FILE} | jq -r .auths["${REGISTRY_HOST}"].auth | base64 -d | cut -d':' -f2) \
-             ${REGISTRY_HOST}
+DOCKER_USER=$(cat "${SECRETS_PATH}/${REGISTRY_SECRET_FILE}" | jq -r ".auths[\"${REGISTRY_HOST}\"].auth" | base64 -d | cut -d':' -f1)
+DOCKER_PASS=$(cat "${SECRETS_PATH}/${REGISTRY_SECRET_FILE}" | jq -r ".auths[\"${REGISTRY_HOST}\"].auth" | base64 -d | cut -d':' -f2)
+
+docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}" "${REGISTRY_HOST}"
 
 # Build and push the multi-architecture image
-docker buildx build --platform ${PLATFORMS} \
-                    -t ${REGISTRY_HOST}/${REGISTRY_ORG}/${IMAGE_REPO}:${IMAGE_TAG} \
+docker buildx build --platform "${PLATFORMS}" \
+                    -t "${REGISTRY_HOST}/${REGISTRY_ORG}/${IMAGE_REPO}:${IMAGE_TAG}" \
                     --push .
 
