2ae1401b4a512b0bb800ccea4709891b034b4117
Punith Kenchappa <pkenchap@in.ibm.com>
2024-08-26 11:01:49 +0530

    MULTIARCH-4696: MAC to multi-arch changes
    Signed-off-by: Punith Kenchappa <pkenchap@in.ibm.com>

--- ci-operator/step-registry/upi/install/powervs/cluster/upi-install-powervs-cluster-commands.sh
+++ ci-operator/step-registry/upi/install/powervs/cluster/upi-install-powervs-cluster-commands.sh
@@ -3,7 +3,7 @@
 set -o nounset
 
 error_handler() {
-  echo "Error: ($1) occurred on $2"
+  echo "Error: (${1}) occurred on (${2})"
 }
 
 trap 'error_handler $? $LINENO' ERR
@@ -54,20 +54,15 @@ echo "${POWERVS_REGION}" > "${SHARED_DIR}"/POWERVS_REGION
 echo "${POWERVS_ZONE}" > "${SHARED_DIR}"/POWERVS_ZONE
 echo "${VPC_REGION}" > "${SHARED_DIR}"/VPC_REGION
 echo "${VPC_ZONE}" > "${SHARED_DIR}"/VPC_ZONE
-# Dev Note: this may be triggering redaction.
-# echo "POWERVS_REGION:- ${POWERVS_REGION}"
-# echo "POWERVS_ZONE:- ${POWERVS_ZONE}"
-# echo "VPC_REGION:- ${VPC_REGION}"
-# echo "VPC_ZONE:- ${VPC_ZONE}"
 export POWERVS_REGION
 export POWERVS_ZONE
 export VPC_REGION
 export VPC_ZONE
 
-if [ -z "$OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE" ]; then
-  echo "OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE is an empty string"
-  exit 1
-fi
+OCP_STREAM="ocp"
+export OCP_STREAM
+# Create a working folder
+mkdir -p "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir
 
 NO_OF_RETRY=${NO_OF_RETRY:-"5"}
 
@@ -87,14 +82,6 @@ function retry {
   done
 }
 
-function setup_upi_workspace(){
-  # Before the workspace is created, download the automation code
-  mkdir -p "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir
-  cd "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir || true
-  curl -sL https://raw.githubusercontent.com/ocp-power-automation/openshift-install-power/"${UPI_AUTOMATION_VERSION}"/openshift-install-powervs -o ./openshift-install-powervs
-  chmod +x ./openshift-install-powervs
-  ./openshift-install-powervs setup -ignore-os-checks
-}
 
 function create_upi_tf_varfile(){
   local workspace_name="${1}"
@@ -103,7 +90,7 @@ function create_upi_tf_varfile(){
   export PUBLIC_KEY_FILE="${CLUSTER_PROFILE_DIR}"/ssh-publickey
   export CLUSTER_DOMAIN="${BASE_DOMAIN}"
   export IBMCLOUD_CIS_CRN="${IBMCLOUD_CIS_CRN}"
-  COREOS_URL=$(/tmp/openshift-install coreos print-stream-json | jq -r '.architectures.ppc64le.artifacts.powervs.formats."ova.gz".disk.location')
+  COREOS_URL=$(openshift-install coreos print-stream-json | jq -r '.architectures.ppc64le.artifacts.powervs.formats."ova.gz".disk.location')
   COREOS_FILE=$(echo "${COREOS_URL}" | sed 's|/| |g' | awk '{print $NF}')
   COREOS_NAME=$(echo "${COREOS_FILE}" | tr '.' '-' | sed 's|-0-powervs-ppc64le-ova-gz|-0-ppc64le-powervs.ova.gz|g')
 
@@ -125,7 +112,7 @@ function create_upi_tf_varfile(){
   PULL_SECRET=$(<"${CLUSTER_PROFILE_DIR}/pull-secret")
   echo "${PULL_SECRET}" > "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir/pull-secret.txt
 
-  cat <<EOF >${IBMCLOUD_HOME_FOLDER}/ocp-install-dir/var-mac-upi.tfvars
+  cat <<EOF >${IBMCLOUD_HOME_FOLDER}/ocp-install-dir/var-multi-arch-upi.tfvars
 ibmcloud_region     = "${POWERVS_REGION}"
 ibmcloud_zone       = "${POWERVS_ZONE}"
 service_instance_id = "${POWERVS_SERVICE_INSTANCE_ID}"
@@ -135,7 +122,7 @@ rhcos_import_image_filename     = "${COREOS_NAME}"
 rhcos_import_image_storage_type = "tier1"
 system_type         = "s922"
 cluster_domain      = "${CLUSTER_DOMAIN}"
-cluster_id_prefix   = "rh-ci"
+cluster_id_prefix   = "multi-arch-p2"
 bastion   = { memory = "16", processors = "1", "count" = 1 }
 bootstrap = { memory = "16", processors = "0.5", "count" = 1 }
 master    = { memory = "16", processors = "0.5", "count" = 3 }
@@ -153,8 +140,8 @@ ibm_cloud_cis_crn = "${IBMCLOUD_CIS_CRN}"
 ibm_cloud_tgw              = "${workspace_name}-tg"
 EOF
 
-  cp "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir/var-mac-upi.tfvars "${SHARED_DIR}"/var-mac-upi.tfvars
-  cat "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir/var-mac-upi.tfvars
+  cp "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir/var-multi-arch-upi.tfvars "${SHARED_DIR}"/var-multi-arch-upi.tfvars
+  cat "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir/var-multi-arch-upi.tfvars
 }
 
 function create_upi_powervs_cluster() {
@@ -162,7 +149,8 @@ function create_upi_powervs_cluster() {
   # Dev Note: https://github.com/ocp-power-automation/openshift-install-power/blob/devel/openshift-install-powervs#L767C1-L767C145
   # May trigger the redaction
   OUTPUT="yes"
-  ./openshift-install-powervs create -var-file var-mac-upi.tfvars -ignore-os-checks -verbose | sed '/.*client-certificate-data*/d; /.*token*/d; /.*client-key-data*/d; /- name: /d; /Login to the console with user/d' | \
+  /tmp/terraform apply -auto-approve -var-file var-multi-arch-upi.tfvars | \
+      sed '/.*client-certificate-data*/d; /.*token*/d; /.*client-key-data*/d; /- name: /d; /Login to the console with user/d' | \
     while read LINE
     do
         if [[ "${LINE}" == *"BEGIN RSA PRIVATE KEY"* ]]
@@ -178,12 +166,11 @@ function create_upi_powervs_cluster() {
         OUTPUT="yes"
         fi
     done || true
-  cp "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir/automation/terraform.tfstate "${SHARED_DIR}"/terraform-mac-upi.tfstate
-  ./openshift-install-powervs output > "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir/mac-upi-output
-  ./openshift-install-powervs access-info > "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir/mac-upi-access-info
-  cd automation/ || true
-  ../terraform output -raw -no-color bastion_private_ip | tr -d '"' > "${SHARED_DIR}"/BASTION_PRIVATE_IP
-  ../terraform output -raw -no-color bastion_public_ip | tr -d '"' > "${SHARED_DIR}"/BASTION_PUBLIC_IP
+  echo "POWERVS CLUSTER IS COMPLETE, log is redacted"
+  cp "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir/automation/terraform.tfstate "${SHARED_DIR}"/terraform-multi-arch-upi.tfstate
+  
+  /tmp/terraform output -raw -no-color bastion_private_ip | tr -d '"' > "${SHARED_DIR}"/BASTION_PRIVATE_IP
+  /tmp/terraform output -raw -no-color bastion_public_ip | tr -d '"' > "${SHARED_DIR}"/BASTION_PUBLIC_IP
   cd .. || true
 
   BASTION_PUBLIC_IP=$(<"${SHARED_DIR}/BASTION_PUBLIC_IP")
@@ -203,25 +190,6 @@ function ic() {
   HOME=${IBMCLOUD_HOME_FOLDER} ibmcloud "$@"
 }
 
-function setup_jq() {
-  if [ -z "$(command -v jq)" ]
-  then
-    echo "jq is not installed, proceed to installing jq"
-    curl -L "https://github.com/jqlang/jq/releases/download/jq-${JQ_VERSION}/jq-linux64" -o /tmp/jq && chmod +x /tmp/jq
-  fi
-}
-
-function setup_openshift_installer() {
-  OCP_STREAM="ocp"
-  ocp_target_version="candidate-4.15"
-  echo "proceed to re-installing openshift-install"
-  curl -L "https://mirror.openshift.com/pub/openshift-v4/multi/clients/${OCP_STREAM}/${ocp_target_version}/amd64/openshift-install-linux.tar.gz" -o "${IBMCLOUD_HOME_FOLDER}"/openshift-install.tar.gz
-  tar -xf "${IBMCLOUD_HOME_FOLDER}"/openshift-install.tar.gz -C "${IBMCLOUD_HOME_FOLDER}"
-  cp "${IBMCLOUD_HOME_FOLDER}"/openshift-install /tmp/
-  OCP_VERSION="$(/tmp/openshift-install version | head -n 1 | awk '{print $2}')"
-  export OCP_VERSION
-  export OCP_STREAM
-}
 
 function fix_user_permissions() {
   if ! whoami &> /dev/null; then
@@ -416,32 +384,6 @@ function create_powervs_workspace() {
   done
 }
 
-function create_powervs_private_network() {
-  local CRN="${1}"
-
-  echo "Creating the Network"
-  echo "PowerVS Target CRN is: ${CRN}"
-  retry "ic pi workspace target ${CRN}"
-
-  ##Create a Power Network using the CRN
-  retry "ic pi subnet create ocp-net --cidr-block 192.168.200.0/24 --net-type private --dns-servers 9.9.9.9 --gateway 192.168.200.1 --ip-range 192.168.200.10-192.168.200.250  --jumbo"
-}
-
-function import_centos_image() {
-  local CRN="${1}"
-
-  # The CentOS-Stream-9 image is stock-image on PowerVS.
-  # This image is available across all PowerVS workspaces.
-  # The VMs created using this image are used in support of ignition on PowerVS.
-  echo "Creating the Centos Stream Image"
-  echo "PowerVS Target CRN is: ${CRN}"
-  retry "ic pi workspace target ${CRN}"
-  retry "ic pi image list"
-
-  ##Import the Centos8 image
-  retry "ic pi image create CentOS-Stream-9 --json"
-  echo "Import image status is: $?"
-}
 
 function create_transit_gateway() {
   local workspace_name="${1}"
@@ -459,22 +401,22 @@ function create_transit_gateway() {
   TGW_STATE=""
   while [ -z "${TGW_STATE}" ]
   do
-  TGW_COUNTER=$((TGW_COUNTER+1)) 
-  TEMP_STATE="$(ic tg gw "${TGW_ID}" --output json 2> /dev/null | jq -r '.status')"
-  echo "tg state: ${TEMP_STATE}"
-  echo ""
-  if [ "${TEMP_STATE}" == "available" ]
-  then
-    TGW_STATE="available"
-  elif [[ $TGW_COUNTER -ge 20 ]]
-  then
-    TGW_STATE="pending"
-    echo "Transit Gateway has not come up in available state... login and verify"
-    exit 2
-  else
-    echo "Waiting for Transit Gateway to become available... [30 seconds]"
-    sleep 30
-  fi
+    TGW_COUNTER=$((TGW_COUNTER+1)) 
+    TEMP_STATE="$(ic tg gw "${TGW_ID}" --output json 2> /dev/null | jq -r '.status')"
+    echo "tg state: ${TEMP_STATE}"
+    echo ""
+    if [ "${TEMP_STATE}" == "available" ]
+    then
+      TGW_STATE="available"
+    elif [[ $TGW_COUNTER -ge 20 ]]
+    then
+      TGW_STATE="pending"
+      echo "Transit Gateway has not come up in available state... login and verify"
+      exit 2
+    else
+      echo "Waiting for Transit Gateway to become available... [30 seconds]"
+      sleep 30
+    fi
   done
 }
 
@@ -490,22 +432,22 @@ function create_vpc() {
   VPC_STATE=""
   while [ -z "${VPC_STATE}" ]
   do
-  VPC_COUNTER=$((VPC_COUNTER+1)) 
-  TEMP_STATE="$(ic is vpc "${vpc_name}" --output json 2> /dev/null | jq -r '.status')"
-  echo "vpc state: ${TEMP_STATE}"
-  echo ""
-  if [ "${TEMP_STATE}" == "available" ]
-  then
-    VPC_STATE="available"
-  elif [[ $VPC_COUNTER -ge 20 ]]
-  then
-    VPC_STATE="pending"
-    echo "VPC has not come up in available state... login and verify"
-    exit 2
-  else
-    echo "Waiting for VPC to become available... [30 seconds]"
-    sleep 30
-  fi
+    VPC_COUNTER=$((VPC_COUNTER+1)) 
+    TEMP_STATE="$(ic is vpc "${vpc_name}" --output json 2> /dev/null | jq -r '.status')"
+    echo "vpc state: ${TEMP_STATE}"
+    echo ""
+    if [ "${TEMP_STATE}" == "available" ]
+    then
+      VPC_STATE="available"
+    elif [[ $VPC_COUNTER -ge 20 ]]
+    then
+      VPC_STATE="pending"
+      echo "VPC has not come up in available state... login and verify"
+      exit 2
+    else
+      echo "Waiting for VPC to become available... [30 seconds]"
+      sleep 30
+    fi
   done
 
   ##Fetch VPC CRN
@@ -533,22 +475,22 @@ function create_vpc() {
   TGW_PER_STATE=""
   while [ -z "${TGW_PER_STATE}" ]
   do
-  TGW_PER_COUNTER=$((TGW_PER_COUNTER+1)) 
-  TEMP_STATE="$(ic tg connection "${TGW_ID}" "${TGW_PER_ID}" --output json 2> /dev/null | jq -r '.status')"
-  echo "tg connection state: ${TEMP_STATE}"
-  echo ""
-  if [ "${TEMP_STATE}" == "attached" ]
-  then
-    TGW_PER_STATE="attached"
-  elif [[ $TGW_PER_COUNTER -ge 20 ]]
-  then
-    TGW_PER_STATE="pending"
-    echo "PER has not attached to Transit Gateway... login and verify"
-    exit 2
-  else
-    echo "Waiting for PER to attached to Transit Gateway... [30 seconds]"
-    sleep 30
-  fi
+    TGW_PER_COUNTER=$((TGW_PER_COUNTER+1)) 
+    TEMP_STATE="$(ic tg connection "${TGW_ID}" "${TGW_PER_ID}" --output json 2> /dev/null | jq -r '.status')"
+    echo "tg connection state: ${TEMP_STATE}"
+    echo ""
+    if [ "${TEMP_STATE}" == "attached" ]
+    then
+      TGW_PER_STATE="attached"
+    elif [[ $TGW_PER_COUNTER -ge 20 ]]
+    then
+      TGW_PER_STATE="pending"
+      echo "PER has not attached to Transit Gateway... login and verify"
+      exit 2
+    else
+      echo "Waiting for PER to attached to Transit Gateway... [30 seconds]"
+      sleep 30
+    fi
   done
 
   ##Attach the VPC to the TG
@@ -562,22 +504,22 @@ function create_vpc() {
   TGW_VPC_STATE=""
   while [ -z "${TGW_VPC_STATE}" ]
   do
-  TGW_VPC_COUNTER=$((TGW_VPC_COUNTER+1)) 
-  TEMP_STATE="$(ic tg connection "${TGW_ID}" "${TGW_VPC_ID}" --output json 2> /dev/null | jq -r '.status')"
-  echo "tg connection state: ${TEMP_STATE}"
-  echo ""
-  if [ "${TEMP_STATE}" == "attached" ]
-  then
-    TGW_VPC_STATE="attached"
-  elif [[ $TGW_VPC_COUNTER -ge 20 ]]
-  then
-    TGW_VPC_STATE="pending"
-    echo "VPC has not attached to Transit Gateway... login and verify"
-    exit 2
-  else
-    echo "Waiting for VPC to attached to Transit Gateway... [30 seconds]"
-    sleep 30
-  fi
+    TGW_VPC_COUNTER=$((TGW_VPC_COUNTER+1)) 
+    TEMP_STATE="$(ic tg connection "${TGW_ID}" "${TGW_VPC_ID}" --output json 2> /dev/null | jq -r '.status')"
+    echo "tg connection state: ${TEMP_STATE}"
+    echo ""
+    if [ "${TEMP_STATE}" == "attached" ]
+    then
+      TGW_VPC_STATE="attached"
+    elif [[ $TGW_VPC_COUNTER -ge 20 ]]
+    then
+      TGW_VPC_STATE="pending"
+      echo "VPC has not attached to Transit Gateway... login and verify"
+      exit 2
+    else
+      echo "Waiting for VPC to attached to Transit Gateway... [30 seconds]"
+      sleep 30
+    fi
   done
 }
 
@@ -589,20 +531,18 @@ case "$CLUSTER_TYPE" in
   mkdir -p "${IBMCLOUD_HOME_FOLDER}"
   export PATH=$PATH:/tmp:/"${IBMCLOUD_HOME_FOLDER}"
 
-  setup_openshift_installer
   # Saving the OCP VERSION so we can use in a subsequent deprovision
   echo "${OCP_VERSION}" > "${SHARED_DIR}"/OCP_VERSION
 
-  setup_jq
   setup_ibmcloud_cli
 
   IBMCLOUD_API_KEY="$(< "${CLUSTER_PROFILE_DIR}/ibmcloud-api-key")"
   export IBMCLOUD_API_KEY
 
-  # Generates a workspace name like rdr-mac-upi-4-14-au-syd-n1
+  # Generates a workspace name like rdr-multi-arch-upi-4-14-au-syd-n1
   # this keeps the workspace unique
   CLEAN_VERSION=$(echo "${OCP_VERSION}" | sed 's/\([0-9]*\.[0-9]*\).*/\1/' | tr '.' '-')
-  WORKSPACE_NAME=rdr-mac-p2-"${CLEAN_VERSION}"-"${POWERVS_ZONE}"
+  WORKSPACE_NAME=rdr-multi-arch-p2-"${CLEAN_VERSION}"-"${POWERVS_ZONE}"
   VPC_NAME="${WORKSPACE_NAME}"-vpc
   echo "${WORKSPACE_NAME}" > "${SHARED_DIR}"/WORKSPACE_NAME
 
@@ -619,11 +559,15 @@ case "$CLUSTER_TYPE" in
   echo "${RESOURCE_GROUP}" > "${SHARED_DIR}"/RESOURCE_GROUP
 
   create_powervs_workspace "${WORKSPACE_NAME}"
-  create_powervs_private_network "${CRN}"
-  import_centos_image "${CRN}"
+  echo "PI: Create Network"
+  ic pi subnet create ocp-net --cidr-block 192.168.200.0/24 --net-type private --dns-servers 9.9.9.9 --gateway 192.168.200.1 --ip-range 192.168.200.10-192.168.200.250 --mtu 9100
+  # The CentOS-Stream-9 image is stock-image on PowerVS.
+  echo "PI: Creating the Centos Stream Image"
+  ic pi image create CentOS-Stream-9 --json
+  echo "Import image status is: $?"
+  setup_terraform_cli
   create_transit_gateway "${WORKSPACE_NAME}"
   create_vpc "${WORKSPACE_NAME}" "${VPC_NAME}"
-  setup_upi_workspace
   create_upi_tf_varfile "${WORKSPACE_NAME}"
   fix_user_permissions
   create_upi_powervs_cluster
