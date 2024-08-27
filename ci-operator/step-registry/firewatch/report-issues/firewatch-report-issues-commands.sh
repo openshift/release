251afd192a59cffcd8f699619f3f66b74f7ac687
Caleb Evans <caevans@redhat.com>
2024-08-27 09:33:09 -0600

    Add logic to use new private gcs bucket functionality (#55980)

--- ci-operator/step-registry/firewatch/report-issues/firewatch-report-issues-commands.sh
+++ ci-operator/step-registry/firewatch/report-issues/firewatch-report-issues-commands.sh
@@ -9,7 +9,10 @@ firewatch jira-config-gen --token-path "${FIREWATCH_JIRA_API_TOKEN_PATH}" --serv
 
 report_command="firewatch report"
 
-# If the user has specified to fail with test failures, then add the --fail-with-test-failures flag
+if [ "${FIREWATCH_PRIVATE_DECK,,}" = "true" ]; then
+    report_command+=" --gcs-bucket qe-private-deck --gcs-creds-file /tmp/secrets/private-deck/creds.json"
+fi
+
 if [ "${FIREWATCH_FAIL_WITH_TEST_FAILURES,,}" = "true" ]; then
     report_command+=" --fail-with-test-failures"
 fi
