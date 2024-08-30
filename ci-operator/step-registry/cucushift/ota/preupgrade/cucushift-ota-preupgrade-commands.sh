d515daa7661ee9bfb8176680cee7f33ec3e67c1c
Jia Liu <jiajliu@redhat.com>
2024-08-30 10:34:31 +0800

    drop signaturestore cases (#56115)

--- ci-operator/step-registry/cucushift/ota/preupgrade/cucushift-ota-preupgrade-commands.sh
+++ ci-operator/step-registry/cucushift/ota/preupgrade/cucushift-ota-preupgrade-commands.sh
@@ -369,46 +369,6 @@ function pre-OCP-53907(){
     return 0
 }
 
-function pre-OCP-69968(){
-    echo "Test Start: ${FUNCNAME[0]}"
-    local spec testurl="http://examplefortest.com"
-    spec=$(oc get clusterversion version -ojson|jq -r '.spec')
-    if [[ "${spec}" == *"signatureStores"* ]]; then
-        echo "There should not be signatureStores by default!"
-        return 1
-    fi
-    if ! oc patch clusterversion version --type merge -p "{\"spec\": {\"signatureStores\": [{\"url\": \"${testurl}\"}]}}"; then
-        echo "Fail to patch clusterversion signatureStores!"
-        return 1
-    fi
-    signstore=$(oc get clusterversion version -ojson|jq -r '.spec.signatureStores[].url')
-    if [[ "${signstore}" != "${testurl}" ]]; then
-        echo "Fail to set clusterversion signatureStores!"
-        return 1
-    fi
-    return 0
-}
-
-function pre-OCP-69948(){
-    echo "Test Start: ${FUNCNAME[0]}"
-    local spec teststore="https://mirror.openshift.com/pub/openshift-v4/signatures/openshift/release"
-    spec=$(oc get clusterversion version -ojson|jq -r '.spec')
-    if [[ "${spec}" == *"signatureStores"* ]]; then
-        echo "There should not be signatureStores by default!"
-        return 1
-    fi
-    if ! oc patch clusterversion version --type merge -p "{\"spec\": {\"signatureStores\": [{\"url\": \"${teststore}\"}]}}"; then
-        echo "Fail to patch clusterversion signatureStores!"
-        return 1
-    fi
-    signstore=$(oc get clusterversion version -ojson|jq -r '.spec.signatureStores[].url')
-    if [[ "${signstore}" != "${teststore}" ]]; then
-        echo "Fail to set clusterversion signatureStores!"
-        return 1
-    fi
-    return 0
-}
-
 function pre-OCP-56083(){
     echo "Pre Test Start: OCP-56083"
     echo "Unset the upgrade channel"
