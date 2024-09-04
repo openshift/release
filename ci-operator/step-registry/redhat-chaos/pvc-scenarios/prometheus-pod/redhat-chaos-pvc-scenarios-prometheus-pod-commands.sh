a261cd21ee8035dbd86533ea36cc83168784dc03
Paige Patton <64206430+paigerube14@users.noreply.github.com>
2024-09-04 12:35:26 -0400

    Adding new es variables (#56144)
    * adding new es variables
    
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED
    
    * adding export separate line
    
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED
    
    * shell changes
    
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED
    
    * adding test of krkn lib version
    
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED
    
    * adding test folder
    
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED
    
    * adding cat log file
    
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED
    
    * adding no test job
    
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED
    
    ---------
    
    Co-authored-by: Auto User <auto@users.noreply.github.com>

--- ci-operator/step-registry/redhat-chaos/pvc-scenarios/prometheus-pod/redhat-chaos-pvc-scenarios-prometheus-pod-commands.sh
+++ ci-operator/step-registry/redhat-chaos/pvc-scenarios/prometheus-pod/redhat-chaos-pvc-scenarios-prometheus-pod-commands.sh
@@ -41,11 +41,14 @@ echo "kubeconfig loc $$KUBECONFIG"
 echo "Using the flattened version of kubeconfig"
 oc config view --flatten > /tmp/config
 
-ES_PASSWORD=$(cat "/secret/es/password")
-ES_USERNAME=$(cat "/secret/es/username")
 
-export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
-export ELASTIC_INDEX=krkn_chaos_ci
+ES_PASSWORD=$(cat "/secret/es/password" || "")
+ES_USERNAME=$(cat "/secret/es/username" || "")
+
+export ES_PASSWORD
+export ES_USERNAME
+
+export ELASTIC_SERVER="https://search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
 
 export KUBECONFIG=/tmp/config
 export KRKN_KUBE_CONFIG=$KUBECONFIG
