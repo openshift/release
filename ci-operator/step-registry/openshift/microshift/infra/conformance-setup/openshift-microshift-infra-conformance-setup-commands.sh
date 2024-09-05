cd1ca4ecb3d51c9a96527f71afebc3d23b49d1e5
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2024-09-05 18:37:53 +0200

    USHIFT-4124: Add microshift git clone sourcing

--- ci-operator/step-registry/openshift/microshift/infra/conformance-setup/openshift-microshift-infra-conformance-setup-commands.sh
+++ ci-operator/step-registry/openshift/microshift/infra/conformance-setup/openshift-microshift-infra-conformance-setup-commands.sh
@@ -6,8 +6,21 @@ curl https://raw.githubusercontent.com/openshift/release/master/ci-operator/step
 source /tmp/ci-functions.sh
 ci_script_prologue
 
+if "${SRC_FROM_GIT}"; then
+  mkdir -p /go/src/github.com/openshift
+  branch=$(echo ${JOB_SPEC} | jq -r '.refs.base_ref')
+  # MicroShift repo is recent enough to use main instead of master.
+  if [ "${branch}" == "master" ]; then
+    branch="main"
+  fi
+  git clone https://github.com/openshift/microshift -b $branch /go/src/github.com/openshift/microshift
+fi
+
 cp /go/src/github.com/openshift/microshift/origin/skip.txt "${SHARED_DIR}/conformance-skip.txt"
 cp "${SHARED_DIR}/conformance-skip.txt" "${ARTIFACT_DIR}/conformance-skip.txt"
+
+#TODo temp
+ls -lh ${SHARED_DIR}
 
 # Disable workload partitioning for annotated pods to avoid throttling.
 ssh "${INSTANCE_PREFIX}" "sudo sed -i 's/resources/#&/g' /etc/crio/crio.conf.d/11-microshift-ovn.conf"
