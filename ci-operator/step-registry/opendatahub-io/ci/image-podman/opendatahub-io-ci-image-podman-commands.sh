5e8682712136a7cb28277d98b77be2ec6a9f4333
Md Shafi Hussain <Md.Shafi.Hussain@ibm.com>
2024-09-16 10:22:36 -0400

    Check buildah

--- ci-operator/step-registry/opendatahub-io/ci/image-podman/opendatahub-io-ci-image-podman-commands.sh
+++ ci-operator/step-registry/opendatahub-io/ci/image-podman/opendatahub-io-ci-image-podman-commands.sh
@@ -5,10 +5,13 @@ set -o errexit
 set -o pipefail
 set -x
 
-su -c '
-#!/bin/bash
-set -x
+#su -c '
+##!/bin/bash
+#set -x
+#
+#dnf install -y podman
+#podman pull quay.io/opendatahub/opendatahub-operator
+#' - newRoot
 
-dnf install -y podman
-podman pull quay.io/opendatahub/opendatahub-operator
-' - newRoot
+echo "CHECK: Does buildah work as non-root user with securityContext unprivileged?"
+buildah pull alpine:latest
