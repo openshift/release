c8dfd263fa0a788a4e17de8f92cf10b4ec269e69
Eran Cohen <eranco@redhat.com>
2024-09-16 11:51:50 +0300

    Rename IBIO log files suffix (#56740)
    Signed-off-by: Eran Cohen <eranco@redhat.com>

--- ci-operator/step-registry/image-based-install-operator/gather/image-based-install-operator-gather-commands.sh
+++ ci-operator/step-registry/image-based-install-operator/gather/image-based-install-operator-gather-commands.sh
@@ -24,8 +24,8 @@ mkdir /root/ibio-gather
 oc get baremetalhost ostest-extraworker-0 -n openshift-machine-api -o yaml > /root/ibio-gather/baremetalhost.yaml
 oc get clusterdeployment ibi-cluster -n ibi-cluster -o yaml > /root/ibio-gather/clusterdeployment.yaml
 oc get imageclusterinstall ibi-cluster -n ibi-cluster -o yaml > /root/ibio-gather/imageclusterinstall.yaml
-oc logs --tail=-1 -l app=image-based-install-operator -n image-based-install-operator -c manager > /root/ibio-gather/image-based-install-operator-manager.yaml
-oc logs --tail=-1 -l app=image-based-install-operator -n image-based-install-operator -c server > /root/ibio-gather/image-based-install-operator-server.yaml
+oc logs --tail=-1 -l app=image-based-install-operator -n image-based-install-operator -c manager > /root/ibio-gather/image-based-install-operator-manager.logs
+oc logs --tail=-1 -l app=image-based-install-operator -n image-based-install-operator -c server > /root/ibio-gather/image-based-install-operator-server.logs
 
 EOF
 
