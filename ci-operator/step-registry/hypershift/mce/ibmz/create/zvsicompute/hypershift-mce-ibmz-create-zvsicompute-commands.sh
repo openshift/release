6c7a0f1d79c3954939489ffc9a9f1da721a8eea9
DAMISETTI-VEERABHADRARAO <damisetti.veerabhadrarao@ibm.com>
2024-09-17 22:12:56 +0530

    MULTIARCH-4994: Updated the script name for proxy server
    Signed-off-by: DAMISETTI-VEERABHADRARAO <damisetti.veerabhadrarao@ibm.com>

--- ci-operator/step-registry/hypershift/mce/ibmz/create/zvsicompute/hypershift-mce-ibmz-create-zvsicompute-commands.sh
+++ ci-operator/step-registry/hypershift/mce/ibmz/create/zvsicompute/hypershift-mce-ibmz-create-zvsicompute-commands.sh
@@ -340,7 +340,7 @@ echo "Getting management cluster basedomain to allow traffic to proxy server"
 mgmt_domain=$(oc whoami --show-server | awk -F'.' '{print $(NF-1)"."$NF}' | cut -d':' -f1)
 
 echo "Getting the proxy setup script"
-cp hosted-control-plane/.archive/trigger_pxeboot.sh $HOME/setup_proxy.sh
+cp hosted-control-plane/.archive/setup_proxy.sh $HOME/setup_proxy.sh
 
 sed -i "s|MGMT_DOMAIN|${mgmt_domain}|" $HOME/setup_proxy.sh 
 sed -i "s|HCP_DOMAIN|${hcp_domain}|" $HOME/setup_proxy.sh 
