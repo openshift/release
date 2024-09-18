79012bfe3291891bbb5ddcf0ac3c9f2975df92a0
Auto User <auto@users.noreply.github.com>
2024-09-18 10:56:28 -0400

    all node scenarios
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED

---
+++ ci-operator/step-registry/ibmcloud/provision/blockstorage/ibmcloud-provision-blockstorage-commands.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# IBM Cloud CLI login
+function ibmcloud_login {
+    export IBMCLOUD_CLI=ibmcloud
+    export IBMCLOUD_HOME=/output   
+    region="${LEASED_RESOURCE}"
+    export region
+    "${IBMCLOUD_CLI}" config --check-version=false
+    echo "Try to login..."
+    "${IBMCLOUD_CLI}" login -r ${region} --apikey @"${CLUSTER_PROFILE_DIR}/ibmcloud-api-key"
+    
+}
+
+function find_resource_group() {
+
+    resource_group=$(jq -r .ibmcloud.resourceGroupName ${SHARED_DIR}/metadata.json)
+    echo "create resource group ... ${resource_group}"
+    ibmcloud target -g ${resource_group}
+}
+
+function update_volume() {
+
+    volume_id=$(ibmcloud is instance $1 --output json | jq -r '.volume_attachments[0].volume.id')
+    ibmcloud is volume-update $volume_id  --profile 5iops-tier 
+
+    ibmcloud is volume $volume_id
+}
+
+ibmcloud_login
+find_resource_group
+
+nodes=$(oc get nodes -l $NODE_LABEL --no-headers | awk '{print $1}')
+
+for node in $nodes; do 
+    echo "node $node"
+    update_volume $node
+done
