3184c74a0c06e53f56e925e9aa913c77f951cdb9
davdhacs <105243888+davdhacs@users.noreply.github.com>
2024-09-18 09:54:59 -0600

    remove extra flags

--- ci-operator/step-registry/stackrox/perf-scale/cluster-density-v2/stackrox-perf-scale-cluster-density-v2-commands.sh
+++ ci-operator/step-registry/stackrox/perf-scale/cluster-density-v2/stackrox-perf-scale-cluster-density-v2-commands.sh
@@ -57,6 +57,3 @@ if [[ "${ENABLE_LOCAL_INDEX}" == "true" ]]; then
     metrics_folder_name=$(find . -maxdepth 1 -type d -name 'collected-metric*' | head -n 1)
     cp -r "${metrics_folder_name}" "${ARTIFACT_DIR}/"
 fi
-
-pwd
-ls -latr *
