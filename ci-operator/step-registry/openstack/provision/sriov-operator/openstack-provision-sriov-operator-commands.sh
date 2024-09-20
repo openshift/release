425d6e5dc1d134181e412fb6ca53b0cacc9e45c9
Martin André <m.andre@redhat.com>
2024-09-20 14:17:09 +0200

    OpenStack: Move creation of SriovOperatorConfig to installation step
    As otherwise this blocks the installation of the operator and the step
    fails.

--- ci-operator/step-registry/openstack/provision/sriov-operator/openstack-provision-sriov-operator-commands.sh
+++ ci-operator/step-registry/openstack/provision/sriov-operator/openstack-provision-sriov-operator-commands.sh
@@ -178,6 +178,24 @@ EOF
         sleep 10
     done
 
+    # This is only needed on ocp 4.16+
+    # introduced https://github.com/openshift/sriov-network-operator/pull/887
+    # u/s https://github.com/k8snetworkplumbingwg/sriov-network-operator/pull/617
+    SRIOV_OPERATOR_CONFIG=$(
+        oc create -f - -o jsonpath='{.metadata.name}' <<EOF
+apiVersion: sriovnetwork.openshift.io/v1
+kind: SriovOperatorConfig
+metadata:
+  name: default
+  namespace: openshift-sriov-network-operator
+spec:
+  enableInjector: true
+  enableOperatorWebhook: true
+  logLevel: 2
+EOF
+    )
+    echo "Created \"$SRIOV_OPERATOR_CONFIG\" SriovOperatorConfig"
+
     if [ -n "${FOUND_SNO:-}" ] ; then
         wait_for_sriov_pods
         wait_for_sriov_network_node_state
