f641ca8a9f9f0987e847a34e445f634c6a6af796
lwan <lwan@redhat.com>
2024-09-20 20:00:53 +0800

    Add subscription channel override

--- ci-operator/step-registry/multiarch/tuning-operator/e2e/olm-kustomize/multiarch-tuning-operator-e2e-olm-kustomize-commands.sh
+++ ci-operator/step-registry/multiarch/tuning-operator/e2e/olm-kustomize/multiarch-tuning-operator-e2e-olm-kustomize-commands.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 export CLEANUP=true
-# execute the slow tests only on one of the runs (choose randomly)
-if [ $((RANDOM % 100)) -lt 50 ]; then
-  export GINKGO_EXTRA_ARGS="--skip=\\[slow-30\\]"
-  echo "[SKIP] Skipping slow tests in the OLM deployment"
-fi
 USE_OLM=true ./hack/deploy-and-e2e.sh
-if [ "${GINKGO_EXTRA_ARGS}" == "" ]; then
-  export GINKGO_EXTRA_ARGS="--skip=\\[slow-30\\]"
-  echo "[SKIP] Skipping slow tests in the Kustomize deployment"
-else
-  GINKGO_EXTRA_ARGS=""
-fi
 USE_OLM=false ./hack/deploy-and-e2e.sh
