20b8f8d04e6e24f0a000515bb66dfda2652cd359
Vivek Shakya <vshakya@vshakya-thinkpadt14gen4.remote.csb>
2024-09-26 07:56:21 -0400

    update script

--- ci-operator/step-registry/web-terminal-operator/lp-rosa-tests/web-terminal-operator-lp-rosa-tests-commands.sh
+++ ci-operator/step-registry/web-terminal-operator/lp-rosa-tests/web-terminal-operator-lp-rosa-tests-commands.sh
@@ -23,8 +23,6 @@ oc delete project $CHE_NAMESPACE || true
 oc new-project $CHE_NAMESPACE
 oc project $CHE_NAMESPACE
 
-mkdir -p "${ARTIFACT_DIR}"/tests
-
 echo "Creating test Pod: interop-wto"
 cat <<EOF | oc create -f -
 apiVersion: v1
@@ -58,15 +56,13 @@ spec:
           value: "${IDP_USER}"
         - name: TS_SELENIUM_OCP_PASSWORD
           value: "${IDP_PASSWD}"
-        - name: DELETE_WORKSPACE_ON_FAILED_TEST
-          value: "true"
         - name: TS_OCP_LOGIN_PAGE_PROVIDER_TITLE
           value: "rosa-htpasswd"
         - name: SAVE_JUNIT_DATA
           value: "${SAVE_JUNIT_DATA}"
       volumeMounts:
         - name: test-run-results
-          mountPath: /tmp/e2e/report
+          mountPath: /tmp/e2e/report/
         - mountPath: /dev/shm
           name: dshm
       resources:
@@ -82,24 +78,15 @@ spec:
       imagePullPolicy: IfNotPresent
       volumeMounts:
         - name: test-run-results
-          mountPath: /tmp/e2e/report
+          mountPath: /tmp/e2e/report/
       command: ["sh"]
       args:
         [
           "-c",
-          "sleep 600",
+          "sleep 400",
         ]
   restartPolicy: Never
 EOF
-
-# oc wait --for=condition=ContainersReady=true pod/${TEST_POD_NAME} -n $CHE_NAMESPACE --timeout=300s || true
-# echo "Extracting logs into artifact dir"
-# oc -n $CHE_NAMESPACE cp ${TEST_POD_NAME}:/tmp/e2e/report -c download-reports "${ARTIFACT_DIR}/tests"
-
-# # Try to collect logs with rsync command
-# mkdir -p "${ARTIFACT_DIR}"/tests/debug
-# oc -n $CHE_NAMESPACE rsync TEST_POD_NAME:/tmp/e2e/report -c download-reports "${ARTIFACT_DIR}/tests/debug" || true
-
 
 mkdir -p "${ARTIFACT_DIR}"/tests
 echo "Waiting for copy synchronization between shared volumes"
