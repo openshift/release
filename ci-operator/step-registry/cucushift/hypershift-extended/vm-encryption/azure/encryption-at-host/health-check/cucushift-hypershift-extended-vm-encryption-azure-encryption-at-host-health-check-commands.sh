3fe0af2a2339968109c3085374db8376aa8ac3e4
Feilian Xie <fxie@redhat.com>
2024-10-01 01:39:05 +0800

    Support toggling --encryption-at-host. (#56602)
    Signed-off-by: Feilian Xie <fxie@redhat.com>

--- ci-operator/step-registry/cucushift/hypershift-extended/vm-encryption/azure/encryption-at-host/health-check/cucushift-hypershift-extended-vm-encryption-azure-encryption-at-host-health-check-commands.sh
+++ ci-operator/step-registry/cucushift/hypershift-extended/vm-encryption/azure/encryption-at-host/health-check/cucushift-hypershift-extended-vm-encryption-azure-encryption-at-host-health-check-commands.sh
@@ -2,6 +2,29 @@
 
 set -euo pipefail
 
+function check_node_encryption_at_host() {
+    local node_name="$1"
+    echo "Checking guest cluster node $node_name"
+
+    np_name=$(KUBECONFIG="${SHARED_DIR}"/nested_kubeconfig oc get node "$node_name" -o jsonpath='{.metadata.labels.hypershift\.openshift\.io/nodePool}')
+    if [[ -z "$np_name" ]]; then
+        echo "Error: got empty NP name for node $node_name" >&2
+        return 1
+    fi
+
+    np_encryption_at_host="$(oc get np -n clusters "$np_name" -o jsonpath='{.spec.platform.azure.encryptionAtHost}')"
+    if [[ $np_encryption_at_host != "Enabled" ]]; then
+        echo "Error: NP $np_name has encryptionAtHost disabled" >&2
+        return 1
+    fi
+
+    vm_encryption_at_host="$(az vm show --name "$node_name" -g "$hc_rg" --query 'securityProfile.encryptionAtHost' -o tsv)"
+    if [[ "${vm_encryption_at_host}" != "true" ]]; then
+        echo "Error: encryption at host disabled for VM $node_name" >&2
+        return 1
+    fi
+}
+
 AZURE_AUTH_LOCATION="${CLUSTER_PROFILE_DIR}/osServicePrincipal.json"
 AZURE_AUTH_CLIENT_ID="$(<"${AZURE_AUTH_LOCATION}" jq -r .clientId)"
 AZURE_AUTH_CLIENT_SECRET="$(<"${AZURE_AUTH_LOCATION}" jq -r .clientSecret)"
@@ -13,13 +36,6 @@ az login --service-principal -u "${AZURE_AUTH_CLIENT_ID}" -p "${AZURE_AUTH_CLIEN
 
 set -x
 
-echo "Checking NodePool specs"
-desId="$(oc get np -A -o jsonpath='{.items[0].spec.platform.azure.diskEncryptionSetID}')"
-if [[ -z "$desId" ]]; then
-    echo "Error: got empty diskEncryptionSetID from NodePool" >&2
-    exit 1
-fi
-
 echo "Getting resource group from HostedCluster"
 hc_rg=$(oc get hc -A -o jsonpath='{.items[0].spec.platform.azure.resourceGroup}')
 if [[ -z "$hc_rg" ]]; then
@@ -30,10 +46,5 @@ fi
 echo "Verifying osDisks on guest cluster nodes are encrypted with the same disk encryption set"
 hc_nodes="$(oc --kubeconfig "${SHARED_DIR}/nested_kubeconfig" get nodes --no-headers | awk '{print $1}')"
 for hc_node in ${hc_nodes}; do
-    echo "Checking guest cluster node $hc_node"
-    hc_node_des_id="$(az vm show --name "${hc_node}" -g "${hc_rg}" --query 'storageProfile.osDisk.managedDisk.diskEncryptionSet.id' -otsv)"
-    if [[ "${hc_node_des_id}" != "${desId}" ]]; then
-        echo "Error: expect disk encryption set $desId but got $hc_node_des_id from VM"
-    fi
-    echo "Guest cluster node $hc_node correctly encrypted"
+    check_node_encryption_at_host "$hc_node"
 done
