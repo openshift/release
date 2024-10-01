13084300c12f3ad9ab5d1394310fe88fd6492e6a
Carlos Cardenosa <ccardeno@redhat.com>
2024-10-01 11:53:51 +0200

    Allow MCE setup for SNO Hub clusters
    * Also increase verbosity in case of failure
    
    Signed-off-by: Carlos Cardenosa <ccardeno@redhat.com>

--- ci-operator/step-registry/acm/mch/acm-mch-commands.sh
+++ ci-operator/step-registry/acm/mch/acm-mch-commands.sh
@@ -1,13 +1,70 @@
 #!/bin/bash
-set -x
 set -o nounset
 set -o errexit
 set -o pipefail
 
 export KUBECONFIG=${SHARED_DIR}/kubeconfig
 
+namespaces_to_check=(
+  "${MCH_NAMESPACE}"
+  "multicluster-engine"
+)
+
+function get_failed_pods_by_name {
+  oc get pods \
+    -n ${1} \
+    --field-selector=status.phase!=Running,status.phase!=Succeeded \
+    -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'
+}
+
+function dump_multiclusterhub_pod_logs {
+  echo
+  echo "Dumping logs for failing PODs..."
+  echo
+  for ns in "${namespaces_to_check[@]}"; do
+   for failed_pod_name in $(get_failed_pods_by_name ${ns}); do
+     echo "Gathering '${failed_pod_name}' POD logs in the '${ns}' namespace..."
+     echo
+     set -x
+     oc -n ${ns} describe pod/${failed_pod_name} > ${ARTIFACT_DIR}/${ns}_${failed_pod_name}.describe.txt
+     oc -n ${ns} logs pods/$failed_pod_name > ${ARTIFACT_DIR}/${ns}_${failed_pod_name}.logs
+     set +x
+     echo
+   done
+  done
+}
+
+function show_multiclusterhub_related_objects {
+  echo
+  echo "### $(date) ###"
+  echo
+  set -x
+  oc get clusterversions,node,mcp,co,operators || echo
+  oc get subscriptions.operators.coreos.com -A || echo
+  oc get ClusterManagementAddOn || echo
+  oc get operator advanced-cluster-management.open-cluster-management -oyaml || echo
+  oc get operator multicluster-engine.multicluster-engine -oyaml || echo
+  oc -n ${MCH_NAMESPACE} get mch multiclusterhub -oyaml || echo
+  set +x
+  echo
+  for ns in "${namespaces_to_check[@]}"; do
+    echo
+    echo "------ ${ns} namespace ------"
+    echo
+    set -x
+    oc -n ${ns} get configmaps,secrets,all || echo
+    set +x
+  done
+}
+
 # create image pull secret for MCH
 oc create secret generic ${IMAGE_PULL_SECRET} -n ${MCH_NAMESPACE} --from-file=.dockerconfigjson=$CLUSTER_PROFILE_DIR/pull-secret --type=kubernetes.io/dockerconfigjson
+
+annotations="annotations: {}"
+if [ -n "${MCH_CATALOG_ANNOTATION}" ];then
+  annotations="annotations:
+    installer.open-cluster-management.io/mce-subscription-spec: '${MCH_CATALOG_ANNOTATION}'"
+fi
 
 echo "Apply multiclusterhub"
 # apply MultiClusterHub crd
@@ -17,25 +74,24 @@ kind: MultiClusterHub
 metadata:
   name: multiclusterhub
   namespace: ${MCH_NAMESPACE}
+  ${annotations}
 spec:
+  availabilityConfig: ${MCH_AVAILABILITY_CONFIG}
   imagePullSecret: ${IMAGE_PULL_SECRET}
 EOF
 
-# Need to sleep a bit before start watching
-sleep 60
+{
+  set -x ;
+  oc -n ${MCH_NAMESPACE} wait --for=jsonpath='{.status.phase}'=Running mch/multiclusterhub --timeout 30m ;
 
-RETRIES=30
-for try in $(seq "${RETRIES}"); do
-  if [[ $(oc get mch -n ${MCH_NAMESPACE} -o=jsonpath='{.items[0].status.phase}') == "Running" ]]; then
-    acm_version=$(oc -n ${MCH_NAMESPACE} get mch multiclusterhub -o jsonpath='{.status.currentVersion}{"\n"}')
-    echo "Success! ACM ${acm_version} is Running"
-    break
-  else
-    if [ $try == $RETRIES ]; then
-      echo "Error MCH failed to reach Running status in alloted time."
-      exit 1
-    fi
-    echo "Try ${try}/${RETRIES}: MCH is not running yet. Checking again in 30 seconds"
-    sleep 30
-  fi
-done
+} || { \
+
+  set +x ;
+  show_multiclusterhub_related_objects ;
+  dump_multiclusterhub_pod_logs ;
+  echo "Error MCH failed to reach Running status in alloted time." ;
+  exit 1 ;
+}
+
+acm_version=$(oc -n ${MCH_NAMESPACE} get mch multiclusterhub -o jsonpath='{.status.currentVersion}{"\n"}')
+echo "Success! ACM ${acm_version} is Running"
