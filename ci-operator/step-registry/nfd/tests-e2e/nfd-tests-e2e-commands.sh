b8a585e72a5ad66ee896a18bcd78d09c9076662d
Vivek Shakya <vshakya@vshakya-thinkpadt14gen4.remote.csb>
2024-10-09 16:41:55 -0400

    update image

--- ci-operator/step-registry/nfd/tests-e2e/nfd-tests-e2e-commands.sh
+++ ci-operator/step-registry/nfd/tests-e2e/nfd-tests-e2e-commands.sh
@@ -18,9 +18,9 @@ spec:
   restartPolicy: Never
   containers:
   - name: run-nfd
-    image: quay.io/ocp-edge-qe/eco-gotests:latest
+     image: quay.io/rh_ee_ggordani/eco-go:latest
     command: [ "/bin/bash", "-c", "--" ]
-    args: [ "scripts/test-runner.sh && sleep 900" ]
+    args: [ "scripts/test-runner.sh && sleep 300" ]
     env:
     - name: KUBECONFIG
       value: "/kubeconfig/kubeconfig"
