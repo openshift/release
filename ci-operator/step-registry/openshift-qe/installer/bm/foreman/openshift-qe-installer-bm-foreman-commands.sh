e75065550dd980552642c6c131830b5d8ca9170a
Jose Castillo Lema <josecastillolema@gmail.com>
2024-10-11 00:00:04 +0200

    Fix quotes
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/foreman/openshift-qe-installer-bm-foreman-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/foreman/openshift-qe-installer-bm-foreman-commands.sh
@@ -10,12 +10,12 @@ set -x
 bastion=$(cat "/secret/address")
 
 # Set the corresponding host on the lab Foreman instance
-sshpass -p "$(cat /secret/login)" ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@${bastion} "
+sshpass -p "$(cat /secret/login)" ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@${bastion} """
   NUM_NODES_CLOUD=$(curl -sS http://$QUADS_INSTANCE/cloud/$LAB_CLOUD\_ocpinventory.json | grep name | wc -l)
   for i in $(curl -sS $QUADS_INSTANCE/cloud/$LAB_CLOUD\_ocpinventory.json | grep name | tail -n $((NUM_NODES_CLOUD-STARTING_NODE)) | head -n $NUM_NODES | awk '{print $2}' | tr -d ,\"); do
     echo $i
     hammer host update --name $i --operatingsystem $FOREMAN_OS -pxe-loader “Grub2 UEFI” --build 1
-  done"
+  done"""
 
 # Use badfish to set the boot interface and bounce the box
 sshpass -p "$(cat /secret/login)" ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@${bastion} "
