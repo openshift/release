dcdf31c1bdcb3c10f6512aa1b98ec16bce24fa52
aabughosh <88486034+aabughosh@users.noreply.github.com>
2024-10-15 11:52:04 +0300

    Update network-flow-matrix-tests-commands.sh

--- ci-operator/step-registry/network-flow-matrix/tests/network-flow-matrix-tests-commands.sh
+++ ci-operator/step-registry/network-flow-matrix/tests/network-flow-matrix-tests-commands.sh
@@ -4,7 +4,7 @@ source $HOME/golang-1.22.4
 echo "Go version: $(go version)"
 git clone https://github.com/openshift-kni/commatrix ${SHARED_DIR}/commatrix
 pushd ${SHARED_DIR}/commatrix || exit
-git checkout origin_test_branch
+git checkout machine-config-test-release
 go mod vendor
 make e2e-test
 popd || exit
