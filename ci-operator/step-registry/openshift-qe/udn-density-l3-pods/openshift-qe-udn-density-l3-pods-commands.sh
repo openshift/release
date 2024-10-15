81daef40e706bf9851748f229b4043f6c5a6630e
Krishna Harsha Voora <14876995+krishvoor@users.noreply.github.com>
2024-10-15 18:22:49 +0530

    [PERFSCALE-3438] Baremetal CPT Bits for udn (#57838)
    * [PERFSCALE-3438] Baremetal CPT Bits for udn
    
    This PR adds neccessary for jobs to run on BM CPT
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>
    
    * Adds pkill ssh
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>
    
    ---------
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>

--- ci-operator/step-registry/openshift-qe/udn-density-l3-pods/openshift-qe-udn-density-l3-pods-commands.sh
+++ ci-operator/step-registry/openshift-qe/udn-density-l3-pods/openshift-qe-udn-density-l3-pods-commands.sh
@@ -4,13 +4,28 @@ set -o nounset
 set -o pipefail
 set -x
 cat /etc/os-release
-oc config view
-oc projects
-oc version
+
+if [ ${BAREMETAL} == "true" ]; then
+  bastion="$(cat /bm/address)"
+  # Copy over the kubeconfig
+  sshpass -p "$(cat /bm/login)" ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@$bastion "cat ~/bm/kubeconfig" > /tmp/kubeconfig
+  # Setup socks proxy
+  sshpass -p "$(cat /bm/login)" ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@$bastion -fNT -D 12345
+  export KUBECONFIG=/tmp/kubeconfig
+  export https_proxy=socks5://localhost:12345
+  export http_proxy=socks5://localhost:12345
+  oc --kubeconfig=/tmp/kubeconfig config set-cluster bm --proxy-url=socks5://localhost:12345
+  cd /tmp
+fi
+
 python --version
 pushd /tmp
 python -m virtualenv ./venv_qe
 source ./venv_qe/bin/activate
+
+oc config view
+oc projects
+oc version
 
 ES_SECRETS_PATH=${ES_SECRETS_PATH:-/secret}
 
@@ -52,4 +67,9 @@ jq ".iterations = $ITERATIONS" $folder_name/index_data.json >> ${SHARED_DIR}/ind
 if [[ "${ENABLE_LOCAL_INDEX}" == "true" ]]; then
     metrics_folder_name=$(find . -maxdepth 1 -type d -name 'collected-metric*' | head -n 1)
     cp -r "${metrics_folder_name}" "${ARTIFACT_DIR}/"
+fi
+
+if [ ${BAREMETAL} == "true" ]; then
+  # kill the ssh tunnel so the job completes
+  pkill ssh
 fi
