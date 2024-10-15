be630bba5c37c72e5cc4a335a29e2988f24f3904
Liangquan Li <liangli@redhat.com>
2024-10-15 14:39:53 +0800

    OCPBUGS-39123 test

--- ci-operator/step-registry/hypershift/kubevirt/create/hypershift-kubevirt-create-commands.sh
+++ ci-operator/step-registry/hypershift/kubevirt/create/hypershift-kubevirt-create-commands.sh
@@ -206,7 +206,10 @@ else
     --control-plane-availability-policy "${CONTROL_PLANE_AVAILABILITY}" \
     --infra-availability-policy "${INFRA_AVAILABILITY}" \
     --service-cidr 172.32.0.0/16 \
-    --cluster-cidr 10.136.0.0/14
+    --cluster-cidr 10.136.0.0/14  > /tmp/hc-manifests/cluster-agent.yaml
+
+    cat /tmp/hc-manifests/cluster-agent.yaml
+    sleep 3600
 fi
 
 
