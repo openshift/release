7366f36641912003518121b48c5e1877ad13c4ca
Shang Gao <sgao@redhat.com>
2024-10-17 15:47:40 +0800

    Debug

--- ci-operator/step-registry/ipi/conf/vsphere/zones/multisubnets/ipi-conf-vsphere-zones-multisubnets-commands.sh
+++ ci-operator/step-registry/ipi/conf/vsphere/zones/multisubnets/ipi-conf-vsphere-zones-multisubnets-commands.sh
@@ -4,41 +4,22 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-if [[ "${CLUSTER_PROFILE_NAME:-}" != "vsphere-elastic" ]]; then
-  echo "using legacy sibling of this step"
-  exit 0
-fi
-
-# ensure LEASED_RESOURCE is set
-if [[ -z "${LEASED_RESOURCE}" ]]; then
-  echo "Failed to acquire lease"
-  exit 1
-fi
-
-echo "$(date -u --rfc-3339=seconds) - sourcing context from vsphere_extra_leased_context"
-
-declare -a vips
-mapfile -t vips < "${SHARED_DIR}"/vips.txt
-
 # if loadbalancer is UserManaged, it's mean using external LB,
 # then keepalived and haproxy will not deployed, but coredns still keep
 if [[ ${LB_TYPE} == "UserManaged" ]]; then
-    APIVIPS_DEF="apiVIPs:
+  declare -a vips
+  mapfile -t vips <"${SHARED_DIR}"/vips.txt
+  APIVIPS_DEF="apiVIPs:
       - ${vips[0]}"
-    INGRESSVIPS_DEF="ingressVIPs:
+  INGRESSVIPS_DEF="ingressVIPs:
       - ${vips[1]}"
-    LB_TYPE_DEF="loadBalancer:
+  LB_TYPE_DEF="loadBalancer:
       type: UserManaged"
-else
-    APIVIPS_DEF=""
-    INGRESSVIPS_DEF=""
-    LB_TYPE_DEF=""
-fi
 
-CONFIG="${SHARED_DIR}/install-config.yaml"
-PATCH="${SHARED_DIR}/multisubnets.yaml.patch"
+  CONFIG="${SHARED_DIR}/install-config.yaml"
+  PATCH="${SHARED_DIR}/multisubnets.yaml.patch"
 
-cat >"${PATCH}" <<EOF
+  cat >"${PATCH}" <<EOF
 platform:
   vsphere:
     ${LB_TYPE_DEF}
@@ -46,4 +27,5 @@ platform:
     ${INGRESSVIPS_DEF}
 EOF
 
-yq-go m -x -i "${CONFIG}" "${PATCH}"
+  yq-go m -x -i "${CONFIG}" "${PATCH}"
+fi
