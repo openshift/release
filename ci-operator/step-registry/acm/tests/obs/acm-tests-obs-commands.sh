c8320f7073f79cb207638b3163c531fb87dc73f2
Vincent Boulos <87659226+vboulos@users.noreply.github.com>
2024-10-23 11:46:36 -0400

    exclude the managed cluster params from Obs

--- ci-operator/step-registry/acm/tests/obs/acm-tests-obs-commands.sh
+++ ci-operator/step-registry/acm/tests/obs/acm-tests-obs-commands.sh
@@ -42,20 +42,20 @@ OC_HUB_CLUSTER_PASS=$(cat $SHARED_DIR/kubeadmin-password)
 export OC_HUB_CLUSTER_PASS
 
 # Set the dynamic vars needed to execute the Observability scenarios on the managed clusters
-MANAGED_CLUSTER_NAME=$(cat $SHARED_DIR/managed.cluster.name)
-export MANAGED_CLUSTER_NAME
+# MANAGED_CLUSTER_NAME=$(cat $SHARED_DIR/managed.cluster.name)
+# export MANAGED_CLUSTER_NAME
 
-MANAGED_CLUSTER_BASE_DOMAIN=$(cat $SHARED_DIR/managed.cluster.base.domain)
-export MANAGED_CLUSTER_BASE_DOMAIN
+# MANAGED_CLUSTER_BASE_DOMAIN=$(cat $SHARED_DIR/managed.cluster.base.domain)
+# export MANAGED_CLUSTER_BASE_DOMAIN
 
-MANAGED_CLUSTER_USER=$(cat $SHARED_DIR/managed.cluster.username)
-export MANAGED_CLUSTER_USER
+# MANAGED_CLUSTER_USER=$(cat $SHARED_DIR/managed.cluster.username)
+# export MANAGED_CLUSTER_USER
 
-MANAGED_CLUSTER_PASS=$(cat $SHARED_DIR/managed.cluster.password)
-export MANAGED_CLUSTER_PASS
+# MANAGED_CLUSTER_PASS=$(cat $SHARED_DIR/managed.cluster.password)
+# export MANAGED_CLUSTER_PASS
 
-MANAGED_CLUSTER_API_URL=$(cat $SHARED_DIR/managed.cluster.api.url)
-export MANAGED_CLUSTER_API_URL
+# MANAGED_CLUSTER_API_URL=$(cat $SHARED_DIR/managed.cluster.api.url)
+# export MANAGED_CLUSTER_API_URL
 
 # Create a .kube directory inside the alabama dir
 mkdir -p /alabama/.kube
