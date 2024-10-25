5a88761c1b2ccfa349713a8779732befe0abf760
He Liu <heli@redhat.com>
2024-10-25 17:29:25 +0800

    check if nodes are recreated after cp is upgraded

--- ci-operator/step-registry/cucushift/upgrade/hypershift/control-plane/toimage/cucushift-upgrade-hypershift-control-plane-toimage-commands.sh
+++ ci-operator/step-registry/cucushift/upgrade/hypershift/control-plane/toimage/cucushift-upgrade-hypershift-control-plane-toimage-commands.sh
@@ -43,16 +43,12 @@ function check_node() {
     node_number=$(oc get node --no-headers | grep -cv STATUS)
     ready_number=$(oc get node --no-headers | awk '$2 == "Ready"' | wc -l)
     if (( node_number == ready_number )); then
-        echo "All nodes status check PASSED"
+        echo "All nodes status Ready"
         return 0
     else
-        if (( ready_number == 0 )); then
-            echo >&2 "No any ready node"
-        else
-            echo >&2 "We found failed node"
-            oc get node --no-headers | awk '$2 != "Ready"'
-        fi
-        return 1
+        echo "Find Not Ready worker nodes, node recreated"
+        oc get no
+        exit 1
     fi
 }
 
@@ -74,12 +70,14 @@ done
 EOT
     oc wait clusterversion/version --for='condition=Available=True' --timeout=15m
 
-    echo "Step #2: Make sure every machine is in 'Ready' status"
-    check_node
-
-    echo "Step #3: Check all pods are in status running or complete"
+    echo "Step #2: Check all pods are in status running or complete"
     check_pod
 }
+
+# original worker node UIDs before controlplane upgrade
+initial_uids=$(oc get nodes -o jsonpath='{.items[*].metadata.uid}')
+IFS=' ' read -r -a initial_array <<< "$initial_uids"
+sorted_initial_uids=$(printf "%s\n" "${initial_array[@]}" | sort | tr '\n' ' ')
 
 if [ ! -f "${SHARED_DIR}/mgmt_kubeconfig" ]; then
     exit 1
@@ -135,4 +133,27 @@ if [ $_upgradeReady -ne 0 ]; then
 fi
 
 export KUBECONFIG="${SHARED_DIR}/kubeconfig"
+
+echo "Monitoring for node recreation for 5 minutes..."
+END_TIME=$((SECONDS + 600))
+while [ $SECONDS -lt $END_TIME ]; do
+  check_node
+  sleep 60
+done
+
+# ensure the worker node UIDs are not changed
+current_uids=$(oc get nodes -o jsonpath='{.items[*].metadata.uid}')
+IFS=' ' read -r -a current_array <<< "$current_uids"
+sorted_current_uids=$(printf "%s\n" "${current_array[@]}" | sort | tr '\n' ' ')
+
+# compare the worker nodes UIDs
+if [ "$sorted_initial_uids" == "$sorted_current_uids" ]; then
+    echo "No changes detected in node UIDs."
+else
+    echo "Node UIDs have changed!"
+    echo "Initial UIDs: $sorted_initial_uids"
+    echo "Current UIDs: $sorted_current_uids"
+    exit 1
+fi
+
 health_check
