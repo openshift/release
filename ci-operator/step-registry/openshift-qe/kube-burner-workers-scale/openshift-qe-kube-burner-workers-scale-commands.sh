1969a67229a4bacbd925fc069e63141349ba9712
Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.rmtusnc.csb>
2024-10-25 08:38:00 -0400

    Resolve conflicts with main
    Signed-off-by: Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.rmtusnc.csb>

--- ci-operator/step-registry/openshift-qe/kube-burner-workers-scale/openshift-qe-kube-burner-workers-scale-commands.sh
+++ ci-operator/step-registry/openshift-qe/kube-burner-workers-scale/openshift-qe-kube-burner-workers-scale-commands.sh
@@ -21,7 +21,7 @@ git clone $REPO_URL $TAG_OPTION --depth 1
 pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
 export WORKLOAD=workers-scale
 export GC=$GARBAGE_COLLECTION
-EXTRA_FLAGS="${METRIC_PROFILES} --additional-worker-nodes ${ADDITIONAL_WORKER_NODES} --enable-autoscaler=${ENABLE_AUTOSCALER}" 
+EXTRA_FLAGS="${METRIC_PROFILES} --additional-worker-nodes ${ADDITIONAL_WORKER_NODES} --enable-autoscaler=${DEPLOY_AUTOSCALER}" 
 
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
 
