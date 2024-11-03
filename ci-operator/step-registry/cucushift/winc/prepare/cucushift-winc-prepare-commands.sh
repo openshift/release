dbe82c600fb0254f586c76156fdbb04fa1d2b037
rrasouli <rrasouli@redhat.com>
2024-11-03 17:50:52 +0200

    Add Azure BYOH support to Prow CI

--- ci-operator/step-registry/cucushift/winc/prepare/cucushift-winc-prepare-commands.sh
+++ ci-operator/step-registry/cucushift/winc/prepare/cucushift-winc-prepare-commands.sh
@@ -6,165 +6,179 @@ set -x
 
 function create_winc_test_configmap()
 {
+ oc create configmap winc-test-config -n winc-test --from-literal=primary_windows_image="${1}" --from-literal=primary_windows_container_image="${2}"
 
-  oc create configmap winc-test-config -n winc-test --from-literal=primary_windows_image="${1}" --from-literal=primary_windows_container_image="${2}"
-
-  # Display pods and configmap
-  oc get pod -owide -n winc-test
-  oc get cm winc-test-config -oyaml -n winc-test
+ # Display pods and configmap
+ oc get pod -owide -n winc-test
+ oc get cm winc-test-config -oyaml -n winc-test
 }
 
 function create_workloads()
 {
-  oc new-project winc-test
-  # turn off the automatic label synchronization required for PodSecurity admission
-  # set pods security profile to privileged. See https://kubernetes.io/docs/concepts/security/pod-security-admission/#pod-security-levels
-  oc label namespace winc-test security.openshift.io/scc.podSecurityLabelSync=false pod-security.kubernetes.io/enforce=privileged  --overwrite
+ oc new-project winc-test
+ # turn off the automatic label synchronization required for PodSecurity admission
+ # set pods security profile to privileged. See https://kubernetes.io/docs/concepts/security/pod-security-admission/#pod-security-levels
+ oc label namespace winc-test security.openshift.io/scc.podSecurityLabelSync=false pod-security.kubernetes.io/enforce=privileged  --overwrite
 
-
-  # Create Windows workload
-  oc create -f - <<EOF
+ # Create Windows workload
+ oc create -f - <<EOF
 apiVersion: v1
 kind: Service
 metadata:
-  name: win-webserver
-  labels:
-    app: win-webserver
+ name: win-webserver
+ labels:
+   app: win-webserver
 spec:
-  ports:
-  # the port that this service should serve on
-  - port: 80
-    targetPort: 80
-  selector:
-    app: win-webserver
-  type: LoadBalancer
+ ports:
+ # the port that this service should serve on
+ - port: 80
+   targetPort: 80
+ selector:
+   app: win-webserver
+ type: LoadBalancer
 ---
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-  labels:
-    app: win-webserver
-  name: win-webserver
+ labels:
+   app: win-webserver
+ name: win-webserver
 spec:
-  selector:
-    matchLabels:
-      app: win-webserver
-  replicas: 5
-  template:
-    metadata:
-      labels:
-        app: win-webserver
-      name: win-webserver
-    spec:
-      nodeSelector:
-        kubernetes.io/os: windows
-      tolerations:
-        - key: "os"
-          value: "Windows"
-          Effect: "NoSchedule"
-      containers:
-        - name: win-webserver
-          image: ${1}
-          imagePullPolicy: IfNotPresent
-          command:
-            - pwsh.exe
-            - -command
-            - \$listener = New-Object System.Net.HttpListener; \$listener.Prefixes.Add('http://*:80/'); \$listener.Start();Write-Host('Listening at http://*:80/'); while (\$listener.IsListening) { \$context = \$listener.GetContext(); \$response = \$context.Response; \$content='<html><body><H1>Windows Container Web Server</H1></body></html>'; \$buffer = [System.Text.Encoding]::UTF8.GetBytes(\$content); \$response.ContentLength64 = \$buffer.Length; \$response.OutputStream.Write(\$buffer, 0, \$buffer.Length); \$response.Close(); };
-          securityContext:
-            runAsNonRoot: false
-            windowsOptions:
-              runAsUserName: "ContainerAdministrator"
+ selector:
+   matchLabels:
+     app: win-webserver
+ replicas: 5
+ template:
+   metadata:
+     labels:
+       app: win-webserver
+     name: win-webserver
+   spec:
+     nodeSelector:
+       kubernetes.io/os: windows
+     tolerations:
+       - key: "os"
+         value: "Windows"
+         Effect: "NoSchedule"
+     containers:
+       - name: win-webserver
+         image: ${1}
+         imagePullPolicy: IfNotPresent
+         command:
+           - pwsh.exe
+           - -command
+           - \$listener = New-Object System.Net.HttpListener; \$listener.Prefixes.Add('http://*:80/'); \$listener.Start();Write-Host('Listening at http://*:80/'); while (\$listener.IsListening) { \$context = \$listener.GetContext(); \$response = \$context.Response; \$content='<html><body><H1>Windows Container Web Server</H1></body></html>'; \$buffer = [System.Text.Encoding]::UTF8.GetBytes(\$content); \$response.ContentLength64 = \$buffer.Length; \$response.OutputStream.Write(\$buffer, 0, \$buffer.Length); \$response.Close(); };
+         securityContext:
+           runAsNonRoot: false
+           windowsOptions:
+             runAsUserName: "ContainerAdministrator"
 EOF
 
-  # Wait up to 5 minutes for Windows workload to be ready
-  oc wait deployment win-webserver -n winc-test --for condition=Available=True --timeout=5m
+ # Wait up to 5 minutes for Windows workload to be ready
+ oc wait deployment win-webserver -n winc-test --for condition=Available=True --timeout=5m
 
-  # Create Linux workload
-  oc create -f - <<EOF
+ # Create Linux workload
+ oc create -f - <<EOF
 apiVersion: v1
 kind: Service
 metadata:
-  name: linux-webserver
-  labels:
-    app: linux-webserver
+ name: linux-webserver
+ labels:
+   app: linux-webserver
 spec:
-  ports:
-  # the port that this service should serve on
-  - port: 8080
-    targetPort: 8080
-  selector:
-    app: linux-webserver
-  type: LoadBalancer
+ ports:
+ # the port that this service should serve on
+ - port: 8080
+   targetPort: 8080
+ selector:
+   app: linux-webserver
+ type: LoadBalancer
 ---
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-  labels:
-    app: linux-webserver
-  name: linux-webserver
+ labels:
+   app: linux-webserver
+ name: linux-webserver
 spec:
-  selector:
-    matchLabels:
-      app: linux-webserver
-  replicas: 1
-  template:
-    metadata:
-      labels:
-        app: linux-webserver
-      name: linux-webserver
-    spec:
-      containers:
-      - name: linux-webserver
-        image: quay.io/openshifttest/hello-openshift:multiarch-winc
-        ports:
-        - containerPort: 8080
+ selector:
+   matchLabels:
+     app: linux-webserver
+ replicas: 1
+ template:
+   metadata:
+     labels:
+       app: linux-webserver
+     name: linux-webserver
+   spec:
+     containers:
+     - name: linux-webserver
+       image: quay.io/openshifttest/hello-openshift:multiarch-winc
+       ports:
+       - containerPort: 8080
 EOF
-  # Wait up to 5 minutes for Linux workload to be ready
-  oc wait deployment linux-webserver -n winc-test --for condition=Available=True --timeout=5m
+ # Wait up to 5 minutes for Linux workload to be ready
+ oc wait deployment linux-webserver -n winc-test --for condition=Available=True --timeout=5m
 }
 
 IAAS_PLATFORM=$(oc get infrastructure cluster -o=jsonpath="{.status.platformStatus.type}"| tr '[:upper:]' '[:lower:]')
 
-
+# Get Windows MachineSet info
 winworker_machineset_name=$(oc get machineset -n openshift-machine-api -o json | jq -r '.items[] | select(.metadata.name | test("win")).metadata.name')
 winworker_machineset_replicas=$(oc get machineset -n openshift-machine-api $winworker_machineset_name -o jsonpath="{.spec.replicas}")
 
-echo "Waiting for Windows nodes to come up in Running state"
-while [[ $(oc -n openshift-machine-api get machineset/${winworker_machineset_name} -o 'jsonpath={.status.readyReplicas}') != "${winworker_machineset_replicas}" ]]; do echo -n "." && sleep 10; done
+echo "Waiting for Windows MachineSet nodes to come up in Running state"
+while [[ $(oc -n openshift-machine-api get machineset/${winworker_machineset_name} -o 'jsonpath={.status.readyReplicas}') != "${winworker_machineset_replicas}" ]]; do 
+   echo -n "." && sleep 10
+done
 
-# Make sure the Windows nodes get in Ready state
+# If BYOH nodes are requested, wait for them too
+if [[ -n "${BYOH_NODE_COUNT:-}" ]]; then
+    echo "Waiting for Windows BYOH nodes to come up"
+    timeout 15m bash -c '
+        while [[ $(oc get nodes -l "kubernetes.io/os=windows,windowsmachineconfig.openshift.io/byoh=true" --no-headers | wc -l) != "${BYOH_NODE_COUNT}" ]]; do
+            echo "Current BYOH nodes status:"
+            oc get nodes -l "kubernetes.io/os=windows,windowsmachineconfig.openshift.io/byoh=true"
+            sleep 30
+        done' || {
+            echo "ERROR: Timed out waiting for ${BYOH_NODE_COUNT} BYOH nodes to come up"
+            exit 1
+        }
+fi
+
+# Make sure all Windows nodes get in Ready state
 oc wait nodes -l kubernetes.io/os=windows --for condition=Ready=True --timeout=15m
 
-# Choose the Windows container vesion depending on the Windows version
+# Choose the Windows container version depending on the Windows version
 # installed on the Windows workers
 os_version=$(oc get nodes -l 'kubernetes.io/os=windows' -o=jsonpath="{.items[0].status.nodeInfo.osImage}")
 
 windows_container_image="mcr.microsoft.com/powershell:lts-nanoserver-ltsc2022"
 if [[ "$os_version" == *"2019"* ]]
 then
-    windows_container_image="mcr.microsoft.com/powershell:lts-nanoserver-1809"
+   windows_container_image="mcr.microsoft.com/powershell:lts-nanoserver-1809"
 fi
 
 case "$IAAS_PLATFORM" in
-  aws)
-	windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.ami.id}" -n openshift-machine-api)
-    ;;
-  azure)
-	windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.image.sku}" -n openshift-machine-api)
-	;;
-  vsphere)
-	windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.template}" -n openshift-machine-api)
-    ;;
-  gcp)
-	# we need the value after family/
-	# in this example projects/windows-cloud/global/images/family/windows-2022-core
-	# windows_os_image_id needs to be windows-2022-core
-	windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.disks[0].image}" -n openshift-machine-api | tr "/" "\n" | tail -n1)
-    ;;
-  *)
-    echo "Cloud provider \"$IAAS_PLATFORM\" is not supported by WMCO"
-    exit 1
-    ;;
+ aws)
+   windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.ami.id}" -n openshift-machine-api)
+   ;;
+ azure)
+   windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.image.sku}" -n openshift-machine-api)
+   ;;
+ vsphere)
+   windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.template}" -n openshift-machine-api)
+   ;;
+ gcp)
+   # we need the value after family/
+   # in this example projects/windows-cloud/global/images/family/windows-2022-core
+   # windows_os_image_id needs to be windows-2022-core
+   windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.disks[0].image}" -n openshift-machine-api | tr "/" "\n" | tail -n1)
+   ;;
+ *)
+   echo "Cloud provider \"$IAAS_PLATFORM\" is not supported by WMCO"
+   exit 1
+   ;;
 esac
 
 create_workloads $windows_container_image
---
+++ ci-operator/step-registry/ipi/conf/azure/windows-byoh/ipi-conf-azure-windows-byoh-commands.sh
@@ -0,0 +1,69 @@
+# ci-operator/step-registry/ipi/conf/azure/windows-byoh/ipi-conf-azure-windows-byoh-commands.sh
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# Wait for WMCO to be up and running
+oc wait deployment windows-machine-config-operator -n openshift-windows-machine-config-operator --for condition=Available=True --timeout=5m
+
+# Ensure userDataSecret exists
+timeout 3m bash -c 'until oc -n openshift-machine-api get secret "${WINDOWS_USER_DATA_SECRET}" 2> /dev/null; do echo -n "." && sleep 15; done'
+
+# Get reference machineset name
+ref_machineset_name=$(oc -n openshift-machine-api get -o 'jsonpath={range .items[*]}{.metadata.name}{"\n"}{end}' machinesets | grep worker | head -n1)
+winworker_machineset_name="windows"
+export ref_machineset_name winworker_machineset_name
+
+# Create Windows MachineSets
+echo "Creating Windows MachineSets..."
+oc get machineset "${ref_machineset_name}" -n openshift-machine-api -o json |
+  jq --arg winworker_machineset_name "${winworker_machineset_name}" \
+     --arg win_os_id "${WINDOWS_OS_ID}" \
+     --arg user_data_secret "${WINDOWS_USER_DATA_SECRET}" \
+     '
+      .metadata.name = $winworker_machineset_name |
+      .spec.replicas = 0 |
+      .spec.selector.matchLabels."machine.openshift.io/cluster-api-machineset" = $winworker_machineset_name |
+      .spec.template.metadata.labels."machine.openshift.io/cluster-api-machineset" = $winworker_machineset_name |
+      .spec.template.metadata.labels."machine.openshift.io/exclude-node-draining" = "" |
+      .spec.template.metadata.labels."machine.openshift.io/os-id" = "Windows" |
+      .spec.template.spec.metadata.labels."node-role.kubernetes.io/worker" = "" |
+      .spec.template.spec.providerSpec.value.disks[0].image = $win_os_id |
+      .spec.template.spec.providerSpec.value.machineType = $instance_type |
+      .spec.template.spec.providerSpec.value.userDataSecret.name = $user_data_secret |
+      del(.status) |
+      del(.metadata.selfLink) |
+      del(.metadata.uid)
+     ' | oc create -f -
+
+# Scale Windows MachineSets
+echo "Scaling Windows MachineSets to ${WINDOWS_NODE_REPLICAS} replicas..."
+oc -n openshift-machine-api scale machineset/"${winworker_machineset_name}" --replicas="${WINDOWS_NODE_REPLICAS}"
+
+# Setup BYOH nodes
+echo "Setting up Windows BYOH nodes..."
+
+# Configure BYOH parameters
+BYOH_NAME="${BYOH_INSTANCE_NAME:-byoh-winc}"
+BYOH_COUNT="${BYOH_NODE_COUNT:-2}"
+WIN_VERSION="${WINDOWS_VERSION:-2022}"
+
+# Execute byoh-auto
+echo "Creating ${BYOH_COUNT} BYOH nodes using byoh-auto..."
+pushd "${SHARED_DIR}/ci-operator/config/openshift/byoh"
+./byoh.sh apply "${BYOH_NAME}" "${BYOH_COUNT}" '' "${WIN_VERSION}" || {
+    echo "ERROR: Failed to create Windows BYOH nodes"
+    exit 1
+}
+popd
+
+# Wait for all Windows nodes to be ready
+echo "Waiting for all Windows nodes (MachineSets and BYOH) to be ready..."
+timeout 20m bash -c 'until oc get nodes -l kubernetes.io/os=windows | grep -q "Ready"; do 
+    echo "Current Windows nodes status:"
+    oc get nodes -l kubernetes.io/os=windows
+    sleep 30
+done'
+
