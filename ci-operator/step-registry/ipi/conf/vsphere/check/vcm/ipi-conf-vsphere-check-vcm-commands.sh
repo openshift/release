863258d5b123643f9ee8f333ed2e1c57a697dc25
Shang Gao <sgao@redhat.com>
2024-11-07 14:15:13 +0800

    dd

--- ci-operator/step-registry/ipi/conf/vsphere/check/vcm/ipi-conf-vsphere-check-vcm-commands.sh
+++ ci-operator/step-registry/ipi/conf/vsphere/check/vcm/ipi-conf-vsphere-check-vcm-commands.sh
@@ -263,8 +263,9 @@ for POOL in "${pools[@]}"; do
   if [[ -n "${VSPHERE_MULTI_NETWORKS}" ]]; then
     # seems only vcenter.ci.ibmc.devcluster.openshift.com-cidatacenter-cicluster have multi-network, other pending create lease
     # if [[ $POOL == "vcenter.ci.ibmc.devcluster.openshift.com-cidatacenter-cicluster" ]]; then
+    if [[ $POOL == "vcenter.ci.ibmc.devcluster.openshift.com-cidatacenter-1-cicluster-1" ]]; then
       networks_number=2
-    # fi
+    fi
   fi
   # shellcheck disable=SC1078
   echo "apiVersion: vspherecapacitymanager.splat.io/v1
