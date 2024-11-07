6432e1f760f7c128bd600d6ed7e102df0ad7c573
Sean Zhao <szhao@redhat.com>
2024-11-07 20:51:45 +0800

    extend execution time

--- ci-operator/step-registry/quay-tests/quay-operator-test/quay-tests-quay-operator-test-commands.sh
+++ ci-operator/step-registry/quay-tests/quay-operator-test/quay-tests-quay-operator-test-commands.sh
@@ -125,7 +125,7 @@ echo "Run extended-platform-tests"
 echo "..." $quayregistry_redis_hostname "... " $quayregistry_clair_scanner_endpoint "..." $quayregistry_postgresql_db_hostname
 
 # QUAY_OPERATOR_TESTCASE: Quay-Allns-Medium|Quay-Allns-High, Quay-High|Quay-Medium
-extended-platform-tests run all --dry-run | grep -E ${QUAY_OPERATOR_TESTCASE} | extended-platform-tests run --timeout 150m --max-parallel-tests 3 --junit-dir="${ARTIFACT_DIR}" -f - || true
+extended-platform-tests run all --dry-run | grep -E ${QUAY_OPERATOR_TESTCASE} | extended-platform-tests run --timeout 240m --max-parallel-tests 3 --junit-dir="${ARTIFACT_DIR}" -f - || true
 
 function handle_result {
 
