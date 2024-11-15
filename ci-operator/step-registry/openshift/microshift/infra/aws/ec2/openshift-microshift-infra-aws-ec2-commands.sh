22f639e803c73546069e35fbabcacdf47d3de267
vanhalenar <thalenar@redhat.com>
2024-11-15 11:48:00 +0000

    Remove test scps

--- ci-operator/step-registry/openshift/microshift/infra/aws/ec2/openshift-microshift-infra-aws-ec2-commands.sh
+++ ci-operator/step-registry/openshift/microshift/infra/aws/ec2/openshift-microshift-infra-aws-ec2-commands.sh
@@ -448,14 +448,7 @@ for aws_region in "${regions[@]}"; do
       echo "${CACHE_REGION}" > "${SHARED_DIR}/cache_region"
 
       ci_script_prologue
-      scp -F "${HOME}/.ssh/config" "ec2-user@${HOST_PUBLIC_IP}:/tmp/init_output.txt" "${ARTIFACT_DIR}/init_ec2_output.txt" || true
-      scp -F "/alabama/.ssh/config" "ec2-user@${HOST_PUBLIC_IP}:/tmp/init_output.txt" "${ARTIFACT_DIR}/init_ec2_output.txt" || true
-      scp -F "/output/.ssh/config" "ec2-user@${HOST_PUBLIC_IP}:/tmp/init_output.txt" "${ARTIFACT_DIR}/init_ec2_output.txt" || true
-
-      ls -lah /
-      ls -lah "${HOME}"
-      echo "${HOME}"
-      ls -lah "${HOME}/.ssh"
+      scp -F "${HOME}/.ssh/config" "ec2-user@${HOST_PUBLIC_IP}:/tmp/init_output.txt" "${ARTIFACT_DIR}/init_ec2_output.txt"
 
       echo "Waiting up to 5 min for RHEL host to be up."
       timeout 5m "${aws}" --region "${REGION}" ec2 wait instance-status-ok --instance-id "${INSTANCE_ID}"
