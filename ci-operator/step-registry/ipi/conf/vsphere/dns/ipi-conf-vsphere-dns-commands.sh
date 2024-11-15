60585520eb72dca1bd67b1ea3fdf2629f8a804ac
Mark Old <mold@redhat.com>
2024-11-15 14:57:36 -0800

    HIVE-2270: add hive vsphere functionality
    Reserve additional vips, if requested
    Add additional DNS records for additional vips
    Export additional vip/dns information to a new shared dir env script

--- ci-operator/step-registry/ipi/conf/vsphere/dns/ipi-conf-vsphere-dns-commands.sh
+++ ci-operator/step-registry/ipi/conf/vsphere/dns/ipi-conf-vsphere-dns-commands.sh
@@ -8,6 +8,34 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+dns_reserve_and_defer_cleanup() {
+  record=$1
+  hostname=$2
+  recordtype=$3
+
+  json_create="{
+    \"Action\": \"UPSERT\",
+    \"ResourceRecordSet\": {
+      \"Name\": \"$hostname\",
+      \"Type\": \"$recordtype\",
+      \"TTL\": 60,
+      \"ResourceRecords\": [{\"Value\": \"$record\"}]
+    }
+  }"
+  jq --argjson jc "$json_create" '.Changes += [$jc]' "${SHARED_DIR}"/dns-create.json > "${SHARED_DIR}"/tmp-create.json && mv "${SHARED_DIR}"/tmp-create.json "${SHARED_DIR}"/dns-create.json
+
+  json_delete="{
+    \"Action\": \"DELETE\",
+    \"ResourceRecordSet\": {
+      \"Name\": \"$hostname\",
+      \"Type\": \"A\",
+      \"TTL\": 60,
+      \"ResourceRecords\": [{\"Value\": \"$record\"}]
+    }
+  }"
+  jq --argjson jd "$json_delete" '.Changes += [$jd]' "${SHARED_DIR}"/dns-delete.json > "${SHARED_DIR}"/tmp-delete.json && mv "${SHARED_DIR}"/tmp-delete.json "${SHARED_DIR}"/dns-delete.json
+}
+
 echo "vmc-ci.devcluster.openshift.com" > "${SHARED_DIR}"/basedomain.txt
 
 cluster_name=${NAMESPACE}-${UNIQUE_HASH}
@@ -52,6 +80,8 @@ fi
 # Load array created in setup-vips:
 # 0: API
 # 1: Ingress
+# 2: Additional cluster API     (optional)
+# 3: Additional cluster Ingress (optional)
 declare -a vips
 mapfile -t vips < "${SHARED_DIR}"/vips.txt
 
@@ -61,81 +91,49 @@ hosted_zone_id="$(aws route53 list-hosted-zones-by-name \
             --output text)"
 echo "${hosted_zone_id}" > "${SHARED_DIR}/hosted-zone.txt"
 
-RECORD_TYPE="A"
-
-if [ "${JOB_NAME_SAFE}" = "launch" ]; then
-  # setup DNS records for clusterbot to point to the IBM VIP
-  api_dns_target='"TTL": 60, "ResourceRecords": [{"Value": "vsphere-clusterbot-2284482-dal12.clb.appdomain.cloud"}]'
-  apps_dns_target='"TTL": 60, "ResourceRecords": [{"Value": "vsphere-clusterbot-2284482-dal12.clb.appdomain.cloud"}]'
-  RECORD_TYPE="CNAME"
-else
-  # Configure DNS direct to respective VIP
-  api_dns_target='"TTL": 60, "ResourceRecords": [{"Value": "'${vips[0]}'"}]'
-  apps_dns_target='"TTL": 60, "ResourceRecords": [{"Value": "'${vips[1]}'"}]'
-fi
-
-# api-int record is needed just for Windows nodes
-# TODO: Remove the api-int entry in future
-echo "Creating DNS records..."
+echo "Creating batch file to create DNS records"
 cat > "${SHARED_DIR}"/dns-create.json <<EOF
 {
 "Comment": "Create public OpenShift DNS records for VSphere IPI CI install",
-"Changes": [{
-    "Action": "UPSERT",
-    "ResourceRecordSet": {
-      "Name": "api.$cluster_domain.",
-      "Type": "$RECORD_TYPE",
-      $api_dns_target
-      }
-    },{
-    "Action": "UPSERT",
-    "ResourceRecordSet": {
-      "Name": "api-int.$cluster_domain.",
-      "Type": "A",
-      "TTL": 60,
-      "ResourceRecords": [{"Value": "${vips[0]}"}]
-      }
-    },{
-    "Action": "UPSERT",
-    "ResourceRecordSet": {
-      "Name": "*.apps.$cluster_domain.",
-      "Type": "$RECORD_TYPE",
-      $apps_dns_target
-      }
-}]}
+"Changes": []
+}
 EOF
 
-# api-int record is needed for Windows nodes
-# TODO: Remove the api-int entry in future
 echo "Creating batch file to destroy DNS records"
-
 cat > "${SHARED_DIR}"/dns-delete.json <<EOF
 {
 "Comment": "Delete public OpenShift DNS records for VSphere IPI CI install",
-"Changes": [{
-    "Action": "DELETE",
-    "ResourceRecordSet": {
-      "Name": "api.$cluster_domain.",
-      "Type": "$RECORD_TYPE",
-      $api_dns_target
-      }
-    },{
-    "Action": "DELETE",
-    "ResourceRecordSet": {
-      "Name": "api-int.$cluster_domain.",
-      "Type": "A",
-      "TTL": 60,
-      "ResourceRecords": [{"Value": "${vips[0]}"}]
-      }
-    },{
-    "Action": "DELETE",
-    "ResourceRecordSet": {
-      "Name": "*.apps.$cluster_domain.",
-      "Type": "$RECORD_TYPE",
-      $apps_dns_target
-      }
-}]}
+"Changes": []
+}
 EOF
+
+HOSTNAMES=("api.$cluster_domain." "*.apps.$cluster_domain.")
+if [ "${VSPHERE_ADDITIONAL_CLUSTER}" = "true" ]; then
+  # Add spoke cluster domain to hostnames in order to provision extra hive cluster
+  additional_cluster_name="hive-${cluster_name}-spoke"
+  additional_cluster_domain="${additional_cluster_name}.${base_domain}"
+  HOSTNAMES+=("api.$additional_cluster_domain." "*.apps.$additional_cluster_domain.")
+  cat > "${SHARED_DIR}/additional_cluster.sh" <<EOF
+export ADDITIONAL_CLUSTER_NAME=$additional_cluster_name
+export ADDITIONAL_CLUSTER_API_VIP=${vips[2]}
+export ADDITIONAL_CLUSTER_INGRESS_VIP=${vips[3]}
+EOF
+fi
+
+for ((i = 0; i < ${#HOSTNAMES[@]}; i++)); do
+  echo "Adding ${HOSTNAMES[$i]} to batch files"
+  if [ "${JOB_NAME_SAFE}" = "launch" ]; then
+    dns_reserve_and_defer_cleanup "vsphere-clusterbot-2284482-dal12.clb.appdomain.cloud" ${HOSTNAMES[$i]} "CNAME"
+  else
+    dns_reserve_and_defer_cleanup ${vips[$i]} ${HOSTNAMES[$i]} "A"
+  fi
+done
+
+# Snowflake for the default api-int, which shares a vip with the default api.
+# api-int record is needed just for Windows nodes
+# TODO: Remove the api-int entry in future
+echo "Adding "api-int.$cluster_domain." to batch files"
+dns_reserve_and_defer_cleanup ${vips[0]} "api-int.$cluster_domain." "A"
 
 id=$(aws route53 change-resource-record-sets --hosted-zone-id "$hosted_zone_id" --change-batch file:///"${SHARED_DIR}"/dns-create.json --query '"ChangeInfo"."Id"' --output text)
 
