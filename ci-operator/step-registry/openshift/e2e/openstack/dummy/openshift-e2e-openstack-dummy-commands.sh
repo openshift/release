3239b9761abb8b732b505e8bc4bea6e7316eec8b
Miguel Carpio <mcarpio@redhat.com>
2024-11-21 14:40:58 +0100

    Get openstack catalog list from the underlying ocp (where run the RHOSO Control Plane)

---
+++ ci-operator/step-registry/openshift/e2e/openstack/dummy/openshift-e2e-openstack-dummy-commands.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# For disconnected or otherwise unreachable environments, we want to
+# have steps use an HTTP(S) proxy to reach the API server. This proxy
+# configuration file should export HTTP_PROXY, HTTPS_PROXY, and NO_PROXY
+# environment variables, as well as their lowercase equivalents (note
+# that libcurl doesn't recognize the uppercase variables).
+if test -f "${SHARED_DIR}/proxy-conf.sh"
+then
+	# shellcheck disable=SC1090
+	source "${SHARED_DIR}/proxy-conf.sh"
+fi
+
+# Get openstack catalog list from the underlying ocp (where run the RHOSO Control Plane)
+KUBECONFIG=${SHARED_DIR}/underlying-kubeconfig oc rsh -n openstack openstackclient openstack catalog list
