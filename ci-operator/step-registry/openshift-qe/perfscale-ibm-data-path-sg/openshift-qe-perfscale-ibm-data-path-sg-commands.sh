118416b3af9032782b477aa4fa3bd5a6c0e6bd03
Sachin Ninganure <sninganu@sninganu.pnq.csb>
2024-11-21 12:23:53 +0530

    incoroprated review comments

--- ci-operator/step-registry/openshift-qe/perfscale-ibm-data-path-sg/openshift-qe-perfscale-ibm-data-path-sg-commands.sh
+++ ci-operator/step-registry/openshift-qe/perfscale-ibm-data-path-sg/openshift-qe-perfscale-ibm-data-path-sg-commands.sh
@@ -9,7 +9,6 @@ trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wa
 # Configure ibmcloud
 # to find way to store region and ic_api_key
 export IBMCLOUD_CLI=ibmcloud
-export IBMCLOUD_HOME=/output
 region=$(jq -r .ibmcloud.region ${SHARED_DIR}/metadata.json)
 export region
 "${IBMCLOUD_CLI}" config --check-version=false
@@ -20,12 +19,12 @@ echo "logging in..."
 resource_group_name=$(jq -r .ibmcloud.resourceGroupName ${SHARED_DIR}/metadata.json)
 echo resource group is $resource_group_name
 
-ibmcloud resource group $resource_group_name || exit 1
-ibmcloud target -g $resource_group_name -r $region
-sg=$(ibmcloud is sgs --resource-group-name $resource_group_name --output json | jq -r '.[] | select(.name | contains("cluster-wide"))|.id')
+"${IBMCLOUD_CLI}" resource group $resource_group_name || exit 1
+"${IBMCLOUD_CLI}" target -g $resource_group_name -r $region
+sg=$("${IBMCLOUD_CLI}" is sgs --resource-group-name $resource_group_name --output json | jq -r '.[] | select(.name | contains("cluster-wide"))|.id')
 echo "Print resource group in detail"
 
-ibmcloud is sgs --resource-group-name $resource_group_name
+"${IBMCLOUD_CLI}" is sgs --resource-group-name $resource_group_name
 echo $sg
 
 CLUSTER_NAME=$(oc get infrastructure cluster -o json | jq -r '.status.apiServerURL' | awk -F.  '{print$2}')
