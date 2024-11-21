87af4ae760ddffbf48f91108dcc84444e200ead1
Niranjan M.R <mrniranjan@redhat.com>
2024-11-21 11:14:25 +0530

    export ANSIBLE_CONFIG environment variable
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>

--- ci-operator/step-registry/telcov10n/functional/hcp-cnf/tests/telcov10n-functional-hcp-cnf-tests-commands.sh
+++ ci-operator/step-registry/telcov10n/functional/hcp-cnf/tests/telcov10n-functional-hcp-cnf-tests-commands.sh
@@ -1,27 +1,54 @@
-#!/bin/bash
-
 set -o nounset
 set -o errexit
 set -o pipefail
 
-# Applying Performance Profile into the cluster
-echo "Running the PP ansible playbook"
-ansible-playbook -vv playbooks/performance_profile.yml -e kubeconfig=$SHARED_DIR/mgmt-kubeconfig
 
-echo "!!!!!!!!!!!!!!!!!!! STARTING THE TEST SCRIPT !!!!!!!!!!!!!!!!!!!"
+#### Local variables
+TELCO_CI_REPO="https://github.com/openshift-kni/telco-ci.git"
 NTO_REPO="https://github.com/openshift/cluster-node-tuning-operator.git"
-NTO_REPO_DIR=${NTO_REPO_DIR:-"$(mktemp -d -t nto-XXXXX)/cluster-node-tuning-operator"}
+
 NTO_BRANCH="master"
 GINKGO_LABEL="tier-0"
 GINKGO_SUITES="test/e2e/performanceprofile/functests"
 
+### This need cleanups
 function clonerepo() {
-	git clone -b "${NTO_BRANCH}" "${NTO_REPO}" "${NTO_REPO_DIR}"
+        git clone -b "${NTO_BRANCH}" "${NTO_REPO}" "${NTO_REPO_DIR}"
 }
+
+# Fix user ID's in a container
+~/fix_uid.sh
+
+echo "******************* Telco CNF Compute QE Setup Command ******************"
+date +%s > "${SHARED_DIR}"/start_time
+
+
+##clone the github/openshift-kni/telco5g-ci
+mkdir -p "${SHARED_DIR}"/repos
+git clone "${TELCO_CI_REPO}" ${SHARED_DIR}/repos/telco-ci
+
+#install ansible modules
+ansible-galaxy collection install -r "${SHARED_DIR}"/repos/telco-ci/ansible-requirements.yaml
+
+### lets display the contents of ${SHARED_DIR}
+ls -ltr "${SHARED_DIR}"
+
+# Applying Performance Profile into the cluster
+# TODO: Check if the kubeconfig file exists
+echo "**************Applying Performance Profile **************"
+export ANSIBLE_CONFIG="${SHARED_DIR}"/repos/telco-ci/ansible.cfg
+ansible-playbook -vv "${SHARED_DIR}"/repos/telco-ci/playbooks/performance_profile.yml -e kubeconfig="${SHARED_DIR}"/mgmt-kubeconfig
+
+
+#### If we made this far we are good with running tests
+echo "********************* Running Tests ****************************"
 
 if (( $(echo "${T5CI_VERSION} < 4.18" | bc -l) )); then
     NTO_BRANCH="release-${T5CI_VERSION}"
 fi
+
+## create NTO REPO DIR only if we have come this far
+NTO_REPO_DIR=${NTO_REPO_DIR:-"$(mktemp -d -t nto-XXXXX)/cluster-node-tuning-operator"}
 
 mkdir -p "${NTO_REPO_DIR}"
 echo "Running on branch ${NTO_BRANCH}"
@@ -46,11 +73,12 @@ echo "Go version: $(go version)"
 
 echo "************ telco5g cnf-tests commands ************"
 
-echo "Running ${GINKGO_LABEL} tests" 
-GOFLAGS=-mod=vendor ginkgo --no-color --v -r ${GINKGO_SUITES}  --timeout=1h --keep-separate-reports --keep-going --flake-attempts=2 --label-filter="${GINKGO_LABEL}" --junit-report=_report_${GINKGO_LABEL}.xml --output-dir=${ARTIFACT_DIR}
+echo "Running ${GINKGO_LABEL} tests"
+GOFLAGS=-mod=vendor ginkgo --no-color --v -r ${GINKGO_SUITES}  \
+--timeout=1h --keep-separate-reports --keep-going --flake-attempts=2 \
+--label-filter="${GINKGO_LABEL}" --junit-report=_report_${GINKGO_LABEL}.xml --output-dir=${ARTIFACT_DIR}
 
 export TESTS_REPORTS_PATH="${ARTIFACT_DIR}/"
-
 popd
 
 echo "!!!!!!!!!!!!!!!!!!! FINISHED THE TEST SCRIPT !!!!!!!!!!!!!!!!!!!"
