b08a13e55b85e9477469c148c02b47e4c0dea4d7
Miguel Carpio <mcarpio@redhat.com>
2024-11-21 16:12:31 +0100

    Don't load proxy config

--- ci-operator/step-registry/openshift/e2e/openstack/dummy/openshift-e2e-openstack-dummy-commands.sh
+++ ci-operator/step-registry/openshift/e2e/openstack/dummy/openshift-e2e-openstack-dummy-commands.sh
@@ -4,16 +4,26 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+function info() {
+  printf '%s: INFO: %s\n' "$(date --utc +%Y-%m-%dT%H:%M:%SZ)" "$*"
+}
+
 # For disconnected or otherwise unreachable environments, we want to
 # have steps use an HTTP(S) proxy to reach the API server. This proxy
 # configuration file should export HTTP_PROXY, HTTPS_PROXY, and NO_PROXY
 # environment variables, as well as their lowercase equivalents (note
 # that libcurl doesn't recognize the uppercase variables).
-if test -f "${SHARED_DIR}/proxy-conf.sh"
-then
-	# shellcheck disable=SC1090
-	source "${SHARED_DIR}/proxy-conf.sh"
+# if test -f "${SHARED_DIR}/proxy-conf.sh"
+# then
+#	# shellcheck disable=SC1090
+#	source "${SHARED_DIR}/proxy-conf.sh"
+# fi
+
+if [[ ! -f "${SHARED_DIR}/underlying-kubeconfig" ]]; then
+	info "underlying-kubeconfig wasn't found"
+    exit 1
 fi
 
 # Get openstack catalog list from the underlying ocp (where run the RHOSO Control Plane)
-KUBECONFIG=${SHARED_DIR}/underlying-kubeconfig oc rsh -n openstack openstackclient openstack catalog list
+export KUBECONFIG=${SHARED_DIR}/underlying-kubeconfig 
+oc rsh -n openstack openstackclient openstack catalog list
