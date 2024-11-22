347c8d540440e688294c26a7b417834236bb74bb
Mike Gahagan <mgahagan@redhat.com>
2024-11-22 14:07:16 -0500

    try vault

--- ci-operator/step-registry/cucushift/installer/rehearse/azure/aro/existing/cucushift-installer-rehearse-azure-aro-existing-commands.sh
+++ ci-operator/step-registry/cucushift/installer/rehearse/azure/aro/existing/cucushift-installer-rehearse-azure-aro-existing-commands.sh
@@ -7,23 +7,24 @@ set -o errexit
 set -o pipefail
 set -x
 
+cp -rv /secrets/* ${SHARED_DIR}
 
-export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
-export AWS_REGION="us-east-1"
-export AWS_PAGER=""
+#export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
+#export AWS_REGION="us-east-1"
+#export AWS_PAGER=""
 
-ls -l ${AWS_SHARED_CREDENTIALS_FILE}
+#ls -l ${AWS_SHARED_CREDENTIALS_FILE}
 
 ## prepare kubeconfig
-aws s3 cp s3://${BUCKET_NAME}/kubeconfig  ${SHARED_DIR}/kubeconfig
+#aws s3 cp s3://${BUCKET_NAME}/kubeconfig  ${SHARED_DIR}/kubeconfig
 #cp ${SHARED_DIR}/kubeconfig ${SHARED_DIR}/nested_kubeconfig
 
 ## run_time env users
 #aws s3 cp s3://${BUCKET_NAME}/runtime_env "${SHARED_DIR}/runtime_env"
 
-aws s3 cp s3://${BUCKET_NAME}/console.url "${SHARED_DIR}/console.url"
-aws s3 cp s3://${BUCKET_NAME}/api.login "${SHARED_DIR}/api.login"
-aws s3 cp s3://${BUCKET_NAME}/api.url "${SHARED_DIR}/api.url"
+#aws s3 cp s3://${BUCKET_NAME}/console.url "${SHARED_DIR}/console.url"
+#aws s3 cp s3://${BUCKET_NAME}/api.login "${SHARED_DIR}/api.login"
+#aws s3 cp s3://${BUCKET_NAME}/api.url "${SHARED_DIR}/api.url"
 
-aws s3 cp s3://${BUCKET_NAME}/cluster-name "${SHARED_DIR}/cluster-name"
-aws s3 cp s3://${BUCKET_NAME}/cluster-type "${SHARED_DIR}/cluster-type"
+#aws s3 cp s3://${BUCKET_NAME}/cluster-name "${SHARED_DIR}/cluster-name"
+#aws s3 cp s3://${BUCKET_NAME}/cluster-type "${SHARED_DIR}/cluster-type"
