098e4488c52afaf858d9be67c9d9bef76a9f93b2
Weinan Liu <weinliu@redhat.com>
2024-11-24 14:40:09 +0800

    add more debug info, add wait

--- ci-operator/step-registry/cucushift/winc/prepare/cucushift-winc-prepare-commands.sh
+++ ci-operator/step-registry/cucushift/winc/prepare/cucushift-winc-prepare-commands.sh
@@ -4,26 +4,56 @@ set -o nounset
 set -o pipefail
 set -x
 
-function create_winc_test_configmap()
-{
-
-  oc create configmap winc-test-config -n winc-test --from-literal=primary_windows_image="${1}" --from-literal=primary_windows_container_image="${2}"
-
-  # Display pods and configmap
-  oc get pod -owide -n winc-test
-  oc get cm winc-test-config -oyaml -n winc-test
+# Function to debug deployment status and logs
+function debug_deployment() {
+    local deployment_name=$1
+    echo "=== Debug info for $deployment_name ==="
+    
+    # Check pod status
+    echo "=== Pod Status ==="
+    oc get pods -n winc-test -l app=$deployment_name -o wide
+    
+    # Show pod details
+    echo "=== Pod Description ==="
+    oc describe pods -n winc-test -l app=$deployment_name
+    
+    # Show deployment status
+    echo "=== Deployment Status ==="
+    oc describe deployment $deployment_name -n winc-test
+    
+    # Show pod logs
+    echo "=== Pod Logs ==="
+    for pod in $(oc get pods -n winc-test -l app=$deployment_name -o name); do
+        echo "--- Logs for $pod ---"
+        oc logs -n winc-test $pod
+    done
+    
+    # Show related events
+    echo "=== Recent Events ==="
+    oc get events -n winc-test --sort-by='.lastTimestamp' | grep $deployment_name
 }
 
-function create_workloads()
-{
-  oc new-project winc-test
-  # turn off the automatic label synchronization required for PodSecurity admission
-  # set pods security profile to privileged. See https://kubernetes.io/docs/concepts/security/pod-security-admission/#pod-security-levels
-  oc label namespace winc-test security.openshift.io/scc.podSecurityLabelSync=false pod-security.kubernetes.io/enforce=privileged  --overwrite
+# Function to create test configmap
+function create_winc_test_configmap() {
+    oc create configmap winc-test-config -n winc-test \
+        --from-literal=primary_windows_image="${1}" \
+        --from-literal=primary_windows_container_image="${2}"
 
+    # Display pods and configmap
+    oc get pod -owide -n winc-test
+    oc get cm winc-test-config -oyaml -n winc-test
+}
 
-  # Create Windows workload
-  oc create -f - <<EOF
+# Function to create Windows and Linux workloads
+function create_workloads() {
+    oc new-project winc-test
+    
+    # Configure Pod Security Admission
+    oc label namespace winc-test security.openshift.io/scc.podSecurityLabelSync=false pod-security.kubernetes.io/enforce=privileged --overwrite
+
+    # Create Windows workload
+    echo "=== Creating Windows Workload ==="
+    oc create -f - <<EOF
 apiVersion: v1
 kind: Service
 metadata:
@@ -32,7 +62,6 @@ metadata:
     app: win-webserver
 spec:
   ports:
-  # the port that this service should serve on
   - port: 80
     targetPort: 80
   selector:
@@ -76,11 +105,13 @@ spec:
               runAsUserName: "ContainerAdministrator"
 EOF
 
-  # Wait up to 5 minutes for Windows workload to be ready
-  oc wait deployment win-webserver -n winc-test --for condition=Available=True --timeout=5m
+    # Wait for Windows workload
+    oc wait deployment win-webserver -n winc-test --for condition=Available=True --timeout=5m
+    debug_deployment "win-webserver"
 
-  # Create Linux workload
-  oc create -f - <<EOF
+    # Create Linux workload
+    echo "=== Creating Linux Workload ==="
+    oc create -f - <<EOF
 apiVersion: v1
 kind: Service
 metadata:
@@ -89,7 +120,6 @@ metadata:
     app: linux-webserver
 spec:
   ports:
-  # the port that this service should serve on
   - port: 8080
     targetPort: 8080
   selector:
@@ -119,54 +149,69 @@ spec:
         ports:
         - containerPort: 8080
 EOF
-  # Wait up to 5 minutes for Linux workload to be ready
-  oc wait deployment linux-webserver -n winc-test --for condition=Available=True --timeout=5m
+
+    # Wait for Linux workload
+    oc wait deployment linux-webserver -n winc-test --for condition=Available=True --timeout=5m
+    debug_deployment "linux-webserver"
+
+    # Show additional cluster information
+    echo "=== Node Status ==="
+    oc get nodes -o wide
+    
+    echo "=== Namespace Security Context ==="
+    oc get namespace winc-test -o yaml
+    
+    echo "=== Network Policies ==="
+    oc get networkpolicy -n winc-test
+    
+    echo "=== Storage Class & PV Status ==="
+    oc get sc,pv,pvc -n winc-test
 }
 
+# Get the infrastructure platform type
 IAAS_PLATFORM=$(oc get infrastructure cluster -o=jsonpath="{.status.platformStatus.type}"| tr '[:upper:]' '[:lower:]')
 
-
+# Get Windows machineset information
 winworker_machineset_name=$(oc get machineset -n openshift-machine-api -o json | jq -r '.items[] | select(.metadata.name | test("win")).metadata.name')
 winworker_machineset_replicas=$(oc get machineset -n openshift-machine-api $winworker_machineset_name -o jsonpath="{.spec.replicas}")
 
+# Wait for Windows nodes
 echo "Waiting for Windows nodes to come up in Running state"
-while [[ $(oc -n openshift-machine-api get machineset/${winworker_machineset_name} -o 'jsonpath={.status.readyReplicas}') != "${winworker_machineset_replicas}" ]]; do echo -n "." && sleep 10; done
+while [[ $(oc -n openshift-machine-api get machineset/${winworker_machineset_name} -o 'jsonpath={.status.readyReplicas}') != "${winworker_machineset_replicas}" ]]; do 
+    echo -n "." && sleep 10
+done
 
-# Make sure the Windows nodes get in Ready state
+# Wait for Windows nodes to be ready
 oc wait nodes -l kubernetes.io/os=windows --for condition=Ready=True --timeout=15m
 
-# Choose the Windows container vesion depending on the Windows version
-# installed on the Windows workers
+# Determine Windows container version based on OS version
 os_version=$(oc get nodes -l 'kubernetes.io/os=windows' -o=jsonpath="{.items[0].status.nodeInfo.osImage}")
 
 windows_container_image="mcr.microsoft.com/powershell:lts-nanoserver-ltsc2022"
-if [[ "$os_version" == *"2019"* ]]
-then
+if [[ "$os_version" == *"2019"* ]]; then
     windows_container_image="mcr.microsoft.com/powershell:lts-nanoserver-1809"
 fi
 
+# Get Windows OS image ID based on platform
 case "$IAAS_PLATFORM" in
-  aws)
-	windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.ami.id}" -n openshift-machine-api)
-    ;;
-  azure)
-	windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.image.sku}" -n openshift-machine-api)
-	;;
-  vsphere)
-	windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.template}" -n openshift-machine-api)
-    ;;
-  gcp)
-	# we need the value after family/
-	# in this example projects/windows-cloud/global/images/family/windows-2022-core
-	# windows_os_image_id needs to be windows-2022-core
-	windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.disks[0].image}" -n openshift-machine-api | tr "/" "\n" | tail -n1)
-    ;;
-  *)
-    echo "Cloud provider \"$IAAS_PLATFORM\" is not supported by WMCO"
-    exit 1
-    ;;
+    aws)
+        windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.ami.id}" -n openshift-machine-api)
+        ;;
+    azure)
+        windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.image.sku}" -n openshift-machine-api)
+        ;;
+    vsphere)
+        windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.template}" -n openshift-machine-api)
+        ;;
+    gcp)
+        windows_os_image_id=$(oc get machineset $winworker_machineset_name -o=jsonpath="{.spec.template.spec.providerSpec.value.disks[0].image}" -n openshift-machine-api | tr "/" "\n" | tail -n1)
+        ;;
+    *)
+        echo "Cloud provider \"$IAAS_PLATFORM\" is not supported by WMCO"
+        exit 1
+        ;;
 esac
 
+# Create workloads and configmap
 create_workloads $windows_container_image
-
 create_winc_test_configmap $windows_os_image_id $windows_container_image
