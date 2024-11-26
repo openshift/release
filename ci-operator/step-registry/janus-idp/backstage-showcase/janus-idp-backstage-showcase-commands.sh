deb96ffdff62fa5353d8e5239c6c4bafc35dbb66
Subhash Khileri <skhileri@redhat.com>
2024-11-26 13:54:24 +0530

    rhdh cluster pool setup

--- ci-operator/step-registry/janus-idp/backstage-showcase/janus-idp-backstage-showcase-commands.sh
+++ ci-operator/step-registry/janus-idp/backstage-showcase/janus-idp-backstage-showcase-commands.sh
@@ -82,4 +82,38 @@ else
 
 fi
 
-bash ./.ibm/pipelines/openshift-ci-tests.sh
+echo "Test Start"
+
+export OPENSHIFT_PASSWORD
+export OPENSHIFT_API
+export OPENSHIFT_USERNAME
+
+OPENSHIFT_API="$(yq e '.clusters[0].cluster.server' "$KUBECONFIG")"
+OPENSHIFT_USERNAME="kubeadmin"
+
+yq -i 'del(.clusters[].cluster.certificate-authority-data) | .clusters[].cluster.insecure-skip-tls-verify=true' "$KUBECONFIG"
+if [[ -s "$KUBEADMIN_PASSWORD_FILE" ]]; then
+    OPENSHIFT_PASSWORD="$(cat "$KUBEADMIN_PASSWORD_FILE")"
+elif [[ -s "${SHARED_DIR}/kubeadmin-password" ]]; then
+    # Recommendation from hypershift qe team in slack channel..
+    OPENSHIFT_PASSWORD="$(cat "${SHARED_DIR}/kubeadmin-password")"
+else
+    echo "Kubeadmin password file is empty... Aborting job"
+    exit 1
+fi
+
+timeout --foreground 5m bash <<-"EOF"
+    while ! oc login "$OPENSHIFT_API" -u "$OPENSHIFT_USERNAME" -p "$OPENSHIFT_PASSWORD" --insecure-skip-tls-verify=true; do
+            sleep 20
+    done
+EOF
+if [ $? -ne 0 ]; then
+    echo "Timed out waiting for login"
+    exit 1
+fi
+
+# sleep 60*60
+oc version
+oc auth can-i '*' '*'
+
+# bash ./.ibm/pipelines/openshift-ci-tests.sh
