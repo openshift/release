f95cc0ece6ceff0818c99f4908c2ed6fd3681d45
pstefans <pstefans@redhat.com>
2024-11-27 19:07:58 +0000

    HOSTEDCP-2193: reuse SPs in hypershift aks e2e
    moving hypershift aks e2e to reuse SPs that are passed in through a file
    from vault. This move is required as each HC created during the e2e run
    was creating 8 SPs which was contributing towards a quota that we were
    hitting.

---
+++ ci-operator/step-registry/hypershift/azure/aks/attach-kv/hypershift-azure-aks-attach-kv-commands.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+AZURE_AUTH_LOCATION="/etc/hypershift-ci-jobs-azurecreds/credentials.json"
+AZURE_AUTH_CLIENT_ID="$(<"${AZURE_AUTH_LOCATION}" jq -r .clientId)"
+AZURE_AUTH_CLIENT_SECRET="$(<"${AZURE_AUTH_LOCATION}" jq -r .clientSecret)"
+AZURE_AUTH_TENANT_ID="$(<"${AZURE_AUTH_LOCATION}" jq -r .tenantId)"
+AZURE_AUTH_SUBSCRIPTION_ID="$(<"${AZURE_AUTH_LOCATION}" jq -r .subscriptionId)"
+
+AZURE_KEY_VAULT_INFO_LOCATION="/etc/hypershift-ci-jobs-azurecreds/keyvault-info.json"
+KV_NAME="$(<"${AZURE_KEY_VAULT_INFO_LOCATION}" jq -r .keyvaultName)"
+KV_RG_NAME="$(<"${AZURE_KEY_VAULT_INFO_LOCATION}" jq -r .keyvaultRGName)"
+
+az --version
+az cloud set --name AzureCloud
+az login --service-principal -u "${AZURE_AUTH_CLIENT_ID}" -p "${AZURE_AUTH_CLIENT_SECRET}" --tenant "${AZURE_AUTH_TENANT_ID}" --output none
+
+set -x
+
+RESOURCE_GROUP="$(<"${SHARED_DIR}/resourcegroup_aks")"
+CLUSTER="$(<"${SHARED_DIR}/cluster-name")"
+
+AZURE_KEY_VAULT_AUTHORIZED_OBJECT_ID=$(az aks show -n $CLUSTER -g $RESOURCE_GROUP | jq .addonProfiles.azureKeyvaultSecretsProvider.identity.objectId -r)
+
+echo "Granting the AKS clusters azureKeyvaultSecretsProvider ServicePrincipal permissions to the KeyVault Resource Group"
+az role assignment create \
+--assignee-object-id $AZURE_KEY_VAULT_AUTHORIZED_OBJECT_ID \
+--role "Key Vault Secrets User" \
+--scope "/subscriptions/${AZURE_AUTH_SUBSCRIPTION_ID}/resourceGroups/${KV_RG_NAME}" \
+--assignee-principal-type ServicePrincipal
+
+echo "Granting ServicePrincipal permissions to the KeyVault"
+SP_ID=$(az ad sp show --id "$AZURE_AUTH_CLIENT_ID" --query id -o tsv)
+KV_ID=$(az keyvault show --name "$KV_NAME" -g "$KV_RG_NAME" --query id -o tsv)
+az role assignment create --assignee "$SP_ID" --scope "$KV_ID" --role "Key Vault Administrator"
