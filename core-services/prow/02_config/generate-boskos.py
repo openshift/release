3650ebb6fab3d1a981275c3abd24def17c9e4de5
Omri Bar Haim <obarhaim@redhat.com>
2024-11-27 11:31:57 +0200

    Adding AWS managed OSD RHOAI QE quota to Boskos config (#59196)
    * adding boskos quota & .awscred for aws-managed-osd-rhoai-qe cluster profile
    
    * updating boskos config
    
    * fix ci-secret-bootstrap after rebase

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -473,6 +473,10 @@ CONFIG = {
     'aws-managed-rosa-rhoai-qe-quota-slice': {
         'us-east-1': 30,
         'us-east-2': 30,
+    },
+    'aws-managed-osd-rhoai-qe-quota-slice': {
+        'us-east-1': 30,
+        'us-east-2': 30,
     }
 }
 
