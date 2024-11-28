622d6fb689e5da6687587c392ca3ab848d4e9d22
oharan2 <oharan@redhat.com>
2024-11-28 19:29:08 +0200

    Fix checks

--- ci-operator/step-registry/mpiit/interop-aws-reporter/mpiit-interop-aws-reporter-commands.sh
+++ ci-operator/step-registry/mpiit/interop-aws-reporter/mpiit-interop-aws-reporter-commands.sh
@@ -2,6 +2,11 @@
 set -o nounset
 set -o errexit
 set -o pipefail
+set -o verbose
 
 sleep 2h
 RUN_COMMAND="poetry run swach --help"
+
+echo "$RUN_COMMAND" | sed -r "s/token [=A-Za-z0-9\.\-]+/token hashed-token /g"
+
+${RUN_COMMAND}
