208fdaa00499bd61d7b8c283190d3b95479da18d
obochan-rh <obochan@redhat.com>
2024-11-28 15:58:58 +0200

    fix chainsaw yaml full path

--- ci-operator/step-registry/telcov10n/functional/dast/tests/telcov10n-functional-dast-tests-commands.sh
+++ ci-operator/step-registry/telcov10n/functional/dast/tests/telcov10n-functional-dast-tests-commands.sh
@@ -4,6 +4,7 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+dastdir="/tmp/telco-dast-qe/tests/dast"
 # set default values
 WORKSPACE=${SHARED_DIR}
 export KUBECONFIG=${WORKSPACE}/kubeconfig
@@ -13,9 +14,9 @@ unset NAMESPACE
 
 # Execute test
 chainsaw test \
---config ".chainsaw.yaml" \
+--config "$dastdir/.chainsaw.yaml" \
 --report-name "junit_telco_tests_dast" \
 --report-path "$ARTIFACT_DIR" \
 --report-format "XML" \
 --test-dir \
-/tmp/telco-dast-qe/tests/dast
+$dastdir
