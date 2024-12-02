07fd7f4dee1aa3d68d7918bc412a6a7225438d7b
Krishna Harsha Voora <krvoora@redhat.com>
2024-12-02 14:39:09 +0530

    Updates the substitution
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>

--- ci-operator/step-registry/openshift-qe/perfscale-ibm-data-path-sg/openshift-qe-perfscale-ibm-data-path-sg-commands.sh
+++ ci-operator/step-registry/openshift-qe/perfscale-ibm-data-path-sg/openshift-qe-perfscale-ibm-data-path-sg-commands.sh
@@ -7,32 +7,30 @@ set -o pipefail
 trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
 
 # Configure ibmcloud
-# to find way to store region and ic_api_key
+# Store region and ic_api_key
 export IBMCLOUD_CLI=ibmcloud
-export IBMCLOUD_HOME=/output
-region=$(jq -r .ibmcloud.region ${SHARED_DIR}/metadata.json)
-export region
+export REGION=$(jq -r .ibmcloud.region ${SHARED_DIR}/metadata.json)
 "${IBMCLOUD_CLI}" config --check-version=false
 
-echo "logging in..."
-"${IBMCLOUD_CLI}" login -r ${region} --apikey @"${CLUSTER_PROFILE_DIR}/ibmcloud-api-key"
+echo "Logging in..."
+"${IBMCLOUD_CLI}" login -r ${REGION} --apikey @"${CLUSTER_PROFILE_DIR}/ibmcloud-api-key"
 "${IBMCLOUD_CLI}" plugin list
-resource_group_name=$(jq -r .ibmcloud.resourceGroupName ${SHARED_DIR}/metadata.json)
-echo resource group is $resource_group_name
+RESOURCE_GROUP_NAME=$(jq -r .ibmcloud.resourceGroupName ${SHARED_DIR}/metadata.json)
+echo "Resource Group is ${RESOURCE_GROUP_NAME}"
 
-"${IBMCLOUD_CLI}" resource group $resource_group_name || exit 1
-"${IBMCLOUD_CLI}" target -g $resource_group_name -r $region
-sg=$(ibmcloud is sgs --resource-group-name $resource_group_name --output json | jq -r '.[] | select(.name | contains("cluster-wide"))|.id')
+"${IBMCLOUD_CLI}" resource group ${RESOURCE_GROUP_NAME} || exit 1
+"${IBMCLOUD_CLI}" target -g ${RESOURCE_GROUP_NAME} -r ${REGION}
+SG=$(${IBMCLOUD_CLI} is sgs --resource-group-name ${RESOURCE_GROUP_NAME} --output json | jq -r '.[] | select(.name | contains("cluster-wide"))|.id')
 echo "Print security group in detail"
 
-ibmcloud is sgs --resource-group-name $resource_group_name
-echo $sg
+ibmcloud is sgs --resource-group-name ${RESOURCE_GROUP_NAME}
+echo ${SG}
 
 CLUSTER_NAME=$(oc get infrastructure cluster -o json | jq -r '.status.apiServerURL' | awk -F.  '{print$2}')
 
 echo "Updating security group rules for data-path test on cluster $CLUSTER_NAME"
-echo "Adding rule to SG $sg and run NPT"
-echo "running ${IBMCLOUD_CLI} is security-group-rule-add $sg inbound tcp --port-min=10000 --port-max=61000 ${IBMCLOUD_CLI} is security-group-rule-add $sg inbound udp --port-min=10000 --port-max=61000"
+echo "Adding rule to Security Group ${SG} and run NPT"
+echo "Running ${IBMCLOUD_CLI} is security-group-rule-add ${SG} inbound tcp --port-min=10000 --port-max=61000 ${IBMCLOUD_CLI} is security-group-rule-add ${SG} inbound udp --port-min=10000 --port-max=61000"
 
-"${IBMCLOUD_CLI}" is security-group-rule-add $sg inbound tcp --port-min=10000 --port-max=61000
-"${IBMCLOUD_CLI}" is security-group-rule-add $sg inbound udp --port-min=10000 --port-max=61000
+"${IBMCLOUD_CLI}" is security-group-rule-add ${SG} inbound tcp --port-min=10000 --port-max=61000
+"${IBMCLOUD_CLI}" is security-group-rule-add ${SG} inbound udp --port-min=10000 --port-max=61000
