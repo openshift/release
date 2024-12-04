8f66e0a1872cccb1ca6e12742b118c10da87e582
Shang Gao <sgao@redhat.com>
2024-12-04 11:16:01 +0800

    debug

--- ci-operator/step-registry/ipi/conf/vsphere/check/vcm/ipi-conf-vsphere-check-vcm-commands.sh
+++ ci-operator/step-registry/ipi/conf/vsphere/check/vcm/ipi-conf-vsphere-check-vcm-commands.sh
@@ -263,8 +263,8 @@ for POOL in "${pools[@]}"; do
   networks_number=1
   if [[ -n "${VSPHERE_MULTI_NETWORKS}" ]]; then
     # seems only vcenter.ci.ibmc.devcluster.openshift.com-cidatacenter-cicluster have multi-network, other pending create lease
-    # if [[ $POOL == "vcenter.ci.ibmc.devcluster.openshift.com-cidatacenter-cicluster" ]]; then
-    if [[ $POOL == "vcenter.ci.ibmc.devcluster.openshift.com-cidatacenter-1-cicluster-1" ]]; then
+    if [[ $POOL == "vcenter.ci.ibmc.devcluster.openshift.com-cidatacenter-cicluster" ]]; then
+    # if [[ $POOL == "vcenter.ci.ibmc.devcluster.openshift.com-cidatacenter-1-cicluster-1" ]]; then
       networks_number=2
     fi
   fi
@@ -289,7 +289,7 @@ spec:
 done
 
 log "waiting for lease to be fulfilled..."
-sleep 3600
+# sleep 3600
 n=0
 until [ "$n" -ge 5 ]
 do
