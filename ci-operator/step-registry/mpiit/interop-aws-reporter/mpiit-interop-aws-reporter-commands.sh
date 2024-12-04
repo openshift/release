fc80ca174da97d83fc667aba3b708e1e75daedee
oharan2 <oharan@redhat.com>
2024-12-04 19:58:27 +0200

    Temp debug

--- ci-operator/step-registry/mpiit/interop-aws-reporter/mpiit-interop-aws-reporter-commands.sh
+++ ci-operator/step-registry/mpiit/interop-aws-reporter/mpiit-interop-aws-reporter-commands.sh
@@ -4,8 +4,10 @@ set -o errexit
 set -o pipefail
 set -o verbose
 
-sleep 2h
-RUN_COMMAND="poetry run swach --help"
+export $SLACK_WEBHOOK_URL
+trap "sleep 30m" EXIT TERM INT SIGINT ERR
+
+RUN_COMMAND="poetry run python interop_aws_reporter/app.py"
 
 echo "$RUN_COMMAND" | sed -r "s/token [=A-Za-z0-9\.\-]+/token hashed-token /g"
 
