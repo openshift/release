96db6430c839fffb9d866d17660539e12576e5ed
Mike Gahagan <mgahagan@redhat.com>
2024-12-06 16:24:32 -0500

    cleanup, add variables

--- ci-operator/step-registry/cucushift/installer/rehearse/azure/aro/existing/cucushift-installer-rehearse-azure-aro-existing-commands.sh
+++ ci-operator/step-registry/cucushift/installer/rehearse/azure/aro/existing/cucushift-installer-rehearse-azure-aro-existing-commands.sh
@@ -7,19 +7,14 @@ set -o errexit
 set -o pipefail
 set -x
 
-#ls -al /secrets
-#cp -rv /secrets/* ${SHARED_DIR}
-
-#export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
-#export AWS_REGION="us-east-1"
-#export AWS_PAGER=""
-
-ls -al ${CLUSTER_PROFILE_DIR}
-
 AZURE_AUTH_LOCATION="${CLUSTER_PROFILE_DIR}/osServicePrincipal.json"
 AZURE_AUTH_CLIENT_ID="$(<"${AZURE_AUTH_LOCATION}" jq -r .clientId)"
 AZURE_AUTH_CLIENT_SECRET="$(<"${AZURE_AUTH_LOCATION}" jq -r .clientSecret)"
 AZURE_AUTH_TENANT_ID="$(<"${AZURE_AUTH_LOCATION}" jq -r .tenantId)"
+AZURE_SA_NAME=${AZURE_SA_NAME:=""}
+AZURE_SA_CONTAINER=${AZURE_SA_CONTAINER:=""}
+ARO_CLUSTER_FILES=${ARO_CLUSTER_FILES:=""}
+
 echo "Logging into Azure Cloud"
 # log in with az
 if [[ "${CLUSTER_TYPE}" == "azuremag" ]]; then
@@ -29,22 +24,6 @@ else
 fi
 az login --service-principal -u "${AZURE_AUTH_CLIENT_ID}" -p "${AZURE_AUTH_CLIENT_SECRET}" --tenant "${AZURE_AUTH_TENANT_ID}" --output none
 
-
-
-az storage blob download --account-name aroclusterinfo --container-name aro-cluster --name console.url --file ${SHARED_DIR}/console.url --auth-mode login
-az storage blob download --account-name aroclusterinfo --container-name aro-cluster --name api.url --file ${SHARED_DIR}/api.url --auth-mode login
-az storage blob download --account-name aroclusterinfo --container-name aro-cluster --name kubeconfig --file ${SHARED_DIR}/kubeconfig --auth-mode login
-
-## prepare kubeconfig
-#aws s3 cp s3://${BUCKET_NAME}/kubeconfig  ${SHARED_DIR}/kubeconfig
-#cp ${SHARED_DIR}/kubeconfig ${SHARED_DIR}/nested_kubeconfig
-
-## run_time env users
-#aws s3 cp s3://${BUCKET_NAME}/runtime_env "${SHARED_DIR}/runtime_env"
-
-#aws s3 cp s3://${BUCKET_NAME}/console.url "${SHARED_DIR}/console.url"
-#aws s3 cp s3://${BUCKET_NAME}/api.login "${SHARED_DIR}/api.login"
-#aws s3 cp s3://${BUCKET_NAME}/api.url "${SHARED_DIR}/api.url"
-
-#aws s3 cp s3://${BUCKET_NAME}/cluster-name "${SHARED_DIR}/cluster-name"
-#aws s3 cp s3://${BUCKET_NAME}/cluster-type "${SHARED_DIR}/cluster-type"
+for f in $ARO_CLUSTER_FILES; do
+  az storage blob download --account-name ${AZURE_SA_NAME} --container-name ${AZURE_SA_CONTAINER} --name ${f} --file ${SHARED_DIR}/${f} --auth-mode login
+done
