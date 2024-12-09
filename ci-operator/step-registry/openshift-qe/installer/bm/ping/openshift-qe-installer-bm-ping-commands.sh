cd4008e9f1114519c2457bfa17a050b7c825814d
Raul Sevilla <rsevilla@redhat.com>
2024-12-09 17:21:10 +0100

    Update dns test
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/ping/openshift-qe-installer-bm-ping-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/ping/openshift-qe-installer-bm-ping-commands.sh
@@ -7,20 +7,5 @@ set -x
 
 #ping -c 5 $bastion
 
-echo "Pinging api.vlan101.rdu3.labs.perfscale.redhat.com"
 dig +short api.vlan101.rdu3.labs.perfscale.redhat.com  || echo "Fail" 
-
-echo "Pinging wiki.rdu3.labs.perfscale.redhat.com"
-dig +short wiki.rdu3.labs.perfscale.redhat.com  || echo "Fail"
-
-echo "Pinging kk.apps.vlan101.rdu3.labs.perfscale.redhat.com"
-dig +short kk.apps.vlan101.rdu3.labs.perfscale.redhat.com  || echo "Fail"
-
-echo "Pinging wiki.rdu2.scalelab.redhat.com"
-dig +short wiki.rdu2.scalelab.redhat.com   || echo "Fail"
-
-echo "Pinging api.vlan603.rdu2.scalelab.redhat.com"
-dig +short api.vlan603.rdu2.scalelab.redhat.com   || echo "Fail"
-
-echo "Pinging kk.apps.vlan603.rdu2.scalelab.redhat.com"
-dig +short kk.apps.vlan603.rdu2.scalelab.redhat.com   || echo "Fail"
+curl -v https://api.vlan101.rdu3.labs.perfscale.redhat.com:6443/healthz -k
