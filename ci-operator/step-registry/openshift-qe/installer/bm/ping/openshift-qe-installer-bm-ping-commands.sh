e7f70747dc5112eca3336759a7fde34254881162
Raul Sevilla <rsevilla@redhat.com>
2024-12-09 13:13:23 +0100

    Test lab dns
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/ping/openshift-qe-installer-bm-ping-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/ping/openshift-qe-installer-bm-ping-commands.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-set -o errexit
 set -o nounset
 set -o pipefail
 set -x
 
-bastion=$(cat "/secret/address")
+#bastion=$(cat "/secret/address")
 
-ping -c 5 $bastion
+#ping -c 5 $bastion
+
+echo "Pinging api.vlan101.rdu3.labs.perfscale.redhat.com"
+dig +short api.vlan101.rdu3.labs.perfscale.redhat.com  || echo "Fail" 
+
+echo "Pinging wiki.rdu3.labs.perfscale.redhat.com"
+dig +short wiki.rdu3.labs.perfscale.redhat.com  || echo "Fail"
+
+echo "Pinging kk.apps.vlan101.rdu3.labs.perfscale.redhat.com"
+dig +short kk.apps.vlan101.rdu3.labs.perfscale.redhat.com  || echo "Fail"
+
+echo "Pinging wiki.rdu2.scalelab.redhat.com"
+dig +short wiki.rdu2.scalelab.redhat.com   || echo "Fail"
+
+echo "Pinging api.vlan603.rdu2.scalelab.redhat.com"
+dig +short api.vlan603.rdu2.scalelab.redhat.com   || echo "Fail"
+
+echo "Pinging kk.apps.vlan603.rdu2.scalelab.redhat.com"
+dig +short kk.apps.vlan603.rdu2.scalelab.redhat.com   || echo "Fail"
