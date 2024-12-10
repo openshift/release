222d227d452dfe5626f13a049304ce34769d2dbd
Cloud User <mihuang@redht.com>
2024-12-10 00:59:07 -0500

    create cluster for testing

--- ci-operator/step-registry/aws/provision/vpc/shared/aws-provision-vpc-shared-commands.sh
+++ ci-operator/step-registry/aws/provision/vpc/shared/aws-provision-vpc-shared-commands.sh
@@ -478,6 +478,7 @@ if [[ ${ENABLE_SHARED_VPC} == "yes" ]]; then
   aws_add_param_to_json "ResourceSharePrincipals" ${CLUSTER_CREATOR_AWS_ACCOUNT_NO} "$vpc_params"
 fi
 
+VPC_CIDR="10.1.0.0/16"
 if [[ -n "${VPC_CIDR}" ]]; then
      aws_add_param_to_json "VpcCidr" ${VPC_CIDR} "$vpc_params"
 fi
