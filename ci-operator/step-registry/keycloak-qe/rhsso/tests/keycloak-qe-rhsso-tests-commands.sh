5cb6d3e7eb53422a0ca64d7c38a734405ee6beaa
vshakya <vshakya@vshakya-thinkpadt14gen4.remote.csb>
2024-12-13 20:38:49 -0500

    add sleep for debug

--- ci-operator/step-registry/keycloak-qe/rhsso/tests/keycloak-qe-rhsso-tests-commands.sh
+++ ci-operator/step-registry/keycloak-qe/rhsso/tests/keycloak-qe-rhsso-tests-commands.sh
@@ -23,9 +23,11 @@ oc login ${OCP_API_URL} --username=${OCP_CRED_USR} --password=${OCP_CRED_PSW} --
 # Run tests
 echo "Executing rhsso tests ref"
 
+sleep 3600
+
 ansible-playbook -v /tmp/tests/ansible-tests/test-ocp-ci-rhbk.yml --extra-vars "ocp_project_name='${OCP_PROJECT_NAME}'"
 
-sleep 3600
+
 
 #copy junit results to artifacts dir
 mkdir -p $ARTIFACT_DIR/rhsso-tests
