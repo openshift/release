76f9713285bb022d1ff6e1318400ff92eb93d202
vshakya <vshakya@vshakya-thinkpadt14gen4.remote.csb>
2024-12-13 20:28:45 -0500

    add commands to login into test cluster

--- ci-operator/step-registry/keycloak-qe/rhsso/tests/keycloak-qe-rhsso-tests-commands.sh
+++ ci-operator/step-registry/keycloak-qe/rhsso/tests/keycloak-qe-rhsso-tests-commands.sh
@@ -12,6 +12,12 @@ cp $KUBECONFIG /tmp/kubeconfig
 export KUBECONFIG=/tmp/kubeconfig
 export PATH=/tmp/tests/ansible-tests/:$PATH
 
+CONSOLE_URL=$(cat $SHARED_DIR/console.url)
+OCP_API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
+OCP_CRED_USR="kubeadmin"
+OCP_CRED_PSW="$(cat ${SHARED_DIR}/kubeadmin-password)"
+oc login ${OCP_API_URL} --username=${OCP_CRED_USR} --password=${OCP_CRED_PSW} --insecure-skip-tls-verify=true
+
 
 
 # Run tests
