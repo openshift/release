76fe028ca828cfc84cd10b2ad6e669604a9ce632
Shang Gao <sgao@redhat.com>
2024-12-16 11:54:49 +0800

    Add nutanix flow networks QE jobs

--- ci-operator/step-registry/ipi/install/nutanix/csi/ipi-install-nutanix-csi-commands.sh
+++ ci-operator/step-registry/ipi/install/nutanix/csi/ipi-install-nutanix-csi-commands.sh
@@ -4,6 +4,17 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# For disconnected or otherwise unreachable environments, we want to
+# have steps use an HTTP(S) proxy to reach the API server. This proxy
+# configuration file should export HTTP_PROXY, HTTPS_PROXY, and NO_PROXY
+# environment variables, as well as their lowercase equivalents (note
+# that libcurl doesn't recognize the uppercase variables).
+if test -f "${SHARED_DIR}/proxy-conf.sh"
+then
+  # shellcheck disable=SC1091
+  source "${SHARED_DIR}/proxy-conf.sh"
+fi
+
 source ${SHARED_DIR}/nutanix_context.sh
 
 echo "$(date -u --rfc-3339=seconds) - Creating CSI manifests..."
