63c5af47b7b98fa9fba00ab348eb992edee8e7b6
Jose Castillo Lema <josecastillolema@gmail.com>
2024-12-16 14:48:49 +0100

    [PERFSCALE-3336] Health check in telco LTA
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/cluster-health/openshift-qe-cluster-health-commands.sh
+++ ci-operator/step-registry/openshift-qe/cluster-health/openshift-qe-cluster-health-commands.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -eu
 
-oc adm wait-for-stable-cluster --minimum-stable-period=${MINIMUM_STABLE_PERIOD} --timeout=${TIMEOUT}
+SSH_ARGS="-i /secret/jh_priv_ssh_key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
+bastion=$(cat "/secret/address")
+
+ssh ${SSH_ARGS} root@${bastion} "
+  KUBECONFIG=/root/$LAB/$LAB_CLOUD/$TYPE/kubeconfig oc adm wait-for-stable-cluster --minimum-stable-period=${MINIMUM_STABLE_PERIOD} --timeout=${TIMEOUT}"
