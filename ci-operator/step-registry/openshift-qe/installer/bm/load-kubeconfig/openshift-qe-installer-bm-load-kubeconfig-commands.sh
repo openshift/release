73bf52f4b0164f1e7c53381f05c29aa7fb8c1b25
Jose Castillo Lema <josecastillolema@gmail.com>
2024-12-16 12:19:50 +0100

    Fix UID issue
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/load-kubeconfig/openshift-qe-installer-bm-load-kubeconfig-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/load-kubeconfig/openshift-qe-installer-bm-load-kubeconfig-commands.sh
@@ -4,6 +4,9 @@ set -o nounset
 set -o pipefail
 set -x
 
+# Fix UID issue (from Telco QE Team)
+~/fix_uid.sh
+
 SSH_ARGS="-i /secret/jh_priv_ssh_key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
 bastion=$(cat "/secret/address")
 
