ca529540a00bcea6eeae86b4dad0bc8fa06330a7
Liangquan Li <liangli@redhat.com>
2024-12-26 14:14:47 +0800

    mce-install: Adjust 'OVERRIDE_HO_IMAGE' logic and display HO info (#60167)

--- ci-operator/step-registry/hypershift/mce/install/hypershift-mce-install-commands.sh
+++ ci-operator/step-registry/hypershift/mce/install/hypershift-mce-install-commands.sh
@@ -233,10 +233,11 @@ metadata:
 data:
   hypershift-operator: ${OVERRIDE_HO_IMAGE}
 EOF
-  while ! [ "$(oc get deployment operator -n hypershift -o jsonpath='{.status.conditions[?(@.type=="Progressing")].reason}')" == NewReplicaSetAvailable ]; do
+  while ! [[ "$(oc get deployment operator -n hypershift -o jsonpath='{.spec.template.spec.containers[*].image}')" == "$OVERRIDE_HO_IMAGE" ]]; do
       echo "wait override hypershift operator IMAGE..."
       sleep 10
   done
+  oc wait deployment -n hypershift operator --for=condition=Available --timeout=5m
 fi
 
 # display HyperShift cli version
@@ -249,3 +250,7 @@ if [ "$arch" == "x86_64" ]; then
   cd -
 fi
 if (( $(awk 'BEGIN {print ("'"$MCE_VERSION"'" > 2.3)}') )); then /tmp/${HYPERSHIFT_NAME} version; else /tmp/${HYPERSHIFT_NAME} --version; fi
+
+# display HyperShift Operator Version
+oc get deployment -n hypershift operator -ojsonpath='{.spec.template.spec.containers[*].image}' | tee "$ARTIFACT_DIR/HyperShiftOperator-info.txt" >/dev/null; echo >> "$ARTIFACT_DIR/HyperShiftOperator-info.txt"
+oc logs -n hypershift -lapp=operator --tail=-1 -c operator | head -1 | jq >> "$ARTIFACT_DIR/HyperShiftOperator-info.txt"
