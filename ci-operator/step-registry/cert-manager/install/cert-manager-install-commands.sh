6898570ecdc4116b6f20e1e533ca84581d0b300c
Yuedong Wu <dwcn22@outlook.com>
2025-01-06 19:38:11 +0800

    ci

--- ci-operator/step-registry/cert-manager/install/cert-manager-install-commands.sh
+++ ci-operator/step-registry/cert-manager/install/cert-manager-install-commands.sh
@@ -4,18 +4,93 @@ set -e
 set -u
 set -o pipefail
 
-if [ -f "${SHARED_DIR}/proxy-conf.sh" ] ; then
-    source "${SHARED_DIR}/proxy-conf.sh"
-    echo "proxy: ${SHARED_DIR}/proxy-conf.sh"
-fi
+function timestamp() {
+    date -u --rfc-3339=seconds
+}
 
-CATSRC=qe-app-registry
-if [[ ! "$(oc get catalogsource qe-app-registry -n openshift-marketplace -o yaml)" =~ "lastObservedState: READY" ]]; then
-    echo "The catalogsource qe-app-registry is either not existing or not ready. Will use redhat-operators to install cert-manager Operator."
-    CATSRC=redhat-operators
-fi
+function run_command() {
+    local cmd="$1"
+    echo "Running Command: ${cmd}"
+    eval "${cmd}"
+}
 
-oc create -f - << EOF
+function set_proxy () {
+    if test -s "${SHARED_DIR}/proxy-conf.sh" ; then
+        echo "Setting proxy configuration..."
+        source "${SHARED_DIR}/proxy-conf.sh"
+    else
+        echo "No proxy settings found. Skipping proxy configuration..."
+    fi
+}
+
+function auth_stage_registry () {
+    echo "Retrieving the 'registry.stage.redhat.io' auth config from shared credentials..." 
+    local stage_registry_path="/var/run/vault/mirror-registry/registry_stage.json"
+    local stage_auth_user=$(jq -r '.user' $stage_registry_path)
+    local stage_auth_password=$(jq -r '.password' $stage_registry_path)
+    local stage_auth_config=$(echo -n " " "$stage_auth_user":"$stage_auth_password" | base64 -w 0)
+
+    echo "Updating the image pull secret with the auth config..."
+    oc extract secret/pull-secret -n openshift-config --confirm --to /tmp
+    local new_dockerconfig="/tmp/.new-dockerconfigjson"
+    jq --argjson a "{\"registry.stage.redhat.io\": {\"auth\": \"$stage_auth_config\"}}" '.auths |= . + $a' "/tmp/.dockerconfigjson" >"$new_dockerconfig"
+    oc set data secret pull-secret -n openshift-config --from-file=.dockerconfigjson=$new_dockerconfig
+}
+
+function wait_for_state() {
+    local object="$1"
+    local state="$2"
+    local timeout="$3"
+    local namespace="${4:-}"
+    local selector="${5:-}"
+
+    echo "Waiting for '${object}' in namespace '${namespace}' with selector '${selector}' to exist..."
+    for _ in {1..30}; do
+        oc get ${object} --selector="${selector}" -n=${namespace} |& grep -ivE "(no resources found|not found)" && break || sleep 5
+    done
+
+    echo "Waiting for '${object}' in namespace '${namespace}' with selector '${selector}' to become '${state}'..."
+    oc wait --for=${state} --timeout=${timeout} ${object} --selector="${selector}" -n="${namespace}"
+    return $?
+}
+
+function create_catalogsource () {
+    echo "Creating a custom catalogsource using image: '$INDEX_IMG'..."
+    oc apply -f - << EOF
+apiVersion: operators.coreos.com/v1alpha1
+kind: CatalogSource
+metadata:
+  name: $CATSRC
+  namespace: openshift-marketplace
+spec:
+  sourceType: grpc
+  image: $INDEX_IMG
+EOF
+
+    if wait_for_state "catalogsource/${CATSRC}" "jsonpath={.status.connectionState.lastObservedState}=READY" "5m" "openshift-marketplace"; then
+        echo "CatalogSource is ready"
+    else
+        echo "Timed out after 5m. Dumping resources for debugging..."
+        run_command "oc get pod -n openshift-marketplace"
+        run_command "oc get event -n openshift-marketplace | grep ${CATSRC}"
+        exit 1
+    fi
+}
+
+function subscribe_operator () {
+    echo "Checking if the PackageManifest exists in the CatalogSource before installing the operator..."
+    output=$(oc get packagemanifest -n openshift-marketplace -l=catalog=$CATSRC --field-selector=metadata.name=openshift-cert-manager-operator 2>&1)
+    if [[ $? -ne 0 ]] || echo "$output" | grep -q "No resources found"; then
+        echo "No PackageManifest found. Skipping installation..."
+        exit 0
+    fi
+
+    if [[ "$TARGET_NAMESPACES" == "!all" ]]; then
+        TARGET_NAMESPACES=""
+    fi
+
+    echo "Creating the Namespace, OperatorGroup and Subscription for the operator installation..."
+    oc apply -f - <<EOF
 apiVersion: v1
 kind: Namespace
 metadata:
@@ -27,8 +102,7 @@ metadata:
   name: cert-manager-operator-og
   namespace: cert-manager-operator
 spec:
-  targetNamespaces:
-  - cert-manager-operator
+  targetNamespaces: [$TARGET_NAMESPACES]
 ---
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
@@ -36,66 +110,48 @@ metadata:
   name: openshift-cert-manager-operator
   namespace: cert-manager-operator
 spec:
-  channel: stable-v1
-  installPlanApproval: Automatic
+  channel: $CHANNEL
   name: openshift-cert-manager-operator
-  source: "$CATSRC"
+  source: $CATSRC
   sourceNamespace: openshift-marketplace
 EOF
 
-MAX_RETRY=20
-INTERVAL=10
-COUNTER=0
-while :;
-do
-    echo "Checking openshift-cert-manager-operator subscription status for the #${COUNTER}-th time ..."
-    if [ "$(oc get subscription openshift-cert-manager-operator -n cert-manager-operator -o=jsonpath='{.status.state}')" == AtLatestKnown ]; then
-        echo "The openshift-cert-manager-operator subscription status becomes ready" && break
-    fi
-    ((++COUNTER))
-    if [[ $COUNTER -eq $MAX_RETRY ]]; then
-        echo "The openshift-cert-manager-operator subscription status is not ready after $((MAX_RETRY * INTERVAL)) seconds. Dumping status:"
-        oc get subscription openshift-cert-manager-operator -n cert-manager-operator -o=jsonpath='{.status}'
+    if wait_for_state "deployment/cert-manager-operator-controller-manager" "condition=Available" "5m" "cert-manager-operator"; then
+        echo "Operator is ready"
+        run_command "oc get csv -n cert-manager-operator"
+    else
+        echo "Timed out after 5m. Dumping resources for debugging..."
+        run_command "oc get pod -n cert-manager-operator"
+        run_command "oc get event -n cert-manager-operator"
+        run_command "oc get csv -n cert-manager-operator"
+        run_command "oc get subscription openshift-cert-manager-operator -n cert-manager-operator -o=yaml}'"
         exit 1
     fi
-    sleep $INTERVAL
-done
 
-MAX_RETRY=20
-INTERVAL=10
-COUNTER=0
-while :;
-do
-    echo "Checking cert-manager-operator CSV status for the #${COUNTER}-th time ..."
-    if [[ "$(oc get --no-headers csv -n cert-manager-operator)" == *cert-manager-operator.*Succeeded ]]; then
-        echo "The cert-manager-operator CSV status becomes ready" && break
-    fi
-    ((++COUNTER))
-    if [[ $COUNTER -eq $MAX_RETRY ]]; then
-        echo "The cert-manager-operator CSV status is not ready after $((MAX_RETRY * INTERVAL)) seconds. Dumping status:"
-        CSV_NAME=$(oc get csv -n cert-manager-operator | grep -E -o '^cert-manager-operator[^ ]*')
-        oc get csv "$CSV_NAME" -n cert-manager-operator -o=jsonpath='{.status}'
+    if wait_for_state "deployment/cert-manager" "condition=Available" "5m" "cert-manager" && \
+        wait_for_state "deployment/cert-manager-webhook" "condition=Available" "5m" "cert-manager" && \
+        wait_for_state "deployment/cert-manager-cainjector" "condition=Available" "5m" "cert-manager"; then
+        echo "Operands are all ready"
+    else
+        echo "Timed out after 5m. Dumping resources for debugging..."
+        run_command "oc get pod -n cert-manager"
+        run_command "oc get event -n cert-manager"
         exit 1
     fi
-    sleep $INTERVAL
-done
+}
 
-MAX_RETRY=30
-INTERVAL=10
-COUNTER=0
-while :;
-do
-    echo "Checking cert-manager pods status for the #${COUNTER}-th time ..."
-    if [ "$(oc get pods -n cert-manager -o=jsonpath='{.items[*].status.phase}')" == "Running Running Running" ]; then
-        echo "[$(date -u --rfc-3339=seconds)] Finished cert-manager Operator installation. The cert-manager pods are all ready."
-        oc get po -n cert-manager
-        break
-    fi
-    ((++COUNTER))
-    if [[ $COUNTER -eq $MAX_RETRY ]]; then
-        echo "The cert-manager pods are not all ready after $((MAX_RETRY * INTERVAL)) seconds. Dumping status:"
-        oc get pods -n cert-manager
-        exit 1
-    fi
-    sleep $INTERVAL
-done
+timestamp
+set_proxy
+
+# If 'INDEX_IMG' is not empty, create the catalogsource using custom index image; otherwise use the default 'redhat-operators'.
+if [ -n "${INDEX_IMG}" ]; then
+    CATSRC=custom-catalog-cert-manager-operator
+    auth_stage_registry
+    create_catalogsource
+else
+    CATSRC=redhat-operators
+fi
+
+subscribe_operator
+
+echo "[$(timestamp)] Succeeded in installing the cert-manager Operator for Red Hat OpenShift!"
