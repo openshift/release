871b45ec307717b618df0e7220fd53ed81826228
Jose Castillo Lema <josecastillolema@gmail.com>
2025-01-06 22:35:22 +0100

    [PERFSCALE-3630] New metal-perfscale-telco config
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/cluster-health/openshift-qe-cluster-health-commands.sh
+++ ci-operator/step-registry/openshift-qe/cluster-health/openshift-qe-cluster-health-commands.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-set -eu
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
 
-oc adm wait-for-stable-cluster --minimum-stable-period=${MINIMUM_STABLE_PERIOD} --timeout=${TIMEOUT}
+# Fix UID issue (from Telco QE Team)
+~/fix_uid.sh
+
+SSH_ARGS="-i /secret/jh_priv_ssh_key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
+bastion=$(cat "/secret/address")
+
+ssh ${SSH_ARGS} root@${bastion} "
+  KUBECONFIG=/root/$LAB/$LAB_CLOUD/$TYPE/kubeconfig oc adm wait-for-stable-cluster --minimum-stable-period=${MINIMUM_STABLE_PERIOD} --timeout=${TIMEOUT}"
