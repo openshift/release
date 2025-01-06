8d84b1f4f58129dbfc03faaaf5076c965eeb2655
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-01-06 12:52:25 +0100

    export kubeconfig and create serviceaccount
    Set default namespace
    
    fix kubeconfig and service account

--- ci-operator/step-registry/redhat-developer/rhdh/helm-aks-nightly/redhat-developer-rhdh-helm-aks-nightly-commands.sh
+++ ci-operator/step-registry/redhat-developer/rhdh/helm-aks-nightly/redhat-developer-rhdh-helm-aks-nightly-commands.sh
@@ -16,9 +16,18 @@ TAG_NAME="next"
 git clone "https://github.com/${GITHUB_ORG_NAME}/${GITHUB_REPOSITORY_NAME}.git"
 cd rhdh || exit
 
-ls $SHARED_DIR
-KUBECONFIG=$SHARED_DIR/kubeconfig
-echo $KUBECONFIG
-oc whoami
+# use kubeconfig from mapt
+KUBECONFIG="${SHARED_DIR}/kubeconfig"
+export KUBECONFIG
+
+K8S_CLUSTER_URL=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
+echo "K8S_CLUSTER_URL: $K8S_CLUSTER_URL"
+kubectl config set-context --current --namespace=default
+kubectl create serviceaccount tester-sa-2
+kubectl create clusterrolebinding tester-sa-2-binding \
+  --clusterrole=cluster-admin \
+  --serviceaccount=default:tester-sa-2
+K8S_CLUSTER_TOKEN=$(kubectl create token tester-sa-2)
+export K8S_CLUSTER_URL K8S_CLUSTER_TOKEN
 
 bash ./.ibm/pipelines/openshift-ci-tests.sh
