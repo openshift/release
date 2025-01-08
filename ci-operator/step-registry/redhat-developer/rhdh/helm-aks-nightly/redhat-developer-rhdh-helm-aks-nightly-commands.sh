8fe59b354ccc28f754005cdb416b22585b126ad4
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-01-08 09:42:48 +0100

    Force ephemeral from different branch
    Test with different branch

--- ci-operator/step-registry/redhat-developer/rhdh/helm-aks-nightly/redhat-developer-rhdh-helm-aks-nightly-commands.sh
+++ ci-operator/step-registry/redhat-developer/rhdh/helm-aks-nightly/redhat-developer-rhdh-helm-aks-nightly-commands.sh
@@ -8,13 +8,14 @@ echo "OC_CLIENT_VERSION: $OC_CLIENT_VERSION"
 
 export GITHUB_ORG_NAME GITHUB_REPOSITORY_NAME NAME_SPACE TAG_NAME
 
-GITHUB_ORG_NAME="redhat-developer"
+GITHUB_ORG_NAME="zdrapela"
 GITHUB_REPOSITORY_NAME="rhdh"
 TAG_NAME="next"
 
 # Clone and checkout the specific PR
 git clone "https://github.com/${GITHUB_ORG_NAME}/${GITHUB_REPOSITORY_NAME}.git"
-cd rhdh || exit
+cd "${GITHUB_REPOSITORY_NAME}" || exit
+git switch -f gke-new-cluster-working-setup
 
 # use kubeconfig from mapt
 chmod 600 "${SHARED_DIR}/kubeconfig"
