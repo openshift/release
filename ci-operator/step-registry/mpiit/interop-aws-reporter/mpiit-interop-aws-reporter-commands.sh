4489ef7f31651f726d45a49c2335ddac40cbe652
oharan2 <oharan@redhat.com>
2025-01-14 15:54:45 +0200

    Update secrets and env vars

--- ci-operator/step-registry/mpiit/interop-aws-reporter/mpiit-interop-aws-reporter-commands.sh
+++ ci-operator/step-registry/mpiit/interop-aws-reporter/mpiit-interop-aws-reporter-commands.sh
@@ -4,8 +4,13 @@ set -o errexit
 set -o pipefail
 
 trap "sleep 30m" EXIT TERM INT SIGINT ERR
-SLACK_WEBHOOK_URL=$(cat /tmp/secrets/slack-webhook-url.json)
-export SLACK_WEBHOOK_URL
+export SLACK_WEBHOOK_URL=$(cat /tmp/secrets/slack-webhook-url.json)
+export CLEANUP_AWS__AUTH__ACCESS_KEY=$(cat /tmp/secrets/aws-access-key.json)
+export CLEANUP_AWS__AUTH__SECRET_KEY=$(cat /tmp/secrets/aws-secret-key.json)
+
+export CLEANUP_AWS__AUTH__REGIONS='["all"]'
+export CLEANUP_AWS__CRITERIA__OCPS__OCP_CLIENT_REGION=$AWS_CLIENT_REGION
+export CLEANUP_AWS__CRITERIA__OCPS__SLA=$CLEANUP_SLA
 
 RUN_COMMAND="poetry run python interop_aws_reporter/app.py"
 
