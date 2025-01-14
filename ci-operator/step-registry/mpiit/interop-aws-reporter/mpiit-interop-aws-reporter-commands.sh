d1c8b2f58cb9a2c17842961f18523af1cb5d5b05
oharan2 <oharan@redhat.com>
2025-01-14 16:06:03 +0200

    Fix shellchecks

--- ci-operator/step-registry/mpiit/interop-aws-reporter/mpiit-interop-aws-reporter-commands.sh
+++ ci-operator/step-registry/mpiit/interop-aws-reporter/mpiit-interop-aws-reporter-commands.sh
@@ -4,9 +4,13 @@ set -o errexit
 set -o pipefail
 
 trap "sleep 30m" EXIT TERM INT SIGINT ERR
-export SLACK_WEBHOOK_URL=$(cat /tmp/secrets/slack-webhook-url.json)
-export CLEANUP_AWS__AUTH__ACCESS_KEY=$(cat /tmp/secrets/aws-access-key.json)
-export CLEANUP_AWS__AUTH__SECRET_KEY=$(cat /tmp/secrets/aws-secret-key.json)
+SLACK_WEBHOOK_URL=$(cat /tmp/secrets/slack-webhook-url)
+CLEANUP_AWS__AUTH__ACCESS_KEY=$(cat /tmp/secrets/aws-access-key)
+CLEANUP_AWS__AUTH__SECRET_KEY=$(cat /tmp/secrets/aws-secret-key)
+
+export SLACK_WEBHOOK_URL
+export CLEANUP_AWS__AUTH__ACCESS_KEY
+export CLEANUP_AWS__AUTH__SECRET_KEY
 
 export CLEANUP_AWS__AUTH__REGIONS='["all"]'
 export CLEANUP_AWS__CRITERIA__OCPS__OCP_CLIENT_REGION=$AWS_CLIENT_REGION
