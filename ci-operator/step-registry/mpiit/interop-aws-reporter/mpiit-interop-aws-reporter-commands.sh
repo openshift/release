2e010a174e20209138d0d69b048afda23198f515
oharan2 <oharan@redhat.com>
2025-01-14 17:11:15 +0200

    Add channel-id and slack-bot-token

--- ci-operator/step-registry/mpiit/interop-aws-reporter/mpiit-interop-aws-reporter-commands.sh
+++ ci-operator/step-registry/mpiit/interop-aws-reporter/mpiit-interop-aws-reporter-commands.sh
@@ -6,10 +6,14 @@ set -o pipefail
 SLACK_WEBHOOK_URL=$(cat /tmp/secrets/slack-webhook-url)
 CLEANUP_AWS__AUTH__ACCESS_KEY=$(cat /tmp/secrets/aws-access-key)
 CLEANUP_AWS__AUTH__SECRET_KEY=$(cat /tmp/secrets/aws-secret-key)
+CHANNEL_ID=$(cat /tmp/secrets/channel-id)
+SLACK_BOT_TOKEN=$(cat /tmp/secrets/slack-bot-token)
 
 export SLACK_WEBHOOK_URL
 export CLEANUP_AWS__AUTH__ACCESS_KEY
 export CLEANUP_AWS__AUTH__SECRET_KEY
+export CHANNEL_ID
+export SLACK_BOT_TOKEN
 
 export CLEANUP_AWS__AUTH__REGIONS='["all"]'
 export CLEANUP_AWS__CRITERIA__OCPS__OCP_CLIENT_REGION=$AWS_CLIENT_REGION
