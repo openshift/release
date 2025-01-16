6760a7466076bf444f47b7cf6188753597ccd349
Sachin Ninganure <sninganu@sninganu.pnq.csb>
2025-01-16 18:35:05 +0530

    changed ditty lin

--- ci-operator/step-registry/openshift-qe/etcd-perf/openshift-qe-etcd-perf-commands.sh
+++ ci-operator/step-registry/openshift-qe/etcd-perf/openshift-qe-etcd-perf-commands.sh
@@ -2,7 +2,8 @@
 #set -o errexit
 set -o nounset
 #set -o pipefail
-oc get route -A |grep ditty
+#oc get route -A |grep ditty
+oc get route -A |grep ditty || echo "No dittybopper"
 NAME=${NAME:=""}
   #CASE 01 create 100 projects in the batches of 500
   #for i in {1..100}; do oc new-project project-$i;oc create configmap project-$i --from-file=/etc/pki/ca-trust/extracted/openssl/ca-bundle.trust.crt; done
