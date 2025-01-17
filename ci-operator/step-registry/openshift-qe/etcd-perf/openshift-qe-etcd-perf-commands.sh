87fff61b093a31456117110cd943a9989db0dc78
Sachin Ninganure <sninganu@sninganu.pnq.csb>
2025-01-17 19:58:44 +0530

    token in etcdanalyse5

--- ci-operator/step-registry/openshift-qe/etcd-perf/openshift-qe-etcd-perf-commands.sh
+++ ci-operator/step-registry/openshift-qe/etcd-perf/openshift-qe-etcd-perf-commands.sh
@@ -108,8 +108,8 @@ EOF
 
   for i in ` oc -n openshift-etcd get pods | grep etcd-ip |awk '{print $1}'`; do oc -n openshift-etcd exec $i -- etcdctl endpoint health; done
   export KUBECONFIG=/tmp/secret/kubeconfig
-  adminpassword=${adminpassword:=""}
-  if [[ -s /tmp/secret/kubeadmin-password ]];then export adminpassword=`cat /tmp/secret/kubeadmin-password`; fi
+  export adminpassword=${adminpassword:=""}
+  if [[ -s /tmp/secret/kubeadmin-password ]];then adminpassword=`cat /tmp/secret/kubeadmin-password`; fi
   APIURL=`grep 'server:' /tmp/secret/kubeconfig | awk -F 'server:' '{print $2}'`
   oc login $APIURL -u kubeadmin -p $adminpassword
   date;oc adm top node;date;ls -lrt /tmp/etcd-tools ;etcd-tools/etcd-analyzer.sh;date
