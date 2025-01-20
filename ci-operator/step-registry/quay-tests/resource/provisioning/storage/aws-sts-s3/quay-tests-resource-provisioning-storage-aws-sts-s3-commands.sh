262f5a0b7a8ff45dc6d5b55318056269ddaf39f5
Sean Zhao <szhao@redhat.com>
2025-01-20 11:06:21 +0800

    sts initinalize

--- ci-operator/step-registry/quay-tests/resource/provisioning/storage/aws-sts-s3/quay-tests-resource-provisioning-storage-aws-sts-s3-commands.sh
+++ ci-operator/step-registry/quay-tests/resource/provisioning/storage/aws-sts-s3/quay-tests-resource-provisioning-storage-aws-sts-s3-commands.sh
@@ -7,7 +7,7 @@ set -o pipefail
 #Create AWS S3 Storage Bucket
 QUAY_OPERATOR_CHANNEL="$QUAY_OPERATOR_CHANNEL"
 QUAY_OPERATOR_SOURCE="$QUAY_OPERATOR_SOURCE"
-QUAY_AWS_S3_BUCKET="quayprowsts$RANDOM"
+QUAY_AWS_STS_S3_BUCKET="quayprowsts$RANDOM"
 
 QUAY_AWS_ACCESS_KEY=$(cat /var/run/quay-qe-aws-secret/access_key)
 QUAY_AWS_SECRET_KEY=$(cat /var/run/quay-qe-aws-secret/secret_key)
@@ -21,41 +21,111 @@ variable "region" {
 variable "aws_bucket" {
   default = "quayawssts"
 }
+
+variable "aws_sts_role_name" {
+default = "quay_prow_role"
+}
+
+variable "aws_sts_user_name" {
+default = "quay_prow_automation"
+}
+
 EOF
 
-cat >>create_aws_bucket.tf <<EOF
+cat >>create_aws_sts.tf <<EOF
 provider "aws" {
   region = "us-east-2"
   access_key = "${QUAY_AWS_ACCESS_KEY}"
   secret_key = "${QUAY_AWS_SECRET_KEY}"
 }
 
-resource "aws_s3_bucket" " quayawssts" {
+#s3 bucket
+resource "aws_s3_bucket" "quayawssts" {
   bucket = var.aws_bucket
   force_destroy = true
 }
 
-resource "aws_s3_bucket_ownership_controls" " quayawssts" {
-  bucket = aws_s3_bucket. quayawssts.id
+resource "aws_s3_bucket_ownership_controls" "quayawssts" {
+  bucket = aws_s3_bucket.quayawssts.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
 resource "aws_s3_bucket_acl" "quayaws_bucket_acl" {
-  depends_on = [aws_s3_bucket_ownership_controls. quayawssts]
+  depends_on = [aws_s3_bucket_ownership_controls.quayawssts]
 
-  bucket = aws_s3_bucket. quayawssts.id
+  bucket = aws_s3_bucket.quayawssts.id
   acl    = "private"
 }
+
+#sts role
+resource "aws_iam_user" "quay" {
+  name = var.aws_sts_user_name
+  path = "/"
+}
+
+resource "aws_iam_access_key" "quay" {
+  user    = aws_iam_user.quay.name
+  depends_on = [aws_iam_user.quay]
+}
+
+resource "aws_iam_role" "quay_ci_role" {
+
+  name = var.aws_sts_role_name
+  assume_role_policy = <<EOF2
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::301721915996:user/${aws_iam_user.quay.name}"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF2
+}
+
+resource "aws_iam_role_policy_attachment" "policy_attachment" {
+
+  role       = aws_iam_role.quay_ci_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+
+}
+
+output "role" {
+  value = aws_iam_role.quay_ci_role.arn
+}
+output "accesskey" {
+  sensitive = true
+  value = aws_iam_access_key.quay.id
+}
+output "secretkey" {
+  sensitive = true
+  value = aws_iam_access_key.quay.secret
+}
+
 EOF
 
-echo "quay aws s3 bucket name is ${QUAY_AWS_S3_BUCKET}"
-export TF_VAR_aws_bucket="${QUAY_AWS_S3_BUCKET}"
+echo "quay aws s3 bucket name is ${QUAY_AWS_STS_S3_BUCKET}"
+export TF_VAR_aws_bucket="${QUAY_AWS_STS_S3_BUCKET}"
+export TF_VAR_aws_sts_role_name="quay_prow_role${RANDOM}"
+export TF_VAR_aws_sts_user_name="quay_prow_automation${RANDOM}"
+
 terraform init
 terraform apply -auto-approve || true
+terraform output role > sts_role_arn
+terraform output accesskey > sts_accesskey
+terraform output secretkey > sts_secretkey
 
 #Share Terraform Var and Terraform Directory
-echo "${QUAY_AWS_S3_BUCKET}" > ${SHARED_DIR}/QUAY_AWS_S3_BUCKET
+echo "${QUAY_AWS_S3_BUCKET}" > "${SHARED_DIR}/QUAY_AWS_STS_S3_BUCKET"
+terraform output role  > "${SHARED_DIR}/QUAY_AWS_STS_STS_ROLE"
+terraform output accesskey  > "${SHARED_DIR}/QUAY_AWS_STS_STS_ACCESSKEY"
+
+
 tar -cvzf terraform.tgz --exclude=".terraform" *
 cp terraform.tgz ${SHARED_DIR}
