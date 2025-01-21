9a19758dcc1c00a6555e51d39018f01b84f843ab
maxu <maxu@redhat.com>
2025-01-21 10:32:26 +0800

    fix

--- ci-operator/step-registry/ipi/conf/ibmcloud/provisionednetwork/ipi-conf-ibmcloud-provisionednetwork-commands.sh
+++ ci-operator/step-registry/ipi/conf/ibmcloud/provisionednetwork/ipi-conf-ibmcloud-provisionednetwork-commands.sh
@@ -14,11 +14,11 @@ declare -a zones=("${region}-1" "${region}-2" "${region}-3")
 if [[ ${ZONES_COUNT} -eq 1 ]]; then
     zone=${zones[0]}
     echo "Adjusted zones to ${zone} based on ZONES_COUNT: ${ZONES_COUNT}."
-    temp_file=$(mktemp)
-    cp ${vpcSubnetsFile} ${temp_file}
-    yq-go r "$temp_file" "platform.ibmcloud.controlPlaneSubnets" -j | jq --arg zone "$zone" '.[] | select(test($zone))' | yq-go w -i "$temp_file" 'platform.ibmcloud.controlPlaneSubnets' "$(cat)"
-    yq-go r "$temp_file" "platform.ibmcloud.computeSubnets" -j | jq --arg zone "$zone" '.[] | select(test($zone))' | yq-go w -i "$temp_file" 'platform.ibmcloud.computeSubnets' "$(cat)"
-    cat $temp_file
+    # temp_file=$(mktemp)
+    # cp ${vpcSubnetsFile} ${temp_file}
+    yq-go r "$vpcSubnetsFile" "platform.ibmcloud.controlPlaneSubnets" -j | jq --arg zone "$zone" '.[] | select(test($zone))' | yq-go w -i "$vpcSubnetsFile" 'platform.ibmcloud.controlPlaneSubnets' "$(cat)"
+    yq-go r "$vpcSubnetsFile" "platform.ibmcloud.computeSubnets" -j | jq --arg zone "$zone" '.[] | select(test($zone))' | yq-go w -i "$vpcSubnetsFile" 'platform.ibmcloud.computeSubnets' "$(cat)"
+    cat $vpcSubnetsFile
     yq-go m -x -i "${CONFIG}" "${vpcSubnetsFile}"
 else
     yq-go m -x -i "${CONFIG}" "${vpcSubnetsFile}"
