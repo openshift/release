0648ae716a02f58ca7d8e2ded463d3ba2029e66c
Rio Liu <rioliu@redhat.com>
2025-01-22 16:25:52 +0800

    add debug flags

--- ci-operator/step-registry/release-qe-tests/release-qe-tests-commands.sh
+++ ci-operator/step-registry/release-qe-tests/release-qe-tests-commands.sh
@@ -16,6 +16,8 @@ export RELEASE_PAYLOAD_MODIFIER_TOKEN=$release_payload_modifier_token
 export GCS_CRED_FILE=/var/run/vault/release-tests-sa/gcs_sa_for_qe_artifact_access.json
 
 echo "Login cluster app.ci"
+which oc
+ls -l `which oc`
 oc login api.ci.l2s4.p1.openshiftapps.com:6443 --token=$RELEASE_PAYLOAD_MODIFIER_TOKEN
 
 echo -e "\n********* Start job controller *********\n"
