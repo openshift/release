c40a385a1a216b55835c78c934003d90e8c5c814
Maxim Patlasov <mpt@redhat.com>
2025-01-21 16:16:55 -0800

    more debug (2)

--- ci-operator/step-registry/openshift/e2e/test/debug2/openshift-e2e-test-debug2-commands.sh
+++ ci-operator/step-registry/openshift/e2e/test/debug2/openshift-e2e-test-debug2-commands.sh
@@ -4,19 +4,18 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-echo "DEBUG....."
+echo "DEBUG2....."
 echo $SHARED_DIR
 ls -al $SHARED_DIR
-echo "DEBUG --1--"
-date > $SHARED_DIR/DATE
+echo "DEBUG2 --1--"
 cat $SHARED_DIR/DATE
-echo "DEBUG --2--"
+echo "DEBUG2 --2--"
 uname -a
 cat /etc/hostname
-echo "DEBUG --3--"
+echo "DEBUG2 --3--"
 ls -l /dev
-echo "DEBUG --4--"
+echo "DEBUG2 --4--"
 ls -l /dev/disk || true
-echo "DEBUG --5--"
+echo "DEBUG2 --5--"
 ls -l /dev/disk/by-id || true
-echo "DEBUG --6--"
+echo "DEBUG2 --6--"
