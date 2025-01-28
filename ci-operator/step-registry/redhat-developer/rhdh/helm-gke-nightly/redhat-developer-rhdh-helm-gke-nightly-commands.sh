1c20535d33dc05d6288f3d18b3e9e39766b3e8c4
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-01-28 13:29:39 +0100

    Remove service account for GKE

--- ci-operator/step-registry/redhat-developer/rhdh/helm-gke-nightly/redhat-developer-rhdh-helm-gke-nightly-commands.sh
+++ ci-operator/step-registry/redhat-developer/rhdh/helm-gke-nightly/redhat-developer-rhdh-helm-gke-nightly-commands.sh
@@ -17,23 +17,7 @@ git clone "https://github.com/${GITHUB_ORG_NAME}/${GITHUB_REPOSITORY_NAME}.git"
 cd "${GITHUB_REPOSITORY_NAME}" || exit
 git switch -f gke-new-cluster-working-setup
 
-# Create a service account and assign cluster url and token
-SA_NAME="tester-sa-2"
-SA_NAMESPACE="default"
-SA_BINDING_NAME="${SA_NAME}-binding"
-if ! kubectl get serviceaccount ${SA_NAME} -n ${SA_NAMESPACE} &> /dev/null; then
-  echo "Creating service account ${SA_NAME}..."
-  kubectl create serviceaccount ${SA_NAME} -n ${SA_NAMESPACE}
-  echo "Creating cluster role binding..."
-  kubectl create clusterrolebinding ${SA_BINDING_NAME} \
-      --clusterrole=cluster-admin \
-      --serviceaccount=${SA_NAMESPACE}:${SA_NAME}
-  echo "Service account and binding created successfully"
-else
-  echo "Service account ${SA_NAME} already exists in namespace ${SA_NAMESPACE}"
-fi
-K8S_CLUSTER_TOKEN=$(kubectl create token tester-sa-2 -n default)
 K8S_CLUSTER_URL=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
-export K8S_CLUSTER_TOKEN K8S_CLUSTER_URL
+export K8S_CLUSTER_URL
 
 bash ./.ibm/pipelines/openshift-ci-tests.sh
