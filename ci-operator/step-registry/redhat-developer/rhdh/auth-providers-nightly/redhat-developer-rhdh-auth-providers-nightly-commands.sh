92b264e9eb4b34782a12584d9e3b00b80a13a69c
albarbaro <abarbaro@redhat.com>
2025-01-30 15:14:26 +0100

    add auth provders nightly

---
+++ ci-operator/step-registry/redhat-developer/rhdh/auth-providers-nightly/redhat-developer-rhdh-auth-providers-nightly-commands.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+export HOME WORKSPACE
+HOME=/tmp
+WORKSPACE=$(pwd)
+cd /tmp || exit
+
+echo "OC_CLIENT_VERSION: $OC_CLIENT_VERSION"
+
+export GITHUB_ORG_NAME GITHUB_REPOSITORY_NAME NAME_SPACE TAG_NAME
+
+GITHUB_ORG_NAME="redhat-developer"
+GITHUB_REPOSITORY_NAME="rhdh"
+TAG_NAME="next"
+
+# Clone and checkout the specific PR
+#git clone "https://github.com/${GITHUB_ORG_NAME}/${GITHUB_REPOSITORY_NAME}.git"
+git clone "https://github.com/albarbaro/rhdh.git"
+cd rhdh || exit
+git switch auth-providers-ci
+
+bash ./.ibm/pipelines/openshift-ci-tests.sh
