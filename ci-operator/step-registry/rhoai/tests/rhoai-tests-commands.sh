545276b6dcb48b79b8a80fbfcc42b590b323c54c
Lisa Wang <liswang@redhat.com>
2025-01-31 10:30:19 -0700

    RHOAI 2.16 tests (#60437)

--- ci-operator/step-registry/rhoai/tests/rhoai-tests-commands.sh
+++ ci-operator/step-registry/rhoai/tests/rhoai-tests-commands.sh
@@ -21,10 +21,30 @@ oc login -u kubeadmin -p "$(cat $SHARED_DIR/kubeadmin-password)" "${API_URL}" --
 OC_HOST=$(oc whoami --show-server)
 OCP_CONSOLE=$(oc whoami --show-console)
 RHODS_DASHBOARD="https://$(oc get route rhods-dashboard -n redhat-ods-applications -o jsonpath='{.spec.host}{"\n"}')"
-
 export OC_HOST
 export OCP_CONSOLE
 export RHODS_DASHBOARD
+
+BUCKET_INFO="/tmp/secrets/ci"
+ENDPOINT_1="$(cat ${BUCKET_INFO}/ENDPOINT_1)"
+ENDPOINT_2="$(cat ${BUCKET_INFO}/ENDPOINT_2)"
+REGION_1="$(cat ${BUCKET_INFO}/REGION_1)"
+REGION_2="$(cat ${BUCKET_INFO}/REGION_2)"
+NAME_1="$(cat ${BUCKET_INFO}/NAME_1)"
+NAME_2="$(cat ${BUCKET_INFO}/NAME_2)"
+NAME_3="$(cat ${BUCKET_INFO}/NAME_3)"
+NAME_4="$(cat ${BUCKET_INFO}/NAME_4)"
+NAME_5="$(cat ${BUCKET_INFO}/NAME_5)"
+
+export ENDPOINT_1
+export ENDPOINT_2
+export REGION_1
+export REGION_2
+export NAME_1
+export NAME_2
+export NAME_3
+export NAME_4
+export NAME_5
 
 # running RHOAI tests
 ./run_interop.sh
