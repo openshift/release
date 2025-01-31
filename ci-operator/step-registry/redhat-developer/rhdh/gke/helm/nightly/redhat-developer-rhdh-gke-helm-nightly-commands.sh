dce92bccf1e92ca3b83fe586df1c65ae3ab64670
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-01-31 11:46:49 +0100

    Adapt script for PR images
    Co-Authored-By: Subhash Khileri <10365050+subhashkhileri@users.noreply.github.com>

--- ci-operator/step-registry/redhat-developer/rhdh/gke/helm/nightly/redhat-developer-rhdh-gke-helm-nightly-commands.sh
+++ ci-operator/step-registry/redhat-developer/rhdh/gke/helm/nightly/redhat-developer-rhdh-gke-helm-nightly-commands.sh
@@ -13,6 +13,10 @@ echo "GIT_PR_NUMBER : $GIT_PR_NUMBER"
 GITHUB_ORG_NAME="redhat-developer"
 GITHUB_REPOSITORY_NAME="rhdh"
 
+export QUAY_REPO="rhdh-community/rhdh"
+export RELEASE_BRANCH_NAME="main"
+RELEASE_TAG_NAME="next"
+
 # Clone and checkout the specific PR
 git clone "https://github.com/${GITHUB_ORG_NAME}/${GITHUB_REPOSITORY_NAME}.git"
 cd "${GITHUB_REPOSITORY_NAME}" || exit
@@ -24,7 +28,7 @@ if [ "$JOB_TYPE" == "presubmit" ] && [[ "$JOB_NAME" != rehearse-* ]]; then
     # If executed as PR check of the repository, switch to PR branch.
     git fetch origin pull/"${GIT_PR_NUMBER}"/head:PR"${GIT_PR_NUMBER}"
     git checkout PR"${GIT_PR_NUMBER}"
-    git merge origin/main --no-edit
+    git merge origin/$RELEASE_BRANCH_NAME --no-edit
     GIT_PR_RESPONSE=$(curl -s "https://api.github.com/repos/${GITHUB_ORG_NAME}/${GITHUB_REPOSITORY_NAME}/pulls/${GIT_PR_NUMBER}")
     LONG_SHA=$(echo "$GIT_PR_RESPONSE" | jq -r '.head.sha')
     SHORT_SHA=$(git rev-parse --short=8 ${LONG_SHA})
@@ -33,7 +37,7 @@ if [ "$JOB_TYPE" == "presubmit" ] && [[ "$JOB_NAME" != rehearse-* ]]; then
     IMAGE_NAME="${QUAY_REPO}:${TAG_NAME}"
 fi
 
-PR_CHANGESET=$(git diff --name-only main)
+PR_CHANGESET=$(git diff --name-only $RELEASE_BRANCH_NAME)
 echo "Changeset: $PR_CHANGESET"
 
 # Check if changes are exclusively within the specified directories
@@ -48,10 +52,10 @@ for change in $PR_CHANGESET; do
     fi
 done
 
-if $ONLY_IN_DIRS || [[ "$JOB_NAME" == rehearse-* ]]; then
-    echo "Skipping wait for new PR image and proceeding with image tag : next"
-    echo "updated image tag : next"
-    TAG_NAME="next"
+if [[ "$ONLY_IN_DIRS" == "true" || "$JOB_NAME" == rehearse-* || "$JOB_TYPE" == "periodic" ]]; then
+    echo "Skipping wait for new PR image and proceeding with image tag : $RELEASE_TAG_NAME"
+    echo "updated image tag : $RELEASE_TAG_NAME"
+    TAG_NAME=$RELEASE_TAG_NAME
 else
     TIMEOUT=3000         # Maximum wait time of 50 mins (3000 seconds)
     INTERVAL=60             # Check every 60 seconds
@@ -83,5 +87,9 @@ else
         fi
     done
 fi
+
+echo "############## Current branch ##############"
+echo "Current branch: $(git branch --show-current)"
+echo "Using Image: ${QUAY_REPO}:${TAG_NAME}"
 
 bash ./.ibm/pipelines/openshift-ci-tests.sh
