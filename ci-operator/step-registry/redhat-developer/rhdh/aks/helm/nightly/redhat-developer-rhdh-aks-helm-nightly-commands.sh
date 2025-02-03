f7422c92fe0d283cfd9a2f0aa39d231ffca9431f
Zbyněk Drápela <61500440+zdrapela@users.noreply.github.com>
2025-02-03 13:53:29 +0100

    RHDH: fix optional PR checks & static token on AKS (#61156)
    * RHDH: fix optional PR checks & static token on AKS
    
    * Adapt script for PR images
    
    Co-Authored-By: Subhash Khileri <10365050+subhashkhileri@users.noreply.github.com>
    
    * Better presubmits option for operator and helm nightly
    
    * add mapt workflow to steps
    
    * Retries for static token
    
    * always_run: false
    
    * Fix indentation
    
    * Set max_concurrency
    
    Set max_concurrency
    
    ---------
    
    Co-authored-by: Subhash Khileri <10365050+subhashkhileri@users.noreply.github.com>

--- ci-operator/step-registry/redhat-developer/rhdh/aks/helm/nightly/redhat-developer-rhdh-aks-helm-nightly-commands.sh
+++ ci-operator/step-registry/redhat-developer/rhdh/aks/helm/nightly/redhat-developer-rhdh-aks-helm-nightly-commands.sh
@@ -6,38 +6,149 @@ cd /tmp || exit
 
 echo "OC_CLIENT_VERSION: $OC_CLIENT_VERSION"
 
-export GITHUB_ORG_NAME GITHUB_REPOSITORY_NAME NAME_SPACE TAG_NAME
-
-GITHUB_ORG_NAME="redhat-developer"
-GITHUB_REPOSITORY_NAME="rhdh"
-TAG_NAME="next"
-
-# Clone and checkout the specific PR
-git clone "https://github.com/${GITHUB_ORG_NAME}/${GITHUB_REPOSITORY_NAME}.git"
-cd "${GITHUB_REPOSITORY_NAME}" || exit
-
 # use kubeconfig from mapt
 chmod 600 "${SHARED_DIR}/kubeconfig"
 KUBECONFIG="${SHARED_DIR}/kubeconfig"
 export KUBECONFIG
 
 # Create a service account and assign cluster url and token
-SA_NAME="tester-sa-2"
-SA_NAMESPACE="default"
-SA_BINDING_NAME="${SA_NAME}-binding"
-if ! kubectl get serviceaccount ${SA_NAME} -n ${SA_NAMESPACE} &> /dev/null; then
-  echo "Creating service account ${SA_NAME}..."
-  kubectl create serviceaccount ${SA_NAME} -n ${SA_NAMESPACE}
-  echo "Creating cluster role binding..."
-  kubectl create clusterrolebinding ${SA_BINDING_NAME} \
-      --clusterrole=cluster-admin \
-      --serviceaccount=${SA_NAMESPACE}:${SA_NAME}
-  echo "Service account and binding created successfully"
+sa_namespace="default"
+sa_name="tester-sa-2"
+sa_binding_name="${sa_name}-binding"
+sa_secret_name="${sa_name}-secret"
+
+if token="$(kubectl get secret ${sa_secret_name} -n ${sa_namespace} -o jsonpath='{.data.token}' 2>/dev/null)"; then
+  K8S_CLUSTER_TOKEN=$(echo "${token}" | base64 --decode)
+  echo "Acquired existing token for the service account into K8S_CLUSTER_TOKEN"
 else
-  echo "Service account ${SA_NAME} already exists in namespace ${SA_NAMESPACE}"
+  echo "Creating service account"
+  if ! kubectl get serviceaccount ${sa_name} -n ${sa_namespace} &> /dev/null; then
+    echo "Creating service account ${sa_name}..."
+    kubectl create serviceaccount ${sa_name} -n ${sa_namespace}
+    echo "Creating cluster role binding..."
+    kubectl create clusterrolebinding ${sa_binding_name} \
+        --clusterrole=cluster-admin \
+        --serviceaccount=${sa_namespace}:${sa_name}
+    echo "Service account and binding created successfully"
+  else
+    echo "Service account ${sa_name} already exists in namespace ${sa_namespace}"
+  fi
+  echo "Creating secret for service account"
+  kubectl apply --namespace="${sa_namespace}" -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ${sa_secret_name}
+  namespace: ${sa_namespace}
+  annotations:
+    kubernetes.io/service-account.name: ${sa_name}
+type: kubernetes.io/service-account-token
+EOF
+
+  retries=12
+  sleep_time=5
+  for ((i=1; i <= retries; i++)); do
+    if token="$(kubectl get secret ${sa_secret_name} -n ${sa_namespace} -o jsonpath='{.data.token}' 2>/dev/null)"; then
+      echo "Successfully got token on attempt $i."
+      break
+    elif [ $i -eq $retries ]; then
+      echo "Failed to get token after $i attempts. Exiting..."
+      exit 1
+    else
+      echo "Failed to get token on attempt $i, retrying..."
+    fi
+    sleep $sleep_time
+  done
+  K8S_CLUSTER_TOKEN=$(echo "${token}" | base64 --decode)
+  echo "Acquired token for the service account into K8S_CLUSTER_TOKEN"
 fi
-K8S_CLUSTER_TOKEN=$(kubectl create token tester-sa-2 -n default)
 K8S_CLUSTER_URL=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
 export K8S_CLUSTER_TOKEN K8S_CLUSTER_URL
+
+# Prepare to git checkout
+export GIT_PR_NUMBER GITHUB_ORG_NAME GITHUB_REPOSITORY_NAME TAG_NAME
+GIT_PR_NUMBER=$(echo "${JOB_SPEC}" | jq -r '.refs.pulls[0].number')
+echo "GIT_PR_NUMBER : $GIT_PR_NUMBER"
+GITHUB_ORG_NAME="redhat-developer"
+GITHUB_REPOSITORY_NAME="rhdh"
+
+export QUAY_REPO="rhdh-community/rhdh"
+export RELEASE_BRANCH_NAME="main"
+RELEASE_TAG_NAME="next"
+
+# Clone and checkout the specific PR
+git clone "https://github.com/${GITHUB_ORG_NAME}/${GITHUB_REPOSITORY_NAME}.git"
+cd "${GITHUB_REPOSITORY_NAME}" || exit
+
+git config --global user.name "rhdh-qe"
+git config --global user.email "rhdh-qe@redhat.com"
+
+if [ "$JOB_TYPE" == "presubmit" ] && [[ "$JOB_NAME" != rehearse-* ]]; then
+    # If executed as PR check of the repository, switch to PR branch.
+    git fetch origin pull/"${GIT_PR_NUMBER}"/head:PR"${GIT_PR_NUMBER}"
+    git checkout PR"${GIT_PR_NUMBER}"
+    git merge origin/$RELEASE_BRANCH_NAME --no-edit
+    GIT_PR_RESPONSE=$(curl -s "https://api.github.com/repos/${GITHUB_ORG_NAME}/${GITHUB_REPOSITORY_NAME}/pulls/${GIT_PR_NUMBER}")
+    LONG_SHA=$(echo "$GIT_PR_RESPONSE" | jq -r '.head.sha')
+    SHORT_SHA=$(git rev-parse --short=8 ${LONG_SHA})
+    TAG_NAME="pr-${GIT_PR_NUMBER}-${SHORT_SHA}"
+    echo "Tag name: $TAG_NAME"
+    IMAGE_NAME="${QUAY_REPO}:${TAG_NAME}"
+fi
+
+PR_CHANGESET=$(git diff --name-only $RELEASE_BRANCH_NAME)
+echo "Changeset: $PR_CHANGESET"
+
+# Check if changes are exclusively within the specified directories
+DIRECTORIES_TO_CHECK=".ibm|e2e-tests"
+ONLY_IN_DIRS=true
+
+for change in $PR_CHANGESET; do
+    # Check if the change is not within the specified directories
+    if ! echo "$change" | grep -qE "^($DIRECTORIES_TO_CHECK)/"; then
+        ONLY_IN_DIRS=false
+        break
+    fi
+done
+
+if [[ "$ONLY_IN_DIRS" == "true" || "$JOB_NAME" == rehearse-* || "$JOB_TYPE" == "periodic" ]]; then
+    echo "Skipping wait for new PR image and proceeding with image tag : $RELEASE_TAG_NAME"
+    echo "updated image tag : $RELEASE_TAG_NAME"
+    TAG_NAME=$RELEASE_TAG_NAME
+else
+    TIMEOUT=3000         # Maximum wait time of 50 mins (3000 seconds)
+    INTERVAL=60             # Check every 60 seconds
+
+    ELAPSED_TIME=0
+
+    while true; do
+        # Check image availability
+        response=$(curl -s "https://quay.io/api/v1/repository/${QUAY_REPO}/tag/?specificTag=$TAG_NAME")
+
+        # Use jq to parse the JSON and see if the tag exists
+        tag_count=$(echo $response | jq '.tags | length')
+
+        if [ "$tag_count" -gt "0" ]; then
+            echo "Docker image $IMAGE_NAME is now available. Time elapsed: $(($ELAPSED_TIME / 60)) minute(s)."
+            break
+        fi
+
+        # Wait for the interval duration
+        sleep $INTERVAL
+
+        # Increment the elapsed time
+        ELAPSED_TIME=$(($ELAPSED_TIME + $INTERVAL))
+
+        # If the elapsed time exceeds the timeout, exit with an error
+        if [ $ELAPSED_TIME -ge $TIMEOUT ]; then
+            echo "Timed out waiting for Docker image $IMAGE_NAME. Time elapsed: $(($ELAPSED_TIME / 60)) minute(s)."
+            exit 1
+        fi
+    done
+fi
+
+echo "############## Current branch ##############"
+echo "Current branch: $(git branch --show-current)"
+echo "Using Image: ${QUAY_REPO}:${TAG_NAME}"
 
 bash ./.ibm/pipelines/openshift-ci-tests.sh
