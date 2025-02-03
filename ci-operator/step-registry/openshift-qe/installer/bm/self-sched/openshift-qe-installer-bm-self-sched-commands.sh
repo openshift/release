350c155b2a23a84e40200412b70d831e5fe1a8ce
Jose Castillo Lema <josecastillolema@gmail.com>
2025-02-02 20:41:35 -0500

    Simpler query with jq
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/self-sched/openshift-qe-installer-bm-self-sched-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/self-sched/openshift-qe-installer-bm-self-sched-commands.sh
@@ -10,13 +10,13 @@ set -x
 PWD=$(cat "/secret/quads_pwd")
 
 # Login to get token
-TOKEN=$(sed -e 's/^"//' -e 's/"$//' <<< $(curl -k -X POST -u "metal-perfscale-cpt@redhat.com:$PWD" -H "Content-Type: application/json" $QUADS_INSTANCE/api/v3/login/ | awk -F\: '{print $2}' | awk -F\, '{print $1}'))
+TOKEN=$(curl -sSk -X POST -u "metal-perfscale-cpt@redhat.com:$PWD" -H "Content-Type: application/json" $QUADS_INSTANCE/api/v3/login/ | jq .'auth_token')
 
 # Get available hosts for self scheduling
-HOST=$(curl -k $QUADS_INSTANCE/api/v3/available\?can_self_schedule\=true | jq '.[0]')
+HOST=$(curl -sSk $QUADS_INSTANCE/api/v3/available\?can_self_schedule\=true | jq '.[0]')
 
 # Create self scheduling assignment
-CLOUD=$(curl -k -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d '{"description": "Temporary allocation from openshift-ci", "owner": "metal-perfscale-cpt", "qinq": 1, "wipe": "true"}' $QUADS_INSTANCE/api/v3/assignments/self | jq .'Cloud.Name')
+CLOUD=$(curl -sSk -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d '{"description": "Temporary allocation from openshift-ci", "owner": "metal-perfscale-cpt", "qinq": 1, "wipe": "true"}' $QUADS_INSTANCE/api/v3/assignments/self | jq .'Cloud.Name')
 
 # Create schedule
 curl -k -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d '{"cloud":"$CLOUD", "hostname": "$HOST"}' $QUADS_INSTANCE/api/v3/schedules
