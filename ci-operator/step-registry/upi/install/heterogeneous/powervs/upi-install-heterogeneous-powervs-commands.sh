6c3a7a90fa955a057b5770bfaa16e5e9c5e879dc
Paul Bastide <pbastide@redhat.com>
2025-02-04 05:09:04 -0500

    fix: calculation is corrected
    Signed-off-by: Paul Bastide <pbastide@redhat.com>

--- ci-operator/step-registry/upi/install/heterogeneous/powervs/upi-install-heterogeneous-powervs-commands.sh
+++ ci-operator/step-registry/upi/install/heterogeneous/powervs/upi-install-heterogeneous-powervs-commands.sh
@@ -349,6 +349,6 @@ download_automation_code
 configure_automation
 setup_powervs_image
 run_automation
-wait_for_additional_nodes_readiness "${ADDITIONAL_WORKERS}"
+wait_for_additional_nodes_readiness "$((2+${ADDITIONAL_WORKERS}))"
 
 exit 0
