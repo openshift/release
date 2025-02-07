e0404b4f7bb5abbaaa2175b188696e2f16f4ae69
Xia Zhao <xzha@redhat.com>
2025-02-07 10:06:25 +0800

    enhance

--- ci-operator/step-registry/olmv1/disable-default-clustercatalogs/olmv1-disable-default-clustercatalogs-commands.sh
+++ ci-operator/step-registry/olmv1/disable-default-clustercatalogs/olmv1-disable-default-clustercatalogs-commands.sh
@@ -8,25 +8,25 @@ function run_command() {
 }
 
 
-function disable_default_catalogsource () {
+function disable_default_clustercatalog () {
     ocp_version=$(oc get -o jsonpath='{.status.desired.version}' clusterversion version)
     major_version=$(echo ${ocp_version} | cut -d '.' -f1)
     minor_version=$(echo ${ocp_version} | cut -d '.' -f2)
     if [[ "X${major_version}" == "X4" && -n "${minor_version}" && "${minor_version}" -gt 17 ]]; then
-        echo "disable olmv1 default clustercatalog"
+        echo "disable olmv1 default clustercatalogs"
         run_command "oc patch clustercatalog openshift-certified-operators -p '{\"spec\": {\"availabilityMode\": \"Unavailable\"}}' --type=merge"
         run_command "oc patch clustercatalog openshift-redhat-operators -p '{\"spec\": {\"availabilityMode\": \"Unavailable\"}}' --type=merge"
         run_command "oc patch clustercatalog openshift-redhat-marketplace -p '{\"spec\": {\"availabilityMode\": \"Unavailable\"}}' --type=merge"
         run_command "oc patch clustercatalog openshift-community-operators -p '{\"spec\": {\"availabilityMode\": \"Unavailable\"}}' --type=merge"
+        sleep 1
         run_command "oc get clustercatalog"        
     fi
 }
-
 
 #################### Main #######################################
 run_command "oc whoami"
 run_command "oc version -o yaml"
 
-disable_default_catalogsource
+disable_default_clustercatalog
 
 
