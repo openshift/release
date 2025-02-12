21b2cd21b5abff6b77db002f1bcf06d4b05279bb
Hilliary Lipsig <hilliary@wisesoft.us>
2025-02-11 21:18:45 -0800

    start of upgrade workflow

---
+++ ci-operator/step-registry/aro-e2e/classic/cluster/update/aro-e2e-update-test-commands.sh
@@ -0,0 +1,7 @@
+#get cluster Kubeadmin and login from shared dir
+kubectl set context --kubeconfig ${SHARED_DIR}/kubeconfig
+#get pull secret
+oc get secret/pull-secret -n openshift-config -o jsonpath='{.data.\.dockerconfigjson}' | base64 -d > existing-pull-secret.json
+jq -s '.[0] * .[1]' existing-pull-secret.json /run/secrets/ci.openshift.io/cluster-profile/pull-secret > merged-pull-secret.json
+#Set Pull secret
+oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=merged-pull-secret.json
