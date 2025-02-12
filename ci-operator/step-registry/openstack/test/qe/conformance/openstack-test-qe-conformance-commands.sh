d700d9c9df26eb1b3c129fb3639167eb6e316f9a
Miguel Carpio <mcarpio@redhat.com>
2025-02-12 16:17:46 +0100

    OpenStack QE CI periodic jobs integration

---
+++ ci-operator/step-registry/openstack/test/qe/conformance/openstack-test-qe-conformance-commands.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+info() {
+	>&2 printf '%s: %s\n' "$(date --utc +%Y-%m-%dT%H:%M:%SZ)" "$*"
+}
+
+export OCP_TESTING_URL="$(<"${SHARED_DIR}/ocp_testing_url")"
+
+export JOB_NUMBER="$(<"${SHARED_DIR}/job_number")"
+
+info "INFO: Get openshift-test conformance parallel test results e2e.log"
+
+# Insecure option for testing purpose until https://github.com/openshift/release/pull/61614 is merged and tested 
+curl --insecure "${OCP_TESTING_URL}/${JOB_NUMBER}/artifact/conformance-parallel-test-results/conformance-parallel-test.log" > "${ARTIFACT_DIR}/e2e.log"
+
+info "INFO: Get openshift-test conformance parallel test results junit"
+
+# Insecure option for testing purpose until https://github.com/openshift/release/pull/61614 is merged and tested
+curl --insecure -o "${ARTIFACT_DIR}/junit.zip" "${OCP_TESTING_URL}/${JOB_NUMBER}/artifact/conformance-parallel-test-results/*zip*/conformance-parallel-test-results.zip"
+
+unzip ${ARTIFACT_DIR}/junit.zip -d ${ARTIFACT_DIR} && mv ${ARTIFACT_DIR}/conformance-parallel-test-results ${ARTIFACT_DIR}/junit
+
+rm -f ${ARTIFACT_DIR}/junit.zip
