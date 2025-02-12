20d54d91ad2c89c9341511ad88dc1b61c63c4b86
Miguel Carpio <mcarpio@redhat.com>
2025-02-12 16:17:46 +0100

    OpenStack QE CI periodic jobs integration

---
+++ ci-operator/step-registry/openstack/test/qe/csi/manila/openstack-test-qe-csi-manila-commands.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+export OCP_TESTING_URL="$(<"${SHARED_DIR}/ocp_testing_url")"
+
+export OCP_RELEASE="$(<"${SHARED_DIR}/osp_release")"
+
+export JOB_NUMBER="$(<"${SHARED_DIR}/job_number")"
+
+curl "${OCP_TESTING_URL}/${JOB_NUMBER}/artifact/manilacsi-test-results/manilacsi-test.log" > "${ARTIFACT_DIR}/e2e.log"
+
+curl -o "${ARTIFACT_DIR}/junit.zip" "${OCP_TESTING_URL}/${JOB_NUMBER}/artifact/manilacsi-test-results/*zip*/manilacsi-test-results.zip"
+
+unzip ${ARTIFACT_DIR}/junit.zip -d ${ARTIFACT_DIR} && mv ${ARTIFACT_DIR}/manilacsi-test-results ${ARTIFACT_DIR}/junit
+
+rm -f ${ARTIFACT_DIR}/junit.zip
---
+++ ci-operator/step-registry/openstack/test/qe/openstack/openstack-test-qe-openstack-commands.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+export OCP_TESTING_URL="$(<"${SHARED_DIR}/ocp_testing_url")"
+
+export OCP_RELEASE="$(<"${SHARED_DIR}/osp_release")"
+
+export JOB_NUMBER="$(<"${SHARED_DIR}/job_number")"
+
+curl "${OCP_TESTING_URL}/${JOB_NUMBER}/artifact/openstack-test-results/manilacsi-test.log" > "${ARTIFACT_DIR}/e2e.log"
+
+curl -o "${ARTIFACT_DIR}/junit.zip" "${OCP_TESTING_URL}/${JOB_NUMBER}/artifact/openstack-test-results/*zip*/openstack-test-results.zip"
+
+unzip ${ARTIFACT_DIR}/junit.zip -d ${ARTIFACT_DIR} && mv ${ARTIFACT_DIR}/openstack-test-results ${ARTIFACT_DIR}/junit
+
+rm -f ${ARTIFACT_DIR}/junit.zip
