a40ed10f7db9c25c46a0c909a8c0c2e840a1de65
Thirumalesh Aaraveti <athiruma@redhat.com>
2025-02-12 18:33:41 +0530

    print files

--- ci-operator/step-registry/openshift-qe/cloud-governance/perf-dept/openshift-qe-cloud-governance-perf-dept-commands.sh
+++ ci-operator/step-registry/openshift-qe/cloud-governance/perf-dept/openshift-qe-cloud-governance-perf-dept-commands.sh
@@ -1,9 +1,17 @@
-#!/bin/bash
+# #!/bin/bash
 
-cat "/secret/perf-dept-creds" > ./perf-creds.sh
-# chmod 777 ./perf-creds.sh
-ls -la
-# source ./perf-creds.sh
+# cat "/secret/perf-dept-creds"  > /tmp/creds.sh
+# chmod 755 /tmp/creds.sh
+# source /tmp/creds.sh
+# export es_host="$(cat /secret/es_host)"
+# export es_port="$(cat /secret/es_port)"
 
-python3 /usr/local/cloud_governance/main.py
+# python3 /usr/local/cloud_governance/main.py
 
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+
+ping -c1 elasticsearch.app.intlab.redhat.com
+echo $?
