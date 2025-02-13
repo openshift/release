ed522efbe911880540b3bb554dca79e23bd6191a
davdhacs <105243888+davdhacs@users.noreply.github.com>
2025-02-13 10:17:34 -0700

    just base claim test

--- ci-operator/step-registry/stackrox/stackrox/e2e-test/stackrox-stackrox-e2e-test-commands.sh
+++ ci-operator/step-registry/stackrox/stackrox/e2e-test/stackrox-stackrox-e2e-test-commands.sh
@@ -2,4 +2,13 @@
 export OPENSHIFT_CI_STEP_NAME="stackrox-stackrox-e2e-test"
 job="${TEST_SUITE:-${JOB_NAME_SAFE#merge-}}"
 job="${job#nightly-}"
-exec .openshift-ci/dispatch.sh "${job}"
+oc get nodes -A || true
+oc get namespaces -A || true
+oc get pods -A || true
+ls -la
+for I in {1..15}; do
+  printf "$I %(%FT%T%z)T\n" -1
+  sleep 60
+done
+oc get pods -A || true
+#exec .openshift-ci/dispatch.sh "${job}"
