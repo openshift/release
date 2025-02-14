097ac5aefd1fa0f0734cbc5490cc387678dfa988
Maxim Patlasov <mpt@redhat.com>
2025-02-13 18:11:50 -0800

    more debug: save and check symlinks for root disk

--- ci-operator/step-registry/openshift/e2e/test/debug/openshift-e2e-test-debug-commands.sh
+++ ci-operator/step-registry/openshift/e2e/test/debug/openshift-e2e-test-debug-commands.sh
@@ -5,13 +5,11 @@ set -o errexit
 set -o pipefail
 
 echo "DEBUG....."
-OUT=$SHARED_DIR/disk-by-id.out
-echo $OUT
-rm -f $OUT
 for n in `oc get no |grep -v "^NAME" |cut -d ' ' -f1`; do
-    echo $n >> $OUT
-    oc debug --to-namespace default node/$n -- ls -l /host/dev/disk/by-id >> $OUT
-    echo "END-OF-$n" >> $OUT
+    OUT=$SHARED_DIR/disk-by-id-for-$n.out
+    oc debug --to-namespace default node/$n -- chroot /host /usr/bin/bash -c "ls -l /dev/disk/by-id |grep \$(basename \$(head -1 /proc/mounts |cut -d ' ' -f1)) |cut -d ' ' -f9" >$OUT
+    echo "NODE $n"
+    cat $OUT
 done
 echo "DEBUG ----"
 echo $SHARED_DIR
