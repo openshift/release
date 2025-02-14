e7874f834c7aec657c9a9e2fdce06f38ac017c89
Maxim Patlasov <mpt@redhat.com>
2025-02-13 18:11:50 -0800

    more debug: save and check symlinks for root disk

--- ci-operator/step-registry/openshift/e2e/test/debug2/openshift-e2e-test-debug2-commands.sh
+++ ci-operator/step-registry/openshift/e2e/test/debug2/openshift-e2e-test-debug2-commands.sh
@@ -6,12 +6,21 @@ set -o pipefail
 
 echo "DEBUG2....."
 for n in `oc get no |grep -v "^NAME" |cut -d ' ' -f1`; do
-    echo $n
-    oc debug --to-namespace default node/$n -- ls -l /host/dev/disk/by-id
+  echo "NODE $n"
+  CURRENT_ROOT_DISK=$(oc debug --to-namespace default node/$n -- chroot /host /usr/bin/bash -c "head -1 /proc/mounts |cut -d ' ' -f1")
+  echo "CURRENT_ROOT_DISK $CURRENT_ROOT_DISK"
+  cat $SHARED_DIR/disk-by-id-for-$n.out
+  for p in $(cat $SHARED_DIR/disk-by-id-for-$n.out); do
+    SYMLINK_PRESENT=$(oc debug --to-namespace default node/$n -- chroot /host /usr/bin/bash -c "if [ -L /dev/disk/by-id/$p ]; then echo 1; else echo 0; fi")
+    if [ $SYMLINK_PRESENT != "1" ]; then
+      echo "Missed symlink $p for node $n"
+      exit 1
+    fi
+    DISK=$(oc debug --to-namespace default node/$n -- chroot /host /usr/bin/realpath /dev/disk/by-id/$p)
+    if [ $CURRENT_ROOT_DISK != $DISK ]; then
+      echo "MISMATCH for node $n: The symlink $p exists, but points to disk $DISK, not $CURRENT_ROOT_DISK as expected"
+      exit 1
+    fi
+  done
 done
-echo "DEBUG2 ----"
-echo $SHARED_DIR
-ls -al $SHARED_DIR
-echo "DEBUG2 --CAT--"
-cat $SHARED_DIR/disk-by-id.out
 echo "DEBUG2 ---- DONE"
