1f52a448039ee58fff2c3d83d9c785eeb6ade42d
Jad Haj Yahya <jhajyahy@redhat.com>
2025-02-18 12:02:53 +0200

    Fix issue on static v6 patch

--- ci-operator/step-registry/baremetal/lab/ipi/conf/virtual-media/ipv6/static/baremetal-lab-ipi-conf-virtual-media-ipv6-static-commands.sh
+++ ci-operator/step-registry/baremetal/lab/ipi/conf/virtual-media/ipv6/static/baremetal-lab-ipi-conf-virtual-media-ipv6-static-commands.sh
@@ -5,7 +5,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-echo "Creating patch file to configure static ipv6"
+echo "Creating patch file to configure redfish virtual media"
 cat > "$SHARED_DIR/static_v6_patch_install_config.yaml" <<EOF
 platform:
   baremetal:
@@ -17,13 +17,18 @@ EOF
 for bmhost in $(yq e -o=j -I=0 '.[]' "$SHARED_DIR/hosts.yaml"); do
   # shellcheck disable=SC1090
   . <(echo "$bmhost" | yq e 'to_entries | .[] | (.key + "=\"" + .value + "\"")')
-  cat >> "$SHARED_DIR/redfish_patch_install_config.yaml" <<EOF
+  cat >> "$SHARED_DIR/static_v6_patch_install_config.yaml" <<EOF
     - name: ${name}
       role: ${name%%-[0-9]*}
       bootMACAddress: ${mac}
       rootDeviceHints:
         ${root_device:+deviceName: ${root_device}}
         ${root_dev_hctl:+hctl: ${root_dev_hctl}}
+      bmc:
+        address: ${redfish_scheme}://${bmc_address}${redfish_base_uri}
+        disableCertificateVerification: true
+        username: ${redfish_user}
+        password: ${redfish_password}
       networkConfig:
         interfaces:
         - name: ${baremetal_iface}
@@ -32,27 +37,17 @@ for bmhost in $(yq e -o=j -I=0 '.[]' "$SHARED_DIR/hosts.yaml"); do
           ipv4:
             enabled: ${ipv4_enabled}
           ipv6:
-            enabled: ${ipv6_enabled}
             dhcp: false
             address:
             - ip: ${ipv6}
               prefix-length: ${INTERNAL_NET_V6_CIDR##*/}
             enabled: ${ipv6_enabled}
-        dns-resolver:
-          config:
-            server:
-              - ${INTERNAL_NET_IPV6}
-        routes:
-          config:
-            - destination: 0.0.0.0/0
-              next-hop-address: ${INTERNAL_NET_IPV6}
-              next-hop-interface: ${baremetal_iface}
 EOF
 
   # Append configurations for disabled interfaces
   IFS=';' read -r -a ipi_disabled_ifaces <<< "${ipi_disabled_ifaces}"
   for iface in "${ipi_disabled_ifaces[@]}"; do
-    cat >> "$SHARED_DIR/redfish_patch_install_config.yaml" <<EOF
+    cat >> "$SHARED_DIR/static_v6_patch_install_config.yaml" <<EOF
         - name: ${iface}
           type: ethernet
           state: up
@@ -64,7 +59,17 @@ EOF
             dhcp: false
 EOF
   done
-
-  cat >> "$SHARED_DIR/redfish_patch_install_config.yaml" <<EOF
+  cat >> "$SHARED_DIR/static_v6_patch_install_config.yaml" <<EOF
+        dns-resolver:
+          config:
+            server:
+            - ${INTERNAL_NET_IPV6}
+        routes:
+          config:
+          - destination: 0.0.0.0/0
+            next-hop-address: ${INTERNAL_NET_IPV6}
+            next-hop-interface: ${baremetal_iface}
+EOF
+  cat >> "$SHARED_DIR/static_v6_patch_install_config.yaml" <<EOF
 EOF
 done
