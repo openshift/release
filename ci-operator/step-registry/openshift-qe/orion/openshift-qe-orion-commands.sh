c6b977bafc2696c51c0043cdd80bf556ec3b8da3
Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.rmtusnc.csb>
2025-02-19 07:13:25 -0500

    Improvising orion output structures
    Signed-off-by: Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.rmtusnc.csb>

--- ci-operator/step-registry/openshift-qe/orion/openshift-qe-orion-commands.sh
+++ ci-operator/step-registry/openshift-qe/orion/openshift-qe-orion-commands.sh
@@ -20,11 +20,6 @@ pushd orion
 
 pip install -r requirements.txt
 
-# Download the latest ACK file
-if [ ${JUNIT} == true ]; then
-  curl -sL https://raw.githubusercontent.com/cloud-bulldozer/orion/refs/heads/main/ack/${VERSION}_${ACK_FILE} > /tmp/${VERSION}_${ACK_FILE}
-fi
-
 if [[ ${ES_TYPE} == "qe" ]]; then
     ES_PASSWORD=$(cat "/secret/qe/password")
     ES_USERNAME=$(cat "/secret/qe/username")
@@ -37,15 +32,27 @@ fi
 
 pip install .
 export EXTRA_FLAGS=" --lookback ${LOOKBACK}d"
+# Download the latest ACK file
+curl -sL https://raw.githubusercontent.com/cloud-bulldozer/orion/refs/heads/main/ack/${VERSION}_${ACK_FILE} > /tmp/${VERSION}_${ACK_FILE}
+
 if [[ ! -z "$UUID" ]]; then
     export EXTRA_FLAGS+=" --uuid ${UUID}"
 fi
+
 if [ ${HUNTER_ANALYZE} == "true" ]; then
     export EXTRA_FLAGS+=" --hunter-analyze"
 fi
-if [ ${JUNIT} == true ]; then
+
+if [ ${OUTPUT_FORMAT} == "JUNIT" ]; then
     export EXTRA_FLAGS+=" --output-format junit"
     export EXTRA_FLAGS+=" --save-output-path=junit.xml"
+elif [ "${OUTPUT_FORMAT}" == "JSON" ]; then
+    export EXTRA_FLAGS+=" --output-format json"
+elif [ "${OUTPUT_FORMAT}" == "TEXT" ]; then
+    export EXTRA_FLAGS+=" --output-format text"
+else
+    echo "Unsupported format: ${OUTPUT_FORMAT}"
+    exit 1
 fi
 
 if [[ ! -z "$ORION_CONFIG" ]]; then
@@ -54,6 +61,10 @@ fi
 
 if [[ ! -z "$ACK_FILE" ]]; then
     export EXTRA_FLAGS+=" --ack /tmp/${VERSION}_${ACK_FILE}"
+fi
+
+if [ ${COLLAPSE} == "true" ]; then
+    export EXTRA_FLAGS+=" --collapse"
 fi
 
 if [[ -n "${ORION_ENVS:-}" ]]; then
@@ -76,10 +87,8 @@ es_metadata_index=${ES_METADATA_INDEX} es_benchmark_index=${ES_BENCHMARK_INDEX} 
 orion_exit_status=$?
 set -e
 
-if [ ${JUNIT} == true ]; then
+if [ ${OUTPUT_FORMAT} == "JUNIT" ]; then
   cp *.xml ${ARTIFACT_DIR}/
-else
-  cat *.csv
 fi
 
 exit $orion_exit_status
