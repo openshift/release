be240aadc3f2a1f6307ee8b16004308a31f61f13
Li Zhang <lzha@redhat.com>
2025-02-20 10:23:17 +0800

    add quayio testing step

---
+++ ci-operator/step-registry/quay-tests/quayio/quay-tests-quayio-commands.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+QUAY_USERNAME=$(cat /var/run/quay-qe-quayio-secret/username)
+QUAY_PASSWORD=$(cat /var/run/quay-qe-quayio-secret/password)
+QUAY_API_TOKEN=$(cat /var/run/quay-qe-quayio-secret/oauth2token)
+
+cd quay-io-tests && yarn install
+
+function copyArtifacts {
+    cp -r ./cypress/results/* $ARTIFACT_DIR
+}
+
+export CYPRESS_QUAY_API_TOKEN="$QUAY_API_TOKEN"
+export CYPRESS_QUAY_PASSWORD="$QUAY_PASSWORD"
+export CYPRESS_QUAY_USER="$QUAY_USERNAME"
+yarn run all
+
+
+#Archive the testing report XML file
+trap copyArtifacts EXIT
