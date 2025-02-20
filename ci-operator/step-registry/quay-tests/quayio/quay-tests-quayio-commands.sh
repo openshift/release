6f5b4c2a9a8c70f75d91ab4c46eb57bbd49d6c75
Li Zhang <lzha@redhat.com>
2025-02-20 16:46:22 +0800

    fix the archive issue and use no color in Cypress Console output

--- ci-operator/step-registry/quay-tests/quayio/quay-tests-quayio-commands.sh
+++ ci-operator/step-registry/quay-tests/quayio/quay-tests-quayio-commands.sh
@@ -8,17 +8,17 @@ QUAY_API_TOKEN=$(cat /var/run/quay-qe-quayio-secret/oauth2token)
 GITHUB_ACCESS_TOKEN=$(cat /var/run/quay-qe-quayio-secret/accesstoken)
 
 git clone https://"$GITHUB_ACCESS_TOKEN"@github.com/quay/quay-tests.git
-cd quay-tests/quay-io-tests && mkdir -p cypress/downloads
+cd quay-tests/quay-io-tests && mkdir -p cypress/downloads && mkdir -p cypress/results 
 yarn install
-
-function copyArtifacts {
-    cp -r ./cypress/results/* $ARTIFACT_DIR
-}
 
 export CYPRESS_QUAY_API_TOKEN="$QUAY_API_TOKEN"
 export CYPRESS_QUAY_PASSWORD="$QUAY_PASSWORD"
 export CYPRESS_QUAY_USER="$QUAY_USERNAME"
 NO_COLOR=1 yarn run all > ./cypress/results/quay_io_testing_report
+
+function copyArtifacts {
+    cp -r ./cypress/results/* $ARTIFACT_DIR || true
+}
 
 #Archive the testing report XML file
 trap copyArtifacts EXIT
