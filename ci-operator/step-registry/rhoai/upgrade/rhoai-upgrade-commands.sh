37a8d96761939ba90e5d1d174a8fa53378ef4663
lisa <liswang@redhat.com>
2025-02-22 15:44:23 -0500

    test

--- ci-operator/step-registry/rhoai/upgrade/rhoai-upgrade-commands.sh
+++ ci-operator/step-registry/rhoai/upgrade/rhoai-upgrade-commands.sh
@@ -44,23 +44,74 @@ export NAME_5
 TEST_SUITE="Post-Upgrade"
 export TEST_SUITE
 
+# create quay pull secret
+QUAY_INFO="/tmp/secrets/ci"
+secrets=$(mktemp -d)
+
+oc extract secret/pull-secret -n openshift-config --to="${secrets}"
+if ! grep 'quay.io/rhoai' < "${secrets}"/.dockerconfigjson
+then
+  jq '.auths["quay.io/rhoai"] = {"auth": "'$(cat ${QUAY_INFO}/quay_token)'"}' "${secrets}"/.dockerconfigjson > "${secrets}"/.dockerconfigjson_update
+  oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson="${secrets}"/.dockerconfigjson_update
+fi
+
+cat <<EOF | oc apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: pull-secret
+  namespace: openshift-marketplace
+---
+apiVersion: operator.openshift.io/v1alpha1
+kind: ImageContentSourcePolicy
+metadata:
+  name: quay-registry
+spec:
+  repositoryDigestMirrors:
+    - source: registry.redhat.io/rhoai
+      mirrors:
+        - quay.io/rhoai
+---
+apiVersion: operators.coreos.com/v1alpha1
+kind: CatalogSource
+metadata:
+  name: rhoai-catalog-dev
+  namespace: openshift-marketplace
+spec:
+  displayName: OpenShift AI Pre-Release
+  publisher: RHOAI Development Catalog
+  image: quay.io/rhoai/rhoai-fbc-fragment:rhoai-${UPGRADE_VERSION}
+  sourceType: grpc
+  secrets:
+     - pull-secret
+EOF
+
+sleep 120
+
+echo "Checking custom catalog..."
+oc get catalogsource -n openshift-marketplace
+
+# update subscription to use the custome catalog
+oc patch subscription rhods-operator -n redhat-ods-operator \
+--type='merge' -p '{"spec":{"source":"rhoai-catalog-dev"}}'
+
 # enable the auto upgrade
 oc patch subscription rhods-operator -n redhat-ods-operator --type='merge' -p '{"spec": {"installPlanApproval": "Automatic"}}'
 oc get subscription rhods-operator -n redhat-ods-operator -o yaml | grep installPlanApproval
 
-cat <<EOF | oc apply -f -
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  name: rhods-operator
-  namespace: redhat-ods-operator
-spec:
-  channel: "fast"  # Change based on the desired channel
-  installPlanApproval: Automatic
-  name: rhods-operator
-  source: rhoai-catalog-dev
-  sourceNamespace: openshift-marketplace
-EOF
+#cat <<EOF | oc apply -f -
+#apiVersion: operators.coreos.com/v1alpha1
+#kind: Subscription
+#metadata:
+#  name: rhods-operator
+#  namespace: redhat-ods-operator
+#spec:
+#  channel: "fast"  # Change based on the desired channel
+#  installPlanApproval: Automatic
+#  name: rhods-operator
+#  source: rhoai-catalog-dev
+#  sourceNamespace: openshift-marketplace
+#EOF
 
 sleep 120
 echo "Checking CSV status"
