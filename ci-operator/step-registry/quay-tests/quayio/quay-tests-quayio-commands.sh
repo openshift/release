c9e312bbf6e4ba7668490bb5be898de181996cad
Li Zhang <lzha@redhat.com>
2025-02-24 14:19:10 +0800

    update to archive the testing report

--- ci-operator/step-registry/quay-tests/quayio/quay-tests-quayio-commands.sh
+++ ci-operator/step-registry/quay-tests/quayio/quay-tests-quayio-commands.sh
@@ -7,6 +7,13 @@ QUAY_PASSWORD=$(cat /var/run/quay-qe-quayio-secret/password)
 QUAY_API_TOKEN=$(cat /var/run/quay-qe-quayio-secret/oauth2token)
 GITHUB_ACCESS_TOKEN=$(cat /var/run/quay-qe-quayio-secret/accesstoken)
 
+function copyArtifacts {
+    cp -r ./cypress/results/* $ARTIFACT_DIR || true
+}
+
+#Archive the testing report XML file
+trap copyArtifacts EXIT
+
 git clone https://"$GITHUB_ACCESS_TOKEN"@github.com/quay/quay-tests.git
 cd quay-tests/quay-io-tests && mkdir -p cypress/downloads && mkdir -p cypress/results 
 yarn install
@@ -14,11 +21,4 @@ yarn install
 export CYPRESS_QUAY_API_TOKEN="$QUAY_API_TOKEN"
 export CYPRESS_QUAY_PASSWORD="$QUAY_PASSWORD"
 export CYPRESS_QUAY_USER="$QUAY_USERNAME"
-NO_COLOR=1 yarn run all
-
-function copyArtifacts {
-    cp -r ./cypress/results/* $ARTIFACT_DIR || true
-}
-
-#Archive the testing report XML file
-trap copyArtifacts EXIT
+NO_COLOR=1 yarn run all > $ARTIFACT_DIR/quayio_testing_report
