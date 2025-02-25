882cca82493eb8417f8ef2e39d16b1a52296e8c1
Sean Zhao <szhao@redhat.com>
2025-02-25 15:47:33 +0800

    initilize quay performance testing

---
+++ ci-operator/step-registry/quay-tests/test/performance/quay-tests-test-performance-commands.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+set -o nounset
+# part 1, Quay performance test
+QUAY_ROUTE=$(cat "$SHARED_DIR"/quayroute)
+QUAY_OAUTH_TOKEN=$(cat "$SHARED_DIR"/quay_oauth2_token)
+ELK_SERVER='https://search-quay-performance-mdaaozleo7nnmgvrze3fduygra.us-east-2.es.amazonaws.com'
+
+#create organization "perftest" and namespace "quay-perf" for Quay performance test
+quay_perf_organization="perftest"
+quay_perf_namespace="quay-perf"
+
+curl --location --request POST "https://${QUAY_ROUTE}/api/v1/organization/" \
+    --header "Content-Type: application/json" \
+    --header "Authorization: Bearer ${QUAY_OAUTH_TOKEN}" \
+    --data-raw '{
+        "name": "'"${quay_perf_organization}"'",
+        "email": "testperf@testperf.com"
+    }' -k
+
+
+#   refer to https://github.com/quay/quay-performance-scripts/README.md
+push_pull_numbers="5000"
+target_hit_size="100"
+concurrency="15"
+
+oc new-project "$quay_perf_namespace"
+oc adm policy add-scc-to-user privileged system:serviceaccount:"$quay_perf_namespace":default
+
+
+#Deploy Quay performance job to OCP namespace $quay_perf_namespace
+cat <<EOF | oc apply -f -
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  name: quay-perf-test-serviceaccount
+rules:
+- apiGroups: ["extensions", "apps", "batch", "security.openshift.io", "policy"]
+  resources: ["deployments", "jobs", "pods", "services", "jobs/status", "podsecuritypolicies", "securitycontextconstraints"]
+  verbs: ["use", "get", "list", "watch", "create", "update", "patch", "delete"]
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: quay-perf-test-role
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: Role
+  name: quay-perf-test-serviceaccount
+subjects:
+- kind: ServiceAccount
+  name: default
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: redis
+  labels:
+    quay-perf-test-component: redis
+spec:
+  replicas: 1 
+  selector:
+    matchLabels:
+      quay-perf-test-component: redis
+  template:
+    metadata:
+      labels:
+        quay-perf-test-component: redis
+    spec:
+      containers:
+      - name: redis-master
+        image: registry.access.redhat.com/rhscl/redis-32-rhel7
+        imagePullPolicy: "IfNotPresent"
+        ports:
+        - containerPort: 6379
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: redis
+  labels:
+    quay-perf-test-component: redis-service
+spec:
+  ports:
+    - port: 6379
+  selector:
+    quay-perf-test-component: redis
+---
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: quay-perf-test-orchestrator
+  labels:
+    quay-perf-test-component: orchestrator
+spec:
+  template:
+    spec:
+      containers:
+      - name: python
+        image: quay.io/quay-qetest/quay-load:latest
+        securityContext:
+          privileged: true
+        env:
+          - name: QUAY_HOST
+            value: "${QUAY_ROUTE}"
+          - name: QUAY_OAUTH_TOKEN
+            value: "${QUAY_OAUTH_TOKEN}"
+          - name: QUAY_ORG
+            value: "${quay_perf_organization}"
+          - name: ES_HOST
+            value: "${ELK_SERVER}"
+          - name: ES_PORT
+            value: "443"
+          - name: PYTHONUNBUFFERED
+            value: "0"
+          - name: ES_INDEX
+            value: "quay-vegeta"
+          - name: PUSH_PULL_IMAGE
+            value: "quay.io/quay-qetest/quay-load:latest"
+          - name: PUSH_PULL_ES_INDEX
+            value: "quay-push-pull"
+          - name: PUSH_PULL_NUMBERS
+            value:  "${push_pull_numbers}"
+          - name: TARGET_HIT_SIZE
+            value: "${target_hit_size}"
+          - name: CONCURRENCY
+            value: "${concurrency}"
+          - name: TEST_NAMESPACE
+            value: "${quay_perf_namespace}"
+          - name: TEST_PHASES
+            value: "LOAD,RUN"
+            # value: "LOAD,RUN,DELETE"
+        resources:
+          requests:
+            cpu: "1"
+            memory: "512Mi"
+        imagePullPolicy: Always
+      restartPolicy: Never
+  backoffLimit: 0
+
+EOF
+
+echo "the Perf Job needs about 4~5 hours to complete"
+echo "check the OCP Quay Perf Job, if it complete, go to Kibana to generate index pattern and get Quay Perf metrics"
+       
