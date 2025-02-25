5c6a60d05a5b7422c73d5ef6ae32d248e0dc4b9a
He Liu <heli@redhat.com>
2025-02-25 15:42:13 +0800

    update wi rg

--- ci-operator/step-registry/azure/provision/service-principal/hypershift/azure-provision-service-principal-hypershift-commands.sh
+++ ci-operator/step-registry/azure/provision/service-principal/hypershift/azure-provision-service-principal-hypershift-commands.sh
@@ -61,6 +61,38 @@ for component in $COMPONENTS; do
     component_to_cert_name+=(["$component"]="$name")
 done
 
+## create secret json files
+for component in "${!component_to_cert_name[@]}"; do
+    CERT_NAME="${component_to_cert_name[$component]}"
+    echo "Processing certificate: $CERT_NAME"
+
+    CERT_DETAILS=$(az keyvault secret show --vault-name $KV_NAME --name $CERT_NAME --query "{value: value, notBefore: attributes.notBefore, expires: attributes.expires}" -o json)
+    CLIENT_SECRET=$(echo $CERT_DETAILS | jq -r '.value')
+    NOT_BEFORE=$(echo $CERT_DETAILS | jq -r '.notBefore')
+    NOT_AFTER=$(echo $CERT_DETAILS | jq -r '.expires')
+    SP_DETAILS=$(az ad sp list --display-name $CERT_NAME --query "[0].{client_id: appId, tenant_id: appOwnerOrganizationId}" -o json)
+    CLIENT_ID=$(echo $SP_DETAILS | jq -r '.client_id')
+    TENANT_ID=$(echo $SP_DETAILS | jq -r '.tenant_id')
+
+    if [[ -z "$CLIENT_ID" || -z "$TENANT_ID" ]]; then
+        echo "Error: Could not retrieve client ID or tenant ID for certificate: $CERT_NAME"
+        continue
+    fi
+
+    JSON_FILE="${SHARED_DIR}/${CERT_NAME}.json"
+    echo "{
+        \"authentication_endpoint\": \"https://login.microsoftonline.com/\",
+        \"client_id\": \"$CLIENT_ID\",
+        \"client_secret\": \"$CLIENT_SECRET\",
+        \"tenant_id\": \"$TENANT_ID\",
+        \"not_before\": \"$NOT_BEFORE\",
+        \"not_after\": \"$NOT_AFTER\"
+    }" > "${JSON_FILE}"
+
+    echo "Created JSON file: ${JSON_FILE}"
+    az keyvault secret set --name "${CERT_NAME}-json" --vault-name "${KV_NAME}" --file "${JSON_FILE}"
+done
+
 # TODO: Remove this once the we used the automated role assignment by "--assign-service-principal-role"
 az role assignment create \
   --assignee "${component_to_client_id[ingress]}"\
@@ -77,46 +109,62 @@ cat <<EOF >"${SHARED_DIR}"/hypershift_azure_mi_file.json
     },
     "cloudProvider": {
         "clientID": "${component_to_client_id[cloud-provider]}",
-        "certificateName": "${component_to_cert_name[cloud-provider]}"
+        "certificateName": "${component_to_cert_name[cloud-provider]}",
+        "credentialsSecretName": "${component_to_cert_name[cloud-provider]}-json",
+        "objectEncoding": "utf-8"
     },
     "nodePoolManagement": {
         "clientID": "${component_to_client_id[capz]}",
-        "certificateName": "${component_to_cert_name[capz]}"
+        "certificateName": "${component_to_cert_name[capz]}",
+        "credentialsSecretName": "${component_to_cert_name[capz]}-json",
+        "objectEncoding": "utf-8"
     },
     "controlPlaneOperator": {
         "clientID": "${component_to_client_id[cpo]}",
-        "certificateName": "${component_to_cert_name[cpo]}"
+        "certificateName": "${component_to_cert_name[cpo]}",
+        "credentialsSecretName": "${component_to_cert_name[cpo]}-json",
+        "objectEncoding": "utf-8"
     },
     "imageRegistry": {
         "clientID": "${component_to_client_id[ciro]}",
-        "certificateName": "${component_to_cert_name[ciro]}"
+        "certificateName": "${component_to_cert_name[ciro]}",
+        "credentialsSecretName": "${component_to_cert_name[ciro]}-json",
+        "objectEncoding": "utf-8"
     },
     "ingress": {
         "clientID": "${component_to_client_id[ingress]}",
-        "certificateName": "${component_to_cert_name[ingress]}"
+        "certificateName": "${component_to_cert_name[ingress]}",
+        "credentialsSecretName": "${component_to_cert_name[ingress]}-json",
+        "objectEncoding": "utf-8"
     },
     "network": {
         "clientID": "${component_to_client_id[cncc]}",
-        "certificateName": "${component_to_cert_name[cncc]}"
+        "certificateName": "${component_to_cert_name[cncc]}",
+        "credentialsSecretName": "${component_to_cert_name[cncc]}-json",
+        "objectEncoding": "utf-8"
     },
     "disk": {
         "clientID": "${component_to_client_id[azure-disk]}",
-        "certificateName": "${component_to_cert_name[azure-disk]}"
+        "certificateName": "${component_to_cert_name[azure-disk]}",
+        "credentialsSecretName": "${component_to_cert_name[azure-disk]}-json",
+        "objectEncoding": "utf-8"
     },
     "file": {
         "clientID": "${component_to_client_id[azure-file]}",
-        "certificateName": "${component_to_cert_name[azure-file]}"
+        "certificateName": "${component_to_cert_name[azure-file]}",
+        "credentialsSecretName": "${component_to_cert_name[azure-file]}-json",
+        "objectEncoding": "utf-8"
     }
 }
 EOF
 
 # Create and Save Managed Identities for the Data Plane Component
-AZURE_DISK_MI_NAME="${SP_NAME_PREFIX}-dp-mi-disk"
-AZURE_FILE_MI_NAME="${SP_NAME_PREFIX}-dp-mi-file"
-IMAGE_REGISTRY_MI_NAME="${SP_NAME_PREFIX}-dp-mi-image-registry"
-AZURE_DISK_CLIENT_ID=$(az identity create --name $AZURE_DISK_MI_NAME --resource-group $RG_NSG --query clientId -o tsv)
-AZURE_FILE_CLIENT_ID=$(az identity create --name $AZURE_FILE_MI_NAME --resource-group $RG_NSG --query clientId -o tsv)
-IMAGE_REGISTRY_CLIENT_ID=$(az identity create --name $IMAGE_REGISTRY_MI_NAME --resource-group $RG_NSG --query clientId -o tsv)
+AZURE_DISK_MI_NAME="${SP_NAME_PREFIX}-disk-dp-mi"
+AZURE_FILE_MI_NAME="${SP_NAME_PREFIX}-file-dp-mi"
+IMAGE_REGISTRY_MI_NAME="${SP_NAME_PREFIX}-image-registry-dp-mi"
+AZURE_DISK_CLIENT_ID=$(az identity create --name "$AZURE_DISK_MI_NAME" --resource-group "$RG_HC" --query clientId -o tsv)
+AZURE_FILE_CLIENT_ID=$(az identity create --name "$AZURE_FILE_MI_NAME" --resource-group "$RG_HC" --query clientId -o tsv)
+IMAGE_REGISTRY_CLIENT_ID=$(az identity create --name "$IMAGE_REGISTRY_MI_NAME" --resource-group "$RG_HC" --query clientId -o tsv)
 
 cat <<EOF >"${SHARED_DIR}"/hypershift_azure_dp_mi_file.json
 {
@@ -125,3 +173,35 @@ cat <<EOF >"${SHARED_DIR}"/hypershift_azure_dp_mi_file.json
   "fileMSIClientID": ${AZURE_FILE_CLIENT_ID}
 }
 EOF
+
+## create federated identities for workload identities
+OIDC_ISSUER_URL="$(cat /etc/hypershift-aro-oidc-issuer/hypershift-aro-oidc-issuer)"
+
+az identity federated-credential create --name "${AZURE_DISK_MI_NAME}"-fed-id \
+--identity-name "${AZURE_DISK_MI_NAME}" \
+--resource-group "$RG_HC" \
+--issuer "${OIDC_ISSUER_URL}" \
+--subject system:serviceaccount:openshift-cluster-csi-drivers:azure-disk-csi-driver-node-sa \
+--audience openshift
+
+az identity federated-credential create --name "${AZURE_FILE_MI_NAME}"-fed-id \
+--identity-name "${AZURE_FILE_MI_NAME}" \
+--resource-group "$RG_HC" \
+--issuer "${OIDC_ISSUER_URL}" \
+--subject system:serviceaccount:openshift-cluster-csi-drivers:azure-file-csi-driver-node-sa \
+--audience openshift
+
+az identity federated-credential create --name "${IMAGE_REGISTRY_MI_NAME}"-fed-id \
+--identity-name "${IMAGE_REGISTRY_MI_NAME}" \
+--resource-group "$RG_HC" \
+--issuer "${OIDC_ISSUER_URL}" \
+--subject system:serviceaccount:openshift-image-registry:cluster-image-registry-operator \
+--audience openshift
+
+az identity federated-credential create --name "${IMAGE_REGISTRY_MI_NAME}"-fed-id \
+--identity-name "${IMAGE_REGISTRY_MI_NAME}" \
+--resource-group "$RG_HC" \
+--issuer "${OIDC_ISSUER_URL}" \
+--subject system:serviceaccount:openshift-image-registry:registry \
+--audience openshift
+
