f52af45b3a136f0bcac5d794b905b889eed6de18
Jose Castillo Lema <josecastillolema@gmail.com>
2025-02-26 17:41:02 +0100

    Add validation wait
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/self-sched/openshift-qe-installer-bm-self-sched-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/self-sched/openshift-qe-installer-bm-self-sched-commands.sh
@@ -13,7 +13,8 @@ quads_pwd=$(cat "/secret/quads_pwd")
 TOKEN=$(curl -sSk -X POST -u "metal-perfscale-cpt@redhat.com:$quads_pwd" -H "Content-Type: application/json" $QUADS_INSTANCE/api/v3/login/ | jq -r .'auth_token')
 
 # Get available hosts for self scheduling
-HOST=$(curl -sSk $QUADS_INSTANCE/api/v3/available\?can_self_schedule\=true | jq -r '.[0]')
+HOST_OUTPUT=$(curl -sSk $QUADS_INSTANCE/api/v3/available\?can_self_schedule\=true)
+HOST=$(echo $HOST_OUTPUT | jq -r '.[0]')
 
 # Create self scheduling assignment
 CLOUD=$(curl -sSk -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d '{"description": "Temporary allocation from openshift-ci", "owner": "metal-perfscale-cpt", "qinq": 1, "wipe": "true"}' $QUADS_INSTANCE/api/v3/assignments/self | jq -r .'cloud.name')
@@ -21,5 +22,8 @@ CLOUD=$(curl -sSk -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: ap
 # Create schedule
 curl -k -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d "{\"cloud\": \"$CLOUD\", \"hostname\": \"$HOST\"}" $QUADS_INSTANCE/api/v3/schedules | jq .'assignment_id' > ${SHARED_DIR}/assignment_id
 
-# TODO: Wait for validation to be completed
-sleep 600
+# Wait for validation to be completed
+while [[ $(curl -sSk $QUADS_INSTANCE/api/v3/assignments/63 | jq -r .validated) != "true" ]]; do
+  echo "Waiting for validation"
+  sleep 15s
+done
