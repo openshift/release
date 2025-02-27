e204166c84fbb09dc0ac9854d8fd5cf9ad357ca4
Jose Castillo Lema <josecastillolema@gmail.com>
2025-02-27 14:27:55 +0100

    Better confidential logging
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/self-sched-terminate/openshift-qe-installer-bm-self-sched-terminate-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/self-sched-terminate/openshift-qe-installer-bm-self-sched-terminate-commands.sh
@@ -2,7 +2,6 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-set -x
 
 # Fix UID issue (from Telco QE Team)
 ~/fix_uid.sh
@@ -10,10 +9,9 @@ set -x
 quads_pwd=$(cat "/secret/quads_pwd")
 
 # Login to get token
-set +x
 TOKEN=$(curl -sSk -X POST -u "metal-perfscale-cpt@redhat.com:$quads_pwd" -H "Content-Type: application/json" $QUADS_INSTANCE/api/v3/login/ | jq -r .'auth_token')
-set -x
 
 ASSIGNMENT_ID=$(cat "${SHARED_DIR}/assignment_id")
 
-curl -k -X POST -H "Authorization: Bearer $TOKEN" $QUADS_INSTANCE/api/v3/assignments/terminate/$ASSIGNMENT_ID
+TERMINATION_OUTPUT=$(curl -k -X POST -H "Authorization: Bearer $TOKEN" $QUADS_INSTANCE/api/v3/assignments/terminate/$ASSIGNMENT_ID)
+echo $TERMINATION_OUTPUT
--- ci-operator/step-registry/openshift-qe/installer/bm/self-sched/openshift-qe-installer-bm-self-sched-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/self-sched/openshift-qe-installer-bm-self-sched-commands.sh
@@ -20,22 +20,33 @@ fi
 # Login to get token
 set +x
 TOKEN=$(curl -sSk -X POST -u "metal-perfscale-cpt@redhat.com:$quads_pwd" -H "Content-Type: application/json" $QUADS_INSTANCE/api/v3/login/ | jq -r .'auth_token')
-set -x
 
 # Get available hosts for self scheduling from a certain hardware model
+echo
+echo "Get available hosts for self scheduling from a certain hardware model ..."
 HOST_OUTPUT=$(curl -sSk $QUADS_INSTANCE/api/v3/available\?can_self_schedule\=true\&model=$MODEL)
+echo $HOST_OUTPUT
 HOST=$(echo $HOST_OUTPUT | jq -r '.[0]')
+echo $HOST
 
 # Create self scheduling assignment
+echo
+echo "Create self scheduling assignment ..."
 CLOUD_OUTPUT=$(curl -sSk -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d '{"description": "Temporary allocation from openshift-ci", "owner": "metal-perfscale-cpt", "qinq": 1, "wipe": "true"}' $QUADS_INSTANCE/api/v3/assignments/self)
+echo $CLOUD_OUTPUT
 CLOUD=$(echo $CLOUD_OUTPUT | jq -r .'cloud.name')
+echo $CLOUD
 
 # Create schedule
+echo
+echo "Create schedule ..."
 ASSIGNMENT_OUTPUT=$(curl -sSk -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d "{\"cloud\": \"$CLOUD\", \"hostname\": \"$HOST\"}" $QUADS_INSTANCE/api/v3/schedules)
+echo $ASSIGNMENT_OUTPUT
 echo $ASSIGNMENT_OUTPUT | jq .'assignment_id' > ${SHARED_DIR}/assignment_id
 
 # Wait for validation to be completed
+set -x
 while [[ $(curl -sSk $QUADS_INSTANCE/api/v3/assignments/63 | jq -r .validated) != "true" ]]; do
-  echo "Waiting for validation"
+  echo "Waiting for validation ..."
   sleep 60s
 done
