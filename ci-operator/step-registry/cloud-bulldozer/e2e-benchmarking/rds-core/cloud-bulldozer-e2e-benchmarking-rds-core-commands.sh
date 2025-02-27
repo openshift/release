2164e37a48b1eb14d5835a8cef5adea90594a5ee
Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.rmtusnc.csb>
2025-02-27 12:07:39 -0500

    Addressing PR comments
    Signed-off-by: Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.rmtusnc.csb>

--- ci-operator/step-registry/cloud-bulldozer/e2e-benchmarking/rds-core/cloud-bulldozer-e2e-benchmarking-rds-core-commands.sh
+++ ci-operator/step-registry/cloud-bulldozer/e2e-benchmarking/rds-core/cloud-bulldozer-e2e-benchmarking-rds-core-commands.sh
@@ -8,12 +8,9 @@ oc config view
 oc projects
 pushd /tmp
 
-
 git clone https://github.com/vishnuchalla/e2e-benchmarking --branch v0.0.1 --depth 1
 pushd e2e-benchmarking
 pushd workloads/kube-burner-ocp-wrapper
 export WORKLOAD=rds-core
-
 current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker=,node-role.kubernetes.io/infra!=,node-role.kubernetes.io/workload!= --output jsonpath="{.items[?(@.status.conditions[-1].type=='Ready')].status.conditions[-1].type}" | wc -w | xargs)
-
 ES_SERVER="" ITERATIONS=${current_worker_count} CHURN=false ./run.sh
