8a0c3f946e253764619201a0f6f5a2e0ab742888
liqcui <liqcui@redhat.com>
2025-03-03 01:48:58 -0500

    update code

--- ci-operator/step-registry/openshift-qe/sdn-ovn-migration/openshift-qe-sdn-ovn-migration-commands.sh
+++ ci-operator/step-registry/openshift-qe/sdn-ovn-migration/openshift-qe-sdn-ovn-migration-commands.sh
@@ -19,10 +19,8 @@ ES_USERNAME=$(cat "/secret/username")
 GSHEET_KEY_LOCATION="/ga-gsheet/gcp-sa-account"
 export GSHEET_KEY_LOCATION
 
-REPO_URL="https://github.com/cloud-bulldozer/e2e-benchmarking";
-LATEST_TAG=$(curl -s "https://api.github.com/repos/cloud-bulldozer/e2e-benchmarking/releases/latest" | jq -r '.tag_name');
-TAG_OPTION="--branch $(if [ "$E2E_VERSION" == "default" ]; then echo "$LATEST_TAG"; else echo "$E2E_VERSION"; fi)";
-git clone -b sdn-ovn-migration $REPO_URL $TAG_OPTION --depth 1
+REPO_URL="https://github.com/liqcui/e2e-benchmarking";
+git clone -b sdn-ovn-migration $REPO_URL --depth 1
 pushd e2e-benchmarking/workloads/sdn-ovn-migration
 export WORKLOAD=sdn-ovn-migration
 
