530a4fe14dddc7cfd4fabfd4179905a61d75a67c
Jianli Wei <jiwei@redhat.com>
2025-03-04 14:30:11 +0800

    WIP: Enable minimal permission for all applicable QE's tests

--- ci-operator/step-registry/gcp/deprovision/minimal-permission/gcp-deprovision-minimal-permission-commands.sh
+++ ci-operator/step-registry/gcp/deprovision/minimal-permission/gcp-deprovision-minimal-permission-commands.sh
@@ -4,6 +4,10 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+if [ ! -f "${SHARED_DIR}/gcp_min_permissions_sa_temporary_key_id" ]; then
+  echo "No 'gcp_min_permissions_sa_temporary_key_id' found, nothing to do." && exit 0
+fi
+
 GOOGLE_PROJECT_ID="$(< ${CLUSTER_PROFILE_DIR}/openshift_gcp_project)"
 export GCP_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/gce.json"
 sa_email=$(jq -r .client_email ${GCP_SHARED_CREDENTIALS_FILE})
@@ -14,6 +18,6 @@ then
 fi
 
 iam_account=$(jq -r .client_email ${SHARED_DIR}/gcp_min_permissions.json)
-key_id=$(jq -r .private_key_id ${SHARED_DIR}/gcp_min_permissions.json)
+key_id="$(< ${SHARED_DIR}/gcp_min_permissions_sa_temporary_key_id)"
 gcloud iam service-accounts keys delete -q "${key_id}" --iam-account="${iam_account}" || exit 1
 echo "$(date -u --rfc-3339=seconds) - Deleted the temporary key of the IAM service account which is used for the minimum permissions testing on GCP."
