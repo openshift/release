3efb132d9daba8e4f6279b117eb094fc96ca8a76
Punith Kenchappa <pkenchap@in.ibm.com>
2025-03-04 19:51:14 +0530

    MULTIARCH-5196: p-px fix failure in the CI (#62065)
    Signed-off-by: Punith Kenchappa <pkenchap@in.ibm.com>

--- ci-operator/step-registry/upi/install/powervs/cluster/upi-install-powervs-cluster-commands.sh
+++ ci-operator/step-registry/upi/install/powervs/cluster/upi-install-powervs-cluster-commands.sh
@@ -168,9 +168,9 @@ function cleanup_prior() {
     echo "Cleaning up prior runs for lease"
     # PowerVS Instances
     echo "Cleaning up target PowerVS workspace"
-    for CRN in $(ibmcloud pi workspace ls --json 2> /dev/null | jq -r --arg name "multi-arch-p-px-${POWERVS_REGION}-1" '.Payload.workspaces[] | select(.name == $name).details.crn')
+    for CRN in $(ibmcloud pi workspace ls --json 2> /dev/null | jq -r --arg name "multi-arch-p-px-${POWERVS_ZONE}-1" '.Payload.workspaces[] | select(.name == $name).details.crn')
     do
-        echo "Targetting power cloud instance"
+        echo "Targeting power cloud instance"
         ibmcloud pi workspace target "${CRN}"
 
         echo "Deleting the PVM Instances"
@@ -313,7 +313,7 @@ release_image_override    = "${TARGET_VERSION}"
 use_zone_info_for_names    = true
 use_ibm_cloud_services     = true
 ibm_cloud_vpc_name         = "${VPC_NAME}"
-private_network_mtu        = 9000
+private_network_mtu        = 1450
 ibm_cloud_vpc_subnet_name  = "sn01"
 ibm_cloud_resource_group   = "${RESOURCE_GROUP}"
 iaas_vpc_region            = "${VPC_REGION}"
