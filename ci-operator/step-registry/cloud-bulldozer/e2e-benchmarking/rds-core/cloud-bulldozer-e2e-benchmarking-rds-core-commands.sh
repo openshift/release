8373e6cfbdbf34859d960787995c8f4e856fcf36
Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.rmtusnc.csb>
2025-03-05 18:57:29 -0500

    Updating bastion cp interface for jetlag

--- ci-operator/step-registry/cloud-bulldozer/e2e-benchmarking/rds-core/cloud-bulldozer-e2e-benchmarking-rds-core-commands.sh
+++ ci-operator/step-registry/cloud-bulldozer/e2e-benchmarking/rds-core/cloud-bulldozer-e2e-benchmarking-rds-core-commands.sh
@@ -4,12 +4,39 @@ set -o nounset
 set -o pipefail
 set -x
 cat /etc/os-release
+
+if [ ${BAREMETAL} == "true" ]; then
+  SSH_ARGS="-i /bm/jh_priv_ssh_key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
+  bastion="$(cat /bm/address)"
+  # Copy over the kubeconfig
+  if [ ! -f "${SHARED_DIR}/kubeconfig" ]; then
+    ssh ${SSH_ARGS} root@$bastion "cat ${KUBECONFIG_PATH}" > /tmp/kubeconfig
+    export KUBECONFIG=/tmp/kubeconfig
+  else
+    export KUBECONFIG=${SHARED_DIR}/kubeconfig
+  fi
+  # Setup socks proxy
+  ssh ${SSH_ARGS} root@$bastion -fNT -D 12345
+  export https_proxy=socks5://localhost:12345
+  export http_proxy=socks5://localhost:12345
+  oc --kubeconfig="$KUBECONFIG" config set-cluster bm --proxy-url=socks5://localhost:12345
+fi
+
+python --version
+pushd /tmp
+python -m virtualenv ./venv_qe
+source ./venv_qe/bin/activate
+
 oc config view
 oc projects
-pushd /tmp
 
 git clone https://github.com/vishnuchalla/e2e-benchmarking --branch v0.0.1 --depth 1
 pushd e2e-benchmarking
 pushd workloads/kube-burner-ocp-wrapper
 export WORKLOAD=rds-core
 ES_SERVER="" ITERATIONS=1 PPROF=false CHURN=false ./run.sh
+
+if [ ${BAREMETAL} == "true" ]; then
+  # kill the ssh tunnel so the job completes
+  pkill ssh
+fi
--- ci-operator/step-registry/openshift-qe/installer/bm/day2/sriov/openshift-qe-installer-bm-day2-sriov-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/day2/sriov/openshift-qe-installer-bm-day2-sriov-commands.sh
@@ -9,13 +9,17 @@ if [ ${BAREMETAL} == "true" ]; then
   SSH_ARGS="-i /bm/jh_priv_ssh_key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
   bastion="$(cat /bm/address)"
   # Copy over the kubeconfig
-  ssh ${SSH_ARGS} root@$bastion "cat ${KUBECONFIG_PATH}" > /tmp/kubeconfig
+  if [ ! -f "${SHARED_DIR}/kubeconfig" ]; then
+    ssh ${SSH_ARGS} root@$bastion "cat ${KUBECONFIG_PATH}" > /tmp/kubeconfig
+    export KUBECONFIG=/tmp/kubeconfig
+  else
+    export KUBECONFIG=${SHARED_DIR}/kubeconfig
+  fi
   # Setup socks proxy
   ssh ${SSH_ARGS} root@$bastion -fNT -D 12345
-  export KUBECONFIG=/tmp/kubeconfig
   export https_proxy=socks5://localhost:12345
   export http_proxy=socks5://localhost:12345
-  oc --kubeconfig=/tmp/kubeconfig config set-cluster bm --proxy-url=socks5://localhost:12345
+  oc --kubeconfig="$KUBECONFIG" config set-cluster bm --proxy-url=socks5://localhost:12345
 fi
 
 oc config view
