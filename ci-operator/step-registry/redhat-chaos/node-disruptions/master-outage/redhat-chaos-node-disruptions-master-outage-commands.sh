0711ef5de07520528cd161ad304211a694af195c
Sahil Shah <sahshah@redhat.com>
2025-03-06 09:48:17 -0500

    hide output once command is run

--- ci-operator/step-registry/redhat-chaos/node-disruptions/master-outage/redhat-chaos-node-disruptions-master-outage-commands.sh
+++ ci-operator/step-registry/redhat-chaos/node-disruptions/master-outage/redhat-chaos-node-disruptions-master-outage-commands.sh
@@ -74,9 +74,9 @@ elif [ "$platform" = "VSphere" ]; then
     VSPHERE_VCENTER_WITHOUTDOT=$(echo "$VSPHERE_VCENTER" | sed 's/\./\\./g')
     jsonpath_username="{.data.${VSPHERE_VCENTER_WITHOUTDOT}\.username}"
     jsonpath_password="{.data.${VSPHERE_VCENTER_WITHOUTDOT}\.password}"
-    VSPHERE_USERNAME=$(oc get secret vsphere-creds -n kube-system -o jsonpath="$jsonpath_username")
+    VSPHERE_USERNAME=$(oc get secret vsphere-creds -n kube-system -o jsonpath="$jsonpath_username" 2>/dev/null)
     export VSPHERE_USERNAME
-    VSPHERE_PASSWORD=$(oc get secret vsphere-creds -n kube-system -o jsonpath="$jsonpath_password")
+    VSPHERE_PASSWORD=$(oc get secret vsphere-creds -n kube-system -o jsonpath="$jsonpath_password" 2>/dev/null)
     export VSPHERE_PASSWORD
 fi
 
