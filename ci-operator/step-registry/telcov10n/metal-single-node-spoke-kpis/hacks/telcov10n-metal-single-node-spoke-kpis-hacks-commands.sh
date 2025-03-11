fc9ead5dfaee7ddb760e8da79949fd3a2bef9534
Carlos Cardenosa <ccardeno@redhat.com>
2025-03-11 16:30:00 +0100

    Move Spoke DNS setup to the spoke job
    Signed-off-by: Carlos Cardenosa <ccardeno@redhat.com>

--- ci-operator/step-registry/telcov10n/metal-single-node-spoke-kpis/hacks/telcov10n-metal-single-node-spoke-kpis-hacks-commands.sh
+++ ci-operator/step-registry/telcov10n/metal-single-node-spoke-kpis/hacks/telcov10n-metal-single-node-spoke-kpis-hacks-commands.sh
@@ -7,6 +7,33 @@ set -o pipefail
 echo "************ telcov10n Fix user IDs in a container ************"
 [ -e "${HOME}/fix_uid.sh" ] && "${HOME}/fix_uid.sh" || echo "${HOME}/fix_uid.sh was not found" >&2
 
+function load_env {
+
+  #### Spoke cluster
+  SPOKE_CLUSTER_NAME="spoke-${OCP_SPOKE_VERSION//./-}"
+  # shellcheck disable=SC2089
+  BAREMETAL_SPOKE_CLUSTER_NIC_MAC="$(cat /var/run/telcov10n/helix72-telcoqe-eng-rdu2-dc-redhat-com/network_spoke_mac_address)"
+  # shellcheck disable=SC2089
+  BAREMETAL_SPOKE_IPv4="$(cat /var/run/telcov10n/helix72-telcoqe-eng-rdu2-dc-redhat-com/network_spoke_ipv4_address)"
+  # shellcheck disable=SC2089
+  BAREMETAL_SPOKE_IPv6="$(cat /var/run/telcov10n/helix72-telcoqe-eng-rdu2-dc-redhat-com/network_spoke_ipv6_address)"
+}
+
+function setup_aux_host_ssh_access {
+
+  echo "************ telcov10n Setup AUX_HOST SSH access ************"
+
+  SSHOPTS=(
+    -o 'ConnectTimeout=5'
+    -o 'StrictHostKeyChecking=no'
+    -o 'UserKnownHostsFile=/dev/null'
+    -o 'ServerAliveInterval=90'
+    -o LogLevel=ERROR
+    -i "${CLUSTER_PROFILE_DIR}/ssh-key"
+  )
+
+}
+
 function set_spoke_cluster_kubeconfig {
 
   echo "************ telcov10n Set Spoke kubeconfig ************"
@@ -66,6 +93,7 @@ function update_base_domain {
 
   echo "************ telcov10n update base domain ************"
 
+  local cluster_base_domain
   cluster_base_domain="$(cat /var/run/telcov10n/helix92-telcoqe-eng-rdu2-dc-redhat-com/cluster_domain_name)"
   set -x
   echo -n "${cluster_base_domain}" >| ${SHARED_DIR}/base_domain
@@ -76,10 +104,73 @@ function update_spoke_cluster_name {
 
   echo "************ telcov10n update spoke cluster name ************"
 
-  spoke_cluster_name="spoke-${OCP_SPOKE_VERSION//./-}"
+  local spoke_cluster_name="spoke-${OCP_SPOKE_VERSION//./-}"
   set -x
   echo -n "${spoke_cluster_name}" >| ${SHARED_DIR}/spoke_cluster_name
   set +x
+}
+
+function update_dns_domains {
+
+  local spoke_cluster_name="spoke-${OCP_SPOKE_VERSION//./-}"
+  local cluster_base_domain
+  cluster_base_domain="$(cat /var/run/telcov10n/helix92-telcoqe-eng-rdu2-dc-redhat-com/cluster_domain_name)"
+  local spoke_base_domain="${spoke_cluster_name}.${cluster_base_domain}"
+
+  echo "************ telcov10n update DNS domains for the Spoke cluster '${spoke_cluster_name}' ************"
+
+  echo
+  echo -n "Using ${spoke_base_domain} domain name..."
+  echo
+
+  local network_spoke_mac_address="${BAREMETAL_SPOKE_CLUSTER_NIC_MAC}"
+  local api_ipv4="${BAREMETAL_SPOKE_IPv4}"
+  local api_ipv6="${BAREMETAL_SPOKE_IPv6}"
+  local file_name="${network_spoke_mac_address//:/-}"
+  local remote_path="/etc/NetworkManager/dnsmasq.d/88-sno-spoke-with-mac_${file_name}.conf"
+  set -x
+  timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" bash -s --  \
+    "${remote_path}" "${network_spoke_mac_address}" "${spoke_base_domain}" "${api_ipv4:-}" "${api_ipv6:-}" << 'EOF'
+set -o nounset
+set -o errexit
+set -o pipefail
+
+dn_conf="${1}"
+mac="${2}"
+spoke_domain="${3}"
+ipv4="${4:-}"
+ipv6="${5:-}"
+
+set -x
+  sudo cat <<EO-dns-file >| ${dn_conf}
+#
+# OCP SPOKE cluster API
+#
+EO-dns-file
+
+if [[ -n ${ipv4} ]]; then
+  sudo cat <<EO-dns-file >> ${dn_conf}
+dhcp-host=${mac},${ipv4}
+address=/api.${spoke_domain}/${ipv4}
+address=/api-int.${spoke_domain}/${ipv4}
+address=/.apps.${spoke_domain}/${ipv4}
+EO-dns-file
+fi
+
+if [[ -n ${ipv6} ]]; then
+  sudo cat <<EO-dns-file >> ${dn_conf}
+# dhcp-host=id:00:03:00:01:${mac},[${ipv6}]
+# address=/api.${spoke_domain}/${ipv6}
+# address=/api-int.${spoke_domain}/${ipv6}
+# address=/.apps.${spoke_domain}/${ipv6}
+EO-dns-file
+fi
+
+sudo systemctl reload-or-restart NetworkManager
+EOF
+
+  set +x
+  echo
 }
 
 function use_shared_ssh_keys_from_vault {
@@ -108,10 +199,15 @@ function hack_spoke_deployment {
   update_host_and_master_yaml_files
   update_spoke_cluster_name
   update_base_domain
+  update_dns_domains
   # use_shared_ssh_keys_from_vault
 }
 
 function main {
+
+  load_env
+
+  setup_aux_host_ssh_access
   set_spoke_cluster_kubeconfig
   hack_spoke_deployment
 }
