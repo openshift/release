26f1977548714d7c370af739574ce1f8315a5eaa
zzhao <zzhao@redhat.com>
2025-03-11 23:40:49 -0600

    debug

--- ci-operator/step-registry/openshift-extended/test/openshift-extended-test-commands.sh
+++ ci-operator/step-registry/openshift-extended/test/openshift-extended-test-commands.sh
@@ -398,6 +398,7 @@ function run {
         --provider "${TEST_PROVIDER}" -o "${ARTIFACT_DIR}/extended.log" \
         --timeout "${TEST_TIMEOUT}m" --junit-dir="${ARTIFACT_DIR}/junit" -f ./case_selected || ret_value=$?
     fi
+    sleep 30m
     set +x
     set +e
     rm -fr ./case_selected
