12f8bf0ae771e3b3a5f3c1ac25363ef706fe607c
Sahil Shah <sahshah@redhat.com>
2025-03-12 12:03:27 -0400

    trying dev-qe vault for username and password, and changing vsp_ip

--- ci-operator/step-registry/redhat-chaos/node-disruptions/master-outage/redhat-chaos-node-disruptions-master-outage-commands.sh
+++ ci-operator/step-registry/redhat-chaos/node-disruptions/master-outage/redhat-chaos-node-disruptions-master-outage-commands.sh
@@ -67,18 +67,22 @@ elif [ "$platform" = "IBMCloud" ]; then
     export TIMEOUT=320
 elif [ "$platform" = "VSphere" ]; then
     export CLOUD_TYPE="vsphere"
-    MASTER_MACHINE=$(oc get machines -A | grep master | head -n 1 | awk '{print $2}')
-    VSPHERE_IP=$(oc get machine $MASTER_MACHINE -n openshift-machine-api -o jsonpath='{.status.addresses[?(@.type=="InternalIP")].address}' | awk '{print $1}')
+    # MASTER_MACHINE=$(oc get machines -A | grep master | head -n 1 | awk '{print $2}')
+    # VSPHERE_IP=$(oc get machine $MASTER_MACHINE -n openshift-machine-api -o jsonpath='{.status.addresses[?(@.type=="InternalIP")].address}' | awk '{print $1}')
+    # export VSPHERE_IP
+    VSPHERE_IP=$(oc get infrastructures.config.openshift.io cluster -o jsonpath='{.spec.platformSpec.vsphere.vcenters[0].server}')
     export VSPHERE_IP
-    VSPHERE_VCENTER=$(oc get infrastructures.config.openshift.io cluster -o jsonpath='{.spec.platformSpec.vsphere.vcenters[0].server}')
-    export VSPHERE_VCENTER
-    VSPHERE_VCENTER_WITHOUTDOT=$(echo "$VSPHERE_VCENTER" | sed 's/\./\\./g')
-    jsonpath_username="{.data.${VSPHERE_VCENTER_WITHOUTDOT}\.username}"
-    jsonpath_password="{.data.${VSPHERE_VCENTER_WITHOUTDOT}\.password}"
-    VSPHERE_USERNAME=$(oc get secret vsphere-creds -n kube-system -o jsonpath="$jsonpath_username")
-    export VSPHERE_USERNAME
-    VSPHERE_PASSWORD=$(oc get secret vsphere-creds -n kube-system -o jsonpath="$jsonpath_password")
-    export VSPHERE_PASSWORD
+    # VSPHERE_IP_WITHOUTDOT=$(echo "$VSPHERE_IP" | sed 's/\./\\./g')
+    # jsonpath_username="{.data.${VSPHERE_IP_WITHOUTDOT}\.username}"
+    # jsonpath_password="{.data.${VSPHERE_IP_WITHOUTDOT}\.password}"
+    # VSPHERE_USERNAME=$(oc get secret vsphere-creds -n kube-system -o jsonpath="$jsonpath_username")
+    # export VSPHERE_USERNAME
+    # VSPHERE_PASSWORD=$(oc get secret vsphere-creds -n kube-system -o jsonpath="$jsonpath_password")
+    # export VSPHERE_PASSWORD
+    vcenter_username_minimal_permission=$(cat "/var/run/vault/devqe-secrets/vcenter_username_minimal_permission")
+    export VSPHERE_USERNAME=$vcenter_username_minimal_permission
+    vcenter_password_minimal_permission=$(cat "/var/run/vault/devqe-secrets/vcenter_password_minimal_permission")
+    export VSPHERE_PASSWORD=$vcenter_password_minimal_permission
 fi
 
 ./node-disruptions/prow_run.sh
