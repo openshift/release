0a75862b6f275ba4477616c3dcfb8540b78c2ba6
Gregory Giguashvili <85498834+ggiguash@users.noreply.github.com>
2025-03-17 13:01:07 +0400

    Add missing jq installation in iso-build and bare-metal steps (#62863)

--- ci-operator/step-registry/hypershift/kubevirt/create/hypershift-kubevirt-create-commands.sh
+++ ci-operator/step-registry/hypershift/kubevirt/create/hypershift-kubevirt-create-commands.sh
@@ -205,7 +205,15 @@ else
     --control-plane-availability-policy "${CONTROL_PLANE_AVAILABILITY}" \
     --infra-availability-policy "${INFRA_AVAILABILITY}" \
     --service-cidr 172.32.0.0/16 \
-    --cluster-cidr 10.136.0.0/14
+    --cluster-cidr 10.136.0.0/14 \
+    --annotations hypershift.openshift.io/kubevirt-vm-jsonpatch='
+    [
+      {
+        "op": "add",
+        "path": "/spec/template/spec/domain/memory/maxGuest",
+        "value": "16Gi"
+      }
+    ]'
 fi
 
 
