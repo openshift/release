231d60bddf8c2d37f02bfc4f2eee3aaff8bd5c95
memodi <memodi@redhat.com>
2025-03-21 16:06:51 -0400

    pushd /tmp

--- ci-operator/step-registry/netobserv/perf-test/deploy/netobserv-perf-test-deploy-commands.sh
+++ ci-operator/step-registry/netobserv/perf-test/deploy/netobserv-perf-test-deploy-commands.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 set -x 
 
-# pushd /tmp
+pushd /tmp
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip  
 awscli-bundle/install -b ~/bin/aws
