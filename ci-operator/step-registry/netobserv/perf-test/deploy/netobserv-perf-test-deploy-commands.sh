0e6b40902cd04a04e07c279dbb2b251898714303
memodi <memodi@redhat.com>
2025-03-24 11:38:44 -0400

    Update Dockerfile and reduce number of nodes

--- ci-operator/step-registry/netobserv/perf-test/deploy/netobserv-perf-test-deploy-commands.sh
+++ ci-operator/step-registry/netobserv/perf-test/deploy/netobserv-perf-test-deploy-commands.sh
@@ -5,16 +5,16 @@ set -o nounset
 set -o pipefail
 set -x 
 
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip  
-awscli-bundle/install -b /bin/aws
-export PATH=$PATH:/bin
-mkdir -p $HOME/.aws
+# curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+# unzip awscli-bundle.zip  
+# awscli-bundle/install -b /bin/aws
+# export PATH=$PATH:/bin
+# mkdir -p $HOME/.aws
+aws help
 cat ${CLUSTER_PROFILE_DIR}/.awscred > $HOME/.aws/config
 export AWS_SHARED_CREDENTIALS_FILE=${CLUSTER_PROFILE_DIR}/.awscred
 aws_region=${REGION:-$LEASED_RESOURCE}
 export AWS_DEFAULT_REGION=$aws_region
-aws --help
 source scripts/netobserv.sh
 deploy_lokistack
 deploy_kafka
