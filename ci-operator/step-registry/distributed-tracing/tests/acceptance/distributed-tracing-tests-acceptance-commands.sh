cb8fd44bc23278b725a04c3b4ce55bc8916b7142
Ishwar Kanse <ikanse@redhat.com>
2025-03-26 18:28:20 +0530

    Update acceptance test job (#63205)

--- ci-operator/step-registry/distributed-tracing/tests/acceptance/distributed-tracing-tests-acceptance-commands.sh
+++ ci-operator/step-registry/distributed-tracing/tests/acceptance/distributed-tracing-tests-acceptance-commands.sh
@@ -16,6 +16,11 @@ fi
 #Copy the distributed-tracing-qe repo files to a writable directory.
 cp -R /tmp/distributed-tracing-qe /tmp/distributed-tracing-tests && cd /tmp/distributed-tracing-tests
 
+#Enable user workload monitoring
+oc apply -f tests/e2e-acceptance/otlp-metrics-traces/01-workload-monitoring.yaml
+#Tag OCP worker nodes required for targetallocator features test
+oc get nodes -l node-role.kubernetes.io/worker -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | xargs -I {} oc label nodes {} ingress-ready=true
+
 # Execute Distributed Tracing tests
 chainsaw test \
 --report-name "junit_distributed_tracing_tests_acceptance" \
