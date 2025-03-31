b9183a6dc1370af7cadf171ae5a8d287717454e5
vshakya <vshakya@vshakya-thinkpadt14gen4.remote.csb>
2025-03-31 10:40:38 -0400

    add ocp username env

--- ci-operator/step-registry/keycloak-qe/rhsso/tests/keycloak-qe-rhsso-tests-commands.sh
+++ ci-operator/step-registry/keycloak-qe/rhsso/tests/keycloak-qe-rhsso-tests-commands.sh
@@ -7,6 +7,12 @@ set -o pipefail
 # Set PATH for OC binary
 export PATH=$PATH:/tmp/tests/ansible-tests/
 
+CONSOLE_URL=$(cat $SHARED_DIR/console.url)
+OCP_API_URL="https://api.${CONSOLE_URL#"https://console-openshift-console.apps."}:6443"
+OCP_CRED_USR="kubeadmin"
+OCP_CRED_PSW="$(cat ${SHARED_DIR}/kubeadmin-password)"
+oc login ${OCP_API_URL} --username=${OCP_CRED_USR} --password=${OCP_CRED_PSW} --insecure-skip-tls-verify=true
+
 # Configure env for test run
 cp $KUBECONFIG /tmp/kubeconfig
 export KUBECONFIG=/tmp/kubeconfig
