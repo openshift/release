c157befb58bf8594570509d34cba8f5aa05137d7
Biagio Manzari <bmanzari@redhat.com>
2025-04-01 18:49:55 +0200

    Deploy Compact OVE cluster on Bare Metal

---
+++ ci-operator/step-registry/baremetal/lab/agent/ove/install/baremetal-lab-agent-ove-install-commands.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+set -o errtrace
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Trap to kill children processes
+trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM ERR
+# Save exit code for must-gather to generate junit
+trap 'echo "$?" > "${SHARED_DIR}/install-status.txt"' TERM ERR
+
+[ -z "${AUX_HOST}" ] && { echo "\$AUX_HOST is not filled. Failing."; exit 1; }
+[ -z "${architecture}" ] && { echo "\$architecture is not filled. Failing."; exit 1; }
+[ -z "${workers}" ] && { echo "\$workers is not filled. Failing."; exit 1; }
+[ -z "${masters}" ] && { echo "\$masters is not filled. Failing."; exit 1; }
+
+if [ -f "${SHARED_DIR}/proxy-conf.sh" ] ; then
+    source "${SHARED_DIR}/proxy-conf.sh"
+fi
+
+function mount_virtual_media() {
+  local host="${1}"
+  local iso_path="${2}"
+  echo "Mounting the ISO image in #${host} via virtual media..."
+  timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" mount.vmedia "${host}" "${iso_path}"
+  local ret=$?
+  if [ $ret -ne 0 ]; then
+    echo "Failed to mount the ISO image in #${host} via virtual media."
+    touch /tmp/virtual_media_mount_failure
+    return 1
+  fi
+  return 0
+}
+
+SSHOPTS=(-o 'ConnectTimeout=5'
+  -o 'StrictHostKeyChecking=no'
+  -o 'UserKnownHostsFile=/dev/null'
+  -o 'ServerAliveInterval=90'
+  -o LogLevel=ERROR
+  -i "${CLUSTER_PROFILE_DIR}/ssh-key")
+
+yq -r e -o=j -I=0 ".[0].host" "${SHARED_DIR}/hosts.yaml" >"${SHARED_DIR}"/host-id.txt
+BASE_DOMAIN=$(<"${CLUSTER_PROFILE_DIR}/base_domain")
+PULL_SECRET_PATH=${CLUSTER_PROFILE_DIR}/pull-secret
+INSTALL_DIR="${INSTALL_DIR:-/tmp/installer}"
+mkdir -p "${INSTALL_DIR}"
+
+# We change the payload image to the one in the mirror registry only when the mirroring happens.
+# For example, in the case of clusters using cluster-wide proxy, the mirroring is not required.
+# To avoid additional params in the workflows definition, we check the existence of the mirror patch file.
+if [ "${DISCONNECTED}" == "true" ] && [ -f "${SHARED_DIR}/install-config-mirror.yaml.patch" ]; then
+  OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE="$(<"${CLUSTER_PROFILE_DIR}/mirror_registry_url")/${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE#*/}"
+fi
+
+# Patching the cluster_name again as the one set in the ipi-conf ref is using the ${UNIQUE_HASH} variable, and
+# we might exceed the maximum length for some entity names we define
+# (e.g., hostname, NFV-related interface names, etc...)
+CLUSTER_NAME=$(<"${SHARED_DIR}/cluster_name")
+
+gnu_arch=$(echo "$architecture" | sed 's/arm64/aarch64/;s/amd64/x86_64/;')
+
+if [ "${FIPS_ENABLED:-false}" = "true" ]; then
+    export OPENSHIFT_INSTALL_SKIP_HOSTCRYPT_VALIDATION=true
+fi
+
+case "${BOOT_MODE}" in
+"iso")
+  echo -e "\nMounting the OVE image in the hosts via virtual media and powering on the hosts..."
+  # shellcheck disable=SC2154
+  for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
+    # shellcheck disable=SC1090
+    . <(echo "$bmhost" | yq e 'to_entries | .[] | (.key + "=\"" + .value + "\"")')
+    if [[ "${name}" == *-a-* ]] && [ "${ADDITIONAL_WORKERS_DAY2}" == "true" ]; then
+      # Do not mount image to additional workers if we need to run them as day2 (e.g., to test single-arch clusters based
+      # on a single-arch payload migrated to a multi-arch cluster)
+      continue
+    fi
+    if [ "${transfer_protocol_type}" == "cifs" ]; then
+      IP_ADDRESS="$(dig +short "${AUX_HOST}")"
+      iso_path="${IP_ADDRESS}/isos/ocp-abi-ove.iso"
+    else
+      # Assuming HTTP or HTTPS
+      iso_path="${transfer_protocol_type:-http}://${AUX_HOST}/ocp-abi-ove.iso"
+    fi
+    mount_virtual_media "${host}" "${iso_path}"
+  done
+
+  wait
+  if [ -f /tmp/virtual_media_mount_failed ]; then
+    echo "Failed to mount the ISO image in one or more hosts"
+    exit 1
+  fi
+;;
+*)
+  echo "Unknown install mode: ${BOOT_MODE}"
+  exit 1
+esac
+
+proxy="$(<"${CLUSTER_PROFILE_DIR}/proxy")"
+# shellcheck disable=SC2154
+for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
+  # shellcheck disable=SC1090
+  . <(echo "$bmhost" | yq e 'to_entries | .[] | (.key + "=\"" + .value + "\"")')
+  if [[ "${name}" == *-a-* ]] && [ "${ADDITIONAL_WORKERS_DAY2}" == "true" ]; then
+    # Do not power on the additional workers if we need to run them as day2 (e.g., to test single-arch clusters based
+    # on a single-arch payload migrated to a multi-arch cluster)
+    continue
+  fi
+  echo "Power on #${host} (${name})..."
+  timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" prepare_host_for_boot "${host}" "${BOOT_MODE}"
+done
+
+
