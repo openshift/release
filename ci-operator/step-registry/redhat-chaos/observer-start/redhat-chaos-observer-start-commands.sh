4b42ac5f961dd05f9f5f532ae9bbc9e166245d81
Paige Patton <prubenda@redhat.com>
2025-04-03 14:41:32 -0400

    adding get of observer status

---
+++ ci-operator/step-registry/redhat-chaos/observer-start/redhat-chaos-observer-start-commands.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o nounset
+
+oc create project $TEST_NAMESPACE
+
+oc label ns $TEST_NAMESPACE security.openshift.io/scc.podSecurityLabelSync=false pod-security.kubernetes.io/enforce=privileged pod-security.kubernetes.io/audit=privileged pod-security.kubernetes.io/warn=privileged --overwrite
+
+
+oc apply -f- <<EOF
+kind: Pod
+apiVersion: v1
+metadata:
+  name: $POD_NAME
+  creationTimestamp: 
+  labels:
+    name: pause-amd64
+spec:
+  containers:
+  - name: pause-amd64
+    image: docker.io/ocpqe/hello-pod
+    securityContext:
+      capabilities: {}
+      privileged: true
+  restartPolicy: Always
+  dnsPolicy: ClusterFirst
+EOF
