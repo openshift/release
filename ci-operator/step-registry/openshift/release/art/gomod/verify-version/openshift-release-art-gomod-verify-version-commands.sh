a89d926293a14855ab7591ef61e9b7bbd877537a
Ashwin Das <ashwindasr@gmail.com>
2025-04-03 18:32:45 -0400

    test bash

--- ci-operator/step-registry/openshift/release/art/gomod/verify-version/openshift-release-art-gomod-verify-version-commands.sh
+++ ci-operator/step-registry/openshift/release/art/gomod/verify-version/openshift-release-art-gomod-verify-version-commands.sh
@@ -1,5 +1,12 @@
-#!/usr/bin/env python3
+#!/bin/bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
+pip3 install yaml --user
+
+python3 -c '
 import os
 import yaml
 import requests
@@ -52,3 +59,5 @@ for gomod_path in gomod_paths:
         print("No issues found")
     else:
         print(f"go mod file not found at path {gomod_path.absolute()}")
+
+'
