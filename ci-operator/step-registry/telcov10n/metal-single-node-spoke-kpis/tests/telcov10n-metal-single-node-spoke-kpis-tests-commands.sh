222af944eba1ee41c05748d07ed5ddb5dd04d976
Carlos Cardenosa <ccardeno@redhat.com>
2025-04-07 10:26:56 +0200

    Add checks after Spoke installation and its Telco operators is completed
    Signed-off-by: Carlos Cardenosa <ccardeno@redhat.com>

--- ci-operator/step-registry/telcov10n/metal-single-node-spoke-kpis/tests/telcov10n-metal-single-node-spoke-kpis-tests-commands.sh
+++ ci-operator/step-registry/telcov10n/metal-single-node-spoke-kpis/tests/telcov10n-metal-single-node-spoke-kpis-tests-commands.sh
@@ -15,20 +15,139 @@ function set_spoke_cluster_kubeconfig {
 
   SPOKE_CLUSTER_NAME=${NAMESPACE}
   secret_kubeconfig=${SPOKE_CLUSTER_NAME}-admin-kubeconfig
-  secret_adm_pass=${SPOKE_CLUSTER_NAME}-admin-password
+  # secret_adm_pass=${SPOKE_CLUSTER_NAME}-admin-password
 
   export KUBECONFIG="${SHARED_DIR}/spoke-${secret_kubeconfig}.yaml"
 
-  if [ -n "${SOCKS5_PROXY}" ]; then
-    _curl="curl -x ${SOCKS5_PROXY}"
-  else
-    _curl="curl"
-  fi
+  # if [ -n "${SOCKS5_PROXY}" ]; then
+  #   _curl="curl -x ${SOCKS5_PROXY}"
+  # else
+  #   _curl="curl"
+  # fi
 }
 
-function test_spoke_cluster_repo {
+##############################################################################################################
+# Test results
+##############################################################################################################
 
-  echo "************ telcov10n Clone and verify spoke_cluster repo ************"
+function run_pytest {
+  test_name=$1
+  test_results_xml_output=${ARTIFACT_DIR}/junit_${test_name}-test-results.xml
+  pytest ${PYTEST_VERBOSITY} ${tc_file} --junitxml=${test_results_xml_output}
+}
+
+function test_deployment_and_services {
+
+  echo "************ telcov10n-spoke Generate Test results ************"
+
+  cat <<EOF-INIT > /tmp/pytest.ini
+[pytest]
+junit_suite_name = telco-verification
+EOF-INIT
+
+cat <<EOF-CONFTEST > /tmp/conftest.py
+import pytest
+
+def pytest_collection_modifyitems(items):
+    for item in items:
+        parts = item.nodeid.split("::")
+        if len(parts) > 1:
+            test_name = ' '.join(parts[-1].split('_')[1:])
+            file_path = parts[0]
+            item._nodeid = f"{file_path}::[sig-telco-verification] {test_name.capitalize()}"
+EOF-CONFTEST
+
+  tc_file="/tmp/${JOB_NAME_SAFE}.py"
+  cat << EOF-PYTEST >| ${tc_file}
+import os
+import time
+import requests
+import pytest
+
+def test_spoke_cluster_operators_are_ready(bash):
+    count = 0
+    attempts = 10
+    while attempts > 0:
+      oc_cmd = f"oc get co --no-headers | grep -v 'True .* False .* False' | wc -l"
+      if bash.run_script_inline([oc_cmd]) == '0':
+        break
+      attempts -= 1
+      time.sleep(60)
+    assert attempts > 0, f"Not all cluster operators are ready yet"
+
+def test_ptp_operator_has_been_deployed(bash):
+    count = 0
+    attempts = 10
+    while attempts > 0:
+      oc_cmd = f"oc -n openshift-ptp get po --no-headers | grep 'ptp-operator.*Running'"
+      if bash.run_script_inline([oc_cmd]) != '':
+        break
+      attempts -= 1
+      time.sleep(60)
+    assert attempts > 0, f"PTP operator is not ready yet"
+
+def test_ptp_linuxdaemon_pod_is_running(bash):
+    count = 0
+    attempts = 10
+    while attempts > 0:
+      oc_cmd = f"oc -n openshift-ptp get po --no-headers | grep 'linuxptp-daemon.*Running'"
+      if bash.run_script_inline([oc_cmd]) != '':
+        break
+      attempts -= 1
+      time.sleep(60)
+    assert attempts > 0, f"PTP operator is not ready yet"
+
+def test_sriov_operator_has_been_deployed(bash):
+    count = 0
+    attempts = 10
+    while attempts > 0:
+      oc_cmd = f"oc -n openshift-sriov-network-operator get po --no-headers | grep 'sriov-network-operator.*Running'"
+      if bash.run_script_inline([oc_cmd]) != '':
+        break
+      attempts -= 1
+      time.sleep(60)
+    assert attempts > 0, f"SRIOV operator is not ready"
+
+def test_sriovnetwork_cr_has_been_deployed(bash):
+    count = 0
+    attempts = 10
+    while attempts > 0:
+      oc_cmd = f"oc -n openshift-sriov-network-operator get SriovNetwork --no-headers | wc -l"
+      if bash.run_script_inline([oc_cmd]) != '0':
+        break
+      attempts -= 1
+      time.sleep(60)
+    assert attempts > 0, f"SriovNetwork CR is not ready"
+
+def test_performace_profile_exists(bash):
+    count = 0
+    attempts = 10
+    while attempts > 0:
+      oc_cmd = f"oc get performanceprofile --no-headers | wc -l"
+      if bash.run_script_inline([oc_cmd]) != '0':
+        break
+      attempts -= 1
+      time.sleep(60)
+    assert attempts > 0, f"Performace Profile CR does not exist"
+
+def test_spoke_cluster_deployed_successfully(bash):
+    assert True
+EOF-PYTEST
+
+  echo
+  echo -----------------
+  echo $tc_file
+  echo -----------------
+  cat $tc_file
+  echo -----------------
+  echo
+  run_pytest check_spoke_installation
+  echo
+}
+
+function test_run_a_pod_in_the_spoke_cluster {
+
+  echo "************ telcov10n Run a POD into Spoke cluster ************"
 
   run_script=$(mktemp --dry-run)
 
@@ -55,16 +174,13 @@ function test_spoke_deployment {
   # The following lines are just a naive example that check web console and
   # run a script inside a POD in the Spoke cluster
 
-  spoke_cluster_url=$(oc whoami --show-console)
-
-  set -x
-  ${_curl} -vkI -u kubeadmin:${secret_adm_pass} ${spoke_cluster_url} || echo "Warning... the console is not present"
-  set +x
-
-  test_spoke_cluster_repo
+  test_deployment_and_services
+  test_run_a_pod_in_the_spoke_cluster
 
   echo
   echo "Success!!! spoke_cluster has been deployed correctly."
+  echo "-----------------------------------------------------"
+  oc get clusterversion
 }
 
 function main {
