4915709a485e08b83f1299c650c39a158fbf8a40
Martin Gencur <mgencur@redhat.com>
2025-04-07 17:27:02 +0200

    Remove kubevirt-health-check-nodecount

--- ci-operator/step-registry/cucushift/hypershift-extended/cilium/health-check/cucushift-hypershift-extended-cilium-health-check-commands.sh
+++ ci-operator/step-registry/cucushift/hypershift-extended/cilium/health-check/cucushift-hypershift-extended-cilium-health-check-commands.sh
@@ -19,11 +19,15 @@ if [[ -f "${SHARED_DIR}/nested_kubeconfig" ]]; then
 fi
 
 echo "Waiting for the guest cluster to be ready"
+# shellcheck disable=SC2016
+timeout 30m bash -c 'while [[ $(oc get nodes --no-headers | wc -l) == 0 ]]; do sleep 15; done'
 oc wait nodes --all --for=condition=Ready=true --timeout=15m
 oc wait clusteroperators --all --for=condition=Available=True --timeout=30m
 oc wait clusteroperators --all --for=condition=Progressing=False --timeout=30m
 oc wait clusteroperators --all --for=condition=Degraded=False --timeout=30m
 oc wait clusterversion/version --for=condition=Available=True --timeout=30m
+
+oc wait --for=condition=Ready pod -n cilium --all --timeout=5m
 
 echo "Performing Cilium connectivity tests"
 trap "dump_connectivity_test_namespace; cleanup_connectivity_test" EXIT
