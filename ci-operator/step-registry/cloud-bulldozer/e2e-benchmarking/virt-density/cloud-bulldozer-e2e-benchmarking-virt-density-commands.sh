9bd32dfa8cef8e619a9ff6cbbf30c846aec57ed4
Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.rmtusnc.csb>
2025-04-08 16:26:18 -0400

    Removing iterations
    Signed-off-by: Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.rmtusnc.csb>

--- ci-operator/step-registry/cloud-bulldozer/e2e-benchmarking/virt-density/cloud-bulldozer-e2e-benchmarking-virt-density-commands.sh
+++ ci-operator/step-registry/cloud-bulldozer/e2e-benchmarking/virt-density/cloud-bulldozer-e2e-benchmarking-virt-density-commands.sh
@@ -13,5 +13,4 @@ git clone https://github.com/vishnuchalla/e2e-benchmarking --branch v0.0.1 --dep
 pushd e2e-benchmarking
 pushd workloads/kube-burner-ocp-wrapper
 export WORKLOAD=virt-density
-current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/worker=,node-role.kubernetes.io/infra!=,node-role.kubernetes.io/workload!= --output jsonpath="{.items[?(@.status.conditions[-1].type=='Ready')].status.conditions[-1].type}" | wc -w | xargs)
-ES_SERVER="" ITERATIONS=${current_worker_count} PPROF=false ./run.sh
+ES_SERVER="" PPROF=false ./run.sh
