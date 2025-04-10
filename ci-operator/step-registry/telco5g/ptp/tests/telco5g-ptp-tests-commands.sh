dac845bac1410a2db75bcce114e7720c33ee343a
David Elie-Dit-Cosaque <86730676+edcdavid@users.noreply.github.com>
2025-04-10 14:58:56 -0500

    Only run dual follower tests for ocp versions > 4.19 (#63671)

--- ci-operator/step-registry/telco5g/ptp/tests/telco5g-ptp-tests-commands.sh
+++ ci-operator/step-registry/telco5g/ptp/tests/telco5g-ptp-tests-commands.sh
@@ -352,7 +352,17 @@ sleep 300
 # get RTC logs
 print_time
 
-TEST_MODES=("dualfollower" "dualnicbc" "bc" "oc")
+# Only run dual follower tests for versions < 4.19
+version=$(oc version -ojson| jq -r '.openshiftVersion')
+min_required="4.19"
+
+if [[ "$(printf '%s\n' "$version" "$min_required" | sort -V | head -n1)" != "$min_required" ]]; then
+  echo "Version is less than 4.19"
+  TEST_MODES=("dualnicbc" "bc" "oc")
+else
+  echo "Version is 4.19 or greater"
+  TEST_MODES=("dualfollower" "dualnicbc" "bc" "oc")
+fi
 
 # Run tests
 for mode in "${TEST_MODES[@]}"; do
