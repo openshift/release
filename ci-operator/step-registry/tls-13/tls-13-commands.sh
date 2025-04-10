519813bd056a643982a37053781798d10fc52a79
Jacob See <5027680+jacobsee@users.noreply.github.com>
2025-04-10 12:33:35 -0700

    Log in as kubeadmin before updating the apiserver config

--- ci-operator/step-registry/tls-13/tls-13-commands.sh
+++ ci-operator/step-registry/tls-13/tls-13-commands.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 export KUBECONFIG=${SHARED_DIR}/kubeconfig
 
+oc login -u kubeadmin "$(oc whoami --show-server=true)" < "$SHARED_DIR/kubeadmin-password"
+
 oc patch apiservers/cluster --type=merge -p '{"spec": {"tlsSecurityProfile":{"modern":{},"type":"Modern"}}}'
 
 oc adm wait-for-stable-cluster
