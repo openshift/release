32ed782ad95773c8fa197ecc3e682c5190354b63
rnetser <rnetser@redhat.com>
2025-04-11 12:23:46 +0300

    remove envs

--- ci-operator/step-registry/rhoai/opendatahub-tests/rhoai-opendatahub-tests-commands.sh
+++ ci-operator/step-registry/rhoai/opendatahub-tests/rhoai-opendatahub-tests-commands.sh
@@ -5,26 +5,26 @@ set -o errexit
 set -o pipefail
 set -o verbose
 
-SECRETS_DIR=/run/secrets/ci.openshift.io/cluster-profile
-AWS_SECRET_ACCESS_KEY=$(cat $SECRETS_DIR/aws-secret-access-key)
-AWS_ACCESS_KEY_ID=$(cat $SECRETS_DIR/aws-access-key-id)
-CI_S3_BUCKET_NAME=$(cat $SECRETS_DIR/ci-s3-bucket-name)
-CI_S3_BUCKET_REGION=$(cat $SECRETS_DIR/ci-s3-bucket-region)
-CI_S3_BUCKET_ENDPOINT=$(cat $SECRETS_DIR/ci-s3-bucket-endpoint)
-MODELS_S3_BUCKET_NAME=$(cat $SECRETS_DIR/models-s3-bucket-name)
-MODELS_S3_BUCKET_REGION=$(cat $SECRETS_DIR/models-s3-bucket-region)
-MODELS_S3_BUCKET_ENDPOINT=$(cat $SECRETS_DIR/models-s3-bucket-endpoint)
-
-
-export AWS_SECRET_ACCESS_KEY
-export AWS_ACCESS_KEY_ID
-export CI_S3_BUCKET_NAME
-export CI_S3_BUCKET_REGION
-export CI_S3_BUCKET_ENDPOINT
-export MODELS_S3_BUCKET_NAME
-export MODELS_S3_BUCKET_REGION
-export MODELS_S3_BUCKET_ENDPOINT
-export KUBECONFIG=${SHARED_DIR}/nested_kubeconfig
+#SECRETS_DIR=/run/secrets/ci.openshift.io/cluster-profile
+#AWS_SECRET_ACCESS_KEY=$(cat $SECRETS_DIR/aws-secret-access-key)
+#AWS_ACCESS_KEY_ID=$(cat $SECRETS_DIR/aws-access-key-id)
+#CI_S3_BUCKET_NAME=$(cat $SECRETS_DIR/ci-s3-bucket-name)
+#CI_S3_BUCKET_REGION=$(cat $SECRETS_DIR/ci-s3-bucket-region)
+#CI_S3_BUCKET_ENDPOINT=$(cat $SECRETS_DIR/ci-s3-bucket-endpoint)
+#MODELS_S3_BUCKET_NAME=$(cat $SECRETS_DIR/models-s3-bucket-name)
+#MODELS_S3_BUCKET_REGION=$(cat $SECRETS_DIR/models-s3-bucket-region)
+#MODELS_S3_BUCKET_ENDPOINT=$(cat $SECRETS_DIR/models-s3-bucket-endpoint)
+#
+#
+#export AWS_SECRET_ACCESS_KEY
+#export AWS_ACCESS_KEY_ID
+#export CI_S3_BUCKET_NAME
+#export CI_S3_BUCKET_REGION
+#export CI_S3_BUCKET_ENDPOINT
+#export MODELS_S3_BUCKET_NAME
+#export MODELS_S3_BUCKET_REGION
+#export MODELS_S3_BUCKET_ENDPOINT
+export KUBECONFIG=${SHARED_DIR}/kubeconfig
 
 RUN_COMMAND="uv run pytest tests/model_serving/model_server \
             -s -p no:logging \
