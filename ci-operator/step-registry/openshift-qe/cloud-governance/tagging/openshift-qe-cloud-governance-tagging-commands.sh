6b07eb9721a0492bd7afb11af69a5237638e3f6f
Thirumalesh Aaraveti <athiruma@redhat.com>
2025-04-14 22:02:23 +0530

    test cost policies

--- ci-operator/step-registry/openshift-qe/cloud-governance/tagging/openshift-qe-cloud-governance-tagging-commands.sh
+++ ci-operator/step-registry/openshift-qe/cloud-governance/tagging/openshift-qe-cloud-governance-tagging-commands.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
 # Export ES environment variables
-es_host="$(cat /secret/es_host)"
+# Export ES environment variables
+es_host="$(cat /secret/es-host)"
 export es_host
-es_port="$(cat /secret/es_port)"
+es_port="$(cat /secret/es-port)"
 export es_port
-perf_services_url="$(cat /secret/perf_services_url)"
-export perf_services_url
-account_group="$(cat /secret/${account_group})"
-s3_bucket=""
-export s3_bucket
+es_index="${POLICIES_GROUP}-$(cat /secret/cost-es-index)"
+export es_index
+cost_explorer_tags_arr=(
+  "PurchaseType" "ChargeType" "User" "Budget" "Project" "Manager"
+  "Owner" "LaunchTime" "Email" "Environment" "User:Spot" "cluster_id"
+)
+
+# Convert array to JSON-like string (double-quoted, comma-separated)
+cost_explorer_tags="$(printf '"%s",' "${cost_explorer_tags_arr[@]}" | sed 's/,$//')"
+cost_explorer_tags="[$cost_explorer_tags]"
+export cost_explorer_tags
+
+account_group="$(cat "/secret/${account_group}")"
+export account_group
+
 IFS=","
 
+
 function run_policy(){
-    regions=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "eu-north-1" "eu-west-3" "eu-west-2"
-             "eu-west-1" "ap-northeast-3" "ap-northeast-2" "ap-northeast-1" "ca-central-1" "sa-east-1"
-             "ap-southeast-1" "ap-southeast-2" "eu-central-1")
-    for region in "${regions[@]}"; do
-        AWS_DEFAULT_REGION="${region}"
-        export AWS_DEFAULT_REGION
-        if [[ -n "$s3_bucket" ]]; then
-          policy_output="s3://${s3_bucket}/${LOGS}/${AWS_DEFAULT_REGION}"
-          export policy_output
-        fi
-        python3 /usr/local/cloud_governance/main.py
-    done
+
+    python3 /usr/local/cloud_governance/main.py
 }
 
 function run_accounts(){
@@ -35,13 +37,11 @@ function run_accounts(){
     chmod 600 /tmp/creds.sh
     source /tmp/creds.sh
 
-    mandatory_tags=$(printf '{"Budget": "%s"}' "$account_name")
-    export mandatory_tags
-
-    echo "Running ${account_name}"
+    echo "Running the CloudGovernance CostExplorer Policies on:- ${account_name}"
     # Run policy function
     run_policy
-    echo "exiting ${account_name}\n\n"
+    echo "exiting ${account_name}"
+    echo 
 
   done
   
