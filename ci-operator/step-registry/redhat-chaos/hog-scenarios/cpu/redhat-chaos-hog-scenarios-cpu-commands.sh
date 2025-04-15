339d561c770bee5c83eec5a36e99d2c2c1160b34
Paige Patton <prubenda@redhat.com>
2025-04-15 07:45:56 -0400

    adding if statement for cp of events

--- ci-operator/step-registry/redhat-chaos/hog-scenarios/cpu/redhat-chaos-hog-scenarios-cpu-commands.sh
+++ ci-operator/step-registry/redhat-chaos/hog-scenarios/cpu/redhat-chaos-hog-scenarios-cpu-commands.sh
@@ -26,6 +26,8 @@ export TELEMETRY_PASSWORD=$telemetry_password
 ./cpu-hog/prow_run.sh
 rc=$?
 
-cp /tmp/events.json ${ARTIFACT_DIR}/events.json
+if [[ $TELEMETRY_EVENTS_BACKUP == "True" ]]; then
+    cp /tmp/events.json ${ARTIFACT_DIR}/events.json
+fi
 echo "Finished running memory hog scenario"
 echo "Return code: $rc"
