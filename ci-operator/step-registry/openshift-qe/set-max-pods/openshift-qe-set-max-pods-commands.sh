86ca1a4db65c6b0dc0bce81cc1963cd7a1e1c9cf
mehabhalodiya <mehabhalodiya@gmail.com>
2025-04-15 15:38:10 +0530

    add matchLabels in kubeletConfig
    Signed-off-by: mehabhalodiya <mehabhalodiya@gmail.com>

--- ci-operator/step-registry/openshift-qe/set-max-pods/openshift-qe-set-max-pods-commands.sh
+++ ci-operator/step-registry/openshift-qe/set-max-pods/openshift-qe-set-max-pods-commands.sh
@@ -5,42 +5,26 @@ set -o nounset
 set -o errexit
 set -o pipesfail
 
-MAX_PODS=500 # Set the desired maxPods value
-
-CLUSTER_NETWORK_CIDR=${CLUSTER_NETWORK_CIDR:-10.128.0.0/20}
-CLUSTER_NETWORK_HOST_PREFIX=${CLUSTER_NETWORK_HOST_PREFIX:-23}
-
-echo "default is to update cidr to 20 w/ hostPrefix of 23 so that max nodes num is 8"
-echo "\nsee below for actual values used\n--------------------------------\n"
-
-cat >> "${SHARED_DIR}/install-config.yaml" << EOF
-networking:
-  networkType: OVNKubernetes
-  clusterNetwork:
-  - cidr: $CLUSTER_NETWORK_CIDR
-    hostPrefix: $CLUSTER_NETWORK_HOST_PREFIX
-EOF
-
-cat "${SHARED_DIR}/install-config.yaml"
-
-# Patch Nodes
-NODES=$(oc get nodes -o jsonpath='{.items[*].metadata.name}')
-
-for NODE in $NODES; do
-  oc patch node "$NODE" --type=merge -p "{\"spec\":{\"podCIDRConfig\":{\"maxPods\": $MAX_PODS}}}"
-done
-
 # Create KubeletConfig
 cat <<EOF | oc apply -f -
 apiVersion: machineconfiguration.openshift.io/v1
 kind: KubeletConfig
 metadata:
-  name: increased-max-pods
+  name: "set-max-pods"
 spec:
+  machineConfigPoolSelector:
+    matchLabels:
+      pools.operator.machineconfiguration.openshift.io/worker: ""
   kubeletConfig:
-    maxPods: $MAX_PODS
+    maxPods: 2550
 EOF
 
-# Apply KubeletConfig to MachineConfigPool
-MCP_NAME=$(oc get machineconfigpools -o jsonpath='{.items[0].metadata.name}')
-oc patch machineconfigpool "$MCP_NAME" --type=merge -p '{"spec":{"configuration":{"kubeletConfig":{"name":"increased-max-pods"}}}}'
+MCP_NAME=$(oc get machineconfigpools -o jsonpath='{.items[?(@.metadata.labels."pools.operator.machineconfiguration.openshift.io/role" == "worker")].metadata.name}')
+if [[ -z "$MCP_NAME" ]]; then
+  echo "Error: Could not find a MachineConfigPool with the 'worker' role."
+  exit 1
+fi
+
+oc adm wait-for-stable-cluster
+
+echo "Successfully configured maxPods on worker nodes."
