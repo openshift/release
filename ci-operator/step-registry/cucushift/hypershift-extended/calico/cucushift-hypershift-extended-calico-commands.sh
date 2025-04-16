55609644431663285c7873d057cea005d9e066ca
Martin Gencur <mgencur@redhat.com>
2025-04-16 07:12:10 +0200

    CNTRLPLANE-705 Build CI to cover Calico CNI for public HCP on AWS
    Exclude the following tests:
    * Test name: The default cluster RBAC policy should have correct RBAC rules
      Reason: The test fails as it finds unexpected RBACs from Calico.
    * Test name: Cluster scoped load balancer healthcheck port and path should be 10256/healthz
      Reason: Reported https://issues.redhat.com/browse/CNTRLPLANE-788. It's a valid bug in HCP
    * Test name: Prometheus [apigroup:image.openshift.io] when installed on the cluster should provide named network metrics
      Reason: Reported https://github.com/projectcalico/calico/issues/10351 against Calico (seem to be breaking the spec)
    * Test name: Unidling*
      Reason: Feature not implemented in Calico.
    * Test name: pod should not start for sysctls not on whitelist [apigroup:k8s.cni.cncf.io] net.ipv4.conf.IFNAME.arp_filter
      Reason: Calico doesn’t validate sysctl conf against the "allowlist.conf". It has its own way.
    * Test name: pod should not start for sysctls not on whitelist [apigroup:k8s.cni.cncf.io] net.ipv4.conf.all.send_redirects
      Reason: Calico doesn’t validate sysctl conf against the "allowlist.conf". It has its own way.
    
    More details in https://docs.google.com/document/d/19lYcivp3eRcQQjhDssZnY89nKM_RG84TyVGCeIk5keA/edit?tab=t.0

---
+++ ci-operator/step-registry/cucushift/hypershift-extended/calico/cucushift-hypershift-extended-calico-commands.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+if [ -f "${SHARED_DIR}/proxy-conf.sh" ] ; then
+  source "${SHARED_DIR}/proxy-conf.sh"
+fi
+
+export KUBECONFIG="${SHARED_DIR}/kubeconfig"
+if [[ -f "${SHARED_DIR}/nested_kubeconfig" ]]; then
+  export KUBECONFIG="${SHARED_DIR}/nested_kubeconfig"
+fi
+
+# Install AWS credentials for the tigera-operator. It creates additional
+# inbound rules for the existing Security Group to allow traffic between its components.
+platform=$(oc get infrastructure cluster -ojsonpath='{.status.platformStatus.type}')
+if [[ "$platform" == "AWS" ]]; then
+  export AWS_SHARED_CREDENTIALS_FILE="/etc/hypershift-ci-jobs-awscreds/credentials"
+  if [[ ${HYPERSHIFT_GUEST_INFRA_OCP_ACCOUNT:-} == "true" ]]; then
+    export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
+  fi
+  aws configure export-credentials --format env > /tmp/aws_creds
+  set +x
+  source /tmp/aws_creds
+  key=$(echo -n "$AWS_ACCESS_KEY_ID" | base64 --wrap=0)
+  pass=$(echo -n "$AWS_SECRET_ACCESS_KEY" | base64 --wrap=0)
+  oc apply -f - <<EOF
+apiVersion: v1
+data:
+  aws_access_key_id: ${key}
+  aws_secret_access_key: ${pass}
+kind: Secret
+metadata:
+  name: aws-creds
+  namespace: kube-system
+type: Opaque
+EOF
+  set -x
+fi
+
+calico_dir=/tmp/calico
+mkdir $calico_dir
+
+wget -qO- "https://github.com/projectcalico/calico/releases/download/v${CALICO_VERSION}/ocp.tgz" | \
+  tar xvz --strip-components=1 -C $calico_dir
+
+# Create namespaces
+find $calico_dir -name "00*" -print0 | xargs -0 -n1 oc apply -f
+# Install operator
+find $calico_dir -name "02*" -print0 | xargs -0 -n1 oc apply -f
+
+timeout 15m bash -c 'until oc get crd installations.operator.tigera.io; do sleep 15; done'
+oc wait --for condition=established --timeout=60s crd installations.operator.tigera.io
+
+timeout 15m bash -c 'until oc get crd apiservers.operator.tigera.io; do sleep 15; done'
+oc wait --for condition=established --timeout=60s crd apiservers.operator.tigera.io
+
+# Install API Server
+oc apply -f "${calico_dir}/01-cr-apiserver.yaml"
+
+# Install Calico with specific setting for node address auto-detection.
+# The specific setting is required as some tests create NetworkAttachmentDefinitions
+# which add network interfaces to the host. Calico then incorrectly chooses this interface
+# and breaks connectivity between nodes. By choosing NodeInternalIP, the address picked by
+# Calico remains same throughout the lifecycle.
+oc apply -f - <<EOF
+apiVersion: operator.tigera.io/v1
+kind: Installation
+metadata:
+  name: default
+spec:
+  variant: Calico
+  calicoNetwork:
+    nodeAddressAutodetectionV4:
+      kubernetes: NodeInternalIP
+EOF
