54d4035c00e6ab58c98080c8f409ad3aeee0f00b
Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.rmtusnc.csb>
2025-04-16 10:57:48 -0400

    Removed unnecessary blocks
    Signed-off-by: Vishnu Challa <vchalla@vchalla-thinkpadp1gen2.rmtusnc.csb>

--- ci-operator/step-registry/cloud-bulldozer/e2e-benchmarking/virt-density/cloud-bulldozer-e2e-benchmarking-virt-density-commands.sh
+++ ci-operator/step-registry/cloud-bulldozer/e2e-benchmarking/virt-density/cloud-bulldozer-e2e-benchmarking-virt-density-commands.sh
@@ -9,22 +9,6 @@ oc projects
 pushd /tmp
 
 if [[ "$JOB_TYPE" == "presubmit" ]] && [[ "$REPO_OWNER" = "cloud-bulldozer" ]] && [[ "$REPO_NAME" = "e2e-benchmarking" ]]; then
-    if [ ${BAREMETAL} == "true" ]; then
-      SSH_ARGS="-i /secret/jh_priv_ssh_key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
-      bastion="$(cat /secret/address)"
-      # Copy over the kubeconfig
-      if [ ! -f "${SHARED_DIR}/kubeconfig" ]; then
-        ssh ${SSH_ARGS} root@$bastion "cat ${KUBECONFIG_PATH}" > /tmp/kubeconfig
-        export KUBECONFIG=/tmp/kubeconfig
-      else
-        export KUBECONFIG=${SHARED_DIR}/kubeconfig
-      fi
-      # Setup socks proxy
-      ssh ${SSH_ARGS} root@$bastion -fNT -D 12345
-      export https_proxy=socks5://localhost:12345
-      export http_proxy=socks5://localhost:12345
-      oc --kubeconfig="$KUBECONFIG" config set-cluster bm --proxy-url=socks5://localhost:12345
-    fi
     git clone https://github.com/${REPO_OWNER}/${REPO_NAME}
     pushd ${REPO_NAME}
     git config --global user.email "ocp-perfscale@redhat.com"
@@ -35,11 +19,6 @@ if [[ "$JOB_TYPE" == "presubmit" ]] && [[ "$REPO_OWNER" = "cloud-bulldozer" ]] &
     export WORKLOAD=virt-density
     export EXTRA_FLAGS="--vms-per-node=5 --vmi-ready-threshold=1m"
     ES_SERVER="" PPROF=false ./run.sh
-
-    if [ ${BAREMETAL} == "true" ]; then
-      # kill the ssh tunnel so the job completes
-      pkill ssh
-    fi
 else
     echo "We are sorry, this job is only meant for cloud-bulldozer/e2e-benchmarking repo PR testing"
 fi
