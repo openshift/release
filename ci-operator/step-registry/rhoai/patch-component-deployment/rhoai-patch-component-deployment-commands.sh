b895edb6312fe8ef9e1ef25dbaf887b006bd1f4d
rnetser <rnetser@redhat.com>
2025-04-16 14:28:29 +0300

    add logs

--- ci-operator/step-registry/rhoai/patch-component-deployment/rhoai-patch-component-deployment-commands.sh
+++ ci-operator/step-registry/rhoai/patch-component-deployment/rhoai-patch-component-deployment-commands.sh
@@ -11,13 +11,18 @@ if [ -n "${ODH_MODEL_CONTROLLER_IMAGE}" ]; then
   echo "Updating odh-model-controller deployment image to ${ODH_MODEL_CONTROLLER_IMAGE}"
 
   echo "Scaling RHOAI operator to 0"
-  oc scale --replicas=0 deployment/rhods-operator -n redhat-ods-operator
+  CMD="oc scale --replicas=0 deployment/rhods-operator -n redhat-ods-operator"
+  echo "${CMD}"
+  ${CMD}
+  #oc scale --replicas=0 deployment/rhods-operator -n redhat-ods-operator
 
   echo "Updating odh-model-controller deployment image to ${ODH_MODEL_CONTROLLER_IMAGE}"
-  oc set image  -n ${namespace}  deployment/odh-model-controller  manager="${ODH_MODEL_CONTROLLER_IMAGE}"
+  CMD="oc set image  -n ${namespace}  deployment/odh-model-controller  manager=${ODH_MODEL_CONTROLLER_IMAGE}"
+  echo "${CMD}"
+  ${CMD}
+  #oc set image  -n ${namespace}  deployment/odh-model-controller  manager="${ODH_MODEL_CONTROLLER_IMAGE}"
 
   echo "Wait For Deployment Replica To Be Ready"
-
 
   # Verify all pods are running
   oc_wait_for_pods() {
