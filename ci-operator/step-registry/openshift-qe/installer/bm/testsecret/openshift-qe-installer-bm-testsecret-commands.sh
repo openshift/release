e356cf08351f45cbbb25bd0bce386b4cfd0b806d
Jose Castillo Lema <josecastillolema@gmail.com>
2025-04-22 14:41:17 +0200

    Tue Apr 22 02:41:16 PM CEST 2025
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/testsecret/openshift-qe-installer-bm-testsecret-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/testsecret/openshift-qe-installer-bm-testsecret-commands.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -eu
 
-[[ if $(cat ${CLUSTER_PROFILE_DIR}/cloud_name) == "cloud19" ]]; then
-    echo "cool!"
+if [[ $(cat ${CLUSTER_PROFILE_DIR}/cloud_name) == "cloud19" ]]; then
+    echo "jetlag"
+elif [[ $(cat ${CLUSTER_PROFILE_DIR}/cloud_name) == "cloud19" ]]; then
+    echo "cpt"
+else
+    echo "error"
 fi
+
+echo "Image: "
+echo $RELEASE_IMAGE_LATEST
