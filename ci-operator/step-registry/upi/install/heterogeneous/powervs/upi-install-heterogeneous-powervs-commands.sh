e5cfbc624a43fb297109d81990473c7277a1261e
Punith Kenchappa <pkenchap@in.ibm.com>
2025-04-22 17:39:37 +0530

    MULTIARCH-5196: x-px fix for vpc delete of p-px on the same resource group
    Signed-off-by: Punith Kenchappa <pkenchap@in.ibm.com>
    
    MULTIARCH-5196: x-px fix for vpc delete and refactor
    
    Signed-off-by: Punith Kenchappa <pkenchap@in.ibm.com>

--- ci-operator/step-registry/upi/install/heterogeneous/powervs/upi-install-heterogeneous-powervs-commands.sh
+++ ci-operator/step-registry/upi/install/heterogeneous/powervs/upi-install-heterogeneous-powervs-commands.sh
@@ -11,9 +11,6 @@ trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wa
 IBMCLOUD_HOME=/tmp/ibmcloud
 export IBMCLOUD_HOME
 
-IBMCLOUD_HOME_FOLDER=/tmp/ibmcloud
-export IBMCLOUD_HOME_FOLDER
-
 REGION="${LEASED_RESOURCE}"
 export REGION
 
@@ -50,18 +47,18 @@ function login_ibmcloud() {
 
 # Download automation code
 function download_automation_code() {
-    mkdir -p ${IBMCLOUD_HOME_FOLDER}
+    mkdir -p ${IBMCLOUD_HOME}
     echo "Downloading the head for ocp4-upi-compute-powervs"
-    cd "${IBMCLOUD_HOME_FOLDER}" \
-        && curl -L https://github.com/IBM/ocp4-upi-compute-powervs/archive/refs/heads/release-"${OCP_VERSION}"-per.tar.gz -o "${IBMCLOUD_HOME_FOLDER}"/ocp-"${OCP_VERSION}".tar.gz \
-        && tar -xzf "${IBMCLOUD_HOME_FOLDER}"/ocp-"${OCP_VERSION}".tar.gz \
-        && mv "${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs-release-${OCP_VERSION}-per" "${IBMCLOUD_HOME_FOLDER}"/ocp4-upi-compute-powervs
+    cd "${IBMCLOUD_HOME}" \
+        && curl -L https://github.com/IBM/ocp4-upi-compute-powervs/archive/refs/heads/release-"${OCP_VERSION}"-per.tar.gz -o "${IBMCLOUD_HOME}"/ocp-"${OCP_VERSION}".tar.gz \
+        && tar -xzf "${IBMCLOUD_HOME}"/ocp-"${OCP_VERSION}".tar.gz \
+        && mv "${IBMCLOUD_HOME}/ocp4-upi-compute-powervs-release-${OCP_VERSION}-per" "${IBMCLOUD_HOME}"/ocp4-upi-compute-powervs
     echo "Down ... Downloading the head for ocp4-upi-compute-powervs"
 }
 
 # Downloads the terraform binary and puts into the path
 function download_terraform_binary() {
-    mkdir -p "${IBMCLOUD_HOME_FOLDER}/ocp-install-dir"
+    mkdir -p "${IBMCLOUD_HOME}/ocp-install-dir"
     echo "Attempting to install terraform using gzip"
     curl -L -o "${IBMCLOUD_HOME}"/ocp-install-dir/terraform.gz -L https://releases.hashicorp.com/terraform/"${TERRAFORM_VERSION}"/terraform_"${TERRAFORM_VERSION}"_linux_amd64.zip \
         && gunzip "${IBMCLOUD_HOME}"/ocp-install-dir/terraform.gz \
@@ -88,15 +85,6 @@ function cleanup_prior() {
             echo "GW VPC Connection not found. VPC Cleanup not needed."
         fi
         break
-    done
-
-    # Delete any vpc older than 24 hrs
-    echo "Cleaning up VPCs"
-    for VPC in $(ibmcloud is vpcs --resource-group-name "${RESOURCE_GROUP}" 2>&1 | grep "${RESOURCE_GROUP}" | grep -v Listing | grep -i ci-op | awk '{print $1}')
-    do
-        echo "VPC=${VPC}"
-        ibmcloud is vpc-delete "${VPC}" -f
-        sleep 10s
     done
 
     echo "Cleaning up workspaces for ${WORKSPACE_NAME}"
@@ -143,7 +131,7 @@ function configure_automation() {
 
     # create workspace for powervs from cli
     echo "Display all the variable values:"
-    POWERVS_REGION=$(bash "${IBMCLOUD_HOME_FOLDER}"/ocp4-upi-compute-powervs/scripts/region.sh "${REGION}")
+    POWERVS_REGION=$(bash "${IBMCLOUD_HOME}"/ocp4-upi-compute-powervs/scripts/region.sh "${REGION}")
     echo "VPC Region is ${REGION}"
     echo "PowerVS region is ${POWERVS_REGION}"
     echo "Resource Group is ${RESOURCE_GROUP}"
@@ -178,9 +166,9 @@ function configure_automation() {
 
     # Invoke create-var-file.sh to generate var.tfvars file
     echo "Creating the var file"
-    cd ${IBMCLOUD_HOME_FOLDER}/ocp4-upi-compute-powervs \
+    cd ${IBMCLOUD_HOME}/ocp4-upi-compute-powervs \
         && bash scripts/create-var-file.sh /tmp/ibmcloud/ocp4-upi-compute-powervs "${ADDITIONAL_WORKERS}" "p-px"
-    cp "${IBMCLOUD_HOME_FOLDER}"/ocp4-upi-compute-powervs/data/var.tfvars "${SHARED_DIR}"/var.tfvars
+    cp "${IBMCLOUD_HOME}"/ocp4-upi-compute-powervs/data/var.tfvars "${SHARED_DIR}"/var.tfvars
 
     #Create the VPC to fixed transit gateway Connection for the TG
     RESOURCE_GROUP_ID=$(ibmcloud resource groups --output json | jq -r '.[] | select(.name == "'${RESOURCE_GROUP}'").id')
@@ -199,11 +187,11 @@ function configure_automation() {
 # run_automation executes the terraform based on automation
 function run_automation() {
     echo "Running init"
-    "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir/terraform -chdir="${IBMCLOUD_HOME_FOLDER}"/ocp4-upi-compute-powervs/ init -upgrade -no-color
+    "${IBMCLOUD_HOME}"/ocp-install-dir/terraform -chdir="${IBMCLOUD_HOME}"/ocp4-upi-compute-powervs/ init -upgrade -no-color
     echo "Running plan"
-    "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir/terraform -chdir="${IBMCLOUD_HOME_FOLDER}"/ocp4-upi-compute-powervs/ plan -var-file=data/var.tfvars -no-color
+    "${IBMCLOUD_HOME}"/ocp-install-dir/terraform -chdir="${IBMCLOUD_HOME}"/ocp4-upi-compute-powervs/ plan -var-file=data/var.tfvars -no-color
     echo "Running apply"
-    "${IBMCLOUD_HOME_FOLDER}"/ocp-install-dir/terraform -chdir="${IBMCLOUD_HOME_FOLDER}"/ocp4-upi-compute-powervs/ apply -var-file=data/var.tfvars -auto-approve -no-color -state="${SHARED_DIR}"/terraform.tfstate
+    "${IBMCLOUD_HOME}"/ocp-install-dir/terraform -chdir="${IBMCLOUD_HOME}"/ocp4-upi-compute-powervs/ apply -var-file=data/var.tfvars -auto-approve -no-color -state="${SHARED_DIR}"/terraform.tfstate
     echo "Finished Running"
 }
 
