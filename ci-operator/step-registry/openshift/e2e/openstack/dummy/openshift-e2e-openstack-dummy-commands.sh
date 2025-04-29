ef6ee9f7f07cb6e3df8cb04a6d645b16508204e6
Miguel Carpio <mcarpio@redhat.com>
2025-04-29 13:03:36 +0200

    Add openshift-e2e-openstack-dummy workflow

---
+++ ci-operator/step-registry/openshift/e2e/openstack/dummy/openshift-e2e-openstack-dummy-commands.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+function info() {
+  printf '%s: INFO: %s\n' "$(date --utc +%Y-%m-%dT%H:%M:%SZ)" "$*"
+}
+
+# For disconnected or otherwise unreachable environments, we want to
+# have steps use an HTTP(S) proxy to reach the API server. This proxy
+# configuration file should export HTTP_PROXY, HTTPS_PROXY, and NO_PROXY
+# environment variables, as well as their lowercase equivalents (note
+# that libcurl doesn't recognize the uppercase variables).
+# if test -f "${SHARED_DIR}/proxy-conf.sh"
+# then
+#	# shellcheck disable=SC1090
+#	source "${SHARED_DIR}/proxy-conf.sh"
+# fi
+
+info "THE LEASED_RESOURCE IS: ${LEASED_RESOURCE}"
+
+if [ ! -z "${LEASED_RESOURCE}"]
+then 
+    CLUSTER_TYPE=${LEASED_RESOURCE}
+else
+    info "LEASED_RESOURCE IS MEPTY. THE CI_OCP_TESTING IS: `cat /var/run/cluster-secrets/${CLUSTER_TYPE}/CI_OCP_TESTING`"
+fi
+
+info "THE CLUSTER TYPE IS: ${CLUSTER_TYPE}"
