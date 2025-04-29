20904e362784cfd84c657abcdc88e516fb1ab0b0
Omri Bar-Haim <obarhaim@redhat.com>
2025-04-29 10:51:46 +0300

    Onboarding RHOAI Dashboard Cypress tests

---
+++ ci-operator/step-registry/rhoai/cypress-tests/rhoai-cypress-tests-commands.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o verbose
+
+export NVM_DIR="$HOME/.nvm" && . "$NVM_DIR/nvm.sh"
+cd odh-dashboard && npm install && npm run build
+
+npm run tests
