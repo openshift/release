b3eb4deb917808ae46722c71b41a20f7b746dfb1
Jinyun Ma <jima@redhat.com>
2025-04-29 16:02:56 +0800

    CORS-3989: Add private cluster coverage for Azure pre-submit jobs

--- ci-operator/step-registry/gather/azure-cli/gather-azure-cli-commands.sh
+++ ci-operator/step-registry/gather/azure-cli/gather-azure-cli-commands.sh
@@ -16,6 +16,16 @@ chmod ug+x /tmp/bin/jq
 curl -L --fail https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/linux/oc.tar.gz | tar xvzf - -C /tmp/bin/ oc
 chmod ug+x /tmp/bin/oc
 
+# For disconnected or otherwise unreachable environments, we want to
+# have steps use an HTTP(S) proxy to reach the API server. This proxy
+# configuration file should export HTTP_PROXY, HTTPS_PROXY, and NO_PROXY
+# environment variables, as well as their lowercase equivalents (note
+# that libcurl doesn't recognize the uppercase variables).
+if test -f "${SHARED_DIR}/proxy-conf.sh"
+then
+    # shellcheck disable=SC1090
+    source "${SHARED_DIR}/proxy-conf.sh"
+fi
 
 # set the parameters we'll need as env vars
 AZURE_AUTH_LOCATION="${CLUSTER_PROFILE_DIR}/osServicePrincipal.json"
