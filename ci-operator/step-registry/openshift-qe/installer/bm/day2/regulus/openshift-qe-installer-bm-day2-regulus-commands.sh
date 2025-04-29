3387d6b4e1a6fe55b01ac572efdb17dfb5e2a4f2
HughNhan <hnhan@redhat.com>
2025-04-29 15:30:40 -0400

    regulus - take 1: ping

---
+++ ci-operator/step-registry/openshift-qe/installer/bm/day2/regulus/openshift-qe-installer-bm-day2-regulus-commands.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+cat /etc/os-release
+
+if [ ${BAREMETAL} == "true" ]; then
+  SSH_ARGS="-i /bm/jh_priv_ssh_key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
+  bastion="$(cat /bm/address)"
+  # Copy over the kubeconfig
+  if [ ! -f "${SHARED_DIR}/kubeconfig" ]; then
+    ssh ${SSH_ARGS} root@$bastion "cat ${KUBECONFIG_PATH}" > /tmp/kubeconfig
+    export KUBECONFIG=/tmp/kubeconfig
+  else
+    export KUBECONFIG=${SHARED_DIR}/kubeconfig
+  fi
+  # Setup socks proxy
+  ssh ${SSH_ARGS} root@$bastion -fNT -D 12345
+  export https_proxy=socks5://localhost:12345
+  export http_proxy=socks5://localhost:12345
+  oc --kubeconfig="$KUBECONFIG" config set-cluster bm --proxy-url=socks5://localhost:12345
+fi
+
+oc config view
+oc projects
+
+# Install the Regulus 
+
+sleep 10
+
+# Regulus is ready
+
+
+if [ ${BAREMETAL} == "true" ]; then
+  # kill the ssh tunnel so the job completes
+  pkill ssh
+fi
+
