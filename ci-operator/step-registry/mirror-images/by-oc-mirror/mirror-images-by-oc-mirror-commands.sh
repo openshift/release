e6356c31fd553d7070089c46af468b705cdaf55f
Sebastian Soto <ssoto@redhat.com>
2025-05-05 11:00:01 -0400

    Optionally remove signatures when using oc-mirror
    Mirroring signatures is enabled by default starting on 4.19, so if
    the image does not have the signature the flow will fail

--- ci-operator/step-registry/mirror-images/by-oc-mirror/mirror-images-by-oc-mirror-commands.sh
+++ ci-operator/step-registry/mirror-images/by-oc-mirror/mirror-images-by-oc-mirror-commands.sh
@@ -41,6 +41,11 @@ fi
 
 echo "OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE: ${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE}"
 
+remove_signatures_flag=""
+if [[ "${REMOVE_SIGNATURES}" == "yes" ]]; then
+    remove_signatures_flag="--remove-signatures"
+fi
+
 # target release
 target_release_image="${MIRROR_REGISTRY_HOST}/${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE#*/}"
 target_release_image_repo="${target_release_image%:*}"
@@ -114,7 +119,7 @@ cp -rf "${new_pull_secret}" "${XDG_RUNTIME_DIR}/containers/auth.json"
 unset REGISTRY_AUTH_PREFERENCE
 
 # execute the oc-mirror command
-run_command "'${oc_mirror_bin}' -c ${image_set_config} docker://${target_release_image_repo} --dest-tls-verify=false --v2 --workspace file://${oc_mirror_dir}"
+run_command "'${oc_mirror_bin}' -c ${image_set_config} docker://${target_release_image_repo} --dest-tls-verify=false --v2 --workspace file://${oc_mirror_dir} ${remove_signatures_flag}"
 
 # Save output from oc-mirror
 result_folder="${oc_mirror_dir}/working-dir"
