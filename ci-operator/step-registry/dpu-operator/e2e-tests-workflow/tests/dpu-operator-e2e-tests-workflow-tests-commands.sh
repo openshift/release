4ef819419fba29a676bfde938a193c723b108b87
Thomas Haller <thaller@redhat.com>
2025-05-06 12:12:02 +0200

    dpu-operator: use TLS to talk with queue-manager
    Signed-off-by: Thomas Haller <thaller@redhat.com>

--- ci-operator/step-registry/dpu-operator/e2e-tests-workflow/tests/dpu-operator-e2e-tests-workflow-tests-commands.sh
+++ ci-operator/step-registry/dpu-operator/e2e-tests-workflow/tests/dpu-operator-e2e-tests-workflow-tests-commands.sh
@@ -2,13 +2,16 @@
 
 echoerr() { echo "$@" 1>&2; }
 
+_curl_qm() {
+    curl --resolve "$endpoint_resolve" --cacert "$queue_manager_tls_crt" "$@"
+}
+
 check_timeout_status() {
-        curl --resolve "${endpoint_resolve}" -X POST "$queue_url/check_timed_out_job" -H "Content-Type: application/json" -d "{\"pull_pull_sha\": \"${PULL_PULL_SHA}\"}"
+    _curl_qm -X POST "$queue_url/check_timed_out_job" -H "Content-Type: application/json" -d "{\"pull_pull_sha\": \"${PULL_PULL_SHA}\"}"
 }
 
 trigger_build() {
-	curl --resolve "${endpoint_resolve}" -X POST "$queue_url" -H "Content-Type: application/json" -d "{\"pullnumber\": \"${PULL_NUMBER}\", \"pull_pull_sha\": \"${PULL_PULL_SHA}\"}"
-
+    _curl_qm -X POST "$queue_url" -H "Content-Type: application/json" -d "{\"pullnumber\": \"${PULL_NUMBER}\", \"pull_pull_sha\": \"${PULL_PULL_SHA}\"}"
 }
 
 
@@ -33,23 +36,25 @@ handle_job_status() {
 
 # Main function to check the pull number and handle retries
 check_pull_number() {
-    check_pull_number_response=$(curl --resolve "${endpoint_resolve}" -X POST "${queue_url}/check_pullnumber" -H "Content-Type: application/json" -d "{\"uuid\": \"${1}\"}")
+    check_pull_number_response="$(_curl_qm -X POST "${queue_url}/check_pullnumber" -H "Content-Type: application/json" -d "{\"uuid\": \"${1}\"}")"
 
     handle_job_status "$check_pull_number_response"
 
     while true; do
-        check_pull_number_response=$(curl --resolve "${endpoint_resolve}" -X POST "${queue_url}/check_pullnumber" -H "Content-Type: application/json" -d "{\"uuid\": \"${1}\"}")
-        
+        check_pull_number_response="$(_curl_qm -X POST "${queue_url}/check_pullnumber" -H "Content-Type: application/json" -d "{\"uuid\": \"${1}\"}")"
+
         handle_job_status "$check_pull_number_response"
 
         sleep 100
     done
 }
 
-queue_url=$(cat "/var/run/token/e2e-test/queue-url")
-queue_endpoint=$(cat "/var/run/token/e2e-test/queue-endpoint")
-ip_address=$(cat "/var/run/token/e2e-test/ip-address")
-endpoint_resolve="${queue_endpoint}:80:${ip_address}"
+queue_manager_tls_host=$(cat "/var/run/token/e2e-test/queue-manager-tls-host")
+queue_manager_tls_ip=$(cat "/var/run/token/e2e-test/queue-manager-tls-ip")
+queue_manager_tls_crt="/var/run/token/jenkins-secrets/queue-manager-tls-crt"
+
+queue_url="https://$queue_manager_tls_host"
+endpoint_resolve="${queue_manager_tls_host}:443:${queue_manager_tls_ip}"
 
 #Check timeout wa
 timed_out_response=$(check_timeout_status)
