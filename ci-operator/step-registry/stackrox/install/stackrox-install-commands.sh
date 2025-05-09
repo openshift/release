3e342daba563b4793a45e30ff1d72995728ce3d8
davdhacs <105243888+davdhacs@users.noreply.github.com>
2025-05-09 12:34:59 -0600

    remove operator changes

--- ci-operator/step-registry/stackrox/install/stackrox-install-commands.sh
+++ ci-operator/step-registry/stackrox/install/stackrox-install-commands.sh
@@ -22,11 +22,6 @@ export KUBECONFIG=${KUBECONFIG:-${SHARED_DIR}/kubeconfig}
 echo "SHARED_DIR=${SHARED_DIR}"
 echo "KUBECONFIG=${KUBECONFIG}"
 
-export SCANNER_V4_MATCHER_READINESS=${SCANNER_V4_MATCHER_READINESS:-}
-SCANNER_V4_MATCHER_READINESS_MAX_WAIT=${SCANNER_V4_MATCHER_READINESS_MAX_WAIT:-60m}
-echo "ROX_SCANNER_V4:${ROX_SCANNER_V4:-}"
-echo "SCANNER_V4_MATCHER_READINESS:${SCANNER_V4_MATCHER_READINESS:-}"
-
 cr_url=https://raw.githubusercontent.com/stackrox/stackrox/master/operator/tests/common
 
 SCRATCH=$(mktemp -d)
@@ -65,7 +60,6 @@ apiVersion: platform.stackrox.io/v1alpha1
 kind: Central
 metadata:
   name: stackrox-central-services
-  namespace: stackrox
 spec:
   imagePullSecrets:
   - name: e2e-test-pull-secret
@@ -95,56 +89,6 @@ spec:
           cpu: 1
     telemetry:
       enabled: false
-EOF
-
-if [[ -n "${SCANNER_V4_MATCHER_READINESS:-}" ]]; then
-  cat <<EOF >> central-cr.yaml
-  customize:
-    envVars:
-      - name: SCANNER_V4_MATCHER_READINESS
-        value: ${SCANNER_V4_MATCHER_READINESS:-}
-EOF
-fi
-
-if [[ "${ROX_SCANNER_V4:-true}" == "true" ]]; then
-  cat <<EOF >> central-cr.yaml
-  scannerV4:
-    # Explicitly enable, scannerV4 is currenlty opt-in
-    scannerComponent: Enabled
-    indexer:
-      scaling:
-        autoScaling: Disabled
-        replicas: 1
-      resources:
-        requests:
-          cpu: "600m"
-          memory: "1500Mi"
-        limits:
-          cpu: "1000m"
-          memory: "2Gi"
-    matcher:
-      scaling:
-        autoScaling: Disabled
-        replicas: 1
-      resources:
-        requests:
-          cpu: "600m"
-          memory: "5Gi"
-        limits:
-          cpu: "1000m"
-          memory: "5500Mi"
-    db:
-      resources:
-        requests:
-          cpu: "200m"
-          memory: "2Gi"
-        limits:
-          cpu: "1000m"
-          memory: "2500Mi"
-EOF
-fi
-
-cat <<EOF >> central-cr.yaml
   scanner:
     analyzer:
       scaling:
@@ -165,15 +109,11 @@ cat <<EOF >> central-cr.yaml
         limits:
           cpu: 2000m
           memory: 4Gi
-EOF
-
-cat <<EOF >> central-cr.yaml
 ---
 apiVersion: v1
 kind: Secret
 metadata:
   name: admin-pass
-  namespace: stackrox
 data:
   password: ${centralAdminPasswordBase64}
 EOF
@@ -183,7 +123,6 @@ apiVersion: platform.stackrox.io/v1alpha1
 kind: SecuredCluster
 metadata:
   name: stackrox-secured-cluster-services
-  namespace: stackrox
 spec:
   clusterName: testing-cluster
   imagePullSecrets:
@@ -295,44 +234,13 @@ function wait_pods_running() {
     || { oc get pods "${@}"; return 1; }
 }
 
-function configure_scanner_readiness() {
-  echo '>>> Configure scanner-v4-matcher to reach ready status when vulnerability database is loaded.'
-  set +e  # ignore errors
-  kubectl wait --for condition=established --timeout=120s deploy/scanner-v4-matcher --namespace stackrox --timeout=120s || true
-  if kubectl describe -n stackrox deploy/scanner-v4-matcher \
-    | grep "SCANNER_V4_MATCHER_READINESS.*${SCANNER_V4_MATCHER_READINESS:-}"; then
-    echo 'scanner-v4-matcher readiness is set'
-    return
-  fi
-  echo 'The scanner-v4-matcher readiness env var is not set. Adding it to the deployment...'
-  kubectl -n stackrox set env deploy/scanner-v4-matcher "SCANNER_V4_MATCHER_READINESS=${SCANNER_V4_MATCHER_READINESS}"
-  echo 'Restarting scanner-v4-matcher to apply the new config during startup...'
-  kubectl rollout restart deploy/scanner-v4-matcher
-  kubectl -n stackrox rollout status deploy/scanner-v4-matcher --timeout=30s
-  kubectl -n stackrox describe deploy/scanner-v4-matcher | grep SCANNER_V4_MATCHER_READINESS \
-    || kubectl describe deploy scanner-v4-matcher --namespace stackrox
-  echo ">>> Finished scanner-v4-matcher configuration readiness=${SCANNER_V4_MATCHER_READINESS:-}."
-  set -e
-}
 
-
-echo '>>> Begin setup'
 install_operator
 wait_pods_running -A -lapp==rhacs-operator,control-plane=controller-manager
 wait_created crd centrals.platform.stackrox.io
 
 oc new-project stackrox >/dev/null || true
 create_cr central
-
-echo '>>> Wait for rhacs-operator to deploy Central based on the crd'
-retry oc get deploy -n stackrox central 2>/dev/null \
-  || oc logs -n openshift-operators deploy/rhacs-operator-controller-manager --tail=15
-
-if [[ "${ROX_SCANNER_V4:-true}" == "true" && -n "${SCANNER_V4_MATCHER_READINESS:-}" ]]; then
-  configure_scanner_readiness &
-  scanner_readiness_configure_pid=$!
-fi
-
 wait_deploy central
 
 get_init_bundle
@@ -346,52 +254,3 @@ wait_deploy scanner-db
 wait_deploy sensor
 wait_deploy admission-control
 oc get deployments -n stackrox
-
-function wait_for_connected_clusters() {
-  nohup oc port-forward --namespace "stackrox" svc/central "8443:443" 1>/dev/null 2>&1 &
-  echo $! > "${SCRATCH}/port_forward_pid"
-  
-  # Wait for secured cluster to be connect to central.
-  max_retry_verify_connected_cluster=30
-  for (( retry_count=1; retry_count <= max_retry_verify_connected_cluster; retry_count++ ));
-  do
-    echo "Verify connected cluster(s) (try ${retry_count}/${max_retry_verify_connected_cluster})"
-    connected_clusters_count=$(curl_central /v1/clusters | jq '.clusters | length')
-    if (( connected_clusters_count >= 1 )); then
-      echo "Found '${connected_clusters_count}' connected cluster(s)"
-      break
-    fi
-  
-    if (( retry_count == max_retry_verify_connected_cluster )); then
-      echo "Error: Waiting for sensor connection reached max retries"
-      exit 1
-    fi
-  
-    sleep 3
-  done
-  
-  # Cleanup nohup
-  kill -9 "$(cat "${SCRATCH}/port_forward_pid")"
-  rm "${SCRATCH}/port_forward_pid"
-}
-wait_for_connected_clusters
-
-if [[ "${ROX_SCANNER_V4:-true}" == "true" ]]; then
-  echo ">>> Wait for 'stackrox scanner-v4' deployments"
-  wait_deploy scanner-v4-db
-  wait_deploy scanner-v4-indexer
-  if [[ -n "${SCANNER_V4_MATCHER_READINESS:-}" ]]; then
-    echo '>>> Follow scanner-v4-matcher logs until ready state'
-    ps -p "${scanner_readiness_configure_pid}" \
-      && wait "${scanner_readiness_configure_pid}" || true
-    kubectl wait pods --for=condition=Ready --selector 'app=scanner-v4-matcher' -n stackrox \
-      --timeout="${SCANNER_V4_MATCHER_READINESS_MAX_WAIT}" \
-      || kubectl logs --tail=20 --selector 'app=scanner-v4-matcher' -n stackrox --timestamps
-    kubectl rollout status deploy/scanner-v4-matcher --timeout=0 -n stackrox
-  else
-    wait_deploy scanner-v4-matcher "${SCANNER_V4_MATCHER_READINESS_MAX_WAIT}"
-  fi
-fi
-
-kubectl get nodes -o wide
-kubectl get pods -o wide --namespace stackrox
