99a468a7a3758db60a611ec25b04e3d0cc388156
Penghao <pewang@redhat.com>
2025-05-13 14:38:37 +0800

    fix

--- ci-operator/step-registry/ipi/conf/vsphere/nested/ipi-conf-vsphere-nested-commands.py
+++ ci-operator/step-registry/ipi/conf/vsphere/nested/ipi-conf-vsphere-nested-commands.py
@@ -53,6 +53,7 @@ with open(os.path.join(shared_dir, "LEASE_single.json")) as f:
 
 os.environ["VCPUS"]                         = str(lease["spec"]["vcpus"])
 os.environ["MEMORY"]                        = str(lease["spec"]["memory"] * 1024)
+os.environ["DISKGB"]                        = str(4 * 1024)
 os.environ["GOVC_CLUSTER"]                  = os.path.basename(lease["status"]["topology"]["computeCluster"])
 os.environ["GOVC_DATACENTER"]               = lease["status"]["topology"]["datacenter"]
 os.environ["GOVC_DATASTORE"]                = os.path.basename(lease["status"]["topology"]["datastore"])
@@ -80,10 +81,13 @@ if os.environ["VCENTER_VERSION"] == "7":
 else:
     vcenter_version="VC8.0.2.00100-22617221-ESXi8.0u2c"
 
+# Create a copy of the current environment
+# env = os.environ.copy()
+
 try:
     r = ansible_runner.run_command(
         executable_cmd='ansible-playbook',
-        cmdline_args=['main.yml', '-i', 'hosts', '--extra-var', 'version=%s' %vcenter_version,'-vvvvvv', '-k'],
+        cmdline_args=['main.yml', '-i', 'hosts', '--extra-var', 'version=%s' %vcenter_version, '--extra-var', 'esxidisk=4096' '-vvvvvv', '-k'],
         input_fd=sys.stdin,
         output_fd=sys.stdout,
         error_fd=sys.stdout,
