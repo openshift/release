e3e9e25c60f289914793d55a5f48280d91551a85
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-05-15 15:25:59 +0200

    Revert "Update redhat-developer-rhdh-aks-mapt-create-commands.sh"
    This reverts commit 1562c7f4dfa19b90db4697a0c0f554d689fe947d.

--- ci-operator/step-registry/redhat-developer/rhdh/aks/mapt/create/redhat-developer-rhdh-aks-mapt-create-commands.sh
+++ ci-operator/step-registry/redhat-developer/rhdh/aks/mapt/create/redhat-developer-rhdh-aks-mapt-create-commands.sh
@@ -20,6 +20,6 @@ mapt azure aks create \
   --backed-url "azblob://${AZURE_STORAGE_BLOB}/aks-${CORRELATE_MAPT}" \
   --conn-details-output "${SHARED_DIR}" \
   --version 1.31 \
-  --vmsize "Standard_D4s_v3" \
+  --vmsize "Standard_D4as_v4" \
   --spot \
   --enable-app-routing
