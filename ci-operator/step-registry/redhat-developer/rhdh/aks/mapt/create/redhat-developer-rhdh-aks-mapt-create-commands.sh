f5203c28d109729e83033e3c477fe7251c920731
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-05-15 08:37:16 +0200

    Update redhat-developer-rhdh-aks-mapt-create-commands.sh

--- ci-operator/step-registry/redhat-developer/rhdh/aks/mapt/create/redhat-developer-rhdh-aks-mapt-create-commands.sh
+++ ci-operator/step-registry/redhat-developer/rhdh/aks/mapt/create/redhat-developer-rhdh-aks-mapt-create-commands.sh
@@ -20,6 +20,6 @@ mapt azure aks create \
   --backed-url "azblob://${AZURE_STORAGE_BLOB}/aks-${CORRELATE_MAPT}" \
   --conn-details-output "${SHARED_DIR}" \
   --version 1.31 \
-  --vmsize "Default" \
+  --vmsize "Standard_D2as_v5" \
   --spot \
   --enable-app-routing
