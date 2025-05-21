8354a520c38e826815e7dfbe1f62f637128c07fa
Carlos Cardenosa <ccardeno@redhat.com>
2025-05-21 11:31:06 +0200

    Wait until the Spoke cluster has been completely deployed
    Signed-off-by: Carlos Cardenosa <ccardeno@redhat.com>

--- ci-operator/step-registry/telcov10n/metal-single-node-spoke-kpis/tests/telcov10n-metal-single-node-spoke-kpis-tests-commands.sh
+++ ci-operator/step-registry/telcov10n/metal-single-node-spoke-kpis/tests/telcov10n-metal-single-node-spoke-kpis-tests-commands.sh
@@ -189,6 +189,15 @@ function test_spoke_deployment {
   echo "-----------------------------------------------------"
   oc get clusterversion
   echo
+
+  if [ -n "${PULL_NUMBER:-}" ]; then
+    echo
+    echo "------------------------ Spoke Details --------------------------------------------------------"
+    echo "kubeconfig: export KUBECONFIG=${SHARED_DIR}/spoke-${secret_kubeconfig}.yaml"
+    echo "Console: $(oc --kubeconfig ${SHARED_DIR}/spoke-${secret_kubeconfig}.yaml whoami --show-console)"
+    secret_adm_pass=${SPOKE_CLUSTER_NAME}-admin-password
+    cat ${SHARED_DIR}/spoke-${secret_adm_pass}.yaml || echo
+  fi
 }
 
 function copy_spoke_kubeconfig_to_bastion_location {
