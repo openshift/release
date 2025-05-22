1fa966ce061dfbeb923136860fbecc275c9f4b1b
Jian Zhang <jiazha@redhat.com>
2025-05-22 15:03:29 +0800

    use the test-private repo for test
    Signed-off-by: Jian Zhang <jiazha@redhat.com>

--- ci-operator/step-registry/olmv1/performance/olmv1-performance-commands.sh
+++ ci-operator/step-registry/olmv1/performance/olmv1-performance-commands.sh
@@ -9,10 +9,10 @@ set -x
 oc version
 oc get co
 oc get nodes
-python --version
+# python --version
 pushd /tmp
-python -m virtualenv ./venv_qe
-source ./venv_qe/bin/activate
+# python -m virtualenv ./venv_qe
+# source ./venv_qe/bin/activate
 
 # connected to ES server
 ES_SECRETS_PATH=${ES_SECRETS_PATH:-/secret}
@@ -23,13 +23,37 @@ if [ -e "${ES_SECRETS_PATH}/host" ]; then
     ES_HOST=$(cat "${ES_SECRETS_PATH}/host")
 fi
 
-# # download kube-burner-ocp
-# KUBE_BURNER_VERSION=1.6.8
+LOG_LEVEL=${LOG_LEVEL:-debug}
+KUBE_DIR=${KUBE_DIR:-/tmp}
+KUBE_BURNER_VERSION=${KUBE_BURNER_VERSION:-1.6.8}
+PERFORMANCE_PROFILE=${PERFORMANCE_PROFILE:-default}
+CHURN=${CHURN:-true}
+PPROF=${PPROF:-true}
+ARCHIVE=${ARCHIVE:-true}
+WORKLOAD=${WORKLOAD:-olm}
+QPS=${QPS:-20}
+BURST=${BURST:-20}
+GC=${GC:-true}
+EXTRA_FLAGS=${EXTRA_FLAGS:-}
+UUID=${UUID:-$(uuidgen)}
+ITERATIONS=${ITERATIONS:-30}
+AIRFLOW_CTX_DAG_ID=${AIRFLOW_CTX_DAG_ID:-}
+PROW_JOB_ID=${PROW_JOB_ID:-}
+BUILD_ID=${BUILD_ID:-}
+ADDITIONAL_PARAMS=${ADDITIONAL_PARAMS:-}
+ES_SERVER=${ES_SERVER:-"https://$ES_USERNAME:$ES_PASSWORD@$ES_HOST"}
+METRICS_ENDPOINT=${METRICS_ENDPOINT:-metrics-endpoint.yml}
 
-git clone https://github.com/kube-burner/kube-burner-ocp.git --branch main --depth 1
-pushd kube-burner-ocp
-make build
-./bin/amd64/kube-burner-ocp olm -h
+# copy olm-metrics.yml and extended-metrics.yml 
+cp /go/src/github.com/openshift/openshift-tests-private/test/extended/operators/benchmark/metrics/* ${KUBE_DIR}
+
+KUBE_BURNER_URL="https://github.com/kube-burner/kube-burner-ocp/releases/download/v${KUBE_BURNER_VERSION}/kube-burner-ocp-V${KUBE_BURNER_VERSION}-linux-x86_64.tar.gz"
+curl --fail --retry 8 --retry-all-errors -sS -L "${KUBE_BURNER_URL}" | tar -xzC "${KUBE_DIR}/" kube-burner-ocp
+
+# git clone https://github.com/kube-burner/kube-burner-ocp.git --branch main --depth 1
+# pushd kube-burner-ocp
+# make build
+# ./bin/amd64/kube-burner-ocp olm -h
 
 # REPO_URL="https://github.com/cloud-bulldozer/e2e-benchmarking";
 # LATEST_TAG=$(curl -s "https://api.github.com/repos/cloud-bulldozer/e2e-benchmarking/releases/latest" | jq -r '.tag_name');
@@ -38,26 +62,36 @@ make build
 # pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
 # export WORKLOAD=olm
 
-export ITERATIONS=30
-export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@$ES_HOST"
+#   echo "Indexing Management cluster stats"
+#   METADATA=$(cat << EOF
+# {
+# "uuid": "${UUID}",
+# "workload": "${WORKLOAD}",
+# "mgmtClusterName": "${MC_NAME}",
+# "hostedClusterName": "${HC_NAME}",
+# "timestamp": "$(date +%s%3N)"
+# }
+# EOF
+# )
 
 if [[ "${ENABLE_LOCAL_INDEX}" == "true" ]]; then
     EXTRA_FLAGS+=" --local-indexing"
 fi
 EXTRA_FLAGS+=" --gc-metrics=true --profile-type=${PROFILE_TYPE}"
 
-export EXTRA_FLAGS
-export ADDITIONAL_PARAMS
+cmd="${KUBE_DIR}/kube-burner-ocp ${WORKLOAD} --log-level=${LOG_LEVEL} --qps=${QPS} --burst=${BURST} --gc=${GC} --uuid ${UUID} --iterations=${ITERATIONS}"
+cmd+=" ${EXTRA_FLAGS}"
 
-export WORKLOAD=olm LOG_LEVEL=debug
-
-cmd="./bin/amd64/kube-burner-ocp ${WORKLOAD} --log-level=${LOG_LEVEL} --qps=${QPS} --burst=${BURST} --gc=${GC} --uuid ${UUID} --iterations=${ITERATIONS}"
-
-# If ES_SERVER is specified
+# # If ES_SERVER is specified
 if [[ -n ${ES_SERVER} ]]; then
-  curl -k -sS -X POST -H "Content-type: application/json" ${ES_SERVER}/ripsaw-kube-burner/_doc -d "${METADATA}" -o /dev/null
-  cmd+=" --es-server=${ES_SERVER} --es-index=ripsaw-kube-burner"
-fi
+  # curl -k -sS -X POST -H "Content-type: application/json" ${ES_SERVER}/ripsaw-kube-burner/_doc -d "${METADATA}" -o /dev/null
+  cmd+=" --es-server=${ES_SERVER} --es-index=ripsaw-kube-burner --metrics-profile=olm-metrics.yml,extended-metrics.yml"
+else
+  echo "ES_SERVER is not set. Skipping Elasticsearch integration. Use local Prometheus endpoints"
+  cmd+=" --metrics-endpoint=${METRICS_ENDPOINT}"
+fi 
+
+
 # If PERFORMANCE_PROFILE is specified
 if [[ -n ${PERFORMANCE_PROFILE} && ${WORKLOAD} =~ "rds-core" ]]; then
   cmd+=" --perf-profile=${PERFORMANCE_PROFILE}"
@@ -70,7 +104,6 @@ fi
 
 # Capture the exit code of the run, but don't exit the script if it fails.
 set +e
-
 echo $cmd
 JOB_START=${JOB_START:-$(date -u +"%Y-%m-%dT%H:%M:%SZ")};
 $cmd
@@ -82,19 +115,19 @@ else
   JOB_STATUS="failure"
 fi
 
-wget http://github.com/cloud-bulldozer/e2e-benchmarking/blob/master/utils/index.sh
-export JOB_START="$JOB_START" JOB_END="$JOB_END" JOB_STATUS="$JOB_STATUS" UUID="$UUID" WORKLOAD="$WORKLOAD" ES_SERVER="$ES_SERVER" 
-source index.sh || true
-
+# curl -L -o index.sh https://raw.githubusercontent.com/cloud-bulldozer/e2e-benchmarking/refs/heads/master/utils/index.sh
+# head -n 10 index.sh
+git clone https://github.com/cloud-bulldozer/e2e-benchmarking --branch master --depth 1
+export JOB_START JOB_END JOB_STATUS UUID WORKLOAD ES_SERVER AIRFLOW_CTX_DAG_ID PROW_JOB_ID BUILD_ID ADDITIONAL_PARAMS
+export ocp_virt_version="" ocp_virt_tuning_policy=""
+source e2e-benchmarking/utils/index.sh || true
 
 rm -f ${SHARED_DIR}/index.json
-
 folder_name=$(ls -t -d /tmp/*/ | head -1)
 jq ".iterations = $ITERATIONS" $folder_name/index_data.json >> ${SHARED_DIR}/index_data.json
 
 cp "${SHARED_DIR}"/index_data.json "${SHARED_DIR}"/${WORKLOAD}-index_data.json 
 cp "${SHARED_DIR}"/${WORKLOAD}-index_data.json  "${ARTIFACT_DIR}"/${WORKLOAD}-index_data.json
-
 
 if [[ "${ENABLE_LOCAL_INDEX}" == "true" ]]; then
     metrics_folder_name=$(find . -maxdepth 1 -type d -name 'collected-metric*' | head -n 1)
@@ -102,4 +135,3 @@ if [[ "${ENABLE_LOCAL_INDEX}" == "true" ]]; then
 fi
 
 echo "OLMv1 benchmark test finised"
-
