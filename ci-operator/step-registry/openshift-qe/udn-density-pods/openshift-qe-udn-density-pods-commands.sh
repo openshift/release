acce464274859b95067cf24e2c879cd2e6239201
Qiujie Li <qili@redhat.com>
2025-05-22 09:58:31 +0800

    add a 3 nodes test without rmp replacement

--- ci-operator/step-registry/openshift-qe/udn-density-pods/openshift-qe-udn-density-pods-commands.sh
+++ ci-operator/step-registry/openshift-qe/udn-density-pods/openshift-qe-udn-density-pods-commands.sh
@@ -78,5 +78,5 @@ if [ ${BAREMETAL} == "true" ]; then
   # kill the ssh tunnel so the job completes
   pkill ssh
 fi
-echo "sleep time to add the fixed rpms"
-sleep 7200
+# echo "sleep time to add the fixed rpms"
+#sleep 7200
