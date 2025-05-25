b038a0d440095b746985714cac5b517b3941015b
Fengshuang Tan <ftan@redhat.com>
2025-05-25 03:29:22 -0400

    update

--- ci-operator/step-registry/stackrox/qa-e2e/stackrox-qa-e2e-commands.sh
+++ ci-operator/step-registry/stackrox/qa-e2e/stackrox-qa-e2e-commands.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 job="${TEST_SUITE:-${JOB_NAME_SAFE#merge-}}"
 job="${job#nightly-}"
+git clone https://github.com/stackrox/stackrox.git
+cd stackrox
 exec .openshift-ci/dispatch.sh "${job}"
