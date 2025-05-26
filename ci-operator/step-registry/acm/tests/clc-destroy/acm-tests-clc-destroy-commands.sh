44799f7f7ca5320d5cfd09850bf3be55a58a0aa7
Fengshuang Tan <ftan@redhat.com>
2025-05-26 17:16:58 -0400

    change the code back for clc step commands

--- ci-operator/step-registry/acm/tests/clc-destroy/acm-tests-clc-destroy-commands.sh
+++ ci-operator/step-registry/acm/tests/clc-destroy/acm-tests-clc-destroy-commands.sh
@@ -35,8 +35,6 @@ CLOUD_PROVIDERS=$(cat $SECRETS_DIR/clc/ocp_cloud_providers)
 export CLOUD_PROVIDERS
 
 # run the test execution script
-#./execute_clc_interop_commands.sh || :
-# run the destroy script
-bash -x ./start-tests.sh
+./execute_clc_interop_commands.sh || :
 
 cp -r reports $ARTIFACT_DIR/
