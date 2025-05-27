39f1a8a1390901d6e7ead358d414cffdddab7d4d
Omri Bar-Haim <obarhaim@redhat.com>
2025-05-27 14:17:11 +0300

    remove label selectors checks from RHOAI DSC deployment

--- ci-operator/step-registry/rhoai/deploy-dsc/rhoai-deploy-dsc-commands.sh
+++ ci-operator/step-registry/rhoai/deploy-dsc/rhoai-deploy-dsc-commands.sh
@@ -19,35 +19,7 @@ file="/tmp/default-dsc.json"
 oc apply -f "${file}"
 
 echo "⏳ Wait for DataScientCluster to be deployed"
-oc wait --for=jsonpath='{.status.phase}'=Ready datasciencecluster/${DSC_NAME} --timeout=9000s 
-
-# Verify RHOAI operator installation
-timeout=400s
-
-namespace="openshift-operators"
-label_selectors=("control-plane=authorino-operator" "name=istio-operator")
-authorino_channel=$(oc get subscription authorino-operator -n $namespace -o=jsonpath='{.spec.channel}{"\n"}')
-if [[ "$authorino_channel" == "tech-preview-v1" ]]; then
-  label_selectors+=("authorino-component=authorino-webhooks")
-fi
-
-echo "Wait For Pods To Be Ready"
-for label_selector in "${label_selectors[@]}"; do
-  oc wait --for=condition=ready=true pod -l ${label_selector} -n ${namespace} --timeout=${timeout}
-done
-
-namespace="openshift-serverless"
-label_selectors=("name=knative-openshift" "name=knative-openshift-ingress" "name=knative-operator")
-for label_selector in "${label_selectors[@]}"; do
-  oc wait --for=condition=ready=true pod -l ${label_selector} -n ${namespace} --timeout=${timeout}
-done
-
-echo "Wait For Deployment Replica To Be Ready"
-namespace="redhat-ods-applications"
-label_selectors=("app=rhods-dashboard" "app=notebook-controller" "app.kubernetes.io/name=modelmesh-controller" "app.kubernetes.io/name=data-science-pipelines-operator" "control-plane=kserve-controller-manager" "app.kubernetes.io/part-of=model-registry-operator")
-for label_selector in "${label_selectors[@]}"; do
-  oc get deployment -l ${label_selector} -n ${namespace} -o json | jq -e '.status | .replicas == .readyReplicas'
-done
+oc wait --for=jsonpath='{.status.phase}'=Ready datasciencecluster/${DSC_NAME} --timeout=9000s
 
 # Verify all pods are running
 oc_wait_for_pods() {
