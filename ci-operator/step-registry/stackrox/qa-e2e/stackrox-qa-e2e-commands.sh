98dc93b227f70a05be1a45f547368e61c0b33039
Fengshuang Tan <tanfengshuang@163.com>
2025-05-28 16:37:12 +0800

    Update stackrox-qa-e2e-commands.sh

--- ci-operator/step-registry/stackrox/qa-e2e/stackrox-qa-e2e-commands.sh
+++ ci-operator/step-registry/stackrox/qa-e2e/stackrox-qa-e2e-commands.sh
@@ -7,7 +7,7 @@ if [ ! -f ".openshift-ci/dispatch.sh" ];then
   if [ ! -d "stackrox" ];then
     git clone https://github.com/stackrox/stackrox.git
   fi
-  cd stackrox
+  cd stackrox || exit
 fi
 
 exec .openshift-ci/dispatch.sh "${job}"
