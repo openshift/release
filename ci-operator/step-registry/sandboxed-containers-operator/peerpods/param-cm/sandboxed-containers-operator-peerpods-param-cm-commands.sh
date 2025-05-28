8a45415748ea648675193c8e61a08b05fcf3cc59
Wainer Moschetta <wainersm@redhat.com>
2025-05-28 15:58:44 -0300

    step-registry/sandboxed-containers-operator: refactor sandboxed-containers-operator-peerpods-param-cm step (#65352)
    * step-registry/sandboxed-containers-operator: simplify reading azure
    credentials
    
    In sandboxed-containers-operator-peerpods-param-cm step,
    removed from_azure_credentials() and read the properties like we do on
    upstream peer-pods jobs.
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>
    
    * step-registry/sandboxed-containers-operator: Retry first az command as login takes time to propagate
    
    In sandboxed-containers-operator-peerpods-param-cm step,
    backport of https://github.com/kata-containers/kata-containers/commit/0e4fb62bb4
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>
    
    * step-registry/sandboxed-containers-operator: refactor peerpods-param-cm step
    
    Refactor sandboxed-containers-operator-peerpods-param-cm step, so that
    we can more easily sync up with
    https://raw.githubusercontent.com/kata-containers/kata-containers/refs/heads/main/ci/openshift-ci/peer-pods-azure.sh
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>
    
    ---------
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- ci-operator/step-registry/sandboxed-containers-operator/peerpods/param-cm/sandboxed-containers-operator-peerpods-param-cm-commands.sh
+++ ci-operator/step-registry/sandboxed-containers-operator/peerpods/param-cm/sandboxed-containers-operator-peerpods-param-cm-commands.sh
@@ -5,14 +5,12 @@ if [ "$ENABLEPEERPODS" != "true" ]; then
     exit 0
 fi
 
+# Switch to a directory with rw permission
+cd /tmp || exit 1
+
 # Create the parameters configmap file in the shared directory so that others steps
 # can reference it.
 PP_CONFIGM_PATH="${SHARED_DIR:-$(pwd)}/peerpods-param-cm.yaml"
-
-from_azure_credentials() {
-        data="$1"
-        oc -n kube-system get secret azure-credentials -o jsonpath="{.data.${data}}" | base64 -d
-}
 
 handle_azure() {
     local AZURE_RESOURCE_GROUP
@@ -36,13 +34,19 @@ handle_azure() {
     local PP_NSG_ID
     local PP_NSG_NAME
 
-    echo "****Disable security to allow e2e****"
+    # Note: Keep the following commands in sync with https://raw.githubusercontent.com/kata-containers/kata-containers/refs/heads/main/ci/openshift-ci/peer-pods-azure.sh
+    # as much as possible.
+
+    ###############################
+    # Disable security to allow e2e
+    ###############################
+
+    # Disable security
     oc adm policy add-scc-to-group privileged system:authenticated system:serviceaccounts
     oc adm policy add-scc-to-group anyuid system:authenticated system:serviceaccounts
     oc label --overwrite ns default pod-security.kubernetes.io/enforce=privileged pod-security.kubernetes.io/warn=baseline pod-security.kubernetes.io/audit=baseline
 
-    AZURE_RESOURCE_GROUP=$(oc get infrastructure/cluster -o jsonpath='{.status.platformStatus.azure.resourceGroupName}')
-
+    oc -n kube-system get secret azure-credentials -o json > azure_credentials.json
     if [ -n "${CLUSTER_PROFILE_DIR:-}" ]; then
         AZURE_AUTH_LOCATION="${CLUSTER_PROFILE_DIR}/osServicePrincipal.json"
         AZURE_CLIENT_ID="$(jq -r .clientId "${AZURE_AUTH_LOCATION}")"
@@ -50,23 +54,31 @@ handle_azure() {
         AZURE_TENANT_ID="$(jq -r .tenantId "${AZURE_AUTH_LOCATION}")"
     else
         # Useful when testing this script outside of ci-operator
-        AZURE_CLIENT_ID="$(from_azure_credentials azure_client_id)"
-        AZURE_CLIENT_SECRET="$(from_azure_credentials azure_client_secret)"
-        AZURE_TENANT_ID="$(from_azure_credentials azure_tenant_id)"
-        AZURE_AUTH_LOCATION="${PWD}/osServicePrincipal.json"
-        echo "{ \"clientId\": \"$AZURE_CLIENT_ID\", \"clientSecret\": \"$AZURE_CLIENT_SECRET\", \"tenantId\": \"$AZURE_TENANT_ID\" }" | \
-            jq > "${AZURE_AUTH_LOCATION}"
+        AZURE_CLIENT_ID="$(jq -r .data.azure_client_id azure_credentials.json|base64 -d)"
+        AZURE_CLIENT_SECRET="$(jq -r .data.azure_client_secret azure_credentials.json|base64 -d)"
+        AZURE_TENANT_ID="$(jq -r .data.azure_tenant_id azure_credentials.json|base64 -d)"
+    fi
+    AZURE_SUBSCRIPTION_ID="$(jq -r .data.azure_subscription_id azure_credentials.json|base64 -d)"
+    rm -f azure_credentials.json
+
+    AZURE_RESOURCE_GROUP=$(oc get infrastructure/cluster -o jsonpath='{.status.platformStatus.azure.resourceGroupName}')
+    az login --service-principal --username "${AZURE_CLIENT_ID}" --password "${AZURE_CLIENT_SECRET}" --tenant "${AZURE_TENANT_ID}"
+    # Recommended on az sites to refresh the subscription
+    az account set --subscription "${AZURE_SUBSCRIPTION_ID}"
+    # This command still sometimes fails directly after login
+    for I in {1..30}; do
+	    AZURE_VNET_NAME=$(az network vnet list --resource-group "${AZURE_RESOURCE_GROUP}" --query "[].{Name:name}" --output tsv ||:)
+	    if [[ -z "${AZURE_VNET_NAME}" ]]; then
+		    sleep "${I}"
+	    else	# VNET set, we are done
+		    break
+	    fi
+    done
+    if [[ -z "${AZURE_VNET_NAME}" ]]; then
+	    echo "Failed to get AZURE_VNET_NAME in 30 iterations"
+	    exit 1
     fi
 
-    echo "****Login to Azure****"
-    az login --service-principal --username "${AZURE_CLIENT_ID}" --password "${AZURE_CLIENT_SECRET}" --tenant "${AZURE_TENANT_ID}" --output none
-    echo "****Login to Azure COMPLETE****"
-
-    echo "****Creating peerpods-param-secret with the keys needed for test case execution****"
-    oc create secret generic peerpods-param-secret --from-file="${AZURE_AUTH_LOCATION}" -n default
-    echo "****Creating peerpods-param-secret COMPLETE****"
-
-    AZURE_VNET_NAME=$(az network vnet list --resource-group "${AZURE_RESOURCE_GROUP}" --query "[].{Name:name}" --output tsv)
     AZURE_SUBNET_NAME=$(az network vnet subnet list --resource-group "${AZURE_RESOURCE_GROUP}" --vnet-name "${AZURE_VNET_NAME}" --query "[].{Id:name} | [? contains(Id, 'worker')]" --output tsv)
     AZURE_SUBNET_ID=$(az network vnet subnet list --resource-group "${AZURE_RESOURCE_GROUP}" --vnet-name "${AZURE_VNET_NAME}" --query "[].{Id:id} | [? contains(Id, 'worker')]" --output tsv)
     AZURE_NSG_ID=$(az network nsg list --resource-group "${AZURE_RESOURCE_GROUP}" --query "[].{Id:id}" --output tsv)
@@ -74,7 +86,7 @@ handle_azure() {
 
     PP_REGION=eastus
     if [[ "${AZURE_REGION}" == "${PP_REGION}" ]]; then
-        echo "****Using the current region ${AZURE_REGION}****"
+        echo "Using the current region ${AZURE_REGION}"
         PP_RESOURCE_GROUP="${AZURE_RESOURCE_GROUP}"
         PP_VNET_NAME="${AZURE_VNET_NAME}"
         PP_SUBNET_NAME="${AZURE_SUBNET_NAME}"
@@ -98,7 +110,26 @@ handle_azure() {
         PP_NSG_ID=$(az network nsg list --resource-group "${PP_RESOURCE_GROUP}" --query "[].{Id:id}" --output tsv)
 fi
 
-    echo "****Creating peerpods-param-cm config map with all the cloud params needed for test case execution****"
+    # Peer-pod requires gateway
+    az network public-ip create \
+        --resource-group "${PP_RESOURCE_GROUP}" \
+        --name MyPublicIP \
+        --sku Standard \
+        --allocation-method Static
+    az network nat gateway create \
+        --resource-group "${PP_RESOURCE_GROUP}" \
+        --name MyNatGateway \
+        --public-ip-addresses MyPublicIP \
+        --idle-timeout 10
+    az network vnet subnet update \
+        --resource-group "${PP_RESOURCE_GROUP}" \
+        --vnet-name "${PP_VNET_NAME}" \
+        --name "${PP_SUBNET_NAME}" \
+        --nat-gateway MyNatGateway
+
+    # Start the downstream-only commands
+
+    # Creating peerpods-param-cm config map with all the cloud params needed for test case execution
     cat <<- EOF > "${PP_CONFIGM_PATH}"
     apiVersion: v1
     kind: ConfigMap
@@ -117,28 +148,13 @@ fi
       PROXY_TIMEOUT: "30m"
 EOF
 
-    echo "AZURE_RESOURCE_GROUP: $PP_RESOURCE_GROUP"
-    echo "AZURE_SUBNET_ID: $PP_SUBNET_ID"
-    echo "AZURE_VNET_NAME: $PP_VNET_NAME"
-    echo "AZURE_NSG_ID: $PP_NSG_ID"
-
-    echo "****Configure Azure network****"
-    az network public-ip create \
-        -g "${PP_RESOURCE_GROUP}" \
-        -n peerpod \
-        -l "${PP_REGION}" \
-        --sku Standard
-    az network nat gateway create \
-        -g "${PP_RESOURCE_GROUP}" \
-        -l "${PP_REGION}" \
-        --public-ip-addresses peerpod \
-        -n peerpod
-    az network vnet subnet update \
-        -g "${PP_RESOURCE_GROUP}" \
-        --vnet-name "${PP_VNET_NAME}" \
-        --nat-gateway peerpod \
-        --ids "${PP_SUBNET_ID}"
-    echo "****Configure Azure network COMPLETE****"
+    if [[ -z "${AZURE_AUTH_LOCATION}" ]]; then
+        AZURE_AUTH_LOCATION="${PWD}/osServicePrincipal.json"
+        echo "{ \"clientId\": \"$AZURE_CLIENT_ID\", \"clientSecret\": \"$AZURE_CLIENT_SECRET\", \"tenantId\": \"$AZURE_TENANT_ID\" }" | \
+            jq > "${AZURE_AUTH_LOCATION}"
+    fi
+    # Creating peerpods-param-secret with the keys needed for test case execution
+    oc create secret generic peerpods-param-secret --from-file="${AZURE_AUTH_LOCATION}" -n default
 }
 
 echo "Creating peerpods-param-cm for azure"
