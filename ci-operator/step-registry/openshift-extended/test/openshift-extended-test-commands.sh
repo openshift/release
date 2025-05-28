9edebc0702d5e6be28775082dd65dea9d665fd34
Milind Yadav <miyadav@redhat.com>
2025-05-28 12:47:40 +0530

    adding shard-args for run selection

--- ci-operator/step-registry/openshift-extended/test/openshift-extended-test-commands.sh
+++ ci-operator/step-registry/openshift-extended/test/openshift-extended-test-commands.sh
@@ -344,7 +344,7 @@ function run {
     fi
 
     echo "final scenarios: ${test_scenarios}"
-    extended-platform-tests run all --dry-run | \
+    extended-platform-tests run all --dry-run ${SHARD_ARGS} | \
         grep -E "${test_scenarios}" | grep -E "${TEST_IMPORTANCE}" > ./case_selected
 
     hardcoded_filters="~NonUnifyCI&;~Flaky&;~DEPRECATED&;~SUPPLEMENTARY&;~VMonly&;~ProdrunOnly&;~StagerunOnly&"
@@ -390,11 +390,11 @@ function run {
     ret_value=0
     set -x
     if [ "W${TEST_PROVIDER}W" == "WnoneW" ]; then
-        extended-platform-tests run --max-parallel-tests ${TEST_PARALLEL} ${SHARD_ARGS} \
+        extended-platform-tests run --max-parallel-tests ${TEST_PARALLEL} \
         -o "${ARTIFACT_DIR}/extended.log" \
         --timeout "${TEST_TIMEOUT}m" --junit-dir="${ARTIFACT_DIR}/junit" -f ./case_selected || ret_value=$?
     else
-        extended-platform-tests run --max-parallel-tests ${TEST_PARALLEL} ${SHARD_ARGS} \
+        extended-platform-tests run --max-parallel-tests ${TEST_PARALLEL}  \
         --provider "${TEST_PROVIDER}" -o "${ARTIFACT_DIR}/extended.log" \
         --timeout "${TEST_TIMEOUT}m" --junit-dir="${ARTIFACT_DIR}/junit" -f ./case_selected || ret_value=$?
     fi
