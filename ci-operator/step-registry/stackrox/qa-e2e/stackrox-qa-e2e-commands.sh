c91026edf0980b9ffba2ffa9fd2e16fcc42196aa
Fengshuang Tan <tanfengshuang@163.com>
2025-06-05 06:06:30 +0800

    Try no "|| exit" again

--- ci-operator/step-registry/stackrox/qa-e2e/stackrox-qa-e2e-commands.sh
+++ ci-operator/step-registry/stackrox/qa-e2e/stackrox-qa-e2e-commands.sh
@@ -7,7 +7,8 @@ if [ ! -f ".openshift-ci/dispatch.sh" ];then
   if [ ! -d "stackrox" ];then
     git clone https://github.com/stackrox/stackrox.git
   fi
-  cd stackrox || exit
+  #cd stackrox || exit
+  cd stackrox
 fi
 
 exec .openshift-ci/dispatch.sh "${job}"
