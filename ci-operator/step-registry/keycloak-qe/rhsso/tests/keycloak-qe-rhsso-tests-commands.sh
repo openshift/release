2cf0792e2d78883a0abc0da1ef5cee1b5856fe4f
vshakya <vshakya@vshakya-thinkpadt14gen4.remote.csb>
2025-06-04 09:55:38 -0400

    test exit codes for test commands script

--- ci-operator/step-registry/keycloak-qe/rhsso/tests/keycloak-qe-rhsso-tests-commands.sh
+++ ci-operator/step-registry/keycloak-qe/rhsso/tests/keycloak-qe-rhsso-tests-commands.sh
@@ -18,8 +18,37 @@ cp -L $KUBECONFIG /tmp/kubeconfig
 echo "Executing rhsso tests ref"
 
 ansible-playbook -v /tmp/tests/ansible-tests/test-ocp-ci-rhbk.yml --extra-vars "ocp_project_name='${OCP_PROJECT_NAME}'"
+ANSIBLE_EXIT_CODE=$?
+
+if [[ $ANSIBLE_EXIT_CODE -ne 0 ]]; then
+  echo " Ansilbe playbook failed with error code $ANSIBLE_EXIT_CODE" 
+  exit 1
+else
+  echo "Execution succesfull"
+fi
+
+
+# if [[ ! -f "/tmp/tests/ansible-tests/junit-results/test-ocp-ci-rhbk-*.xml" ]]; then
+#   echo "JUNIT file not found at : /tmp/tests/ansible-tests/junit-results/test-ocp-ci-rhbk-*.xml"
+#   exit 1
+# else
+#   echo "JUNIT file found  "
+# fi
+
 
 #copy junit results to artifacts dir
+echo "creating rhsso-tests directory"
 mkdir -p $ARTIFACT_DIR/rhsso-tests
-cp -r /tmp/tests/ansible-tests/junit-results/test-ocp-ci-rhbk-*.xml  $ARTIFACT_DIR/rhsso-tests/junit_rhsso_tests_results.xml
+echo "copy junit results to artifacts dir"
+cp -r  /tmp/tests/ansible-tests/junit-results/test-ocp-ci-rhbk-*.xml $ARTIFACT_DIR/rhsso-tests/junit_rhsso_tests_results.xml
+
+#Parse JUnit sml for failure
+JUNIT_FILE="$ARTIFACT_DIR/rhsso-tests/junit_rhsso_tests_results.xml"
+if grep -qE "<failure|<error" "$JUNIT_FILE" ; then
+  echo " failures or errors found in junit xml "
+  exit 1
+else
+  echo "All tests passed, exiting with success"
+  exit 0
+fi
 
