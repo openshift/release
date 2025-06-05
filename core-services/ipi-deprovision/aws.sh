72cad183e7a61fe5b5b373fa6a913edeb3043a51
Hector Vido <39673799+hector-vido@users.noreply.github.com>
2025-06-05 14:16:14 -0300

    [DPTP-4437] Consider key prefix as a filter to deprovision clusters (#65738)

--- core-services/ipi-deprovision/aws.sh
+++ core-services/ipi-deprovision/aws.sh
@@ -55,7 +55,8 @@ echo "deprovisioning clusters with an expirationDate before ${aws_cluster_age_cu
 # --region is necessary when there is no profile customization
 for region in $( aws ec2 describe-regions --region us-east-1 --query "Regions[].{Name:RegionName}" --output text ); do
 	echo "deprovisioning in AWS region ${region} ..."
-	for cluster in $( aws ec2 describe-vpcs --output json --region "${region}" | jq --arg date "${aws_cluster_age_cutoff}" -r -S '.Vpcs[] | select (.Tags[]? | (.Key == "expirationDate" and .Value < $date)) | .Tags[] | select (.Value == "owned") | .Key' ); do
+	aws ec2 describe-vpcs --output json --region us-east-1 | jq --arg date "${aws_cluster_age_cutoff}" -r '.Vpcs[] | select(.Tags[]? | select(.Key == "expirationDate" and .Value < $date)) | .Tags[]? | select((.Key | startswith("kubernetes.io/cluster/")) and (.Value == "owned")) | .Key' > /tmp/clusters
+	while read cluster; do
 		workdir="${logdir}/${cluster:22}"
 		mkdir -p "${workdir}"
 		cat <<-EOF >"${workdir}/metadata.json"
@@ -69,7 +70,7 @@ for region in $( aws ec2 describe-regions --region us-east-1 --query "Regions[].
 		}
 		EOF
 		echo "will deprovision AWS cluster ${cluster} in region ${region}"
-	done
+	done < /tmp/clusters
 done
 
 clusters=$( find "${logdir}" -mindepth 1 -type d )
