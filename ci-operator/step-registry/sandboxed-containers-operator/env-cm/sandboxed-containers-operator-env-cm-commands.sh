7f2263f2d7b745f700eb77d388838cd753d02957
Wainer dos Santos Moschetta <wainersm@redhat.com>
2025-06-06 15:31:56 -0300

    step-registry/sandboxed-containers-operator: add Pre-GA handling to env-cm
    In case of TEST_RELEASE_TYPE equals to "Pre-GA" then it should create
    the catalog source and the user is expected to set CATALOG_SOURCE_NAME
    as well as OPERATOR_INDEX_VERSION.
    
    Mirroring to konflux's quay.io project.
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- ci-operator/step-registry/sandboxed-containers-operator/env-cm/sandboxed-containers-operator-env-cm-commands.sh
+++ ci-operator/step-registry/sandboxed-containers-operator/env-cm/sandboxed-containers-operator-env-cm-commands.sh
@@ -8,10 +8,106 @@ configmap_path="${SHARED_DIR:-$(pwd)}/env-cm.yaml"
 #echo "Giving a 10min stabilization time for AWS fresh 4.18 cluster before applying kataconfig as workaround for KATA-3451"
 #sleep 600
 
+create_catsrc() {
+  local catsrc_name="$1"
+  local catsrc_version="$2"
+  local catsrc_image="$3"
+  local catsrc_path="${SHARED_DIR:-$(pwd)}/catsrc_${catsrc_name}.yaml"
+
+  echo "Create a custom catalogsource named ${catsrc_name} for internal builds"
+
+  cat<<-EOF | tee "${catsrc_path}"
+  apiVersion: operators.coreos.com/v1alpha1
+  kind: CatalogSource
+  metadata:
+    name: "${catsrc_name}"
+    namespace: openshift-marketplace
+  spec:
+    displayName: QE
+    image: "${catsrc_image}:${catsrc_version}"
+    publisher: QE
+    sourceType: grpc
+EOF
+
+  oc apply -f "${catsrc_path}"
+}
+
+mirror_konflux() {
+  local mirror_path="${SHARED_DIR:-$(pwd)}/mirror_konflux.yaml"
+
+  echo "Create mirror for konflux images"
+
+cat<<EOF | tee "${mirror_path}"
+---
+apiVersion: config.openshift.io/v1
+kind: ImageTagMirrorSet
+metadata:
+  name: osc-registry
+spec:
+  imageTagMirrors:
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-monitor
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-monitor-rhel9
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-caa
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-cloud-api-adaptor-rhel9
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-caa-webhook
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-cloud-api-adaptor-webhook-rhel9
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-podvm-builder
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-podvm-builder-rhel9
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-podvm-payload
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-podvm-payload-rhel9
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-operator
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-rhel9-operator
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-must-gather
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-must-gather-rhel9
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-operator-bundle
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-operator-bundle
+---
+apiVersion: config.openshift.io/v1
+kind: ImageDigestMirrorSet
+metadata:
+  name: osc-registry
+spec:
+  imageDigestMirrors:
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-monitor
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-monitor-rhel9
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-caa
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-cloud-api-adaptor-rhel9
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-caa-webhook
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-cloud-api-adaptor-webhook-rhel9
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-podvm-builder
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-podvm-builder-rhel9
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-podvm-payload
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-podvm-payload-rhel9
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-operator
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-rhel9-operator
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-must-gather
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-must-gather-rhel9
+    - mirrors:
+        - quay.io/redhat-user-workloads/ose-osc-tenant/osc-operator-bundle
+      source: registry.redhat.io/openshift-sandboxed-containers/osc-operator-bundle
+EOF
+
+  oc apply -f "${mirror_path}"
+}
+
 if [[ "$TEST_RELEASE_TYPE" == "Pre-GA" ]]; then
-  # TODO: implement me.
-  echo "Apply catalog source. Not implemented."
-  exit 1
+  mirror_konflux
+  create_catsrc "${CATALOG_SOURCE_NAME}" "${OPERATOR_INDEX_VERSION}" "${OPERATOR_INDEX_IMAGE}"
 fi
 
 cat <<EOF | tee "${configmap_path}"
