51c42e97924ec9de3694e4edd08cce3c60944502
Raul Sevilla <rsevilla@redhat.com>
2025-06-11 00:24:35 +0200

    Ues metrics-aggregated in tests with 116 and 249 workers
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- ci-operator/step-registry/openshift-qe/virt-udn-density/openshift-qe-virt-udn-density-commands.sh
+++ ci-operator/step-registry/openshift-qe/virt-udn-density/openshift-qe-virt-udn-density-commands.sh
@@ -30,7 +30,7 @@ current_worker_count=$(oc get nodes --no-headers -l node-role.kubernetes.io/work
 ES_SERVER="" EXTRA_FLAGS="--layer3=${ENABLE_L3} --iteration=${current_worker_count}" CHURN=false ./run.sh
 
 # The measurable run
-EXTRA_FLAGS="--gc-metrics=true --iteration=${ITERATIONS} --layer3=${ENABLE_L3} --vmi-ready-threshold=${VMI_READY_THRESHOLD} --metrics-profile=${METRICS_PROFILE}"
+EXTRA_FLAGS="--gc-metrics=true --iteration=${ITERATIONS} --layer3=${ENABLE_L3} --vmi-ready-threshold=${VMI_READY_THRESHOLD}s --metrics-profile=${METRICS_PROFILE}"
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@$ES_HOST"
 
 if [[ "${ENABLE_LOCAL_INDEX}" == "true" ]]; then
