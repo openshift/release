ac2805720c0fe4bbc4aff1275045832a0e4c0e6d
mkralik3 <mkralik@redhat.com>
2025-06-10 10:59:50 +0200

    [OSSM-9601] Update kiali int/cypress job to be able to run with ossm3, enable kiali int

--- ci-operator/step-registry/servicemesh/kiali/cypress-tests/execute/servicemesh-kiali-cypress-tests-execute-commands.sh
+++ ci-operator/step-registry/servicemesh/kiali/cypress-tests/execute/servicemesh-kiali-cypress-tests-execute-commands.sh
@@ -19,14 +19,47 @@ else #login for ROSA & Hypershift platforms
   eval "$(cat "${SHARED_DIR}/api.login")"
 fi
 
-# download the istio version with 1.19.1 bookinfo app
-hack/istio/download-istio.sh -iv 1.22.3
-hack/istio/install-testing-demos.sh -c oc -in ${SMCP_NAMESPACE}
-sleep 120
-oc wait --for condition=Successful kiali/kiali -n ${SMCP_NAMESPACE} --timeout=250s
-oc wait --for condition=available deployment/kiali -n ${SMCP_NAMESPACE} --timeout=250s
+if [ "${OSSM_VERSION}" == "2" ]
+then
+  # download istio samples
+  hack/istio/download-istio.sh -iv 1.22.3
+  # install demo apps
+  hack/istio/install-testing-demos.sh -c oc -in ${ISTIO_NAMESPACE}
+elif [ "${OSSM_VERSION}" == "3" ]
+then
+  # remove v from ISTIO version if there is any
+  [[ $ISTIO_VERSION == v* ]] && ISTIO_VERSION="${ISTIO_VERSION#v}" || ISTIO_VERSION="$ISTIO_VERSION"
+  hack/istio/download-istio.sh -iv ${ISTIO_VERSION}
+  # install testing apps
+  hack/istio/install-testing-demos.sh -c oc -in ${ISTIO_NAMESPACE}
+  # enable monitoring in demo apps
+  hack/use-openshift-prometheus.sh -in ${ISTIO_NAMESPACE} -n alpha -ml ossm-3 -kcns ${ISTIO_NAMESPACE} -np false
+  hack/use-openshift-prometheus.sh -in ${ISTIO_NAMESPACE} -n beta -ml ossm-3 -kcns ${ISTIO_NAMESPACE} -np false
+  hack/use-openshift-prometheus.sh -in ${ISTIO_NAMESPACE} -n gamma -ml ossm-3 -kcns ${ISTIO_NAMESPACE} -np false
+  hack/use-openshift-prometheus.sh -in ${ISTIO_NAMESPACE} -n default -ml ossm-3 -kcns ${ISTIO_NAMESPACE} -np false
+  hack/use-openshift-prometheus.sh -in ${ISTIO_NAMESPACE} -n bookinfo -ml ossm-3 -kcns ${ISTIO_NAMESPACE} -np false
+  hack/use-openshift-prometheus.sh -in ${ISTIO_NAMESPACE} -n sleep -ml ossm-3 -kcns ${ISTIO_NAMESPACE} -np false
+  # install custom grafana
+  oc apply -n ${ISTIO_NAMESPACE} -f https://raw.githubusercontent.com/istio/istio/${ISTIO_VERSION}/samples/addons/grafana.yaml
+  oc wait -n ${ISTIO_NAMESPACE} --for=condition=available deployment/grafana --timeout=150s
+  # Expose grafana route (for Kiali) (delete route first if exists from previous run)
+  oc delete -n ${ISTIO_NAMESPACE} route grafana || true
+  oc expose -n ${ISTIO_NAMESPACE} service grafana --name=grafana
+  sleep 5s
+  GRAFANA_URL=$(oc get route grafana -o jsonpath='{.spec.host}' -n ${ISTIO_NAMESPACE})
+  oc patch kiali kiali -n ${ISTIO_NAMESPACE} -p "{\"spec\":{\"external_services\":{\"grafana\": {\"internal_url\": \"http://grafana.${ISTIO_NAMESPACE}:3000\"}}}}" --type=merge
+  oc patch kiali kiali -n ${ISTIO_NAMESPACE} -p "{\"spec\":{\"external_services\":{\"grafana\": {\"external_url\": \"http://${GRAFANA_URL}\"}}}}" --type=merge
+  oc patch kiali kiali -n ${ISTIO_NAMESPACE} -p "{\"spec\":{\"external_services\":{\"grafana\": {\"enabled\": true}}}}" --type=merge
+else
+  echo "Unsuported OSSM_VERSION ${OSSM_VERSION}!"
+  exit 1
+fi
 
-KIALI_ROUTE=$(oc get route kiali -n ${SMCP_NAMESPACE} -o=jsonpath='{.spec.host}')
+sleep 120
+oc wait --for condition=Successful kiali/kiali -n ${ISTIO_NAMESPACE} --timeout=250s
+oc wait --for condition=available deployment/kiali -n ${ISTIO_NAMESPACE} --timeout=250s
+
+KIALI_ROUTE=$(oc get route kiali -n ${ISTIO_NAMESPACE} -o=jsonpath='{.spec.host}')
 export CYPRESS_BASE_URL="https://${KIALI_ROUTE}"
 export CYPRESS_USERNAME=${OCP_CRED_USR}
 export CYPRESS_PASSWD=${OCP_CRED_PSW}
@@ -34,11 +67,11 @@ export CYPRESS_AUTH_PROVIDER="kube:admin"
 
 # for flaky tests
 export CYPRESS_RETRIES=2
-export TEST_GROUP="not @crd-validation and not @multi-cluster and not @skip-lpinterop"
+export TEST_GROUP="not @crd-validation and not @multi-cluster and not @smoke and not @ambient and not @waypoint and not @cytoscape and not @skip-lpinterop"
 yarn cypress:run:test-group:junit || true # do not fail on a exit code != 0 as it matches number of failed tests
 # save screenshots from the 1st run
 cp -r cypress/screenshots ${ARTIFACT_DIR}/ || true
-export TEST_GROUP="@crd-validation and not @multi-cluster and not @skip-lpinterop"
+export TEST_GROUP="@crd-validation and not @multi-cluster and not @smoke and not @ambient and not @waypoint and not @cytoscape and not @skip-lpinterop"
 yarn cypress:run:test-group:junit || true # do not fail on a exit code != 0 as it matches number of failed tests
 
 # merge all reports together
@@ -48,6 +81,7 @@ echo "Copying result xml and screenshots to ${ARTIFACT_DIR}"
 # the file name must start with 'junit'
 cp cypress/results/combined-report.xml ${ARTIFACT_DIR}/junit-kiali-cypress.xml
 cp -r cypress/screenshots ${ARTIFACT_DIR}/ || true
+cp -r /tmp/kiali/cypress/screenshots ${ARTIFACT_DIR}/ || true
 
 # cleaning demo apps
-hack/istio/install-testing-demos.sh -d true -c oc -in ${SMCP_NAMESPACE}
+hack/istio/install-testing-demos.sh -d true -c oc -in ${ISTIO_NAMESPACE}
