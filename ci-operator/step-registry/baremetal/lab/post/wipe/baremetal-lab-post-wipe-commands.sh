a065ea3ec630fe9769f6153c76a263afc6c4a42b
Steeve Goveas <sgoveas@redhat.com>
2025-06-10 23:01:51 +0100

    Reset iLO for hosts with ipxe_via_media
    The existing arm64 hosts in RDU2 lab for metal qe need a iLO reset to
    avoid kernel crash issue

--- ci-operator/step-registry/baremetal/lab/post/wipe/baremetal-lab-post-wipe-commands.sh
+++ ci-operator/step-registry/baremetal/lab/post/wipe/baremetal-lab-post-wipe-commands.sh
@@ -16,7 +16,7 @@ SSHOPTS=(-o 'ConnectTimeout=5'
 
 [ -z "${PULL_NUMBER:-}" ] && \
   timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" \
-    test -f /var/builds/${NAMESPACE}/preserve && \
+    test -f /var/builds/"${NAMESPACE}"/preserve && \
   exit 0
 
 if [ "${SELF_MANAGED_NETWORK}" != "true" ]; then
@@ -76,6 +76,60 @@ quit
 EOF
 }
 
+function check_ilo() {
+  echo "Checking if #${host} iLO/BMC to become responsive..."
+
+  start_time=$(date +%s)
+  while true; do
+    current_time=$(date +%s)
+    elapsed=$((current_time - start_time))
+
+    if [ "$elapsed" -ge "$TIMEOUT_SECONDS" ]; then
+        echo "Error: #${host} iLO/BMC did not become responsive after ${TIMEOUT_SECONDS} seconds." >&2
+        exit 1
+    fi
+
+    # Try to get BMC info (a simple, low-impact command)
+    if timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" ipmitool -I lanplus -H "${bmc_address}" \
+      -U "${bmc_user}" -P "${bmc_pass}" mc info >/dev/null 2>&1; then
+      echo "iLO/BMC on #${host} is responsive. Reset complete. (Elapsed: ${elapsed}s)"
+      break
+    else
+      echo "iLO/BMC on #${host} is still unresponsive. Retrying in ${POLL_INTERVAL} seconds..."
+      sleep "$POLL_INTERVAL"
+    fi
+  done
+}
+
+function ilo_reset() {
+  local bmc_address="${1}"
+  local bmc_forwarded_port="${2}"
+  local bmc_user="${3}"
+  local bmc_pass="${4}"
+  local vendor="${5}"
+  local ipxe_via_vmedia="${6}"
+  local host="${bmc_forwarded_port##1[0-9]}"
+  host="${host##0}"
+#  TIMEOUT_SECONDS=120
+#  POLL_INTERVAL=10
+#  iloreset="ipmitool -I lanplus -H ${bmc_address} -U ${bmc_user} -P ${bmc_pass} mc reset cold"
+#  echo "$(date): Reseting iLO for host #$host"
+#  timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" "${iloreset}"
+#
+#  echo -e "\nWaiting for 3 mins for #${host} iLO/BMC to reset.."
+#  sleep 120
+#  check_ilo
+  power_button="ipmitool -I lanplus -H ${bmc_address} -U ${bmc_user} -P ${bmc_pass} power on"
+  timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" "${power_button} on"
+  echo "Power cycle #${host} and wait for 3 mins"
+  sleep 180
+  echo "Power off #${host}"
+  timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" "${power_button} off"
+  #timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" prepare_host_for_boot "${host}" "pxe"
+  #echo "Reset #${host} with pxe and wait for 4 mins"
+  #sleep 240
+}
+
 function reset_host() {
   local bmc_address="${1}"
   local bmc_forwarded_port="${2}"
@@ -90,6 +144,7 @@ function reset_host() {
     -U "$bmc_user" -P "$bmc_pass" power status | grep -iq "Error: Unable to establish IPMI"; then
     echo "Resetting ${host} pdu"
     reset_pdu "${pdu_uri}"
+    echo -e "Waiting for PDU reset of host #${host}..."
     local max_try=20
     while [ "$max_try" -gt 0 ] && ! ipmitool -I lanplus -H "${AUX_HOST}" -p "${bmc_forwarded_port}" \
       -U "$bmc_user" -P "$bmc_pass" power status | grep -iq "Chassis Power is"; do
@@ -98,7 +153,7 @@ function reset_host() {
       max_try=$(( max_try - 1 ))
     done
     if [ $max_try -le 0 ]; then
-      echo "${host} PDU is unreachable, contact @metal-qe-team"
+      echo "#${host} PDU is unreachable, contact @metal-qe-team"
       echo "$bmc_host:$bmc_forwarded_port" >> /tmp/failed
       return 1
     fi
@@ -124,6 +179,7 @@ function reset_host() {
 # can happen safely, i.e., a concurrent reservation job has to wait until the disk of host X is wiped before being allowed
 # to reserve X
 touch /tmp/failed
+
 for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
   # shellcheck disable=SC1090
   . <(echo "$bmhost" | yq e 'to_entries | .[] | (.key + "=\"" + .value + "\"")')
@@ -133,6 +189,17 @@ for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
     echo "Error while unmarshalling hosts entries"
     exit 1
   fi
+  if [ "${ipxe_via_vmedia}" == "true" ]; then
+    echo -e "Host #${host} requires an ilo reset as a workaround to avoid kernel crashes\n"
+    ilo_reset "${bmc_address}" "${bmc_forwarded_port}" "${bmc_user}" "${bmc_pass}" "${vendor}" "${ipxe_via_vmedia}" &
+  fi
+done
+wait
+
+for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
+  # shellcheck disable=SC1090
+  . <(echo "$bmhost" | yq e 'to_entries | .[] | (.key + "=\"" + .value + "\"")')
+  # shellcheck disable=SC2154
   reset_host "${bmc_address}" "${bmc_forwarded_port}" "${bmc_user}" "${bmc_pass}" "${vendor}" "${ipxe_via_vmedia}" \
     "${pdu_uri:-}" &
 done
@@ -152,6 +219,8 @@ for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
   # shellcheck disable=SC2154
   timeout -s 9 5m ssh "${SSHOPTS[@]}" "root@${AUX_HOST}" umount.vmedia "${host}"
 done
+
+echo "Disk wipe completed"
 
 if [ -s /tmp/failed ]; then
   echo The following nodes failed to power off:
