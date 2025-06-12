d87b6b9168e2819457a00927ad1e89f0b311d2a0
Jim Ma <ema@redhat.com>
2025-06-12 14:24:04 +0800

    Fix remove image registry shell

--- ci-operator/step-registry/cucushift/installer/rehearse/azure/aks/hypershift/disable-image-registry/remove-image-registry/cucushift-installer-rehearse-azure-aks-hypershift-disable-image-registry-remove-image-registry-commands.sh
+++ ci-operator/step-registry/cucushift/installer/rehearse/azure/aks/hypershift/disable-image-registry/remove-image-registry/cucushift-installer-rehearse-azure-aks-hypershift-disable-image-registry-remove-image-registry-commands.sh
@@ -2,7 +2,9 @@
 set -exuo pipefail
 
 if [[ $USE_HYPERSHIFT_AZURE_CREDS == "true" ]]; then
-   jq 'del(.imageRegistry)' /etc/hypershift-ci-jobs-azurecreds/managed-identities.json $SHARED_DIR/managed-identities-del-image-registry.json
+   jq 'del(.imageRegistry)' /etc/hypershift-ci-jobs-azurecreds/managed-identities.json > $SHARED_DIR/managed-identities-del-image-registry.json
 else
-   jq 'del(.imageRegistry)' /etc/hypershift-aro-azurecreds/managed-identities.json $SHARED_DIR/managed-identities-del-image-registry.json
+   jq 'del(.imageRegistry)' /etc/hypershift-aro-azurecreds/managed-identities.json > $SHARED_DIR/managed-identities-del-image-registry.json
 fi
+
+cat  $SHARED_DIR/managed-identities-del-image-registry.json
