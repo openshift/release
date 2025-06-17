95f50223ac7d359d1d2ca069be42c54eb3e268ba
Ke Wang <kewang@redhat.com>
2025-06-18 02:29:51 +0800

    Check security ciphers and config after tls13 enalbed (#65837)

--- ci-operator/step-registry/tls-13/tls-13-commands.sh
+++ ci-operator/step-registry/tls-13/tls-13-commands.sh
@@ -3,8 +3,12 @@ set -euo pipefail
 
 export KUBECONFIG=${SHARED_DIR}/kubeconfig
 
-oc login -u kubeadmin "$(oc whoami --show-server=true)" < "$SHARED_DIR/kubeadmin-password"
-
 oc patch apiservers/cluster --type=merge -p '{"spec": {"tlsSecurityProfile":{"modern":{},"type":"Modern"}}}'
 
 oc adm wait-for-stable-cluster
+
+tls_profile=$(oc get apiserver/cluster -ojson | jq -r .spec.tlsSecurityProfile.type)
+if [[ "$tls_profile" != "Modern" ]]; then
+  echo "Error: TLS Security Profile is '$tls_profile', expected 'Modern'"
+  exit 1
+fi
