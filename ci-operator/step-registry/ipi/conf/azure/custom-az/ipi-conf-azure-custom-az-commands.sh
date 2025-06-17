9e6cb9c394c5957ccb59a8631a9eccf62d6c1aae
Jinyun Ma <jima@redhat.com>
2025-06-17 09:56:17 +0800

    Azure: cover instance type with ConfidentialComputingType:TDX test (#66057)

--- ci-operator/step-registry/ipi/conf/azure/custom-az/ipi-conf-azure-custom-az-commands.sh
+++ ci-operator/step-registry/ipi/conf/azure/custom-az/ipi-conf-azure-custom-az-commands.sh
@@ -10,14 +10,26 @@ echo "controlPlane Azure custom zones: ${CP_CUSTOM_AZURE_AZ}"
 echo "Compute Azure custom zones: ${COMPUTE_CUSTOM_AZURE_AZ}"
 
 CONFIG_PATCH="${SHARED_DIR}/install-config-zones.yaml.patch"
-cat > "${CONFIG_PATCH}" << EOF
+if [[ -n "${CP_CUSTOM_AZURE_AZ}" ]]; then
+    cat > "${CONFIG_PATCH}" << EOF
 controlPlane:
   platform:
     azure:
       zones: ${CP_CUSTOM_AZURE_AZ}
+EOF
+fi
+if [[ -n "${COMPUTE_CUSTOM_AZURE_AZ}" ]]; then
+    cat >> "${CONFIG_PATCH}" << EOF
 compute:
 - platform:
     azure:
       zones: ${COMPUTE_CUSTOM_AZURE_AZ}
 EOF
-yq-go m -x -i "${CONFIG}" "${CONFIG_PATCH}"
+fi
+if [[ -s "${CONFIG_PATCH}" ]]; then
+    yq-go m -x -i "${CONFIG}" "${CONFIG_PATCH}"
+    echo "CONFIG_PATCH content:"
+    cat ${CONFIG_PATCH}
+else
+    echo "CONFIG_PATCH is empty, skip the setting in install-config!"
+fi
