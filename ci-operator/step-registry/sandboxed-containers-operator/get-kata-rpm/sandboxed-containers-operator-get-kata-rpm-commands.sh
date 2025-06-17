eb9aa5501054f4db9bcc17bc1e42d7a1ba1c8a70
Wainer dos Santos Moschetta <wainersm@redhat.com>
2025-06-17 17:11:16 -0300

    step-registry/sandboxed-containers-operator: new KATA_RPM_BUILD_TASK parameter to get-kata-rpm
    The URL to download RPM from candidate builds changes. Added the
    KATA_RPM_BUILD_TASK parameter which refers to the the Brew task ID.
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- ci-operator/step-registry/sandboxed-containers-operator/get-kata-rpm/sandboxed-containers-operator-get-kata-rpm-commands.sh
+++ ci-operator/step-registry/sandboxed-containers-operator/get-kata-rpm/sandboxed-containers-operator-get-kata-rpm-commands.sh
@@ -8,15 +8,19 @@
 
 cd /tmp || exit 1
 
-# Read from secrets
-KATA_RPM_BASE_URL=$(cat /usr/local/sandboxed-containers-operator-ci-secrets/secrets/KATA_RPM_BASE_URL)
-
 arch=$(uname -m)
-ver=$(echo "$KATA_RPM_VERSION" | cut -d- -f1)
-build=$(echo "$KATA_RPM_VERSION" | cut -d- -f2)
-brew_auth="$(oc get -n openshift-config secret/pull-secret -ojson  | jq -r '.data.".dockerconfigjson"' |  base64 -d | jq -r '.auths."registry.redhat.io".auth' | base64 -d)"
+if [ -n "${KATA_RPM_BUILD_TASK}" ];then
+    KATA_RPM_BASE_TASK_URL=$(cat /usr/local/sandboxed-containers-operator-ci-secrets/secrets/KATA_RPM_BUILD_TASK_BASE_URL)
+    # To the base URL it's appended the "last four digits of task ID"/"full task ID"
+    KATA_RPM_BUILD_URL="${KATA_RPM_BASE_TASK_URL}/${KATA_RPM_BUILD_TASK: -4}/${KATA_RPM_BUILD_TASK}/kata-containers-${KATA_RPM_VERSION}.${arch}.rpm"
+else
+    ver=$(echo "$KATA_RPM_VERSION" | cut -d- -f1)
+    build=$(echo "$KATA_RPM_VERSION" | cut -d- -f2)
+    KATA_RPM_BASE_URL=$(cat /usr/local/sandboxed-containers-operator-ci-secrets/secrets/KATA_RPM_BASE_URL)
+    KATA_RPM_BUILD_URL="${KATA_RPM_BASE_URL}/${ver}/${build}/${arch}/kata-containers-${KATA_RPM_VERSION}.${arch}.rpm"
+fi
 
-KATA_RPM_BUILD_URL="${KATA_RPM_BASE_URL}/${ver}/${build}/${arch}/kata-containers-${KATA_RPM_VERSION}.${arch}.rpm"
+brew_auth="$(oc get -n openshift-config secret/pull-secret -ojson  | jq -r '.data.".dockerconfigjson"' |  base64 -d | jq -r '.auths."registry.redhat.io".auth' | base64 -d)"
 
 md5sum_file="${KATA_RPM_BUILD_MD5SUM}  kata-containers.rpm"
 curl -L -k -o kata-containers.rpm -u "${brew_auth}" "${KATA_RPM_BUILD_URL}"
