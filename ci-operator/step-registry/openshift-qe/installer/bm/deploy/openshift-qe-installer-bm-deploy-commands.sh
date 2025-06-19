11cf128337e4e35dbd802819b5c2124894fc61a9
Raul Sevilla <rsevilla@redhat.com>
2025-06-19 16:13:08 +0200

    Update pull-secret path
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/deploy/openshift-qe-installer-bm-deploy-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/deploy/openshift-qe-installer-bm-deploy-commands.sh
@@ -160,7 +160,7 @@ scp -q ${SSH_ARGS} /tmp/all-updated.yml root@${bastion}:${jetlag_repo}/ansible/v
 scp -q ${SSH_ARGS} ${CLUSTER_PROFILE_DIR}/pull_secret root@${bastion}:${jetlag_repo}/pull_secret.txt
 scp -q ${SSH_ARGS} /tmp/clean-resources.sh root@${bastion}:/tmp/
 scp -q ${SSH_ARGS} /tmp/prereqs-updated.sh root@${bastion}:/tmp/
-scp -q ${SSH_ARGS} /tmp/pull-secret root@${bastion}:/root/.docker/config.json
+scp -q ${SSH_ARGS} ${CLUSTER_PROFILE_DIR}/pull-secret root@${bastion}:/root/.docker/config.json
 
 if [[ ${TYPE} == 'sno' ]]; then
   KUBECONFIG_SRC='/root/sno/{{ groups.sno[0] }}/kubeconfig'
