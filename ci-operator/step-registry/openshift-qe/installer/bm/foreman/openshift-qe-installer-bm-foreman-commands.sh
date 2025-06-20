d2e62bbc91360c3f199a9eeccfefe010ffcb9039
José Castillo Lema <josecastillolema@gmail.com>
2025-06-20 04:43:53 -0400

    [PERFSCALE-3886] Isolate cluster profiles through Vault (poweroff/foreman) (#66067)
    * PERFSCALE-3886 - Isolate cluster profiles through Vault (poweroff)
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    * Foreman step corresponing changes
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    * Remove vault vars
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    * Remove secret reference
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    * Remove foreman reboot
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    * Add timeout
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    * Hide QUADS_INSTANCE var
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    * Fix foreman-wait script
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    * Hide the key from self-sched step
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    ---------
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/foreman/openshift-qe-installer-bm-foreman-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/foreman/openshift-qe-installer-bm-foreman-commands.sh
@@ -7,32 +7,34 @@ set -x
 # Fix UID issue (from Telco QE Team)
 ~/fix_uid.sh
 
-SSH_ARGS="-i /secret/jh_priv_ssh_key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
-bastion=$(cat "/secret/address")
+SSH_ARGS="-i ${CLUSTER_PROFILE_DIR}/jh_priv_ssh_key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
+bastion=$(cat ${CLUSTER_PROFILE_DIR}/address)
+LAB=$(cat ${CLUSTER_PROFILE_DIR}/lab)
+LAB_CLOUD=$(cat ${CLUSTER_PROFILE_DIR}/lab_cloud)
+export LAB_CLOUD
+QUADS_INSTANCE=$(cat ${CLUSTER_PROFILE_DIR}/quads_instance_${LAB})
+export QUADS_INSTANCE
 
 # 1. Set the corresponding host on the lab Foreman instance
 # 2. Use badfish to set the boot interface and bounce the box
 cat > /tmp/foreman-deploy.sh << 'EOF'
 echo 'Running foreman-deploy.sh'
-USER=$(curl -sSk $QUADS_INSTANCE | jq -r ".nodes[0].pm_user")
-PWD=$(curl -sSk $QUADS_INSTANCE  | jq -r ".nodes[0].pm_password")
-for i in $(curl -sSk $QUADS_INSTANCE | jq -r ".nodes[$STARTING_NODE:$(($STARTING_NODE+$NUM_NODES))][].name"); do
-  hammer --verify-ssl false -u $LAB_CLOUD -p $PWD host update --name $i --operatingsystem "$FOREMAN_OS" --pxe-loader "Grub2 UEFI" --build 1
+OCPINV=$QUADS_INSTANCE/instack/$LAB_CLOUD\_ocpinventory.json
+USER=$(curl -sSk $OCPINV | jq -r ".nodes[0].pm_user")
+PSWD=$(curl -sSk $OCPINV  | jq -r ".nodes[0].pm_password")
+for i in $(curl -sSk $OCPINV | jq -r ".nodes[$STARTING_NODE:$(($STARTING_NODE+$NUM_NODES))][].name"); do
+  hammer --verify-ssl false -u $LAB_CLOUD -p $PSWD host update --name $i --operatingsystem "$FOREMAN_OS" --pxe-loader "Grub2 UEFI" --build 1
   sleep 10
-  badfish -H mgmt-$i -u $USER -p $PWD -i ~/badfish_interfaces.yml -t foreman
+  badfish --reboot-only -H mgmt-$i -u $USER -p $PSWD
 done
 EOF
-if [[ $LAB == "performancelab" ]]; then
-  export QUADS_INSTANCE="https://quads2.rdu3.labs.perfscale.redhat.com/instack/$LAB_CLOUD\_ocpinventory.json"
-elif [[ $LAB == "scalelab" ]]; then
-  export QUADS_INSTANCE="https://quads2.rdu2.scalelab.redhat.com/instack/$LAB_CLOUD\_ocpinventory.json"
-fi
 envsubst '${FOREMAN_OS},${LAB_CLOUD},${NUM_NODES},${QUADS_INSTANCE},${STARTING_NODE}' < /tmp/foreman-deploy.sh > /tmp/foreman-deploy_updated-$LAB_CLOUD.sh
 
 # Wait until the newly deployed servers are accessible via ssh
 cat > /tmp/foreman-wait.sh << 'EOF'
 echo 'Running foreman-wait.sh'
-for i in $(curl -sSk $QUADS_INSTANCE | jq -r ".nodes[$STARTING_NODE:$(($STARTING_NODE+$NUM_NODES))][].name"); do
+OCPINV=$QUADS_INSTANCE/instack/$LAB_CLOUD\_ocpinventory.json
+for i in $(curl -sSk $OCPINV | jq -r ".nodes[$STARTING_NODE:$(($STARTING_NODE+$NUM_NODES))][].name"); do
   while ! nc -z $i 22; do
     echo "Trying SSH port on host $i ..."
     sleep 60
