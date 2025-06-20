3ad4e4306ebe7323388b117d3e237c89c243328e
Raul Sevilla <rsevilla@redhat.com>
2025-06-20 23:54:34 +0200

    Release info
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/ping/openshift-qe-installer-bm-ping-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/ping/openshift-qe-installer-bm-ping-commands.sh
@@ -5,5 +5,7 @@ set -o pipefail
 set -x
 
 bastion=$(cat "/secret/address")
+ls -l ${CLUSTER_PROFILE_DIR}/pull_secret
+oc adm release info $RELEASE_IMAGE_LATEST -a ${CLUSTER_PROFILE_DIR}/pull_secret
 
 ping -c 5 $bastion
