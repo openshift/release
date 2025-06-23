eb9f7dccfc3b4b745d6f48438d64ae2db16c0d32
Raul Sevilla <rsevilla@redhat.com>
2025-06-23 17:08:43 +0200

    Jetlag PR 655
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/deploy/openshift-qe-installer-bm-deploy-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/deploy/openshift-qe-installer-bm-deploy-commands.sh
@@ -10,7 +10,7 @@ set -x
 SSH_ARGS="-i ${CLUSTER_PROFILE_DIR}/jh_priv_ssh_key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
 bastion=$(cat ${CLUSTER_PROFILE_DIR}/address)
 CRUCIBLE_URL=$(cat ${CLUSTER_PROFILE_DIR}/crucible_url)
-JETLAG_PR=${JETLAG_PR:-}
+JETLAG_PR=655
 REPO_NAME=${REPO_NAME:-}
 PULL_NUMBER=${PULL_NUMBER:-}
 KUBECONFIG_SRC=""
