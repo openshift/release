1843c99d7618a34eddcd891d42410bf6b9af0398
Niha Tahoor <Niha.Tahoor.Mohammed@ibm.com>
2025-06-23 13:59:31 +0530

    local-z-changes

--- ci-operator/step-registry/distributed-tracing/install/ibm-z/cluster-create/distributed-tracing-install-ibm-z-cluster-create-commands.sh
+++ ci-operator/step-registry/distributed-tracing/install/ibm-z/cluster-create/distributed-tracing-install-ibm-z-cluster-create-commands.sh
@@ -6,4 +6,28 @@ set -o pipefail
 cd /tmp
 pwd
 
-/usr/bin/openshift-ci.sh create ibmcloudz
+KUBECONFIG_FILE="${SHARED_DIR}/kubeconfig"
+PRIVATE_KEY_FILE="/root/.ssh/id_rsa"
+SSH_KEY_PATH=$PRIVATE_KEY_FILE
+
+SSH_ARGS="-i ${SSH_KEY_PATH} -o MACs=hmac-sha2-256 -o StrictHostKeyChecking=no -o LogLevel=ERROR -o UserKnownHostsFile=/dev/null"
+
+IP_JUMPHOST=128.168.131.205
+CLUSTER_VARS_PATH="/root/ocp-cluster-ibmcloud/ibmcloud-openshift-provisioning/cluster-vars"
+
+SSH_CMD=$(cat <<EOF
+export OCP_CLUSTER_VERSION="${OCP_CLUSTER_VERSION}";
+export CLUSTER_NAME="${CLUSTER_NAME}";
+export PULL_SECRET_FILE="${PULL_SECRET_FILE}";
+export IC_API_KEY="${IC_API_KEY}";
+echo "OCP_CLUSTER_VERSION=\$OCP_CLUSTER_VERSION" > $CLUSTER_VARS_PATH;
+echo "CLUSTER_NAME=\$CLUSTER_NAME" >> $CLUSTER_VARS_PATH;
+echo "PULL_SECRET_FILE=\$PULL_SECRET_FILE" >> $CLUSTER_VARS_PATH;
+echo "IC_API_KEY=\$IC_API_KEY" >> $CLUSTER_VARS_PATH;
+/root/ocp-cluster-ibmcloud/ibmcloud-openshift-provisioning/create-cluster.sh
+EOF
+)
+
+ssh $SSH_ARGS root@$IP_JUMPHOST "$SSH_CMD" > "$KUBECONFIG_FILE"
+
+KUBECONFIG="$KUBECONFIG_FILE" ./oc get nodes
