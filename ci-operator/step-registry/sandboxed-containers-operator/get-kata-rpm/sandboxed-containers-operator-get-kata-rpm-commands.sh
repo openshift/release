6fa9f897810e27424c0243b2c270f90eadb6e2b0
Wainer Moschetta <wainersm@redhat.com>
2025-06-25 14:13:33 -0300

    OSC downstream: new base-job for pre-GA testing of OSC (#66077)
    * step-registry/sandboxed-containers-operator: add debug to get-kata-rpm
    
    Print useful information to debug errors on get-kata-rpm step.
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>
    
    * step-registry/sandboxed-containers-operator: new KATA_RPM_BUILD_TASK parameter to get-kata-rpm
    
    The URL to download RPM from candidate builds changes. Added the
    KATA_RPM_BUILD_TASK parameter which refers to the the Brew task ID.
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>
    
    * ci-operator/sandboxed-containers-operator: new
    downstream-candidate job
    
    Created a base-workflow to test pre-GA builds of OSC.
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>
    
    ---------
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- ci-operator/step-registry/sandboxed-containers-operator/get-kata-rpm/sandboxed-containers-operator-get-kata-rpm-commands.sh
+++ ci-operator/step-registry/sandboxed-containers-operator/get-kata-rpm/sandboxed-containers-operator-get-kata-rpm-commands.sh
@@ -8,18 +8,25 @@
 
 cd /tmp || exit 1
 
-# Read from secrets
-KATA_RPM_BASE_URL=$(cat /usr/local/sandboxed-containers-operator-ci-secrets/secrets/KATA_RPM_BASE_URL)
-
 arch=$(uname -m)
-ver=$(echo "$KATA_RPM_VERSION" | cut -d- -f1)
-build=$(echo "$KATA_RPM_VERSION" | cut -d- -f2)
-brew_auth="$(oc get -n openshift-config secret/pull-secret -ojson  | jq -r '.data.".dockerconfigjson"' |  base64 -d | jq -r '.auths."registry.redhat.io".auth' | base64 -d)"
+if [ -n "${KATA_RPM_BUILD_TASK}" ];then
+    KATA_RPM_BASE_TASK_URL=$(cat /usr/local/sandboxed-containers-operator-ci-secrets/secrets/KATA_RPM_BUILD_TASK_BASE_URL)
+    # To the base URL it's appended the "last four digits of task ID"/"full task ID"
+    KATA_RPM_BUILD_URL="${KATA_RPM_BASE_TASK_URL}/${KATA_RPM_BUILD_TASK: -4}/${KATA_RPM_BUILD_TASK}/kata-containers-${KATA_RPM_VERSION}.${arch}.rpm"
+else
+    ver=$(echo "$KATA_RPM_VERSION" | cut -d- -f1)
+    build=$(echo "$KATA_RPM_VERSION" | cut -d- -f2)
+    KATA_RPM_BASE_URL=$(cat /usr/local/sandboxed-containers-operator-ci-secrets/secrets/KATA_RPM_BASE_URL)
+    KATA_RPM_BUILD_URL="${KATA_RPM_BASE_URL}/${ver}/${build}/${arch}/kata-containers-${KATA_RPM_VERSION}.${arch}.rpm"
+fi
 
-KATA_RPM_BUILD_URL="${KATA_RPM_BASE_URL}/${ver}/${build}/${arch}/kata-containers-${KATA_RPM_VERSION}.${arch}.rpm"
+brew_auth="$(oc get -n openshift-config secret/pull-secret -ojson  | jq -r '.data.".dockerconfigjson"' |  base64 -d | jq -r '.auths."registry.redhat.io".auth' | base64 -d)"
 
 md5sum_file="${KATA_RPM_BUILD_MD5SUM}  kata-containers.rpm"
 curl -L -k -o kata-containers.rpm -u "${brew_auth}" "${KATA_RPM_BUILD_URL}"
+echo "Debug:"
+ls -l kata-containers.rpm || true
+echo "Checking against md5sum ${KATA_RPM_BUILD_MD5SUM}"
 echo "${md5sum_file}" | md5sum -c -
 
 nodes=$(oc get node -l node-role.kubernetes.io/worker= -o name)
