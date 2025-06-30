ede7d1ada7e19fbfee65002efd5c90ab0f0dde59
José Castillo Lema <josecastillolema@gmail.com>
2025-06-30 04:08:12 -0400

    [PERFSCALE-3993] Testing self-scheduling in scalelab (#66471)
    * Testing self-scheduling in scalelab
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    * Dell R630 as the default server model
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    * Fix missing secret ref
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    * Print assignment id on terminate
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    * Enable verbose mode for term
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    * Default to curl --fail
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    * Change term to scalelab
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
    ---------
    
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/self-sched/openshift-qe-installer-bm-self-sched-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/self-sched/openshift-qe-installer-bm-self-sched-commands.sh
@@ -14,17 +14,17 @@ QUADS_INSTANCE=$(cat ${CLUSTER_PROFILE_DIR}/quads_instance_${LAB})
 set +x
 echo
 echo "Login to get token ..."
-TOKEN=$(curl -sSk -X POST -u "metal-perfscale-cpt@redhat.com:$quads_pwd" -H "Content-Type: application/json" $QUADS_INSTANCE/api/v3/login/ | jq -r .'auth_token')
+TOKEN=$(curl -fsSk -X POST -u "metal-perfscale-cpt@redhat.com:$quads_pwd" -H "Content-Type: application/json" $QUADS_INSTANCE/api/v3/login/ | jq -r .'auth_token')
 
 # Get available hosts for self scheduling from a certain hardware model
 echo
 echo "Fail if there are not enough hosts ..."
-NUM_AVAILABLE=$(curl -sSk $QUADS_INSTANCE/api/v3/available\?can_self_schedule\=true\&model=$MODEL | jq .[] | wc -l)
+NUM_AVAILABLE=$(curl -fsSk $QUADS_INSTANCE/api/v3/available\?can_self_schedule\=true\&model=$MODEL | jq .[] | wc -l)
 if [ "$NUM_SERVERS" -gt "$NUM_AVAILABLE" ]; then
   exit 1
 else
   echo "Get available hosts for self scheduling from a certain hardware model ..."
-  HOSTS=$(curl -sSk $QUADS_INSTANCE/api/v3/available\?can_self_schedule\=true\&model=$MODEL | jq .[0:$NUM_SERVERS] | jq -r .[])
+  HOSTS=$(curl -fsSk $QUADS_INSTANCE/api/v3/available\?can_self_schedule\=true\&model=$MODEL | jq .[0:$NUM_SERVERS] | jq -r .[])
   echo $HOSTS
 fi
 
@@ -32,7 +32,7 @@ fi
 if [[ $PUBLIC_VLAN == "true" ]]; then
   echo
   echo "Checking for free VLANs ..."
-  VLAN_ID=$(curl -sSk $QUADS_INSTANCE/api/v3/vlans/free | jq .[0].'vlan_id')
+  VLAN_ID=$(curl -fsSk $QUADS_INSTANCE/api/v3/vlans/free | jq .[0].'vlan_id')
   echo "The VLAN id is: $VLAN_ID"
   if [[ $VLAN_ID == "null" ]]; then
     echo "No free VLANs available"
@@ -44,9 +44,9 @@ fi
 echo
 echo "Create self scheduling assignment ..."
 if [ -z "${VLAN_ID}" ]; then
-  CLOUD_OUTPUT=$(curl -sSk -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d '{"description": "Temporary allocation from openshift-ci", "owner": "metal-perfscale-cpt", "qinq": 1, "wipe": "true"}' $QUADS_INSTANCE/api/v3/assignments/self)
+  CLOUD_OUTPUT=$(curl -fsSk -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d '{"description": "Temporary allocation from openshift-ci", "owner": "metal-perfscale-cpt", "qinq": 1, "wipe": "true"}' $QUADS_INSTANCE/api/v3/assignments/self)
 else
-  CLOUD_OUTPUT=$(curl -sSk -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d '{"description": "Temporary allocation from openshift-ci", "owner": "metal-perfscale-cpt", "qinq": 1, "vlan": "'"$VLAN_ID"'", "wipe": "true"}' $QUADS_INSTANCE/api/v3/assignments/self)
+  CLOUD_OUTPUT=$(curl -fsSk -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d '{"description": "Temporary allocation from openshift-ci", "owner": "metal-perfscale-cpt", "qinq": 1, "vlan": "'"$VLAN_ID"'", "wipe": "true"}' $QUADS_INSTANCE/api/v3/assignments/self)
 fi
 echo $CLOUD_OUTPUT | jq .
 CLOUD=$(echo $CLOUD_OUTPUT | jq -r .'cloud.name')
@@ -59,7 +59,7 @@ echo "Create schedule ..."
 for i in $HOSTS; do
   echo
   echo "Requesting allocation of host $i ..."
-  ASSIGNMENT_OUTPUT=$(curl -sSk -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d "{\"cloud\": \"$CLOUD\", \"hostname\": \"$i\"}" $QUADS_INSTANCE/api/v3/schedules)
+  ASSIGNMENT_OUTPUT=$(curl -fsSk -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d "{\"cloud\": \"$CLOUD\", \"hostname\": \"$i\"}" $QUADS_INSTANCE/api/v3/schedules)
   echo $ASSIGNMENT_OUTPUT | jq .
 done
 ASSIGNMENT_ID=$(echo $ASSIGNMENT_OUTPUT | jq -r .'assignment_id')
@@ -69,7 +69,7 @@ echo $ASSIGNMENT_ID > ${SHARED_DIR}/assignment_id
 
 # Wait for validation to be completed
 set -x
-while [[ $(curl -sSk $QUADS_INSTANCE/api/v3/assignments/$ASSIGNMENT_ID | jq -r .validated) != "true" ]]; do
+while [[ $(curl -fsSk $QUADS_INSTANCE/api/v3/assignments/$ASSIGNMENT_ID | jq -r .validated) != "true" ]]; do
   echo
   echo "Waiting for validation ..."
   sleep 60s
