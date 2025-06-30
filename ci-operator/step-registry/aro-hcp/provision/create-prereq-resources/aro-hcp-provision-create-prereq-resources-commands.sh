93f97a14cfa54c5a42883f3fb63888c079f6e3a1
David Eads <deads2k@users.noreply.github.com>
2025-06-30 13:33:20 -0400

    add credentials for creating ARO HCP integration clusters (#66526)

--- ci-operator/step-registry/aro-hcp/provision/create-prereq-resources/aro-hcp-provision-create-prereq-resources-commands.sh
+++ ci-operator/step-registry/aro-hcp/provision/create-prereq-resources/aro-hcp-provision-create-prereq-resources-commands.sh
@@ -4,6 +4,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# read the secrets and login as the user
+export TEST_USER_CLIENT_ID; TEST_USER_CLIENT_ID=$(cat /var/run/hcp-integration-credentials/client-id)
+export TEST_USER_CLIENT_SECRET; TEST_USER_CLIENT_SECRET=$(cat /var/run/hcp-integration-credentials/client-secret)
+export TEST_USER_TENANT_ID; TEST_USER_TENANT_ID=$(cat /var/run/hcp-integration-credentials/tenant)
+az login --service-principal -u "${TEST_USER_CLIENT_ID}" -p "${TEST_USER_CLIENT_SECRET}" --tenant "${TEST_USER_TENANT_ID}"
+
 
 # Defined here because it's used here.
 is_int_testing_subscription() {
