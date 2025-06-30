42a3f9a6f048f7708905f42ca27f0c062ed5a69b
Dan Manor <dmanor@redhat.com>
2025-06-30 19:41:41 +0300

    MGMT-20671: Add workflow and jobs for heterogeneous ofcir cluster installation

--- ci-operator/step-registry/assisted/ofcir/baremetal/heterogeneous/setup/assisted-ofcir-baremetal-heterogeneous-setup-commands.sh
+++ ci-operator/step-registry/assisted/ofcir/baremetal/heterogeneous/setup/assisted-ofcir-baremetal-heterogeneous-setup-commands.sh
@@ -5,13 +5,13 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-echo "************  assisted equinix teardown command ************"
+echo "************  assisted ofcir baremetal hetrogeneous setup command ************"
 
 # TODO: Remove once OpenShift CI supports it out of the box (see https://access.redhat.com/articles/4859371)
 fix_uid.sh
 
 cd "${ANSIBLE_PLAYBOOK_DIRECTORY}"
 ansible-playbook --extra-vars "@vars/ci.yml" \
-                 --extra-vars "@vars/ci_equinix_infrastucture.yml" \
+                 --extra-vars "@vars/ci_ofcir_hetrogeneous_infrastructure.yml" \
                  --extra-vars "${ANSIBLE_EXTRA_VARS}" \
-                 "${ANSIBLE_PLAYBOOK_DESTROY_INFRA}"
+                 "${ANSIBLE_PLAYBOOK_CREATE_INFRA}"
