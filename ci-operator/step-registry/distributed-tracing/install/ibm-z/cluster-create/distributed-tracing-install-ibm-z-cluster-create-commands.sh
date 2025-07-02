f55391cebba5b3b3aa28cbcaa21ffb0cfe7626bf
Jitendra Singh <jitensin@redhat.com>
2025-07-02 11:05:33 +0000

    Resolved conflicts

--- ci-operator/step-registry/distributed-tracing/install/ibm-z/cluster-create/distributed-tracing-install-ibm-z-cluster-create-commands.sh
+++ ci-operator/step-registry/distributed-tracing/install/ibm-z/cluster-create/distributed-tracing-install-ibm-z-cluster-create-commands.sh
@@ -6,23 +6,27 @@ set -o pipefail
 cd /tmp
 pwd
 
-<<<<<<< HEAD
+
+SECRET_DIR="/tmp/vault/ibmz-ci-creds"
+PRIVATE_KEY_FILE="${SECRET_DIR}/IPI_SSH_KEY"
+
+
 # Define required values these we can export in the env
-CLUSTER_VERSION=4.18.1
-CLUSTER_NAME=ocp-demo-ci-script
-PULL_SECRET_FILE='/root/ocp-pull-secrte.json'
-
-
+#CLUSTER_VERSION=4.18.1
+#CLUSTER_NAME=ocp-demo-ci-script
+#PULL_SECRET_FILE='/root/ocp-pull-secrte.json'
 
 # install kubectl
 #curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 #chmod +x kubectl
 
 # Kubeconfig file
-KUBECONFIG_FILE="/tmp/kubeconfig"
+KUBECONFIG_FILE="${SHARED_DIR}/kubeconfig"
 
 IP_JUMPHOST=128.168.131.205
 CLUSTER_VARS_PATH="/root/ocp-cluster-ibmcloud/ibmcloud-openshift-provisioning/cluster-vars"
+Add commentMore actions
+SSH_ARGS=" -i ${SSH_KEY_PATH} -o StrictHostKeyChecking=no -o LogLevel=ERROR -o UserKnownHostsFile=/dev/null"
 
 # Define SSH command just like Power script
 SSH_CMD=$(cat <<EOF
@@ -41,34 +45,6 @@ EOF
 )
 
 # Run the SSH command
-ssh root@$IP_JUMPHOST "$SSH_CMD" > "$KUBECONFIG_FILE"
+ssh $SSH_ARGS  root@$IP_JUMPHOST "$SSH_CMD" > "$KUBECONFIG_FILE"
 
 KUBECONFIG="$KUBECONFIG_FILE" ./kubectl get nodes
-=======
-# install kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
-
-SECRET_DIR="/tmp/vault/ibmz-ci-creds"
-PRIVATE_KEY_FILE="${SECRET_DIR}/IPI_SSH_KEY"
-
-# https://docs.ci.openshift.org/docs/architecture/step-registry/#sharing-data-between-steps
-KUBECONFIG_FILE="${SHARED_DIR}/kubeconfig"
-
-SSH_KEY_PATH="/tmp/id_rsa"
-SSH_ARGS="-i ${SSH_KEY_PATH} -o MACs=hmac-sha2-256 -o StrictHostKeyChecking=no -o LogLevel=ERROR -o UserKnownHostsFile=/dev/null"
-if [[ $FIPS_ENABLED == true ]]; then
-    SSH_CMD="manage-cluster.sh acquire -v ${OCP_CLUSTER_VERSION} --fips"
-else
-    SSH_CMD="manage-cluster.sh acquire -v ${OCP_CLUSTER_VERSION}"
-fi
-
-# setup ssh key
-cp -f $PRIVATE_KEY_FILE $SSH_KEY_PATH
-chmod 400 $SSH_KEY_PATH
-
-# acquire a ocp cluster
-ssh $SSH_ARGS root@128.168.131.205 "$SSH_CMD" > $KUBECONFIG_FILE
-
-KUBECONFIG="$KUBECONFIG_FILE" ./kubectl get nodes
->>>>>>> new-origin/ibmz-ci-cluster-creation
--- ci-operator/step-registry/distributed-tracing/install/ibm-z/cluster-destroy/distributed-tracing-install-ibm-z-cluster-destroy-commands.sh
+++ ci-operator/step-registry/distributed-tracing/install/ibm-z/cluster-destroy/distributed-tracing-install-ibm-z-cluster-destroy-commands.sh
@@ -6,7 +6,6 @@ set -o pipefail
 cd /tmp
 pwd
 
-<<<<<<< HEAD
 OUTPUT_FILE="/tmp/deletedcluster-logs"
 IP_JUMPHOST=128.168.131.205
 # Define SSH command just like Power script
@@ -18,34 +17,6 @@ cd /root/ocp-cluster-ibmcloud/ibmcloud-openshift-provisioning
 ./delete-cluster.sh
 EOF
 )
+SSH_ARGS=" -i ${SSH_KEY_PATH} -o StrictHostKeyChecking=no -o LogLevel=ERROR -o UserKnownHostsFile=/dev/null"
 
-
-ssh root@$IP_JUMPHOST "$SSH_CMD" > "$OUTPUT_FILE"
-=======
-# install kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
-
-SECRET_DIR="/tmp/vault/ibmz-ci-creds"
-PRIVATE_KEY_FILE="${SECRET_DIR}/IPI_SSH_KEY"
-
-# https://docs.ci.openshift.org/docs/architecture/step-registry/#sharing-data-between-steps
-KUBECONFIG_FILE="${SHARED_DIR}/kubeconfig"
-
-CLUSTER_NAME=$(KUBECONFIG="$KUBECONFIG_FILE" ./kubectl config view --minify -o jsonpath='{.clusters[].name}')
-
-SSH_KEY_PATH="/tmp/id_rsa"
-SSH_ARGS="-i ${SSH_KEY_PATH} -o MACs=hmac-sha2-256 -o StrictHostKeyChecking=no -o LogLevel=ERROR -o UserKnownHostsFile=/dev/null"
-if [[ $FIPS_ENABLED == true ]]; then
-    SSH_CMD="manage-cluster.sh release --fips --name ${CLUSTER_NAME}"
-else
-    SSH_CMD="manage-cluster.sh release --name ${CLUSTER_NAME}"
-fi
-
-# setup ssh key
-cp -f $PRIVATE_KEY_FILE $SSH_KEY_PATH
-chmod 400 $SSH_KEY_PATH
-
-# release a ocp cluster
-ssh $SSH_ARGS root@128.168.131.205 "$SSH_CMD" > $KUBECONFIG_FILE
->>>>>>> new-origin/ibmz-ci-cluster-creation
+ssh $SSH_ARGS root@$IP_JUMPHOST "$SSH_CMD" > "$OUTPUT_FILE"
