9c9beb5d5fba4177d91d79e0cc9bef1e1721f040
Prafful Kapse <pkapse@redhat.com>
2025-07-02 15:45:17 +0530

    Updated shell and added openshift-upgrade-status-commands.sh

--- ci-operator/step-registry/openshift/upgrade/status/openshift-upgrade-status-commands.sh
+++ ci-operator/step-registry/openshift/upgrade/status/openshift-upgrade-status-commands.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 set -xeuo pipefail
 
+function set_cluster_access() {
+    if [ -f "${SHARED_DIR}/kubeconfig" ] ; then
+        export KUBECONFIG=${SHARED_DIR}/kubeconfig
+	echo "KUBECONFIG: ${KUBECONFIG}"
+    fi
+    cp -Lrvf "${KUBECONFIG}" /tmp/kubeconfig
+    if [ -f "${SHARED_DIR}/proxy-conf.sh" ] ; then
+        source "${SHARED_DIR}/proxy-conf.sh"
+	echo "proxy: ${SHARED_DIR}/proxy-conf.sh"
+    fi
+}
+
+set_cluster_access
+
 echo "Displaying OpenShift upgrade status:"
 OC_ENABLE_CMD_UPGRADE_STATUS=true oc adm upgrade status
