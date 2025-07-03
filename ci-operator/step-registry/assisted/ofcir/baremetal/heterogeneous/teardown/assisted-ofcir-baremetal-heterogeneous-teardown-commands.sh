0e8cb9c737c5eb4c1c97867ea2749229fc1f3a27
Dan Manor <dmanor@redhat.com>
2025-07-03 10:21:57 +0300

    NO-ISSUE: Fix typos (#66677)

--- ci-operator/step-registry/assisted/ofcir/baremetal/heterogeneous/teardown/assisted-ofcir-baremetal-heterogeneous-teardown-commands.sh
+++ ci-operator/step-registry/assisted/ofcir/baremetal/heterogeneous/teardown/assisted-ofcir-baremetal-heterogeneous-teardown-commands.sh
@@ -5,13 +5,13 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-echo "************  assisted ofcir baremetal hetrogeneous teardown command ************"
+echo "************  assisted ofcir baremetal heterogeneous teardown command ************"
 
 # TODO: Remove once OpenShift CI supports it out of the box (see https://access.redhat.com/articles/4859371)
 fix_uid.sh
 
 cd "${ANSIBLE_PLAYBOOK_DIRECTORY}"
 ansible-playbook --extra-vars "@vars/ci.yml" \
-                 --extra-vars "@vars/ci_ofcir_hetrogeneous_infrastructure.yml" \
+                 --extra-vars "@vars/ci_ofcir_heterogeneous_infrastructure.yml" \
                  --extra-vars "${ANSIBLE_EXTRA_VARS}" \
                  "${ANSIBLE_PLAYBOOK_DESTROY_INFRA}"
