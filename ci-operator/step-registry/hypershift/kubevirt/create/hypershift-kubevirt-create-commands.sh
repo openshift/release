0f4c802d1cfc67ef364789fae93381b44d0561d4
Zheng Feng <zh.feng@gmail.com>
2025-07-04 10:40:06 +0800

    test

--- ci-operator/step-registry/hypershift/kubevirt/create/hypershift-kubevirt-create-commands.sh
+++ ci-operator/step-registry/hypershift/kubevirt/create/hypershift-kubevirt-create-commands.sh
@@ -216,7 +216,9 @@ else
     --control-plane-availability-policy ${CONTROL_PLANE_AVAILABILITY} \
     --infra-availability-policy ${INFRA_AVAILABILITY} \
     --service-cidr 172.32.0.0/16 \
-    --cluster-cidr 10.136.0.0/14  $(support_np_skew)"
+    --cluster-cidr 10.136.0.0/14 --render --render-sensitive > /tmp/hc.yaml"
+
+  wait 3600
 fi
 
 
