901e4331adf58017bef8c7545ac8506d46e5b8e7
Maysa De Macedo Souza <maysa.macedo95@gmail.com>
2025-07-05 17:23:06 -0300

    Include Kueue periodic jobs (#66496)
    This commit includes periodic jobs for Kueue in
    OCP cluster with 4.18 and 4.19. It uses the images
    from the Operator and Operand that are available in
    the bundle. Also, the job failures are recorded in the
    forum-node-jira channel.

--- ci-operator/step-registry/kueue-operator/image-patch/kueue-operator-image-patch-commands.sh
+++ ci-operator/step-registry/kueue-operator/image-patch/kueue-operator-image-patch-commands.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+if [[ "$JOB_TYPE" == *"periodic"* ]]; then
+    echo "Skipping step due to JOB_TYPE matching $JOB_TYPE"
+    exit 0
+fi
+
 source "${SHARED_DIR}/env"
 echo "Using Operator Image: ${OPERATOR_IMAGE}"
 echo "Using Bundle Image: ${BUNDLE_IMAGE}"
