a495268f1ab353225012cfde4776b51480b5b975
Patricia Salajova <salajova.p@gmail.com>
2025-07-09 15:39:57 +0200

    Print file content

--- ci-operator/step-registry/psalajova/echo/echo/psalajova-echo-echo-commands.sh
+++ ci-operator/step-registry/psalajova/echo/echo/psalajova-echo-echo-commands.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "Pre-step executing: This should list 1 file in my-secrets dir."
+echo "Pre-step executing: This should list 1 file in my-secrets dir, and its content."
 
 SECRETS_DIR="/var/my-secrets"
 for file in "$SECRETS_DIR"/*; do
   if [ -f "$file" ]; then
-    echo "Processing: $file"
+    echo "Secret content:"
+    cat "$file"
+    echo "--- End of secret content ---"
   fi
 done
 
-sleep 30m
+sleep 10m
