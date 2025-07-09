ee52f3af49d3d0806d343338c3c4a1e8983d72bd
Mohammed Tahoor <mtahoor@redhat.com>
2025-07-09 16:49:16 +0530

    Update distributed-tracing-install-ibm-z-cluster-destroy-commands.sh

--- ci-operator/step-registry/distributed-tracing/install/ibm-z/cluster-destroy/distributed-tracing-install-ibm-z-cluster-destroy-commands.sh
+++ ci-operator/step-registry/distributed-tracing/install/ibm-z/cluster-destroy/distributed-tracing-install-ibm-z-cluster-destroy-commands.sh
@@ -2,21 +2,25 @@
 
 set -o errexit
 set -o pipefail
+set -e
 
 cd /tmp
 pwd
 
+#give the path for your sys key.
+SSH_KEY_PATH="/tmp/my_sys_key" 
 OUTPUT_FILE="/tmp/deletedcluster-logs"
 IP_JUMPHOST=128.168.131.205
+SSH_ARGS=" -i ${SSH_KEY_PATH} -o StrictHostKeyChecking=no -o LogLevel=ERROR -o UserKnownHostsFile=/dev/null"
+
+
 # Define SSH command just like Power script
 SSH_CMD=$(cat <<EOF
 set -e
-
-
 cd /root/ocp-cluster-ibmcloud/ibmcloud-openshift-provisioning
 ./delete-cluster.sh
 EOF
 )
-SSH_ARGS=" -i ${SSH_KEY_PATH} -o StrictHostKeyChecking=no -o LogLevel=ERROR -o UserKnownHostsFile=/dev/null"
 
-ssh $SSH_ARGS root@$IP_JUMPHOST "$SSH_CMD" > "$OUTPUT_FILE"
+
+ssh $SSH_ARGS root@$IP_JUMPHOST "$SSH_CMD" >  "$OUTPUT_FILE"
