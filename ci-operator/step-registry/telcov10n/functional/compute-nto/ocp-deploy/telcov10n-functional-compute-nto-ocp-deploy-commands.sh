40381727aefa3c24ef904b06637ded6d18eca9b2
Carlos Cardenosa <ccardeno@redhat.com>
2025-07-10 15:24:32 +0200

    Cluster deployment in NTO env - Prow
    Signed-off-by: Carlos Cardenosa <ccardeno@redhat.com>

---
+++ ci-operator/step-registry/telcov10n/functional/compute-nto/ocp-deploy/telcov10n-functional-compute-nto-ocp-deploy-commands.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e
+set -o pipefail
+MOUNTED_HOST_INVENTORY="/var/host_variables"
+
+process_inventory() {
+    local directory="$1"
+    local dest_file="$2"
+
+    if [ -z "$directory" ]; then
+        echo "Usage: process_inventory <directory> <dest_file>"
+        return 1
+    fi
+
+    if [ ! -d "$directory" ]; then
+        echo "Error: '$directory' is not a valid directory"
+        return 1
+    fi
+
+    find "$directory" -type f | while IFS= read -r filename; do
+        if [[ $filename == *"secretsync-vault-source-path"* ]]; then
+          continue
+        else
+          echo "$(basename "${filename}")": \'"$(cat "$filename")"\'
+        fi
+    done > "${dest_file}"
+
+    echo "Processing complete. Check \"${dest_file}\""
+}
+
+echo "Set CLUSTER_NAME env var"
+if [[ -f "${SHARED_DIR}/cluster_name" ]]; then
+    CLUSTER_NAME=$(cat "${SHARED_DIR}/cluster_name")
+fi
+export CLUSTER_NAME=${CLUSTER_NAME}
+echo CLUSTER_NAME="${CLUSTER_NAME}"
+
+echo "Create group_vars directory"
+mkdir -p /eco-ci-cd/inventories/ocp-deployment/group_vars
+
+find /var/group_variables/common/ -mindepth 1 -type d | while read -r dir; do
+    echo "Process group inventory file: ${dir}"
+    process_inventory "$dir" /eco-ci-cd/inventories/ocp-deployment/group_vars/"$(basename "${dir}")"
+done
+
+echo "Create host_vars directory"
+mkdir -p /eco-ci-cd/inventories/ocp-deployment/host_vars
+
+echo "Load network mutation env variablies if present"
+if [[ -f "${SHARED_DIR}/set_ocp_net_vars.sh" ]]; then
+    # shellcheck source=/dev/null
+    source "${SHARED_DIR}/set_ocp_net_vars.sh"
+fi
+
+cd /eco-ci-cd
+echo "Deploy OCP for compute-nto testing"
+ansible-playbook ./playbooks/deploy-ocp-hybrid-multinode.yml -i ./inventories/ocp-deployment/build-inventory.py \
+    --extra-vars "release=${VERSION} cluster_name=${CLUSTER_NAME} kubeconfig=/home/telcov10n/project/generated/${CLUSTER_NAME}/auth/kubeconfig"
+
+echo "Store inventory in SHARED_DIR"
+cp -r /eco-ci-cd/inventories/ocp-deployment/host_vars/* "${SHARED_DIR}"/
+cp -r /eco-ci-cd/inventories/ocp-deployment/group_vars/* "${SHARED_DIR}"/
