42f1ae9ee19d4a0f0a64dc62807a43d08a61d044
Carlos Cardenosa <ccardeno@redhat.com>
2025-07-10 15:24:32 +0200

    Cluster deployment in NTO env - Prow
    Signed-off-by: Carlos Cardenosa <ccardeno@redhat.com>

---
+++ ci-operator/step-registry/telcov10n/functional/compute-nto/setup-cluster-env/telcov10n-functional-compute-nto-setup-cluster-env-commands.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+echo "Setup compute-nto pipeline environment"
+cd /eco-ci-cd/
+export ANSIBLE_REMOTE_TEMP="/tmp"
+set -x
+ansible-playbook ./playbooks/setup-cluster-env.yml --extra-vars "release=${VERSION} dest_directory=${SHARED_DIR}"
+set +x
+
+echo "Cluster name"
+cat ${SHARED_DIR}/cluster_name
+
+echo "OCP primary nic"
+cat ${SHARED_DIR}/ocp_nic
+
+echo "OCP secondary nic"
+cat ${SHARED_DIR}/secondary_nic 
