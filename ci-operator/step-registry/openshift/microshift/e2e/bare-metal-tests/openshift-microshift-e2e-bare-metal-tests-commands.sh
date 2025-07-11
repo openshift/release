7e8ef20d0a10ca51b8b11a20881ceb6841ebb041
Patryk Matuszak <pmatusza@redhat.com>
2025-07-11 18:02:56 +0200

    Bare metal tests: lvm only for MicroShift 4.14 and 4.15 (#66974)

--- ci-operator/step-registry/openshift/microshift/e2e/bare-metal-tests/openshift-microshift-e2e-bare-metal-tests-commands.sh
+++ ci-operator/step-registry/openshift/microshift/e2e/bare-metal-tests/openshift-microshift-e2e-bare-metal-tests-commands.sh
@@ -47,6 +47,21 @@ if [[ "${SUITE}" == "nvidia-device-plugin" ]]; then
 
     ssh "${INSTANCE_PREFIX}" \
         "cp -r /home/${HOST_USER}/microshift-main/scripts/ci-nvidia-device-plugin /home/${HOST_USER}/microshift/scripts/ci-nvidia-device-plugin/"
+
+    if [[ "${JOB_NAME}" == *"release-4.14"* || "${JOB_NAME}" == *"release-4.15"* ]]; then
+        # MicroShift 4.14 and 4.15 healthcheck scripts do not detect missing LVM setup and always expect CSI to be present. For those version, we setup LVM.
+        # Step infra-lvm-install cannot be used because bare-metal-tests step is shared with bare metal instances which are not set up for LVM (missing partitions).
+
+        # Sometimes, devices are in different order and nvme1 stores OS while nvme0 should hold LVM for topolvm.
+        # If /dev/nvme0 is already partitioned (operating system), then use nvme1 for lvm.
+        # If `partx /dev/nvme0n1` fails (rc=1), it couldn't read partition table, so it's the one to use for lvm.
+        if ssh "${INSTANCE_PREFIX}" "sudo partx /dev/nvme0n1"; then
+            device="/dev/nvme1n1"
+        else
+            device="/dev/nvme0n1"
+        fi
+        ssh "${INSTANCE_PREFIX}" "lsblk ; sudo dnf install -y lvm2 && sudo pvcreate ${device} && sudo vgcreate rhel ${device}"
+    fi
 fi
 
 setup_ok=true
