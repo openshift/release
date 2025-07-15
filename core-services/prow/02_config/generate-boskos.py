823161ed376e9ade04fdc866ee093b379b611e2b
Justin Pierce <jupierce@redhat.com>
2025-07-15 17:34:10 -0400

    Revert "reduce the amount of resource slice requests for the aws account (#66…" (#67085)
    This reverts commit d63617dd5ab6910803000e43618a6940e3072cc4.

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -6,10 +6,11 @@ import yaml
 CONFIG = {
 
     'aws-quota-slice': {
-        'us-east-1': 35,
-        'us-east-2': 30,
-        'us-west-1': 30,
-        'us-west-2': 30,
+        # Wild guesses.  We'll see when we hit quota issues
+        'us-east-1': 50,
+        'us-east-2': 35,
+        'us-west-1': 35,
+        'us-west-2': 35,
     },
     'aws-1-qe-quota-slice': {
         'us-east-1': 10,
