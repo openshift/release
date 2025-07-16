b94b4034b3303fb80f20f6f1975cdc5a3ba96601
Omri Bar-Haim <obarhaim@redhat.com>
2025-07-16 14:13:53 +0300

    Fix STOP_ON_FAILURE param in opendatahub tests

--- ci-operator/step-registry/rhoai/opendatahub-tests/rhoai-opendatahub-tests-commands.sh
+++ ci-operator/step-registry/rhoai/opendatahub-tests/rhoai-opendatahub-tests-commands.sh
@@ -47,10 +47,10 @@ if [ -n "${TEST_SELECTORS}" ]; then
     RUN_COMMAND+=" -k ${TEST_SELECTORS} "
 fi
 
-if [ "${STOP_ON_FAILURE}" = "false" ]; then
-    RUN_COMMAND+=" || true "
-fi
-
 echo "$RUN_COMMAND"
 
-${RUN_COMMAND}
+if [ "${STOP_ON_FAILURE}" = "false" ]; then
+  "${RUN_COMMAND}" || true
+else
+  "${RUN_COMMAND}"
+fi
