d72275b5b55f1d71d8e8be00b37f072efc59d3c1
David Eads <deads@redhat.com>
2025-07-16 16:03:38 -0400

    sweep all known resource groups to do data collection

--- ci-operator/step-registry/aro-hcp/provision/aro-hcp-gather-extra/aro-hcp-provision-aro-hcp-gather-extra-commands.sh
+++ ci-operator/step-registry/aro-hcp/provision/aro-hcp-gather-extra/aro-hcp-provision-aro-hcp-gather-extra-commands.sh
@@ -10,26 +10,36 @@ export TEST_USER_CLIENT_SECRET; TEST_USER_CLIENT_SECRET=$(cat /var/run/hcp-integ
 export TEST_USER_TENANT_ID; TEST_USER_TENANT_ID=$(cat /var/run/hcp-integration-credentials/tenant)
 az login --service-principal -u "${TEST_USER_CLIENT_ID}" -p "${TEST_USER_CLIENT_SECRET}" --tenant "${TEST_USER_TENANT_ID}"
 
-mkdir "${ARTIFACT_DIR}/${CUSTOMER_RG_NAME}"
-az deployment group list --resource-group "${CUSTOMER_RG_NAME}" -o yaml > "${ARTIFACT_DIR}/${CUSTOMER_RG_NAME}/deployment-group-list.yaml"
+ls -al "${SHARED_DIR}"/
+for file in "${SHARED_DIR}"/tracked-resource-group_*; do
+    if [ -f "$file" ]; then
+        full_filename=$(basename "$file")
+        resource_group_name=${full_filename#tracked-resource-group_}
+        mkdir "${ARTIFACT_DIR}/${resource_group_name}"
 
-# List all deployments in the specified resource group and extract their names
-deployment_names=$(az deployment group list --resource-group "${CUSTOMER_RG_NAME}" --query "[].name" -o tsv)
+        az deployment group list --resource-group "${resource_group_name}" -o yaml > "${ARTIFACT_DIR}/${resource_group_name}/deployment-group-list.yaml"
 
-# Check if any deployments were found
-if [ -z "$deployment_names" ]; then
-    echo "No deployments found in resource group: ${CUSTOMER_RG_NAME}"
-    exit 0
-fi
+        # List all deployments in the specified resource group and extract their names
+        deployment_names=$(az deployment group list --resource-group "${resource_group_name}" --query "[].name" -o tsv)
 
-echo "Deployment operations for resource group: ${CUSTOMER_RG_NAME}"
-echo "--------------------------------------------------------"
+        # Check if any deployments were found
+        if [ -z "$deployment_names" ]; then
+            echo "No deployments found in resource group: ${resource_group_name}"
+            exit 0
+        fi
 
-# Loop through each deployment name and list its operations
-for deployment_name in $deployment_names; do
-    az deployment operation group list --resource-group "${CUSTOMER_RG_NAME}" --name "$deployment_name" -o yaml > "${ARTIFACT_DIR}/${CUSTOMER_RG_NAME}/deployment-operation-${deployment_name}.yaml"
+        echo "Deployment operations for resource group: ${resource_group_name}"
+        echo "--------------------------------------------------------"
 
-    echo "Deployment: $deployment_name"
-    az deployment operation group list --resource-group "${CUSTOMER_RG_NAME}" --name "$deployment_name" -o table
-    echo "--------------------------------------------------------"
+        # Loop through each deployment name and list its operations
+        for deployment_name in $deployment_names; do
+            az deployment operation group list --resource-group "${resource_group_name}" --name "$deployment_name" -o yaml > "${ARTIFACT_DIR}/${resource_group_name}/deployment-operation-${deployment_name}.yaml"
+
+            echo "Deployment: $deployment_name"
+            az deployment operation group list --resource-group "${resource_group_name}" --name "$deployment_name" -o table
+            echo "--------------------------------------------------------"
+        done
+    fi
 done
+
+
