4a80ad19eae60c419fb7dac0ce13cc29d0ccc9bf
David Eads <deads2k@users.noreply.github.com>
2025-07-16 11:13:55 -0400

    unset goflags to allow non-vendoring in ARO-HCP (#67120)

--- ci-operator/step-registry/aro-hcp/tests/run-aro-hcp-tests/aro-hcp-tests-run-aro-hcp-tests-commands.sh
+++ ci-operator/step-registry/aro-hcp/tests/run-aro-hcp-tests/aro-hcp-tests-run-aro-hcp-tests-commands.sh
@@ -4,12 +4,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+unset GOFLAGS
 make -C test/
-
 
 export AZURE_CLIENT_ID; AZURE_CLIENT_ID=$(cat /var/run/hcp-integration-credentials/client-id)
 export AZURE_CLIENT_SECRET; AZURE_CLIENT_SECRET=$(cat /var/run/hcp-integration-credentials/client-secret)
 export AZURE_TENANT_ID; AZURE_TENANT_ID=$(cat /var/run/hcp-integration-credentials/tenant)
-
 
 ./test/aro-hcp-tests run-suite integration/parallel
