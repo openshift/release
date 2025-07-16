355d6f566a7fccaa5ef4e780e7477369e1e9352c
Xiuwang <xiuwang@Xiuwangs-MacBook-Pro.local>
2025-07-16 09:03:38 +0800

    Test 54720

--- ci-operator/step-registry/hypershift/mce/agent/create/hostedcluster/hypershift-mce-agent-create-hostedcluster-commands.sh
+++ ci-operator/step-registry/hypershift/mce/agent/create/hostedcluster/hypershift-mce-agent-create-hostedcluster-commands.sh
@@ -22,7 +22,6 @@ support_np_skew() {
     fi
   fi
   extra_flags+="&& /tmp/yq e -i '(select(.kind == \"NodePool\").spec.release.image) = \"$NODEPOOL_RELEASE_IMAGE_LATEST\"' /tmp/hc.yaml "
-  extra_flags+="&& oc apply -f /tmp/hc.yaml"
   echo "$extra_flags"
 }
 
@@ -106,7 +105,10 @@ eval "/tmp/${HYPERSHIFT_NAME} create cluster agent ${EXTRA_ARGS} \
   --api-server-address=api.${CLUSTER_NAME}.${BASEDOMAIN} \
   --image-content-sources ${SHARED_DIR}/mgmt_icsp.yaml \
   --ssh-key=${SHARED_DIR}/id_rsa.pub \
-  --release-image ${RELEASE_IMAGE} $(support_np_skew)"
+  --release-image ${RELEASE_IMAGE} --render-sensitive --render > /tmp/hc.yaml"
+
+cat /tmp/hc.yaml
+sleep 3600
 
 echo "Waiting for cluster to become available"
 oc wait --timeout=30m --for=condition=Available --namespace=local-cluster hostedcluster/${CLUSTER_NAME}
