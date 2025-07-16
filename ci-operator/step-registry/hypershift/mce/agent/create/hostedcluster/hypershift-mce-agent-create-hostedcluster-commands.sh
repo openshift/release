b116693a4260245ae12be9ccaa75ed630b9d07ee
Xiuwang <xiuwang@Xiuwangs-MacBook-Pro.local>
2025-07-16 09:03:38 +0800

    Test 54720

--- ci-operator/step-registry/hypershift/mce/agent/create/hostedcluster/hypershift-mce-agent-create-hostedcluster-commands.sh
+++ ci-operator/step-registry/hypershift/mce/agent/create/hostedcluster/hypershift-mce-agent-create-hostedcluster-commands.sh
@@ -17,7 +17,6 @@ support_np_skew() {
     extra_flags+=$( (( $(awk 'BEGIN {print ("'"$MCE_VERSION"'" > 2.6)}') )) && echo "--render-sensitive --render > /tmp/hc.yaml " || echo "--render > /tmp/hc.yaml " )
   fi
   extra_flags+="&& /tmp/yq e -i '(select(.kind == \"NodePool\").spec.release.image) = \"$NODEPOOL_RELEASE_IMAGE_LATEST\"' /tmp/hc.yaml "
-  extra_flags+="&& oc apply -f /tmp/hc.yaml"
   echo "$extra_flags"
 }
 
@@ -107,7 +106,10 @@ eval "/tmp/${HYPERSHIFT_NAME} create cluster agent ${EXTRA_ARGS} \
   --api-server-address=api.${CLUSTER_NAME}.${BASEDOMAIN} \
   --image-content-sources ${SHARED_DIR}/mgmt_icsp.yaml \
   --ssh-key=${SHARED_DIR}/id_rsa.pub \
-  --release-image ${RELEASE_IMAGE} $(support_np_skew)"
+  --release-image ${RELEASE_IMAGE} --render-sensitive --render > /tmp/hc.yaml"
+
+cat /tmp/hc.yaml
+sleep 3600
 
 if (( $(awk 'BEGIN {print ("'"$MCE_VERSION"'" < 2.4)}') )); then
   echo "MCE version is less than 2.4"
