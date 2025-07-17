5d20ea459919a7ebe9c1417492a5805c9cd3c9af
memodi <memodi@redhat.com>
2025-07-16 22:22:05 -0400

    update path

--- ci-operator/step-registry/netobserv-cli/tests/netobserv-cli-tests-commands.sh
+++ ci-operator/step-registry/netobserv-cli/tests/netobserv-cli-tests-commands.sh
@@ -4,12 +4,12 @@
 set -x
 short_sha=$(git rev-parse --short HEAD)
 USER=netobserv VERSION=$short_sha make commands
+export PATH=$PATH:$PWD/build
 GINKGO_VERSION=$(go list -mod=readonly -m -f '{{ .Version }}'  github.com/onsi/ginkgo/v2)
 export GINKGO_VERSION
 # export tmpHome="/tmp/home"
 # mkdir -p $tmpHome
 # export GOBIN=$PATH:$tmpHome
-# export PATH=$PATH:$PWD/build:$tmpHome
 # download the ginkgo cli binary
 go install github.com/onsi/ginkgo/v2/ginkgo@$GINKGO_VERSION
 ginkgo version
