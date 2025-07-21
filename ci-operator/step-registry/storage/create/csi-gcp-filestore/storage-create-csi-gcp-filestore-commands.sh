c6dc8e7c50d8fa9e8119a18a38750a1f418914f6
Penghao <pewang@redhat.com>
2025-07-21 17:00:25 +0800

    Fix: gcp filestore csi storageclass config (#67057)
    Signed-off-by: Penghao <pewang@redhat.com>

--- ci-operator/step-registry/storage/create/csi-gcp-filestore/storage-create-csi-gcp-filestore-commands.sh
+++ ci-operator/step-registry/storage/create/csi-gcp-filestore/storage-create-csi-gcp-filestore-commands.sh
@@ -11,32 +11,38 @@ set -o pipefail
 
 if [[ -f "${SHARED_DIR}/proxy-conf.sh" ]]
 then
-	# shellcheck disable=SC1090
+	# shellcheck disable=SC1091
 	source "${SHARED_DIR}/proxy-conf.sh"
 fi
 
-CLUSTER_NAME="$(oc get -o jsonpath='{.status.infrastructureName}{"\n"}' infrastructure cluster)"
-if [[ -s "${SHARED_DIR}/xpn.json" ]]
-then
-	echo "Reading variables from 'xpn_project_setting.json'..."
-	cat ${CLUSTER_PROFILE_DIR}/xpn_project_setting.json
-	HOST_PROJECT=$(jq -r '.hostProject' "${CLUSTER_PROFILE_DIR}/xpn_project_setting.json")
-	HOST_PROJECT_NETWORK=$(jq -r '.clusterNetwork' "${CLUSTER_PROFILE_DIR}/xpn_project_setting.json")
-	NETWORK=$(basename ${HOST_PROJECT_NETWORK})
-	NETWORK_NAME=projects/${HOST_PROJECT}/global/networks/${NETWORK}
-else 
-	NETWORK_NAME="$CLUSTER_NAME-network"
-fi
-
-export CLUSTER_NAME
-export NETWORK_NAME
 export STORAGECLASS_LOCATION=${SHARED_DIR}/filestore-sc.yaml
 export MANIFEST_LOCATION=${SHARED_DIR}/${TEST_CSI_DRIVER_MANIFEST}
+export CONNECT_MODE="DIRECT_PEERING"
+
+CLUSTER_NAME="$(oc get -o jsonpath='{.status.infrastructureName}{"\n"}' infrastructure cluster)"
+NETWORK_NAME="${CLUSTER_NAME}-network"
+
+if [[ -s "${SHARED_DIR}/install-config.yaml" ]]; then
+  install_config_network="$(yq-go r "${SHARED_DIR}/install-config.yaml" 'platform.gcp.network' 2>/dev/null)"
+  if [[ -n "$install_config_network" ]]; then
+    NETWORK_NAME="$install_config_network"
+    echo "Getting NETWORK_NAME=\"${NETWORK_NAME}\" from install-config"
+  fi
+fi
+
+# Cross-Project Networking(XPN) extra configure
+if [[ -s "${SHARED_DIR}/xpn.json" ]]; then
+  CONNECT_MODE="PRIVATE_SERVICE_ACCESS"
+  SHARED_NETWORK_PROJECT=$(yq-go r "${SHARED_DIR}"/install-config.yaml 'platform.gcp.networkProjectID')
+  # Using projects/<project-id>/global/networks/<network-name> is required for filestore CSI driver when the network is not in the current project.
+  NETWORK_NAME=projects/${SHARED_NETWORK_PROJECT}/global/networks/${NETWORK_NAME}
+  echo "XPN cluster, getting NETWORK_NAME=\"${NETWORK_NAME}\" full path"
+fi
 
 # Create StorageClass
 # shared vpc, parameter should add "connect-mode: PRIVATE_SERVICE_ACCESS"
 echo "Creating a StorageClass"
-cat <<EOF >>$STORAGECLASS_LOCATION
+cat <<EOF >>"$STORAGECLASS_LOCATION"
 apiVersion: storage.k8s.io/v1
 kind: StorageClass
 metadata:
@@ -45,21 +51,16 @@ provisioner: filestore.csi.storage.gke.io
 volumeBindingMode: WaitForFirstConsumer
 allowVolumeExpansion: true
 parameters:
-  connect-mode: DIRECT_PEERING
+  connect-mode: $CONNECT_MODE
   network: $NETWORK_NAME
   reserved-ipv4-cidr: 10.192.0.0/16 # GCE will pick NFS server addresses from this range, should not collide with the defaul OCP networking config
   labels: kubernetes-io-cluster-$CLUSTER_NAME=owned
 EOF
-
-if [[ -s "${SHARED_DIR}/xpn.json" ]]
-then
-	sed -i 's/DIRECT_PEERING/PRIVATE_SERVICE_ACCESS/' $STORAGECLASS_LOCATION 
-fi
 	
 echo "Using StorageClass file ${STORAGECLASS_LOCATION}"
-cat ${STORAGECLASS_LOCATION}
+cat "${STORAGECLASS_LOCATION}"
 
-oc create -f ${STORAGECLASS_LOCATION}
+oc create -f "${STORAGECLASS_LOCATION}"
 echo "Created StorageClass from file ${STORAGECLASS_LOCATION}"
 
 oc create -f - <<EOF
@@ -70,12 +71,11 @@ metadata:
 spec:
   managementState: Managed
 EOF
-
 echo "Created ClusterCSIDriver object"
 
 # Create test manifest
 echo "Creating a manifest file"
-cat <<EOF >>$MANIFEST_LOCATION
+cat <<EOF >>"$MANIFEST_LOCATION"
 ShortName: filestore-csi
 StorageClass:
   FromExistingClassName: filestore-csi
@@ -109,6 +109,4 @@ Timeouts:
 EOF
 
 echo "Using manifest file ${MANIFEST_LOCATION}"
-cat ${MANIFEST_LOCATION}
-
-oc get sc/filestore-csi -o yaml
+cat "${MANIFEST_LOCATION}"
