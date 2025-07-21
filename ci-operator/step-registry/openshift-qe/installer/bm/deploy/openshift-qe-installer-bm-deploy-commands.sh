37f8012598d76c4da565ad4b6887010ba75477c9
Jose Castillo Lema <josecastillolema@gmail.com>
2025-07-21 12:04:56 +0200

    Correct var name
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/deploy/openshift-qe-installer-bm-deploy-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/deploy/openshift-qe-installer-bm-deploy-commands.sh
@@ -88,7 +88,7 @@ fi
 envsubst < /tmp/all.yml > /tmp/all-updated.yml
 
 # Copy the ssh key to the bastion host
-OCPINV=$QUADS_INSTANCE/instack/$CLOUD\_ocpinventory.json
+OCPINV=$QUADS_INSTANCE/instack/$LAB_CLOUD\_ocpinventory.json
 bastion=$(curl -sSk $OCPINV | jq -r ".nodes[0].name")
 sshpass -p $LOGIN ssh-copy-id -o StrictHostKeyChecking=no root@${bastion}
 ssh -p $LOGIN -o StrictHostKeyChecking=no root@${bastion} hostname
