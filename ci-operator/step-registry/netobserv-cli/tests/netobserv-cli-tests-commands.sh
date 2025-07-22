caced49443a80ab0fb3820dbbb254c06599da639
Mehul Modi <memodi@redhat.com>
2025-07-22 11:37:41 -0400

    Update ci-operator/step-registry/netobserv-cli/tests/netobserv-cli-tests-commands.sh
    Co-authored-by: Julien Pinsonneau <91894519+jpinsonneau@users.noreply.github.com>

--- ci-operator/step-registry/netobserv-cli/tests/netobserv-cli-tests-commands.sh
+++ ci-operator/step-registry/netobserv-cli/tests/netobserv-cli-tests-commands.sh
@@ -7,7 +7,6 @@ export PATH=$PATH:$PWD/build
 GINKGO_VERSION=$(go list -mod=readonly -m -f '{{ .Version }}'  github.com/onsi/ginkgo/v2)
 export GINKGO_VERSION
 # download the ginkgo cli binary
-go mod tidy
 go install -mod=mod github.com/onsi/ginkgo/v2/ginkgo@$GINKGO_VERSION
 ginkgo version
 test -n "${KUBECONFIG:-}" && echo "${KUBECONFIG}" || echo "no KUBECONFIG is defined"
