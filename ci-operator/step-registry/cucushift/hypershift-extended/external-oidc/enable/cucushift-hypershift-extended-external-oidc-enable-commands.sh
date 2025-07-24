1e1cfa058d092440caab8ae4cc2e3da52a7c57f4
Martin Gencur <mgencur@redhat.com>
2025-07-24 12:13:57 +0200

    Apply the original to be able to check CRDs

--- ci-operator/step-registry/cucushift/hypershift-extended/external-oidc/enable/cucushift-hypershift-extended-external-oidc-enable-commands.sh
+++ ci-operator/step-registry/cucushift/hypershift-extended/external-oidc/enable/cucushift-hypershift-extended-external-oidc-enable-commands.sh
@@ -67,7 +67,8 @@ yq-v4 'select(.kind == "HostedCluster") *= load("/tmp/patch.yaml")' "${SHARED_DI
     > "${SHARED_DIR}"/hypershift_create_cluster_render_ext_oidc_enabled.yaml
 
 echo "Applying patched artifacts"
-oc apply -f "${SHARED_DIR}"/hypershift_create_cluster_render_ext_oidc_enabled.yaml
+# TMP: Apply the original rendered yaml file
+oc apply -f "${SHARED_DIR}"/hypershift_create_cluster_render.yaml
 
 HOSTED_CLUSTER_RELEASE=$(yq-v4 'select(.kind == "HostedCluster") | .spec.release.image' "${SHARED_DIR}"/hypershift_create_cluster_render.yaml)
 HOSTED_CLUSTER_NAME=$(yq-v4 'select(.kind == "HostedCluster") | .metadata.name' "${SHARED_DIR}"/hypershift_create_cluster_render.yaml)
