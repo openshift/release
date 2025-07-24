536bc1bc796f3729bd946ea43bcec3baf6286817
Li Zhang <lzha@redhat.com>
2025-07-24 14:57:01 +0800

    add quay-version env

--- ci-operator/step-registry/quay-tests/test/quay-api/quay-tests-test-quay-api-commands.sh
+++ ci-operator/step-registry/quay-tests/test/quay-api/quay-tests-test-quay-api-commands.sh
@@ -5,22 +5,6 @@ set -euo pipefail
 QUAY_USERNAME=$(cat /var/run/quay-qe-quay-secret/username)
 QUAY_PASSWORD=$(cat /var/run/quay-qe-quay-secret/password)
 
-declare -A versions=(
-    ["stable-3.12"]="3.12"
-    ["stable-3.13"]="3.13"
-    ["stable-3.14"]="3.14"
-    ["stable-3.15"]="3.15"
-    ["stable-3.16"]="3.16"
-    ["stable-3.17"]="3.17"
-)
-
-QUAY_VERSION="${versions[${QUAY_OPERATOR_CHANNEL}]:-}"
-if [[ -z "$QUAY_VERSION" ]]; then
-    echo "Unknown QUAY_OPERATOR_CHANNEL: ${QUAY_OPERATOR_CHANNEL}" >&2
-    exit 1
-fi
-export CYPRESS_QUAY_VERSION="$QUAY_VERSION"
-
 echo "Running Quay Automation API testing cases......"
 cd quay-api-tests
 QUAY_ROUTE=$(cat "$SHARED_DIR"/quayroute)
@@ -29,6 +13,7 @@ QUAY_APP_HOSTNAME=${QUAY_ROUTE#*//}
 export CYPRESS_QUAY_ENDPOINT="$QUAY_APP_HOSTNAME"
 export CYPRESS_QUAY_USER="$QUAY_USERNAME"
 export CYPRESS_QUAY_PASSWORD="$QUAY_PASSWORD"
+export CYPRESS_QUAY_VERSION="$QUAY_VERSION"
 
 yarn install
 NO_COLOR=1 yarn run cypress run --spec "cypress/e2e/quay_api_testing_all.cy.js" --browser electron --headless --reporter cypress-multi-reporters --reporter-options configFile=reporter-config.json > quay_api_testing_report || true
