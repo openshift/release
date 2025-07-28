c4968720ea04f207a0a3952e478445da1bb44d77
Zbyněk Drápela <zbynek.drapela@gmail.com>
2025-07-28 15:51:14 +0200

    Update redhat-developer-rhdh-eks-mapt-orphaned-destroy-commands.sh

--- ci-operator/step-registry/redhat-developer/rhdh/eks/mapt/orphaned/destroy/redhat-developer-rhdh-eks-mapt-orphaned-destroy-commands.sh
+++ ci-operator/step-registry/redhat-developer/rhdh/eks/mapt/orphaned/destroy/redhat-developer-rhdh-eks-mapt-orphaned-destroy-commands.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-set -e
+OVERALL_EXIT_CODE=0
+
+# shellcheck disable=SC2317
+handle_error() {
+  echo "An error occurred"
+  # Additional error handling logic
+  OVERALL_EXIT_CODE=1
+}
+
+# Set the error handler function
+trap handle_error ERR
 
 AWS_ACCESS_KEY_ID=$(cat /tmp/secrets/AWS_ACCESS_KEY_ID)
 AWS_SECRET_ACCESS_KEY=$(cat /tmp/secrets/AWS_SECRET_ACCESS_KEY)
@@ -11,14 +21,19 @@ export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_DEFAULT_REGION AWS_S3_BUCKET
 # Read the array from file
 mapfile -t CORRELATE_MAPT_ARRAY < "${SHARED_DIR}/s3_top_level_folders.txt"
 
+total=${#CORRELATE_MAPT_ARRAY[@]}
+current=0
 # Iterate over each value
 for S3_TOP_LEVEL_FOLDER in "${CORRELATE_MAPT_ARRAY[@]}"; do
+  ((current++))
+  echo "Processing MAPT: ${S3_TOP_LEVEL_FOLDER} ($current/$total)"
+
   # Skip empty lines
   [ -z "$S3_TOP_LEVEL_FOLDER" ] && continue
-
-  echo "Processing MAPT: ${S3_TOP_LEVEL_FOLDER}"
 
   mapt aws eks destroy \
       --project-name "eks" \
       --backed-url "s3://${AWS_S3_BUCKET}/${S3_TOP_LEVEL_FOLDER}"
 done
+
+exit $OVERALL_EXIT_CODE
