2259d35d315f15e16cf07857a62eca7a84fe0ea1
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2025-07-28 12:59:58 +0200

    USHIFT-5589: Change conformance optional instance types

--- ci-operator/step-registry/openshift/microshift/infra/lvm-install/openshift-microshift-infra-lvm-install-commands.sh
+++ ci-operator/step-registry/openshift/microshift/infra/lvm-install/openshift-microshift-infra-lvm-install-commands.sh
@@ -8,7 +8,7 @@ trap_install_status_exit_code $EXIT_CODE_LVM_INSTALL_FAILURE
 
 device="/dev/xvdc"
 
-if [[ "${EC2_INSTANCE_TYPE%.*}" =~ .*"g".* || "${EC2_INSTANCE_TYPE%.*}" =~ "t3".* || "${EC2_INSTANCE_TYPE%.*}" =~ c7i.2xlarge ]]; then
+if [[ "${EC2_INSTANCE_TYPE%.*}" =~ .*"g".* || "${EC2_INSTANCE_TYPE%.*}" =~ "t3".* || "${EC2_INSTANCE_TYPE%.*}" =~ "c7i".* ]]; then
   # Sometimes, devices are in different order and nvme1 stores OS while nvme0 should hold LVM for topolvm.
   # If /dev/nvme0 is already partitioned (operating system), then use nvme1 for lvm.
   # If `partx /dev/nvme0n1` fails (rc=1), it couldn't read partition table, so it's the one to use for lvm.
