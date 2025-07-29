58c20c6ebb2297cbb2527b9add66f79d560da768
David Eads <deads2k@users.noreply.github.com>
2025-07-29 17:32:48 -0400

    react to demo script changes (#67635)
    * react to demo script changes
    
    * add subscription to cleanup scripts

--- ci-operator/step-registry/aro-hcp/provision/create-nodepool/aro-hcp-provision-create-nodepool-commands.sh
+++ ci-operator/step-registry/aro-hcp/provision/create-nodepool/aro-hcp-provision-create-nodepool-commands.sh
@@ -4,6 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+set -x # Turn on command tracing
+
 # read the secrets and login as the user
 export TEST_USER_CLIENT_ID; TEST_USER_CLIENT_ID=$(cat /var/run/hcp-integration-credentials/client-id)
 export TEST_USER_CLIENT_SECRET; TEST_USER_CLIENT_SECRET=$(cat /var/run/hcp-integration-credentials/client-secret)
