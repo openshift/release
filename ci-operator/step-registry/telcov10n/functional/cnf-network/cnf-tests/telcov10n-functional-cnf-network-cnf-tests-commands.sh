cc9ec13d71a44d5ecfbe7e9eaf3cf81002631cfa
Nikita Kononov <nkononov@redhat.com>
2025-07-29 16:34:39 -0400

    cnf network: added 4.15 ci lane (#67290)

--- ci-operator/step-registry/telcov10n/functional/cnf-network/cnf-tests/telcov10n-functional-cnf-network-cnf-tests-commands.sh
+++ ci-operator/step-registry/telcov10n/functional/cnf-network/cnf-tests/telcov10n-functional-cnf-network-cnf-tests-commands.sh
@@ -52,8 +52,8 @@ ansible-playbook ./playbooks/cnf/deploy-run-cnf-tests-script.yaml \
         features='$FEATURES_TO_TEST' \
         oo_install_ns=metallb-system \
         cnf_test_dir=$PROJECT_DIR/ \
-        cnf_tests_skip=$CNF_TESTS_SKIP \
         cnf_test_perf_test_profile=$CNF_TESTS_PERF_PROFILE \
+        cnf_tests_skip='$CNF_TESTS_SKIP' \
         cnftests_git_dest=cnf-features-deploy"
 
 echo "Set bastion ssh configuration"
