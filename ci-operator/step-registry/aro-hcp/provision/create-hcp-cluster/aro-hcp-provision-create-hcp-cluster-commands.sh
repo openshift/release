85db31a191144b15e433873c378007f0938e7670
David Eads <deads2k@users.noreply.github.com>
2025-07-29 17:32:48 -0400

    react to demo script changes (#67635)
    * react to demo script changes
    
    * add subscription to cleanup scripts

--- ci-operator/step-registry/aro-hcp/provision/create-hcp-cluster/aro-hcp-provision-create-hcp-cluster-commands.sh
+++ ci-operator/step-registry/aro-hcp/provision/create-hcp-cluster/aro-hcp-provision-create-hcp-cluster-commands.sh
@@ -4,6 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+set -x # Turn on command tracing
+
 # read the secrets and login as the user
 export TEST_USER_CLIENT_ID; TEST_USER_CLIENT_ID=$(cat /var/run/hcp-integration-credentials/client-id)
 export TEST_USER_CLIENT_SECRET; TEST_USER_CLIENT_SECRET=$(cat /var/run/hcp-integration-credentials/client-secret)
@@ -11,20 +13,7 @@ export TEST_USER_TENANT_ID; TEST_USER_TENANT_ID=$(cat /var/run/hcp-integration-c
 az login --service-principal -u "${TEST_USER_CLIENT_ID}" -p "${TEST_USER_CLIENT_SECRET}" --tenant "${TEST_USER_TENANT_ID}"
 
 export CUSTOMER_RG_NAME; CUSTOMER_RG_NAME=$(cat "${SHARED_DIR}/customer-resource-group-name.txt")
-
-MANAGED_RESOURCE_GROUP="$CLUSTER_NAME-rg-managed"
-
-NSG_ID=$(az deployment group show \
-          --name 'infra' \
-          --subscription "${SUBSCRIPTION}" \
-          --resource-group "${CUSTOMER_RG_NAME}" \
-          --query "properties.outputs.networkSecurityGroupId.value" -o tsv)
-
-SUBNET_ID=$(az deployment group show \
-          --name 'infra' \
-          --subscription "${SUBSCRIPTION}" \
-          --resource-group "${CUSTOMER_RG_NAME}" \
-          --query "properties.outputs.subnetId.value" -o tsv)
+MANAGED_RESOURCE_GROUP="${CUSTOMER_RG_NAME}-rg-managed"
 
 az deployment group create \
   --name 'aro-hcp'\
@@ -32,7 +21,8 @@ az deployment group create \
   --resource-group "${CUSTOMER_RG_NAME}" \
   --template-file demo/bicep/cluster.bicep \
   --parameters \
-    networkSecurityGroupId="${NSG_ID}" \
-    subnetId="${SUBNET_ID}" \
+    vnetName="${CUSTOMER_VNET_NAME}" \
+    subnetName="${CUSTOMER_VNET_SUBNET1}" \
+    nsgName="${CUSTOMER_NSG}" \
     clusterName="${CLUSTER_NAME}" \
     managedResourceGroupName="${MANAGED_RESOURCE_GROUP}"
