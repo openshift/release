a254dd094773abc1bd0226a8cf3475a7e285da2b
Martin Gencur <mgencur@redhat.com>
2025-07-30 09:05:30 +0200

    Create day-2 secret once HC is Available

--- ci-operator/step-registry/cucushift/hypershift-extended/external-oidc/create-day2-secret/cucushift-hypershift-extended-external-oidc-create-day2-secret-commands.sh
+++ ci-operator/step-registry/cucushift/hypershift-extended/external-oidc/create-day2-secret/cucushift-hypershift-extended-external-oidc-create-day2-secret-commands.sh
@@ -2,19 +2,27 @@
 
 set -euo pipefail
 
-if [ ! -f "${SHARED_DIR}/nested_kubeconfig" ]; then
-  exit 1
+CLUSTER_NAME="$(oc get hc -A -o jsonpath='{.items[0].metadata.name}')"
+export CLUSTER_NAME
+if [[ -z "$CLUSTER_NAME" ]]; then
+    echo "Unable to find the hosted cluster's name"
+    exit 1
 fi
 
-if [ -f "${SHARED_DIR}/proxy-conf.sh" ] ; then
-    source "${SHARED_DIR}/proxy-conf.sh"
-fi
+# Wait for the HC to be Available
+timeout 25m bash -c "
+  until [[ \$(oc get -n clusters hostedcluster/${CLUSTER_NAME} -o jsonpath='{.status.version.history[?(@.state!=\"\")].state}') = Available ]]; do
+      sleep 15
+  done
+"
+
+echo "Getting kubeconfig of the hosted cluster"
+hypershift create kubeconfig --namespace=clusters --name="${CLUSTER_NAME}" > "${SHARED_DIR}"/nested_kubeconfig
 
 CONSOLE_CLIENT_SECRET="$(</var/run/hypershift-ext-oidc-app-console/client-secret)"
 # The name must match the name of the empty secret created
 # in step cucushift-hypershift-extended-external-oidc-enable.
 CONSOLE_CLIENT_SECRET_NAME=authid-console-openshift-console
-
 
 oc create secret generic "$CONSOLE_CLIENT_SECRET_NAME" -n openshift-config \
     --from-literal=clientSecret="$CONSOLE_CLIENT_SECRET" \
