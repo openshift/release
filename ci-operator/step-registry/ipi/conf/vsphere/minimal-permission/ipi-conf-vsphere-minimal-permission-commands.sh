1c9b672d6190475c7f71d0f4a3cc2c88fd584a38
Shang Gao <sgao@redhat.com>
2025-07-30 17:11:04 +0800

    Fix minimal permission issue (#66920)

--- ci-operator/step-registry/ipi/conf/vsphere/minimal-permission/ipi-conf-vsphere-minimal-permission-commands.sh
+++ ci-operator/step-registry/ipi/conf/vsphere/minimal-permission/ipi-conf-vsphere-minimal-permission-commands.sh
@@ -14,10 +14,22 @@ source "${VCENTER_AUTH_PATH}"
 CONFIG="${SHARED_DIR}/install-config.yaml"
 PATCH="${SHARED_DIR}/permission-user.yaml.patch"
 
+# shellcheck disable=SC2153
+ocp_minor_version=$(oc version -o json | jq -r '.openshiftVersion' | cut -d '.' -f2)
+if [ "${ocp_minor_version}" -lt 13 ]; then
 cat > "${PATCH}" << EOF
 platform:
   vsphere:
     username: "${vcenter_username_minimal_permission}"
     password: "${vcenter_password_minimal_permission}"
 EOF
+else
+cat > "${PATCH}" << EOF
+platform:
+  vsphere:
+    vcenters:
+    - user: "${vcenter_username_minimal_permission}"
+      password: "${vcenter_password_minimal_permission}"
+EOF
+fi
 yq-go m -x -i "${CONFIG}" "${PATCH}"
