4a1d33144556447ddf3a40dcf9793dd9c32600fa
Sean Zhao <szhao@redhat.com>
2025-07-31 09:16:27 +0800

    tag to stage

--- ci-operator/step-registry/quay-tests/test/stage-performance/quay-tests-test-stage-performance-commands.sh
+++ ci-operator/step-registry/quay-tests/test/stage-performance/quay-tests-test-stage-performance-commands.sh
@@ -114,7 +114,7 @@ spec:
     spec:
       containers:
       - name: python
-        image: quay.io/quay-qetest/quay-load:latest
+        image: quay.io/quay-qetest/quay-load:stage
         securityContext:
           privileged: true
         env:
@@ -133,7 +133,7 @@ spec:
           - name: ES_INDEX
             value: "quay-vegeta"
           - name: PUSH_PULL_IMAGE
-            value: "quay.io/quay-qetest/quay-load:latest"
+            value: "quay.io/quay-qetest/quay-load:stage"
           - name: PUSH_PULL_ES_INDEX
             value: "quay-push-pull"
           - name: PUSH_PULL_NUMBERS
