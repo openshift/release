9c2fba338288189f5afc31cf190b98e765dce257
Justin Pierce <jupierce@redhat.com>
2025-08-12 18:27:31 -0400

    Allow use of RoleBinding instead of ClusterRoleBinding

--- ci-operator/step-registry/openshift/e2e/netadmin-rbac/openshift-e2e-netadmin-rbac-commands.sh
+++ ci-operator/step-registry/openshift/e2e/netadmin-rbac/openshift-e2e-netadmin-rbac-commands.sh
@@ -12,4 +12,24 @@ unset KUBECONFIG
 # Allow any service account in the test namespace the use of net_admin / net_raw. The target
 # SCC adds these capabilities to pods by these service accounts by default. So any test pod
 # running after this one should receive these caps.
-oc adm policy add-scc-to-group -n "${NAMESPACE}" restricted-v2-plus-netadmin "system:serviceaccount:${NAMESPACE}"
+export TARGET_SCC_NAME="restricted-v2-plus-netadmin"
+
+oc create -f - <<EOF
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: ${TARGET_SCC_NAME}-binding
+  namespace: ${NAMESPACE}
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: ${TARGET_SCC_NAME}
+subjects:
+$(oc get sa -n "${NAMESPACE}" \
+  --no-headers \
+  -o custom-columns=:metadata.name \
+  | grep -Ev '^(default|builder|deployer)$' \
+  | awk -v ns="${NAMESPACE}" '{print "- kind: ServiceAccount\n  name: "$1"\n  namespace: "ns}')
+EOF
+
+
