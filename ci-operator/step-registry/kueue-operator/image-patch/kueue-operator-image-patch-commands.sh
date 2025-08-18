c816370ae55f75544f8ebe173b21759c5bc544d8
Pannaga Rao Bhoja Ramamanohara <pbhojara@pbhojara-thinkpadp16vgen1.rht.csb>
2025-08-18 16:24:36 -0400

    Re-evaluate bundle testing for Kueue operator
    Signed-off-by: Pannaga Rao Bhoja Ramamanohara

--- ci-operator/step-registry/kueue-operator/image-patch/kueue-operator-image-patch-commands.sh
+++ ci-operator/step-registry/kueue-operator/image-patch/kueue-operator-image-patch-commands.sh
@@ -7,8 +7,8 @@ if [[ "$JOB_TYPE" == *"periodic"* ]]; then
 fi
 
 source "${SHARED_DIR}/env"
-echo "Using Operator Image: ${OPERATOR_IMAGE}"
+echo "Using Operator Image: ${KUEUE_OPERATOR_IMAGE}"
 echo "Using Bundle Image: ${BUNDLE_IMAGE}"
 
 CSV=$(oc get csv -n openshift-kueue-operator -o jsonpath='{.items[0].metadata.name}')
-oc patch csv -n openshift-kueue-operator $CSV --type=json -p="[{\"op\": \"replace\", \"path\": \"/spec/install/spec/deployments/0/spec/template/spec/containers/0/image\", \"value\": \"$OPERATOR_IMAGE\"}]"
+oc patch csv -n openshift-kueue-operator $CSV --type=json -p="[{\"op\": \"replace\", \"path\": \"/spec/install/spec/deployments/0/spec/template/spec/containers/0/image\", \"value\": \"$KUEUE_OPERATOR_IMAGE\"}]"
