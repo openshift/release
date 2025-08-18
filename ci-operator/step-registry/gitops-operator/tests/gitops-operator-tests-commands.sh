4a109ef5caa3dc979428faa57469a25bd6343f6a
Varsha B <vab@redhat.com>
2025-08-18 13:20:47 +0530

    interop test changes
    Signed-off-by: Varsha B <vab@redhat.com>

--- ci-operator/step-registry/gitops-operator/tests/gitops-operator-tests-commands.sh
+++ ci-operator/step-registry/gitops-operator/tests/gitops-operator-tests-commands.sh
@@ -6,10 +6,6 @@ unset CI
 
 exit_code=0
 scripts/openshift-CI-kuttl-tests.sh
-kubectl kuttl test test/openshift/e2e/sequential --config test/openshift/e2e/sequential/kuttl-test.yaml --report xml || exit_code=1
-
-
-# Move results to Artifacts directory
-cp ./kuttl-test.xml ${ARTIFACT_DIR}/junit_gitops-sequential.xml
+make e2e-tests-sequential e2e-tests-parallel || exit_code=1
 
 exit $exit_code
