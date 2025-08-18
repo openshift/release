d65aaf406ce161b2faebc6660248e410f99665ae
Tomas David <tdavid@redhat.com>
2025-08-18 11:01:06 +0200

    small_impr

--- ci-operator/step-registry/release-qe-tests/jira-notificator/release-qe-tests-jira-notificator-commands.sh
+++ ci-operator/step-registry/release-qe-tests/jira-notificator/release-qe-tests-jira-notificator-commands.sh
@@ -1,3 +1,2 @@
-jira_token=$(cat "/var/run/vault/release-tests-token/jira_token")
-export JIRA_TOKEN=$jira_token
+export JIRA_TOKEN=$(cat "/var/run/vault/release-tests-token/jira_token")
 oarctl jira-notificator --dry-run 
