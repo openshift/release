583890149da0b9ac88ed515691983ad66b15538c
Paige Patton <prubenda@redhat.com>
2025-08-20 16:43:30 -0400

    taking out else

--- ci-operator/step-registry/openshift-qe/orion/openshift-qe-orion-commands.sh
+++ ci-operator/step-registry/openshift-qe/orion/openshift-qe-orion-commands.sh
@@ -123,7 +123,7 @@ if [ -f "${KUBECONFIG}" ]; then
     master_count=$masters
     export master_count
 
-    infra_count=$infras
+    infra_count=$infra
     export infra_count
 
     total_node_count=$all
@@ -135,7 +135,7 @@ if [ -f "${KUBECONFIG}" ]; then
     cloud_infrastructure=$(oc get infrastructure cluster -o jsonpath='{.status.platformStatus.type}')
     export cloud_infrastructure
     cluster_type=""
-    if [ "$platform" = "AWS" ]; then
+    if [ "$cloud_infrastructure" = "AWS" ]; then
         cluster_type=$(oc get infrastructure cluster -o jsonpath='{.status.platformStatus.aws.resourceTags[?(@.key=="red-hat-clustertype")].value}') || echo "Cluster Install Failed"
     fi
     if [ -z "$cluster_type" ]; then
@@ -164,7 +164,7 @@ if [[ -n "${LOOKBACK_SIZE}" ]]; then
     export EXTRA_FLAGS+=" --lookback-size ${LOOKBACK_SIZE}"
 fi
 
-if [[ -n "${DEBUG}" && ${DEBUG} == true ]]; then
+if [[ -n "${DEBUG+x}" && ${DEBUG} == true ]]; then
     export EXTRA_FLAGS+=" --debug"
 fi
 
