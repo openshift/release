9da3b08851aa17e73eb03adb1f3f2caaeebbb48e
Paige Patton <prubenda@redhat.com>
2025-08-20 17:33:09 -0400

    aways debug

--- ci-operator/step-registry/openshift-qe/orion/openshift-qe-orion-commands.sh
+++ ci-operator/step-registry/openshift-qe/orion/openshift-qe-orion-commands.sh
@@ -173,7 +173,7 @@ cat $CONFIG
 set +e
 set -o pipefail
 FILENAME=$(echo $CONFIG | awk -F/ '{print $2}' | awk -F. '{print $1}')
-es_metadata_index=${ES_METADATA_INDEX} es_benchmark_index=${ES_BENCHMARK_INDEX} VERSION=${VERSION} jobtype="periodic" orion cmd --node-count ${IGNORE_JOB_ITERATIONS} --config ${CONFIG} ${EXTRA_FLAGS} | tee ${ARTIFACT_DIR}/$FILENAME.txt
+es_metadata_index=${ES_METADATA_INDEX} es_benchmark_index=${ES_BENCHMARK_INDEX} VERSION=${VERSION} jobtype="periodic" orion cmd --node-count ${IGNORE_JOB_ITERATIONS} --config ${CONFIG} ${EXTRA_FLAGS} --debug| tee ${ARTIFACT_DIR}/$FILENAME.txt 
 orion_exit_status=$?
 set -e
 
