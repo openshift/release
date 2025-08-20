da800f2aa824a2a80b0f879d21e2c1a700dd0c13
Paige Patton <prubenda@redhat.com>
2025-08-20 12:46:12 -0400

    adding run orion

--- ci-operator/step-registry/openshift-qe/orion/openshift-qe-orion-commands.sh
+++ ci-operator/step-registry/openshift-qe/orion/openshift-qe-orion-commands.sh
@@ -124,7 +124,7 @@ if [ -f "${KUBECONFIG}" ]; then
 
     infra_count=$infras
     export infra_count
-    
+
     TOTAL_NODE_COUNT=$all
     export TOTAL_NODE_COUNT
     node_instance_type=$worker_type
@@ -160,9 +160,6 @@ fi
 if [[ -n "${LOOKBACK_SIZE}" ]]; then
     EXTRA_FLAGS+=" --lookback-size ${LOOKBACK_SIZE}"
 fi
-
-
-
 
 set +e
 set -o pipefail
