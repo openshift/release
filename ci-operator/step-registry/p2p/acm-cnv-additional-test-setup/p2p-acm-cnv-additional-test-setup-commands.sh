015541bd1b97143006b3295e9bed43d6bfe29c16
vshakya <vshakya@vshakya-thinkpadt14gen4.remote.csb>
2025-08-25 15:11:23 -0400

    update yaml

--- ci-operator/step-registry/p2p/acm-cnv-additional-test-setup/p2p-acm-cnv-additional-test-setup-commands.sh
+++ ci-operator/step-registry/p2p/acm-cnv-additional-test-setup/p2p-acm-cnv-additional-test-setup-commands.sh
@@ -57,8 +57,6 @@ if [[ -f "${AWSCRED}" ]]; then
             aws_secret_access_key: "${AWS_SECRET_ACCESS_KEY}"
             baseDomain: "${BASE_DOMAIN}"
             pullSecret: "$(cat "${CLUSTER_PROFILE_DIR}/config.json")"
-            ssh-privatekey: "$(cat "${CLUSTER_PROFILE_DIR}/ssh-privatekey")"
-            ssh-publickey: "$(cat "${CLUSTER_PROFILE_DIR}/ssh-publickey")"
             httpProxy: ""
             httpsProxy: ""
             noProxy: ""
