e52fe8f109d0f0a67f5400b3713cb4454069c7b0
Ryan Phillips <rphillips@redhat.com>
2025-08-26 20:15:05 -0500

    use --no-abbrev-commit

--- ci-operator/step-registry/kueue-operator/image-env-setup/kueue-operator-image-env-setup-commands.sh
+++ ci-operator/step-registry/kueue-operator/image-env-setup/kueue-operator-image-env-setup-commands.sh
@@ -35,7 +35,7 @@ EOF
 
 echo "Current PWD: $(pwd)"
 ls -lah
-REVISION=$(git log --oneline -1 | awk '{print $4}' | tr -d "'")
+REVISION=$(git log --oneline -1 --no-abbrev-commit | awk '{print $1}' | tr -d "'")
 echo "Current Git branch:"
 git branch --show-current
 
@@ -44,21 +44,21 @@ git log --oneline -5
 echo "Git status:"
 git status
 export OPERATOR_IMAGE="quay.io/redhat-user-workloads/kueue-operator-tenant/${OPERATOR_COMPONENT}:on-pr-${REVISION}"
-echo "export OPERATOR_IMAGE=${OPERATOR_IMAGE}" >> "${SHARED_DIR}/env"
+echo "export OPERATOR_IMAGE=${OPERATOR_IMAGE}" >>"${SHARED_DIR}/env"
 
 REPO="quay.io/redhat-user-workloads/kueue-operator-tenant/${BUNDLE_COMPONENT}"
 BUNDLE_IMAGE=$(skopeo list-tags docker://$REPO | jq -r '.Tags[]' | grep -E '(on-pr-)?[a-f0-9]{40}$' | grep ${REVISION} | while read -r tag; do
-    created=$(skopeo inspect docker://$REPO:$tag 2>/dev/null | jq -r '.Created')
-    if [ "$created" != "null" ] && [ -n "$created" ]; then echo "$created $tag"; fi
+	created=$(skopeo inspect docker://$REPO:$tag 2>/dev/null | jq -r '.Created')
+	if [ "$created" != "null" ] && [ -n "$created" ]; then echo "$created $tag"; fi
 done | sort | tail -n1 | awk -v repo="$REPO" '{print repo ":" $2}')
 
 if [[ -z "$BUNDLE_IMAGE" ]]; then
-  echo "ERROR: Failed to resolve BUNDLE_IMAGE from $REPO"
-  exit 1
+	echo "ERROR: Failed to resolve BUNDLE_IMAGE from $REPO"
+	exit 1
 fi
 
 echo "Resolved BUNDLE_IMAGE: ${BUNDLE_IMAGE}"
-echo "export BUNDLE_IMAGE=${BUNDLE_IMAGE}" >> "${SHARED_DIR}/env"
+echo "export BUNDLE_IMAGE=${BUNDLE_IMAGE}" >>"${SHARED_DIR}/env"
 
 oc create namespace openshift-kueue-operator || true
 oc label ns openshift-kueue-operator openshift.io/cluster-monitoring=true --overwrite
