1f22144594620aa742dd2618770b4d0b0fbcd259
oharan2 <oharan@redhat.com>
2025-08-27 16:13:07 +0300

    Increase sleep time

--- ci-operator/step-registry/mpiit/data-router-reporter/mpiit-data-router-reporter-commands.sh
+++ ci-operator/step-registry/mpiit/data-router-reporter/mpiit-data-router-reporter-commands.sh
@@ -3,7 +3,7 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-sleep 30m
+sleep 1h
 
 export OCP_VERSION
 export DATAROUTER_RESULTS="${SHARED_DIR}/results/*.xml"
@@ -15,8 +15,10 @@ export REPORTPORTAL_CMP
 
 export REPORTPORTAL_LAUNCH_ATTRIBUTES="[
   {\"key\": \"version\", \"value\": \"${OCP_VERSION}\"},
-  {\"key\": \"environment\", \"value\": \"staging\"},
+  {\"key\": \"environment\", \"value\": \"prod\"},
   {\"key\": \"component\", \"value\": \"${REPORTPORTAL_CMP}\"},
   {\"key\": \"team\", \"value\": \"mpiit\"}
 ]"
+export DATAROUTER_METADATA_URL="https://raw.githubusercontent.com/openshift/release/1bf8eeea33187be897d912cb7149602461cf5975/ci-operator/step-registry/mpiit/data-router-reporter/metadata.json"
+
 datarouter-openshift-ci
