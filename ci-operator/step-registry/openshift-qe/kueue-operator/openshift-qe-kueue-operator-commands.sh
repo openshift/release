fc79f90fcb9cd5f8483bd49f288198cea4048afd
avasilev <avasilev@redhat.com>
2025-08-28 17:36:02 +0200

    kueue operator cpt: add kueue perfscale chain and add Kueue CR installation
    Signed-off-by: avasilev <avasilev@redhat.com>

--- ci-operator/step-registry/openshift-qe/kueue-operator/openshift-qe-kueue-operator-commands.sh
+++ ci-operator/step-registry/openshift-qe/kueue-operator/openshift-qe-kueue-operator-commands.sh
@@ -23,4 +23,26 @@ pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
 
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
 
+# Install the Kueue CR and wait until completion
+oc apply -f - <<EOF
+apiVersion: kueue.openshift.io/v1
+kind: Kueue
+metadata:
+  name: cluster
+  labels:
+    app.kubernetes.io/managed-by: kustomize
+    app.kubernetes.io/name: kueue-operator
+  namespace: openshift-kueue-operator
+spec:
+  config:
+    integrations:
+      frameworks:
+        - BatchJob
+        - Pod
+  logLevel: Normal
+  operatorLogLevel: Normal
+  managementState: Managed
+EOF
+oc wait Kueue cluster --for=jsonpath='{.status.conditions[?(@.type=="Available")].status}'='True' --timeout=300s
+
 WORKLOAD=kueue-operator-$WORKLOAD_TYPE ./run.sh
