f6f8f162e55a8a7fb3cc47c4a3ec66d71a9e718a
vshakya <vshakya@vshakya-thinkpadt14gen4.remote.csb>
2025-08-28 08:00:47 -0400

    update files and rename

--- ci-operator/step-registry/p2p/acm-cnv-additional-test-setup/p2p-acm-cnv-additional-test-setup-commands.sh
+++ ci-operator/step-registry/p2p/acm-cnv-additional-test-setup/p2p-acm-cnv-additional-test-setup-commands.sh
@@ -34,47 +34,38 @@ EOF
 oc label managedcluster local-cluster cluster.open-cluster-management.io/clusterset=managed-cluster-set --overwrite
 
 
+
 AWSCRED="${CLUSTER_PROFILE_DIR}/.awscred"
-
-
-
 
 if [[ -f "${AWSCRED}" ]]; then
 
   AWS_ACCESS_KEY_ID=$(cat "${AWSCRED}" | grep aws_access_key_id | tr -d ' ' | cut -d '=' -f 2)
   AWS_SECRET_ACCESS_KEY=$(cat "${AWSCRED}" | grep aws_secret_access_key | tr -d ' ' | cut -d '=' -f 2)
 
-  oc create -f - <<EOF
-  apiVersion: v1
-  kind: Secret
-  type: Opaque
-  metadata:
-    name: acm-aws-secret
-    namespace: ocm
-    labels:
-        cluster.open-cluster-management.io/type: aws
-        cluster.open-cluster-management.io/credentials: ""
-  stringData:
-    aws_access_key_id: "${AWS_ACCESS_KEY_ID}"
-    aws_secret_access_key: "${AWS_SECRET_ACCESS_KEY}"
-    baseDomain: "${BASE_DOMAIN}"
-    httpProxy: ""
-    httpsProxy: ""
-    noProxy: ""
-    additionalTrustBundle: ""
-EOF
+  oc -n ocm create secret generic acm-aws-secret  \
+      --type=Opaque \
+      --from-literal=aws_access_key_id="$AWS_ACCESS_KEY_ID" \
+      --from-literal=aws_secret_access_key="$AWS_SECRET_ACCESS_KEY" \
+      --from-literal=baseDomain="${BASE_DOMAIN}" \
+      --from-file=pullSecret="${CLUSTER_PROFILE_DIR}/config.json" \
+      --from-file=ssh-privatekey="${CLUSTER_PROFILE_DIR}/ssh-privatekey" \
+      --from-file=ssh-publickey="${CLUSTER_PROFILE_DIR}/ssh-publickey" \
+      --from-literal=httpProxy="" \
+      --from-literal=noProxy="" \
+      --from-literal=AdditionalTrustBundle=""
+
+  oc label secret acm-aws-secret \
+  cluster.open-cluster-management.io/type=aws \
+  cluster.open-cluster-management.io/credentials="" \
+  -n ocm --overwrite
+
+
 
   echo "secret created"
 else
   echo "Did not find compatible cloud provider cluster_profile"
   exit 1
 fi
-
-
-sleep 14400
-PULLSPEC="$(curl -fsSl "https://amd64.ocp.releases.ci.openshift.org/api/v1/releasestream/4.20.0-0.nightly/latest" | jq -r '.pullSpec')"
-echo $PULLSPEC
-
 
 oc create -f - <<EOF
 apiVersion: policy.open-cluster-management.io/v1
