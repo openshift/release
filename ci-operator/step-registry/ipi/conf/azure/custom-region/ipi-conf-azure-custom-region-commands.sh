3f1ad8bd6392b8a5851cbd159e2dd63b7bdc58e6
Jinyun Ma <jima@redhat.com>
2025-08-29 09:18:09 +0800

    fix

--- ci-operator/step-registry/ipi/conf/azure/custom-region/ipi-conf-azure-custom-region-commands.sh
+++ ci-operator/step-registry/ipi/conf/azure/custom-region/ipi-conf-azure-custom-region-commands.sh
@@ -7,10 +7,15 @@ set -o pipefail
 CONFIG="${SHARED_DIR}/install-config.yaml"
 PATCH="${SHARED_DIR}/custom-region.yaml.patch"
 
+REGION="${CUSTOM_AZURE_REGION}"
+if [[ -z "${CUSTOM_AZURE_REGION}" ]]; then
+    REGION="${LEASED_RESOURCE}"
+fi
+
 cat > "${PATCH}" << EOF
 platform:
  azure:
-   region: ${CUSTOM_AZURE_REGION}
+   region: ${REGION}
 EOF
   yq-go m -x -i "${CONFIG}" "${PATCH}"
   rm "${PATCH}"
