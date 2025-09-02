fa2e1b32bfbe04b71d92269ec5f57116e8621a00
Raúl Sevilla <rsevilla@redhat.com>
2025-09-02 10:36:42 +0200

    Fix pprof logic (#68608)
    * Fix pprof logic
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    * Temp test
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    * Revert "Temp test"
    
    This reverts commit c229dd8b35a24b1c8dff0e7ca469dc24e4c15de3.
    
    * Remove unused FLAGS variable
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    ---------
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- ci-operator/step-registry/openshift-qe/cluster-density-v2/openshift-qe-cluster-density-v2-commands.sh
+++ ci-operator/step-registry/openshift-qe/cluster-density-v2/openshift-qe-cluster-density-v2-commands.sh
@@ -70,7 +70,7 @@ export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@$ES_HOST"
 if [[ "${ENABLE_LOCAL_INDEX}" == "true" ]]; then
     EXTRA_FLAGS+=" --local-indexing"
 fi
-EXTRA_FLAGS+=" --gc-metrics=true --profile-type=${PROFILE_TYPE}"
+EXTRA_FLAGS+=" --gc-metrics=true --profile-type=${PROFILE_TYPE} --pprof=${PPROF}"
 
 if [[ -n "${USER_METADATA}" ]]; then
     USER_METADATA=$(echo "$USER_METADATA" | xargs)
@@ -100,4 +100,7 @@ cp "${SHARED_DIR}"/${WORKLOAD}-index_data.json  "${ARTIFACT_DIR}"/${WORKLOAD}-in
 if [[ "${ENABLE_LOCAL_INDEX}" == "true" ]]; then
     metrics_folder_name=$(find . -maxdepth 1 -type d -name 'collected-metric*' | head -n 1)
     cp -r "${metrics_folder_name}" "${ARTIFACT_DIR}/"
+fi
+if [[ ${PPROF} == "true" ]]; then
+  cp -r pprof-data "${ARTIFACT_DIR}/"
 fi
