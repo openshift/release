a8552a07c4bf98581894c0cdcb96ae19c7d9ef5b
Neil Girard <45597105+vr4manta@users.noreply.github.com>
2025-09-03 16:09:11 -0400

    Fixed vm ip logic for install-gather (#68771)

--- ci-operator/step-registry/upi/install/vsphere/upi-install-vsphere-commands.sh
+++ ci-operator/step-registry/upi/install/vsphere/upi-install-vsphere-commands.sh
@@ -67,6 +67,9 @@ function gather_console_and_bootstrap() {
     source "${SHARED_DIR}/govc.sh"
     unset SSL_CERT_FILE
     unset GOVC_TLS_CA_CERTS
+
+    infra_id=$(jq -r '.infraID' "${installer_dir}/metadata.json")
+
     # list all the virtual machines in the folder/rp
     clustervms=$(govc ls "/${GOVC_DATACENTER}/vm/${cluster_name}")
     GATHER_BOOTSTRAP_ARGS=()
@@ -84,11 +87,22 @@ function gather_console_and_bootstrap() {
       # with ip addresses as the value
       # wait 1 minute for an ip address to become available
 
+      # If hostname has cluster name in it (newer powercli installs), strip the host name off for the IP logic below to work for gather
+      if [[ "${hostname}" == "${infra_id}"* ]]; then
+        hostname=${hostname#${infra_id}-}
+      fi
+
+      # Older UPI installs have "-0" after bootstrap.  We'll remove those to be consistent with newer builds.
+      if [[ "${hostname}" == "bootstrap-0" ]]; then
+        hostname="bootstrap"
+      fi
+
+      echo "declaring ${hostname//-/_}_ip"
       # shellcheck disable=SC2140
       declare "${hostname//-/_}_ip"="$(govc vm.ip -wait=1m -vm.ipath="$ipath" | awk -F',' '{print $1}')"
     done
 
-    GATHER_BOOTSTRAP_ARGS+=('--bootstrap' "${bootstrap_0_ip}")
+    GATHER_BOOTSTRAP_ARGS+=('--bootstrap' "${bootstrap_ip}")
     GATHER_BOOTSTRAP_ARGS+=('--master' "${control_plane_0_ip}" '--master' "${control_plane_1_ip}" '--master' "${control_plane_2_ip}")
 
     # 4.5 and prior used the terraform.tfstate for gather bootstrap. This causes an error with:
