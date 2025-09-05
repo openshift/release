c6d07db0424d80b4080459dbbcdbe2a2fc4d615d
Jose Castillo Lema <josecastillolema@gmail.com>
2025-09-05 22:57:21 +0200

    [PERFSCALE-3706] Fix healt-check for private vlan
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/cluster-health/openshift-qe-cluster-health-commands.sh
+++ ci-operator/step-registry/openshift-qe/cluster-health/openshift-qe-cluster-health-commands.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -eu
 
+if [ ${BAREMETAL} == "true" ]; then
+  SSH_ARGS="-i ${CLUSTER_PROFILE_DIR}/jh_priv_ssh_key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
+  bastion="$(cat ${CLUSTER_PROFILE_DIR}/address)"
+  # Setup socks proxy
+  ssh ${SSH_ARGS} root@$bastion -fNT -D 12345
+  export https_proxy=socks5://localhost:12345
+  export http_proxy=socks5://localhost:12345
+  oc config set-cluster bm --proxy-url=socks5://localhost:12345
+fi
+
 # For disconnected environments, source proxy config if available
 if test -f "${SHARED_DIR}/proxy-conf.sh"; then
   # shellcheck disable=SC1090
