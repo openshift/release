e68016abfe8e3cdea2a7901522373479aa7dd050
Jibin Pattara <jibinpattara@Jibins-MacBook-Pro.local>
2025-09-08 15:38:56 +0530

    testing OCP RELEASE IMAGE
    Signed-off-by: Jibin Pattara <jibinpattara@Jibins-MacBook-Pro.local>

--- ci-operator/step-registry/hypershift/mce/ibmz/create/s390x-mgmt/hypershift-mce-ibmz-create-s390x-mgmt-commands.sh
+++ ci-operator/step-registry/hypershift/mce/ibmz/create/s390x-mgmt/hypershift-mce-ibmz-create-s390x-mgmt-commands.sh
@@ -12,6 +12,9 @@ export CLUSTER_ARCH
 cat "${AGENT_IBMZ_CREDENTIALS}/abi-pull-secret" | jq -c > "$HOME/pull-secret" 
 export PULL_SECRET_FILE="$HOME/pull-secret"
 
+echo "Printing OCP release image"
+echo $OCP_RELEASE_IMAGE
+
 ssh_key_string=$(cat "${AGENT_IBMZ_CREDENTIALS}/httpd-vsi-key")
 export ssh_key_string
 tmp_ssh_key="/tmp/ssh-private-key"
