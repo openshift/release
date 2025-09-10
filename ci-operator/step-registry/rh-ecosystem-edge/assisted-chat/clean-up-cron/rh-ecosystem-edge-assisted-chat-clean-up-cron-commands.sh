96dd34e746768d9754d8a4c5b902ead89470d343
aantal <aantal@redhat.com>
2025-09-10 17:15:04 +0200

    changing the clean-up script to delete only the clusters created by the test

--- ci-operator/step-registry/rh-ecosystem-edge/assisted-chat/clean-up-cron/rh-ecosystem-edge-assisted-chat-clean-up-cron-commands.sh
+++ ci-operator/step-registry/rh-ecosystem-edge/assisted-chat/clean-up-cron/rh-ecosystem-edge-assisted-chat-clean-up-cron-commands.sh
@@ -5,8 +5,14 @@ set -o errexit
 set -o pipefail
 
 ASSISTED_SERVICE_URL="https://api.stage.openshift.com/api/assisted-install/v2"
-UNIQUE_ID=$(cat ${SHARED_DIR}/eval_test_unique_id)
-echo "${UNIQUE_ID}"
+
+n=4 # number of hours to subtract
+current_time=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+past_time=$(date -u -d "$current_time -$n hours" +"%Y-%m-%dT%H:%M:%SZ")
+unix_past_time=$(date -d "${past_time}" +"%s")
+
+echo "Current UTC time: $current_time"
+echo "Time $n hours ago (UTC): $past_time"
 
 OCM_TOKEN=$(curl -X POST https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token \
   -H "Content-Type: application/x-www-form-urlencoded" \
@@ -21,26 +27,27 @@ if [[ $(cat ${ARTIFACT_DIR}/available_clusters.json | jq 'length') -eq 0 ]]; the
   exit 0
 fi
 
-echo "Cluster were found, you can see the data in artifacts/available_clusters.json"
-
 cat ${ARTIFACT_DIR}/available_clusters.json | jq '[.[] |{id, name, created_at}]' > ${ARTIFACT_DIR}/relevant_cluster_data.json
 touch ${ARTIFACT_DIR}/clusters_to_delete
 
 jq -c '.[]' ${ARTIFACT_DIR}/relevant_cluster_data.json | while read item; do
   id=$(echo "$item" | jq -r '.id')
+  created_at=$(echo "$item" | jq -r '.created_at')
   name=$(echo "$item" | jq -r '.name')
-  if [[ "$name" == *"-${UNIQUE_ID}" ]]; then
-    echo "The cluster '${name}' is going to be deleted"
-    echo "$id" >> ${ARTIFACT_DIR}/clusters_to_delete
+  unix_created_at=$(date -d "${created_at}" +"%s")
+  if [[ $unix_created_at -lt $unix_past_time ]]; then
+    echo "The cluster with id: '${id}' and name '${name}' has to be deleted, because it was created before '${past_time}'."
+    echo "It's creation time is '${created_at}'."
+    echo "${id}" >> ${ARTIFACT_DIR}/clusters_to_delete
+  else
+    echo "The cluster with id: '${id}' and name: '${name}' was created at '${created_at}', which is after '${past_time}'."
   fi
 done
 
 if [[ ! -s "${ARTIFACT_DIR}/clusters_to_delete" ]]; then
-  echo "No clusters were found whith '${UNIQUE_ID}' in it's name."
+  echo "No clusters were found which are older than '${past_time}'."
   exit 0
 fi
-
-echo "Deleting the clusters enumerated in artifacts/clusters_to_delete"
 
 cat ${ARTIFACT_DIR}/clusters_to_delete | xargs -I@ curl -X DELETE -H "Authorization: Bearer ${OCM_TOKEN}" "${ASSISTED_SERVICE_URL}/clusters/@"
 
