6023dd29e99e39078df8d094d6cab1c384623faf
Stephen Benjamin <stephen@redhat.com>
2025-09-10 13:52:44 -0400

    Enable aggressive retry strategy for origin (#69061)
    Aggressive retry mode runs all failed tests up to 10 attempts each to
    confirm failures are real. Tests with failures below the configured
    threshold are allowed to pass as flaky; >=4 failures create rollup
    failure test case with concatenated outputs from all attempts.
    
    This should reduce retests on origin.

--- ci-operator/step-registry/single-node/e2e/test/single-node-e2e-test-commands.sh
+++ ci-operator/step-registry/single-node/e2e/test/single-node-e2e-test-commands.sh
@@ -21,6 +21,16 @@ unset KUBECONFIG
 oc adm policy add-role-to-group system:image-puller system:unauthenticated --namespace "${NAMESPACE}"
 export KUBECONFIG=$KUBECONFIG_BAK
 
+# Starting in 4.21, we will aggressively retry test failures only in
+# presubmits to determine if a failure is a flake or legitimate. This is
+# to reduce the number of retests on PR's.
+# TODO: Remove "origin" and run everywhere
+if [[ "$JOB_TYPE" == "presubmit" && ( "$PULL_BASE_REF" == "main" || "$PULL_BASE_REF" == "master" ) && "$REPO_NAME" == "origin" ]]; then
+    if openshift-tests run --help | grep -q 'retry-strategy'; then
+        TEST_ARGS+=" --retry-strategy=aggressive"
+    fi
+fi
+
 # HACK: HyperShift clusters use their own profile type, but the cluster type
 # underneath is actually AWS and the type identifier is derived from the profile
 # type. For now, just treat the `hypershift` type the same as `aws` until
