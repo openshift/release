f6a9df0816296722d87d4fb548dc30cc8498cf3b
DAMISETTI-VEERABHADRARAO <damisetti.veerabhadrarao@ibm.com>
2025-09-14 12:18:26 +0530

    added debug statements
    Signed-off-by: DAMISETTI-VEERABHADRARAO <damisetti.veerabhadrarao@ibm.com>

--- ci-operator/step-registry/hypershift/mce/ibmz/create/s390xmgmt/hypershift-mce-ibmz-create-s390xmgmt-commands.sh
+++ ci-operator/step-registry/hypershift/mce/ibmz/create/s390xmgmt/hypershift-mce-ibmz-create-s390xmgmt-commands.sh
@@ -78,7 +78,4 @@ echo "Copying kubeconfig into SHARED_DIR"
 cp "$HOME/$CLUSTER_NAME/auth/kubeconfig" "$SHARED_DIR/kubeconfig"
 echo "Kubeconfig copied into SHARED_DIR"
 
-echo "Killing any ssh processes started by this script"
-#pkill -f 'ssh -o StrictHostKeyChecking=no -i /output/'
 
-pkill -f 'ssh -o StrictHostKeyChecking=no -i /output/' || true
