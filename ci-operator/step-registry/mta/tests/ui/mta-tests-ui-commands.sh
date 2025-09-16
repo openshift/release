04cb1c6a6520f6fd95dc013bc3b512f3c3413b84
Diane Frazzette <dfrazzet@redhat.com>
2025-09-15 23:40:34 -0400

    [INTEROP-8370] Expand MTA from 7.2.1 to 7.3.1; update Cypress dictionary.

--- ci-operator/step-registry/mta/tests/ui/mta-tests-ui-commands.sh
+++ ci-operator/step-registry/mta/tests/ui/mta-tests-ui-commands.sh
@@ -15,9 +15,8 @@ export CYPRESS_INCLUDE_TAGS=$MTA_TESTS_UI_SCOPE
 # Always return true, otherwise the script will fail before it is able to archive anything
 echo "Executing Cypress tests..."
 npx cypress run \
-    --config video=false \
-    --spec $CYPRESS_SPEC \
-    --env "tackleUrl=${TARGET_URL}" || true
+    --config video=false,baseUrl=${TARGET_URL} \
+    --spec $CYPRESS_SPEC || true
 
 # Combine results into one JUnit results file
 echo "Merging results reports..."
