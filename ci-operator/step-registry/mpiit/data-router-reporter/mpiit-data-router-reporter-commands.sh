478795e75c5b8e41324170cfa47e5c60d4df09b8
oharan2 <oharan@redhat.com>
2025-09-17 16:59:33 +0300

    Update metadata.json URL and remove attributes list

--- ci-operator/step-registry/mpiit/data-router-reporter/mpiit-data-router-reporter-commands.sh
+++ ci-operator/step-registry/mpiit/data-router-reporter/mpiit-data-router-reporter-commands.sh
@@ -3,22 +3,15 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-sleep 3h
-
 export DATAROUTER_RESULTS="${SHARED_DIR}/*.xml"
-export REPORTPORTAL_HOSTNAME
-export REPORTPORTAL_PROJECT="lp-interop-dr_personal"
 export OCP_VERSION
-export REPORTPORTAL_LAUNCH_NAME="lp-interop-${OCP_VERSION}"
+
 export REPORTPORTAL_APPLY_TFA="${AAPLY_TFA}"
 export REPORTPORTAL_CMP
+export REPORTPORTAL_HOSTNAME
+export REPORTPORTAL_PROJECT="lp-interop-dr_personal"
+export REPORTPORTAL_LAUNCH_NAME="lp-interop-${OCP_VERSION}"
 
-export REPORTPORTAL_LAUNCH_ATTRIBUTES="[
-  {\"key\": \"version\", \"value\": \"${OCP_VERSION}\"},
-  {\"key\": \"environment\", \"value\": \"prod\"},
-  {\"key\": \"component\", \"value\": \"${REPORTPORTAL_CMP}\"},
-  {\"key\": \"team\", \"value\": \"mpiit\"}
-]"
-export DATAROUTER_METADATA_URL="https://raw.githubusercontent.com/oharan2/cspi-utils/8fadc6dde0d67f549dfea0acd8356b8184aac99a/data-router/metadata.json"
+export DATAROUTER_METADATA_URL="https://raw.githubusercontent.com/CSPI-QE/cspi-utils/refs/heads/main/data-router/metadata.json"
 
 datarouter-openshift-ci
