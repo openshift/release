b864de9f5ca533984383c08335d1c23ea7e87801
Stephen Finucane <stephenfin@redhat.com>
2025-09-18 10:33:05 +0100

    hypershift/openstack: Change base domain used (#69319)
    * hypershift/openstack: Change base domain used
    
    The DPTP team recently reworked how credentials are managed, and the
    credentials we have no longer allow access to this zone. Change to a
    zone we do have authority over.
    
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>
    
    * openstack: Log on missing credentials file
    
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>
    
    * Remove irrelevant configuration
    
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>
    
    * Misc formatting
    
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>
    
    * openstack: use default aws profile for dnsrecords steps
    
    There's no good reason to use a different profile now that the team owns
    the credentials. Using the default profile makes credential management
    simpler as the HCP workflows expect to use the default profile.
    
    ---------
    
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>
    Co-authored-by: Martin André <m.andre@redhat.com>

--- ci-operator/step-registry/openstack/deprovision/dnsrecords/openstack-deprovision-dnsrecords-commands.sh
+++ ci-operator/step-registry/openstack/deprovision/dnsrecords/openstack-deprovision-dnsrecords-commands.sh
@@ -10,22 +10,26 @@ export AWS_DEFAULT_OUTPUT=json
 
 if [ -z "${AWS_PROFILE:-}" ]; then
   unset AWS_PROFILE
-fi 
+fi
+
+if [ ! -f "${AWS_SHARED_CREDENTIALS_FILE}" ]; then
+  echo "Credentials file is not correctly mounted"
+fi
 
 echo "Getting the hosted zone ID for domain: ${BASE_DOMAIN}"
 HOSTED_ZONE_ID="$(aws route53 list-hosted-zones-by-name \
-            --dns-name "${BASE_DOMAIN}" \
-            --query "HostedZones[? Config.PrivateZone != \`true\` && Name == \`${BASE_DOMAIN}.\`].Id" \
-            --output text)"
-			
+  --dns-name "${BASE_DOMAIN}" \
+  --query "HostedZones[? Config.PrivateZone != \`true\` && Name == \`${BASE_DOMAIN}.\`].Id" \
+  --output text)"
+
 if [[ -f "${SHARED_DIR}/dns_up.json" ]]; then
-	echo "Deleting DNS records."
-	sed '
-	    s/UPSERT/DELETE/;
-	    s/Upsert/Delete/;
-	    ' "${SHARED_DIR}/dns_up.json" > "${SHARED_DIR}/dns_down.json"
-	cp "${SHARED_DIR}/dns_down.json" "${ARTIFACT_DIR}/"
-	aws route53 change-resource-record-sets --hosted-zone-id "$HOSTED_ZONE_ID" --change-batch "file://${SHARED_DIR}/dns_down.json"
+  echo "Deleting DNS records."
+  sed '
+    s/UPSERT/DELETE/;
+    s/Upsert/Delete/;
+    ' "${SHARED_DIR}/dns_up.json" > "${SHARED_DIR}/dns_down.json"
+  cp "${SHARED_DIR}/dns_down.json" "${ARTIFACT_DIR}/"
+  aws route53 change-resource-record-sets --hosted-zone-id "$HOSTED_ZONE_ID" --change-batch "file://${SHARED_DIR}/dns_down.json"
 else
-	echo "File 'dns_up.json' not found. DNS records not deleted."
+  echo "File 'dns_up.json' not found. DNS records not deleted."
 fi
