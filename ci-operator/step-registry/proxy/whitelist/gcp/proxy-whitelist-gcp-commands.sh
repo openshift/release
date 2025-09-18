19e209aa3cd7a2aa5babbca5ce1b8ca5841460a7
Jianli Wei <jiwei@redhat.com>
2025-09-18 10:39:09 +0800

    CORS-3371: add QE tests for GCP custom endpoint disconnect & proxy cluster

--- ci-operator/step-registry/proxy/whitelist/gcp/proxy-whitelist-gcp-commands.sh
+++ ci-operator/step-registry/proxy/whitelist/gcp/proxy-whitelist-gcp-commands.sh
@@ -6,9 +6,32 @@ set -o pipefail
 
 # https://docs.openshift.com/container-platform/4.17/installing/install_config/configuring-firewall.html#configuring-firewall
 
-cat <<EOF > ${SHARED_DIR}/proxy_whitelist.txt
+if [[ "${ENABLE_GCP_CUSTOM_ENDPOINT}" == "yes" ]]; then
+    case "${CCO_MANUAL_TYPE}" in
+        "gcp_wif")
+        cat <<EOF > ${SHARED_DIR}/proxy_whitelist.txt
+.p.googleapis.com
+sts.googleapis.com
+iamcredentials.googleapis.com
+EOF
+        ;;
+        "gcp_static_users")
+        cat <<EOF > ${SHARED_DIR}/proxy_whitelist.txt
+.p.googleapis.com
+oauth2.googleapis.com
+EOF
+        ;;
+        *)
+        cat <<EOF > ${SHARED_DIR}/proxy_whitelist.txt
+.p.googleapis.com
+EOF
+        ;;
+    esac
+else
+    cat <<EOF > ${SHARED_DIR}/proxy_whitelist.txt
 .googleapis.com
 accounts.google.com
 EOF
+fi
 
 cp ${SHARED_DIR}/proxy_whitelist.txt ${ARTIFACT_DIR}/
