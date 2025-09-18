a0d21c9f7a26407d58dde9d204cbd678a138611f
Carlos Cardenosa <ccardeno@redhat.com>
2025-09-18 11:09:53 +0200

    Run on accepted EC's or Accepted Nightlies based on availability (once in 2 days)
    Signed-off-by: Carlos Cardenosa <ccardeno@redhat.com>

--- ci-operator/step-registry/telcov10n/functional/compute-nto/ocp-deploy/telcov10n-functional-compute-nto-ocp-deploy-commands.sh
+++ ci-operator/step-registry/telcov10n/functional/compute-nto/ocp-deploy/telcov10n-functional-compute-nto-ocp-deploy-commands.sh
@@ -72,7 +72,8 @@ main() {
         --extra-vars "release=${VERSION}" \
         --extra-vars "cluster_name=${CLUSTER_NAME}" \
         --extra-vars "kubeconfig=/home/telcov10n/project/generated/${CLUSTER_NAME}/auth/kubeconfig" \
-        --extra-vars "ocp_version_facts_release_type=${OCP_VERSION_RELEASE_TYPE}"
+        --extra-vars "ocp_version_facts_release_type=${OCP_VERSION_RELEASE_TYPE}" \
+        --extra-vars "ocp_version_release_age_max_days=${OCP_VERSION_RELEASE_AGE_MAX_DAYS}"
 
     echo "Store inventory in SHARED_DIR"
     cp -r /eco-ci-cd/inventories/ocp-deployment/host_vars/* "${SHARED_DIR}"/
