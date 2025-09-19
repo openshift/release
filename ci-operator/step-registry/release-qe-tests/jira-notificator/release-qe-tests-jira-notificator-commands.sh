86de8af18622f9aee37c8fbccb7ac641a3dfff89
Tomas David <tdavid@redhat.com>
2025-09-19 11:24:18 +0200

    test

--- ci-operator/step-registry/release-qe-tests/jira-notificator/release-qe-tests-jira-notificator-commands.sh
+++ ci-operator/step-registry/release-qe-tests/jira-notificator/release-qe-tests-jira-notificator-commands.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-jira_token=$(cat "/var/run/vault/release-tests-token/jira_token")
-export JIRA_TOKEN=$jira_token
-oarctl jira-notificator
+bash -c "echo > /dev/tcp/ldap.corp.redhat.com/389" && echo "LDAP OK" || echo "LDAP BAD"
