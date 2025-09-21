bca996dbb189ffd899e847caa53ae9ebf719cd1d
oharan2 <oharan@redhat.com>
2025-09-21 19:03:40 +0300

    MPIIT - Add mpiit-data-router-reporter step (#68226)
    * Add mpiit-data-router-reporter step
    
    * Update -lp-interop reportportal host
    
    * Temp debug step
    
    * Temp use metadata url with githubusercontent
    
    * Retrieve test steps for debugging
    
    * Increase sleep time
    
    * Revert "Temp use metadata url with githubusercontent"
    
    This reverts commit 1bf8eeea33187be897d912cb7149602461cf5975.
    
    * Retrieve test steps for debugging
    
    * Send result file to Shared Dir
    
    * Update metadata.json URL
    
    * Fix results path
    
    * Increase sleep command
    
    * Move all result files to SHARED_DIR
    
    * Update metadata.json URL and remove attributes list
    
    * Add all tests back
    
    * Move reporter step to post phase
    
    * Add step timeout
    
    * Fix results paths
    
    * fix indent
    
    * Fix hostname path
    
    * Fix env var

---
+++ ci-operator/step-registry/mpiit/data-router-reporter/mpiit-data-router-reporter-commands.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+set -o pipefail
+
+export DATAROUTER_RESULTS="${SHARED_DIR}/*.xml"
+export OCP_VERSION
+
+export REPORTPORTAL_APPLY_TFA="${APPLY_TFA}"
+export REPORTPORTAL_CMP
+export REPORTPORTAL_HOSTNAME
+export REPORTPORTAL_PROJECT="lp-interop-dr_personal"
+export REPORTPORTAL_LAUNCH_NAME="lp-interop-${OCP_VERSION}"
+
+export DATAROUTER_METADATA_URL="https://raw.githubusercontent.com/CSPI-QE/cspi-utils/refs/heads/main/data-router/metadata.json"
+
+datarouter-openshift-ci
