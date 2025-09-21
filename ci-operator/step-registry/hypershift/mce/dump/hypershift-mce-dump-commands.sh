9c341f1ff34eedd009f2ed26aa1fa91593f7689a
DAMISETTI-VEERABHADRARAO <damisetti.veerabhadrarao@ibm.com>
2025-09-21 18:55:30 +0530

    Updated conformance flow
    Signed-off-by: DAMISETTI-VEERABHADRARAO <damisetti.veerabhadrarao@ibm.com>

--- ci-operator/step-registry/hypershift/mce/dump/hypershift-mce-dump-commands.sh
+++ ci-operator/step-registry/hypershift/mce/dump/hypershift-mce-dump-commands.sh
@@ -13,10 +13,12 @@ HO_IMAGE=$(oc get deployment -n hypershift operator -ojsonpath='{.spec.template.
 mkdir /tmp/hs-cli
 brew_registry_auth=$(echo -n "${BREW_IMAGE_REGISTRY_USERNAME}:$(<$BREW_IMAGE_REGISTRY_TOKEN_PATH)" | base64 -w 0)
 echo '{}' | jq --arg auth "$brew_registry_auth" '.auths += {"brew.registry.redhat.io": {"auth": $auth}}' > /tmp/brew_configjson
+
+
 NEW_HO_IMAGE=$(echo "$HO_IMAGE" | sed 's|registry.redhat.io/multicluster-engine|quay.io:443/acm-d|')
 oc image extract "$NEW_HO_IMAGE" --path /usr/bin/hypershift-no-cgo:/tmp/hs-cli --registry-config=/tmp/.dockerconfigjson
 
-oc image extract "$NEW_HO_IMAGE" --path /usr/bin/hypershift-no-cgo:/tmp/hs-cli --registry-config=/tmp/brew_configjson
+
 chmod +x /tmp/hs-cli/hypershift-no-cgo
 HCP_CLI="/tmp/hs-cli/hypershift-no-cgo"
 
