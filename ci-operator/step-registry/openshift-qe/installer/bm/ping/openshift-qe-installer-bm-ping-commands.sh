5c2e04655b7d49e468f0b9738102795690d3c69b
Vicente Zepeda Mas <vzepedam@redhat.com>
2025-09-22 19:16:28 +0200

    Test curl
    Signed-off-by: Vicente Zepeda Mas <vzepedam@redhat.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/ping/openshift-qe-installer-bm-ping-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/ping/openshift-qe-installer-bm-ping-commands.sh
@@ -11,4 +11,11 @@ ls -l $ARTIFACT_DIR
 
 echo "LS DIR: PWD"
 ls -l $PWD
+
+export prow_artifacts_base_url="https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/test-platform-results/logs"
+task_id=$BUILD_ID
+job_id=$JOB_NAME
+prowjobjson_url="${prow_artifacts_base_url}/${job_id}/${task_id}/prowjob.json"
+pull_number=$(curl -s "$prowjobjson_url" | jq -r '.metadata.labels."prow.k8s.io/refs.pull"')
+echo $pull_number
 # ---->
