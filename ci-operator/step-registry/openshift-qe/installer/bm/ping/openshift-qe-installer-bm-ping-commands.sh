215a34b6c1169137a28aa731694d863029163fdb
Vicente Zepeda Mas <vzepedam@redhat.com>
2025-09-22 16:48:39 +0200

    Testing prow directories
    Signed-off-by: Vicente Zepeda Mas <vzepedam@redhat.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/ping/openshift-qe-installer-bm-ping-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/ping/openshift-qe-installer-bm-ping-commands.sh
@@ -4,6 +4,11 @@ set -o nounset
 set -o pipefail
 set -x
 
-bastion=$(cat "/secret/address")
+# Test remove after determining which is the correct directory
+# <----
+echo "LS DIR ARTIFACT_DIR: ${ARTIFACT_DIR}"
+ls -l $ARTIFACT_DIR
 
-ping -c 5 $bastion
+echo "LS DIR ARTIFACTS: ${ARTIFACTS}"
+ls -l $ARTIFACTS
+# ---->
