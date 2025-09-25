98fc365bfa97536a193534316c389b38769e40e3
Venkatesh <ves@redhat.com>
2025-09-25 12:16:41 +0530

    install tools from script

--- ci-operator/step-registry/aro-hcp/provision/create-dev-env/aro-hcp-provision-create-dev-env-commands.sh
+++ ci-operator/step-registry/aro-hcp/provision/create-dev-env/aro-hcp-provision-create-dev-env-commands.sh
@@ -13,9 +13,15 @@ export TEST_USER_TENANT_ID; TEST_USER_TENANT_ID=$(cat /var/run/hcp-integration-c
 az login --service-principal -u "${TEST_USER_CLIENT_ID}" -p "${TEST_USER_CLIENT_SECRET}" --tenant "${TEST_USER_TENANT_ID}"
 az bicep install
 az bicep version
+echo "${CUSTOMER_SUBSCRIPTION}"
 az account show
 az account set --subscription "ARO Hosted Control Planes (EA Subscription 1)"
 az account show
+
+# install required tools
+curl -sL "https://github.com/jqlang/jq/releases/latest/download/jq-linux-amd64" -o /usr/local/bin/jq && chmod +x /usr/local/bin/jq
+curl -sL "https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64" -o /usr/local/bin/yq && chmod +x /usr/local/bin/yq
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && chmod 700 get_helm.sh && ./get_helm.sh && rm get_helm.sh
 
 unset GOFLAGS
 make infra.all deployall
