73b156b42a927b094b9bf8bf505d84b1fad00905
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2025-10-07 14:28:57 +0200

    TRT-2338: Grab latest arm nightly for microshift rebase

--- ci-operator/step-registry/openshift/microshift/infra/rpm-install-aws/openshift-microshift-infra-rpm-install-aws-commands.sh
+++ ci-operator/step-registry/openshift/microshift/infra/rpm-install-aws/openshift-microshift-infra-rpm-install-aws-commands.sh
@@ -99,12 +99,14 @@ if [ -n "${REBASE_TO}" ]; then
   cd /go/src/github.com/openshift/microshift/
   DEST_DIR="${HOME}"/.local/bin ./scripts/fetch_tools.sh yq
   # Extract the ARM image from the nightly release
-  ARM_RELEASE_IMAGE="${OPENSHIFT_RELEASE_IMAGE_ARM}"
-  if [[ -z "${ARM_RELEASE_IMAGE}" ]]; then
+  oc registry login --to=/tmp/registry.json
+  release_arm64="$(oc image info --registry-config=/tmp/registry.json ${OPENSHIFT_RELEASE_IMAGE_ARM} -o json | jq -r '.config.config.Labels."io.openshift.release"')"
+  if [[ -z "${release_arm64}" ]]; then
     echo "Failed to extract ARM release image from nightly release image"
     trap_install_status_exit_code "$EXIT_CODE_REBASE_FAILURE"
     exit 1
   fi
+  ARM_RELEASE_IMAGE="registry.ci.openshift.org/ocp-arm64/release-arm64:${release_arm64}"
   # Bail out without error if the rebase fails. Next steps should be skipped if this happens.
   PULLSPEC_RELEASE_AMD64="${REBASE_TO}" \
   PULLSPEC_RELEASE_ARM64="${ARM_RELEASE_IMAGE}" \
