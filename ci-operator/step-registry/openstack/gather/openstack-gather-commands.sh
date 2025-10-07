4674ecb1a348d97cbf8046c6911a9336cd225989
Stephen Finucane <stephenfin@redhat.com>
2025-10-07 11:01:34 +0100

    openstack-gather: Trivial formatting
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- ci-operator/step-registry/openstack/gather/openstack-gather-commands.sh
+++ ci-operator/step-registry/openstack/gather/openstack-gather-commands.sh
@@ -10,14 +10,14 @@ OPENSTACK_EXTERNAL_NETWORK="${OPENSTACK_EXTERNAL_NETWORK:-$(<"${SHARED_DIR}/OPEN
 CREATE_FIPS=1
 
 if test -f "${SHARED_DIR}/proxy-conf.sh"; then
-    # shellcheck source=/dev/null
-    source "${SHARED_DIR}/proxy-conf.sh"
-    CREATE_FIPS=0
+	# shellcheck source=/dev/null
+	source "${SHARED_DIR}/proxy-conf.sh"
+	CREATE_FIPS=0
 fi
 
 if [[ "$CONFIG_TYPE" == *"proxy"* ]]; then
-    BASTION_FIP=$(<"${SHARED_DIR}/BASTION_FIP")
-    BASTION_USER=$(<"${SHARED_DIR}/BASTION_USER")
+	BASTION_FIP=$(<"${SHARED_DIR}/BASTION_FIP")
+	BASTION_USER=$(<"${SHARED_DIR}/BASTION_USER")
 fi
 
 collect_bootstrap_logs() {
@@ -59,36 +59,36 @@ collect_bootstrap_logs() {
 			done
 			# Ideally this would be removed once the openshift-install gather bootstrap starts supporting proxy https://issues.redhat.com/browse/CORS-2367
 			SSH_PRIV_KEY_PATH="${CLUSTER_PROFILE_DIR}/ssh-privatekey"
-    			if test -f "${SHARED_DIR}/squid-credentials.txt"; then
-    			    echo "This job uses a proxy but without a bastion, `openshift-install gather` is not supported yet, see CORS-2367"
+			if test -f "${SHARED_DIR}/squid-credentials.txt"; then
+				echo "This job uses a proxy but without a bastion, `openshift-install gather` is not supported yet, see CORS-2367"
 			else
-                            if [[ "$CONFIG_TYPE" == *"proxy"* ]]; then
-			    	# configure the local container environment to have the correct SSH configuration
-			    	if ! whoami &> /dev/null; then
-			    		if [[ -w /etc/passwd ]]; then
-			    			echo "${BASTION_USER}:x:$(id -u):0:${BASTION_USER} user:${HOME}:/sbin/nologin" >> /etc/passwd
-			    		fi
-			    	fi
-			    	SSH_ARGS="-o ConnectTimeout=10 -o StrictHostKeyChecking=no -i $SSH_PRIV_KEY_PATH"
-			    	SSH_CMD="ssh $SSH_ARGS $BASTION_USER@$BASTION_FIP"
-			    	SCP_CMD="scp $SSH_ARGS"
-			    	if ! $SSH_CMD uname -a; then
-			    		echo "ERROR: Bastion proxy is not reachable via $BASTION_FIP"
-			    		exit 1
-			    	fi
-			    	echo "Moving credentials and openshift binary to Bastion Proxy"
-			    	$SCP_CMD "$SSH_PRIV_KEY_PATH" /bin/openshift-install "${BASTION_USER}@${BASTION_FIP}:/tmp"
-			    	echo "Gathering bootstrap logs from Bastion Proxy"
-			    	$SSH_CMD bash - << EOF
+				if [[ "$CONFIG_TYPE" == *"proxy"* ]]; then
+					# configure the local container environment to have the correct SSH configuration
+					if ! whoami &> /dev/null; then
+						if [[ -w /etc/passwd ]]; then
+							echo "${BASTION_USER}:x:$(id -u):0:${BASTION_USER} user:${HOME}:/sbin/nologin" >> /etc/passwd
+						fi
+					fi
+					SSH_ARGS="-o ConnectTimeout=10 -o StrictHostKeyChecking=no -i $SSH_PRIV_KEY_PATH"
+					SSH_CMD="ssh $SSH_ARGS $BASTION_USER@$BASTION_FIP"
+					SCP_CMD="scp $SSH_ARGS"
+					if ! $SSH_CMD uname -a; then
+						echo "ERROR: Bastion proxy is not reachable via $BASTION_FIP"
+						exit 1
+					fi
+					echo "Moving credentials and openshift binary to Bastion Proxy"
+					$SCP_CMD "$SSH_PRIV_KEY_PATH" /bin/openshift-install "${BASTION_USER}@${BASTION_FIP}:/tmp"
+					echo "Gathering bootstrap logs from Bastion Proxy"
+					$SSH_CMD bash - << EOF
 /tmp/openshift-install gather bootstrap --key /tmp/ssh-privatekey ${GATHER_BOOTSTRAP_ARGS[@]}
 EOF
-			    	echo "Copying logs"
-			    	$SCP_CMD "${BASTION_USER}@${BASTION_FIP}:/home/${BASTION_USER}/log-bundle-*.tar.gz" "${SHARED_DIR}/"
-			    else
-			    	openshift-install gather bootstrap --key "${SSH_PRIV_KEY_PATH}" "${GATHER_BOOTSTRAP_ARGS[@]}"
-			    	cp log-bundle-*.tar.gz "${ARTIFACT_DIR}"
-			    	echo "${FIPS[@]}" | xargs --no-run-if-empty openstack floating ip delete
-			    fi
+					echo "Copying logs"
+					$SCP_CMD "${BASTION_USER}@${BASTION_FIP}:/home/${BASTION_USER}/log-bundle-*.tar.gz" "${SHARED_DIR}/"
+				else
+					openshift-install gather bootstrap --key "${SSH_PRIV_KEY_PATH}" "${GATHER_BOOTSTRAP_ARGS[@]}"
+					cp log-bundle-*.tar.gz "${ARTIFACT_DIR}"
+					echo "${FIPS[@]}" | xargs --no-run-if-empty openstack floating ip delete
+				fi
 			fi
 		fi
 	fi
@@ -112,27 +112,27 @@ for volume in $(jq -r '.[].ID' "${ARTIFACT_DIR_JSON}/openstack_volume_list.json"
 done | jq --slurp '.' > "${ARTIFACT_DIR_JSON}/openstack_volume_show.json"
 
 openstack port list -f json \
-        | jq --arg CLUSTER_NAME "$CLUSTER_NAME" 'map(select(.Name | test($CLUSTER_NAME)))' \
-        > "${ARTIFACT_DIR_JSON}/openstack_port_list.json"
+    | jq --arg CLUSTER_NAME "$CLUSTER_NAME" 'map(select(.Name | test($CLUSTER_NAME)))' \
+    > "${ARTIFACT_DIR_JSON}/openstack_port_list.json"
 
 for port in $(jq -r '.[].ID' "${ARTIFACT_DIR_JSON}/openstack_port_list.json"); do
 	openstack port show "$port" -f json
 done | jq --slurp '.' > "${ARTIFACT_DIR_JSON}/openstack_port_show.json"
 
 openstack subnet list -f json \
-        | jq --arg CLUSTER_NAME "$CLUSTER_NAME" 'map(select(.Name | test($CLUSTER_NAME)))' \
-        > "${ARTIFACT_DIR_JSON}/openstack_subnet_list.json"
+    | jq --arg CLUSTER_NAME "$CLUSTER_NAME" 'map(select(.Name | test($CLUSTER_NAME)))' \
+    > "${ARTIFACT_DIR_JSON}/openstack_subnet_list.json"
 
 for port in $(jq -r '.[].ID' "${ARTIFACT_DIR_JSON}/openstack_subnet_list.json"); do
-        openstack subnet show "$port" -f json
+	openstack subnet show "$port" -f json
 done | jq --slurp '.' > "${ARTIFACT_DIR_JSON}/openstack_subnet_show.json"
 
 openstack floating ip list --long -f json \
-        | jq --arg CLUSTER_NAME "$CLUSTER_NAME" 'map(select(.Description | test($CLUSTER_NAME)))' \
-        > "${ARTIFACT_DIR_JSON}/openstack_fip_list.json"
+    | jq --arg CLUSTER_NAME "$CLUSTER_NAME" 'map(select(.Description | test($CLUSTER_NAME)))' \
+    > "${ARTIFACT_DIR_JSON}/openstack_fip_list.json"
 
 for fip in $(jq -r '.[].ID' "${ARTIFACT_DIR_JSON}/openstack_fip_list.json"); do
-        openstack floating ip show "$fip" -f json
+	openstack floating ip show "$fip" -f json
 done | jq --slurp '.' > "${ARTIFACT_DIR_JSON}/openstack_fip_show.json"
 
 mkdir -p "${ARTIFACT_DIR}/nodes"
@@ -154,7 +154,7 @@ done
 openstack port list | grep "$CLUSTER_NAME" > "${ARTIFACT_DIR}/openstack_ports.log" || true
 for port in $(openstack port list -c Name -f value | { grep "$CLUSTER_NAME" || true; } | sort); do
 	echo -e "\n$ openstack port show $port" >> "${ARTIFACT_DIR}/openstack_ports.log"
-	openstack port show "$port"               >> "${ARTIFACT_DIR}/openstack_ports.log"
+	openstack port show "$port"             >> "${ARTIFACT_DIR}/openstack_ports.log"
 done
 
 openstack subnet list | grep "$CLUSTER_NAME" > "${ARTIFACT_DIR}/openstack_subnets.log" || true
