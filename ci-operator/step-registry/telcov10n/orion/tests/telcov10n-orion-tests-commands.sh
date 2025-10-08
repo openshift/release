f718aed2b7d81e847fc8d0b942ab8e376d9b8b85
Abraham <apalanis@redhat.com>
2025-10-08 08:12:58 -0400

    orion: test

--- ci-operator/step-registry/telcov10n/orion/tests/telcov10n-orion-tests-commands.sh
+++ ci-operator/step-registry/telcov10n/orion/tests/telcov10n-orion-tests-commands.sh
@@ -76,6 +76,7 @@ fi
 
 set +e
 set -o pipefail
+echo "Start orion test"
 FILENAME=$(echo $CONFIG | awk -F/ '{print $2}' | awk -F. '{print $1}')
 ES_IDX=${ES_METADATA_INDEX} ES_SERVER=${ES_SERVER} orion cmd --config ${CONFIG} ${EXTRA_FLAGS} | tee ${ARTIFACT_DIR}/$FILENAME.txt
 orion_exit_status=$?
