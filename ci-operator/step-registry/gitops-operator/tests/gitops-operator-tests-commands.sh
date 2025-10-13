f259dcf6e461d0d4c1521154e1e7528dd808eed4
Varsha B <vab@redhat.com>
2025-10-13 12:19:08 +0530

    change branch version
    Signed-off-by: Varsha B <vab@redhat.com>

--- ci-operator/step-registry/gitops-operator/tests/gitops-operator-tests-commands.sh
+++ ci-operator/step-registry/gitops-operator/tests/gitops-operator-tests-commands.sh
@@ -2,14 +2,12 @@
 
 set -x
 
-unset CI
-
 exit_code=0
 scripts/openshift-CI-kuttl-tests.sh
-kubectl kuttl test test/openshift/e2e/sequential --config test/openshift/e2e/sequential/kuttl-test.yaml --report xml || exit_code=1
+unset CI
+make e2e-tests-ginkgo || exit_code=1
 
-
-# Move results to Artifacts directory
-cp ./kuttl-test.xml ${ARTIFACT_DIR}/junit_gitops-sequential.xml
+# Find report
+ls
 
 exit $exit_code
