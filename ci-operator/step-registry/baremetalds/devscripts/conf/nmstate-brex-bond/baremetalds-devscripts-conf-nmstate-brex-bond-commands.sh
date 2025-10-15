084843f3289eae2b7997aa10a52e31c976621686
Emilia Desch <emy@redhat.com>
2025-10-15 16:38:25 +0200

    rename ref to reflect it's creating a nmstate-managed br-ex bond

--- ci-operator/step-registry/baremetalds/devscripts/conf/nmstate-brex-bond/baremetalds-devscripts-conf-nmstate-brex-bond-commands.sh
+++ ci-operator/step-registry/baremetalds/devscripts/conf/nmstate-brex-bond/baremetalds-devscripts-conf-nmstate-brex-bond-commands.sh
@@ -4,6 +4,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-echo "************ baremetalds devscripts conf nmstate-brex command ************"
+echo "************ baremetalds devscripts conf nmstate-brex-bond command ************"
 
 echo "ASSETS_EXTRA_FOLDER=./network-configs/nmstate-brex-bond" >> "${SHARED_DIR}/dev-scripts-additional-config"
