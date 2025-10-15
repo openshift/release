2fc9f97e8c405ce239ab050c373ee06fd469a6d4
Emilia Desch <emy@redhat.com>
2025-10-15 16:33:38 +0200

    remove dual stack enablement for ref nmstate-brex-bond

--- ci-operator/step-registry/baremetalds/devscripts/conf/nmstate-brex/baremetalds-devscripts-conf-nmstate-brex-commands.sh
+++ ci-operator/step-registry/baremetalds/devscripts/conf/nmstate-brex/baremetalds-devscripts-conf-nmstate-brex-commands.sh
@@ -6,5 +6,4 @@ set -o pipefail
 
 echo "************ baremetalds devscripts conf nmstate-brex command ************"
 
-echo "IP_STACK=v4v6" >> "${SHARED_DIR}/dev-scripts-additional-config"
 echo "ASSETS_EXTRA_FOLDER=./network-configs/nmstate-brex-bond" >> "${SHARED_DIR}/dev-scripts-additional-config"
