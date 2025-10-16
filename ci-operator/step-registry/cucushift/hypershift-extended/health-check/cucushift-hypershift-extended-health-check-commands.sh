1e5bc244b276c5526bbc3db2bd26d49b1b4c229d
Jim Ma <ema@redhat.com>
2025-10-16 16:38:49 +0800

    Add the feature gate check in the health check step;Check if volumeattributeclass feature gate is enabled when the hostedcluster is created with --feature-set=TechPreviewNoUpgrade in 4.19 nightly job

--- ci-operator/step-registry/cucushift/hypershift-extended/health-check/cucushift-hypershift-extended-health-check-commands.sh
+++ ci-operator/step-registry/cucushift/hypershift-extended/health-check/cucushift-hypershift-extended-health-check-commands.sh
@@ -232,6 +232,25 @@ check_kms_encryption_config() {
   return 0
 }
 
+check_volumeattributeclass() {
+    local response
+    response=$(oc get --raw /apis/storage.k8s.io/v1beta1 2>/dev/null)
+    if [[ -n "$response" ]]; then
+        log "/apis/storage.k8s.io/v1beta1 is served as expected"
+    else
+        error "/apis/storage.k8s.io/v1beta1 is not served"
+        return 1
+    fi
+
+    if oc get volumeattributesclass &>/dev/null; then
+        log "VolumeAttributesClass resource exist as expected"
+    else
+        error "VolumeAttributesClass resource does NOT exist"
+        return 1
+    fi
+
+}
+
 ### Main ###
 log "Starting Hypershift health checks..."
 
@@ -280,5 +299,28 @@ retry check_pod_status || exit 1
 
 log "Saving guest cluster pod list to artifacts..."
 oc get pod -A > "${ARTIFACT_DIR}/guest-pods"
+
+if [[ -n "$CHECK_ENABLED_FEATURE_GATES" ]]; then
+    log "Checking enabled feature gates..."
+    read -ra gates_array <<< "$CHECK_ENABLED_FEATURE_GATES"
+    enabled_features=$(oc get featuregate cluster -o jsonpath='{range .status.featureGates[*].enabled[*]}{.name}{" "}{end}')
+
+    for gate in "${gates_array[@]}"; do
+      log "Checking $gate..."
+      if [[ " $enabled_features " == *" $gate "* ]]; then
+            log "Found $gate in the enabled featureGates list"
+      else
+            error "$gate is NOT found from the the enabled featureGates list"
+            exit 1
+      fi
+      # in case we need to check more thing for specific feature
+      if [[ "${gate}" == "VolumeAttributeClass" ]]; then
+        check_volumeattributeclass|| exit 1
+        log "More check for VolumeAttributesClass feature gate finished."
+      fi
+    done
+    log "✅All feature gates check finished."
+fi
+
 
 log "✅ All health checks passed."
