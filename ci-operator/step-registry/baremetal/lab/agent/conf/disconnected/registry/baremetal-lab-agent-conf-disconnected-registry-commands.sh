d17a3eb740ce8add75903383d11d55f5f34fdebe
Biagio Manzari <bmanzari@redhat.com>
2025-10-16 17:28:23 +0200

    fix

--- ci-operator/step-registry/baremetal/lab/agent/conf/disconnected/registry/baremetal-lab-agent-conf-disconnected-registry-commands.sh
+++ ci-operator/step-registry/baremetal/lab/agent/conf/disconnected/registry/baremetal-lab-agent-conf-disconnected-registry-commands.sh
@@ -9,7 +9,7 @@ buildfarm_secrets="/var/run/vault/secrets/.dockerconfigjson"
 
 pull_secret=$(<"${buildfarm_secrets}")
 
-cp $buildfarm_secrets $ARTIFACT_DIR/pull_secrets
+
 
 # Add buildfarm secrets if the mirror registry secrets are not available.
 if [ ! -f "${SHARED_DIR}/pull_secret_ca.yaml.patch" ]; then
@@ -19,3 +19,28 @@ pullSecret: >
   ${pull_secret}
 EOF
 fi
+
+# RELEASE_IMAGE_LATEST=registry.build06.ci.openshift.org/ci-op-3vpg3xwh/release@sha256:5591b23351d40563417fb22339dbf7125e5d4659752f5a2a6a44a355c9f1201a
+
+# take the part before the first occurrence of '/', i.e. 'registry.build06.ci.openshift.org'
+echo "${RELEASE_IMAGE_LATEST%%/*}" >> "${SHARED_DIR}/mirror_registry_url"
+
+install_config_mirror_patch="${SHARED_DIR}/install-config-mirror.yaml.patch"
+
+# take the part before the first occurrence of '@', i.e. 'registry.build06.ci.openshift.org/ci-op-3vpg3xwh/release'
+imcs="
+imageContentSources:
+  - mirrors:
+      - ${RELEASE_IMAGE_LATEST%%@*}
+    source: ${RELEASE_IMAGE_LATEST%%@*}
+  - mirrors:
+      - ${RELEASE_IMAGE_LATEST%%@*}
+    source: quay.io/openshift-release-dev/ocp-v4.0-art-dev
+"
+
+echo "${imcs}" >> "${install_config_mirror_patch}"
+
+CONFIG="${SHARED_DIR}/install-config.yaml"
+
+# imageContentSources patch
+yq-go m -x -i "${CONFIG}" "${install_config_mirror_patch}"
