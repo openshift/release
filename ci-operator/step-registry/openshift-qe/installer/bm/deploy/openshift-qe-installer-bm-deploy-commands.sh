7f86abfd4afee431a52eb1fcbb75052c5ef5e783
Jose Castillo Lema <josecastillolema@gmail.com>
2025-10-19 12:04:26 +0200

    Add proxy setup to some steps
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/deploy/openshift-qe-installer-bm-deploy-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/deploy/openshift-qe-installer-bm-deploy-commands.sh
@@ -58,9 +58,6 @@ payload_url: "${RELEASE_IMAGE_LATEST}"
 image_type: "minimal-iso"
 EOF
 
-echo "This is the all.yml file:"
-cat /tmp/all.yml
-
 if [[ $PUBLIC_VLAN == "false" ]]; then
   echo "Private network deployment"
   echo -e "enable_bond: $BOND" >> /tmp/all.yml
