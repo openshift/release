03c4c3bfbc56beb56a58125b43ffc19de91e391b
Sean Zhao <szhao@redhat.com>
2025-10-21 16:32:30 +0800

    clean unrelated

--- ci-operator/step-registry/quay-tests/deprovisioning-aws-gcp-azure-storage-buckets/quay-tests-deprovisioning-aws-gcp-azure-storage-buckets-commands.sh
+++ ci-operator/step-registry/quay-tests/deprovisioning-aws-gcp-azure-storage-buckets/quay-tests-deprovisioning-aws-gcp-azure-storage-buckets-commands.sh
@@ -4,6 +4,7 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+
 if [[ "$QUAY_STORAGE_PROVIDER" == 'gcp' ]]; then
     #Copy GCP auth.json from mounted secret to current directory
     mkdir -p QUAY_GCP && cd QUAY_GCP
