122a8c555373087c4a23224a80081336f8397dc4
oharan2 <oharan@redhat.com>
2025-10-23 20:58:48 +0300

    Add ACS job on 4.21

--- ci-operator/step-registry/stackrox/qa-e2e/stackrox-qa-e2e-commands.sh
+++ ci-operator/step-registry/stackrox/qa-e2e/stackrox-qa-e2e-commands.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
+
+function install_yq_if_not_exists() {
+    # Install yq manually if not found in image
+    echo "Checking if yq exists"
+    cmd_yq="$(yq --version 2>/dev/null || true)"
+    if [ -n "$cmd_yq" ]; then
+        echo "yq version: $cmd_yq"
+    else
+        echo "Installing yq"
+        mkdir -p /tmp/bin
+        export PATH=$PATH:/tmp/bin/
+        curl -L "https://github.com/mikefarah/yq/releases/latest/download/yq_linux_$(uname -m | sed 's/aarch64/arm64/;s/x86_64/amd64/')" \
+         -o /tmp/bin/yq && chmod +x /tmp/bin/yq
+    fi
+}
+
+
+function mapTestsForComponentReadiness() {
+    if [[ $MAP_TESTS == "true" ]]; then
+        results_file="${1}"
+        echo "Patching Tests Result File: ${results_file}"
+        if [ -f "${results_file}" ]; then
+            install_yq_if_not_exists
+            echo "Mapping Test Suite Name To: CNV-lp-interop"
+            yq eval -px -ox -iI0 '.testsuite."+@name" = "ACS-lp-interop"' $results_file
+        fi
+    fi
+}
+
 job="${TEST_SUITE:-${JOB_NAME_SAFE#merge-}}"
 job="${job#nightly-}"
 
@@ -11,3 +40,17 @@ if [ ! -f ".openshift-ci/dispatch.sh" ];then
 fi
 
 exec .openshift-ci/dispatch.sh "${job}"
+
+original_results="${ARTIFACT_DIR}/original_results/"
+mkdir "${original_results}"
+
+for resultfile in $(find "${ARTIFACT_DIR}" -type f -iname "*.xml"); do
+  # Keep a copy of all the original Junit files before modifying them
+  cp "${results_file}" "${original_results}"
+
+  # Map tests if needed for related use cases
+  mapTestsForComponentReadiness "${results_file}"
+
+  # Send junit file to shared dir for Data Router Reporter step
+  cp "${results_file}" "${SHARED_DIR}"
+done
