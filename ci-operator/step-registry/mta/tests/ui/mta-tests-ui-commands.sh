79fe81cca36ca37b7a5d982ff87e4173fc488855
Simran <sgill@redhat.com>
2025-10-23 19:00:57 -0700

    Add MTA job for component readiness

--- ci-operator/step-registry/mta/tests/ui/mta-tests-ui-commands.sh
+++ ci-operator/step-registry/mta/tests/ui/mta-tests-ui-commands.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
-set -o nounset
 set -o errexit
+set -o nounset
 set -o pipefail
+
+function install_yq_if_not_exists() {
+    # Install yq manually if not found in image
+    echo "Checking if yq exists"
+    cmd_yq="$(yq --version 2>/dev/null || true)"
+    if [ -n "$cmd_yq" ]; then
+        echo "yq version: $cmd_yq"
+    else
+        echo "Installing yq"
+        mkdir -p /tmp/bin
+        export PATH=$PATH:/tmp/bin/
+        curl -L "https://github.com/mikefarah/yq/releases/latest/download/yq_linux_$(uname -m | sed 's/aarch64/arm64/;s/x86_64/amd64/')" \
+            -o /tmp/bin/yq && chmod +x /tmp/bin/yq
+    fi
+}
+
+function mapTestsForComponentReadiness() {
+    if [[ $MAP_TESTS == "true" ]]; then
+        results_file="${1}"
+        echo "Patching Tests Result File: ${results_file}"
+        if [ -f "${results_file}" ]; then
+            install_yq_if_not_exists
+            echo "Mapping Test Suite Name To: ACS-lp-interop"
+            yq eval -px -ox -iI0 '.testsuite."+@name" = "ACS-lp-interop"' "${results_file}"
+        fi
+    fi
+}
 
 # Set the TARGET_URL value using the $SHARED_DIR/console.url file
 CONSOLE_URL=$(cat $SHARED_DIR/console.url)
@@ -32,3 +59,17 @@ if [ -d "/tmp/tackle-ui-tests/cypress/screenshots/" ]; then
     mkdir -p $ARTIFACT_DIR/screenshots
     cp -r /tmp/tackle-ui-tests/cypress/screenshots/* $ARTIFACT_DIR/screenshots/
 fi
+
+original_results="${ARTIFACT_DIR}/original_results"
+mkdir -p "${original_results}"
+
+for result_file in $(find "${ARTIFACT_DIR}" -type f -iname "*.xml"); do
+  # Keep a copy of all the original Junit files before modifying them
+  cp "${result_file}" "${original_results}"
+
+  # Map tests if needed for related use cases
+  mapTestsForComponentReadiness "${result_file}"
+
+  # Send junit file to shared dir for Data Router Reporter step
+  cp "${result_file}" "${SHARED_DIR}"
+done
