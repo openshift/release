9664a37d606e58ac3095e7a71871aac9b69f6e89
oharan2 <oharan@redhat.com>
2025-10-29 17:28:21 +0200

    Fix mapping scripts

--- ci-operator/step-registry/stackrox/qa-e2e/stackrox-qa-e2e-commands.sh
+++ ci-operator/step-registry/stackrox/qa-e2e/stackrox-qa-e2e-commands.sh
@@ -21,9 +21,8 @@ function mapTestsForComponentReadiness() {
         results_file="${1}"
         echo "Patching Tests Result File: ${results_file}"
         if [ -f "${results_file}" ]; then
-            install_yq_if_not_exists
             echo "Mapping Test Suite Name To: ACS-lp-interop"
-            yq eval -px -ox -iI0 '.testsuite."+@name" = "ACS-lp-interop"' $results_file || echo "Warning: yq failed for ${results_file}, debug manually" >&2
+            yq eval -ox -iI0 '.testsuite."+@name" = "ACS-lp-interop"' $results_file || echo "Warning: yq failed for ${results_file}, debug manually" >&2
         fi
     fi
 }
@@ -32,19 +31,23 @@ function mapTestsForComponentReadiness() {
 # Archive results function
 function cleanup-collect() {
     if [[ $MAP_TESTS == "true" ]]; then
+      install_yq_if_not_exists
       original_results="${ARTIFACT_DIR}/original_results/"
       mkdir "${original_results}"
       echo "Collecting original results in ${original_results}"
 
       find "${ARTIFACT_DIR}" -type f -iname "*.xml" | while IFS= read -r result_file; do
+        base_dir=$(basename "$(dirname "${result_file}")")
+        file_name=$(basename "${result_file}")
+
         # Keep a copy of all the original Junit files before modifying them
-        cp "${result_file}" "${original_results}/$(basename "$result_file")"
+        cp "${result_file}" "${original_results}/${base_dir}_${file_name}"
 
         # Map tests if needed for related use cases
         mapTestsForComponentReadiness "${result_file}"
 
         # Send junit file to shared dir for Data Router Reporter step
-        cp "$result_file" "${SHARED_DIR}/$(basename "$result_file")"
+        cp "${result_file}" "${SHARED_DIR}/${base_dir}_${file_name}"
       done
     fi
 }
