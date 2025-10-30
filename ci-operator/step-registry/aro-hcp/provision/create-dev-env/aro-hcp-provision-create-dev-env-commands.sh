d4996a74614c2d056c3e3c65238fdb6b835c207c
Venkatesh <ves@redhat.com>
2025-10-30 13:02:08 +0530

    ci-operator: Azure/ARO-HCP: add a job using dev environment

--- ci-operator/step-registry/aro-hcp/provision/create-dev-env/aro-hcp-provision-create-dev-env-commands.sh
+++ ci-operator/step-registry/aro-hcp/provision/create-dev-env/aro-hcp-provision-create-dev-env-commands.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# Create an ARO HCP Cluster + Node pool using bicep.
 set -o errexit
 set -o nounset
 set -o pipefail
-
-set -x # Turn on command tracing
+set -x 
 
 # read the secrets and login as the user
 export TEST_USER_CLIENT_ID; TEST_USER_CLIENT_ID=$(cat /var/run/hcp-integration-credentials/client-id)
@@ -15,27 +13,9 @@ az bicep install
 az bicep version
 az account set --subscription "${CUSTOMER_SUBSCRIPTION}"
 az account show
-
-# install required tools
-# Create tools directory
-mkdir -p /tmp/tools
-# installs kubectl and kubelogin
-az aks install-cli --install-location /tmp/tools/kubectl --kubelogin-install-location /tmp/tools/kubelogin
-/tmp/tools/kubectl version
-/tmp/tools/kubelogin --version 
-
-# Add to PATH
-export PATH="/tmp/tools:$PATH"
-export USER="cide"
+kubectl version
+kubelogin --version
 PRINCIPAL_ID=$(az ad sp show --id "${TEST_USER_CLIENT_ID}" --query id -o tsv)
 export PRINCIPAL_ID
 unset GOFLAGS
-make install-tools
-PATH=$(go env GOPATH)/bin:$PATH
-export PATH
-if make entrypoint/Region; then
-    cp timing.yaml "${ARTIFACT_DIR}/" || true
-else
-    cp timing.yaml "${ARTIFACT_DIR}/" || true
-    exit 1
-fi
+make entrypoint/Region TIMING_OUTPUT=${SHARED_DIR}/steps.yaml DEPLOY_ENV=prow
--- ci-operator/step-registry/aro-hcp/provision/delete-dev-env/aro-hcp-provision-delete-dev-env-commands.sh
+++ ci-operator/step-registry/aro-hcp/provision/delete-dev-env/aro-hcp-provision-delete-dev-env-commands.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# Create an ARO HCP Cluster + Node pool using bicep.
 set -o errexit
 set -o nounset
 set -o pipefail
-
-set -x # Turn on command tracing
+set -x
 
 # read the secrets and login as the user
 export TEST_USER_CLIENT_ID; TEST_USER_CLIENT_ID=$(cat /var/run/hcp-integration-credentials/client-id)
@@ -16,22 +14,11 @@ az bicep version
 az account set --subscription "${CUSTOMER_SUBSCRIPTION}"
 az account show
 az config set core.disable_confirm_prompt=true
-
-# install required tools
-# Create tools directory
-mkdir -p /tmp/tools
-# installs kubectl and kubelogin
-az aks install-cli --install-location /tmp/tools/kubectl --kubelogin-install-location /tmp/tools/kubelogin
-/tmp/tools/kubectl version
-/tmp/tools/kubelogin --version
-
-# Add to PATH
-export PATH="/tmp/tools:$PATH"
-export USER="cide"
+kubectl version
+kubelogin --version
 PRINCIPAL_ID=$(az ad sp show --id "${TEST_USER_CLIENT_ID}" --query id -o tsv)
 export PRINCIPAL_ID
 unset GOFLAGS
-make install-tools
-PATH=$(go env GOPATH)/bin:$PATH
-export PATH
-make cleanup-entrypoint/Region CLEANUP_DRY_RUN=false CLEANUP_WAIT=false
+make cleanup-entrypoint/Region DEPLOY_ENV=prow CLEANUP_DRY_RUN=false CLEANUP_WAIT=false
+make visualize TIMING_OUTPUT=${SHARED_DIR}/steps.yaml VISUALIZATION_OUTPUT=${ARTIFACT_DIR}/timing
+cp ${SHARED_DIR}/steps.yaml ${ARTIFACT_DIR}
