fd547e5228093e1f622d38fd357749660a04008c
skordas <skordas@redhat.com>
2025-11-05 11:21:52 -0500

    Starting script from ocp-qe-perfscale-ci repository

--- ci-operator/step-registry/openshift-qe/ec-report/openshift-qe-ec-report-commands.sh
+++ ci-operator/step-registry/openshift-qe/ec-report/openshift-qe-ec-report-commands.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-echo "Start test"
-echo "$OCP_XY_VERSION"
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+
+# Change this when main script will be merged to main branch.
+# OCP_QE_PERFSCALE_CI_REPO="https://github.com/openshift-eng/ocp-qe-perfscale-ci"
+# OCP_QE_PERFSCALE_CI_BRANCH="main"
+OCP_QE_PERFSCALE_CI_REPO="https://github.com/skordas/ocp-qe-perfscale-ci"
+OCP_QE_PERFSCALE_CI_BRANCH="ec-report"
+
+python --version
+push /tmp
+python -m virtualenv ./venv_qe
+source ./venv_qe/bin/activate
+
+git clone ${OCP_QE_PERFSCALE_CI_REPO} -b ${OCP_QE_PERFSCALE_CI_BRANCH} --single-branch --depth=1
+pushd ocp-qe-perfscale-ci/prow/ec-report
+python ec-report.py
