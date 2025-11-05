686822a67d7c35c44ef9b3a0f87bb4a3370f9727
Jose Castillo Lema <josecastillolema@gmail.com>
2025-11-05 11:09:52 +0100

    Better logs
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/orion/openshift-qe-orion-commands.sh
+++ ci-operator/step-registry/openshift-qe/orion/openshift-qe-orion-commands.sh
@@ -20,6 +20,7 @@ pushd orion
 
 # Invoked from orion repo by the openshift-ci bot
 if [[ -n '${PULL_NUMBER}' ]] && [[ '${REPO_NAME}' == 'orion' ]]; then
+  echo "Invoked from orion repo by the openshift-ci bot, switching to PR#${PULL_NUMBER}"
   git pull origin pull/${PULL_NUMBER}/head:${PULL_NUMBER} --rebase
   git switch ${PULL_NUMBER}
 fi
