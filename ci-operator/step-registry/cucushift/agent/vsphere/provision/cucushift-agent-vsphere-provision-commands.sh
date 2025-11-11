a87db06b2503215f78655cc94c9d5c09eeaa5a37
mhanss <mhans@redhat.com>
2025-11-11 12:42:40 +0530

    vsphere test: no space left

--- ci-operator/step-registry/cucushift/agent/vsphere/provision/cucushift-agent-vsphere-provision-commands.sh
+++ ci-operator/step-registry/cucushift/agent/vsphere/provision/cucushift-agent-vsphere-provision-commands.sh
@@ -24,8 +24,8 @@ source "${SHARED_DIR}/govc.sh"
 unset SSL_CERT_FILE
 unset GOVC_TLS_CA_CERTS
 
-echo "Installing from initial release $OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE"
-oc adm release extract -a "${CLUSTER_PROFILE_DIR}/pull-secret" "$OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE" \
+#echo "Installing from initial release $OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE"
+oc adm release extract -a "${CLUSTER_PROFILE_DIR}/pull-secret" registry.build10.ci.openshift.org/ci-ln-mxk5lb2/release:latest \
   --command=openshift-install --to=/tmp
 
 echo "Creating agent image..."
@@ -97,7 +97,7 @@ for ((i = 0; i < total_host; i++)); do
   vm_name=${hostnames[$i]}
   echo "creating Vm $vm_name.."
   /tmp/govc vm.create \
-    -m=32768 \
+    -m=16384 \
     -g=coreos64Guest \
     -c=${cpu} \
     -disk=120GB \
