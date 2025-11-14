d4299ef5644241561698a622aa93f30e6473bd1b
Deep Mistry <dmistry@redhat.com>
2025-11-14 11:48:31 -0500

    migrate ofcir to new url

--- ci-operator/step-registry/ofcir/release/ofcir-release-commands.sh
+++ ci-operator/step-registry/ofcir/release/ofcir-release-commands.sh
@@ -9,7 +9,7 @@ echo "************ ofcir packet teardown command ************"
 set -x
 CIRFILE=$SHARED_DIR/cir
 if [ -e "$CIRFILE" ] ; then
-    OFCIRURL="https://ofcir-service.ofcir-system.svc.cluster.local/v1/ofcir"
+    OFCIRURL="https://ofcir.apps-int.master.ci.devcluster.openshift.com/v1/ofcir"
     OFCIRTOKEN="$(cat "${CLUSTER_PROFILE_DIR}/ofcir-auth-token")"
     rv="$(head -n 1 "${SHARED_DIR}/install-status.txt" | awk '{print $1}' 2> /dev/null || echo 99)"
     curl --retry-all-errors --retry-delay 60 --retry 1 -kfX DELETE -H "X-OFCIRTOKEN: $OFCIRTOKEN" "$OFCIRURL/$(jq -r .name < "$CIRFILE")?name=$JOB_NAME/$BUILD_ID&rv=$rv"
