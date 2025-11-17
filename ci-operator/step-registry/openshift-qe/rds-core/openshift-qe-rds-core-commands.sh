e6179c7f5ee98d6a94be65ff1a0c0e00acc98457
Raviteja Sahukari <rsahukar@redhat.com>
2025-11-17 20:13:17 +0530

    [PERFSCALE-4265] Updated additional parameters which are required for RDS-CORE cluster to run pods at high churn rate

--- ci-operator/step-registry/openshift-qe/rds-core/openshift-qe-rds-core-commands.sh
+++ ci-operator/step-registry/openshift-qe/rds-core/openshift-qe-rds-core-commands.sh
@@ -23,4 +23,45 @@ pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
 
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
 
-WORKLOAD=rds-core PERFORMANCE_PROFILE=cpt-pao EXTRA_FLAGS="--alerting=true --check-health=true --profile-type=${PROFILE_TYPE}" ./run.sh
+# LINES 26-59 ill be removed once PR https://github.com/kube-burner/kube-burner-ocp/pull/335 is merged and KUBE-BURNER version is updated
+# GET KUBE-BURNER BINARY FROM THE BASTION HOST
+SSH_ARGS="-i ${CLUSTER_PROFILE_DIR}/jh_priv_ssh_key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
+bastion=$(cat ${CLUSTER_PROFILE_DIR}/address)
+
+LAB=$(cat ${CLUSTER_PROFILE_DIR}/lab)
+export LAB
+LAB_CLOUD=$(cat ${CLUSTER_PROFILE_DIR}/lab_cloud || cat ${SHARED_DIR}/lab_cloud)
+export LAB_CLOUD
+
+scp -q ${SSH_ARGS} root@${bastion}:/root/${LAB}/${LAB_CLOUD}/kube-burner-ocp-cluster /tmp/kube-burner-ocp-cluster
+chmod +x /tmp/kube-burner-ocp-cluster
+md5sum /tmp/kube-burner-ocp-cluster
+
+# UPDATE THE RUN.SH SCRIPT TO USE THE NEW KUBE-BURNER BINARY
+# ⚠️ Define your file and the command you want to insert
+FILE="./run.sh"
+INSERT_COMMAND='cp /tmp/kube-burner-ocp-cluster /tmp/kube-burner-ocp'
+
+LAST_LINE_NUM=$(grep -n "download_binary" "$FILE" | tail -1 | cut -d: -f1)
+
+if [ -z "$LAST_LINE_NUM" ]; then
+    echo "Error: 'download_binary' not found in $FILE."
+    exit 1
+fi
+
+
+echo "Found last occurrence of 'download_binary' on line $LAST_LINE_NUM."
+echo "Inserting command on line $((LAST_LINE_NUM + 1))..."
+
+sed -i.bak "${LAST_LINE_NUM}a\\
+${INSERT_COMMAND}" "$FILE"
+
+echo "Insertion complete. A backup of the original file was saved as ${FILE}.bak"
+
+
+# RUN THE WORKLOAD
+if [ "$CHURN" == "true" ]; then
+  EXTRA_FLAGS="${EXTRA_FLAGS} --churn-cycles ${CHURN_CYCLES} --churn-percent ${CHURN_PERCENT} --dpdk-devicepool ${SRIOV_DPDK_DEVICEPOOL} --net-devicepool ${SRIOV_NET_DEVICEPOOL}"
+fi
+
+WORKLOAD=rds-core PERFORMANCE_PROFILE=${PERFORMANCE_PROFILE} EXTRA_FLAGS="${EXTRA_FLAGS} --profile-type=${PROFILE_TYPE}" ./run.sh
