93e454a6fd651e252cb01d610a97717c2757f408
Jianli Wei <jiwei@redhat.com>
2025-11-20 15:27:29 +0800

    OCPSTRAT-2394: CORS-4263 and CORS-4231

--- ci-operator/step-registry/gcp/provision/minimal-permission/gcp-provision-minimal-permission-commands.sh
+++ ci-operator/step-registry/gcp/provision/minimal-permission/gcp-provision-minimal-permission-commands.sh
@@ -12,8 +12,29 @@ set -o pipefail
 EXIT_CODE=100
 trap 'if [[ "$?" == 0 ]]; then EXIT_CODE=0; fi; echo "${EXIT_CODE}" > "${SHARED_DIR}/install-pre-config-status.txt"' EXIT TERM
 
-if [[ "${GCP_INSTALL_USE_MINIMAL_PERMISSIONS}" == "no" ]] && [[ "${GCP_CCO_MANUAL_USE_MINIMAL_PERMISSIONS}" == "no" ]]; then
-  echo "$(date -u --rfc-3339=seconds) - GCP_INSTALL_USE_MINIMAL_PERMISSIONS and GCP_CCO_MANUAL_USE_MINIMAL_PERMISSIONS are no, nothing to do." && exit 0
+# See https://docs.openshift.com/container-platform/4.12/installing/installing_gcp/installing-gcp-account.html#minimum-required-permissions-ipi-gcp_installing-gcp-account
+# There are pre-configured 2 IAM service accounts, along with some custom roles.
+# The IAM service account for IPI: ipi-min-permissions-sa@${GOOGLE_PROJECT_ID}.iam.gserviceaccount.com
+# The IAM service account for UPI: upi-min-permissions-sa@${GOOGLE_PROJECT_ID}.iam.gserviceaccount.com
+# Currently we only deal with IPI in Prow CI.
+if [[ "${GCP_INSTALL_USE_MINIMAL_PERMISSIONS}" == "yes" ]]; then
+  echo "$(date -u --rfc-3339=seconds) - Going to use the IAM service account of general minimal permissions"
+  sa_filename="ipi-min-permissions-sa.json"
+elif [[ "${MINIMAL_PERMISSIONS_WITHOUT_ACT_AS}" == "yes" ]]; then
+  echo "$(date -u --rfc-3339=seconds) - Going to use the IAM service account of general minimal permissions, and without 'iam.serviceAccounts.actAs'"
+  sa_filename="ipi-min-perm-without-actAs-sa.json"
+elif [[ "${GCP_CCO_MANUAL_USE_MINIMAL_PERMISSIONS}" == "yes" ]]; then
+  echo "$(date -u --rfc-3339=seconds) - Going to use the IAM service account of minimal permissions for CCO in Manual mode"
+  sa_filename="ipi-xpn-cco-manual-permissions.json"
+elif [[ "${MINIMAL_PERMISSIONS_WITHOUT_FIREWALL_PROVISION}" == "yes" ]]; then
+  echo "$(date -u --rfc-3339=seconds) - Going to use the IAM service account of general minimal permissions, and without 'compute.firewalls.create' & 'compute.firewalls.delete' permissions"
+  sa_filename="ipi-min-perm-no-fw-sa.json"
+elif [[ "${MINIMAL_PERMISSIONS_CCO_MANUAL_WITHOUT_FIREWALL_PROVISION}" == "yes" ]]; then
+  echo "$(date -u --rfc-3339=seconds) - Going to use the IAM service account of minimal permissions for CCO in Manual mode, and without 'compute.firewalls.create' & 'compute.firewalls.delete' permissions"
+  sa_filename="ipi-cco-manual-no-fw-sa.json"
+else
+  echo "$(date -u --rfc-3339=seconds) - Not miminal permissions testing, going to use the deafult IAM service account."
+  exit 0
 fi
 
 GOOGLE_PROJECT_ID="$(< ${CLUSTER_PROFILE_DIR}/openshift_gcp_project)"
@@ -23,23 +44,6 @@ if ! gcloud auth list | grep -E "\*\s+${sa_email}"
 then
   gcloud auth activate-service-account --key-file="${GCP_SHARED_CREDENTIALS_FILE}"
   gcloud config set project "${GOOGLE_PROJECT_ID}"
-fi
-
-# See https://docs.openshift.com/container-platform/4.12/installing/installing_gcp/installing-gcp-account.html#minimum-required-permissions-ipi-gcp_installing-gcp-account
-# There are pre-configured 2 IAM service accounts, along with some custom roles.
-# The IAM service account for IPI: ipi-min-permissions-sa@${GOOGLE_PROJECT_ID}.iam.gserviceaccount.com
-# The IAM service account for UPI: upi-min-permissions-sa@${GOOGLE_PROJECT_ID}.iam.gserviceaccount.com
-# Currently we only deal with IPI in Prow CI.
-
-if [[ "${MINIMAL_PERMISSIONS_WITHOUT_ACT_AS}" == "yes" ]]; then
-  echo "$(date -u --rfc-3339=seconds) - Going to use the IAM service account of general minimal permissions, and without 'iam.serviceAccounts.actAs'"
-  sa_filename="ipi-min-perm-without-actAs-sa.json"
-elif [[ "${GCP_CCO_MANUAL_USE_MINIMAL_PERMISSIONS}" == "yes" ]]; then
-  echo "$(date -u --rfc-3339=seconds) - Going to use the IAM service account of minimal permissions for CCO in Manual mode"
-  sa_filename="ipi-xpn-cco-manual-permissions.json"
-else
-  echo "$(date -u --rfc-3339=seconds) - Going to use the IAM service account of general minimal permissions"
-  sa_filename="ipi-min-permissions-sa.json"
 fi
 
 if [ -f "${CLUSTER_PROFILE_DIR}/${sa_filename}" ]; then
