458f710742c4b3241d49cab39dfdb596f22cf1f0
Tom Buskey <tbuskey@redhat.com>
2025-11-26 13:54:23 -0500

    Add INITDATA for coco testing (#71786)
    coco testing needs to have a trustee to query.  We already have
    trusteeURL.  This adds INITDATA to prow so it can be passed to
    osc-config and patched into peer-pods-cm
    
    Signed-off-by: Tom Buskey <tbuskey@redhat.com>

--- ci-operator/step-registry/sandboxed-containers-operator/create-prowjob/sandboxed-containers-operator-create-prowjob-commands.sh
+++ ci-operator/step-registry/sandboxed-containers-operator/create-prowjob/sandboxed-containers-operator-create-prowjob-commands.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # script to create prowjobs in ci-operator/config/openshift/sandboxed-containers-operator using environment variables.
-# Usage: 
+# Usage:
 #   ./sandboxed-containers-operator-create-prowjob-commands.sh gen    # Generate prowjob configuration
 #   ./sandboxed-containers-operator-create-prowjob-commands.sh run    # Run prowjobs
 # should be run in a branch of a fork of https://github.com/openshift/release/
@@ -204,6 +204,9 @@ validate_and_set_defaults() {
     # Trustee URL Configuration (defaults to empty string)
     TRUSTEE_URL="${TRUSTEE_URL:-""}"
 
+    # Init Data Configuration (defaults to empty string)
+    INITDATA="${INITDATA:-''}"
+
     # Catalog Source Configuration
     echo "Configuring catalog sources..."
 
@@ -292,6 +295,7 @@ show_usage() {
     echo "  OSC_CATALOG_TAG                - OSC catalog tag (auto-detected if not provided)"
     echo "  TRUSTEE_CATALOG_TAG            - Trustee catalog tag (auto-detected if not provided)"
     echo "  TRUSTEE_URL                    - Trustee URL (default: empty)"
+    echo "  INITDATA                       - Initdata from Trustee(default: empty) The gzipped and base64 encoded initdata.toml file from Trustee"
 }
 
 # Main function
@@ -358,6 +362,7 @@ generate_workflow() {
     "CATALOG_SOURCE_NAME: ${CATALOG_SOURCE_NAME}"
     "ENABLE_MUST_GATHER: \"${ENABLE_MUST_GATHER}\""
     "EXPECTED_OPERATOR_VERSION: ${EXPECTED_OSC_VERSION}"
+    "INITDATA: ${INITDATA:-\"\"}"
     "INSTALL_KATA_RPM: \"${INSTALL_KATA_RPM}\""
     "KATA_RPM_VERSION: ${KATA_RPM_VERSION:-\"\"}"
     "MUST_GATHER_IMAGE: ${MUST_GATHER_IMAGE}"
