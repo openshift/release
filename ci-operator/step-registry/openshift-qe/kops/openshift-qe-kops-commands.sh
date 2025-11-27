d1d002d01e3521455318735a47943d898c1ec506
Jose Castillo Lema <josecastillolema@gmail.com>
2025-11-27 16:35:26 +0100

    Debug mode
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/kops/openshift-qe-kops-commands.sh
+++ ci-operator/step-registry/openshift-qe/kops/openshift-qe-kops-commands.sh
@@ -3,12 +3,13 @@
 set -o nounset
 set -o errexit
 set -o pipefail
+set -v
 
 export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 export NAME=mycluster.k8s.local
 export KOPS_STATE_STORE=s3://my-kops-state-store
 
-curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+curl -Lo kops "https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | jq -r '.tag_name')/kops-linux-amd64"
 chmod +x kops
 
 ./kops create cluster ${NAME} \
