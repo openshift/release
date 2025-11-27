79bce17f4f1088f55de6845b67633cd27a33dcf6
avasilev <avasilev@redhat.com>
2025-11-27 16:56:11 +0100

    OSSM CPT: first draft
    Signed-off-by: avasilev <avasilev@redhat.com>

--- ci-operator/step-registry/openshift-qe/servicemesh/sidecar/openshift-qe-servicemesh-sidecar-commands.sh
+++ ci-operator/step-registry/openshift-qe/servicemesh/sidecar/openshift-qe-servicemesh-sidecar-commands.sh
@@ -23,20 +23,21 @@ pushd /tmp
 python -m virtualenv ./venv_qe
 source ./venv_qe/bin/activate
 
+# TODO: add a step to install gateway for ingress tests
+
 ES_PASSWORD=$(cat "/secret/password")
 ES_USERNAME=$(cat "/secret/username")
 
+# Clone the e2e repo
 REPO_URL="https://github.com/cloud-bulldozer/e2e-benchmarking";
 LATEST_TAG=$(curl -s "https://api.github.com/repos/cloud-bulldozer/e2e-benchmarking/releases/latest" | jq -r '.tag_name');
 TAG_OPTION="--branch $(if [ "$E2E_VERSION" == "default" ]; then echo "$LATEST_TAG"; else echo "$E2E_VERSION"; fi)";
 git clone $REPO_URL $TAG_OPTION --depth 1
-pushd e2e-benchmarking/workloads/network-perf-v2
+pushd e2e-benchmarking/workloads/ingress-perf
 
-# Clean up resources from possible previous tests.
-oc delete ns netperf --wait=true --ignore-not-found=true
-
-# Only store the results from the full run versus the smoke test.
+# ES Configuration
 export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
+export ES_INDEX="ingress-performance"
 
-export LOCAL=$LOCAL; export EXTERNAL_SERVER_ADDRESS=$EXTERNAL_SERVER_ADDRESS; export WORKLOAD_NAME=$WORKLOAD_NAME; export WORKLOAD=$WORKLOAD;
+# Start the Workload
 ./run.sh
