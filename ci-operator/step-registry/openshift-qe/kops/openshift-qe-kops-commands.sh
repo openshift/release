0c935c100343c1a86c110c1b3e7e61dc55c75f3d
Jose Castillo Lema <josecastillolema@gmail.com>
2025-11-27 17:06:35 +0100

    Place the executable in /tmp
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/kops/openshift-qe-kops-commands.sh
+++ ci-operator/step-registry/openshift-qe/kops/openshift-qe-kops-commands.sh
@@ -9,10 +9,10 @@ export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 export NAME=mycluster.k8s.local
 export KOPS_STATE_STORE=s3://my-kops-state-store
 
-curl -Lo kops "https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | jq -r '.tag_name')/kops-linux-amd64"
-chmod +x kops
+curl -Lo /tmp/kops "https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | jq -r '.tag_name')/kops-linux-amd64"
+chmod +x /tmp/kops
 
-./kops create cluster ${NAME} \
+./tmp/kops create cluster ${NAME} \
   --cloud=aws \
   --zones=us-west-2a \
   --yes
@@ -20,4 +20,4 @@ chmod +x kops
 kubectl get node
 ls ~/.kube/config
 
-./kops delete cluster --name=${NAME} --yes
+./tmp/kops delete cluster --name=${NAME} --yes
