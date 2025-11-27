57985d7cc5f7b60abf51a0d26e98ea2228af6d44
Jose Castillo Lema <josecastillolema@gmail.com>
2025-11-27 15:41:03 +0100

    kops download
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/kops/openshift-qe-kops-commands.sh
+++ ci-operator/step-registry/openshift-qe/kops/openshift-qe-kops-commands.sh
@@ -8,9 +8,14 @@ export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 export NAME=mycluster.k8s.local
 export KOPS_STATE_STORE=s3://my-kops-state-store
 
-kops create cluster ${NAME} \
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+
+./kops create cluster ${NAME} \
   --cloud=aws \
   --zones=us-west-2a \
   --yes
-  
-kops delete cluster --name=${NAME} --yes
+
+kubectl get node
+ls ~/.kube/config
+
+./kops delete cluster --name=${NAME} --yes
