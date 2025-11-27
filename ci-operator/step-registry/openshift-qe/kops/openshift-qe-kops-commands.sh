71954ae3f1847984796ba8d68b391b550dd454ef
Jose Castillo Lema <josecastillolema@gmail.com>
2025-11-27 16:08:52 +0100

    fix download issue
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/kops/openshift-qe-kops-commands.sh
+++ ci-operator/step-registry/openshift-qe/kops/openshift-qe-kops-commands.sh
@@ -8,7 +8,7 @@ export AWS_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/.awscred"
 export NAME=mycluster.k8s.local
 export KOPS_STATE_STORE=s3://my-kops-state-store
 
-curl -Lo kops "https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64"
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops
 
 ./kops create cluster ${NAME} \
