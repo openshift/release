daf68228eefa6bdcb441a13ac9c917421faa69f0
Patryk Matuszak <pmatusza@redhat.com>
2025-12-01 11:41:36 +0100

    trigger ushift 4.21 rebase

--- ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
+++ ci-operator/step-registry/openshift/microshift/rebase/openshift-microshift-rebase-commands.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -xeuo pipefail
 
-if [[ "$JOB_NAME" == rehearse* ]]; then
-    echo "INFO: \$JOB_NAME starts with rehearse - running in DRY RUN mode"
-    export DRY_RUN=y
-fi
-
 export PATH="${HOME}/.local/bin:${PATH}"
 python3 -m ensurepip --upgrade
 pip3 install setuptools-rust cryptography pyyaml pygithub gitpython
@@ -13,5 +8,6 @@ pip3 install setuptools-rust cryptography pyyaml pygithub gitpython
 cp /secrets/import-secret/.dockerconfigjson ${HOME}/.pull-secret.json
 
 cd /go/src/github.com/openshift/microshift/
+sed -i 's,DRY_RUN=y,DRY_RUN=,g' ./scripts/auto-rebase/rebase_job_entrypoint.sh
 DEST_DIR=${HOME}/.local/bin ./scripts/fetch_tools.sh yq
 ./scripts/auto-rebase/rebase_job_entrypoint.sh
