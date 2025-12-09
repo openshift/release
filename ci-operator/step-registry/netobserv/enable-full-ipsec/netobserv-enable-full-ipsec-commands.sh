87cfa68a92fd463f0027e266587d719be246028a
Oliver Smakal <osmakal@osmakal-thinkpadp1gen7.rmtcz.csb>
2025-12-09 14:37:44 +0100

    rm custom ipsec setup

--- ci-operator/step-registry/netobserv/enable-full-ipsec/netobserv-enable-full-ipsec-commands.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -o nounset
-set -o errexit
-set -o pipefail
-
-oc patch networks.operator.openshift.io cluster --type=merge -p '{"spec":{"defaultNetwork":{"ovnKubernetesConfig":{"ipsecConfig":{"mode":  "Full" }}}}}'
