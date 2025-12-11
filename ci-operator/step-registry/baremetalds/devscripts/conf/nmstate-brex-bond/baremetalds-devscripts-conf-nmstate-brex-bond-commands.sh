826fed78023f77b7a1a5a7b30bed3a5b69be412c
Emilia Desch <emy@redhat.com>
2025-12-11 20:48:36 +0100

    add additional network parameters to ref
    Signed-off-by: Emilia Desch <emy@redhat.com>

--- ci-operator/step-registry/baremetalds/devscripts/conf/nmstate-brex-bond/baremetalds-devscripts-conf-nmstate-brex-bond-commands.sh
+++ ci-operator/step-registry/baremetalds/devscripts/conf/nmstate-brex-bond/baremetalds-devscripts-conf-nmstate-brex-bond-commands.sh
@@ -9,3 +9,8 @@ echo "************ baremetalds devscripts conf nmstate-brex-bond command *******
 echo "NETWORK_CONFIG_FOLDER=/root/dev-scripts/network-configs/bond" >> "${SHARED_DIR}/dev-scripts-additional-config"
 echo "ASSETS_EXTRA_FOLDER=/root/dev-scripts/network-configs/nmstate-brex-bond" >> "${SHARED_DIR}/dev-scripts-additional-config"
 echo "BOND_PRIMARY_INTERFACE=true" >> "${SHARED_DIR}/dev-scripts-additional-config"
+echo "EXTRA_NETWORK_NAMES=nmstate1 nmstate2" >> "${SHARED_DIR}/dev-scripts-additional-config"
+echo "NMSTATE1_NETWORK_SUBNET_V4=192.168.221.0/24" >> "${SHARED_DIR}/dev-scripts-additional-config"
+echo "NMSTATE1_NETWORK_SUBNET_V6=fd2e:6f44:5dd8:ca56::/120" >> "${SHARED_DIR}/dev-scripts-additional-config"
+echo "NMSTATE2_NETWORK_SUBNET_V4=192.168.222.0/24" >> "${SHARED_DIR}/dev-scripts-additional-config"
+echo "NMSTATE2_NETWORK_SUBNET_V6=fd2e:6f44:5dd8:cc56::/120" >> "${SHARED_DIR}/dev-scripts-additional-config"
