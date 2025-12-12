7ba0be5c53db2d55ae917007fec1c03bf08c6b76
Nick Boldt <nboldt@redhat.com>
2025-12-12 12:15:34 -0400

    revise PR: only skip e2e if the bot auto-approved this skippable PR
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- ci-operator/step-registry/redhat-developer/rhdh/ocp/helm/redhat-developer-rhdh-ocp-helm-commands.sh
+++ ci-operator/step-registry/redhat-developer/rhdh/ocp/helm/redhat-developer-rhdh-ocp-helm-commands.sh
@@ -2,11 +2,21 @@
 
 # Check for [skip-build] commit comments in the PR title
 if [ "$JOB_TYPE" == "presubmit" ] && [[ "$JOB_NAME" != rehearse-* ]]; then
-   PR_TITLE=$(curl -s "https://api.github.com/repos/${GITHUB_ORG_NAME}/${GITHUB_REPOSITORY_NAME}/pulls/${GIT_PR_NUMBER}" | jq -r '.title')
-    echo "PR_TITLE: $PR_TITLE"
+    PR_TITLE=$(curl -s "https://api.github.com/repos/${GITHUB_ORG_NAME}/${GITHUB_REPOSITORY_NAME}/pulls/${GIT_PR_NUMBER}" | jq -r '.title')
+    # only skip e2e tests for a [skip-build] PR that has been auto-approved by github-actions[bot]
     if [[ "$PR_TITLE" == *"[skip-build]"* ]]; then
-        echo "PR_TITLE contains '[skip-build]', skipping all test execution with exit code 0"
-        exit 0
+        echo "PR_TITLE: $PR_TITLE"
+        APPROVALS=$(curl -s "https://api.github.com/repos/${GITHUB_ORG_NAME}/${GITHUB_REPOSITORY_NAME}/pulls/${GIT_PR_NUMBER}/reviews") # json array of approvals
+        # iterate through the approvals and check if the approval is from the .user.login = "github-actions[bot]" and if the .state = "APPROVED" 
+        approval_count=$(echo "$APPROVALS" | jq 'length')
+        for ((i=0; i<approval_count; i++)); do
+            user_login=$(echo "$APPROVALS" | jq -r ".[$i].user.login")
+            state=$(echo "$APPROVALS" | jq -r ".[$i].state")
+            if [[ "$user_login" == "github-actions[bot]" ]] && [[ "$state" == "APPROVED" ]]; then
+                echo "Auto-approved by github-actions[bot], so no need to run tests; skip all test execution with exit code 0"
+                exit 0
+            fi
+        done
     fi
 fi
 
