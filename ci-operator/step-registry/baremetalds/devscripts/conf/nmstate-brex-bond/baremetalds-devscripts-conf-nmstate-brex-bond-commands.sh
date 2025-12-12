7e16c74dcc3df3a94b6812c79e408d11d91e0337
Emilia Desch <emy@redhat.com>
2025-12-12 15:25:44 +0100

    add network type
    Signed-off-by: Emilia Desch <emy@redhat.com>

--- ci-operator/step-registry/baremetalds/devscripts/conf/nmstate-brex-bond/baremetalds-devscripts-conf-nmstate-brex-bond-commands.sh
+++ ci-operator/step-registry/baremetalds/devscripts/conf/nmstate-brex-bond/baremetalds-devscripts-conf-nmstate-brex-bond-commands.sh
@@ -6,6 +6,7 @@ set -o pipefail
 
 echo "************ baremetalds devscripts conf nmstate-brex-bond command ************"
 
+echo "NETWORK_TYPE=\"OVNKubernetes\"" >> "${SHARED_DIR}/dev-scripts-additional-config"
 echo "NETWORK_CONFIG_FOLDER=/root/dev-scripts/network-configs/bond" >> "${SHARED_DIR}/dev-scripts-additional-config"
 echo "ASSETS_EXTRA_FOLDER=/root/dev-scripts/network-configs/nmstate-brex-bond" >> "${SHARED_DIR}/dev-scripts-additional-config"
 echo "BOND_PRIMARY_INTERFACE=true" >> "${SHARED_DIR}/dev-scripts-additional-config"
