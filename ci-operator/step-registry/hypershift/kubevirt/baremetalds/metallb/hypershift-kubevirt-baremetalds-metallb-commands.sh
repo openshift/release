cefe824c376d2e1cf80cf417a0f1faf3ef24446b
He Liu <heli@redhat.com>
2025-12-14 08:01:40 +0800

    debug dualstack for bm kubevirt hypershift

--- ci-operator/step-registry/hypershift/kubevirt/baremetalds/metallb/hypershift-kubevirt-baremetalds-metallb-commands.sh
+++ ci-operator/step-registry/hypershift/kubevirt/baremetalds/metallb/hypershift-kubevirt-baremetalds-metallb-commands.sh
@@ -5,6 +5,14 @@ set -o errexit
 set -o pipefail
 set -x
 
+# Source ds-vars.conf if available to get IP_STACK from management cluster
+IP_STACK=${IP_STACK:-v4}
+if [ -f "${SHARED_DIR}/ds-vars.conf" ]; then
+  # shellcheck source=/dev/null
+  source "${SHARED_DIR}/ds-vars.conf"
+  IP_STACK=${DS_IP_STACK:-v4}
+fi
+
 oc create -f - <<EOF
 apiVersion: metallb.io/v1beta1
 kind: MetalLB
@@ -13,7 +21,9 @@ metadata:
   namespace: metallb-system
 EOF
 
-oc create -f - <<EOF
+echo "Configure IPAddressPool for IP_STACK=${IP_STACK}"
+if [[ $IP_STACK == "v4" ]]; then
+  oc create -f - <<EOF
 apiVersion: metallb.io/v1beta1
 kind: IPAddressPool
 metadata:
@@ -23,6 +33,36 @@ spec:
   addresses:
   - 192.168.111.30-192.168.111.50
 EOF
+elif [[ $IP_STACK == "v4v6" ]] || [[ $IP_STACK == "v6v4" ]]; then
+  # For dual-stack, provide both IPv4 and IPv6 address pools
+  # The order doesn't matter for MetalLB - it just makes both ranges available
+  # The fd2e:6f44:5dd8:c956:: prefix is standard for baremetalds environments
+  oc create -f - <<EOF
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: metallb
+  namespace: metallb-system
+spec:
+  addresses:
+  - 192.168.111.30-192.168.111.50
+  - fd2e:6f44:5dd8:c956::100-fd2e:6f44:5dd8:c956::110
+EOF
+elif [[ $IP_STACK == "v6" ]]; then
+  oc create -f - <<EOF
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: metallb
+  namespace: metallb-system
+spec:
+  addresses:
+  - fd2e:6f44:5dd8:c956::100-fd2e:6f44:5dd8:c956::110
+EOF
+else
+  echo "Unsupported IP_STACK: $IP_STACK"
+  exit 1
+fi
 
 oc create -f - <<EOF
 apiVersion: metallb.io/v1beta1
