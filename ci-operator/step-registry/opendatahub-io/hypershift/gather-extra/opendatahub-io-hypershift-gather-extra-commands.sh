7833fdd0430ce41d54b8e233268842546ae9dafa
Scott Dodson <sdodson@redhat.com>
2025-12-23 10:29:58 -0500

    Fix search urls for KaaS and PromeCIeus

--- ci-operator/step-registry/opendatahub-io/hypershift/gather-extra/opendatahub-io-hypershift-gather-extra-commands.sh
+++ ci-operator/step-registry/opendatahub-io/hypershift/gather-extra/opendatahub-io-hypershift-gather-extra-commands.sh
@@ -314,7 +314,7 @@ if [[ -n "${prometheus}" ]]; then
 	cat >> ${SHARED_DIR}/custom-links.txt <<-EOF
 	<script>
 	let prom = document.createElement('a');
-	prom.href="https://promecieus.dptools.openshift.org/?search="+document.referrer;
+	prom.href="https://promecieus.dptools.openshift.org/?search="+encodeURIComponent(window.location.href);
 	prom.title="Creates a new prometheus deployment with data from this job run.";
 	prom.innerHTML="PromeCIeus";
 	prom.target="_blank";
