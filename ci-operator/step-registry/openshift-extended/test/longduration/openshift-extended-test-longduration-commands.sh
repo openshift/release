9f6b9128b610b9e0c1d4245d1beace33a44fae37
maxu <maxu@redhat.com>
2025-12-23 23:32:04 +0800

    fix

--- ci-operator/step-registry/openshift-extended/test/longduration/openshift-extended-test-longduration-commands.sh
+++ ci-operator/step-registry/openshift-extended/test/longduration/openshift-extended-test-longduration-commands.sh
@@ -278,7 +278,7 @@ if [[ $OVERRIDE_OC_MIRROR == "true" ]]; then
         which oc
         uname -m
         lscpu | grep "Architecture"
-        oc image extract "${tag}" --path=/usr/bin/oc-mirror:. -a "${CLUSTER_PROFILE_DIR}/pull-secret" --filter-by-os="linux/amd64"
+        oc image extract "${tag}" --path=/usr/bin/oc-mirror:. -a "${CLUSTER_PROFILE_DIR}/pull-secret" --filter-by-os="linux/amd64" --command-os="linux/amd64"
         ls -la ./oc-mirror
         md5sum ./oc-mirror
         chmod +x ./oc-mirror
