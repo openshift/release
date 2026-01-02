f3f518e1514cf0f0c62ca61158e9e21d473fe87b
roi <roivazquez@gmail.com>
2026-01-02 17:06:37 +0100

    Avoid the use of '--' in resource names. (#72838)
    The presence of '--' makes ci-operator split the resource name and only
    return the part before it in the lease envrironment variable injected
    into the step pod. This is the intended behavior as seen in https://github.com/openshift/ci-tools/blob/105b66773d9c894cf8beee3e6234f1783ee04012/pkg/steps/lease.go#L82-L87.

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -258,18 +258,10 @@ CONFIG = {
     'aro-hcp-test-tenant-quota-slice': {
         'default': 10,
     },
-    'aro-hcp-test-msi-containers-dev': {
-        'rg': 120,
-    },
-    'aro-hcp-test-msi-containers-int': {
-        'rg': 120,
-    },
-    'aro-hcp-test-msi-containers-stg': {
-        'rg': 120,
-    },
-    'aro-hcp-test-msi-containers-prod': {
-        'rg': 120,
-    },
+    'aro-hcp-test-msi-containers-dev': {},
+    'aro-hcp-test-msi-containers-int': {},
+    'aro-hcp-test-msi-containers-stg': {},
+    'aro-hcp-test-msi-containers-prod': {},
     'equinix-ocp-metal-quota-slice': {
         'default': 140,
     },
@@ -688,6 +680,13 @@ for i in range(4):
 
 for i in range(2):
     CONFIG['powervs-8-quota-slice']['fran-powervs-8-quota-slice-{}'.format(i)] = 1
+
+for i in range(120):
+    CONFIG['aro-hcp-test-msi-containers-dev']['aro-hcp-test-msi-containers-dev-{}'.format(i)] = 1
+    CONFIG['aro-hcp-test-msi-containers-int']['aro-hcp-test-msi-containers-int-{}'.format(i)] = 1
+    CONFIG['aro-hcp-test-msi-containers-stg']['aro-hcp-test-msi-containers-stg-{}'.format(i)] = 1
+    CONFIG['aro-hcp-test-msi-containers-prod']['aro-hcp-test-msi-containers-prod-{}'.format(i)] = 1
+
 
 
 config = {
