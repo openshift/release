8b68a4a5afe537bf1ab9a61bf47b6a62993ee8b9
Tomas David <tdavid@redhat.com>
2026-01-14 15:14:53 +0100

    OCPERT-292: Set REGISTRY_AUTH_FILE for image consistency check job

--- ci-operator/step-registry/release-qe-tests/image-consistency-check/release-qe-tests-image-consistency-check-commands.sh
+++ ci-operator/step-registry/release-qe-tests/image-consistency-check/release-qe-tests-image-consistency-check-commands.sh
@@ -5,7 +5,13 @@ echo "Running Image Consistency Check"
 echo "Payload URL: $MULTISTAGE_PARAM_OVERRIDE_PAYLOAD_URL"
 echo "Merge Request ID: $MULTISTAGE_PARAM_OVERRIDE_MERGE_REQUEST_ID"
 
+# Get the GitLab token from the vault
 gitlab_token=$(cat "/var/run/vault/release-tests-token/gitlab_token")
 export GITLAB_TOKEN=$gitlab_token
 
+# Get the registry auth file from the vault
+registry_auth_file="/var/run/vault/release-tests-registry-conf/art-images"
+export REGISTRY_AUTH_FILE=$registry_auth_file
+
+# Run the image consistency check
 oarctl image-consistency-check --payload-url "$MULTISTAGE_PARAM_OVERRIDE_PAYLOAD_URL" --mr-id "$MULTISTAGE_PARAM_OVERRIDE_MERGE_REQUEST_ID"
