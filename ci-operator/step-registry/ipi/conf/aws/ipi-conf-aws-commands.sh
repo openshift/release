5e3b6931f5b69e22c12c67538a576cd74bc4b1e8
Justin Pierce <jupierce@redhat.com>
2026-01-21 10:18:16 -0500

    No longer exclude exotic topologies from NAT instance

--- ci-operator/step-registry/ipi/conf/aws/ipi-conf-aws-commands.sh
+++ ci-operator/step-registry/ipi/conf/aws/ipi-conf-aws-commands.sh
@@ -233,8 +233,6 @@ if [[ "${CI_NAT_REPLACE:-false}" == 'auto' ]]; then
     CI_NAT_REPLACE='false_4_21_jobs_are_excluded'
   elif [[ "${CLUSTER_PROFILE_NAME}" != "aws" && ! "${CLUSTER_PROFILE_NAME}" =~ ^aws-[0-9]+$ ]]; then
     CI_NAT_REPLACE='false_CLUSTER_PROFILE_NAME_is_not_a_testplatform_aws_profile'
-  elif [[ "${JOB_NAME}" == *'microshift'* || "${JOB_NAME}" == *'hypershift'* || "${JOB_NAME}" == *'vpc'* || "${JOB_NAME}" == *'single-node'* ]]; then
-    CI_NAT_REPLACE='false_job_uses_non_standalone_install_topology'
   else
     CI_NAT_REPLACE='true'
   fi
