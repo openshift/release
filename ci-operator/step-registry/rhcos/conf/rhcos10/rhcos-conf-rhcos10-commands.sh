0b0189a08d18f9e6e114cdc3ee06967b036d0e72
Scott Dodson <sdodson@redhat.com>
2026-01-27 14:42:51 -0500

    Rhcos10 dualstream techpreview (#73173)
    * rhcos10: Move to 4.22 TPNU jobs leveraging dual stream impl
    
    * `make update` to generate jobs
    
    Separating out the generated changes from the content changes so that
    continuously rebasing this is easier

--- ci-operator/step-registry/rhcos/conf/rhcos10/rhcos-conf-rhcos10-commands.sh
+++ ci-operator/step-registry/rhcos/conf/rhcos10/rhcos-conf-rhcos10-commands.sh
@@ -4,31 +4,47 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo "Creating manifests to set osImageURL to the relevant RHCOS 10 version"
 
-cat > "${SHARED_DIR}/manifest_rhcos10_worker.yaml" <<EOF
+echo "Configuring the MCPs for rhel-10 osImageStream"
+# these haven't changed in six years so lets assume for now they're stable
+# source https://github.com/openshift/machine-config-operator/tree/main/manifests
+cat > "${SHARED_DIR}/manifest_master.machineconfigpool.yaml" <<EOF
 apiVersion: machineconfiguration.openshift.io/v1
-kind: MachineConfig
+kind: MachineConfigPool
 metadata:
+  name: master
   labels:
-    machineconfiguration.openshift.io/role: worker
-  name: os-layer-custom-worker
+    "operator.machineconfiguration.openshift.io/required-for-upgrade": ""
+    "machineconfiguration.openshift.io/mco-built-in": ""
+    "pools.operator.machineconfiguration.openshift.io/master": ""
 spec:
-  osImageURL: quay.io/openshift-release-dev/ocp-v4.0-art-dev:4.20-10.1-node-image
-  baseOSExtensionsContainerImage: quay.io/openshift-release-dev/ocp-v4.0-art-dev:4.20-10.1-node-image-extensions
+  machineConfigSelector:
+    matchLabels:
+      "machineconfiguration.openshift.io/role": "master"
+  nodeSelector:
+    matchLabels:
+      node-role.kubernetes.io/master: ""
+  osImageStream:
+    name: rhel-10
 
 EOF
 
-cat > "${SHARED_DIR}/manifest_rhcos10_master.yaml" <<EOF
+cat > "${SHARED_DIR}/manifest_worker.machineconfigpool.yaml" <<EOF
 apiVersion: machineconfiguration.openshift.io/v1
-kind: MachineConfig
+kind: MachineConfigPool
 metadata:
+  name: worker
   labels:
-    machineconfiguration.openshift.io/role: master
-  name: os-layer-custom-master
+    "machineconfiguration.openshift.io/mco-built-in": ""
+    "pools.operator.machineconfiguration.openshift.io/worker": ""
 spec:
-  osImageURL: quay.io/openshift-release-dev/ocp-v4.0-art-dev:4.20-10.1-node-image
-  baseOSExtensionsContainerImage: quay.io/openshift-release-dev/ocp-v4.0-art-dev:4.20-10.1-node-image-extensions
+  machineConfigSelector:
+    matchLabels:
+      "machineconfiguration.openshift.io/role": "worker"
+  nodeSelector:
+    matchLabels:
+      node-role.kubernetes.io/worker: ""
+  osImageStream:
+    name: rhel-10
 
 EOF
-
