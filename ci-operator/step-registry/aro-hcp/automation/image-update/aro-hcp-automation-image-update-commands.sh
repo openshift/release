a61c82b32e5b214ff9932ee5965010def2f86690
hbhushan3 <hbhushan@redhat.com>
2026-01-27 08:40:27 -0500

    update make target

--- ci-operator/step-registry/aro-hcp/automation/image-update/aro-hcp-automation-image-update-commands.sh
+++ ci-operator/step-registry/aro-hcp/automation/image-update/aro-hcp-automation-image-update-commands.sh
@@ -134,9 +134,9 @@ export AZURE_TENANT_ID
 
 debug "azure: authentication configured successfully (credentials redacted)"
 
-# Image Updater: Build and run the image-updater tool
+# Image Updater: Run the image-updater tool
 info "image: fetching the latest image digests for all components"
-make image-updater OUTPUT_FILE="${IMAGE_UPDATER_OUTPUT}" OUTPUT_FORMAT="${IMAGE_UPDATER_OUTPUT_FORMAT}"
+make image-updater-update OUTPUT_FILE="${IMAGE_UPDATER_OUTPUT}" OUTPUT_FORMAT="${IMAGE_UPDATER_OUTPUT_FORMAT}"
 
 # Check if there are any changes from image updates
 if [[ $(git status --porcelain) == "" ]]; then
