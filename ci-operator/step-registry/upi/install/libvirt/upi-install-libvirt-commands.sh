816f05c31579e8db36c70afbe83de5c03a173306
KeerthanaAP <akeerthana1995@gmail.com>
2026-01-28 19:49:56 +0530

    Debug libvirt issue

--- ci-operator/step-registry/upi/install/libvirt/upi-install-libvirt-commands.sh
+++ ci-operator/step-registry/upi/install/libvirt/upi-install-libvirt-commands.sh
@@ -241,6 +241,7 @@ else
       --format qcow2 \
       --capacity ${VOLUME_CAPACITY} || echo "Volume ${VOLUME_NAME} already exists, proceed without creation"
 
+    date
     # Upload the rhcos image to the source volume
     echo "Uploading rhcos image to source volume..."
     ${VIRSH} -k 60 -K 5 vol-upload \
@@ -248,6 +249,7 @@ else
       --pool ${POOL_NAME} \
       ${INSTALL_DIR}/${VOLUME_NAME}
   fi
+  date
 
   # Generate manifests for cluster modifications
   echo "Generating manifests..."
@@ -464,6 +466,7 @@ done
 date "+%F %X" > "${SHARED_DIR}/CLUSTER_INSTALL_START_TIME"
 
 if [ "$INSTALLER_TYPE" == "agent" ]; then
+  sleep 30m
   restart_nodes &
   ${OCPINSTALL} --dir "${INSTALL_DIR}" agent wait-for bootstrap-complete --log-level=debug &
 else
