d18eb874da7e60d7a8243597cedd607cad79eae2
memodi <memodi@redhat.com>
2026-02-04 12:20:43 -0500

    debug FLP regression

--- ci-operator/step-registry/netobserv/perf-test/deploy/netobserv-perf-test-deploy-commands.sh
+++ ci-operator/step-registry/netobserv/perf-test/deploy/netobserv-perf-test-deploy-commands.sh
@@ -57,7 +57,8 @@ if [[ ${FLP_CONSUMER_REPLICAS:-} ]]; then
 fi
 
 createFlowCollector ${PARAMETERS}
-
+FLP_PR_IMAGE="quay.io/netobserv/flowlogs-pipeline:6abb827"
+EBPFAGENT_PR_IMAGE="quay.io/netobserv/netobserv-ebpf-agent:c44862a"
 if [[ $PATCH_EBPFAGENT_IMAGE == "true" && -n $EBPFAGENT_PR_IMAGE ]]; then
     patch_netobserv "ebpf" "$EBPFAGENT_PR_IMAGE"
 fi
@@ -66,6 +67,7 @@ if [[ $PATCH_FLOWLOGS_IMAGE == "true" && -n $FLP_PR_IMAGE ]]; then
     patch_netobserv "flp" "$FLP_PR_IMAGE"
 fi
 
+patch_netobserv "plugin" "quay.io/netobserv/network-observability-console-plugin:1d61a10"
 # get NetObserv metadata
 NETOBSERV_RELEASE=$(oc get pods -l app=netobserv-operator -o jsonpath="{.items[*].spec.containers[0].env[?(@.name=='OPERATOR_CONDITION_NAME')].value}" -A)
 
