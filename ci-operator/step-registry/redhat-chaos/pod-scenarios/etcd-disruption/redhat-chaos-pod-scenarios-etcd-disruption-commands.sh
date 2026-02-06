029452b2b4c0dc58551e8e29f9983f14c9788697
Paige Patton <64206430+paigerube14@users.noreply.github.com>
2026-02-06 10:38:25 -0500

    Pod status (#74344)
    * adding get last status
    
    * adding oc instead of kubectl
    
    * adding creation timestamp
    
    * creation timestamp

--- ci-operator/step-registry/redhat-chaos/pod-scenarios/etcd-disruption/redhat-chaos-pod-scenarios-etcd-disruption-commands.sh
+++ ci-operator/step-registry/redhat-chaos/pod-scenarios/etcd-disruption/redhat-chaos-pod-scenarios-etcd-disruption-commands.sh
@@ -25,18 +25,28 @@ export TELEMETRY_PASSWORD=$telemetry_password
 oc get nodes --kubeconfig $KRKN_KUBE_CONFIG
 console_url=$(oc get routes -n openshift-console console -o jsonpath='{.spec.host}')
 export HEALTH_CHECK_URL=https://$console_url
+
+oc get pods -n $TARGET_NAMESPACE -o jsonpath='{range .items[*]}{.metadata.name}{"\t"}{.status.phase}{"\t"}{.status.conditions[?(@.type=="Ready")].lastTransitionTime}{"\t"}{.status.startTime}{"\t"}{.metadata.creationTimestamp}{"\n"}{end}'     
+
+
 set -o nounset
 set -o pipefail
 set -x
 
+
+
+
 ./pod-scenarios/prow_run.sh
 rc=$?
 echo "Done running the test!" 
+oc get pods -n $TARGET_NAMESPACE -o jsonpath='{range .items[*]}{.metadata.name}{"\t"}{.status.phase}{"\t"}{.status.conditions[?(@.type=="Ready")].lastTransitionTime}{"\t"}{.status.startTime}{"\t"}{.metadata.creationTimestamp}{"\n"}{end}'     
 
-cat /tmp/*.log 
+
+cat /tmp/*.log
 if [[ $TELEMETRY_EVENTS_BACKUP == "True" ]]; then
     cp /tmp/events.json ${ARTIFACT_DIR}/events.json
 fi
 
 echo "Return code: $rc"
 exit $rc
+ 
