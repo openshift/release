e035125fea75511cb1101f2484d5cb0edd0880cd
Sean Zhao <szhao@redhat.com>
2026-02-06 10:59:46 +0800

    Update Quay CI configs to 3.16 and add v3-17 bundle mirrors
    - Update QUAY_OPERATOR_CHANNEL and QUAY_VERSION from 3.15 to 3.16
    - Update QUAY_INDEX_IMAGE_BUILD to stable-3-16-v4-20 image
    - Add v3-17 bundle mirrors to ICSP configuration

--- ci-operator/step-registry/quay-tests/enable-quay-catalogsource/quay-tests-enable-quay-catalogsource-commands.sh
+++ ci-operator/step-registry/quay-tests/enable-quay-catalogsource/quay-tests-enable-quay-catalogsource-commands.sh
@@ -90,6 +90,7 @@ spec:
     - quay.io/redhat-user-workloads/quay-eng-tenant/quay-operator-bundle-v3-14
     - quay.io/redhat-user-workloads/quay-eng-tenant/quay-operator-bundle-v3-15
     - quay.io/redhat-user-workloads/quay-eng-tenant/quay-operator-bundle-v3-16
+    - quay.io/redhat-user-workloads/quay-eng-tenant/quay-operator-bundle-v3-17
     source: registry.redhat.io/quay/quay-operator-bundle
   - mirrors:
     - quay.io/redhat-user-workloads/quay-eng-tenant/container-security-operator-bundle-v3-10
@@ -99,6 +100,7 @@ spec:
     - quay.io/redhat-user-workloads/quay-eng-tenant/container-security-operator-bundle-v3-14
     - quay.io/redhat-user-workloads/quay-eng-tenant/container-security-operator-bundle-v3-15
     - quay.io/redhat-user-workloads/quay-eng-tenant/container-security-operator-bundle-v3-16
+    - quay.io/redhat-user-workloads/quay-eng-tenant/container-security-operator-bundle-v3-17
     source: registry.redhat.io/quay/quay-container-security-operator-bundle
   - mirrors:
     - quay.io/redhat-user-workloads/quay-eng-tenant/quay-bridge-operator-bundle-v3-10
@@ -108,6 +110,7 @@ spec:
     - quay.io/redhat-user-workloads/quay-eng-tenant/quay-bridge-operator-bundle-v3-14
     - quay.io/redhat-user-workloads/quay-eng-tenant/quay-bridge-operator-bundle-v3-15
     - quay.io/redhat-user-workloads/quay-eng-tenant/quay-bridge-operator-bundle-v3-16
+    - quay.io/redhat-user-workloads/quay-eng-tenant/quay-bridge-operator-bundle-v3-17
     source: registry.redhat.io/quay/quay-bridge-operator-bundle
   - mirrors:
     - quay.io/redhat-user-workloads/quay-eng-tenant/quay-quay-v3-10
