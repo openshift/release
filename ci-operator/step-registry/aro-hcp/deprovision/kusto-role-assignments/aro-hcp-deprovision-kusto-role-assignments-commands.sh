1cb3e17dfffd23cbff7298f27d3e7b7c1807f554
Mariusz Mazur <mmazur@redhat.com>
2026-02-09 14:47:50 +0100

    Don't dump all sub ids on each 'az login' (#74570)

--- ci-operator/step-registry/aro-hcp/deprovision/kusto-role-assignments/aro-hcp-deprovision-kusto-role-assignments-commands.sh
+++ ci-operator/step-registry/aro-hcp/deprovision/kusto-role-assignments/aro-hcp-deprovision-kusto-role-assignments-commands.sh
@@ -9,7 +9,7 @@ export AZURE_TENANT_ID; AZURE_TENANT_ID=$(cat "${CLUSTER_PROFILE_DIR}/tenant")
 export AZURE_CLIENT_SECRET; AZURE_CLIENT_SECRET=$(cat "${CLUSTER_PROFILE_DIR}/client-secret")
 export CUSTOMER_SUBSCRIPTION; CUSTOMER_SUBSCRIPTION=$(cat "${CLUSTER_PROFILE_DIR}/kusto-subscription-name")
 export SUBSCRIPTION_ID; SUBSCRIPTION_ID=$(cat "${CLUSTER_PROFILE_DIR}/subscription-id")
-az login --service-principal -u "${AZURE_CLIENT_ID}" -p "${AZURE_CLIENT_SECRET}" --tenant "${AZURE_TENANT_ID}"
+az login --service-principal -u "${AZURE_CLIENT_ID}" -p "${AZURE_CLIENT_SECRET}" --tenant "${AZURE_TENANT_ID}" --output none
 az account set --subscription "${SUBSCRIPTION_ID}"
 
 ./test/aro-hcp-tests cleanup kusto-role-assignments
