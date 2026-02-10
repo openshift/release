82fb1ace23e6fe1f9dfb7f192a77f00e5debc5c9
Mohammed Tahoor <mtahoor@redhat.com>
2026-02-10 12:32:38 +0530

    Update jumphost IP address in cluster create script

--- ci-operator/step-registry/openshift-observability/ibm-z/cluster-create/openshift-observability-ibm-z-cluster-create-commands.sh
+++ ci-operator/step-registry/openshift-observability/ibm-z/cluster-create/openshift-observability-ibm-z-cluster-create-commands.sh
@@ -8,7 +8,7 @@ cd /tmp
 # --- SETUP AND VARIABLES ---
 SECRET_DIR="/tmp/vault/ibmz-ci-creds"
 PRIVATE_KEY_FILE="${SECRET_DIR}/IPI_SSH_KEY"
-IP_JUMPHOST=128.168.131.205
+IP_JUMPHOST=128.168.131.115
 CLUSTER_VARS_PATH="/root/ocp-cluster-ibmcloud/ibmcloud-openshift-provisioning/cluster-vars"
 SSH_KEY_PATH="/tmp/id_rsa"
 
