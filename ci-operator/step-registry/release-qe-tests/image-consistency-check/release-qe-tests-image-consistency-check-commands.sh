908afc3d97372403caf4a0ff5351fad9bacfe5b4
Tomas David <tdavid@redhat.com>
2026-02-10 12:03:02 +0100

    OCPERT-323: Use certificate as secret in Image Consistency Check prow job (#74666)

--- ci-operator/step-registry/release-qe-tests/image-consistency-check/release-qe-tests-image-consistency-check-commands.sh
+++ ci-operator/step-registry/release-qe-tests/image-consistency-check/release-qe-tests-image-consistency-check-commands.sh
@@ -13,5 +13,9 @@ export GITLAB_TOKEN=$gitlab_token
 registry_auth_file="/var/run/vault/release-tests-registry-conf/art-images"
 export REGISTRY_AUTH_FILE=$registry_auth_file
 
+# Get the Current IT Root CAs from the vault
+current_it_root_cas="/var/run/vault/release-tests-cert/current_it_root_cas.pem"
+export REQUESTS_CA_BUNDLE=$current_it_root_cas
+
 # Run the image consistency check
 oarctl image-consistency-check --payload-url "$MULTISTAGE_PARAM_OVERRIDE_PAYLOAD_URL" --mr-id "$MULTISTAGE_PARAM_OVERRIDE_MERGE_REQUEST_ID"
