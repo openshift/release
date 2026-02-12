7dad9533ac2bdce1d84ef78f8204555c2f3b492e
Mariusz Mazur <mmazur@redhat.com>
2026-02-12 18:23:45 +0100

    aro-hcp: always gzip files in SHARED_DIR (#74193)

--- ci-operator/step-registry/aro-hcp/test/local/aro-hcp-test-local-commands.sh
+++ ci-operator/step-registry/aro-hcp/test/local/aro-hcp-test-local-commands.sh
@@ -26,5 +26,5 @@ az account set --subscription "${SUBSCRIPTION_ID}"
 make e2e/local -o test/aro-hcp-tests SKIP_CERT_VERIFICATION=true FRONTEND_ADDRESS="https://${FRONTEND_ADDRESS}" ADMIN_API_ADDRESS="https://${ADMIN_API_ADDRESS}"
 
 # the make target produces a junit.xml in ARTIFACT_DIR.  We want to copy to SHARED_DIR so we can create
-# direct debugging links for the individual tests that failed.
-cp "${ARTIFACT_DIR}/junit.xml" "${SHARED_DIR}/junit-e2e.xml"
+# direct debugging links for the individual tests that failed. Gzip it due to 3mb SHARED_DIR limit.
+gzip -c "${ARTIFACT_DIR}/junit.xml" > "${SHARED_DIR}/junit-e2e.xml.gz"
