e2bdb40085a6ccedc534f5c79c7a180c35c5cfb3
Mohammed Tahoor <mtahoor@redhat.com>
2026-02-13 15:42:37 +0530

    Update SECRET_DIR path for SSH credentials

--- ci-operator/step-registry/openshift-observability/ibm-z/cluster-create/openshift-observability-ibm-z-cluster-create-commands.sh
+++ ci-operator/step-registry/openshift-observability/ibm-z/cluster-create/openshift-observability-ibm-z-cluster-create-commands.sh
@@ -6,7 +6,7 @@ set -o pipefail
 cd /tmp
 
 # --- SETUP AND VARIABLES ---
-SECRET_DIR="/tmp/vault/ibmz-ci-creds"
+SECRET_DIR="/tmp/vault/ibmz-ci-creds/ssh-creds-dt"
 PRIVATE_KEY_FILE="${SECRET_DIR}/IPI_SSH_KEY"
 IP_JUMPHOST=128.168.131.115
 CLUSTER_VARS_PATH="/root/ocp-cluster-ibmcloud/ibmcloud-openshift-provisioning/cluster-vars"
