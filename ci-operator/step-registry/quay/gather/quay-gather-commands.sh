b3a2b28c0a6b36c0ac0a528db0db93ca50323f8d
jbpratt <bpratt@redhat.com>
2026-02-24 11:40:41 -0600

    feat(quay): gather everything on timeout (#75213)
    and increase the timeout to 15m
    
    Signed-off-by: Brady Pratt <bpratt@redhat.com>

--- ci-operator/step-registry/quay/gather/quay-gather-commands.sh
+++ ci-operator/step-registry/quay/gather/quay-gather-commands.sh
@@ -8,3 +8,12 @@ oc get quayregistries --all-namespaces -o json >"$ARTIFACT_DIR/quayregistries.js
 oc get noobaas --all-namespaces -o json >"$ARTIFACT_DIR/noobaas.json" || true
 oc get quayintegrations -o json >"$ARTIFACT_DIR/quayintegrations.json" || true
 
+# Collect quay namespace diagnostics
+oc -n quay get pods -o json >"$ARTIFACT_DIR/quay-pods.json" || true
+oc -n quay get deployments -o json >"$ARTIFACT_DIR/quay-deployments.json" || true
+oc -n quay get events --sort-by='.lastTimestamp' -o json >"$ARTIFACT_DIR/quay-events.json" || true
+
+# Collect pod logs for key components
+for pod in $(oc -n quay get pods -o jsonpath='{.items[*].metadata.name}' 2>/dev/null || true); do
+    oc -n quay logs "$pod" --all-containers >"$ARTIFACT_DIR/quay-pod-${pod}.log" 2>&1 || true
+done
