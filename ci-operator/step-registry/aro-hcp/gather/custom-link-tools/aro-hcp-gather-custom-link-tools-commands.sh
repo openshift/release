81b7e6ea0415639813da760fb6c408d25bf8a670
Mariusz Mazur <mmazur@redhat.com>
2026-02-27 15:04:09 +0100

    Support new custom links argument with a fallback (#75374)

--- ci-operator/step-registry/aro-hcp/gather/custom-link-tools/aro-hcp-gather-custom-link-tools-commands.sh
+++ ci-operator/step-registry/aro-hcp/gather/custom-link-tools/aro-hcp-gather-custom-link-tools-commands.sh
@@ -2,6 +2,18 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-set -o xtrace 
+set -o xtrace
 
-test/aro-hcp-tests custom-link-tools --timing-input ${SHARED_DIR} --output ${ARTIFACT_DIR}/
+# Temporary: support both pre- and post- binaries for PR
+#            https://github.com/Azure/ARO-HCP/pull/4181/
+# The new binary requires --rendered-config; passing it to the old binary breaks.
+if test/aro-hcp-tests custom-link-tools --help 2>&1 | grep -q -- '--rendered-config'; then
+  test/aro-hcp-tests custom-link-tools \
+    --timing-input "${SHARED_DIR}" \
+    --output "${ARTIFACT_DIR}/" \
+    --rendered-config "${SHARED_DIR}/config.yaml"
+else
+  test/aro-hcp-tests custom-link-tools \
+    --timing-input "${SHARED_DIR}" \
+    --output "${ARTIFACT_DIR}/"
+fi
