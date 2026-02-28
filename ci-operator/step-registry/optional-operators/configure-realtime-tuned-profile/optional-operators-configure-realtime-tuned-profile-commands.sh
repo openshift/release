c3d13c5783545dd55263eb89592ee8afd75a484d
E Hila <ehila@redhat.com>
2026-02-28 14:35:38 -0500

    upkeep: update tuned profile used for realtime (#75237)
    update the tuned profile used for realtime gcp lanes to account for rhel10
    
    Signed-off-by: ehila <ehila@redhat.com>

--- ci-operator/step-registry/optional-operators/configure-realtime-tuned-profile/optional-operators-configure-realtime-tuned-profile-commands.sh
+++ ci-operator/step-registry/optional-operators/configure-realtime-tuned-profile/optional-operators-configure-realtime-tuned-profile-commands.sh
@@ -7,12 +7,8 @@ set -o pipefail
 node_role=${APPLY_NODE_ROLE:=worker}
 max_cpu=8
 isolated_cpu=${COMPUTE_NODE_ISOLATED_CPU:-4}
-sched_rt_runtime_us=-1
-stalld_service="service.stalld=start,enable"
-gcp_pattern="[n|c|m|a]{1}[1-9]{1}d?-(standard|highcpu|highmem|highgpu){1}-([0-9]+)"
+gcp_pattern="[ncma][1-9]d?-(standard|highcpu|highmem|highgpu)-([0-9]+)"
 
-# Currently RT is only supported on GCP
-# if non supported node type provided, will default to 4 isolated and 8 max CPU usage
 if [[ ${COMPUTE_NODE_TYPE-"not_provided"} =~ $gcp_pattern ]]
 then
   max_cpu=${BASH_REMATCH[2]}
@@ -26,13 +22,6 @@ if [[ "$isolated_cpu" == "$max_cpu" ]]; then
   echo "max and isolated cpu are equal, setting isolated CPU to $isolated_cpu"
 fi
 
-if [ ${STALLD_ENABLED:="true"} != "true" ]
-then
-  echo "disabling stalld and setting default realtime timeout"
-  sched_rt_runtime_us=950000
-  stalld_service=""
-fi
-
 echo "Creating new realtime tuned profile on cluster"
 oc create -f - <<EOF
 apiVersion: tuned.openshift.io/v1
@@ -44,66 +33,39 @@ spec:
   profile:
   - data: |
       [main]
-      summary=Openshift node optimized for deterministic performance at the cost of increased power consumption, focused on low latency network performance. Based on Tuned 2.11 and Cluster node tuning (oc 4.5)
-      include=openshift-node,cpu-partitioning
+      summary=Optimize for realtime workloads
+      include = network-latency
 
       [variables]
       isolated_cores=$isolated_cpu-$(( max_cpu - 1))
-      not_isolated_cores_expanded=\${f:cpulist_invert:\${isolated_cores_expanded}}
-
-      [cpu]
-      force_latency=cstate.id:1|3
-      governor=performance
-      energy_perf_bias=performance
-      min_perf_pct=100
-
-      [service]
-      $stalld_service
-
-      [vm]
-      transparent_hugepages=never
-
-      [irqbalance]
-      banned_cpus=""
-
-      [scheduler]
-      runtime=0
-      group.ksoftirqd=0:f:11:*:ksoftirqd.*
-      group.rcuc=0:f:11:*:rcuc.*
-      default_irq_smp_affinity = ignore
-
-      [sysctl]
-      kernel.hung_task_timeout_secs = 600
-      kernel.nmi_watchdog = 0
-      kernel.sched_rt_runtime_us = $sched_rt_runtime_us
-      kernel.timer_migration = 0
-      kernel.numa_balancing=0
-      net.core.busy_read=50
-      net.core.busy_poll=50
-      net.ipv4.tcp_fastopen=3
-      vm.stat_interval = 10
-      kernel.sched_min_granularity_ns=10000000
-      vm.dirty_ratio=10
-      vm.dirty_background_ratio=3
-      vm.swappiness=10
-      kernel.sched_migration_cost_ns=5000000
-
-      [selinux]
-      avc_cache_threshold=8192
+      isolate_managed_irq=Y
+      disable_pstate = \${f:cpuinfo_check:GenuineIntel:intel_pstate=disable:AuthenticAMD:amd_pstate=disable:}
+      isolated_cores_assert_check = \\\${isolated_cores}
+      assert1=\${f:assertion_non_equal:isolated_cores are set:\${isolated_cores}:\${isolated_cores_assert_check}}
+      not_isolated_cpumask = \${f:cpulist2hex_invert:\${isolated_cores}}
+      isolated_cores_expanded=\${f:cpulist_unpack:\${isolated_cores}}
+      isolated_cpumask=\${f:cpulist2hex:\${isolated_cores_expanded}}
+      isolated_cores_online_expanded=\${f:cpulist_online:\${isolated_cores}}
+      assert2=\${f:assertion:isolated_cores contains online CPU(s):\${isolated_cores_expanded}:\${isolated_cores_online_expanded}}
+      managed_irq=\${f:regex_search_ternary:\${isolate_managed_irq}:\b[y,Y,1,t,T]\b:managed_irq,:}
 
       [net]
-      nf_conntrack_hashsize=131072
+      channels=combined \${f:check_net_queue_count:\${netdev_queue_count}}
+
+      [sysctl]
+      kernel.sched_rt_runtime_us = -1
+
+      [sysfs]
+      /sys/bus/workqueue/devices/writeback/cpumask = \${not_isolated_cpumask}
+      /sys/devices/virtual/workqueue/cpumask = \${not_isolated_cpumask}
+      /sys/devices/virtual/workqueue/*/cpumask = \${not_isolated_cpumask}
+      /sys/devices/system/machinecheck/machinecheck*/ignore_ce = 1
 
       [bootloader]
-      # set empty values to disable RHEL initrd setting in cpu-partitioning
-      initrd_remove_dir=
-      initrd_dst_img=
-      initrd_add_dir=
-      # overrides cpu-partitioning cmdline
-      cmdline_cpu_part=+nohz=on rcu_nocbs=\${isolated_cores} tuned.non_isolcpus=\${not_isolated_cpumask} intel_pstate=disable nosoftlockup
-      cmdline_realtime=+intel_iommu=on iommu=pt isolcpus=managed_irq,\${isolated_cores} systemd.cpu_affinity=\${not_isolated_cores_expanded}
-      cmdline_additionalArg=+ nmi_watchdog=0 audit=0 mce=off processor.max_cstate=1 idle=poll intel_idle.max_cstate=0 nohz_full=\${isolated_cores}
-      cmdline_network_latency=skew_tick=1 tsc=reliable rcupdate.rcu_normal_after_boot=1
+      cmdline_realtime=+isolcpus=\${managed_irq}\${isolated_cores} \${disable_pstate} nosoftlockup nohz_full=\${isolated_cores} rcu_nocbs=\${isolated_cores} nohz=on audit=0
+
+      [irqbalance]
+      banned_cpus=\${isolated_cores}
     name: openshift-node-performance-manual
   recommend:
   - machineConfigLabels:
