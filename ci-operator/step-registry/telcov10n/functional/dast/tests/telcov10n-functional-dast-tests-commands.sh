9a5bd3b8a5eecba420e67d3fff26dd4f7fae34ae
oblau <oblau@redhat.com>
2026-03-05 11:43:06 +0200

    dast: add env var to choose dast repo source
    Signed-off-by: oblau <oblau@redhat.com>

--- ci-operator/step-registry/telcov10n/functional/dast/tests/telcov10n-functional-dast-tests-commands.sh
+++ ci-operator/step-registry/telcov10n/functional/dast/tests/telcov10n-functional-dast-tests-commands.sh
@@ -4,6 +4,10 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# Clone test files at runtime since .containerignore in eco-ci-cd
+# excludes tests/ from the Docker build context
+git clone --depth 1 -b "${DAST_TEST_BRANCH}" "${DAST_TEST_REPO}" /tmp/telco-dast-qe
+
 dastdir="/tmp/telco-dast-qe/tests/dast"
 # set default values
 WORKSPACE=${SHARED_DIR}
