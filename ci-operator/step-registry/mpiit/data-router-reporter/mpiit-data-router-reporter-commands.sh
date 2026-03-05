aec09dc58e20372e03c90947c8b036a536bc397a
oharan2 <oharan@redhat.com>
2026-03-05 16:24:56 +0200

    MPIIT dr-reporter: Add support for FIPS attribute, extract job related info (#75392)
    * Force reporting values to be mentioned in the job config
    
    * Add support for fips-enabled based on job name
    
    * Remove backup ocp version from workflow
    
    * Allow default CMP name
    
    * Fix script vars set

--- ci-operator/step-registry/mpiit/data-router-reporter/mpiit-data-router-reporter-commands.sh
+++ ci-operator/step-registry/mpiit/data-router-reporter/mpiit-data-router-reporter-commands.sh
@@ -14,25 +14,44 @@ function reportToDataRouter() {
     fi
 }
 
+function extractOCPVersion() {
+    # If OCP_VERSION is not set, we don't want to fail the entire launch report process
+    # The version attribute is not essentially required, so extract it from the JOB_NAME
+    if [[ -z "${OCP_VERSION}" ]]; then
+        if [[ "${JOB_NAME}" =~ ocp-([0-9]+\.[0-9]+) ]]; then
+            export OCP_VERSION="${BASH_REMATCH[1]}"
+        else
+            export OCP_VERSION="unknown"
+        fi
+    fi
+    true
+}
+
 export DATAROUTER_RESULTS="${SHARED_DIR}/*.xml"
-export OCP_VERSION
 export REPORTPORTAL_APPLY_TFA="${APPLY_TFA}"
 export REPORTPORTAL_CMP
 export REPORTPORTAL_HOSTNAME
 export REPORTPORTAL_PROJECT="lp-interop-dr_personal"
 export REPORTPORTAL_LAUNCH_NAME="${REPORTPORTAL_CMP}"
 
+extractOCPVersion
+
+typeset version="${OCP_VERSION}"
+typeset fips="${FIPS_ENABLED}"
+
 REPORTPORTAL_LAUNCH_ATTRIBUTES="$(
     jq -nc \
         --arg jobName "${JOB_NAME}" \
         --arg buildID "${BUILD_ID}" \
-        --arg ocpVer "${OCP_VERSION}" \
+        --arg ocpVer "${version}" \
         --arg rpCompName "${REPORTPORTAL_CMP}" \
+        --arg fipsEnabled "${fips}" \
         '[
             {key: "job_name", value: $jobName},
             {key: "build_id", value: $buildID},
             {key: "ocp_release", value: $ocpVer},
-            {key: "component_name", value: $rpCompName}
+            {key: "component_name", value: $rpCompName},
+            {key: "fips_enabled", value: $fipsEnabled}
         ]'
 )"
 export REPORTPORTAL_LAUNCH_ATTRIBUTES
