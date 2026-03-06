f74d0df08fbc7d1a97cb124c8f3b08352837425f
Mariusz Mazur <mmazur@redhat.com>
2026-03-05 16:26:35 -0800

    Use kusto linking everywhere (#75747)
    * Run link step for int/stg/prod jobs
    
    * Remove tmp dual custom link tool call

--- ci-operator/step-registry/aro-hcp/gather/custom-link-tools/aro-hcp-gather-custom-link-tools-commands.sh
+++ ci-operator/step-registry/aro-hcp/gather/custom-link-tools/aro-hcp-gather-custom-link-tools-commands.sh
@@ -6,16 +6,7 @@ set -o xtrace
 
 export AZURE_TOKEN_CREDENTIALS=prod
 
-# Temporary: support both pre- and post- binaries for PR
-#            https://github.com/Azure/ARO-HCP/pull/4181/
-# The new binary requires --rendered-config; passing it to the old binary breaks.
-if test/aro-hcp-tests custom-link-tools --help 2>&1 | grep -q -- '--rendered-config'; then
-  test/aro-hcp-tests custom-link-tools \
-    --timing-input "${SHARED_DIR}" \
-    --output "${ARTIFACT_DIR}/" \
-    --rendered-config "${SHARED_DIR}/config.yaml"
-else
-  test/aro-hcp-tests custom-link-tools \
-    --timing-input "${SHARED_DIR}" \
-    --output "${ARTIFACT_DIR}/"
-fi
+test/aro-hcp-tests custom-link-tools \
+  --timing-input "${SHARED_DIR}" \
+  --output "${ARTIFACT_DIR}/" \
+  --rendered-config "${SHARED_DIR}/config.yaml"
