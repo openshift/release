29f935d48beab15224bd975fdf73086ac3f48b06
zhouying7780 <yinzhou@redhat.com>
2026-03-06 13:59:10 +0800

    add debug time

--- ci-operator/step-registry/hypershift/kubevirt/create/hypershift-kubevirt-create-commands.sh
+++ ci-operator/step-registry/hypershift/kubevirt/create/hypershift-kubevirt-create-commands.sh
@@ -215,7 +215,9 @@ else
 fi
 
 echo "Waiting for cluster to become available"
-sleep 5h
+
+
+echo "Waiting for cluster to become available"
 oc wait --timeout=30m --for=condition=Available --namespace=${CLUSTER_NAMESPACE_PREFIX} "hostedcluster/${CLUSTER_NAME}"
 echo "Cluster became available, creating kubeconfig"
 $HCP_CLI create kubeconfig --namespace="${CLUSTER_NAMESPACE_PREFIX}" --name="${CLUSTER_NAME}" >"${SHARED_DIR}/nested_kubeconfig"
