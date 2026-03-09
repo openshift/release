f8d20ec36a9a3ec7f493323edbd1be096dd32a29
miyadav <miyadav@redhat.com>
2026-03-09 16:36:37 +0000

    missing pod specs updated

--- ci-operator/step-registry/hive/operator/install/hive-operator-install-commands.sh
+++ ci-operator/step-registry/hive/operator/install/hive-operator-install-commands.sh
@@ -196,6 +196,14 @@ spec:
           valueFrom:
             fieldRef:
               fieldPath: metadata.namespace
+        - name: POD_NAME
+          valueFrom:
+            fieldRef:
+              fieldPath: metadata.name
+        - name: POD_NAMESPACE
+          valueFrom:
+            fieldRef:
+              fieldPath: metadata.namespace
         imagePullPolicy: Always
         livenessProbe:
           httpGet:
