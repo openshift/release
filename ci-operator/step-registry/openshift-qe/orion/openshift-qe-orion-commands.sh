4719318ec7f25b34d8c01f6fe4177b3926409245
Mohit Sheth <msheth@redhat.com>
2026-03-09 11:25:59 -0400

    Add --viz flag to orion command and point to visualization branch (#75133)
    Signed-off-by: Mohit Sheth <msheth@redhat.com>

--- ci-operator/step-registry/openshift-qe/orion/openshift-qe-orion-commands.sh
+++ ci-operator/step-registry/openshift-qe/orion/openshift-qe-orion-commands.sh
@@ -188,7 +188,7 @@ export es_metadata_index=${ES_METADATA_INDEX} es_benchmark_index=${ES_BENCHMARK_
 if [[ -n $pull_number ]]; then
     export pull_number=${pull_number}
 fi
-orion --node-count ${IGNORE_JOB_ITERATIONS} --config ${ORION_CONFIG} ${EXTRA_FLAGS} | tee ${ARTIFACT_DIR}/${FILENAME}.txt
+orion --node-count ${IGNORE_JOB_ITERATIONS} --config ${ORION_CONFIG} ${EXTRA_FLAGS} --viz | tee ${ARTIFACT_DIR}/${FILENAME}.txt
 orion_exit_status=$?
 set -e
 
@@ -287,7 +287,7 @@ process_change_point() {
 }
 process_change_point
 
-cp *.csv *.xml *.json *.txt "${ARTIFACT_DIR}/" 2>/dev/null || true
+cp *.csv *.xml *.json *.txt *.html "${ARTIFACT_DIR}/" 2>/dev/null || true
 
 if [ $orion_exit_status -eq 3 ]; then
   echo "Orion returned exit code 3, which means there are no results to analyze."
