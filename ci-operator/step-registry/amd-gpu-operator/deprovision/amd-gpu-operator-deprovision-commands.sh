a8d8497ea2694ceb3530bbf2cefa3fe18c60c57f
Tomer Newman <tnewman@redhat.com>
2026-03-10 17:16:52 +0200

    Making amd-ci use custom amd gpu versions (#75506)
    * Making amd-ci use custom amd gpu versions
    
    * Add gpu_operator_version to provision and deprovision steps

--- ci-operator/step-registry/amd-gpu-operator/deprovision/amd-gpu-operator-deprovision-commands.sh
+++ ci-operator/step-registry/amd-gpu-operator/deprovision/amd-gpu-operator-deprovision-commands.sh
@@ -59,6 +59,7 @@ version_channel: ${VERSION_CHANNEL:-stable}
 
 operators:
   install: false
+  gpu_operator_version: "${GPU_OPERATOR_VERSION:-1.4}"
   machine_config_role: worker
   driver_version: "30.20.1"
   enable_metrics: true
