d347026e1070156b30faf9c9061e9e3725806c6d
Neeraj Krishna Gopalakrishna <ngopalak@redhat.com>
2026-03-13 18:07:23 +0530

    Test cri-o CI change

--- ci-operator/step-registry/cri-o/setup-fedora/test/cri-o-setup-fedora-test-commands.sh
+++ ci-operator/step-registry/cri-o/setup-fedora/test/cri-o-setup-fedora-test-commands.sh
@@ -13,8 +13,44 @@ instance_name=$(<"${SHARED_DIR}/gcp-instance-ids.txt")
 timeout --kill-after 10m 400m ssh "${SSHOPTS[@]}" ${IP} -- bash - <<EOF
     SOURCE_DIR="/usr/go/src/github.com/cri-o/cri-o"
     cd "\${SOURCE_DIR}/contrib/test/ci"
-    # Add libpathrs-devel to Fedora packages for testing
-    sed -i '/- crun-wasm$/a\      # required for building runc with libpathrs support.\n      - libpathrs-devel' system-packages.yml
+
+    # Update system-packages.yml to replace libpathrs-devel with build dependencies
+    sed -i '/# required for building runc with libpathrs support\./d' system-packages.yml
+    sed -i 's/      - libpathrs-devel/      # required for building libpathrs from source.\n      - rust\n      - cargo\n      - cargo-c\n      - git/' system-packages.yml
+
+    # Create build/libpathrs.yml
+    cat > build/libpathrs.yml << 'LIBPATHRS_EOF'
+---
+- name: clone libpathrs source repo
+  git:
+    repo: "https://github.com/cyphar/libpathrs.git"
+    dest: "/tmp/libpathrs"
+    version: "main"
+    force: yes
+
+- name: build libpathrs
+  shell: cargo cbuild --release
+  args:
+    chdir: "/tmp/libpathrs"
+  environment:
+    PATH: "{{ ansible_env.HOME }}/.cargo/bin:{{ ansible_env.PATH }}"
+
+- name: install libpathrs
+  shell: cargo cinstall --release --prefix=/usr --libdir=/usr/lib64
+  args:
+    chdir: "/tmp/libpathrs"
+  environment:
+    PATH: "{{ ansible_env.HOME }}/.cargo/bin:{{ ansible_env.PATH }}"
+
+- name: cleanup libpathrs source
+  file:
+    path: "/tmp/libpathrs"
+    state: absent
+LIBPATHRS_EOF
+
+    # Update setup.yml to add libpathrs build step before runc
+    sed -i '/- name: clone build and install runc/i\- name: build and install libpathrs\n  include_tasks: "build/libpathrs.yml"\n  when: ansible_distribution in ['"'"'Fedora'"'"']\n' setup.yml
+
     ansible-playbook setup-main.yml --connection=local -vvv
     ANSIBLE_EXIT_CODE=\$?
     sudo rm -rf "\${SOURCE_DIR}"
