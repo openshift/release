bc6167e05fadd570446420125bfb9d643207a046
Inbharaj <imani@redhat.com>
2026-03-13 09:55:16 +0530

    Readonly mod

--- ci-operator/step-registry/aro-hcp/exporter/aro-hcp-exporter-commands.sh
+++ ci-operator/step-registry/aro-hcp/exporter/aro-hcp-exporter-commands.sh
@@ -24,13 +24,13 @@ az login --service-principal \
 
 echo "CI-built image: ${ARO_HCP_EXPORTER}"
 
+export GOFLAGS='-mod=readonly'
+
 # TODO: Remove after ARO-HCP oc_mirror branch merges to main
 git remote add upstream https://github.com/Azure/ARO-HCP.git || true
 git fetch upstream oc_mirror
 git checkout upstream/oc_mirror -- tooling/aro-hcp-exporter/Makefile
 
-# Use the Makefile acr-push target which resolves ACR coordinates
-# via setup-templatize-env.mk and pushes using oc image mirror
 make -C tooling/aro-hcp-exporter acr-push \
   DEPLOY_ENV="${DEPLOY_ENV}" \
   SOURCE_IMAGE="${ARO_HCP_EXPORTER}"
