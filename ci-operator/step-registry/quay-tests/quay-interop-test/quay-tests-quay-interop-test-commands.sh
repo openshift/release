a5ea249e9b9b9c31ef140e319cea1a23fe1a9f66
Wei Hua Hu <58208478+huweihua-redhat@users.noreply.github.com>
2026-03-18 10:40:35 +0800

    Replace yarn by npm in the interopp testing (#76382)

--- ci-operator/step-registry/quay-tests/quay-interop-test/quay-tests-quay-interop-test-commands.sh
+++ ci-operator/step-registry/quay-tests/quay-interop-test/quay-tests-quay-interop-test-commands.sh
@@ -39,7 +39,7 @@ function copyArtifacts {
 }
 
 # Install Dependcies defined in packages.json
-yarn install || true
+npm install || true
 
 #Finally Copy the Junit Testing XML files and Screenshots to /tmp/artifacts
 trap copyArtifacts EXIT
@@ -73,5 +73,5 @@ else
     export CYPRESS_QUAY_VERSION="${QUAY_VERSION}"
 fi
 
-NO_COLOR=1 yarn run smoke || true
+NO_COLOR=1 npm run smoke || true
 
