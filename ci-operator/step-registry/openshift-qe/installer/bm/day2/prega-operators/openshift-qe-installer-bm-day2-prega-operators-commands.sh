b01d28602586c484e84af0626fded6618556db38
Raviteja Sahukari <rsahukar@redhat.com>
2026-03-19 11:53:23 +0530

    [PERFSCALE-4513] Update the prega_auth path to get secret from Vault
    Signed-off-by: Raviteja Sahukari <rsahukar@redhat.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/day2/prega-operators/openshift-qe-installer-bm-day2-prega-operators-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/day2/prega-operators/openshift-qe-installer-bm-day2-prega-operators-commands.sh
@@ -14,15 +14,16 @@ if [ ${OCP_BUILD} == "dev" ]; then
   echo "Patching OperatorHub to disable all default sources"
   oc patch OperatorHub cluster --type json -p '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]'
 
-  PREGA_AUTH="$(cat /secret/prega_auth)"
+  echo "Getting the existing pull secret from the cluster and merge the prega_auth secret to pull images from quay.io"
   oc get secret pull-secret -n openshift-config -o json | jq -r '.data[".dockerconfigjson"]' | base64 -d > /tmp/existing_pull_secret.json
-  echo ${PREGA_AUTH} > /tmp/prega_pull_secret.json
+  cp ${CLUSTER_PROFILE_DIR}/prega_auth /tmp/prega_pull_secret.json
   jq -s '.[0] * .[1]' /tmp/existing_pull_secret.json /tmp/prega_pull_secret.json > /tmp/merged_pull_secret.json
   oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=/tmp/merged_pull_secret.json
   sleep 300
   kubectl wait --for jsonpath='{.status.updatedMachineCount}'="$(oc get node --no-headers -l node-role.kubernetes.io/worker=true | wc -l)" --timeout=60m mcp worker
   oc adm wait-for-stable-cluster --minimum-stable-period=2m --timeout=20m
 
+  echo "Applying the ImageDigestMirrorSet manifest"
   OCP_VERSION=$(oc get clusterversion --no-headers | grep -o '[4].[0-9][0-9]' | head -1 | awk '{print "v"$0}')
   OPERATOR_PREGA_VERSION=$(curl -s 'https://quay.io/api/v1/repository/prega/prega-operator-index/tag/?limit=100&page=1' | jq --arg version "$OCP_VERSION" -r '.tags[].name | select(startswith($version))' | sort -V | tail -1)
   echo "Installing PREGA Operator ${OPERATOR_PREGA_VERSION} for OCP ${OCP_VERSION}"
@@ -54,5 +55,3 @@ EOF
 else
   echo "OCP_BUILD is not dev, skipping PREGA Operator Index installation"
 fi
-
-
