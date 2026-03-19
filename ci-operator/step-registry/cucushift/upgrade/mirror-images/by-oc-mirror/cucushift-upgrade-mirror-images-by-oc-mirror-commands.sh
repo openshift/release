14cd9e2bab8a376a4c271de4252533e9e81344d5
Jianlin Liu <jialiu@redhat.com>
2026-03-19 10:37:04 +0800

    allow users to use original payload image to run cluster install and upgrade instead of build farm imagestream url (#76145)

--- ci-operator/step-registry/cucushift/upgrade/mirror-images/by-oc-mirror/cucushift-upgrade-mirror-images-by-oc-mirror-commands.sh
+++ ci-operator/step-registry/cucushift/upgrade/mirror-images/by-oc-mirror/cucushift-upgrade-mirror-images-by-oc-mirror-commands.sh
@@ -44,11 +44,31 @@ function check_signed() {
     fi
 }
 
+function isPreVersion() {
+  local version="$1"
+  local required_ocp_version="$2"
+  local isPre=0
+  if [ -n "${version}" ] && [ "$(printf '%s\n' "${required_ocp_version}" "${version}" | sort --version-sort | head -n1)" = "${required_ocp_version}" ]; then
+    isPre=1
+  fi
+  return $isPre
+}
+
 # private mirror registry host
 # <public_dns>:<port>
 MIRROR_REGISTRY_HOST=$(head -n 1 "${SHARED_DIR}/mirror_registry_url")
 echo "MIRROR_REGISTRY_HOST: $MIRROR_REGISTRY_HOST"
 echo "OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE: ${OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE}"
+if [[ "${USE_ORIGINAL_OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE}" == "true" ]]; then
+  ORIGINAL_OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE=$(KUBECONFIG="" oc get is release -o jsonpath='{range .status.tags[*].items[*]}{.image}{" "}{.dockerImageReference}{"\n"}{end}' | grep "^$(echo "$OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE" | sed 's/.*@//')" | awk '{print $2}')
+  echo "User want the original payload for cluster upgrade, overwrite OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE to ${ORIGINAL_OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE}"
+  export OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE=${ORIGINAL_OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE}
+fi
+
+if [[ -z "$OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE" ]]; then
+  echo "OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE is an empty string, exiting"
+  exit 1
+fi
 
 # target release
 target_release_image="${MIRROR_REGISTRY_HOST}/${OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE#*/}"
@@ -58,8 +78,7 @@ echo "target_release_image_repo: $target_release_image_repo"
 
 # since ci-operator gives steps KUBECONFIG pointing to cluster under test under some circumstances,
 # unset KUBECONFIG to ensure this step always interact with the build farm.
-unset KUBECONFIG
-oc registry login
+KUBECONFIG="" oc registry login
 
 run_command "which oc"
 run_command "oc version --client"
@@ -92,11 +111,26 @@ cp -rf "${new_pull_secret}" "${XDG_RUNTIME_DIR}/containers/auth.json"
 
 unset REGISTRY_AUTH_PREFERENCE
 
+payload_signed="true"
+check_signed "${OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE}" || payload_signed="false"
+
+mirrorCmd="'${oc_mirror_bin}' -c ${image_set_config} docker://${target_release_image_repo} --dest-tls-verify=false --v2 --workspace file://${oc_mirror_dir}"
+
+# ref OCPBUGS-56009
+ocp_version=$(oc adm release info --registry-config ${CLUSTER_PROFILE_DIR}/pull-secret ${OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE} -o jsonpath='{.metadata.version}' | cut -d. -f 1,2)
+if ! isPreVersion "${ocp_version}" "4.19" && [[ "${payload_signed}" == "false" ]] ; then
+    mirrorCmd="${mirrorCmd} --ignore-release-signature"
+fi
+
 # execute the oc-mirror command
-run_command "'${oc_mirror_bin}' -c ${image_set_config} docker://${target_release_image_repo} --dest-tls-verify=false --v2 --workspace file://${oc_mirror_dir}"
+run_command "${mirrorCmd}"
 
 # Save output from oc-mirror
 result_folder="${oc_mirror_dir}/working-dir"
+run_command "find '${result_folder}' | sort"
+
+export KUBECONFIG=${SHARED_DIR}/kubeconfig
+
 idms_file="${result_folder}/cluster-resources/idms-oc-mirror.yaml"
 itms_file="${result_folder}/cluster-resources/itms-oc-mirror.yaml"
 
@@ -105,13 +139,31 @@ if [ ! -s "${idms_file}" ]; then
     exit 1
 else
     run_command "cat '${idms_file}'"
-    run_command "cp -rf '${idms_file}' ${SHARED_DIR}"
+    upgrade_idms_file="${SHARED_DIR}/upgrade_$(basename ${idms_file})"
+    run_command "cp -rf '${idms_file}' ${upgrade_idms_file}"
+    echo "=== checking the difference of idms yamls generated for install and upgrade ==="
+    install_idms_file="${SHARED_DIR}/idms-oc-mirror.yaml"
+    diff_ret=0
+    run_command "diff <(grep -A 1000 'imageDigestMirrors:' '${install_idms_file}') <(grep -A 1000 'imageDigestMirrors:' '${upgrade_idms_file}')" || diff_ret="$?"
+    if [[ ${diff_ret} -ne 0 ]]; then
+        echo "idms changed, applying the new one"
+        run_command "oc apply -f ${upgrade_idms_file}"
+    fi
 fi
 
 if [ -s "${itms_file}" ]; then
     echo "${itms_file} found"
     run_command "cat '${itms_file}'"
-    run_command "cp -rf '${itms_file}' ${SHARED_DIR}"
+    upgrade_itms_file="${SHARED_DIR}/upgrade_$(basename ${itms_file})"
+    run_command "cp -rf '${itms_file}' ${upgrade_itms_file}"
+    echo "=== checking the difference of itms yamls generated for install and upgrade ==="
+    install_itms_file="${SHARED_DIR}/itms-oc-mirror.yaml"
+    diff_ret=0
+    run_command "diff <(grep -A 1000 'imageTagMirrors:' '${install_itms_file}') <(grep -A 1000 'imageTagMirrors:' '${upgrade_itms_file}')" || diff_ret="$?"
+    if [[ ${diff_ret} -ne 0 ]]; then
+        echo "itms changed, applying the new one"
+        run_command "oc apply -f ${upgrade_itms_file}"
+    fi
 fi
 
 if [[ "${MIRROR_GRAPH_DATA}" == "true" ]]; then
@@ -123,22 +175,20 @@ if [[ "${MIRROR_GRAPH_DATA}" == "true" ]]; then
         run_command "cat '${us_file}'"
         run_command "cp -rf '${us_file}' ${SHARED_DIR}"
     fi
-    run_command "ls '${result_folder}'"
     
-    export TARGET="${OPENSHIFT_UPGRADE_RELEASE_IMAGE_OVERRIDE}"
-    if ! check_signed "${TARGET}"; then
+    if [[ "${payload_signed}" == "false" ]]; then
         echo "You're mirroring an unsigned images, don't apply signature"
     else
         echo "You're mirroring a signed images, will apply signature"
         # oc-mirror v2 support mirror with signatures from 4.18
-        sig_folder="${result_folder}/signatures"
-        if [[ ! -d "${sig_folder}" || -z "$(ls -A ${sig_folder})" ]]; then
+        sig_file="${result_folder}/cluster-resources/signature-configmap.json"
+        if [[ ! -s "${sig_file}" ]]; then
             echo "signatures not found, exit..."
             exit 1
         fi
-        run_command "ls '${sig_folder}'"
-        export KUBECONFIG=${SHARED_DIR}/kubeconfig
-        oc apply -f "${sig_folder}"
+        run_command "cat '${sig_file}'"
+        echo ""
+        run_command "oc apply -f '${sig_file}'"
     fi
 fi
 
