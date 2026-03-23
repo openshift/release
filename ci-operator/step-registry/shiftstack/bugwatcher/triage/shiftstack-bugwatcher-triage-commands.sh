9421ec55b24f57e3aebfdf2a6910a919b752588a
Stephen Finucane <stephenfin@redhat.com>
2026-03-23 20:25:01 +0100

    openstack: Add missing credentials for ghira, bugwatcher (#76697)
    * bugwatcher: Add missing secret mounts
    
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>
    
    * ghira: Add missing secret mounts
    
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>
    
    ---------
    
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- ci-operator/step-registry/shiftstack/bugwatcher/triage/shiftstack-bugwatcher-triage-commands.sh
+++ ci-operator/step-registry/shiftstack/bugwatcher/triage/shiftstack-bugwatcher-triage-commands.sh
@@ -2,10 +2,14 @@
 
 set -Eeuo pipefail
 
+JIRA_ACCOUNT_ID="$(</var/run/bugwatcher/jira-account-id)"
+JIRA_EMAIL="$(</var/run/bugwatcher/jira-email)"
 JIRA_TOKEN="$(</var/run/bugwatcher/jira-token)"
 SLACK_HOOK="$(</var/run/slack-hooks/forum-shiftstack)"
 PEOPLE="$(</var/run/team/people.yaml)"
 
+export JIRA_ACCOUNT_ID
+export JIRA_EMAIL
 export JIRA_TOKEN
 export SLACK_HOOK
 export PEOPLE
--- ci-operator/step-registry/shiftstack/ghira/shiftstack-ghira-commands.sh
+++ ci-operator/step-registry/shiftstack/ghira/shiftstack-ghira-commands.sh
@@ -2,10 +2,14 @@
 
 set -Eeuo pipefail
 
+JIRA_ACCOUNT_ID="$(</var/run/ghira/jira-account-id)"
+JIRA_EMAIL="$(</var/run/ghira/jira-email)"
 JIRA_TOKEN="$(</var/run/ghira/jira-token)"
 GITHUB_TOKEN="$(</var/run/ghira/github-token)"
 PEOPLE="$(</var/run/team/people.yaml)"
 
+export JIRA_ACCOUNT_ID
+export JIRA_EMAIL
 export JIRA_TOKEN
 export GITHUB_TOKEN
 export PEOPLE
