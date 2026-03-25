018e483a35d738a27ed506b95cdfcdafccf00a82
Tomas David <tdavid@redhat.com>
2026-03-25 03:25:15 +0100

    OCPERT-154: Add JIRA_USERNAME env to Jira Notificator (#76321)

--- ci-operator/step-registry/release-qe-tests/jira-notificator/release-qe-tests-jira-notificator-commands.sh
+++ ci-operator/step-registry/release-qe-tests/jira-notificator/release-qe-tests-jira-notificator-commands.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+jira_username=$(cat "/var/run/vault/release-tests-token/jira_username")
+export JIRA_USERNAME=$jira_username
 jira_token=$(cat "/var/run/vault/release-tests-token/jira_token")
 export JIRA_TOKEN=$jira_token
 oarctl jira-notificator
