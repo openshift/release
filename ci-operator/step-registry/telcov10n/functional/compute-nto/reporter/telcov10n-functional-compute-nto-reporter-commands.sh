b7526281e8f770b31cacd7d295c878103291785d
Shai Oren <sobarzan@redhat.com>
2026-03-26 16:42:37 +0200

    Align elevated playbooks in eco-ci-cd repo

--- ci-operator/step-registry/telcov10n/functional/compute-nto/reporter/telcov10n-functional-compute-nto-reporter-commands.sh
+++ ci-operator/step-registry/telcov10n/functional/compute-nto/reporter/telcov10n-functional-compute-nto-reporter-commands.sh
@@ -56,7 +56,7 @@ cp "${SHARED_DIR}"/junit_*.xml /tmp/junit/ 2>/dev/null || echo "No junit_*.xml f
 echo "Upload compute-nto test reports"
 cd /eco-ci-cd
 # shellcheck disable=SC2154
-ansible-playbook ./playbooks/cnf/upload-report.yaml -i ./inventories/ocp-deployment/build-inventory.py \
+ansible-playbook ./playbooks/upload-report.yaml -i ./inventories/ocp-deployment/build-inventory.py \
     --extra-vars "kubeconfig=/home/telcov10n/project/generated/${CLUSTER_NAME}/auth/kubeconfig \
         reporter_template_name='${REPORTER_TEMPLATE_NAME}' processed_report_dir=/tmp/reports \
         junit_report_dir=/tmp/junit reports_directory=/tmp/upload"
