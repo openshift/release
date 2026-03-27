73fb683ea6d5bc92e0778d413025260c51864615
Biagio Manzari <bmanzari@redhat.com>
2026-03-27 10:52:35 +0100

    Konflux/Prow CI integration

--- ci-operator/step-registry/agent-qe/baremetal/install/ove/agent-qe-baremetal-install-ove-commands.sh
+++ ci-operator/step-registry/agent-qe/baremetal/install/ove/agent-qe-baremetal-install-ove-commands.sh
@@ -46,7 +46,11 @@ for bmhost in $(yq e -o=j -I=0 '.[]' "${SHARED_DIR}/hosts.yaml"); do
      iso_path="${IP_ADDRESS}/isos/${AGENT_ISO}"
    else
      # Assuming HTTP or HTTPS
-     iso_path="${transfer_protocol_type:-http}://${AUX_HOST}/${AGENT_ISO}"
+     if [ -n "${OVE_ISO_STORAGE_HOST}" ]; then
+        iso_path="${transfer_protocol_type:-http}://${OVE_ISO_STORAGE_HOST}/${CLUSTER_NAME}.agent-ove.x86_64.iso"
+     else
+        iso_path="${transfer_protocol_type:-http}://${AUX_HOST}/${AGENT_ISO}"
+     fi
    fi
    mount_virtual_media "${host}" "${iso_path}"
 
---
+++ ci-operator/step-registry/agent-qe/baremetal/install/ove/disconnected/konflux/extract-iso/agent-qe-baremetal-install-ove-disconnected-konflux-extract-iso-commands.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -o errtrace
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Trap to kill children processes
+trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM ERR
+
+[ -z "${AUX_HOST}" ] && { echo "\$AUX_HOST is not filled. Failing."; exit 1; }
+[ -z "${SNAPSHOT}" ] && { echo "\$SNAPSHOT is not filled. Failing."; exit 1; }
+
+[ ! -f "${SHARED_DIR}/proxy-conf.sh" ] && { echo "Proxy conf file is not found. Failing."; exit 1; }
+
+source "${SHARED_DIR}/proxy-conf.sh"
+
+SSHOPTS=(-o 'ConnectTimeout=5'
+  -o 'StrictHostKeyChecking=no'
+  -o 'UserKnownHostsFile=/dev/null'
+  -o 'ServerAliveInterval=90'
+  -o LogLevel=ERROR
+  -i "${CLUSTER_PROFILE_DIR}/ssh-key")
+
+CLUSTER_NAME=$(<"${SHARED_DIR}/cluster_name")
+
+# SNAPSHOT example value passed by Konflux
+# {"application":"ove-ui-4-21","components":[{"name":"ove-ui-iso-4-21","containerImage":"quay.io/redhat-user-workloads/ocp-agent-based-installer-tenant/ove-ui-iso-4-21@sha256:c5c5269aec05dd1b16fedfd762b312f0f7b0858633d1f0850d17969f09e3df33",
+# "source":{"git":{"url":"https://github.com/openshift/agent-installer-utils","revision":"d5e584590355867515ff90bb9ccd2e9019073441"}}}],"artifacts":{}}
+
+
+# Make the code compatible with runs triggered by Konflux and manual testing
+KONFLUX_SNAPSHOT=$(echo ${SNAPSHOT} | jq -r '.components[].containerImage' )
+
+echo "Konflux snapshot ID: ${KONFLUX_SNAPSHOT}"
+
+timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${OVE_ISO_STORAGE_HOST}" extract_ove_iso.sh "${KONFLUX_SNAPSHOT}" "${CLUSTER_NAME}.agent-ove.x86_64.iso"
+
+# APPLY MANOJ'S PATCH FOR BAREMETAL SERIAL CONSOLE
+
+# Update the Ignition configuration to enable serial console access in the agent-interactive-console.service
+# Change TTYPath=/dev/tty15 to TTYPath=/dev/ttyS0
+# Remove ConditionPathExists=/dev/fb0
+# Remove ExecStartPre=/usr/bin/chvt 15
+# Add necessary kernel arguments to support serial console communication
+# console=ttyS0,115200n8
+
+timeout -s 9 10m ssh "${SSHOPTS[@]}" "root@${OVE_ISO_STORAGE_HOST}" patch_ove_iso.sh "${CLUSTER_NAME}.agent-ove.x86_64.iso"
