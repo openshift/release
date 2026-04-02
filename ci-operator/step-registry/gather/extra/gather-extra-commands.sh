b90b1642806caf6034422ece854001d68f6e1448
Damiano Donati <damiano.donati@gmail.com>
2026-04-02 13:09:15 +0200

    gather-extra: collect clusterapis.operator.openshift.io CRs (#77292)
    Add the ClusterAPI operator CR to the set of resources gathered
    during CI artifact collection, enabling better diagnostics for
    CAPI-managed clusters.

--- ci-operator/step-registry/gather/extra/gather-extra-commands.sh
+++ ci-operator/step-registry/gather/extra/gather-extra-commands.sh
@@ -104,6 +104,8 @@ queue ${ARTIFACT_DIR}/${CAPI_PLATFORM}machines.infrastructure.cluster.x-k8s.io.j
 queue ${ARTIFACT_DIR}/oc_cmds/${CAPI_PLATFORM}machines.infrastructure.cluster.x-k8s.io oc --insecure-skip-tls-verify --request-timeout=5s get ${CAPI_PLATFORM}machines.infrastructure.cluster.x-k8s.io -A
 queue ${ARTIFACT_DIR}/${CAPI_PLATFORM}machinetemplates.infrastructure.cluster.x-k8s.io.json oc --insecure-skip-tls-verify --request-timeout=5s get ${CAPI_PLATFORM}machinetemplates.infrastructure.cluster.x-k8s.io -A -o json
 queue ${ARTIFACT_DIR}/oc_cmds/${CAPI_PLATFORM}machinetemplates.infrastructure.cluster.x-k8s.io oc --insecure-skip-tls-verify --request-timeout=5s get ${CAPI_PLATFORM}machinetemplates.infrastructure.cluster.x-k8s.io -A
+queue ${ARTIFACT_DIR}/clusterapis.operator.openshift.io.json oc --insecure-skip-tls-verify --request-timeout=5s get clusterapis.operator.openshift.io -o json
+queue ${ARTIFACT_DIR}/oc_cmds/clusterapis.operator.openshift.io oc --insecure-skip-tls-verify --request-timeout=5s get clusterapis.operator.openshift.io
 queue ${ARTIFACT_DIR}/namespaces.json oc --insecure-skip-tls-verify --request-timeout=5s get namespaces -o json
 queue ${ARTIFACT_DIR}/oc_cmds/namespaces oc --insecure-skip-tls-verify --request-timeout=5s get namespaces
 queue ${ARTIFACT_DIR}/nodes.json oc --insecure-skip-tls-verify --request-timeout=5s get nodes -o json
