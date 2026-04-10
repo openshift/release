2e3d052888fa03af3d158b4653edd6d8817fdaeb
Gerd Oberlechner <goberlec@redhat.com>
2026-04-10 19:19:49 +0200

    ARO-HCP: gather observability (#77572)
    add a workflow step for aro-hcp-local-e2e to
    * collect and chart key metrics
    * collect and report alerts that fired and resolved during the e2e tests
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

---
+++ ci-operator/step-registry/aro-hcp/gather/observability/aro-hcp-gather-observability-commands.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+export AZURE_CLIENT_ID; AZURE_CLIENT_ID=$(cat "${CLUSTER_PROFILE_DIR}/client-id")
+export AZURE_TENANT_ID; AZURE_TENANT_ID=$(cat "${CLUSTER_PROFILE_DIR}/tenant")
+export AZURE_CLIENT_SECRET; AZURE_CLIENT_SECRET=$(cat "${CLUSTER_PROFILE_DIR}/client-secret")
+export INFRA_SUBSCRIPTION_ID; INFRA_SUBSCRIPTION_ID=$(cat "${CLUSTER_PROFILE_DIR}/infra-subscription-id")
+az login --service-principal -u "${AZURE_CLIENT_ID}" -p "${AZURE_CLIENT_SECRET}" --tenant "${AZURE_TENANT_ID}" --output none
+az account set --subscription "${INFRA_SUBSCRIPTION_ID}"
+
+export DEPLOY_ENV="prow"
+
+START_TIME_FALLBACK_ARGS=""
+if [[ -f "${SHARED_DIR}/write-config-timestamp-rfc3339" ]]; then
+  START_TIME_FALLBACK_ARGS="--start-time-fallback $(cat "${SHARED_DIR}/write-config-timestamp-rfc3339")"
+fi
+
+test/aro-hcp-tests gather-observability \
+  --timing-input "${SHARED_DIR}" \
+  --output "${ARTIFACT_DIR}/" \
+  --rendered-config "${SHARED_DIR}/config.yaml" \
+  --subscription-id "${INFRA_SUBSCRIPTION_ID}" \
+  --severity-threshold Sev3 \
+  ${START_TIME_FALLBACK_ARGS}
