eb18dc5f4a67451040de7004e8cc00e412b29386
Mariusz Mazur <mmazur@redhat.com>
2026-04-11 12:10:37 +0200

    Drop cluster profiles, switch to manual vault and lease management (#76075)
    * Drop releases and cluster profiles
    
    * Manually mount secrets
    
    CI Operator with cluster profiles was smart enough to point at the
    secret of the selected profile and that is how our vault is currently
    set up (split into "profile" secrets).
    Without ci operator managing this, we're syncing directly to
    'test-credentials', mounting from there and picking the correct secret
    manually.
    
    But let's make sure for each step we only mount the secrets that it can
    actually use:
    
    Steps invoked with all 4 profiles (dev/int/stg/prod):
      - aro-hcp-deprovision-expired-resource-groups
      - aro-hcp-gather-custom-link-tools
    
    Steps only invoked with int/stg/prod (3 of 4):
      - aro-hcp-test-persistent
    
    Steps only ever invoked with dev (1 of 4):
      - aro-hcp-deprovision-environment
      - aro-hcp-deprovision-kusto-role-assignments
      - aro-hcp-gather-provision-failure
      - aro-hcp-provision-environment
      - aro-hcp-provision-global-pipeline
      - aro-hcp-secrets-validation
      - aro-hcp-test-local
    
    * Add cluster profile quota replacements
    
    Same names, so the boskos config can stay the same.
    
    Jobs that had leases from cluster profiles but did not get a
    replacement, cause they only actually needed access to secrets:
    - global-pipeline-postsubmit (has its own global lease)
    - secret-validation
    - delete-expired*
    
    * make update

--- ci-operator/step-registry/aro-hcp/test/local/aro-hcp-test-local-commands.sh
+++ ci-operator/step-registry/aro-hcp/test/local/aro-hcp-test-local-commands.sh
@@ -3,6 +3,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+export CLUSTER_PROFILE_DIR="/var/run/aro-hcp-${VAULT_SECRET_PROFILE}"
+
 export AZURE_CLIENT_ID; AZURE_CLIENT_ID=$(cat "${CLUSTER_PROFILE_DIR}/client-id")
 export AZURE_TENANT_ID; AZURE_TENANT_ID=$(cat "${CLUSTER_PROFILE_DIR}/tenant")
 export AZURE_CLIENT_SECRET; AZURE_CLIENT_SECRET=$(cat "${CLUSTER_PROFILE_DIR}/client-secret")
