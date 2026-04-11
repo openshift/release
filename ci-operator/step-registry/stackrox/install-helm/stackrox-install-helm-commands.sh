d8b30edb9db4a95f5b18699737bc4f492b64d367
JoukoVirtanen <jouko@stackrox.com>
2026-04-11 09:04:39 -0700

    --set runtimeDataControl.excludeOpenshift=true

--- ci-operator/step-registry/stackrox/install-helm/stackrox-install-helm-commands.sh
+++ ci-operator/step-registry/stackrox/install-helm/stackrox-install-helm-commands.sh
@@ -245,7 +245,8 @@ function install_secured_cluster_with_helm() {
   /tmp/helm/linux-amd64/helm upgrade --install --namespace stackrox --create-namespace stackrox-secured-cluster-services "${SCRATCH}/secured-cluster-services" \
   --values "${SCRATCH}/helm-init-bundle-values.yaml" \
   --set clusterName=remote \
-  --set imagePullSecrets.allowNone=true
+  --set imagePullSecrets.allowNone=true \
+  --set runtimeDataControl.excludeOpenshift=true
 }
 
 echo '>>> Begin setup'
