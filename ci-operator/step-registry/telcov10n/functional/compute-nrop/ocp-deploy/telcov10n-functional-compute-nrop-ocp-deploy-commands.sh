e718efdf057b3a1c8a64e667ece83f0ff4b9b03f
Eran Ifrach <76165038+eifrach@users.noreply.github.com>
2026-04-14 10:41:10 +0300

    NROP: e2e testing 4.21 (#76730)
    Signed-off-by: Eran Ifrach <eifrach@redhat.com>

---
+++ ci-operator/step-registry/telcov10n/functional/compute-nrop/ocp-deploy/telcov10n-functional-compute-nrop-ocp-deploy-commands.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+set -o pipefail
+set -x
+
+ECO_CI_CD_INVENTORY_PATH="/eco-ci-cd/inventories/ocp-deployment"
+
+echo "Checking if the job should be skipped..."
+if [ -f "${SHARED_DIR}/skip.txt" ]; then
+  echo "Detected skip.txt file — skipping the job"
+  exit 0
+fi
+
+
+echo "Copy inventory files"
+mkdir -pv ${ECO_CI_CD_INVENTORY_PATH}/group_vars
+mkdir -pv ${ECO_CI_CD_INVENTORY_PATH}/host_vars
+
+for file in ${SHARED_DIR}/*; do 
+    if [[ "$file" == *"group_vars_"* || "$file" == *"host_vars_"* ]]; then
+        DEST_DIR=$( basename $file | cut -d'_' -f1,2 )
+        DEST_FILE=$( basename $file | cut -d'_' -f3 )
+        cp $file ${ECO_CI_CD_INVENTORY_PATH}/$DEST_DIR/$DEST_FILE
+    fi
+done
+
+echo "Set CLUSTER_NAME env var"
+if [[ -f "${SHARED_DIR}/cluster_name" ]]; then
+    CLUSTER_NAME=$(cat "${SHARED_DIR}/cluster_name")
+fi
+
+
+export CLUSTER_NAME=${CLUSTER_NAME}
+echo "CLUSTER_NAME=${CLUSTER_NAME}"
+
+cd /eco-ci-cd
+
+echo "Clean old clusters"
+ansible-playbook ./playbooks/compute/delete_old_clusters.yml \
+    -i ./inventories/ocp-deployment/build-inventory.py
+
+EXTRA_VARS="kubeconfig=/home/telcov10n/project/generated/${CLUSTER_NAME}/auth/kubeconfig"
+EXTRA_VARS="${EXTRA_VARS} release=${VERSION}"
+EXTRA_VARS="${EXTRA_VARS} cluster_name=${CLUSTER_NAME}"
+EXTRA_VARS="${EXTRA_VARS} ocp_version_facts_release_type=${OCP_VERSION_RELEASE_TYPE}"
+EXTRA_VARS="${EXTRA_VARS} ocp_version_release_age_max_days=${OCP_VERSION_RELEASE_AGE_MAX_DAYS}"
+EXTRA_VARS="${EXTRA_VARS} internal_registry=true"
+
+echo "Deploy SNO OCP for compute-nto testing"
+ansible-playbook ./playbooks/deploy-ocp-hybrid-multinode.yml \
+    -i ./inventories/ocp-deployment/build-inventory.py \
+    --extra-vars "${EXTRA_VARS}"
+
