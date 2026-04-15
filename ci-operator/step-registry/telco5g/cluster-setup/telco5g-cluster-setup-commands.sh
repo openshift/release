cc6b1cd38ba38f0447f770c0ebfd7c56284e4cf1
Sergey <6213510+sshnaidm@users.noreply.github.com>
2026-04-15 22:52:49 +0300

    Rename e-cluster.yml to release-cluster.yml

--- ci-operator/step-registry/telco5g/cluster-setup/telco5g-cluster-setup-commands.sh
+++ ci-operator/step-registry/telco5g/cluster-setup/telco5g-cluster-setup-commands.sh
@@ -141,7 +141,7 @@ IFS=- read -r CLUSTER_NAME CLUSTER_API_IP CLUSTER_API_PORT CLUSTER_HV_IP CLUSTER
 PLAN_NAME="${CLUSTER_NAME}_ci"
 echo "${CLUSTER_NAME}" > ${ARTIFACT_DIR}/job-cluster
 
-cat << EOF > $SHARED_DIR/e-cluster.yml
+cat << EOF > $SHARED_DIR/release-cluster.yml
 ---
 - name: Release cluster $CLUSTER_NAME
   hosts: bastion
