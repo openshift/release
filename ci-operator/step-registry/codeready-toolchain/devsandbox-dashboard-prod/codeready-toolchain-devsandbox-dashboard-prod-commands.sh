7964ebec6a3a3f687f9083cd09278cb645ebc16b
Rafaela Soares <rsoaresd@redhat.com>
2026-04-28 13:17:00 +0100

    improvement

--- ci-operator/step-registry/codeready-toolchain/devsandbox-dashboard-prod/codeready-toolchain-devsandbox-dashboard-prod-commands.sh
+++ ci-operator/step-registry/codeready-toolchain/devsandbox-dashboard-prod/codeready-toolchain-devsandbox-dashboard-prod-commands.sh
@@ -2,8 +2,6 @@
 set -euo pipefail
 
 export KUBECONF=/tmp/kubeconfig
-printf '%s' "${KUBECONFIG_E2E_TESTS}" | base64 -d > "${KUBECONF}"
+cat /usr/local/sandbox-secrets/KUBECONFIG_E2E_TESTS 2>/dev/null | base64 -d > "${KUBECONF}"
 make test-devsandbox-dashboard-in-container-prod \
-  SSO_USERNAME="${SSO_USERNAME}" \
-  SSO_PASSWORD="${SSO_PASSWORD}" \
   KUBECONFIG="${KUBECONF}"
