2385e65fa748dcafd35ae061163b94701075005b
Raviteja Sahukari <rsahukar@redhat.com>
2026-04-28 13:43:52 +0530

    [PERFSCALE-4575] Added support to override the OCP_INVENTORY_PATH while deploying the OCP Clusters
    Signed-off-by: Raviteja Sahukari <rsahukar@redhat.com>

--- ci-operator/step-registry/openshift-qe/installer/bm/deploy/openshift-qe-installer-bm-deploy-commands.sh
+++ ci-operator/step-registry/openshift-qe/installer/bm/deploy/openshift-qe-installer-bm-deploy-commands.sh
@@ -86,6 +86,14 @@ image_type: "minimal-iso"
 reset_idrac: $RESET_IDRAC
 EOF
 
+# Append ocp_inventory_override if enabled
+if [[ $OCP_INVENTORY_OVERRIDE == "true" ]]; then
+   OCP_INVENTORY_PATH=$(cat ${CLUSTER_PROFILE_DIR}/ocp_inventory_path)
+   cat <<EOF >>/tmp/all.yml
+   ocp_inventory_override: $OCP_INVENTORY_PATH
+EOF
+fi
+
 if [[ $PUBLIC_VLAN == "false" ]]; then
   echo "Private network deployment"
   sed -i "s/^enable_bond: .*/enable_bond: $BOND/" /tmp/all.yml
