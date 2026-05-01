76498c6340a0163f9246a01a8ca407619199d9fa
omer-vishlitzky <omer.vishlitzky@gmail.com>
2026-05-01 11:20:34 +0300

    MGMT-24219: add CaaS presubmit E2E jobs to osac-installer
    New step-registry components (CaaS-specific, does not touch VMaaS):
    - osac-project-caas-installer: installs OSAC with MCE+LVMS+MetalLB
    - osac-project-caas-agent-setup: creates InfraEnv, boots agent VM
    - osac-project-caas-test: runs CaaS E2E tests
    - osac-project-ofcir-caas: CaaS workflow combining the above
    
    Three presubmit jobs on osac-installer triggered by run_if_changed.

--- ci-operator/step-registry/osac-project/baremetal/test/osac-project-baremetal-test-commands.sh
+++ ci-operator/step-registry/osac-project/baremetal/test/osac-project-baremetal-test-commands.sh
@@ -6,14 +6,14 @@ set -o pipefail
 
 echo "Running OSAC E2E test: ${TEST}"
 
-timeout -s 9 60m ssh -F "${SHARED_DIR}/ssh_config" ci_machine bash -s "${TEST}" "${E2E_NAMESPACE}" "${E2E_VM_TEMPLATE}" "${OSAC_CLUSTER_TEMPLATE}" "${OSAC_TEST_INFRA_IMAGE}" <<'REMOTE_EOF'
+timeout -s 9 60m ssh -F "${SHARED_DIR}/ssh_config" ci_machine bash -s "${TEST}" "${E2E_NAMESPACE}" "${E2E_VM_TEMPLATE}" "${E2E_CLUSTER_TEMPLATE}" "${OSAC_TEST_IMAGE}" <<'REMOTE_EOF'
 set -euo pipefail
 
 TEST="$1"
 E2E_NAMESPACE="$2"
 E2E_VM_TEMPLATE="$3"
-OSAC_CLUSTER_TEMPLATE="$4"
-OSAC_TEST_INFRA_IMAGE="$5"
+E2E_CLUSTER_TEMPLATE="$4"
+OSAC_TEST_IMAGE="$5"
 
 KUBECONFIG=$(find ${KUBECONFIG} -type f -print -quit 2>/dev/null)
 [[ -z "${KUBECONFIG}" ]] && echo "ERROR: No kubeconfig found" && exit 1
@@ -28,9 +28,9 @@ podman run --authfile "${PULL_SECRET_PATH}" --rm --network=host \
   -e OSAC_VM_KUBECONFIG=/root/.kube/config \
   -e OSAC_NAMESPACE="${E2E_NAMESPACE}" \
   -e OSAC_VM_TEMPLATE="${E2E_VM_TEMPLATE}" \
-  -e OSAC_CLUSTER_TEMPLATE="${OSAC_CLUSTER_TEMPLATE}" \
+  -e OSAC_CLUSTER_TEMPLATE="${E2E_CLUSTER_TEMPLATE}" \
   -e OSAC_PULL_SECRET_PATH=/root/pull-secret \
-  "${OSAC_TEST_INFRA_IMAGE}" \
+  "${OSAC_TEST_IMAGE}" \
   make test TEST="${TEST}"
 REMOTE_EOF
 
--- ci-operator/step-registry/osac-project/caas/installer/osac-project-caas-installer-commands.sh
+++ ci-operator/step-registry/osac-project/caas/installer/osac-project-caas-installer-commands.sh
@@ -4,13 +4,9 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-echo "************ osac-installer commands ************"
-echo "--- Running with the following parameters ---"
+echo "************ osac CaaS installer ************"
 echo "E2E_NAMESPACE: ${E2E_NAMESPACE}"
 echo "E2E_KUSTOMIZE_OVERLAY: ${E2E_KUSTOMIZE_OVERLAY}"
-echo "E2E_VM_TEMPLATE: ${E2E_VM_TEMPLATE}"
-echo "OSAC_INSTALLER_IMAGE: ${OSAC_INSTALLER_IMAGE}"
-echo "-------------------------------------------"
 
 base64 -d /var/run/osac-installer-aap/license > /tmp/license.zip
 
@@ -22,26 +18,15 @@ export KUBECONFIG=\$(find \${KUBECONFIG} -type f -print -quit)
 
 oc annotate sc lvms-vg1 storageclass.kubernetes.io/is-default-class=true --overwrite
 
-echo "Waiting for OpenShift Virtualization to be ready..."
-oc wait --for=condition=Available hyperconverged/kubevirt-hyperconverged -n openshift-cnv --timeout=900s
-
-cat <<NADEOF | oc apply -f -
-apiVersion: k8s.cni.cncf.io/v1
-kind: NetworkAttachmentDefinition
-metadata:
-  name: default
-  namespace: openshift-ovn-kubernetes
-spec:
-  config: '{"cniVersion": "0.4.0", "name": "ovn-kubernetes", "type": "ovn-k8s-cni-overlay"}'
-NADEOF
-
 podman run --authfile /root/pull-secret --rm --network=host \
 -v \${KUBECONFIG}:/root/.kube/config:z \
 -v /root/pull-secret:/installer/overlays/${E2E_KUSTOMIZE_OVERLAY}/files/quay-pull-secret.json:z \
 -v /tmp/license.zip:/installer/overlays/${E2E_KUSTOMIZE_OVERLAY}/files/license.zip:z \
 -e INSTALLER_NAMESPACE=${E2E_NAMESPACE} \
 -e INSTALLER_KUSTOMIZE_OVERLAY=${E2E_KUSTOMIZE_OVERLAY} \
--e INSTALLER_VM_TEMPLATE=${E2E_VM_TEMPLATE} \
+-e MCE_SERVICE=true \
+-e STORAGE_SERVICE=true \
+-e INGRESS_SERVICE=true \
 ${OSAC_INSTALLER_IMAGE} sh /installer/scripts/setup.sh
 
 EOF
