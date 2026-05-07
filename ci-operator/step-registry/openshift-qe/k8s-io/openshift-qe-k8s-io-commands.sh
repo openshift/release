9a90983f55dfb8deb8109e67f109a725dc558b7f
Jose Castillo Lema <josecastillolema@gmail.com>
2026-05-07 15:18:29 +0200

    Shell suggestions improvements
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- ci-operator/step-registry/openshift-qe/k8s-io/openshift-qe-k8s-io-commands.sh
+++ ci-operator/step-registry/openshift-qe/k8s-io/openshift-qe-k8s-io-commands.sh
@@ -2,7 +2,6 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-set -x
 
 
 # For disconnected or otherwise unreachable environments, we want to
@@ -17,17 +16,24 @@ fi
 
 pushd /tmp
 
-ES_PASSWORD=$(cat "/secret/password")
-ES_USERNAME=$(cat "/secret/username")
+ES_PASSWORD="$(< /secret/password)"
+ES_USERNAME="$(< /secret/username)"
 
-REPO_URL="https://github.com/cloud-bulldozer/e2e-benchmarking";
-LATEST_TAG=$(curl -s "https://api.github.com/repos/cloud-bulldozer/e2e-benchmarking/releases/latest" | jq -r '.tag_name');
-TAG_OPTION="--branch $(if [ "$E2E_VERSION" == "default" ]; then echo "$LATEST_TAG"; else echo "$E2E_VERSION"; fi)";
-git clone $REPO_URL $TAG_OPTION --depth 1
+REPO_URL="https://github.com/cloud-bulldozer/e2e-benchmarking"
+LATEST_TAG="$(
+  curl --fail --silent --show-error --retry 5 --retry-delay 2 --connect-timeout 10 \
+    "https://api.github.com/repos/cloud-bulldozer/e2e-benchmarking/releases/latest" \
+  | jq -er '.tag_name'
+)"
+BRANCH="${E2E_VERSION}"
+if [[ "${BRANCH}" == "default" ]]; then
+  BRANCH="${LATEST_TAG}"
+fi
+git clone --branch "${BRANCH}" --depth 1 "${REPO_URL}"
 pushd e2e-benchmarking/workloads/k8s-io
 
 # Only store the results from the full run versus the smoke test.
-export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
+export ES_SERVER="https://${ES_USERNAME}:${ES_PASSWORD}@search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"
 
 export CONFIG=$CONFIG; export CLEANUP=$CLEANUP;
 ./run.sh
