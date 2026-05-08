1dcd460d96da12225c06887bb52efce4c2341d08
oharan2 <oharan@redhat.com>
2026-05-08 18:27:09 +0300

    MPIIT: Refactor mpiit-data-router-reporter step (#78819)
    * Refactor mpiit-data-router-reporter step. Remove old env from jobs
    
    Apply suggestion from @coderabbitai[bot]
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    Apply suggestions from code review
    
    Co-authored-by: Edoardo Tirtarahardja <54961875+etirta@users.noreply.github.com>
    
    Improve ref document
    
    Reorder env vars
    
    Provide DR docs
    
    * Update DR docs link

--- ci-operator/step-registry/mpiit/data-router-reporter/mpiit-data-router-reporter-commands.sh
+++ ci-operator/step-registry/mpiit/data-router-reporter/mpiit-data-router-reporter-commands.sh
@@ -1,59 +1,37 @@
 #!/bin/bash
-set -o nounset
-set -o errexit
-set -o pipefail
+set -euxo pipefail; shopt -s inherit_errexit
 
+# Legacy backward compatibility. TODO: To be removed once all caller are migrated.
+: "${DR__RP__CR_COMP_NAME:=${REPORTPORTAL_CMP}}"
 
-function reportToDataRouter() {
-    if [[ $REPORT_TO_DR == "true" ]]; then
-        if [[ $REPORTPORTAL_CMP == "" ]]; then
-            echo "Required variable 'REPORTPORTAL_CMP' is missing!"
-            exit 1
-        fi
-      datarouter-openshift-ci
+[ -n "${DR__RP__CR_COMP_NAME}" ]
+
+# If `OCP_VERSION` is not set, try to extract it form `JOB_NAME`.
+if [ -z "${OCP_VERSION}" ]; then
+    if [[ "${JOB_NAME}" =~ ocp-([0-9]+\.[0-9]+) ]]; then
+        OCP_VERSION="${BASH_REMATCH[1]}"
+    else
+        OCP_VERSION="unknown"
     fi
-}
+fi
 
-function extractOCPVersion() {
-    # If OCP_VERSION is not set, we don't want to fail the entire launch report process
-    # The version attribute is not essentially required, so extract it from the JOB_NAME
-    if [[ -z "${OCP_VERSION}" ]]; then
-        if [[ "${JOB_NAME}" =~ ocp-([0-9]+\.[0-9]+) ]]; then
-            export OCP_VERSION="${BASH_REMATCH[1]}"
-        else
-            export OCP_VERSION="unknown"
-        fi
-    fi
-    true
-}
+DATAROUTER_RESULTS="${SHARED_DIR}/*.xml" \
+    REPORTPORTAL_LAUNCH_NAME="${DR__RP__CR_COMP_NAME}" \
+    REPORTPORTAL_LAUNCH_ATTRIBUTES="$(
+        jq -nc \
+            --arg jobName "${JOB_NAME}" \
+            --arg buildID "${BUILD_ID}" \
+            --arg ocpVer "${OCP_VERSION}" \
+            --arg crCompName "${DR__RP__CR_COMP_NAME}" \
+            --arg fipsEnabled "${FIPS_ENABLED}" \
+            '[
+                {key: "job_name", value: $jobName},
+                {key: "build_id", value: $buildID},
+                {key: "ocp_release", value: $ocpVer},
+                {key: "ComponentReadiness_ComponentName", value: $crCompName},
+                {key: "fips_enabled", value: $fipsEnabled}
+            ]'
+    )" \
+    datarouter-openshift-ci
 
-export DATAROUTER_RESULTS="${SHARED_DIR}/*.xml"
-export REPORTPORTAL_APPLY_TFA="${APPLY_TFA}"
-export REPORTPORTAL_CMP
-export REPORTPORTAL_HOSTNAME
-export REPORTPORTAL_PROJECT="lp-interop-dr_personal"
-export REPORTPORTAL_LAUNCH_NAME="${REPORTPORTAL_CMP}"
-
-extractOCPVersion
-
-typeset version="${OCP_VERSION}"
-typeset fips="${FIPS_ENABLED}"
-
-REPORTPORTAL_LAUNCH_ATTRIBUTES="$(
-    jq -nc \
-        --arg jobName "${JOB_NAME}" \
-        --arg buildID "${BUILD_ID}" \
-        --arg ocpVer "${version}" \
-        --arg rpCompName "${REPORTPORTAL_CMP}" \
-        --arg fipsEnabled "${fips}" \
-        '[
-            {key: "job_name", value: $jobName},
-            {key: "build_id", value: $buildID},
-            {key: "ocp_release", value: $ocpVer},
-            {key: "component_name", value: $rpCompName},
-            {key: "fips_enabled", value: $fipsEnabled}
-        ]'
-)"
-export REPORTPORTAL_LAUNCH_ATTRIBUTES
-
-reportToDataRouter
+true
