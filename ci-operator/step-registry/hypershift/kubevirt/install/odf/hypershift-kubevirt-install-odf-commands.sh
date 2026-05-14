9b178c2dfc97d5b8a8cef238046adddfad579da8
shwetam2004 <muthukumarshweta@gmail.com>
2026-05-14 12:19:40 +0530

    odf nightly build and region change for conformance virt job
    Signed-off-by: shwetam2004 <muthukumarshweta@gmail.com>

--- ci-operator/step-registry/hypershift/kubevirt/install/odf/hypershift-kubevirt-install-odf-commands.sh
+++ ci-operator/step-registry/hypershift/kubevirt/install/odf/hypershift-kubevirt-install-odf-commands.sh
@@ -24,52 +24,290 @@ if oc get packagemanifest -l "catalog=${ODF_SUBSCRIPTION_SOURCE}" -n openshift-m
   ODF_OPERATOR_CHANNEL=$(oc get packagemanifest -l "catalog=${ODF_SUBSCRIPTION_SOURCE}" -n openshift-marketplace \
     -o jsonpath='{.items[?(@.metadata.name=="odf-operator")].status.channels[*].name}' | tr ' ' '\n' | sort -V | tail -1)
 else
-  echo "odf-operator package not found in ${ODF_SUBSCRIPTION_SOURCE} catalog, creating fallback catalog source"
+  echo "odf-operator package not found in ${ODF_SUBSCRIPTION_SOURCE} catalog, creating ODF 4.22 catalog source with ICSP"
 
-  CURRENT_IMAGE=$(oc get catalogsource redhat-operators -n openshift-marketplace -o jsonpath='{.spec.image}')
-  echo "Current redhat-operators catalog image: ${CURRENT_IMAGE}"
+  # Create ImageContentSourcePolicy for ODF 4.22
+  echo "Creating ImageContentSourcePolicy for ODF repositories"
+  oc create -f - <<EOF || true
+apiVersion: operator.openshift.io/v1alpha1
+kind: ImageContentSourcePolicy
+metadata:
+  name: df-repo-4.22.0
+spec:
+  repositoryDigestMirrors:
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-ceph-volsync-plugin-mover-rhel9
+    - brew.registry.redhat.io/odf4/ceph-volsync-plugin-mover-rhel9
+    source: registry.redhat.io/odf4/ceph-volsync-plugin-mover-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-ceph-volsync-plugin-operator-bundle
+    - brew.registry.redhat.io/odf4/ceph-volsync-plugin-operator-bundle
+    source: registry.redhat.io/odf4/ceph-volsync-plugin-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-ceph-volsync-plugin-rhel9-operator
+    - brew.registry.redhat.io/odf4/ceph-volsync-plugin-rhel9-operator
+    source: registry.redhat.io/odf4/ceph-volsync-plugin-rhel9-operator
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-cephcsi-operator-bundle
+    - brew.registry.redhat.io/odf4/cephcsi-operator-bundle
+    source: registry.redhat.io/odf4/cephcsi-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-cephcsi-rhel9-operator
+    - brew.registry.redhat.io/odf4/cephcsi-rhel9-operator
+    source: registry.redhat.io/odf4/cephcsi-rhel9-operator
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-cephcsi-rhel9
+    - brew.registry.redhat.io/odf4/cephcsi-rhel9
+    source: registry.redhat.io/odf4/cephcsi-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-devicefinder-rhel9
+    - brew.registry.redhat.io/odf4/devicefinder-rhel9
+    source: registry.redhat.io/odf4/devicefinder-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-mcg-core-rhel9
+    - brew.registry.redhat.io/odf4/mcg-core-rhel9
+    source: registry.redhat.io/odf4/mcg-core-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-mcg-operator-bundle
+    - brew.registry.redhat.io/odf4/mcg-operator-bundle
+    source: registry.redhat.io/odf4/mcg-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-mcg-rhel9-operator
+    - brew.registry.redhat.io/odf4/mcg-rhel9-operator
+    source: registry.redhat.io/odf4/mcg-rhel9-operator
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-ocs-client-console-rhel9
+    - brew.registry.redhat.io/odf4/ocs-client-console-rhel9
+    source: registry.redhat.io/odf4/ocs-client-console-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-ocs-client-operator-bundle
+    - brew.registry.redhat.io/odf4/ocs-client-operator-bundle
+    source: registry.redhat.io/odf4/ocs-client-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-ocs-client-rhel9-operator
+    - brew.registry.redhat.io/odf4/ocs-client-rhel9-operator
+    source: registry.redhat.io/odf4/ocs-client-rhel9-operator
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-ocs-metrics-exporter-rhel9
+    - brew.registry.redhat.io/odf4/ocs-metrics-exporter-rhel9
+    source: registry.redhat.io/odf4/ocs-metrics-exporter-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-ocs-operator-bundle
+    - brew.registry.redhat.io/odf4/ocs-operator-bundle
+    source: registry.redhat.io/odf4/ocs-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-ocs-rhel9-operator
+    - brew.registry.redhat.io/odf4/ocs-rhel9-operator
+    source: registry.redhat.io/odf4/ocs-rhel9-operator
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-ocs-tls-profiles-operator-bundle
+    - brew.registry.redhat.io/odf4/ocs-tls-profiles-operator-bundle
+    source: registry.redhat.io/odf4/ocs-tls-profiles-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-blackbox-exporter-rhel9
+    - brew.registry.redhat.io/odf4/odf-blackbox-exporter-rhel9
+    source: registry.redhat.io/odf4/odf-blackbox-exporter-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-cloudnative-pg-rhel9-operator
+    - brew.registry.redhat.io/odf4/odf-cloudnative-pg-rhel9-operator
+    source: registry.redhat.io/odf4/odf-cloudnative-pg-rhel9-operator
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-console-rhel9
+    - brew.registry.redhat.io/odf4/odf-console-rhel9
+    source: registry.redhat.io/odf4/odf-console-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-cosi-sidecar-rhel9
+    - brew.registry.redhat.io/odf4/odf-cosi-sidecar-rhel9
+    source: registry.redhat.io/odf4/odf-cosi-sidecar-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-csi-addons-operator-bundle
+    - brew.registry.redhat.io/odf4/odf-csi-addons-operator-bundle
+    source: registry.redhat.io/odf4/odf-csi-addons-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-csi-addons-rhel9-operator
+    - brew.registry.redhat.io/odf4/odf-csi-addons-rhel9-operator
+    source: registry.redhat.io/odf4/odf-csi-addons-rhel9-operator
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-csi-addons-sidecar-rhel9
+    - brew.registry.redhat.io/odf4/odf-csi-addons-sidecar-rhel9
+    source: registry.redhat.io/odf4/odf-csi-addons-sidecar-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-dependencies-operator-bundle
+    - brew.registry.redhat.io/odf4/odf-dependencies-operator-bundle
+    source: registry.redhat.io/odf4/odf-dependencies-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-drbd-rhel9
+    - brew.registry.redhat.io/odf4/odf-drbd-rhel9
+    source: registry.redhat.io/odf4/odf-drbd-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-external-snapshotter-operator-bundle
+    - brew.registry.redhat.io/odf4/odf-external-snapshotter-operator-bundle
+    source: registry.redhat.io/odf4/odf-external-snapshotter-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-external-snapshotter-rhel9-operator
+    - brew.registry.redhat.io/odf4/odf-external-snapshotter-rhel9-operator
+    source: registry.redhat.io/odf4/odf-external-snapshotter-rhel9-operator
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-external-snapshotter-sidecar-rhel9
+    - brew.registry.redhat.io/odf4/odf-external-snapshotter-sidecar-rhel9
+    source: registry.redhat.io/odf4/odf-external-snapshotter-sidecar-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-multicluster-console-rhel9
+    - brew.registry.redhat.io/odf4/odf-multicluster-console-rhel9
+    source: registry.redhat.io/odf4/odf-multicluster-console-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-multicluster-operator-bundle
+    - brew.registry.redhat.io/odf4/odf-multicluster-operator-bundle
+    source: registry.redhat.io/odf4/odf-multicluster-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-multicluster-rhel9-operator
+    - brew.registry.redhat.io/odf4/odf-multicluster-rhel9-operator
+    source: registry.redhat.io/odf4/odf-multicluster-rhel9-operator
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-must-gather-rhel9
+    - brew.registry.redhat.io/odf4/odf-must-gather-rhel9
+    source: registry.redhat.io/odf4/odf-must-gather-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-operator-bundle
+    - brew.registry.redhat.io/odf4/odf-operator-bundle
+    source: registry.redhat.io/odf4/odf-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-prometheus-operator-bundle
+    - brew.registry.redhat.io/odf4/odf-prometheus-operator-bundle
+    source: registry.redhat.io/odf4/odf-prometheus-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odf-rhel9-operator
+    - brew.registry.redhat.io/odf4/odf-rhel9-operator
+    source: registry.redhat.io/odf4/odf-rhel9-operator
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odr-cluster-operator-bundle
+    - brew.registry.redhat.io/odf4/odr-cluster-operator-bundle
+    source: registry.redhat.io/odf4/odr-cluster-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odr-hub-operator-bundle
+    - brew.registry.redhat.io/odf4/odr-hub-operator-bundle
+    source: registry.redhat.io/odf4/odr-hub-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odr-recipe-operator-bundle
+    - brew.registry.redhat.io/odf4/odr-recipe-operator-bundle
+    source: registry.redhat.io/odf4/odr-recipe-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-odr-rhel9-operator
+    - brew.registry.redhat.io/odf4/odr-rhel9-operator
+    source: registry.redhat.io/odf4/odr-rhel9-operator
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-rook-ceph-operator-bundle
+    - brew.registry.redhat.io/odf4/rook-ceph-operator-bundle
+    source: registry.redhat.io/odf4/rook-ceph-operator-bundle
+  - mirrors:
+    - quay.io/rhceph-dev/odf4-rook-ceph-rhel9-operator
+    - brew.registry.redhat.io/odf4/rook-ceph-rhel9-operator
+    source: registry.redhat.io/odf4/rook-ceph-rhel9-operator
+  - mirrors:
+    - quay.io/rhceph-dev/openshift-ose-csi-external-attacher-rhel9
+    - brew.registry.redhat.io/openshift4/ose-csi-external-attacher-rhel9
+    source: registry.redhat.io/openshift4/ose-csi-external-attacher-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/openshift-ose-csi-external-provisioner-rhel9
+    - brew.registry.redhat.io/openshift4/ose-csi-external-provisioner-rhel9
+    source: registry.redhat.io/openshift4/ose-csi-external-provisioner-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/openshift-ose-csi-external-resizer-rhel9
+    - brew.registry.redhat.io/openshift4/ose-csi-external-resizer-rhel9
+    source: registry.redhat.io/openshift4/ose-csi-external-resizer-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/openshift-ose-csi-external-snapshot-metadata-rhel9
+    - brew.registry.redhat.io/openshift4/ose-csi-external-snapshot-metadata-rhel9
+    source: registry.redhat.io/openshift4/ose-csi-external-snapshot-metadata-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/openshift-ose-csi-external-snapshotter-rhel9
+    - brew.registry.redhat.io/openshift4/ose-csi-external-snapshotter-rhel9
+    source: registry.redhat.io/openshift4/ose-csi-external-snapshotter-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/openshift-ose-csi-node-driver-registrar-rhel9
+    - brew.registry.redhat.io/openshift4/ose-csi-node-driver-registrar-rhel9
+    source: registry.redhat.io/openshift4/ose-csi-node-driver-registrar-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/openshift-ose-kube-rbac-proxy-rhel9
+    - brew.registry.redhat.io/openshift4/ose-kube-rbac-proxy-rhel9
+    source: registry.redhat.io/openshift4/ose-kube-rbac-proxy-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/openshift-ose-oauth-proxy-rhel9
+    - brew.registry.redhat.io/openshift4/ose-oauth-proxy-rhel9
+    source: registry.redhat.io/openshift4/ose-oauth-proxy-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/openshift-ose-prometheus-alertmanager-rhel9
+    - brew.registry.redhat.io/openshift4/ose-prometheus-alertmanager-rhel9
+    source: registry.redhat.io/openshift4/ose-prometheus-alertmanager-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/openshift-ose-prometheus-config-reloader-rhel9
+    - brew.registry.redhat.io/openshift4/ose-prometheus-config-reloader-rhel9
+    source: registry.redhat.io/openshift4/ose-prometheus-config-reloader-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/openshift-ose-prometheus-rhel9-operator
+    - brew.registry.redhat.io/openshift4/ose-prometheus-rhel9-operator
+    source: registry.redhat.io/openshift4/ose-prometheus-rhel9-operator
+  - mirrors:
+    - quay.io/rhceph-dev/openshift-ose-prometheus-rhel9
+    - brew.registry.redhat.io/openshift4/ose-prometheus-rhel9
+    source: registry.redhat.io/openshift4/ose-prometheus-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/rhceph-9-rhel9
+    - brew.registry.redhat.io/rhceph/rhceph-9-rhel9
+    source: registry.redhat.io/rhceph/rhceph-9-rhel9
+  - mirrors:
+    - quay.io/rhceph-dev/rhel9-postgresql-16
+    - brew.registry.redhat.io/rhel9/postgresql-16
+    source: registry.redhat.io/rhel9/postgresql-16
+EOF
 
-  CURRENT_TAG=$(echo "${CURRENT_IMAGE}" | sed 's/.*:v//')
-  CURRENT_MAJOR=$(echo "${CURRENT_TAG}" | cut -d. -f1)
-  CURRENT_MINOR=$(echo "${CURRENT_TAG}" | cut -d. -f2)
-  FALLBACK_MINOR=$((CURRENT_MINOR - 1))
-  FALLBACK_TAG="v${CURRENT_MAJOR}.${FALLBACK_MINOR}"
-  FALLBACK_IMAGE="registry.redhat.io/redhat/redhat-operator-index:${FALLBACK_TAG}"
-
-  echo "Creating fallback catalog source with image: ${FALLBACK_IMAGE}"
-  ODF_CATALOG_SOURCE="redhat-operators-fallback"
+  # Create ODF CatalogSource
+  echo "Creating ODF 4.22 CatalogSource"
   oc apply -f - <<EOCATALOG
+---
 apiVersion: operators.coreos.com/v1alpha1
 kind: CatalogSource
 metadata:
-  name: ${ODF_CATALOG_SOURCE}
+  labels:
+    ocs-operator-internal: 'true'
+  name: redhat-operators-odf
   namespace: openshift-marketplace
 spec:
-  displayName: Red Hat Operators Fallback (${FALLBACK_TAG})
-  image: ${FALLBACK_IMAGE}
+  displayName: Openshift Data Foundation
+  icon:
+    base64data: PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxOTIgMTQ1Ij48ZGVmcz48c3R5bGU+LmNscy0xe2ZpbGw6I2UwMDt9PC9zdHlsZT48L2RlZnM+PHRpdGxlPlJlZEhhdC1Mb2dvLUhhdC1Db2xvcjwvdGl0bGU+PHBhdGggZD0iTTE1Ny43Nyw2Mi42MWExNCwxNCwwLDAsMSwuMzEsMy40MmMwLDE0Ljg4LTE4LjEsMTcuNDYtMzAuNjEsMTcuNDZDNzguODMsODMuNDksNDIuNTMsNTMuMjYsNDIuNTMsNDRhNi40Myw2LjQzLDAsMCwxLC4yMi0xLjk0bC0zLjY2LDkuMDZhMTguNDUsMTguNDUsMCwwLDAtMS41MSw3LjMzYzAsMTguMTEsNDEsNDUuNDgsODcuNzQsNDUuNDgsMjAuNjksMCwzNi40My03Ljc2LDM2LjQzLTIxLjc3LDAtMS4wOCwwLTEuOTQtMS43My0xMC4xM1oiLz48cGF0aCBjbGFzcz0iY2xzLTEiIGQ9Ik0xMjcuNDcsODMuNDljMTIuNTEsMCwzMC42MS0yLjU4LDMwLjYxLTE3LjQ2YTE0LDE0LDAsMCwwLS4zMS0zLjQybC03LjQ1LTMyLjM2Yy0xLjcyLTcuMTItMy4yMy0xMC4zNS0xNS43My0xNi42QzEyNC44OSw4LjY5LDEwMy43Ni41LDk3LjUxLjUsOTEuNjkuNSw5MCw4LDgzLjA2LDhjLTYuNjgsMC0xMS42NC01LjYtMTcuODktNS42LTYsMC05LjkxLDQuMDktMTIuOTMsMTIuNSwwLDAtOC40MSwyMy43Mi05LjQ5LDI3LjE2QTYuNDMsNi40MywwLDAsMCw0Mi41Myw0NGMwLDkuMjIsMzYuMywzOS40NSw4NC45NCwzOS40NU0xNjAsNzIuMDdjMS43Myw4LjE5LDEuNzMsOS4wNSwxLjczLDEwLjEzLDAsMTQtMTUuNzQsMjEuNzctMzYuNDMsMjEuNzdDNzguNTQsMTA0LDM3LjU4LDc2LjYsMzcuNTgsNTguNDlhMTguNDUsMTguNDUsMCwwLDEsMS41MS03LjMzQzIyLjI3LDUyLC41LDU1LC41LDc0LjIyYzAsMzEuNDgsNzQuNTksNzAuMjgsMTMzLjY1LDcwLjI4LDQ1LjI4LDAsNTYuNy0yMC40OCw1Ni43LTM2LjY1LDAtMTIuNzItMTEtMjcuMTYtMzAuODMtMzUuNzgiLz48L3N2Zz4=
+    mediatype: image/svg+xml
+  image: quay.io/rhceph-dev/ocs-registry:latest-stable-4.22
+  priority: 100
   publisher: Red Hat
   sourceType: grpc
   updateStrategy:
     registryPoll:
-      interval: 10m
+      interval: 15m
 EOCATALOG
 
-  echo "Waiting for fallback catalog source to become ready"
+  # Update global pull secret to include brew.registry.redhat.io
+  echo "Updating global pull secret with brew.registry.redhat.io credentials"
+  oc patch secret pull-secret -n openshift-config --type=json -p='[{"op": "add", "path": "/data/.dockerconfigjson", "value": "fHNoYXJlZC1xZS10ZW1wLnNyYzUuNzViNGQ1OmV5SmhiR2NpT2lKU1V6VXhNaUo5LmV5SnpkV0lpT2lJNU1HRmlOVGMxTlRZME4yWTBOelUyT0RRek56ZG1NR0UwTXpZMFpUTmxaaUo5LlF6YnJsTjQ5TFRIREFKcWFPcEhuVExTRFJfclBLbF9PdjB6VTJWV21OVDRBMUNXZFA3TWR1aDR6azhRbmZUWGtSaXV0UWRIQVJRMUVYMEZpM1pEeDk0aVd2OEpROEc3Ri1Lez1NZmJvMGkzNXNkTjN5M1VxU3pBd0FuQ3h1ZUtxMGVhOUFkVGxUbktvSjk5N0llaFJzTTA5Ui1TX1JQNDJqSFN6b2VpSXEwdlZpeXlMS0lKTVVfUjdaOXB1amRNTlhyS3FNYURZN050NXdXNVVEQkZuZXJfSXN2eUQ1R0tjLXNhc3c5SHQyb0FuRDZYWGo4Y3NUSjc5SmpBam41SjhYMmVBNVN5dXpRelM2Ukp3Njl2YmVwMmlxcjJfeS1XS3l2aWVzQnp5T2c2ZzRobzlxOFlUVUY1SnpOSlU0YkZoVEhOOWdQUGhEOUFmRGpsekpxcko4SHVKeEU3MnpKVk54ZzNiV21ldmJGNzVzdXpzTEtVSURCTkZKQWFwNXloRDFsR3VLUzdnYUtjNnd1ajZVLU80a3ZrQWlaUUdTQ3VMU2hJMUVpNXRBV3E1RGFoWGJVWmdJdWZLUmcxX00wYVpqU1NSUWpNSXBGa19ZTGItR3JsMERFMTlMb29XWDAhV1BwUjhDc3VLTFVFTGVFOGNJbEpjUDNlRzJkMV9tTzYzSGJkbEo5QlNaUXh3dWpGd3hwbndReEE3TG1oVXZCdDJOU3V3LVlIVDdkeFN3X3V6YlByX0ZwWmhaQnEzY01XZHhxV0VlOWFtT0xUbnJzbjE4Zms3RWpJd0RWc3hEc1pMRlp6NjhzSlUyRFNFdmdTWEhRcjBWR3lSeHRWcTY2RkJGbEhYaXpjVHY1MXZ2Wk84T0Z5dmxfbXkzamZPUjJOTGdWb09MX3JNRWhyc3JF"}]' || true
+
+  ODF_CATALOG_SOURCE="redhat-operators-odf"
+  
+  echo "Waiting for ODF catalog source to become ready"
   for ((i=1; i <= 60; i++)); do
     STATE=$(oc get catalogsource "${ODF_CATALOG_SOURCE}" -n openshift-marketplace \
       -o jsonpath='{.status.connectionState.lastObservedState}' 2>/dev/null || true)
     if [[ "${STATE}" == "READY" ]]; then
-      echo "Fallback catalog source is ready"
+      echo "ODF catalog source is ready"
       break
     fi
     echo "Try ${i}/60: catalog source not ready yet (state: ${STATE}). Retrying in 10s"
     sleep 10
   done
 
-  echo "Waiting for odf-operator packagemanifest to appear in fallback catalog"
+  echo "Waiting for odf-operator packagemanifest to appear in ODF catalog"
   for ((i=1; i <= 60; i++)); do
     if oc get packagemanifest -l "catalog=${ODF_CATALOG_SOURCE}" -n openshift-marketplace -o name 2>/dev/null | grep -q 'odf-operator'; then
-      echo "odf-operator package found in fallback catalog"
+      echo "odf-operator package found in ODF catalog"
       break
     fi
     echo "Try ${i}/60: odf-operator not available yet. Retrying in 10s"
