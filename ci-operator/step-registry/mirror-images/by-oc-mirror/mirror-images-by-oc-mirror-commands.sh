7dbdff1536e292a70a271bb79a04dc2c9193348e
Alex Guidi <aguidi@redhat.com>
2026-05-15 15:55:19 +0200

    fixup! Download oc-mirror from mirror.openshift.com instead of using container image
    Add --fail flag to curl downloads for immediate HTTP error detection

--- ci-operator/step-registry/mirror-images/by-oc-mirror/mirror-images-by-oc-mirror-commands.sh
+++ ci-operator/step-registry/mirror-images/by-oc-mirror/mirror-images-by-oc-mirror-commands.sh
@@ -166,15 +166,15 @@ oc_mirror_download_dir=$(mktemp -d)
 pushd "${oc_mirror_download_dir}"
 echo "Downloading oc-mirror from https://mirror.openshift.com/pub/openshift-v4/${ARCH}/clients/ocp/${oc_mirror_version}/"
 # Download version-specific oc-mirror from mirror.openshift.com to match the payload version
-curl -L --retry 5 --connect-timeout 30 -o oc-mirror.tar.gz \
+curl -fL --retry 5 --connect-timeout 30 -o oc-mirror.tar.gz \
     "https://mirror.openshift.com/pub/openshift-v4/${ARCH}/clients/ocp/${oc_mirror_version}/oc-mirror.tar.gz"
 # When oc-mirror is removed from the OCP payload, replace the above curl command with this one to always use the latest version:
-# curl -L --retry 5 --connect-timeout 30 -o oc-mirror.tar.gz \
+# curl -fL --retry 5 --connect-timeout 30 -o oc-mirror.tar.gz \
 #     "https://mirror.openshift.com/pub/openshift-v4/${ARCH}/clients/ocp/latest/oc-mirror.tar.gz"
 
 # Verify the integrity of the downloaded tarball
 echo "Verifying oc-mirror.tar.gz integrity..."
-curl -L --retry 5 --connect-timeout 30 -o sha256sum.txt \
+curl -fL --retry 5 --connect-timeout 30 -o sha256sum.txt \
     "https://mirror.openshift.com/pub/openshift-v4/${ARCH}/clients/ocp/${oc_mirror_version}/sha256sum.txt"
 grep "oc-mirror.tar.gz" sha256sum.txt | sha256sum -c - || {
     echo "ERROR: oc-mirror.tar.gz checksum verification failed"
