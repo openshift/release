282d0bf9064e337fbed2d43d127ba4e85b5d0ea6
sivaram <singana.sivaram.naidu@ibm.com>
2026-05-27 16:26:01 +0530

    hypershift s390xmgmt: use forked ibmcloud-openshift-provisioning

--- ci-operator/step-registry/hypershift/mce/ibmz/create/s390xmgmt/hypershift-mce-ibmz-create-s390xmgmt-commands.sh
+++ ci-operator/step-registry/hypershift/mce/ibmz/create/s390xmgmt/hypershift-mce-ibmz-create-s390xmgmt-commands.sh
@@ -12,7 +12,7 @@ export CLUSTER_ARCH
 cat "${AGENT_IBMZ_CREDENTIALS}/abi-pull-secret" | jq -c > "$HOME/pull-secret" 
 export PULL_SECRET_FILE="$HOME/pull-secret"
 
-ssh_key_string=$(cat "${AGENT_IBMZ_CREDENTIALS}/httpd-vsi-key")
+ssh_key_string=$(cat "${AGENT_IBMZ_CREDENTIALS}/httpd-vsi-key-addon-key")
 export ssh_key_string
 tmp_ssh_key="/tmp/ssh-private-key"
 envsubst <<"EOF" >${tmp_ssh_key}
@@ -25,9 +25,9 @@ chmod 0600 ${tmp_ssh_key}
 IC_API_KEY=$(cat "${AGENT_IBMZ_CREDENTIALS}/ibmcloud-apikey")
 export IC_API_KEY
 
-# Run the clone
+# Run the clone (fork + deploy key from httpd-vsi-key-addon-key in hypershift-agent-ibmz-credentials)
 GIT_SSH_COMMAND="ssh -i $tmp_ssh_key -o IdentitiesOnly=yes -o StrictHostKeyChecking=no" \
-git clone -b image-name-fix git@github.ibm.com:OpenShift-on-Z/ibmcloud-openshift-provisioning.git
+git clone -b image-name-fix git@github.ibm.com:Singana-Sivaram-Naidu/ibmcloud-openshift-provisioning.git
 
 #Navigate to clone directory
 cd "ibmcloud-openshift-provisioning" || {
