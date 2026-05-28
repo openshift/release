290cc5548a1edf634f0ad6c84c552d83a389aa8f
oharan2 <oharan@redhat.com>
2026-05-28 17:09:40 +0300

    Remove exitstatus, update generic approach to fetch exittrap

--- ci-operator/step-registry/gitops-operator/tests/gitops-operator-tests-commands.sh
+++ ci-operator/step-registry/gitops-operator/tests/gitops-operator-tests-commands.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -euxo pipefail; shopt -s inherit_errexit
 
-typeset -i exit_code=0
-
 # Map results by setting identifier prefix in tests suites names for reporting tools
 # Merge original results into a single file and compress
 # Send modified file to shared dir for Data Router Reporter step
 if [ "${MAP_TESTS}" = "true" ]; then
     eval "$(
-        curl -fsSL \
+        typeset -a _fURL=()
+        type -t wget 1>/dev/null && _fURL=(wget -qO-) || _fURL=(curl -fsSL)
+        "${_fURL[@]}" \
             https://raw.githubusercontent.com/RedHatQE/OpenShift-LP-QE--Tools/refs/heads/main/libs/bash/ci-operator/interop/common/ExitTrap--PostProcessPrep.sh
     )"; trap '
         LP_IO__ET_PPP__NEW_TS_NAME="${DR__RP__CR_COMP_NAME}--%s" \
@@ -19,7 +19,6 @@ fi
 scripts/openshift-CI-kuttl-tests.sh
 
 make ginkgo
-./bin/ginkgo -v --trace --timeout 210m --junit-report=openshift-gitops-parallel-e2e.xml -r ./test/openshift/e2e/ginkgo/parallel || exit_code=1
+./bin/ginkgo -v --trace --timeout 210m --junit-report=openshift-gitops-parallel-e2e.xml -r ./test/openshift/e2e/ginkgo/parallel
 
 true
-exit "${exit_code}"
