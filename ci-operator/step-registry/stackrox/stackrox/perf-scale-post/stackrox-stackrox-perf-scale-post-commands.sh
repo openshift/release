30c9066ec79777b59346729cc518a49117ee668a
JoukoVirtanen <jouko@stackrox.com>
2026-06-08 11:16:09 -0700

    Not checking if script exists before running it

--- ci-operator/step-registry/stackrox/stackrox/perf-scale-post/stackrox-stackrox-perf-scale-post-commands.sh
+++ ci-operator/step-registry/stackrox/stackrox/perf-scale-post/stackrox-stackrox-perf-scale-post-commands.sh
@@ -2,10 +2,4 @@
 
 export OPENSHIFT_CI_STEP_NAME="stackrox-stackrox-perf-scale-post"
 
-if [[ -f scripts/ci/jobs/ocp-perf-scale-tests-post.sh ]]; then
-    exec scripts/ci/jobs/ocp-perf-scale-tests-post.sh
-else
-    echo "ocp-perf-scale-tests-post.sh script was not found in the target repo."
-    echo "This is expected for branches that don't have the post script yet."
-    echo "Skipping diagnostic collection."
-fi
+exec scripts/ci/jobs/ocp-perf-scale-tests-post.sh
