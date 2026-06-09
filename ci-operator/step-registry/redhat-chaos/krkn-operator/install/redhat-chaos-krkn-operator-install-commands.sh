d87d0d636d7de06f8f710511e669ed68d81a92b7
Paige Patton <prubenda@redhat.com>
2026-06-09 10:21:33 -0400

    adding install test steps

--- ci-operator/step-registry/redhat-chaos/krkn-operator/install/redhat-chaos-krkn-operator-install-commands.sh
+++ ci-operator/step-registry/redhat-chaos/krkn-operator/install/redhat-chaos-krkn-operator-install-commands.sh
@@ -5,8 +5,26 @@ set -o nounset
 set -o pipefail
 set -x
 
+
 helm install krkn-operator oci://quay.io/krkn-chaos/charts/krkn-operator \
   --version $KRKN_OPERATOR_VERSION \
   --namespace $TARGET_NAMESPACE \
   --create-namespace \
   --set acm.enabled=true
+
+echo "✓ krkn-operator installed"
+
+kubectl get managedclusters
+
+kubectl get all -n $TARGET_NAMESPACE
+
+echo ""
+echo "Waiting for krkn-operator deployment to be ready..."
+kubectl wait --for=condition=available deployment/krkn-operator-operator \
+  -n $TARGET_NAMESPACE \
+  --timeout=300s
+
+kubectl wait --for=condition=ready pod \
+  -n $TARGET_NAMESPACE \
+  -l app.kubernetes.io/component=operator \
+  --timeout=120s
