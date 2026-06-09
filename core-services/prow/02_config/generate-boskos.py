df19755f76ea17776d2b2abfc757aeab060ef07b
Rael Garcia <rael@redhat.com>
2026-06-09 21:03:25 +0200

    Add ARO-HCP dev shard2 and shard3 Boskos slot pools
    Onboard two new DEV E2E customer subscriptions (Dev-03, Dev-00) as
    aro-hcp-dev-shard2-slot and aro-hcp-dev-shard3-slot (1 slot each),
    synced from test/e2e-config/e2e-slots.yaml in Azure/ARO-HCP.

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -312,6 +312,8 @@ CONFIG = {
     # BEGIN ARO-HCP E2E SLOT TYPES
     'aro-hcp-dev-shard1-slot': {},
     'aro-hcp-dev-shard0-slot': {},
+    'aro-hcp-dev-shard2-slot': {},
+    'aro-hcp-dev-shard3-slot': {},
     # END ARO-HCP E2E SLOT TYPES
     'aro-hcp-msi-mock-cs-sp-dev': {},
     'equinix-ocp-metal-quota-slice': {
@@ -777,6 +779,10 @@ for i in range(7):
     CONFIG['aro-hcp-dev-shard1-slot']['aro-hcp-dev-shard1-slot-{i:0>2}'.format(i=i)] = 1
 for i in range(15):
     CONFIG['aro-hcp-dev-shard0-slot']['aro-hcp-dev-shard0-slot-{i:0>2}'.format(i=i)] = 1
+for i in range(1):
+    CONFIG['aro-hcp-dev-shard2-slot']['aro-hcp-dev-shard2-slot-{i:0>2}'.format(i=i)] = 1
+for i in range(1):
+    CONFIG['aro-hcp-dev-shard3-slot']['aro-hcp-dev-shard3-slot-{i:0>2}'.format(i=i)] = 1
 # END ARO-HCP E2E SLOT RESOURCES
 for i in range(20):
     CONFIG['aro-hcp-msi-mock-cs-sp-dev']['aro-hcp-msi-mock-cs-sp-dev-{}'.format(i)] = 1
