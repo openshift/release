b364db902807efb716971c5e52c77f62736b20d9
Gregory Giguashvili <85498834+ggiguash@users.noreply.github.com>
2026-06-10 09:28:42 +0300

    Switch CI Doctor jobs to the new directory structure (#80264)

--- ci-operator/step-registry/openshift/edge-tooling/microshift-ci/post/openshift-edge-tooling-microshift-ci-post-commands.sh
+++ ci-operator/step-registry/openshift/edge-tooling/microshift-ci/post/openshift-edge-tooling-microshift-ci-post-commands.sh
@@ -11,7 +11,7 @@ else
 fi
 
 download_report() {
-    local -r report_name="microshift-ci-doctor-report.html"
+    local -r report_name="report-microshift-ci-doctor.html"
     local -r report_url="${GCSWEB_JOB_URL}/artifacts/microshift-ci-doctor/openshift-edge-tooling-microshift-ci-doctor/artifacts/${report_name}"
     local -r output_file="${ARTIFACT_DIR}/0-${report_name%.html}-summary.html"
 
