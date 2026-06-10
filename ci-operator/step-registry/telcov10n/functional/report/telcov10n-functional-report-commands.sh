4527b7d25d48ddea5f17063ca142d8764fc60328
Timur Mamyshev <tmamyshe@redhat.com>
2026-06-10 13:31:43 +0300

    revert report commands.sh,  fix in separate PR

--- ci-operator/step-registry/telcov10n/functional/report/telcov10n-functional-report-commands.sh
+++ ci-operator/step-registry/telcov10n/functional/report/telcov10n-functional-report-commands.sh
@@ -32,7 +32,7 @@ fi
 export CLUSTER_NAME="${CLUSTER_NAME}"
 echo "CLUSTER_NAME=${CLUSTER_NAME}"
 
-HUB_KUBECONFIG="/home/telcov10n/project/generated/${CLUSTER_NAME}/auth/kubeconfig"
+KUBECONFIG="/home/telcov10n/project/generated/${CLUSTER_NAME}/auth/kubeconfig"
 
 echo "Remove report directories"
 rm -rf /tmp/reports /tmp/junit
@@ -79,7 +79,7 @@ fi
 echo "Uploading reports to Polarion and Report Portal"
 ansible-playbook ./playbooks/upload-report.yaml \
   -i ./inventories/cnf/switch-config.yaml \
-  --extra-vars "kubeconfig=${HUB_KUBECONFIG} \
+  --extra-vars "kubeconfig=${KUBECONFIG} \
     reporter_template_name='${REPORTER_TEMPLATE_NAME}' \
     processed_report_dir=/tmp/reports \
     junit_report_dir=/tmp/junit \
