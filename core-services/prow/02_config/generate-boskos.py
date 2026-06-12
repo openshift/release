49d69eae2b0d66e0880711e5bd40564ca985dd3f
Roi Vazquez <roivazquez@gmail.com>
2026-06-12 11:47:41 +0200

    ARO-HCP: Add slots for the new STG e2e subscription

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -314,6 +314,7 @@ CONFIG = {
     'aro-hcp-dev-shard0-slot': {},
     'aro-hcp-dev-shard2-slot': {},
     'aro-hcp-dev-shard3-slot': {},
+    'aro-hcp-stg-shard0-slot': {},
     # END ARO-HCP E2E SLOT TYPES
     'aro-hcp-msi-mock-cs-sp-dev': {},
     'equinix-ocp-metal-quota-slice': {
@@ -783,6 +784,8 @@ for i in range(1):
     CONFIG['aro-hcp-dev-shard2-slot']['aro-hcp-dev-shard2-slot-{i:0>2}'.format(i=i)] = 1
 for i in range(1):
     CONFIG['aro-hcp-dev-shard3-slot']['aro-hcp-dev-shard3-slot-{i:0>2}'.format(i=i)] = 1
+for i in range(1):
+    CONFIG['aro-hcp-stg-shard0-slot']['aro-hcp-stg-shard0-slot-{i:0>2}'.format(i=i)] = 1
 # END ARO-HCP E2E SLOT RESOURCES
 for i in range(20):
     CONFIG['aro-hcp-msi-mock-cs-sp-dev']['aro-hcp-msi-mock-cs-sp-dev-{}'.format(i)] = 1
