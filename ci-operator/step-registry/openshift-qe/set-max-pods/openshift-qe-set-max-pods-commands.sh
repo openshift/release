edbe5fb7252c6e986add98c7b94cbed47f4aaf49
Raviteja Sahukari <rsahukar@redhat.com>
2026-06-24 16:24:42 +0530

    [PERFSCALE-4575] Enable set-max-pods to support SNO Cluster
    Signed-off-by: Raviteja Sahukari <rsahukar@redhat.com>

--- ci-operator/step-registry/openshift-qe/set-max-pods/openshift-qe-set-max-pods-commands.sh
+++ ci-operator/step-registry/openshift-qe/set-max-pods/openshift-qe-set-max-pods-commands.sh
@@ -6,7 +6,18 @@ set -o pipefail
 
 MAX_PODS="${MAX_PODS:-250}"
 
-echo "Setting maxPods to ${MAX_PODS} for worker nodes..."
+if [[ $TYPE == "sno" ]]; then
+  MCP_NAME=$(oc get mcp -l pools.operator.machineconfiguration.openshift.io/master= -o jsonpath='{.items[*].metadata.name}')
+else
+  MCP_NAME=$(oc get mcp -l pools.operator.machineconfiguration.openshift.io/worker= -o jsonpath='{.items[*].metadata.name}')
+fi
+
+if [[ -z "$MCP_NAME" ]]; then
+  echo "Error: Could not find a MachineConfigPool for the cluster"
+  exit 1
+fi
+
+echo "Setting maxPods to ${MAX_PODS} for ${MCP_NAME} nodes..."
 
 cat <<EOF | oc apply -f -
 apiVersion: machineconfiguration.openshift.io/v1
@@ -16,16 +27,10 @@ metadata:
 spec:
   machineConfigPoolSelector:
     matchLabels:
-      pools.operator.machineconfiguration.openshift.io/worker: ""
+      pools.operator.machineconfiguration.openshift.io/${MCP_NAME}: ""
   kubeletConfig:
     maxPods: ${MAX_PODS}
 EOF
-
-MCP_NAME=$(oc get mcp -l pools.operator.machineconfiguration.openshift.io/worker= -o jsonpath='{.items[*].metadata.name}')
-if [[ -z "$MCP_NAME" ]]; then
-  echo "Error: Could not find a MachineConfigPool with the 'worker' role."
-  exit 1
-fi
 
 echo "Waiting for cluster to stabilize after applying KubeletConfig..."
 oc adm wait-for-stable-cluster
