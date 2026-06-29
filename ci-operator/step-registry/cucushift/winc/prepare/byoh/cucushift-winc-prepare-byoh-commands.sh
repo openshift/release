e5a2b1601d0faab4489826679a3b28420131d3cd
rrasouli <rrasouli@redhat.com>
2026-06-29 16:46:30 +0300

    WINC-1926: Implement Windows Server 2022/2025 dual-version CI test matrix
    Alternate Server 2022 and 2025 between debug/presubmit and periodic jobs
    across all platforms for main, release-4.23, and release-5.0. Add new
    vSphere proxy and disconnected debug jobs. Remove Server 2019 references
    from step-registry scripts and documentation.

--- ci-operator/step-registry/cucushift/winc/prepare/byoh/cucushift-winc-prepare-byoh-commands.sh
+++ ci-operator/step-registry/cucushift/winc/prepare/byoh/cucushift-winc-prepare-byoh-commands.sh
@@ -34,9 +34,8 @@ function setup_image_mirroring()
 
   # Create ImageTagMirrorSet to redirect Windows images to mirror
   # Includes PowerShell containers and CSI driver images for storage tests
-  # PowerShell: Server 2019 (1809), Server 2022 (ltsc2022)
+  # PowerShell: Server 2022 (ltsc2022), Server 2025 (ltsc2022 until Microsoft publishes ltsc2025)
   # CSI: Azure File and vSphere drivers for OCP-66352
-  # TODO: Remove Server 2019 support after AMI/image upgrades to Server 2022
   cat <<EOF | oc create -f -
 apiVersion: config.openshift.io/v1
 kind: ImageTagMirrorSet
@@ -245,10 +244,6 @@ fi
 os_version=$(oc get nodes -l 'kubernetes.io/os=windows' -o=jsonpath="{.items[0].status.nodeInfo.osImage}")
 
 windows_container_image="mcr.microsoft.com/powershell:lts-nanoserver-ltsc2022"
-if [[ "$os_version" == *"2019"* ]]
-then
-    windows_container_image="mcr.microsoft.com/powershell:lts-nanoserver-1809"
-fi
 
 echo "Windows OS version: ${os_version}"
 echo "Windows OS image ID: ${windows_os_image_id}"
