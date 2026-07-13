95f360e3b58ce68bc18d62ba7b069af129bf5c3c
JoukoVirtanen <jouko@stackrox.com>
2026-07-13 14:47:25 -0700

    X-Smart-Squash: Squashed 29 commits:
    55c07a3781f First attempt
    9ad00e07abf Using public berserker images
    f9f0693cdbf Using the kube-burner-ocp binary with berserker load
    1492d4b450a Reduced tests duration
    9cc97d0fe5c Updated OWNERS
    1b97aa596d1 make registry-metadata
    c7cc0b000f0 e2e-benchmarking path should be correct
    8f9a72fe6c3 Not running other tests
    9ac28fbaeb7 Temporarily removing pprof
    06048d04ef6 Add PPROF=false workaround for custom e2e-benchmarking fork
    359d9c9ff56 Disabling garbage collection at the end of berserker runs
    316fd16e12f Removed stackrox-stackrox-jv-run-perf-scale-using-comment__perf-scale.yaml. Reduced nodes to 3 from 24
    503ba6822d1 Reduced PODS_PER_NODE to 50
    d452056e8b3 Using hard coded image 4.11.x-688-g405c8cf703
    7a7cd278fbc Fixed formatting
    87d41a5a10e Added ci-operator/jobs/openshift/rdma-cni/openshift-rdma-cni-release-5.1-presubmits.yaml
    bcd2d0cbba0 Reduced number of berserker replicas
    d06bb8bbb7b Swithed to using stackrox-perfscale-berserker-test chain
    e61834757c0 Removed unused CD_V2_EXTRA_FLAGS and PODS_PER_NODE
    4371771ec4b Capturing pod statuses
    dc5a7518b2b Pod capture should be properly cleaned up
    7449c447a92 Added grace period
    89f261ee874 Added --qps=60 --burst=60
    a8624c080ae Separate job for berserker
    7d35918f956 make update
    be0174a9165 Updated ACS image
    c2535fdd9f3 Setting MAIN_IMAGE_TAG
    80ee4b1a44b make update to fix things after rebase
    ede31481cf0 For now not waiting for scanner v4 readiness

---
+++ ci-operator/step-registry/openshift-qe/berserker-load/openshift-qe-berserker-load-commands.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+cat /etc/os-release
+
+# For disconnected or otherwise unreachable environments, we want to
+# have steps use an HTTP(S) proxy to reach the API server. This proxy
+# configuration file should export HTTP_PROXY, HTTPS_PROXY, and NO_PROXY
+# environment variables, as well as their lowercase equivalents (note
+# that libcurl doesn't recognize the uppercase variables).
+if test -f "${SHARED_DIR}/proxy-conf.sh"; then
+  # shellcheck disable=SC1090
+  source "${SHARED_DIR}/proxy-conf.sh"
+fi
+
+oc config view
+oc projects
+pushd /tmp
+
+UUID=$(uuidgen)
+ES_SECRETS_PATH=${ES_SECRETS_PATH:-/secret}
+
+ES_HOST=${ES_HOST:-"search-ocp-qe-perf-scale-test-elk-hcm7wtsqpxy7xogbu72bor4uve.us-east-1.es.amazonaws.com"}
+ES_PASSWORD=$(cat "${ES_SECRETS_PATH}/password")
+ES_USERNAME=$(cat "${ES_SECRETS_PATH}/username")
+if [ -e "${ES_SECRETS_PATH}/host" ]; then
+    ES_HOST=$(cat "${ES_SECRETS_PATH}/host")
+fi
+
+#Support Libvirt Hypershift Cluster
+cluster_infra=$(oc get  infrastructure cluster -ojsonpath='{.status.platformStatus.type}')
+hypershift_pods=$(! oc -n hypershift get pods| grep operator >/dev/null ||oc -n hypershift get pods| grep operator |wc -l)
+if [[ $cluster_infra == "BareMetal" && $hypershift_pods -ge 1 ]];then
+  echo "Executing berserker-load in hypershift cluster"
+  echo "Configure KUBECONFIG for hosted cluster and execute kube-burner in it"
+  export KUBECONFIG=$SHARED_DIR/nested_kubeconfig
+fi
+
+# Management Kubeconfig for ROSA-HCP
+# Set this variable only for HCP clusters on AWS
+CONTROL_PLANE_TOPOLOGY=$(oc get infrastructure cluster -o jsonpath='{.status.controlPlaneTopology}')
+if [[ ${CONTROL_PLANE_TOPOLOGY} == "External" && $cluster_infra == "AWS" ]]; then
+    if [[ -f "${SHARED_DIR}/hs-mc.kubeconfig" ]]; then
+        # Check if the cluster is accessible from prow environment,
+        # Set this variable only if accessible
+        MC_CLUSTER_INFRA=$(oc --kubeconfig="${SHARED_DIR}/hs-mc.kubeconfig" get  infrastructure cluster -ojsonpath='{.status.platformStatus.type}')
+        if [[ $MC_CLUSTER_INFRA == "AWS" ]]; then
+            export MC_KUBECONFIG="${SHARED_DIR}/hs-mc.kubeconfig"
+            export ES_INDEX=ripsaw-kube-burner
+        fi
+    fi
+fi
+
+REPO_URL="${E2E_REPO:-https://github.com/cloud-bulldozer/e2e-benchmarking}";
+if [ "$E2E_VERSION" == "default" ]; then
+  LATEST_TAG=$(curl -s "https://api.github.com/repos/cloud-bulldozer/e2e-benchmarking/releases/latest" | jq -r '.tag_name');
+  TAG_OPTION="--branch ${LATEST_TAG}"
+else
+  TAG_OPTION="--branch ${E2E_VERSION}"
+fi
+git clone $REPO_URL $TAG_OPTION --depth 1 e2e-benchmarking
+pushd e2e-benchmarking/workloads/kube-burner-ocp-wrapper
+
+export PPROF=false  # Workaround for custom e2e-benchmarking fork that still references PPROF
+export WORKLOAD=berserker-load
+EXTRA_FLAGS="${BERSERKER_EXTRA_FLAGS} ${KB_FLAGS} --gc=${BERSERKER_GC} --profile-type=${PROFILE_TYPE}"
+EXTRA_FLAGS+=" --job-iterations=${JOB_ITERATIONS}"
+EXTRA_FLAGS+=" --job-pause=${JOB_PAUSE}"
+EXTRA_FLAGS+=" --process-load-replicas=${PROCESS_LOAD_REPLICAS}"
+EXTRA_FLAGS+=" --endpoint-load-replicas=${ENDPOINT_LOAD_REPLICAS}"
+EXTRA_FLAGS+=" --connection-load-replicas=${CONNECTION_LOAD_REPLICAS}"
+EXTRA_FLAGS+=" --churn-duration=${CHURN_DURATION}"
+EXTRA_FLAGS+=" --churn-delay=${CHURN_DELAY}"
+EXTRA_FLAGS+=" --churn-percent=${CHURN_PERCENT}"
+EXTRA_FLAGS+=" --process-load-image=${PROCESS_LOAD_IMAGE}"
+EXTRA_FLAGS+=" --endpoint-load-image=${ENDPOINT_LOAD_IMAGE}"
+EXTRA_FLAGS+=" --connection-load-image=${CONNECTION_LOAD_IMAGE}"
+
+export ES_SERVER="https://$ES_USERNAME:$ES_PASSWORD@$ES_HOST"
+
+export EXTRA_FLAGS UUID
+
+# Start periodic pod state capture in background
+POD_CAPTURE_INTERVAL=${POD_CAPTURE_INTERVAL:-60}
+if [[ "${POD_CAPTURE_ENABLED:-true}" == "true" ]]; then
+  POD_STATE_DIR="${ARTIFACT_DIR}/pod-states"
+  mkdir -p "${POD_STATE_DIR}"
+  (
+    while true; do
+      TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+      oc get pods --all-namespaces -o wide > "${POD_STATE_DIR}/pods-${TIMESTAMP}.txt" 2>&1 || true
+      sleep ${POD_CAPTURE_INTERVAL}
+    done
+  ) &
+  POD_CAPTURE_PID=$!
+  echo "Started pod state capture (PID: ${POD_CAPTURE_PID}, interval: ${POD_CAPTURE_INTERVAL}s)"
+fi
+
+cleanup_pod_capture() {
+  if [[ -n "${POD_CAPTURE_PID:-}" ]]; then
+    kill ${POD_CAPTURE_PID} 2>/dev/null || true
+    echo "Stopped pod state capture"
+  fi
+}
+trap cleanup_pod_capture EXIT
+
+./run.sh
+
+METRICS_FOLDER="collected-metrics-${UUID}"
+if [[ -f ${METRICS_FOLDER}/jobSummary.json ]]; then
+  cp -r ${METRICS_FOLDER} "${ARTIFACT_DIR}/"
+  if [[ ${JOB_NAME} == *openshift-eng-ocp-qe-perfscale-ci* ]] && [[ ${JOB_TYPE} == "periodic" ]]; then
+    set +e
+    OCP_PERF_DASH_HOST=$(cat ${ES_SECRETS_PATH}/ocp-perf-dash-address)
+    OCP_PERF_DASH_DIR="/usr/share/ocp-perf-dash/${JOB_NAME}/${WORKLOAD}/${UUID}"
+    METRICS="${METRICS_FOLDER}/*QuantilesMeasurement*.json ${METRICS_FOLDER}/jobSummary.json"
+    SSH_ARGS="-i ${ES_SECRETS_PATH}/ocp-perf-dash-id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+    ssh ${SSH_ARGS} ${OCP_PERF_DASH_HOST} "mkdir -p ${OCP_PERF_DASH_DIR}"
+    scp ${SSH_ARGS} ${METRICS} ${OCP_PERF_DASH_HOST}:${OCP_PERF_DASH_DIR}
+    set -e
+  fi
+fi
