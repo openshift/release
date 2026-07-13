97bd85cc722480336dc8e75eb409fa8bee0b04f0
Mark Hamzy <hamzy@us.ibm.com>
2026-07-13 09:07:30 -0500

    PowerVC: Use version 2.4.6
    Use version 2.4.6 of the ocp-ipi-powervc tools

--- ci-operator/step-registry/ipi/install/powervc/install/ipi-install-powervc-install-commands.sh
+++ ci-operator/step-registry/ipi/install/powervc/install/ipi-install-powervc-install-commands.sh
@@ -22,7 +22,7 @@ set -o errtrace
 #   DEBUG - Enable debug logging (default: false)
 
 # Global constants
-readonly POWERVC_TOOL_VERSION="v2.4.5"
+readonly POWERVC_TOOL_VERSION="v2.4.6"
 readonly YQ_VERSION="v4.53.2"
 readonly IBMCLOUD_VERSION="2.45.0"
 
