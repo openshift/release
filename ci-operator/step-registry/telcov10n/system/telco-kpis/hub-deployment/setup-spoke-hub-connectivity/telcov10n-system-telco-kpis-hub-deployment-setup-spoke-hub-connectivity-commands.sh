a472099a09c7cb7b68492924988af14fab263704
Carlos Cardenosa <ccardeno@redhat.com>
2026-07-16 12:29:39 +0200

    Add Bastion and Hub deployment support
    Signed-off-by: Carlos Cardenosa <ccardeno@redhat.com>

--- ci-operator/step-registry/telcov10n/system/telco-kpis/hub-deployment/setup-spoke-hub-connectivity/telcov10n-system-telco-kpis-hub-deployment-setup-spoke-hub-connectivity-commands.sh
+++ ci-operator/step-registry/telcov10n/system/telco-kpis/hub-deployment/setup-spoke-hub-connectivity/telcov10n-system-telco-kpis-hub-deployment-setup-spoke-hub-connectivity-commands.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+source "${SHARED_DIR}/telco-kpis-common-functions.sh"
+
+export_env_vars_from_json 'setup_spoke_hub_connectivity' "${INFRA_SETTINGS:-}" "${INFRA_SETTINGS_DEFAULTS:-}"
+
 # TODO: Implement spoke-hub connectivity setup using Ansible playbook
 # Expected playbook: repos/eco-ci-cd/playbooks/telco-kpis/setup-spoke-hub-connectivity.yml
 #
