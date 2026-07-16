86d94b0ad3f5a1db01805040abca78ad90e286f3
Carlos Cardenosa <ccardeno@redhat.com>
2026-07-16 12:29:39 +0200

    Add Bastion and Hub deployment support
    Signed-off-by: Carlos Cardenosa <ccardeno@redhat.com>

--- ci-operator/step-registry/telcov10n/system/telco-kpis/hub-deployment/install-hub-operators/telcov10n-system-telco-kpis-hub-deployment-install-hub-operators-commands.sh
+++ ci-operator/step-registry/telcov10n/system/telco-kpis/hub-deployment/install-hub-operators/telcov10n-system-telco-kpis-hub-deployment-install-hub-operators-commands.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+source "${SHARED_DIR}/telco-kpis-common-functions.sh"
+
+export_env_vars_from_json 'install_hub_operators' "${INFRA_SETTINGS:-}" "${INFRA_SETTINGS_DEFAULTS:-}"
+
 # TODO: Implement hub operator installation using Ansible playbook
 # Expected playbook: repos/eco-ci-cd/playbooks/telco-kpis/install-hub-operators.yml
 #
