9fedcf83e60e4a7437126266bd0bf462f48ca633
Raul Sevilla <rsevilla@redhat.com>
2026-07-16 17:05:38 +0200

    Migrate config files to ocp-perfscale
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- hack/validate-release-jobs.py
+++ hack/validate-release-jobs.py
@@ -19,7 +19,7 @@ job_definitions_paths = [
     'ci-operator/jobs/openshift/microshift',
     'ci-operator/jobs/openshift/cluster-control-plane-machine-set-operator',
     'ci-operator/jobs/openshift/osde2e',
-    'ci-operator/jobs/openshift-eng/ocp-qe-perfscale-ci',
+    'ci-operator/jobs/openshift-eng/ocp-perfscale',
     'ci-operator/jobs/openshift-kni/eco-ci-cd'
 ]
 
