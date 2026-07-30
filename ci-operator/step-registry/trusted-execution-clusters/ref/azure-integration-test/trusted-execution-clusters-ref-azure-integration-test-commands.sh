58f464484b5b32985a1c1fd393fea42a2f9f9839
Jakob Naucke <jnaucke@redhat.com>
2026-07-30 12:00:50 +0200

    trusted-execution-clusters: Enable full suite
    Following up on #79393. Debug first.
    
    Signed-off-by: Jakob Naucke <jnaucke@redhat.com>

--- ci-operator/step-registry/trusted-execution-clusters/ref/azure-integration-test/trusted-execution-clusters-ref-azure-integration-test-commands.sh
+++ ci-operator/step-registry/trusted-execution-clusters/ref/azure-integration-test/trusted-execution-clusters-ref-azure-integration-test-commands.sh
@@ -16,7 +16,8 @@ export AZURE_SUBSCRIPTION_ID TEST_IMAGE TEST_NAMESPACE_PREFIX
 
 export VIRT_PROVIDER=azure
 export PLATFORM=openshift
-export TEST_TIMEOUT_MULTIPLIER=2
+# Lowest timeout is 1 minute, allow for one read timeout (5 minutes) retry
+export TEST_TIMEOUT_MULTIPLIER=6
 
 az login --service-principal \
   -u "$(cat /tmp/secrets/azure/client-id)" \
@@ -25,11 +26,20 @@ az login --service-principal \
 
 eval "$(ssh-agent -s)"
 
+# git remote add test https://github.com/Jakob-Naucke/trusted-cluster-operator
+# git fetch test
+# git switch get-cached-ci
+
+# export REGISTRY=quay.io/jnaucke
+# export TAG=20260819-85dcabf
+export INTEGRATION_TEST_THREADS=2
+
 echo "[INFO] Install cert-manager"
 CRT_MGR_VERSION=$(go list -m -f '{{.Version}}' github.com/cert-manager/cert-manager)
 oc apply -f "https://github.com/cert-manager/cert-manager/releases/download/${CRT_MGR_VERSION}/cert-manager.yaml"
 
-# TODO stabilize tests enough to be able to add more
-echo "[INFO] Running basic attestation test..."
-sed -i '/^attestation-tests:/{n;s/$/ test_attestation/}' Makefile
-make attestation-tests
+echo "[INFO] Running integration tests until failure..."
+while make integration-tests
+do :
+done
+exit 1
