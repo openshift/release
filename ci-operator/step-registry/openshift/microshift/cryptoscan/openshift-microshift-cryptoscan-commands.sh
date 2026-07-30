c917ae39b74932417d56e12aa2ff213ca34fbc7f
fracappa <fcappa@redhat.com>
2026-07-30 12:15:26 +0200

    fix: add new workflow for 5.0+ releases to run rebase with cryptoscan

--- ci-operator/step-registry/openshift/microshift/cryptoscan/openshift-microshift-cryptoscan-commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-cp /secrets/import-secret/.dockerconfigjson ${HOME}/.pull-secret.json
-
-export CRYPTO_SCAN=true
-cd /go/src/github.com/openshift/microshift/
-./scripts/auto-rebase/rebase_job_entrypoint.sh
