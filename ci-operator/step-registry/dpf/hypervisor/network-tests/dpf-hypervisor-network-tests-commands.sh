7bf25fbaeffc4b62eb799a5bdcdace96e5e36a05
Jose Castillo Lema <josecastillolema@gmail.com>
2026-07-31 18:28:23 +0200

    Fix bash warnings
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
        gitleaks.version: 8.30.0
        gitleaks.check-secrets: ENABLED

--- ci-operator/step-registry/dpf/hypervisor/network-tests/dpf-hypervisor-network-tests-commands.sh
+++ ci-operator/step-registry/dpf/hypervisor/network-tests/dpf-hypervisor-network-tests-commands.sh
@@ -6,9 +6,11 @@ echo "=== Run DPF Kubernetes Traffic Flow Tests ==="
 echo "Verifying cluster access..."
 oc get nodes
 
-export TFT_SERVER_NODE=$(oc get nodes --no-headers | grep worker-dpu | awk 'NR==1 {print $1}')
+TFT_SERVER_NODE=$(oc get nodes --no-headers | grep worker-dpu | awk 'NR==1 {print $1}')
+export TFT_SERVER_NODE
 echo "TFT_SERVER_NODE: ${TFT_SERVER_NODE}"
-export TFT_CLIENT_NODE=$(oc get nodes --no-headers | grep worker-dpu | awk 'NR==2 {print $1}')
+TFT_CLIENT_NODE=$(oc get nodes --no-headers | grep worker-dpu | awk 'NR==2 {print $1}')
+export TFT_CLIENT_NODE
 echo "TFT_CLIENT_NODE: ${TFT_CLIENT_NODE}"
 
 make run-traffic-flow-tests
