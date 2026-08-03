845b7b71135dc550a2f52d061a0857b93ef697ea
Paige Patton <64206430+paigerube14@users.noreply.github.com>
2026-08-03 14:33:58 -0400

    Archive pdf (#82757)
    * adding saving artifact
    
    * merge
    
    * adding tmp report path

--- ci-operator/step-registry/redhat-chaos/pod-scenarios/etcd-disruption/redhat-chaos-pod-scenarios-etcd-disruption-commands.sh
+++ ci-operator/step-registry/redhat-chaos/pod-scenarios/etcd-disruption/redhat-chaos-pod-scenarios-etcd-disruption-commands.sh
@@ -70,5 +70,10 @@ if [[ $TELEMETRY_EVENTS_BACKUP == "True" ]]; then
     cp /tmp/events.json ${ARTIFACT_DIR}/events.json
 fi
 
+
+if [[ -f /tmp/report.out.pdf ]]; then
+  cp /tmp/report.out.pdf ${ARTIFACT_DIR}/kraken.report.pdf
+fi
+
 echo "Return code: $rc"
 exit $rc
