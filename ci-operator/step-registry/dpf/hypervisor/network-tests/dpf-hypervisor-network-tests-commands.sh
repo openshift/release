e675975a9bef481113a099109db457f7afa4150c
Jose Castillo Lema <josecastillolema@gmail.com>
2026-08-04 10:12:00 +0200

    Set TFT_KUBECONFIG
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
        gitleaks.version: 8.30.0
        gitleaks.check-secrets: ENABLED

--- ci-operator/step-registry/dpf/hypervisor/network-tests/dpf-hypervisor-network-tests-commands.sh
+++ ci-operator/step-registry/dpf/hypervisor/network-tests/dpf-hypervisor-network-tests-commands.sh
@@ -8,6 +8,7 @@ echo "=== Run DPF Kubernetes Traffic Flow Tests ==="
 echo "Verifying cluster access..."
 oc get nodes
 
+export TFT_KUBECONFIG="${SHARED_DIR}/kubeconfig"
 TFT_SERVER_NODE=$(oc get nodes --no-headers | grep worker-dpu | awk 'NR==1 {print $1}')
 export TFT_SERVER_NODE
 echo "TFT_SERVER_NODE: ${TFT_SERVER_NODE}"
