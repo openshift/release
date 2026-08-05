cb7decc68c816f22662c06d18664d8e65012f108
Gerd Oberlechner <goberlec@redhat.com>
2026-08-05 17:40:44 +0200

    ARO-HCP: Add INT westus3 e2e periodic job (#82884)
    * Add INT westus3 e2e periodic job
    
    Add integration-e2e-parallel-westus3 test variant targeting
    westus3 region for INT environment e2e testing.
    
    * boskos config for aro-hcp int westus3
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>
    
    ---------
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- core-services/prow/02_config/generate-boskos.py
+++ core-services/prow/02_config/generate-boskos.py
@@ -331,6 +331,7 @@ CONFIG = {
     'aro-hcp-dev-shard3-slot': {},
     'aro-hcp-dev-hypershift-westus3-slot': {},
     'aro-hcp-int-shard0-slot': {},
+    'aro-hcp-int-westus3-shard0-slot': {},
     'aro-hcp-prod-shard0-slot': {},
     'aro-hcp-prod-shard1-slot': {},
     'aro-hcp-prod-testtenant-slot': {},
@@ -822,6 +823,8 @@ for i in range(1):
     CONFIG['aro-hcp-dev-hypershift-westus3-slot']['aro-hcp-dev-hypershift-westus3-slot-{i:0>2}'.format(i=i)] = 1
 for i in range(1):
     CONFIG['aro-hcp-int-shard0-slot']['aro-hcp-int-shard0-slot-{i:0>2}'.format(i=i)] = 1
+for i in range(1):
+    CONFIG['aro-hcp-int-westus3-shard0-slot']['aro-hcp-int-westus3-shard0-slot-{i:0>2}'.format(i=i)] = 1
 for i in range(3):
     CONFIG['aro-hcp-prod-shard0-slot']['aro-hcp-prod-shard0-slot-{i:0>2}'.format(i=i)] = 1
 for i in range(3):
