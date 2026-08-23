ff6a77808a95d76c4bd16225bfac2d22d82863ea
oharan2 <oharan@redhat.com>
2026-08-23 22:09:13 +0300

    Fix artifact collection of PDF generating steps

--- ci-operator/step-registry/redhat-chaos/power-outage/redhat-chaos-power-outage-commands.sh
+++ ci-operator/step-registry/redhat-chaos/power-outage/redhat-chaos-power-outage-commands.sh
@@ -84,15 +84,22 @@ export TELEMETRY_PASSWORD=$telemetry_password
 
 export TIMEOUT=$POWER_OUTAGE_TIMEOUT
 
+collect_artifacts() {
+  local rc=$?
+  set +o errexit
+  # Finished running power outages
+  if [[ "${TELEMETRY_EVENTS_BACKUP:-}" == "True" && -f /tmp/events.json ]]; then
+    cp /tmp/events.json "${ARTIFACT_DIR}/events.json"
+  fi
+  if [[ -f /tmp/report.out.pdf ]]; then
+    cp /tmp/report.out.pdf "${ARTIFACT_DIR}/kraken.report.pdf"
+  fi
+  exit "$rc"
+}
+
+trap collect_artifacts EXIT
+
+set -euxo pipefail; shopt -s inherit_errexit
+
 ./power-outage/prow_run.sh
-rc=$?
-if [[ $TELEMETRY_EVENTS_BACKUP == "True" ]]; then
-    cp /tmp/events.json ${ARTIFACT_DIR}/events.json
-fi
 
-
-if [[ -f /tmp/report.out.pdf ]]; then
-  cp /tmp/report.out.pdf ${ARTIFACT_DIR}/kraken.report.pdf
-fi
-echo "Finished running power outages"
-echo "Return code: $rc"
