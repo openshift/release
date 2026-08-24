d624756e4c12844c7acdd1f02ba92f0d06bf71e6
Ori Haran <oharan@redhat.com>
2026-08-24 17:18:37 +0300

    Fix artifact collection of PDF generating steps (#83861)

--- ci-operator/step-registry/redhat-chaos/container-scenarios/etcd-hangup/redhat-chaos-container-scenarios-etcd-hangup-commands.sh
+++ ci-operator/step-registry/redhat-chaos/container-scenarios/etcd-hangup/redhat-chaos-container-scenarios-etcd-hangup-commands.sh
@@ -40,15 +40,23 @@ set -o nounset
 set -o pipefail
 set -x
 
+collect_artifacts() {
+  local rc=$?
+
+  set +o errexit
+  # Finished running container scenarios
+  if [[ "${TELEMETRY_EVENTS_BACKUP:-}" == "True" && -f /tmp/events.json ]]; then
+    cp /tmp/events.json "${ARTIFACT_DIR}/events.json"
+  fi
+  if [[ -f /tmp/report.out.pdf ]]; then
+    cp /tmp/report.out.pdf "${ARTIFACT_DIR}/kraken.report.pdf"
+  fi
+  exit "$rc"
+}
+
+trap collect_artifacts EXIT
+
+set -euxo pipefail; shopt -s inherit_errexit
+
 ./container-scenarios/prow_run.sh
-rc=$?
-if [[ $TELEMETRY_EVENTS_BACKUP == "True" ]]; then
-    cp /tmp/events.json ${ARTIFACT_DIR}/events.json
-fi
 
-if [[ -f /tmp/report.out.pdf ]]; then
-  cp /tmp/report.out.pdf ${ARTIFACT_DIR}/kraken.report.pdf
-fi
-
-echo "Finished running container scenarios"
-echo "Return code: $rc"
