1e76cb3b3eca1f07539922d8ca897fec4e92cc42
davdhacs <105243888+davdhacs@users.noreply.github.com>
2026-08-24 18:12:35 -0600

    get tag

--- ci-operator/config/stackrox/stackrox/config.py
+++ ci-operator/config/stackrox/stackrox/config.py
@@ -776,13 +776,13 @@ def check_configs(data):
         #   allow -stable, not -latest, for master
         #   no -stable or -latest tags for release branches
         # > latest is OK for in-flight PRs, but the check should still fail, to prevent merging such PR
+        tag = config.build_root_tag
         if config.branch == 'master':
             if 'latest' in tag:
                 check_error(f"{config.short_filename}: master branch '{config.branch}' "
                             f"must not use 'latest' build root tag '{tag}'. "
                             f"Pin to 'stable' or a specific version (e.g. stackrox-ui-test-0.5.7).")
         if config.branch.startswith('release-'):
-            tag = config.build_root_tag
             if not tag:
                 break
             if 'latest' in tag:
