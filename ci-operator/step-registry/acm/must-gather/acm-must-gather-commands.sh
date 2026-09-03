7024669b9f0faad3068a74749c29e1193109b751
shakyav <vshakya@redhat.com>
2026-09-03 00:16:55 -0400

    ad the fix for unsigned image usage

--- ci-operator/step-registry/acm/must-gather/acm-must-gather-commands.sh
+++ ci-operator/step-registry/acm/must-gather/acm-must-gather-commands.sh
@@ -8,5 +8,10 @@ export KUBECONFIG=${SHARED_DIR}/kubeconfig
 # Get ACM Version x.y instead of x.y.z
 ACM_VERSION=`echo "$(oc get csv -n ${ACM_NAMESPACE} -l operators.coreos.com/advanced-cluster-management.${ACM_NAMESPACE}= -o=jsonpath='{.items[].spec.version}')" | cut -d'.' -f1,2`
 
+if [[ -z "${ACM_VERSION}" ]]; then
+  echo "ACM CSV not found in namespace ${ACM_NAMESPACE}; skipping must-gather"
+  exit 0
+fi
+
 # ACM must-gather
 oc adm must-gather --image=registry.redhat.io/rhacm2/acm-must-gather-rhel9:v"$ACM_VERSION" --dest-dir="${ARTIFACT_DIR}"
