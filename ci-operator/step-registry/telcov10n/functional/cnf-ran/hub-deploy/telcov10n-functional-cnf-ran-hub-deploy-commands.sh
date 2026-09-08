0773fb18957cbc012fd5029496a31b59150d9e78
Shai Oren <sobarzan@redhat.com>
2026-09-08 10:51:35 +0300

    Add support for multistage param release age

--- ci-operator/step-registry/telcov10n/functional/cnf-ran/hub-deploy/telcov10n-functional-cnf-ran-hub-deploy-commands.sh
+++ ci-operator/step-registry/telcov10n/functional/cnf-ran/hub-deploy/telcov10n-functional-cnf-ran-hub-deploy-commands.sh
@@ -74,7 +74,7 @@ fi
 cd /eco-ci-cd
 
 echo "Running deploy-ocp-sno for ${CLUSTER_NAME} (version=${VERSION})"
-EXTRA_VARS="release=${VERSION} cluster_name=${CLUSTER_NAME} disconnected=true release_age_max_days=${RELEASE_AGE_MAX_DAYS}"
+EXTRA_VARS="release=${VERSION} cluster_name=${CLUSTER_NAME} disconnected=true release_age_max_days=${MULTISTAGE_PARAM_OVERRIDE_RELEASE_AGE_MAX_DAYS}"
 if [ "${DISABLE_INSIGHTS}" = "true" ]; then
   EXTRA_VARS="${EXTRA_VARS} disable_insights=true"
 fi
