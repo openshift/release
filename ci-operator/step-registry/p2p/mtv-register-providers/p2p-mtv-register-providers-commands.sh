d52c33c1fd3dd8d015225f357674ce870a8575e6
shakyav <vshakya@redhat.com>
2026-09-11 16:24:22 -0400

    chore: implement review suggestion

--- ci-operator/step-registry/p2p/mtv-register-providers/p2p-mtv-register-providers-commands.sh
+++ ci-operator/step-registry/p2p/mtv-register-providers/p2p-mtv-register-providers-commands.sh
@@ -8,7 +8,7 @@ set -euxo pipefail; shopt -s inherit_errexit
 eval "$(
     typeset -a _fURL=()
     type -t wget 1>/dev/null && _fURL=(wget -nv -O-) || _fURL=(curl -fsSL)
-    "${_fURL[@]}" https://raw.githubusercontent.com/RedHatQE/OpenShift-LP-QE--Tools/refs/heads/main/libs/bash/common/EnsureReqs.sh
+    "${_fURL[@]}" https://raw.githubusercontent.com/RedHatQE/OpenShift-LP-QE--Tools/f63f1f606b1d76f6ef2a3e78b4ec1ad7362d4fac/libs/bash/common/EnsureReqs.sh
 )"; EnsureReqs jq
 
 if [[ -n "${SHARED_DIR}" && -s "${SHARED_DIR}/proxy-conf.sh" ]]; then
