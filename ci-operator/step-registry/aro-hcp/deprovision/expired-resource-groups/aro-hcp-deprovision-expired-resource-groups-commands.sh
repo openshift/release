b61a8965eccd6b62635a7a0340e9406f3287e730
Danilo Gemoli <dgemoli@redhat.com>
2026-09-14 12:26:19 +0200

    wip2

--- ci-operator/step-registry/aro-hcp/deprovision/expired-resource-groups/aro-hcp-deprovision-expired-resource-groups-commands.sh
+++ ci-operator/step-registry/aro-hcp/deprovision/expired-resource-groups/aro-hcp-deprovision-expired-resource-groups-commands.sh
@@ -8,7 +8,7 @@ export CLUSTER_PROFILE_DIR="/var/run/aro-hcp-${VAULT_SECRET_PROFILE}"
 
 # Tracing is disabled while the client-secret is read and used, otherwise xtrace
 # expands it into the publicly readable build log.
-set +o xtrace
+# set +o xtrace
 export AZURE_CLIENT_ID; AZURE_CLIENT_ID=$(cat "${CLUSTER_PROFILE_DIR}/client-id")
 export AZURE_TENANT_ID; AZURE_TENANT_ID=$(cat "${CLUSTER_PROFILE_DIR}/tenant")
 export AZURE_CLIENT_SECRET; AZURE_CLIENT_SECRET=$(cat "${CLUSTER_PROFILE_DIR}/client-secret")
