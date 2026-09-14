4c42df3663a444d9877bd1676a6e8669d0d3790a
Jakub Guzik <jguzik@redhat.com>
2026-09-14 19:41:40 +0200

    Prevent credentials from being written to Prow logs (#85135)
    Signed-off-by: Jakub Guzik <jguzik@redhat.com>

--- ci-operator/step-registry/cucushift/installer/rehearse/aws/cases/multi-cidr/provision/cucushift-installer-rehearse-aws-cases-multi-cidr-provision-commands.sh
+++ ci-operator/step-registry/cucushift/installer/rehearse/aws/cases/multi-cidr/provision/cucushift-installer-rehearse-aws-cases-multi-cidr-provision-commands.sh
@@ -543,7 +543,7 @@ mkdir -p "${XDG_RUNTIME_DIR}"
 # to make "oc registry login" interact with the build farm, set KUBECONFIG to empty,
 # so that the credentials of the build farm registry can be saved in docker client config file.
 # A direct connection is required while communicating with build-farm, instead of through proxy
-KUBECONFIG="" oc --loglevel=8 registry login
+KUBECONFIG="" oc registry login
 ocp_version=$(oc adm release info ${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE} --output=json | jq -r '.metadata.version' | cut -d. -f 1,2)
 echo "OCP Version: $ocp_version"
 
