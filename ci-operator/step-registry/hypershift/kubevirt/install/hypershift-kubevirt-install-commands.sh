56374f915ae41be9f97674c00dbc901ea907904f
Martin Gencur <mgencur@redhat.com>
2026-09-15 10:39:49 +0200

    Remove failing curl to Prow due to authentication issues

--- ci-operator/step-registry/hypershift/kubevirt/install/hypershift-kubevirt-install-commands.sh
+++ ci-operator/step-registry/hypershift/kubevirt/install/hypershift-kubevirt-install-commands.sh
@@ -28,19 +28,7 @@ function add_pullsecret() {
   rm /tmp/.dockerconfigjson /tmp/.dockerconfigjson.new
 }
 
-
-# Get yq tool
-YQ="/tmp/yq"
-curl -L -o ${YQ} https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
-chmod +x ${YQ}
-
-# Dynamically get CNV catalog image and channel that were provided to the job via gangway API
-CNV_PRERELEASE_CATALOG_IMAGE=$(curl -s https://prow.ci.openshift.org/prowjob?prowjob="${PROW_JOB_ID}" |\
-  ${YQ} e '.spec.pod_spec.containers[0].env[] | select(.name == "CNV_PRERELEASE_CATALOG_IMAGE") | .value')
-CNV_SUBSCRIPTION_CHANNEL=$(curl -s https://prow.ci.openshift.org/prowjob?prowjob="${PROW_JOB_ID}" |\
-  ${YQ} e '.spec.pod_spec.containers[0].env[] | select(.name == "CNV_CHANNEL") | .value')
-
-if [ "${CNV_SUBSCRIPTION_SOURCE}" == "redhat-operators" ]
+if [ "${CNV_SUBSCRIPTION_SOURCE:-}" == "redhat-operators" ]
 then
   CNV_RELEASE_CHANNEL=stable
 elif [ -n "${CNV_PRERELEASE_CATALOG_IMAGE}" ] && [ -n "${CNV_SUBSCRIPTION_CHANNEL}" ]
